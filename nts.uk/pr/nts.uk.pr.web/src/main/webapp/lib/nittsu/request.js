2a1365307f5e02cc802905866b1e6ce1c67ead2e
m_kitahira <m_kitahira@nittsusystem.co.jp>
2016-11-03 18:43:19 +0700

    developing request.ts

---
+++ nts.uk/pr/nts.uk.pr.web/src/main/webapp/lib/nittsu/request.js
@@ -0,0 +1,96 @@
+﻿var nts;
+(function (nts) {
+    (function (uk) {
+        (function (request) {
+            var QueryString = (function () {
+                function QueryString() {
+                    this.items = {};
+                }
+                QueryString.parseUrl = function (url) {
+                    var instance = new QueryString();
+                    var queryString = url.split('?')[1];
+                    if (queryString) {
+                        var queryEntries = queryString.split('&');
+                        for (var i = 0; i < queryEntries.length; i++) {
+                            var entryParts = queryEntries[i].split('=');
+                            instance.set(entryParts[0], entryParts[1]);
+                        }
+                    }
+                    return instance;
+                };
+
+                QueryString.build = function (entriesObj) {
+                    var instance = new QueryString();
+
+                    for (var key in entriesObj) {
+                        instance.set(key, entriesObj[key]);
+                    }
+
+                    return instance;
+                };
+
+                QueryString.prototype.get = function (key) {
+                    return this.items[key];
+                };
+
+                QueryString.prototype.set = function (key, value) {
+                    if (key === null || key === undefined || key === '') {
+                        return;
+                    }
+                    this.items[key] = value;
+                };
+
+                QueryString.prototype.remove = function (key) {
+                    delete this.items[key];
+                };
+
+                QueryString.prototype.mergeFrom = function (otherObj) {
+                    for (var otherKey in otherObj.items) {
+                        this.set(otherKey, otherObj.items[otherKey]);
+                    }
+                };
+
+                QueryString.prototype.count = function () {
+                    var count = 0;
+                    for (var key in this.items) {
+                        count++;
+                    }
+                    return count;
+                };
+
+                QueryString.prototype.hasItems = function () {
+                    return this.count() !== 0;
+                };
+
+                QueryString.prototype.serialize = function () {
+                    var entryStrings = [];
+                    for (var key in this.items) {
+                        entryStrings.push(key + '=' + this.items[key]);
+                    }
+
+                    return entryStrings.join('&');
+                };
+                return QueryString;
+            })();
+            request.QueryString = QueryString;
+
+            var Locator = (function () {
+                function Locator(url) {
+                    this.rawUrl = url;
+                    this.queryString = QueryString.parseUrl(url);
+                }
+                Locator.prototype.serialize = function () {
+                    if (this.queryString.hasItems()) {
+                        return this.rawUrl + '?' + this.queryString.serialize();
+                    } else {
+                        return this.rawUrl;
+                    }
+                };
+                return Locator;
+            })();
+            request.Locator = Locator;
+        })(uk.request || (uk.request = {}));
+        var request = uk.request;
+    })(nts.uk || (nts.uk = {}));
+    var uk = nts.uk;
+})(nts || (nts = {}));
---
+++ nts.uk/pr/nts.uk.pr.web/src/main/webapp/lib/nittsu/request.ts
@@ -0,0 +1,96 @@
+﻿module nts.uk.request {
+
+    export class QueryString {
+
+        items: { [key: string]: any };
+
+        constructor() {
+            this.items = {};
+        }
+
+        static parseUrl(url: string): QueryString {
+            var instance = new QueryString();
+            var queryString = url.split('?')[1];
+            if (queryString) {
+                var queryEntries = queryString.split('&');
+                for (var i = 0; i < queryEntries.length; i++) {
+                    var entryParts = queryEntries[i].split('=');
+                    instance.set(entryParts[0], entryParts[1]);
+                }
+            }
+            return instance;
+        }
+
+        static build(entriesObj: { [key: string]: any }) {
+            var instance = new QueryString();
+
+            for (var key in entriesObj) {
+                instance.set(key, entriesObj[key]);
+            }
+
+            return instance;
+        }
+
+        get(key: string): any {
+            return this.items[key];
+        }
+
+        set(key: string, value: any) {
+            if (key === null || key === undefined || key === '') {
+                return;
+            }
+            this.items[key] = value;
+        }
+
+        remove(key: string) {
+            delete this.items[key];
+        }
+
+        mergeFrom(otherObj: QueryString) {
+            for (var otherKey in otherObj.items) {
+                this.set(otherKey, otherObj.items[otherKey]);
+            }
+        }
+
+        count() {
+            var count = 0;
+            for (var key in this.items) {
+                count++;
+            }
+            return count;
+        }
+
+        hasItems() {
+            return this.count() !== 0;
+        }
+
+        serialize() {
+            var entryStrings = [];
+            for (var key in this.items) {
+                entryStrings.push(key + '=' + this.items[key]);
+            }
+
+            return entryStrings.join('&');
+        }
+    }
+
+    export class Locator {
+
+        rawUrl: string;
+        queryString: QueryString;
+
+        constructor(url: string) {
+            this.rawUrl = url;
+            this.queryString = QueryString.parseUrl(url);
+        }
+
+        serialize() {
+            if (this.queryString.hasItems()) {
+                return this.rawUrl + '?' + this.queryString.serialize();
+            } else {
+                return this.rawUrl;
+            }
+        }
+    }
+
+}
