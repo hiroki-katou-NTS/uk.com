889bf85b4e3e61810623730c62e952c7ead94c30
m_kitahira <m_kitahira@nittsusystem.co.jp>
2016-11-03 12:50:37 +0700

    switch button component

--- nts.uk/pr/nts.uk.pr.web/src/main/webapp/lib/nittsu/ui/ko-ext.js
+++ nts.uk/pr/nts.uk.pr.web/src/main/webapp/lib/nittsu/ui/ko-ext.js
@@ -5,6 +5,9 @@
             (function (koExtentions) {
                 var validation = nts.uk.ui.validation;
 
+                /**
+                * TextBox
+                */
                 var NtsTextBoxBindingHandler = (function () {
                     function NtsTextBoxBindingHandler() {
                     }
@@ -39,6 +42,97 @@
                     return NtsTextBoxBindingHandler;
                 })();
 
+                /**
+                * Switch button binding handler
+                */
+                var NtsSwitchButtonBindingHandler = (function () {
+                    /**
+                    * Constructor.
+                    */
+                    function NtsSwitchButtonBindingHandler() {
+                    }
+                    /**
+                    * Init.
+                    */
+                    NtsSwitchButtonBindingHandler.prototype.init = function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+                    };
+
+                    /**
+                    * Update
+                    */
+                    NtsSwitchButtonBindingHandler.prototype.update = function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+                        // Get data.
+                        var data = valueAccessor();
+                        var selectedCssClass = 'selected';
+
+                        // Get options.
+                        var options = ko.unwrap(data.options);
+
+                        // Get options value.
+                        var optionValue = ko.unwrap(data.optionsValue);
+                        var optionText = ko.unwrap(data.optionsText);
+                        var selectedValue = ko.unwrap(data.value);
+
+                        // Container.
+                        var container = $(element);
+
+                        // Remove deleted button.
+                        $('button', container).each(function (index, btn) {
+                            var $btn = $(btn);
+                            var btnValue = $(btn).data('swbtn');
+
+                            // Check if btn is contained in options.
+                            var foundFlag = _.findIndex(options, function (opt) {
+                                return opt[optionValue] == btnValue;
+                            }) != -1;
+
+                            if (!foundFlag) {
+                                $btn.remove();
+                                return;
+                            }
+                        });
+
+                        // Start binding new state.
+                        _.forEach(options, function (opt) {
+                            var value = opt[optionValue];
+                            var text = opt[optionText];
+
+                            // Find button.
+                            var targetBtn;
+                            $('button', container).each(function (index, btn) {
+                                var btnValue = $(btn).data('swbtn');
+                                if (btnValue == value) {
+                                    targetBtn = $(btn);
+                                }
+
+                                if (btnValue == selectedValue) {
+                                    $(btn).addClass(selectedCssClass);
+                                } else {
+                                    $(btn).removeClass(selectedCssClass);
+                                }
+                            });
+
+                            if (targetBtn) {
+                                // Do nothing.
+                            } else {
+                                // Recreate
+                                var btn = $('<button>').text(text).addClass('nts-switch-button').data('swbtn', value).on('click', function () {
+                                    var selectedValue = $(this).data('swbtn');
+                                    data.value(selectedValue);
+                                    $('button', container).removeClass(selectedCssClass);
+                                    $(this).addClass(selectedCssClass);
+                                });
+                                if (selectedValue == value) {
+                                    btn.addClass(selectedCssClass);
+                                }
+                                container.append(btn);
+                            }
+                        });
+                    };
+                    return NtsSwitchButtonBindingHandler;
+                })();
+
+                ko.bindingHandlers['ntsSwitchButton'] = new NtsSwitchButtonBindingHandler();
                 ko.bindingHandlers['ntsTextBox'] = new NtsTextBoxBindingHandler();
             })(ui.koExtentions || (ui.koExtentions = {}));
             var koExtentions = ui.koExtentions;
--- nts.uk/pr/nts.uk.pr.web/src/main/webapp/lib/nittsu/ui/ko-ext.ts
+++ nts.uk/pr/nts.uk.pr.web/src/main/webapp/lib/nittsu/ui/ko-ext.ts
@@ -2,6 +2,9 @@
 
     import validation = nts.uk.ui.validation;
 
+    /**
+     * TextBox
+     */
     class NtsTextBoxBindingHandler implements KnockoutBindingHandler {
 
         constraint: validation.CharType;
@@ -38,5 +41,96 @@
         }
     }
 
+    /**
+     * Switch button binding handler
+     */
+    class NtsSwitchButtonBindingHandler implements KnockoutBindingHandler {
+        /**
+         * Constructor.
+         */
+        constructor() {
+        }
+
+        /**
+         * Init.
+         */
+        init(element: any, valueAccessor: () => any, allBindingsAccessor: () => any, viewModel: any, bindingContext: KnockoutBindingContext): void {
+        }
+
+        /**
+         * Update
+         */
+        update(element: any, valueAccessor: () => any, allBindingsAccessor: () => any, viewModel: any, bindingContext: KnockoutBindingContext): void {
+            // Get data.
+            var data = valueAccessor();
+            var selectedCssClass = 'selected';
+            // Get options.
+            var options: Array<any> = ko.unwrap(data.options);
+
+            // Get options value.
+            var optionValue = ko.unwrap(data.optionsValue);
+            var optionText = ko.unwrap(data.optionsText);
+            var selectedValue = ko.unwrap(data.value);
+            // Container.
+            var container = $(element);
+
+            // Remove deleted button.
+            $('button', container).each(function (index, btn) {
+                var $btn = $(btn);
+                var btnValue = $(btn).data('swbtn');
+                // Check if btn is contained in options.
+                var foundFlag = _.findIndex(options, function (opt) {
+                    return opt[optionValue] == btnValue;
+                }) != -1;
+
+                if (!foundFlag) {
+                    $btn.remove();
+                    return;
+                }
+            })
+
+        // Start binding new state.
+        _.forEach(options, function (opt) {
+                var value = opt[optionValue];
+                var text = opt[optionText];
+
+                // Find button.
+                var targetBtn: JQuery;
+                $('button', container).each(function (index, btn) {
+                    var btnValue = $(btn).data('swbtn');
+                    if (btnValue == value) {
+                        targetBtn = $(btn);
+                    }
+
+                    if (btnValue == selectedValue) {
+                        $(btn).addClass(selectedCssClass);
+                    } else {
+                        $(btn).removeClass(selectedCssClass);
+                    }
+                })
+
+                if (targetBtn) {
+                    // Do nothing.
+                } else {
+                    // Recreate
+                    var btn = $('<button>').text(text)
+                        .addClass('nts-switch-button')
+                        .data('swbtn', value)
+                        .on('click', function () {
+                            var selectedValue = $(this).data('swbtn');
+                            data.value(selectedValue);
+                            $('button', container).removeClass(selectedCssClass);
+                            $(this).addClass(selectedCssClass);
+                        })
+                    if (selectedValue == value) {
+                        btn.addClass(selectedCssClass);
+                    }
+                    container.append(btn);
+                }
+            })
+        }
+    }
+
+    ko.bindingHandlers['ntsSwitchButton'] = new NtsSwitchButtonBindingHandler();
     ko.bindingHandlers['ntsTextBox'] = new NtsTextBoxBindingHandler();
 }
