d9ee9584f35d9fe0f79fcee33a692769c175b32a
NSVC\hoanld <hoan_ld@nittsusystem-vn.com>
2016-11-03 16:50:05 +0700

    Add JAX-RS Application path "/rest"

---
+++ nts.uk/pr/proto/nts.uk.pr.proto.ws/src/main/java/nts/uk/ctx/pr/proto/ws/ExampleApp.java
@@ -0,0 +1,8 @@
+package nts.uk.ctx.pr.proto.ws;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+@ApplicationPath("/rest")
+public class ExampleApp extends Application{
+
+}
