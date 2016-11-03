3db63e2986e900f9bc0f63133b83b547cf1d23de
m_kitahira <m_kitahira@nittsusystem.co.jp>
2016-11-03 19:12:03 +0700

    add WebApiSetting

--- nts.uk/pr/proto/nts.uk.pr.proto.ws/src/main/java/nts/uk/ctx/pr/proto/ws/ExampleApp.java
+++
@@ -1,8 +0,0 @@
-package nts.uk.ctx.pr.proto.ws;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-@ApplicationPath("/rest")
-public class ExampleApp extends Application{
-
-}
