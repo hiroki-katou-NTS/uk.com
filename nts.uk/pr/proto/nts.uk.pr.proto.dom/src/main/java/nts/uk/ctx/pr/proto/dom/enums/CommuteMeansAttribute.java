b7dc466910c9795a28c47579f18239b46b9e8275
Bùi Chình <chinh_bv@nittsusystem-vn.com>
2016-11-08 17:24:30 +0700

    move enum class to package enum

--- nts.uk/pr/proto/nts.uk.pr.proto.dom/src/main/java/nts/uk/ctx/pr/proto/dom/enums/CommuteMeansAttribute.java
+++ nts.uk/pr/proto/nts.uk.pr.proto.dom/src/main/java/nts/uk/ctx/pr/proto/dom/enums/CommuteMeansAttribute.java
@@ -1,4 +1,4 @@
-package nts.uk.ctx.pr.proto.dom.paymentcreatedata;
+package nts.uk.ctx.pr.proto.dom.enums;
 
 /**
  * Enum: commute means attribute
--- nts.uk/pr/proto/nts.uk.pr.proto.dom/src/main/java/nts/uk/ctx/pr/proto/dom/paymentcreatedata/PersonalEmploymentContract.java
+++ nts.uk/pr/proto/nts.uk.pr.proto.dom/src/main/java/nts/uk/ctx/pr/proto/dom/paymentcreatedata/PersonalEmploymentContract.java
@@ -2,6 +2,7 @@ package nts.uk.ctx.pr.proto.dom.paymentcreatedata;
 
 import lombok.Getter;
 import nts.arc.layer.dom.AggregateRoot;
+import nts.uk.ctx.pr.proto.dom.enums.PayrollSystem;
 
 public class PersonalEmploymentContract extends AggregateRoot {
 	
