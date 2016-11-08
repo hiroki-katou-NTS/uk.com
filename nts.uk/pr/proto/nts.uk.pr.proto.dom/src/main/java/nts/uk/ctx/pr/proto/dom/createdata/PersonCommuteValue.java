fd82f7b4d853261a5aacfe0ea852029cbe510519
Bùi Chình <chinh_bv@nittsusystem-vn.com>
2016-11-08 17:00:31 +0700

    payroll create data: update value object person commute Value

--- nts.uk/pr/proto/nts.uk.pr.proto.dom/src/main/java/nts/uk/ctx/pr/proto/dom/createdata/PersonCommuteValue.java
+++ nts.uk/pr/proto/nts.uk.pr.proto.dom/src/main/java/nts/uk/ctx/pr/proto/dom/createdata/PersonCommuteValue.java
@@ -1,13 +1,39 @@
 package nts.uk.ctx.pr.proto.dom.createdata;
 
+import lombok.Getter;
+
 /**
  * Value Object: Person commute 
  *
  */
 public class PersonCommuteValue {
+	/**
+	 * Commute cycle
+	 */
+	@Getter
 	private String commuteCycle;
+	
+	/**
+	 * Commute allowance
+	 */
+	@Getter
 	private long commuteAllowance;
+	
+	/**
+	 * Commute means attribute
+	 */
+	@Getter
 	private CommuteMeansAttribute commuteMeansAttribute;
+	
+	/**
+	 * Payroll start date
+	 */
+	@Getter
 	private long payStartDate;
+	
+	/**
+	 * User or Not
+	 */
+	@Getter
 	private UserOrNot userOrNot;
 }
