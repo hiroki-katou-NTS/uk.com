1e507705207fd9e1661909d9b19cda59729ea224
1406-V0012\dudt <du_dt@nittsusystem-vn.comdu_dt@nittsusystem-vn.com>
2016-11-08 16:55:39 +0700

    DuDT: create domain LayoutMasterDetail

---
+++ nts.uk/pr/proto/nts.uk.pr.proto.dom/src/main/java/nts/uk/ctx/pr/proto/dom/layoutmasterdetail/CategoryAtr.java
@@ -0,0 +1,18 @@
+package nts.uk.ctx.pr.proto.dom.layoutmasterdetail;
+/**
+ * 
+ * カテゴリ区分
+ *
+ */
+public enum CategoryAtr {
+	//0:支給
+	PAYMENT,
+	//1:控除
+	DEDUCTION,
+	//2:勤怠
+	DILIGENCE,
+	//3:記事
+	ARTICLES,
+	//9:その他
+	OTHER
+}
---
+++ nts.uk/pr/proto/nts.uk.pr.proto.dom/src/main/java/nts/uk/ctx/pr/proto/dom/layoutmasterdetail/ItemCode.java
@@ -0,0 +1,19 @@
+package nts.uk.ctx.pr.proto.dom.layoutmasterdetail;
+
+import nts.uk.shr.com.primitive.CodePrimitiveValue;
+
+public class ItemCode extends CodePrimitiveValue<ItemCode>{
+	/**
+	 * Constructor
+	 * @param rawValue
+	 */
+	public ItemCode(String rawValue) {
+		super(rawValue);		
+	}
+
+	/**
+	 * serialVersionUID
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
