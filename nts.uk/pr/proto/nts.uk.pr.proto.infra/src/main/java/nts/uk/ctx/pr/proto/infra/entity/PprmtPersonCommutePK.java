3d868b1f459fe793863d6a7ff749abf8fcfea8a8
NSVC\hungnm <hungnm910@gmail.com>
2016-11-09 14:36:02 +0700

    Adding entity classes

---
+++ nts.uk/pr/proto/nts.uk.pr.proto.infra/src/main/java/nts/uk/ctx/pr/proto/infra/entity/PprmtPersonCommutePK.java
@@ -0,0 +1,26 @@
+package nts.uk.ctx.pr.proto.infra.entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class PprmtPersonCommutePK implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name = "CCD")
+	public String ccd;
+	
+	@Column(name = "PID")
+	public String pId;
+	
+	@Column(name = "STR_YM")
+	public BigDecimal strYm;
+
+}
