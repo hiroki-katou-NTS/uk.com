dde913c32fb4f810cb7a9b4622d08995ab67fe8e
m_kitahira <m_kitahira@nittsusystem.co.jp>
2016-11-10 16:17:24 +0700

    remove unnecessary d.ts files

--- nts.uk/pr/nts.uk.pr.web/src/main/webapp/lib/nittsu/nts.uk.d.ts
+++
@@ -1,9 +0,0 @@
-﻿declare var __viewContext: nts.uk.ViewContext;
-
-declare module nts.uk {
-    interface ViewContext {
-        primitiveValueConstraints: { [key: string]: nts.uk.ui.validation.PrimitiveValueConstraint };
-    }
-
-    
-}
--- nts.uk/pr/nts.uk.pr.web/src/main/webapp/lib/nittsu/ui/ko-ext.d.ts
+++
@@ -1,2 +0,0 @@
-declare module nts.uk.ui.koExtentions {
-}
