8ac91469dd6f2542e6f79ac130529bd21357f572
NSVC\hungnm <hungnm910@gmail.com>
2016-11-14 16:45:42 +0700

    add domain repository class

--- nts.uk/pr/proto/nts.uk.pr.proto.dom/src/main/java/nts/uk/ctx/pr/proto/dom/paymentdata/PersonalEmploymentContract.java
+++ nts.uk/pr/proto/nts.uk.pr.proto.dom/src/main/java/nts/uk/ctx/pr/proto/dom/paymentdata/PersonalEmploymentContract.java
@@ -1,7 +1,10 @@
 package nts.uk.ctx.pr.proto.dom.paymentdata;
 
+import java.util.Date;
+
 import lombok.Getter;
 import nts.arc.layer.dom.AggregateRoot;
+import nts.uk.ctx.core.dom.company.CompanyCode;
 import nts.uk.shr.com.primitive.PersonId;
 
 /**
@@ -21,11 +24,35 @@ public class PersonalEmploymentContract extends AggregateRoot {
 	 */
 	@Getter
 	private PersonId personId;
+	
+	/**
+	 * Company Code
+	 */
+	@Getter
+	private CompanyCode companyCode;
+	
+	/**
+	 * Start Date
+	 */
+	@Getter 
+	private Date strD;
 
-	public PersonalEmploymentContract(PayrollSystem payrollSystem, PersonId personId) {
+	/**
+	 * End Date
+	 */
+	@Getter 
+	private Date endD;
+
+	
+
+	public PersonalEmploymentContract(PayrollSystem payrollSystem, PersonId personId, CompanyCode companyCode,
+			Date strD, Date endD) {
 		super();
 		this.payrollSystem = payrollSystem;
 		this.personId = personId;
+		this.companyCode = companyCode;
+		this.strD = strD;
+		this.endD = endD;
 	}
 
 	/**
