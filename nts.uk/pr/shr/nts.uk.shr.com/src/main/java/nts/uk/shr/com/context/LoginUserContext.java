5af47cf12ae71ad501f164c2d7e0df0db4eaa4dd
m_kitahira <m_kitahira@nittsusystem.co.jp>
2016-11-15 15:35:49 +0700

    add AppContexts

---
+++ nts.uk/pr/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/com/context/LoginUserContext.java
@@ -0,0 +1,12 @@
+package nts.uk.shr.com.context;
+
+public class LoginUserContext {
+
+	public String companyCode() {
+		return "1234";
+	}
+	
+	public String employeeCode() {
+		return "1234567890AB";
+	}
+}
