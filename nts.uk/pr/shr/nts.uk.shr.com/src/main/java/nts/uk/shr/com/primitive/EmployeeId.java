753f473943905a23485f19a01946d9813c45cb73
hoanld <hoan_ld@nittsusystem-vn.com>
2016-11-15 10:47:35 +0700

    add sample draft class with Validation annotation

---
+++ nts.uk/pr/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/com/primitive/EmployeeId.java
@@ -0,0 +1,29 @@
+package nts.uk.shr.com.primitive;
+
+import nts.arc.primitive.constraint.CharType;
+import nts.arc.primitive.constraint.StringCharType;
+import nts.arc.primitive.constraint.StringMaxLengh;
+import nts.gul.text.IdentifierUtil;
+
+@StringMaxLengh(36)
+@StringCharType(CharType.ALPHA_NUMERIC)
+public class EmployeeId extends CodePrimitiveValue<EmployeeId>{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+    
+	public EmployeeId(String rawValue) {
+		super(rawValue);
+	}
+	
+	/**
+     * create new UserId instance
+     * 
+     * @return new UserId instance
+     */
+    public static EmployeeId newPersonId() {
+        return new EmployeeId(IdentifierUtil.randomUniqueId());
+    }
+}
