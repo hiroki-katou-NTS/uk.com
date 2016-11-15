488b90173783ae2755d83f4a798a660700c8cb45
unknown <dungvd@1406-V0005.nsvc.com>
2016-11-15 14:35:10 +0700

    Update

--- nts.uk/pr/proto/nts.uk.pr.proto.dom/src/main/java/nts/uk/ctx/pr/proto/dom/paymentdata/repository/PersonalPaymentSettingRepository.java
+++ nts.uk/pr/proto/nts.uk.pr.proto.dom/src/main/java/nts/uk/ctx/pr/proto/dom/paymentdata/repository/PersonalPaymentSettingRepository.java
@@ -2,10 +2,11 @@ package nts.uk.ctx.pr.proto.dom.paymentdata.repository;
 
 import java.util.Optional;
 
+import nts.arc.time.YearMonth;
 import nts.uk.ctx.pr.proto.dom.paymentdata.paymentsetting.PersonalPaymentSetting;
 
 public interface PersonalPaymentSettingRepository {
 
-	Optional<PersonalPaymentSetting> find(String companyCode, Integer personId);
+	Optional<PersonalPaymentSetting> find(String companyCode, Integer personId, YearMonth baseYm);
 
 }
---
+++ nts.uk/pr/proto/nts.uk.pr.proto.infra/src/main/java/nts/uk/ctx/pr/proto/infra/repository/paymentdata/JpaPersonalPaymentSettingRepository.java
@@ -0,0 +1,5 @@
+package nts.uk.ctx.pr.proto.infra.repository.paymentdata;
+
+public class JpaPersonalPaymentSettingRepository {
+
+}
