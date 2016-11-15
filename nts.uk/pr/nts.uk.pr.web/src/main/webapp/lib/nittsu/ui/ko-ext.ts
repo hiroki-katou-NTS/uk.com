1cec3e094714a64efa0480479efd766c7aa44136
NSVC\lamdt <lam_dt@nittsusystem-vn.com>
2016-11-15 12:46:16 +0700

    Dialog Component v1.2

--- nts.uk/pr/nts.uk.pr.web/src/main/webapp/lib/nittsu/ui/ko-ext.ts
+++ nts.uk/pr/nts.uk.pr.web/src/main/webapp/lib/nittsu/ui/ko-ext.ts
@@ -96,41 +96,56 @@
         update(element: any, valueAccessor: () => any, allBindingsAccessor: () => any, viewModel: any, bindingContext: KnockoutBindingContext): void {
             // Get data.
             var data = valueAccessor();
+            var dialogtype: string = ko.unwrap(data.dialogtype);
             var title: string = ko.unwrap(data.title);
             var message: string = ko.unwrap(data.message);
-            var okButtonColor: string = ko.unwrap(data.okButtonColor);
-            var buttonSize: string = ko.unwrap(data.buttonSize);
             var modal: boolean = ko.unwrap(data.modal);
             var show: boolean = ko.unwrap(data.show);
-
+            var okButtonColor: string = ko.unwrap(data.okButtonColor);
+            var okButtonText: string = ko.unwrap(data.okButtonText);
+            var cancelButtonText: string = ko.unwrap(data.cancelButtonText);
+            var buttonSize: string = ko.unwrap(data.buttonSize);
+            
             var $dialog = $("<div id='ntsDialog'></div>");
             if(show == true) {
                 $('body').append($dialog);
-                $dialog.dialog({
-                    title: title,
-                    modal: modal,
-                    closeOnEscape: false,
-                    buttons: [{
-                        text: "はい",
-                        "class": "yes " + buttonSize + " " + okButtonColor,
-                        click: function(){
-                            bindingContext.$data.okButtonCommand();
-                            $(this).dialog("close");
-                        }
-                    },
-                    {
-                        text: "いいえ",
+                // Create Buttons
+                var buttons = [];
+                var okButton = {
+                    text: okButtonText,
+                    "class": "yes " + buttonSize + " " + okButtonColor,
+                    click: function(){
+                        bindingContext.$data.okButtonCommand();
+                        $(this).dialog("close");
+                    }
+                };
+                
+                // Create default values foreach DialogType
+                if(dialogtype === "confirm") {
+                    okButton.text = okButtonText || "はい";
+                    buttons.push(okButton);
+                    buttons.push({
+                        text: cancelButtonText || "いいえ",
                         "class": "no " + buttonSize,
                         click: function(){
                             bindingContext.$data.cancelButtonCommand();
                             $(this).dialog("close");
                         }
-                    }],
+                    });
+                }
+                else {
+                    okButton.text = okButtonText || "OK";
+                    buttons.push(okButton);
+                }
+                $dialog.dialog({
+                    title: title,
+                    modal: modal,
+                    closeOnEscape: false,
+                    buttons: buttons,
                     open: function () {
-                        $(this).closest('.ui-dialog').css('z-index', 120001).find('.ui-dialog-titlebar-close').hide();
-                        $(this).find('button.yes').focus();
+                        $(this).parent().find('.ui-dialog-buttonset > button.yes').focus();
+                        $(this).parent().find('.ui-dialog-buttonset > button').removeClass('ui-button ui-corner-all ui-widget');
                         $('.ui-widget-overlay').last().css('z-index', 120000);
-                        
                     },
                     close: function (event) {
                         bindingContext.$data.show(false);
@@ -138,7 +153,7 @@
                 }).text(message);
             }
             else {
-                if($('#ntsDialog').dialog( "instance" ) != null)
+                if($('#ntsDialog').dialog("instance") != null)
                     $('#ntsDialog').dialog("destroy");
                 $('#ntsDialog').remove();
             }
