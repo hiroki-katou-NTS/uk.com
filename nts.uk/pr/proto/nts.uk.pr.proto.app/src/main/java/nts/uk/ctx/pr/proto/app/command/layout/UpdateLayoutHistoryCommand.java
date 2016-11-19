89febe106ef0575de6e8b0273512a6f4b741748d
Vu Tien Lam <lam_vt@nittsusystem-vn.com>
2016-11-19 09:55:53 +0700

    change construct of packages

---
+++ nts.uk/pr/proto/nts.uk.pr.proto.app/src/main/java/nts/uk/ctx/pr/proto/app/command/layout/UpdateLayoutHistoryCommand.java
@@ -0,0 +1,19 @@
+package nts.uk.ctx.pr.proto.app.command.layout;
+
+import lombok.Getter;
+import nts.uk.ctx.pr.proto.dom.layout.LayoutMaster;
+import nts.uk.shr.com.context.AppContexts;
+
+@Getter
+public class UpdateLayoutHistoryCommand {
+	
+	private int startYmNew;
+	//Giá trị startYm trước khi nó được sửa
+	private int startYmOriginal;
+	private String stmtCode;
+
+	public LayoutMaster toDomain(int endYm, int layoutAtr, String stmtName){
+		return LayoutMaster.createFromJavaType(
+				AppContexts.user().companyCode(), this.startYmNew, this.stmtCode, endYm, layoutAtr, stmtName);
+	}
+}
---
+++ nts.uk/pr/proto/nts.uk.pr.proto.app/src/main/java/nts/uk/ctx/pr/proto/app/find/layout/line/LayoutMasterLineFinder.java
@@ -0,0 +1,24 @@
+package nts.uk.ctx.pr.proto.app.find.layout.line;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+
+import nts.uk.ctx.pr.proto.dom.layout.line.LayoutMasterLineRepository;
+
+@RequestScoped
+public class LayoutMasterLineFinder {
+	@Inject
+	private LayoutMasterLineRepository repository;
+	
+	public List<LayoutMasterLineDto> getLines(
+			String companyCd,
+			String stmtCd,
+			int startYm){
+		return this.repository.getLines(companyCd, stmtCd, startYm).stream()
+				.map(item -> LayoutMasterLineDto.fromDomain(item))
+				.collect(Collectors.toList());
+	}
+}
