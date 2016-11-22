24f8bb4468c70612c5639aa751e819a08721dc47
Bùi Chình <chinh_bv@nittsusystem-vn.com>
2016-11-22 17:30:06 +0700

    add ui qpp004/a

---
+++ nts.uk/pr/nts.uk.pr.web/src/main/webapp/view/qpp/004/a/service.ts
@@ -0,0 +1,53 @@
+module qpp004.service {
+    var paths = {
+        getPaymentDateProcessing: "pr/proto/paymentdatemaster/processing/find"
+    }
+    
+    /**
+     * Get list payment date processing.
+     */
+    export function getPaymentDateProcessingMaster(): JQueryPromise<Array<PaymentDateProcessingMasterModel>> {
+        var dfd = $.Deferred<Array<any>>();
+        nts.uk.request.ajax(paths.getPaymentDateProcessing)
+            .done(function(res: Array<any>){
+                dfd.resolve(res);
+            })
+            .fail(function(res) {
+                dfd.reject(res);
+            })
+        return dfd.promise(); 
+    }
+    
+    
+    
+    /**
+     * PaymentDateProcessingMasterModel.
+     * Contain enum define information.
+     */
+    export interface PaymentDateProcessingMasterModel {
+        /**
+         * Payroll bonus attribute
+         */
+        payBonusAttribute: number;
+    
+        /**
+         * Processing Number.
+         */
+        processingNo: number;
+    
+        /**
+         * Processing Name.
+         */
+        processingName: string;
+    
+        /**
+         * Current processing year month.
+         */
+        currentProcessingYm: number;
+    
+        /**
+         * Display attribute.
+         */
+        displayAttribute: number;
+    }
+}
