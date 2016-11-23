e636f244a04fe5d5fbdcc5eb7395894402d32228
DuDT <du_dt@nittsusystem-vn.comdu_dt@nittsusystem-vn.com>
2016-11-23 15:37:41 +0700

    update

--- nts.uk/pr/proto/nts.uk.pr.proto.ws/src/main/java/nts/uk/ctx/pr/proto/ws/layout/LayoutWebService.java
+++ nts.uk/pr/proto/nts.uk.pr.proto.ws/src/main/java/nts/uk/ctx/pr/proto/ws/layout/LayoutWebService.java
@@ -1,8 +1,11 @@
 package nts.uk.ctx.pr.proto.ws.layout;
 
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import nts.uk.ctx.pr.proto.app.command.layout.CreateLayoutCommand;
@@ -13,10 +16,12 @@ import nts.uk.ctx.pr.proto.app.command.layout.DeleteLayoutHistoryCommand;
 import nts.uk.ctx.pr.proto.app.command.layout.DeleteLayoutHistoryCommandHandler;
 import nts.uk.ctx.pr.proto.app.command.layout.UpdateLayoutHistoryCommand;
 import nts.uk.ctx.pr.proto.app.command.layout.UpdateLayoutHistoryCommandHandler;
+import nts.uk.ctx.pr.proto.app.find.layout.LayoutDto;
+import nts.uk.ctx.pr.proto.app.find.layout.LayoutMasterFinder;
 
-@Path("pr/proto/layoutMaster")
+@Path("pr/proto/layout")
 @Produces("application/json")
-public class LayoutDataWebService {
+public class LayoutWebService {
 	@Inject
 	private CreateLayoutCommandHandler createLayoutData;
 	@Inject
@@ -25,24 +30,46 @@ public class LayoutDataWebService {
 	private UpdateLayoutHistoryCommandHandler updateData;
 	@Inject
 	private DeleteLayoutHistoryCommandHandler deleteData;
+	@Inject
+	private LayoutMasterFinder find;
+	
+	@POST
+	@Path("findAllLayout/{companyCode}")
+	public List<LayoutDto> getAllLayout(@PathParam("companyCode") String companyCode){
+		return this.find.getAllLayout(companyCode);		
+	}
+	@POST
+	@Path("findLayout/{companyCode, stmtCode, startYm}")
+	public LayoutDto getLayout(@PathParam("companyCode") String companyCode,
+			@PathParam("stmtCode") String stmtCode,
+			@PathParam("startYm") int startYm){
+		return this.find.getLayout(companyCode, stmtCode, startYm).get();
+	}
+	
+	@POST
+	@Path("findAllLayoutWithMaxStartYm/{companyCode}")
+	public List<LayoutDto> getLayoutsWithMaxStartYm(@PathParam("companyCode") String companyCode){
+		return this.find.getLayoutsWithMaxStartYm(companyCode);
+	}
+	
 	@POST
 	@Path("createLayout")
-	private void createLayout(CreateLayoutCommand command){
+	public void createLayout(CreateLayoutCommand command){
 		this.createLayoutData.handle(command);
 	}
 	@POST
 	@Path("createLayoutHistory")
-	private void createLayoutHistory(CreateLayoutHistoryCommand command){
+	public void createLayoutHistory(CreateLayoutHistoryCommand command){
 		this.createHistoryData.handle(command);
 	}
 	@POST
 	@Path("updateData")
-	private void updateData(UpdateLayoutHistoryCommand command){
+	public void updateData(UpdateLayoutHistoryCommand command){
 		this.updateData.handle(command);
 	}
 	@POST
 	@Path("deleteData")
-	private void deleteData(DeleteLayoutHistoryCommand command){
+	public void deleteData(DeleteLayoutHistoryCommand command){
 		this.deleteData.handle(command);
 	}
 }
