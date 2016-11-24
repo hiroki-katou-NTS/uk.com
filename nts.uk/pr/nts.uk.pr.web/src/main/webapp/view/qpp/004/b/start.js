c5634d41c869bdfef82ac7189999a23af2df328c
NSVC\hungnm <hungnm910@gmail.com>
2016-11-24 17:44:38 +0700

    update ui qpp004

--- nts.uk/pr/nts.uk.pr.web/src/main/webapp/view/qpp/004/b/start.js
+++ nts.uk/pr/nts.uk.pr.web/src/main/webapp/view/qpp/004/b/start.js
@@ -1,5 +1,10 @@
 __viewContext.ready(function () {
     var data = this.transferred.get();
-    var screenModel = new qpp004.b.viewmodel.ScreenModel(data);
-    this.bind(screenModel);
+    if (typeof data === 'undefined' || data === null) {
+        nts.uk.request.jump("/view/qpp/004/a/index.xhtml", {});
+    }
+    else {
+        var screenModel = new qpp004.b.viewmodel.ScreenModel(data);
+        this.bind(screenModel);
+    }
 });
