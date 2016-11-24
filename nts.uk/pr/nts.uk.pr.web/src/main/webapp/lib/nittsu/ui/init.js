b9f7be6c1e484012b2999ace17ec5d808e8a4303
NSVC\lamdt <lam_dt@nittsusystem-vn.com>
2016-11-24 08:53:48 +0700

    Error Dialog: add script file default, add VM

--- nts.uk/pr/nts.uk.pr.web/src/main/webapp/lib/nittsu/ui/init.js
+++ nts.uk/pr/nts.uk.pr.web/src/main/webapp/lib/nittsu/ui/init.js
@@ -4,6 +4,7 @@ var nts;
     (function (uk) {
         var ui;
         (function (ui) {
+            var option = nts.uk.ui.option;
             var init;
             (function (init) {
                 var _start;
@@ -11,14 +12,67 @@ var nts;
                     _start = callback;
                 };
                 __viewContext.bind = function (contentViewModel) {
-                    var viewModel = {
-                        content: contentViewModel // developer's view model
+                    ui._viewModel = {
+                        content: contentViewModel,
+                        kiban: new KibanViewModel() // Kiban's view model
                     };
-                    ko.applyBindings(viewModel);
+                    ko.applyBindings(ui._viewModel);
                 };
                 $(function () {
                     _start.call(__viewContext);
                 });
+                // Kiban ViewModel
+                var KibanViewModel = (function () {
+                    function KibanViewModel() {
+                        var self = this;
+                        self.errorDialogViewModel = {
+                            title: ko.observable("Error Dialog title"),
+                            headers: ko.observableArray([
+                                new ErrorHeader("tab", "タブ", 90, true),
+                                new ErrorHeader("location", "エラー箇所", 115, true),
+                                new ErrorHeader("message", "エラー詳細", 250, true)
+                            ]),
+                            errors: ko.observableArray([
+                                { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" }, { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" },
+                                { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" }, { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" },
+                                { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" }, { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" },
+                                { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" }, { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" },
+                                { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" }, { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" },
+                                { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" }, { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" },
+                                { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" }, { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" },
+                                { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" }, { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" },
+                                { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" }, { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" },
+                                { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" }, { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" },
+                                { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" }, { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" },
+                                { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" }, { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" },
+                                { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" }, { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" },
+                                { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" }, { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" },
+                                { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" }, { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" },
+                                { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" }, { tab: "基本情報", location: "メールアドレス", message: "メールアドレスは必須項目です" }
+                            ]),
+                            option: ko.mapping.fromJS(new option.ErrorDialogOption()),
+                            closeButtonClicked: function () { },
+                            open: function () {
+                                var self = this;
+                                self.option.show(true);
+                            },
+                            hide: function () {
+                                var self = this;
+                                self.option.show(false);
+                            }
+                        };
+                    }
+                    return KibanViewModel;
+                }());
+                var ErrorHeader = (function () {
+                    function ErrorHeader(name, text, width, visible) {
+                        this.name = name;
+                        this.text = text;
+                        this.width = width;
+                        this.visible = visible;
+                    }
+                    return ErrorHeader;
+                }());
             })(init || (init = {}));
         })(ui = uk.ui || (uk.ui = {}));
     })(uk = nts.uk || (nts.uk = {}));
--- nts.uk/pr/nts.uk.pr.web/src/main/webapp/lib/nittsu/ui/init.ts
+++ nts.uk/pr/nts.uk.pr.web/src/main/webapp/lib/nittsu/ui/init.ts
@@ -1,6 +1,10 @@
 ﻿module nts.uk.ui {
-    
+     
+    import option = nts.uk.ui.option;
+    export var _viewModel: any;
+
     module init {
+        
         var _start: any;
         
         __viewContext.ready = function (callback: () => void) {
@@ -8,16 +12,74 @@
         };
         
         __viewContext.bind = function (contentViewModel: any) {
-            var viewModel = {
-                content: contentViewModel // developer's view model
-                //kiban: ... kiban's view model
+            _viewModel = {
+                content: contentViewModel, // developer's view model
+                kiban: new KibanViewModel() // Kiban's view model
             };
-            
-            ko.applyBindings(viewModel);
+            ko.applyBindings(_viewModel);
         }
         
         $(function () {
             _start.call(__viewContext);
         });
+        
+        // Kiban ViewModel
+        class KibanViewModel {
+            errorDialogViewModel: any;
+            
+            constructor(){
+                var self = this;
+                self.errorDialogViewModel = {
+                    title: ko.observable("Error Dialog title"),
+                    headers: ko.observableArray([
+                        new ErrorHeader("tab", "タブ", 90, true),
+                        new ErrorHeader("location", "エラー箇所", 115, true),
+                        new ErrorHeader("message", "エラー詳細", 250, true)
+                    ]),
+                    errors: ko.observableArray([
+                        {tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},{tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},
+                        {tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},{tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},
+                        {tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},{tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},
+                        {tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},{tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},
+                        {tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},{tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},
+                        {tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},{tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},
+                        {tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},{tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},
+                        {tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},{tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},
+                        {tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},{tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},
+                        {tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},{tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},
+                        {tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},{tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},
+                        {tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},{tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},
+                        {tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},{tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},
+                        {tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},{tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},
+                        {tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},{tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},
+                        {tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"},{tab: "基本情報",　location: "メールアドレス", message: "メールアドレスは必須項目です"}
+                    ]),
+                    option: ko.mapping.fromJS(new option.ErrorDialogOption()),
+                    closeButtonClicked: function(){},
+                    open: function(){
+                        var self = this;
+                        self.option.show(true);
+                    },
+                    hide: function(){
+                        var self = this;
+                        self.option.show(false);
+                    }
+                }
+            }
+        }
+        
+        class ErrorHeader{
+            name: string;
+            text: string;
+            width: number;
+            visible: boolean;
+            
+            constructor(name:string, text: string, width: number, visible: boolean) {
+                this.name = name;
+                this.text = text;
+                this.width = width;
+                this.visible = visible;
+            }
+        }
     }
 }
