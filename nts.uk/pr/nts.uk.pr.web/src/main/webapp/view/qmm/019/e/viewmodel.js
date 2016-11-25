b4caba86e9fbb46355db59d70ff16d7f721ccc2a
DuDT <du_dt@nittsusystem-vn.comdu_dt@nittsusystem-vn.com>
2016-11-25 18:16:53 +0700

    udate e

--- nts.uk/pr/nts.uk.pr.web/src/main/webapp/view/qmm/019/e/viewmodel.js
+++ nts.uk/pr/nts.uk.pr.web/src/main/webapp/view/qmm/019/e/viewmodel.js
@@ -5,17 +5,59 @@ var qmm019;
         var viewmodel;
         (function (viewmodel) {
             var ScreenModel = (function () {
+                /**
+                 * Init screen model.
+                 */
                 function ScreenModel() {
+                    var self = this;
+                    self.selectedLayoutAtr = ko.observable(null);
+                    self.selectLayoutCode = ko.observable(null);
+                    self.selectLayoutName = ko.observable(null);
+                    self.selectLayoutStartYm = ko.observable(null);
+                    self.selectLayoutEndYm = ko.observable(null);
+                    self.selectLayout = ko.observable(null);
                 }
                 // start function
                 ScreenModel.prototype.start = function () {
                     var self = this;
+                    e.service.getLayout("1", 201606).done(function (layout) {
+                        self.selectLayout(layout);
+                        self.startDiaglog();
+                    }).fail(function (res) {
+                        alert(res);
+                    });
                     var dfd = $.Deferred();
                     dfd.resolve();
                     // Return.
                     return dfd.promise();
                 };
-                ScreenModel.prototype.startDiaglo = function () {
+                ScreenModel.prototype.startDiaglog = function () {
+                    var self = this;
+                    var layout = self.selectLayout();
+                    var code = layout.stmtCode.trim();
+                    if (code.length < 2) {
+                        code = "0" + code;
+                    }
+                    self.selectLayoutCode(code);
+                    self.selectLayoutName(layout.stmtName);
+                    self.selectLayoutStartYm(nts.uk.text.formatYearMonth(layout.startYm));
+                    self.selectLayoutEndYm(nts.uk.text.formatYearMonth(layout.endYm));
+                };
+                ScreenModel.prototype.layoutProcess = function () {
+                    var self = this;
+                    //履歴の編集-削除処理
+                    if ($("#layoutDetele").is(":checked")) {
+                        self.dataDelete();
+                    }
+                };
+                ScreenModel.prototype.dataDelete = function () {
+                    var self = this;
+                    //明細書マスタ.DEL-1
+                    e.service.deleteLayout(self.selectLayout()).done(function () {
+                        alert("削除しました。");
+                    }).fail(function (res) {
+                        alert(res);
+                    });
                 };
                 return ScreenModel;
             }());
