7eb056cc8865a68fdbe8d66db74d1fcc5a7928a9
Truong Quoc Phong <phong_tq@nittsusystem-vn.com>
2016-11-28 16:36:45 +0700

    update code qmm.019

--- nts.uk/pr/nts.uk.pr.web/src/main/webapp/lib/nittsu/ui/notify.ts
+++ nts.uk/pr/nts.uk.pr.web/src/main/webapp/lib/nittsu/ui/notify.ts
@@ -4,9 +4,9 @@
     
     module error {
         
-        ui.documentReady.add(() => {
-            $('#func-notifier-errors').position({ my: 'left+5 top-5', at: 'left bottom', of: $('#functions-area') });
-        });
-        
+//        ui.documentReady.add(() => {
+//            $('#func-notifier-errors').position({ my: 'left+5 top-5', at: 'left bottom', of: $('#functions-area') });
+//        });
+         
     }
 }
