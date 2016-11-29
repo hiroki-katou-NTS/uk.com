517ecd7b069b82df5b7ff3fecd6fec014b290365
Vu Dinh Dung <dungvd@1406-V0005.nsvc.com>
2016-11-29 14:42:51 +0700

    Add dialog

---
+++ nts.uk/pr/nts.uk.pr.web/src/main/webapp/view/qpp/005/f/viewmodel.js
@@ -0,0 +1,31 @@
+var nts;
+(function (nts) {
+    var uk;
+    (function (uk) {
+        var pr;
+        (function (pr) {
+            var view;
+            (function (view) {
+                var qpp;
+                (function (qpp) {
+                    var _005;
+                    (function (_005) {
+                        var f;
+                        (function (f) {
+                            var viewmodel;
+                            (function (viewmodel) {
+                                var ScreenModel = (function () {
+                                    function ScreenModel() {
+                                        var self = this;
+                                    }
+                                    return ScreenModel;
+                                }());
+                                viewmodel.ScreenModel = ScreenModel;
+                            })(viewmodel = f.viewmodel || (f.viewmodel = {}));
+                        })(f = _005.f || (_005.f = {}));
+                    })(_005 = qpp._005 || (qpp._005 = {}));
+                })(qpp = view.qpp || (view.qpp = {}));
+            })(view = pr.view || (pr.view = {}));
+        })(pr = uk.pr || (uk.pr = {}));
+    })(uk = nts.uk || (nts.uk = {}));
+})(nts || (nts = {}));
