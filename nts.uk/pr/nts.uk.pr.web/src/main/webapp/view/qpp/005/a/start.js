2f075c1a8b36b63303f5139bbd32109d3aaed893
Nguyen Van Vu <vu_nv@nittsusystem-vn.com>
2016-11-30 15:17:27 +0700

    udpate grid setting

--- nts.uk/pr/nts.uk.pr.web/src/main/webapp/view/qpp/005/a/start.js
+++ nts.uk/pr/nts.uk.pr.web/src/main/webapp/view/qpp/005/a/start.js
@@ -1,46 +1,79 @@
-__viewContext.ready(function () {
-    var screenModel = new nts.uk.pr.view.qpp005.viewmodel.ScreenModel();
-    screenModel.startPage().done(function () {
-        __viewContext.bind(screenModel);
-        var categoryPayment = screenModel.paymentDataResult().categories()[0];
-        var categoryDeduct = screenModel.paymentDataResult().categories()[1];
-        var categoryArticle = screenModel.paymentDataResult().categories()[3];
-        var $paymentLastItem = categoryPayment.categoryAttribute() + '_' + (categoryPayment.lineCounts() - 1) + '_8';
-        $("#" + $paymentLastItem).addClass('disabled');
-        var $deductLastItem = categoryDeduct.categoryAttribute() + '_' + (categoryDeduct.lineCounts() - 1) + '_8';
-        $("#" + $deductLastItem).addClass('disabled');
-        var $articleLastItem = categoryArticle.categoryAttribute() + '_' + (categoryArticle.lineCounts() - 1) + '_8';
-        $("#" + $articleLastItem).addClass('disabled');
-        $('.tb-category').each(function () {
-            var tblid = this.id;
-            var $tbl = $("#" + tblid);
-            window.orientation = 'vnext';
-            window.hlist = [];
-            $tbl.find('input').not('[readonly]').each(function () { hlist.push($(this)); });
-            for (var i = 0; i < hlist.length; i++) {
-                var next = (i === hlist.length - 1) ? hlist[0] : hlist[i + 1];
-                hlist[i].data('hnext', next);
-            }
-            window.vlist = [];
-            var countColumns = 9;
-            var countRows = $tbl.find('tr').length;
-            for (var c = 0; c < countColumns; c++) {
-                for (var r = 0; r < countRows; r++) {
-                    var $input = $('#' + tblid + '_' + r + '_' + c).not('[readonly]');
-                    if ($input.length === 1)
-                        vlist.push($input);
-                }
-            }
-            for (var i = 0; i < vlist.length; i++) {
-                var next = (i === vlist.length - 1) ? vlist[0] : vlist[i + 1];
-                vlist[i].data('vnext', next);
-            }
-            $tbl.on('keydown', 'input', function (e) {
-                if (e.keyCode == 9) {
-                    $(this).data(orientation).focus();
-                    return false;
-                }
-            });
-        });
-    });
-});
+var nts;
+(function (nts) {
+    var uk;
+    (function (uk) {
+        var pr;
+        (function (pr) {
+            var view;
+            (function (view) {
+                var qpp005;
+                (function (qpp005) {
+                    __viewContext.ready(function () {
+                        var screenModel = new nts.uk.pr.view.qpp005.viewmodel.ScreenModel();
+                        screenModel.startPage().done(function () {
+                            __viewContext.bind(screenModel);
+                            var categoryPayment = screenModel.paymentDataResult().categories()[0];
+                            var categoryDeduct = screenModel.paymentDataResult().categories()[1];
+                            var categoryArticle = screenModel.paymentDataResult().categories()[3];
+                            //        var $paymentLastItem = 'ct' + (<any>categoryPayment).categoryAttribute() + '_' + ((<any>categoryPayment).lineCounts() - 1) + '_8';
+                            //        $("#" + $paymentLastItem).addClass('disabled');
+                            //        var $deductLastItem = (<any>categoryDeduct).categoryAttribute() + '_' + ((<any>categoryDeduct).lineCounts() - 1) + '_8';
+                            //        $("#" + $deductLastItem).addClass('disabled');
+                            //        var $articleLastItem = (<any>categoryArticle).categoryAttribute() + '_' + ((<any>categoryArticle).lineCounts() - 1) + '_8';
+                            //        $("#" + $articleLastItem).addClass('disabled');
+                            $('#pơpup-orientation').ntsPopup({
+                                position: {
+                                    my: 'left top',
+                                    at: 'left bottom',
+                                    of: $('#btSetting')
+                                }
+                            });
+                            utils.gridSetup(screenModel.switchButton().selectedRuleCode());
+                        });
+                    });
+                    var utils;
+                    (function (utils) {
+                        function gridSetup(orientation) {
+                            $('.tb-category').each(function () {
+                                var tblid = this.id;
+                                var $tbl = $("#" + tblid);
+                                window.orientation = orientation;
+                                window.hlist = [];
+                                $tbl.find('input').each(function () { hlist.push($(this)); });
+                                for (var i = 0; i < hlist.length; i++) {
+                                    var next = (i === hlist.length - 1) ? hlist[0] : hlist[i + 1];
+                                    hlist[i].data('hnext', next);
+                                }
+                                window.vlist = [];
+                                var countColumns = 9;
+                                var countRows = $tbl.find('tr').length;
+                                var lastItemId = tblid + '_' + (countRows - 1) + '_' + 8;
+                                for (var c = 0; c < countColumns; c++) {
+                                    for (var r = 0; r < countRows; r++) {
+                                        var input = tblid + '_' + r + '_' + c;
+                                        var $input = $('#' + input);
+                                        if (input === lastItemId)
+                                            $input.addClass('disabled');
+                                        if ($input.length === 1 && input !== lastItemId)
+                                            vlist.push($input);
+                                    }
+                                }
+                                for (var i = 0; i < vlist.length; i++) {
+                                    var next = (i === vlist.length - 1) ? vlist[0] : vlist[i + 1];
+                                    vlist[i].data('vnext', next);
+                                }
+                                $tbl.on('keydown', 'input', function (e) {
+                                    if (e.keyCode == 9) {
+                                        $(this).data(orientation).focus();
+                                        return false;
+                                    }
+                                });
+                            });
+                        }
+                        utils.gridSetup = gridSetup;
+                    })(utils = qpp005.utils || (qpp005.utils = {}));
+                })(qpp005 = view.qpp005 || (view.qpp005 = {}));
+            })(view = pr.view || (pr.view = {}));
+        })(pr = uk.pr || (uk.pr = {}));
+    })(uk = nts.uk || (nts.uk = {}));
+})(nts || (nts = {}));
