a8734c8382130143ebf3598200bff694dbcc1590
Vu Tien Lam <lam_vt@nittsusystem-vn.com>
2016-11-30 17:38:26 +0700

    update for Hung

--- nts.uk/pr/nts.uk.pr.web/src/main/webapp/view/qmm/019/f/start.js
+++ nts.uk/pr/nts.uk.pr.web/src/main/webapp/view/qmm/019/f/start.js
@@ -1,6 +1,11 @@
 __viewContext.ready(function () {
     //var data = this.transferred.get();
-    var data = nts.uk.ui.windows.getShared('param');
+    var param = nts.uk.ui.windows.getShared('param');
+    var data = {
+        categoryId: param.categoryId,
+        itemCode: param.itemCode,
+        isUpdate: param.isUpdate //true
+    };
     var screenModel = new qmm019.f.viewmodel.ScreenModel(data);
     screenModel.start().done(function () {
         __viewContext.bind(screenModel);
