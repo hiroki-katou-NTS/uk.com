d4b40f7b4c9cfd481b6d357c0f0ec3646e2fddcd
DuDT <du_dt@nittsusystem-vn.comdu_dt@nittsusystem-vn.com>
2016-12-01 11:43:01 +0700

    commit

--- nts.uk/pr/nts.uk.pr.web/src/main/webapp/view/qmm/019/g/viewmodel.ts
+++ nts.uk/pr/nts.uk.pr.web/src/main/webapp/view/qmm/019/g/viewmodel.ts
@@ -98,7 +98,7 @@ module qmm019.g.viewmodel {
         buildComboboxChange(layoutCd) : void {
             var self = this;
             _.forEach(self.layouts(), function(layout){
-                if(layout.stmtCode.trim() == layoutCd){
+                if(layout.stmtCode == layoutCd){
                     self.startYmCopy(layout.startYm);
                     return false;    
                 }
@@ -182,7 +182,7 @@ module qmm019.g.viewmodel {
                 checkCopy: true,
                 stmtCodeCopied: self.createNewSelect() + "",
                 startYmCopied: self.startYmCopy(),
-                stmtCode: $('#INP_001').val() + "",
+                stmtCode: $('#INP_001').val(),
                 startYm: + $('#INP_003').val().replace('/',''),
                 layoutAtr: 3,
                 stmtName: $('#INP_002').val() + "",
