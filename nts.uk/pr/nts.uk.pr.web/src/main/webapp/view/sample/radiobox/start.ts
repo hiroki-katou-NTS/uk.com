85d1d9c2db9ac677d63931f75c09c994a6fdf396
NSVC\lamdt <lam_dt@nittsusystem-vn.com>
2016-12-09 17:45:30 +0700

    Update UI Document: RadioBox, Errors, Clearfix

---
+++ nts.uk/pr/nts.uk.pr.web/src/main/webapp/view/sample/radiobox/start.ts
@@ -0,0 +1,21 @@
+__viewContext.ready(function () {
+    class ScreenModel {
+        enable: KnockoutObservable<boolean>;
+        selectedValue: KnockoutObservable<any>;
+        items: KnockoutObservableArray<any>;
+        
+        constructor() {
+            var self = this;
+            self.enable = ko.observable(true);
+            self.items = ko.observableArray([
+                {value: 1, text: 'One'},
+                {value: 2, text: 'Two'},
+                {value: 3, text: 'Three'}
+            ]);
+            self.selectedValue = ko.observable({value: 1, text: 'One'});
+        }
+    }
+    
+    this.bind(new ScreenModel());
+    
+});
