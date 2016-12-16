9cce983c133a142f2e8dfe7ccc1cb77bf37cac1e
Nguyen Van Vu <vu_nv@nittsusystem-vn.com>
2016-12-16 17:45:51 +0700

    update ui null -> 0

--- nts.uk/pr/nts.uk.pr.web/src/main/webapp/view/qpp/005/a/viewmodel.ts
+++ nts.uk/pr/nts.uk.pr.web/src/main/webapp/view/qpp/005/a/viewmodel.ts
@@ -75,10 +75,10 @@ module nts.uk.pr.view.qpp005.a {
             register() {
                 var self = this;
                 // TODO: Check error input
-                if (!self.validator()) {
-                    nts.uk.ui.dialog.alert('入力にエラーがあります。');
-                    return false;
-                }
+//                if (!self.validator()) {
+//                    nts.uk.ui.dialog.alert('入力にエラーがあります。');
+//                    return false;
+//                }
 
                 qpp005.a.service.register(self.employee(), self.paymentDataResult()).done(function(res) {
                     self.startPage().done(function() {
