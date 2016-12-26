74317d83d0dd70c888348cf4e12deebadfe59287
NSVN-DuongHongTin <tin_dh@nittsusystem-vn.com>
2016-12-26 17:38:37 +0700

    swap menu

---
+++ nts.uk/pr/nts.uk.pr.web/src/main/webapp/view/sample/swaplist/start.js
@@ -0,0 +1,27 @@
+__viewContext.ready(function () {
+    var ScreenModel = (function () {
+        function ScreenModel() {
+            this.itemsSwap = ko.observableArray([
+                new ItemModel('001', '基本給', "description 1"),
+                new ItemModel('150', '役職手当', "description 2"),
+                new ItemModel('ABC', '基12本ghj給', "description 3")
+            ]);
+            this.columns = ko.observableArray([
+                { headerText: 'コード', prop: 'code', width: 100 },
+                { headerText: '名称', prop: 'name', width: 200 },
+                { headerText: '説明', prop: 'description', width: 200 }
+            ]);
+            this.currentCodeListSwap = ko.observableArray([]);
+        }
+        return ScreenModel;
+    }());
+    var ItemModel = (function () {
+        function ItemModel(code, name, description) {
+            this.code = code;
+            this.name = name;
+            this.description = description;
+        }
+        return ItemModel;
+    }());
+    this.bind(new ScreenModel());
+});
---
+++ nts.uk/pr/nts.uk.pr.web/src/main/webapp/view/sample/swaplist/start.ts
@@ -0,0 +1,40 @@
+__viewContext.ready(function () {
+    
+    class ScreenModel {
+        itemsSwap: KnockoutObservableArray<ItemModel>;
+        columns: KnockoutObservableArray<nts.uk.ui.NtsGridListColumn>;
+        currentCodeListSwap: KnockoutObservableArray<any>;
+        
+        constructor() {
+            
+            this.itemsSwap = ko.observableArray([
+                new ItemModel('001', '基本給', "description 1"),
+                new ItemModel('150', '役職手当', "description 2"),
+                new ItemModel('ABC', '基12本ghj給', "description 3")
+            ]);
+            
+            this.columns = ko.observableArray([
+                { headerText: 'コード', prop: 'code', width: 100 },
+                { headerText: '名称', prop: 'name', width: 200 },
+                { headerText: '説明', prop: 'description', width: 200 }
+            ]);
+            this.currentCodeListSwap = ko.observableArray([]);
+        }
+        
+    }
+    
+    class ItemModel {
+        code: string;
+        name: string;
+        description: string;
+        constructor(code: string, name: string, description: string) {
+            this.code = code;
+            this.name = name;
+            this.description = description;      
+        }
+    }
+
+    
+    this.bind(new ScreenModel());
+    
+});
