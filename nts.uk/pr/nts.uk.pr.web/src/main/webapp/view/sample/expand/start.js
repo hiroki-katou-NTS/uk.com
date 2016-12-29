b0df2569ad258bd3b24a4e1d38d2849640b474c9
hoanld <hoan_ld@nittsusystem-vn.com>
2016-12-29 11:17:47 +0700

    update sample

--- nts.uk/pr/nts.uk.pr.web/src/main/webapp/view/sample/expand/start.js
+++ nts.uk/pr/nts.uk.pr.web/src/main/webapp/view/sample/expand/start.js
@@ -5,6 +5,8 @@ __viewContext.ready(function () {
             self.date = ko.observable(new Date('2016/12/01'));
             self.show = ko.observable(true);
             self.itemList = ko.observableArray([]);
+            self.btnText = ko.computed(function () { if (self.show())
+                return "-"; return "+"; });
             for (var i = 1; i <= 12; i++) {
                 self.itemList.push({ index: i });
             }
