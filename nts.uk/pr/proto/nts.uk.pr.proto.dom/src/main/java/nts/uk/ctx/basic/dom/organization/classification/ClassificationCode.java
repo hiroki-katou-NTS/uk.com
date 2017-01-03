2a7778ac44eae948ee08a743438dca7491aff990
luongdv <luongdv@1406-V0013.nsvc.com>
2017-01-03 13:43:41 +0700

    create entity cmm014

--- nts.uk/pr/proto/nts.uk.pr.proto.dom/src/main/java/nts/uk/ctx/basic/dom/organization/classification/ClassificationCode.java
+++ nts.uk/pr/proto/nts.uk.pr.proto.dom/src/main/java/nts/uk/ctx/basic/dom/organization/classification/ClassificationCode.java
@@ -1,7 +1,16 @@
 package nts.uk.ctx.basic.dom.organization.classification;
 
+import nts.arc.primitive.constraint.StringMaxLength;
 import nts.uk.shr.com.primitive.CodePrimitiveValue;
 
-public class ClassificationCode{
+@StringMaxLength(10)
+public class ClassificationCode extends CodePrimitiveValue<ClassificationCode> {
+
+	/** serialVersionUID */
+	private static final long serialVersionUID = 1L;
+
+	public ClassificationCode(String rawValue) {
+		super(rawValue);
+	}
 
 }
