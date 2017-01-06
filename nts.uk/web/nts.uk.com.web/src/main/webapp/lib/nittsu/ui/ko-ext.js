8ee01f8474f5c078b2d1e80ff5b20c278fb16b48
NSVN-DuongHongTin <tin_dh@nittsusystem-vn.com>
2017-01-06 13:14:30 +0700

    update search swaplist

--- nts.uk/web/nts.uk.com.web/src/main/webapp/lib/nittsu/ui/ko-ext.js
+++ nts.uk/web/nts.uk.com.web/src/main/webapp/lib/nittsu/ui/ko-ext.js
@@ -1977,10 +1977,10 @@ var nts;
                                 if (searchedValues !== undefined) {
                                     if (selected.length === 0 || selected[0].id !== searchedValues[primaryKey]) {
                                         var scrollContainer = $(grid1Id + "_scrollContainer");
-                                        var current = $(grid1Id).ntsGridList("getSelected");
-                                        if (current.length > 0) {
-                                            var rowidstr = "tr[data-id='" + current[0].id + "']";
-                                            scrollContainer.scrollTop($(rowidstr).position().top);
+                                        var current = $(grid1Id).igGrid("selectedRows");
+                                        if (current.length > 0 && scrollContainer.length > 0) {
+                                            $(grid1Id).igGrid("virtualScrollTo", current[0].index === tempOrigiSour.length - 1
+                                                ? current[0].index : current[0].index + 1);
                                         }
                                     }
                                 }
