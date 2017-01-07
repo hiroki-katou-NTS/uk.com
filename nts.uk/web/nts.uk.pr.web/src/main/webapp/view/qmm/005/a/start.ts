76dae2a3acbf9cbd6fdb3938e1c641033c2a68b9
NSVN-Nguyen Van Vuong <vuong_nv@nittsusystem-vn.com>
2017-01-07 08:38:42 +0700

    update ts file
    update ts file test commit function

---
+++ nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qmm/005/a/start.ts
@@ -0,0 +1,6 @@
+module nts.uk.pr.view.qmm005.a {
+    __viewContext.ready(function() {
+        let screenModel = new nts.uk.pr.view.qmm005.a.viewmodel.viewModel();
+        this.bind(screenModel);
+    });
+}
---
+++ nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qmm/005/a/viewmodel.ts
@@ -0,0 +1,7 @@
+module nts.uk.pr.view.qmm005.a.viewmodel {
+    export class viewModel {
+
+        constructor() {
+        }
+    }
+}
