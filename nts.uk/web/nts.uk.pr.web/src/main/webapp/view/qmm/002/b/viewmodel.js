5c20ca3c2d4fb8fa4fd6e4dd2b1a37000f3d9a26
NSVN-BuiVanChinh <chinh_bv@nittsusystem-vn.com>
2017-01-09 16:46:31 +0700

    add html qmm002/b

---
+++ nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qmm/002/b/viewmodel.js
@@ -0,0 +1,58 @@
+var nts;
+(function (nts) {
+    var uk;
+    (function (uk) {
+        var pr;
+        (function (pr) {
+            var view;
+            (function (view) {
+                var qmm002;
+                (function (qmm002) {
+                    var b;
+                    (function (b) {
+                        var viewmodel;
+                        (function (viewmodel) {
+                            var ScreenModel = (function () {
+                                function ScreenModel() {
+                                    var self = this;
+                                    self.lst_001 = ko.observableArray([]);
+                                    self.selectedCodes = ko.observableArray([]);
+                                    self.selectedCodes.subscribe(function (val) {
+                                        console.log(val);
+                                    });
+                                }
+                                ScreenModel.prototype.startPage = function () {
+                                    var self = this;
+                                    b.service.getBankList().done(function (result) {
+                                        self.lst_001(result);
+                                    });
+                                };
+                                ScreenModel.prototype.excute = function () {
+                                    var self = this;
+                                    nts.uk.ui.windows.setShared("data", self.selectedCodes());
+                                };
+                                ScreenModel.prototype.close = function () {
+                                    var self = this;
+                                    nts.uk.ui.windows.close();
+                                };
+                                return ScreenModel;
+                            }());
+                            viewmodel.ScreenModel = ScreenModel;
+                            var Node = (function () {
+                                function Node(code, name, childs) {
+                                    var self = this;
+                                    self.code = code;
+                                    self.name = name;
+                                    self.nodeText = self.code + ' ' + self.name;
+                                    self.childs = childs;
+                                }
+                                return Node;
+                            }());
+                            viewmodel.Node = Node;
+                        })(viewmodel = b.viewmodel || (b.viewmodel = {}));
+                    })(b = qmm002.b || (qmm002.b = {}));
+                })(qmm002 = view.qmm002 || (view.qmm002 = {}));
+            })(view = pr.view || (pr.view = {}));
+        })(pr = uk.pr || (uk.pr = {}));
+    })(uk = nts.uk || (nts.uk = {}));
+})(nts || (nts = {}));
