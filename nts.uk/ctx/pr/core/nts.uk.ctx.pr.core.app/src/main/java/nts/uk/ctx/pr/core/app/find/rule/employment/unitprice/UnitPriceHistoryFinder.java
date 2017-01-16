735449abcacdca4c2ff870311892d74bcc74f9e4
nws-anhnm <anhnm@newwave.vn>
2017-01-16 15:22:25 +0700

    add webservice, command & commandhandler for unitpricehistory

---
+++ nts.uk/ctx/pr/core/nts.uk.ctx.pr.core.app/src/main/java/nts/uk/ctx/pr/core/app/find/rule/employment/unitprice/UnitPriceHistoryFinder.java
@@ -0,0 +1,14 @@
+package nts.uk.ctx.pr.core.app.find.rule.employment.unitprice;
+
+import java.util.List;
+
+public class UnitPriceHistoryFinder {
+
+	public UnitPriceHistoryDto find(String companyCode, String id) {
+		return null;
+	}
+
+	public List<UnitPriceHistoryDto> findAll(String companyCode) {
+		return null;
+	}
+}
