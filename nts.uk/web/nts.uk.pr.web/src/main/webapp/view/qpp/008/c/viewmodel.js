12dcb3f04066b7d324b07e584091efddc326c836
nguyenanhhai <hai_na@nittsusystem-vn.com>
2017-01-18 15:21:50 +0700

    qpp008 screen c,d,e

--- nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qpp/008/c/viewmodel.js
+++ nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qpp/008/c/viewmodel.js
@@ -12,19 +12,44 @@ var qpp008;
                     self.paymentDateProcessingList = ko.observableArray([]);
                     self.selectedPaymentDate = ko.observable(null);
                     /*SwapList*/
+                    //swapList1
                     self.itemsSwap = ko.observableArray([]);
-                    /*TextEditer*/
-                    self.cInp002Code = ko.observable(false);
                     var array = [];
+                    var array1 = [];
+                    var array2 = [];
                     for (var i = 0; i < 10000; i++) {
-                        array.push(new ItemModel("test" + i, '基本給', "description"));
+                        array.push(new ItemModel("testz" + i, '基本給', "description"));
                     }
                     self.itemsSwap(array);
                     self.columnsSwap = ko.observableArray([
                         { headerText: 'コード', prop: 'code', width: 60 },
                         { headerText: '名称', prop: 'name', width: 116 }
                     ]);
-                    this.currentCodeListSwap = ko.observableArray([]);
+                    self.currentCodeListSwap = ko.observableArray([]);
+                    //swapList2
+                    self.itemsSwap2 = ko.observableArray([]);
+                    for (var i = 0; i < 10000; i++) {
+                        array1.push(new ItemModel("testx" + i, '基本給', "description"));
+                    }
+                    self.itemsSwap2(array1);
+                    self.columnsSwap2 = ko.observableArray([
+                        { headerText: 'コード', prop: 'code', width: 60 },
+                        { headerText: '名称', prop: 'name', width: 116 }
+                    ]);
+                    self.currentCodeListSwap2 = ko.observableArray([]);
+                    //swapList3
+                    self.itemsSwap3 = ko.observableArray([]);
+                    for (var i = 0; i < 10000; i++) {
+                        array2.push(new ItemModel("testy" + i, '基本給', "description"));
+                    }
+                    self.itemsSwap3(array2);
+                    self.columnsSwap3 = ko.observableArray([
+                        { headerText: 'コード', prop: 'code', width: 60 },
+                        { headerText: '名称', prop: 'name', width: 116 }
+                    ]);
+                    self.currentCodeListSwap3 = ko.observableArray([]);
+                    /*TextEditer*/
+                    self.cInp002Code = ko.observable(false);
                     /*TabPanel*/
                     self.tabs = ko.observableArray([
                         { id: 'tab-1', title: 'Tab Title 1', content: '.tab-content-1', enable: ko.observable(true), visible: ko.observable(true) },
@@ -53,20 +78,38 @@ var qpp008;
                         self.currentItem(ko.mapping.fromJS(self.getItem(codeChanged)));
                         self.cInp002Code(false);
                     });
+                    self.items2 = ko.computed(function () {
+                        var x = [];
+                        x = x.concat(self.currentCodeListSwap());
+                        x = x.concat(self.currentCodeListSwap2());
+                        x = x.concat(self.currentCodeListSwap3());
+                        return x;
+                    }, self).extend({ deferred: true });
+                    //            self.currentCodeListSwap2.subscribe(function(swapItems) {
+                    //                self.items2.removeAll();
+                    //                _.forEach(swapItems, function(value) {
+                    //                    console.log(value)
+                    //                    if (self.findDuplicateSwaps(value.code) == false) {
+                    //                        self.items2.push(value);
+                    //                    }
+                    //
+                    //                });
+                    //
+                    //            });
                     //gridList2
-                    self.items2 = ko.observableArray([]);
-                    var str = ['a0', 'b0', 'c0', 'd0'];
-                    for (var j = 0; j < 4; j++) {
-                        for (var i = 1; i < 51; i++) {
-                            var code = i < 10 ? str[j] + '0' + i : str[j] + i;
-                            this.items2.push(new ItemModel(code, code, code, code));
-                        }
-                    }
-                    self.columns = ko.observableArray([
+                    //            self.items2 = ko.observableArray([]);
+                    //            var str = ['a0', 'b0', 'c0', 'd0'];
+                    //            for (var j = 0; j < 4; j++) {
+                    //                for (var i = 1; i < 51; i++) {
+                    //                    var code = i < 10 ? str[j] + '0' + i : str[j] + i;
+                    //                    this.items2.push(new ItemModel(code, code, code, code));
+                    //                }
+                    //            }
+                    self.columns2 = ko.observableArray([
                         { headerText: 'コード', prop: 'code', width: 60 },
                         { headerText: '名称', prop: 'name', width: 120 },
                     ]);
-                    this.currentCode2 = ko.observable();
+                    self.currentCode2 = ko.observable();
                 }
                 ScreenModel.prototype.refreshLayout = function () {
                     var self = this;
@@ -79,12 +122,43 @@ var qpp008;
                     //            self.currentItem = ko.observable(ko.mapping.fromJS(_.first(self.items())));
                     //elf.cInp002Code = ko.observable(false);
                 };
+                ScreenModel.prototype.insertData = function () {
+                    var self = this;
+                    var newData = ko.toJS(self.currentItem());
+                    if (self.cInp002Code()) {
+                        self.items.push(newData);
+                    }
+                    else {
+                        var updateIndex = _.findIndex(self.items(), function (item) { return item.code == newData.code; });
+                        self.items.splice(updateIndex, 1, newData);
+                    }
+                };
+                ScreenModel.prototype.deleteData = function () {
+                    var self = this;
+                    var newDelData = ko.toJS(self.currentItem());
+                    var deleData = _.findIndex(self.items(), function (item) { return item.code == newDelData.code; });
+                    self.items.splice(deleData, 1);
+                };
                 ScreenModel.prototype.getItem = function (codeNew) {
                     var self = this;
                     var currentItem = _.find(self.items(), function (item) {
                         return item.code === codeNew;
                     });
                     return currentItem;
+                };
+                ScreenModel.prototype.findDuplicateSwaps = function (codeNew) {
+                    var self = this;
+                    var value;
+                    var checkItemSwap = _.find(self.items2(), function (item) {
+                        return item.code == codeNew;
+                    });
+                    if (checkItemSwap == undefined) {
+                        value = false;
+                    }
+                    else {
+                        value = true;
+                    }
+                    return value;
                 };
                 ScreenModel.prototype.addItem = function () {
                     this.items.push(new ItemModel('999', '基本給', "description 1", "other1"));
--- nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qpp/008/c/viewmodel.ts
+++ nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qpp/008/c/viewmodel.ts
@@ -5,9 +5,18 @@ module qpp008.c.viewmodel {
         allowEditCode: KnockoutObservable<boolean> = ko.observable(false);
         isUpdate: KnockoutObservable<boolean> = ko.observable(true);
         /*SwapList*/
+        //swapList1
         itemsSwap: KnockoutObservableArray<ItemModel>;
         columnsSwap: KnockoutObservableArray<nts.uk.ui.NtsGridListColumn>;
         currentCodeListSwap: KnockoutObservableArray<any>;
+        //swapList2
+        itemsSwap2: KnockoutObservableArray<ItemModel>;
+        columnsSwap2: KnockoutObservableArray<nts.uk.ui.NtsGridListColumn>
+        currentCodeListSwap2: KnockoutObservableArray<any>;
+        //swapList3
+        itemsSwap3: KnockoutObservableArray<ItemModel>;
+        columnsSwap3: KnockoutObservableArray<nts.uk.ui.NtsGridListColumn>
+        currentCodeListSwap3: KnockoutObservableArray<any>;
         /*TabPanel*/
         tabs: KnockoutObservableArray<nts.uk.ui.NtsTabPanelModel>;
         selectedTab: KnockoutObservable<string>;
@@ -16,12 +25,12 @@ module qpp008.c.viewmodel {
         items: KnockoutObservableArray<ItemModel>;
         columns: KnockoutObservableArray<any>;
         currentCode: KnockoutObservable<any>;
-//        currentItem: any;
+        //        currentItem: any;
         nameValue: KnockoutObservable<string>;
         codeValue: KnockoutObservable<any>;
         //gridList2
         items2: KnockoutObservableArray<ItemModel>;
-        //columns2: KnockoutObservableArray<any>;
+        columns2: KnockoutObservableArray<any>;
         currentCode2: KnockoutObservable<any>;
         currentItem2: KnockoutObservable<any>;
         /*TextEditer*/
@@ -33,12 +42,13 @@ module qpp008.c.viewmodel {
             self.paymentDateProcessingList = ko.observableArray([]);
             self.selectedPaymentDate = ko.observable(null);
             /*SwapList*/
+            //swapList1
             self.itemsSwap = ko.observableArray([]);
-            /*TextEditer*/
-            self.cInp002Code = ko.observable(false);
             var array = [];
+            var array1 = [];
+            var array2 = [];
             for (var i = 0; i < 10000; i++) {
-                array.push(new ItemModel("test" + i, '基本給', "description"));
+                array.push(new ItemModel("testz" + i, '基本給', "description"));
             }
             self.itemsSwap(array);
 
@@ -47,7 +57,33 @@ module qpp008.c.viewmodel {
                 { headerText: '名称', prop: 'name', width: 116 }
             ]);
 
-            this.currentCodeListSwap = ko.observableArray([]);
+            self.currentCodeListSwap = ko.observableArray([]);
+            //swapList2
+            self.itemsSwap2 = ko.observableArray([]);
+            for (var i = 0; i < 10000; i++) {
+                array1.push(new ItemModel("testx" + i, '基本給', "description"));
+            }
+            self.itemsSwap2(array1);
+
+            self.columnsSwap2 = ko.observableArray([
+                { headerText: 'コード', prop: 'code', width: 60 },
+                { headerText: '名称', prop: 'name', width: 116 }
+            ]);
+            self.currentCodeListSwap2 = ko.observableArray([]);
+            //swapList3
+            self.itemsSwap3 = ko.observableArray([]);
+            for (var i = 0; i < 10000; i++) {
+                array2.push(new ItemModel("testy" + i, '基本給', "description"));
+            }
+            self.itemsSwap3(array2);
+            self.columnsSwap3 = ko.observableArray([
+                { headerText: 'コード', prop: 'code', width: 60 },
+                { headerText: '名称', prop: 'name', width: 116 }
+            ]);
+            self.currentCodeListSwap3 = ko.observableArray([]);
+            /*TextEditer*/
+            self.cInp002Code = ko.observable(false);
+
             /*TabPanel*/
             self.tabs = ko.observableArray([
                 { id: 'tab-1', title: 'Tab Title 1', content: '.tab-content-1', enable: ko.observable(true), visible: ko.observable(true) },
@@ -77,35 +113,68 @@ module qpp008.c.viewmodel {
                 self.currentItem(ko.mapping.fromJS(self.getItem(codeChanged)));
                 self.cInp002Code(false);
             });
-           
-            
+            self.items2 = ko.computed(function(){
+                var x = [];
+                x = x.concat(self.currentCodeListSwap());
+                x = x.concat(self.currentCodeListSwap2());
+                x = x.concat(self.currentCodeListSwap3());
+                return x;
+            }, self).extend({ deferred: true });
+//            self.currentCodeListSwap2.subscribe(function(swapItems) {
+//                self.items2.removeAll();
+//                _.forEach(swapItems, function(value) {
+//                    console.log(value)
+//                    if (self.findDuplicateSwaps(value.code) == false) {
+//                        self.items2.push(value);
+//                    }
+//
+//                });
+//
+//            });
+
             //gridList2
-            self.items2 = ko.observableArray([]);
-            var str = ['a0', 'b0', 'c0', 'd0'];
-            for (var j = 0; j < 4; j++) {
-                for (var i = 1; i < 51; i++) {
-                    var code = i < 10 ? str[j] + '0' + i : str[j] + i;
-                    this.items2.push(new ItemModel(code, code, code, code));
-                }
-            }
-            self.columns = ko.observableArray([
+//            self.items2 = ko.observableArray([]);
+            //            var str = ['a0', 'b0', 'c0', 'd0'];
+            //            for (var j = 0; j < 4; j++) {
+            //                for (var i = 1; i < 51; i++) {
+            //                    var code = i < 10 ? str[j] + '0' + i : str[j] + i;
+            //                    this.items2.push(new ItemModel(code, code, code, code));
+            //                }
+            //            }
+            self.columns2 = ko.observableArray([
                 { headerText: 'コード', prop: 'code', width: 60 },
                 { headerText: '名称', prop: 'name', width: 120 },
 
             ]);
-            this.currentCode2 = ko.observable();
-            
+            self.currentCode2 = ko.observable();
+
         }
         refreshLayout(): void {
             let self = this;
             //self.allowEditCode(true);
             self.cInp002Code(true);
-            self.currentItem(ko.mapping.fromJS(new ItemModel('', '', '','','')));
+            self.currentItem(ko.mapping.fromJS(new ItemModel('', '', '', '', '')));
             //self.currentCode() = ko.observable();
-           // self.currentItem=ko.observable(new ItemModel('', '', '','',''));
-//            self.currentCode = ko.observable();
-//            self.currentItem = ko.observable(ko.mapping.fromJS(_.first(self.items())));
+            // self.currentItem=ko.observable(new ItemModel('', '', '','',''));
+            //            self.currentCode = ko.observable();
+            //            self.currentItem = ko.observable(ko.mapping.fromJS(_.first(self.items())));
             //elf.cInp002Code = ko.observable(false);
+        }
+        insertData() {
+            let self = this;
+            let newData = ko.toJS(self.currentItem());
+            if (self.cInp002Code()) {
+                self.items.push(newData);
+            } else {
+                let updateIndex = _.findIndex(self.items(), function(item) { return item.code == newData.code; });
+                self.items.splice(updateIndex, 1, newData);
+            }
+        }
+        deleteData() {
+            let self = this;
+            let newDelData = ko.toJS(self.currentItem());
+            let deleData = _.findIndex(self.items(), function(item) { return item.code == newDelData.code; });
+            self.items.splice(deleData, 1);
         }
 
         getItem(codeNew): ItemModel {
@@ -116,7 +185,21 @@ module qpp008.c.viewmodel {
 
             return currentItem;
         }
-
+        findDuplicateSwaps(codeNew) {
+            let self = this;
+            let value;
+            let checkItemSwap = _.find(self.items2(), function(item) {
+                return item.code == codeNew
+            });
+            if (checkItemSwap == undefined) {
+                value = false;
+            }
+            else {
+                value = true;
+            }
+            return value;
+        }
+        
         addItem() {
             this.items.push(new ItemModel('999', '基本給', "description 1", "other1"));
             this.items2.push(new ItemModel('999', '基本給', "description 1", "other1"));
