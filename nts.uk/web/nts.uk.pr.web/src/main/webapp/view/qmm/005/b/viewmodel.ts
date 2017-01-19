52bbb80619b1021183ef695f229f9867c3c672e4
NSVN-Nguyen Van Vuong <vuong_nv@nittsusystem-vn.com>
2017-01-19 08:47:30 +0700

    commit new source
    commit new module 005-c

--- nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qmm/005/b/viewmodel.ts
+++ nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qmm/005/b/viewmodel.ts
@@ -4,13 +4,66 @@ module nts.uk.pr.view.qmm005.b.viewmodel {
     export class ViewModel {
         date: any;
         selectedCodes: any;
+        tableItems: TableRowItem[];
+        listboxItem: SelectItem[];
         constructor() {
             this.date = ko.observable(new Date());
-            this.selectedCodes = ko.observable('2');
+            this.selectedCodes = ko.observable(2);
+            
+            this.listboxItem = ko.observableArray([
+                new SelectItem(1, "Option 1"),
+                new SelectItem(2, "Option 2"),
+                new SelectItem(3, "Option 3"),
+                new SelectItem(4, "Option 4"),
+                new SelectItem(5, "Option 5"),
+                new SelectItem(6, "Option 6"),
+                new SelectItem(7, "Option 7"),
+                new SelectItem(8, "Option 8"),
+                new SelectItem(9, "Option 9"),
+                new SelectItem(10, "Option 10"),
+                new SelectItem(11, "Option 11"),
+                new SelectItem(12, "Option 12"),
+                new SelectItem(13, "Option 13"),
+                new SelectItem(14, "Option 14"),
+                new SelectItem(15, "Option 15"),
+                new SelectItem(16, "Option 16"),
+                new SelectItem(17, "Option 17")
+            ]);
+            
+            this.tableItems = ko.observableArray([
+                new TableRowItem(1),
+                new TableRowItem(2),
+                new TableRowItem(3),
+                new TableRowItem(4),
+                new TableRowItem(5),
+                new TableRowItem(6),
+                new TableRowItem(7),
+                new TableRowItem(8),
+                new TableRowItem(9),
+                new TableRowItem(10),
+                new TableRowItem(11),
+                new TableRowItem(12)
+            ]);
         }
         toggleColumns(item, event): void {
             $('.toggle').toggleClass('hidden');
+            $(event.currentTarget).parent('td').toggleClass('checkbox-cols');
             ($(event.currentTarget).text() == "-" && $(event.currentTarget).text('+')) || $(event.currentTarget).text('-');
+        }
+    }
+
+    class SelectItem {
+        constructor(public index: number,public label: string){
+        }
+    
+    }
+
+    class TableRowItem {
+        constructor(public index: number) {
+        }
+
+        toggleCalendar(item, event): void {
+            $(event.currentTarget).parent('div').find('input[type=text]').trigger('click');
         }
     }
 }
---
+++ nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qmm/005/c/viewmodel.ts
@@ -0,0 +1,6 @@
+let ko: any, $: any;
+
+module nts.uk.pr.view.qmm005.c.viewmodel {
+    export class ViewModel {
+    }
+}
