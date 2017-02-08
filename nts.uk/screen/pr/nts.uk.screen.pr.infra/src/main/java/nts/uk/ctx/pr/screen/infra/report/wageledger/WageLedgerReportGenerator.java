3e08235cf845802fb1985008e3037c70f0e0b3ab
nws_tiendd <tiendd_int@newwave.vn>
2017-02-08 18:08:49 +0700

    Add WageLedgerReportGenerator

---
+++ nts.uk/screen/pr/nts.uk.screen.pr.infra/src/main/java/nts/uk/ctx/pr/screen/infra/report/wageledger/WageLedgerReportGenerator.java
@@ -0,0 +1,5 @@
+package nts.uk.ctx.pr.screen.infra.report.wageledger;
+
+public class WageLedgerReportGenerator {
+
+}
---
+++ nts.uk/screen/pr/nts.uk.screen.pr.infra/src/main/java/nts/uk/ctx/pr/screen/infra/report/wageledger/dto/WageLedgerReportDto.java
@@ -0,0 +1,5 @@
+package nts.uk.ctx.pr.screen.infra.report.wageledger.dto;
+
+public class WageLedgerReportDto {
+
+}
