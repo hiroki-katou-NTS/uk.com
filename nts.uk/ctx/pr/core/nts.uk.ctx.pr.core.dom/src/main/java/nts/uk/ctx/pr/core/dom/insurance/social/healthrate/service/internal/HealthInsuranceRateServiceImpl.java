8f7c70ccfcd32dc6b0f405762861f93f43d4e429
Nws_ThanhNC <thanhnc@newwave.vn>
2017-02-09 11:38:07 +0700

    Add service internal.

---
+++ nts.uk/ctx/pr/core/nts.uk.ctx.pr.core.dom/src/main/java/nts/uk/ctx/pr/core/dom/insurance/social/healthrate/service/internal/HealthInsuranceRateServiceImpl.java
@@ -0,0 +1,30 @@
+/******************************************************************
+ * Copyright (c) 2016 Nittsu System to present.                   *
+ * All right reserved.                                            *
+ *****************************************************************/
+package nts.uk.ctx.pr.core.dom.insurance.social.healthrate.service.internal;
+
+import javax.ejb.Stateless;
+
+import nts.uk.ctx.pr.core.dom.insurance.social.healthrate.HealthInsuranceRate;
+import nts.uk.ctx.pr.core.dom.insurance.social.healthrate.service.HealthInsuranceRateService;
+
+/**
+ * The Class HealthInsuranceRateServiceImpl.
+ */
+@Stateless
+public class HealthInsuranceRateServiceImpl implements HealthInsuranceRateService {
+
+	@Override
+	public void validateRequiredItem(HealthInsuranceRate rate) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void validateDateRange(HealthInsuranceRate rate) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ nts.uk/ctx/pr/core/nts.uk.ctx.pr.core.dom/src/main/java/nts/uk/ctx/pr/core/dom/insurance/social/service/internal/SocialInsuranceOfficeServiceImpl.java
@@ -0,0 +1,30 @@
+/******************************************************************
+ * Copyright (c) 2016 Nittsu System to present.                   *
+ * All right reserved.                                            *
+ *****************************************************************/
+package nts.uk.ctx.pr.core.dom.insurance.social.service.internal;
+
+import javax.ejb.Stateless;
+
+import nts.uk.ctx.pr.core.dom.insurance.social.SocialInsuranceOffice;
+import nts.uk.ctx.pr.core.dom.insurance.social.service.SocialInsuranceOfficeService;
+
+/**
+ * The Class LaborInsuranceOfficeServiceImpl.
+ */
+@Stateless
+public class SocialInsuranceOfficeServiceImpl implements SocialInsuranceOfficeService {
+
+	@Override
+	public void validateRequiredItem(SocialInsuranceOffice office) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void checkDuplicateCode(SocialInsuranceOffice office) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
