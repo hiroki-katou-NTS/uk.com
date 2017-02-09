86b061b9a302678e44b8606607efd525a1a205d6
NSVC\lamdt <lam_dt@nittsusystem-vn.com>
2017-02-09 18:18:30 +0700

    Update LinkButton component

--- nts.uk/web/nts.uk.com.web/src/main/webapp/view/sample/linkbutton/start.ts
+++ nts.uk/web/nts.uk.com.web/src/main/webapp/view/sample/linkbutton/start.ts
@@ -1,8 +1,15 @@
 __viewContext.ready(function () {
     class ScreenModel {
+        linkText: KnockoutObservable<string>;
         
         constructor() {
             var self = this;
+            self.linkText = ko.observable("Do something");
+        }
+        
+        doSomething(s) {
+            var self = this;
+            self.linkText(self.linkText() + "❤" + s);
         }
     }
     
