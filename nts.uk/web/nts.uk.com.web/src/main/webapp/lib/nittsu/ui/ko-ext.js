9a57269088f3b57cd7a5f60bd8f74249704eb414
NSVC\lamdt <lam_dt@nittsusystem-vn.com>
2017-02-09 18:18:57 +0700

    Update LinkButton component

--- nts.uk/web/nts.uk.com.web/src/main/webapp/lib/nittsu/ui/ko-ext.js
+++ nts.uk/web/nts.uk.com.web/src/main/webapp/lib/nittsu/ui/ko-ext.js
@@ -1832,13 +1832,17 @@ var nts;
                      */
                     NtsLinkButtonBindingHandler.prototype.init = function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
                         var data = valueAccessor();
-                        var jump = data.jump;
+                        var jump = ko.unwrap(data.jump);
+                        var action = data.action;
                         var linkText = $(element).text();
-                        var $linkButton = $(element).wrap('<div/>').parent().empty()
-                            .text(linkText)
+                        var $linkButton = $(element).wrap('<div class="ntsControl"/>')
                             .addClass('link-button')
                             .click(function () {
-                            alert(jump);
+                            event.preventDefault();
+                            if (!nts.uk.util.isNullOrUndefined(action))
+                                action.call(viewModel);
+                            else if (!nts.uk.util.isNullOrUndefined(jump))
+                                nts.uk.request.jump(jump);
                         });
                     };
                     /**
