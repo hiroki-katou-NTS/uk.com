1ba05fc33bdb0a87088218bb888f465d0c46d78c
nws-dientx <dientx@newwave.vn>
2017-02-09 16:35:40 +0700

    update insurance office command

--- nts.uk/ctx/pr/core/nts.uk.ctx.pr.core.app/src/main/java/nts/uk/ctx/pr/core/app/insurance/social/command/RegisterSocialOfficeCommandHandler.java
+++ nts.uk/ctx/pr/core/nts.uk.ctx.pr.core.app/src/main/java/nts/uk/ctx/pr/core/app/insurance/social/command/RegisterSocialOfficeCommandHandler.java
@@ -7,17 +7,10 @@ import javax.transaction.Transactional;
 import nts.arc.layer.app.command.CommandHandler;
 import nts.arc.layer.app.command.CommandHandlerContext;
 import nts.uk.ctx.core.dom.company.CompanyCode;
-import nts.uk.ctx.pr.core.app.insurance.social.find.dto.SocialInsuranceOfficeDto;
 import nts.uk.ctx.pr.core.app.service.insurance.social.SocialInsuranceService;
-import nts.uk.ctx.pr.core.dom.insurance.Address;
-import nts.uk.ctx.pr.core.dom.insurance.KanaAddress;
-import nts.uk.ctx.pr.core.dom.insurance.OfficeCode;
-import nts.uk.ctx.pr.core.dom.insurance.OfficeName;
-import nts.uk.ctx.pr.core.dom.insurance.PicName;
-import nts.uk.ctx.pr.core.dom.insurance.PicPosition;
-import nts.uk.ctx.pr.core.dom.insurance.PotalCode;
-import nts.uk.ctx.pr.core.dom.insurance.ShortName;
 import nts.uk.ctx.pr.core.dom.insurance.social.SocialInsuranceOffice;
+import nts.uk.ctx.pr.core.dom.insurance.social.SocialInsuranceOfficeRepository;
+import nts.uk.shr.com.context.AppContexts;
 
 @Stateless
 public class RegisterSocialOfficeCommandHandler extends CommandHandler<RegisterSocialOfficeCommand> {
@@ -25,38 +18,24 @@ public class RegisterSocialOfficeCommandHandler extends CommandHandler<RegisterS
 	@Inject
 	SocialInsuranceService insuranceSocialService;
 
+	@Inject
+	SocialInsuranceOfficeRepository socialInsuranceOfficeRepository;
+
 	@Override
 	@Transactional
-	protected void handle(CommandHandlerContext<RegisterSocialOfficeCommand> command) {
-
-		SocialInsuranceOfficeDto socialInsuranceOfficeDto = command.getCommand().getSocialInsuranceOfficeDto();
-		// TODO get data from session
-		// Integer companyCode = 1;
-		// String contractCode = "1";
-
-		// TODO convert Dto to Domain
-		SocialInsuranceOffice SocialInsuranceOfficeDomain = new SocialInsuranceOffice(
-				new CompanyCode(socialInsuranceOfficeDto.getCompanyCode()),
-				new OfficeCode(socialInsuranceOfficeDto.getCode()), new OfficeName(socialInsuranceOfficeDto.getName()),
-				new ShortName(socialInsuranceOfficeDto.getShortName()),
-				new PicName(socialInsuranceOfficeDto.getPicName()),
-				new PicPosition(socialInsuranceOfficeDto.getPicPosition()),
-				new PotalCode(socialInsuranceOfficeDto.getPotalCode()), socialInsuranceOfficeDto.getPrefecture(),
-				new Address(socialInsuranceOfficeDto.getAddress1st()),
-				new Address(socialInsuranceOfficeDto.getAddress2nd()),
-				new KanaAddress(socialInsuranceOfficeDto.getKanaAddress1st()),
-				new KanaAddress(socialInsuranceOfficeDto.getKanaAddress2nd()),
-				socialInsuranceOfficeDto.getPhoneNumber(), socialInsuranceOfficeDto.getHealthInsuOfficeRefCode1st(),
-				socialInsuranceOfficeDto.getHealthInsuOfficeRefCode2nd(),
-				socialInsuranceOfficeDto.getPensionOfficeRefCode1st(),
-				socialInsuranceOfficeDto.getPensionOfficeRefCode2nd(),
-				socialInsuranceOfficeDto.getWelfarePensionFundCode(),
-				socialInsuranceOfficeDto.getOfficePensionFundCode(), socialInsuranceOfficeDto.getHealthInsuCityCode(),
-				socialInsuranceOfficeDto.getHealthInsuOfficeSign(), socialInsuranceOfficeDto.getPensionCityCode(),
-				socialInsuranceOfficeDto.getPensionOfficeSign(), socialInsuranceOfficeDto.getHealthInsuOfficeCode(),
-				socialInsuranceOfficeDto.getHealthInsuAssoCode(), socialInsuranceOfficeDto.getMemo());
-
-		insuranceSocialService.add(SocialInsuranceOfficeDomain);
+	protected void handle(CommandHandlerContext<RegisterSocialOfficeCommand> context) {
+		RegisterSocialOfficeCommand command = context.getCommand();
+		// Get the current company code.
+		CompanyCode companyCode = new CompanyCode(AppContexts.user().companyCode());
+		
+		//convert to domain
+		SocialInsuranceOffice socialInsuranceOffice = command.toDomain(companyCode);
+		
+		// Validate
+		// unitPriceHistoryService.validateRequiredItem(unitPriceHistory);
+		// unitPriceHistoryService.validateDateRange(unitPriceHistory);
+		
+		socialInsuranceOfficeRepository.add(socialInsuranceOffice);
 		return;
 	}
 }
--- nts.uk/ctx/pr/core/nts.uk.ctx.pr.core.app/src/main/java/nts/uk/ctx/pr/core/app/insurance/social/command/UpdateSocialOfficeCommandHandler.java
+++ nts.uk/ctx/pr/core/nts.uk.ctx.pr.core.app/src/main/java/nts/uk/ctx/pr/core/app/insurance/social/command/UpdateSocialOfficeCommandHandler.java
@@ -7,18 +7,10 @@ import javax.transaction.Transactional;
 import nts.arc.layer.app.command.CommandHandler;
 import nts.arc.layer.app.command.CommandHandlerContext;
 import nts.uk.ctx.core.dom.company.CompanyCode;
-import nts.uk.ctx.pr.core.app.insurance.social.find.dto.SocialInsuranceOfficeDto;
 import nts.uk.ctx.pr.core.app.service.insurance.social.SocialInsuranceService;
-import nts.uk.ctx.pr.core.dom.insurance.Address;
-import nts.uk.ctx.pr.core.dom.insurance.KanaAddress;
-import nts.uk.ctx.pr.core.dom.insurance.OfficeCode;
-import nts.uk.ctx.pr.core.dom.insurance.OfficeName;
-import nts.uk.ctx.pr.core.dom.insurance.PicName;
-import nts.uk.ctx.pr.core.dom.insurance.PicPosition;
-import nts.uk.ctx.pr.core.dom.insurance.PotalCode;
-import nts.uk.ctx.pr.core.dom.insurance.ShortName;
 import nts.uk.ctx.pr.core.dom.insurance.social.SocialInsuranceOffice;
 import nts.uk.ctx.pr.core.dom.insurance.social.SocialInsuranceOfficeRepository;
+import nts.uk.shr.com.context.AppContexts;
 
 @Stateless
 public class UpdateSocialOfficeCommandHandler extends CommandHandler<UpdateSocialOfficeCommand> {
@@ -31,37 +23,23 @@ public class UpdateSocialOfficeCommandHandler extends CommandHandler<UpdateSocia
 
 	@Override
 	@Transactional
-	protected void handle(CommandHandlerContext<UpdateSocialOfficeCommand> command) {
-		// TODO Auto-generated method stub
-		SocialInsuranceOfficeDto socialInsuranceOfficeDto = command.getCommand().getSocialInsuranceOfficeDto();
+	protected void handle(CommandHandlerContext<UpdateSocialOfficeCommand> context) {
 
+		UpdateSocialOfficeCommand command = context.getCommand();
+		// Get the current company code.
+		CompanyCode companyCode = new CompanyCode(AppContexts.user().companyCode());
+		
 		// Convert Dto to Domain
-		SocialInsuranceOffice SocialInsuranceOfficeDomain = new SocialInsuranceOffice(
-				new CompanyCode(socialInsuranceOfficeDto.getCompanyCode()),
-				new OfficeCode(socialInsuranceOfficeDto.getCode()), new OfficeName(socialInsuranceOfficeDto.getName()),
-				new ShortName(socialInsuranceOfficeDto.getShortName()),
-				new PicName(socialInsuranceOfficeDto.getPicName()),
-				new PicPosition(socialInsuranceOfficeDto.getPicPosition()),
-				new PotalCode(socialInsuranceOfficeDto.getPotalCode()), socialInsuranceOfficeDto.getPrefecture(),
-				new Address(socialInsuranceOfficeDto.getAddress1st()),
-				new Address(socialInsuranceOfficeDto.getAddress2nd()),
-				new KanaAddress(socialInsuranceOfficeDto.getKanaAddress1st()),
-				new KanaAddress(socialInsuranceOfficeDto.getKanaAddress2nd()),
-				socialInsuranceOfficeDto.getPhoneNumber(), socialInsuranceOfficeDto.getHealthInsuOfficeRefCode1st(),
-				socialInsuranceOfficeDto.getHealthInsuOfficeRefCode2nd(),
-				socialInsuranceOfficeDto.getPensionOfficeRefCode1st(),
-				socialInsuranceOfficeDto.getPensionOfficeRefCode2nd(),
-				socialInsuranceOfficeDto.getWelfarePensionFundCode(),
-				socialInsuranceOfficeDto.getOfficePensionFundCode(), socialInsuranceOfficeDto.getHealthInsuCityCode(),
-				socialInsuranceOfficeDto.getHealthInsuOfficeSign(), socialInsuranceOfficeDto.getPensionCityCode(),
-				socialInsuranceOfficeDto.getPensionOfficeSign(), socialInsuranceOfficeDto.getHealthInsuOfficeCode(),
-				socialInsuranceOfficeDto.getHealthInsuAssoCode(), socialInsuranceOfficeDto.getMemo());
+		SocialInsuranceOffice socialInsuranceOffice = command.toDomain(companyCode);
+		
+		//TODO validate
+		
 		SocialInsuranceOffice findOffice = socialInsuranceOfficeRepository
-				.findById(command.getCommand().getSocialInsuranceOfficeDto().getCode().toString());
+				.findById(command.getCode().toString());
 		if (findOffice == null) {
 			// TODO show error message
 		} else {
-			insuranceSocialService.update(SocialInsuranceOfficeDomain);
+			insuranceSocialService.update(socialInsuranceOffice);
 		}
 		// TODO return item update
 		return;
