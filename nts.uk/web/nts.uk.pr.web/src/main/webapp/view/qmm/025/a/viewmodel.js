a28af46a81e70d1c772b34d3a339ed542828658d
Nguyen Hoang Son <son_hoang@nittsusystem-vn.com>
2017-02-13 15:28:48 +0700

    update code QMM025.a

--- nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qmm/025/a/viewmodel.js
+++ nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qmm/025/a/viewmodel.js
@@ -23,6 +23,104 @@ var qmm025;
             var ScreenModel = (function () {
                 function ScreenModel() {
                     var self = this;
+                    // checkbox square
+                    $.ig.checkboxMarkupClasses = "ui-state-default ui-corner-all ui-igcheckbox-small";
+                    $("#grid").igGrid({
+                        columns: [
+                            { headerText: "部門", key: "ID", dataType: "string", width: "100px" },
+                            { headerText: "コード", key: "Code", dataType: "string", width: "100px" },
+                            { headerText: "名称", key: "Name", dataType: "string", width: "120px" },
+                            { headerText: "住民税納付先", key: "TaxPaymentDestination", dataType: "string", width: "100px" },
+                            { headerText: "年税額", key: "AnnualTaxAmount", dataType: "string", width: "100px" },
+                            { headerText: "全月", key: "TotalMonth", dataType: "bool", width: "35px" },
+                            { headerText: "6月", key: "Jun", dataType: "string", width: "100px" },
+                            { headerText: "7月", key: "Jul", dataType: "string", width: "100px" },
+                            { headerText: "8月", key: "Aug", dataType: "string", width: "100px" },
+                            { headerText: "9月", key: "Sep", dataType: "string", width: "100px" },
+                            { headerText: "10月", key: "Oct", dataType: "string", width: "100px" },
+                            { headerText: "11月", key: "Nov", dataType: "string", width: "100px" },
+                            { headerText: "12月", key: "Dec", dataType: "string", width: "100px" },
+                            { headerText: "1月", key: "Jan", dataType: "string", width: "100px" },
+                            { headerText: "2月", key: "Feb", dataType: "string", width: "100px" },
+                            { headerText: "3月", key: "Mar", dataType: "string", width: "100px" },
+                            { headerText: "4月", key: "Apr", dataType: "string", width: "100px" },
+                            { headerText: "5月", key: "May", dataType: "string", width: "100px" },
+                        ],
+                        renderCheckboxes: true,
+                        primaryKey: 'Code',
+                        autoGenerateColumns: false,
+                        dataSource: nwCustomersWithOrders,
+                        width: "1200px",
+                        height: "500px",
+                        features: [
+                            {
+                                name: "ColumnFixing",
+                                showFixButtons: false,
+                                fixingDirection: "left",
+                                columnSettings: [
+                                    {
+                                        columnKey: "ID",
+                                        isFixed: true,
+                                        allowFixing: false
+                                    },
+                                    {
+                                        columnKey: "Code",
+                                        isFixed: true,
+                                        allowFixing: false
+                                    },
+                                    {
+                                        columnKey: "Name",
+                                        isFixed: true,
+                                        allowFixing: false
+                                    },
+                                    {
+                                        columnKey: "TaxPaymentDestination",
+                                        isFixed: true,
+                                        allowFixing: false
+                                    },
+                                    {
+                                        columnKey: "AnnualTaxAmount",
+                                        isFixed: true,
+                                        allowFixing: false,
+                                    },
+                                ]
+                            },
+                            {
+                                name: 'Paging',
+                                type: "local",
+                                pageSize: 20,
+                                pageSizeDropDownTrailingLabel: "件",
+                                /* pagerRecordsLabelTemplate: "$recordCount$ records", */
+                                nextPageLabelText: "",
+                                prevPageLabelText: "",
+                            },
+                            {
+                                name: 'Selection',
+                                multipleSelection: true,
+                                //allow use arrow keys for the selection of cells/rows
+                                activation: true,
+                                // click vao row, sau khi chuyen sang trang khac roi quay lai van click tai row do
+                                persist: true,
+                            },
+                            {
+                                name: "Updating",
+                                editMode: "cell",
+                                enableAddRow: false,
+                                enableDeleteRow: false,
+                                editCellStarting: function (evt, ui) {
+                                    if (ui.columnKey === "TotalMonth" && $(evt.originalEvent.target).hasClass("ui-icon-check")) {
+                                        ui.value = !ui.value;
+                                    }
+                                }
+                            },
+                            {
+                                name: 'RowSelectors',
+                                enableCheckBoxes: true,
+                                //enableSelectAllForPaging: false -> khong hien dong chu selectAllRow
+                                enableSelectAllForPaging: false,
+                            }
+                        ]
+                    });
                 }
                 ScreenModel.prototype.startPage = function () {
                     var self = this;
@@ -40,10 +138,6 @@ var qmm025;
                         readonly: ko.observable(false)
                     };
                     var dfd = $.Deferred();
-                    a.service.getPaymentDateProcessingList().done(function () {
-                        dfd.resolve();
-                    }).fail(function (res) {
-                    });
                     return dfd.promise();
                 };
                 return ScreenModel;
--- nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qmm/025/a/viewmodel.ts
+++ nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qmm/025/a/viewmodel.ts
@@ -14,14 +14,116 @@ var nwCustomersWithOrders = [{ "ID": "ALFKI", "Code": "A0000001", "Name": "Maria
     { "ID": "ANATR", "Code": "A00000014", "Name": "Ana Trujillo", "TaxPaymentDestination": "Newyork", "AnnualTaxAmount": "250,000", "TotalMonth": false, "Jun": "21,200円", "Jul": "21,200円", "Aug": "21,200円", "Sep": "21,200円", "Oct": "21,200円", "Nov": "21,200円", "Dec": "21,200円", "Jan": "21,200円", "Feb": "21,200円", "Mar": "21,200円", "Apr": "21,200円", "May": "21,200円" },
     { "ID": "ANATR", "Code": "A00000015", "Name": "Ana Trujillo", "TaxPaymentDestination": "Newyork", "AnnualTaxAmount": "250,000", "TotalMonth": false, "Jun": "21,200円", "Jul": "21,200円", "Aug": "21,200円", "Sep": "21,200円", "Oct": "21,200円", "Nov": "21,200円", "Dec": "21,200円", "Jan": "21,200円", "Feb": "21,200円", "Mar": "21,200円", "Apr": "21,200円", "May": "21,200円" },
 ];
+
 module qmm025.a.viewmodel {
     export class ScreenModel {
 
         texteditor1: any;
         nwCustomersWithOrders: any;
+
         constructor() {
             var self = this;
 
+            // checkbox square
+            $.ig.checkboxMarkupClasses = "ui-state-default ui-corner-all ui-igcheckbox-small";
+
+            $("#grid").igGrid({
+                columns: [
+                    { headerText: "部門", key: "ID", dataType: "string", width: "100px" },
+                    { headerText: "コード", key: "Code", dataType: "string", width: "100px" },
+                    { headerText: "名称", key: "Name", dataType: "string", width: "120px" },
+                    { headerText: "住民税納付先", key: "TaxPaymentDestination", dataType: "string", width: "100px" },
+                    { headerText: "年税額", key: "AnnualTaxAmount", dataType: "string", width: "100px" },
+                    { headerText: "全月", key: "TotalMonth", dataType: "bool", width: "35px" },
+                    { headerText: "6月", key: "Jun", dataType: "string", width: "100px" },
+                    { headerText: "7月", key: "Jul", dataType: "string", width: "100px" },
+                    { headerText: "8月", key: "Aug", dataType: "string", width: "100px" },
+                    { headerText: "9月", key: "Sep", dataType: "string", width: "100px" },
+                    { headerText: "10月", key: "Oct", dataType: "string", width: "100px" },
+                    { headerText: "11月", key: "Nov", dataType: "string", width: "100px" },
+                    { headerText: "12月", key: "Dec", dataType: "string", width: "100px" },
+                    { headerText: "1月", key: "Jan", dataType: "string", width: "100px" },
+                    { headerText: "2月", key: "Feb", dataType: "string", width: "100px" },
+                    { headerText: "3月", key: "Mar", dataType: "string", width: "100px" },
+                    { headerText: "4月", key: "Apr", dataType: "string", width: "100px" },
+                    { headerText: "5月", key: "May", dataType: "string", width: "100px" },
+                ],
+                renderCheckboxes: true,
+                primaryKey: 'Code',
+
+                autoGenerateColumns: false,
+                dataSource: nwCustomersWithOrders,
+                width: "1200px",
+                height: "500px",
+                features: [
+                    {
+                        name: "ColumnFixing",
+                        showFixButtons: false,
+                        fixingDirection: "left",
+                        columnSettings: [
+                            {
+                                columnKey: "ID",
+                                isFixed: true,
+                                allowFixing: false
+                            },
+                            {
+                                columnKey: "Code",
+                                isFixed: true,
+                                allowFixing: false
+                            },
+                            {
+                                columnKey: "Name",
+                                isFixed: true,
+                                allowFixing: false
+                            },
+                            {
+                                columnKey: "TaxPaymentDestination",
+                                isFixed: true,
+                                allowFixing: false
+                            },
+                            {
+                                columnKey: "AnnualTaxAmount",
+                                isFixed: true,
+                                allowFixing: false,
+                            },
+                        ]
+                    },
+                    {
+                        name: 'Paging',
+                        type: "local",
+                        pageSize: 20,
+                        pageSizeDropDownTrailingLabel: "件",
+                        /* pagerRecordsLabelTemplate: "$recordCount$ records", */
+                        nextPageLabelText: "",
+                        prevPageLabelText: "",
+                    },
+                    {
+                        name: 'Selection',
+                        multipleSelection: true,
+                        //allow use arrow keys for the selection of cells/rows
+                        activation: true,
+                        // click vao row, sau khi chuyen sang trang khac roi quay lai van click tai row do
+                        persist: true,
+                    },
+                    {
+                        name: "Updating",
+                        editMode: "cell",
+                        enableAddRow: false,
+                        enableDeleteRow: false,
+                        editCellStarting: function(evt, ui) {
+                            if (ui.columnKey === "TotalMonth" && $(evt.originalEvent.target).hasClass("ui-icon-check")) {
+                                ui.value = !ui.value;
+                            }
+                        }
+                    },
+                    {
+                        name: 'RowSelectors',
+                        enableCheckBoxes: true,
+                        //enableSelectAllForPaging: false -> khong hien dong chu selectAllRow
+                        enableSelectAllForPaging: false,
+                    }
+                ]
+            });
         }
 
         startPage(): JQueryPromise<any> {
@@ -34,7 +136,6 @@ module qmm025.a.viewmodel {
                     placeholder: "",
                     width: "45px",
                     textalign: "left"
-
                 })),
                 required: ko.observable(true),
                 enable: ko.observable(true),
@@ -42,11 +143,6 @@ module qmm025.a.viewmodel {
             };
 
             var dfd = $.Deferred();
-            service.getPaymentDateProcessingList().done(function() {
-                dfd.resolve();
-            }).fail(function(res) {
-
-            });
             return dfd.promise();
         }
     }
