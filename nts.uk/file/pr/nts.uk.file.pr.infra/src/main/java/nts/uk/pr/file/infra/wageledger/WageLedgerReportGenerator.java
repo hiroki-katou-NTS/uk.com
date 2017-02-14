a6324890a5750096b4cd34c34f9f1956ad7730da
nws_tiendd <tiendd_int@newwave.vn>
2017-02-14 18:02:02 +0700

    Fix error source.

--- nts.uk/file/pr/nts.uk.file.pr.infra/src/main/java/nts/uk/pr/file/infra/wageledger/WageLedgerReportGenerator.java
+++ nts.uk/file/pr/nts.uk.file.pr.infra/src/main/java/nts/uk/pr/file/infra/wageledger/WageLedgerReportGenerator.java
@@ -1,28 +1,28 @@
-package nts.uk.pr.file.infra.wageledger;
-
-import java.io.OutputStream;
-
-import com.aspose.cells.SaveFormat;
-import com.aspose.cells.Workbook;
-
-import lombok.val;
-import nts.arc.layer.infra.file.export.FileGenerator;
-import nts.arc.layer.infra.file.export.FileGeneratorContext;
-
-public class WageLedgerReportGenerator  extends FileGenerator{
-
-	@Override
-	protected void generate(FileGeneratorContext context) {
-		OutputStream os = this.createNewFile(context, "test.xlsx");
-		// Sample generate report file.
-		try {
-			val workbook = new Workbook();
-			workbook.getWorksheets().get(0).getCells().get("A1").setValue("Hello Aspose!");
-			workbook.save(os, SaveFormat.XLSX);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-		
-	}
-
-}
+//package nts.uk.pr.file.infra.wageledger;
+//
+//import java.io.OutputStream;
+//
+//import com.aspose.cells.SaveFormat;
+//import com.aspose.cells.Workbook;
+//
+//import lombok.val;
+//import nts.arc.layer.infra.file.export.FileGenerator;
+//import nts.arc.layer.infra.file.export.FileGeneratorContext;
+//
+//public class WageLedgerReportGenerator  extends FileGenerator{
+//
+//	@Override
+//	protected void generate(FileGeneratorContext context) {
+//		OutputStream os = this.createNewFile(context, "test.xlsx");
+//		// Sample generate report file.
+//		try {
+//			val workbook = new Workbook();
+//			workbook.getWorksheets().get(0).getCells().get("A1").setValue("Hello Aspose!");
+//			workbook.save(os, SaveFormat.XLSX);
+//		} catch (Exception e) {
+//			throw new RuntimeException(e);
+//		}
+//		
+//	}
+//
+//}
