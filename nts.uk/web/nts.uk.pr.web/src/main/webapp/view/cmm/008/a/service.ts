43dab870f9018187ccef98f5fd7ef63c747e842c
DuDT <du_dt@nittsusystem-vn.comdu_dt@nittsusystem-vn.com>
2017-02-16 11:08:00 +0700

    add service

---
+++ nts.uk/web/nts.uk.pr.web/src/main/webapp/view/cmm/008/a/service.ts
@@ -0,0 +1,10 @@
+module cmm008.a.service{
+    var path = {
+        getAllEmployment: "pr/proto/basic/employment/findallemployments",
+        createEmployment: "pr/proto/basic/employment/createemployment",
+        updateEmployment: "pr/proto/basic/employment/updateemployment",
+        deleteEmployment: "pr/proto/basic/employment/deleteemployment" 
+    }
+    
+    export function getAllEmployment() : 
+}
