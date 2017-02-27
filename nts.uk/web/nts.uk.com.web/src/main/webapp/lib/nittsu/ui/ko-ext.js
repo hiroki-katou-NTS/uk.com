711a9d50b074d5c11fed661905af0a397a3d6895
TruongQuocPhong <phong_tq@nittsusystem-vn.com>
2017-02-27 15:14:32 +0700

    update config

--- nts.uk/web/nts.uk.com.web/src/main/webapp/lib/nittsu/ui/ko-ext.js
+++ nts.uk/web/nts.uk.com.web/src/main/webapp/lib/nittsu/ui/ko-ext.js
@@ -428,7 +428,7 @@ var nts;
                         var searchBox = $(element);
                         var data = valueAccessor();
                         var fields = ko.unwrap(data.fields);
-                        var searchText = (data.searchText !== undefined) ? ko.unwrap(data.searchText) : "検索";
+                        var searchText = (data.searchText !== undefined) ? ko.unwrap(data.searchText) : "讀懃ｴ｢";
                         var selected = data.selected;
                         var selectedKey = null;
                         if (data.selectedKey) {
@@ -1609,8 +1609,8 @@ var nts;
                         }
                         else {
                             var displayColumns = [
-                                { headerText: "コード", key: optionsValue, dataType: "string", hidden: true },
-                                { headerText: "コード／名称", key: optionsText, dataType: "string" }
+                                { headerText: "繧ｳ繝ｼ繝・, key: optionsValue, dataType: ", string: ", hidden: true }, },
+                                { headerText: "繧ｳ繝ｼ繝会ｼ丞錐遘ｰ", key: optionsText, dataType: "string" }
                             ];
                         }
                         // Init ig grid.
@@ -2169,12 +2169,12 @@ var nts;
                                     .append("<input id = " + searchAreaId + "-input" + " class = 'ntsSearchInput ntsSearchBox'/>");
                                 $SearchArea.find(".ntsSearchButtonContainer")
                                     .append("<button id = " + searchAreaId + "-btn" + " class='ntsSearchButton search-btn caret-bottom'/>");
-                                $SearchArea.find(".ntsSearchInput").attr("placeholder", "コード・名称で検索・・・").keyup(function (event, ui) {
+                                $SearchArea.find(".ntsSearchInput").attr("placeholder", "繧ｳ繝ｼ繝峨・蜷咲ｧｰ縺ｧ讀懃ｴ｢繝ｻ繝ｻ繝ｻ").keyup(function (event, ui) {
                                     if (event.which === 13) {
                                         search($SearchArea, targetId, primaryKey);
                                     }
                                 });
-                                $SearchArea.find(".ntsSearchButton").text("検索").click(function (event, ui) {
+                                $SearchArea.find(".ntsSearchButton").text("讀懃ｴ｢").click(function (event, ui) {
                                     search($SearchArea, targetId, primaryKey);
                                 });
                             };
