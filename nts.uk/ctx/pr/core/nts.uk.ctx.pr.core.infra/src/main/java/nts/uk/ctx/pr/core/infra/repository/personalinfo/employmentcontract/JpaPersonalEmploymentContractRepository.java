bfb63e5df58b2664a64873e54e2303eabd5fe9df
DuDT <du_dt@nittsusystem-vn.comdu_dt@nittsusystem-vn.com>
2017-02-28 10:35:41 +0700

    sua

--- nts.uk/ctx/pr/core/nts.uk.ctx.pr.core.infra/src/main/java/nts/uk/ctx/pr/core/infra/repository/personalinfo/employmentcontract/JpaPersonalEmploymentContractRepository.java
+++ nts.uk/ctx/pr/core/nts.uk.ctx.pr.core.infra/src/main/java/nts/uk/ctx/pr/core/infra/repository/personalinfo/employmentcontract/JpaPersonalEmploymentContractRepository.java
@@ -1,6 +1,5 @@
 package nts.uk.ctx.pr.core.infra.repository.personalinfo.employmentcontract;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -14,8 +13,6 @@ import nts.arc.time.GeneralDate;
 import nts.gul.collection.ListUtil;
 import nts.uk.ctx.pr.core.dom.personalinfo.employmentcontract.PersonalEmploymentContract;
 import nts.uk.ctx.pr.core.dom.personalinfo.employmentcontract.PersonalEmploymentContractRepository;
-import nts.uk.ctx.pr.core.infra.LocalDateConverter;
-import nts.uk.ctx.pr.core.infra.entity.personalinfo.commute.PprmtPersonCommute;
 import nts.uk.ctx.pr.core.infra.entity.personalinfo.employmentcontract.PclmtPersonEmpContract;
 import nts.uk.ctx.pr.core.infra.entity.personalinfo.employmentcontract.PclmtPersonEmpContractPK;
 
@@ -34,7 +31,7 @@ public class JpaPersonalEmploymentContractRepository extends JpaRepository
 	
 
 	@Override
-	public List<PersonalEmploymentContract> findAll(String companyCode, List<String> personIds, LocalDate baseYmd) {
+	public List<PersonalEmploymentContract> findAll(String companyCode, List<String> personIds, GeneralDate baseYmd) {
 		List<PersonalEmploymentContract> results = new ArrayList<>();
 		ListUtil.split(personIds, DbConsts.MAX_CONDITIONS_OF_IN_STATEMENT, personIdList -> {
 			this.queryProxy().query(SELECT_BY_CCD_PID_STRD_ENDD, PclmtPersonEmpContract.class)
@@ -47,20 +44,20 @@ public class JpaPersonalEmploymentContractRepository extends JpaRepository
 	private static PersonalEmploymentContract toDomain(PclmtPersonEmpContract entity) {
 		val domain = PersonalEmploymentContract.createFromJavaType(entity.payrollSystem,
 				entity.pclmtPersonEmpContractPK.pId, entity.pclmtPersonEmpContractPK.pId,
-				GeneralDate.localDate(entity.pclmtPersonEmpContractPK.strD), GeneralDate.localDate(entity.endD));
+				entity.pclmtPersonEmpContractPK.strD, entity.endD);
 		//entity.toDomain(domain);
 		return domain;
 	}
 
 	@Override
-	public Optional<PersonalEmploymentContract> find(String companyCode, String personId, LocalDate baseYmd) {
+	public Optional<PersonalEmploymentContract> find(String companyCode, String personId, GeneralDate baseYmd) {
 		return this.queryProxy()
 				.find(new PclmtPersonEmpContractPK(companyCode, personId, baseYmd), PclmtPersonEmpContract.class)
 				.map(c -> toDomain(c));
 	}
 
 	@Override
-	public Optional<PersonalEmploymentContract> findActive(String companyCode, String personId, LocalDate baseYmd) {
+	public Optional<PersonalEmploymentContract> findActive(String companyCode, String personId, GeneralDate baseYmd) {
 		List<PersonalEmploymentContract> empContractList = this.queryProxy().query(SEL_2, PclmtPersonEmpContract.class)
 				.setParameter("ccd", companyCode)
 				.setParameter("pId", personId)
@@ -71,6 +68,9 @@ public class JpaPersonalEmploymentContractRepository extends JpaRepository
 			return Optional.empty();
 		}
 		
+		return Optional.of(empContractList.get(0));
+	}
+}
 		return Optional.of(empContractList.get(0));
 	}
 }
