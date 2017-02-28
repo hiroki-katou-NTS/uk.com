5c7dab9d6bfaf19316d203fdc1fd38504f6dc7f2
nws_tiendd <tiendd_int@newwave.vn>
2017-02-28 13:36:59 +0700

    update infra wage ledger.

--- nts.uk/ctx/pr/report/nts.uk.ctx.pr.report.infra/src/main/java/nts/uk/ctx/pr/report/infra/repository/wageledger/JpaWLOutputSettingRepository.java
+++ nts.uk/ctx/pr/report/nts.uk.ctx.pr.report.infra/src/main/java/nts/uk/ctx/pr/report/infra/repository/wageledger/JpaWLOutputSettingRepository.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2015 Nittsu System to present.                   *
  * All right reserved.                                            *
  *****************************************************************/
-package nts.uk.ctx.pr.report.infra.wageledger;
+package nts.uk.ctx.pr.report.infra.repository.wageledger;
 
 import javax.ejb.Stateless;
 
