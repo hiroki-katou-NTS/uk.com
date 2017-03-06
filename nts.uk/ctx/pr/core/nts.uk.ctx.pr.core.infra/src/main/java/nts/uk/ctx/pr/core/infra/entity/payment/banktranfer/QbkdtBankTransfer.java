4e1f5508831d7a211c10269e7ead4f837d5cfc99
NSVN-BuiVanChinh <chinh_bv@nittsusystem-vn.com>
2017-03-06 16:46:04 +0700

    add entity QbkdtBankTranfer

--- nts.uk/ctx/pr/core/nts.uk.ctx.pr.core.infra/src/main/java/nts/uk/ctx/pr/core/infra/entity/payment/banktranfer/QbkdtBankTransfer.java
+++ nts.uk/ctx/pr/core/nts.uk.ctx.pr.core.infra/src/main/java/nts/uk/ctx/pr/core/infra/entity/payment/banktranfer/QbkdtBankTransfer.java
@@ -1,23 +1,77 @@
 package nts.uk.ctx.pr.core.infra.entity.payment.banktranfer;
 
+import java.math.BigDecimal;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import nts.arc.time.GeneralDate;
+import nts.uk.shr.infra.data.entity.TableEntity;
 
-@AllArgsConstructor
-@Data
-@NoArgsConstructor
+
 @Entity
 @Table(name="QBKDT_BANK_TRANSFER")
-public class QbkdtBankTransfer {
+public class QbkdtBankTransfer extends TableEntity {
 	
-	@EmbeddedId
-	public QbkdtBankTransferPk qbkdtBankTransferPk;
-	
-	
-	
+    @EmbeddedId
+    public QbkdtBankTransferPK qbkdtBankTransferPK;
+    
+    @Basic(optional = false)
+    @Column(name = "CNAME_KANA")
+    public String cnameKana;
+    @Basic(optional = false)
+    @Column(name = "FROM_BANK_KN_NAME")
+    public String fromBankKnName;
+    @Basic(optional = false)
+    @Column(name = "FROM_BRANCH_KN_NAME")
+    public String fromBranchKnName;
+    @Basic(optional = false)
+    @Column(name = "TO_BANK_KN_NAME")
+    public String toBankKnName;
+    @Basic(optional = false)
+    @Column(name = "TO_BRANCH_KN_NAME")
+    public String toBranchKnName;
+    @Basic(optional = false)
+    @Column(name = "TO_ACCOUNT_KN_NAME")
+    public String toAccountKnName;
+    @Basic(optional = false)
+    @Column(name = "DEPCD")
+    public String depcd;
+    @Basic(optional = false)
+    @Column(name = "PAYMENT_MNY")
+    public BigDecimal paymentMny;
+    @Basic(optional = false)
+    @Column(name = "PROCESSING_NO")
+    public int processingNo;
+    @Basic(optional = false)
+    @Column(name = "PROCESSING_YM")
+    public int processingYm;
+
+    public QbkdtBankTransfer() {
+    }
+
+    public QbkdtBankTransfer(QbkdtBankTransferPK qbkdtBankTransferPK) {
+        this.qbkdtBankTransferPK = qbkdtBankTransferPK;
+    }
+
+    public QbkdtBankTransfer(QbkdtBankTransferPK qbkdtBankTransferPK, String cnameKana, String fromBankKnName, String fromBranchKnName, String toBankKnName, String toBranchKnName, String toAccountKnName, String depcd, BigDecimal paymentMny, int processingNo, int processingYm) {
+        this.qbkdtBankTransferPK = qbkdtBankTransferPK;
+        this.cnameKana = cnameKana;
+        this.fromBankKnName = fromBankKnName;
+        this.fromBranchKnName = fromBranchKnName;
+        this.toBankKnName = toBankKnName;
+        this.toBranchKnName = toBranchKnName;
+        this.toAccountKnName = toAccountKnName;
+        this.depcd = depcd;
+        this.paymentMny = paymentMny;
+        this.processingNo = processingNo;
+        this.processingYm = processingYm;
+    }
+
+    public QbkdtBankTransfer(String ccd, String pid, String fromBankCd, String fromBranchCd, int fromAccountAtr, String fromAccountNo, String toBankCd, String toBranchCd, int toAccountAtr, String toAccountNo, int payBonusAtr, GeneralDate payDate, int sparePayAtr) {
+        this.qbkdtBankTransferPK = new QbkdtBankTransferPK(ccd, pid, fromBankCd, fromBranchCd, fromAccountAtr, fromAccountNo, toBankCd, toBranchCd, toAccountAtr, toAccountNo, payBonusAtr, payDate, sparePayAtr);
+    }
 }
---
+++ nts.uk/ctx/pr/core/nts.uk.ctx.pr.core.infra/src/main/java/nts/uk/ctx/pr/core/infra/entity/payment/banktranfer/QbkdtBankTransferPK.java
@@ -0,0 +1,70 @@
+package nts.uk.ctx.pr.core.infra.entity.payment.banktranfer;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+import nts.arc.time.GeneralDate;
+
+@Embeddable
+public class QbkdtBankTransferPK {
+	@Basic(optional = false)
+    @Column(name = "CCD")
+    public String ccd;
+    @Basic(optional = false)
+    @Column(name = "PID")
+    public String pid;
+    @Basic(optional = false)
+    @Column(name = "FROM_BANK_CD")
+    public String fromBankCd;
+    @Basic(optional = false)
+    @Column(name = "FROM_BRANCH_CD")
+    public String fromBranchCd;
+    @Basic(optional = false)
+    @Column(name = "FROM_ACCOUNT_ATR")
+    public int fromAccountAtr;
+    @Basic(optional = false)
+    @Column(name = "FROM_ACCOUNT_NO")
+    public String fromAccountNo;
+    @Basic(optional = false)
+    @Column(name = "TO_BANK_CD")
+    public String toBankCd;
+    @Basic(optional = false)
+    @Column(name = "TO_BRANCH_CD")
+    public String toBranchCd;
+    @Basic(optional = false)
+    @Column(name = "TO_ACCOUNT_ATR")
+    public int toAccountAtr;
+    @Basic(optional = false)
+    @Column(name = "TO_ACCOUNT_NO")
+    public String toAccountNo;
+    @Basic(optional = false)
+    @Column(name = "PAY_BONUS_ATR")
+    public int payBonusAtr;
+    @Basic(optional = false)
+    @Column(name = "PAY_DATE")
+    public GeneralDate payDate;
+    @Basic(optional = false)
+    @Column(name = "SPARE_PAY_ATR")
+    public int sparePayAtr;
+
+    public QbkdtBankTransferPK() {
+    }
+
+    public QbkdtBankTransferPK(String ccd, String pid, String fromBankCd, String fromBranchCd, int fromAccountAtr, String fromAccountNo, String toBankCd, String toBranchCd, int toAccountAtr, String toAccountNo, int payBonusAtr, GeneralDate payDate, int sparePayAtr) {
+        this.ccd = ccd;
+        this.pid = pid;
+        this.fromBankCd = fromBankCd;
+        this.fromBranchCd = fromBranchCd;
+        this.fromAccountAtr = fromAccountAtr;
+        this.fromAccountNo = fromAccountNo;
+        this.toBankCd = toBankCd;
+        this.toBranchCd = toBranchCd;
+        this.toAccountAtr = toAccountAtr;
+        this.toAccountNo = toAccountNo;
+        this.payBonusAtr = payBonusAtr;
+        this.payDate = payDate;
+        this.sparePayAtr = sparePayAtr;
+    }
+
+}
