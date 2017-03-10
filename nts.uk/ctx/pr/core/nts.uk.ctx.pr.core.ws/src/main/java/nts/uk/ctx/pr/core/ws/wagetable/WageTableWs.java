eb9f5796856d423e4c202d9202057df53734d0d5
Nws_ThanhNC <thanhnc@newwave.vn>
2017-03-10 15:55:01 +0700

    Update QMM016 ws.

--- nts.uk/ctx/pr/core/nts.uk.ctx.pr.core.ws/src/main/java/nts/uk/ctx/pr/core/ws/wagetable/WageTableWs.java
+++ nts.uk/ctx/pr/core/nts.uk.ctx.pr.core.ws/src/main/java/nts/uk/ctx/pr/core/ws/wagetable/WageTableWs.java
@@ -4,8 +4,6 @@
  *****************************************************************/
 package nts.uk.ctx.pr.core.ws.wagetable;
 
-import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -18,33 +16,21 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import nts.uk.ctx.core.dom.company.CompanyCode;
 import nts.uk.ctx.pr.core.app.wagetable.command.WageTableHistoryAddCommand;
 import nts.uk.ctx.pr.core.app.wagetable.command.WageTableHistoryAddCommandHandler;
 import nts.uk.ctx.pr.core.app.wagetable.command.WageTableHistoryUpdateCommand;
 import nts.uk.ctx.pr.core.app.wagetable.command.WageTableHistoryUpdateCommandHandler;
-import nts.uk.ctx.pr.core.app.wagetable.command.dto.WageTableDemensionDetailDto;
-import nts.uk.ctx.pr.core.app.wagetable.command.dto.WageTableHeadDto;
-import nts.uk.ctx.pr.core.app.wagetable.command.dto.WageTableHistoryDto;
-import nts.uk.ctx.pr.core.app.wagetable.command.dto.WageTableItemDto;
 import nts.uk.ctx.pr.core.dom.base.simplehistory.SimpleHistoryBaseService;
-import nts.uk.ctx.pr.core.dom.wagetable.DemensionNo;
-import nts.uk.ctx.pr.core.dom.wagetable.ElementCount;
-import nts.uk.ctx.pr.core.dom.wagetable.ElementType;
 import nts.uk.ctx.pr.core.dom.wagetable.WageTableCode;
 import nts.uk.ctx.pr.core.dom.wagetable.WageTableHead;
 import nts.uk.ctx.pr.core.dom.wagetable.WageTableHeadRepository;
-import nts.uk.ctx.pr.core.dom.wagetable.WtElementRefNo;
-import nts.uk.ctx.pr.core.dom.wagetable.element.CodeItem;
-import nts.uk.ctx.pr.core.dom.wagetable.element.RangeItem;
-import nts.uk.ctx.pr.core.dom.wagetable.element.RefMode;
-import nts.uk.ctx.pr.core.dom.wagetable.element.StepMode;
 import nts.uk.ctx.pr.core.dom.wagetable.history.WageTableHistory;
 import nts.uk.ctx.pr.core.dom.wagetable.history.WageTableHistoryRepository;
 import nts.uk.ctx.pr.core.dom.wagetable.history.service.WageTableHistoryService;
 import nts.uk.ctx.pr.core.ws.base.simplehistory.SimpleHistoryWs;
 import nts.uk.ctx.pr.core.ws.base.simplehistory.dto.HistoryModel;
 import nts.uk.ctx.pr.core.ws.base.simplehistory.dto.MasterModel;
+import nts.uk.ctx.pr.core.ws.wagetable.dto.WageTableHeadDto;
 import nts.uk.ctx.pr.core.ws.wagetable.dto.WageTableHistoryModel;
 import nts.uk.shr.com.context.AppContexts;
 
@@ -88,20 +74,22 @@ public class WageTableWs extends SimpleHistoryWs<WageTableHead, WageTableHistory
 		// Get the detail history.
 		Optional<WageTableHistory> optWageTableHistory = this.wageTableHistoryRepo
 				.findHistoryByUuid(id);
-		WageTableHistoryModel dto = null;
+		WageTableHistoryModel historyDto = null;
 
 		// Check exsit.
 		if (optWageTableHistory.isPresent()) {
 			WageTableHistory wageTableHistory = optWageTableHistory.get();
 			Optional<WageTableHead> optWageTable = this.wageTableHeadRepo.findByCode(
 					wageTableHistory.getCompanyCode().v(), wageTableHistory.getWageTableCode().v());
-			dto = WageTableHistoryModel.builder().build();
-			wageTableHistory.saveToMemento(dto);
-			dto.setWageTableCode(optWageTable.get().getCode());
+			historyDto = new WageTableHistoryModel();
+			WageTableHeadDto headDto = WageTableHeadDto.builder().build();
+			wageTableHistory.saveToMemento(historyDto);
+			optWageTable.get().saveToMemento(headDto);
+			historyDto.setHead(headDto);
 		}
 
 		// Return
-		return dto;
+		return historyDto;
 	}
 
 	/**
@@ -113,58 +101,65 @@ public class WageTableWs extends SimpleHistoryWs<WageTableHead, WageTableHistory
 	 */
 	@POST
 	@Path("create")
-	public HistoryModel create() {
-		WageTableHistoryAddCommand command = null;
-		CompanyCode companyCode = new CompanyCode(AppContexts.user().companyCode());
-
-		List<CodeItem> codeItems = Arrays.asList(new CodeItem("refCode1", "uuid1"),
-				new CodeItem("refCode2", "uuid2"));
-
-		RefMode refMode = new RefMode(ElementType.MASTER_REF, companyCode,
-				new WtElementRefNo("tb01"), codeItems);
-
-		List<RangeItem> rangeItems = Arrays.asList(new RangeItem(1, 1d, 5d, "uuid3"),
-				new RangeItem(2, 6d, 10d, "uuid4"));
-
-		StepMode stepModeDto = new StepMode(ElementType.AGE_FIX, BigDecimal.valueOf(1),
-				BigDecimal.valueOf(10), BigDecimal.valueOf(5), rangeItems);
-
-		WageTableDemensionDetailDto demensionDetailDto1 = new WageTableDemensionDetailDto();
-		demensionDetailDto1.setDemensionNo(DemensionNo.DEMENSION_1ST);
-		demensionDetailDto1.setElementModeSetting(refMode);
-
-		WageTableDemensionDetailDto demensionDetailDto2 = new WageTableDemensionDetailDto();
-		demensionDetailDto2.setDemensionNo(DemensionNo.DEMENSION_2ND);
-		demensionDetailDto2.setElementModeSetting(stepModeDto);
-
-		List<WageTableDemensionDetailDto> demensionDetails = Arrays.asList(demensionDetailDto1,
-				demensionDetailDto2);
-
-		WageTableItemDto wageTableItemDto = new WageTableItemDto();
-		wageTableItemDto.setElement1Id("uuid1");
-		wageTableItemDto.setElement2Id("uuid3");
-		// wageTableItemDto.setElement3Id("element3Id");
-		wageTableItemDto.setAmount(BigDecimal.valueOf(111111));
-
-		List<WageTableItemDto> valueItems = Arrays.asList(wageTableItemDto);
-
-		WageTableHistoryDto wageTableHistoryDto = new WageTableHistoryDto();
-		wageTableHistoryDto.setStartMonth("2016/08");
-		wageTableHistoryDto.setEndMonth("2016/09");
-		wageTableHistoryDto.setDemensionDetails(demensionDetails);
-		wageTableHistoryDto.setValueItems(valueItems);
-
-		WageTableHeadDto wageTableHeadDto = new WageTableHeadDto();
-		wageTableHeadDto.setCode("001");
-		wageTableHeadDto.setName("WageTable1");
-		wageTableHeadDto.setDemensionSet(ElementCount.Two.value);
-		wageTableHeadDto.setMemo("memo");
-		wageTableHeadDto.setDemensionDetails(demensionDetails);
-
-		command = new WageTableHistoryAddCommand();
-		command.setCreateHeader(true);
-		command.setWageTableHistoryDto(wageTableHistoryDto);
-		command.setWageTableHeadDto(wageTableHeadDto);
+	public HistoryModel create(WageTableHistoryAddCommand command) {
+		// WageTableHistoryAddCommand command = null;
+		// CompanyCode companyCode = new
+		// CompanyCode(AppContexts.user().companyCode());
+		//
+		// List<CodeItem> codeItems = Arrays.asList(new CodeItem("refCode1",
+		// "uuid1"),
+		// new CodeItem("refCode2", "uuid2"));
+		//
+		// RefMode refMode = new RefMode(ElementType.MASTER_REF, companyCode,
+		// new WtElementRefNo("tb01"), codeItems);
+		//
+		// List<RangeItem> rangeItems = Arrays.asList(new RangeItem(1, 1d, 5d,
+		// "uuid3"),
+		// new RangeItem(2, 6d, 10d, "uuid4"));
+		//
+		// StepMode stepModeDto = new StepMode(ElementType.AGE_FIX,
+		// BigDecimal.valueOf(1),
+		// BigDecimal.valueOf(10), BigDecimal.valueOf(5), rangeItems);
+		//
+		// WageTableDemensionDetailDto demensionDetailDto1 = new
+		// WageTableDemensionDetailDto();
+		// demensionDetailDto1.setDemensionNo(DemensionNo.DEMENSION_1ST);
+		// demensionDetailDto1.setElementModeSetting(refMode);
+		//
+		// WageTableDemensionDetailDto demensionDetailDto2 = new
+		// WageTableDemensionDetailDto();
+		// demensionDetailDto2.setDemensionNo(DemensionNo.DEMENSION_2ND);
+		// demensionDetailDto2.setElementModeSetting(stepModeDto);
+		//
+		// List<WageTableDemensionDetailDto> demensionDetails =
+		// Arrays.asList(demensionDetailDto1,
+		// demensionDetailDto2);
+		//
+		// WageTableItemDto wageTableItemDto = new WageTableItemDto();
+		// wageTableItemDto.setElement1Id("uuid1");
+		// wageTableItemDto.setElement2Id("uuid3");
+		// // wageTableItemDto.setElement3Id("element3Id");
+		// wageTableItemDto.setAmount(BigDecimal.valueOf(111111));
+		//
+		// List<WageTableItemDto> valueItems = Arrays.asList(wageTableItemDto);
+		//
+		// WageTableHistoryDto wageTableHistoryDto = new WageTableHistoryDto();
+		// wageTableHistoryDto.setStartMonth("2016/08");
+		// wageTableHistoryDto.setEndMonth("2016/09");
+		// wageTableHistoryDto.setDemensionDetails(demensionDetails);
+		// wageTableHistoryDto.setValueItems(valueItems);
+		//
+		// WageTableHeadDto wageTableHeadDto = new WageTableHeadDto();
+		// wageTableHeadDto.setCode("001");
+		// wageTableHeadDto.setName("WageTable1");
+		// wageTableHeadDto.setDemensionSet(ElementCount.Two.value);
+		// wageTableHeadDto.setMemo("memo");
+		// wageTableHeadDto.setDemensionDetails(demensionDetails);
+		//
+		// command = new WageTableHistoryAddCommand();
+		// command.setCreateHeader(true);
+		// command.setWageTableHistoryDto(wageTableHistoryDto);
+		// command.setWageTableHeadDto(wageTableHeadDto);
 
 		WageTableHistory history = this.createWageTableHistoryCommandHandler.handle(command);
 		return HistoryModel.builder().uuid(history.getUuid()).start(history.getStart().v())
