246f12c50720f8fb2b71db0c5019fd40b2207ddf
NSVC\sonnh <hongsonndvn2014@gmail.com>
2017-03-15 17:16:59 +0700

    Update code QMM013 create object PersonalUnitPrice

--- nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qmm/013/b/start.ts
+++
@@ -1,8 +0,0 @@
-module qmm013.b {
-    __viewContext.ready(function() {
-        var screenModel = new qmm013.b.viewmodel.ScreenModel();
-        screenModel.startPage().done(function() {
-            __viewContext.bind(screenModel);
-        });
-    });
-}
