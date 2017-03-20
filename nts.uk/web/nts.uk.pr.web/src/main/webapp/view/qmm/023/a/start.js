ec27c498949f76ee5b205aacab4e2c75d416b269
Vu Tien Lam <lam_vt@nittsusystem-vn.com>
2017-03-20 16:38:33 +0700

    update confirmSave for dirtyChecker

--- nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qmm/023/a/start.js
+++ nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qmm/023/a/start.js
@@ -7,6 +7,7 @@ var qmm023;
             __viewContext.ready(function () {
                 var screenModel = new qmm023.a.viewmodel.ScreenModel();
                 screenModel.startPage().done(function () {
+                    nts.uk.ui.confirmSave(screenModel.currentTaxDirty);
                     __viewContext.bind(screenModel);
                 });
             });
