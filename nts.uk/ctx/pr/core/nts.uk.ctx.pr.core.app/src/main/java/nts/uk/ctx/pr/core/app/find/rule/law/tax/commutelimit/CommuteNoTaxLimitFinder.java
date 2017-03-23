94645294d22ec11c0c3b22e62f8cd5fb9497fe65
Vu Cong Tuong <tuong_vc@nittsusystem-vn.com>
2017-03-23 15:53:20 +0700

    update webservice qmm023

--- nts.uk/ctx/pr/core/nts.uk.ctx.pr.core.app/src/main/java/nts/uk/ctx/pr/core/app/find/rule/law/tax/commutelimit/CommuteNoTaxLimitFinder.java
+++ nts.uk/ctx/pr/core/nts.uk.ctx.pr.core.app/src/main/java/nts/uk/ctx/pr/core/app/find/rule/law/tax/commutelimit/CommuteNoTaxLimitFinder.java
@@ -1,11 +1,13 @@
 package nts.uk.ctx.pr.core.app.find.rule.law.tax.commutelimit;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+import nts.uk.ctx.pr.core.dom.rule.law.tax.commutelimit.CommuteNoTaxLimit;
 import nts.uk.ctx.pr.core.dom.rule.law.tax.commutelimit.CommuteNoTaxLimitRepository;
 import nts.uk.shr.com.context.AppContexts;
 
@@ -21,4 +23,13 @@ public class CommuteNoTaxLimitFinder {
 				.map(item -> CommuteNoTaxLimitDto.getValuesFromDomain(item)).collect(Collectors.toList());
 	}
 
+	public CommuteNoTaxLimitDto getCommuteNoTaxLimit(String taxLimitCode) {
+		String companyCode = AppContexts.user().companyCode();
+		Optional<CommuteNoTaxLimit> exitCommuteNoTaxLimit = this.repository.getCommuteNoTaxLimit(companyCode,
+				taxLimitCode);
+		if(!exitCommuteNoTaxLimit.isPresent()){
+			return null;
+		}
+		return CommuteNoTaxLimitDto.getValuesFromDomain(exitCommuteNoTaxLimit.get());
+	}
 }
