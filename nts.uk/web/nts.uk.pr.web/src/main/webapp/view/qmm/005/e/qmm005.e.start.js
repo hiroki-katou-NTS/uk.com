4057bc637b2bdc843fa3ce47eeee5b6244df88e0
NSVN-Nguyen Van Vuong <vuong_nv@nittsusystem-vn.com>
2017-03-24 15:39:08 +0700

    update mapping class design with java package

--- nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qmm/005/e/qmm005.e.start.js
+++
@@ -1,9 +0,0 @@
-var qmm005;
-(function (qmm005) {
-    var e;
-    (function (e) {
-        __viewContext.ready(function () {
-            __viewContext.bind(new e.ViewModel());
-        });
-    })(e = qmm005.e || (qmm005.e = {}));
-})(qmm005 || (qmm005 = {}));
