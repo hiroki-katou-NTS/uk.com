2a640e6d86fded07da9a3050b8410b3550d84847
NSVN-NguyenLeBaoSon <son_nlb@nittsusystem-vn.com>
2017-03-24 15:51:59 +0700

    Add @Stateless @Transactional for class

--- nts.uk/ctx/pr/core/nts.uk.ctx.pr.core.app/src/main/java/nts/uk/ctx/pr/core/app/command/itemmaster/itemsalarybd/UpdateItemSalaryBDCommandHandler.java
+++ nts.uk/ctx/pr/core/nts.uk.ctx.pr.core.app/src/main/java/nts/uk/ctx/pr/core/app/command/itemmaster/itemsalarybd/UpdateItemSalaryBDCommandHandler.java
@@ -1,13 +1,16 @@
 package nts.uk.ctx.pr.core.app.command.itemmaster.itemsalarybd;
 
+import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 
 import nts.arc.error.BusinessException;
 import nts.arc.error.RawErrorMessage;
 import nts.arc.layer.app.command.CommandHandler;
 import nts.arc.layer.app.command.CommandHandlerContext;
 import nts.uk.ctx.pr.core.dom.itemmaster.itemsalarybd.ItemSalaryBDRepository;
-
+@Stateless
+@Transactional
 public class UpdateItemSalaryBDCommandHandler extends CommandHandler<UpdateItemSalaryBDCommand> {
 
 	@Inject
