794a626b9abd8ef73b0e7767c1d852408bebea55
nws_tiendd <tiendd_int@newwave.vn>
2017-03-24 17:14:49 +0700

    Add Wage Ledger report new layout.

--- nts.uk/file/pr/nts.uk.file.pr.infra/src/main/java/nts/uk/pr/file/infra/wageledger/WageLedgerBaseGenerator.java
+++ nts.uk/file/pr/nts.uk.file.pr.infra/src/main/java/nts/uk/pr/file/infra/wageledger/WageLedgerBaseGenerator.java
@@ -4,13 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.aspose.cells.Cell;
+import com.aspose.cells.Color;
 import com.aspose.cells.FindOptions;
 import com.aspose.cells.LookInType;
 import com.aspose.cells.Worksheet;
 
+import nts.uk.file.pr.app.export.wageledger.data.share.HeaderReportData;
+import nts.uk.shr.infra.file.report.aspose.cells.AsposeCellsReportContext;
 import nts.uk.shr.infra.file.report.aspose.cells.AsposeCellsReportGenerator;
 
 public class WageLedgerBaseGenerator extends AsposeCellsReportGenerator{
+	
+	/** The Constant BLUE_COLOR. */
+	protected static final Color BLUE_COLOR = Color.fromArgb(197, 241, 247);
+	
+	/** The Constant GREEN_COLOR. */
+	protected static final Color GREEN_COLOR = Color.fromArgb(199, 243, 145);
 	
 	/**
 	 * Safe sub list.
@@ -48,5 +57,41 @@ public class WageLedgerBaseGenerator extends AsposeCellsReportGenerator{
 		Cell cell = ws.getCells().find(content, null, findOptions);
 		
 		return cell;
+	}
+	
+	
+	/**
+	 * Fill header data.
+	 *
+	 * @param reportContext the report context
+	 * @param headerData the header data
+	 */
+	protected void fillHeaderData(AsposeCellsReportContext reportContext, HeaderReportData headerData) {
+		Worksheet ws = reportContext.getDesigner().getWorkbook().getWorksheets().get(0);
+		String infoPadding = "        ";
+		
+		// Fill Department Label.
+		Cell depLabelCell = this.findCellWithContent(ws, "DepartmentLabel");
+		depLabelCell.setValue("【部門】");
+		
+		// Fill Department info.
+		Cell depInfo = this.findCellWithContent(ws, "DepartmentInfo");
+		depInfo.setValue(headerData.departmentCode + infoPadding + headerData.departmentName);
+		
+		// Fill Employee label.
+		Cell empLabel = this.findCellWithContent(ws, "EmployeeLabel");
+		empLabel.setValue("【社員】");
+		
+		// Fill Employee info.
+		Cell empInfo = this.findCellWithContent(ws, "EmployeeInfo");
+		empInfo.setValue(headerData.employeeCode + infoPadding + headerData.departmentName);
+		
+		// Fill Sex label.
+		Cell sexLabelCell = this.findCellWithContent(ws, "SexLabel");
+		sexLabelCell.setValue("【性別】");
+		
+		// Fill Sex Info.
+		Cell sexInfoCell = this.findCellWithContent(ws, "SexInfo");
+		sexInfoCell.setValue(headerData.sex);
 	}
 }
