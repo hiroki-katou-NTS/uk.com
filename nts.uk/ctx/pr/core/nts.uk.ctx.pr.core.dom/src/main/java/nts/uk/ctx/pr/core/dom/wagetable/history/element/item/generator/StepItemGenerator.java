f3a8591d021a6e6566d8e711c2bebbc2f3533e68
Nws_ThanhNC <thanhnc@newwave.vn>
2017-03-28 14:50:37 +0700

    Update StepItemGenerator.

--- nts.uk/ctx/pr/core/nts.uk.ctx.pr.core.dom/src/main/java/nts/uk/ctx/pr/core/dom/wagetable/history/element/item/generator/StepItemGenerator.java
+++ nts.uk/ctx/pr/core/nts.uk.ctx.pr.core.dom/src/main/java/nts/uk/ctx/pr/core/dom/wagetable/history/element/item/generator/StepItemGenerator.java
@@ -78,13 +78,13 @@ public class StepItemGenerator implements ItemGenerator {
 			index++;
 			BigDecimal end = start.add(interval).subtract(minStep);
 
-			RangeItem rangeItem = new RangeItem(index, start.doubleValue(),
-					((end.compareTo(upperLimit) <= 0) ? end : upperLimit).doubleValue(),
+			RangeItem rangeItem = new RangeItem(index, start,
+					((end.compareTo(upperLimit) <= 0) ? end : upperLimit),
 					new ElementId(IdentifierUtil.randomUniqueId()));
 
 			// Replace exist element id.
-			rangeItem = new RangeItem(index, start.doubleValue(),
-					((end.compareTo(upperLimit) <= 0) ? end : upperLimit).doubleValue(),
+			rangeItem = new RangeItem(index, start,
+					((end.compareTo(upperLimit) <= 0) ? end : upperLimit),
 					mapRangeItems.getOrDefault(rangeItem, rangeItem.getUuid()));
 
 			items.add(rangeItem);
