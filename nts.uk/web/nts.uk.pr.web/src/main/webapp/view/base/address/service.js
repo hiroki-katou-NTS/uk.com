6c33ddf797c1f242a3772267f9be8283a05bb3ac
nws-duongtv <nws-duongtv@newwave.vn>
2017-03-28 11:49:27 +0700

    update base zipcode qmm010

--- nts.uk/web/nts.uk.pr.web/src/main/webapp/view/base/address/service.js
+++ nts.uk/web/nts.uk.pr.web/src/main/webapp/view/base/address/service.js
@@ -19,6 +19,59 @@ var nts;
                                 return nts.uk.request.ajax(pathService.findAddressZipCode + '/' + zipCode);
                             }
                             service.findAddressZipCode = findAddressZipCode;
+                            function findAddressZipCodeToRespone(zipCode) {
+                                var dfd = $.Deferred();
+                                var addressRespone;
+                                addressRespone = new model.AddressRespone('0', '', null);
+                                if (zipCode && zipCode != '') {
+                                    service.findAddressZipCode(zipCode).done(function (data) {
+                                        var datalength = 0;
+                                        if (data != null) {
+                                            datalength = data.length;
+                                        }
+                                        if (datalength == 0) {
+                                            addressRespone = new model.AddressRespone('0', 'ER010', null);
+                                            dfd.resolve(addressRespone);
+                                        }
+                                        if (datalength == 1) {
+                                            addressRespone = new model.AddressRespone('1', '', data[0]);
+                                            dfd.resolve(addressRespone);
+                                        }
+                                        if (datalength >= 2) {
+                                            addressRespone = new model.AddressRespone('2', '', null);
+                                            dfd.resolve(addressRespone);
+                                        }
+                                    }).fail(function (error) {
+                                        addressRespone = new model.AddressRespone('0', error.messageId, null);
+                                        dfd.resolve(addressRespone);
+                                    });
+                                }
+                                else {
+                                    addressRespone = new model.AddressRespone('0', 'ER001', null);
+                                    dfd.resolve(addressRespone);
+                                }
+                                return dfd.promise();
+                            }
+                            service.findAddressZipCodeToRespone = findAddressZipCodeToRespone;
+                            function findAddressZipCodeSelection(zipCode) {
+                                var dfd = $.Deferred();
+                                var addressRespone;
+                                addressRespone = new model.AddressRespone('0', '', null);
+                                nts.uk.ui.windows.setShared('zipCode', zipCode);
+                                nts.uk.ui.windows.sub.modal("/view/base/address/index.xhtml", { height: 400, width: 530, title: "郵便番号" }).onClosed(function () {
+                                    var zipCodeRes = nts.uk.ui.windows.getShared('zipCodeRes');
+                                    if (zipCodeRes) {
+                                        addressRespone = new model.AddressRespone('1', '', zipCodeRes);
+                                        dfd.resolve(addressRespone);
+                                    }
+                                    else {
+                                        addressRespone = new model.AddressRespone('0', 'ER010', null);
+                                        dfd.resolve(addressRespone);
+                                    }
+                                });
+                                return dfd.promise();
+                            }
+                            service.findAddressZipCodeSelection = findAddressZipCodeSelection;
                             function getinfor(address) {
                                 return address.id + ' ' + address.prefecture + ' ' + address.town
                                     + ' ' + address.prefecture + ' ' + address.zipCode;
@@ -32,6 +85,15 @@ var nts;
                                     return AddressSelection;
                                 }());
                                 model.AddressSelection = AddressSelection;
+                                var AddressRespone = (function () {
+                                    function AddressRespone(errorCode, message, address) {
+                                        this.errorCode = errorCode;
+                                        this.message = message;
+                                        this.address = address;
+                                    }
+                                    return AddressRespone;
+                                }());
+                                model.AddressRespone = AddressRespone;
                             })(model = service.model || (service.model = {}));
                         })(service = address_1.service || (address_1.service = {}));
                     })(address = base.address || (base.address = {}));
--- nts.uk/web/nts.uk.pr.web/src/main/webapp/view/base/address/viewmodel.ts
+++ nts.uk/web/nts.uk.pr.web/src/main/webapp/view/base/address/viewmodel.ts
@@ -7,17 +7,23 @@ module nts.uk.pr.view.base.address {
 
             addressList: KnockoutObservableArray<AddressSelection>;
             zipCode: KnockoutObservable<string>;
+            columns : KnockoutObservableArray<any>;
 
             constructor() {
                 var self = this;
                 self.addressList = ko.observableArray<AddressSelection>([]);
                 self.zipCode = ko.observable('');
                 var strZipCode: string = nts.uk.ui.windows.getShared('zipCode');
+                self.columns = ko.observableArray([
+                    { headerText: '住所', prop: 'id', width: 200 },
+                    { headerText: 'ｶﾅ', prop: 'name', width: 200 }
+                ]);
                 service.findAddressZipCode(strZipCode).done(data => {
                     self.addressList(data);
                 }).fail(function(error) {
                     console.log(error)
                 });
+                
             }
 
             private chooseZipCode() {
