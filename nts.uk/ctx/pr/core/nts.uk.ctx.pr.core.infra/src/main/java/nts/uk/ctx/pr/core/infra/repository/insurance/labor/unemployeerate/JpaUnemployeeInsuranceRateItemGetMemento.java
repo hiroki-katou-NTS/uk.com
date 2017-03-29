c7aaa8f78e83abbb72f58834f4013bb2f366b60a
nws-duongtv <nws-duongtv@newwave.vn>
2017-03-29 09:41:36 +0700

    update qmm011 save database

--- nts.uk/ctx/pr/core/nts.uk.ctx.pr.core.infra/src/main/java/nts/uk/ctx/pr/core/infra/repository/insurance/labor/unemployeerate/JpaUnemployeeInsuranceRateItemGetMemento.java
+++ nts.uk/ctx/pr/core/nts.uk.ctx.pr.core.infra/src/main/java/nts/uk/ctx/pr/core/infra/repository/insurance/labor/unemployeerate/JpaUnemployeeInsuranceRateItemGetMemento.java
@@ -53,10 +53,10 @@ public class JpaUnemployeeInsuranceRateItemGetMemento implements UnemployeeInsur
 	@Override
 	public UnemployeeInsuranceRateItemSetting getCompanySetting() {
 		UnemployeeInsuranceRateItemSetting unemployeeInsuranceRateItemSetting;
-		
+
 		unemployeeInsuranceRateItemSetting = new UnemployeeInsuranceRateItemSetting();
 		switch (this.careerGroup) {
-		
+
 		case Agroforestry:
 			unemployeeInsuranceRateItemSetting
 				.setRate(Double.valueOf(String.valueOf(this.typeValue.getCEmpRateGeneral())));
@@ -66,16 +66,16 @@ public class JpaUnemployeeInsuranceRateItemGetMemento implements UnemployeeInsur
 
 		case Contruction:
 			unemployeeInsuranceRateItemSetting
-				.setRate(Double.valueOf(String.valueOf(this.typeValue.getCEmpRateConst())));
+				.setRate(Double.valueOf(String.valueOf(this.typeValue.getCEmpRateOther())));
 			unemployeeInsuranceRateItemSetting
-				.setRoundAtr(RoundingMethod.valueOf(this.typeValue.getCEmpRoundConst()));
+				.setRoundAtr(RoundingMethod.valueOf(this.typeValue.getCEmpRoundOther()));
 			break;
 
 		case Other:
 			unemployeeInsuranceRateItemSetting
-				.setRate(Double.valueOf(String.valueOf(this.typeValue.getCEmpRateOther())));
+				.setRate(Double.valueOf(String.valueOf(this.typeValue.getCEmpRateConst())));
 			unemployeeInsuranceRateItemSetting
-				.setRoundAtr(RoundingMethod.valueOf(this.typeValue.getCEmpRoundOther()));
+				.setRoundAtr(RoundingMethod.valueOf(this.typeValue.getCEmpRoundConst()));
 			break;
 		}
 		return unemployeeInsuranceRateItemSetting;
@@ -90,7 +90,7 @@ public class JpaUnemployeeInsuranceRateItemGetMemento implements UnemployeeInsur
 	@Override
 	public UnemployeeInsuranceRateItemSetting getPersonalSetting() {
 		UnemployeeInsuranceRateItemSetting unemployeeInsuranceRateItemSetting;
-		
+
 		unemployeeInsuranceRateItemSetting = new UnemployeeInsuranceRateItemSetting();
 		switch (this.careerGroup) {
 		case Agroforestry:
