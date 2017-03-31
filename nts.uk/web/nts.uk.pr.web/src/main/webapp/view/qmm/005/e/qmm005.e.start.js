a21ee2e616ae2fcccd880cec06c40d6a088d9afa
NSVN-Nguyen Van Vuong <vuong_nv@nittsusystem-vn.com>
2017-03-31 15:24:46 +0700

    Update all js and map on qmm005
    Update change for js generator (wtf)

---
+++ nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qmm/005/e/qmm005.e.start.js
@@ -0,0 +1,10 @@
+var qmm005;
+(function (qmm005) {
+    var e;
+    (function (e) {
+        __viewContext.ready(function () {
+            __viewContext.bind(new e.ViewModel());
+        });
+    })(e = qmm005.e || (qmm005.e = {}));
+})(qmm005 || (qmm005 = {}));
+//# sourceMappingURL=qmm005.e.start.js.map
