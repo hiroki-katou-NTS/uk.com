f207082c12c6aee5f60bc6b78d4313b818b95d75
nws-quyennt <quyennt@newwave.vn>
2017-04-12 11:06:20 +0700

    update qet002

--- nts.uk/file/pr/nts.uk.file.pr.infra/src/main/java/nts/uk/pr/file/infra/accumulatedpayment/JpaAccPaymentReportRepository.java
+++ nts.uk/file/pr/nts.uk.file.pr.infra/src/main/java/nts/uk/pr/file/infra/accumulatedpayment/JpaAccPaymentReportRepository.java
@@ -7,7 +7,10 @@ package nts.uk.pr.file.infra.accumulatedpayment;
 import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
 
+import nts.arc.layer.infra.data.JpaRepository;
+import nts.arc.time.YearMonth;
 import nts.uk.file.pr.app.export.accumulatedpayment.AccPaymentRepository;
 import nts.uk.file.pr.app.export.accumulatedpayment.data.AccPaymentItemData;
 import nts.uk.file.pr.app.export.accumulatedpayment.query.AccPaymentReportQuery;
@@ -15,7 +18,7 @@ import nts.uk.file.pr.app.export.accumulatedpayment.query.AccPaymentReportQuery;
 
 
 @Stateless
-public class JpaAccPaymentReportRepository implements AccPaymentRepository{
+public class JpaAccPaymentReportRepository extends JpaRepository implements AccPaymentRepository{
 
 	/* (non-Javadoc)
 	 * @see nts.uk.ctx.pr.screen.app.report.qet002.AccPaymentRepository
@@ -24,6 +27,27 @@ public class JpaAccPaymentReportRepository implements AccPaymentRepository{
 	@Override
 	public List<AccPaymentItemData> getItems(String companyCode, AccPaymentReportQuery query) {
 		// TODO Auto-generated method stub
+		EntityManager em = this.getEntityManager();
+		
+		// Create Year Month.
+		YearMonth startYearMonth = YearMonth.of(query.getTargetYear(), 1);
+		YearMonth endYearMonth = YearMonth.of(query.getTargetYear(), 12);
+		String queryString = "SELECT ec.empCd, e.employmentName"
+				+ "FROM ReportPbsmtPersonBase p, "
+				+ "ReportPclmtPersonEmpContract ec"
+				+ "ReportCmnmtEmp e"
+				+ "ReportQpdmtPayday pd"
+				+ "WHERE p.pid = :PID"
+				+ "AND ec.pclmtPersonEmpContractPK.ccd = :CCD"
+				+ "AND p.pid = ec.pclmtPersonEmpContractPK.pId"
+				+ "AND ec.pclmtPersonEmpContractPK.strD <= :BASE_YMD"
+				+ "AND ec.pclmtPersonEmpContractPK.strD >= :BASE_YMD"
+				+ "AND e.cmnmtEmpPk.companyCode = :CCD"
+				+ "AND e.cmnmtEmpPk.employmentCode = ec.empCd"
+				+ "AND pd.qpdmtPaydayPK.ccd = :CCD"
+				+ "AND pd.qpdmtPaydayPK.payBonusAtr = :PAY_BONUS_ATR" //1
+				+ "AND pd.qpdmtPaydayPK.processingNo = e.processingNo";
+		
 		return null;
 	}
 }
