051a8dc7b09fd3dcdf4803a94b5fb04d99b97d92
PhamBaVu <vu_pb@nittsusystem.co.jp>
2017-04-14 10:09:30 +0900

    add i18n java api

---
+++ nts.uk/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/infra/i18n/SystemProperties.java
@@ -0,0 +1,8 @@
+package nts.uk.shr.infra.i18n;
+
+import java.util.Locale;
+
+public class SystemProperties {
+	public static final String SYSTEM_ID = "SYSTEM";
+	public static final Locale DEFAULT_LANGUAGE = Locale.JAPANESE;
+}
