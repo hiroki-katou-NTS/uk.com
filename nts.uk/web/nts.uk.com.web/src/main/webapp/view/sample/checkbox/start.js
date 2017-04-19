ef9fcc0e9d6f1424696b999621226ce64603dba9
NSVC\lamdt <lam_dt@nittsusystem-vn.com>
2017-04-19 12:54:21 +0700

    CheckBox support readonly mode

--- nts.uk/web/nts.uk.com.web/src/main/webapp/view/sample/checkbox/start.js
+++ nts.uk/web/nts.uk.com.web/src/main/webapp/view/sample/checkbox/start.js
@@ -4,8 +4,10 @@ __viewContext.ready(function () {
             var self = this;
             self.checked = ko.observable(true);
             self.enable = ko.observable(true);
+            self.readonly = ko.observable(false);
         }
         return ScreenModel;
     }());
     this.bind(new ScreenModel());
 });
+//# sourceMappingURL=start.js.map
