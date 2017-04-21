e589373921ecddc27cef0fa03a8c0df9f0523cf6
NSVN-BuiVanChinh <chinh_bv@nittsusystem-vn.com>
2017-04-21 11:32:36 +0700

    update entity for query

--- nts.uk/file/pr/nts.uk.file.pr.infra/src/main/java/nts/uk/pr/file/infra/residentialtax/JpaResidentialTaxReportRepository.java
+++ nts.uk/file/pr/nts.uk.file.pr.infra/src/main/java/nts/uk/pr/file/infra/residentialtax/JpaResidentialTaxReportRepository.java
@@ -6,8 +6,12 @@ import javax.ejb.Stateless;
 
 import nts.arc.layer.infra.data.JpaRepository;
 import nts.arc.time.GeneralDate;
+import nts.uk.ctx.basic.infra.entity.company.CmnmtCompany;
+import nts.uk.ctx.basic.infra.entity.company.CmnmtCompanyPK;
 import nts.uk.ctx.pr.core.dom.enums.CategoryAtr;
 import nts.uk.ctx.pr.core.dom.paymentdata.PayBonusAtr;
+import nts.uk.ctx.pr.core.infra.entity.rule.law.tax.residential.output.QcpmtRegalDocCom;
+import nts.uk.ctx.pr.core.infra.entity.rule.law.tax.residential.output.QcpmtRegalDocComPK;
 import nts.uk.file.pr.app.export.residentialtax.ResidentialTaxReportRepository;
 import nts.uk.file.pr.app.export.residentialtax.data.CompanyDto;
 import nts.uk.file.pr.app.export.residentialtax.data.PaymentDetailDto;
@@ -15,9 +19,6 @@ import nts.uk.file.pr.app.export.residentialtax.data.PersonResitaxDto;
 import nts.uk.file.pr.app.export.residentialtax.data.ResidentialTaxDto;
 import nts.uk.file.pr.app.export.residentialtax.data.ResidentialTaxSlipDto;
 import nts.uk.file.pr.app.export.residentialtax.data.RetirementPaymentDto;
-import nts.uk.pr.file.infra.entity.ReportCmnmtCompany;
-import nts.uk.pr.file.infra.entity.ReportCmnmtCompanyPK;
-import nts.uk.pr.file.infra.entity.ReportQcpmtRegalDocComPK;
 
 @Stateless
 public class JpaResidentialTaxReportRepository extends JpaRepository implements ResidentialTaxReportRepository {
@@ -45,7 +46,7 @@ public class JpaResidentialTaxReportRepository extends JpaRepository implements 
 
 	private String QREDT_RETIREMENT_PAYMENT_SEL_2 = "SELECT NEW " + RetirementPaymentDto.class.getName() + ""
 			+ "(z.qredtRetirementPaymentPK.pid, z.qredtRetirementPaymentPK.payDate, z.trialPeriodSet, z.exclusionYears, z.additionalBoardYears, z.boardYears, z.totalPaymentMny, z.deduction1Mny, z.deduction2Mny, z.deduction3Mny, z.otherRetirementPayOp, z.taxCalMethodSet, z.incomeTaxMny, z.cityTaxMny, z.prefectureTaxMny, z.totalDeductionMny, z.actualRecieveMny, z.withholdingMeno)"
-			+ "FROM ReportQredtRetirementPayment z "
+			+ "FROM QredtRetirementPayment z "
 			+ "WHERE z.qredtRetirementPaymentPK.ccd =:companyCode "
 			+ "AND z.qredtRetirementPaymentPK.pid IN :personId "
 			+ "AND z.qredtRetirementPaymentPK.payDate >=:StartYearMonth "
@@ -59,15 +60,17 @@ public class JpaResidentialTaxReportRepository extends JpaRepository implements 
 
 	@Override
 	public CompanyDto findCompany(String companyCode) {
-		ReportCmnmtCompanyPK key = new ReportCmnmtCompanyPK(companyCode);
-		return this.queryProxy().find(key, ReportCmnmtCompany.class)
+		CmnmtCompanyPK key = new CmnmtCompanyPK(companyCode);
+		return this.queryProxy().find(key, CmnmtCompany.class)
 				.map(x-> new CompanyDto(x.cmnmtCompanyPk.companyCd, x.cName, x.postal, x.address1, x.address2)).get();
 	}
 
 	@Override
 	public CompanyDto findRegalCompany(String companyCode, String regalDocCompanyCode) {
-		ReportQcpmtRegalDocComPK key = new ReportQcpmtRegalDocComPK(companyCode, regalDocCompanyCode);
-		return this.queryProxy().find(key, CompanyDto.class).get();
+		QcpmtRegalDocComPK key = new QcpmtRegalDocComPK(companyCode, regalDocCompanyCode);
+		return this.queryProxy().find(key, QcpmtRegalDocCom.class)
+				.map(x -> new CompanyDto(companyCode, x.regalDocCname, String.valueOf(x.telNo), x.payerAddress1, x.payerAddress2))
+				.get();
 	}
 
 	@Override
