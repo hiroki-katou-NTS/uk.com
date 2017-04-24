41571737ead6374a3ad5dd48929c26e4860b2569
NSVC\lanlt <lan_lt@nitssusystem-vn.com>
2017-04-24 14:27:09 +0700

    alert

--- nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qmm/003/c/qmm003.c.vm.ts
+++ nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qmm/003/c/qmm003.c.vm.ts
@@ -3,52 +3,47 @@ module qmm003.c.viewmodel {
         items: KnockoutObservableArray<ResidentalTaxNode>;
         singleSelectedCode: KnockoutObservable<string>;
         filteredData: KnockoutObservableArray<ResidentalTaxNode> = ko.observableArray([]);
-        currentNode: ResidentalTaxNode = null;
+        currentNode: service.model.ResidentialTaxDto = null;
         nodeRegionPrefectures: KnockoutObservableArray<ResidentalTaxNode> = ko.observableArray([]);
-        japanLocation: Array<qmm003.c.service.model.RegionObject> = [];
+        japanLocation: Array<service.model.RegionObject> = [];
         precfecture: Array<ResidentalTaxNode> = [];
         itemPrefecture: KnockoutObservableArray<ResidentalTaxNode> = ko.observableArray([]);
-        residentalTaxList: KnockoutObservableArray<qmm003.c.service.model.ResidentialTaxDto> = ko.observableArray([]);
+        residentalTaxList: KnockoutObservableArray<service.model.ResidentialTaxDto> = ko.observableArray([]);
         yes: boolean = null;
         constructor() {
             let self = this;
             self.init();
             self.singleSelectedCode.subscribe(function(newValue) {
                 if (newValue.length > 2) {
-                    self.processWhenCurrentCodeChange(newValue);
+                    self.currentNode = _.find(self.residentalTaxList(), function(obj: service.model.ResidentialTaxDto) {
+                        return obj.resiTaxCode = newValue;
+                    });
                 }
             });
 
         }
-        processWhenCurrentCodeChange(newValue: string) {
-            let self = this;
-            let node: ResidentalTaxNode;
-            node = _.find(self.filteredData(), function(obj: ResidentalTaxNode) {
-                return obj.code = newValue;
-            });
-            self.currentNode = node;
-        }
-
         clickButton(): any {
             let self = this;
             self.yes = true;
             nts.uk.ui.windows.setShared('yes', self.yes, true);
-            nts.uk.ui.windows.setShared('currentNode', self.currentNode, true);
-            nts.uk.ui.windows.setShared('items', self.items(), true);
-            nts.uk.ui.windows.close();
+            if (self.currentNode) {
+                nts.uk.ui.windows.setShared('currentNode', self.currentNode, true);
+                nts.uk.ui.windows.close();
+            } else {
+                nts.uk.ui.dialog.alert("住民税納付先コード が選択されていません。");
+            }
 
         }
         cancelButton(): void {
             let self = this;
             self.yes = false;
             nts.uk.ui.windows.setShared('yes', self.yes, true);
-            nts.uk.ui.windows.setShared('items', this.items(), true);
             nts.uk.ui.windows.close();
         }
         init(): void {
             let self = this;
             self.items = ko.observableArray([]);
-            self.singleSelectedCode = ko.observable(null);
+            self.singleSelectedCode = ko.observable("");
         }
         //11.初期データ取得処理 11. Initial data acquisition processing
         start(): JQueryPromise<any> {
@@ -78,30 +73,11 @@ module qmm003.c.viewmodel {
             return dfd.promise();
         }
 
-        // tìm index để khi chọn root thì ra hiển thị ra thằng đầu tiên của 1 thằng root
-        findIndex(items: Array<ResidentalTaxNode>, newValue: string): any {
-            let index: number;
-            let count: number = -1;
-            let prefectureArray = [];
-            _.each(items, function(obj: ResidentalTaxNode) {
-                count++;
-                index = _.findIndex(obj.childs, function(obj1: ResidentalTaxNode) {
-                    return obj1.code === newValue;
-                });
-                if (index > -1) {
-                    prefectureArray.push(count, index);
-                }
-
-            });
-
-            return prefectureArray;
-        }
-
         buildResidentalTaxTree() {
             let self = this;
             var child = [];
             let i = 0;
-            _.each(self.residentalTaxList(), function(objResi: qmm003.c.service.model.ResidentialTax) {
+            _.each(self.residentalTaxList(), function(objResi: service.model.ResidentialTax) {
                 _.each(self.japanLocation, function(objRegion: service.model.RegionObject) {
                     let isChild: boolean = false;
                     let isPrefecture: boolean = false;
