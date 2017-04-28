6a454b24132671f969d2bec7245a4373f10f0edf
nguyenanhhai <hai_na@nittsusystem-vn.com>
2017-04-28 13:25:50 +0700

    stop tracking js file

--- nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qmm/020/a/qmm020.a.start.js
+++
@@ -1,8 +0,0 @@
-__viewContext.ready(function () {
-    var viewModel = {
-        viewmodelA: new qmm020.a.viewmodel.ScreenModel(),
-        viewmodelB: new qmm020.b.viewmodel.ScreenModel(),
-        viewmodelC: new qmm020.c.viewmodel.ScreenModel()
-    };
-    __viewContext.bind(viewModel);
-});
