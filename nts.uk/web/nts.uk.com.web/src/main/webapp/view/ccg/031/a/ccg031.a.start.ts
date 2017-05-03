181387091a66ed334858942d43bacabb3d0fe204
NSVC\lamdt <lam_dt@nittsusystem-vn.com>
2017-05-03 17:26:40 +0700

    Update a screen UI

--- nts.uk/web/nts.uk.com.web/src/main/webapp/view/ccg/031/a/ccg031.a.start.ts
+++ nts.uk/web/nts.uk.com.web/src/main/webapp/view/ccg/031/a/ccg031.a.start.ts
@@ -1,6 +1,8 @@
 module ccg031.a {
     __viewContext.ready(function() {
         var screenModel = new viewmodel.ScreenModel();
-        __viewContext.bind(screenModel);
+        screenModel.startPage().done(function() {
+            __viewContext.bind(screenModel);
+        });
     });
 }
--- nts.uk/web/nts.uk.com.web/src/main/webapp/view/ccg/031/a/ccg031.a.vm.ts
+++ nts.uk/web/nts.uk.com.web/src/main/webapp/view/ccg/031/a/ccg031.a.vm.ts
@@ -1,16 +1,67 @@
 module ccg031.a.viewmodel {
     export class ScreenModel {
-        
+        // Column & Row
+        minRow: number = 4;
+        minColumn: number = 6;
+        totalRow: KnockoutObservable<number>;
+        totalColumn: KnockoutObservable<number>;
+        totalRowArray: KnockoutComputed<number[]>;
+        totalColumnArray: KnockoutComputed<number[]>;
+
         constructor() {
             var self = this;
+            this.totalRow = ko.observable(this.minRow);
+            this.totalColumn = ko.observable(this.minColumn);
+            this.totalRowArray = ko.computed((): Array<number> => {
+                return self.numberToArray(self.totalRow());
+            });
+            this.totalColumnArray = ko.computed((): Array<number> => {
+                return self.numberToArray(self.totalColumn());
+            });
         }
-        
+
+        /** Start Page */
         startPage(): JQueryPromise<any> {
             var self = this;
-            
             var dfd = $.Deferred();
             dfd.resolve();
             return dfd.promise();
+        }
+
+        /** Add Column */
+        addColumn(): void {
+            this.totalColumn(this.totalColumn() + 1);
+            this.placementOverflow();
+        }
+
+        /** Add Column */
+        addRow(): void {
+            this.totalRow(this.totalRow() + 1);
+            this.placementOverflow();
+        }
+        
+        /** Add Column */
+        private placementOverflow(): void {
+            if (this.totalRow() > this.minRow)
+                $(".placement-container").addClass("overflow-y");
+            if (this.totalColumn() > this.minColumn)
+                $(".placement-container").addClass("overflow-x");
+        }
+
+        /** Open Add TopPage-Part Dialog */
+        openDialog(row: number, column: number) {
+            nts.uk.ui.windows.setShared("size", {row: row, column: column}, false);
+            nts.uk.ui.windows.sub.modal("/view/ccg/031/b/index.xhtml");
+        }
+
+        /** Convert number to Array<number> */
+        private numberToArray(value: number): Array<number> {
+            var temp = [];
+            var length = _.toInteger(value);
+            for (var i = 1; i <= length; i++) {
+                temp.push(i);
+            }
+            return temp;
         }
     }
 }
