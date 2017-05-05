98a133b652a2219adc58e876d283237e710f04bd
Dương Ngô <ngoducduonglucky@gmail.com>
2017-05-05 16:04:24 +0700

    add equals+hashcode

--- nts.uk/ctx/pr/report/nts.uk.ctx.pr.report.dom/src/main/java/nts/uk/ctx/pr/report/dom/salarydetail/aggregate/SalaryAggregateItem.java
+++ nts.uk/ctx/pr/report/nts.uk.ctx.pr.report.dom/src/main/java/nts/uk/ctx/pr/report/dom/salarydetail/aggregate/SalaryAggregateItem.java
@@ -47,4 +47,35 @@ public class SalaryAggregateItem extends DomainObject {
 		memento.setSalaryAggregateItemName(this.name);
 		memento.setSubItemCodes(this.subItemCodes);
 	}
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((header == null) ? 0 : header.hashCode());
+        return result;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        SalaryAggregateItem other = (SalaryAggregateItem) obj;
+        if (header == null) {
+            if (other.header != null)
+                return false;
+        } else if (!header.equals(other.header))
+            return false;
+        return true;
+    }
 }
