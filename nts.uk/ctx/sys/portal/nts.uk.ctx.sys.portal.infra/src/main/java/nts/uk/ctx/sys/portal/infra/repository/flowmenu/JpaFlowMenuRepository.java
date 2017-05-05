e5ad94f2006858b2e41c723feb8d6f75eeb06f68
DuDT <du_dt@nittsusystem-vn.comdu_dt@nittsusystem-vn.com>
2017-05-05 10:04:00 +0700

    xu ly single data

--- nts.uk/ctx/sys/portal/nts.uk.ctx.sys.portal.infra/src/main/java/nts/uk/ctx/sys/portal/infra/repository/flowmenu/JpaFlowMenuRepository.java
+++ nts.uk/ctx/sys/portal/nts.uk.ctx.sys.portal.infra/src/main/java/nts/uk/ctx/sys/portal/infra/repository/flowmenu/JpaFlowMenuRepository.java
@@ -19,15 +19,12 @@ import nts.uk.ctx.sys.portal.infra.entity.flowmenu.CcgmtFlowMenuPK;
 @Stateless
 public class JpaFlowMenuRepository extends JpaRepository implements FlowMenuRepository{
 	
-	private final String SELECT= "SELECT c FROM CcgmtFlowMenu c";
-	private final String SELECT_SINGLE = "SELECT c FROM CcgmtFlowMenu c WHERE c.ccgmtFlowMenuPK.companyID = :companyID AND c.ccgmtFlowMenuPK.toppagePartID = :toppagePartID";
-	private final String SELECT_ALL_BY_COMPANY = SELECT + " WHERE c.ccgmtFlowMenuPK.companyID = :companyID";
-	
 	private final String SELECT_FLOWMENU = "SELECT m.ccgmtFlowMenuPK.toppagePartID,  m.fileName, m.defClassAtr, t.code, t.name, t.topPagePartType, t.width, t.height "
-			+ "FROM CcgmtFlowMenu m "
-			+ "INNER JOIN CcgmtTopPagePart t "
-			+ "ON m.ccgmtFlowMenuPK.toppagePartID = t.ccgmtTopPagePartPK.topPagePartID "
-			+ "AND m.ccgmtFlowMenuPK.companyID = :companyId";
+			+ " FROM CcgmtFlowMenu m "
+			+ " INNER JOIN CcgmtTopPagePart t "
+			+ " ON m.ccgmtFlowMenuPK.toppagePartID = t.ccgmtTopPagePartPK.topPagePartID "
+			+ " AND m.ccgmtFlowMenuPK.companyID = :companyId";
+	private final String FLOWMENU_BY_TOPPAGEPARTID = SELECT_FLOWMENU + " AND m.ccgmtFlowMenuPK.toppagePartID = :topPagePartId";
 	/**
 	 * Get List FlowMenu
 	 * @param companyID
@@ -47,7 +44,14 @@ public class JpaFlowMenuRepository extends JpaRepository implements FlowMenuRepo
 					int type = Integer.valueOf(c[5].toString());
 					int widthSize = Integer.valueOf(c[6].toString());
 					int heightSize = Integer.valueOf(c[7].toString());
-					return FlowMenuTopPagePart.createFromJavaType(toppagePartID, fileName, defClassAtr, code, name, type, widthSize, heightSize);
+					return FlowMenuTopPagePart.createFromJavaType(toppagePartID, 
+							fileName, 
+							defClassAtr, 
+							code, 
+							name, 
+							type, 
+							widthSize,
+							heightSize);
 				});
 	}
 	/**
@@ -58,12 +62,27 @@ public class JpaFlowMenuRepository extends JpaRepository implements FlowMenuRepo
 	 */
 	@Override
 	public Optional<FlowMenuTopPagePart> findByCode(String companyID, String toppagePartID) {
-//		return this.queryProxy()
-//				.query(SELECT_SINGLE, CcgmtFlowMenu.class)
-//				.setParameter("companyID", companyID)
-//				.setParameter("toppagePartID", toppagePartID)
-//				.getSingle(c -> toDomain(c));
-		return null;
+		return this.queryProxy()
+				.query(FLOWMENU_BY_TOPPAGEPARTID, Object[].class)
+				.setParameter("companyId", companyID)
+				.setParameter("topPagePartId", toppagePartID)
+				.getSingle(c -> {
+					String fileName = (String) c[1];
+					int defClassAtr = Integer.valueOf(c[2].toString());
+					String code = (String) c[3];
+					String name = (String) c[4];
+					int type = Integer.valueOf(c[5].toString());
+					int widthSize = Integer.valueOf(c[6].toString());
+					int heightSize = Integer.valueOf(c[7].toString());
+					return FlowMenuTopPagePart.createFromJavaType(toppagePartID, 
+							fileName, 
+							defClassAtr, 
+							code, 
+							name, 
+							type, 
+							widthSize,
+							heightSize);
+				});
 	}
 	/**
 	 * Add
@@ -96,16 +115,6 @@ public class JpaFlowMenuRepository extends JpaRepository implements FlowMenuRepo
 		this.commandProxy().remove(CcgmtFlowMenu.class, new CcgmtFlowMenuPK(companyID, toppagePartID));
 		
 	}
-	
-	private FlowMenu toDomain(CcgmtFlowMenu entity) {
-		return FlowMenu.createFromJavaType(
-				entity.ccgmtFlowMenuPK.companyID,
-				entity.ccgmtFlowMenuPK.toppagePartID, 
-				entity.fileID, 
-				entity.fileName, 
-				entity.defClassAtr);
-	}
-	
 	private CcgmtFlowMenu toEntity(FlowMenu domain) {
 		return new CcgmtFlowMenu(new CcgmtFlowMenuPK(domain.getCompanyID(), 
 				domain.getToppagePartID()),
