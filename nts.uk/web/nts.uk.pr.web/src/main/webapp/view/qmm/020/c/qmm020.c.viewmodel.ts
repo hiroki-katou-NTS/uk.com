1ed96f79f84e4dbcb078275df88f1e9a18f7f0c8
nguyenanhhai <hai_na@nittsusystem-vn.com>
2017-05-08 08:43:53 +0700

    qmm020_C

--- nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qmm/020/c/qmm020.c.viewmodel.ts
+++ nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qmm/020/c/qmm020.c.viewmodel.ts
@@ -228,6 +228,8 @@ module qmm020.c.viewmodel {
                 let maxDate: TotalModel = _.find(self.itemTotalList(), function(obj) { return parseInt(obj.endYm()) == itemMax; });
                 self.maxDate = (maxDate.startYm || "").toString();
                 self.maxItem(maxDate);
+                console.log(self.maxItem());
+                console.log(self.maxDate);
                 dfd.resolve();
             }).fail(function(res) {
                 alert(res);
@@ -473,9 +475,7 @@ module qmm020.c.viewmodel {
             this.endYm = ko.observable(param.endYm);
         }
     }
-    class EmpDetail {
-
-    }
+    
 }
 
 $(function() {
