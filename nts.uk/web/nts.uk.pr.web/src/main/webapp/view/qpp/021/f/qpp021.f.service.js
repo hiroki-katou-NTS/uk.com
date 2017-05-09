9d83bc5044ae98b99b459cd249a937b7eb5b6ba7
nws-duongtv <nws-duongtv@newwave.vn>
2017-05-09 17:41:15 +0700

    update ui f

--- nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qpp/021/f/qpp021.f.service.js
+++ nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qpp/021/f/qpp021.f.service.js
@@ -1,20 +1,44 @@
-var qpp021;
-(function (qpp021) {
-    var f;
-    (function (f) {
-        var service;
-        (function (service) {
-            var paths = {
-                save: "ctx/pr/report/payment/contact/personalsetting/save"
-            };
-            /**
-             * Save
-             */
-            function save(data) {
-                return nts.uk.request.ajax(paths.save, data);
-            }
-            service.save = save;
-        })(service = f.service || (f.service = {}));
-    })(f = qpp021.f || (qpp021.f = {}));
-})(qpp021 || (qpp021 = {}));
+var nts;
+(function (nts) {
+    var uk;
+    (function (uk) {
+        var pr;
+        (function (pr) {
+            var view;
+            (function (view) {
+                var qpp021;
+                (function (qpp021) {
+                    var f;
+                    (function (f) {
+                        var service;
+                        (function (service) {
+                            var paths = {
+                                findRefundPadding: "ctx/pr/report/payment/refundsetting/refundpadding/printtype/two/find",
+                                saveRefundPadding: "ctx/pr/report/payment/refundsetting/refundpadding/printtype/two/save"
+                            };
+                            function findRefundPadding() {
+                                return nts.uk.request.ajax(paths.findRefundPadding);
+                            }
+                            service.findRefundPadding = findRefundPadding;
+                            function saveRefundPadding(dto) {
+                                var data = { dto: dto };
+                                return nts.uk.request.ajax(paths.saveRefundPadding, data);
+                            }
+                            service.saveRefundPadding = saveRefundPadding;
+                            var model;
+                            (function (model) {
+                                var RefundPaddingTwoDto = (function () {
+                                    function RefundPaddingTwoDto() {
+                                    }
+                                    return RefundPaddingTwoDto;
+                                }());
+                                model.RefundPaddingTwoDto = RefundPaddingTwoDto;
+                            })(model = service.model || (service.model = {}));
+                        })(service = f.service || (f.service = {}));
+                    })(f = qpp021.f || (qpp021.f = {}));
+                })(qpp021 = view.qpp021 || (view.qpp021 = {}));
+            })(view = pr.view || (pr.view = {}));
+        })(pr = uk.pr || (uk.pr = {}));
+    })(uk = nts.uk || (nts.uk = {}));
+})(nts || (nts = {}));
 //# sourceMappingURL=qpp021.f.service.js.map
--- nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qpp/021/f/qpp021.f.vm.js
+++ nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qpp/021/f/qpp021.f.vm.js
@@ -1,70 +1,96 @@
-var qpp021;
-(function (qpp021) {
-    var f;
-    (function (f) {
-        var viewmodel;
-        (function (viewmodel) {
-            var ScreenModel = (function () {
-                function ScreenModel() {
-                    this.myModel = ko.observable(new MyModel());
-                    this.splitLineOutput = ko.observableArray([
-                        new SelectionModel('1', 'する'),
-                        new SelectionModel('2', 'しない')
-                    ]);
-                }
-                /**
-                 * Start page.
-                 */
-                ScreenModel.prototype.startPage = function () {
-                    var self = this;
-                    var dfd = $.Deferred();
-                    dfd.resolve();
-                    return dfd.promise();
-                };
-                /**
-                 * Event when click save button.
-                 */
-                ScreenModel.prototype.onSaveBtnClicked = function () {
-                    var self = this;
-                    // Validate.
-                    if ($('.nts-input').ntsError('hasError')) {
-                        return;
-                    }
-                    // Save.
-                    f.service.save(ko.toJS(self.myModel));
-                };
-                /**
-                 * Event when click close button.
-                 */
-                ScreenModel.prototype.onCloseBtnClicked = function () {
-                    nts.uk.ui.windows.close();
-                };
-                return ScreenModel;
-            }());
-            viewmodel.ScreenModel = ScreenModel;
-            var MyModel = (function () {
-                function MyModel() {
-                    this.inp1 = ko.observable('1');
-                    this.inp2 = ko.observable('2');
-                    this.inp3 = ko.observable('3');
-                    this.inp4 = ko.observable('4');
-                    this.selectedSplitLineOutput = ko.observable('1');
-                }
-                return MyModel;
-            }());
-            viewmodel.MyModel = MyModel;
-            /**
-             *  Class SelectionModel.
-             */
-            var SelectionModel = (function () {
-                function SelectionModel(code, name) {
-                    this.code = code;
-                    this.name = name;
-                }
-                return SelectionModel;
-            }());
-            viewmodel.SelectionModel = SelectionModel;
-        })(viewmodel = f.viewmodel || (f.viewmodel = {}));
-    })(f = qpp021.f || (qpp021.f = {}));
-})(qpp021 || (qpp021 = {}));
+var nts;
+(function (nts) {
+    var uk;
+    (function (uk) {
+        var pr;
+        (function (pr) {
+            var view;
+            (function (view) {
+                var qpp021;
+                (function (qpp021) {
+                    var f;
+                    (function (f) {
+                        var RefundPaddingTwoDto = f.service.model.RefundPaddingTwoDto;
+                        var viewmodel;
+                        (function (viewmodel) {
+                            var ScreenModel = (function () {
+                                function ScreenModel() {
+                                    var self = this;
+                                    self.refundPaddingTwoModel = new RefundPaddingTwoModel();
+                                    self.splitLineOutput = ko.observableArray([
+                                        new SelectionModel('0', 'する'),
+                                        new SelectionModel('1', 'しない')
+                                    ]);
+                                    self.sizeLimitOption = ko.mapping.fromJS(new nts.uk.ui.option.NumberEditorOption({
+                                        grouplength: 3,
+                                        decimallength: 2
+                                    }));
+                                }
+                                ScreenModel.prototype.startPage = function () {
+                                    var self = this;
+                                    var dfd = $.Deferred();
+                                    f.service.findRefundPadding().done(function (data) {
+                                        self.refundPaddingTwoModel.updateDataDto(data);
+                                        dfd.resolve(self);
+                                    }).fail(function (error) {
+                                        console.log(error);
+                                    });
+                                    return dfd.promise();
+                                };
+                                ScreenModel.prototype.onSaveBtnClicked = function () {
+                                    var self = this;
+                                    if ($('.nts-input').ntsError('hasError')) {
+                                        return;
+                                    }
+                                    f.service.saveRefundPadding(self.refundPaddingTwoModel.toDto());
+                                };
+                                ScreenModel.prototype.onCloseBtnClicked = function () {
+                                    nts.uk.ui.windows.close();
+                                };
+                                return ScreenModel;
+                            }());
+                            viewmodel.ScreenModel = ScreenModel;
+                            var RefundPaddingTwoModel = (function () {
+                                function RefundPaddingTwoModel() {
+                                    this.upperAreaPaddingTop = ko.observable(0);
+                                    this.underAreaPaddingTop = ko.observable(0);
+                                    this.paddingLeft = ko.observable(0);
+                                    this.breakLineMargin = ko.observable(0);
+                                    this.isShowBreakLine = ko.observable(0);
+                                }
+                                RefundPaddingTwoModel.prototype.updateDataDto = function (dto) {
+                                    this.upperAreaPaddingTop(dto.upperAreaPaddingTop);
+                                    this.underAreaPaddingTop(dto.underAreaPaddingTop);
+                                    this.paddingLeft(dto.paddingLeft);
+                                    this.breakLineMargin(dto.breakLineMargin);
+                                    this.isShowBreakLine(dto.isShowBreakLine);
+                                };
+                                RefundPaddingTwoModel.prototype.toDto = function () {
+                                    var dto;
+                                    dto = new RefundPaddingTwoDto();
+                                    dto.upperAreaPaddingTop = this.underAreaPaddingTop();
+                                    dto.underAreaPaddingTop = this.underAreaPaddingTop();
+                                    dto.paddingLeft = this.paddingLeft();
+                                    dto.breakLineMargin = this.breakLineMargin();
+                                    dto.isShowBreakLine = this.isShowBreakLine();
+                                    return dto;
+                                };
+                                return RefundPaddingTwoModel;
+                            }());
+                            viewmodel.RefundPaddingTwoModel = RefundPaddingTwoModel;
+                            var SelectionModel = (function () {
+                                function SelectionModel(code, name) {
+                                    this.code = code;
+                                    this.name = name;
+                                }
+                                return SelectionModel;
+                            }());
+                            viewmodel.SelectionModel = SelectionModel;
+                        })(viewmodel = f.viewmodel || (f.viewmodel = {}));
+                    })(f = qpp021.f || (qpp021.f = {}));
+                })(qpp021 = view.qpp021 || (view.qpp021 = {}));
+            })(view = pr.view || (pr.view = {}));
+        })(pr = uk.pr || (uk.pr = {}));
+    })(uk = nts.uk || (nts.uk = {}));
+})(nts || (nts = {}));
 //# sourceMappingURL=qpp021.f.vm.js.map
