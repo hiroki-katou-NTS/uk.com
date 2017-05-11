48622f2d3caf6abeca9ec0ab03405411ef6f727f
NSVN-PhamMinhDuc <duc_pm@nittsusystem-vn.com>
2017-05-11 14:28:43 +0700

    update kdl021

--- nts.uk/web/nts.uk.at.web/src/main/webapp/view/kdl/021/a/kdl021.a.service.ts
+++ nts.uk/web/nts.uk.at.web/src/main/webapp/view/kdl/021/a/kdl021.a.service.ts
@@ -1,12 +1,12 @@
 module kdl021.a.service {
     var paths = {
-        getAllDivItem: "at/record/divergencetime/getAllItem"
+        getAllDivItem: "at/record/divergencetime/getPossibleItem"
     }
     /**
     * get all divergence item id(id co the chon)
     */
-    export function getAllDivItemId(): JQueryPromise<Array<model.DivergenceItem>> {
-        return nts.uk.request.ajax("at", paths.getAllDivItem);
+    export function getPossibleItem(arrPossible: Array<string>): JQueryPromise<Array<model.DivergenceItem>> {
+        return nts.uk.request.ajax("at", paths.getAllDivItem , arrPossible);
     }
     export module model {
         export class DivergenceItem {
@@ -17,4 +17,5 @@ module kdl021.a.service {
             attendanceAtr: number;
         }
     }
+
 }
