94fb38fd8ae28066c1686abb06dd54ca739cc5bd
nws-anhnm <anhnm@newwave.vn>
2017-05-16 18:11:32 +0700

    update generator.

--- nts.uk/file/pr/nts.uk.file.pr.infra/src/main/java/nts/uk/pr/file/infra/payment/PaymentReportVerticalGenerator.java
+++ nts.uk/file/pr/nts.uk.file.pr.infra/src/main/java/nts/uk/pr/file/infra/payment/PaymentReportVerticalGenerator.java
@@ -6,15 +6,11 @@ package nts.uk.pr.file.infra.payment;
 
 import java.util.List;
 
-import com.aspose.cells.BorderType;
 import com.aspose.cells.Cell;
-import com.aspose.cells.CellBorderType;
 import com.aspose.cells.Cells;
-import com.aspose.cells.Color;
 import com.aspose.cells.PageOrientationType;
 import com.aspose.cells.PageSetup;
 import com.aspose.cells.Style;
-import com.aspose.cells.TextAlignmentType;
 import com.aspose.cells.Workbook;
 import com.aspose.cells.Worksheet;
 
@@ -29,120 +25,202 @@ import nts.uk.shr.infra.file.report.aspose.cells.AsposeCellsReportGenerator;
  */
 public class PaymentReportVerticalGenerator extends AsposeCellsReportGenerator implements PaymentGenerator {
 
-	private static final int FIRST_ITEM = 0;
-	private static final String SHEET_NAME = "SHEET 1";
+	/** The Constant FIRST_ITEM. */
+	public static final int FIRST_ITEM = 0;
+
+	/** The Constant SHEET_NAME. */
+	public static final String SHEET_NAME = "SHEET 1";
+
+	/** The Constant FIRST_COLUMN. */
+	public static final int FIRST_COLUMN = 0;
+
+	/** The Constant ITEMS_PER_ROW. */
+	public static final int ITEMS_PER_ROW = 9;
+
+	/** The section title style. */
+	private Style sectionTitleStyle;
+
+	/** The header style. */
+	private Style headerStyle;
+
+	/** The name style. */
+	private Style nameStyle;
+
+	/** The value style. */
+	private Style valueStyle;
+
+	/** The cells. */
+	private Cells cells;
 
 	/*
 	 * (non-Javadoc)
 	 * 
 	 * @see
-	 * nts.uk.pr.file.infra.wagetable.Generator#generate(nts.uk.shr.infra.file.
-	 * report.aspose.cells.AsposeCellsReportContext,
-	 * nts.uk.ctx.pr.core.app.wagetable.command.WtUpdateCommand)
+	 * nts.uk.pr.file.infra.payment.PaymentGenerator#generate(nts.uk.shr.infra.
+	 * file.report.aspose.cells.AsposeCellsReportContext,
+	 * nts.uk.file.pr.app.export.payment.data.PaymentReportData)
 	 */
 	@Override
 	public void generate(AsposeCellsReportContext context, PaymentReportData reportData) {
 		Workbook workbook = context.getWorkbook();
 		Worksheet worksheet = workbook.getWorksheets().get(FIRST_ITEM);
 		this.setupPage(worksheet);
+		this.cells = worksheet.getCells();
 		worksheet.setName(SHEET_NAME);
-		Cells cells = worksheet.getCells();
-		Style defaultStyle = cells.get("A2").getStyle();
+
+		// Get style from template.
+		this.valueStyle = cells.get("B11").getStyle();
+		this.nameStyle = cells.get("B10").getStyle();
+		this.headerStyle = cells.get("A10").getStyle();
+		this.sectionTitleStyle = cells.get("A8").getStyle();
 
 		// Get first employee
-		PaymentReportDto dto = reportData.getReportData().get(FIRST_ITEM);
+		PaymentReportDto employee = reportData.getReportData().get(FIRST_ITEM);
 
-		// Payment
-		// Cell cell = cells.get("A1");
-		// cell.setValue("支給項目 ");
-		List<SalaryItemDto> paymentItems = dto.getPaymentItems();
-		List<SalaryItemDto> deductionItems = dto.getDeductionItems();
-		List<SalaryItemDto> attendanceItems = dto.getAttendanceItems();
-		List<SalaryItemDto> articleItems = dto.getArticleItems();
+		// Set Title.
+		cells.get("D1").setValue("給与明細書");
+		// TODO: convert to Japanese era.
+		cells.get("D2").setValue(employee.getProcessingYm());
 
-		// Fill data.
-		Ahihi ahihi = new Ahihi();
-		ahihi.style = defaultStyle;
-		ahihi.cells = cells;
+		// Set Employee information.
+		cells.get("B4").setValue("部門コード");
+		cells.get("F4").setValue("個人コード");
+		cells.get("H4").setValue("氏名");
+		cells.get("B4").setStyle(nameStyle);
+		cells.get("F4").setStyle(nameStyle);
+		cells.get("H4").setStyle(nameStyle);
+		cells.get("B5").setValue(employee.getDepartmentInfo().getDepartmentCode());
+		cells.get("F5").setValue(employee.getEmployeeInfo().getEmployeeCode());
+		cells.get("H5").setValue(employee.getEmployeeInfo().getEmployeeName());
 
-		// Print
-		ahihi.printHeader("payment");
-		ahihi.print(paymentItems);
-		ahihi.nextRow();
+		// Write category content.
+		CategoryWriter writer = new CategoryWriter();
 
-		ahihi.printHeader("deduction");
-		ahihi.print(deductionItems);
-		ahihi.nextRow();
+		writer.writeSectionTitle("支給額");
+		writer.writeCategoryHeader("支給");
+		writer.writeCategoryContent(employee.getPaymentItems());
+		writer.nextRow();
+		writer.nextRow();
 
-		ahihi.printHeader("attendance");
-		ahihi.print(attendanceItems);
+		writer.writeSectionTitle("控除");
+		writer.writeCategoryHeader("控除");
+		writer.writeCategoryContent(employee.getDeductionItems());
+		writer.nextRow();
+		writer.nextRow();
+		writer.nextRow();
 
-		ahihi.printHeader("article");
-		ahihi.print(articleItems);
+		writer.writeSectionTitle("勤怠/記事");
+		writer.writeCategoryHeader("勤怠");
+		writer.writeCategoryContent(employee.getAttendanceItems());
+
+		writer.writeCategoryHeader("記事");
+		writer.writeCategoryContent(employee.getArticleItems());
+		writer.nextRow();
+
+		// Remarks.
+		Cell remark = cells.get(writer.currentRow, FIRST_COLUMN);
+		remark.setValue("備考:");
+		remark.setStyle(sectionTitleStyle);
 
 	}
 
+	/**
+	 * Sets the up page.
+	 *
+	 * @param worksheet the new up page
+	 */
 	private void setupPage(Worksheet worksheet) {
 		PageSetup pageSetup = worksheet.getPageSetup();
 		pageSetup.setOrientation(PageOrientationType.PORTRAIT);
 	}
 
-}
+	/**
+	 * The Class CategoryWriter.
+	 */
+	private class CategoryWriter {
 
-class Ahihi {
-	public static int firstColumn = 0;
-	public static int totalColumns = 1;
-	int firstRow = 10;
-	int currentRow = firstRow;
-	int currentColumn = firstColumn + 1;
-	Style style;
-	Cells cells;
+		/** The first row. */
+		protected int firstRow = 9;
 
-	public void enter() {
-		//cells.get(currentRow, currentColumn).setStyle(style);
-		//cells.get(currentRow+1, currentColumn).setStyle(style);
-		currentColumn = firstColumn + 1;
-		currentRow += 2;
-	}
+		/** The current row. */
+		protected int currentRow = firstRow;
 
-	public void nextRow() {
-		currentRow += 1;
-		firstRow +=1;
-	}
+		/** The current column. */
+		protected int currentColumn = FIRST_COLUMN + 1;
 
-	public void print(List<SalaryItemDto> listItem) {
-		for (int i = 0; i < listItem.size(); i++) {
-			if (currentColumn == 9) {
-				enter();
+		/**
+		 * Enter.
+		 */
+		// Reset column & enter 2 rows.
+		protected void enter() {
+			currentColumn = FIRST_COLUMN + 1;
+			currentRow += 2;
+		}
+
+		/**
+		 * Next row.
+		 */
+		protected void nextRow() {
+			currentRow += 1;
+			firstRow += 1;
+		}
+
+		/**
+		 * Write category content.
+		 *
+		 * @param listItem the list item
+		 */
+		protected void writeCategoryContent(List<SalaryItemDto> listItem) {
+			listItem.forEach(item -> {
+				if (currentColumn == ITEMS_PER_ROW + 1) {
+					enter();
+				}
+				Cell nameCell = cells.get(currentRow, currentColumn);
+				Cell valueCell = cells.get(currentRow + 1, currentColumn);
+				nameCell.setStyle(nameStyle);
+				valueCell.setStyle(valueStyle);
+
+				nameCell.setValue(item.getItemName());
+				valueCell.setValue(item.getItemVal());
+				currentColumn++;
+			});
+			enter();
+			mergeHeader();
+			firstRow = currentRow;
+		}
+
+		/**
+		 * Write section title.
+		 *
+		 * @param title the title
+		 */
+		protected void writeSectionTitle(String title) {
+			Cell sectionTitle = cells.get(firstRow - 1, FIRST_COLUMN);
+			sectionTitle.setValue(title);
+			sectionTitle.setStyle(sectionTitleStyle);
+		}
+
+		/**
+		 * Write category header.
+		 *
+		 * @param headerName the header name
+		 */
+		protected void writeCategoryHeader(String headerName) {
+			cells.get(firstRow, FIRST_COLUMN).setValue(headerName);
+		}
+
+		/**
+		 * Merge header.
+		 */
+		protected void mergeHeader() {
+			final int totalColumns = 1;
+			final int totalRows = currentRow - firstRow;
+			int temp = firstRow;
+			while (temp < currentRow) {
+				cells.get(temp, FIRST_COLUMN).setStyle(headerStyle);
+				temp++;
 			}
-			SalaryItemDto item = listItem.get(i);
-			Cell nameCell = cells.get(currentRow, currentColumn);
-			Cell valueCell = cells.get(currentRow + 1, currentColumn);
-			nameCell.setStyle(style);
-			valueCell.setStyle(style);
-
-			nameCell.setValue(item.getItemName());
-			valueCell.setValue(item.getItemVal());
-			currentColumn++;
+			cells.merge(firstRow, FIRST_COLUMN, totalRows, totalColumns);
 		}
-		merge();
-		enter();
-		firstRow = currentRow;
 	}
-
-	public void printHeader(String val) {
-		Cell cell = cells.get(firstRow, firstColumn);
-		cell.setValue(val);
-	}
-
-	public void merge() {
-		int row = firstRow;
-		int totalRows = currentRow+2-firstRow;
-		while (row < currentRow+2) {
-			cells.get(row, firstColumn).setStyle(style);
-			row++;
-		}
-		cells.merge(firstRow, firstColumn, totalRows, totalColumns);
-	}
-
 }
