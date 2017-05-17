4fd2faa6b17187cb38452f8e7d016ff297ea0ca2
NWS-ThanhNC <thanhnc@newwave.vn>
2017-05-17 17:22:17 +0700

    Modify KMF001 - Screen A.

--- nts.uk/web/nts.uk.pr.web/src/main/webapp/view/kmf/001/a/kmf001.a.vm.ts
+++ nts.uk/web/nts.uk.pr.web/src/main/webapp/view/kmf/001/a/kmf001.a.vm.ts
@@ -8,6 +8,54 @@ module nts.uk.pr.view.kmf001.a {
             constructor() {
                 var self = this;
             }
+
+            /**
+             * Start page.
+             */
+            private startPage(): JQueryPromise<any> {
+                var self = this;
+                var dfd = $.Deferred();
+
+                // resolve.
+                dfd.resolve();
+
+                return dfd.promise();
+            }
+
+            // 優先順位の設定
+            public openPrioritySettingDialog(): void {
+                nts.uk.ui.windows.sub.modal('/view/kmf/001/b/index.xhtml', { title: '優先順位の設定', dialogClass: 'no-close' });
+            }
+
+            // 年次有給休暇
+            public openAnnualPaidLeaveSettingDialog(): void {
+                nts.uk.ui.windows.sub.modal('/view/kmf/001/c/index.xhtml', { title: '優先順位の設定', dialogClass: 'no-close' });
+            }
+
+            // 積立年休
+            public openYearlyReservedSettingDialog(): void {
+                nts.uk.ui.windows.sub.modal('/view/kmf/001/d/index.xhtml', { title: '優先順位の設定', dialogClass: 'no-close' });
+            }
+
+            // 代休
+            public openTemporaryHolidaysSettingDialog(): void {
+                nts.uk.ui.windows.sub.modal('/view/kmf/001/f/index.xhtml', { title: '優先順位の設定', dialogClass: 'no-close' });
+            }
+
+            // 振休
+            public openResurgenceSettingDialog(): void {
+                nts.uk.ui.windows.sub.modal('/view/kmf/001/h/index.xhtml', { title: '優先順位の設定', dialogClass: 'no-close' });
+            }
+
+            // 60H超休
+            public open60hOvertimeSettingDialog(): void {
+                nts.uk.ui.windows.sub.modal('/view/kmf/001/j/index.xhtml', { title: '優先順位の設定', dialogClass: 'no-close' });
+            }
+
+            // 看護介護休暇
+            public openNursingCareLeaveSettingDialog(): void {
+                nts.uk.ui.windows.sub.modal('/view/kmf/001/l/index.xhtml', { title: '優先順位の設定', dialogClass: 'no-close' });
+            }
         }
     }
 }
