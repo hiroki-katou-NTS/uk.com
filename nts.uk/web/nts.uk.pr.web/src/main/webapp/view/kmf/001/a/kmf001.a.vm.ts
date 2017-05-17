4becd3f2246147b998cbdba4e814a9e97415a700
NWS-ThanhNC <thanhnc@newwave.vn>
2017-05-17 09:04:55 +0700

    KMF001: Add UI screen A

---
+++ nts.uk/web/nts.uk.pr.web/src/main/webapp/view/kmf/001/a/kmf001.a.vm.ts
@@ -0,0 +1,13 @@
+module nts.uk.pr.view.kmf001.a {
+    export module viewmodel {
+        export class ScreenModel {
+
+            // Dirty checker
+            dirtyChecker: nts.uk.ui.DirtyChecker;
+
+            constructor() {
+                var self = this;
+            }
+        }
+    }
+}
