78a21b172a2b9ee6329820383578fb315662097e
nws-quyennt <quyennt@newwave.vn>
2017-05-18 18:10:43 +0700

    UI kmf001 screen D

---
+++ nts.uk/web/nts.uk.pr.web/src/main/webapp/view/kmf/001/d/kmf001.d.vm.ts
@@ -0,0 +1,39 @@
+module nts.uk.pr.view.kmf001.a {
+    export module viewmodel {
+        export class ScreenModel {
+            
+            numberOfYear: KnockoutObservable<number>;
+            maxOfDays: KnockoutObservable<number>;
+            textEditorOption: KnockoutObservable<any>;
+
+            // Dirty checker
+            dirtyChecker: nts.uk.ui.DirtyChecker;
+
+            constructor() {
+                var self = this;
+                self.numberOfYear = ko.observable(1);
+                self.maxOfDays = ko.observable(40);
+                self.textEditorOption = ko.mapping.fromJS(new nts.uk.ui.option.TextEditorOption({
+                    width: "50px",
+                    textmode: "text",
+                    placeholder: "Type st",
+                    textalign: "left"
+                }));
+            }
+            
+            public startPage(): JQueryPromise<void> {
+                var dfd = $.Deferred<void>();
+                dfd.resolve();
+                return dfd.promise();
+            }
+            
+            public backToHistorySelection() {
+                
+            }
+            
+            public register() {
+                
+            }
+        }
+    }
+}
