77db4e0c6a8f81ae8356722ad0d86c7f380bbf6d
NSVN-BuiVanChinh <chinh_bv@nittsusystem-vn.com>
2017-05-19 11:32:36 +0700

    qpp014-i: update export text

--- nts.uk/file/pr/nts.uk.file.pr.app/src/main/java/nts/uk/file/pr/app/export/banktransfer/data/BankTransferIReport.java
+++ nts.uk/file/pr/nts.uk.file.pr.app/src/main/java/nts/uk/file/pr/app/export/banktransfer/data/BankTransferIReport.java
@@ -1,8 +1,11 @@
 package nts.uk.file.pr.app.export.banktransfer.data;
 
+import java.util.List;
+
 import lombok.Data;
 
 @Data
 public class BankTransferIReport {
 	private BankTransferIRpHeader header;
+	private List<BankReportData> data;
 }
