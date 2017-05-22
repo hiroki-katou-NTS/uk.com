af94c5ea919a769ba67de925bddc624c9e1fd300
Vu Cong Tuong <tuong_vc@nittsusystem-vn.com>
2017-05-22 09:50:30 +0700

    update code ui screen B

--- nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qpp/021/b/qpp021.b.start.ts
+++ nts.uk/web/nts.uk.pr.web/src/main/webapp/view/qpp/021/b/qpp021.b.start.ts
@@ -1,8 +1,9 @@
-module nts.uk.pr.view.qpp021.b {
+module qpp021.b {
     __viewContext.ready(function() {
         let screenModel = new viewmodel.ScreenModel();
         screenModel.startPage().done(function() {
             __viewContext.bind(screenModel);
+            //
         });
     });
 }
