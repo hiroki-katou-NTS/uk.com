cf4014c1d6818e70af3f8187e16fff5958929972
nws-duongtv <nws-duongtv@newwave.vn>
2017-05-23 17:31:22 +0700

    update dto report

--- nts.uk/file/pr/nts.uk.file.pr.app/src/main/java/nts/uk/file/pr/app/export/payment/data/dto/PaymentRefundPaddingDto.java
+++ nts.uk/file/pr/nts.uk.file.pr.app/src/main/java/nts/uk/file/pr/app/export/payment/data/dto/PaymentRefundPaddingDto.java
@@ -4,10 +4,11 @@
  *****************************************************************/
 package nts.uk.file.pr.app.export.payment.data.dto;
 
-import java.math.BigDecimal;
-
 import lombok.Getter;
 import lombok.Setter;
+import nts.uk.ctx.pr.report.app.payment.refundsetting.refundpadding.find.dto.RefundPaddingOnceOut;
+import nts.uk.ctx.pr.report.app.payment.refundsetting.refundpadding.find.dto.RefundPaddingThreeOut;
+import nts.uk.ctx.pr.report.app.payment.refundsetting.refundpadding.find.dto.RefundPaddingTwoOut;
 
 /**
  * The Class PrintSettingDto.
@@ -17,40 +18,13 @@ import lombok.Setter;
 public class PaymentRefundPaddingDto {
 	/** The print type. */
 	private int printType;
-
-	/** The padding top. */
-	// 余白上 E_INP_001
-	private BigDecimal paddingTop;
-
-	/** The padding left. */
-	// 余白左 E_INP_002, F_INP_003, G_INP_004
-	private BigDecimal paddingLeft;
-
-	/** The upper area padding top. */
-	// 上段上 F_INP_001, G_INP_001
-	private BigDecimal upperAreaPaddingTop;
-
-	/** The under area padding top. */
-	// 下段上 F_INP_002, G_INP_003
-	private BigDecimal underAreaPaddingTop;
-
-	/** The is show break line. */
-	// 区切線の出力 F_SEL_001, G_SEL_001
-	private int isShowBreakLine;
-
-	/** The break line margin. */
-	// 区切線の補正 F_INP_004
-	private BigDecimal breakLineMargin;
-
-	/** The middle area padding top. */
-	// 中段上 G_INP_002
-	private BigDecimal middleAreaPaddingTop;
-
-	/** The break line margin top. */
-	// 区切線の補正上 G_INP_005
-	private BigDecimal breakLineMarginTop;
-
-	/** The break line margin buttom. */
-	// 区切線の補正下 G_INP_006
-	private BigDecimal breakLineMarginButtom;
+	
+	/** The refund padding two dto. */
+	private RefundPaddingTwoOut refundPaddingTwoDto;
+	
+	/** The refund padding once dto. */
+	private RefundPaddingOnceOut refundPaddingOnceDto;
+	
+	/** The refund padding three dto. */
+	private RefundPaddingThreeOut refundPaddingThreeDto;
 }
