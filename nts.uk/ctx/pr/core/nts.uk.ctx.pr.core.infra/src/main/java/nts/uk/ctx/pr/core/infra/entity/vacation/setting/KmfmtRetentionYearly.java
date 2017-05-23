76675b131af9db0977a4ca5f453d5ddba9c40935
nws_tiendd <tiendd_int@newwave.vn>
2017-05-23 15:30:50 +0700

    Add entity KmfmtRetentionYearly.

---
+++ nts.uk/ctx/pr/core/nts.uk.ctx.pr.core.infra/src/main/java/nts/uk/ctx/pr/core/infra/entity/vacation/setting/KmfmtRetentionYearly.java
@@ -0,0 +1,71 @@
+/******************************************************************
+ * Copyright (c) 2015 Nittsu System to present.                   *
+ * All right reserved.                                            *
+ *****************************************************************/
+package nts.uk.ctx.pr.core.infra.entity.vacation.setting;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import nts.uk.shr.infra.data.entity.UkJpaEntity;
+
+/**
+ * The Class KmfmtRetentionYearly.
+ */
+@Entity
+@Table(name = "KMFMT_RETENTION_YEARLY")
+public class KmfmtRetentionYearly extends UkJpaEntity {
+    
+    /** The cid. */
+    @Id
+    @NotNull
+    @Column(name = "CID")
+    private String cid;
+    
+    /** The year amount. */
+    @NotNull
+    @Column(name = "YEAR_AMOUNT")
+    private short yearAmount;
+    
+    /** The max days retention. */
+    @NotNull
+    @Column(name = "MAX_DAYS_RETENTION")
+    private short maxDaysRetention;
+    
+    /* (non-Javadoc)
+     * @see nts.arc.layer.infra.data.entity.JpaEntity#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (cid != null ? cid.hashCode() : 0);
+        return hash;
+    }
+
+    /* (non-Javadoc)
+     * @see nts.arc.layer.infra.data.entity.JpaEntity#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof KmfmtRetentionYearly)) {
+            return false;
+        }
+        KmfmtRetentionYearly other = (KmfmtRetentionYearly) object;
+        if ((this.cid == null && other.cid != null) || (this.cid != null && !this.cid.equals(other.cid))) {
+            return false;
+        }
+        return true;
+    }
+
+	/* (non-Javadoc)
+	 * @see nts.arc.layer.infra.data.entity.JpaEntity#getKey()
+	 */
+	@Override
+	protected Object getKey() {
+		return this.cid;
+	}
+    
+}
