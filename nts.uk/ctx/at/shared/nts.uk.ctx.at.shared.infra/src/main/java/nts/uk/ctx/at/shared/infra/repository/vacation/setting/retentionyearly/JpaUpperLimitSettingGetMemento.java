dc725daf170868fd4fd21b8203a502696e8a398c
nws-quyennt <quyennt@newwave.vn>
2017-05-24 09:36:57 +0700

    update file name kmf001 d

---
+++ nts.uk/ctx/at/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/repository/vacation/setting/retentionyearly/JpaUpperLimitSettingGetMemento.java
@@ -0,0 +1,26 @@
+package nts.uk.ctx.at.shared.infra.repository.vacation.setting.retentionyearly;
+
+import nts.uk.ctx.at.shared.dom.vacation.setting.retentionyearly.MaxDaysRetention;
+import nts.uk.ctx.at.shared.dom.vacation.setting.retentionyearly.RetentionYearsAmount;
+import nts.uk.ctx.at.shared.dom.vacation.setting.retentionyearly.UpperLimitSettingGetMemento;
+import nts.uk.ctx.at.shared.infra.entity.vacation.setting.KmfmtRetentionYearly;
+
+public class JpaUpperLimitSettingGetMemento implements UpperLimitSettingGetMemento{
+	
+	private KmfmtRetentionYearly typeValue;
+
+	public JpaUpperLimitSettingGetMemento(KmfmtRetentionYearly typeValue) {
+			this.typeValue = typeValue;
+		}
+	
+	@Override
+	public RetentionYearsAmount getRetentionYearsAmount() {
+		return new RetentionYearsAmount((int) this.typeValue.getYearAmount());
+	}
+
+	@Override
+	public MaxDaysRetention getMaxDaysCumulation() {
+		return new MaxDaysRetention((int) this.typeValue.getMaxDaysRetention());
+	}
+
+}
