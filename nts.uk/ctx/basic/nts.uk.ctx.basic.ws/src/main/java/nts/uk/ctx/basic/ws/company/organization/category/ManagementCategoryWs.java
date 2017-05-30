bf0db0080848417e5491aa576b86a9bfa7d2b82f
nws-duongtv <nws-duongtv@newwave.vn>
2017-05-30 17:33:42 +0700

    add webservice

---
+++ nts.uk/ctx/basic/nts.uk.ctx.basic.ws/src/main/java/nts/uk/ctx/basic/ws/company/organization/category/ManagementCategoryWs.java
@@ -0,0 +1,36 @@
+/******************************************************************
+ * Copyright (c) 2017 Nittsu System to present.                   *
+ * All right reserved.                                            *
+ *****************************************************************/
+package nts.uk.ctx.basic.ws.company.organization.category;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import nts.arc.layer.ws.WebService;
+import nts.uk.ctx.basic.app.find.company.organization.category.ManagementCategoryFinder;
+import nts.uk.ctx.basic.app.find.company.organization.category.dto.ManagementCategoryFindDto;
+
+/**
+ * The Class ManagementCategoryWs.
+ */
+@Path("basic/company/organization/management/category")
+@Produces(MediaType.APPLICATION_JSON)
+public class ManagementCategoryWs  extends WebService{
+
+	/** The finder. */
+	@Inject
+	private ManagementCategoryFinder finder;
+	
+	
+	@Path("findAll")
+	@POST
+	public List<ManagementCategoryFindDto> init() {
+		return this.finder.findAll();
+	}
+}
