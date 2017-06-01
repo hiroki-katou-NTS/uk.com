f69bc78ab53a85734044df6dae97fc3a33295b85
NSVN-PhamMinhDuc <duc_pm@nittsusystem-vn.com>
2017-06-01 13:05:41 +0700

    fix loi depoy

--- nts.uk/web/nts.uk.at.web/src/main/webapp/view/kdl/014/a/kdl014.a.service.ts
+++ nts.uk/web/nts.uk.at.web/src/main/webapp/view/kdl/014/a/kdl014.a.service.ts
@@ -1,21 +1,21 @@
 module kdl014.a.service {
-//    var paths = {
-//        //getStampByEmployeeCode: "at/record/stampreference/getstampbyemployeecode/{cardNumber}/{startDate}/{endDate}"
-//    }
-//    /**
-//     * get list External Budget
-//     */
-//    export function getListExternalBudget(): JQueryPromise<any> {
-//        return nts.uk.request.ajax("at", paths.getStampByEmployeeCode);
-//    }
-//    
-//
-//    export module model {
-//        export class ExternalBudgetDto {
-//            externalBudgetCode: string;
-//            externalBudgetName: string;
-//            budgetAtr: number;
-//            unitAtr: number;
-//        }
-//    }
+    var paths = {
+        getStampByEmployeeCode: "at/record/stampreference/getstampbyemployeecode/{cardNumber}/{startDate}/{endDate}"
+    }
+    /**
+     * get list External Budget
+     */
+    export function getListExternalBudget(): JQueryPromise<any> {
+        return nts.uk.request.ajax("at", paths.getStampByEmployeeCode);
+    }
+    
+
+    export module model {
+        export class ExternalBudgetDto {
+            externalBudgetCode: string;
+            externalBudgetName: string;
+            budgetAtr: number;
+            unitAtr: number;
+        }
+    }
 }
