1e206362beb2fd990dd0fb905a84f9882e215c13
nws-duongtv <nws-duongtv@newwave.vn>
2017-06-01 10:35:51 +0700

    update primitivevalue

--- nts.uk/ctx/basic/nts.uk.ctx.basic.dom/src/main/java/nts/uk/ctx/basic/dom/company/organization/employee/EmployeeId.java
+++ nts.uk/ctx/basic/nts.uk.ctx.basic.dom/src/main/java/nts/uk/ctx/basic/dom/company/organization/employee/EmployeeId.java
@@ -1,5 +1,28 @@
+/******************************************************************
+ * Copyright (c) 2017 Nittsu System to present.                   *
+ * All right reserved.                                            *
+ *****************************************************************/
 package nts.uk.ctx.basic.dom.company.organization.employee;
 
-public class EmployeeId {
+import nts.arc.primitive.StringPrimitiveValue;
+import nts.arc.primitive.constraint.StringMaxLength;
+
+/**
+ * The Class EmployeeId.
+ */
+@StringMaxLength(120)
+public class EmployeeId extends StringPrimitiveValue<EmployeeId> {
+
+	/** The Constant serialVersionUID. */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Instantiates a new employee id.
+	 *
+	 * @param rawValue the raw value
+	 */
+	public EmployeeId(String rawValue) {
+		super(rawValue);
+	}
 
 }
--- nts.uk/ctx/basic/nts.uk.ctx.basic.dom/src/main/java/nts/uk/ctx/basic/dom/company/organization/employee/PersonId.java
+++ nts.uk/ctx/basic/nts.uk.ctx.basic.dom/src/main/java/nts/uk/ctx/basic/dom/company/organization/employee/PersonId.java
@@ -1,5 +1,28 @@
+/******************************************************************
+ * Copyright (c) 2017 Nittsu System to present.                   *
+ * All right reserved.                                            *
+ *****************************************************************/
 package nts.uk.ctx.basic.dom.company.organization.employee;
 
-public class PersonId {
+import nts.arc.primitive.StringPrimitiveValue;
+import nts.arc.primitive.constraint.StringMaxLength;
+
+/**
+ * The Class PersonId.
+ */
+@StringMaxLength(120)
+public class PersonId extends StringPrimitiveValue<PersonId> {
+
+	/** The Constant serialVersionUID. */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Instantiates a new person id.
+	 *
+	 * @param rawValue the raw value
+	 */
+	public PersonId(String rawValue) {
+		super(rawValue);
+	}
 
 }
