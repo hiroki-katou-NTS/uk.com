202332225f6168cfc7573751aa8ee77b99cd911a
Dương Ngô <ngoducduonglucky@gmail.com>
2017-06-02 17:50:12 +0700

    generate entity for kmf001-L

---
+++ nts.uk/ctx/at/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/entity/vacation/setting/nursingleave/KmfmtWorkType_.java
@@ -0,0 +1,18 @@
+/******************************************************************
+ * Copyright (c) 2017 Nittsu System to present.                   *
+ * All right reserved.                                            *
+ *****************************************************************/
+package nts.uk.ctx.at.shared.infra.entity.vacation.setting.nursingleave;
+
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+/**
+ * The Class KmfmtWorkType_.
+ */
+@StaticMetamodel(KmfmtWorkType.class)
+public class KmfmtWorkType_ {
+    
+    /** The cid. */
+    public static volatile SingularAttribute<KmfmtWorkType, String> cid;
+}
