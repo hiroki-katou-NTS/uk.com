1f914d984633db56fc92049446839085979bfe37
nws_tiendd <tiendd_int@newwave.vn>
2017-06-05 13:40:00 +0700

    add stateless.

--- nts.uk/ctx/basic/nts.uk.ctx.basic.infra/src/main/java/nts/uk/ctx/basic/infra/repository/company/organization/employment/JpaEmploymentRepository.java
+++ nts.uk/ctx/basic/nts.uk.ctx.basic.infra/src/main/java/nts/uk/ctx/basic/infra/repository/company/organization/employment/JpaEmploymentRepository.java
@@ -2,9 +2,12 @@ package nts.uk.ctx.basic.infra.repository.company.organization.employment;
 
 import java.util.List;
 
+import javax.ejb.Stateless;
+
 import nts.uk.ctx.basic.dom.company.organization.employment.Employment;
 import nts.uk.ctx.basic.dom.company.organization.employment.EmploymentRepository;
 
+@Stateless
 public class JpaEmploymentRepository implements EmploymentRepository {
 
 	@Override
