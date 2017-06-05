4e0c94a01902945f63391d5b1150142adaa0d06e
nws-anhnm <anhnm@newwave.vn>
2017-06-05 18:07:58 +0700

    add statutory working hours setting domain.

---
+++ nts.uk/ctx/at/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/employment/statutory/worktime/NormalSetting.java
@@ -0,0 +1,17 @@
+/******************************************************************
+ * Copyright (c) 2017 Nittsu System to present.                   *
+ * All right reserved.                                            *
+ *****************************************************************/
+package nts.uk.ctx.at.shared.dom.employment.statutory.worktime;
+
+/**
+ * The Class NormalSetting.
+ */
+public class NormalSetting {
+	
+	/** The statutory setting. */
+	private WorkingTimeSetting statutorySetting;
+
+	/** The week start. */
+	private WeekStart weekStart;
+}
---
+++ nts.uk/ctx/at/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/employment/statutory/worktime/YearMonth.java
@@ -0,0 +1,5 @@
+package nts.uk.ctx.at.shared.dom.employment.statutory.worktime;
+
+public class YearMonth {
+
+}
