8fcde71f68ca33f21c800e11f643a8ebb891a5e6
NSVC\hungnm <hungnm910@gmail.com>
2017-06-06 11:17:40 +0700

    init domain KMK005

---
+++ nts.uk/ctx/at/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/bonuspay/primitives/ReferenceDateCode.java
@@ -0,0 +1,12 @@
+/**
+ * 10:31:15 AM Jun 6, 2017
+ */
+package nts.uk.ctx.at.shared.dom.bonuspay.primitives;
+
+/**
+ * @author hungnm
+ *
+ */
+public class ReferenceDateCode {
+
+}
---
+++ nts.uk/ctx/at/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/bonuspay/timeitem/BonusPayTimeItem.java
@@ -0,0 +1,12 @@
+/**
+ * 9:15:47 AM Jun 6, 2017
+ */
+package nts.uk.ctx.at.shared.dom.bonuspay.timeitem;
+
+/**
+ * @author hungnm
+ *
+ */
+public class BonusPayTimeItem {
+
+}
