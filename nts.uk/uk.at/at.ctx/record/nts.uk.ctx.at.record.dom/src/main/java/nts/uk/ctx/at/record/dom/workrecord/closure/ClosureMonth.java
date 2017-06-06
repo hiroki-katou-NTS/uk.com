e86eea4fe9bca6697caa7a57b5f5a7c11de132ee
nws-duongtv <nws-duongtv@newwave.vn>
2017-06-06 17:14:31 +0700

    update jpa Closure

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/workrecord/closure/ClosureMonth.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/workrecord/closure/ClosureMonth.java
@@ -5,15 +5,26 @@
 package nts.uk.ctx.at.record.dom.workrecord.closure;
 
 import lombok.Getter;
+import lombok.Setter;
 import nts.arc.layer.dom.DomainObject;
 
 /**
  * The Class ClosureMonth.
  */
 @Getter
+@Setter
 public class ClosureMonth extends DomainObject{
 	
 	/** The processing date. */
 	private ClosureYearMonth processingDate;
+	
+	/**
+	 * Instantiates a new closure month.
+	 *
+	 * @param value the value
+	 */
+	public ClosureMonth(Integer value) {
+		this.processingDate = ClosureYearMonth.of(value);
+	}
 	
 }
--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/workrecord/closure/ClosureRepository.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/workrecord/closure/ClosureRepository.java
@@ -1,5 +1,38 @@
+/******************************************************************
+ * Copyright (c) 2017 Nittsu System to present.                   *
+ * All right reserved.                                            *
+ *****************************************************************/
 package nts.uk.ctx.at.record.dom.workrecord.closure;
 
-public class ClosureRepository {
+import java.util.List;
+
+/**
+ * The Interface ClosureRepository.
+ */
+public interface ClosureRepository {
+	
+	/**
+	 * Adds the.
+	 *
+	 * @param closure the closure
+	 */
+	public void add(Closure closure);
+	
+	
+	/**
+	 * Update.
+	 *
+	 * @param closure the closure
+	 */
+	public void update(Closure closure);
+	
+	
+	/**
+	 * Gets the all closure.
+	 *
+	 * @param companyId the company id
+	 * @return the all closure
+	 */
+	public List<Closure> getAllClosure(String companyId);
 
 }
