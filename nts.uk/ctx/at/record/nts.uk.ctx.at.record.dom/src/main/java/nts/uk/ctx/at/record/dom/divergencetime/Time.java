92cd349d88d5d6ad015de3175a355f17d78d74bb
NSVN-TranThiHoa <hoa_tt@nittsusystem-vn.com>
2017-06-06 10:02:49 +0700

    kmk011: bổ sung constraint cho kiểu Time

--- nts.uk/ctx/at/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/divergencetime/Time.java
+++ nts.uk/ctx/at/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/divergencetime/Time.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 import nts.arc.primitive.DecimalPrimitiveValue;
 import nts.arc.primitive.PrimitiveValue;
 import nts.arc.primitive.constraint.DecimalRange;
-@DecimalRange(min="0",max="5999")
+@DecimalRange(max="5999",min="0")
 public class Time extends DecimalPrimitiveValue<PrimitiveValue<BigDecimal>> {
 	private static final long serialVersionUID = 1L;
 	public Time(BigDecimal rawValue) {
