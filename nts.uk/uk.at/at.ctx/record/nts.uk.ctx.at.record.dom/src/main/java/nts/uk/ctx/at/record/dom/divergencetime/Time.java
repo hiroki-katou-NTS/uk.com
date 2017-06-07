46e30190b98caa5757ac2f501d5a4c6d375238d1
NSVN-TranThiHoa <hoa_tt@nittsusystem-vn.com>
2017-06-07 09:48:03 +0700

    kmk011: thêm name

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/divergencetime/Time.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/divergencetime/Time.java
@@ -4,12 +4,17 @@ import java.math.BigDecimal;
 
 import nts.arc.primitive.DecimalPrimitiveValue;
 import nts.arc.primitive.PrimitiveValue;
+import nts.arc.primitive.TimeDurationPrimitiveValue;
 import nts.arc.primitive.constraint.DecimalRange;
 import nts.arc.primitive.constraint.TimeRange;
 @TimeRange(max="99:59", min="00:00")
-public class Time extends DecimalPrimitiveValue<PrimitiveValue<BigDecimal>> {
-	private static final long serialVersionUID = 1L;
-	public Time(BigDecimal rawValue) {
-		super(rawValue);
+public class Time extends TimeDurationPrimitiveValue<PrimitiveValue<BigDecimal>> {
+	 private static final long serialVersionUID = 1L;
+	 public Time(int hour, int minute) {
+	  super(hour, minute);
+	 }
+	 
+	 public Time(BigDecimal time) {
+	  super(Long.valueOf(time.toString()));
+	 }
 	}
-}
