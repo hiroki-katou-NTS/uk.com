f4e19583a674e707781f83957d7f8b3a151d26b9
Pham Ba Vu <vu_pb@nittsusystem.co.jp>
2017-06-07 10:26:43 +0700

    replace com.js.web by lastest version of com.js.web

--- nts.uk/uk.base/base.web/nts.uk.com.js.web/src/main/webapp/lib/nittsu/ui/ko-ext/editor-ko-ext.ts
+++ nts.uk/uk.base/base.web/nts.uk.com.js.web/src/main/webapp/lib/nittsu/ui/ko-ext/editor-ko-ext.ts
@@ -189,9 +189,10 @@ module nts.uk.ui.koExtentions {
         }
 
         getValidator(data: any): validation.IValidator {
+            var name: string = data.name !== undefined ? ko.unwrap(data.name) : "";
             var required: boolean = (data.required !== undefined) ? ko.unwrap(data.required) : false;
             var constraintName = (data.constraint !== undefined) ? ko.unwrap(data.constraint) : "";
-            return new validation.StringValidator(constraintName, { required: required });
+            return new validation.StringValidator(name, constraintName, { required: required });
         }
     }
 
@@ -217,9 +218,10 @@ module nts.uk.ui.koExtentions {
         }
 
         getValidator(data: any): validation.IValidator {
+            var name: string = data.name !== undefined ? ko.unwrap(data.name) : "";
             var required: boolean = (data.required !== undefined) ? ko.unwrap(data.required) : false;
             var constraintName = (data.constraint !== undefined) ? ko.unwrap(data.constraint) : "";
-            return new validation.StringValidator(constraintName, { required: required });
+            return new validation.StringValidator(name, constraintName, { required: required });
         }
     }
 
@@ -276,11 +278,12 @@ module nts.uk.ui.koExtentions {
         }
 
         getValidator(data: any): validation.IValidator {
+            var name = data.name !== undefined ? ko.unwrap(data.name) : "";
             var constraintName = (data.constraint !== undefined) ? ko.unwrap(data.constraint) : "";
             var required = (data.required !== undefined) ? ko.unwrap(data.required) : false;
             this.editorOption['required'] = required;   
             
-            return new validation.NumberValidator(constraintName, this.editorOption);
+            return new validation.NumberValidator(name, constraintName, this.editorOption);
         }
     }
 
@@ -311,12 +314,13 @@ module nts.uk.ui.koExtentions {
         }
 
         getValidator(data: any): validation.IValidator {
+            var name = data.name !== undefined ? ko.unwrap(data.name) : "";
             var constraintName = (data.constraint !== undefined) ? ko.unwrap(data.constraint) : "";
             var option = (data.option !== undefined) ? ko.mapping.toJS(data.option) : this.getDefaultOption();
             var required: boolean = (data.required !== undefined) ? ko.unwrap(data.required) : false;
             var inputFormat: string = (data.inputFormat !== undefined) ? ko.unwrap(data.inputFormat) : option.inputFormat;
             var mode: string = (data.mode !== undefined) ? ko.unwrap(data.mode) : "";
-            return new validation.TimeValidator(constraintName, { required: required, outputFormat: inputFormat, mode: mode });
+            return new validation.TimeValidator(name, constraintName, { required: required, outputFormat: inputFormat, mode: mode });
         }
     }
 
