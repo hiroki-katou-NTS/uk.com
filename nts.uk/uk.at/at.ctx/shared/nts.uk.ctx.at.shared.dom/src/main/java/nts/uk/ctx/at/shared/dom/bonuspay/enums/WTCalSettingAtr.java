121fc37811db45906172d32bb720acf8715c13c8
NSVC\hungnm <hungnm910@gmail.com>
2017-06-07 10:43:51 +0700

    update domain kmk005

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/bonuspay/enums/WTCalSettingAtr.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/bonuspay/enums/WTCalSettingAtr.java
@@ -3,10 +3,21 @@
  */
 package nts.uk.ctx.at.shared.dom.bonuspay.enums;
 
+import lombok.AllArgsConstructor;
+
 /**
  * @author hungnm
  *
  */
+@AllArgsConstructor
 public enum WTCalSettingAtr {
 
+	// 自動計算しない
+	NON_CALCULATION(0, "Enum_AutoCalcAtr_NonCalculation"),
+	// 自動計算する
+	CALCULATION(1, "Enum_AutoCalcAtr_Calculation");
+
+	public final int value;
+
+	public final String nameId;
 }
