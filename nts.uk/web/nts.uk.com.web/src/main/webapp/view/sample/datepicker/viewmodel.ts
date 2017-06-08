4a01ddb17dee433625437b56fc2dae751a9b0b33
NSVC-DinhTheLam <lam_dt@nittsusystem-vn.com>
2017-06-08 10:56:51 +0700

    Update catalog document

--- nts.uk/web/nts.uk.com.web/src/main/webapp/view/sample/datepicker/viewmodel.ts
+++ nts.uk/web/nts.uk.com.web/src/main/webapp/view/sample/datepicker/viewmodel.ts
@@ -11,26 +11,6 @@ module sample.datepicker.viewmodel {
             //self.date = ko.observable(new Date(2000,0,1));
             self.date = ko.observable(nts.uk.time.UTCDate(2000, 0, 1));
         }
-
-        pushDataUp() {
-            var self = this;
-            var data = new TestDto("0002", "Test Dto 0002", self.date(), self.date())
-            nts.uk.request.ajax("com", "ctx/proto/test/find", data).done(function(res: any) {
-                console.log(res.dateTime);
-                var dto = new TestDto(res.code, res.name, res.date, res.dateTime);
-                self.dateString(moment.utc(dto.dateTime).format());
-            });
-        }
-
-        pushDataDown() {
-            var self = this;
-            var data = new TestDto("0001", "Test Dto 0001", self.dateString(), self.dateString())
-            nts.uk.request.ajax("com", "ctx/proto/test/find", data).done(function(res: any) {
-                console.log(res.dateTime);
-                var dto = new TestDto(res.code, res.name, res.date, res.dateTime);
-                self.date(dto.dateTime);
-            });
-        }
     }
 
     class TestDto {
