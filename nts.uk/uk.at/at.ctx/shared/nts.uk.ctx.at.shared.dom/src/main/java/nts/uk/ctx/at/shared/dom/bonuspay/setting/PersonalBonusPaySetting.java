be0ec017a6edc2a9b4024d2683bd20a5e675adca
NSVC\hungnm <hungnm910@gmail.com>
2017-06-08 12:51:40 +0700

    update domain kmk005

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/bonuspay/setting/PersonalBonusPaySetting.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/bonuspay/setting/PersonalBonusPaySetting.java
@@ -3,10 +3,33 @@
  */
 package nts.uk.ctx.at.shared.dom.bonuspay.setting;
 
+import lombok.Getter;
+import nts.arc.layer.dom.AggregateRoot;
+import nts.uk.ctx.at.shared.dom.bonuspay.primitives.BonusPaySettingCode;
+import nts.uk.ctx.at.shared.dom.bonuspay.primitives.EmployeeId;
+
 /**
  * @author hungnm
  *
  */
-public class PersonalBonusPaySetting {
+@Getter
+public class PersonalBonusPaySetting extends AggregateRoot {
 
+	private EmployeeId employeeId;
+
+	private BonusPaySettingCode bonusPaySettingCode;
+
+	private PersonalBonusPaySetting(EmployeeId employeeId, BonusPaySettingCode bonusPaySettingCode) {
+		super();
+		this.employeeId = employeeId;
+		this.bonusPaySettingCode = bonusPaySettingCode;
+	}
+
+	private PersonalBonusPaySetting() {
+		super();
+	}
+
+	public static PersonalBonusPaySetting createFromJavaType(String employeeId, String bonusPaySettingCode) {
+		return new PersonalBonusPaySetting(new EmployeeId(employeeId), new BonusPaySettingCode(bonusPaySettingCode));
+	}
 }
