bc02cffebb151c85eea8ba895a222344ea92ba15
NSVC\hieult <hieultph03963@fpt.edu.vn>
2017-06-08 10:06:44 +0700

    UPdate view model CCG 030

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/030/a/ccg030.a.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/030/a/ccg030.a.vm.ts
@@ -74,7 +74,7 @@ module ccg030.a.viewmodel {
             var flowMenu = ko.mapping.toJS(self.selectedFlowMenu);
             var topPageCode = flowMenu.topPageCode;
             $(".nts-input").trigger("validate");
-            if (util.isNullOrEmpty(self.selectedFlowMenu().fileID())
+            if (util.isNullOrEmpty(self.selectedFlowMenu().fileID()))
                 $('#file_upload').ntsError('set', '選択されていないファイル');
             if (!errors.hasError()) {
                 nts.uk.ui.block.invisible();
