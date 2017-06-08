66cf3a1070dd738b20b5e53ac4918673749df5c3
NSVC\hungnm <hungnm910@gmail.com>
2017-06-08 14:46:19 +0700

    update repository domain kmk005

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/bonuspay/repository/WPBonusPaySettingRepository.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/bonuspay/repository/WPBonusPaySettingRepository.java
@@ -3,10 +3,22 @@
  */
 package nts.uk.ctx.at.shared.dom.bonuspay.repository;
 
+import java.util.List;
+
+import nts.uk.ctx.at.shared.dom.bonuspay.setting.WorkplaceBonusPaySetting;
+
 /**
  * @author hungnm
  *
  */
 public interface WPBonusPaySettingRepository {
+
+	List<WorkplaceBonusPaySetting> getListSetting(List<String> lstWorkplace);
+
+	void addListSetting(List<WorkplaceBonusPaySetting> lstSetting);
+
+	void updateListSetting(List<WorkplaceBonusPaySetting> lstSetting);
+
+	void removeListSetting(List<WorkplaceBonusPaySetting> lstSetting);
 
 }
