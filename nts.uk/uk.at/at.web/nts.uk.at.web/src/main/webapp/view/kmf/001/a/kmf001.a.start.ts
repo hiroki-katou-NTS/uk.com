64d5c36f96612422901938f058966c2e1f5a41ae
NWS-ThanhNC <thanhnc@newwave.vn>
2017-06-09 15:19:19 +0700

    Revert file.

---
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmf/001/a/kmf001.a.start.ts
@@ -0,0 +1,8 @@
+module nts.uk.pr.view.kmf001.a {
+    __viewContext.ready(function() {
+        var screenModel = new viewmodel.ScreenModel();
+        screenModel.startPage().done(function() {
+            __viewContext.bind(screenModel);
+        });
+    });
+}
