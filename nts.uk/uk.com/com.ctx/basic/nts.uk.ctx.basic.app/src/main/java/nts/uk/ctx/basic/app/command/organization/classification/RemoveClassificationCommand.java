53e9e195747ba83ddb71d33dc4b50b2af86cb27c
nws_tiendd <tiendd_int@newwave.vn>
2017-06-12 11:49:19 +0700

    Remove unused class.

--- nts.uk/uk.com/com.ctx/basic/nts.uk.ctx.basic.app/src/main/java/nts/uk/ctx/basic/app/command/organization/classification/RemoveClassificationCommand.java
+++
@@ -1,9 +0,0 @@
-package nts.uk.ctx.basic.app.command.organization.classification;
-
-import lombok.Getter;
-
-@Getter
-public class RemoveClassificationCommand {
-	
-	private String classificationCode;
-}
