5eae1d7750ea8b7422f38cfb6065b2236881a356
Nguyen Hoang Son <son_hoang@nittsusystem-vn.com>
2017-06-12 14:45:16 +0700

    dom + infa ccg018

--- nts.uk/uk.com/com.ctx/sys/portal/nts.uk.ctx.sys.portal.infra/src/main/java/nts/uk/ctx/sys/portal/infra/entity/toppagesetting/CcgptTopPagePersonSetPK.java
+++ nts.uk/uk.com/com.ctx/sys/portal/nts.uk.ctx.sys.portal.infra/src/main/java/nts/uk/ctx/sys/portal/infra/entity/toppagesetting/CcgptTopPagePersonSetPK.java
@@ -2,11 +2,15 @@ package nts.uk.ctx.sys.portal.infra.entity.toppagesetting;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
-import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
+/**
+ * 
+ * @author sonnh1
+ *
+ */
 @AllArgsConstructor
 @NoArgsConstructor
 @Embeddable
@@ -14,7 +18,7 @@ public class CcgptTopPagePersonSetPK {
 	/** The companyId. */
 	@Column(name = "CID")
 	public String companyId;
-	
+
 	/** The employee id. */
 	@Column(name = "SID")
 	public String employeeId;
