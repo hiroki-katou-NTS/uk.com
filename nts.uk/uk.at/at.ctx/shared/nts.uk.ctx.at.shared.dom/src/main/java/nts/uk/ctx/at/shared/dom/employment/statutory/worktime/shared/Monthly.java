b0d59d3712caf7f4487d9d9f53e077b206ab6134
nws-anhnm <anhnm@newwave.vn>
2017-06-12 15:35:28 +0700

    update domain.

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/employment/statutory/worktime/shared/Monthly.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/employment/statutory/worktime/shared/Monthly.java
@@ -4,17 +4,18 @@
  *****************************************************************/
 package nts.uk.ctx.at.shared.dom.employment.statutory.worktime.shared;
 
+import lombok.Getter;
 import nts.arc.layer.dom.DomainObject;
-import nts.uk.ctx.at.shared.dom.common.AttendanceTime;
 import nts.uk.ctx.at.shared.dom.common.Month;
 
 /**
  * The Class Monthly.
  */
+@Getter
 public class Monthly extends DomainObject {
 
 	/** The time. */
-	private AttendanceTime time;
+	private int time;
 
 	/** The month. */
 	private Month month;
