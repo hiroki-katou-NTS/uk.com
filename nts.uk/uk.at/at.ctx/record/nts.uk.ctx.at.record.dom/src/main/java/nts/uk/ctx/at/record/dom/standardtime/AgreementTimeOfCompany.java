a459205717d7a3bab1fd4b149978b01388c8d324
NSVC\nampt <nam_pt@nittsusystem-vn.com>
2017-06-13 17:37:24 +0700

    update domain + infra KMK008

---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/standardtime/AgreementTimeOfCompany.java
@@ -0,0 +1,20 @@
+package nts.uk.ctx.at.record.dom.standardtime;
+
+import lombok.Getter;
+import nts.uk.ctx.at.record.dom.standardtime.enums.LaborSystemType;
+
+/**
+ * 
+ * @author nampt
+ *
+ */
+@Getter
+public class AgreementTimeOfCompany {
+	
+	private String companyId;
+
+	private String basicSettingId;
+	
+	private LaborSystemType laborSystemType;
+
+}
