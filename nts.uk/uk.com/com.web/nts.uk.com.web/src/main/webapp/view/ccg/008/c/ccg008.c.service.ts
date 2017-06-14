da7d588360a9c9e38cc7d0301d25e663c283402f
NSVN-TranThiHoa <hoa_tt@nittsusystem-vn.com>
2017-06-14 15:56:21 +0700

    ccg002: dialogC

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/008/c/ccg008.c.service.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/008/c/ccg008.c.service.ts
@@ -2,20 +2,28 @@ module nts.uk.com.view.ccg008.c.service {
 
     var paths: any = {
         getSelectMyTopPage: "topageselfsetting/select",
-        save: "topageselfsetting/save"
+        save: "topageselfsetting/save",
+        getTopPageSelfSet: "topageselfsetting/finditemselected"
     }
     
     /**
      * 
      */
-    export function getSelectMyTopPage(): JQueryPromise<Array<any>> {
-        return nts.uk.request.ajax(paths.getSelectMyTopPage);
+    export function getSelectMyTopPage(): JQueryPromise<Array<viewmodel.model.Node>> {
+        return nts.uk.request.ajax("com",paths.getSelectMyTopPage);
     }
     
     /**
      * 
      */
     export function save(data: any): JQueryPromise<Array<any>> {
-        return nts.uk.request.ajax(paths.save, data);
+        return nts.uk.request.ajax("com",paths.save, data);
+    }
+    /**
+     * hoatt
+     * get top page self set
+     */
+    export function getTopPageSelfSet(): JQueryPromise<viewmodel.model.TopPageSelfSet>{
+        return nts.uk.request.ajax("com",paths.getTopPageSelfSet);
     }
 }
