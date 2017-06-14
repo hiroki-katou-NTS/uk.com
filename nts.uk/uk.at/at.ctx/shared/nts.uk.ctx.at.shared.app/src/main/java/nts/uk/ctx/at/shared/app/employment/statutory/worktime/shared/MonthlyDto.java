5a47f000714ce0e1d91811d2794f181c8d9f341d
nws-anhnm <anhnm@newwave.vn>
2017-06-14 10:22:53 +0700

    update app+ws

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.app/src/main/java/nts/uk/ctx/at/shared/app/employment/statutory/worktime/shared/MonthlyDto.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.app/src/main/java/nts/uk/ctx/at/shared/app/employment/statutory/worktime/shared/MonthlyDto.java
@@ -29,7 +29,7 @@ public class MonthlyDto {
 	 * @return the list
 	 */
 	public static List<MonthlyDto> fromDomain(List<Monthly> domain) {
-		return domain.stream().map(item -> new MonthlyDto(item.getMonth().v(), item.getTime().v()))
+		return domain.stream().map(item -> new MonthlyDto(item.getMonth().getValue(), item.getTime().v()))
 				.collect(Collectors.toList());
 	}
 }
