a06deadd14ca8c3977bbe405bdc97a260567779b
nws-anhnm <anhnm@newwave.vn>
2017-06-20 12:04:15 +0700

    update workplaceWtSetting.

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/employment/statutory/worktime/workplace/WorkPlaceWtSettingRepository.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/employment/statutory/worktime/workplace/WorkPlaceWtSettingRepository.java
@@ -4,6 +4,7 @@
  *****************************************************************/
 package nts.uk.ctx.at.shared.dom.employment.statutory.worktime.workplace;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -30,15 +31,26 @@ public interface WorkPlaceWtSettingRepository {
 	 *
 	 * @param companyId the company id
 	 * @param year the year
+	 * @param workPlaceId the work place id
 	 */
-	void remove(String companyId, int year);
+	void remove(String companyId, int year, String workPlaceId);
 
 	/**
 	 * Find.
 	 *
 	 * @param companyId the company id
 	 * @param year the year
+	 * @param workPlaceId the work place id
 	 * @return the optional
 	 */
-	Optional<WorkPlaceWtSetting> find(String companyId, int year);
+	Optional<WorkPlaceWtSetting> find(String companyId, int year, String workPlaceId);
+
+	/**
+	 * Find all.
+	 *
+	 * @param companyId the company id
+	 * @param year the year
+	 * @return the list
+	 */
+	List<String> findAll(String companyId, int year);
 }
