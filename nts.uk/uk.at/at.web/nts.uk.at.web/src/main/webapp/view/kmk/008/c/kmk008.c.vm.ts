21ea013e00c6c82aa489b67ac5dc70b52ede6d7e
Vu Cong Tuong <tuong_vc@nittsusystem-vn.com>
2017-06-20 17:44:52 +0700

    commit coding Ui kmk008

---
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/008/c/kmk008.c.vm.ts
@@ -0,0 +1,25 @@
+module nts.uk.at.view.kmk008.c {
+    export module viewmodel {
+        export class ScreenModel {
+            printType: number;
+            constructor() {
+                let self = this;
+                            
+            }
+            startPage(): JQueryPromise<any> {
+                let self = this;
+                let dfd = $.Deferred();
+                return dfd.promise();
+            }
+        }
+
+        export class ItemModel {
+            code: number;
+            name: string;
+            constructor(code: number, name: string) {
+                this.code = code;
+                this.name = name;
+            }
+        }
+    }
+}
