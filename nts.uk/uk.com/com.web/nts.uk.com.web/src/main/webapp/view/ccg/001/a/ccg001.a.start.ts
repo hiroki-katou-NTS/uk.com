bcf70b89a4308364ddcc8fbc1ad7c337eb173768
nws-duongtv <nws-duongtv@newwave.vn>
2017-06-20 16:33:42 +0700

    add ccg001 component

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/001/a/ccg001.a.start.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/001/a/ccg001.a.start.ts
@@ -3,10 +3,6 @@ module nts.uk.com.view.ccg001.a {
         var screenModel = new viewmodel.ScreenModel();
         screenModel.startPage().done(function() {
             __viewContext.bind(screenModel);
-            $(".accordion").accordion({
-                active: false,
-                collapsible: true
-            });
         });
     });
 }
