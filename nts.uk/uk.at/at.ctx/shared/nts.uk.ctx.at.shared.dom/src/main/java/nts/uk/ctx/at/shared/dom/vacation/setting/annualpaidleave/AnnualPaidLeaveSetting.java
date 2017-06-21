d7030e2415c50c3287a27297a95ca9cb1f66994d
NWS-ThanhNC <thanhnc@newwave.vn>
2017-06-21 10:34:22 +0700

    Update behavior KMF001 domain.

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/vacation/setting/annualpaidleave/AnnualPaidLeaveSetting.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/vacation/setting/annualpaidleave/AnnualPaidLeaveSetting.java
@@ -14,56 +14,69 @@ import nts.uk.ctx.at.shared.dom.vacation.setting.ManageDistinct;
  */
 @Getter
 
-/* (non-Javadoc)
+/*
+ * (non-Javadoc)
+ * 
  * @see java.lang.Object#hashCode()
  */
-@EqualsAndHashCode(callSuper = true, of = {"companyId"})
+@EqualsAndHashCode(callSuper = true, of = { "companyId" })
 public class AnnualPaidLeaveSetting extends AggregateRoot {
-    
-    /** The company id. */
-    // 会社ID
-    private String companyId;
-    
-    /** The acquisition setting. */
-    // 取得設定
-    private AcquisitionSetting acquisitionSetting;
-    
-    /** The year manage type. */
-    // 年休管理区分
-    private ManageDistinct yearManageType;
-    
-    /** The manage annual setting. */
-    // 年休管理設定
-    private ManageAnnualSetting manageAnnualSetting;
-    
-    /** The time setting. */
-    // 時間年休管理設定
-    private TimeVacationSetting timeSetting;
-    
-    /**
-     * Instantiates a new annual paid leave setting.
-     *
-     * @param memento the memento
-     */
-    public AnnualPaidLeaveSetting(AnnualPaidLeaveSettingGetMemento memento) {
-        super();
-        this.companyId = memento.getCompanyId();
-        this.acquisitionSetting = memento.getAcquisitionSetting();
-        this.yearManageType = memento.getYearManageType();
-        this.manageAnnualSetting = memento.getManageAnnualSetting();
-        this.timeSetting = memento.getTimeSetting();
-    }
-    
-    /**
-     * Save to memento.
-     *
-     * @param memento the memento
-     */
-    public void saveToMemento(AnnualPaidLeaveSettingSetMemento memento) {
-        memento.setCompanyId(this.companyId);
-        memento.setAcquisitionSetting(this.acquisitionSetting);
-        memento.setYearManageType(this.yearManageType);
-        memento.setManageAnnualSetting(this.manageAnnualSetting);
-        memento.setTimeSetting(this.timeSetting);
-    }
+
+	/** The company id. */
+	// 会社ID
+	private String companyId;
+
+	/** The acquisition setting. */
+	// 取得設定
+	private AcquisitionSetting acquisitionSetting;
+
+	/** The year manage type. */
+	// 年休管理区分
+	private ManageDistinct yearManageType;
+
+	/** The manage annual setting. */
+	// 年休管理設定
+	private ManageAnnualSetting manageAnnualSetting;
+
+	/** The time setting. */
+	// 時間年休管理設定
+	private TimeVacationSetting timeSetting;
+
+	/**
+	 * Checks if is managed.
+	 *
+	 * @return true, if is managed
+	 */
+	public boolean isManaged() {
+		return this.yearManageType.equals(ManageDistinct.YES);
+	}
+
+	/**
+	 * Instantiates a new annual paid leave setting.
+	 *
+	 * @param memento
+	 *            the memento
+	 */
+	public AnnualPaidLeaveSetting(AnnualPaidLeaveSettingGetMemento memento) {
+		super();
+		this.companyId = memento.getCompanyId();
+		this.acquisitionSetting = memento.getAcquisitionSetting();
+		this.yearManageType = memento.getYearManageType();
+		this.manageAnnualSetting = memento.getManageAnnualSetting();
+		this.timeSetting = memento.getTimeSetting();
+	}
+
+	/**
+	 * Save to memento.
+	 *
+	 * @param memento
+	 *            the memento
+	 */
+	public void saveToMemento(AnnualPaidLeaveSettingSetMemento memento) {
+		memento.setCompanyId(this.companyId);
+		memento.setAcquisitionSetting(this.acquisitionSetting);
+		memento.setYearManageType(this.yearManageType);
+		memento.setManageAnnualSetting(this.manageAnnualSetting);
+		memento.setTimeSetting(this.timeSetting);
+	}
 }
