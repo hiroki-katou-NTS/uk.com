d2615b9e089bd7c48a77d6662104a37e1df82e32
nws-duongtv <nws-duongtv@newwave.vn>
2017-06-22 14:56:34 +0700

    fix bug

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/012/a/kmk012.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/012/a/kmk012.a.vm.ts
@@ -67,6 +67,7 @@ module nts.uk.at.view.kmk012.a {
                 
                 
                 self.closureModel.useClassification.subscribe(function(val: number){
+                    self.clearValiate();
                     if (val == 0) {
                         self.enableUseClassification(false);
                         self.enableChangeClosureDateAnd(self.enableChangeClosureDate() && self.enableUseClassification());
@@ -144,22 +145,26 @@ module nts.uk.at.view.kmk012.a {
                 dto = new ClosureSaveDto();
                 dto.closureId = self.closureModel.closureId();
                 dto.useClassification = self.closureModel.useClassification();
-                dto.month = self.closureModel.month();
+                if (dto.useClassification == 1) {
+                    dto.month = self.closureModel.month();
+                }else {
+                    dto.month = 0;
+                }
                 return dto;
             }
             
-            clearValiate(){
-                $('#inpMonth').ntsError('clear');
-                $('#inpname').ntsError('clear');
-                 
+            clearValiate() {
+                $('.nts-input').ntsError('clear')
             }
             
             validateClient(): boolean {
-                $("#inpMonth").ntsEditor("validate");
-                $("#inpname").ntsEditor("validate");
-                
-                if($('.nts-input').ntsError('hasError')) {
-                    return true;
+                var self = this;
+                if (self.closureModel.useClassification() == 1) {
+                    $("#inpMonth").ntsEditor("validate");
+                    $("#inpname").ntsEditor("validate");
+                    if ($('.nts-input').ntsError('hasError')) {
+                        return true;
+                    }
                 }
                 return false;
             }
@@ -169,7 +174,7 @@ module nts.uk.at.view.kmk012.a {
                 if (self.closureModel.useClassification() == 1 && self.validateClient()) {
                     return;
                 }
-                self.clearValiate();
+                //self.clearValiate();
                 service.saveClosure(self.collectData()).done(function() {
                     if (self.closureModel.useClassification() == 1) {
                         service.saveClosureHistory(self.collectDataHistory()).done(function() {
