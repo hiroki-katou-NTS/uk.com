773b5eb55d2f8ca46ff0db26817face8239dff79
nws-anhnm <anhnm@newwave.vn>
2017-06-23 09:16:58 +0700

    fix magic number.

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.app/src/main/java/nts/uk/ctx/at/shared/app/employment/statutory/worktime/shared/MonthlyDto.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.app/src/main/java/nts/uk/ctx/at/shared/app/employment/statutory/worktime/shared/MonthlyDto.java
@@ -8,10 +8,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import lombok.Data;
+import nts.gul.util.Time;
 import nts.uk.ctx.at.shared.dom.employment.statutory.worktime.shared.Monthly;
 
 /**
  * The Class MonthlyDto.
+ */
+
+/**
+ * Instantiates a new monthly dto.
  */
 @Data
 public class MonthlyDto {
@@ -26,13 +31,13 @@ public class MonthlyDto {
 	 * From domain.
 	 *
 	 * @param domain the domain
-	 * @return the sets the
+	 * @return the list
 	 */
 	public static List<MonthlyDto> fromDomain(List<Monthly> domain) {
 		return domain.stream().map(item -> {
 			MonthlyDto dto = new MonthlyDto();
 			dto.setMonth(item.getMonth().getValue());
-			dto.setTime(item.getTime().v()/60);
+			dto.setTime(item.getTime().v() / Time.STEP);
 			return dto;
 		}).collect(Collectors.toList());
 	}
