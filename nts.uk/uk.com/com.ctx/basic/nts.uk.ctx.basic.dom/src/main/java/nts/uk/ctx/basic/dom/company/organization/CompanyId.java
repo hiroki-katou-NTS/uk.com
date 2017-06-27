9cc279235d1d95b9a9a440b0b06f498a2f764e90
nws_tiendd <tiendd_int@newwave.vn>
2017-06-27 15:43:59 +0700

    Add comment class.

--- nts.uk/uk.com/com.ctx/basic/nts.uk.ctx.basic.dom/src/main/java/nts/uk/ctx/basic/dom/company/organization/CompanyId.java
+++ nts.uk/uk.com/com.ctx/basic/nts.uk.ctx.basic.dom/src/main/java/nts/uk/ctx/basic/dom/company/organization/CompanyId.java
@@ -1,3 +1,7 @@
+/******************************************************************
+ * Copyright (c) 2015 Nittsu System to present.                   *
+ * All right reserved.                                            *
+ *****************************************************************/
 package nts.uk.ctx.basic.dom.company.organization;
 
 import nts.arc.primitive.constraint.CharType;
@@ -5,6 +9,9 @@ import nts.arc.primitive.constraint.StringCharType;
 import nts.arc.primitive.constraint.StringMaxLength;
 import nts.uk.shr.com.primitive.CodePrimitiveValue;
 
+/**
+ * The Class CompanyId.
+ */
 @StringCharType(CharType.ALPHA_NUMERIC)
 @StringMaxLength(17)
 public class CompanyId extends CodePrimitiveValue<CompanyId> {
