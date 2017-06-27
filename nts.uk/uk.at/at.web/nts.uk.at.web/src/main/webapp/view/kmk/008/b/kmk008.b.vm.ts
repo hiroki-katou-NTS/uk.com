8438baf60b32eb105e7bf04304f74e70aa1b55b0
Vu Cong Tuong <tuong_vc@nittsusystem-vn.com>
2017-06-27 15:05:27 +0700

    coding screen C -D

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/008/b/kmk008.b.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/008/b/kmk008.b.vm.ts
@@ -5,10 +5,14 @@ module nts.uk.at.view.kmk008.b {
             enable: KnockoutObservable<boolean>;
             tabs: KnockoutObservableArray<nts.uk.ui.NtsTabPanelModel>;
             selectedTab: KnockoutObservable<string>;
-            viewmodelC: any = new kmk008.c.viewmodel.ScreenModel();;
+            viewmodelC: any;
+            viewmodelD: any;
+            laborSystemAtr: number = 0;
             constructor() {
                 let self = this;
                 self.show = ko.observable(true);
+                self.viewmodelC = new kmk008.c.viewmodel.ScreenModel(self.laborSystemAtr);
+                self.viewmodelD = new kmk008.d.viewmodel.ScreenModel(self.laborSystemAtr);
                 self.show.subscribe(function(newVal) {
                     if (newVal)
                         $("#sidebar").ntsSideBar("show", 1);
