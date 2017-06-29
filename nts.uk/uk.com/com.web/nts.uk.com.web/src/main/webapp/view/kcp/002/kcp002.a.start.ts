828762c0fdb3e56ab64e5fcb8d45fa06b43679df
nws-hangntt <hangntt@newwave.vn>
2017-06-29 17:41:09 +0700

    KCP002 Update view

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/kcp/002/kcp002.a.start.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/kcp/002/kcp002.a.start.ts
@@ -1,6 +1,12 @@
 module kcp002.a {
-    __viewContext.ready(function () {
+    __viewContext.ready(function() {
         var screenModel = new viewmodel.ScreenModel();
         __viewContext.bind(screenModel);
+        //load Classification list
+        $('#classification-list-setting').ntsListComponent(screenModel.listComponentOption).done(function() {
+            screenModel.classificationList($('#classification-list-setting').getDataList());
+            screenModel.jsonData(JSON.stringify($('#classification-list-setting').getDataList(), undefined,2));
+        });
+
     });
 }
