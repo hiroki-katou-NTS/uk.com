2970c2431be92535f467eb0265cdfbde645c17c0
voducanhvn <voducanhvn@gmail.com>
2017-06-30 14:08:10 +0700

    update UI kmk 008

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/008/k/kmk008.k.service.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/008/k/kmk008.k.service.ts
@@ -1,15 +1,16 @@
 module nts.uk.at.view.kmk008.k {
     export module service {
-        export class Service {
-            paths = {
-
-            };
-            constructor() { }
-
-            public functionDemo(printType: number): JQueryPromise<any> {
-                return null;
-            };
-
+        var paths: any = {
+            findByCodeList: "at/shared/worktime/findByCodeList",
+            findByTime: "at/shared/worktime/findByTime",
+        }
+        
+        export function findByCodeList(command): JQueryPromise<any> {
+            return nts.uk.request.ajax(paths.findByCodeList, command);
+        }
+        
+        export function findByTime(command): JQueryPromise<any> {
+            return nts.uk.request.ajax(paths.findByTime, command);
         }
     }
 }
