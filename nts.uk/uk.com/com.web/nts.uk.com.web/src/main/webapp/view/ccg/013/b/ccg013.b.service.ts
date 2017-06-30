99d69ef00cfcda187a476814808095ce6d3c2fdc
NSVN-BuiVanChinh <chinh_bv@nittsusystem-vn.com>
2017-06-30 09:36:59 +0700

    ccg013-b: fix error typescript and add validate and add focus text box menu bar name

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/013/b/ccg013.b.service.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/013/b/ccg013.b.service.ts
@@ -3,9 +3,29 @@ module nts.uk.sys.view.ccg013.b.service {
         getEditMenuBar: "sys/portal/webmenu/edit",
     }
     
-    export function getEditMenuBar(): JQueryPromise<Array<any>>{
+    export function getEditMenuBar(): JQueryPromise<EditMenuBarDto>{
         return nts.uk.request.ajax("com", paths.getEditMenuBar);
     }
     
-
+    export interface EditMenuBarDto {
+        listMenuClassification: Array<any>,
+        listSelectedAtr: Array<any>,
+        listStandardMenu: Array<MenuBarDto>,
+        listSystem: Array<any>
+    }
+    
+    export interface MenuBarDto {
+        afterLoginDisplay: number,
+        classification: number,
+        code: string,
+        companyId: string,
+        displayName: string,
+        displayOrder: number,
+        logSettingDisplay: number,
+        menuAtr: number,
+        system: number,
+        targetItems: string,
+        url: string,
+        webMenuSetting: number    
+    }
 }
