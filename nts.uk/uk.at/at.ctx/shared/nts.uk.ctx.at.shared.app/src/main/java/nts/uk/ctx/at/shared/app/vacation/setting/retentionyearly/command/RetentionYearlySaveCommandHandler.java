304c3f569e4f2b5f58f74fc2ea437614a1f87a14
nws-quyennt <quyennt@newwave.vn>
2017-06-30 16:45:19 +0700

    update kmf001 screen d, e

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.app/src/main/java/nts/uk/ctx/at/shared/app/vacation/setting/retentionyearly/command/RetentionYearlySaveCommandHandler.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.app/src/main/java/nts/uk/ctx/at/shared/app/vacation/setting/retentionyearly/command/RetentionYearlySaveCommandHandler.java
@@ -11,6 +11,9 @@ import javax.inject.Inject;
 
 import nts.arc.layer.app.command.CommandHandler;
 import nts.arc.layer.app.command.CommandHandlerContext;
+import nts.uk.ctx.at.shared.app.vacation.setting.retentionyearly.command.dto.RetentionYearlyDto;
+import nts.uk.ctx.at.shared.dom.vacation.setting.ManageDistinct;
+import nts.uk.ctx.at.shared.dom.vacation.setting.annualpaidleave.AnnualPaidLeaveSettingRepository;
 import nts.uk.ctx.at.shared.dom.vacation.setting.retentionyearly.RetentionYearlySetting;
 import nts.uk.ctx.at.shared.dom.vacation.setting.retentionyearly.RetentionYearlySettingRepository;
 import nts.uk.shr.com.context.AppContexts;
@@ -25,6 +28,9 @@ public class RetentionYearlySaveCommandHandler extends CommandHandler<RetentionY
 	/** The repository. */
 	@Inject
 	private RetentionYearlySettingRepository repository;
+	
+	@Inject
+	private AnnualPaidLeaveSettingRepository isManagedRepo;
 
 	/*
 	 * (non-Javadoc)
@@ -42,14 +48,29 @@ public class RetentionYearlySaveCommandHandler extends CommandHandler<RetentionY
 
 		// Get Command
 		RetentionYearlySaveCommand command = context.getCommand();
-
+		
+		Optional<RetentionYearlySetting> yearlyRetentionOpt = this.repository.findByCompanyId(companyId);
+		// Find "isManaged"
+		int isManaged = this.isManagedRepo.findByCompanyId(companyId).getYearManageType().value;
+		
+		// Check is managed, keep old values when is not managed
+		if (yearlyRetentionOpt.isPresent() && isManaged == ManageDistinct.NO.value) {
+			RetentionYearlySetting retentionYearlySettingDB = yearlyRetentionOpt.get();
+			RetentionYearlyDto yearlyRetentionDto = command.getRetentionYearly();
+			yearlyRetentionDto.setLeaveAsWorkDays(retentionYearlySettingDB.getLeaveAsWorkDays());
+			yearlyRetentionDto.getUpperLimitSettingDto().setMaxDaysCumulation(
+					retentionYearlySettingDB.getUpperLimitSetting().getMaxDaysCumulation().v());
+			yearlyRetentionDto.getUpperLimitSettingDto().setRetentionYearsAmount(
+					retentionYearlySettingDB.getUpperLimitSetting().getRetentionYearsAmount().v());
+		}
+		
+		// To Domain
 		RetentionYearlySetting retentionYearlySetting = command.toDomain(companyId);
 
 		// validate domain
 		retentionYearlySetting.validate();
 		
-		Optional<RetentionYearlySetting> data = this.repository.findByCompanyId(retentionYearlySetting.getCompanyId());
-		if (data.isPresent()) {
+		if (yearlyRetentionOpt.isPresent()) {
 			this.repository.update(retentionYearlySetting);
 		} else {
 			this.repository.insert(retentionYearlySetting);
