82183437ebac096f36e72fef582c0a42e09a4f6f
NSVN-BuiVanChinh <chinh_bv@nittsusystem-vn.com>
2017-07-07 08:54:18 +0700

    fix error conflic code enum System

--- nts.uk/uk.com/com.ctx/sys/portal/nts.uk.ctx.sys.portal.dom/src/main/java/nts/uk/ctx/sys/portal/dom/enums/System.java
+++ nts.uk/uk.com/com.ctx/sys/portal/nts.uk.ctx.sys.portal.dom/src/main/java/nts/uk/ctx/sys/portal/dom/enums/System.java
@@ -5,27 +5,27 @@ package nts.uk.ctx.sys.portal.dom.enums;
  * @author sonnh1
  *
  */
-public enum System {
+public enum System {	
 	/**
 	 * 0:共通
 	 */
-	Common(0),
+	COMMON(0),
 	/**
 	 * 1:勤次郎
 	 */
-	TimeSheet(1),
+	TIME_SHEET(1),
 	/**
 	 * 2:オフィスヘルパー
 	 */
-	OfficeHelper(2),
+	OFFICE_HELPER(2),
 	/**
 	 * 3:Ｑ太郎
 	 */
-	Kyuyou(3),
+	KYUYOU(3),
 	/**
 	 * 4:人事郎
 	 */
-	Jinjirou(4);
+	JINJIROU(4);
 
 	public final int value;
 
