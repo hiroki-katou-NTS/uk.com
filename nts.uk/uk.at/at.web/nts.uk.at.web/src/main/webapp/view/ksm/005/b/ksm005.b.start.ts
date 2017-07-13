d7eaf92b8b9626676e19d9837d707afd71bd388c
nws-duongtv <nws-duongtv@newwave.vn>
2017-07-13 18:12:47 +0700

    update ui ksm005

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksm/005/b/ksm005.b.start.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksm/005/b/ksm005.b.start.ts
@@ -1,6 +1,6 @@
-module nts.uk.at.view.kmk012.a {
+module nts.uk.at.view.ksm005.b {
     __viewContext.ready(function() {
         var screenModel = new viewmodel.ScreenModel();
-        __viewContext.bind(screenModel);
+            __viewContext.bind(screenModel);
     });
 }
---
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksm/005/c/ksm005.c.service.ts
@@ -0,0 +1,16 @@
+module nts.uk.at.view.ksm005.c {
+    export module service {
+        var paths = {
+        }
+        
+        export module model {
+
+            export interface MonthlyPatternDto {
+                monthlyPatternCode: string;
+                monthlyPatternName: string;
+            }
+                       
+        }
+
+    }
+}
