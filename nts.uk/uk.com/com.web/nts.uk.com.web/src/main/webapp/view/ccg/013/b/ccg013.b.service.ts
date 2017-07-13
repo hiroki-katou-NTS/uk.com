4feced9fc3b7ab4e4c453542bb77d9523b7bed3c
Nguyen Hoang Son <son_hoang@nittsusystem-vn.com>
2017-07-13 17:21:29 +0700

    update code CCG013 B

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/013/b/ccg013.b.service.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/013/b/ccg013.b.service.ts
@@ -2,30 +2,15 @@ module nts.uk.sys.view.ccg013.b.service {
     var paths: any = {
         getEditMenuBar: "sys/portal/webmenu/edit",
     }
-    
-    export function getEditMenuBar(): JQueryPromise<EditMenuBarDto>{
+
+    export function getEditMenuBar(): JQueryPromise<EditMenuBarDto> {
         return nts.uk.request.ajax("com", paths.getEditMenuBar);
     }
-    
+
     export interface EditMenuBarDto {
         listMenuClassification: Array<any>,
         listSelectedAtr: Array<any>,
         listStandardMenu: Array<MenuBarDto>,
         listSystem: Array<any>
-    }
-    
-    export interface MenuBarDto {
-        afterLoginDisplay: number,
-        classification: number,
-        code: string,
-        companyId: string,
-        displayName: string,
-        displayOrder: number,
-        logSettingDisplay: number,
-        menuAtr: number,
-        system: number,
-        targetItems: string,
-        url: string,
-        webMenuSetting: number    
     }
 }
