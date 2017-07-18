6ff9c031a2739175b494b1bc052f64474eb3b354
nws-duongtv <nws-duongtv@newwave.vn>
2017-07-18 16:53:52 +0700

    update ui ksm005 b

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksm/005/b/ksm005.b.service.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksm/005/b/ksm005.b.service.ts
@@ -3,6 +3,9 @@ module nts.uk.at.view.ksm005.b {
         var paths = {
             findAllMonthlyPattern : "ctx/at/schedule/pattern/monthy/findAll",
             findByIdMonthlyPattern: "ctx/at/schedule/pattern/monthy/findById",
+            addMonthlyPattern: "ctx/at/schedule/pattern/monthy/add",
+            updateMonthlyPattern: "ctx/at/schedule/pattern/monthy/update",
+            deleteMonthlyPattern: "ctx/at/schedule/pattern/monthy/delete",
             findByIdWorkMonthlySetting: "ctx/at/schedule/pattern/work/monthy/setting/findById"
         }
         
@@ -23,6 +26,25 @@ module nts.uk.at.view.ksm005.b {
          */
         export function findByIdMonthlyPattern(monthlyPatternCode: string): JQueryPromise<model.WorkMonthlySettingDto[]> {
             return nts.uk.request.ajax('at', paths.findByIdMonthlyPattern + "/" + monthlyPatternCode);
+        }
+        
+        /**
+         * call service add monthly pattern
+         */
+        export function addMonthlyPattern(dto: model.MonthlyPatternDto) :JQueryPromise<void> {
+            return nts.uk.request.ajax('at', paths.addMonthlyPattern, {dto: dto});
+        }
+        /**
+         * call service update monthly pattern
+         */
+        export function updateMonthlyPattern(dto: model.MonthlyPatternDto) :JQueryPromise<void> {
+            return nts.uk.request.ajax('at', paths.updateMonthlyPattern, {dto: dto});
+        }
+        /**
+         * call service delete monthly pattern
+         */
+        export function deleteMonthlyPattern(code: string) :JQueryPromise<void> {
+            return nts.uk.request.ajax('at', paths.deleteMonthlyPattern, {monthlyPattnernCode: code});
         }
         
         export module model {
