d6ee8e0ac9491b6f145226b035712af856b388a1
NSVC\yennth <yen_nt@nittsusystem-vn.com>
2017-07-19 15:28:25 +0700

    ksm002-d: update UI

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksm/002/d/ksm002.d.start.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksm/002/d/ksm002.d.start.ts
@@ -2,6 +2,7 @@
         let screenModel = new ksm002.d.viewmodel.ScreenModel();
         screenModel.startPage().done(function() {
             __viewContext.bind(screenModel);
+            $("#startMonth").focus();
             }); 
         });
 
