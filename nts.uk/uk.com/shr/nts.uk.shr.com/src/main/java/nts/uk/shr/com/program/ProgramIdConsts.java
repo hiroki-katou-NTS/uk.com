13dfca7b5e0daf0ba313b6bce6a873fd540e5bf6
DuDT <du_dt@nittsusystem-vn.comdu_dt@nittsusystem-vn.com>
2017-07-19 15:14:27 +0700

    fix bug #84077

--- nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/com/program/ProgramIdConsts.java
+++ nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/com/program/ProgramIdConsts.java
@@ -5,4 +5,36 @@ public class ProgramIdConsts {
 	public static final String Ccg001d = "Ccg001d";
 
 	public static final String CCG015A = "CCG015A";
+	
+	public static final String CCG015B = "CCG015B";
+	
+	public static final String CCG015C = "CCG015C";
+	
+	public static final String CCG014A = "CCG014A";
+	
+	public static final String CCG014B = "CCG014B";
+	
+	public static final String CCG030A = "CCG030A";
+	
+	public static final String CCG030B = "CCG030B";
+	
+	public static final String CCG031A = "CCG031A";
+	
+	public static final String CCG031B = "CCG031B";
+	
+	public static final String CCG031C = "CCG031C";
+	
+	public static final String KMK011A = "KMK011A";
+	
+	public static final String KMK011B = "KMK011B";
+	
+	public static final String KML001A = "KML001A";
+	
+	public static final String KML001B = "KML001B";
+	
+	public static final String KML001C = "KML001C";
+	
+	public static final String KML001D = "KML001D";
+	
+	public static final String KDL024A = "KDL024A";
 }
