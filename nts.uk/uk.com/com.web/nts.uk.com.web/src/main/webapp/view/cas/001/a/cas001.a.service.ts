4fc8a600e33fdeee72f996cb6fd240e1344c0f6c
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2017-07-26 10:39:41 +0700

    Update CAS001: A : add A1,A2,A3

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cas/001/a/cas001.a.service.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cas/001/a/cas001.a.service.ts
@@ -1,3 +1,21 @@
 module nts.uk.com.view.cas001.a.service {
+    import ajax = nts.uk.request.ajax;
+    var paths = {
+    }
+    export function getPersonRoleList(): JQueryPromise<any> {
 
+        return;
+    }
+
+
+    export interface IPersonRole {
+        roleCode: string;
+        roleName: string;
+        AllowMapBrowsing: number;
+        AllowMapUpLoad: number;
+        AllowDocumentUpload: number;
+        AllowDocumentReference: number;
+        AllowAvatarUpload: number;
+        AllowAvatarReference: number;
+    }
 }
