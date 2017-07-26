6956fd2be4fde27495d8646f1b32175333f4e113
NTS\tin_dh <tin_dh@nittsusystem.co.jp>
2017-07-26 11:20:27 +0900

    create MasterListExportGenerator

---
+++ nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/infra/file/report/masterlist/webservice/MasterListExportQuery.java
@@ -0,0 +1,11 @@
+package nts.uk.shr.infra.file.report.masterlist.webservice;
+
+import lombok.Data;
+
+@Data
+public class MasterListExportQuery {
+
+	private String domainId;
+	
+	private String domainType;
+}
