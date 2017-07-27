f1201f4bce662f2148e099d6f395e813af9a3e88
NWS-ThanhNC <thanhnc@newwave.vn>
2017-07-27 16:03:01 +0700

    Rename col.

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/entity/vacation/setting/sixtyhours/KshstEmp60hVacationPK.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/entity/vacation/setting/sixtyhours/KshstEmp60hVacationPK.java
@@ -12,76 +12,71 @@ import lombok.Setter;
 
 /**
  * The Class KshstEmp60hVacationPK.
- *
- * @author NWS_THANHNC_PC
  */
-
 @Getter
 @Setter
 @Embeddable
 public class KshstEmp60hVacationPK {
-    
-    /** The cid. */
-    @Column(name = "CID")
-    private String cid;
-    
-    /** The contract type cd. */
-    @Column(name = "EMP_TYPE_CD")
-    private String contractTypeCd;
 
-    /**
-     * Instantiates a new kshst emp 60 h vacation PK.
-     */
-    public KshstEmp60hVacationPK() {
-    }
+	/** The cid. */
+	@Column(name = "CID")
+	private String cid;
 
-    /**
-     * Instantiates a new kshst emp 60 h vacation PK.
-     *
-     * @param cid the cid
-     * @param contractTypeCd the contract type cd
-     */
-    public KshstEmp60hVacationPK(String cid, String contractTypeCd) {
-        this.cid = cid;
-        this.contractTypeCd = contractTypeCd;
-    }
+	/** The contract type cd. */
+	@Column(name = "EMPCD")
+	private String contractTypeCd;
 
-    /* (non-Javadoc)
-     * @see java.lang.Object#hashCode()
-     */
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (cid != null ? cid.hashCode() : 0);
-        hash += (contractTypeCd != null ? contractTypeCd.hashCode() : 0);
-        return hash;
-    }
+	/**
+	 * Instantiates a new kshst emp 60 h vacation PK.
+	 */
+	public KshstEmp60hVacationPK() {
+	}
 
-    /* (non-Javadoc)
-     * @see java.lang.Object#equals(java.lang.Object)
-     */
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof KshstEmp60hVacationPK)) {
-            return false;
-        }
-        KshstEmp60hVacationPK other = (KshstEmp60hVacationPK) object;
-        if ((this.cid == null && other.cid != null) || (this.cid != null && !this.cid.equals(other.cid))) {
-            return false;
-        }
-        if ((this.contractTypeCd == null && other.contractTypeCd != null) || (this.contractTypeCd != null && !this.contractTypeCd.equals(other.contractTypeCd))) {
-            return false;
-        }
-        return true;
-    }
+	/**
+	 * Instantiates a new kshst emp 60 h vacation PK.
+	 *
+	 * @param cid the cid
+	 * @param contractTypeCd the contract type cd
+	 */
+	public KshstEmp60hVacationPK(String cid, String contractTypeCd) {
+		this.cid = cid;
+		this.contractTypeCd = contractTypeCd;
+	}
 
-    /* (non-Javadoc)
-     * @see java.lang.Object#toString()
-     */
-    @Override
-    public String toString() {
-        return "entity.KshstEmp60hVacationPK[ cid=" + cid + ", contractTypeCd=" + contractTypeCd + " ]";
-    }
-    
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		int hash = 0;
+		hash += (cid != null ? cid.hashCode() : 0);
+		hash += (contractTypeCd != null ? contractTypeCd.hashCode() : 0);
+		return hash;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object object) {
+		if (!(object instanceof KshstEmp60hVacationPK)) {
+			return false;
+		}
+		KshstEmp60hVacationPK other = (KshstEmp60hVacationPK) object;
+		if ((this.cid == null && other.cid != null)
+				|| (this.cid != null && !this.cid.equals(other.cid))) {
+			return false;
+		}
+		if ((this.contractTypeCd == null && other.contractTypeCd != null)
+				|| (this.contractTypeCd != null
+						&& !this.contractTypeCd.equals(other.contractTypeCd))) {
+			return false;
+		}
+		return true;
+	}
+
 }
