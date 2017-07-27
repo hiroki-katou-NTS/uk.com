500e9775dbc3864959a1edd7c0570124c86063b6
TruongQuocPhong <phong_tq@nittsusystem-vn.com>
2017-07-27 15:05:12 +0700

    update ccg013

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/013/g/ccg013.g.start.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/013/g/ccg013.g.start.ts
@@ -2,6 +2,7 @@ module ccg013.g {
     __viewContext.ready(function() {
         var screenModel = new ccg013.g.viewmodel.ScreenModel();
         screenModel.start();
-         __viewContext.bind(screenModel);
+        __viewContext.bind(screenModel);
+        $("#profile-input").focus();
     });
 }
