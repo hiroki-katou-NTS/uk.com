2b3aaa76c66e63259f42337d74ea4872f96fce5a
Leu Thi Lan <lan_lt@nitssusystem-vn.com>
2017-07-27 14:57:24 +0700

    webservice

---
+++ nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.ws/src/main/java/nts/uk/ctx/bs/person/ws/roles/PersonInfoItemAuthWebservice.java
@@ -0,0 +1,44 @@
+package nts.uk.ctx.bs.person.ws.roles;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import find.roles.auth.item.PersonInfoItemAuthDto;
+import find.roles.auth.item.PersonInfoItemAuthFinder;
+import nts.arc.layer.ws.WebService;
+
+@Path("ctx/bs/person/roles/auth/item")
+@Produces("application/json")
+public class PersonInfoItemAuthWebservice extends WebService {
+	@Inject
+	PersonInfoItemAuthFinder personInfoItemAuthFinder;
+
+	@POST
+	@Path("findAll")
+	public List<PersonInfoItemAuthDto> getAllPersonItemAuth() {
+		return personInfoItemAuthFinder.getAllPersonItemAuth();
+
+	}
+
+	@POST
+	@Path("find/{roleId}/{personCategoryAuthId}")
+	public List<PersonInfoItemAuthDto> getAllPersonItemAuthByCategory(@PathParam("roleId") String roleId,
+			@PathParam("personCategoryAuthId") String personCategoryAuthId) {
+		return personInfoItemAuthFinder.getAllPersonItemAuthByCategory(roleId, personCategoryAuthId);
+
+	}
+
+	@POST
+	@Path("find/{roleId}/{personCategoryAuthId}/{personItemDefId}")
+	public PersonInfoItemAuthDto getDetailPersonItemAuth(@PathParam("roleId") String roleId,
+			@PathParam("personCategoryAuthId") String personCategoryAuthId,
+			@PathParam("personItemDefId") String personItemDefId) {
+		return personInfoItemAuthFinder.getDetailPersonItemAuth(roleId, personCategoryAuthId, personItemDefId).get();
+
+	}
+}
