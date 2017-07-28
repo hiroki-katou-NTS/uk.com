87c4b1807776cb19b383e209a978b1fea17cb1ed
NSVN-PhamMinhDuc <duc_pm@nittsusystem-vn.com>
2017-07-28 11:24:58 +0700

    ksm002  A fix bug duplice list ID

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksm/002/a/ksm002.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksm/002/a/ksm002.a.vm.ts
@@ -393,10 +393,10 @@ module ksm002.a.viewmodel {
         openKsm002CDialog() {
             var self = this;
             nts.uk.ui.windows.sub.modal('/view/ksm/002/c/index.xhtml',{ title: "割増項目の設定", dialogClass: "no-close" }).onClosed(function(): any {
-                self.start();
-                nts.uk.ui._viewModel.bind(content).done(function(){
+                self.start().done(function(){
                     $(".ntsCheckBox").attr("tabindex",5);
-                })      
+                    $(".chkBox ").find("label").eq(0).focus();
+                });
             })
         }
 
@@ -476,7 +476,7 @@ module ksm002.a.viewmodel {
             this.backgroundColor = 'white';
             this.textColor = '#31859C';
             self.listText = listText;
-            self.listId = listId;
+            self.listId = _.cloneDeep(listId);
         }
     }
 
