9ce69ff1483f6f767441a55d1cd8e963932b5419
Leu Thi Lan <lan_lt@nitssusystem-vn.com>
2017-07-28 13:14:35 +0700

    screen D check box

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cas/001/d/cas001.d.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cas/001/d/cas001.d.vm.ts
@@ -1,11 +1,10 @@
 module nts.uk.com.view.cas001.d.viewmodel {
- import windows = nts.uk.ui.windows;
+    import windows = nts.uk.ui.windows;
     import errors = nts.uk.ui.errors;
     import resource = nts.uk.resource;
 
     export class ScreenModel {
         roleList: KnockoutObservableArray<any>;
-        categoryList: KnockoutObservableArray<CategoryAuth>;
         columns: KnockoutObservableArray<NtsGridListColumn>;
         currentRoleCode: KnockoutObservable<string>;
         companyCode: KnockoutObservable<string>;
@@ -15,18 +14,73 @@ module nts.uk.com.view.cas001.d.viewmodel {
         constructor() {
             var self = this;
             self.init();
+            $("#grid").igGrid({
+                columns: [
+                    {
+                        headerText: "<input id='selfAuth' type='checkbox' checked='true' tabindex='-1' >他人</input>", key: 'selfAuth', width: "80px",height:"40px", formatter: function(evt: any, row: any) {
+                            let cb = $("<input  class = 'checkRow selfAuth' type='checkbox'/>");
+                            cb.attr("data-id", row.roleCode);
+                            return cb[0].outerHTML;
+                        }
+                    },
+                    {
+                        headerText: "<input id='otherAuth' type='checkbox' checked='true' tabindex='-1'>本人</input>", key: 'otherAuth', width: "80px", height:"40px",formatter: function(evt: any, row: any) {
+                            let cb = $("<input  class = 'checkRow otherAuth' type='checkbox'/>");
+                            cb.attr("data-id", row.roleCode);
+                            return cb[0].outerHTML;
+                        }
+                    },
+                    { headerText: "コード", key: "roleCode", dataType: "string", width: "100px",height:"40px", hidden: true },
+                    { headerText: "カテゴリ名", key: "roleName", dataType: "string", width: "50px",height:"40px" },
+                    { headerText: '説明', key: 'description', width: "35px", hidden: true,height:"40px" },
 
+                ],
+                primaryKey: 'roleCode',
+                autoGenerateColumns: false,
+                autoCommit: true,
+                dataSource: self.roleList(),
+                width: "700px",
+                height: "310px",
+                features: [
+                    {
+                        name: "Updating",
+                        enableAddRow: false,
+                        editMode: "row",
+                        enableDeleteRow: false,
+                        columnSettings: [
+                            { columnKey: "selfAuth", readOnly: true },
+                            { columnKey: "otherAuth", readOnly: true },
+                            { columnKey: "roleCode", readOnly: true },
+                            { columnKey: "roleName", readOnly: true },
+                            { columnKey: "description", readOnly: true },
+                        ]
+                    }]
+            });
+            $(document).on("click", "#selfAuth", function(evt, ui) {
+                $("#grid").igGridUpdating("endEdit");
+                $("#grid").find("tr").each((index, element) => {
+                    // change state of all checkbox delete
+                    $(element).find(".selfAuth").prop("checked", $("#selfAuth").prop("checked")).trigger("change");
+                });
+            });
+            $(document).on("click", "#otherAuth", function(evt, ui) {
+                $("#grid").igGridUpdating("endEdit");
+                $("#grid").find("tr").each((index, element) => {
+                    // change state of all checkbox delete
+                    $(element).find(".otherAuth").prop("checked", $("#otherAuth").prop("checked")).trigger("change");
+                });
+            });
 
         }
         init(): void {
             var self = this;
-            self.roleList = ko.observableArray([new PersonRole({roleCode:"1", roleName:'A2',selfAuth:true,otherAuth:true}), new PersonRole({roleCode:'2', roleName:'B', selfAuth: true,otherAuth:false})]);
+            self.roleList = ko.observableArray([new PersonRole({ roleCode: "1", roleName: 'A2', selfAuth: true, otherAuth: true }), new PersonRole({ roleCode: '2', roleName: 'B', selfAuth: true, otherAuth: false })]);
             self.categoryList = ko.observableArray([]);
             self.currentRoleCode = ko.observable('');
             self.columns = ko.observableArray([
                 { headerText: 'コード', key: 'roleCode', width: 100, hidden: true },
-                { headerText: '他人', key: 'selfAuth', width: 50,template: "<input type='checkbox' checked='${selfAuth}'/>"},
-                { headerText: '本人', key: 'otherAuth', width: 50,template: "<input type='checkbox' checked='${otherAuth}'/>"},
+                { headerText: '他人', key: 'selfAuth', width: 50, template: "<input type='checkbox' checked='${selfAuth}'/>" },
+                { headerText: '本人', key: 'otherAuth', width: 50, template: "<input type='checkbox' checked='${otherAuth}'/>" },
                 { headerText: 'カテゴリ名', key: 'roleName', width: 180 },
                 { headerText: '説明', key: 'description', width: 50, hidden: true }
             ]);
@@ -39,10 +93,10 @@ module nts.uk.com.view.cas001.d.viewmodel {
             self.selectItemCode = ko.observable(1);
 
         }
-        creatCategory(){
+        creatCategory() {
             windows.close();
         }
-        closeDialog(){
+        closeDialog() {
             windows.close();
         }
     }
