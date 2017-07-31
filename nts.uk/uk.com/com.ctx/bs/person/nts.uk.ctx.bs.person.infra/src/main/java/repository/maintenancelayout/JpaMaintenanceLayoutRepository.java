2efb3639ddce598f6a6c483640ded5589a409a41
NSVN-TruongVanLai <hoanglaisoict@gmail.com>
2017-07-31 13:57:41 +0700

    jpa cps008

--- nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.infra/src/main/java/repository/maintenancelayout/JpaMaintenanceLayoutRepository.java
+++ nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.infra/src/main/java/repository/maintenancelayout/JpaMaintenanceLayoutRepository.java
@@ -6,6 +6,11 @@ package repository.maintenancelayout;
 import java.util.List;
 import java.util.Optional;
 
+import javax.ejb.Stateless;
+
+import entity.maintenencelayout.PpemtMaintenanceLayout;
+import entity.maintenencelayout.PpemtMaintenanceLayoutPk;
+import lombok.val;
 import nts.arc.layer.infra.data.JpaRepository;
 import nts.uk.ctx.bs.person.dom.person.maintenancelayout.MaintenanceLayout;
 import nts.uk.ctx.bs.person.dom.person.maintenancelayout.MaintenanceLayoutRepository;
@@ -14,34 +19,56 @@ import nts.uk.ctx.bs.person.dom.person.maintenancelayout.MaintenanceLayoutReposi
  * @author laitv
  *
  */
-public class JpaMaintenanceLayoutRepository extends JpaRepository implements MaintenanceLayoutRepository{
+@Stateless
+public class JpaMaintenanceLayoutRepository extends JpaRepository implements MaintenanceLayoutRepository {
+
+	private String getAllMaintenanceLayout = "select c FROM  PpemtMaintenanceLayout c";
+
+	private String getDetailLayout = getAllMaintenanceLayout + " Where c.ppemtMaintenanceLayoutPk.layoutId =: layoutId";
+
+	private static MaintenanceLayout toDomain(PpemtMaintenanceLayout entity) {
+		val domain = MaintenanceLayout.createFromJavaType(
+				entity.companyId,
+				entity.ppemtMaintenanceLayoutPk.layoutId,
+				entity.layoutCode, 
+				entity.layoutName);
+		return domain;
+	}
+
+	private static PpemtMaintenanceLayout toEntity(MaintenanceLayout domain) {
+		PpemtMaintenanceLayout entity = new PpemtMaintenanceLayout();
+		entity.ppemtMaintenanceLayoutPk = new PpemtMaintenanceLayoutPk(domain.getMaintenanceLayoutID());
+		entity.companyId = domain.getCompanyId();
+		entity.layoutCode = domain.getLayoutCode().v();
+		entity.layoutName = domain.getLayoutName().v();
+		return entity;
+	}
+
+	@Override
+	public List<MaintenanceLayout> getAllMaintenanceLayout() {
+		return this.queryProxy().query(getAllMaintenanceLayout, PpemtMaintenanceLayout.class).getList(c -> toDomain(c));
+	}
 
 	@Override
 	public void add(MaintenanceLayout maintenanceLayout) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void update(MaintenanceLayout maintenanceLayout) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void remove(String maintenanceLayoutID) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public Optional<MaintenanceLayout> getDetailMaintenanceLayout(String layoutID) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<MaintenanceLayout> getAllMaintenanceLayout() {
 		// TODO Auto-generated method stub
 		return null;
 	}
