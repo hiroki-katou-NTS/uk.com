1f8d1fdbe3f15ae7134738e58f69ade79d788d36
nws-dientx <dientx@newwave.vn>
2017-08-01 11:20:38 +0700

    ccg007 c,d init UI

---
+++ nts.uk/uk.com/com.ctx/sys/gateway/nts.uk.ctx.sys.gateway.app/src/main/java/nts/uk/ctx/sys/gateway/app/find/login/dto/CompanyDto.java
@@ -0,0 +1,17 @@
+/******************************************************************
+ * Copyright (c) 2017 Nittsu System to present.                   *
+ * All right reserved.                                            *
+ *****************************************************************/
+package nts.uk.ctx.sys.gateway.app.find.login.dto;
+
+/**
+ * The Class CompanyDto.
+ */
+public class CompanyDto {
+
+	/** The code. */
+	public String code;
+	
+	/** The name. */
+	public String name;
+}
