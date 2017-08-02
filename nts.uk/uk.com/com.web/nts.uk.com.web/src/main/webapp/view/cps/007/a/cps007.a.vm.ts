d5e1c65e725d6b50ec5e6b2b33432d116425b72a
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2017-08-02 17:41:33 +0700

    daily commit changes
    Commit all changes for view a

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/007/a/cps007.a.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/007/a/cps007.a.vm.ts
@@ -2,52 +2,16 @@ module cps007.a.vm {
     let __viewContext: any = window['__viewContext'] || {};
     export class ViewModel {
         layout: KnockoutObservable<Layout> = ko.observable(new Layout({ layoutID: '', layoutCode: '', layoutName: '' }));
-        showMode: KnockoutObservable<number> = ko.observable(0);
-        category: KnockoutObservable<string> = ko.observable('');
-        items: KnockoutObservableArray<IItemClassification> = ko.observableArray([]);
-        item: KnockoutObservable<any> = ko.observable(undefined);
-        editable: KnockoutObservable<boolean> = ko.observable(true);
         constructor() {
-            let self = this;
+            let self = this,
+                layout = self.layout();
 
             for (let i = 1; i < 10; i++) {
-                self.items.push({ cid: i.toString(), cname: '000' + i, typeID: 1, dispOrder: 1 });
+                layout.itemsClassification.push({ id: 'ID' + i, code: 'COD' + i, name: 'Name ' + i, typeID: 1, dispOrder: 1 });
             }
         }
 
         start() {
-        }
-
-        addLine() {
-            let self = this,
-                items = self.items();
-
-            // add new line to list item
-            if (!_.last(items).typeID) {
-                self.items.push({ cid: (items.length + 1).toString(), cname: '000' + items.length + 1, typeID: 1, dispOrder: 1 });
-            }
-        }
-
-        removeItem(item) {
-            let self = this,
-                items = self.items();
-
-            self.items.remove(x => x.cid == item.cid);
-            self.items.valueHasMutated();
-        }
-
-        preventStopSort(data, evt, ui) {
-            let item = data.item,
-                index = data.targetIndex,
-                source = data.targetParent();
-
-            if (item.typeID == source[index].typeID) {
-                data.cancelDrop = true;
-            }
-        }
-
-        showDialogB() {
-            nts.uk.ui.windows.sub.modal('../b/index.xhtml');
         }
     }
 
@@ -62,19 +26,10 @@ module cps007.a.vm {
         typeState: number;
     }
 
-    interface IPersonInfoCategory {
-        catId: string;
+    interface IItemClassification {
+        id: string;
         code: string;
         name: string;
-        isUsed?: boolean;
-        isFixed: boolean;
-        empType: number;
-        parrentId: string;
-    }
-
-    interface IItemClassification {
-        cid: string;
-        cname: string;
         dispOrder: number;
         typeID: number;
         itemsDefinition?: Array<IItemDefinition>;
@@ -84,6 +39,7 @@ module cps007.a.vm {
         layoutID: string;
         layoutCode: string;
         layoutName: string;
+        editable?: boolean;
         itemsClassification?: Array<IItemClassification>;
     }
 
@@ -91,6 +47,7 @@ module cps007.a.vm {
         layoutID: KnockoutObservable<string> = ko.observable('');
         layoutCode: KnockoutObservable<string> = ko.observable('');
         layoutName: KnockoutObservable<string> = ko.observable('');
+        editable: KnockoutObservable<boolean> = ko.observable(true);
         itemsClassification: KnockoutObservableArray<IItemClassification> = ko.observableArray([]);
 
         constructor(param: ILayout) {
@@ -99,6 +56,10 @@ module cps007.a.vm {
             self.layoutID(param.layoutID);
             self.layoutCode(param.layoutCode);
             self.layoutName(param.layoutName);
+
+            if (param.editable != undefined) {
+                self.editable(param.editable);
+            }
 
             // replace x by class that implement this interface
             self.itemsClassification(param.itemsClassification || []);
