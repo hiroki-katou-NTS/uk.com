b0cf21ddd42db92ae364c63ed42142006de5c6f8
Leu Thi Lan <lan_lt@nitssusystem-vn.com>
2017-08-03 16:41:26 +0700

    đang code dở

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cas/001/d/cas001.d.start.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cas/001/d/cas001.d.start.ts
@@ -7,28 +7,22 @@ module nts.uk.com.view.cas001.d {
 $(function() {
     $("#grid").igGrid({
         columns: [
+            { headerText: "Id", key: "categoryId", dataType: "string", width: "50px", height: "40px", hidden: true },
             {
                 headerText: "<input id='selfAuth' type='checkbox'  tabindex='2' >他人</input>", key: 'selfAuth',
-                width: "80px", height: "40px", formatter: function(evt: any, row: any) {
-                    let cb = $("<input  class = 'checkRow selfAuth' type='checkbox' tabindex='4'/>");
-                    cb.attr("data-id", row.roleCode);
-                    return cb[0].outerHTML;
-                }
+                width: "40px", height: "40px",
+                template: "<input  id='a' style='width:30px, height:40px' class = 'checkRow selfAuth' type='checkbox' data-checked='${selfAuth}' data-id='${categoryId}' tabindex='4'/>"
             },
             {
                 headerText: "<input id='otherAuth' type='checkbox'  tabindex='3'>本人</input>", key: 'otherAuth',
-                width: "80px", height: "40px", formatter: function(evt: any, row: any) {
-                    let cb = $("<input  class = 'checkRow otherAuth' type='checkbox' tabindex='5'/>");
-                    cb.attr("data-id", row.roleCode);
-                    return cb[0].outerHTML;
-                }
+                width: "40px", height: "40px",
+                template: "<input id='b' style='width:30px, height:40px'  class = 'checkRow otherAuth' type='checkbox' data-checked='${otherAuth}' data-id='${categoryId}' tabindex='4'/>"
             },
-            { headerText: "コード", key: "roleCode", dataType: "string", width: "100px", height: "40px", hidden: true },
-            { headerText: "カテゴリ名", key: "roleName", dataType: "string", width: "50px", height: "40px" },
-            { headerText: '説明', key: 'description', width: "35px", hidden: true, height: "40px" },
+            { headerText: "コード", key: "categoryCode", dataType: "string", width: "80px", height: "40px" },
+            { headerText: "カテゴリ名", key: "categoryName", dataType: "string", width: "100px", height: "40px" }
 
         ],
-        primaryKey: 'roleCode',
+        primaryKey: 'categoryId',
         autoGenerateColumns: false,
         autoCommit: true,
         dataSource: [],
@@ -41,26 +35,56 @@ $(function() {
                 editMode: "row",
                 enableDeleteRow: false,
                 columnSettings: [
+                    { columnKey: "categoryId", readOnly: true },
                     { columnKey: "selfAuth", readOnly: true },
                     { columnKey: "otherAuth", readOnly: true },
-                    { columnKey: "roleCode", readOnly: true },
-                    { columnKey: "roleName", readOnly: true },
-                    { columnKey: "description", readOnly: true },
+                    { columnKey: "categoryCode", readOnly: true },
+                    { columnKey: "categoryName", readOnly: true }
                 ]
             }]
     });
     $(document).on("click", "#selfAuth", function(evt, ui) {
         $("#grid").igGridUpdating("endEdit");
         $("#grid").find("tr").each((index, element) => {
-            // change state of all checkbox delete
             $(element).find(".selfAuth").prop("checked", $("#selfAuth").prop("checked")).trigger("change");
         });
     });
     $(document).on("click", "#otherAuth", function(evt, ui) {
         $("#grid").igGridUpdating("endEdit");
         $("#grid").find("tr").each((index, element) => {
-            // change state of all checkbox delete
             $(element).find(".otherAuth").prop("checked", $("#otherAuth").prop("checked")).trigger("change");
         });
+    });
+
+    $(document).on('click', '#a', function(evt) {
+        let id = $(evt.target).parents('tr').data('id'),
+            data: Array<any> = __viewContext["viewModel"].categoryList(),
+            item = _.find(data, x => x.categoryId == id);
+        if ($(evt.target).is(':checked')) {
+            item.Selfselected = true;
+        }
+        else {
+            item.Selfselected = false;
+        }
+    });
+
+    $(document).on('click', '#b', function(evt) {
+        let id = $(evt.target).parents('tr').data('id'),
+            data: Array<any> = __viewContext["viewModel"].categoryList(),
+            item = _.find(data, x => x.categoryId == id);
+        if ($(evt.target).is(':checked')) {
+            item.Otherselected = true;
+        }
+        else {
+            item.Otherselected = false;
+        }
+    });
+    $("#grid").igGrid({
+        dataRendered: function(evt, ui) {
+            $("#grid").find("input[type=checkbox]").each(function() {
+                let $this = $(this);
+                $this.prop('checked', $this.data('checked'));
+            });
+        }
     });
 })
