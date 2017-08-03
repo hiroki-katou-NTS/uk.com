6de5bf5e0c92a79639d00bdc87fcf09498d8b322
NSVC\anhvd <anh_vd@nittsusystem-vn.com>
2017-08-03 15:39:08 +0700

    Update code KDW008 b.

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/008/b/kdw008.b.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/008/b/kdw008.b.vm.ts
@@ -1,35 +1,53 @@
 module nts.uk.at.view.kdw008.b {
     export module viewmodel {
         export class ScreenModel {
+
+            //swap list
+            itemsSwap: KnockoutObservableArray<ItemModel>;
+            columns: KnockoutObservableArray<nts.uk.ui.NtsGridListColumn>;
+            currentCodeListSwap: KnockoutObservableArray<any>;
+            test: KnockoutObservableArray<any>;
+
+            tabs: KnockoutObservableArray<nts.uk.ui.NtsTabPanelModel>;
+            selectedTab: KnockoutObservable<string>;
+
             items: KnockoutObservableArray<ItemModel>;
             columns2: KnockoutObservableArray<NtsGridListColumn>;
             currentCode: KnockoutObservable<any>;
-            
+
             constructor() {
+                var self = this;
+
                 this.items = ko.observableArray([]);
-            
-                for(let i = 1; i < 3; i++) {
+
+                for (let i = 1; i < 3; i++) {
                     this.items.push(new ItemModel('00' + i, '基本給'));
                 }
-                
+
                 this.columns2 = ko.observableArray([
-                    { headerText: 'コード', key: 'code', width: 70},
+                    { headerText: 'コード', key: 'code', width: 70 },
                     { headerText: '勤務種別名称', key: 'name', width: 120 }
                 ]);
-                
+
                 this.currentCode = ko.observable();
-                   
+
+                self.tabs = ko.observableArray([
+                    { id: 'tab-1', title: 'Tab Title 1', content: '.tab-content-1', enable: ko.observable(true), visible: ko.observable(true) },
+                    { id: 'tab-2', title: 'Tab Title 2', content: '.tab-content-2', enable: ko.observable(true), visible: ko.observable(true) }
+                ]);
+                self.selectedTab = ko.observable('tab-1');
+
             }
-            
+
             startPage(): JQueryPromise<any> {
                 let self = this;
                 let dfd = $.Deferred();
                 dfd.resolve();
                 return dfd.promise();
             }
-            
+
         }
-        
+
         export class ItemModel {
             code: string;
             name: string;
