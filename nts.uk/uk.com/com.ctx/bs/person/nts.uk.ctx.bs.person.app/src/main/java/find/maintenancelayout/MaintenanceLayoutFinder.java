794d9a12d17288a6bfa725b72c3e8a22d77a8ce3
NSVN-TruongVanLai <hoanglaisoict@gmail.com>
2017-08-04 13:04:45 +0700

    update cps008

--- nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.app/src/main/java/find/maintenancelayout/MaintenanceLayoutFinder.java
+++ nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.app/src/main/java/find/maintenancelayout/MaintenanceLayoutFinder.java
@@ -3,14 +3,17 @@
  */
 package find.maintenancelayout;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+import find.layoutitemclassification.LayoutPersonInfoClsDto;
+import find.person.info.item.PerInfoItemDefDto;
+import nts.uk.ctx.bs.person.dom.person.info.item.PernfoItemDefRepositoty;
+import nts.uk.ctx.bs.person.dom.person.layoutitemclassification.LayoutPersonInfoClsRepository;
 import nts.uk.ctx.bs.person.dom.person.maintenancelayout.MaintenanceLayoutRepository;
 
 /**
@@ -23,8 +26,47 @@ public class MaintenanceLayoutFinder {
 	@Inject
 	private MaintenanceLayoutRepository maintenanceLayoutRepository;
 
+	@Inject
+	private LayoutPersonInfoClsRepository layoutPersonInfoClsRepository;
+	
+//	@Inject
+//	private PernfoItemDefRepositoty itemDefRepositoty;
+
 	public List<MaintenanceLayoutDto> getAllLayout() {
-		return this.maintenanceLayoutRepository.getAllMaintenanceLayout().stream()
+
+		List<MaintenanceLayoutDto> listLayoutDto = new ArrayList<>();
+
+		// get All Maintenance Layout
+		listLayoutDto = this.maintenanceLayoutRepository.getAllMaintenanceLayout().stream()
 				.map(item -> MaintenanceLayoutDto.fromDomain(item)).collect(Collectors.toList());
+
+		// get all ItemClassification have LayoutID = listLayoutDto[0].layoutId
+		if (listLayoutDto.size() > 0) {
+			String layoutID = listLayoutDto.get(0).maintenanceLayoutID;
+
+			List<LayoutPersonInfoClsDto> listItemCls = new ArrayList<>();
+
+			listItemCls = this.layoutPersonInfoClsRepository.getAllItemCls(layoutID).stream()
+					.map(item -> LayoutPersonInfoClsDto.fromDomain(item)).collect(Collectors.toList());
+			
+			// get List ItemDifination cho mỗi ItemClassification
+			
+			List<PerInfoItemDefDto> listItemClsDf = new ArrayList<>();
+			
+			listItemCls.forEach(
+						item ->{
+						
+													
+						});
+			
+			
+			listLayoutDto.get(0).setListItemClsDto(listItemCls);
+			
+			// 
+			
+		}
+
+		return listLayoutDto;
+
 	}
 }
