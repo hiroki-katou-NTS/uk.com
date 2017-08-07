d8f34a9b4423ce578de3355c3728a31db591af44
NSVC\nampt <nam_pt@nittsusystem-vn.com>
2017-08-07 17:38:08 +0700

    update KDW 008

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/repository/attendance/JpaAttendanceItemRepository.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/repository/attendance/JpaAttendanceItemRepository.java
@@ -17,6 +17,9 @@ public class JpaAttendanceItemRepository extends JpaRepository implements Attend
 	private final String SELECT_POSSIBLE_ITEM = SELECT_NO_WHERE
 			+ " WHERE c.kmnmtAttendanceItemPK.companyId = :companyId"
 			+ " AND c.kmnmtAttendanceItemPK.attendanceItemId IN :listPossibleItem";
+	private final String SELECT_ATTENDANCE_ITEM = SELECT_NO_WHERE
+			+ " WHERE c.kmnmtAttendanceItemPK.companyId = :companyId "
+			+ " AND c.useAtr = :useAtr ";
 
 	private static AttendanceItem toDomain(KmnmtAttendanceItem entity) {
 		val domain = AttendanceItem.createSimpleFromJavaType(
@@ -30,11 +33,18 @@ public class JpaAttendanceItemRepository extends JpaRepository implements Attend
 	}
 
 	@Override
+	public List<AttendanceItem> getAttendanceItems(String companyId, int useAtr) {
+		return this.queryProxy().query(SELECT_ATTENDANCE_ITEM, KmnmtAttendanceItem.class)
+				.setParameter("companyId", companyId)
+				.setParameter("useAtr", useAtr).getList(f -> toDomain(f));
+	}
+
+
+	@Override
 	public List<AttendanceItem> getPossibleAttendanceItems(String companyId, List<Integer> lstPossible) {
 		return this.queryProxy().query(SELECT_POSSIBLE_ITEM, KmnmtAttendanceItem.class)
 				.setParameter("companyId", companyId)
 				.setParameter("listPossibleItem", lstPossible)
 				.getList(c -> toDomain(c));
 	}
-
 }
