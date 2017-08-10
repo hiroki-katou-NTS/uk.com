eda6dc497136a32a08c3757fc38fb7b173ca378a
ddung\dotiendung <dung24391@gmail.com>
2017-08-10 10:22:10 +0700

    Update code ui KDW003 B

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/003/b/kdw003.b.service.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/003/b/kdw003.b.service.ts
@@ -1,12 +1,11 @@
 module nts.uk.at.view.kdw003.b {
     export module service {
         var paths: any = {
-            getListBonusPayTimeItem: "at/share/bonusPayTimeItem/getListBonusPayTimeItem",
-            checkInit: "at/share/bonusPayTimeItem/checkInit"
+            getErrorReferences: "screen/at/correctionofdailyperformance/errorreference/getErrorReferences",
         }
         
-        export function getListBonusPTimeItem(): JQueryPromise<any> {
-            return nts.uk.request.ajax(paths.getListBonusPayTimeItem);
+        export function getErrorReferences(obj): JQueryPromise<any> {
+            return nts.uk.request.ajax("at",paths.getErrorReferences,obj);
         }
     }
 }
