64efbe7a5c114bd3e4be8fec6fabc7f2e7ca9cb4
Vu Cong Tuong <tuong_vc@nittsusystem-vn.com>
2017-08-11 11:17:17 +0700

    Code CPS005- change path webservice :  class: PernfoItemDefWebservice  of function: getAllPerInfoItemDefByCtgIdForLayout, getPerInfoItemDefByIdForLayout, getPerInfoItemDefByListIdForLayout

--- nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.app/src/main/java/command/person/info/category/AddPerInfoCtgCommandHandler.java
+++ nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.app/src/main/java/command/person/info/category/AddPerInfoCtgCommandHandler.java
@@ -6,6 +6,7 @@ import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import command.person.info.item.AddItemCommand;
+import command.person.info.item.MappingDtoToDomain;
 import command.person.info.item.SingleItemCommand;
 import nts.arc.layer.app.command.CommandHandler;
 import nts.arc.layer.app.command.CommandHandlerContext;
@@ -26,6 +27,7 @@ public class AddPerInfoCtgCommandHandler extends CommandHandler<AddPerInfoCtgCom
 
 	private final static String SPECIAL_CTG_CODE = "CO";
 	private final static String SPECIAL_ITEM_CODE = "IO";
+	private final static int ITEM_CODE_DEFAUT_NUMBER = 0;
 
 	@Override
 	protected void handle(CommandHandlerContext<AddPerInfoCtgCommand> context) {
@@ -34,23 +36,30 @@ public class AddPerInfoCtgCommandHandler extends CommandHandler<AddPerInfoCtgCom
 		String newCtgCode = createNewCode(perInfoCtgRep.getPerInfoCtgCodeLastest(contractCd), SPECIAL_CTG_CODE);
 
 		List<String> companyIdList = GetListCompanyOfContract.LIST_COMPANY_OF_CONTRACT;
+		List<String> ctgIdList = null;
+		AddItemCommand addItemCommand = null;
 		PersonInfoCategory perInfoCtg = PersonInfoCategory.createFromJavaType(PersonInfoCategory.ROOT_COMPANY_ID,
 				newCtgCode, perInfoCtgCommand.getCategoryName().v(), perInfoCtgCommand.getCategoryType().value);
 
 		this.perInfoCtgRep.addPerInfoCtgRoot(perInfoCtg, contractCd);
 		this.perInfoCtgRep.addPerInfoCtgWithListCompany(perInfoCtg, contractCd, companyIdList);
+		// get
+		String newItemCode = createNewCode(null, SPECIAL_ITEM_CODE);
+		addItemCommand = new AddItemCommand(perInfoCtg.getPersonInfoCategoryId(), newItemCode, newItemCode, null, 0,
+				null);
+		PersonInfoItemDefinition itemStartDate = MappingDtoToDomain.mappingFromDomaintoDtoForStartDate(addItemCommand);
+		pernfoItemDefRep.addPerInfoItemDefRoot(itemStartDate, contractCd);
+		ctgIdList = perInfoCtgRep.getPerInfoCtgIdList(PersonInfoCategory.ROOT_COMPANY_ID, newCtgCode);
+		pernfoItemDefRep.addPerInfoItemDefByCtgIdList(itemStartDate, ctgIdList);
 
-		List<String> ctgIdList = perInfoCtgRep.getPerInfoCtgIdList(PersonInfoCategory.ROOT_COMPANY_ID, newCtgCode);
-		String newItemCode = createNewCode(
-				pernfoItemDefRep.getPerInfoItemCodeLastest(PersonInfoItemDefinition.ROOT_CONTRACT_CODE, newCtgCode),
-				SPECIAL_ITEM_CODE);
-		SingleItemCommand singleItemCommand = null;
-		AddItemCommand addItemCommand = new AddItemCommand(perInfoCtg.getPersonInfoCategoryId(), newItemCode,
-				newItemCode, null, 0, singleItemCommand);
 	}
 
 	private String createNewCode(String codeLastest, String strSpecial) {
-		String numberCode = String.valueOf(Integer.parseInt(codeLastest.substring(2, 7)) + 1);
+		String numberCode = String.valueOf(ITEM_CODE_DEFAUT_NUMBER + 1);
+		if (codeLastest != null) {
+			numberCode = String.valueOf(Integer.parseInt(codeLastest.substring(2, 7)) + 1);
+		}
+
 		for (int i = 5; i > 0; i++) {
 			if (i == numberCode.length()) {
 				break;
