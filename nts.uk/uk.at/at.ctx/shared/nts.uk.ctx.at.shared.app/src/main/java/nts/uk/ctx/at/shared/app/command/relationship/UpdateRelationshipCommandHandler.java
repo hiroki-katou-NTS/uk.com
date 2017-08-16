6012c65c48d98790959b018260401084e6766315
NSVC\yennth <yen_nt@nittsusystem-vn.com>
2017-08-16 08:58:17 +0700

    KMF004-h: Code UI

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.app/src/main/java/nts/uk/ctx/at/shared/app/command/relationship/UpdateRelationshipCommandHandler.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.app/src/main/java/nts/uk/ctx/at/shared/app/command/relationship/UpdateRelationshipCommandHandler.java
@@ -2,6 +2,7 @@ package nts.uk.ctx.at.shared.app.command.relationship;
 
 import java.util.Optional;
 
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import nts.arc.layer.app.command.CommandHandler;
@@ -9,7 +10,7 @@ import nts.arc.layer.app.command.CommandHandlerContext;
 import nts.uk.ctx.at.shared.dom.relationship.Relationship;
 import nts.uk.ctx.at.shared.dom.relationship.repository.RelationshipRepository;
 import nts.uk.shr.com.context.AppContexts;
-
+@Stateless
 public class UpdateRelationshipCommandHandler extends CommandHandler<UpdateRelationshipCommand> {
 	@Inject
 	private RelationshipRepository relaRep;
