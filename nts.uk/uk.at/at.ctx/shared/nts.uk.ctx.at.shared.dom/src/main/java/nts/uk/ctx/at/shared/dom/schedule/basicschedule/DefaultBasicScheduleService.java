5c402da9076dc3411015b17c29b92144811e131a
Nguyen Hoang Son <son_hoang@nittsusystem-vn.com>
2017-08-17 15:30:19 +0700

    KSU001: update code UI and code common BasicScheduleService

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/schedule/basicschedule/DefaultBasicScheduleService.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/schedule/basicschedule/DefaultBasicScheduleService.java
@@ -1,6 +1,10 @@
 package nts.uk.ctx.at.shared.dom.schedule.basicschedule;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import nts.arc.error.BusinessException;
+import nts.gul.text.StringUtil;
 
 /**
  * 
@@ -9,6 +13,9 @@ import javax.ejb.Stateless;
  */
 @Stateless
 public class DefaultBasicScheduleService implements BasicScheduleService {
+//	
+//	@Inject
+//	public 
 
 	@Override
 	public SetupType checkNeededOfWorkTimeSetting(String workTypeCd) {
@@ -30,14 +37,36 @@ public class DefaultBasicScheduleService implements BasicScheduleService {
 
 	@Override
 	public SetupType checkRequired(SetupType morningWorkStyle, SetupType afternoonWorkStyle) {
-		// TODO Auto-generated method stub
-		return null;
+
+		if (morningWorkStyle == SetupType.REQUIRED) {
+			return SetupType.REQUIRED;
+		}
+		
+		if (morningWorkStyle == SetupType.NOT_REQUIRED) {
+			return afternoonWorkStyle;
+		}
+
+		if (afternoonWorkStyle == SetupType.REQUIRED) {
+			return SetupType.REQUIRED;
+		}
+
+		if (afternoonWorkStyle == SetupType.NOT_REQUIRED || afternoonWorkStyle == SetupType.OPTIONAL) {
+			return SetupType.OPTIONAL;
+		}
+
+		throw new RuntimeException("ERROR!!");
 	}
 
 	@Override
-	public void ErrorCheckingStatus(String workTypeCd, String workTimeCd) {
-		// TODO Auto-generated method stub
-
+	public void checkPairWorkTypeWorkTime(String workTypeCd, String workTimeCd) {
+		SetupType setupType = this.checkNeededOfWorkTimeSetting(workTypeCd);
+		if (setupType == SetupType.REQUIRED && StringUtil.isNullOrEmpty(workTimeCd, true)) {
+			throw new BusinessException("Msg_435");
+		}
+		
+		if (setupType == SetupType.NOT_REQUIRED && !StringUtil.isNullOrEmpty(workTimeCd, true)) {
+			throw new BusinessException("Msg_434");
+		}
 	}
 
 }
