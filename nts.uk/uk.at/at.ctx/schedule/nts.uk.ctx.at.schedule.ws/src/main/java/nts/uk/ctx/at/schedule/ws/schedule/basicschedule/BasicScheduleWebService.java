039441e37ef15df83296260328aa2b2cc91cdf1a
nws-anhnm <anhnm@newwave.vn>
2017-08-18 13:24:16 +0700

    + fix bug không checkpair đúng khi workTimeCode = 000
    + sửa vài cái linh tinh

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.ws/src/main/java/nts/uk/ctx/at/schedule/ws/schedule/basicschedule/BasicScheduleWebService.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.ws/src/main/java/nts/uk/ctx/at/schedule/ws/schedule/basicschedule/BasicScheduleWebService.java
@@ -69,6 +69,9 @@ public class BasicScheduleWebService extends WebService {
 	@Path("checkPairWorkTypeWorkTime/{workTypeCode}/{workTimeCode}")
 	public void checkPairWorkTypeWorkTime(@PathParam("workTypeCode") String workTypeCode,
 			@PathParam("workTimeCode") String workTimeCode) {
+		if (workTimeCode.equalsIgnoreCase("000")) {
+			workTimeCode = "";
+		}
 		this.basicScheduleService.checkPairWorkTypeWorkTime(workTypeCode, workTimeCode);
 	}
 }
