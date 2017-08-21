5640633cce5c1bdf14538e0676360bedacbbd242
Vu Tien Lam <lam_vt@nittsusystem-vn.com>
2017-08-21 15:04:05 +0700

    add stateless

--- nts.uk/uk.com/com.ctx/bs/company/nts.uk.ctx.bs.company.app/src/main/java/nts/uk/ctx/bs/company/app/find/share/CompanyAdapterImpl.java
+++ nts.uk/uk.com/com.ctx/bs/company/nts.uk.ctx.bs.company.app/src/main/java/nts/uk/ctx/bs/company/app/find/share/CompanyAdapterImpl.java
@@ -2,6 +2,7 @@ package nts.uk.ctx.bs.company.app.find.share;
 
 import java.util.Optional;
 
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import nts.uk.ctx.bs.company.app.find.company.CompanyDto;
@@ -9,6 +10,7 @@ import nts.uk.ctx.bs.company.app.find.company.CompanyFinder;
 import nts.uk.shr.com.company.CompanyAdapter;
 import nts.uk.shr.com.company.CompanyInfor;
 
+@Stateless
 public class CompanyAdapterImpl implements CompanyAdapter{
 	
 	@Inject
