e72d0f009b07fecf0b8845e5403059432f41f6c9
TruongQuocPhong <phong_tq@nittsusystem-vn.com>
2017-08-23 09:57:49 +0700

    update UI kmf004

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmf/004/a/kmf004.a.service.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmf/004/a/kmf004.a.service.ts
@@ -2,5 +2,7 @@ module nts.uk.at.view.kmf004.a.service {
     var paths: any = {
         
     }
-    
+    export function findSpecialHoliday(parameter: any): JQueryPromise<viewmodel.model.AgentDto> {
+        return nts.uk.request.ajax("com",paths.findAgent, parameter);
+    }
 }
