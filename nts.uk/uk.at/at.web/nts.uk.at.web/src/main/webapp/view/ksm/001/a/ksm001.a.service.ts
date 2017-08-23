677df08c86772b807fb103231520f66b19220ea0
nws-duongtv <nws-duongtv@newwave.vn>
2017-08-23 17:06:06 +0700

    update ui ksm001

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksm/001/a/ksm001.a.service.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksm/001/a/ksm001.a.service.ts
@@ -1,44 +1,85 @@
 module nts.uk.at.view.ksm001.a {
     export module service {
         var paths = {
-            findAllMonthlyEstimateTime: "ctx/at/schedule/shift/estimate/company/find",
+            findCompanyEstablishment: "ctx/at/schedule/shift/estimate/company/find",
             saveCompanyEstimate: "ctx/at/schedule/shift/estimate/company/save"
         }
 
         /**
          * call service get all monthly estimate time of company
          */
-        export function findAllMonthlyEstimateTime(targetYear: number): JQueryPromise<model.CompanyEstimateTimeDto> {
-            return nts.uk.request.ajax('at', paths.findAllMonthlyEstimateTime + '/' + targetYear);
+        export function findCompanyEstablishment(targetYear: number): JQueryPromise<model.CompanyEstablishmentDto> {
+            return nts.uk.request.ajax('at', paths.findCompanyEstablishment + '/' + targetYear);
         }
-        
-         /**
-         * call service save estimate company
-         */
-        export function saveCompanyEstimate(targetYear: number, dto: model.CompanyEstimateTimeDto): JQueryPromise<void> {
-            return nts.uk.request.ajax('at', paths.saveCompanyEstimate, { estimateTime: dto, targetYear: targetYear });
+
+        /**
+        * call service save estimate company
+        */
+        export function saveCompanyEstimate(targetYear: number, dto: model.CompanyEstablishmentDto): JQueryPromise<void> {
+            return nts.uk.request.ajax('at', paths.saveCompanyEstimate, {
+                estimateTime: dto.estimateTime,
+                estimatePrice: dto.estimatePrice,
+                estimateNumberOfDay: dto.estimateNumberOfDay,
+                targetYear: targetYear
+            });
         }
-        
+
         export module model {
-            
+
             export interface TargetYearDto {
                 code: string;
                 name: number;
             }
-            
-            export interface EstimateTimeDto{
+
+            export interface EstimateTimeDto {
                 month: number;
                 time1st: number;
                 time2nd: number;
                 time3rd: number;
                 time4th: number;
-                time5th: number;    
-                            
+                time5th: number;
+
+            }
+            export interface EstimatePriceDto {
+                month: number;
+                price1st: number;
+                price2nd: number;
+                price3rd: number;
+                price4th: number;
+                price5th: number;
+
             }
             
+            export interface EstimateDaysDto {
+                month: number;
+                days1st: number;
+                days2nd: number;
+                days3rd: number;
+                days4th: number;
+                days5th: number;
+
+            }
+
             export interface CompanyEstimateTimeDto {
                 monthlyEstimates: EstimateTimeDto[];
                 yearlyEstimate: EstimateTimeDto;
+            }
+            
+
+            export interface CompanyEstimatePriceDto {
+                monthlyEstimates: EstimatePriceDto[];
+                yearlyEstimate: EstimatePriceDto;
+            }
+            
+            export interface CompanyEstimateDaysDto {
+                monthlyEstimates: EstimateDaysDto[];
+                yearlyEstimate: EstimateDaysDto;
+            }
+
+            export interface CompanyEstablishmentDto {
+                estimateTime: CompanyEstimateTimeDto;
+                estimatePrice: CompanyEstimatePriceDto;
+                estimateNumberOfDay: CompanyEstimateDaysDto;
             }
 
         }
