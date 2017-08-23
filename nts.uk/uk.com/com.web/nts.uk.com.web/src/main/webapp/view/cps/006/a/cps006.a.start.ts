6e7f5542dc71f68d9dc1462cfc40021cbea74dc5
Leu Thi Lan <lan-lt@nittsusystem-vn.com>
2017-08-23 09:04:34 +0700

    CPS006: UI

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/006/a/cps006.a.start.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/006/a/cps006.a.start.ts
@@ -1,12 +1,6 @@
-module nts.uk.com.view.cas001.a {
+module nts.uk.com.view.cps006.a {
     __viewContext.ready(function() {
-        __viewContext['screenModel'] = new viewmodel.ScreenModel();
-        __viewContext['screenModel'].start().done(function() {
-            __viewContext.bind(__viewContext['screenModel']);
-            let helpButton = "<button id=\"A2_012\" data-bind=\"ntsHelpButton: {image: \'A2_012.png\', position: \'right top\', enable: true }\">?</button>";
-            nts.uk.ui.ig.grid.header.getCell('A2_008', 'setting').append($(helpButton));
-            ko.applyBindings(__viewContext['screenModel'], nts.uk.ui.ig.grid.header.getCell('A2_008', 'setting')[0]);
-        });
-
+        __viewContext["viewModel"] = new viewmodel.ScreenModel();
+        __viewContext.bind(__viewContext["viewModel"]);
     });
 }
