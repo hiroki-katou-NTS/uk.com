3ae7c1c3acde4eb4680e895b41f52255133dac63
NSVN-TrieuKhacTu <tu_tk@nittsusystem-vn.com>
2017-08-25 17:43:26 +0700

    update code server Kaf000

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/common/service/OtherCommonAlgorithmService.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/common/service/OtherCommonAlgorithmService.java
@@ -10,6 +10,13 @@ import nts.arc.time.GeneralDate;
 
 public interface OtherCommonAlgorithmService {
 	/**
+	 * 1.職場別就業時間帯を取得
+	 * @param companyID
+	 * @param employeeID
+	 * @param referenceDate
+	 */
+	public void getWorkingHoursByWorkplace(String companyID,String employeeID,GeneralDate referenceDate);
+	/**
 	 * 4.社員の当月の期間を算出する
 	 * @param companyId 会社ID
 	 * @param employeeId 社員ID
