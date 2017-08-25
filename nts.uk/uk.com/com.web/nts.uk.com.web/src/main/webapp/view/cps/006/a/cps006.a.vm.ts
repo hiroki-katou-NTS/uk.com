327c4f1d442cd80f02109da6ce9807f31f806c7f
Leu Thi Lan <lan-lt@nittsusystem-vn.com>
2017-08-25 14:43:15 +0700

    đang code dở

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/006/a/cps006.a.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/006/a/cps006.a.vm.ts
@@ -57,13 +57,16 @@ module nts.uk.com.view.cps006.a.viewmodel {
             self.itemList.removeAll();
             service.getAllPerInfoItemDefByCtgId(id).done(function(data: Array<any>) {
                 if (data.length > 0) {
-                    self.itemList(_.map(data, x => new ItemInfo({
+                    self.itemList(_.map(data, x => {return {
                         id: x.id,
                         perInfoCtgId: x.perInfoCtgId,
                         itemName: x.itemName,
                         systemRequired: x.systemRequired,
                         isAbolition: x.isAbolition == 1 ? "<i  style=\"margin-left: 10px\" class=\"icon icon-close\"></i>" : ""
-                    })));
+                    }}));
+                    
+                    self.currentCategory().displayIsAbolished = (_.filter(data, x =>{return x.systemRequired == false})).length > 0 ? true: false;
+                    
                 };
             });
 
@@ -148,8 +151,8 @@ module nts.uk.com.view.cps006.a.viewmodel {
             setShared('CDL020_PARAMS', cats);
             nts.uk.ui.windows.sub.modal('/view/cdl/022/a/index.xhtml', { title: '' }).onClosed(function(): any {
                 let CTGlist: Array<any> = getShared('CDL020_VALUES'),
-                    i: number = 0;
-                let CTGsorrList = _.map(CTGlist, x => {
+                    i: number = 0,
+                    CTGsorrList = _.map(CTGlist, x => {
                     return {
                         id: x.id,
                         order: i++
@@ -165,7 +168,7 @@ module nts.uk.com.view.cps006.a.viewmodel {
             let self = this,
                 cat = ko.toJS(self.currentCategory),
                 command = {
-                    id: cat.id,
+                    categoryId: cat.id,
                     categoryName: cat.categoryName,
                     isAbolition: cat.isAbolition
 
@@ -173,7 +176,7 @@ module nts.uk.com.view.cps006.a.viewmodel {
 
             service.updateCtgInfo(command).done(function(data) {
                 dialog.info({ messageId: "Msg_15" }).then(function() {
-                    self.start(command.id);
+                    self.start(command.categoryId);
                 });
             })
 
@@ -243,6 +246,7 @@ module nts.uk.com.view.cps006.a.viewmodel {
         categoryName: KnockoutObservable<string>;
         categoryType: number;
         isAbolition: KnockoutObservable<boolean>;
+        displayIsAbolished :  boolean = false;
         constructor(params: ICategoryInfoDetail) {
             this.id = ko.observable(params.id);
             this.categoryNameDefault = params.categoryNameDefault;
