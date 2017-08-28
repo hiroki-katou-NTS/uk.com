b19eea8b69bf95e7e43c54745c47bb236e8dfa09
nws-quyennt <quyennt@newwave.vn>
2017-08-28 18:10:21 +0700

    update kcp009

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/kcp/009/kcp009.a.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/kcp/009/kcp009.a.vm.ts
@@ -8,12 +8,7 @@ module kcp009.a.viewmodel {
         systemType: KnockoutObservable<number>;
         isDisplayOrganizationName: KnockoutObservable<boolean>;
         targetBtnText: string;
-        
-        empDisplayCode: KnockoutObservable<string>;
-        empBusinessName: KnockoutObservable<string>;
-        selectedNumberOfPeople: KnockoutObservable<string>;
-        organizationName: KnockoutObservable<string>;
-        keySearch: KnockoutObservable<string>;
+
         systemReferenceList: KnockoutObservableArray<any>;
         selectedSystem: KnockoutObservable<number>;
         
@@ -25,6 +20,13 @@ module kcp009.a.viewmodel {
             self.empList = ko.observableArray([
             {id: '01', code: 'A000000000001', businessName: '日通　純一郎1', workplaceName: '名古屋支店', depName: 'Dep Name'}, 
             {id: '04', code: 'A000000000004', businessName: '日通　純一郎4', workplaceName: '名古屋支店', depName: 'Dep Name'},
+            {id: '05', code: 'A000000000005', businessName: '日通　純一郎5', workplaceName: '名古屋支店', depName: 'Dep Name'},
+            {id: '06', code: 'A000000000006', businessName: '日通　純一郎6', workplaceName: '名古屋支店', depName: 'Dep Name'},
+            {id: '07', code: 'A000000000007', businessName: '日通　純一郎7', workplaceName: '名古屋支店', depName: 'Dep Name'},
+            {id: '08', code: 'A000000000008', businessName: '日通　純一郎8', workplaceName: '名古屋支店', depName: 'Dep Name'},
+            {id: '09', code: 'A000000000009', businessName: '日通　純一郎9', workplaceName: '名古屋支店', depName: 'Dep Name'},
+            {id: '10', code: 'A000000000010', businessName: '日通　純一郎10', workplaceName: '名古屋支店', depName: 'Dep Name'},
+            {id: '11', code: 'A000000000011', businessName: '日通　純一郎11', workplaceName: '名古屋支店', depName: 'Dep Name'},
             {id: '02', code: 'A000000000002', businessName: '日通　純一郎2', workplaceName: '名古屋支店', depName: 'Dep Name'},
             {id: '03', code: 'A000000000003', businessName: '日通　純一郎3', workplaceName: '名古屋支店', depName: 'Dep Name'}]);
             self.systemType = ko.observable(SystemType.EMPLOYMENT);
@@ -33,12 +35,7 @@ module kcp009.a.viewmodel {
                 self.reloadComponent();
             });
             self.targetBtnText = nts.uk.resource.getText("KCP009_3");
-            self.empDisplayCode = ko.observable('empcode');
-            self.empBusinessName = ko.observable("Name");
-            self.organizationName = ko.observable(null);
             self.selectedItem = ko.observable(null);
-            self.selectedNumberOfPeople = ko.observable(null);
-            self.keySearch = ko.observable(null);
             self.systemReferenceList = ko.observableArray([
                 { code: 1, name: 'Employment System' },
                 { code: 2, name: 'Other Systems' },
@@ -54,11 +51,7 @@ module kcp009.a.viewmodel {
                 // Reload Component
                 self.reloadComponent();
             });
-//            self.isShowProfile = ko.computed(function() {
-//                return self.empList().length > 0;
-//            });
-            
-            
+
             // Initial listComponentOption
             self.listComponentOption = {
                 systemReference: self.systemType(),
@@ -67,8 +60,6 @@ module kcp009.a.viewmodel {
                 targetBtnText: self.targetBtnText,
                 selectedItem: self.selectedItem
             };
-            
-            
             
         }
         
@@ -80,7 +71,7 @@ module kcp009.a.viewmodel {
             self.listComponentOption.targetBtnText = self.targetBtnText;
             self.listComponentOption.employeeInputList(self.empList());
             // Load listComponent
-            $('#emp-list').ntsLoadListComponent(self.listComponentOption);
+            $('#emp-component').ntsLoadListComponent(self.listComponentOption);
         }
     }
      
