a4d4182802d5ff1075810696c13b24abb3b8d2ef
NSVC\dudt <du_dt@nittsusystem-vn.com>
2017-08-28 18:28:16 +0700

    CMM018K: khoi dong man hinh

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmm/018/k/cmm018.k.start.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmm/018/k/cmm018.k.start.ts
@@ -3,7 +3,9 @@ module cmm008.k{
         var screenModel = new cmm018.k.viewmodel.ScreenModel();
         //screenModel.start().done(function() {
             __viewContext.bind(screenModel);
-            
+        var self = screenModel;
+            $('#tree-grid').ntsTreeComponent(self.treeGrid);
+            $('#component-items-list').ntsListComponent(self.listComponentOption);
         //});
     });
 }
