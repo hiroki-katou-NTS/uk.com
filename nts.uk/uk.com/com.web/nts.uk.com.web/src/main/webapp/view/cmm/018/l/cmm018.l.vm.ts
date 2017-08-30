1f38a23531f6b3954a32cf821095f0edf6abc064
NSVC\anhvd <anh_vd@nittsusystem-vn.com>
2017-08-30 15:11:07 +0700

    Code client CMM018 L.

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmm/018/l/cmm018.l.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmm/018/l/cmm018.l.vm.ts
@@ -1,7 +1,13 @@
-module nts.uk.at.view.kdw002.a {
+module nts.uk.at.view.cmm018.l {
     export module viewmodel {
         export class ScreenModel {
-            
+            date: KnockoutObservable<string>;
+            yearMonth: KnockoutObservable<number>;
+            constructor() {
+                var self = this;            
+                self.date = ko.observable('20000101');
+                self.yearMonth = ko.observable(200001);
+            }
         }
     }
 }
