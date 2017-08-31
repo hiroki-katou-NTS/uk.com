035dbb74cbbf4556727add21379b09de501925ce
NSVC\anhvd <anh_vd@nittsusystem-vn.com>
2017-08-31 16:28:05 +0700

    Update code client side CMM018 L and CMM018 M.

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmm/018/m/cmm018.m.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmm/018/m/cmm018.m.vm.ts
@@ -3,22 +3,22 @@ module nts.uk.at.view.cmm018.m {
         export class ScreenModel {
             
             //Enable checkbox
-            enable3: KnockoutObservable<boolean>;
-            enable2: KnockoutObservable<boolean>;
-            enable1: KnockoutObservable<boolean>;
+            checkValue3: KnockoutObservable<boolean>;
+            checkValue2: KnockoutObservable<boolean>;
+            checkValue1: KnockoutObservable<boolean>;
             
             date: KnockoutObservable<string>;
-            yearMonth: KnockoutObservable<number>;
             constructor() {
                 var self = this;
                 
                 //Enable checkbox
-                self.enable3 = ko.observable(true);
-                self.enable2 = ko.observable(true);
-                self.enable1 = ko.observable(true);
+                self.checkValue3 = ko.observable(true);
+                self.checkValue2 = ko.observable(true);
+                self.checkValue1 = ko.observable(true);
                             
-                self.date = ko.observable('20000101');
-                self.yearMonth = ko.observable(200001);
+                //self.date = ko.observable('20000101');
+                var currentDate = (new Date()).toISOString().split('T')[0];
+                self.date = ko.observable(currentDate);
             }
         }
     }
