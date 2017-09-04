8ba1eb7b5fac9a438bd47ca860ea39e06dc481dc
NTS\keisuke_hoshina <keisuke_hoshina@nittsusystem.co.jp>
2017-09-04 14:58:35 +0900

    add AttendanceClock

---
+++ nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/com/time/AttendanceClock.java
@@ -0,0 +1,14 @@
+package nts.uk.shr.com.time;
+
+import nts.arc.primitive.IntegerPrimitiveValue;
+
+public class AttendanceClock extends IntegerPrimitiveValue<AttendanceClock>{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public AttendanceClock(int dayTime) {
+		super(dayTime);
+	}
+}
