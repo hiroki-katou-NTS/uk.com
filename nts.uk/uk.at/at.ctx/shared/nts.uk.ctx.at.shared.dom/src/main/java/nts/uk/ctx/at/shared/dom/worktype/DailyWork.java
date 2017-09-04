a71e6e3d9bea324e1eedc31377c6e85a98044a82
NTS\keisuke_hoshina <keisuke_hoshina@nittsusystem.co.jp>
2017-09-04 14:54:36 +0900

    add methods to DailyWork

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/worktype/DailyWork.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/worktype/DailyWork.java
@@ -4,9 +4,14 @@
  *****************************************************************/
 package nts.uk.ctx.at.shared.dom.worktype;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 /**
  * The Class DailyWork.
  */
+@NoArgsConstructor
+@AllArgsConstructor
 public class DailyWork { // 1日の勤務
 
 	/** The work type unit. */
@@ -20,4 +25,42 @@ public class DailyWork { // 1日の勤務
 
 	/** The afternoon. */
 	private WorkTypeClassification afternoon; // 午後
+	
+	/**
+	 * 出勤区分区分の取得
+	 * @return 出勤休日区分
+	 */
+	public AttendanceHolidayAttr getAttendanceHolidayAttr() {
+		if (this.workTypeUnit == WorkTypeUnit.OneDay) {
+			if (this.oneDay.isHoliday()) {
+				return AttendanceHolidayAttr.HOLIDAY;
+			} else {
+				return AttendanceHolidayAttr.FULL_TIME;
+			}
+		} else {
+			if (this.morning.isAttendance() && this.afternoon.isAttendance()) {
+				return AttendanceHolidayAttr.FULL_TIME;
+			} else if (this.morning.isAttendance() && this.afternoon.isHoliday()) {
+				return AttendanceHolidayAttr.MORNING;
+			} else if (this.morning.isHoliday() && this.afternoon.isAttendance()) {
+				return AttendanceHolidayAttr.AFTERNOON;
+			} else {
+				return AttendanceHolidayAttr.HOLIDAY;
+			}
+		}
+	}
+	
+	/**
+	 * 平日出勤か判定
+	 * @return　平日出勤である
+	 */
+	public boolean isWeekDayAttendance() {
+		if(this.workTypeUnit == WorkTypeUnit.OneDay) {
+			return this.oneDay.isWeekDayAttendance();
+		}
+		else if(this.workTypeUnit == WorkTypeUnit.MonringAndAfternoon) {
+			return morning.isWeekDayAttendance() || afternoon.isWeekDayAttendance();
+		}
+		return false;
+	}
 }
