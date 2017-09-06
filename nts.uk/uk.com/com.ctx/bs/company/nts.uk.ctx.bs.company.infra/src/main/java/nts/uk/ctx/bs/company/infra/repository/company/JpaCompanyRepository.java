967d81b9d58d5e558f91aec081396aaa8933a76b
NSVN-TruongVanLai <NSVN-TruongVanLai@users.noreply.github.com>
2017-09-06 13:52:29 +0700

    request list #51

--- nts.uk/uk.com/com.ctx/bs/company/nts.uk.ctx.bs.company.infra/src/main/java/nts/uk/ctx/bs/company/infra/repository/company/JpaCompanyRepository.java
+++ nts.uk/uk.com/com.ctx/bs/company/nts.uk.ctx.bs.company.infra/src/main/java/nts/uk/ctx/bs/company/infra/repository/company/JpaCompanyRepository.java
@@ -4,14 +4,16 @@
  *****************************************************************/
 package nts.uk.ctx.bs.company.infra.repository.company;
 
+import java.util.List;
 import java.util.Optional;
 
 import javax.ejb.Stateless;
 
+import lombok.val;
 import nts.arc.layer.infra.data.JpaRepository;
 import nts.uk.ctx.bs.company.dom.company.Company;
 import nts.uk.ctx.bs.company.dom.company.CompanyRepository;
-import nts.uk.ctx.bs.company.infra.entity.company.CmnmtCompany;
+import nts.uk.ctx.bs.company.infra.entity.company.BcmdtCompany;
 
 /**
  * The Class JpaCompanyRepository.
@@ -19,23 +21,48 @@ import nts.uk.ctx.bs.company.infra.entity.company.CmnmtCompany;
 @Stateless
 public class JpaCompanyRepository extends JpaRepository implements CompanyRepository {
 
-	/* (non-Javadoc)
-	 * @see nts.uk.ctx.basic.dom.company.CompanyRepository#getComanyId(java.lang.String)
+	private static final String GETALLCOMPANY;
+
+	static {
+		StringBuilder builderString = new StringBuilder();
+		builderString = new StringBuilder();
+		builderString.append("SELECT e");
+		builderString.append(" FROM BcmdtCompany e");
+		GETALLCOMPANY = builderString.toString();
+	}
+
+	private static Company toDomainNotFullAtr(BcmdtCompany entity) {
+		val domain = Company.createFromJavaType(entity.getCcd(), entity.getCompanyName(), entity.getCid(),
+				entity.getAbolitionAtr());
+		return domain;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * nts.uk.ctx.basic.dom.company.CompanyRepository#getComanyId(java.lang.String)
 	 */
 	@Override
 	public Optional<Company> getComanyById(String companyId) {
-		return this.queryProxy().find(companyId, CmnmtCompany.class)
-				.map(company -> this.toDomain(company));
+		return this.queryProxy().find(companyId, BcmdtCompany.class).map(company -> this.toDomain(company));
 	}
 
 	/**
 	 * To domain.
 	 *
-	 * @param entity the entity
+	 * @param entity
+	 *            the entity
 	 * @return the company
 	 */
-	private Company toDomain(CmnmtCompany entity){
+	private Company toDomain(BcmdtCompany entity) {
 		return new Company(new JpaCompanyGetMemento(entity));
 	}
-	
+
+	@Override
+	public List<Company> getAllCompany() {
+		// TODO Auto-generated method stub
+		return this.queryProxy().query(GETALLCOMPANY, BcmdtCompany.class).getList(c -> toDomainNotFullAtr(c));
+	}
+
 }
