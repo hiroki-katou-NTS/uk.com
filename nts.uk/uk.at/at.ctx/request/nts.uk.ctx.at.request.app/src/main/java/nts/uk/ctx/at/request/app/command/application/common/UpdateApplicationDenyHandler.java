9c27d908124232ffebabbc149f5ee0ec2b94dcec
NSVN-TrieuKhacTu <tu_tk@nittsusystem-vn.com>
2017-09-06 17:13:36 +0700

    code server kaf000

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.app/src/main/java/nts/uk/ctx/at/request/app/command/application/common/UpdateApplicationDenyHandler.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.app/src/main/java/nts/uk/ctx/at/request/app/command/application/common/UpdateApplicationDenyHandler.java
@@ -1,24 +1,25 @@
 package nts.uk.ctx.at.request.app.command.application.common;
 
-import java.util.Optional;
-
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 
-import nts.arc.error.BusinessException;
 import nts.arc.layer.app.command.CommandHandler;
 import nts.arc.layer.app.command.CommandHandlerContext;
 import nts.uk.ctx.at.request.dom.application.common.Application;
-import nts.uk.ctx.at.request.dom.application.common.ApplicationRepository;
+import nts.uk.ctx.at.request.dom.application.common.service.detailscreen.after.AfterDenialProcess;
+import nts.uk.ctx.at.request.dom.application.common.service.detailscreen.before.BeforeProcessDenial;
+
 import nts.uk.shr.com.context.AppContexts;
 
 @Stateless
 @Transactional
-public class UpdateApplicationCommandHandler extends CommandHandler<UpdateApplicationCommand> {
+public class UpdateApplicationDenyHandler extends CommandHandler<UpdateApplicationCommand> {
 
 	@Inject
-	private ApplicationRepository appRepo;
+	private BeforeProcessDenial beforeProcessDenialRepo;
+	@Inject 
+	private AfterDenialProcess afterDenialProcessRepo;
 	
 	@Override
 	protected void handle(CommandHandlerContext<UpdateApplicationCommand> context) {
@@ -43,13 +44,11 @@ public class UpdateApplicationCommandHandler extends CommandHandler<UpdateApplic
 				appCommand.getReflectPerTime(), 
 				appCommand.getReflectPerState(), 
 				appCommand.getReflectPerEnforce());
+		//9-1 .詳細画面否認前の処理
+		beforeProcessDenialRepo.detailedScreenProcessBeforeDenial();
+		//9.2 
+		afterDenialProcessRepo.detailedScreenAfterDenialProcess(companyID, application.getApplicationID());
 		
-		Optional<Application> app = appRepo.getAppById(companyID, appCommand.getApplicationID());
-		if(app.isPresent()) {
-			appRepo.updateApplication(application);
-		}else {
-			throw new BusinessException("K ton tai");
-		}
 		
 	}
 
