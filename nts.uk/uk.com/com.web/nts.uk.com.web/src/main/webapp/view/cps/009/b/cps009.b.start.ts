2b3411bd22bbaf61e10735065e09df69cc13359f
Leu Thi Lan <lan-lt@nittsusystem-vn.com>
2017-09-08 13:13:58 +0700

    screen B

---
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/009/b/cps009.b.start.ts
@@ -0,0 +1,6 @@
+module nts.uk.com.view.cps009.a {
+    __viewContext.ready(function() {
+        __viewContext["viewModel"] = new viewmodel.ScreenModel();
+        __viewContext.bind(__viewContext["viewModel"]);
+    });
+}
