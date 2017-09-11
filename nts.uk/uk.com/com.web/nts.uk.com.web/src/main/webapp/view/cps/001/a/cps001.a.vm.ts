fe9bfc2c4b154b35ff8120902e73f364c1d7cbe1
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2017-09-11 10:59:47 +0700

    init front end files for modules cps001

---
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/001/a/cps001.a.vm.ts
@@ -0,0 +1,40 @@
+module cps001.a.vm {
+    import info = nts.uk.ui.dialog.info;
+    import alert = nts.uk.ui.dialog.alert;
+    import text = nts.uk.resource.getText;
+
+    let __viewContext: any = window['__viewContext'] || {},
+        block = window["nts"]["uk"]["ui"]["block"]["grayout"],
+        unblock = window["nts"]["uk"]["ui"]["block"]["clear"],
+        invisible = window["nts"]["uk"]["ui"]["block"]["invisible"];
+
+    export class ViewModel {
+
+        constructor() {
+            let self = this;
+
+            self.start();
+        }
+
+        start() {
+            let self = this;
+
+        }
+
+        saveData() {
+            let self = this;
+
+            // push data layout to webservice
+            block();
+            service.saveData({}).done(() => {
+                self.start();
+                info({ messageId: "Msg_15" }).then(function() {
+                    unblock();
+                });
+            }).fail((mes) => {
+                unblock();
+                alert(mes.message);
+            });
+        }
+    }
+}
