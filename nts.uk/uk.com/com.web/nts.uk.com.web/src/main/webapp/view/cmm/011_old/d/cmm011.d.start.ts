c824132a9bd4c44890d223a3c24f027a55f1f500
NWS-ThanhNC <thanhnc@newwave.vn>
2017-09-11 16:47:11 +0700

    Update ValidatorScript class after rename package

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmm/011_old/d/cmm011.d.start.ts
+++
@@ -1,8 +0,0 @@
-module cmm011.d {
-    __viewContext.ready(function() {
-        var screenModel = new viewmodel.ScreenModel();
-        screenModel.start().done(function() {
-            __viewContext.bind(screenModel);
-        });
-    });
-}
