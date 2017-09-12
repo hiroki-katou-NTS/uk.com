ea5989c1c2fce137442fe5028b0087b29e21891d
nws-anhnm <anhnm@newwave.vn>
2017-09-12 17:35:12 +0700

    + update viewmodel

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/002/a/kmk002.a.service.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/002/a/kmk002.a.service.ts
@@ -1,69 +1,71 @@
 module nts.uk.at.view.kmk002.a {
     export module service {
+
         /**
          *  Service paths
          */
-        var servicePath: any = {
-            findCompanySetting: 'ctx/at/shared/employment/statutory/worktime/company/find',
-            saveCompanySetting: 'ctx/at/shared/employment/statutory/worktime/company/save',
-            removeCompanySetting: 'ctx/at/shared/employment/statutory/worktime/company/remove',
-            findEmploymentSetting: 'ctx/at/shared/employment/statutory/worktime/employment/find',
-            findAllEmploymentSetting: 'ctx/at/shared/employment/statutory/worktime/employment/findall',
-            saveEmploymentSetting: 'ctx/at/shared/employment/statutory/worktime/employment/save',
-            removeEmploymentSetting: 'ctx/at/shared/employment/statutory/worktime/employment/remove',
-            findWorkplaceSetting: 'ctx/at/shared/employment/statutory/worktime/workplace/find',
-            findAllWorkplaceSetting: 'ctx/at/shared/employment/statutory/worktime/workplace/findall',
-            saveWorkplaceSetting: 'ctx/at/shared/employment/statutory/worktime/workplace/save',
-            removeWorkplaceSetting: 'ctx/at/shared/employment/statutory/worktime/workplace/remove',
-            findBeginningMonth: 'basic/company/beginningmonth/find'
+        let servicePath: any = {
+            findOptionalItemDetail: 'ctx/at/shared/workrecord/optionalitem/find',
+            saveOptionalItem: 'ctx/at/shared/workrecord/optionalitem/save',
+            removeOptionalItem: 'ctx/at/shared/workrecord/optionalitem/remove',
+            findOptionalItemHeaders: 'ctx/at/shared/workrecord/optionalitem/findall',
         };
 
-        export function getStartMonth(): JQueryPromise<any> {
-            return nts.uk.request.ajax('com', servicePath.findBeginningMonth);
+        export function saveOptionalItem(command: model.OptionalItemDto): JQueryPromise<any> {
+            return nts.uk.request.ajax(servicePath.saveOptionalItem, command);
         }
 
-        export function saveCompanySetting(command: any): JQueryPromise<any> {
-            return nts.uk.request.ajax(servicePath.saveCompanySetting, command);
+        export function findOptionalItemDetail(): JQueryPromise<model.OptionalItemDto> {
+            return nts.uk.request.ajax(servicePath.findCompanySetting + '/' + '');
         }
 
-        export function findCompanySetting(year: number): JQueryPromise<any> {
-            return nts.uk.request.ajax(servicePath.findCompanySetting + '/' + year);
+        export function findOptionalItemHeaders(): JQueryPromise<Array<model.OptionalItemHeader>> {
+            return nts.uk.request.ajax(servicePath.findOptionalItemHeaders);
         }
 
-        export function removeCompanySetting(command: any): JQueryPromise<any> {
+        export function removeOptionalItem(command: any): JQueryPromise<any> {
             return nts.uk.request.ajax(servicePath.removeCompanySetting, command);
         }
 
-        export function saveEmploymentSetting(command: any): JQueryPromise<any> {
-            return nts.uk.request.ajax(servicePath.saveEmploymentSetting, command);
-        }
-
-        export function findEmploymentSetting(request: any): JQueryPromise<any> {
-            return nts.uk.request.ajax(servicePath.findEmploymentSetting, request);
-        }
-
-        export function findAllEmploymentSetting(year: number): JQueryPromise<any> {
-            return nts.uk.request.ajax(servicePath.findAllEmploymentSetting + '/' + year);
-        }
-
-        export function removeEmploymentSetting(command: any): JQueryPromise<any> {
-            return nts.uk.request.ajax(servicePath.removeEmploymentSetting, command);
-        }
-
-        export function saveWorkplaceSetting(command: any): JQueryPromise<any> {
-            return nts.uk.request.ajax(servicePath.saveWorkplaceSetting, command);
-        }
-
-        export function findWorkplaceSetting(request: any): JQueryPromise<any> {
-            return nts.uk.request.ajax(servicePath.findWorkplaceSetting, request);
-        }
-
-        export function findAllWorkplaceSetting(year: number): JQueryPromise<any> {
-            return nts.uk.request.ajax(servicePath.findAllWorkplaceSetting + '/' + year);
-        }
-
-        export function removeWorkplaceSetting(command: any): JQueryPromise<any> {
-            return nts.uk.request.ajax(servicePath.removeWorkplaceSetting, command);
+        /**
+         * Data Model
+         */
+        export module model {
+            export interface OptionalItemHeader {
+                optionalItemNo: string;
+                optionalItemName: string;
+                usageAtr: number;
+                performanceAtr: number;
+            }
+            export interface OptionalItemDto {
+                optionalItemNo: string;
+                optionalItemName: string;
+                optionalItemAtr: number;
+                usageAtr: number;
+                empConditionAtr: number;
+                performanceAtr: number;
+                calculationResultRange: CalculationResultRangeDto;
+            }
+            export interface CalculationResultRangeDto {
+                upperCheck: boolean;
+                lowerCheck: boolean;
+                numberUpper: number;
+                numberLower: number;
+                amountUpper: number;
+                amountLower: number;
+                timeUpper: number;
+                timeLower: number;
+            }
+            export interface OptionalItemFormulaDto {
+                formulaId: string;
+                formulaName: string;
+                optionalItemNo: string;
+                formulaAtr: number;
+                symbolValue: string;
+                formulaSetting: any;
+                monthlyRounding: any;
+                dailyRounding: any;
+            }
         }
 
     }
