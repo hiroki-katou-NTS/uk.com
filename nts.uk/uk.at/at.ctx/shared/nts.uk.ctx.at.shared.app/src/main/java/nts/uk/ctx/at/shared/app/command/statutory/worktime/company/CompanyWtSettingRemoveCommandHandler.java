28ddd1f27aa498978bc685709e04fe0417a13c3f
nws-anhnm <anhnm@newwave.vn>
2017-09-12 13:18:30 +0700

    change package.

---
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.app/src/main/java/nts/uk/ctx/at/shared/app/command/statutory/worktime/company/CompanyWtSettingRemoveCommandHandler.java
@@ -0,0 +1,44 @@
+/******************************************************************
+ * Copyright (c) 2017 Nittsu System to present.                   *
+ * All right reserved.                                            *
+ *****************************************************************/
+package nts.uk.ctx.at.shared.app.command.statutory.worktime.company;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+import nts.arc.layer.app.command.CommandHandler;
+import nts.arc.layer.app.command.CommandHandlerContext;
+import nts.uk.ctx.at.shared.dom.statutory.worktime.company.CompanyWtSettingRepository;
+import nts.uk.shr.com.context.AppContexts;
+
+/**
+ * The Class CompanySettingRemoveCommandHandler.
+ */
+@Stateless
+@Transactional
+public class CompanyWtSettingRemoveCommandHandler extends CommandHandler<CompanyWtSettingRemoveCommand> {
+
+	/** The repository. */
+	@Inject
+	private CompanyWtSettingRepository repository;
+
+	/** The company id. */
+	String companyId = AppContexts.user().companyId();
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * nts.arc.layer.app.command.CommandHandler#handle(nts.arc.layer.app.command
+	 * .CommandHandlerContext)
+	 */
+	@Override
+	protected void handle(CommandHandlerContext<CompanyWtSettingRemoveCommand> context) {
+		// Get Command
+		CompanyWtSettingRemoveCommand command = context.getCommand();
+		this.repository.remove(companyId, command.getYear());
+	}
+
+}
---
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/repository/statutory/worktime/WtSettingConstant.java
@@ -0,0 +1,26 @@
+/******************************************************************
+ * Copyright (c) 2017 Nittsu System to present.                   *
+ * All right reserved.                                            *
+ *****************************************************************/
+package nts.uk.ctx.at.shared.infra.repository.statutory.worktime;
+
+/**
+ * The Class WtSettingConstant.
+ */
+public class WtSettingConstant {
+	
+	/** The Constant NORMAL. */
+	public static final int NORMAL = 0;
+	
+	/** The Constant FLEX. */
+	public static final int FLEX = 1;
+	
+	/** The Constant DEFORMED. */
+	public static final int DEFORMED = 2;
+	
+	/** The Constant STATUTORY. */
+	public static final int STATUTORY = 0;
+	
+	/** The Constant SPECIFIED. */
+	public static final int SPECIFIED = 1;
+}
