8c98e5092b28fa42498a9f7de58363ace483d455
nws-duongtv <nws-duongtv@newwave.vn>
2017-09-13 15:56:44 +0700

    add ui cdl003

---
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cdl/003/demo/cdl003.parent.vm.ts
@@ -0,0 +1,36 @@
+module nts.uk.com.view.cdl003.parent.viewmodel {
+    
+    export class ScreenModel {
+        //codes from parent screen
+        canSelectClassificationCodes: KnockoutObservable<string>;
+        selectClassificationCodes: KnockoutObservable<string>;
+        selectMode: KnockoutObservable<boolean>;
+        constructor() {
+            var self = this;
+            //construct codes 
+            self.canSelectClassificationCodes = ko.observable('0000000001,0000000002,0000000003,0000000005');
+            self.selectMode = ko.observable(true);
+            self.selectClassificationCodes = ko.observable('');
+        }
+
+        /**
+         * open dialog cdl003
+         */
+        public openDialogCDL003(): void {
+            let self = this;
+            let canSelected = self.canSelectClassificationCodes() ? self.canSelectClassificationCodes().split(',') : [];
+            nts.uk.ui.windows.setShared('inputCDL003', {
+                canSelected: canSelected,
+                isMultiple: self.selectMode()
+            }, true);
+
+            nts.uk.ui.windows.sub.modal('/view/cdl/003/a/index.xhtml').onClosed(function(): any {
+                //view all code of selected item 
+                var output = nts.uk.ui.windows.getShared('outputCDL003');
+                if (output) {
+                    self.selectClassificationCodes(output.selectedCode);
+                }
+            })
+        }
+    }
+}
