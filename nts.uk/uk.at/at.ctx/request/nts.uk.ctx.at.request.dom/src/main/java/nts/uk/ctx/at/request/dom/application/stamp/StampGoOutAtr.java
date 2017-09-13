30d8c770082a4c1665cc342ba084515e8899c8cc
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2017-09-13 18:07:00 +0700

    KAF002: update DOM

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/stamp/StampGoOutAtr.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/stamp/StampGoOutAtr.java
@@ -9,38 +9,21 @@ package nts.uk.ctx.at.request.dom.application.stamp;
  * 外出理由
  *
  */
-public enum StampGoOutReason {
+public enum StampGoOutAtr {
 	
-	/**
-	 * 私用
-	 */
 	PRIVATE(0,"私用"),
 	
-	/**
-	 * 公用
-	 */
 	PUBLIC(1,"公用"),
 	
-	/**
-	 * 有償
-	 */
 	COMPENSATION(2,"有償"),
 	
-	/**
-	 * 組合
-	 */
-	UNION(3,"組合"),
-	
-	/**
-	 * 離席
-	 */
-	LEAVE_SEAT(4,"離席");
+	UNION(3,"組合");
 	
 	public final int value;
 	
 	public final String name;
 	
-	StampGoOutReason(int value, String name){
+	StampGoOutAtr(int value, String name){
 		this.value = value;
 		this.name = name;
 	}
