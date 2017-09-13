be4d8233bbafa5c97c962a0c8510552f958606a2
NTS\keisuke_hoshina <keisuke_hoshina@nittsusystem.co.jp>
2017-09-13 16:28:22 +0900

    Add the ability to adjust the time sheet when the time sheet is duplicated

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/dailyprocess/calc/DeductionTimeSheetAdjustDuplicationTime.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/dailyprocess/calc/DeductionTimeSheetAdjustDuplicationTime.java
@@ -1,10 +1,14 @@
 package nts.uk.ctx.at.record.dom.dailyprocess.calc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import lombok.Getter;
 import lombok.Setter;
 import nts.uk.ctx.at.shared.dom.common.time.TimeSpanForCalc;
+import nts.uk.ctx.at.shared.dom.worktime.WorkTimeMethodSet;
+import nts.uk.ctx.at.shared.dom.worktime.fluidworkset.fluidbreaktimeset.BreakClockOfManageAtr;
 
 /**
  * 控除時間帯の重複部分を調整する処理を担うクラス
@@ -14,10 +18,10 @@ import nts.uk.ctx.at.shared.dom.common.time.TimeSpanForCalc;
 public class DeductionTimeSheetAdjustDuplicationTime {
 	@Getter
 	@Setter
-	private List<TimeSpanForCalc> timeSpanList;
+	private List<TimeSheetOfDeductionItem> timeSpanList;
 	
 	private DeductionTimeSheetAdjustDuplicationTime(List<TimeSheetOfDeductionItem> useDedTimeSheet) {
-		useDedTimeSheet.forEach(tc -> timeSpanList.add(tc.calculationTimeSheet));
+		this.timeSpanList = useDedTimeSheet;
 	}
 	/**
 	 * Constructor
@@ -27,58 +31,64 @@ public class DeductionTimeSheetAdjustDuplicationTime {
 		 return new DeductionTimeSheetAdjustDuplicationTime(useDedTimeSheet); 
 	}
 	
-	public List<TimeSpanForCalc> adjust(){
-		for(???) {
-			if(timeSpanList.get(0).checkDuplication(timeSpanList.get(1)).isDuplicated()){
-				
-			}
-			else {
-				
-			}
-		}
-	}
-	
-	
 	/**
-	 * n番目の時間帯を修正する(完全に被っていない前提)
-	 * @param nowListNumber
-	 */
-	public void moveFirstItem(int nowListNumber) {
-		timeSpanList.set(nowListNumber, reCreateTimeSpan(nowListNumber,adjustItemAtr.now));
-	}
-	
-	/**
-	 * n+1番目の時間帯を修正する(完全に被っていない前提)
-	 * @param nextListNumber
-	 */
-	public void moveSecondItem(int nextListNumber) {
-		timeSpanList.set(nextListNumber, reCreateTimeSpan(nextListNumber,adjustItemAtr.next));
-	}
-	
-	
-	/**
-	 * 時間帯を被らないように補正する
-	 * @param listNumber
-	 * @param itemAtr
+	 * 控除時間の重複部分調整
+	 * @param setMethod
+	 * @param clockManage
 	 * @return
 	 */
-	public TimeSpanForCalc reCreateTimeSpan(int listNumber,adjustItemAtr itemAtr) {
-		if(itemAtr.isNow()) {
-			return timeSpanList.get(listNumber).getNotDuplicationWith(timeSpanList.get(listNumber + 1)).get();
+	public List<TimeSheetOfDeductionItem> reCreate(WorkTimeMethodSet setMethod,BreakClockOfManageAtr clockManage){
+		List<TimeSheetOfDeductionItem> originCopyList = timeSpanList;
+		List<TimeSheetOfDeductionItem> toReturnCopyList = new ArrayList<TimeSheetOfDeductionItem>();
+		
+		while(originCopyList.size() > toReturnCopyList.size()) {
+			for(int number = 0 ;  number < originCopyList.size() ; number++) {
+				if(number > 0) {
+					originCopyList = toReturnCopyList;
+					toReturnCopyList.clear();
+				}
+				if(isDeplicated(originCopyList.get(number).calculationTimeSheet, originCopyList.get(number+1).calculationTimeSheet)){
+					toReturnCopyList.addAll(convertFromTimeSpanToDeductionItem(originCopyList.get(number), originCopyList.get(number + 1), setMethod, clockManage));
+					if(toReturnCopyList.size()>number) {
+						/*追加された*/
+						toReturnCopyList.addAll(originCopyList.subList(number + 2, originCopyList.size()));
+						break;
+					}
+					else if(toReturnCopyList.size() < number) {
+						/*削除された*/
+					}
+				}
+				else {
+					toReturnCopyList.add(originCopyList.get(number));
+				}
+			}
+
 		}
-		else {
-			return timeSpanList.get(listNumber + 1).getNotDuplicationWith(timeSpanList.get(listNumber)).get();
-		}
+		return toReturnCopyList;
 	}
 	
+	public List<TimeSheetOfDeductionItem> convertFromTimeSpanToDeductionItem(TimeSheetOfDeductionItem nowItem,TimeSheetOfDeductionItem nextItem,WorkTimeMethodSet setMethod,BreakClockOfManageAtr clockManage){
+		return nowItem.DeplicateBreakGoOut(nextItem,setMethod,clockManage);
+	}
 	
 	/**
-	 * 指定した場所に要素が存在するかの判定(Listの中にNULLが無いという前提)
-	 * @param ListNumber チェックしたい要素の番号(1～リストの長さ)
-	 * @return 存在する
+	 * アクセスしようとしている場所の調整(配列の最大要素数を超えていた場合最大数に減らす)
+	 * @param upperlimit 配列の要素数
+	 * @param now　アクセスしようとしている場所
+	 * @return 調整後の値
 	 */
-	public boolean existNextItem(int ListNumber) {
-		return timeSpanList.size()>=ListNumber;
+	public int adjustUpperLimit(int upperlimit,int now) {
+		return (upperlimit > now)?now:upperlimit;
+	}
+	
+	/**
+	 * 重複チェック
+	 * @param nowTimeSpan
+	 * @param nextTimeSpan
+	 * @return　重複している
+	 */
+	public boolean isDeplicated(TimeSpanForCalc nowTimeSpan,TimeSpanForCalc nextTimeSpan) {
+		return nowTimeSpan.checkDuplication(nextTimeSpan).isDuplicated();
 	}
 	
 	private enum adjustItemAtr{
