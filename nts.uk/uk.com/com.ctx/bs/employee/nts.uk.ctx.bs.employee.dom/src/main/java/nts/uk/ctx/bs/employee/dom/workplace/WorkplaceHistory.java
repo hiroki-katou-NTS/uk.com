20a599689ec4d98dd9823a7a0df4c1283014eb88
nws-dientx <dientx@newwave.vn>
2017-09-14 09:54:43 +0700

    cmm011 update domain  qa #85701

--- nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.dom/src/main/java/nts/uk/ctx/bs/employee/dom/workplace/WorkplaceHistory.java
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.dom/src/main/java/nts/uk/ctx/bs/employee/dom/workplace/WorkplaceHistory.java
@@ -18,10 +18,6 @@ public class WorkplaceHistory extends DomainObject {
 	//履歴ID
 	private HistoryId historyId;
 
-	/** The workplace id. */
-	//職場ID
-	private WorkplaceId workplaceId;
-
 	/** The period. */
 	//期間
 	private Period period;
@@ -33,7 +29,6 @@ public class WorkplaceHistory extends DomainObject {
 	 */
 	public WorkplaceHistory(WorkplaceHistoryGetMemento memento) {
 		this.historyId = memento.getHistoryId();
-		this.workplaceId = memento.getWorkplaceId();
 		this.period = memento.getPeriod();
 	}
 
@@ -44,7 +39,6 @@ public class WorkplaceHistory extends DomainObject {
 	 */
 	public void saveToMemento(WorkplaceHistorySetMemento memento) {
 		memento.setHistoryId(this.historyId);
-		memento.setWorkplaceId(this.workplaceId);
 		memento.setPeriod(this.period);
 	}
 }
