7950ad2d60c03ed8b04dda0ece2c6b3d632ba0da
Nguyen Van Tuan <tuannv@nsvc.com>
2017-09-14 16:20:36 +0700

    cps016 create UI

---
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/016/a/cps016.a.service.ts
@@ -0,0 +1,49 @@
+module nts.uk.com.view.cps016.a.service {
+    export function getItems() {
+        return $.Deferred().resolve(new DemoData().listItems).promise();
+    }
+
+    export function getItem(id: string) {
+        let listItems = new DemoData().listItems,
+            item = _.find(listItems, x => x.id == id);
+
+        return $.Deferred().resolve(item).promise();
+    }
+
+    class DemoData {
+        listItems = [{
+            id: 1,
+            name: '個人情報選択項目の定義',
+            numberCodeDigits: 1,
+            numberDigits: 10,
+            numberExternalCodeDigits: 20,
+            integrationCode: 1,
+            share: 2,
+            enable: 1,
+            notes: 'FFDFDGFdsfgdgfdg...'
+        },
+            {
+                id: 2,
+                name: '画面状態遷移図',
+                numberCodeDigits: 2,
+                numberDigits: 10,
+                numberExternalCodeDigits: 30,
+                integrationCode: 1,
+                share: 3,
+                enable: 0,
+                notes: 'Shits...'
+            },
+            {
+                id: 3,
+                name: 'モーダル画面起動中',
+                numberCodeDigits: 1,
+                numberDigits: 20,
+                numberExternalCodeDigits: 40,
+                integrationCode: 1,
+                share: 1,
+                enable: 1,
+                notes: 'モーダル画面起動中...'
+            }
+        ];
+    }
+}
---
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/016/a/cps016.a.start.ts
@@ -0,0 +1,6 @@
+module nts.uk.com.view.cps016.a {
+    __viewContext.ready(function() {
+        __viewContext.screenModel = new viewmodel.ScreenModel();
+        __viewContext.bind(__viewContext.screenModel);
+    });
+}
