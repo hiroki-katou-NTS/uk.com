732c8946fa1c94120fde4d51e632c478de55dd92
NSVN-TruongVanLai <NSVN-TruongVanLai@users.noreply.github.com>
2017-09-15 13:51:41 +0700

    update domain Employee

--- nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.dom/src/main/java/nts/uk/ctx/bs/employee/dom/employeeinfo/Employee.java
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.dom/src/main/java/nts/uk/ctx/bs/employee/dom/employeeinfo/Employee.java
@@ -14,34 +14,42 @@ import nts.arc.layer.dom.AggregateRoot;
 @NoArgsConstructor
 public class Employee extends AggregateRoot {
 
-	/** The CompanyId */
+	/** The CompanyId 会社ID */
 	private String companyId;
 
-	/** The personId */
+	/** The personId 個人ID */
 	private String pId;
 
-	/** The employeeId */
+	/** The employeeId 社員ID */
 	private String sId;
 
-	/** The employeeCode */
+	/** The employeeCode  社員コード*/
 	private EmployeeCode sCd;
 
-	/** The Company Mail */
+	/** The Company Mail  会社メールアドレス */
 	private EmployeeMail companyMail;
 
 	/** The Company Mobile Mail - 会社携帯メールアドレス */
 	private EmployeeMail mobileMail;
 
-	/** The Company Mobile */
+	/** The Company Mobile 会社携帯電話番号 */
 	private CompanyMobile companyMobile;
 
-	/** The List JobEntryHistory */
+	/** The List JobEntryHistory 入社履歴*/
 	private List<JobEntryHistory> listEntryJobHist;
 
 	public static Employee createFromJavaType(String companyId, String pId, String sId, String sCd, String companyMail,
 			String mobileMail, String companyMobile) {
-		return new Employee(companyId, pId, sId, new EmployeeCode(sCd), new EmployeeMail(companyMail),
-				new EmployeeMail(mobileMail), new CompanyMobile(companyMobile), null);
+		return new Employee(
+				companyId,
+				pId, 
+				sId, 
+				new EmployeeCode(sCd), 
+				new EmployeeMail(companyMail),
+				new EmployeeMail(mobileMail), 
+				new CompanyMobile(companyMobile),
+				null);
 	}
+	
 
 }
