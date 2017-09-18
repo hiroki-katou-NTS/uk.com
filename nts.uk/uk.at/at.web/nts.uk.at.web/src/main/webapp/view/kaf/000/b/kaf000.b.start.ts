3531ab02cd0901f2f7f8fc83dbc44458be030dbc
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2017-09-18 14:33:37 +0700

    KAF000: update B UI

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/000/b/kaf000.b.start.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/000/b/kaf000.b.start.ts
@@ -3,23 +3,8 @@ let __viewContext: any = window["__viewContext"] || {};
 __viewContext.ready(function() {
     var totalModel = {
         screenModel: new kaf000.b.viewmodel.ScreenModel(),
-        screenModel9: new nts.uk.at.view.kaf009.b.viewmodel.ScreenModel(),
-        m1: new kaf002.m1.viewmodel.ScreenModel(),
-        m2: new kaf002.m2.viewmodel.ScreenModel(),
-        m3: new kaf002.m3.viewmodel.ScreenModel(),
-        m4: new kaf002.m4.viewmodel.ScreenModel(),
-        m5: new kaf002.m5.viewmodel.ScreenModel(),
-        cm: new kaf002.cm.viewmodel.ScreenModel()
+        screenModel9: new nts.uk.at.view.kaf009.b.viewmodel.ScreenModel()
     };
-    
-    $.extend(totalModel.cm, { 
-        m1: totalModel.m1,
-        m2: totalModel.m2,
-        m3: totalModel.m3,
-        m4: totalModel.m4,
-        m5: totalModel.m5
-    });
-
 
     //totalModel.screenModel.start().done(function() {
         __viewContext.bind(totalModel);
