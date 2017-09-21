f32ea3dbc1ca2f4676ea5cfe3cde89d2e954faee
NSVC\dudt <du_dt@nittsusystem-vn.com>
2017-09-21 14:47:04 +0700

    CMM018 clear comment

--- nts.uk/uk.com/wk.ctx/nts.uk.ctx.workflow.dom/src/main/java/nts/uk/ctx/workflow/dom/approvermanagement/workroot/workplace/WorkplaceApproverAdaptor.java
+++ nts.uk/uk.com/wk.ctx/nts.uk.ctx.workflow.dom/src/main/java/nts/uk/ctx/workflow/dom/approvermanagement/workroot/workplace/WorkplaceApproverAdaptor.java
@@ -7,16 +7,6 @@ package nts.uk.ctx.workflow.dom.approvermanagement.workroot.workplace;
 import java.util.Optional;
 
 import nts.arc.time.GeneralDate;
-
-/**
-<<<<<<< HEAD
- * 
- * @author dudt
- *
-=======
- * The Interface WorkplaceApproverAdaptor.
->>>>>>> pj/at/develop
- */
 // 基準日時点の対象の職場IDに一致する職場コードと職場名称を取得する
 public interface WorkplaceApproverAdaptor {
 
