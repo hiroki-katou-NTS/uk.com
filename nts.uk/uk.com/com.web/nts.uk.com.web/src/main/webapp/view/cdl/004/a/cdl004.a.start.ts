bb431112e941629e566804d7916f6132c52e68ed
nws-duongtv <nws-duongtv@newwave.vn>
2017-09-21 12:35:06 +0700

    ADD UI cdl 004

---
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cdl/004/a/cdl004.a.start.ts
@@ -0,0 +1,12 @@
+module nts.uk.com.view.cdl004.a {
+
+    __viewContext.ready(function() {
+
+        var screenModel = new viewmodel.ScreenModel();
+        __viewContext.bind(screenModel);
+        $('#jobtitle').ntsListComponent(screenModel.jobtitles).done(function() {
+            $('#jobtitle').focus();
+        });
+
+    });
+}
