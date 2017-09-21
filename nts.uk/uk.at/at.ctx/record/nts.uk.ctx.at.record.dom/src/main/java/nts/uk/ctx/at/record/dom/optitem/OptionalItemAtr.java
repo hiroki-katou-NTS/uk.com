07c3c5727c0f107a3f7b37a6702392fc8a5d5f26
nws-anhnm <anhnm@newwave.vn>
2017-09-21 13:50:04 +0700

    update enum

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/optitem/OptionalItemAtr.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/optitem/OptionalItemAtr.java
@@ -4,27 +4,69 @@
  *****************************************************************/
 package nts.uk.ctx.at.record.dom.optitem;
 
-import lombok.AllArgsConstructor;
-
 /**
  * The Enum OptionalItemAttribute.
  */
 // 任意項目の属性
-@AllArgsConstructor
 public enum OptionalItemAtr {
 
 	/** The times. */
 	// 回数
-	TIMES(0),
+	TIMES(0, "Enum_OptionalItemAtr_TIMES", "回数"),
 
 	/** The amount. */
 	// 金額
-	AMOUNT(1),
+	AMOUNT(1, "Enum_OptionalItemAtr_AMOUNT", "金額"),
 
 	/** The time. */
 	// 時間
-	TIME(2);
+	TIME(2, "Enum_OptionalItemAtr_TIME", "時間");
 
 	/** The value. */
-	public final int value;
+	public int value;
+
+	/** The name id. */
+	public String nameId;
+
+	/** The description. */
+	public String description;
+
+	/** The Constant values. */
+	private final static OptionalItemAtr[] values = OptionalItemAtr.values();
+
+	/**
+	 * Instantiates a new optional item atr.
+	 *
+	 * @param value the value
+	 * @param nameId the name id
+	 * @param description the description
+	 */
+	private OptionalItemAtr(int value, String nameId, String description) {
+		this.value = value;
+		this.nameId = nameId;
+		this.description = description;
+	}
+
+	/**
+	 * Value of.
+	 *
+	 * @param value the value
+	 * @return the emp condition atr
+	 */
+	public static OptionalItemAtr valueOf(Integer value) {
+		// Invalid object.
+		if (value == null) {
+			return null;
+		}
+
+		// Find value.
+		for (OptionalItemAtr val : OptionalItemAtr.values) {
+			if (val.value == value) {
+				return val;
+			}
+		}
+
+		// Not found.
+		return null;
+	}
 }
