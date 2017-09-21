670b7fa612fa936620b3704ed9b51021741397d2
NSVC\anhvd <anh_vd@nittsusystem-vn.com>
2017-09-21 16:29:05 +0700

    Update code UI + server side CDL024.

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cdl/024/cdl024.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cdl/024/cdl024.vm.ts
@@ -1,47 +1,35 @@
-module nts.uk.at.view.cdl024 {
-    export module viewmodel {
-        export class ScreenModel {
-            
-            items: KnockoutObservableArray<ItemModel>;
-            columns: KnockoutObservableArray<any>;
-            currentCodeList: KnockoutObservableArray<any>;
-            
-            constructor() {
-                var self = this;
-                
-                //Init data for fillter list
-                this.items = ko.observableArray([]);
-                var str = ['a0', 'b0', 'c0', 'd0'];
-                for(var j = 0; j < 4; j++) {
-                    for(var i = 1; i < 51; i++) {    
-                        var code = i < 10 ? str[j] + '0' + i : str[j] + i;         
-                        this.items.push(new ItemModel(code,code));
-                    } 
-                }
-                this.columns = ko.observableArray([
-                    { headerText: 'コード', prop: 'code', width: 100 },
-                    { headerText: '名称', prop: 'name', width: 230 }
-                ]);
-                this.currentCodeList = ko.observableArray([]);
-            }
+module nts.uk.at.view.cdl024.viewmodel {
+    export class ScreenModel {
+        items: KnockoutObservableArray<IItemModel> = ko.observableArray([]);
+        currentCodeList: KnockoutObservableArray<string> = ko.observableArray([]);
 
-            startPage(): JQueryPromise<any> {
-                let self = this;
-                let dfd = $.Deferred();
+        constructor() {
+            let self = this,
+                items = self.items,
+                codeList = self.currentCodeList;
+        }
+
+        startPage(): JQueryPromise<any> {
+            let self = this,
+                items = self.items,
+                codeList = self.currentCodeList,
+                dfd = $.Deferred();
+
+            items.removeAll();
+            service.getAll().done((data: Array<IItemModel>) => {
+                items(data);
                 dfd.resolve();
-                return dfd.promise();
-            }
+            });
             
+            //nts.uk.ui.windows.getShared("companyId");
+            codeList(['A5', 'A7', 'A2']);
+
+            return dfd.promise();
         }
-        
-        class ItemModel {
-            code: string;
-            name: string;
-            constructor(code: string, name: string) {
-                this.code = code;
-                this.name = name;
-            }
-        }
-        
+    }
+
+    export interface IItemModel {
+        code: string;
+        name: string;
     }
 }
