d4927727b47bfd1e0bcf7322229afaf74de38805
NSVC\dudt <du_dt@nittsusystem-vn.com>
2017-09-22 08:47:14 +0700

    KAF000: vm test

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/000/test/main.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/000/test/main.ts
@@ -1,31 +1,31 @@
-module nts.uk.at.view.kaf000.test.viewmodel{
+module nts.uk.at.view.kaf000.test.viewmodel {
     import setShared = nts.uk.ui.windows.setShared;
     import getShared = nts.uk.ui.windows.getShared;
     import windows = nts.uk.ui.windows;
-    export class ScreenModel{
-        appInfor : KnockoutObservableArray<ApproverDtoK> = ko.observableArray([]);
-        //test
-        openDetail() {
+    export class ScreenModel {
+        appInfor: KnockoutObservableArray<ApproverDtoK> = ko.observableArray([]);
+        
+        constructor() {
             var self = this;
-            //承認者
-            self.appInfor.removeAll();
-            self.appInfor.push(new ApproverDtoK('18a9bf80-4a30-4e3b-9b63-6aee4e0bac1b',4));
-            self.appInfor.push(new ApproverDtoK('e3ee58d6-4ed3-4b88-a6e9-e91e2545ea7d',7));
-            self.appInfor.push(new ApproverDtoK('1445DA47-3CF9-4B0A-B819-96D20721881C',4));
-            //確定者 code
-            setShared("CMM008K_PARAM", { 
-                                        appInfor: self.appInfor()
-                                        });
-            windows.sub.modal("/view/kaf/000/b/index.xhtml", { title: "", dialogClass: "no-close" });    
-            
-        }        
-    }   
-        export class ApproverDtoK{
-            appId: string;
-            appType: number;
-            constructor(appId: string, appType: number){
-                this.appId = appId;
-                this.appType = appType;
-            }
-        } 
+            self.appInfor.push(new ApproverDtoK('18a9bf80-4a30-4e3b-9b63-6aee4e0bac1b', 4));
+            self.appInfor.push(new ApproverDtoK('e3ee58d6-4ed3-4b88-a6e9-e91e2545ea7d', 7));
+            self.appInfor.push(new ApproverDtoK('1445DA47-3CF9-4B0A-B819-96D20721881C', 9));
+        }
+        openKaf002b() {
+            var self = this;
+            nts.uk.request.jump("/view/kaf/000/b/index.xhtml", self.appInfor()[1]);
+        }
+        openKaf004e() {
+            var self = this;
+            nts.uk.request.jump("/view/kaf/000/b/index.xhtml", self.appInfor()[2]);
+        }
+    }
+    export class ApproverDtoK {
+        appID: string;
+        appType: number;
+        constructor(appID: string, appType: number) {
+            this.appID = appID;
+            this.appType = appType;
+        }
+    }
 }
