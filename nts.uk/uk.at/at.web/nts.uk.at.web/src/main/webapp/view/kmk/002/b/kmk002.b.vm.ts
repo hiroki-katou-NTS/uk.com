f4e978496d604344007ed0a8c277df5dc6e0b783
nws-anhnm <anhnm@newwave.vn>
2017-09-25 17:34:40 +0700

    update vm kmk002b

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/002/b/kmk002.b.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/002/b/kmk002.b.vm.ts
@@ -1,19 +1,73 @@
 module nts.uk.at.view.kmk002.b {
     export module viewmodel {
+        import EmpConditionDto = service.model.EmpConditionDto;
+        import ConditionDto = service.model.ConditionDto;
+
         export class ScreenModel {
+            empCondition: EmpCondition;
 
             constructor() {
+                this.empCondition = new EmpCondition();
             }
 
             /**
              * Start page.
              */
             public startPage(): JQueryPromise<void> {
-                var self = this;
-                var dfd = $.Deferred<void>();
-                dfd.resolve();
+                let self = this;
+                let dfd = $.Deferred<void>();
+                service.find('001').done(res => {
+                    //TODO testing.
+                    self.empCondition.fromDto(res);
+                    console.log(self.empCondition);
+                    dfd.resolve();
+                });
                 return dfd.promise();
             }
+
+            public save(): void {
+
+            }
+        }
+
+        class EmpCondition {
+            optionalItemNo: string;
+            empConditions: Array<Condition>;
+
+            constructor() {
+                this.optionalItemNo = '';
+                this.empConditions = new Array();
+            }
+
+            public fromDto(dto: EmpConditionDto): void {
+                let self = this;
+                self.optionalItemNo = dto.optionalItemNo;
+                self.empConditions = dto.empConditions.map(item => new Condition(item));
+            }
+
+            public toDto(): EmpConditionDto {
+                return null;
+            }
+
+        }
+
+        class Condition {
+            empCd: string;
+            empName: string;
+            empApplicableAtr: KnockoutObservable<number>;
+
+            switchds: KnockoutObservableArray<any>;
+
+            constructor(dto: ConditionDto) {
+                this.empCd = dto.empCd;
+                this.empName = dto.empName;
+                this.empApplicableAtr = ko.observable(dto.empApplicableAtr);
+                this.switchds = ko.observableArray([
+                    { code: '0', name: nts.uk.resource.getText("KMK002_15") }, // not used
+                    { code: '1', name: nts.uk.resource.getText("KMK002_14") } // used
+                ]);
+            }
+
         }
     }
 }
