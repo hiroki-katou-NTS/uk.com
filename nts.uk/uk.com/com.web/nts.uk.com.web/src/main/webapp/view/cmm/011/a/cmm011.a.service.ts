42e93793e7ee09bac55ba71d5ffcabdee1d2c608
Dương Ngô <ngoducduonglucky@gmail.com>
2017-09-25 13:41:16 +0700

    fix mapping data tree cmm011-a

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmm/011/a/cmm011.a.service.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmm/011/a/cmm011.a.service.ts
@@ -8,15 +8,7 @@ module nts.uk.com.view.cmm011.a {
         };
         
         export function findLstWorkPlace(baseDate: Date): JQueryPromise<Array<model.TreeWorkplace>> {
-            let dfd = $.Deferred();
-            nts.uk.request.ajax(servicePath.findLstWorkPlace,{startDate: baseDate}).done(function(res: Array<model.TreeWorkplace>) {
-                let list = _.map(res, function(item) {
-                    return new model.TreeWorkplace(item.workplaceId, item.code, item.name, item.hierarchyCode, item.childs);
-                });
-
-                dfd.resolve(list);
-            });
-            return dfd.promise();
+            return nts.uk.request.ajax(servicePath.findLstWorkPlace,{startDate: baseDate});
         }
         
         /**
@@ -24,26 +16,14 @@ module nts.uk.com.view.cmm011.a {
         */
         export module model {
             
-            export class TreeWorkplace {
+            export interface TreeWorkplace {
                 workplaceId: string;
                 code: string;
                 name: string;
-                nodeText: string;
+                nodeText?: string;
                 hierarchyCode: string;
-                level: number;
+                level?: number;
                 childs: Array<TreeWorkplace>;
-
-                constructor(workplaceId: string, code: string, name: string, hierarchyCode: string,
-                    childs: Array<TreeWorkplace>) {
-                    let self = this;
-                    self.workplaceId = workplaceId;
-                    self.code = code;
-                    self.name = name;
-                    self.nodeText = code + " " + name;
-                    self.hierarchyCode = hierarchyCode;
-                    self.level = hierarchyCode.length / 3;
-                    self.childs = childs;
-                }
             }
         }
     }
