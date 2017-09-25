8e30f13d67f300478f4e3024fb7d55acb2e8b322
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2017-09-25 11:04:31 +0700

    CPS002 Update UI screen A

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/002/a/cps002.a.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/002/a/cps002.a.vm.ts
@@ -3,8 +3,6 @@ module cps002.a.vm {
     import alert = nts.uk.ui.dialog.alert;
     import text = nts.uk.resource.getText;
 
-
-
     export class ViewModel {
         stepList = [
             { content: '.step-1' },
@@ -15,6 +13,13 @@ module cps002.a.vm {
         stepSelected = ko.observable({ id: 'step-1', content: '.step-1' });
         date: KnockoutObservable<Date> = ko.observable(nts.uk.time.UTCDate(2000, 0, 1));
         simpleValue: KnockoutObservable<String> = ko.observable('pikamieo');
+        itemList: KnockoutObservableArray<any> = ko.observableArray([
+            new BoxModel(1, text('CPS002_26')),
+            new BoxModel(2, text('CPS002_27')),
+            new BoxModel(3, text('CPS002_28'))
+        ]);
+        selectedId: KnockoutObservable<number> = ko.observable(1);
+        enable: KnockoutObservable<boolean> = ko.observable(true);
         constructor() {
 
 
@@ -22,6 +27,16 @@ module cps002.a.vm {
 
         next() {
             $('#wizard').ntsWizard("next");
+        }
+    }
+
+    class BoxModel {
+        id: number;
+        name: string;
+        constructor(id, name) {
+            var self = this;
+            self.id = id;
+            self.name = name;
         }
     }
 
