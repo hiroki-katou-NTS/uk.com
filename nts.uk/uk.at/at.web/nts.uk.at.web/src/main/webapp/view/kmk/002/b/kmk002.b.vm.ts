23bdc616953d35cf03956338abcc5d46a37b2f59
nws-anhnm <anhnm@newwave.vn>
2017-09-26 17:50:08 +0700

    update vm kmk002B

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/002/b/kmk002.b.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/002/b/kmk002.b.vm.ts
@@ -1,5 +1,6 @@
 module nts.uk.at.view.kmk002.b {
     export module viewmodel {
+
         import EmpConditionDto = service.model.EmpConditionDto;
         import ConditionDto = service.model.ConditionDto;
 
@@ -19,24 +20,47 @@ module nts.uk.at.view.kmk002.b {
                 service.find('001').done(res => {
                     //TODO testing.
                     self.empCondition.fromDto(res);
+                    self.empCondition.initNtsGrid();
+
                     console.log(self.empCondition);
                     dfd.resolve();
                 });
                 return dfd.promise();
             }
 
-            public save(): void {
-
+            public applyAll(): void {
+                let self = this;
+                self.empCondition.applyAll();
+                self.empCondition.updateNtsGrid();
             }
+
+            public notApplyAll(): void {
+                let self = this;
+                self.empCondition.notApplyAll();
+                self.empCondition.updateNtsGrid();
+            }
+
+            public save(): void {
+                let self = this;
+                let dto = self.empCondition.toDto();
+                console.log(dto);
+            }
+
         }
 
         class EmpCondition {
             optionalItemNo: string;
             empConditions: Array<Condition>;
 
+            switchds: Array<any>;
+
             constructor() {
                 this.optionalItemNo = '';
                 this.empConditions = new Array();
+                this.switchds = [
+                    { code: 1, name: nts.uk.resource.getText("KMK002_51") }, // apply
+                    { code: 0, name: nts.uk.resource.getText("KMK002_52") } // not apply
+                ];
             }
 
             public fromDto(dto: EmpConditionDto): void {
@@ -46,7 +70,56 @@ module nts.uk.at.view.kmk002.b {
             }
 
             public toDto(): EmpConditionDto {
-                return null;
+                let self = this;
+                let dto = <EmpConditionDto>{};
+
+                dto.optionalItemNo = self.optionalItemNo;
+                dto.empConditions = self.empConditions.map(item => item.toDto());
+
+                return dto;
+            }
+
+            public applyAll(): void {
+                let self = this;
+                console.log(self.empConditions);
+                _.each(self.empConditions, item => item.apply() );
+                console.log(self.empConditions);
+            }
+
+            public notApplyAll(): void {
+                let self = this;
+                console.log(self.empConditions);
+                _.each(self.empConditions, item => item.notApply() );
+                console.log(self.empConditions);
+            }
+
+            public updateNtsGrid(): void {
+                let self = this;
+                _.each(self.empConditions, item => {
+                    $("#grid-emp-condition").ntsGrid("updateRow", item.empCd, item);
+                });
+            }
+
+            public initNtsGrid(): void {
+                let self = this;
+                $("#grid-emp-condition").ntsGrid({
+                    width: '300px',
+                    height: '400px',
+                    dataSource: this.empConditions,
+                    primaryKey: 'empCd',
+                    virtualization: true,
+                    virtualizationMode: 'continuous',
+                    columns: [
+                        { headerText: '', key: 'empCd', dataType: 'string', width: '50px', hidden: true },
+                        { headerText: nts.uk.resource.getText("KMK002_49"), key: 'empName', dataType: 'string', width: '100px' },
+                        { headerText: nts.uk.resource.getText("KMK002_50"), key: 'empApplicableAtr', dataType: 'number', width: '150px', ntsControl: 'SwitchButtons' },
+                    ],
+                    features: [{ name: 'Resizing' }],
+                    ntsFeatures: [{ name: 'CopyPaste' }],
+                    ntsControls: [{ name: 'SwitchButtons', options: this.switchds,
+                        optionsValue: 'code', optionsText: 'name', controlType: 'SwitchButtons', enable: true
+                    }]
+                });
             }
 
         }
@@ -54,18 +127,29 @@ module nts.uk.at.view.kmk002.b {
         class Condition {
             empCd: string;
             empName: string;
-            empApplicableAtr: KnockoutObservable<number>;
-
-            switchds: KnockoutObservableArray<any>;
+            empApplicableAtr: number;
 
             constructor(dto: ConditionDto) {
                 this.empCd = dto.empCd;
                 this.empName = dto.empName;
-                this.empApplicableAtr = ko.observable(dto.empApplicableAtr);
-                this.switchds = ko.observableArray([
-                    { code: '0', name: nts.uk.resource.getText("KMK002_15") }, // not used
-                    { code: '1', name: nts.uk.resource.getText("KMK002_14") } // used
-                ]);
+                this.empApplicableAtr = 0;
+            }
+
+            public apply(): void {
+                this.empApplicableAtr = 1;
+            }
+
+            public notApply(): void {
+                this.empApplicableAtr = 0;
+            }
+
+            public toDto(): ConditionDto {
+                let self = this;
+                let dto = <ConditionDto>{};
+                dto.empCd = self.empCd;
+                dto.empName = self.empName;
+                dto.empApplicableAtr = self.empApplicableAtr;
+                return dto;
             }
 
         }
