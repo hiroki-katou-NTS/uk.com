990282cb42f9aed7d0eec64952104e55874b0fc3
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2017-09-26 11:31:19 +0700

    dynamic contraint (not use)
    chuyển sang dùng typescript dynamic constraint

--- nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.app/src/main/java/nts/uk/ctx/bs/person/app/find/person/info/item/DynamicConstraintFinderImpl.java
+++ nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.app/src/main/java/nts/uk/ctx/bs/person/app/find/person/info/item/DynamicConstraintFinderImpl.java
@@ -9,9 +9,16 @@ import javax.inject.Inject;
 import nts.uk.ctx.bs.person.dom.person.info.item.ItemType;
 import nts.uk.ctx.bs.person.dom.person.info.item.ItemTypeState;
 import nts.uk.ctx.bs.person.dom.person.info.item.PerInfoItemDefRepositoty;
+import nts.uk.ctx.bs.person.dom.person.info.numericitem.NumericItem;
 import nts.uk.ctx.bs.person.dom.person.info.singleitem.SingleItem;
+import nts.uk.ctx.bs.person.dom.person.info.stringitem.StringItem;
+import nts.uk.ctx.bs.person.dom.person.info.timeitem.TimeItem;
+import nts.uk.ctx.bs.person.dom.person.info.timepointitem.TimePointItem;
 import nts.uk.shr.com.context.AppContexts;
 import nts.uk.shr.com.primitive.dynamic.DynamicConstraintFinder;
+import nts.uk.shr.com.primitive.dynamic.types.NumberConstraintDescriptor;
+import nts.uk.shr.com.primitive.dynamic.types.StringConstraintDescriptor;
+import nts.uk.shr.com.primitive.dynamic.types.TimeConstraintDescriptor;
 
 @Stateless
 public class DynamicConstraintFinderImpl implements DynamicConstraintFinder {
@@ -28,20 +35,49 @@ public class DynamicConstraintFinderImpl implements DynamicConstraintFinder {
 			ItemType type = state.getItemType();
 			if (type == ItemType.SINGLE_ITEM) {
 				SingleItem item = (SingleItem) state;
+				String itemCode = m.getItemCode().v();
+				boolean required = m.getIsRequired().value == 1;
+
 				switch (item.getDataTypeState().getDataTypeValue()) {
 				default:
 				case STRING:
-					return null;
+					StringItem stringItem = (StringItem) item.getDataTypeState();
+					switch (stringItem.getStringItemType()) {
+					default:
+					case ANY:
+						return new StringConstraintDescriptor(itemCode, stringItem.getStringItemLength().v(), "Any",
+								' ', true, true, null, required);
+					case ALPHANUMERIC:
+						return new StringConstraintDescriptor(itemCode, stringItem.getStringItemLength().v(),
+								"AlphaNumeric", ' ', true, true, null, required);
+					case ANYHALFWIDTH:
+						return new StringConstraintDescriptor(itemCode, stringItem.getStringItemLength().v(),
+								"AnyHalfWidth", ' ', true, true, null, required);
+					case KANA:
+						return new StringConstraintDescriptor(itemCode, stringItem.getStringItemLength().v(), "Kana",
+								' ', true, true, null, required);
+					case NUMERIC:
+						return new StringConstraintDescriptor(itemCode, stringItem.getStringItemLength().v(), "Numeric",
+								' ', true, true, null, required);
+					}
 				case NUMERIC:
-					return null;
+					NumericItem numericItem = (NumericItem) item.getDataTypeState();
+					return new NumberConstraintDescriptor(itemCode, numericItem.getNumericItemMin().v().toString(),
+							numericItem.getNumericItemMax().v().toString(), "Numeric",
+							numericItem.getDecimalPart().v().toString(), required);
 				case DATE:
-					return null;
+					//DateItem dateItem = (DateItem) item.getDataTypeState();
+					return new TimeConstraintDescriptor(itemCode, "", "", "Date", required);
 				case SELECTION:
-					return null;
+					return new StringConstraintDescriptor(itemCode, 0, "Any", ' ', false, false, null, required);
 				case TIME:
-					return null;
+					TimeItem timeItem = (TimeItem) item.getDataTypeState();
+					return new TimeConstraintDescriptor(m.getItemCode().v(), timeItem.getMin().toString(),
+							timeItem.getMax().toString(), "Time", m.getIsRequired().value == 1);
 				case TIMEPOINT:
-					return null;
+					TimePointItem tpointItem = (TimePointItem) item.getDataTypeState();
+					return new TimeConstraintDescriptor(itemCode, tpointItem.getTimePointItemMin().v().toString(),
+							tpointItem.getTimePointItemMax().v().toString(), "TimePoint", m.getIsRequired().value == 1);
 				}
 			} else {
 				return null;
