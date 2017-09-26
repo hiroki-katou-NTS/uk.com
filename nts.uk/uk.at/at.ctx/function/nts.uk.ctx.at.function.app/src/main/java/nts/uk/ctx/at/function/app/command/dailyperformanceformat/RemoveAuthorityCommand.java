3a270d17a951fd1a3d471f603f5405562c3e8816
NSVC\nampt <nam_pt@nittsusystem-vn.com>
2017-09-26 18:01:32 +0700

    update KDW 008 : screen A

---
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.app/src/main/java/nts/uk/ctx/at/function/app/command/dailyperformanceformat/RemoveAuthorityCommand.java
@@ -0,0 +1,16 @@
+package nts.uk.ctx.at.function.app.command.dailyperformanceformat;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author nampt
+ *
+ */
+@Data
+@NoArgsConstructor
+public class RemoveAuthorityCommand {
+	
+	private String dailyPerformanceFormatCode;
+
+}
