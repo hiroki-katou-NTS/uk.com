947b47788639e6718788ee67dec2929739c271b4
NTS\keisuke_hoshina <keisuke_hoshina@nittsusystem.co.jp>
2017-09-26 15:03:27 +0900

    marge

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/dailyprocess/calc/CalculationTimeSheet.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/dailyprocess/calc/CalculationTimeSheet.java
@@ -11,6 +11,7 @@ import nts.uk.ctx.at.shared.dom.bonuspay.setting.BonusPayTimesheet;
 import nts.uk.ctx.at.shared.dom.common.time.TimeSpanDuplication;
 import nts.uk.ctx.at.shared.dom.common.time.TimeSpanForCalc;
 import nts.uk.ctx.at.shared.dom.worktime.fixedworkset.timespan.TimeSpanWithRounding;
+import nts.uk.shr.com.time.AttendanceClock;
 import nts.uk.shr.com.time.TimeWithDayAttr;
 
 /**
@@ -18,14 +19,12 @@ import nts.uk.shr.com.time.TimeWithDayAttr;
  * @author keisuke_hoshina
  *
  */
+@Getter
 public abstract class CalculationTimeSheet {
-	@Getter
 	protected TimeSpanWithRounding timeSheet;
-	@Getter
 	protected final TimeSpanForCalc calculationTimeSheet;
 	protected final List<TimeSheetOfDeductionItem> deductionTimeSheets = new ArrayList<>();
 	protected List<BonusPayTimesheet> bonusPayTimeSheet;
-	@Getter
 	protected Optional<MidNightTimeSheet> midNightTimeSheet;
 	
 	
@@ -37,12 +36,51 @@ public abstract class CalculationTimeSheet {
 	 */
 	public CalculationTimeSheet(TimeSpanWithRounding timeSheet,
 								TimeSpanForCalc calculationTimeSheet,
+								List<TimeSheetOfDeductionItem> deductionTimeSheets,
+								List<BonusPayTimesheet> bonusPayTimeSheet,
 								Optional<MidNightTimeSheet> midNighttimeSheet) {
 		this.timeSheet = timeSheet;
 		this.calculationTimeSheet = calculationTimeSheet;
 		this.midNightTimeSheet = midNighttimeSheet;
 	}
 	
+	
+	/**
+	 * 指定時間を終了とする時間帯作成
+	 * @return
+	 */
+	public TimeSpanForCalc reCreateTreatAsSiteiTimeEnd(int transTime,OverTimeWorkFrameTimeSheet overTimeWork) {
+		TimeSpanForCalc copySpan = calculationTimeSheet;
+		return overTimeWork.syukusyou(copySpan.lengthAsMinutes() - transTime);
+	}
+	
+	/**
+	 * 指定時間帯を指定時間に従って縮小
+	 * @param assingnTime 指定時間
+	 * @return 縮小後の時間帯
+	 */
+	public TimeSpanForCalc syukusyou(int assignTime) {
+		int shortened = calcTotalTime() - assignTime;
+		int newEnd = calculationTimeSheet.getStart().valueAsMinutes() - shortened;
+		
+		TimeSpanForCalc newTimeSpan = new TimeSpanForCalc(new TimeWithDayAttr(shortened),new TimeWithDayAttr(newEnd));
+		List<TimeSheetOfDeductionItem> refineList = duplicateNewTimeSpan(newTimeSpan);
+		int deductionTime = 0;
+		for(TimeSheetOfDeductionItem deductionItem : refineList) {
+			deductionTime = deductionItem.calcTotalTime();
+			newTimeSpan = new TimeSpanForCalc(new TimeWithDayAttr(shortened),new TimeWithDayAttr(newEnd + deductionTime));
+		}
+		return newTimeSpan;
+	}
+	
+	/**
+	 *　時間帯と重複している控除時間帯のみを抽出する
+	 * @param newTimeSpan 時間帯
+	 * @return　控除時間帯リスト
+	 */
+	public List<TimeSheetOfDeductionItem> duplicateNewTimeSpan(TimeSpanForCalc newTimeSpan){
+		return deductionTimeSheets.stream().filter(tc -> newTimeSpan.contains(tc.calculationTimeSheet)).collect(Collectors.toList());
+	}
 	
 	
 	/**
@@ -69,15 +107,18 @@ public abstract class CalculationTimeSheet {
 	 * @return　控除の合計時間
 	 */
 	public int recursiveTotalTime() {
-		if(deductionTimeSheets.isEmpty()) return 0 ;
+		if(deductionTimeSheets.isEmpty()) return calculationTimeSheet.lengthAsMinutes() ;
 		int totalDedTime = 0;
 		for(TimeSheetOfDeductionItem dedTimeSheet : deductionTimeSheets) {
 			totalDedTime += dedTimeSheet.recursiveTotalTime();
 		}
-		return totalDedTime;
+		return totalDedTime - calculationTimeSheet.lengthAsMinutes();
 	}
 	
+<<<<<<< Updated upstream
 	
+=======
+>>>>>>> Stashed changes
 	/**
 	 * 指定時間に従って時間帯の縮小
 	 * @return
@@ -111,4 +152,70 @@ public abstract class CalculationTimeSheet {
 	public TimeSpanForCalc createTimeSpan(TimeSpanForCalc timeSpan, TimeWithDayAttr time) {
 		return contractTimeSheet(timeSpan.)
 	}
+	
+	/**
+	 * 加給時間帯のリストを作り直す
+	 * @param baseTime 基準時間
+	 * @param isDateBefore 基準時間より早い時間を切り出す
+	 * @return 切り出した加給時間帯
+	 */
+	public List<BonusPayTimesheet> recreateBonusPayListBeforeBase(TimeWithDayAttr baseTime,boolean isDateBefore){
+		List<BonusPayTimesheet> bonusPayList = new ArrayList<>();
+		for(BonusPayTimesheet bonusPay : bonusPayList) {
+			if(bonusPay.contains(baseTime)) {
+				bonusPayList.add(bonusPay.reCreateOwn(baseTime,isDateBefore));
+			}
+			else if(bonusPay.calculationTimeSheet.getEnd().lessThan(baseTime) && isDateBefore) {
+				bonusPayList.add(bonusPay);
+			}
+			else if(bonusPay.calculationTimeSheet.getStart().greaterThan(baseTime) && !isDateBefore) {
+				bonusPayList.add(bonusPay);
+			}
+		}
+		return bonusPayList; 
+	}
+	
+	/**
+	 * 控除時間帯のリストを作り直す
+	 * @param baseTime 基準時間
+	 * @param isDateBefore 基準時間より早い時間を切り出す
+	 * @return 切り出したl控除時間帯
+	 */
+	public List<TimeSheetOfDeductionItem> recreateDeductionItemBeforeBase(TimeWithDayAttr baseTime,boolean isDateBefore){
+		List<TimeSheetOfDeductionItem> deductionList = new ArrayList<>();
+		for(TimeSheetOfDeductionItem deductionItem : this.deductionTimeSheets) {
+			if(deductionItem.contains(baseTime)) {
+					deductionList.add(deductionItem.reCreateOwn(baseTime,isDateBefore));
+			}
+			else if(deductionItem.calculationTimeSheet.getEnd().lessThan(baseTime) && isDateBefore) {
+				deductionList.add(deductionItem);
+			}
+			else if(deductionItem.calculationTimeSheet.getStart().greaterThan(baseTime) && !isDateBefore) {
+				deductionList.add(deductionItem);
+			}
+		}
+		return deductionList;
+	}
+	
+	/**
+	 * 深夜時間帯のリストを作り直す
+	 * @param baseTime 基準時間
+	 * @param isDateBefore 基準時間より早い時間を切り出す
+	 * @return 切り出した深夜時間帯
+	 */
+	public Optional<MidNightTimeSheet> recreateMidNightTimeSheetBeforeBase(TimeWithDayAttr baseTime,boolean isDateBefore){
+		if(this.midNightTimeSheet.isPresent()) {
+			if(midNightTimeSheet.get().calculationTimeSheet.contains(baseTime)) {
+				return midNightTimeSheet.get().midNightTimeSheet.get().reCreateOwn(baseTime,midNightTimeSheet.get().calculationTimeSheet.getEnd());
+			}
+			else if(midNightTimeSheet.get().calculationTimeSheet.getEnd().lessThan(baseTime) && isDateBefore) {
+				return midNightTimeSheet;
+			}
+			else if(midNightTimeSheet.get().calculationTimeSheet.getStart().greaterThan(baseTime) && !isDateBefore) {
+				return midNightTimeSheet;
+			}
+		}
+		return Optional.empty();
+	}
+	
 }
