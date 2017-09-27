f2f748e39347776677d8c3d8e99bd9549bdacc36
NSVC\hieult <hieultph03963@fpt.edu.vn>
2017-09-27 10:28:40 +0700

    Create Dom Infra WorkManagementMutilple

---
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/entity/workmanagementmultiple/KshstWorkManagementMultiple.java
@@ -0,0 +1,38 @@
+package nts.uk.ctx.at.shared.infra.entity.workmanagementmultiple;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import nts.uk.shr.infra.data.entity.UkJpaEntity;
+
+/**
+ * author hieult
+ */
+@Entity
+@Table(name = "KRQDT_APP_LATE_OR_LEAVE")
+@AllArgsConstructor
+@NoArgsConstructor
+public class KshstWorkManagementMultiple extends UkJpaEntity implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@EmbeddedId
+	public KshstWorkManagementMultiplePK kshstWorkManagementPK;
+	
+	@Column(name = "Use_ATR")
+	public int useATR;
+	@Override
+	
+	protected Object getKey() {
+		return kshstWorkManagementPK;
+	}
+
+}
