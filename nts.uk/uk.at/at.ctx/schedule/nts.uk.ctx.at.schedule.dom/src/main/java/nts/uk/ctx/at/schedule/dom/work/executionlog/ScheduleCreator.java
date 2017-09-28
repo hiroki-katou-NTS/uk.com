f67568111fbc87583ead03d9893c59d25650ad29
nws-duongtv <nws-duongtv@newwave.vn>
2017-09-28 14:58:34 +0700

    add int fordel domain ksc001

---
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/work/executionlog/ScheduleCreator.java
@@ -0,0 +1,15 @@
+/******************************************************************
+ * Copyright (c) 2017 Nittsu System to present.                   *
+ * All right reserved.                                            *
+ *****************************************************************/
+package nts.uk.ctx.at.schedule.dom.work.executionlog;
+
+import nts.arc.layer.dom.AggregateRoot;
+
+/**
+ * The Class ScheduleCreator.
+ */
+// スケジュール作成対象者
+public class ScheduleCreator extends AggregateRoot{
+
+}
---
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/work/executionlog/ScheduleExecutionLog.java
@@ -0,0 +1,15 @@
+/******************************************************************
+ * Copyright (c) 2017 Nittsu System to present.                   *
+ * All right reserved.                                            *
+ *****************************************************************/
+package nts.uk.ctx.at.schedule.dom.work.executionlog;
+
+import nts.arc.layer.dom.AggregateRoot;
+
+/**
+ * The Class ScheduleExecutionLog.
+ */
+// スケジュール作成実行ログ
+public class ScheduleExecutionLog extends AggregateRoot {
+
+}
