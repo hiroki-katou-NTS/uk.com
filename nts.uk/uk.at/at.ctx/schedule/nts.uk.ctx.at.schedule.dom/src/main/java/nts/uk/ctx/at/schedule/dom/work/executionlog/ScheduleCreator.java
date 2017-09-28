7568330194efdd607128a8f745acf4d72ae1d499
nws-duongtv <nws-duongtv@newwave.vn>
2017-09-28 15:48:39 +0700

    add domain ksc001

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/work/executionlog/ScheduleCreator.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/work/executionlog/ScheduleCreator.java
@@ -4,12 +4,26 @@
  *****************************************************************/
 package nts.uk.ctx.at.schedule.dom.work.executionlog;
 
+import lombok.Getter;
 import nts.arc.layer.dom.AggregateRoot;
 
 /**
  * The Class ScheduleCreator.
  */
 // スケジュール作成対象者
+@Getter
 public class ScheduleCreator extends AggregateRoot{
+	
+	/** The execution id. */
+	// 実行ID
+	private String executionId;
+	
+	/** The execution status. */
+	// 実行状況
+	private ExecutionStatus executionStatus;
+	
+	/** The employee id. */
+	// 社員ID
+	private String employeeId;
 
 }
