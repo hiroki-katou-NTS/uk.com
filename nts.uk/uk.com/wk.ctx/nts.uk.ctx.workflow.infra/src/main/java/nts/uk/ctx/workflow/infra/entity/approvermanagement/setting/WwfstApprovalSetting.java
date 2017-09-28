9967c09cee9d7b02c9f484d07f578b4c39513827
NSVC\dudt <du_dt@nittsusystem-vn.com>
2017-09-28 16:20:26 +0700

    KAF000: xoa mot so thong tin thua

--- nts.uk/uk.com/wk.ctx/nts.uk.ctx.workflow.infra/src/main/java/nts/uk/ctx/workflow/infra/entity/approvermanagement/setting/WwfstApprovalSetting.java
+++ nts.uk/uk.com/wk.ctx/nts.uk.ctx.workflow.infra/src/main/java/nts/uk/ctx/workflow/infra/entity/approvermanagement/setting/WwfstApprovalSetting.java
@@ -3,7 +3,6 @@ package nts.uk.ctx.workflow.infra.entity.approvermanagement.setting;
 import java.io.Serializable;
 
 import javax.persistence.Column;
-import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
