76a7a42a44bebaa3676abf302e323c7d16305b19
NTS\tin_dh <tin_dh@nittsusystem.co.jp>
2017-09-29 15:16:00 +0900

    fix path reference

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/sample/functionwrap/window/start.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/sample/functionwrap/window/start.ts
@@ -13,7 +13,7 @@ __viewContext.ready(function () {
             // Set parent value
             nts.uk.ui.windows.setShared("parentValue", this.value());
             nts.uk.ui.windows.setShared("isTransistReturnData", this.isTransistReturnData());
-            nts.uk.ui.windows.sub.modal("/view/sample/window/subwindow.xhtml").onClosed(() => {
+            nts.uk.ui.windows.sub.modal("/view/sample/functionwrap/window/subwindow.xhtml").onClosed(() => {
                 // Get child value
                 var returnValue = nts.uk.ui.windows.getShared("childValue");
                 alert("My child say: " + returnValue);
