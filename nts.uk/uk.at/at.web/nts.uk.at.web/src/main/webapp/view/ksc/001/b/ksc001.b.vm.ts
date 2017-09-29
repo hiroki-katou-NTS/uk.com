b277fc3229e6841f6686f155f6679cdbdd248beb
nws-duongtv <nws-duongtv@newwave.vn>
2017-09-29 11:33:56 +0700

    update ui ksc001 A->B->C->D->E->F

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksc/001/b/ksc001.b.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksc/001/b/ksc001.b.vm.ts
@@ -1,7 +1,7 @@
 module nts.uk.at.view.ksc001.b {
 
     import NtsWizardStep = service.model.NtsWizardStep;
-    
+
     export module viewmodel {
         export class ScreenModel {
             stepList: Array<NtsWizardStep>;
@@ -16,11 +16,19 @@ module nts.uk.at.view.ksc001.b {
             selectSchedules: KnockoutObservableArray<any>;
             selectedRuleCode: any;
             checkAllCase: KnockoutObservable<boolean>;
-             enable: KnockoutObservable<boolean>;
+            enable: KnockoutObservable<boolean>;
             required: KnockoutObservable<boolean>;
             dateValue: KnockoutObservable<any>;
             startDateString: KnockoutObservable<string>;
             endDateString: KnockoutObservable<string>;
+
+            // Employee tab
+            lstPersonComponentOption: any;
+            selectedEmployeeCode: KnockoutObservableArray<string>;
+            employeeName: KnockoutObservable<string>;
+            employeeList: KnockoutObservableArray<UnitModel>;
+            alreadySettingPersonal: KnockoutObservableArray<UnitAlreadySettingModel>;
+            ccgcomponentPerson: GroupOption;
             constructor() {
                 var self = this;
                 self.enable = ko.observable(true);
@@ -43,11 +51,11 @@ module nts.uk.at.view.ksc001.b {
                     { content: '.step-1' },
                     { content: '.step-2' },
                     { content: '.step-3' },
-                    { content: '.step-4' },
-                    { content: '.step-5' },
-                    { content: '.step-6' }
+                    { content: '.step-4' }
                 ];
                 self.selectedEmployee = ko.observableArray([]);
+                self.selectedEmployeeCode = ko.observableArray([]);
+                self.alreadySettingPersonal = ko.observableArray([]);
                 self.showinfoSelectedEmployee = ko.observable(false);
                 self.baseDate = ko.observable(new Date());
 
@@ -68,38 +76,38 @@ module nts.uk.at.view.ksc001.b {
                     * Define how to use this list employee by yourself in the function's body.
                     */
                     onSearchAllClicked: function(dataList: EmployeeSearchDto[]) {
-                        self.showinfoSelectedEmployee(true);
                         self.selectedEmployee(dataList);
+                        self.applyKCP005ContentSearch(dataList);
                     },
                     onSearchOnlyClicked: function(data: EmployeeSearchDto) {
-                        self.showinfoSelectedEmployee(true);
                         var dataEmployee: EmployeeSearchDto[] = [];
                         dataEmployee.push(data);
                         self.selectedEmployee(dataEmployee);
+                        self.applyKCP005ContentSearch(dataEmployee);
                     },
                     onSearchOfWorkplaceClicked: function(dataList: EmployeeSearchDto[]) {
-                        self.showinfoSelectedEmployee(true);
                         self.selectedEmployee(dataList);
+                        self.applyKCP005ContentSearch(dataList);
                     },
                     onSearchWorkplaceChildClicked: function(dataList: EmployeeSearchDto[]) {
-                        self.showinfoSelectedEmployee(true);
                         self.selectedEmployee(dataList);
+                        self.applyKCP005ContentSearch(dataList);
                     },
                     onApplyEmployee: function(dataEmployee: EmployeeSearchDto[]) {
                         self.showinfoSelectedEmployee(true);
-                        self.selectedEmployee(dataEmployee);
+                        self.applyKCP005ContentSearch(dataEmployee);
                     }
 
                 }
                 self.stepSelected = ko.observable({ id: 'step-1', content: '.step-1' });
-                
-                
+
+
                 self.selectSchedules = ko.observableArray([
                     { code: '1', name: nts.uk.resource.getText("KSC001_74") },
                     { code: '2', name: nts.uk.resource.getText("KSC001_75") }
                 ]);
                 self.selectedRuleCode = ko.observable(1);
-                        
+
             }
 
             begin() {
@@ -121,8 +129,119 @@ module nts.uk.at.view.ksc001.b {
                 var index = this.stepList.indexOf(this.stepSelected());
                 $('#wizard').ntsWizard("goto", index);
             }
+
+            /**
+            * apply ccg001 search data to kcp005
+            */
+            public applyKCP005ContentSearch(dataList: EmployeeSearchDto[]): void {
+                var self = this;
+                self.employeeList([]);
+                var employeeSearchs: UnitModel[] = [];
+                for (var employeeSearch of dataList) {
+                    var employee: UnitModel = {
+                        code: employeeSearch.employeeCode,
+                        name: employeeSearch.employeeName,
+                        workplaceName: employeeSearch.workplaceName
+                    };
+                    employeeSearchs.push(employee);
+                }
+                self.employeeList(employeeSearchs);
+
+                self.lstPersonComponentOption = {
+                    isShowAlreadySet: false,
+                    isMultiSelect: true,
+                    listType: ListType.EMPLOYEE,
+                    employeeInputList: self.employeeList,
+                    selectType: SelectType.SELECT_BY_SELECTED_CODE,
+                    selectedCode: self.selectedEmployeeCode,
+                    isDialog: true,
+                    isShowNoSelectRow: false,
+                    alreadySettingList: self.alreadySettingPersonal,
+                    isShowWorkPlaceName: true,
+                    isShowSelectAllButton: false,
+                    maxWidth: 550,
+                    maxRows: 15
+                };
+
+            }
+            /**
+             * function next page by selection employee goto page (C)
+             */
+            private nextPageEmployee(): void {
+                var self = this;
+                self.next();
+            }
+            /**
+             * function previous page by selection employee goto page (C)
+             */
+            private previousPageC(): void {
+                var self = this;
+                self.previous();
+            }
+            /**
+             * function next page by selection employee goto page (D)
+             */
+            private nextPageC(): void {
+                var self = this;
+                self.next();
+            }
+            /**
+             * function previous page by selection employee goto page (D)
+             */
+            private previousPageD(): void {
+                var self = this;
+                self.previous();
+            }
+            /**
+             * function next page by selection employee goto page (E)
+             */
+            private nextPageD(): void {
+                var self = this;
+                self.next();
+            }
+            /**
+             * function previous page by selection employee goto page (E)
+             */
+            private previousPageE(): void {
+                var self = this;
+                self.previous();
+            }
+            /**
+             * finish next page by selection employee goto page (F)
+             */
+            private finish(): void {
+                var self = this;
+                 nts.uk.ui.windows.sub.modal("/view/ksc/001/f/index.xhtml").onClosed(function() {
+                });
+            }
+
         }
-        
+        export class ListType {
+            static EMPLOYMENT = 1;
+            static Classification = 2;
+            static JOB_TITLE = 3;
+            static EMPLOYEE = 4;
+        }
+
+        export interface UnitModel {
+            code: string;
+            name?: string;
+            workplaceName?: string;
+            isAlreadySetting?: boolean;
+        }
+
+        export class SelectType {
+            static SELECT_BY_SELECTED_CODE = 1;
+            static SELECT_ALL = 2;
+            static SELECT_FIRST_ITEM = 3;
+            static NO_SELECT = 4;
+        }
+
+        export interface UnitAlreadySettingModel {
+            code: string;
+            isAlreadySetting: boolean;
+        }
+
         export interface EmployeeSearchDto {
             employeeId: string;
 
