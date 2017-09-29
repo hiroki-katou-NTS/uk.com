ffe41389c50d9e6a8fb9a83a041efaa1cc2de21b
nws-duongtv <nws-duongtv@newwave.vn>
2017-09-29 17:35:21 +0700

    update ui ksc001

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksc/001/b/ksc001.b.start.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksc/001/b/ksc001.b.start.ts
@@ -1,13 +1,16 @@
 module nts.uk.at.view.ksc001.b {
     __viewContext.ready(function() {
         let screenModel = new viewmodel.ScreenModel();
-        __viewContext.bind(screenModel);
-        $('#ccgcomponent').ntsGroupComponent(screenModel.ccgcomponent).done(function() {
-            screenModel.employeeList = ko.observableArray<UnitModel>([]);
-            screenModel.applyKCP005ContentSearch([]);
-            // Load employee list component
-            $('#employeeSearch').ntsListComponent(screenModel.lstPersonComponentOption).done(function() {
+        screenModel.startPage().done(function(self) {
+            __viewContext.bind(self);
+            $('#ccgcomponent').ntsGroupComponent(self.ccgcomponent).done(function() {
+                self.employeeList = ko.observableArray<UnitModel>([]);
+                self.applyKCP005ContentSearch([]);
+                // Load employee list component
+                $('#employeeSearch').ntsListComponent(self.lstPersonComponentOption).done(function() {
+                });
             });
         });
+
     });
 }
