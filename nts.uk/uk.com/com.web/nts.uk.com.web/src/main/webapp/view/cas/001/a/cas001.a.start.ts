1177205b6a780756f00deb344b56547c5e7a391b
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2017-09-29 17:30:57 +0700

    optimize code CAS001

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cas/001/a/cas001.a.start.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cas/001/a/cas001.a.start.ts
@@ -6,6 +6,29 @@ module nts.uk.com.view.cas001.a {
             let helpButton = "<button id=\"A2_012\" data-bind=\"ntsHelpButton: {image: \'A2_012.png\', position: \'right top\', enable: true }\">?</button>";
             nts.uk.ui.ig.grid.header.getCell('A2_008', 'setting').append($(helpButton));
             ko.applyBindings(__viewContext['screenModel'], nts.uk.ui.ig.grid.header.getCell('A2_008', 'setting')[0]);
+
+
+            //register click change all event
+            $(function() {
+                $('#anotherSelectedAll_auth, #seftSelectedAll_auth').on('click', '.nts-switch-button', function() {
+
+                    __viewContext['screenModel'].changeAll($(this).parent().attr('id'));
+
+                });
+
+                $('#anotherSelectedAll_auth').on('keydown', function(event) {
+                    if (event.keyCode === 37 || event.keyCode === 39) {
+                        __viewContext['screenModel'].changeAll($(this).attr('id'));
+                    }
+                });
+
+                $('#seftSelectedAll_auth').on('keydown', function(event) {
+                    if (event.keyCode === 37 || event.keyCode === 39) {
+                        __viewContext['screenModel'].changeAll($(this).attr('id'));
+                    }
+                });
+
+            });
         });
 
     });
