4db85a7c2b5489a3e50d9d00abaf26285fe77cd1
nws-anhnm <anhnm@newwave.vn>
2017-10-02 09:51:53 +0700

    add imported employment

---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.ac/src/main/java/nts/uk/ctx/at/record/ac/organization/EmploymentAdapterImpl.java
@@ -0,0 +1,38 @@
+/******************************************************************
+ * Copyright (c) 2017 Nittsu System to present.                   *
+ * All right reserved.                                            *
+ *****************************************************************/
+package nts.uk.ctx.at.record.ac.organization;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.Stateless;
+
+import nts.uk.ctx.at.record.dom.organization.EmploymentAdapter;
+import nts.uk.ctx.at.record.dom.organization.EmploymentImported;
+
+/**
+ * The Class EmploymentAdapterImpl.
+ */
+@Stateless
+public class EmploymentAdapterImpl implements EmploymentAdapter {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * nts.uk.ctx.at.record.dom.organization.EmploymentAdapter#getAllEmployment(
+	 * java.lang.String)
+	 */
+	@Override
+	public List<EmploymentImported> getAllEmployment(String comId) {
+		List<EmploymentImported> list = new ArrayList<EmploymentImported>();
+		//TODO: mock data.
+		for (int i = 0; i < 10; i++) {
+			list.add(new EmploymentImported(comId, ""+i, "name"+i));
+		}
+		return list;
+	}
+
+}
