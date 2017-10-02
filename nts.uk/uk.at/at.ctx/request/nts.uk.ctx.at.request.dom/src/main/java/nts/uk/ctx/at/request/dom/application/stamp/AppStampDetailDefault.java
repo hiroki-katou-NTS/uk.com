00f92ca883e0d01f37b2fa9732b047b4ac794a08
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2017-10-02 17:48:49 +0700

    KAF002: update AppStamp APP command, DOM

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/stamp/AppStampDetailDefault.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/stamp/AppStampDetailDefault.java
@@ -1,12 +1,19 @@
 package nts.uk.ctx.at.request.dom.application.stamp;
 
+import java.util.List;
+import java.util.UUID;
+
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import nts.uk.ctx.at.request.dom.application.common.PrePostAtr;
-import nts.uk.ctx.at.request.dom.application.common.service.detailscreen.after.AfterProcessDetail;
+import nts.uk.ctx.at.request.dom.application.common.appapprovalphase.AppApprovalPhase;
+import nts.uk.ctx.at.request.dom.application.common.appapprovalphase.AppApprovalPhaseRepository;
+import nts.uk.ctx.at.request.dom.application.common.approvalframe.ApprovalFrameRepository;
+import nts.uk.ctx.at.request.dom.application.common.approveaccepted.ApproveAcceptedRepository;
+import nts.uk.ctx.at.request.dom.application.common.service.detailscreen.after.DetailAfterUpdate;
 import nts.uk.ctx.at.request.dom.application.common.service.detailscreen.before.BeforePreBootMode;
-import nts.uk.ctx.at.request.dom.application.common.service.detailscreen.before.DetailBeforeProcessRegister;
+import nts.uk.ctx.at.request.dom.application.common.service.detailscreen.before.DetailBeforeUpdate;
 /**
  * 
  * @author Doan Duy Hung
@@ -28,10 +35,19 @@ public class AppStampDetailDefault implements AppStampDetailDomainService {
 	private PreLaunchScreenSetting preLaunchScreenSetting;*/
 	
 	@Inject
-	private DetailBeforeProcessRegister detailBeforeProcessRegister;
+	private DetailBeforeUpdate detailBeforeProcessRegister;
 	
 	@Inject
-	private AfterProcessDetail afterProcessDetail;
+	private DetailAfterUpdate afterProcessDetail;
+	
+	@Inject
+	private AppApprovalPhaseRepository appApprovalPhaseRepository;
+	
+	@Inject
+	private ApprovalFrameRepository approvalFrameRepository;
+	
+	@Inject
+	private ApproveAcceptedRepository approveAcceptedRepository; 
 	
 	@Override
 	public void appStampPreProcess(AppStamp appStamp) {
@@ -42,59 +58,41 @@ public class AppStampDetailDefault implements AppStampDetailDomainService {
 	}
 
 	@Override
-	public void appStampGoOutPermitUpdate(String titleReason, String detailReason, AppStamp appStamp) {
+	public void appStampUpdate(String titleReason, String detailReason, AppStamp appStamp, List<AppApprovalPhase> appApprovalPhases) {
 		this.appStampCommonDomainService.appReasonCheck(titleReason, detailReason, appStamp);
-		this.appStampCommonDomainService.validateReason(appStamp);
-		this.appStampUpdate(appStamp);
-		
-	}
-
-	@Override
-	public void appStampWorkUpdate(String titleReason, String detailReason, AppStamp appStamp) {
-		this.appStampCommonDomainService.appReasonCheck(titleReason, detailReason, appStamp);
-		this.appStampCommonDomainService.validateReason(appStamp);
-		this.appStampUpdate(appStamp);
-		
-	}
-
-	@Override
-	public void appStampCancelUpdate(String titleReason, String detailReason, AppStamp appStamp) {
-		this.appStampCommonDomainService.appReasonCheck(titleReason, detailReason, appStamp);
-		this.appStampCommonDomainService.validateReason(appStamp);
-		this.appStampUpdate(appStamp);
-		
-	}
-
-	@Override
-	public void appStampOnlineRecordUpdate(String titleReason, String detailReason, AppStamp appStamp) {
-		this.appStampCommonDomainService.appReasonCheck(titleReason, detailReason, appStamp);
-		this.appStampCommonDomainService.validateReason(appStamp);
-		this.appStampUpdate(appStamp);
-		
-	}
-
-	@Override
-	public void appStampOtherUpdate(String titleReason, String detailReason, AppStamp appStamp) {
-		// TODO Auto-generated method stub
-		
+		// this.appStampCommonDomainService.validateReason(appStamp);
+		this.appStampUpdateProcess(appStamp);
+		// this.approvalUpdate(appApprovalPhases, appStamp.getApplicationID());
 	}
 	
-	private void appStampUpdate(AppStamp appStamp) {
-		StampRequestMode StampRequestMode = appStamp.getStampRequestMode();
-		this.detailBeforeProcessRegister.processBeforeDetailScreenRegistration(
+	private void appStampUpdateProcess(AppStamp appStamp) {
+		/*this.detailBeforeProcessRegister.processBeforeDetailScreenRegistration(
 				appStamp.getCompanyID(), 
 				appStamp.getApplicantSID(), 
 				appStamp.getApplicationDate(), 
 				1, 
 				appStamp.getApplicationID(), 
-				PrePostAtr.PREDICT);
-		switch(StampRequestMode){
-			case STAMP_GO_OUT_PERMIT: appStampRepository.updateStampGoOutPermit(appStamp);break;
-			case STAMP_ADDITIONAL: appStampRepository.updateStampWork(appStamp);break;
-			case STAMP_CANCEL: appStampRepository.updateStampCancel(appStamp);break;
-			case STAMP_ONLINE_RECORD: appStampRepository.updateStampOnlineRecord(appStamp);break;
-			default: break;
-		}
-		this.afterProcessDetail.processAfterDetailScreenRegistration(appStamp.getCompanyID(), appStamp.getApplicationID());
+				PrePostAtr.PREDICT);*/
+		appStampRepository.updateStamp(appStamp);
+		// this.afterProcessDetail.processAfterDetailScreenRegistration(appStamp.getCompanyID(), appStamp.getApplicationID());
+	}
+	
+	private void approvalUpdate(List<AppApprovalPhase> appApprovalPhases, String appID){
+		appApprovalPhases.forEach(appApprovalPhase -> {
+			appApprovalPhase.setAppID(appID);
+			String phaseID = UUID.randomUUID().toString();
+			appApprovalPhase.setPhaseID(phaseID);
+			appApprovalPhaseRepository.update(appApprovalPhase);
+			appApprovalPhase.getListFrame().forEach(approvalFrame -> {
+				String frameID = UUID.randomUUID().toString();
+				approvalFrame.setFrameID(frameID);
+				approvalFrameRepository.update(approvalFrame, phaseID);
+				approvalFrame.getListApproveAccepted().forEach(appAccepted -> {
+					String appAcceptedID = UUID.randomUUID().toString();
+					appAccepted.setAppAcceptedID(appAcceptedID);
+					approveAcceptedRepository.updateApproverAccepted(appAccepted, frameID);
+				});
+			});
+		});
 	}
 }
