fd6b2d65c455e34e3d6be4da81975cc03e589218
TruongQuocPhong <phong_tq@nittsusystem-vn.com>
2017-10-03 09:09:49 +0700

    kml002-h: fix error miss stateless

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.app/src/main/java/nts/uk/ctx/at/schedule/app/command/budget/schedulevertical/fixedverticalsetting/UpdateVerticalTimeSettingCommandHandler.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.app/src/main/java/nts/uk/ctx/at/schedule/app/command/budget/schedulevertical/fixedverticalsetting/UpdateVerticalTimeSettingCommandHandler.java
@@ -3,7 +3,9 @@ package nts.uk.ctx.at.schedule.app.command.budget.schedulevertical.fixedvertical
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 
 import nts.arc.layer.app.command.CommandHandlerContext;
 import nts.arc.layer.app.command.CommandHandlerWithResult;
@@ -11,6 +13,13 @@ import nts.uk.ctx.at.schedule.dom.budget.schedulevertical.fixedverticalsetting.F
 import nts.uk.ctx.at.schedule.dom.budget.schedulevertical.fixedverticalsetting.VerticalTime;
 import nts.uk.shr.com.context.AppContexts;
 
+/**
+ * Update Vertical Time Setting
+ * @author phongtq
+ *
+ */
+@Stateless
+@Transactional
 public class UpdateVerticalTimeSettingCommandHandler extends CommandHandlerWithResult<AddVerticalTimeSettingCommand, List<String>> {
 	
 	@Inject
