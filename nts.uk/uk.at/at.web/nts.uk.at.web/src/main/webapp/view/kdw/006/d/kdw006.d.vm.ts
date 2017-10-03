af84b10ac59ec45ca802f4915f4b864d1c9da0d6
NSVC\anhvd <anh_vd@nittsusystem-vn.com>
2017-10-03 17:39:56 +0700

    Update code UI KDW006 D.

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/006/d/kdw006.d.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/006/d/kdw006.d.vm.ts
@@ -1,13 +1,26 @@
 module nts.uk.at.view.kdw006.d {
     export module viewmodel {
         export class ScreenModel {
+            //Define radio button group
+            itemList: KnockoutObservableArray<any> = ko.observableArray([
+                new BoxModel(1, 'エラーがあっても確認できる'),
+                new BoxModel(2, 'エラーを訂正するまでチェックできない'),
+                new BoxModel(3, 'エラーを訂正するまで登録できない')
+            ]);;
+            selectedId: KnockoutObservable<number> = ko.observable(1);
+            enable2: KnockoutObservable<boolean> = ko.observable(true);
+
+            //Define checkbox
+            enable: KnockoutObservable<boolean> = ko.observable(true);
+
             constructor() {
+                let self = this;
             }
 
-            saveData(){
+            saveData() {
                 alert('screen d');
             }
-            
+
             start(): JQueryPromise<any> {
                 let self = this;
                 let dfd = $.Deferred();
@@ -15,6 +28,16 @@ module nts.uk.at.view.kdw006.d {
                 return dfd.promise();
             }
 
+        }
+
+        export class BoxModel {
+            id: number;
+            name: string;
+            constructor(id, name) {
+                var self = this;
+                self.id = id;
+                self.name = name;
+            }
         }
     }
 }
