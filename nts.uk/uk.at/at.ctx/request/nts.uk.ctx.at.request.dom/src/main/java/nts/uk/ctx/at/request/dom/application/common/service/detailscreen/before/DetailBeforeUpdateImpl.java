71d11cb31bfdd43cad32dd64620b8306dee49e90
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2017-10-03 09:56:50 +0700

    KAF000: update UI index b, update commonservice: comments

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/common/service/detailscreen/before/DetailBeforeUpdateImpl.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/common/service/detailscreen/before/DetailBeforeUpdateImpl.java
@@ -5,6 +5,7 @@ import javax.inject.Inject;
 
 import nts.arc.time.GeneralDate;
 import nts.uk.ctx.at.request.dom.application.common.PrePostAtr;
+import nts.uk.ctx.at.request.dom.application.common.service.newscreen.before.NewBeforeRegister;
 import nts.uk.ctx.at.request.dom.application.common.service.other.OtherCommonAlgorithm;
 
 @Stateless
@@ -13,13 +14,16 @@ public class DetailBeforeUpdateImpl implements DetailBeforeUpdate {
 	@Inject
 	private OtherCommonAlgorithm otherCommonAlgorithmService;
 	
+	@Inject
+	private NewBeforeRegister newBeforeRegister;
+	
 	public void processBeforeDetailScreenRegistration(String companyID, String employeeID, GeneralDate appDate, int employeeRouteAtr, String appID, PrePostAtr postAtr ){
 
 		// 選択した勤務種類の矛盾チェック(check sự mâu thuẫn của worktype đã chọn)
 		// selectedWorkTypeConflictCheck();
 		
 		// アルゴリズム「確定チェック」を実施する(thực hiện xử lý 「確定チェック」)
-		otherCommonAlgorithmService.employeePeriodCurrentMonthCalculate(companyID, employeeID, appDate);
+		newBeforeRegister.confirmationCheck(companyID, employeeID, appDate);
 		
 		exclusiveCheck();
 	}
