295520b8317565452b666e61aa4977e10bfba519
NSVC\nampt <nam_pt@nittsusystem-vn.com>
2017-10-05 16:57:02 +0700

    update kmk 008 A screen + B screen

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.infra/src/main/java/nts/uk/ctx/at/function/infra/repository/dailyperformanceformat/JpaAuthorityFormatSheetRepository.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.infra/src/main/java/nts/uk/ctx/at/function/infra/repository/dailyperformanceformat/JpaAuthorityFormatSheetRepository.java
@@ -21,6 +21,8 @@ public class JpaAuthorityFormatSheetRepository extends JpaRepository implements 
 	private static final String UPDATE_BY_KEY;
 
 	private static final String DEL_BY_KEY;
+	
+	private static final String IS_EXIST_DATA;
 
 	static {
 		StringBuilder builderString = new StringBuilder();
@@ -48,6 +50,15 @@ public class JpaAuthorityFormatSheetRepository extends JpaRepository implements 
 		builderString
 				.append("AND a.kfnmtAuthorityFormSheetPK.dailyPerformanceFormatCode = :dailyPerformanceFormatCode ");
 		DEL_BY_KEY = builderString.toString();
+		
+		builderString = new StringBuilder();
+		builderString.append("SELECT COUNT(a) ");
+		builderString.append("FROM KfnmtAuthorityFormSheet a ");
+		builderString
+				.append("WHERE a.kfnmtAuthorityFormSheetPK.companyId = :companyId ");
+		builderString.append("AND a.kfnmtAuthorityFormSheetPK.dailyPerformanceFormatCode = :dailyPerformanceFormatCode ");
+		builderString.append("AND a.kfnmtAuthorityFormSheetPK.sheetNo = :sheetNo ");
+		IS_EXIST_DATA = builderString.toString();
 	}
 
 	@Override
@@ -76,6 +87,14 @@ public class JpaAuthorityFormatSheetRepository extends JpaRepository implements 
 	public void remove(String companyId, DailyPerformanceFormatCode dailyPerformanceFormatCode) {
 		this.getEntityManager().createQuery(DEL_BY_KEY).setParameter("companyId", companyId)
 				.setParameter("dailyPerformanceFormatCode", dailyPerformanceFormatCode.v()).executeUpdate();
+	}
+
+	@Override
+	public boolean checkExistData(String companyId, DailyPerformanceFormatCode dailyPerformanceFormatCode,
+			BigDecimal sheetNo) {
+		return this.queryProxy().query(IS_EXIST_DATA, long.class).setParameter("companyId", companyId)
+				.setParameter("dailyPerformanceFormatCode", dailyPerformanceFormatCode.v())
+				.setParameter("sheetNo", sheetNo).getSingle().get() > 0;
 	}
 
 	private static AuthorityFormatSheet toDomain(KfnmtAuthorityFormSheet kfnmtAuthorityFormSheet) {
