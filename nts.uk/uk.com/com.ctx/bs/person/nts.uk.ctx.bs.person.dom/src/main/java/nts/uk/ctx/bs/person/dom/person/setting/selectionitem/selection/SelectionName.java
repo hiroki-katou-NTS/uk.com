31d65373b356607dfca0e0c081a5632ba3e3872a
Nguyen Van Tuan <tuannv@nsvc.com>
2017-10-09 15:59:12 +0700

    create domain Selection (Screen cps017)

--- nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.dom/src/main/java/nts/uk/ctx/bs/person/dom/person/setting/selectionitem/selection/SelectionName.java
+++ nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.dom/src/main/java/nts/uk/ctx/bs/person/dom/person/setting/selectionitem/selection/SelectionName.java
@@ -1,17 +1,17 @@
-package nts.uk.ctx.bs.person.dom.person.setting.selectionitem;
+package nts.uk.ctx.bs.person.dom.person.setting.selectionitem.selection;
 
 import nts.arc.primitive.StringPrimitiveValue;
 import nts.arc.primitive.constraint.StringMaxLength;
 
-@StringMaxLength(500)
-public class Memo extends StringPrimitiveValue<Memo>{
+@StringMaxLength(80)
+public class SelectionName extends StringPrimitiveValue<SelectionName>{
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	public Memo(String rawValue) {
+
+	public SelectionName(String rawValue) {
 		super(rawValue);
 		// TODO Auto-generated constructor stub
 	}
