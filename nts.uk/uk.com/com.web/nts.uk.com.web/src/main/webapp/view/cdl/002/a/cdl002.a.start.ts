a0da4a9e757ea94f6691210d6f40167b98507c33
nws_tiendd <tiendd_int@newwave.vn>
2017-10-10 12:58:13 +0700

    Fix bug NUK-63.

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cdl/002/a/cdl002.a.start.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cdl/002/a/cdl002.a.start.ts
@@ -4,9 +4,7 @@ module nts.uk.com.view.cdl002.a {
         __viewContext.bind(screenModel);
         // Load Component
         $('#emp-component').ntsListComponent(screenModel.listComponentOption).done(function() {
-//            $('#emp-component').focusComponent();
-            $('#emp-component').focus();
-            
+            $('#emp-component').focusComponent();
         });
     });
 }
