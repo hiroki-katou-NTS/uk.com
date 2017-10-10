6f7c4545617e99b76ea860b05abaa09c91b32810
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2017-10-10 08:54:18 +0700

    add getItemListFromCtgId web services

---
+++ nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.dom/src/main/java/nts/uk/ctx/bs/person/dom/person/setting/copysetting/EmpCopySettingItemRepository.java
@@ -0,0 +1,10 @@
+package nts.uk.ctx.bs.person.dom.person.setting.copysetting;
+
+import java.util.List;
+
+import nts.uk.ctx.bs.person.dom.person.info.setting.copysetting.EmpCopySettingItem;
+
+public interface EmpCopySettingItemRepository {
+
+	List<EmpCopySettingItem> getAll(String ctgId);
+}
