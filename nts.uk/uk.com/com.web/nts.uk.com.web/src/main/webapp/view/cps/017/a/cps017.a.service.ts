a288d67b388a936e515d5f69fe53264944dbffa7
Nguyen Van Tuan <tuannv@nsvc.com>
2017-10-11 10:01:06 +0700

    Update code UI CPS 017 -  A

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/017/a/cps017.a.service.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/017/a/cps017.a.service.ts
@@ -2,11 +2,17 @@ module nts.uk.com.view.cps017.a.service {
     import ajax = nts.uk.request.ajax;
     import format = nts.uk.text.format;
     var paths = {
-        getAllSelectionItems: "ctx/bs/person/info/setting/selection/findAll"
+        getAllSelectionItems: "ctx/bs/person/info/setting/selection/findAll",
+        getPerInfoSelectionItem: "ctx/bs/person/info/setting/selection/findItem/{0}"
     }
 
     export function getAllSelectionItems() {
         return ajax(paths.getAllSelectionItems);
+    }
+
+    export function getPerInfoSelectionItem(selectionItemId: string) {
+        let _path = format(paths.getPerInfoSelectionItem, selectionItemId);
+        return nts.uk.request.ajax("com", _path);
     }
 }
 
