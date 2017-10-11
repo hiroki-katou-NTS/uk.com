7654be1eb5c5cbfd263262d21ee8910ca21c31ed
haivh <haivh@newwave.vn>
2017-10-11 14:56:17 +0700

    make new code UI CMM014. fix ClassificationWs.java and JpaClassificationRepository.java (remove function)

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmm/014_old/a/cmm014.a.start.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmm/014_old/a/cmm014.a.start.ts
@@ -1,7 +1,8 @@
-module nts.uk.com.view.cmm014.a {
+module nts.uk.com.view.cmm014_old.a {
     __viewContext.ready(function() {
         let screenModel = new viewmodel.ScreenModel();
         screenModel.start().done(function() {
+            nts.uk.ui.confirmSave(screenModel.dirty);
             __viewContext.bind(screenModel);
         });
     });
