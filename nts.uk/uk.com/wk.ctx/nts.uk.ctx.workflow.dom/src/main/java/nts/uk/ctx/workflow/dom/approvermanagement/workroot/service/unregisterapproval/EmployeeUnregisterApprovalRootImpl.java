f900abc2c7ce0408ee4dae03c0ac1fc7657d255c
NSVC\dudt <du_dt@nittsusystem-vn.com>
2017-10-11 18:49:03 +0700

    CMM018L: Add information workplace

--- nts.uk/uk.com/wk.ctx/nts.uk.ctx.workflow.dom/src/main/java/nts/uk/ctx/workflow/dom/approvermanagement/workroot/service/unregisterapproval/EmployeeUnregisterApprovalRootImpl.java
+++ nts.uk/uk.com/wk.ctx/nts.uk.ctx.workflow.dom/src/main/java/nts/uk/ctx/workflow/dom/approvermanagement/workroot/service/unregisterapproval/EmployeeUnregisterApprovalRootImpl.java
@@ -14,6 +14,7 @@ import nts.uk.ctx.workflow.dom.adapter.bs.EmployeeAdapter;
 import nts.uk.ctx.workflow.dom.adapter.bs.PersonAdapter;
 import nts.uk.ctx.workflow.dom.adapter.bs.dto.EmployeeImport;
 import nts.uk.ctx.workflow.dom.adapter.workplace.WorkplaceAdapter;
+import nts.uk.ctx.workflow.dom.adapter.workplace.WorkplaceImport;
 import nts.uk.ctx.workflow.dom.approvermanagement.workroot.ApplicationType;
 import nts.uk.ctx.workflow.dom.approvermanagement.workroot.CompanyApprovalRoot;
 import nts.uk.ctx.workflow.dom.approvermanagement.workroot.CompanyApprovalRootRepository;
@@ -52,50 +53,40 @@ public class EmployeeUnregisterApprovalRootImpl implements EmployeeUnregisterApp
 		}
 		// ドメインモデル「会社別就業承認ルート」を取得する(lấy thông tin domain「会社別就業承認ルート」)
 		List<CompanyApprovalRoot> comInfo = comRootRepository.findByBaseDateOfCommon(companyId, baseDate);
-		if (CollectionUtil.isEmpty(comInfo)) {
-			// TODO thuc hien khi co tra loi QA
-			// return lstUnRegister;
-		} else {
-			List<CompanyApprovalRoot> comInfoCommon = comInfo.stream()
-					.filter(x -> x.getEmploymentRootAtr().value == EmploymentRootAtr.COMMON.value)
-					.collect(Collectors.toList());
-			if (!CollectionUtil.isEmpty(comInfoCommon)) {
-				return lstUnRegister;
-
-			}
+		List<CompanyApprovalRoot> comInfoCommon = comInfo.stream()
+				.filter(x -> x.getEmploymentRootAtr().value == EmploymentRootAtr.COMMON.value)
+				.collect(Collectors.toList());
+		if (!CollectionUtil.isEmpty(comInfoCommon)) {
+			return lstUnRegister;
 		}
 
 		// 就業ルート区分が共通の「会社別就業承認ルート」がない場合(không có thông tin 「会社別就業承認ルート」 của 就業ルート区分là
 		// common)
 		// ドメインモデル「職場別就業承認ルート」を取得する(lấy thông tin domain 「職場別就業承認ルート」)
-		List<WorkplaceApprovalRoot> wpInfo = wpRootRepository.findAllByBaseDate(companyId, baseDate);
+		List<WorkplaceApprovalRoot> wpInfoByCid = wpRootRepository.findAllByBaseDate(companyId, baseDate);
 		// ドメインモデル「個人別就業承認ルート」を取得する(lấy thông tin domain 「個人別就業承認ルート」)
 		List<PersonApprovalRoot> psInfo = psRootRepository.findAllByBaseDate(companyId, baseDate);
 
-		for (EmployeeImport empInfor : lstEmps) {
-			EmployeeUnregisterOutput empInfo = new EmployeeUnregisterOutput();
+		for (EmployeeImport empImport : lstEmps) {
 			List<String> appTypes = new ArrayList<>();
 			for (ApplicationType appType : ApplicationType.values()) {
-				// 社員の対象申請の承認ルートを取得する(lấy dữ liệu approve route của đối tượng đơn xin của nhân
-				// viên)
+				// 社員の対象申請の承認ルートを取得する(lấy dữ liệu approve route của đối tượng đơn xin của nhân viên)
 				boolean isEmpRoot =false;
-				if (empInfor.getSId() != null) {
-					isEmpRoot = employeeOfApprovalRoot.lstEmpApprovalRoot(companyId, comInfo, wpInfo, psInfo, empInfor,
-							appType, baseDate);
-				}
+				isEmpRoot = employeeOfApprovalRoot.lstEmpApprovalRoot(companyId, comInfo, wpInfoByCid, psInfo, empImport,
+						appType, baseDate);
 				// 承認ルート未登録出力対象として追加する(thêm vào đối tượng chưa cài đặt approve route để output)
 				if (!isEmpRoot) {
 					appTypes.add(appType.nameId);
 				}
 			}
-
+			//set workplace infor
+			WorkplaceImport wpInforBySid = wpNameInfor.findBySid(empImport.getSId(), baseDate);
+			empImport.setWpCode(wpInforBySid.getWkpCode());
+			empImport.setWpName(wpInforBySid.getWkpName());
 			if (!CollectionUtil.isEmpty(appTypes)) {
-
-				empInfo.setAppType(appTypes);
-
-				empInfor.setPName(psInfor.getPersonInfo(empInfor.getSId()).getEmployeeName());
-				empInfo.setEmpInfor(empInfor);
-				lstUnRegister.add(empInfo);
+				empImport.setPName(psInfor.getPersonInfo(empImport.getSId()).getEmployeeName());
+				EmployeeUnregisterOutput dataOuput = new EmployeeUnregisterOutput(empImport, appTypes);
+				lstUnRegister.add(dataOuput);
 			}
 		}
 
