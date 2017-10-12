3aa5d2576ee1a4a0c64d8edb395eafa3f9da46a0
nws-anhnm <anhnm@newwave.vn>
2017-10-12 17:04:13 +0700

    -fix entity relation
    -fix typo
    -fix bug empty List
    -fix bug not flush

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/optitem/calculation/ItemSelectionDto.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/optitem/calculation/ItemSelectionDto.java
@@ -4,12 +4,14 @@
  *****************************************************************/
 package nts.uk.ctx.at.record.app.command.optitem.calculation;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import lombok.Getter;
 import lombok.Setter;
 import nts.arc.enums.EnumAdaptor;
+import nts.gul.collection.CollectionUtil;
 import nts.uk.ctx.at.record.dom.optitem.calculation.ItemSelectionGetMemento;
 import nts.uk.ctx.at.record.dom.optitem.calculation.MinusSegment;
 import nts.uk.ctx.at.record.dom.optitem.calculation.SelectedAttendanceItem;
@@ -48,6 +50,9 @@ public class ItemSelectionDto implements ItemSelectionGetMemento {
 	 */
 	@Override
 	public List<SelectedAttendanceItem> getListSelectedAttendanceItem() {
+		if (CollectionUtil.isEmpty(this.attendanceItems)) {
+			return Collections.emptyList();
+		}
 		return this.attendanceItems.stream().map(item -> new SelectedAttendanceItem(item))
 				.collect(Collectors.toList());
 	}
