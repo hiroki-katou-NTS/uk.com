230adc342e32a4c7e26ff2fc091da4c74cdbe45a
nws-duongtv <nws-duongtv@newwave.vn>
2017-10-12 10:53:41 +0700

    update domain KSC001 F

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/schedule/basicschedule/personalfee/WorkSchedulePersonFee.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/schedule/basicschedule/personalfee/WorkSchedulePersonFee.java
@@ -2,22 +2,23 @@
  * Copyright (c) 2017 Nittsu System to present.                   *
  * All right reserved.                                            *
  *****************************************************************/
-package nts.uk.ctx.at.schedule.dom.schedule.basicschedule.personalcost;
+package nts.uk.ctx.at.schedule.dom.schedule.basicschedule.personalfee;
 
 import lombok.Getter;
 import nts.arc.layer.dom.DomainObject;
 
 /**
- * The Class WorkSchedulePersonCost.
+ * The Class WorkSchedulePersonFee.
  */
 // 勤務予定人件費
 @Getter
-public class WorkSchedulePersonCost extends DomainObject{
+public class WorkSchedulePersonFee extends DomainObject{
 	
 	/** The no. */
 	// NO
 	private ExtraTimeItemNo no;
 
+	/** The personal fee amount. */
 	// 人件費金額
-	//private personalCostAmount 
+	private PersonalFeeAmount personalFeeAmount; 
 }
