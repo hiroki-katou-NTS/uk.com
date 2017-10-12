bdd894244ed49e9d250a334243c4f66c0bc8e9ca
NTS\tin_dh <tin_dh@nittsusystem.co.jp>
2017-10-12 17:56:55 +0900

    update UkJpaHelper

--- nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/infra/helper/UkJpaHelper.java
+++ nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/infra/helper/UkJpaHelper.java
@@ -1,13 +1,8 @@
 package nts.uk.shr.infra.helper;
 
-import java.lang.reflect.Field;
-import java.util.Collection;
-import java.util.List;
-
 import javax.ejb.Stateless;
 
-import nts.arc.layer.infra.helper.JPAHelper;
-import nts.arc.layer.ws.json.JacksonUtil;
+import nts.arc.layer.infra.data.helper.JPAHelper;
 import nts.arc.time.GeneralDateTime;
 import nts.uk.shr.infra.data.entity.UkJpaEntity;
 
@@ -15,32 +10,14 @@ import nts.uk.shr.infra.data.entity.UkJpaEntity;
 public class UkJpaHelper implements JPAHelper {
 
 	@Override
-	public boolean makeDirty(Object entity) {
+	public void makeDirty(Object entity) {
 		try {
 			if(entity instanceof UkJpaEntity){
-				return makeDirtyDetail((UkJpaEntity) entity);
-			} else if (entity instanceof Collection<?>) {
-				boolean flag = false;
-				for (Object e : (Collection<?>) entity){
-					flag = flag && makeDirty(e);
-				}
-				return flag;
+				((UkJpaEntity) entity).setUpdDate(GeneralDateTime.now());
 			}
-		} catch (IllegalArgumentException | IllegalAccessException e) {
+		} catch (IllegalArgumentException e) {
 			e.printStackTrace();
 		}
-		return false;
-	}
-	
-	private <T extends UkJpaEntity> boolean makeDirtyDetail(T entity) throws IllegalArgumentException, IllegalAccessException {
-		boolean flag = false;
-		entity.setUpdDate(GeneralDateTime.now());
-		List<Field> fields = JacksonUtil.getAllFieldFromClass(entity.getClass());
-		for(Field field : fields){
-			Object fieldValue = field.get(entity);
-			flag = flag && makeDirty(fieldValue);
-		}
-		return flag;
 	}
 
 }
