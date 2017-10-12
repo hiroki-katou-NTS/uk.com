2173312dc35433b1c8865a931860dcce2ee4f8af
Nguyen Van Tuan <tuannv@nsvc.com>
2017-10-12 11:24:20 +0700

    update code CPS016

--- nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.app/src/main/java/find/person/setting/selectionitem/PerInfoHistorySelectionDto.java
+++ nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.app/src/main/java/find/person/setting/selectionitem/PerInfoHistorySelectionDto.java
@@ -3,21 +3,22 @@ package find.person.setting.selectionitem;
 import lombok.Value;
 import nts.arc.time.GeneralDate;
 import nts.uk.ctx.bs.person.dom.person.setting.selectionitem.PerInfoHistorySelection;
+import nts.uk.shr.com.time.calendar.period.DatePeriod;
 
 @Value
 public class PerInfoHistorySelectionDto {
 	private String histId;
 	private String selectionItemId;
 	private String companyCode;
-	private GeneralDate startDate;
-	private GeneralDate endDate;
+	// private GeneralDate startDate;
+	// private GeneralDate endDate;
+	private DatePeriod period;
 
 	public static PerInfoHistorySelectionDto fromDomainHistorySelection(PerInfoHistorySelection domain) {
-		return new PerInfoHistorySelectionDto(
-				domain.getHistId(), 
-				domain.getSelectionItemId(), 
-				domain.getCompanyCode(),
-				domain.getStartDate(), 
-				domain.getEndDate());
+		return new PerInfoHistorySelectionDto(domain.getHistId(), domain.getSelectionItemId(), domain.getCompanyCode(),
+				// domain.getStartDate(),
+				// domain.getEndDate());
+				domain.getPeriod());
+
 	}
 }
--- nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.infra/src/main/java/repository/person/setting/selectionitem/JpaPerInfoHistorySelectionRepository.java
+++ nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.infra/src/main/java/repository/person/setting/selectionitem/JpaPerInfoHistorySelectionRepository.java
@@ -11,6 +11,7 @@ import nts.arc.layer.infra.data.JpaRepository;
 import nts.arc.time.GeneralDate;
 import nts.uk.ctx.bs.person.dom.person.setting.selectionitem.PerInfoHistorySelection;
 import nts.uk.ctx.bs.person.dom.person.setting.selectionitem.PerInfoHistorySelectionRepository;
+import nts.uk.shr.com.time.calendar.period.DatePeriod;
 
 @Stateless
 public class JpaPerInfoHistorySelectionRepository extends JpaRepository implements PerInfoHistorySelectionRepository {
@@ -21,10 +22,9 @@ public class JpaPerInfoHistorySelectionRepository extends JpaRepository implemen
 
 	private static final String SELECT_ALL_HISTORY_STARTDATE_SELECTION = SELECT_ALL
 			+ " WHERE si.startDate < :startDate";
-	
+
 	private static final String SELECT_ALL_HISTORY_COMPANYID_SELECTION = SELECT_ALL
 			+ " WHERE si.companyId < :companyId";
-	
 
 	@Override
 	public void add(PerInfoHistorySelection domain) {
@@ -40,36 +40,43 @@ public class JpaPerInfoHistorySelectionRepository extends JpaRepository implemen
 	}
 
 	private PerInfoHistorySelection toDomain(PpemtHistorySelection entity) {
+		DatePeriod datePeriod = new DatePeriod(entity.startDate, entity.endDate);
+
 		return PerInfoHistorySelection.createHistorySelection(entity.histidPK.histidPK, entity.selectionItemId,
-				entity.companyId, entity.endDate, entity.startDate);
+				entity.companyId, datePeriod);
 	}
 
 	@Override
 	public List<PerInfoHistorySelection> historySelection(String selectionItemId) {
+
 		return this.queryProxy().query(SELECT_ALL_HISTORY_SELECTION, PpemtHistorySelection.class)
 				.setParameter("selectionItemId", selectionItemId).getList(c -> toDomain(c));
 	}
 
 	public Optional<PerInfoHistorySelection> getHistorySelectionItem(String histId) {
 		PpemtHistorySelectionPK pkHistorySelection = new PpemtHistorySelectionPK(histId);
+
 		return this.queryProxy().find(pkHistorySelection, PpemtHistorySelection.class).map(c -> toDomain(c));
 	}
 
 	private static PpemtHistorySelection toHistEntity(PerInfoHistorySelection domain) {
 		PpemtHistorySelectionPK key = new PpemtHistorySelectionPK(domain.getHistId());
-		return new PpemtHistorySelection(key, domain.getSelectionItemId(), domain.getCompanyCode(), domain.getEndDate(),
-				domain.getStartDate());
+
+		return new PpemtHistorySelection(key, domain.getSelectionItemId(), domain.getCompanyCode(),
+				domain.getPeriod().start(), domain.getPeriod().end());
 	}
 
 	// historyStartDateSelection
 	@Override
 	public List<PerInfoHistorySelection> historyStartDateSelection(GeneralDate startDate) {
+
 		return this.queryProxy().query(SELECT_ALL_HISTORY_STARTDATE_SELECTION, PpemtHistorySelection.class)
 				.setParameter("startDate", startDate).getList(c -> toDomain(c));
 	}
 
 	@Override
 	public List<PerInfoHistorySelection> getAllPerInfoHistorySelection(String companyId) {
+
 		return this.queryProxy().query(SELECT_ALL_HISTORY_COMPANYID_SELECTION, PpemtHistorySelection.class)
 				.setParameter("companyId", companyId).getList(c -> toDomain(c));
 	}
