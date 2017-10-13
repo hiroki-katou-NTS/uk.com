65a068b106b309b2c48f6fb7c36cee4b904a1c0f
Leu Thi Lan <lan-lt@nittsusystem-vn.com>
2017-10-13 19:30:32 +0700

    CPS009: display itemlst

--- nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.dom/src/main/java/nts/uk/ctx/bs/person/dom/person/setting/init/item/PerInfoInitValueSetItemRepository.java
+++ nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.dom/src/main/java/nts/uk/ctx/bs/person/dom/person/setting/init/item/PerInfoInitValueSetItemRepository.java
@@ -3,14 +3,28 @@ package nts.uk.ctx.bs.person.dom.person.setting.init.item;
 import java.util.List;
 
 public interface PerInfoInitValueSetItemRepository {
+	
 	/**
 	 * get all init value item
-	 * @param initValueSettingCtgId
+	 * @param perInfoCtgId
 	 * @return
 	 */
-	List<PerInfoInitValueSetItem> getAllItem(String initValueSettingCtgId);
+	List<PerInfoInitValueSetItem> getAllItem(String perInfoCtgId);
 	
+	/**
+	 * get detail item
+	 * @param perInfoCtgId
+	 * @param perInfoItemDefId
+	 * @return
+	 */
+	PerInfoInitValueSetItem getDetailItem(String perInfoCtgId, String perInfoItemDefId);
+	
+	/**
+	 * check category have item list ? 
+	 * @param perInfoCtgId
+	 * @return
+	 */
 	boolean isExist(String perInfoCtgId);
-	PerInfoInitValueSetItem getDetailItem(String initValueSettingCtgId, String perInfoItemDefId);
+
 
 }
