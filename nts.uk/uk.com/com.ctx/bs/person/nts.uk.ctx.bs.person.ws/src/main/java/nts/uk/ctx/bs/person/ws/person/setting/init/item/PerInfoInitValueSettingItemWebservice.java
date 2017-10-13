0a53e82d4439b2492750d9c46ccbb68942c6f7eb
Leu Thi Lan <lan-lt@nittsusystem-vn.com>
2017-10-13 10:09:24 +0700

    CPS009: webservice

---
+++ nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.ws/src/main/java/nts/uk/ctx/bs/person/ws/person/setting/init/item/PerInfoInitValueSettingItemWebservice.java
@@ -0,0 +1,12 @@
+package nts.uk.ctx.bs.person.ws.person.setting.init.item;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import nts.arc.layer.ws.WebService;
+
+@Path("ctx/bs/person/info/setting/init/item")
+@Produces("application/json")
+public class PerInfoInitValueSettingItemWebservice extends WebService {
+
+}
