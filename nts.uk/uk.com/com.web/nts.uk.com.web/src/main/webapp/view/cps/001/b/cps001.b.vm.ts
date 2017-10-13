af7de64958357b6380687c0c6dc6805110a33890
NSVN-TruongVanLai <NSVN-TruongVanLai@users.noreply.github.com>
2017-10-13 14:03:15 +0700

    update cps001

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/001/b/cps001.b.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/001/b/cps001.b.vm.ts
@@ -4,6 +4,7 @@ module cps001.b.vm {
     import close = nts.uk.ui.windows.close;
     import setShared = nts.uk.ui.windows.setShared;
     import getShared = nts.uk.ui.windows.getShared;
+    import showDialog = nts.uk.ui.dialog;
     let __viewContext: any = window['__viewContext'] || {};
 
     export class ViewModel {
@@ -32,16 +33,19 @@ module cps001.b.vm {
 
         pushData() {
             let self = this,
-                empDelete : IModelDto = ko.toJS(self.empDelete);
+                empDelete: IModelDto = ko.toJS(self.empDelete);
             nts.uk.ui.dialog.confirm({ messageId: "Msg_18" }).ifYes(() => {
                 let self = this,
                     //employeeId: any = getShared('CPS001B_PARAM') || null;
                     employeeId = "90000000-0000-0000-0000-000000000001";
                 if (employeeId) {
-                    let command ={sId: employeeId , reason :empDelete.reason };
-                    service.deleteEmp(command).done((result) => {
-                        setShared('CPS001B_VALUE', {});
-                        console.log(result);
+                    let command = { sId: employeeId, reason: empDelete.reason };
+                    service.deleteEmp(command).done(() => {
+                        showDialog.info({ messageId: "Msg_16" }).then(function() {
+                            setShared('CPS001B_VALUE', {});
+                            close();
+                        });
+
                     });
                 }
             }).ifCancel(() => {
