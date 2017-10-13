b25218311e67c778ce11bd65efa29a4e6123008b
nws_tiendd <tiendd_int@newwave.vn>
2017-10-13 11:08:04 +0700

    fix bug NUK-57

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cdl/008/a/cdl008.a.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cdl/008/a/cdl008.a.vm.ts
@@ -61,12 +61,12 @@ module nts.uk.com.view.cdl008.a {
                 var self = this;
                 if(self.isMultiple){
                     if(!self.selectedMulWorkplace() || self.selectedMulWorkplace().length == 0){
-                        nts.uk.ui.dialog.alertError({ messageId: "Msg_643" });
+                        nts.uk.ui.dialog.alertError({ messageId: "Msg_643" }).then(() => nts.uk.ui.windows.close());
                         return;    
                     }    
                 }else {
                      if(!self.selectedSelWorkplace || !self.selectedSelWorkplace()){
-                        nts.uk.ui.dialog.alertError({ messageId: "Msg_643" });
+                        nts.uk.ui.dialog.alertError({ messageId: "Msg_643" }).then(() => nts.uk.ui.windows.close());
                         return;    
                     }      
                 }
