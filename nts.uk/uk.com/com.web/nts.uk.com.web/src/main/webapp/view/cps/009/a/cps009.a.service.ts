34404cf8dbfa743b008879da9a712c9f5d30d03f
Leu Thi Lan <lan-lt@nittsusystem-vn.com>
2017-10-13 10:10:47 +0700

    CPS009-A:  display initSet & initCtg

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/009/a/cps009.a.service.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/009/a/cps009.a.service.ts
@@ -2,5 +2,22 @@ module nts.uk.com.view.cps009.a.service {
     import ajax = nts.uk.request.ajax;
     import format = nts.uk.text.format;
     let paths = {
+        getAll: "ctx/bs/person/info/setting/init/findAll",
+        getAllCtg : "ctx/bs/person/info/setting/init/ctg/findAll"
     }
+   /**
+    * Get all init value setting
+    */
+    export function getAll() {
+        return ajax(paths.getAll);
+    }
+    
+       /**
+    * Get all init value setting
+    */
+    export function getAllCtg() {
+        return ajax(paths.getAllCtg);
+    }
+    
+
 }
