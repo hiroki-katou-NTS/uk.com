f5a5e33ec2fca263a3891952fe263bd0ab3b5008
NSVN-TRAN THANH TRUNG <trung_tt@nittsusystem-vn.com>
2017-10-16 14:47:35 +0700

    update db  team

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.app/src/main/java/nts/uk/ctx/at/schedule/app/find/shift/team/TeamDto.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.app/src/main/java/nts/uk/ctx/at/schedule/app/find/shift/team/TeamDto.java
@@ -1,10 +1,12 @@
 package nts.uk.ctx.at.schedule.app.find.shift.team;
 
 import lombok.Value;
-import nts.uk.ctx.at.schedule.app.find.shift.specificdayset.company.CompanySpecificDateDto;
-import nts.uk.ctx.at.schedule.dom.shift.specificdayset.company.CompanySpecificDateItem;
 import nts.uk.ctx.at.schedule.dom.shift.team.Team;
-
+/**
+ * 
+ * @author Trung Tran
+ *
+ */
 @Value
 public class TeamDto {
 	private String workPlaceId;
