40b0276c4e567f3a5588a4f2b4293550a2510718
NSVC\anhvd <anh_vd@nittsusystem-vn.com>
2017-10-16 11:00:48 +0700

    Code UI KDW001 b,c,d,j.

---
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/001/b/kdw001.b.vm.ts
@@ -0,0 +1,12 @@
+module nts.uk.at.view.kdw001.b {
+    export module viewmodel {
+        export class ScreenModel {
+            constructor() {
+            }
+            
+            navigateView() {
+                
+            }
+        }
+    }
+}
