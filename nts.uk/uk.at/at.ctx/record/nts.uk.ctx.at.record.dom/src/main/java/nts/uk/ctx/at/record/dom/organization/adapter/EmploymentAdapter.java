90a623ce1a51daefc8fbf55266f595dd97a31dca
nws-anhnm <anhnm@newwave.vn>
2017-10-16 10:24:02 +0700

    change package EmploymentAdapter

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/organization/adapter/EmploymentAdapter.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/organization/adapter/EmploymentAdapter.java
@@ -2,9 +2,11 @@
  * Copyright (c) 2017 Nittsu System to present.                   *
  * All right reserved.                                            *
  *****************************************************************/
-package nts.uk.ctx.at.record.dom.organization;
+package nts.uk.ctx.at.record.dom.organization.adapter;
 
 import java.util.List;
+
+import nts.uk.ctx.at.record.dom.organization.EmploymentImported;
 
 /**
  * The Interface EmploymentAdapter.
