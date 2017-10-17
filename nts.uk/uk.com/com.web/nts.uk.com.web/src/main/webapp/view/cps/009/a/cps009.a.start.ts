de8d3ff4397358afe142ae1fff30c18e8e2c62cf
Leu Thi Lan <lan-lt@nittsusystem-vn.com>
2017-10-17 14:53:21 +0700

    CPS009: code UI

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/009/a/cps009.a.start.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/009/a/cps009.a.start.ts
@@ -16,9 +16,9 @@ module nts.uk.com.view.cps009.a {
         let helpButton = '<button id=\"A2_012\" style=\'margin-left:20px;\' data-bind=\" click: openBDialog\">' + nts.uk.resource.getText('CPS009_22') + '</button>'
             + '<button style=\'margin-left: 10px;\' data-bind=\"ntsHelpButton: {image: \'helpcps0092.png\',position: \'right top\', enable: true }\">?</button>';
 
-        getCell('grid2', 'combo').append($(helpButton));
+        getCell('grid2', 'refMethodType').append($(helpButton));
 
-        ko.applyBindings(__viewContext['viewModel'], getCell('grid2', 'combo')[0]);
+        ko.applyBindings(__viewContext['viewModel'], getCell('grid2', 'refMethodType')[0]);
 
     });
 }
@@ -26,22 +26,52 @@ module nts.uk.com.view.cps009.a {
 function init() {
 
     $("#grid2").ntsGrid({
-        width: '600px',
+        width: '700px',
         height: '400px',
-        dataSource: __viewContext["viewModel"].items,
-        primaryKey: 'id',
+        dataSource: __viewContext["viewModel"].currentCategory().itemList,
+        primaryKey: 'perInfoItemDefId',
         virtualization: true,
         virtualizationMode: 'continuous',
+        virtualrecordsrender: function(evt, ui) {
+            if ($("#grid2").data("igGrid") === undefined) {
+                return;
+            }
+            var ds = ui.owner.dataSource.data();
+            $(ds)
+                .each(
+                function(index, el: any) {
+                    let nameCell = $("#grid2").igGrid("cellAt", 0, index);
+                    let referenceCell = $("#grid2").igGrid("cellAt", 1, index);
+
+                    if (el.isRequired === 1) {
+                        $(nameCell).addClass('requiredCell');
+                        $(referenceCell).addClass('requiredCell');
+                    } else {
+                        $(nameCell).addClass('notrequiredCell');
+                        $(referenceCell).addClass('notrequiredCell');
+                    }
+
+                });
+        },
         columns: [
-            { headerText: 'ID', key: 'id', dataType: 'number', width: '100px', ntsControl: 'Label' },
-            { headerText: '', key: 'combo', dataType: 'string', width: '300px', ntsControl: 'Combobox' },
-            { headerText: nts.uk.resource.getText('CPS009_23'), key: 'id', dataType: 'number', width: '200px' },
+            { headerText: "", key: 'perInfoItemDefId', dataType: 'string', width: '100px', hidden: true },
+            { headerText: '', key: 'isRequired', dataType: 'number', width: '300px', hidden: true },
+            { headerText: nts.uk.resource.getText('CPS009_21'), key: 'itemName', dataType: 'string', width: '100px' },
+            { headerText: '', key: 'refMethodType', dataType: 'number', width: '300px', ntsControl: 'Combobox' },
+            { headerText: nts.uk.resource.getText('CPS009_23'), key: 'value', dataType: 'string', width: '200px' }
         ],
         features: [],
         ntsFeatures: [{ name: 'CopyPaste' },
-         { name: 'CellEdit' }],
+            { name: 'CellEdit' }],
         ntsControls: [
             { name: 'Combobox', options: __viewContext["viewModel"].comboItems, optionsValue: 'code', optionsText: 'name', columns: __viewContext["viewModel"].comboColumns, controlType: 'ComboBox', enable: true }
         ]
     });
+
+    $(document).delegate("#grid2", "ntsgridcontrolvaluechanged", function(evt, ui) {
+
+        console.log(ui);
+
+    });
+
 }
