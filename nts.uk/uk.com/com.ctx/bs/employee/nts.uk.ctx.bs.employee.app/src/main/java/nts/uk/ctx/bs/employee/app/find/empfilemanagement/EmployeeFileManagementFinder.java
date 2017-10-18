3f741307dcbd6abb50231ba2a615cf3390ae4988
NSVN-PhamXuanVinh <vinh_px@nittsusystem-vn.com>
2017-10-18 11:12:17 +0700

    cps 001 d dom

---
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.app/src/main/java/nts/uk/ctx/bs/employee/app/find/empfilemanagement/EmployeeFileManagementFinder.java
@@ -0,0 +1,5 @@
+package nts.uk.ctx.bs.employee.app.find.empfilemanagement;
+
+public class EmployeeFileManagementFinder {
+
+}
---
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.app/src/main/java/nts/uk/ctx/bs/employee/app/find/empfilemanagement/dto/EmployeeFileManagementDto.java
@@ -0,0 +1,13 @@
+package nts.uk.ctx.bs.employee.app.find.empfilemanagement.dto;
+
+import lombok.Data;
+
+@Data
+public class EmployeeFileManagementDto {
+	/**employee id*/
+	private String employeeId;
+	/**file id*/
+	private String fileId;
+	/**file type*/
+	private String fileType;
+}
