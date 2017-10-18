c31d557b5fac3bcd008822501fbfb8fe498ab608
nws-anhnm <anhnm@newwave.vn>
2017-10-18 15:03:45 +0700

    +add MonthlyAttendanceItemRepository
    +update OptionalItemFinder

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/find/optitem/OptionalItemFinder.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/find/optitem/OptionalItemFinder.java
@@ -12,12 +12,21 @@ import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import nts.uk.ctx.at.record.app.find.optitem.calculation.FormulaDto;
+import nts.uk.ctx.at.record.dom.dailyattendanceitem.DailyAttendanceItem;
+import nts.uk.ctx.at.record.dom.dailyattendanceitem.enums.DailyAttendanceAtr;
+import nts.uk.ctx.at.record.dom.dailyattendanceitem.repository.DailyAttendanceItemRepository;
+import nts.uk.ctx.at.record.dom.monthlyattendanceitem.MonthlyAttendanceItem;
+import nts.uk.ctx.at.record.dom.monthlyattendanceitem.MonthlyAttendanceItemAtr;
+import nts.uk.ctx.at.record.dom.monthlyattendanceitem.MonthlyAttendanceItemRepository;
 import nts.uk.ctx.at.record.dom.optitem.OptionalItem;
+import nts.uk.ctx.at.record.dom.optitem.OptionalItemAtr;
 import nts.uk.ctx.at.record.dom.optitem.OptionalItemRepository;
+import nts.uk.ctx.at.record.dom.optitem.PerformanceAtr;
 import nts.uk.ctx.at.record.dom.optitem.calculation.CalculationAtr;
 import nts.uk.ctx.at.record.dom.optitem.calculation.Formula;
 import nts.uk.ctx.at.record.dom.optitem.calculation.FormulaId;
 import nts.uk.ctx.at.record.dom.optitem.calculation.FormulaRepository;
+import nts.uk.ctx.at.record.dom.optitem.calculation.OperatorAtr;
 import nts.uk.ctx.at.record.dom.optitem.calculation.disporder.FormulaDispOrder;
 import nts.uk.ctx.at.record.dom.optitem.calculation.disporder.FormulaDispOrderRepository;
 import nts.uk.shr.com.context.AppContexts;
@@ -28,11 +37,11 @@ import nts.uk.shr.com.context.AppContexts;
 @Stateless
 public class OptionalItemFinder {
 
-	/** The repo. */
+	/** The repository. */
 	@Inject
 	private OptionalItemRepository repository;
 
-	/** The repo. */
+	/** The formula repo. */
 	@Inject
 	private FormulaRepository formulaRepo;
 
@@ -40,17 +49,121 @@ public class OptionalItemFinder {
 	@Inject
 	private FormulaDispOrderRepository orderRepo;
 
+	/** The monthly repo. */
+	@Inject
+	private MonthlyAttendanceItemRepository monthlyRepo;
+
+	/** The daily repo. */
+	@Inject
+	private DailyAttendanceItemRepository dailyRepo;
+
 	/**
 	 * Find.
 	 *
+	 * @param optionalItemNo the optional item no
 	 * @return the optional item dto
 	 */
 	public OptionalItemDto find(String optionalItemNo) {
 		OptionalItemDto dto = new OptionalItemDto();
-		OptionalItem dom = this.repository.find(AppContexts.user().companyId(), optionalItemNo).get();
-		dom.saveToMemento(dto);
-		dto.setFormulas(this.getFormulas(optionalItemNo));
+		String companyId = AppContexts.user().companyId();
+		OptionalItem optionalItem = this.repository.find(AppContexts.user().companyId(), optionalItemNo).get();
+		optionalItem.saveToMemento(dto);
+		List<FormulaDto> listFormula = this.getFormulas(optionalItemNo);
+
+		// Get list attendance item
+		Map<Integer, String> attendanceItems;
+		if (optionalItem.getPerformanceAtr() == PerformanceAtr.DAILY_PERFORMANCE) {
+			attendanceItems = this.getDailyAttendanceItems(optionalItem.getOptionalItemAtr());
+		} else {
+			attendanceItems = this.getMonthlyAttendanceItems(optionalItem.getOptionalItemAtr());
+		}
+
+		// Get list formula order.
+		Map<FormulaId, Integer> orders = this.getFormulaOrders(companyId, optionalItem.getOptionalItemNo().v());
+
+		// Set order & attendance item name & operator text.
+		listFormula.forEach(item -> {
+			// Set order
+			item.setOrderNo(orders.get(new FormulaId(item.getFormulaId())));
+
+			// set attendance item name if calculationAtr == item selection.
+			if (item.getCalcAtr() == CalculationAtr.ITEM_SELECTION.value) {
+
+				item.getItemSelection().getAttendanceItems().forEach(attendanceItem -> {
+					String attendanceName = attendanceItems.get(attendanceItem.getAttendanceItemId());
+					String operatorText = OperatorAtr.valueOf(attendanceItem.getOperator()).description;
+					attendanceItem.setAttendanceItemName(attendanceName);
+					attendanceItem.setOperatorText(operatorText);
+				});
+
+			}
+		});
+
+		// Set list formula.
+		dto.setFormulas(listFormula);
 		return dto;
+	}
+
+	/**
+	 * Gets the formula orders.
+	 *
+	 * @param companyId the company id
+	 * @param itemNo the item no
+	 * @return the formula orders
+	 */
+	private Map<FormulaId, Integer> getFormulaOrders(String companyId, String itemNo) {
+		return this.orderRepo.findByOptItemNo(companyId, itemNo).stream()
+				.collect(Collectors.toMap(FormulaDispOrder::getOptionalItemFormulaId, dod -> dod.getDispOrder().v()));
+	}
+
+	/**
+	 * Gets the monthly attendance items.
+	 *
+	 * @param attribute the attribute
+	 * @return the monthly attendance items
+	 */
+	private Map<Integer, String> getMonthlyAttendanceItems(OptionalItemAtr attribute) {
+		MonthlyAttendanceItemAtr converted = MonthlyAttendanceItemAtr.NUMBER;
+		switch (attribute) {
+		case NUMBER:
+			converted = MonthlyAttendanceItemAtr.NUMBER;
+			break;
+		case AMOUNT:
+			converted = MonthlyAttendanceItemAtr.AMOUNT;
+			break;
+		case TIME:
+			converted = MonthlyAttendanceItemAtr.TIME;
+			break;
+		default:
+			break;
+		}
+		return this.monthlyRepo.findByAtr(AppContexts.user().companyId(), converted.value).stream().collect(
+				Collectors.toMap(MonthlyAttendanceItem::getAttendanceItemId, item -> item.getAttendanceName().v()));
+	}
+
+	/**
+	 * Gets the daily attendance items.
+	 *
+	 * @param attribute the attribute
+	 * @return the daily attendance items
+	 */
+	private Map<Integer, String> getDailyAttendanceItems(OptionalItemAtr attribute) {
+		DailyAttendanceAtr converted = DailyAttendanceAtr.NumberOfTime;
+		switch (attribute) {
+		case NUMBER:
+			converted = DailyAttendanceAtr.NumberOfTime;
+			break;
+		case AMOUNT:
+			converted = DailyAttendanceAtr.AmountOfMoney;
+			break;
+		case TIME:
+			converted = DailyAttendanceAtr.Time;
+			break;
+		default:
+			break;
+		}
+		return this.dailyRepo.findByAtr(AppContexts.user().companyId(), converted.value).stream().collect(
+				Collectors.toMap(DailyAttendanceItem::getAttendanceItemId, item -> item.getAttendanceName().v()));
 	}
 
 	/**
@@ -65,10 +178,6 @@ public class OptionalItemFinder {
 		// Get list formula
 		List<Formula> list = this.formulaRepo.findByOptItemNo(comId, itemNo);
 
-		// Get list formula order.
-		Map<FormulaId, Integer> orders = this.orderRepo.findByOptItemNo(comId, itemNo).stream()
-				.collect(Collectors.toMap(FormulaDispOrder::getOptionalItemFormulaId, dod -> dod.getDispOrder().v()));
-
 		// Convert to dto.
 		List<FormulaDto> listDto = list.stream().map(item -> {
 			FormulaDto dto = new FormulaDto();
@@ -76,29 +185,8 @@ public class OptionalItemFinder {
 			return dto;
 		}).collect(Collectors.toList());
 
-		// Get list daily attendance item
-		//List<MonthlyAttendanceItem> monthly = this.monthlyRepo.findByAtr(comId, item.getFormulaAtr());
-		//List<DailyAttendanceItem> daily = this.dailyRepo
-
-		listDto.forEach(item -> {
-			// Set order
-			item.setOrderNo(orders.get(new FormulaId(item.getFormulaId())));
-
-			// set attendance item name if calculationAtr == item selection.
-			if (item.getCalcAtr() == CalculationAtr.ITEM_SELECTION.value) {
-				
-				item.getItemSelection().getAttendanceItems().forEach(attendanceItem -> {
-					attendanceItem.getAttendanceItemId(); //TODO set name & operator
-					attendanceItem.getOperator();
-					attendanceItem.setAttendanceItemName("name");
-					attendanceItem.setOperatorText("+");
-				});
-				
-			}
-		});
-
 		return listDto;
-		
+
 	}
 
 	/**
@@ -113,7 +201,6 @@ public class OptionalItemFinder {
 			item.saveToMemento(dto);
 			return dto;
 		}).collect(Collectors.toList());
-
 
 		return listDto;
 	}
---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.infra/src/main/java/nts/uk/ctx/at/record/infra/repository/monthlyattendanceitem/JpaMonthlyAttendanceItemRepository.java
@@ -0,0 +1,32 @@
+/******************************************************************
+ * Copyright (c) 2017 Nittsu System to present.                   *
+ * All right reserved.                                            *
+ *****************************************************************/
+package nts.uk.ctx.at.record.infra.repository.monthlyattendanceitem;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.ejb.Stateless;
+
+import nts.arc.layer.infra.data.JpaRepository;
+import nts.uk.ctx.at.record.dom.monthlyattendanceitem.MonthlyAttendanceItem;
+import nts.uk.ctx.at.record.dom.monthlyattendanceitem.MonthlyAttendanceItemRepository;
+
+/**
+ * The Class JpaMonthlyAttendanceItemRepository.
+ */
+@Stateless
+public class JpaMonthlyAttendanceItemRepository extends JpaRepository implements MonthlyAttendanceItemRepository {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see nts.uk.ctx.at.record.dom.monthlyattendanceitem.
+	 * MonthlyAttendanceItemRepository#findByAtr(java.lang.String, int)
+	 */
+	@Override
+	public List<MonthlyAttendanceItem> findByAtr(String companyId, int itemAtr) {
+		return Collections.emptyList();// TODO wait for entity
+	}
+}
