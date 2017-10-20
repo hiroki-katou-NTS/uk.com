2dc59c801a21055cb00f7a805aa1d12b6372601e
NSVC\anhvd <anh_vd@nittsusystem-vn.com>
2017-10-20 17:46:26 +0700

    Add source code UI KDW001 B, C, D.

---
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/001/d/kdw001.d.vm.ts
@@ -0,0 +1,11 @@
+module nts.uk.at.view.kdw001.d {
+    import getText = nts.uk.resource.getText;
+
+    export module viewmodel {
+        export class ScreenModel {
+            constructor() {
+                
+            }
+        }
+    }
+}
