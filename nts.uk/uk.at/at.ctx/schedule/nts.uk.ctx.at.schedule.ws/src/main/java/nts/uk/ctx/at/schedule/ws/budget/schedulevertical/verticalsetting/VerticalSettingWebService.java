0f76e190b6ecae76efd002e8ce7ca437e55dc4c1
TANLV\Tu Than Mat Xanh <tan_lv@nittsusystem-vn.com>
2017-10-26 17:16:32 +0700

    KML002 - C: Add code server

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.ws/src/main/java/nts/uk/ctx/at/schedule/ws/budget/schedulevertical/verticalsetting/VerticalSettingWebService.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.ws/src/main/java/nts/uk/ctx/at/schedule/ws/budget/schedulevertical/verticalsetting/VerticalSettingWebService.java
@@ -11,6 +11,7 @@ import javax.ws.rs.Produces;
 import nts.uk.ctx.at.schedule.app.command.budget.schedulevertical.verticalsetting.DeleteVerticalSettingCommandHandler;
 import nts.uk.ctx.at.schedule.app.command.budget.schedulevertical.verticalsetting.VerticalSettingCommand;
 import nts.uk.ctx.at.schedule.app.command.budget.schedulevertical.verticalsetting.VerticalSettingCommandHandler;
+import nts.uk.ctx.at.schedule.app.find.budget.schedulevertical.verticalsetting.DailyItemsDto;
 import nts.uk.ctx.at.schedule.app.find.budget.schedulevertical.verticalsetting.VerticalSettingDto;
 import nts.uk.ctx.at.schedule.app.find.budget.schedulevertical.verticalsetting.VerticalSettingFinder;
 
@@ -44,6 +45,13 @@ public class VerticalSettingWebService {
 	@POST
 	public VerticalSettingDto getVerticalCalSetByCode(@PathParam("verticalCalCd") String verticalCalCd) {
 		return verticalSettingFinder.getVerticalCalSetByCode(verticalCalCd);
+	}
+	
+	/** Find Daily Items by attribute. */
+	@Path("getDailyItems/{attribute}")
+	@POST
+	public List<DailyItemsDto> getDailyItems(@PathParam("attribute") int attribute) {
+		return verticalSettingFinder.getDailyItems(attribute);
 	}
 	
 	/** Add new vertical cal set. */
