e1ab0f91c22ba8267dc015e22eb42c54fc128703
m_kitahira <m_kitahira@nittsusystem.co.jp>
2017-10-26 12:58:03 +0900

    fix error of test

--- nts.uk/uk.com/shr/nts.uk.shr.com/src/test/java/nts/uk/shr/infra/i18n/resource/I18NResourceContentProcessorTest.java
+++ nts.uk/uk.com/shr/nts.uk.shr.com/src/test/java/nts/uk/shr/infra/i18n/resource/I18NResourceContentProcessorTest.java
@@ -8,7 +8,6 @@ import java.util.Arrays;
 import org.junit.Test;
 
 import lombok.val;
-import nts.arc.i18n.Locale;
 
 public class I18NResourceContentProcessorTest {
 
@@ -16,7 +15,7 @@ public class I18NResourceContentProcessorTest {
 	public void noNestNoParam() {
 		val target = new I18NResourceContentProcessor(id -> localize(id));
 		
-		String result = target.process(Locale.JA, "TEXT1", Arrays.asList());
+		String result = target.process("ja", "TEXT1", Arrays.asList());
 		assertThat(result, is("TEXT1"));
 	}
 
@@ -24,7 +23,7 @@ public class I18NResourceContentProcessorTest {
 	public void nestedNoParam() {
 		val target = new I18NResourceContentProcessor(id -> localize(id));
 		
-		String result = target.process(Locale.JA, "TEXT2 has {#T1}!!", Arrays.asList());
+		String result = target.process("ja", "TEXT2 has {#T1}!!", Arrays.asList());
 		assertThat(result, is("TEXT2 has TEXT1!!"));
 	}
 
@@ -32,7 +31,7 @@ public class I18NResourceContentProcessorTest {
 	public void nestedNoParam2() {
 		val target = new I18NResourceContentProcessor(id -> localize(id));
 		
-		String result = target.process(Locale.JA, "<{#T2}>", Arrays.asList());
+		String result = target.process("ja", "<{#T2}>", Arrays.asList());
 		assertThat(result, is("<TEXT2 has TEXT1!!>"));
 	}
 
@@ -40,7 +39,7 @@ public class I18NResourceContentProcessorTest {
 	public void param2() {
 		val target = new I18NResourceContentProcessor(id -> localize(id));
 		
-		String result = target.process(Locale.JA, "test {0} - {1}", Arrays.asList("ABC", "hello"));
+		String result = target.process("ja", "test {0} - {1}", Arrays.asList("ABC", "hello"));
 		assertThat(result, is("test ABC - hello"));
 	}
 	
@@ -48,7 +47,7 @@ public class I18NResourceContentProcessorTest {
 	public void param3_resourced_param() {
 		val target = new I18NResourceContentProcessor(id -> localize(id));
 		
-		String result = target.process(Locale.JA, "test {2} - {0} - {1}", Arrays.asList("ABC", "<{#T1}>", "にほんご"));
+		String result = target.process("ja", "test {2} - {0} - {1}", Arrays.asList("ABC", "<{#T1}>", "にほんご"));
 		assertThat(result, is("test にほんご - ABC - <TEXT1>"));
 	}
 	
