9062f67e082ae4b66ce1e097bf18359e42cf8ccd
Vu Thang Loi <loi_vt@nittsusystem-vn.com>
2017-10-27 15:16:17 +0700

    update entity, getovertime

---
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/overtime/service/OvertimeServiceImpl.java
@@ -0,0 +1,16 @@
+package nts.uk.ctx.at.request.dom.application.overtime.service;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class OvertimeServiceImpl implements OvertimeService {
+
+	@Override
+	public int checkOvertime(String url) {
+		
+		return 0;
+	}
+
+	
+
+}
