a3c7c0516a2adab0b8643fc9532e7da5d71ef0d8
TANLV\Tu Than Mat Xanh <tan_lv@nittsusystem-vn.com>
2017-10-27 10:23:57 +0700

    KML002 - C: Update server code

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.app/src/main/java/nts/uk/ctx/at/schedule/app/find/budget/schedulevertical/verticalsetting/VerticalSettingFinder.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.app/src/main/java/nts/uk/ctx/at/schedule/app/find/budget/schedulevertical/verticalsetting/VerticalSettingFinder.java
@@ -8,7 +8,9 @@ import java.util.stream.Collectors;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+import nts.uk.ctx.at.schedule.app.find.budget.external.ExternalBudgetDto;
 import nts.uk.ctx.at.schedule.app.find.scheduleitemmanagement.ScheduleItemDto;
+import nts.uk.ctx.at.schedule.dom.budget.external.ExternalBudgetRepository;
 import nts.uk.ctx.at.schedule.dom.budget.schedulevertical.verticalsetting.VerticalCalSet;
 import nts.uk.ctx.at.schedule.dom.budget.schedulevertical.verticalsetting.VerticalSettingRepository;
 import nts.uk.ctx.at.schedule.dom.scheduleitemmanagement.ScheduleItemManagementRepository;
@@ -25,8 +27,14 @@ public class VerticalSettingFinder {
 	@Inject
 	private VerticalSettingRepository repository;
 	
+	//@Inject
+	//private DailyAttendanceItemRepository dailyAttItemRepository;
+	
 	@Inject
 	private ScheduleItemManagementRepository scheduleRepository;
+	
+	@Inject
+	private ExternalBudgetRepository externalBudgerRepository;
 	
 	/**
 	 * Find by company id.
@@ -64,38 +72,71 @@ public class VerticalSettingFinder {
 	 *
 	 * @return the list
 	 */
-	public List<DailyItemsDto> getDailyItems(int attribute) {
+	public List<DailyItemsDto> getDailyItems(int attribute, int budgetAtr, int unitAtr) {
 		// user contexts
 		String companyId = AppContexts.user().companyId();
 		
 		List<DailyItemsDto> dailyItemsDtos = new ArrayList<>();
 
+		// Get daily data
+		DailyItemsDto dailyItem = new DailyItemsDto();
+		dailyItem.setCompanyId(companyId);
+		dailyItem.setId(0);
+		dailyItem.setItemId("001");
+		dailyItem.setItemName("Daily 001");
+		dailyItem.setItemType(ItemTypes.Daily.value);
+		dailyItem.setDispOrder(0);
+		
+		dailyItemsDtos.add(dailyItem);
+		
 		// Get schedule data
 		List<ScheduleItemDto> scheduleItems = this.scheduleRepository.findAllScheduleItemByAtr(companyId, attribute).stream().map(c -> ScheduleItemDto.fromDomain(c))
 				.collect(Collectors.toList());
-		
-		// Sort schedule items by display order
-		
-		
-		// Add schedule items to result list
+
 		for(int i = 0; i < scheduleItems.size(); i++) {
-			DailyItemsDto dailyItem = new DailyItemsDto();
-			dailyItem.setCompanyId(companyId);
-			dailyItem.setId(i);
-			dailyItem.setItemId(scheduleItems.get(i).getScheduleItemId());
-			dailyItem.setItemName(scheduleItems.get(i).getScheduleItemName());
-			dailyItem.setItemType(0);
-			dailyItem.setDispOrder(scheduleItems.get(i).getDispOrder());
+			DailyItemsDto dailyItem2 = new DailyItemsDto();
+			int index = i == 0 ? 1 : i;
 			
-			dailyItemsDtos.add(dailyItem);
+			dailyItem2.setCompanyId(companyId);
+			dailyItem2.setId(dailyItemsDtos.size() + index);
+			dailyItem2.setItemId(scheduleItems.get(i).getScheduleItemId());
+			dailyItem2.setItemName(scheduleItems.get(i).getScheduleItemName());
+			dailyItem2.setItemType(ItemTypes.Schedule.value);
+			dailyItem2.setDispOrder(scheduleItems.get(i).getDispOrder());
+			
+			dailyItemsDtos.add(dailyItem2);
 		}
 		
-		// Get budget data
-		
-		
 		// Get external data
-		
+		List<ExternalBudgetDto> externalItems = this.externalBudgerRepository.findByAtr(companyId, budgetAtr, unitAtr).stream().map(c -> ExternalBudgetDto.fromDomain(c))
+				.collect(Collectors.toList());
 				
+		for(int i = 0; i < scheduleItems.size(); i++) {
+			DailyItemsDto dailyItem3 = new DailyItemsDto();
+			int index = i == 0 ? 1 : i;
+			
+			dailyItem3.setCompanyId(companyId);
+			dailyItem3.setId(dailyItemsDtos.size() + index);
+			dailyItem3.setItemId(externalItems.get(i).getExternalBudgetCode());
+			dailyItem3.setItemName(externalItems.get(i).getExternalBudgetName());
+			dailyItem3.setItemType(ItemTypes.External.value);
+			dailyItem3.setDispOrder(Integer.parseInt(externalItems.get(i).getExternalBudgetCode()));
+			
+			dailyItemsDtos.add(dailyItem3);
+		}
+		
 		return dailyItemsDtos;
+	}
+}
+
+enum ItemTypes {
+	Daily(0),
+	Schedule(1),	
+	External(2);
+	
+	int value;  
+	
+	ItemTypes(int value){  
+		this.value=value;  
 	}
 }
