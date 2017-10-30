f20e0b6c8a7ef812bfb0b7964d21f57484d12dc7
Nguyen Van Tuan <tuannv@nsvc.com>
2017-10-30 16:48:51 +0700

    update code UI cps017, B, C

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/017/a/cps017.a.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/017/a/cps017.a.vm.ts
@@ -76,8 +76,8 @@ module nts.uk.com.view.cps017.a.viewmodel {
                     //                    }                    if (itemList && itemList.length) {
                         itemList.forEach(x => self.listSelection.push(x));
                         self.selection().selectionID(self.listSelection()[0].selectionID);
-                    }else{
-                        self.registerData();    
+                    } else {
+                        self.registerData();
                     }
 
                 });
@@ -85,6 +85,7 @@ module nts.uk.com.view.cps017.a.viewmodel {
 
             // sub theo selectionID: 
             selection.selectionID.subscribe(x => {
+                //self.listSelection.removeAll();
                 if (x) {
                     let selectLists = _.find(self.listSelection(), (item) => {
                         return item.selectionID == x;
@@ -94,6 +95,7 @@ module nts.uk.com.view.cps017.a.viewmodel {
                     selection.externalCD(selectLists.externalCD);
                     selection.memoSelection(selectLists.memoSelection);
                 }
+                //nts.uk.ui.errors.clearAll();
                 self.checkCreate(false);
             });
 
@@ -164,12 +166,11 @@ module nts.uk.com.view.cps017.a.viewmodel {
 
         //新規モード
         add() {
-            //alert("insert!");
             let self = this,
                 currentItem: Selection = self.selection(),
                 listSelection: Array<Selection> = self.listSelection(),
                 _selectionCD = _.find(listSelection, x => x.selectionCD == currentItem.selectionCD());
-
+            //oldIndex = _.findIndex(listSelection, x => x.selectionID == currentItem.selectionID());
             currentItem.histId(self.historySelection().histId());
             command = ko.toJS(currentItem);
 
@@ -178,15 +179,16 @@ module nts.uk.com.view.cps017.a.viewmodel {
             } else {
                 service.saveDataSelection(command).done(function() {
                     self.listSelection.removeAll();
-
                     service.getAllOrderItemSelection(self.historySelection().histId()).done((itemList: Array<ISelection>) => {
                         if (itemList && itemList.length) {
                             itemList.forEach(x => self.listSelection.push(x));
                             self.selection().selectionID(self.listSelection()[0].selectionID);
                         }
+                        //let newItem = itemList[oldIndex];
+                        //currentItem.selectionID(newItem.selectionID);
                     });
+                    nts.uk.ui.dialog.alert({ messageId: "Msg_15" });
                     self.listSelection.valueHasMutated();
-                    //self.selection().selectionCD(selectionCD);
 
                 });
             }
@@ -199,23 +201,22 @@ module nts.uk.com.view.cps017.a.viewmodel {
                 currentItem: Selection = self.selection(),
                 listSelection: Array<Selection> = self.listSelection(),
                 _selectionCD = _.find(listSelection, x => x.selectionCD == currentItem.selectionCD());
-            //oldIndex = _.findIndex(listSelection, x => x.)
+            oldIndex = _.findIndex(listSelection, x => x.selectionID == currentItem.selectionID());
             currentItem.histId(self.historySelection().histId());
             command = ko.toJS(currentItem);
 
             service.updateDataSelection(command).done(function() {
                 self.listSelection.removeAll();
-                
                 service.getAllOrderItemSelection(self.historySelection().histId()).done((itemList: Array<ISelection>) => {
                     if (itemList && itemList.length) {
                         itemList.forEach(x => self.listSelection.push(x));
-                        self.selection().selectionID(self.listSelection()[0].selectionID);
+                        //self.selection().selectionID(self.listSelection()[0].selectionID);
                     }
+                    let newItem = itemList[oldIndex];
+                    currentItem.selectionID(newItem.selectionID);
                 });
-
+                nts.uk.ui.dialog.alert({ messageId: "Msg_15" });
                 self.listSelection.valueHasMutated();
-                //self.selection().selectionCD(selectionCD);
-
 
             });
         }
@@ -234,7 +235,7 @@ module nts.uk.com.view.cps017.a.viewmodel {
             lastIndex = items.length - 1;
 
             if (items.length > 0) {
-                confirm({ messageId: "Msg_551" }).ifYes(() => {
+                confirm({ messageId: "Msg_18" }).ifYes(() => {
                     service.removeDataSelection(command).done(function() {
                         self.listSelection.removeAll();
 
@@ -251,7 +252,6 @@ module nts.uk.com.view.cps017.a.viewmodel {
                         });
                         self.listItems.valueHasMutated();
                         nts.uk.ui.dialog.alert({ messageId: "Msg_16" });
-
                     });
 
                 }).ifNo(() => {
@@ -265,6 +265,19 @@ module nts.uk.com.view.cps017.a.viewmodel {
         }
 
         //ダイアログC画面
+        openDialogB() {
+            let self = this,
+                currentItem: Selection = self.selection();
+
+            //set histID 
+            setShared('selectedHisId', self.historySelection().histId());
+            block.invisible();
+            modal('/view/cps/017/b/index.xhtml', { title: '' }).onClosed(function(): any {
+                block.clear();
+            });
+        }
+        
+        //ダイアログC画面
         openDialogC() {
             let self = this,
                 obj = {
@@ -273,11 +286,8 @@ module nts.uk.com.view.cps017.a.viewmodel {
                 };
 
             setShared('historyInfo', obj);
-
             block.invisible();
-
             modal('/view/cps/017/c/index.xhtml', { title: '' }).onClosed(function(): any {
-
                 block.clear();
             });
         }
@@ -289,13 +299,9 @@ module nts.uk.com.view.cps017.a.viewmodel {
                     sel_id: "0001",
                     sel_name: " Du DT"
                 };
-
             setShared('historyInfo', obj);
-
             block.invisible();
-
             modal('/view/cps/017/d/index.xhtml', { title: '' }).onClosed(function(): any {
-
                 block.clear();
             });
         }
@@ -354,7 +360,6 @@ module nts.uk.com.view.cps017.a.viewmodel {
         selectionName: string;
         externalCD: string;
         memoSelection: string;
-
         initSelection: number;
     }
     class Selection {
@@ -364,7 +369,6 @@ module nts.uk.com.view.cps017.a.viewmodel {
         selectionName: KnockoutObservable<string> = ko.observable('');
         externalCD: KnockoutObservable<string> = ko.observable('');
         memoSelection: KnockoutObservable<string> = ko.observable('');
-
         initSelection: KnockoutObservable<number> = ko.observable();
 
         constructor(param: ISelection) {
@@ -375,8 +379,6 @@ module nts.uk.com.view.cps017.a.viewmodel {
             self.selectionName(param.selectionName || '');
             self.externalCD(param.externalCD || '');
             self.memoSelection(param.memoSelection || '');
-
-            //self.initSelection(param.initSelection === 1 ? true : false);
             self.initSelection(param.initSelection || '');
 
         }
@@ -411,271 +413,3 @@ function makeIcon(value, row) {
         return "●";
     return '';
 }
-
-
-
-
-
-/*
-module nts.uk.com.view.cps017.a.viewmodel {
-    import error = nts.uk.ui.errors;
-    import text = nts.uk.resource.getText;
-    import close = nts.uk.ui.windows.close;
-    import dialog = nts.uk.ui.dialog;
-    import getShared = nts.uk.ui.windows.getShared;
-    import setShared = nts.uk.ui.windows.setShared;
-    import block = nts.uk.ui.block;
-    import modal = nts.uk.ui.windows.sub.modal;
-
-    export class ViewModel {
-        sel_item_lst: KnockoutObservableArray<SelectionItem>;
-
-        currentItemSel: KnockoutObservable<SelectionItem>;
-
-        item_sel_col: KnockoutObservableArray<NtsGridListColumn>;
-
-        comboItems = [new ItemModel('1', '基本給'),
-            new ItemModel('2', '役職手当'),
-            new ItemModel('3', '基本給2')];
-
-        comboColumns =
-        [{ prop: 'code', length: 4 },
-            { prop: 'name', length: 8 }];
-
-         items = _(new Array(10)).map((x, i) => new InitAndOrderSelection({
-            id: i,
-            sel_cd: "000" + i.toString(),
-            sel_name: "A" + i.toString(),
-            init_sel: true
-        })).value();
-
-        constructor() {
-            let self = this;
-
-            self.init();
-
-        }
-
-        init() {
-            let self = this;
-
-            self.sel_item_lst = ko.observableArray([{
-                new SelectionItem({
-                    sel_item_id: "0001",
-                    sel_item_name: "Success",
-                    history_lst: [{
-                        new HistorySelection({
-                            history_id: "0001",
-                            period: "2017/01/01 ~ 2017/09/29"
-                        })
-                    }]
-                })
-            }]);
-            self.currentItemSel = ko.observable(new SelectionItem({
-                sel_item_id: "0001",
-                sel_item_name: "Success",
-                history_lst: [{
-                    new HistorySelection({
-                        history_id: "0001",
-                        period: "2017/01/01 ~ 2017/09/29"
-                    })
-                }]
-            });
-            self.item_sel_col = ko.observableArray([
-                { headerText: 'id', key: 'sel_item_id', width: 100, hidden: true },
-                { headerText: text('CPS017_7'), key: 'sel_item_name', width: 200 }
-            ]);
-        }
-
-
-        //dialog C
-        openDialogC() {
-            let self = this,
-                obj = {
-                    sel_id: "0001",
-                    sel_name: " Du DT"
-                };
-
-            setShared('historyInfo', obj);
-
-            block.invisible();
-
-            modal('/view/cps/017/c/index.xhtml', { title: '' }).onClosed(function(): any {
-
-                block.clear();
-            });
-        }
-
-        //dialog D
-        openDialogD() {
-            let self = this,
-                obj = {
-                    sel_id: "0001",
-                    sel_name: " Du DT"
-                };
-
-            setShared('historyInfo', obj);
-
-            block.invisible();
-
-            modal('/view/cps/017/d/index.xhtml', { title: '' }).onClosed(function(): any {
-
-                block.clear();
-            });
-        }
-    }
-
-
-    export interface ISelectionItem {
-        sel_item_id: string;
-        sel_item_name: string;
-        history_lst: Array<HistorySelection> = [];
-    }
-
-    // list bên trái trên cùng
-    export class SelectionItem {
-        current_sel_item_id: KnockoutObservable<string> = ko.observable('');
-        sel_item_id: string = "0001";
-        sel_item_name: string = "Test1";
-
-
-        //list history của sel_item_id
-        history_lst: KnockoutObservableArray<HistorySelection> = ko.observableArray([{
-            new HistorySelection({
-                history_id: "0001",
-                period: "2017/01/01 ~ 2017/09/29"
-            })
-        }]);
-
-        //current id của history
-        currentId: KnockoutObservable<string> = ko.observable('');
-        constructor(params: ISelectionItem) {
-            let self = this;
-
-            self.sel_item_id = (params.sel_item_id || "");
-            self.sel_item_name = params.sel_item_name || "";
-            self.history_lst(params.history_lst);
-        }
-    }
-
-    // định nghĩa đối tượng history selection
-    export interface IHistorySelection {
-        history_id: string;
-        period: string;
-
-    }
-
-    export class HistorySelection {
-
-        history_id: string = "0001";
-        period: string = "2017/01/01 ~ 2017/09/29";
-        currentHistory_id: KnockoutObservable<string> = ko.observable('');
-
-        constructor(params: IHistorySelection) {
-            let self = this;
-            self.history_id = params.history_id || "";
-            self.period = params.period || "";
-        }
-
-    }
-
-    // định nghĩa list selection ở content giữa bao gồm code, name
-    export interface ISelection {
-        id: string;
-        sel_code: string;
-        sel_name: string;
-
-    }
-
-    export class Selection {
-        id: KnockoutObservableArray<string>;
-        sel_code: string;
-        sel_name: string;
-
-        constructor(params: ISelection) {
-            let self = this;
-
-            self.id(params.id || " ");
-            self.sel_code = params.sel_code;
-            self.sel_name = params.sel_name;
-
-        }
-
-    }
-
-    export class ItemModel {
-        code: string;
-        name: string;
-
-        constructor(code: string, name: string) {
-            this.code = code;
-            this.name = name;
-        }
-    }
-
-    export class GridItem {
-        id: number;
-        flag: boolean;
-        ruleCode: string;
-        combo: string;
-        text1: string;
-        constructor(index: number) {
-            this.id = index;
-            this.flag = index % 2 == 0;
-            this.ruleCode = String(index % 3 + 1);
-            this.combo = String(index % 3 + 1);
-            this.text1 = "TEXT";
-        }
-    }
-
-    export interface IInitAndOrderSelection {
-        id: number;
-        sel_cd: string;
-        sel_name: string;
-        init_sel: boolean;
-    }
-
-    export class InitAndOrderSelection {
-
-        id: number; // so thu tu hien thi trong grid
-        sel_cd: string;
-        sel_name: string;
-        init_sel: boolean;
-
-        constructor(params: IInitAndOrderSelection) {
-            let self = this;
-
-            self.id = params.id || 0;
-            self.sel_cd = params.sel_cd || " ";
-            self.sel_name = params.sel_name || " ";
-            self.init_sel = params.init_sel || false;
-        }
-
-    }
-
-    // interface SelectionOrder
-    export interface ISelectionOrder {
-        sel_id: string;
-        history_id: string;
-        orderLst: Array<InitAndOrderSelection> = [];
-    }
-
-    // class SelectionOrder
-    export class SelectionOrder {
-        sel_id: string;
-        history_id: string;
-        orderLst: Array<InitAndOrderSelection> = [];
-
-        constructor(params: ISelectionOrder) {
-            let self = this;
-
-            self.sel_id = params.sel_id || "";
-            self.history_id = params.history_id || "";
-            self.orderLst = params.orderLst || [];
-        }
-
-    }
-
-
-}
-*/
--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/017/b/cps017.b.start.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/017/b/cps017.b.start.ts
@@ -1,14 +1,45 @@
-
 module nts.uk.com.view.cps017.b {
-
-    let __viewContext: any = window["__viewContext"] || {};
-
     __viewContext.ready(function() {
+        __viewContext["viewModel"] = new viewmodel.ScreenModel();
 
-        var screenModel = new viewmodel.ViewModel();
-
-        __viewContext.bind(__viewContext["viewModel"]);
-
+        __viewContext["viewModel"].start().done(function() {
+            //InitIggrid();
+            __viewContext.bind(__viewContext["viewModel"]);
+        });
     });
 }
+function InitIggrid() {
+    /*
+    let __viewContext: any = window["__viewContext"] || {};
+    $("#item_register_grid").igGrid({
+        columns: [
+            { headerText: '', key: 'selectionID', width: 20, hidden: true, },
+            {
+                headerText: nts.uk.resource.getText('CPS017_18'), key: 'initSelection', width: 100,
+                template: "<input data-id='${selectionID}' type='checkbox' tabindex='1'/>"
+            },
+            { headerText: nts.uk.resource.getText('CPS017_16'), key: 'selectionCD', width: 120 },
+            { headerText: nts.uk.resource.getText('CPS017_17'), key: 'selectionName', width: 140 }
+        ],
+        primaryKey: 'selectionID',
+        autoGenerateColumns: false,
+        dataSource: __viewContext["viewModel"].listSelection(),
+        width: "600px",
+        height: "400px",
+        features: [],
 
+    });
+    $("#item_register_grid").closest('.ui-iggrid').addClass('nts-gridlist');
+    $(document).on('click', 'input[type="checkbox"]', function(evt) {
+        let id = $(evt.target).parents('tr').data('id'),
+            data: Array<any> = __viewContext["viewModel"].listSelection(),
+            item = _.find(data, x => x.selectionID == id);
+        if ($(evt.target).is(':checked')) {
+            item.selected = true;
+        }
+        else {
+            item.selected = false;
+        }
+    });
+   */
+}
