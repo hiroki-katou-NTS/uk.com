4e8c8f739369cc649bba80b6f0fd406d74f3fc24
nws-dientx <dientx@newwave.vn>
2017-10-30 17:34:15 +0700

    support get role from list role id

--- nts.uk/uk.com/com.ctx/sys/auth/nts.uk.ctx.sys.auth.infra/src/main/java/nts/uk/ctx/sys/auth/infra/repository/role/JpaRoleRepository.java
+++ nts.uk/uk.com/com.ctx/sys/auth/nts.uk.ctx.sys.auth.infra/src/main/java/nts/uk/ctx/sys/auth/infra/repository/role/JpaRoleRepository.java
@@ -51,4 +51,31 @@ public class JpaRoleRepository extends JpaRepository implements RoleRepository {
 		}).collect(Collectors.toList());
 	}
 
+	/* (non-Javadoc)
+	 * @see nts.uk.ctx.sys.auth.dom.role.RoleRepository#findByListRoleId(java.lang.String, java.util.List)
+	 */
+	@Override
+	public List<Role> findByListRoleId(String companyId, List<String> lstRoleId) {
+		EntityManager em = this.getEntityManager();
+		CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+
+		CriteriaQuery<SacmtRole> cq = criteriaBuilder.createQuery(SacmtRole.class);
+		Root<SacmtRole> root = cq.from(SacmtRole.class);
+
+		// select root
+		cq.select(root);
+
+		// add where
+		List<Predicate> predicateList = new ArrayList<>();
+
+		predicateList.add(root.get(SacmtRole_.id).in(lstRoleId));
+		predicateList.add(criteriaBuilder.equal(root.get(SacmtRole_.cid), companyId));
+		cq.where(predicateList.toArray(new Predicate[] {}));
+
+		List<SacmtRole> sacmtRoles = em.createQuery(cq).getResultList();
+		return sacmtRoles.stream().map(sacmtRole -> {
+			return new Role(new JpaRoleGetMemento(sacmtRole));
+		}).collect(Collectors.toList());
+	}
+
 }
