23b37622205fc5a62ec2d35d8ec009788b7bd72d
NSVN-PhamVanDan <dan_pv@nittsusystem-vn.com>
2017-11-01 09:48:37 +0700

    impl repo current address and widowhistory

---
+++ nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.infra/src/main/java/entity/person/currentaddress/CurrentAddRepoImpl.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package entity.person.currentaddress;
+
+import nts.arc.time.GeneralDate;
+import nts.uk.ctx.bs.person.dom.person.currentdddress.CurrentAddress;
+import nts.uk.ctx.bs.person.dom.person.currentdddress.CurrentAddressRepository;
+
+/**
+ * @author danpv
+ *
+ */
+public class CurrentAddRepoImpl implements CurrentAddressRepository{
+
+	/* (non-Javadoc)
+	 * @see nts.uk.ctx.bs.person.dom.person.currentdddress.CurrentAddressRepository#get(java.lang.String, nts.arc.time.GeneralDate)
+	 */
+	@Override
+	public CurrentAddress get(String personId, GeneralDate standandDate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.infra/src/main/java/entity/person/info/widowhistory/WidowHisRepoImpl.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package entity.person.info.widowhistory;
+
+import nts.uk.ctx.bs.person.dom.person.info.widowhistory.WidowHistory;
+import nts.uk.ctx.bs.person.dom.person.info.widowhistory.WidowHistoryRepository;
+
+/**
+ * @author danpv
+ *
+ */
+public class WidowHisRepoImpl implements WidowHistoryRepository{
+
+	/* (non-Javadoc)
+	 * @see nts.uk.ctx.bs.person.dom.person.info.widowhistory.WidowHistoryRepository#get()
+	 */
+	@Override
+	public WidowHistory get() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
