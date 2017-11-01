6a7ea6758abf593d458858c26aaa1bd171d811d4
NSVN-PhamVanDan <dan_pv@nittsusystem-vn.com>
2017-11-01 17:07:12 +0700

    LeaveHoliday implement repo

--- nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.dom/src/main/java/nts/uk/ctx/bs/employee/dom/leaveholiday/LeaveHoliday.java
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.dom/src/main/java/nts/uk/ctx/bs/employee/dom/leaveholiday/LeaveHoliday.java
@@ -1,10 +1,15 @@
 package nts.uk.ctx.bs.employee.dom.leaveholiday;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import nts.arc.layer.dom.AggregateRoot;
+import nts.arc.time.GeneralDate;
 import nts.uk.shr.com.time.calendar.period.DatePeriod;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class LeaveHoliday extends AggregateRoot {
 
 	/**
@@ -18,5 +23,14 @@ public class LeaveHoliday extends AggregateRoot {
 	private DatePeriod period;
 	/** LeaveHolidayState */
 	private LeaveHolidayState leaveHolidayState;
-	
+
+	public static LeaveHoliday createLeaveHoliday(String sid, String leaveHolidayId, GeneralDate start, GeneralDate end,
+			int leaveHolidayType, String familyMemberId, String reasonLeaveHoliday, boolean multiple,
+			GeneralDate birthday) {
+		DatePeriod period = new DatePeriod(start, end);
+		LeaveHolidayState leaveHolidayState = LeaveHolidayState.createLeaveHolidayState(leaveHolidayType,
+				familyMemberId, reasonLeaveHoliday, multiple, birthday);
+		return new LeaveHoliday(sid, leaveHolidayId, period, leaveHolidayState);
+	}
+
 }
--- nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.dom/src/main/java/nts/uk/ctx/bs/employee/dom/leaveholiday/MidweekClosure.java
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.dom/src/main/java/nts/uk/ctx/bs/employee/dom/leaveholiday/MidweekClosure.java
@@ -1,9 +1,13 @@
 package nts.uk.ctx.bs.employee.dom.leaveholiday;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import nts.arc.time.GeneralDate;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MidweekClosure {
 	/**
 	 * 産前休業
