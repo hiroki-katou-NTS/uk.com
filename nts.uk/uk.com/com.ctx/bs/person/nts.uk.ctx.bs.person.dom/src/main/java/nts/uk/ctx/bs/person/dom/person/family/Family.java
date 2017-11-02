7370a38c1b6d2a5b765ac96923ed6214367e7f08
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2017-11-02 14:41:20 +0700

    update Family  domain

--- nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.dom/src/main/java/nts/uk/ctx/bs/person/dom/person/family/Family.java
+++ nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.dom/src/main/java/nts/uk/ctx/bs/person/dom/person/family/Family.java
@@ -6,10 +6,10 @@ package nts.uk.ctx.bs.person.dom.person.family;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import nts.arc.enums.EnumAdaptor;
 import nts.arc.layer.dom.AggregateRoot;
 import nts.arc.time.GeneralDate;
 import nts.uk.ctx.bs.person.dom.person.info.CountryId;
-import nts.uk.ctx.bs.person.dom.person.info.fullnameset.FullNameSet;
 
 /**
  * @author danpv
@@ -18,48 +18,50 @@ import nts.uk.ctx.bs.person.dom.person.info.fullnameset.FullNameSet;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+// 家族
 public class Family extends AggregateRoot {
-
+	// 生年月日
 	private GeneralDate birthday;
-
+	// 死亡年月日
 	private GeneralDate deadDay;
-
+	// 入籍年月日
 	private GeneralDate entryDate;
-
+	// 除籍年月日
 	private GeneralDate expelledDate;
-
+	// 家族ID
 	private FamilyId familyId;
-
-	private FullNameSet name;
-
-	private FullNameSet nameMultiLang;
-
-	private FullNameSet nameRomaji;
-
-	private CountryId nationality;
-
+	// 氏名
+	private NameSetType nameType;
+	// 氏名他言語
+	private NameSetType nameMultiLangType;
+	// 氏名ローマ字
+	private NameSetType nameRomajiType;
+	// 国籍
+	private CountryId nationalityId;
+	// 職業
 	private String occupationName;
-
+	// 個人ID
 	private String personId;
+	// 続柄
+	private String relationshipId;
+	// 支援介護区分
+	private SupportCareType supportCareType;
+	// 同居別居区分
+	private TogSepDivisionType togSepDivisionType;
+	// 勤労学生
+	private WorkStudentType workStudentType;
 
-	private String relationship;
-
-	private int supportCareType;
-
-	private String notificationName;
-
-	private int togSepDivision;
-
-	private int workStudent;
-
-	public static Family createFromJavaType(GeneralDate birthday, GeneralDate deadDay, GeneralDate entryDate, GeneralDate expelledDate,
-			String familyId, String name, String nameKana, String nameMulti, String nameMultiKana, String nameRomaji,
-			String nameRomajiKana, String nationality, String occupationName, String personId, String relationship,
-			int supportCareType, String notificationName, int togSepDivision, int workStudent) {
+	public static Family createFromJavaType(GeneralDate birthday, GeneralDate deadDay, GeneralDate entryDate,
+			GeneralDate expelledDate, String familyId, int nameType, int nameMultiLangType, int nameRomajiType,
+			String nationalityId, String occupationName, String personId, String relationship, int supportCareType,
+			int togSepDivisionType, int workStudentType) {
 		return new Family(birthday, deadDay, entryDate, expelledDate, new FamilyId(familyId),
-				new FullNameSet(name, nameKana), new FullNameSet(nameMulti, nameMultiKana),
-				new FullNameSet(nameRomaji, nameRomajiKana), new CountryId(nationality), occupationName, personId,
-				relationship, supportCareType, notificationName, togSepDivision, workStudent);
+				EnumAdaptor.valueOf(nameType, NameSetType.class),
+				EnumAdaptor.valueOf(nameMultiLangType, NameSetType.class),
+				EnumAdaptor.valueOf(nameRomajiType, NameSetType.class), new CountryId(nationalityId), occupationName,
+				personId, relationship, EnumAdaptor.valueOf(supportCareType, SupportCareType.class),
+				EnumAdaptor.valueOf(togSepDivisionType, TogSepDivisionType.class),
+				EnumAdaptor.valueOf(workStudentType, WorkStudentType.class));
 
 	}
 }
