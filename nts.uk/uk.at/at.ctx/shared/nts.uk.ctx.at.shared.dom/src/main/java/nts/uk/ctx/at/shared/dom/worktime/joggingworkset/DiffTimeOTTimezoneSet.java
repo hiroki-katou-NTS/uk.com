1876409bf7b29086532315d89c91a78da862fc94
nws-dientx <dientx@newwave.vn>
2017-11-08 14:16:21 +0700

    kmk003 init domain diffTime

---
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/worktime/joggingworkset/DiffTimeOTTimezoneSet.java
@@ -0,0 +1,15 @@
+/******************************************************************
+ * Copyright (c) 2017 Nittsu System to present.                   *
+ * All right reserved.                                            *
+ *****************************************************************/
+package nts.uk.ctx.at.shared.dom.worktime.joggingworkset;
+
+/**
+ * The Class DiffTimeOTTimezoneSet.
+ */
+public class DiffTimeOTTimezoneSet {
+
+	/** The is update start time. */
+	// 開始時刻に合わせて時刻を変動させる
+	private boolean isUpdateStartTime;
+}
