14fc166f6eba8c33d9cd67600ce547488df0db8e
m_kitahira <m_kitahira@nittsusystem.co.jp>
2017-11-10 14:01:12 +0900

    common modules for person register

---
+++ nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/pereg/app/command/userdef/PeregUserDefAddCommand.java
@@ -0,0 +1,10 @@
+package nts.uk.shr.pereg.app.command.userdef;
+
+import nts.uk.shr.pereg.app.command.ItemsByCategory;
+
+public class PeregUserDefAddCommand extends PeregUserDefCommand {
+	
+	public PeregUserDefAddCommand(ItemsByCategory itemsByCategory) {
+		super(itemsByCategory);
+	}
+}
