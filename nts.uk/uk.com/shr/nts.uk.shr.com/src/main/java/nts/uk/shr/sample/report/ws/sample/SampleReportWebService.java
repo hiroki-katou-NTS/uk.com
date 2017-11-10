3c99fbdc19f9ae26cfb7b4c0d9358ffeeae16578
NSVN-NguyenDacManh <manh_nd@nittsusystem.co.jp>
2017-11-10 10:55:07 +0900

    - Add report header - Create sample bumon loop

--- nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/sample/report/ws/sample/SampleReportWebService.java
+++ nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/sample/report/ws/sample/SampleReportWebService.java
@@ -6,6 +6,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 import nts.arc.layer.app.file.export.ExportServiceResult;
+import nts.uk.shr.sample.aspose.SampleAsposeExportService;
 import nts.uk.shr.sample.report.app.export.sample.SampleReportExportService;
 import nts.uk.shr.sample.report.app.export.sample.SampleReportQuery;
 
@@ -16,11 +17,18 @@ public class SampleReportWebService {
 	@Inject
 	private SampleReportExportService exportService;
 	
+	@Inject
+	private SampleAsposeExportService asposeService;
+	
 	@POST
 	@Path("generate")
 	public ExportServiceResult generate(SampleReportQuery query) {
-		
 		return this.exportService.start(query);
 	}
 	
+	@POST
+	@Path("stripe")
+	public ExportServiceResult stripe() {
+		return this.asposeService.start(null);
+	}
 }
