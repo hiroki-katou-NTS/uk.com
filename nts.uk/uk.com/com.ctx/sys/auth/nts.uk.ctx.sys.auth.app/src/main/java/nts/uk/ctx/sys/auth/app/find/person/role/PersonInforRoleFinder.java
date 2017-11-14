b1a59a40b1aa8a591ec2f72ee82e35a66293835d
ThuongDangXuan <thuong_dx@nittsusystem-vn.com>
2017-11-14 11:25:11 +0700

    update PersonInforRoleFinder

--- nts.uk/uk.com/com.ctx/sys/auth/nts.uk.ctx.sys.auth.app/src/main/java/nts/uk/ctx/sys/auth/app/find/person/role/PersonInforRoleFinder.java
+++ nts.uk/uk.com/com.ctx/sys/auth/nts.uk.ctx.sys.auth.app/src/main/java/nts/uk/ctx/sys/auth/app/find/person/role/PersonInforRoleFinder.java
@@ -4,7 +4,8 @@ import java.util.Optional;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-
+import nts.uk.ctx.sys.auth.dom.role.PersonRole;
+import nts.uk.ctx.sys.auth.dom.role.PersonRoleRepository;
 import nts.uk.ctx.sys.auth.dom.role.Role;
 import nts.uk.ctx.sys.auth.dom.role.RoleRepository;
 import nts.uk.ctx.sys.auth.dom.role.RoleType;
@@ -14,11 +15,28 @@ import nts.uk.shr.com.context.AppContexts;
 public class PersonInforRoleFinder {
 	@Inject
 	private RoleRepository roleRepo;
-	
-	public PersonInfoRole find(){
+
+	@Inject
+	private PersonRoleRepository personRoleRepo;
+
+	public PersonInfoRole find() {
+		PersonInfoRole result = new PersonInfoRole();
 		String companyId = AppContexts.user().companyId();
-		Optional<Role> roleOPt= roleRepo.findByType(companyId, RoleType.PERSONAL_INFO);
-		
-		return null;		
+		// get domain role
+		Optional<Role> roleOpt = roleRepo.findByType(companyId, RoleType.PERSONAL_INFO);
+		if (roleOpt.isPresent()) {
+			Role role = roleOpt.get();
+			result.setRoleId(role.getRoleId());
+			result.setAssignAtr(role.getAssignAtr());
+			result.setEmployeeReferenceRange(role.getEmployeeReferenceRange());
+			result.setRoleType(role.getRoleType());
+			result.setRoleCode(role.getRoleCode().toString());
+			
+			// get domain PersonRole
+			Optional<PersonRole> personRoleOpt = personRoleRepo.find(role.getRoleId());
+			if (personRoleOpt.isPresent())
+				result.setReferFutureDate(personRoleOpt.get().getReferFutureDate());
+		}
+		return result;
 	}
 }
