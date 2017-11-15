de340147c6d1ac4e60b8e43212f99b321e1cc0ff
HopNT3SI <32725057+HopNT3SI@users.noreply.github.com>
2017-11-15 18:30:43 +0700

    subjobposition

---
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.app/src/main/java/nts/uk/ctx/bs/employee/app/command/position/jobposition/SubJobPositionCommand.java
@@ -0,0 +1,29 @@
+package nts.uk.ctx.bs.employee.app.command.position.jobposition;
+
+import lombok.Getter;
+import nts.arc.time.GeneralDate;
+import nts.uk.shr.pereg.app.PeregItem;
+
+@Getter
+public class SubJobPositionCommand {
+
+	
+	/**職務職位ID（兼務） sub job position id*/
+	private String subJobPosId;
+	
+	/**所属部門ID affiliation department id*/
+	@PeregItem("")
+	private String affiDeptId;
+	
+	/**役職ID job title id*/
+	@PeregItem("")
+	private String jobTitleId;
+	
+	/**Start date*/
+	@PeregItem("")
+	private GeneralDate startDate;
+	
+	/**End date*/
+	@PeregItem("")
+	private GeneralDate endDate;
+}
