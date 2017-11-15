7307b4577f354763a81b34dbca4d2419378d17de
ThuongDangXuan <thuong_dx@nittsusystem-vn.com>
2017-11-15 16:17:58 +0700

    update PersonInforRoleFinder

--- nts.uk/uk.com/com.ctx/sys/auth/nts.uk.ctx.sys.auth.app/src/main/java/nts/uk/ctx/sys/auth/app/find/person/role/PersonInforRoleFinder.java
+++ nts.uk/uk.com/com.ctx/sys/auth/nts.uk.ctx.sys.auth.app/src/main/java/nts/uk/ctx/sys/auth/app/find/person/role/PersonInforRoleFinder.java
@@ -1,9 +1,15 @@
 package nts.uk.ctx.sys.auth.app.find.person.role;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+
 import nts.uk.ctx.sys.auth.dom.role.PersonRole;
 import nts.uk.ctx.sys.auth.dom.role.PersonRoleRepository;
 import nts.uk.ctx.sys.auth.dom.role.Role;
@@ -19,24 +25,30 @@ public class PersonInforRoleFinder {
 	@Inject
 	private PersonRoleRepository personRoleRepo;
 
-	public PersonInfoRole find() {
-		PersonInfoRole result = new PersonInfoRole();
+	public List<PersonInfoRole> find() {
+		List<PersonInfoRole> result = new ArrayList<PersonInfoRole>();
+		
 		String companyId = AppContexts.user().companyId();
 		// get domain role
-		Optional<Role> roleOpt = roleRepo.findByType(companyId, RoleType.PERSONAL_INFO);
-		if (roleOpt.isPresent()) {
-			Role role = roleOpt.get();
-			result.setRoleId(role.getRoleId());
-			result.setAssignAtr(role.getAssignAtr());
-			result.setEmployeeReferenceRange(role.getEmployeeReferenceRange());
-			result.setRoleType(role.getRoleType());
-			result.setRoleCode(role.getRoleCode().toString());
-			
-			// get domain PersonRole
-			Optional<PersonRole> personRoleOpt = personRoleRepo.find(role.getRoleId());
-			if (personRoleOpt.isPresent())
-				result.setReferFutureDate(personRoleOpt.get().getReferFutureDate());
-		}
+		List<Role> roles = roleRepo.findByType(companyId, RoleType.PERSONAL_INFO);
+		if (roles !=null && !roles.isEmpty()) {
+			List<String> roleIds = roles.stream().map(x ->x.getRoleId()).collect(Collectors.toList());
+			Map<String, PersonRole> mapPerson = personRoleRepo.find(roleIds).stream().collect(Collectors.toMap(PersonRole::getRoleId,  Function.identity()));
+			PersonInfoRole personInfoRole = new PersonInfoRole();
+			roles.forEach(role ->{
+				personInfoRole.setRoleId(role.getRoleId());
+				personInfoRole.setAssignAtr(role.getAssignAtr());
+				personInfoRole.setEmployeeReferenceRange(role.getEmployeeReferenceRange());
+				personInfoRole.setRoleType(role.getRoleType());
+				personInfoRole.setRoleCode(role.getRoleCode().toString());
+				
+				// get domain PersonRole
+				Optional<PersonRole> personRoleOpt = personRoleRepo.find(role.getRoleId());
+				if (personRoleOpt.isPresent())
+					personInfoRole.setReferFutureDate(personRoleOpt.get().getReferFutureDate());
+				result.add(personInfoRole);
+			});
+		}	
 		return result;
 	}
 }
