66367fa7ecd7f71eb9806ba515427d6fdee92618
Hung Tran <32724898+1capedbaldy@users.noreply.github.com>
2017-11-15 16:07:40 +0700

    CAS014: update app

--- nts.uk/uk.com/com.ctx/sys/auth/nts.uk.ctx.sys.auth.app/src/main/java/nts/uk/ctx/sys/auth/app/find/grant/RoleSetGrantedJobTitleFinder.java
+++ nts.uk/uk.com/com.ctx/sys/auth/nts.uk.ctx.sys.auth.app/src/main/java/nts/uk/ctx/sys/auth/app/find/grant/RoleSetGrantedJobTitleFinder.java
@@ -8,6 +8,7 @@ import javax.inject.Inject;
 
 import org.apache.commons.lang3.StringUtils;
 
+import nts.arc.time.GeneralDate;
 import nts.uk.ctx.sys.auth.app.find.roleset.RoleSetDto;
 import nts.uk.ctx.sys.auth.dom.grant.RoleSetGrantedJobTitleRepository;
 import nts.uk.ctx.sys.auth.dom.roleset.RoleSetRepository;
@@ -27,18 +28,27 @@ public class RoleSetGrantedJobTitleFinder {
 	@Inject
 	private RoleSetGrantedJobTitleRepository roleSetJobRepo;
 
-	public GrantRoleSetJobDto getAllData(String refDate) {
+	public GrantRoleSetJobDto getAllData(GeneralDate refDate) {
 		String companyId = AppContexts.user().companyId();
 		if (StringUtils.isNoneEmpty(companyId))
 			return null;
 
 		// get Job Title by date, companyId
+		/*List<JobTitleInfo> jobs = this.jobTitleInfoRepository.findAll(companyId, baseDate);
+
+		return jobs.stream()
+				.map(job -> JobTitleItemDto.builder().id(job.getJobTitleId())
+				.code(job.getJobTitleCode().v()).name(job.getJobTitleName().v()).build())
+				.sorted((job1, job2) -> job1.getCode().compareTo(job2.getCode()))
+				.collect(Collectors.toList());
+		*/
 
 		// get Sequence Master
 
 		// get Role Set by companyId, sort ASC
 		List<RoleSetDto> listRoleSet = roleSetRepo.findByCompanyId(companyId).stream()
 				.map(item -> RoleSetDto.build(item)).collect(Collectors.toList());
+		listRoleSet.sort((rs1, rs2) -> rs1.getRoleSetCd().compareTo(rs2.getRoleSetCd()));
 
 		// get Role Set Granted Job Title
 		RoleSetGrantedJobTitleDto roleSetJob = roleSetJobRepo.getOneByCompanyId(companyId)
