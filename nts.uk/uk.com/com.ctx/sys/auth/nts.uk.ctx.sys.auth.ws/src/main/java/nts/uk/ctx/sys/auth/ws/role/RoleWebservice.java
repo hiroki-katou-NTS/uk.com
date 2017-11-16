6cc924218cf4a04af18f20c5633a7053a0fc2c56
NSVN-TrieuKhacTu <tu_tk@nittsusystem-vn.com>
2017-11-16 11:43:12 +0700

    fix error webservice

--- nts.uk/uk.com/com.ctx/sys/auth/nts.uk.ctx.sys.auth.ws/src/main/java/nts/uk/ctx/sys/auth/ws/role/RoleWebservice.java
+++ nts.uk/uk.com/com.ctx/sys/auth/nts.uk.ctx.sys.auth.ws/src/main/java/nts/uk/ctx/sys/auth/ws/role/RoleWebservice.java
@@ -8,14 +8,14 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import nts.arc.layer.ws.WebService;
-import nts.uk.ctx.sys.auth.app.find.person.role.PersonInforRoleFinder;
+import nts.uk.ctx.sys.auth.app.find.person.role.PersonInformationRoleFinder;
 import nts.uk.ctx.sys.auth.app.find.person.role.dto.RoleDto;
 
 @Path("ctx/sys/auth/role")
 @Produces("application/json")
 public class RoleWebservice extends WebService {
 	@Inject
-	private PersonInforRoleFinder personInforRoleFinder;
+	private PersonInformationRoleFinder personInforRoleFinder;
 	
 	@POST
 	@Path("getlistrolebytype/{roleType}")
