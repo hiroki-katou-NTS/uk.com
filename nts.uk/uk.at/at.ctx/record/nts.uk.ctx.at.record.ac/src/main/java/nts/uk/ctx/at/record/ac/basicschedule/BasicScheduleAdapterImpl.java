d44772e670a84ac3a5d6682ff9d85461569f06c1
NSVC\nampt <nam_pt@nittsusystem-vn.com>
2017-11-16 09:36:29 +0700

    update KIF 001

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.ac/src/main/java/nts/uk/ctx/at/record/ac/basicschedule/BasicScheduleAdapterImpl.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.ac/src/main/java/nts/uk/ctx/at/record/ac/basicschedule/BasicScheduleAdapterImpl.java
@@ -6,14 +6,23 @@ import javax.ejb.Stateless;
 
 import nts.uk.ctx.at.record.dom.adapter.basicschedule.BasicScheduleAdapter;
 import nts.uk.ctx.at.record.dom.adapter.basicschedule.BasicScheduleSidDto;
+import nts.uk.ctx.at.record.dom.adapter.basicschedule.WorkScheduleSidDto;
 
 @Stateless
 public class BasicScheduleAdapterImpl implements BasicScheduleAdapter{
 
 	@Override
-	public List<BasicScheduleSidDto> findAll() {
+	public List<BasicScheduleSidDto> findAllBasicSchedule() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	@Override
+	public List<WorkScheduleSidDto> findAllWorkSchedule() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
 
 }
