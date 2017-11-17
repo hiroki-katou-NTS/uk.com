4a0c4ae15237f2c8f394adb79091f95ae50f4bb7
HopNT3SI <32725057+HopNT3SI@users.noreply.github.com>
2017-11-17 16:46:00 +0700

    [app] Update user def command

--- nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/pereg/app/command/userdef/PeregUserDefCommand.java
+++ nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/pereg/app/command/userdef/PeregUserDefCommand.java
@@ -15,6 +15,10 @@ public abstract class PeregUserDefCommand {
 	/** Record Id, but this is null when new record */
 	private final String recordId;
 	
+	private String employeeId;
+	
+	private String personId;
+	
 	/** input items */
 	private final List<ItemValue> items;
 	
@@ -25,6 +29,14 @@ public abstract class PeregUserDefCommand {
 	public PeregUserDefCommand(String categoryId, String recordId, List<ItemValue> items) {
 		this.categoryId = categoryId;
 		this.recordId = recordId;
+		this.items = items;
+	}
+	
+	public PeregUserDefCommand(String categoryId, String recordId, String employeeId, String personId,List<ItemValue> items) {
+		this.categoryId = categoryId;
+		this.recordId = recordId;
+		this.employeeId = employeeId;
+		this.personId = personId;
 		this.items = items;
 	}
 }
