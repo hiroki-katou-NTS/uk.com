687e9083732ae9688c8c1c3942ae39b2b8c82794
NSVN-TranThiHoa <hoa_tt@nittsusystem-vn.com>
2017-11-17 16:33:50 +0700

    CPS017-b: hien thi ui

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/017/b/cps017.b.service.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/017/b/cps017.b.service.ts
@@ -2,11 +2,16 @@ module nts.uk.com.view.cps017.b.service {
     import ajax = nts.uk.request.ajax;
     import format = nts.uk.text.format;
     var paths = {
-        getAllOrderSetting: "ctx/bs/person/info/setting/selection/OrderSetting/{0}"
+        getAllOrderSetting: "ctx/bs/person/info/setting/selection/OrderSetting/{0}",
+        updateDataSelection: "ctx/bs/person/info/setting/selection/updateSelection",
     }
-
+    //get all
     export function getAllOrderSetting(histId: string) {
         let _path = format(paths.getAllOrderSetting, histId);
         return nts.uk.request.ajax("com", _path);
+    }
+    // update data Selection:
+    export function updateDataSelection(command) {
+        return ajax(paths.updateDataSelection, command);
     }
 }
--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/017/b/cps017.b.start.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/017/b/cps017.b.start.ts
@@ -3,43 +3,106 @@ module nts.uk.com.view.cps017.b {
         __viewContext["viewModel"] = new viewmodel.ScreenModel();
 
         __viewContext["viewModel"].start().done(function() {
-            //InitIggrid();
+            checkBox();
             __viewContext.bind(__viewContext["viewModel"]);
         });
     });
 }
-function InitIggrid() {
-    /*
-    let __viewContext: any = window["__viewContext"] || {};
-    $("#item_register_grid").igGrid({
-        columns: [
-            { headerText: '', key: 'selectionID', width: 20, hidden: true, },
-            {
-                headerText: nts.uk.resource.getText('CPS017_18'), key: 'initSelection', width: 100,
-                template: "<input data-id='${selectionID}' type='checkbox' tabindex='1'/>"
-            },
-            { headerText: nts.uk.resource.getText('CPS017_16'), key: 'selectionCD', width: 120 },
-            { headerText: nts.uk.resource.getText('CPS017_17'), key: 'selectionName', width: 140 }
-        ],
-        primaryKey: 'selectionID',
+function checkBox() {
+    var previousId = "";
+    var columnSettings = [
+        { headerText: '', key: 'selectionID', hidden: true },
+        { headerText: '', key: 'id', width: '20px;' },
+        {
+            headerText: nts.uk.resource.getText('CPS017_18'), key: 'initSelection',
+            template: "<input style='width:30px, height:40px' class='checkRow initSelection' type='checkbox'"
+            + " data-checked='${initSelection}' data-id='${id}' tabindex='4'/>"
+        },
+        { headerText: nts.uk.resource.getText('CPS017_16'), key: 'selectionCD', readonly: true },
+        { headerText: nts.uk.resource.getText('CPS017_17'), key: 'selectionName' }
+    ];
+    let source = ko.toJS(__viewContext["viewModel"].listSelection);
+    $('#item_register_grid2').igGrid({
+        dataSource: source,
+        primaryKey: "id",
+        dataSourceType: "json",
+        enableHoverStyles: false,
         autoGenerateColumns: false,
-        dataSource: __viewContext["viewModel"].listSelection(),
-        width: "600px",
-        height: "400px",
-        features: [],
+        columns: columnSettings,
+        width: "370px",
+        height: "270px",
+        features: [{
+            name: 'Selection',
+            mode: 'row',
+            activation: false,
+        },
+            {
+                name: "Updating",
+                columnSettings: [
+                    {
+                        columnKey : "id",
+                        readOnly: true,
+                        required: true,
+                        validation: true
+                    },
+                    {
+                        columnKey : "initSelection",
+                        readOnly: true,
+                        required: true,
+                        validation: true
+                    },
+                    {
+                        columnKey : "selectionCD",
+                        readOnly: true,
+                        required: true,
+                        validation: true
+                    },
+                    {
+                        columnKey : "selectionName",
+                        readOnly: true,
+                        required: true,
+                        validation: true
+                    }
+                ],
+                validation: false,
+                enableAddRow: false,
+                editMode: "cell",
+                excelNavigationMode: true,
+                enableDeleteRow: false
+            }
+        ],
+        dataRendered: function(evt, ui) {
+            $("#item_register_grid2").find("input[type=checkbox]").each(function() {
+                let $this = $(this);
+                $this.prop('checked', $this.data('checked'));
+            });
+        }
 
     });
-    $("#item_register_grid").closest('.ui-iggrid').addClass('nts-gridlist');
-    $(document).on('click', 'input[type="checkbox"]', function(evt) {
-        let id = $(evt.target).parents('tr').data('id'),
-            data: Array<any> = __viewContext["viewModel"].listSelection(),
-            item = _.find(data, x => x.selectionID == id);
-        if ($(evt.target).is(':checked')) {
-            item.selected = true;
-        }
-        else {
-            item.selected = false;
-        }
+    $("#item_register_grid2").closest('.ui-iggrid').addClass('nts-gridlist');
+    //su kien khi click vao o check box
+    $('#item_register_grid2').on("click", ".checkRow.initSelection", function(evt, ui) {
+        let _this = $(this),
+            data: Array<any> = ko.toJS(__viewContext["viewModel"].listSelection);
+        //chuyen toan bo nhung item da chon -> k chon
+        _.each(data, function(item) {
+            if (item.id != _this.parents('tr').data('id')) {
+                $('#item_register_grid2').igGridUpdating("setCellValue", item.id, "initSelection", false);
+                item.initSelection = false;
+            } else {
+                if (previousId === item.id) {
+                    previousId = "";
+                    item.initSelection = false;
+                } else {
+                    item.initSelection = true;
+                    previousId = item.id;
+                }
+            }
+        });
+        //bind lai data
+        let source = ko.toJS(data);
+        $('#item_register_grid2').igGrid("option", "dataSource", source);
+        $('#item_register_grid2').igGrid("dataBind");
     });
-   */
 }
+
