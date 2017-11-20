21f0a5c61432d50c6c4a2a6ea3d50183fbb2e5da
Duong Ngo <ngoducduonglucky@gmail.com>
2017-11-20 16:59:39 +0700

    refactor package export file ksu006

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.app/src/main/java/nts/uk/ctx/at/schedule/app/export/budget/external/actualresult/ExtBudgetErrorExportService.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.app/src/main/java/nts/uk/ctx/at/schedule/app/export/budget/external/actualresult/ExtBudgetErrorExportService.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2017 Nittsu System to present.                   *
  * All right reserved.                                            *
  *****************************************************************/
-package nts.uk.ctx.at.schedule.app.find.budget.external.actualresult.export;
+package nts.uk.ctx.at.schedule.app.export.budget.external.actualresult;
 
 import java.util.ArrayList;
 import java.util.Arrays;
