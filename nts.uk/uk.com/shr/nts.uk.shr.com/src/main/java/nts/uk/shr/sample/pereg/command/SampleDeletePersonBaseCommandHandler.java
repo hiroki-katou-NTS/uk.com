83162264cdacb7d23325358726b9bbee8967f72d
m_kitahira <m_kitahira@nittsusystem.co.jp>
2017-11-21 13:17:24 +0900

    fix miss @Stateless

--- nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/sample/pereg/command/SampleDeletePersonBaseCommandHandler.java
+++ nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/sample/pereg/command/SampleDeletePersonBaseCommandHandler.java
@@ -1,9 +1,12 @@
 package nts.uk.shr.sample.pereg.command;
 
+import javax.ejb.Stateless;
+
 import nts.arc.layer.app.command.CommandHandler;
 import nts.arc.layer.app.command.CommandHandlerContext;
 import nts.uk.shr.pereg.app.command.PeregDeleteCommandHandler;
 
+@Stateless
 public class SampleDeletePersonBaseCommandHandler extends CommandHandler<SampleDeletePersonBaseCommand>
 		implements PeregDeleteCommandHandler<SampleDeletePersonBaseCommand> {
 
