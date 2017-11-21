e9544052cd96b35c2b3294e9bc0190a986f98a97
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2017-11-21 13:57:12 +0700

    KAF005: add find/update function on detail screen

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.infra/src/main/java/nts/uk/ctx/at/request/infra/entity/application/overtime/KrqdtAppOvertime.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.infra/src/main/java/nts/uk/ctx/at/request/infra/entity/application/overtime/KrqdtAppOvertime.java
@@ -9,11 +9,13 @@ import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Version;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import nts.uk.ctx.at.request.dom.application.overtime.AppOverTime;
 import nts.uk.shr.infra.data.entity.UkJpaEntity;
 
 /**
@@ -31,6 +33,10 @@ public class KrqdtAppOvertime extends UkJpaEntity implements Serializable {
     private static final long serialVersionUID = 1L;
     @EmbeddedId
     protected KrqdtAppOvertimePK krqdtAppOvertimePK;
+    
+    @Version
+	@Column(name="EXCLUS_VER")
+	public Long version;
    
     @Column(name = "OVERTIME_ATR")
     private int overtimeAtr;
@@ -71,6 +77,40 @@ public class KrqdtAppOvertime extends UkJpaEntity implements Serializable {
 		return krqdtAppOvertimePK;
 	}
 
+	public KrqdtAppOvertime fromDomainValue(AppOverTime appOverTime){
+		this.version = appOverTime.getVersion();
+		this.setWorkTypeCode(appOverTime.getWorkTypeCode().v());
+		this.setSiftCode(appOverTime.getSiftCode().v());
+		this.setWorkClockFrom1(appOverTime.getWorkClockFrom1());
+		this.setWorkClockTo1(appOverTime.getWorkClockTo1());
+		this.setWorkClockFrom2(appOverTime.getWorkClockFrom2());
+		this.setWorkClockTo2(appOverTime.getWorkClockTo2());
+		// krqdtAppOvertime.setOvertimeInputs(krqdtAppOvertime.getOvertimeInputs().stream().);
+		this.setOvertimeAtr(appOverTime.getOverTimeAtr().value);
+		this.setOvertimeShiftNight(appOverTime.getOverTimeShiftNight());
+		this.setFlexExcessTime(appOverTime.getFlexExessTime());
+		this.setDivergenceReason(appOverTime.getDivergenceReason());
+		for(int i = 0; i<this.getOvertimeInputs().size(); i++){
+			this.getOvertimeInputs().get(i).fromDomainValue(appOverTime.getOverTimeInput().get(i));
+		}
+		return this;
+	}
+	
+	public AppOverTime toDomain(){
+		return new AppOverTime(
+				this.krqdtAppOvertimePK.getCid(), 
+				this.krqdtAppOvertimePK.getAppId(), 
+				this.getOvertimeAtr(), 
+				this.getWorkTypeCode(), 
+				this.getSiftCode(), 
+				this.getWorkClockFrom1(), 
+				this.getWorkClockTo1(), 
+				this.getWorkClockFrom2(), 
+				this.getWorkClockTo2(), 
+				this.getDivergenceReason(), 
+				this.getFlexExcessTime(), 
+				this.getOvertimeShiftNight());
+	}
     
 }
 
