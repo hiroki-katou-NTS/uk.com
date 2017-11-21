6f9d880dc6f615a15013de2e8723556bf617acec
haivh <haivh@newwave.vn>
2017-11-21 13:30:19 +0700

    add SystemResourceDto

--- nts.uk/uk.com/com.screen/nts.uk.screen.com.app/src/main/java/nts/uk/screen/com/app/command/systemresource/SystemResourceSaveCommand.java
+++ nts.uk/uk.com/com.screen/nts.uk.screen.com.app/src/main/java/nts/uk/screen/com/app/command/systemresource/SystemResourceSaveCommand.java
@@ -3,12 +3,12 @@ package nts.uk.screen.com.app.command.systemresource;
 import java.util.List;
 
 import lombok.Data;
-import nts.uk.screen.com.app.systemresource.dto.SystemResoureceDto;
+import nts.uk.screen.com.app.systemresource.dto.SystemResourceDto;
 
 @Data
 public class SystemResourceSaveCommand {
 	
-	private List<SystemResoureceDto> listData;
+	private List<SystemResourceDto> listData;
 	
 	/**
 	 * Instantiates a new mail server save command.
---
+++ nts.uk/uk.com/com.screen/nts.uk.screen.com.app/src/main/java/nts/uk/screen/com/app/systemresource/dto/SystemResourceDto.java
@@ -0,0 +1,17 @@
+package nts.uk.screen.com.app.systemresource.dto;
+
+import lombok.Data;
+
+@Data
+public class SystemResourceDto {
+	
+	private String resourceId;
+	
+	private String resourceContent;
+	
+	public SystemResourceDto() {
+		super();
+		
+	}
+
+}
