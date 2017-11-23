c0874193b91004c2cf4b9762b0790dc0598abfe2
NSVN-PhamXuanVinh <vinh_px@nittsusystem-vn.com>
2017-11-23 11:28:33 +0700

    cps 001 a pereg finder

--- nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.app/src/main/java/nts/uk/ctx/bs/employee/app/find/person/PersonLayoutDto.java
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.app/src/main/java/nts/uk/ctx/bs/employee/app/find/person/PersonLayoutDto.java
@@ -2,17 +2,21 @@ package nts.uk.ctx.bs.employee.app.find.person;
 
 import java.util.List;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import nts.uk.ctx.bs.employee.dom.regpersoninfo.personinfoadditemdata.item.EmpInfoItemData;
 import nts.uk.ctx.bs.person.dom.person.personinfoctgdata.item.PersonInfoItemData;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 public class PersonLayoutDto {
-	private PersonPeregDto dto;
-	private List<PersonInfoItemData> lstPersonInfoItemData;
+	public PersonPeregDto dto;
+	public List<PersonInfoItemData> perOptionalData;
+	public List<EmpInfoItemData> empOptionalData;
+	public PersonLayoutDto(PersonPeregDto dto, List<PersonInfoItemData> perOptionalData){
+		this.dto = dto;
+		this.perOptionalData = perOptionalData;
+	}
 }
