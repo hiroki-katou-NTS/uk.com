462f9838f1e7cea5a3db9cdfca05619b58dd1029
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2017-11-23 16:21:13 +0700

    add EmployeeInfoFinder in pereg

---
+++ nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.app/src/main/java/nts/uk/ctx/pereg/app/find/employee/EmpInfoDto.java
@@ -0,0 +1,16 @@
+package nts.uk.ctx.pereg.app.find.employee;
+
+import lombok.Getter;
+import lombok.Value;
+
+@Value
+@Getter
+public class EmpInfoDto {
+
+	String cardNo;
+
+	String EmployeeCode;
+
+	String LoginId;
+
+}
