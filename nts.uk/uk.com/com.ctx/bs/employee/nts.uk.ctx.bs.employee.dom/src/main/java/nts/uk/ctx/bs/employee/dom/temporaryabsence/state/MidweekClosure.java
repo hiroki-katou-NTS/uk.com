82f44648a4f55403d2c105fd114c9fc51497a409
NSVN-PhamVanDan <dan_pv@nittsusystem-vn.com>
2017-11-23 14:49:21 +0700

    update domain with int-functions

--- nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.dom/src/main/java/nts/uk/ctx/bs/employee/dom/temporaryabsence/state/MidweekClosure.java
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.dom/src/main/java/nts/uk/ctx/bs/employee/dom/temporaryabsence/state/MidweekClosure.java
@@ -6,34 +6,37 @@ package nts.uk.ctx.bs.employee.dom.temporaryabsence.state;
 import nts.uk.ctx.bs.employee.dom.temporaryabsence.TempAbsenceHisItem;
 
 /**
- * @author danpv
- * Domain Name : 産前休業
+ * @author danpv Domain Name : 産前休業
  *
  */
 public class MidweekClosure extends TempAbsenceHisItem {
 
 	/**
-	 * Type: Optional
-	 * 多胎妊娠区分 Multiple pregnancy segment
+	 * Type: Optional 多胎妊娠区分 Multiple pregnancy segment
 	 */
-	private Integer multiple;
-	
-	
-	/**
-	 * @param multiple
-	 */
-	public MidweekClosure(Integer multiple) {
+	private Boolean multiple;
+
+	public MidweekClosure() {
 		super();
+	}
+
+	private MidweekClosure(String historyId, String employeeId, GenericString remarks, Integer soInsPayCategory,
+			Boolean multiple) {
+		super(LeaveHolidayType.MIDWEEK_CLOSURE, historyId, employeeId, remarks, soInsPayCategory);
 		this.multiple = multiple;
 	}
 
-	public int getMultiple() {
+	public static MidweekClosure init(String historyId, String employeeId, String remarks,
+			Integer soInsPayCategory, Boolean multiple) {
+		return new MidweekClosure(historyId, employeeId, new GenericString(remarks), soInsPayCategory, multiple);
+	}
+
+	public Boolean getMultiple() {
 		return multiple;
 	}
 
-	public void setMultiple(Integer multiple) {
+	public void setMultiple(Boolean multiple) {
 		this.multiple = multiple;
 	}
-	
 
 }
