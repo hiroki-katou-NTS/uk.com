1ec304219fe6b4379935f836610c5fdd3a04a864
ddung\dotiendung <dung24391@gmail.com>
2017-11-23 15:19:52 +0700

    code dom cas003

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cas/003/a/cas003.a.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cas/003/a/cas003.a.vm.ts
@@ -5,17 +5,37 @@ module nts.uk.com.view.cas003.a {
         import infor = nts.uk.ui.dialog.info;
         import alert = nts.uk.ui.dialog.alert;
         export class ScreenModel {
-            PassPolicyCheck: KnockoutObservable<boolean>;
+            passPolicyCheck: KnockoutObservable<boolean>;
             firstTimeCheck: KnockoutObservable<boolean>;
             violationPassCheck: KnockoutObservable<boolean>;
             accLockCheck: KnockoutObservable<boolean>;
             simpleValue: KnockoutObservable<string>;
+            lowestDigits: KnockoutObservable<number>;
+            alphabetDigit: KnockoutObservable<number>;
+            numberOfDigits: KnockoutObservable<number>;
+            symbolCharacters: KnockoutObservable<number>;
+            historyCount: KnockoutObservable<number>;
+            validityPeriod: KnockoutObservable<number>;
+            notificationPasswordChange: KnockoutObservable<number>;
+            lockInterval: KnockoutObservable<number>;
+            errorCount: KnockoutObservable<number>;
+            lockOutMessage: KnockoutObservable<String>;
             
            
             constructor() {
                 var self = this;
                 self.simpleValue = ko.observable("");
-                self.PassPolicyCheck = ko.observable(true);
+                self.lowestDigits = ko.observable(null);
+                self.alphabetDigit = ko.observable(null);
+                self.numberOfDigits = ko.observable(null);
+                self.symbolCharacters = ko.observable(null);
+                self.historyCount = ko.observable(null);
+                self.validityPeriod = ko.observable(null);
+                self.notificationPasswordChange = ko.observable(null);
+                self.lockInterval = ko.observable(null);
+                self.errorCount = ko.observable(null);
+                self.lockOutMessage = ko.observable("");
+                self.passPolicyCheck = ko.observable(true);
                 self.violationPassCheck = ko.observable(true);
                 self.firstTimeCheck = ko.observable(true);
                 self.accLockCheck = ko.observable(true);
