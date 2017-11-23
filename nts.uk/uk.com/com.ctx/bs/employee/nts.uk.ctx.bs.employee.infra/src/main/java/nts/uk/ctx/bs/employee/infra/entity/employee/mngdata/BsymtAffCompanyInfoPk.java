ca8602fb407f82d9bb7d5b92ce2ffddb0f2d2690
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2017-11-23 11:23:40 +0700

    create domain & entity for employee history

---
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.infra/src/main/java/nts/uk/ctx/bs/employee/infra/entity/employee/mngdata/BsymtAffCompanyInfoPk.java
@@ -0,0 +1,16 @@
+package nts.uk.ctx.bs.employee.infra.entity.employee.mngdata;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+
+public class BsymtAffCompanyInfoPk implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Column(name = "SID")
+	public String sId;
+
+	@Column(name = "PID")
+	public String pId;
+
+}
