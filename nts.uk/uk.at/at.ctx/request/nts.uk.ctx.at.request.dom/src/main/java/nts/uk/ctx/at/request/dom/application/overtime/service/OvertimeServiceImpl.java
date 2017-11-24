2f5ce7cffe6a8018609e6d7d46d56b2f325a34bb
NSVC\dudt <du_dt@nittsusystem-vn.com>
2017-11-24 16:40:42 +0700

    KAF005: sua code

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/overtime/service/OvertimeServiceImpl.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/overtime/service/OvertimeServiceImpl.java
@@ -2,11 +2,14 @@ package nts.uk.ctx.at.request.dom.application.overtime.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import nts.arc.time.GeneralDate;
+import nts.gul.collection.CollectionUtil;
 import nts.uk.ctx.at.request.dom.application.Application;
 import nts.uk.ctx.at.request.dom.application.ApplicationRepository;
 import nts.uk.ctx.at.request.dom.application.UseAtr;
@@ -56,67 +59,75 @@ public class OvertimeServiceImpl implements OvertimeService {
 	public List<WorkTypeOvertime> getWorkType(String companyID, String employeeID,
 			RequestAppDetailSetting requestAppDetailSetting,List<AppEmploymentSetting> appEmploymentSettings) {
 		List<WorkTypeOvertime> result = new ArrayList<>();
-		List<WorkType> workTypes = new ArrayList<>();
-		if (requestAppDetailSetting != null) {
-			// 時刻計算利用チェック
-			if (requestAppDetailSetting.getTimeCalUseAtr().value == UseAtr.USE.value) {
-				// アルゴリズム「社員所属雇用履歴を取得」を実行する 
-				SEmpHistImport sEmpHistImport = employeeAdapter.getEmpHist(companyID, employeeID, GeneralDate.today());
-				if (sEmpHistImport != null) {
-					// ドメインモデル「申請別対象勤務種類」を取得
-					if (appEmploymentSettings != null && appEmploymentSettings.size() > 0) {
-						
-						List<String> workTypeCodes = new ArrayList<>();
-						List<AppEmployWorkType> employWorkTypes = appEmploymentSettings.get(0).getLstWorkType();
-						for(AppEmployWorkType appEmployWorkType : employWorkTypes){
-							workTypeCodes.add(appEmployWorkType.getWorkTypeCode());
-						}
-						workTypes = this.workTypeRepository.findNotDeprecatedByListCode(companyID, workTypeCodes);
-						result = convertWorkType(workTypes);
-						result.sort((a,b) -> a.getWorkTypeCode().compareTo(b.getWorkTypeCode()));
-						return result;
-					}
-				}
-					/*
-					 * ドメインモデル「勤務種類」を取得
-					 */
-				// １日の勤務＝以下に該当するもの
-				//　出勤、休出、振出、連続勤務
-				List<Integer> allDayAtrs = new ArrayList<>();
-				//出勤
-				allDayAtrs.add(0);
-				//休出
-				allDayAtrs.add(11);
-				//振出
-				allDayAtrs.add(7);
-				// 連続勤務
-				allDayAtrs.add(10);
-				// 午前 また 午後 in (休日, 振出, 年休, 出勤, 特別休暇, 欠勤, 代休, 時間消化休暇)
-				List<Integer> halfAtrs = new ArrayList<>();
-				// 休日
-				halfAtrs.add(1);
-				// 振出
-				halfAtrs.add(7);
-				// 年休
-				halfAtrs.add(8);
-				// 出勤
-				halfAtrs.add(0);
-				//特別休暇
-				halfAtrs.add(4);
-				// 欠勤
-				halfAtrs.add(5);
-				// 代休
-				halfAtrs.add(6);
-				//時間消化休暇
-				halfAtrs.add(9);
-				
-				workTypes = workTypeRepository.findWorkType(companyID, 0, allDayAtrs, halfAtrs);
-				result = convertWorkType(workTypes);
+		if(requestAppDetailSetting == null || requestAppDetailSetting.getTimeCalUseAtr() != UseAtr.USE) {
+			return result;
+		}
+		// 時刻計算利用チェック
+		// アルゴリズム「社員所属雇用履歴を取得」を実行する 
+		SEmpHistImport sEmpHistImport = employeeAdapter.getEmpHist(companyID, employeeID, GeneralDate.today());
+		
+		if (sEmpHistImport != null 
+				&& !CollectionUtil.isEmpty(appEmploymentSettings)) {
+			//ドメインモデル「申請別対象勤務種類」.勤務種類リストを表示する(hien thi list(申請別対象勤務種類))
+			List<AppEmployWorkType> lstEmploymentWorkType = appEmploymentSettings.get(0).getLstWorkType()
+					.stream()
+					.filter(x -> x.getEmploymentCode()== sEmpHistImport.getEmploymentCode()).collect(Collectors.toList());
+			if(CollectionUtil.isEmpty(lstEmploymentWorkType)) {
 				return result;
 			}
+			List<String> workTypeCodes = new ArrayList<>();
+			lstEmploymentWorkType.forEach(x -> {workTypeCodes.add(x.getWorkTypeCode());});			
+			result = this.workTypeRepository.findNotDeprecatedByListCode(companyID, workTypeCodes).stream()
+					.map(x -> new WorkTypeOvertime(x.getWorkTypeCode().v(), x.getName().v())).collect(Collectors.toList());
+			return result;
 		}
+		List<Integer> allDayAtrs = allDayAtrs();
+		List<Integer> halfAtrs = halfAtrs();
+		result = workTypeRepository.findWorkType(companyID, 0, allDayAtrs, halfAtrs).stream()
+				.map(x -> new WorkTypeOvertime(x.getWorkTypeCode().v(), x.getName().v())).collect(Collectors.toList());
+		return result;
+	}
+	/**
+	 * // １日の勤務＝以下に該当するもの
+	 * 　出勤、休出、振出、連続勤務
+	 * @return
+	 */
+	private List<Integer> allDayAtrs(){
 		
-		return null;
+		List<Integer> allDayAtrs = new ArrayList<>();
+		//出勤
+		allDayAtrs.add(0);
+		//休出
+		allDayAtrs.add(11);
+		//振出
+		allDayAtrs.add(7);
+		// 連続勤務
+		allDayAtrs.add(10);
+		return allDayAtrs;
+	}
+	/**
+	 * 午前 また 午後 in (休日, 振出, 年休, 出勤, 特別休暇, 欠勤, 代休, 時間消化休暇)
+	 * @return
+	 */
+	private List<Integer> halfAtrs(){
+		List<Integer> halfAtrs = new ArrayList<>();
+		// 休日
+		halfAtrs.add(1);
+		// 振出
+		halfAtrs.add(7);
+		// 年休
+		halfAtrs.add(8);
+		// 出勤
+		halfAtrs.add(0);
+		//特別休暇
+		halfAtrs.add(4);
+		// 欠勤
+		halfAtrs.add(5);
+		// 代休
+		halfAtrs.add(6);
+		//時間消化休暇
+		halfAtrs.add(9);
+		return halfAtrs;
 	}
 
 	@Override
@@ -154,14 +165,4 @@ public class OvertimeServiceImpl implements OvertimeService {
 		//Register overtime
 		overTimeRepository.Add(domain);
 	}
-	private List<WorkTypeOvertime> convertWorkType(List<WorkType> workTypes){
-		List<WorkTypeOvertime> workTypeOvertimes = new ArrayList<>();
-		for(WorkType workType : workTypes){
-			WorkTypeOvertime workTypeOvertime = new WorkTypeOvertime(workType.getWorkTypeCode().toString(),workType.getName().toString());
-			workTypeOvertimes.add(workTypeOvertime);
-		}
-		return workTypeOvertimes;
-	}
-	
-
 }
