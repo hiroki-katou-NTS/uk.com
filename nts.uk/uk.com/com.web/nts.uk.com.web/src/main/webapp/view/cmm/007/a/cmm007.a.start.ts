ef15914dad33ce3963c87424115c981b17095676
nws-haivh <haivh@newwave.vn>
2017-11-24 15:30:00 +0700

    coding service D screen CMM007

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmm/007/a/cmm007.a.start.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmm/007/a/cmm007.a.start.ts
@@ -1,15 +1,18 @@
 module nts.uk.com.view.cmm007.a {
     import viewModelTabB = nts.uk.com.view.cmm007.b.viewmodel;
     import viewModelTabC = nts.uk.com.view.cmm007.c.viewmodel;
+    import viewModelTabD = nts.uk.com.view.cmm007.d.viewmodel;
     
     __viewContext.ready(function() {
         let tabB = new viewModelTabB.ScreenModel();
         let tabC = new viewModelTabC.ScreenModel();
+        let tabD = new viewModelTabD.ScreenModel();
         
-        $.when(tabB.start_page(), tabC.start_page()).done(function(){
+        $.when(tabB.start_page(), tabC.start_page(), tabD.start_page()).done(function(){
             let screenModel =  {
                 systemDefine: tabB,
-                temporaryAbsenceFr: tabC
+                temporaryAbsenceFr: tabC,
+                planYearHolidayFr: tabD
             };
              __viewContext.bind(screenModel);
         }) 
