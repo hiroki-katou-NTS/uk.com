0fdd3a26ec3df6648805c660ded1610a26860a0b
HopNT3SI <32725057+HopNT3SI@users.noreply.github.com>
2017-11-24 18:18:27 +0700

    tempabsence

--- nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.dom/src/main/java/nts/uk/ctx/bs/employee/dom/temporaryabsence/TemporaryAbsenceHistRepository.java
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.dom/src/main/java/nts/uk/ctx/bs/employee/dom/temporaryabsence/TemporaryAbsenceHistRepository.java
@@ -1,7 +1,25 @@
 package nts.uk.ctx.bs.employee.dom.temporaryabsence;
 
+import java.util.Optional;
+
+import nts.uk.shr.com.history.DateHistoryItem;
+
 public interface TemporaryAbsenceHistRepository {
 
+	/**
+	 * Get TemporaryAbsenceHist by employeeId
+	 * @param employeeId
+	 * @param histId
+	 * @return
+	 */
+	Optional<TempAbsenceHistory> getTemporaryAbsenceHistByEmployeeId(String employeeId);
+	/**
+	 * Get TemporaryAbsenceHist by histId
+	 * @param employeeId
+	 * @param histId
+	 * @return
+	 */
+	Optional<TempAbsenceHistory> getTemporaryAbsenceHistByHistId(String employeeId, String histId);
 	/**
 	 * ドメインモデル「休職休業」を新規登録する
 	 * @param domain
@@ -11,12 +29,12 @@ public interface TemporaryAbsenceHistRepository {
 	 * 取得した「休職休業」を更新する
 	 * @param domain
 	 */
-	void updateTemporaryAbsenceHist(TempAbsenceHistory domain);
+	void updateTemporaryAbsenceHist(TempAbsenceHistory domain, DateHistoryItem item);
 	
 	/**
 	 * ドメインモデル「休職休業」を削除する
 	 * @param domain
 	 */
-	void deleteTemporaryAbsenceHist(String histId);
+	void deleteTemporaryAbsenceHist(TempAbsenceHistory domain, DateHistoryItem item);
 	
 }
