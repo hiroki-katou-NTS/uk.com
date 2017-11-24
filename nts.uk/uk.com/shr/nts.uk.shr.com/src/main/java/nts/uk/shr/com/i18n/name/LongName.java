08f663d5c1eaa0ce933e4b0075a45828571e2c70
NTS\tin_dh <tin_dh@nittsusystem.co.jp>
2017-11-24 17:17:12 +0900

    move primitive

--- nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/com/i18n/name/LongName.java
+++ nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/com/i18n/name/LongName.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2017 Nittsu System to present.                   *
  * All right reserved.                                            *
  *****************************************************************/
-package nts.uk.shr.com.primitive;
+package nts.uk.shr.com.i18n.name;
 
 import nts.arc.primitive.StringPrimitiveValue;
 import nts.arc.primitive.constraint.StringMaxLength;
