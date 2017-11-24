a0dc8d36ae1b80f121d069c024bc87f2f8ba4f00
NSVC\nampt <nam_pt@nittsusystem-vn.com>
2017-11-24 17:07:58 +0700

    update for Du's team

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.infra/src/main/java/nts/uk/ctx/at/record/infra/entity/workinformation/KrcdtWorkScheduleTime.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.infra/src/main/java/nts/uk/ctx/at/record/infra/entity/workinformation/KrcdtWorkScheduleTime.java
@@ -13,38 +13,36 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.NoArgsConstructor;
+import nts.arc.time.GeneralDate;
 import nts.uk.ctx.at.record.dom.workinformation.ScheduleTimeSheet;
 import nts.uk.shr.infra.data.entity.UkJpaEntity;
 
 /**
  * 
- * @author nampt
- * 日別実績の勤務情報.予定時間帯
+ * @author nampt 日別実績の勤務情報.予定時間帯
  *
  */
 @NoArgsConstructor
 @Entity
 @Table(name = "KRCDT_WORK_SCHEDULE_TIME")
 public class KrcdtWorkScheduleTime extends UkJpaEntity implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
 
 	@EmbeddedId
 	public KrcdtWorkScheduleTimePK krcdtWorkScheduleTimePK;
-	
+
 	@Column(name = "ATTENDANCE")
 	public Integer attendance;
 
 	@Column(name = "LEAVE_WORK")
 	public Integer leaveWork;
-	
+
 	@ManyToOne
-	@JoinColumns({
-	    @JoinColumn(name="SID", referencedColumnName="SID", insertable = false, updatable = false),
-	    @JoinColumn(name="YMD", referencedColumnName="YMD", insertable = false, updatable = false)
-	})
+	@JoinColumns({ @JoinColumn(name = "SID", referencedColumnName = "SID", insertable = false, updatable = false),
+			@JoinColumn(name = "YMD", referencedColumnName = "YMD", insertable = false, updatable = false) })
 	public KrcdtDaiPerWorkInfo daiPerWorkInfo;
-	
+
 	public KrcdtWorkScheduleTime(KrcdtWorkScheduleTimePK krcmtWorkScheduleTimePK, Integer attendance,
 			Integer leaveWork) {
 		super();
@@ -57,14 +55,22 @@ public class KrcdtWorkScheduleTime extends UkJpaEntity implements Serializable {
 	protected Object getKey() {
 		return this.krcdtWorkScheduleTimePK;
 	}
-	
+
 	public ScheduleTimeSheet toDomain() {
-		ScheduleTimeSheet domain = new ScheduleTimeSheet(this.krcdtWorkScheduleTimePK.workNo, this.attendance, this.leaveWork);
+		ScheduleTimeSheet domain = new ScheduleTimeSheet(this.krcdtWorkScheduleTimePK.workNo, this.attendance,
+				this.leaveWork);
 		return domain;
 	}
-	
+
 	public static List<ScheduleTimeSheet> toDomain(List<KrcdtWorkScheduleTime> entities) {
 		return entities.stream().map(c -> c.toDomain()).collect(Collectors.toList());
+	}
+
+	public static KrcdtWorkScheduleTime toEntity(String employeeId, GeneralDate ymd,
+			ScheduleTimeSheet scheduleTimeSheet) {
+		return new KrcdtWorkScheduleTime(
+				new KrcdtWorkScheduleTimePK(employeeId, ymd, scheduleTimeSheet.getWorkNo().v()),
+				scheduleTimeSheet.getAttendance().v(), scheduleTimeSheet.getLeaveWork().v());
 	}
 
 }
