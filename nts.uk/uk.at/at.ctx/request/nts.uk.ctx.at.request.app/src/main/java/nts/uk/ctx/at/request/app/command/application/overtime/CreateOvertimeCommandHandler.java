aed53f16d84e4448ec86b05edc7b0de41fe2f7ff
thangnq3si <thang.nq@3si.vn>
2017-11-24 16:15:25 +0700

    Updated: fix bug: setting KRQST_COM_APP_CF_DETAIL.TIME_CAL_USE_ATR = 0 -> register exception. -> input time field value null

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.app/src/main/java/nts/uk/ctx/at/request/app/command/application/overtime/CreateOvertimeCommandHandler.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.app/src/main/java/nts/uk/ctx/at/request/app/command/application/overtime/CreateOvertimeCommandHandler.java
@@ -54,7 +54,7 @@ public class CreateOvertimeCommandHandler extends CommandHandler<CreateOvertimeC
 		String appID = IdentifierUtil.randomUniqueId();
 
 		// Phase list
-		List<AppApprovalPhase> pharseList = getAppApprovalPhaseList(command, companyId, appID);
+		List<AppApprovalPhase> pharseList = CheckBeforeRegisterOvertime.getAppApprovalPhaseList(command, companyId, appID);
 		// Create Application
 		Application appRoot = factoryOvertime.buildApplication(appID, command.getApplicationDate(),
 				command.getPrePostAtr(), command.getApplicationReason(),
@@ -71,7 +71,7 @@ public class CreateOvertimeCommandHandler extends CommandHandler<CreateOvertimeC
 				workClockTo1, workClockFrom2, workClockTo2,
 				command.getDivergenceReasonContent().replaceFirst(":", System.lineSeparator()),
 				command.getFlexExessTime(), command.getOverTimeShiftNight(),
-				getOverTimeInput(command, companyId, appID));
+				CheckBeforeRegisterOvertime.getOverTimeInput(command, companyId, appID));
 
 		// 2-2.新規画面登録時承認反映情報の整理
 		registerService.newScreenRegisterAtApproveInfoReflect(appRoot.getApplicantSID(), appRoot);
@@ -82,77 +82,5 @@ public class CreateOvertimeCommandHandler extends CommandHandler<CreateOvertimeC
 		// 2-3.新規画面登録後の処理を実行
 		newAfterRegister.processAfterRegister(appRoot);
 
-	}
-
-	/**
-	 * Convert Phase command list to Approve Phase list
-	 * 
-	 * @param command
-	 *            : create command
-	 * @param companyId:
-	 *            会社ID
-	 * @param appID:
-	 *            申請ID
-	 * @return
-	 */
-	private List<AppApprovalPhase> getAppApprovalPhaseList(CreateOvertimeCommand command, String companyId,
-			String appID) {
-		return command.getAppApprovalPhaseCmds().stream()
-				.map(appApprovalPhaseCmd -> new AppApprovalPhase(companyId, appID, IdentifierUtil.randomUniqueId(),
-						EnumAdaptor.valueOf(appApprovalPhaseCmd.approvalForm, ApprovalForm.class),
-						appApprovalPhaseCmd.dispOrder,
-						EnumAdaptor.valueOf(appApprovalPhaseCmd.approvalATR, ApprovalAtr.class),
-						appApprovalPhaseCmd.getListFrame().stream()
-								.map(approvalFrame -> new ApprovalFrame(companyId,
-										IdentifierUtil.randomUniqueId(), approvalFrame.dispOrder,
-										approvalFrame.listApproveAccepted
-												.stream()
-												.map(approveAccepted -> ApproveAccepted.createFromJavaType(companyId,
-														IdentifierUtil.randomUniqueId(), approveAccepted.approverSID,
-														ApprovalAtr.UNAPPROVED.value, approveAccepted.confirmATR, null,
-														approveAccepted.reason, approveAccepted.representerSID))
-												.collect(Collectors.toList())))
-								.collect(Collectors.toList())))
-				.collect(Collectors.toList());
-	}
-
-	private List<OverTimeInput> getOverTimeInput(CreateOvertimeCommand command, String Cid, String appId) {
-		List<OverTimeInput> overTimeInputs = new ArrayList<OverTimeInput>();
-		/**
-		 * 休出時間 ATTENDANCE_ID = 0
-		 */
-		if (null != command.getBreakTimes()) {
-			overTimeInputs.addAll(getOverTimeInput(command.getBreakTimes(), Cid, appId, AttendanceAtr.Time.value));
-		}
-		/**
-		 * 残業時間 ATTENDANCE_ID = 1
-		 */
-		if (null != command.getOvertimeHours()) {
-			overTimeInputs
-					.addAll(getOverTimeInput(command.getOvertimeHours(), Cid, appId, AttendanceAtr.TimeOfDay.value));
-		}
-		/**
-		 * 加給時間 ATTENDANCE_ID = 2
-		 */
-		if (null != command.getRestTime()) {
-			overTimeInputs
-					.addAll(getOverTimeInput(command.getRestTime(), Cid, appId, AttendanceAtr.NumberOfTime.value));
-		}
-		/**
-		 * 加給時間 ATTENDANCE_ID = 3
-		 */
-		if (null != command.getBonusTimes()) {
-			overTimeInputs.addAll(getOverTimeInput(command.getBonusTimes(), Cid, appId, AttendanceAtr.Attribute.value));
-		}
-		return overTimeInputs;
-	}
-
-	private List<OverTimeInput> getOverTimeInput(List<OvertimeInputCommand> inputCommand, String Cid, String appId,
-			int attendanceId) {
-		return inputCommand.stream()
-				.filter(item -> item.getStartTime() != 0 || item.getEndTime() != 0 || item.getApplicationTime() != 0)
-				.map(item -> OverTimeInput.createSimpleFromJavaType(Cid, appId, attendanceId, item.getFrameNo(),
-						item.getStartTime(), item.getEndTime(), item.getApplicationTime(),item.getTimeItemTypeAtr()))
-				.collect(Collectors.toList());
 	}
 }
