186bb3a408be83f457c118cd19dfbff0b94dcd15
NSVN-PhamVanDan <dan_pv@nittsusystem-vn.com>
2017-11-27 09:42:03 +0700

    add AssgWorkPlace

---
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.app/src/main/java/nts/uk/ctx/bs/employee/app/find/assignedworkplace/AssgWorkPlaceFinder.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package nts.uk.ctx.bs.employee.app.find.assignedworkplace;
+
+import nts.uk.shr.pereg.app.find.PeregQuery;
+import nts.uk.shr.pereg.app.find.PeregSingleFinder;
+import nts.uk.shr.pereg.app.find.dto.PeregDto;
+
+/**
+ * @author danpv
+ *
+ */
+public class AssgWorkPlaceFinder implements PeregSingleFinder{
+
+	@Override
+	public String targetCategoryCode() {
+		return "CS00010";
+	}
+
+	@Override
+	public Class<?> dtoClass() {
+		return AssgWorkPlaceDto.class;
+	}
+
+	/* (non-Javadoc)
+	 * @see nts.uk.shr.pereg.app.find.PeregSingleFinder#getSingleData(nts.uk.shr.pereg.app.find.PeregQuery)
+	 */
+	@Override
+	public PeregDto getSingleData(PeregQuery query) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
