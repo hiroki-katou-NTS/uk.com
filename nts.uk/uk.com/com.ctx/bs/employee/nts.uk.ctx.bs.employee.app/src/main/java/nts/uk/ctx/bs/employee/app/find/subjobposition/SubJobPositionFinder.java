48a15c6b6e5c9ec988fbf55287fb2435bc2a88f0
NSVN-PhamVanDan <dan_pv@nittsusystem-vn.com>
2017-11-27 09:41:39 +0700

    add SubJobPositionFinder

---
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.app/src/main/java/nts/uk/ctx/bs/employee/app/find/subjobposition/SubJobPositionFinder.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package nts.uk.ctx.bs.employee.app.find.subjobposition;
+
+import nts.uk.shr.pereg.app.find.PeregQuery;
+import nts.uk.shr.pereg.app.find.PeregSingleFinder;
+import nts.uk.shr.pereg.app.find.dto.PeregDto;
+
+/**
+ * @author danpv
+ *
+ */
+public class SubJobPositionFinder implements PeregSingleFinder{
+
+	@Override
+	public String targetCategoryCode() {
+		return "CS00012";
+	}
+
+	@Override
+	public Class<?> dtoClass() {
+		return SubJobPositionDto.class;
+	}
+
+	/* (non-Javadoc)
+	 * @see nts.uk.shr.pereg.app.find.PeregSingleFinder#getSingleData(nts.uk.shr.pereg.app.find.PeregQuery)
+	 */
+	@Override
+	public PeregDto getSingleData(PeregQuery query) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
