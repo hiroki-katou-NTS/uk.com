e716c9a4f00ddd7b4aa8640147a33dfde1badfed
HopNT3SI <32725057+HopNT3SI@users.noreply.github.com>
2017-11-27 18:08:05 +0700

    AffiliationDepartment

--- nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.app/src/main/java/nts/uk/ctx/bs/employee/app/command/department/AddAffiliationDepartmentCommandHandler.java
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.app/src/main/java/nts/uk/ctx/bs/employee/app/command/department/AddAffiliationDepartmentCommandHandler.java
@@ -1,5 +1,8 @@
 package nts.uk.ctx.bs.employee.app.command.department;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
@@ -7,8 +10,12 @@ import lombok.val;
 import nts.arc.layer.app.command.CommandHandlerContext;
 import nts.arc.layer.app.command.CommandHandlerWithResult;
 import nts.gul.text.IdentifierUtil;
-import nts.uk.ctx.bs.employee.dom.department.AffDepartmentRepository;
-import nts.uk.ctx.bs.employee.dom.department.AffiliationDepartment;
+import nts.uk.ctx.bs.employee.dom.department.affiliate.AffDepartmentHistory;
+import nts.uk.ctx.bs.employee.dom.department.affiliate.AffDepartmentHistoryItem;
+import nts.uk.ctx.bs.employee.dom.department.affiliate.AffDepartmentHistoryItemRepository;
+import nts.uk.ctx.bs.employee.dom.department.affiliate.AffDepartmentHistoryRepository;
+import nts.uk.shr.com.history.DateHistoryItem;
+import nts.uk.shr.com.time.calendar.period.DatePeriod;
 import nts.uk.shr.pereg.app.command.PeregAddCommandHandler;
 import nts.uk.shr.pereg.app.command.PeregAddCommandResult;
 
@@ -17,7 +24,10 @@ public class AddAffiliationDepartmentCommandHandler extends CommandHandlerWithRe
 	implements PeregAddCommandHandler<AddAffiliationDepartmentCommand>{
 
 	@Inject
-	private AffDepartmentRepository affDepartmentRepository;
+	private AffDepartmentHistoryRepository affDepartmentHistoryRepository;
+	
+	@Inject
+	private AffDepartmentHistoryItemRepository affDepartmentHistoryItemRepository;
 	
 	@Override
 	public String targetCategoryCd() {
@@ -33,12 +43,26 @@ public class AddAffiliationDepartmentCommandHandler extends CommandHandlerWithRe
 	protected PeregAddCommandResult  handle(CommandHandlerContext<AddAffiliationDepartmentCommand> context) {
 		val command = context.getCommand();
 		
-		String newId = IdentifierUtil.randomUniqueId();
+		String newHistId = IdentifierUtil.randomUniqueId();
 		
-		AffiliationDepartment domain = AffiliationDepartment.createDmainFromJavaType(newId, command.getStartDate(), command.getEndDate(), command.getEmployeeId(), command.getDepartmentId());
-		affDepartmentRepository.addAffDepartment(domain);
+		DateHistoryItem dateItem = new DateHistoryItem(newHistId, new DatePeriod(command.getStartDate(), command.getEndDate()));
 		
-		return new PeregAddCommandResult(newId);
+		AffDepartmentHistory itemToBeAdded = null;
+		Optional<AffDepartmentHistory> itemHist = affDepartmentHistoryRepository.getAffDepartmentHistorytByEmployeeId(command.getEmployeeId());
+		if (itemHist.isPresent()){
+			itemHist.get().add(dateItem);
+			itemToBeAdded = itemHist.get();
+		} else {
+			itemToBeAdded = new AffDepartmentHistory(command.getEmployeeId(),new ArrayList<>());
+			itemToBeAdded.add(dateItem);
+		}
+		
+		affDepartmentHistoryRepository.addAffDepartment(itemToBeAdded);
+		
+		AffDepartmentHistoryItem domain = AffDepartmentHistoryItem.createFromJavaType(newHistId, command.getEmployeeId(), command.getDepartmentCode(), command.getAffHistoryTranfsType(), command.getDepartmentCode());
+		affDepartmentHistoryItemRepository.addAffDepartment(domain);
+		
+		return new PeregAddCommandResult(newHistId);
 	}
 
 }
--- nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.infra/src/main/java/nts/uk/ctx/bs/employee/infra/repository/department/affiliate/JpaAffDepartmentHistoryItemRepository.java
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.infra/src/main/java/nts/uk/ctx/bs/employee/infra/repository/department/affiliate/JpaAffDepartmentHistoryItemRepository.java
@@ -1,30 +1,57 @@
 package nts.uk.ctx.bs.employee.infra.repository.department.affiliate;
 
+import java.util.Optional;
+
 import javax.ejb.Stateless;
 
 import nts.arc.layer.infra.data.JpaRepository;
-import nts.uk.ctx.bs.employee.dom.workplace.affiliate.AffWorkplaceHistoryItem;
-import nts.uk.ctx.bs.employee.dom.workplace.affiliate.AffWorkplaceHistoryItemRepository_v1;
+import nts.uk.ctx.bs.employee.dom.department.affiliate.AffDepartmentHistoryItem;
+import nts.uk.ctx.bs.employee.dom.department.affiliate.AffDepartmentHistoryItemRepository;
+import nts.uk.ctx.bs.employee.infra.entity.department.BsymtAffiDepartmentHistItem;
 
 @Stateless
-public class JpaAffDepartmentHistoryItemRepository extends JpaRepository implements AffWorkplaceHistoryItemRepository_v1{
+public class JpaAffDepartmentHistoryItemRepository extends JpaRepository implements AffDepartmentHistoryItemRepository{
 
+	/**
+	 * Convert from domain to entity
+	 * @param domain
+	 * @return
+	 */
+	private BsymtAffiDepartmentHistItem toEntity(AffDepartmentHistoryItem domain){
+		return new BsymtAffiDepartmentHistItem(domain.getHistoryId(),domain.getEmployeeId(),domain.getDepartmentCode().v(), domain.getAffHistoryTranfsType(),domain.getDepartmentCode().v());
+	}
+	
+	private void updateEntity(AffDepartmentHistoryItem domain, BsymtAffiDepartmentHistItem entity){
+		entity.setSid(domain.getEmployeeId());
+		entity.setDepCode(domain.getDepartmentCode().v());
+		entity.setAffHistTranfsType(domain.getAffHistoryTranfsType());
+		entity.setDepCode(domain.getDistributionRatio().v());
+	}
+	
+	
 	@Override
-	public void addAffWorkplaceHistory(AffWorkplaceHistoryItem domain) {
-		// TODO Auto-generated method stub
-		
+	public void addAffDepartment(AffDepartmentHistoryItem domain) {
+		this.commandProxy().insert(toEntity(domain));
 	}
 
 	@Override
-	public void deleteAffWorkplaceHistory(String histID) {
-		// TODO Auto-generated method stub
+	public void updateAffDepartment(AffDepartmentHistoryItem domain) {
+		Optional<BsymtAffiDepartmentHistItem> existItem = this.queryProxy().find(domain.getHistoryId(), BsymtAffiDepartmentHistItem.class);
+		if (!existItem.isPresent()){
+			throw new RuntimeException("invalid BsymtAffiDepartmentHistItem");
+		}
+		updateEntity(domain,existItem.get());
 		
+		this.commandProxy().update(existItem.get());
 	}
 
 	@Override
-	public void updateAffWorkplaceHistory(AffWorkplaceHistoryItem domain) {
-		// TODO Auto-generated method stub
-		
+	public void deleteAffDepartment(String histId) {
+		Optional<BsymtAffiDepartmentHistItem> existItem = this.queryProxy().find(histId, BsymtAffiDepartmentHistItem.class);
+		if (!existItem.isPresent()){
+			throw new RuntimeException("invalid BsymtAffiDepartmentHistItem");
+		}
+		this.commandProxy().remove(BsymtAffiDepartmentHistItem.class, histId);
 	}
 
 
