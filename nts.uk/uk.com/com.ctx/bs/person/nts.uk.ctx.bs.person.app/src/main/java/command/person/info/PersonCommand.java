f81d55fb68e91245ae3950e98b56cb5a40e8025c
HopNT3SI <32725057+HopNT3SI@users.noreply.github.com>
2017-11-28 17:57:56 +0700

    add item code to person

--- nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.app/src/main/java/command/person/info/PersonCommand.java
+++ nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.app/src/main/java/command/person/info/PersonCommand.java
@@ -10,62 +10,62 @@ public class PersonCommand {
 	
 	/** The Birthday */
 	// 生年月日
-	@PeregItem("")
+	@PeregItem("IS00017")
 	private GeneralDate birthDate;
 
 	/** The BloodType */
 	// 血液型
-	@PeregItem("")
+	@PeregItem("IS00019")
 	private int bloodType;
 
 	/** The Gender - 性別 */
-	@PeregItem("")
+	@PeregItem("IS00018")
 	private int gender;
 
 	/** The person id - 個人ID */
 	private String personId;
 
 	/** The PersonNameGroup - 個人名グループ*/
-	@PeregItem("")
+	@PeregItem("IS00003")
 	private String personName;
 
-	@PeregItem("")
+	@PeregItem("IS00004")
 	private String personNameKana;
 
-	@PeregItem("")
+	@PeregItem("IS00009")
 	private String businessName;
 	
-	@PeregItem("")
+	@PeregItem("IS00010")
 	private String businessNameKana;
 
-	@PeregItem("")
+	@PeregItem("IS00011")
 	private String businessEnglishName;
 
-	@PeregItem("")
+	@PeregItem("IS00012")
 	private String businessOtherName;
 
-	@PeregItem("")
+	@PeregItem("IS00005")
 	private String personRomanji;
 	
-	@PeregItem("")
+	@PeregItem("IS00006")
 	private String personRomanjiKana;
 
-	@PeregItem("")
+	@PeregItem("IS00013")
 	private String oldName;
 	
-	@PeregItem("")
+	@PeregItem("IS00014")
 	private String oldNameKana;
 
-	@PeregItem("")
+	@PeregItem("IS00015")
 	private String todokedeFullName;
 	
-	@PeregItem("")
+	@PeregItem("IS00016")
 	private String todokedeFullNameKana;
 
-	@PeregItem("")
+	@PeregItem("IS00007")
 	private String PersonalNameMultilingual;
 	
-	@PeregItem("")
+	@PeregItem("IS00008")
 	private String PersonalNameMultilingualKana;
 
 }
