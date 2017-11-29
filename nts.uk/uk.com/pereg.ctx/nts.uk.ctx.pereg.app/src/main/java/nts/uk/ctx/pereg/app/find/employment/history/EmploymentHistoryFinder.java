da8ae4bf2c0bb366b5a04fbdd3239c2ee23673de
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2017-11-29 18:41:57 +0700

    add EmploymentHistoryFinder

---
+++ nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.app/src/main/java/nts/uk/ctx/pereg/app/find/employment/history/EmploymentHistoryFinder.java
@@ -0,0 +1,57 @@
+package nts.uk.ctx.pereg.app.find.employment.history;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import nts.uk.ctx.bs.employee.dom.employment.history.EmploymentHistory;
+import nts.uk.ctx.bs.employee.dom.employment.history.EmploymentHistoryRepository;
+import nts.uk.shr.pereg.app.find.PeregFinder;
+import nts.uk.shr.pereg.app.find.PeregQuery;
+import nts.uk.shr.pereg.app.find.dto.DataClassification;
+import nts.uk.shr.pereg.app.find.dto.PeregDomainDto;
+
+@Stateless
+public class EmploymentHistoryFinder implements PeregFinder<EmploymentHistoryDto> {
+
+	@Inject
+	private EmploymentHistoryRepository empHistoryRepo;
+
+	@Override
+	public String targetCategoryCode() {
+		return "CS00014";
+	}
+
+	@Override
+	public Class<EmploymentHistoryDto> dtoClass() {
+
+		return EmploymentHistoryDto.class;
+
+	}
+
+	@Override
+	public DataClassification dataType() {
+
+		return DataClassification.EMPLOYEE;
+	}
+
+	@Override
+	public PeregDomainDto getSingleData(PeregQuery query) {
+		Optional<EmploymentHistory> optHis = this.empHistoryRepo.getByEmployeeId(query.getEmployeeId());
+
+		if (!optHis.isPresent()) {
+			return null;
+		}
+		return EmploymentHistoryDto.createFromDomain(optHis.get());
+
+	}
+
+	@Override
+	public List<PeregDomainDto> getListData(PeregQuery query) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
