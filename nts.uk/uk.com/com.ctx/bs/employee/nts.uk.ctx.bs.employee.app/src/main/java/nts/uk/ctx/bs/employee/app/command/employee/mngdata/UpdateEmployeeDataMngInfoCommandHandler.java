22b140470680e64409f9c237bc35db141fd02632
HopNT3SI <32725057+HopNT3SI@users.noreply.github.com>
2017-11-29 13:59:30 +0700

    EmployeeDataMngInfo Command

---
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.app/src/main/java/nts/uk/ctx/bs/employee/app/command/employee/mngdata/UpdateEmployeeDataMngInfoCommandHandler.java
@@ -0,0 +1,42 @@
+package nts.uk.ctx.bs.employee.app.command.employee.mngdata;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import lombok.val;
+import nts.arc.layer.app.command.CommandHandler;
+import nts.arc.layer.app.command.CommandHandlerContext;
+import nts.uk.ctx.bs.employee.dom.employee.mgndata.EmployeeDataMngInfo;
+import nts.uk.ctx.bs.employee.dom.employee.mgndata.EmployeeDataMngInfoRepository;
+import nts.uk.shr.com.context.AppContexts;
+import nts.uk.shr.pereg.app.command.PeregUpdateCommandHandler;
+
+@Stateless
+public class UpdateEmployeeDataMngInfoCommandHandler extends CommandHandler<UpdateEmployeeDataMngInfoCommand>
+	implements PeregUpdateCommandHandler<UpdateEmployeeDataMngInfoCommand>{
+	
+	@Inject
+	private EmployeeDataMngInfoRepository employeeDataMngInfoRepository;
+	
+	@Override
+	public String targetCategoryCd() {
+		return "CS00001";
+	}
+
+	@Override
+	public Class<?> commandClass() {
+		return UpdateEmployeeDataMngInfoCommand.class;
+	}
+
+	@Override
+	protected void handle(CommandHandlerContext<UpdateEmployeeDataMngInfoCommand> context) {
+		
+		val command = context.getCommand();
+		String companyId = AppContexts.user().companyId();
+		
+		EmployeeDataMngInfo domain = EmployeeDataMngInfo.createFromJavaType(companyId,command.getPersonId(), command.getEmployeeId(),command.getEmployeeCode(),0,null,null,command.getExternalCode());
+		
+		employeeDataMngInfoRepository.update(domain);
+	}
+
+}
