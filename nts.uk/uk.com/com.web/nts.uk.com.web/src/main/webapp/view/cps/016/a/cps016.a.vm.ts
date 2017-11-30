fa1d1fa0cfea8cb0337c949ae29bd47b8d9f69b2
Nguyen Van Tuan <tuannv@nsvc.com>
2017-11-30 16:43:50 +0700

    fixbug cps017: size dialog

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/016/a/cps016.a.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/016/a/cps016.a.vm.ts
@@ -156,7 +156,7 @@ module nts.uk.com.view.cps016.a.viewmodel {
                         }
                         setShared('CPS017_PARAMS', params);
 
-                        modal('/view/cps/017/a/index.xhtml', { title: '', height: 1000, width: 1500 }).onClosed(function(): any {
+                        modal('/view/cps/017/a/index.xhtml', { title: '', height: 800, width: 1400 }).onClosed(function(): any {
                         });
                     }).ifNo(() => {
                         self.listItems.valueHasMutated();
@@ -252,7 +252,7 @@ module nts.uk.com.view.cps016.a.viewmodel {
                 }
             setShared('CPS017_PARAMS', params);
 
-            modal('/view/cps/017/a/index.xhtml', { title: '', height: 1000, width: 1500 }).onClosed(function(): any {
+            modal('/view/cps/017/a/index.xhtml', { title: '', height: 800, width: 1400 }).onClosed(function(): any {
             });
         }
 
