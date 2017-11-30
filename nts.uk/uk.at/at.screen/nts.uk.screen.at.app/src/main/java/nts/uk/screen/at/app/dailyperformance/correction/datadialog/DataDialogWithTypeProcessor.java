33d38b1f388eee27eaa38fe9e78c8510f9b20a0a
NSVN-NguyenXuanThanh <thanh_nx@nittsusystem-vn.com>
2017-11-30 10:03:31 +0700

    update kdw/003/a

---
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/dailyperformance/correction/datadialog/DataDialogWithTypeProcessor.java
@@ -0,0 +1,54 @@
+package nts.uk.screen.at.app.dailyperformance.correction.datadialog;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class DataDialogWithTypeProcessor {
+
+	// 勤務種類
+	public void getDutyType(String workTypeCode, String employmentCode) {
+
+	}
+
+	// 就業時間帯
+	public void getWorkHours() {
+
+	}
+
+	// 勤務場所
+	public void getServicePlace() {
+
+	}
+
+	// 乖離理由
+	public void getReason() {
+
+	}
+
+	// 職場
+	public void getWorkPlace() {
+
+	}
+
+	// 分類
+	public void getClassification() {
+
+	}
+
+	// 職位
+	public void getPossition() {
+
+	}
+
+	// 雇用区分
+	public void getEmployment() {
+
+	}
+
+	public AttendanceItemTypeDialog getTypeDialog(List<String> attendanceId) {
+
+		return null;
+	}
+}
