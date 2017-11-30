1b9c9bd5b8383529f83c445795fbe6747f32a01b
NSVN-BuiVanChinh <chinh_bv@nittsusystem-vn.com>
2017-11-30 12:44:42 +0700

    ksu001-LX: fix primitive value

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/shift/team/TeamCode.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/shift/team/TeamCode.java
@@ -1,6 +1,8 @@
 package nts.uk.ctx.at.schedule.dom.shift.team;
 
 import nts.arc.primitive.StringPrimitiveValue;
+import nts.arc.primitive.constraint.CharType;
+import nts.arc.primitive.constraint.StringCharType;
 import nts.arc.primitive.constraint.StringMaxLength;
 import nts.uk.shr.com.primitive.ZeroPaddedCode;
 
@@ -9,6 +11,7 @@ import nts.uk.shr.com.primitive.ZeroPaddedCode;
  * @author sonnh1
  *
  */
+@StringCharType(CharType.ALPHA_NUMERIC)
 @StringMaxLength(2)
 @ZeroPaddedCode
 public class TeamCode extends StringPrimitiveValue<TeamCode> {
