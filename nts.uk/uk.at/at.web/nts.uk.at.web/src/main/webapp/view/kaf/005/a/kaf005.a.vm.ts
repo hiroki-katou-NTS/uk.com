9a317d3e1bed3511260f18698c34b6e029b8d693
thangnq3si <thang.nq@3si.vn>
2017-12-05 12:48:49 +0700

    Updated: Add param to message 424

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/005/a/kaf005.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/005/a/kaf005.a.vm.ts
@@ -356,11 +356,11 @@ module nts.uk.at.view.kaf005.a.viewmodel {
                         for (let i = 0; i < self.overtimeHours().length; i++) {
                             self.changeColor( self.overtimeHours()[i].attendanceID(), self.overtimeHours()[i].frameNo());
                         }
-                        dialog.alertError({messageId:"Msg_424"}) .then(function() { nts.uk.ui.block.clear(); }); 
+                        dialog.alertError({messageId:"Msg_424", messageParams: [self.employeeName(), moment(self.appDate()).format(self.DATEFORMART)]}) .then(function() { nts.uk.ui.block.clear(); }); 
                     }else{
                       //Change background color
                         self.changeColor( data.attendanceId, data.frameNo);
-                        dialog.alertError({messageId:"Msg_424"}) .then(function() { nts.uk.ui.block.clear(); }); 
+                        dialog.alertError({messageId:"Msg_424", messageParams: [self.employeeName(), moment(self.appDate()).format(self.DATEFORMART), $('#overtimeHoursHeader_'+data.attendanceId+'_'+data.frameNo).text()]}) .then(function() { nts.uk.ui.block.clear(); }); 
                     }                    
                 }
             }).fail((res) => {
