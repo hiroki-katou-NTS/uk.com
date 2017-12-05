2937295f690b84706b5ed3e7dde91c75ee705564
ThuongDangXuan <thuong_dx@nittsusystem-vn.com>
2017-12-05 13:04:10 +0700

    update UI cas009

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cas/009/a/cas009.a.service.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cas/009/a/cas009.a.service.ts
@@ -1,14 +1,14 @@
 module cas009.a.service {
     var paths: any = {
         getPersonInfoRole: "ctx/sys/auth/role/find/person/role",
-        deletePersonInfoRole: "ctx/sys/auth/role/delete/person/infor",
+        deletePersonInfoRole: "ctx/sys/auth/role/remove/person/infor",
         savePersonInfoRole: "ctx/sys/auth/role/save/person/infor",
         getOptItemEnum: 'ctx/sys/auth/role/get/enum/reference/range'
     }
     
     /** Get Role */
-    export function getPersonInfoRole(roleIds: Array<string>): JQueryPromise<Array<any>> {
-        return nts.uk.request.ajax("com", paths.getPersonInfoRole);
+    export function getPersonInfoRole(roleIds: Array<string>): JQueryPromise<any> {
+        return nts.uk.request.ajax("com", paths.getPersonInfoRole, roleIds);
     }
 
     /** Function is used to delete Role */
