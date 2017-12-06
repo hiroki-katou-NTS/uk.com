fbb3260c1266dbe014098c3caa6d95ae041f770e
NSVC\anhvd <anh_vd@nittsusystem-vn.com>
2017-12-06 13:14:51 +0700

    Add function findByListCardNo for Stamp domain.

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/stamp/StampRepository.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/stamp/StampRepository.java
@@ -3,6 +3,13 @@ package nts.uk.ctx.at.record.dom.stamp;
 import java.util.List;
 
 public interface StampRepository {
+	/**
+	 * Get list StampItem by List Card Number.
+	 * @param lstCardNumber
+	 * @return StampItem list
+	 */
+	List<StampItem> findByListCardNo(List<String> lstCardNumber);
+	
 	// get List Stamp by Card Number and Day of Work
 	//List<StampItem> findByEmployeeCode(String companyId, String cardNumber, String startDate, String endDate);
 	List<StampItem> findByEmployeeCode(String companyId, List<String> lstCardNumber, String startDate, String endDate);
