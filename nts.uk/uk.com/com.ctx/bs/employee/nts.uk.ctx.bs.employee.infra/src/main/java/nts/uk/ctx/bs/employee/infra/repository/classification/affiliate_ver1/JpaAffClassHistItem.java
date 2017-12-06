7148923fff362577152ebb718b947731e864e31f
NSVN-PhamVanDan <dan_pv@nittsusystem-vn.com>
2017-12-06 11:07:53 +0700

    fix bug query syntax error

--- nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.infra/src/main/java/nts/uk/ctx/bs/employee/infra/repository/classification/affiliate_ver1/JpaAffClassHistItem.java
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.infra/src/main/java/nts/uk/ctx/bs/employee/infra/repository/classification/affiliate_ver1/JpaAffClassHistItem.java
@@ -20,33 +20,33 @@ import nts.uk.ctx.bs.employee.infra.entity.classification.affiliate_ver1.BsymtAf
 @Stateless
 public class JpaAffClassHistItem extends JpaRepository implements AffClassHistItemRepository_ver1 {
 
-	private final String GET_BY_SID_DATE = "select hi from KmnmtAffClassHistItem_Ver1 hi"
-			+ " join in KmnmtAffClassHistory_Ver1 h on hi.histId = h.histId"
-			+ " where hi.sid = :sid and h.startDate <= :referDate and h.endDate >= referDate";
+	private final String GET_BY_SID_DATE = "select hi from BsymtAffClassHistItem_Ver1 hi"
+			+ " inner join BsymtAffClassHistory_Ver1 h on hi.historyId = h.historyId"
+			+ " where hi.sid = :sid and h.startDate <= :referDate and h.endDate >= :referDate";
 
 	@Override
 	public Optional<AffClassHistItem_ver1> getByEmpIdAndReferDate(String employeeId, GeneralDate referenceDate) {
-		/*Optional<KmnmtAffClassHistItem_Ver1> optionData = this.queryProxy()
-				.query(GET_BY_SID_DATE, KmnmtAffClassHistItem_Ver1.class).setParameter("sid", employeeId)
+		Optional<BsymtAffClassHistItem_Ver1> optionData = this.queryProxy()
+				.query(GET_BY_SID_DATE, BsymtAffClassHistItem_Ver1.class).setParameter("sid", employeeId)
 				.setParameter("referDate", referenceDate).getSingle();
 		if (optionData.isPresent()) {
-			KmnmtAffClassHistItem_Ver1 ent = optionData.get();
+			BsymtAffClassHistItem_Ver1 ent = optionData.get();
 			return Optional
 					.of(AffClassHistItem_ver1.createFromJavaType(ent.sid, ent.historyId, ent.classificationCode));
 		}
-		return Optional.empty();*/
-		return Optional.of(AffClassHistItem_ver1.createFromJavaType("909909139840", "98765432109876543210654321", "1213132132121231321"));
+		return Optional.empty();
 	}
-	
+
 	@Override
 	public Optional<AffClassHistItem_ver1> getByHistoryId(String historyId) {
-		Optional<BsymtAffClassHistItem_Ver1> optionData = this.queryProxy().find(historyId, BsymtAffClassHistItem_Ver1.class);
-		if ( optionData.isPresent() ) {
+		Optional<BsymtAffClassHistItem_Ver1> optionData = this.queryProxy().find(historyId,
+				BsymtAffClassHistItem_Ver1.class);
+		if (optionData.isPresent()) {
 			return Optional.of(toDomain(optionData.get()));
 		}
 		return Optional.empty();
 	}
-	
+
 	private AffClassHistItem_ver1 toDomain(BsymtAffClassHistItem_Ver1 entity) {
 		return AffClassHistItem_ver1.createFromJavaType(entity.sid, entity.historyId, entity.classificationCode);
 	}
@@ -83,6 +83,5 @@ public class JpaAffClassHistItem extends JpaRepository implements AffClassHistIt
 		}
 		this.commandProxy().update(entityOpt.get());
 	}
-
 
 }
