1670906672cfbd79942e2eb9934c784b48443049
Hung Tran <32724898+1capedbaldy@users.noreply.github.com>
2017-12-08 11:36:22 +0700

    cas014: update app

--- nts.uk/uk.com/com.ctx/sys/auth/nts.uk.ctx.sys.auth.app/src/main/java/nts/uk/ctx/sys/auth/app/find/grant/rolesetperson/RoleSetGrantedPersonFinder.java
+++ nts.uk/uk.com/com.ctx/sys/auth/nts.uk.ctx.sys.auth.app/src/main/java/nts/uk/ctx/sys/auth/app/find/grant/rolesetperson/RoleSetGrantedPersonFinder.java
@@ -60,7 +60,7 @@ public class RoleSetGrantedPersonFinder {
 		if (listRoleSetPerson != null && !listRoleSetPerson.isEmpty()) {
 			List<RoleSetGrantedPersonDto> listRoleSetPersonDto = new ArrayList<>();
 			for (RoleSetGrantedPerson rp : listRoleSetPerson) {
-				EmployeeImport empInfo = new EmployeeImport("XXXX YYYY", rp.getEmployeeID(), "99999999");// employeeAdapter.findByEmpId(rp.getEmployeeID());
+				EmployeeImport empInfo = employeeAdapter.findByEmpId(rp.getEmployeeID());
 				RoleSetGrantedPersonDto dto = new RoleSetGrantedPersonDto(rp.getRoleSetCd().v(), rp.getEmployeeID(),
 						empInfo.getEmployeeCode(), empInfo.getPersonalName(), rp.getValidPeriod().start(),
 						rp.getValidPeriod().end());
