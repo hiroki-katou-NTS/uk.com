f7496a29f46e7faa396984d74b462178e12a51ae
NSVC\hungnm <hungnm910@gmail.com>
2017-12-08 11:53:09 +0700

    KDW007: APP

---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/find/workrecord/erroralarm/condition/ErAlAtdItemConditionDto.java
@@ -0,0 +1,36 @@
+/**
+ * 3:00:49 PM Dec 4, 2017
+ */
+package nts.uk.ctx.at.record.app.find.workrecord.erroralarm.condition;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author hungnm
+ *
+ */
+@Setter
+@Getter
+public class ErAlAtdItemConditionDto {
+	private int NO;
+	private int conditionAtr;
+	private boolean useAtr;
+	private int uncountableAtdItem;
+	private List<Integer> countableAddAtdItems;
+	private List<Integer> countableSubAtdItems;
+	private int conditionType;
+	private int compareOperator;
+	private int singleAtdItem;
+	private BigDecimal compareStartValue;
+	private BigDecimal compareEndValue;
+	
+	public ErAlAtdItemConditionDto() {
+		super();
+	}
+	
+}
+
