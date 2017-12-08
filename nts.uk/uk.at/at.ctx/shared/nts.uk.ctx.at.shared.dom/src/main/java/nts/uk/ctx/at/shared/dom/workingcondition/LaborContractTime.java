e67787aae258bbd025eb305d1bd8ada5e8372f1e
haivh <haivh@newwave.vn>
2017-12-08 10:04:26 +0700

    update domain work condition

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/workingcondition/LaborContractTime.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/workingcondition/LaborContractTime.java
@@ -1,5 +1,22 @@
 package nts.uk.ctx.at.shared.dom.workingcondition;
 
-public class LaborContractTime {
+import nts.arc.primitive.TimeClockPrimitiveValue;
+
+/**
+ * The Class LaborContractTime.
+ */
+public class LaborContractTime extends TimeClockPrimitiveValue<LaborContractTime> {
+
+	/** The Constant serialVersionUID. */
+	private static final long serialVersionUID = 6044051654921619509L;
+	
+	/**
+	 * Instantiates a new labor contract time.
+	 *
+	 * @param minutesFromZeroOClock the minutes from zero O clock
+	 */
+	public LaborContractTime(int minutesFromZeroOClock) {
+		super(minutesFromZeroOClock);
+	}
 
 }
