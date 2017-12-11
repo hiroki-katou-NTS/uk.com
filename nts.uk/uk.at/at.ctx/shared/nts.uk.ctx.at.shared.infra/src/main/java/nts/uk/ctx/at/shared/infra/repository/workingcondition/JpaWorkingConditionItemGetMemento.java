9b2274442fadadaace65f5cfa898823c4b0a8f8a
haivh <haivh@newwave.vn>
2017-12-11 15:58:06 +0700

    infra Working condition Item

---
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/repository/workingcondition/JpaWorkingConditionItemGetMemento.java
@@ -0,0 +1,86 @@
+package nts.uk.ctx.at.shared.infra.repository.workingcondition;
+
+import nts.uk.ctx.at.shared.dom.workingcondition.BreakdownTimeDay;
+import nts.uk.ctx.at.shared.dom.workingcondition.LaborContractTime;
+import nts.uk.ctx.at.shared.dom.workingcondition.NotUseAtr;
+import nts.uk.ctx.at.shared.dom.workingcondition.PersonalDayOfWeek;
+import nts.uk.ctx.at.shared.dom.workingcondition.PersonalWorkCategory;
+import nts.uk.ctx.at.shared.dom.workingcondition.ScheduleMethod;
+import nts.uk.ctx.at.shared.dom.workingcondition.WorkingConditionItemGetMemento;
+import nts.uk.ctx.at.shared.dom.workingcondition.WorkingSystem;
+
+public class JpaWorkingConditionItemGetMemento implements WorkingConditionItemGetMemento {
+
+	@Override
+	public NotUseAtr getScheduleManagementAtr() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public NotUseAtr getVacationAddedTimeAtr() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WorkingSystem getLaborSystem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PersonalWorkCategory getWorkCategory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LaborContractTime getContractTime() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public NotUseAtr getAutoIntervalSetAtr() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getHistoryId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PersonalDayOfWeek getWorkDayOfWeek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getEmployeeId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public NotUseAtr getAutoStampSetAtr() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ScheduleMethod getScheduleMethod() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BreakdownTimeDay getHolidayAddTimeSet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
