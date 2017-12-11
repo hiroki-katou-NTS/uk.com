2cee7661160372ae546acf37a42d5f160ef8c636
NSVN-PhamVanDan <dan_pv@nittsusystem-vn.com>
2017-12-11 08:51:47 +0700

    fix add and delete command

--- nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.app/src/main/java/nts/uk/ctx/bs/employee/app/command/employment/history/AddEmploymentHistoryCommandHandler.java
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.app/src/main/java/nts/uk/ctx/bs/employee/app/command/employment/history/AddEmploymentHistoryCommandHandler.java
@@ -15,23 +15,25 @@ import nts.uk.ctx.bs.employee.dom.employment.history.EmploymentHistoryService;
 import nts.uk.ctx.bs.employee.dom.employment.history.EmploymentHistoryItem;
 import nts.uk.ctx.bs.employee.dom.employment.history.EmploymentHistoryItemRepository;
 import nts.uk.ctx.bs.employee.dom.employment.history.EmploymentHistoryRepository;
+import nts.uk.shr.com.context.AppContexts;
 import nts.uk.shr.com.history.DateHistoryItem;
 import nts.uk.shr.com.time.calendar.period.DatePeriod;
 import nts.uk.shr.pereg.app.command.PeregAddCommandHandler;
 import nts.uk.shr.pereg.app.command.PeregAddCommandResult;
 
 @Stateless
-public class AddEmploymentHistoryCommandHandler extends CommandHandlerWithResult<AddEmploymentHistoryCommand,PeregAddCommandResult>
-implements PeregAddCommandHandler<AddEmploymentHistoryCommand> {
+public class AddEmploymentHistoryCommandHandler
+		extends CommandHandlerWithResult<AddEmploymentHistoryCommand, PeregAddCommandResult>
+		implements PeregAddCommandHandler<AddEmploymentHistoryCommand> {
 
 	@Inject
 	private EmploymentHistoryRepository employmentHistoryRepository;
 	@Inject
 	private EmploymentHistoryItemRepository employmentHistoryItemRepository;
-	
+
 	@Inject
 	private EmploymentHistoryService employmentHistoryService;
-	
+
 	@Override
 	public String targetCategoryCd() {
 		return "CS00014";
@@ -46,22 +48,26 @@ implements PeregAddCommandHandler<AddEmploymentHistoryCommand> {
 	protected PeregAddCommandResult handle(CommandHandlerContext<AddEmploymentHistoryCommand> context) {
 		val command = context.getCommand();
 		String newHistID = IdentifierUtil.randomUniqueId();
-		DateHistoryItem dateItem = new DateHistoryItem(newHistID, new DatePeriod(command.getStartDate(), command.getEndDate()));
-		
+		String companyId = AppContexts.user().companyId();
+		DateHistoryItem dateItem = new DateHistoryItem(newHistID,
+				new DatePeriod(command.getStartDate(), command.getEndDate()));
+
 		Optional<EmploymentHistory> histBySid = employmentHistoryRepository.getByEmployeeId(command.getEmployeeId());
-		
-		EmploymentHistory itemtoBeAdded = new EmploymentHistory(command.getEmployeeId(), new ArrayList<>());
-		if (histBySid.isPresent()){
+
+		EmploymentHistory itemtoBeAdded = new EmploymentHistory(companyId, command.getEmployeeId(), new ArrayList<>());
+		if (histBySid.isPresent()) {
 			itemtoBeAdded = histBySid.get();
 		}
-		
+
 		itemtoBeAdded.add(dateItem);
-		
+
 		employmentHistoryService.add(itemtoBeAdded);
-		
-		EmploymentHistoryItem histItem = EmploymentHistoryItem.createFromJavaType(newHistID, command.getEmployeeId(), command.getSalarySegment()!= null?command.getSalarySegment().intValue():0, command.getEmploymentCode());
+
+		EmploymentHistoryItem histItem = EmploymentHistoryItem.createFromJavaType(newHistID, command.getEmployeeId(),
+				command.getEmploymentCode(),
+				command.getSalarySegment() != null ? command.getSalarySegment().intValue() : 0);
 		employmentHistoryItemRepository.adḍ̣̣̣(histItem);
-		
+
 		return new PeregAddCommandResult(newHistID);
 	}
 
