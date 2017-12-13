eb31e2def6ae3799718f3283c11f66da611961ef
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2017-12-13 10:14:52 +0700

    KAF: update code: add  13.実績を取得する to ApplicationWebservice.getDataApprovalRoot

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.app/src/main/java/nts/uk/ctx/at/request/app/find/application/common/GetDataApprovalRootOfSubjectRequest.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.app/src/main/java/nts/uk/ctx/at/request/app/find/application/common/GetDataApprovalRootOfSubjectRequest.java
@@ -1,7 +1,5 @@
 package nts.uk.ctx.at.request.app.find.application.common;
 
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,11 +9,12 @@ import javax.inject.Inject;
 import org.apache.logging.log4j.util.Strings;
 
 import nts.arc.time.GeneralDate;
-import nts.gul.collection.CollectionUtil;
+import nts.uk.ctx.at.request.app.find.application.common.dto.AchievementDto;
+import nts.uk.ctx.at.request.app.find.application.common.dto.RootData;
 import nts.uk.ctx.at.request.dom.application.common.adapter.bs.EmployeeRequestAdapter;
-import nts.uk.ctx.at.request.dom.application.common.adapter.bs.dto.ConcurrentEmployeeRequest;
 import nts.uk.ctx.at.request.dom.application.common.adapter.workflow.ApprovalRootAdapter;
-import nts.uk.ctx.at.request.dom.application.common.adapter.workflow.dto.ApproverInfoImport;
+import nts.uk.ctx.at.request.dom.application.common.service.other.CollectAchievement;
+import nts.uk.ctx.at.request.dom.application.common.service.other.output.AchievementOutput;
 import nts.uk.shr.com.context.AppContexts;
 
 @Stateless
@@ -23,10 +22,11 @@ public class GetDataApprovalRootOfSubjectRequest {
 	
 	@Inject 
 	private ApprovalRootAdapter approvalRootRepo;
-	@Inject
-	private EmployeeRequestAdapter employeeAdapter;
 	
-	public List<ApprovalRootOfSubjectRequestDto> getApprovalRootOfSubjectRequest(ObjApprovalRootInput objApprovalRootInput){
+	@Inject
+	private CollectAchievement collectAchievement;
+	
+	public RootData getApprovalRootOfSubjectRequest(ObjApprovalRootInput objApprovalRootInput){
 		String companyID = AppContexts.user().companyId();
 		String sid = "";
 		if(Strings.isBlank(objApprovalRootInput.getSid())){
@@ -35,13 +35,15 @@ public class GetDataApprovalRootOfSubjectRequest {
 			sid = objApprovalRootInput.getSid();
 		}
 		GeneralDate generalDate = GeneralDate.fromString(objApprovalRootInput.getStandardDate(), "yyyy/MM/dd");
-		List<ApprovalRootOfSubjectRequestDto> data =  this.approvalRootRepo.getApprovalRootOfSubjectRequest(companyID,
+		List<ApprovalRootOfSubjectRequestDto> listApproval =  this.approvalRootRepo.getApprovalRootOfSubjectRequest(companyID,
 				sid, objApprovalRootInput.getEmploymentRootAtr(), 
 				objApprovalRootInput.getAppType(),generalDate)
 				.stream()
 				.map(c->ApprovalRootOfSubjectRequestDto.fromDomain(c))
 				.collect(Collectors.toList());
-		return data;
+		AchievementOutput achievementOutput = collectAchievement.getAchievement(companyID, sid, generalDate);
+		AchievementDto achievementDto = AchievementDto.convertFromAchievementOutput(achievementOutput);
+		return new RootData(listApproval, achievementDto);
 	}
 
 }
