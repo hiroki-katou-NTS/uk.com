7cd87c7cf60f96c0c1c9ef5d1aee2ddc67ebee25
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2017-12-13 16:29:37 +0700

    workflow: add ApprovalRootState Infra

---
+++ nts.uk/uk.com/wk.ctx/nts.uk.ctx.workflow.infra/src/main/java/nts/uk/ctx/workflow/infra/repository/approverstatemanagement/JpaApprovalRootStateRepository.java
@@ -0,0 +1,30 @@
+package nts.uk.ctx.workflow.infra.repository.approverstatemanagement;
+
+import java.util.Optional;
+
+import javax.ejb.Stateless;
+
+import nts.arc.layer.infra.data.JpaRepository;
+import nts.uk.ctx.workflow.dom.approverstatemanagement.ApprovalRootState;
+import nts.uk.ctx.workflow.dom.approverstatemanagement.ApprovalRootStateRepository;
+/**
+ * 
+ * @author Doan Duy Hung
+ *
+ */
+@Stateless
+public class JpaApprovalRootStateRepository extends JpaRepository implements ApprovalRootStateRepository {
+
+	@Override
+	public Optional<ApprovalRootState> findEmploymentApp(String companyID, String rootStateID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(ApprovalRootState approvalRootState) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
