9ecbde62beef11664cb7936a9d03b0960c41568e
NSVN-PhamVanDan <dan_pv@nittsusystem-vn.com>
2017-12-13 10:35:35 +0700

    edit add recordId to dto

--- nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.app/src/main/java/nts/uk/ctx/bs/employee/app/find/employee/mngdata/EmployeeDataMngInfoDto.java
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.app/src/main/java/nts/uk/ctx/bs/employee/app/find/employee/mngdata/EmployeeDataMngInfoDto.java
@@ -1,11 +1,13 @@
 package nts.uk.ctx.bs.employee.app.find.employee.mngdata;
 
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import nts.uk.ctx.bs.employee.dom.employee.mgndata.EmployeeDataMngInfo;
 import nts.uk.shr.pereg.app.PeregItem;
 import nts.uk.shr.pereg.app.find.dto.PeregDomainDto;
 
 @Setter
+@NoArgsConstructor
 public class EmployeeDataMngInfoDto extends PeregDomainDto {
 	/** 社員コード */
 
@@ -15,19 +17,15 @@ public class EmployeeDataMngInfoDto extends PeregDomainDto {
 	/** 外部コード */
 	@PeregItem("IS00002")
 	private String externalCode;
+	
+	public EmployeeDataMngInfoDto(String recordId, String employeeCode,String externalCode ) {
+		super(recordId);
+		this.employeeCode = employeeCode;
+		this.externalCode = externalCode;
+	}
 
 	public static EmployeeDataMngInfoDto fromDomain(EmployeeDataMngInfo domain) {
-		if (domain == null) {
-			return null;
-		}
-
-		EmployeeDataMngInfoDto dto = new EmployeeDataMngInfoDto();
-
-		dto.setPersonId(domain.getPersonId());
-		dto.setEmployeeId(domain.getEmployeeId());
-		dto.setEmployeeCode(domain.getEmployeeCode().v());
-		dto.setExternalCode(domain.getExternalCode().v());
-
-		return dto;
+		return new EmployeeDataMngInfoDto(domain.getEmployeeId(), domain.getEmployeeCode().v(),
+				domain.getExternalCode().v());
 	}
 }
