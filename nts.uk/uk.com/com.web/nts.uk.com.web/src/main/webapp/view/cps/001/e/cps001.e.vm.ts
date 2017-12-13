f3c954d208a7dbaa73f058dd37993f7302d8cbb5
NSVN-TruongVanLai <NSVN-TruongVanLai@users.noreply.github.com>
2017-12-13 09:06:40 +0700

    update code cps001.f (check size file)

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/001/e/cps001.e.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/001/e/cps001.e.vm.ts
@@ -51,6 +51,7 @@ module cps001.e.vm {
             if (isImageLoaded.imgOnView) {
                 if (self.isChange()) {
                     $("#test").ntsImageEditor("upload", { stereoType: "image" }).done(function(data) {
+                        self.empFileMn().employeeId = "000426a2-181b-4c7f-abc8-6fff9f4f983a";
                         self.empFileMn().fileId = data.id;
                         self.updateImage(self.oldEmpFileMn, ko.toJS(self.empFileMn()));
                     });
