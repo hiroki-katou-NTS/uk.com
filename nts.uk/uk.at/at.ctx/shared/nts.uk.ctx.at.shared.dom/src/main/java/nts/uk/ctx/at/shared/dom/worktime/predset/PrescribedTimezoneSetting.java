b5151d61b3c32a7e1d83208765cfcc5ef38c989d
nws-anhnm <anhnm@newwave.vn>
2017-12-14 14:36:37 +0700

    {KMK003} refactor validate msg_773,774

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/worktime/predset/PrescribedTimezoneSetting.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/worktime/predset/PrescribedTimezoneSetting.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import lombok.Getter;
+import lombok.val;
 import nts.arc.error.BusinessException;
 import nts.arc.layer.dom.DomainObject;
 import nts.uk.shr.com.time.TimeWithDayAttr;
@@ -159,42 +160,43 @@ public class PrescribedTimezoneSetting extends DomainObject {
 		 * 勤務NO2 = する, 勤務NO=1の開始～終了の間  or 勤務NO=2の開始～終了の間であること
 		 * or 勤務NO2 = しない,  勤務NO=1の開始～終了の間であること
 		 */
-		validTimeDay();
+		this.validTimeDay();
 	}
 	
 	/**
 	 * Valid time day.
 	 */
 	private void validTimeDay() {
-		// 使用しない
-		if (this.getTimezone(SHIFT_TWO).getUseAtr() == UseSetting.NOT_USE) {
-			
-			//get timezone workno#1
-			TimezoneUse tzWorkNo1 = this.getTimezone(SHIFT_ONE);
-			
-			boolean isInValidTimeEndMorning = this.getMorningEndTime().lessThan(tzWorkNo1.getStart())
-					|| this.getMorningEndTime().greaterThan(tzWorkNo1.getEnd());
-			
-			boolean isInValidTimeStrAfternoon = this.getAfternoonStartTime().lessThan(tzWorkNo1.getStart())
-					|| this.getAfternoonStartTime().greaterThan(tzWorkNo1.getEnd());
-			
-			if (isInValidTimeEndMorning || isInValidTimeStrAfternoon) {
-				throw new BusinessException("Msg_773");
-			}
-		}
 		// 使用する
-		else {
-			boolean isInValidTimeDay = this.lstTimezone.stream().filter(timezone -> {
-				boolean isValidTimeEndMorning = this.getMorningEndTime().greaterThanOrEqualTo(timezone.getStart())
-						&& this.getMorningEndTime().lessThanOrEqualTo(timezone.getEnd());
-				boolean isValidTimeStrAfternoon = this.getAfternoonStartTime().greaterThanOrEqualTo(timezone.getStart())
-						&& this.getAfternoonStartTime().lessThanOrEqualTo(timezone.getEnd());
-				return isValidTimeEndMorning || isValidTimeStrAfternoon;
-			}).collect(Collectors.toList()).isEmpty();
-			if (isInValidTimeDay) {
+		if (this.getTimezoneShiftTwo().isUsed()) {
+			if (!(this.isMorningAndAfternoonInShift1() || this.isMorningAndAfternoonInShift2())) {
 				throw new BusinessException("Msg_774");
 			}
 		}
+		// 使用しない
+		else if (!this.isMorningAndAfternoonInShift1()) {
+			throw new BusinessException("Msg_773");
+		}
+	}
+
+	/**
+	 * Checks if is morning and afternoon in shift 1.
+	 *
+	 * @return true, if is morning and afternoon in shift 1
+	 */
+	private boolean isMorningAndAfternoonInShift1() {
+		val tzWorkNo1 = this.getTimezoneShiftOne();
+		return tzWorkNo1.consistOf(this.getAfternoonStartTime()) && tzWorkNo1.consistOf(this.getAfternoonStartTime());
+	}
+
+	/**
+	 * Checks if is morning and afternoon in shift 2.
+	 *
+	 * @return true, if is morning and afternoon in shift 2
+	 */
+	private boolean isMorningAndAfternoonInShift2() {
+		val tzWorkNo2 = this.getTimezoneShiftTwo();
+		return tzWorkNo2.consistOf(this.getAfternoonStartTime()) && tzWorkNo2.consistOf(this.getAfternoonStartTime());
 	}
 	
 }
