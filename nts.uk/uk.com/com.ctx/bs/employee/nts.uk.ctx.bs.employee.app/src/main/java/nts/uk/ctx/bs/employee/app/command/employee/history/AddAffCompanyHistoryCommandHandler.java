b17c4450b15228d9aa0ddcee15b16871a5be692c
NSVN-TranThiHoa <hoa_tt@nittsusystem-vn.com>
2017-12-15 11:04:41 +0700

    update code for oracle

--- nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.app/src/main/java/nts/uk/ctx/bs/employee/app/command/employee/history/AddAffCompanyHistoryCommandHandler.java
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.app/src/main/java/nts/uk/ctx/bs/employee/app/command/employee/history/AddAffCompanyHistoryCommandHandler.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+import org.apache.commons.lang3.StringUtils;
+
+import eu.medsea.util.StringUtil;
 import lombok.val;
 import nts.arc.layer.app.command.CommandHandlerContext;
 import nts.arc.layer.app.command.CommandHandlerWithResult;
@@ -23,18 +26,19 @@ import nts.uk.shr.pereg.app.command.PeregAddCommandHandler;
 import nts.uk.shr.pereg.app.command.PeregAddCommandResult;
 
 @Stateless
-public class AddAffCompanyHistoryCommandHandler extends CommandHandlerWithResult<AddAffCompanyHistoryCommand, PeregAddCommandResult>
-	implements PeregAddCommandHandler<AddAffCompanyHistoryCommand>{
-	
+public class AddAffCompanyHistoryCommandHandler
+		extends CommandHandlerWithResult<AddAffCompanyHistoryCommand, PeregAddCommandResult>
+		implements PeregAddCommandHandler<AddAffCompanyHistoryCommand> {
+
 	@Inject
 	private AffCompanyHistRepository affCompanyHistRepository;
-	
+
 	@Inject
 	private AffCompanyInfoRepository affCompanyInfoRepository;
-	
+
 	@Inject
 	private AffCompanyHistService affCompanyHistService;
-	
+
 	@Override
 	public String targetCategoryCd() {
 		return "CS00003";
@@ -49,23 +53,27 @@ public class AddAffCompanyHistoryCommandHandler extends CommandHandlerWithResult
 	protected PeregAddCommandResult handle(CommandHandlerContext<AddAffCompanyHistoryCommand> context) {
 		val command = context.getCommand();
 		String newHistId = IdentifierUtil.randomUniqueId();
-		
-		
+
 		AffCompanyHist listHist = affCompanyHistRepository.getAffCompanyHistoryOfEmployee(command.getSId());
-		
+
 		AffCompanyHistByEmployee itemToBeAdded = new AffCompanyHistByEmployee(command.getSId(), new ArrayList<>());
-		if (listHist != null){
+		if (listHist != null) {
 			itemToBeAdded = listHist.getAffCompanyHistByEmployee(command.getSId());
 		}
 		// 所属期間．終了日が指定されない場合（＝退職していない）、所属期間．終了日＝9999/12/31を自動的に設定する。
-		AffCompanyHistItem hist = new AffCompanyHistItem(newHistId, false, new DatePeriod(command.getStartDate(), command.getEndDate()!= null? command.getEndDate():  GeneralDate.fromString(ConstantUtils.MAX_DATE, ConstantUtils.FORMAT_DATE_YYYYMMDD) ));
+		AffCompanyHistItem hist = new AffCompanyHistItem(newHistId, false,
+				new DatePeriod(command.getStartDate(), command.getEndDate() != null ? command.getEndDate()
+						: GeneralDate.fromString(ConstantUtils.MAX_DATE, ConstantUtils.FORMAT_DATE_YYYYMMDD)));
 		itemToBeAdded.add(hist);
-		
-		affCompanyHistService.add(itemToBeAdded,command.getPId());
-		
-		AffCompanyInfo histItem = AffCompanyInfo.createFromJavaType(newHistId, command.getRecruitmentClassification(), command.getAdoptionDate(), command.getRetirementAllowanceCalcStartDate());
+
+		affCompanyHistService.add(itemToBeAdded, command.getPId());
+
+		AffCompanyInfo histItem = AffCompanyInfo.createFromJavaType(
+				newHistId, !StringUtils.isEmpty(command.getRecruitmentClassification())
+						? command.getRecruitmentClassification() : " ",
+				command.getAdoptionDate(), command.getRetirementAllowanceCalcStartDate());
 		affCompanyInfoRepository.add(histItem);
-		
+
 		return new PeregAddCommandResult(newHistId);
 	}
 
--- nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.app/src/main/java/nts/uk/ctx/pereg/app/command/addemployee/AddEmployeeCommandHandler.java
+++ nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.app/src/main/java/nts/uk/ctx/pereg/app/command/addemployee/AddEmployeeCommandHandler.java
@@ -170,7 +170,7 @@ public class AddEmployeeCommandHandler extends CommandHandlerWithResult<AddEmplo
 	private void addNewPerson() {
 
 		Person newPerson = Person.createFromJavaType(GeneralDate.min(), BloodType.Unselected.value,
-				GenderPerson.Male.value, personId, "", "", command.getEmployeeName(), "", "", "", "", "", "", "", "",
+				GenderPerson.Male.value, personId, " ", "", command.getEmployeeName(), " ", "", "", "", "", "", "", "",
 				"", "", "");
 
 		this.personRepo.addNewPerson(newPerson);
@@ -290,7 +290,7 @@ public class AddEmployeeCommandHandler extends CommandHandlerWithResult<AddEmplo
 
 		this.companyHistRepo.add(newComHist);
 
-		AffCompanyInfo newComInfo = AffCompanyInfo.createFromJavaType(comHistId, "", GeneralDate.max(),
+		AffCompanyInfo newComInfo = AffCompanyInfo.createFromJavaType(comHistId, " ", GeneralDate.max(),
 				GeneralDate.max());
 
 		this.companyInfoRepo.add(newComInfo);
