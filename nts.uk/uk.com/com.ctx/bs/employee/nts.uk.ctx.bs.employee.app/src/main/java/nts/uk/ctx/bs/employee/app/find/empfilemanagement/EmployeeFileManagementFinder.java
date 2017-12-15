ca8006ec6cfad76c99bdd2c2c03c90fa7b531ae1
NSVN-TruongVanLai <NSVN-TruongVanLai@users.noreply.github.com>
2017-12-15 10:20:12 +0700

    update code cps001.d

--- nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.app/src/main/java/nts/uk/ctx/bs/employee/app/find/empfilemanagement/EmployeeFileManagementFinder.java
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.app/src/main/java/nts/uk/ctx/bs/employee/app/find/empfilemanagement/EmployeeFileManagementFinder.java
@@ -1,6 +1,7 @@
 package nts.uk.ctx.bs.employee.app.find.empfilemanagement;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.ejb.Stateless;
@@ -17,31 +18,39 @@ public class EmployeeFileManagementFinder {
 
 	@Inject
 	private EmpFileManagementRepository empFileManagementRepository;
-	
+
 	@Inject
 	private EmployeeDataMngInfoRepository emplRepo;
 
 	@Inject
 	private FileStorage fileStorage;
-	
 
 	public EmployeeFileManagementDto getAvaOrMap(String employeeId, int fileType) {
-		List<EmployeeFileManagementDto> lst = empFileManagementRepository.getDataByParams(employeeId, fileType).stream()
-				.map(x -> new EmployeeFileManagementDto(x.getPId(), x.getFileID(), x.getTypeFile().value))
-				.collect(Collectors.toList());
-		return lst.size() > 0 ? lst.get(0) : new EmployeeFileManagementDto();
+		Optional<EmployeeDataMngInfo> empOpt = emplRepo.findByEmpId(employeeId);
+		if (empOpt.isPresent()) {
+			EmployeeDataMngInfo emp= empOpt.get();
+			List<EmployeeFileManagementDto> lst = empFileManagementRepository.getDataByParams(emp.getPersonId(), fileType)
+					.stream().map(x -> new EmployeeFileManagementDto(x.getPId(), x.getFileID(), x.getTypeFile().value))
+					.collect(Collectors.toList());
+			return lst.size() > 0 ? lst.get(0) : new EmployeeFileManagementDto();
+		} else {
+			return null;
+		}
+
 	}
 
 	public boolean checkEmpFileMnExist(String employeeId, int fileType) {
 		EmployeeDataMngInfo employee = emplRepo.findByEmpId(employeeId).get();
-		return empFileManagementRepository.checkObjectExist(employee.getPersonId(), fileType);
+		boolean v = empFileManagementRepository.checkObjectExist(employee.getPersonId(), fileType);
+		return v;
 	}
 
 	public List<EmployeeFileManagementDto> getListDocumentFile(String employeeId) {
 
-		 List<EmployeeFileManagementDto> s =  empFileManagementRepository.getListDocumentFile(employeeId, 2).stream()
+		List<EmployeeFileManagementDto> s = empFileManagementRepository.getListDocumentFile(employeeId, 2).stream()
 				.map(x -> new EmployeeFileManagementDto(x[0].toString(), x[1].toString(),
-						fileStorage.getInfo(x[1].toString()).get().getOriginalName(), Integer.parseInt(x[2].toString())))
+						fileStorage.getInfo(x[1].toString()).get().getOriginalName(),
+						Integer.parseInt(x[2].toString())))
 				.collect(Collectors.toList());
 		return s;
 
