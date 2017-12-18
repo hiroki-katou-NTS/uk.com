5c01474c41fb0e751ee74df66a0e29bdfdadb5cf
NWS-ThanhNC <thanhnc@newwave.vn>
2017-12-18 11:40:21 +0700

    Impl JpaWorkingConditionRepository & JpaWorkingConditionItemRepository

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/workingcondition/WorkingConditionRepository.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/workingcondition/WorkingConditionRepository.java
@@ -12,11 +12,52 @@ import java.util.Optional;
 public interface WorkingConditionRepository {
 
 	/**
+	 * Gets the working condition history.
+	 *
+	 * @param companyId the company id
+	 * @param sId the s id
+	 * @param historyId the history id
+	 * @return the working condition history
+	 */
+	Optional<WorkingCondition> getWorkingConditionHistory(String companyId, String sId,
+			String historyId);
+
+	/**
 	 * Gets the all woking condition.
 	 *
 	 * @param companyId the company id
 	 * @param sId the s id
 	 * @return the all woking condition
 	 */
-	Optional<WorkingCondition> getAllWokingCondition(String companyId, String sId);
+	Optional<WorkingCondition> getBySid(String companyId, String sId);
+
+	/**
+	 * Gets the by sid.
+	 *
+	 * @param sId the s id
+	 * @return the by sid
+	 */
+	Optional<WorkingCondition> getBySid(String sId);
+
+	/**
+	 * Adds the.
+	 *
+	 * @param workingCondition the working condition
+	 */
+	void add(WorkingCondition workingCondition);
+
+	/**
+	 * Update.
+	 *
+	 * @param workingCondition the working condition
+	 */
+	void update(WorkingCondition workingCondition);
+
+	/**
+	 * Delete.
+	 *
+	 * @param historyId the history id
+	 */
+	void  remove(String employeeId);
+
 }
