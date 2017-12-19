cbc9b2a3663358335796f89ad00f68c39abdf2a1
TruongQuocPhong <phong_tq@nittsusystem-vn.com>
2017-12-19 13:42:49 +0700

    update entity KMK013

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/entity/calculation/holiday/KshstRoundingMonthSetPK.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/entity/calculation/holiday/KshstRoundingMonthSetPK.java
@@ -3,12 +3,14 @@ package nts.uk.ctx.at.shared.infra.entity.calculation.holiday;
 import java.io.Serializable;
 
 import javax.persistence.Column;
+import javax.persistence.Embeddable;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Embeddable
 public class KshstRoundingMonthSetPK implements Serializable {
 	private static final long serialVersionUID = 1L;
 	/** 会社ID */
