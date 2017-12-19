36ccfa5fcefbff0c4beb5e5f625e44b929bf9f8e
NWS - Vu Duy Tung <tungvd@newwave.vn>
2017-12-19 09:29:46 +0700

    [KMK003] update tab 10

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/003/a10/kmk003.a10.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/003/a10/kmk003.a10.vm.ts
@@ -1,55 +1,130 @@
 module a10 {
     
+    import WorkTimeSettingEnumDto = nts.uk.at.view.kmk003.a.service.model.worktimeset.WorkTimeSettingEnumDto;
+    import EnumConstantDto = nts.uk.at.view.kmk003.a.service.model.worktimeset.EnumConstantDto;
+    
+    import MainSettingModel = nts.uk.at.view.kmk003.a.viewmodel.MainSettingModel;
+    
     /**
      * Screen Model - Tab 10
-     * 
-     * TODO
+     * 就業時間帯の共通設定 -> 加給設定
+     * WorkTimeCommonSet -> BonusPaySettingCode
      */
     class ScreenModel {
         
         // Screen mode
         isDetailMode: KnockoutObservable<boolean>;
+        workTimeDailyAtr: KnockoutObservable<number>;
+        workTimeMethodSet: KnockoutObservable<number>;
         
-        // Data - Text value: bonusPaySettingCode 加給時間帯コード
+        // Screen data model
+        model: MainSettingModel;
+        settingEnum: WorkTimeSettingEnumDto;
+        
+        // Detail mode - Data
         bonusPaySettingCode: KnockoutObservable<string>;
+        
+        // Simple mode - Data  
         
         /**
          * Constructor
          */
-        constructor() {
+        constructor(screenMode: any, model: MainSettingModel, settingEnum: WorkTimeSettingEnumDto) {
             let _self = this;
+            
+            // Binding data
+            _self.model = model; 
+            _self.settingEnum = settingEnum;
+            
+            // Init all data           
+            _self.bonusPaySettingCode = ko.observable('');                                 
             
             // Detail mode and simple mode is same
             _self.isDetailMode = ko.observable(null);
             _self.isDetailMode.subscribe(newValue => {
                 (newValue === true) ? _self.loadDetailMode() : _self.loadSimpleMode();
+            });                                   
+            _self.workTimeDailyAtr = ko.observable(0);
+            _self.workTimeMethodSet = ko.observable(0);               
+            
+            // Subscribe Detail/Simple mode 
+            screenMode.subscribe((value: any) => {
+                value == "2" ? _self.isDetailMode(true) : _self.isDetailMode(false);
             });
             
-            // Init data
-            _self.bonusPaySettingCode = ko.observable("");
-            
-            //TODO
-            _self.isDetailMode = ko.observable(true);     
+            // Call first time
+            screenMode == "2" ? _self.isDetailMode(true) : _self.isDetailMode(false);
         }
         
         /**
-         * UI: change to Detail mode
+         * UI - Detail: change to Detail mode
          */
         private loadDetailMode(): void {
             let _self = this;
-            //TODO
+            
+            // Subscribe Work Setting mode
+            _self.model.workTimeSetting.workTimeDivision.workTimeDailyAtr.subscribe(newValue => {
+                _self.workTimeDailyAtr(newValue);
+                _self.changeWorkSettingModeDetail();
+            });  
+            _self.model.workTimeSetting.workTimeDivision.workTimeMethodSet.subscribe(newValue => {
+                _self.workTimeMethodSet(newValue);
+                _self.changeWorkSettingModeDetail();
+            });  
+            
+            // Call first time
+            _self.workTimeDailyAtr(_self.model.workTimeSetting.workTimeDivision.workTimeDailyAtr());
+            _self.workTimeMethodSet(_self.model.workTimeSetting.workTimeDivision.workTimeMethodSet());
+            _self.changeWorkSettingModeDetail();
         }
         
         /**
-         * UI: change to Simple mode
+         * UI - Detail: change WorkSetting mode
          */
-        private loadSimpleMode(): void {
-            let _self = this;
+        private changeWorkSettingModeDetail(): void {
+            let _self = this;        
+                       
             //TODO
+            if (_self.workTimeDailyAtr() === _self.settingEnum.workTimeDailyAtr[0].value) {
+                // Regular work
+                switch (_self.workTimeMethodSet()) {
+                    case 0: {
+                        _self.changeBindingDetail(_self.model.fixedWorkSetting.commonSetting.raisingSalarySet);                                    
+                    } break;
+                    case 1: {
+                        //_self.changeBindingDetail(_self.model.diffWorkSetting.commonSetting.lateEarlySet.otherClassSets);
+                    } break;
+                    case 2: {
+                        //_self.changeBindingDetail(_self.model.flowWorkSetting.commonSetting.lateEarlySet.otherClassSets);
+                    } break;               
+                    default: {
+                        //_self.changeBindingDetail(_self.model.fixedWorkSetting.commonSetting.lateEarlySet.otherClassSets);
+                    }
+                } 
+            } else {
+                // Flex work
+                //_self.changeBindingDetail(_self.model.flexWorkSetting.commonSetting.lateEarlySet.otherClassSets); 
+            }               
+        }       
+        
+        /**
+         * UI - Detail: change Binding Detail mode
+         */
+        private changeBindingDetail(raisingSalarySet: KnockoutObservable<string>): void {
+            let _self = this;
+            _self.bonusPaySettingCode = raisingSalarySet;
+        }
+        
+        /**
+         * UI - Simple: change to Simple mode
+         */
+        private loadSimpleMode(): void {          
+            let _self = this;
+            
+            // No simple mode
+            _self.loadDetailMode();
         }
     }
-    
-    
     
     /**
      * Knockout Binding Handler - Tab 10
@@ -75,8 +150,11 @@ module a10 {
                 .mergeRelativePath('/view/kmk/003/a10/index.xhtml').serialize();
             // Get data
             let input = valueAccessor();
+            let screenMode = input.screenMode;
+            let model = input.model;
+            let settingEnum = input.enum;
 
-            let screenModel = new ScreenModel();
+            let screenModel = new ScreenModel(screenMode, model, settingEnum);
             $(element).load(webserviceLocator, () => {
                 ko.cleanNode($(element)[0]);
                 ko.applyBindingsToDescendants(screenModel, $(element)[0]);
