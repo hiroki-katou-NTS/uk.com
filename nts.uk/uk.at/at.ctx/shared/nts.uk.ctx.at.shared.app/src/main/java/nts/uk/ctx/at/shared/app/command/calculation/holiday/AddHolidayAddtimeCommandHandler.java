f235fbc711a3515545b8edf5727eb49c7aa34345
TruongQuocPhong <phong_tq@nittsusystem-vn.com>
2017-12-20 14:45:44 +0700

    update app kmk013

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.app/src/main/java/nts/uk/ctx/at/shared/app/command/calculation/holiday/AddHolidayAddtimeCommandHandler.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.app/src/main/java/nts/uk/ctx/at/shared/app/command/calculation/holiday/AddHolidayAddtimeCommandHandler.java
@@ -4,13 +4,15 @@ package nts.uk.ctx.at.shared.app.command.calculation.holiday;
  * The class Add Holiday Addtime Command Handler
  */
 import java.util.List;
+
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 import nts.arc.layer.app.command.CommandHandlerContext;
 import nts.arc.layer.app.command.CommandHandlerWithResult;
 import nts.uk.ctx.at.shared.dom.calculation.holiday.HolidayAddtime;
 import nts.uk.ctx.at.shared.dom.calculation.holiday.HolidayAddtimeRepository;
 import nts.uk.shr.com.context.AppContexts;
-
+@Stateless
 public class AddHolidayAddtimeCommandHandler extends CommandHandlerWithResult<AddHolidayAddtimeCommand, List<String>> {
 	
 	/** The Repository */
