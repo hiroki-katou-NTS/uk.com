23af96d57af92ae8c6bcb81de784520c0d7b4000
NSVC\anhvd <anh_vd@nittsusystem-vn.com>
2017-12-20 16:41:17 +0700

    KDL014: Fix bug self test.

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/find/stamp/StampDto.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/find/stamp/StampDto.java
@@ -29,23 +29,23 @@ public class StampDto {
 	private String employeeCode;
 	private String pName;
 	
-
+	//Convert from Stamp domain to stamp dto, when exist data and null.
 	public static StampDto fromDomain(StampItem domain, EmployeeRecordImport empInfor) {
-		return new StampDto(domain.getCardNumber().v(), 
-				domain.getAttendanceTime().v(), 
-				domain.getStampCombinationAtr().value,
-				domain.getStampCombinationAtr().name,
-				domain.getSiftCd().v(), 
-				domain.getStampMethod().value,
-				domain.getStampMethod().name,
-				domain.getStampAtr().value,
-				domain.getStampAtr().name,
-				domain.getWorkLocationCd().v(), 
-				domain.getWorkLocationName().v(),
-				domain.getGoOutReason().value,
-				domain.getGoOutReason().name,
-				domain.getDate(),
-				domain.getEmployeeId(),
+		return new StampDto(domain.getCardNumber() == null ? "" : domain.getCardNumber().v(), 
+				domain.getAttendanceTime() == null? -1 : domain.getAttendanceTime().v(), 
+				domain.getStampCombinationAtr() == null? -1 : domain.getStampCombinationAtr().value,
+				domain.getStampCombinationAtr() == null? "" : domain.getStampCombinationAtr().name,
+				domain.getSiftCd() == null? "" : domain.getSiftCd().v(), 
+				domain.getStampMethod() == null? -1 : domain.getStampMethod().value,
+				domain.getStampMethod() == null? "" : domain.getStampMethod().name,
+				domain.getStampAtr() == null? -1 : domain.getStampAtr().value,
+				domain.getStampAtr() == null? "" : domain.getStampAtr().name,
+				domain.getWorkLocationCd() == null? "" : domain.getWorkLocationCd().v(), 
+				domain.getWorkLocationName() == null? "" : domain.getWorkLocationName().v(),
+				domain.getGoOutReason() == null? -1 : domain.getGoOutReason().value,
+				domain.getGoOutReason() == null? "" : domain.getGoOutReason().name,
+				domain.getDate() == null? null : domain.getDate(),
+				domain.getEmployeeId() == null? "" : domain.getEmployeeId(),
 				empInfor.getEmployeeCode(),
 				empInfor.getPname());
 	}
--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdl/014/a/kdl014.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdl/014/a/kdl014.a.vm.ts
@@ -74,7 +74,11 @@ module kdl014.a.viewmodel {
                 
                 if (lstStamp.length > 0) {
                     _.forEach(lstStamp, function(item) {
-                        lstSource.push(new StampModel(moment(item.date, 'YYYY/MM/DD').format('YYYY/MM/DD (ddd)'), _.padStart(nts.uk.time.parseTime(item.attendanceTime, true).format(), 5, '0'), item.stampReasonName, item.stampAtrName, item.stampMethodName, item.workLocationName, item.stampCombinationName));
+                        //When cardNumber != '';
+                        if(item.cardNumber != ''){
+                            lstSource.push(new StampModel(moment(item.date, 'YYYY/MM/DD').format('YYYY/MM/DD (ddd)'), _.padStart(nts.uk.time.parseTime(item.attendanceTime, true).format(), 5, '0'), item.stampReasonName, item.stampAtrName, item.stampMethodName, item.workLocationName, item.stampCombinationName));
+                        }
+                        
                         //Set employeeName by Dto of emloyeeName.
                         self.employeeName = item.pname;
                         self.employeeCode = item.employeeCode;
