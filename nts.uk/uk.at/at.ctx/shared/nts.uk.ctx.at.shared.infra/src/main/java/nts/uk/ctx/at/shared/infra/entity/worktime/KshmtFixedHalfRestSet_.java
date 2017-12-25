2611d6ae84abba1969064c491937e725b533acb7
NWS-ThanhNC <thanhnc@newwave.vn>
2017-12-25 09:19:25 +0700

    Add KMK003's metamodel

---
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/entity/worktime/KshmtFixedHalfRestSet_.java
@@ -0,0 +1,18 @@
+package nts.uk.ctx.at.shared.infra.entity.worktime;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import nts.uk.ctx.at.shared.infra.entity.worktime.KshmtFixedHalfRestSetPK;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-12-23T17:54:14")
+@StaticMetamodel(KshmtFixedHalfRestSet.class)
+public class KshmtFixedHalfRestSet_ { 
+
+    public static volatile SingularAttribute<KshmtFixedHalfRestSet, KshmtFixedHalfRestSetPK> kshmtFixedHalfRestSetPK;
+    public static volatile SingularAttribute<KshmtFixedHalfRestSet, Integer> startTime;
+    public static volatile SingularAttribute<KshmtFixedHalfRestSet, Integer> endTime;
+    public static volatile SingularAttribute<KshmtFixedHalfRestSet, Integer> exclusVer;
+    public static volatile SingularAttribute<KshmtFixedHalfRestSet, Integer> periodNo;
+
+}
---
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/entity/worktime/KshmtFlowStampReflect_.java
@@ -0,0 +1,18 @@
+package nts.uk.ctx.at.shared.infra.entity.worktime;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import nts.uk.ctx.at.shared.infra.entity.worktime.KshmtFlowStampReflectPK;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-12-23T17:54:14")
+@StaticMetamodel(KshmtFlowStampReflect.class)
+public class KshmtFlowStampReflect_ { 
+
+    public static volatile SingularAttribute<KshmtFlowStampReflect, KshmtFlowStampReflectPK> kshmtFlowStampReflectPK;
+    public static volatile SingularAttribute<KshmtFlowStampReflect, Integer> strClock;
+    public static volatile SingularAttribute<KshmtFlowStampReflect, Integer> endClock;
+    public static volatile SingularAttribute<KshmtFlowStampReflect, Integer> attendAtr;
+    public static volatile SingularAttribute<KshmtFlowStampReflect, Integer> exclusVer;
+
+}
