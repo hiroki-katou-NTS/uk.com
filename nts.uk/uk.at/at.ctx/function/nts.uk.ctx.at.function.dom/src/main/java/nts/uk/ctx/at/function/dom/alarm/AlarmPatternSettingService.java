0dc7f0094d1d190509f9871ee3fc47afda679e65
ThuongDangXuan <thuong_dx@nittsusystem-vn.com>
2017-12-26 18:45:46 +0700

    update domain + update command for domain

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/alarm/AlarmPatternSettingService.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/alarm/AlarmPatternSettingService.java
@@ -1,5 +1,5 @@
 package nts.uk.ctx.at.function.dom.alarm;
 
 public interface AlarmPatternSettingService {
-	public void checkDuplicateCode(String alarmPatternCode);
+	public boolean checkDuplicateCode(String alarmPatternCode);
 }
