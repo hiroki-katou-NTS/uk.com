a6f12aeefdd5c9f6036f253f88c94d5b8316aa0b
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2017-12-27 17:25:15 +0700

    fix bug: #86885

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/007/a/layout-control-ko-ext.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/007/a/layout-control-ko-ext.ts
@@ -1874,12 +1874,14 @@ module nts.custombinding {
                                         data = _(data)
                                             .filter(m => !m.isAbolition)
                                             .filter(f => {
-                                                if (item.id === "COM1_00000000000000000000000_CS00002") {
-                                                    return f.id !== "COM1_000000000000000_CS00002_IS00003";
-                                                }
+                                                if (location.href.indexOf('/view/cps/007/a/') > -1) {
+                                                    if (item.id === "COM1_00000000000000000000000_CS00002") {
+                                                        return f.id !== "COM1_000000000000000_CS00002_IS00003";
+                                                    }
 
-                                                if (item.id === "COM1_00000000000000000000000_CS00003") {
-                                                    return f.id !== "COM1_000000000000000_CS00003_IS00020";
+                                                    if (item.id === "COM1_00000000000000000000000_CS00003") {
+                                                        return f.id !== "COM1_000000000000000_CS00003_IS00020";
+                                                    }
                                                 }
 
                                                 return true;
