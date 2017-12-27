5bd8eef4d75c7071515bd2476dcdbd0f39f48c2e
NSVN-PhamVanThanh <thanh_pv@nittsusystem-vn.com>
2017-12-27 13:01:28 +0700

    Update ExtractionPeriodDaily

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.app/src/main/java/nts/uk/ctx/at/function/app/command/alarm/extractionrange/ExtractionRangeDto.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.app/src/main/java/nts/uk/ctx/at/function/app/command/alarm/extractionrange/ExtractionRangeDto.java
@@ -4,6 +4,7 @@ import nts.arc.enums.EnumAdaptor;
 import nts.uk.ctx.at.function.dom.alarm.extractionrange.EndDate;
 import nts.uk.ctx.at.function.dom.alarm.extractionrange.PreviousClassification;
 import nts.uk.ctx.at.function.dom.alarm.extractionrange.StartDate;
+import nts.uk.ctx.at.function.dom.alarm.extractionrange.daily.EndSpecify;
 import nts.uk.ctx.at.function.dom.alarm.extractionrange.daily.ExtractionPeriodDaily;
 import nts.uk.ctx.at.function.dom.alarm.extractionrange.daily.StartSpecify;
 
@@ -44,7 +45,11 @@ public class ExtractionRangeDto {
 		
 		EndDate endDate = new EndDate(endSpecify);
 		
-		
-		return null;
+		if(endSpecify == EndSpecify.DAYS.value){
+			endDate.setEndDay(EnumAdaptor.valueOf(endPreviousDay, PreviousClassification.class), (int)endDay, (int)endDay==0?true:false);
+		}else if(endSpecify == EndSpecify.MONTH.value){
+			endDate.setEndMonth(EnumAdaptor.valueOf(endPreviousMonth, PreviousClassification.class), (int)endMonth, (int)endMonth==0?true:false);
+		}
+		return new ExtractionPeriodDaily(extractionId, extractionRange, startDate, endDate);
 	}
 }
