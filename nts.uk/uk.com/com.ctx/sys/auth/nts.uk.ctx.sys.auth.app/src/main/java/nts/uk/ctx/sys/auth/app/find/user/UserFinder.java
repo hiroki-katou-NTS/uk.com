342c616dc1ebd7d067dee40a485a5a21a673ac21
NSVN-PhamVanThanh <thanh_pv@nittsusystem-vn.com>
2017-12-28 11:25:53 +0700

    Update ExtractionPeriodDaily

--- nts.uk/uk.com/com.ctx/sys/auth/nts.uk.ctx.sys.auth.app/src/main/java/nts/uk/ctx/sys/auth/app/find/user/UserFinder.java
+++ nts.uk/uk.com/com.ctx/sys/auth/nts.uk.ctx.sys.auth.app/src/main/java/nts/uk/ctx/sys/auth/app/find/user/UserFinder.java
@@ -3,7 +3,6 @@ package nts.uk.ctx.sys.auth.app.find.user;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.ejb.Stateless;
