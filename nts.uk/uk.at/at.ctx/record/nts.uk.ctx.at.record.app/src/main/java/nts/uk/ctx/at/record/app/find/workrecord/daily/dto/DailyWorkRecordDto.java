f16c509eab5e61d048f490d0cf73a94498290e9c
NTS\tin_dh <tin_dh@nittsusystem.co.jp>
2017-12-29 09:06:44 +0900

    update convert attendance item

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/find/workrecord/daily/dto/DailyWorkRecordDto.java
+++
@@ -1,8 +0,0 @@
-package nts.uk.ctx.at.record.app.find.workrecord.daily.dto;
-
-import nts.uk.ctx.at.record.app.find.workrecord.WorkRecordDto;
-import nts.uk.ctx.at.shared.app.util.attendanceitem.type.ConvertibleAttendanceItem;
-
-public class DailyWorkRecordDto extends WorkRecordDto implements ConvertibleAttendanceItem {
-
-}
