a5208b59180e020804bc40507ec0bbbfd94a34b8
NWS - Vu Duy Tung <tungvd@newwave.vn>
2018-01-04 20:05:15 +0700

    [Fix_Bug] hot fix 2

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/repository/worktime/fixedset/JpaFixRestHalfdayTzSetMemento.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/repository/worktime/fixedset/JpaFixRestHalfdayTzSetMemento.java
@@ -63,57 +63,31 @@ public class JpaFixRestHalfdayTzSetMemento implements FixRestTimezoneSetSetMemen
 	@Override
 	public void setLstTimezone(List<DeductionTime> lstTimezone) {
 		// KSHMT_FIXED_HALF_REST_SET
-//		if (CollectionUtil.isEmpty(lstTimezone)) {
-//			// remove by condition
-//			this.entity.setKshmtFixedHalfRestSets(this.entity.getKshmtFixedHalfRestSets().stream()
-//					.filter(set -> set.getKshmtFixedHalfRestSetPK().getAmPmAtr() != this.type.value)
-//					.collect(Collectors.toList()));
-//			return;
-//		}
-//		
-//		// Get settings with other type
-//		List<KshmtFixedHalfRestSet> otherList = this.entity.getKshmtFixedHalfRestSets().stream()
-//				.filter(entity -> entity.getKshmtFixedHalfRestSetPK().getAmPmAtr() != this.type.value)
-//				.collect(Collectors.toList());
-//		
-//		// Get settings with current type
-//		List<KshmtFixedHalfRestSet> currentList = this.entity.getKshmtFixedHalfRestSets().stream()
-//				.filter(entity -> entity.getKshmtFixedHalfRestSetPK().getAmPmAtr() == this.type.value)
-//				.collect(Collectors.toList());
-//		
-//		
-//		Map<KshmtFixedHalfRestSetPK, KshmtFixedHalfRestSet> currentSets = this.entity.getKshmtFixedHalfRestSets().stream()
-//				.map(KshmtFixedHalfRestSet.class::cast)
-//				.filter(entity -> entity.getKshmtFixedHalfRestSetPK().getAmPmAtr() == this.type.value)
-//				.collect(Collectors.toMap(entity -> entity.getKshmtFixedHalfRestSetPK(), Function.identity()));
-//		
-//		currentList.addAll(lstTimezone.stream()
-//				.map(domain -> {
-//					KshmtFixedHalfRestSetPK pk = new KshmtFixedHalfRestSetPK(this.cid, this.worktimeCd, this.type.value);				
-//					KshmtFixedHalfRestSet entity = currentSets.get(pk);
-//					if (entity == null) {
-//						entity = new KshmtFixedHalfRestSet();
-//						entity.setKshmtFixedHalfRestSetPK(pk);
-//					}			
-//					domain.saveToMemento(new JpaFixedRestTZDeductionTimeSetMemento<KshmtFixedHalfRestSet>(entity));
-//					return entity;
-//				})
-//				.collect(Collectors.toList()));
-//		
-//		this.entity.setKshmtFixedHalfRestSets(currentList);
-		
 		if (CollectionUtil.isEmpty(lstTimezone)) {
-			this.entity.setKshmtFixedHalfRestSets(new ArrayList<>());
-		} else {
-			periodNo = 0;
-			this.entity.setKshmtFixedHalfRestSets(lstTimezone.stream().map(domain -> {
-				periodNo++;
-				KshmtFixedHalfRestSetPK pk = new KshmtFixedHalfRestSetPK(this.cid, this.worktimeCd, this.type.value, periodNo);				
-				KshmtFixedHalfRestSet entity = new KshmtFixedHalfRestSet();
-				entity.setKshmtFixedHalfRestSetPK(pk);
-				domain.saveToMemento(new JpaFixedRestTZDeductionTimeSetMemento<KshmtFixedHalfRestSet>(entity));
-				return entity;
-			}).collect(Collectors.toList()));
+			// remove by condition
+			this.entity.setKshmtFixedHalfRestSets(this.entity.getKshmtFixedHalfRestSets().stream()
+					.filter(set -> set.getKshmtFixedHalfRestSetPK().getAmPmAtr() != this.type.value)
+					.collect(Collectors.toList()));
+			return;
 		}
+		
+		// Get settings with other type
+		List<KshmtFixedHalfRestSet> otherList = this.entity.getKshmtFixedHalfRestSets().stream()
+				.filter(entity -> entity.getKshmtFixedHalfRestSetPK().getAmPmAtr() != this.type.value)
+				.collect(Collectors.toList());
+		
+		periodNo = 0;
+		otherList.addAll(lstTimezone.stream()
+				.map(domain -> {
+					periodNo++;
+					KshmtFixedHalfRestSetPK pk = new KshmtFixedHalfRestSetPK(this.cid, this.worktimeCd, this.type.value, periodNo);				
+					KshmtFixedHalfRestSet entity = new KshmtFixedHalfRestSet();
+					entity.setKshmtFixedHalfRestSetPK(pk);
+					domain.saveToMemento(new JpaFixedRestTZDeductionTimeSetMemento<KshmtFixedHalfRestSet>(entity));
+					return entity;
+				})
+				.collect(Collectors.toList()));
+		
+		this.entity.setKshmtFixedHalfRestSets(otherList);
 	}
 }
