6ae4791a224d0ab02bb9bec6df5591794bcdb857
ThuongDangXuan <thuong_dx@nittsusystem-vn.com>
2018-01-06 17:21:32 +0700

    update UI and dto response from server

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kal/004/a/kal004.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kal/004/a/kal004.a.vm.ts
@@ -10,21 +10,25 @@ module kal004.a.model {
     import share = kal004.share.model;
     import service = kal004.a.service; 
         export class ScreenModel {
-                items: KnockoutObservableArray<share.AlarmPatternSettingDto>;
-                columns2: KnockoutObservableArray<any>;
+                alarmSource: KnockoutObservableArray<share.AlarmPatternSettingDto>;
+                alarmHeader: KnockoutObservableArray<any>;
                 currentCode: KnockoutObservable<string>;  
                 createMode : KnockoutObservable<boolean>;
                 alarmCode : KnockoutObservable<string>;
                 alarmName : KnockoutObservable<string>;
                 tabs: KnockoutObservableArray<nts.uk.ui.NtsTabPanelModel>;  
-                selectedTab: KnockoutObservable<string>;                      
+                selectedTab: KnockoutObservable<string>; 
+                checkConditionList: KnockoutObservableArray<share.ModelCheckConditonCode>;
+                checkHeader: KnockoutObservableArray<nts.uk.ui.NtsGridListColumn>;
+                currentCodeListSwap: KnockoutObservableArray<any>;
+                checkSource :  Array<share.ModelCheckConditonCode>;                            
             
                 // SetPermission
                 setPermissionModel: any = new nts.uk.at.view.kal004.tab3.viewmodel.ScreenModel();
             
             constructor() {
                 let self = this;
-                self.items = ko.observableArray([]);                
+                self.alarmSource = ko.observableArray([]);                
                 self.tabs = ko.observableArray([
                     { id: 'tab-1', title: getText('KAL004_12'), content: '.tab-content-1', enable: ko.observable(true), visible: ko.observable(true) },
                     { id: 'tab-2', title: getText('KAL004_13'), content: '.tab-content-2', enable: ko.observable(true), visible: ko.observable(true) },
@@ -32,9 +36,9 @@ module kal004.a.model {
                 ]);
                 self.selectedTab = ko.observable('tab-1');
                             
-                self.columns2 = ko.observableArray([
-                { headerText: 'コード', key: 'alarmPatternCD', width: 80 },
-                { headerText: '名称', key: 'alarmPatternName', width: 220 },
+                self.alarmHeader = ko.observableArray([
+                { headerText: 'コード', key: 'alarmPatternCD', width: 60 },
+                { headerText: '名称', key: 'alarmPatternName', width: 190 },
                 ]);
                 self.alarmCode = ko.observable('');
                 self.alarmName = ko.observable('');
@@ -44,25 +48,67 @@ module kal004.a.model {
                     if(newV =='') self.createMode(true);
                     else {                        
                         self.createMode(false);
-                        let currentAlarm = _.find(self.items(), function(a){return a.alarmPatternCD == newV});
+                        let currentAlarm = _.find(self.alarmSource(), function(a){return a.alarmPatternCD == newV});
                         self.alarmCode(currentAlarm.alarmPatternCD);
                         self.alarmName(currentAlarm.alarmPatternName);
+                        
+                        var currentCodeListSwap1 = [];
+                        var checkSource1 =self.checkSource.slice();    
+                        currentAlarm.checkConList.forEach((x) =>{
+                            x.checkConditionCodes.forEach((y)=>{
+                                  let CD =_.find(self.checkSource, { 'category': x.alarmCategory, 'checkConditonCode': y });                          
+                                  currentCodeListSwap1.push(CD.GUID);
+                                            
+                                 _.remove(checkSource1, function(n) {
+                                              return n.category == x.alarmCategory && n.checkConditonCode==y;
+                                            });                                 
+                                    });    
+                        });
+                        self.checkConditionList(checkSource1);
+                        self.currentCodeListSwap(currentCodeListSwap1);
+                           
+
                     }                        
                 });
+                
+                self.checkConditionList = ko.observableArray([]);
+                self.checkSource = [];
+                    
+                self.checkHeader = ko.observableArray([
+                    { headerText: getText('KAL004_21'), key: 'categoryName', width: 100 },
+                    { headerText: getText('KAL004_17'), key: 'checkConditonCode', width: 50 },
+                    { headerText: getText('KAL004_18'), key: 'checkConditionName', width: 200 }
+                ]);
+    
+                self.currentCodeListSwap = ko.observableArray([]);
+                
             }
-            
+
             public  startPage(): any{
-                let self = this;
-                service.getAlarmPattern().done((res) =>{
-                    if(res.length>0){
-                        self.items(res);
-                        self.currentCode(res[0].alarmPatternCD);   
-                    }
-                }).fail((error) =>{
-                    nts.uk.ui.dialog.alert({ messageId: error.messageId });
-                });
+                let self = this;     
+                
+                
+                service.getCheckConditionCode().done((res1) =>{                        
+                    self.checkConditionList(res1);
+                    self.checkSource =res1;
+                    
+                    service.getAlarmPattern().done((res) =>{
+                        if(res.length>0){
+                            self.alarmSource(res);
+                            self.currentCode(res[0].alarmPatternCD);   
+                        }
+                    }).fail((error) =>{
+                        nts.uk.ui.dialog.alert({ messageId: error.messageId });
+                    });                                        
+                }).fail((error1)=>{
+                        nts.uk.ui.dialog.alert({ messageId: error1.messageId });
+                });                
+                
             }
  
-    }   
+    }
+    
+    
+    
 }
 
--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kal/004/share/model.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kal/004/share/model.ts
@@ -15,12 +15,32 @@ module kal004.share.model {
     }
     
     export interface AlarmCheckConditonCodeDto{
-        category: number;
+        category: EnumConstantDto;
         checkConditonCode: string;
         checkConditionName: string;
         listRoleId: Array<string>;    
     }
-    
+    export class ModelCheckConditonCode {
+           GUID: string;
+           category: number;
+           categoryName: string;
+           checkConditonCode: string;
+           checkConditionName: string;
+           listRoleId: Array<string>; 
+           constructor(category: number,categoryName: string, checkConditonCode: string, checkConditionName: string, listRoleId: Array<string>) {
+               this.category = category;
+               this.categoryName = categoryName;
+               this.checkConditonCode = checkConditonCode;
+               this.checkConditionName = checkConditionName;
+               this.listRoleId = listRoleId;
+               this.GUID = nts.uk.util.randomId();               
+           }
+    }
+      export interface EnumConstantDto {
+        value: number;
+        fieldName: string;
+        localizedName: string;
+      }  
     
     
 }
