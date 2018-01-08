1d62ff84346813af1e2496017aab1c867c0cfde2
nws_tiendd <tiendd_int@newwave.vn>
2018-01-08 11:35:34 +0700

    Fix timerange validate

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/003/base/timerange/timerange.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/003/base/timerange/timerange.ts
@@ -103,6 +103,9 @@ module kmk003.base.timerange {
             var startTime = isGlobalValidate ? self.convertTimeInput(startInputId) : self.startTime();
             var endTime = isGlobalValidate ? self.convertTimeInput(endInputId) : self.endTime();
             var paramId = element.data('param');
+            if($('#' + startInputId).prop('disabled') && $('#' + endInputId).prop('disabled')) {
+                return true;
+            }
             
             // clear error
             $('#' + startInputId).ntsError('clear');
@@ -123,7 +126,7 @@ module kmk003.base.timerange {
                 }
                 return false;
             }
-            return true
+            return true;
         }
         
         private convertTimeInput(id: string): number {
