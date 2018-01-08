cfa575bac23f3a0cf0eee13277099ddd91ec8964
NSVN-TrieuKhacTu <tu_tk@nittsusystem-vn.com>
2018-01-08 17:44:40 +0700

    update code server kal003

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/workrecord/erroralarm/FixedConditionWorkRecordName.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/workrecord/erroralarm/FixedConditionWorkRecordName.java
@@ -1,4 +1,4 @@
-package nts.uk.ctx.at.function.dom.alarm.checkcondition;
+package nts.uk.ctx.at.record.dom.workrecord.erroralarm;
 
 import nts.arc.primitive.StringPrimitiveValue;
 import nts.arc.primitive.constraint.StringMaxLength;
