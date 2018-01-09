a03f8dab347f22f8e353b252d122eaa2322893d5
nws-anhnm <anhnm@newwave.vn>
2018-01-09 14:28:11 +0700

    {KMK003} fix bug duong ngo

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/003/a2/kmk003.a2.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/003/a2/kmk003.a2.vm.ts
@@ -236,15 +236,16 @@ module a2 {
                 //============= Convert =============
                 let item: TimezoneModel = self.parentModel.predetemineTimeSetting.prescribedTimezoneSetting.shiftOne;
                 item.valueChangedNotifier.subscribe(() => {
-                    // TODO: bind data
+                    // TODO: anh Duong check lai cho nay nhe
+                    let timeRange: TimePeriod = {
+                        startTime: item.start(),
+                        endTime: item.end()
+                    }
+                    self.dataSourceOneDaySimpleMode([]);
+                    self.dataSourceOneDaySimpleMode.push(new TimeZoneModel(timeRange, emTimezone ? emTimezone.timezone.rounding.roundingTime() : 0,
+                        emTimezone ? emTimezone.timezone.rounding.rounding() : 0));
                 });
-                let timeRange: TimePeriod = {
-                    startTime: item.start(),
-                    endTime: item.end()
-                }
-                self.dataSourceOneDaySimpleMode([]);
-                self.dataSourceOneDaySimpleMode.push(new TimeZoneModel(timeRange, emTimezone ? emTimezone.timezone.rounding.roundingTime() : 0,
-                    emTimezone ? emTimezone.timezone.rounding.rounding() : 0));
+                item.valueChangedNotifier.valueHasMutated();
             }
             // Detail mode
             else {
