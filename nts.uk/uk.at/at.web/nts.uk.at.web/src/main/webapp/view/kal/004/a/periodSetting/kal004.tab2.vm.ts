a089550293adcc65fa54e34f8c7d5505cc32591b
NSVC-DinhTheLam <lam_dt@nittsusystem-vn.com>
2018-01-09 15:16:28 +0700

    Update KAL004 UI

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kal/004/a/periodSetting/kal004.tab2.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kal/004/a/periodSetting/kal004.tab2.vm.ts
@@ -1,14 +1,28 @@
 module nts.uk.at.view.kal004.tab2.viewModel {
+    import share = nts.uk.at.view.kal004.share.model;
     
     export class ScreenModel {
-        listAlarmPerSetting: KnockoutObservableArray<any>;
+        listCheckConditionCode: KnockoutObservableArray<share.ModelCheckConditonCode> = ko.observableArray([]);
+        listCheckCondition: KnockoutObservableArray<share.CheckCondition> = ko.observableArray([]);
+        
         constructor() {
+            var self = this;
+            self.listCheckConditionCode.subscribe((newList) => {
+                self.changeCheckCondition(newList);
+            });
+        }
+        
+        private changeCheckCondition(listCheckCode: Array<share.ModelCheckConditonCode>): void {
+            var self = this;
             
         }
-        private OpenDialog(): void {
+        
+        private openDialog(): void {
              nts.uk.ui.windows.sub.modal("../b/index.xhtml").onClosed(() => {
                 console.log("success!");
             }); 
         }
     }
+    
+    
 }
