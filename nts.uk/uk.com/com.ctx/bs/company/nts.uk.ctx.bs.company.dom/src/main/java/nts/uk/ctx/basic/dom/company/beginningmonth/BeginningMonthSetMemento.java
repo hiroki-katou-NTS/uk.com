70e40a0e54440f79e9a160ae096fa79727f60dc2
haivh <haivh@newwave.vn>
2018-01-10 11:43:39 +0700

    fix bug redmine: http://192.168.50.4:3000/issues/87175

--- nts.uk/uk.com/com.ctx/bs/company/nts.uk.ctx.bs.company.dom/src/main/java/nts/uk/ctx/basic/dom/company/beginningmonth/BeginningMonthSetMemento.java
+++ nts.uk/uk.com/com.ctx/bs/company/nts.uk.ctx.bs.company.dom/src/main/java/nts/uk/ctx/basic/dom/company/beginningmonth/BeginningMonthSetMemento.java
@@ -4,10 +4,26 @@
  *****************************************************************/
 package nts.uk.ctx.basic.dom.company.beginningmonth;
 
+import nts.uk.ctx.bs.company.dom.company.CompanyId;
+import nts.uk.ctx.bs.company.dom.company.StartMonth;
+
 /**
  * The Interface BeginningMonthSetMemento.
  */
 public interface BeginningMonthSetMemento {
+	
+	/**
+	 * Sets the company id.
+	 *
+	 * @param companyId the new company id
+	 */
+	void setCompanyId(CompanyId companyId);
 
+	/**
+	 * Sets the month.
+	 *
+	 * @param startMonth the new month
+	 */
+	void setMonth(StartMonth startMonth);
 
 }
