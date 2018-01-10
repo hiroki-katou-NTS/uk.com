d80f2185e8fc8d5f07183d05ec3db50e9ca9c8f9
NSVC\dudt <du_dt@nittsusystem-vn.com>
2018-01-10 17:12:54 +0700

    KAF000: dinh nghia programID

--- nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/com/program/ProgramIdConsts.java
+++ nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/com/program/ProgramIdConsts.java
@@ -600,7 +600,9 @@ public class ProgramIdConsts {
 	public static final String KSC001I = "KSC001I";
 	public static final String KSC001K = "KSC001K";
 	public static final String KAF002A = "KAF002A";
+	public static final String KAF002B = "KAF002B";
 	public static final String KAF004A = "KAF004A";
+	public static final String KAF004B = "KAF004B";
 	public static final String KAF005A = "KAF005A";
 	public static final String KAF007A = "KAF007A";
 	public static final String KAF009A = "KAF009A";
