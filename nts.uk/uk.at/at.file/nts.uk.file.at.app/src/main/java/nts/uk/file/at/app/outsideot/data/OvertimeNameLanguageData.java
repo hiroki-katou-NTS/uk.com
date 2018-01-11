d2ec9a27be9b13524ed608a3d0115a6641cc5bf4
nws-duongtv <nws-duongtv@newwave.vn>
2018-01-11 19:31:41 +0700

    [KMK010] fix report

--- nts.uk/uk.at/at.file/nts.uk.file.at.app/src/main/java/nts/uk/file/at/app/outsideot/data/OvertimeNameLanguageData.java
+++ nts.uk/uk.at/at.file/nts.uk.file.at.app/src/main/java/nts/uk/file/at/app/outsideot/data/OvertimeNameLanguageData.java
@@ -11,6 +11,11 @@ import lombok.Setter;
  * The Class OvertimeNameLanguageData.
  */
 
+/**
+ * Gets the view time.
+ *
+ * @return the view time
+ */
 @Getter
 @Setter
 public class OvertimeNameLanguageData{
@@ -20,11 +25,27 @@ public class OvertimeNameLanguageData{
 	
 	/** The overtime no. */
 	private Integer overtimeNo;
+	
+	/** The is use. */
+	private Boolean isUse;
+	
+	/** The view time. */
+	private String viewTime;
 
-	public OvertimeNameLanguageData(String language, Integer overtimeNo) {
+	/**
+	 * Instantiates a new overtime name language data.
+	 *
+	 * @param language the language
+	 * @param overtimeNo the overtime no
+	 * @param isUse the is use
+	 */
+	public OvertimeNameLanguageData(String language, Integer overtimeNo, Boolean isUse) {
+		super();
 		this.language = language;
 		this.overtimeNo = overtimeNo;
+		this.isUse = isUse;
 	}
+
 	
 
 }
