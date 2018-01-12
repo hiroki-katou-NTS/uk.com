399330304f34ef8d352fcdb5bdaca00bf490dd70
NWS-ThanhNC <thanhnc@newwave.vn>
2018-01-12 11:20:52 +0700

    Update 当月の期間を算出する

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/workrule/closure/service/DefaultClosureServiceImpl.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/workrule/closure/service/DefaultClosureServiceImpl.java
@@ -76,7 +76,12 @@ public class DefaultClosureServiceImpl implements ClosureService {
 	 * @return the expection date
 	 */
 	private GeneralDate getExpectionDate(Boolean lastDayOfMonth, int year, int month, int day) {
-		month = (month == 0) ? 12 : month;
+		
+		if(month == 0) {
+			month =  12;
+			year = year - 1;
+		}
+		
 		return (lastDayOfMonth || !DateUtil.isDateOfMonth(year, month, day))
 				? DateUtil.getLastDateOfMonth(year, month) : GeneralDate.ymd(year, month, day);
 	}
