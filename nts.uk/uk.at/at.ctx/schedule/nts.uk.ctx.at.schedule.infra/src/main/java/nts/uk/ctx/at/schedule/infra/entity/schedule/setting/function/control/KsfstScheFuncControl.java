a1f619e19270b6c811aba55f621725938ff4f1a7
NSVC\tanlv <tan_lv@nittsusystem-vn.com>
2018-01-16 16:21:05 +0700

    KSM011: Add Infra code

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.infra/src/main/java/nts/uk/ctx/at/schedule/infra/entity/schedule/setting/function/control/KsfstScheFuncControl.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.infra/src/main/java/nts/uk/ctx/at/schedule/infra/entity/schedule/setting/function/control/KsfstScheFuncControl.java
@@ -1,15 +1,17 @@
 package nts.uk.ctx.at.schedule.infra.entity.schedule.setting.function.control;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import nts.uk.ctx.at.schedule.dom.schedule.setting.function.control.DateInWeek;
 import nts.uk.shr.infra.data.entity.UkJpaEntity;
 
 /**
@@ -30,127 +32,130 @@ public class KsfstScheFuncControl extends UkJpaEntity implements Serializable {
 	
 	/** アラームチェック使用区分 */
 	@Column(name = "ALARM_CHECK_USE_CLS")
-	private int alarmCheckUseCls;
+	public int alarmCheckUseCls;
 	
 	/** 確定使用区分 */
 	@Column(name = "CONFIRMED_CLS")
-	private int confirmedCls;
+	public int confirmedCls;
 	
 	/** 公開使用区分 */
 	@Column(name = "PUBLIC_CLS")
-	private int publicCls;
+	public int publicCls;
 
 	/** 出力使用区分 */
 	@Column(name = "OUTPUT_CLS")
-	private int outputCls;
+	public int outputCls;
 
 	/** 勤務希望使用区分 */
 	@Column(name = "WORK_DORMITION_CLS")
-	private int workDormitionCls;
+	public int workDormitionCls;
 
 	/** チーム使用区分 */
 	@Column(name = "TEAM_CLS")
-	private int teamCls;
+	public int teamCls;
 
 	/** ランク使用区分 */
 	@Column(name = "RANK_CLS")
-	private int rankCls;
+	public int rankCls;
 
 	/** 週開始曜日 */
 	@Column(name = "START_DATE_IN_WEEK")
-	private DateInWeek startDateInWeek;
+	public int startDateInWeek;
 
 	/** 略名 */
 	@Column(name = "SHORT_NAME_DISP")
-	private int shortNameDisp;
+	public int shortNameDisp;
 
 	/** 時刻 */
 	@Column(name = "TIME_DISP")
-	private int timeDisp;
+	public int timeDisp;
 
 	/** 記号 */
 	@Column(name = "SYMBOL_DISP")
-	private int symbolDisp;
+	public int symbolDisp;
 
 	/** ２８日 */
 	@Column(name = "28_DAYS_CYCLE")
-	private int twentyEightDaysCycle;
+	public int twentyEightDaysCycle;
 
 	/** 末日 */
 	@Column(name = "LAST_DAY_DISP")
-	private int lastDayDisp;
+	public int lastDayDisp;
 
 	/** 個人別使用区分 */
 	@Column(name = "INDIVIDUAL_DISP")
-	private int individualDisp;
+	public int individualDisp;
 
 	/** 日付別使用区分 */
 	@Column(name = "DISP_BY_DATE")
-	private int dispByDate;
+	public int dispByDate;
 
 	/** シフト別使用区分 */
 	@Column(name = "INDICATION_BY_SHIFT")
-	private int indicationByShift;
+	public int indicationByShift;
 
 	/** 通常勤務 */
 	@Column(name = "REGULAR_WORK")
-	private int regularWork;
+	public int regularWork;
 
 	/** 流動勤務 */
 	@Column(name = "FLUID_WORK")
-	private int fluidWork;
+	public int fluidWork;
 
 	/** フレックス勤務 */
 	@Column(name = "WORKING_FOR_FLEX")
-	private int workingForFlex;
+	public int workingForFlex;
 
 	/** 残業枠勤務 */
 	@Column(name = "OVERTIME_WORK")
-	private int overtimeWork;
+	public int overtimeWork;
 
 	/** 通常作成使用区分 */
 	@Column(name = "NORMAL_CREATION")
-	private int normalCreation;
+	public int normalCreation;
 
 	/** ｼﾐｭﾚｰｼｮﾝ使用区分 */
 	@Column(name = "SIMULATION_CLS")
-	private int simulationCls;
+	public int simulationCls;
 
 	/** 取り込み使用区分 */
 	@Column(name = "CAPTURE_USAGE_CLS")
-	private int captureUsageCls;
+	public int captureUsageCls;
 
 	/** 完了機能使用区分 */
 	@Column(name = "COMPLETED_FUNC_CLS")
-	private int completedFuncCls;
+	public int completedFuncCls;
 
 	/** 完了実行方法 */
 	@Column(name = "HOW_TO_COMPLETE")
-	private int howToComplete;
+	public int howToComplete;
 	
 	/** アラームチェック区分 */
 	@Column(name = "ALARM_CHECK_CLS")
-	private int alarmCheckCls;
+	public int alarmCheckCls;
 
 	/** アラーム実行方法 */
 	@Column(name = "EXECUTION_METHOD")
-	private int executionMethod;
+	public int executionMethod;
 
 	/** 手修正解除区分 */
 	@Column(name = "HANDLE_REPAIR_ATR")
-	private int handleRepairAtr;
+	public int handleRepairAtr;
 
 	/** 確定区分 */
 	@Column(name = "CONFIRM")
-	private int confirm;
+	public int confirm;
 
 	/** 検索方法 */
 	@Column(name = "SEARCH_METHOD")
-	private int searchMethod;
+	public int searchMethod;
 
 	/** 検索方法選択表示区分 */
 	@Column(name = "SEARCH_METHOD_DISP_CLS")
-	private int searchMethodDispCls;
+	public int searchMethodDispCls;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy="scheFuncControl", orphanRemoval = true)
+	public List<KsfstScheFuncCondition> scheFuncConditions;
 	
 	@Override
 	protected Object getKey() {
