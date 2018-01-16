4f5d3fbb2edbe441fe18a6638685e965d5842d03
NSVN-TRAN THANH TRUNG <trung_tt@nittsusystem-vn.com>
2018-01-16 13:11:55 +0700

    fix search ksu001 o.o1.jb

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksu/001/o/ksu001.o.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksu/001/o/ksu001.o.vm.ts
@@ -18,10 +18,10 @@ module nts.uk.at.view.ksu001.o.viewmodel {
         startDateScreenA: any = null;
         endDateScreenA: any = null;
         isEnableClearSearchButton: KnockoutObservable<boolean> = ko.observable(false);
-        checkStateWorkTypeCode: any = null;  
+        checkStateWorkTypeCode: any = null;
         checkNeededOfWorkTimeSetting: any = null;
         workEmpCombines: any = null;
-        
+
         constructor() {
             let self = this;
             self.listWorkType = ko.observableArray([]);
@@ -119,10 +119,15 @@ module nts.uk.at.view.ksu001.o.viewmodel {
             if (!self.time1() && !self.time2()) {
                 nts.uk.ui.dialog.alertError({ messageId: "Msg_53" });
                 self.isEnableClearSearchButton(false);
+                self.clear();
                 return;
             }
             if (self.time1() && self.time2() && moment(self.time1(), 'HH:mm').isSameOrAfter(moment(self.time2(), 'HH:mm'))) {
                 nts.uk.ui.dialog.alertError({ messageId: "Msg_54" });
+                self.clear();
+                return;
+            }
+            if (nts.uk.ui.errors.hasError()) {
                 return;
             }
             self.listWorkTimeComboBox([]);
