23ca20279ba76ff6e85438306d3f24a965c5a4dd
NSVC\hieult <hieultph03963@fpt.edu.vn>
2018-01-17 13:04:00 +0700

    Update KAL 004

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kal/004/a/setpermission/kal004.tab3.service.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kal/004/a/setpermission/kal004.tab3.service.ts
@@ -2,8 +2,8 @@ module nts.uk.at.view.kal004.tab3.service {
     var paths: any = {
         getListRoleName: "ctx/sys/auth/role/getrolebylistroleid"
     }
-    export function getListRoleName(data): JQueryPromise<any> {
-        return nts.uk.request.ajax("com", paths.getListRoleName);
+    export function getListRoleName(data :any): JQueryPromise<any> {
+        return nts.uk.request.ajax("com", paths.getListRoleName , data);
     }
 
 
