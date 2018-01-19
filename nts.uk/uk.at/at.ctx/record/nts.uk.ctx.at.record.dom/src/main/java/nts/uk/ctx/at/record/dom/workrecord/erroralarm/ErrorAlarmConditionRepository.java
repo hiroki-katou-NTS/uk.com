93e4e0d34cc1dabfc7f0e4d77fb18c57d74cdf01
Hieunv <hieu.nv@3si.vn>
2018-01-19 17:15:51 +0700

    kal003-b

---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/workrecord/erroralarm/ErrorAlarmConditionRepository.java
@@ -0,0 +1,29 @@
+package nts.uk.ctx.at.record.dom.workrecord.erroralarm;
+
+import java.util.List;
+import java.util.Optional;
+
+import nts.uk.ctx.at.record.dom.workrecord.erroralarm.condition.ErrorAlarmCondition;
+
+/**
+ * @author hieunv
+ *
+ */
+public interface ErrorAlarmConditionRepository {
+
+	// get condition by eralCheckId
+	Optional<ErrorAlarmCondition> findConditionByErrorAlamCheckId(String eralCheckId);
+
+	// get condition by list eralCheckId
+	List<ErrorAlarmCondition> findConditionByListErrorAlamCheckId(List<String> listEralCheckId);
+
+	// insert an error/alarm setting
+	void addErrorAlarmCondition(ErrorAlarmCondition domain);
+
+	// update an error/alarm setting
+	void updateErrorAlarmCondition(ErrorAlarmCondition domain);
+
+	// remove an error/alarm setting
+	void removeErrorAlarmCondition(List<String> listErAlCheckID);
+
+}
