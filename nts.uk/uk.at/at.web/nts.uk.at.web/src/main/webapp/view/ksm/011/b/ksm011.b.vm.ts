c4dcc9d397725dc5ecb46f98c6bea7e45963ee45
NSVC\tanlv <tan_lv@nittsusystem-vn.com>
2018-01-24 11:40:09 +0700

    KSM011 - B: Add code

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksm/011/b/ksm011.b.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksm/011/b/ksm011.b.vm.ts
@@ -3,16 +3,22 @@ module nts.uk.at.view.ksm011.b.viewmodel {
 
     export class ScreenModel {
         qualificationMark: KnockoutObservable<string>;
+        qualificationMarkRequired: KnockoutObservable<boolean>;
         qualificationMarkEnable: KnockoutObservable<boolean>;
         openDialogEnable: KnockoutObservable<boolean>;
         credentialListEnable: KnockoutObservable<boolean>;
         credentialList: KnockoutObservable<string>;
-        leftItems: KnockoutObservableArray<ItemModel>;
-        rightItems: KnockoutObservableArray<ItemModel>;
+        leftItems: KnockoutObservableArray<any>;
+        rightItems: KnockoutObservableArray<any>;
         leftColumns: KnockoutObservableArray<any>;
         rightColumns: KnockoutObservableArray<any>;
         currentLCodeList: KnockoutObservableArray<any>;
         currentRCodeList: KnockoutObservableArray<any>;
+        
+        addAllEnable: KnockoutObservable<boolean>;
+        singleAddEnable: KnockoutObservable<boolean>;
+        singleRemoveEnable: KnockoutObservable<boolean>;
+        removeAllEnable: KnockoutObservable<boolean>;
         
         halfDayCls: KnockoutObservableArray<any>;
         selectedHalfDayCls: KnockoutObservable<number>;
@@ -28,12 +34,25 @@ module nts.uk.at.view.ksm011.b.viewmodel {
             var self = this;
             self.qualificationMark = ko.observable("");
             self.qualificationMarkEnable = ko.observable(false);
+            self.qualificationMarkRequired = ko.observable(false);
             self.openDialogEnable = ko.observable(false);
             self.credentialListEnable = ko.observable(false);
             
             self.credentialList = ko.observable("");
             self.leftItems = ko.observableArray([]);
             self.rightItems = ko.observableArray([]);
+            
+            self.leftItems = ko.observableArray([
+                { code: 0, name: nts.uk.resource.getText("Com_Employment") },
+                { code: 1, name: nts.uk.resource.getText("Com_Workplace") },
+                { code: 2, name: nts.uk.resource.getText("Com_Class") },
+                { code: 3, name: nts.uk.resource.getText("Com_Jobtitle") },
+                { code: 4, name: nts.uk.resource.getText("KSM011_55") },
+                { code: 5, name: nts.uk.resource.getText("KSM011_56") },
+                { code: 6, name: nts.uk.resource.getText("KSM011_57") },
+                { code: 7, name: nts.uk.resource.getText("KSM011_58") },
+                { code: 8, name: nts.uk.resource.getText("KSM011_59") }
+            ]);
             
             self.leftColumns = ko.observableArray([
                 { headerText: nts.uk.resource.getText("KSM011_78"), prop: 'code', width: 10, hidden: true },
@@ -48,6 +67,19 @@ module nts.uk.at.view.ksm011.b.viewmodel {
             ]);
             
             self.currentRCodeList = ko.observableArray([]);
+            
+            self.addAllEnable = ko.observable(true);
+            self.singleAddEnable = ko.observable(false);
+            self.singleRemoveEnable = ko.observable(false);
+            self.removeAllEnable = ko.observable(true);
+            
+            if(self.leftItems().length == 0) {
+                self.addAllEnable(false);
+            }
+            
+            if(self.rightItems().length == 0) {
+                self.removeAllEnable(false);
+            }
             
             self.halfDayCls = ko.observableArray([
                 { code: 0, name: nts.uk.resource.getText("KSM011_8") },
@@ -76,8 +108,38 @@ module nts.uk.at.view.ksm011.b.viewmodel {
             ]);
 
             self.selectedInsufficient = ko.observable(0);
+            
+            self.currentLCodeList.subscribe(function(items) {
+                if(items.length > 0) {
+                    self.singleAddEnable(true);
+                } else {
+                    self.singleAddEnable(false);
+                }
+            });
+            
+            self.currentRCodeList.subscribe(function(items) {
+                if(items.length > 0) {
+                    self.singleRemoveEnable(true);
+                } else {
+                    self.singleRemoveEnable(false);
+                }
+            });
+            
+            self.displayQualMark();
         }
 
+        displayQualMark() {
+            var self = this;
+            
+            if(self.rightItems().length > 0) {
+                self.qualificationMarkRequired(true);
+                self.qualificationMarkEnable(true);
+            } else {
+                self.qualificationMarkRequired(false);
+                self.qualificationMarkEnable(false);
+            }
+        }
+        
         /**
          * Start page.
          */
@@ -96,20 +158,258 @@ module nts.uk.at.view.ksm011.b.viewmodel {
         registration() {
             var self = this;
             
+            // clear all error
+            nts.uk.ui.errors.clearAll();
+            
+            $("#qualMark").trigger("validate");
+            
+            if(self.rightItems().length <= 0) {
+                nts.uk.ui.dialog.alertError({ messageId: "Msg_752" });
+                return;
+            }
+            
+            if (nts.uk.ui.errors.hasError()) {
+                return;
+            }
+            
+            var schePerInfoAtrData = [];
+            _.forEach(self.rightItems(), function(item){
+                schePerInfoAtrData.push({
+                    personInfoAtr: item.code   
+                });
+            });
+            
+            var scheQualifySetData = [];
+            
+            var data = new ScheDispControlDto({
+                personSyQualify: self.qualificationMark(),
+                symbolHalfDayAtr: self.selectedHalfDayCls(),
+                symbolAtr: self.selectedEmpSignCls(),
+                pubHolidayExcessAtr: self.selectedObtain(),
+                pubHolidayShortageAtr: self.selectedInsufficient(),
+                symbolHalfDayName: "",
+                schePerInfoAtr: schePerInfoAtrData.length > 0 ? schePerInfoAtrData : null,
+                scheQualifySet: scheQualifySetData.length > 0 ? scheQualifySetData : null
+            });
+            
+            service.saveScheDispControl(data).done(function() {
+                nts.uk.ui.dialog.info({ messageId: "Msg_15" });
+            }).fail(function(error) {
+                nts.uk.ui.dialog.alertError({ messageId: error.messageId, messageParams: error.parameterIds });
+            });
         }
         
+        /**
+         * Add all items from left to right.
+         */
+        addAll() {
+            var self = this;
+            
+            if(self.leftItems().length > 0) {
+                if(self.rightItems().length == 0) {
+                    self.rightItems(self.leftItems());
+                    self.leftItems([]);
+                    self.addAllEnable(false);
+                    self.removeAllEnable(true);
+                    self.currentRCodeList([]);
+                } else {
+                    var rightItems = self.rightItems();
+                    _.forEach(self.leftItems(), function(item){
+                        rightItems.push({
+                            code: item.code,
+                            name: item.name   
+                        });
+                    });
+                    
+                    var sortedItems = _.sortBy(rightItems, [function(o) { return o.code; }]);
+                    self.leftItems([]);
+                    self.rightItems([]);
+                    self.rightItems(sortedItems);
+                }                
+            }
+            
+            self.currentRCodeList([]);
+            self.singleAddEnable(false);
+            self.displayQualMark();
+        }
+        
+        /**
+         * Return an item from left to right.
+         */
+        singleAdd() {
+            var self = this;
+            
+            if(self.currentLCodeList().length > 0) {
+                var leftItems = self.leftItems();
+                _.forEach(self.currentLCodeList(), function(item){
+                    var temp = _.find(leftItems, function(obj) {
+                        return item == obj.code;
+                    });
+                    
+                    self.rightItems.push({
+                        code: temp.code,
+                        name: temp.name   
+                    });
+                    
+                    var evens = _.remove(leftItems, function(item) {
+                        return item.code == temp.code;
+                    });
+                });
+                    
+                self.leftItems([]);
+                var sortedLItems = _.sortBy(leftItems, [function(o) { return o.code; }]);
+                var sortedRItems = _.sortBy(self.rightItems(), [function(o) { return o.code; }]);
+                self.leftItems(sortedLItems);
+                self.rightItems([]);
+                self.rightItems(sortedRItems);
+                self.singleAddEnable(false);
+                self.currentRCodeList([]);
+                
+                if(self.rightItems().length > 0) {
+                    self.removeAllEnable(true);
+                }
+                
+                self.displayQualMark();
+            }
+        }
+        
+        /**
+         * Return an item from right to left.
+         */
+        singleRemove() {
+            var self = this;
+            
+            if(self.currentRCodeList().length > 0) {
+                var rightItems = self.rightItems();
+                _.forEach(self.currentRCodeList(), function(item){
+                    var temp = _.find(rightItems, function(obj) {
+                        return item == obj.code;
+                    });
+                    
+                    self.leftItems.push({
+                        code: temp.code,
+                        name: temp.name   
+                    });
+                    
+                    var evens = _.remove(rightItems, function(item) {
+                        return item.code == temp.code;
+                    });
+                });
+                    
+                self.rightItems([]);
+                var sortedLItems = _.sortBy(self.leftItems(), [function(o) { return o.code; }]);
+                var sortedRItems = _.sortBy(rightItems, [function(o) { return o.code; }]);
+                self.rightItems(sortedRItems);
+                self.leftItems([]);
+                self.leftItems(sortedLItems);
+                self.singleRemoveEnable(false);
+                self.currentLCodeList([]);
+                
+                if(self.leftItems().length > 0) {
+                    self.addAllEnable(true);
+                }
+                
+                self.displayQualMark();
+            }
+        }
+        
+        /**
+         * Return all items from right to left.
+         */
+        removeAll() {
+            var self = this;
+            
+            if(self.rightItems().length > 0) {
+                if(self.leftItems().length == 0) {
+                    self.leftItems(self.rightItems());
+                    self.rightItems([]);
+                    self.removeAllEnable(false);
+                    self.addAllEnable(true);
+                    self.currentLCodeList([]);
+                } else {
+                    var leftItems = self.leftItems();
+                    _.forEach(self.rightItems(), function(item){
+                        leftItems.push({
+                            code: item.code,
+                            name: item.name   
+                        });
+                    });
+                    
+                    var sortedItems = _.sortBy(leftItems, [function(o) { return o.code; }]);
+                    self.rightItems([]);
+                    self.leftItems([]);
+                    self.leftItems(sortedItems);
+                }                
+            }
+            
+            self.currentLCodeList([]);
+            self.singleRemoveEnable(false);
+            self.displayQualMark();
+        }
+        
+        /**
+         * Open dialog function.
+         */
         openDialog() {
             var self = this;
             
         }
     }
+     
+    class ScheDispControlDto {
+        personSyQualify: string;
+        symbolHalfDayAtr: number;
+        symbolAtr: number;
+        pubHolidayExcessAtr: number;
+        pubHolidayShortageAtr: number;
+        symbolHalfDayName: string;
+        schePerInfoAtr: Array<SchePerInfoAtrDto>;
+        scheQualifySet: Array<ScheQualifySetDto>;
+        
+        constructor(param: IScheDispControlDto) {
+            this.personSyQualify = param.personSyQualify;
+            this.symbolHalfDayAtr = param.symbolHalfDayAtr;
+            this.symbolAtr = param.symbolAtr;
+            this.pubHolidayExcessAtr = param.pubHolidayExcessAtr;
+            this.pubHolidayShortageAtr = param.pubHolidayShortageAtr;
+            this.symbolHalfDayName = param.symbolHalfDayName;
+            this.schePerInfoAtr = param.schePerInfoAtr;
+            this.scheQualifySet = param.scheQualifySet;
+        }
+    }
     
-    class ItemModel {
-       code: number;
-       name: string;
-       constructor(code: number, name: string) {
-           this.code = code;
-           this.name = name;
-       }
-   }
+    interface IScheDispControlDto {
+        personSyQualify: string,
+        symbolHalfDayAtr: number,
+        symbolAtr: number,
+        pubHolidayExcessAtr: number,
+        pubHolidayShortageAtr: number,
+        symbolHalfDayName: string,
+        schePerInfoAtr: Array<SchePerInfoAtrDto>,
+        scheQualifySet: Array<ScheQualifySetDto> 
+    }
+    
+    class SchePerInfoAtrDto {
+        personInfoAtr: number;
+        
+        constructor(param: ISchePerInfoAtrDto) {
+            this.personInfoAtr = param.personInfoAtr;
+        }
+    }
+    
+    interface ISchePerInfoAtrDto {
+        personInfoAtr: number;
+    }
+    
+    class ScheQualifySetDto {
+        qualifyCode: string;
+        
+        constructor(param: IScheQualifySetDto) {
+            this.qualifyCode = param.qualifyCode;
+        }
+    }
+    
+    interface IScheQualifySetDto {
+        qualifyCode: string;
+    }
 }
