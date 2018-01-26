cbecb88f50f91d61de6421da120e407b22748c47
NSVN-PhamXuanVinh <vinh_px@nittsusystem-vn.com>
2018-01-26 13:33:49 +0700

    update get list info

--- nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/pereg/app/ComboBoxObject.java
+++ nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/pereg/app/ComboBoxObject.java
@@ -1,16 +1,31 @@
 package nts.uk.shr.pereg.app;
 
-import lombok.Value;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
-@Value
+@Getter
+@AllArgsConstructor
 public class ComboBoxObject {
 	
 	private String optionValue;
 	
 	private String optionText;
 	
+	@Setter
+	private String firstCtgName;
+	
+	public ComboBoxObject(String value, String optionText) {
+		this.optionText = optionText;
+		this.optionValue = value;
+	}
+	
+	private static ComboBoxObject getComBox(String value, String optionText) {
+		return new ComboBoxObject(value, optionText, "");
+	}
+		
 	public static ComboBoxObject toComboBoxObject(String value, String leftSymbolText, String rightSymbolText){
 		String optionText = leftSymbolText + " ~ " + rightSymbolText;
-		return new ComboBoxObject(value, optionText);
+		return getComBox(value, optionText);
 	};
 }
