fdf47d560944c46ff744533e29fe4a92f5782529
NSVN-TranThiHoa <hoa_tt@nittsusystem-vn.com>
2018-01-29 11:28:21 +0700

    CMM045: bổ sung vào domain 申請承認設定

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/setting/company/request/RequestSettingRepository.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/setting/company/request/RequestSettingRepository.java
@@ -1,11 +1,18 @@
 package nts.uk.ctx.at.request.dom.setting.company.request;
+
+import java.util.Optional;
+
 /**
  * 
  * @author Doan Duy Hung
  *
  */
 public interface RequestSettingRepository {
-	
-	public RequestSetting findByCompany(String companyID);
+	/**
+	 * ドメインモデル「承認一覧表示設定」を取得する
+	 * @param companyID
+	 * @return
+	 */
+	public Optional<RequestSetting> findByCompany(String companyID);
 	
 }
