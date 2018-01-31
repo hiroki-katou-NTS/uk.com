dbf59c38488adbbe51510f5b2be8b68764ed09eb
NSVC\hungnm <hungnm910@gmail.com>
2018-01-31 10:28:21 +0700

    kaf022: update service

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.app/src/main/java/nts/uk/ctx/at/request/app/command/setting/workplace/ApprovalSettingWkpCommandHandler.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.app/src/main/java/nts/uk/ctx/at/request/app/command/setting/workplace/ApprovalSettingWkpCommandHandler.java
@@ -3,9 +3,14 @@
  */
 package nts.uk.ctx.at.request.app.command.setting.workplace;
 
+import java.util.Optional;
+
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+import nts.arc.layer.app.command.CommandHandler;
+import nts.arc.layer.app.command.CommandHandlerContext;
+import nts.uk.ctx.at.request.dom.setting.workplace.RequestOfEachWorkplace;
 import nts.uk.ctx.at.request.dom.setting.workplace.RequestOfEachWorkplaceRepository;
 
 /**
@@ -13,9 +18,20 @@ import nts.uk.ctx.at.request.dom.setting.workplace.RequestOfEachWorkplaceReposit
  *
  */
 @Stateless
-public class ApprovalSettingWkpCommandHandler {
+public class ApprovalSettingWkpCommandHandler extends CommandHandler<ApprovalSettingWkpCommand> {
 
 	@Inject
 	private RequestOfEachWorkplaceRepository repo;
+
+	@Override
+	protected void handle(CommandHandlerContext<ApprovalSettingWkpCommand> context) {
+		ApprovalSettingWkpCommand command = context.getCommand();
+		Optional<RequestOfEachWorkplace> existedSetting = repo.getRequestByWorkplace(command.getCompanyId(), command.getWkpId());
+		if(existedSetting.isPresent()){
+			repo.update(command.toDomain());
+		} else {
+			repo.add(command.toDomain());
+		}
+	}
 	
 }
