38ac4b1e1a122fc3aa14e432438fc8714330453c
NSVC-DinhTheLam <lam_dt@nittsusystem-vn.com>
2018-02-02 17:46:19 +0700

    update check return data from F screen

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kal/004/a/periodSetting/kal004.tab2.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kal/004/a/periodSetting/kal004.tab2.vm.ts
@@ -63,7 +63,7 @@ module nts.uk.at.view.kal004.tab2.viewModel {
                 nts.uk.ui.windows.setShared("categoryName", ModelCheckConditonCode.categoryName);
                 nts.uk.ui.windows.sub.modal("../f/index.xhtml").onClosed(() => {
                     let data = nts.uk.ui.windows.getShared("segmentationOfCycle");
-                    if(!nts.uk.util.isNullOrUndefined(data)){
+                    if(!nts.uk.util.isNullOrUndefined(data) && !nts.uk.util.isNullOrUndefined(data.decisionEnum)){
                         self.extractionPeriodUnit(categoryId,ModelCheckConditonCode.extractionPeriodUnit.extractionId,data.decisionEnum);
                     }
                 }
