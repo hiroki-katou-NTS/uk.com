4212dc001109df96c432022c70ef3249e249a5c6
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2018-02-05 19:16:30 +0700

    update ItemValueType

--- nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.app/src/main/java/nts/uk/ctx/pereg/app/find/layout/RegisterLayoutFinder.java
+++ nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.app/src/main/java/nts/uk/ctx/pereg/app/find/layout/RegisterLayoutFinder.java
@@ -171,6 +171,7 @@ public class RegisterLayoutFinder {
 		item.setRow(0);
 		item.setRequired(itemDef.getIsRequired() == 1);
 		item.setType(itemDef.getItemTypeState().getItemType());
+		item.setItemParentCode(itemDef.getItemParentCode());
 		if (itemDef.getItemTypeState().getItemType() != 1) {
 			SingleItemDto sigleItem = (SingleItemDto) itemDef.getItemTypeState();
 			item.setItem(sigleItem.getDataTypeState());
