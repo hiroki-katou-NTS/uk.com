99df012dd7cfba43483d1e847cf76b06e783505d
NTS\keisuke_hoshina <keisuke_hoshina@nittsusystem.co.jp>
2018-02-05 19:02:47 +0900

    マージのためにコミット

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.pubimp/src/main/java/nts/uk/ctx/at/record/pubimp/dailyprocess/scheduletime/ScheduleTimePubImpl.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.pubimp/src/main/java/nts/uk/ctx/at/record/pubimp/dailyprocess/scheduletime/ScheduleTimePubImpl.java
@@ -1,11 +1,14 @@
 package nts.uk.ctx.at.record.pubimp.dailyprocess.scheduletime;
 
+import javax.ejb.Stateless;
+
 import nts.arc.time.GeneralDate;
 import nts.uk.ctx.at.record.pub.dailyprocess.scheduletime.ScheduleTimePub;
 import nts.uk.ctx.at.record.pub.dailyprocess.scheduletime.ScheduleTimePubExport;
 import nts.uk.ctx.at.record.pub.dailyprocess.scheduletime.ScheduleTimePubImport;
 import nts.uk.ctx.at.shared.dom.common.time.AttendanceTime;
 
+@Stateless
 public class ScheduleTimePubImpl implements ScheduleTimePub{
 
 	@Override
