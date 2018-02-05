b57dc7360c010a3610b0b5f6ed848d93e9800ce4
NSVC\yennth <yen_nt@nittsusystem-vn.com>
2018-02-05 15:50:14 +0700

    Kaf022: update code server get by list jobtitle id

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/022/a/kaf022.a.service.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/022/a/kaf022.a.service.ts
@@ -21,7 +21,7 @@ module nts.uk.at.view.kmf022.a.service {
         findProByApp: "at/request/application/setting/proxy/findApp",
         // A14_3 
         findJobAssign: "job/assign/setting/getjob",
-        //A15_4
+        //A15_4, truyền lần lượt job id trong list lấy được để lấy ra A15_4. (ver 2)
         findJobTitleSearch: "workflow/jobtitlesearchset/job/getbyId",
         // A16_7, A16_8
         findMail: "at/request/application/mail/holiday",
@@ -69,8 +69,22 @@ module nts.uk.at.view.kmf022.a.service {
         // I
         findLateEarly: "at/request/application/applicationlatearrival/findByCompanyID",
         
+        //jobId. Muốn lấy được jobid thì phải truyền companyId và baseDate
+        findJobId: "bs/employee/jobtitle/findAll",
+        // lấy xong list job Id thì có thể truyền cả list jobId để lấy list A15_4
+        findJobTitleSearchList: "workflow/jobtitlesearchset/job/getbyCode",
+        
 //        add: 'at/shared/yearservicecom/add',
 //        update: 'at/shared/yearservicecom/update'
+    }
+    
+    export function findJobTitleSearchList(param: any): JQueryPromise<void>{
+        return nts.uk.request.ajax(paths.findJobTitleSearchList, param);    
+    }
+    
+    // view model cho thêm đối tượng Kcp003Dto chỉ có 1 trường baseDate: truyền ngày hiện tại
+    export function findJobId(date: viewmodel.Kcp003Dto): JQueryPromise<void>{
+        return nts.uk.request.ajax("com", paths.findJobId + "/" + date);     
     }
     
     export function findLateEarly() {
