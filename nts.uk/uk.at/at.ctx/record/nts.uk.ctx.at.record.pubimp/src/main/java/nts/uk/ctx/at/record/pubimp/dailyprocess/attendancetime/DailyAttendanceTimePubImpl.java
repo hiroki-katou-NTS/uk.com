995511cdc8640f3835ced336c64913caf76cd7cc
NTS\keisuke_hoshina <keisuke_hoshina@nittsusystem.co.jp>
2018-02-07 19:12:31 +0900

    change  list →　Map

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.pubimp/src/main/java/nts/uk/ctx/at/record/pubimp/dailyprocess/attendancetime/DailyAttendanceTimePubImpl.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.pubimp/src/main/java/nts/uk/ctx/at/record/pubimp/dailyprocess/attendancetime/DailyAttendanceTimePubImpl.java
@@ -1,6 +1,6 @@
 package nts.uk.ctx.at.record.pubimp.dailyprocess.attendancetime;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 import lombok.val;
 import nts.uk.ctx.at.record.dom.daily.TimeWithCalculation;
@@ -19,110 +19,27 @@ public class DailyAttendanceTimePubImpl implements DailyAttendanceTimePub{
 	@Override
 	public DailyAttendanceTimePubExport calcDailyAttendance(DailyAttendanceTimePubImport imp) {
 
-		val overTime = new ArrayList<TimeWithCalculation>();
-		overTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		overTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		overTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		overTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		overTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		overTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		overTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		overTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		overTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		overTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
+		val overTimeFrames = new HashMap<OverTimeFrameNo,TimeWithCalculation>();
+		val holidayWorkFrames = new HashMap<HolidayWorkFrameNo,TimeWithCalculation>();
+		val bonusPays = new HashMap<Integer,TimeWithCalculation>();
+		val specBonusPays = new HashMap<Integer,TimeWithCalculation>();
+		for(int loopNumber = 1 ; loopNumber <=10 ; loopNumber++ ) {
+			//残業
+			overTimeFrames.put(new OverTimeFrameNo(loopNumber), TimeWithCalculation.sameTime(new AttendanceTime(0)));
+			//休出
+			holidayWorkFrames.put(new HolidayWorkFrameNo(loopNumber), TimeWithCalculation.sameTime(new AttendanceTime(0)));
+			//加給
+			bonusPays.put(loopNumber, TimeWithCalculation.sameTime(new AttendanceTime(0)));
+			//特定日加給
+			specBonusPays.put(loopNumber, TimeWithCalculation.sameTime(new AttendanceTime(0)));
+			
+		}
 		
-		val overTimeFrameNo = new ArrayList<OverTimeFrameNo>();
-		overTimeFrameNo.add(new OverTimeFrameNo(1));
-		overTimeFrameNo.add(new OverTimeFrameNo(2));
-		overTimeFrameNo.add(new OverTimeFrameNo(3));
-		overTimeFrameNo.add(new OverTimeFrameNo(4));
-		overTimeFrameNo.add(new OverTimeFrameNo(5));
-		overTimeFrameNo.add(new OverTimeFrameNo(6));
-		overTimeFrameNo.add(new OverTimeFrameNo(7));
-		overTimeFrameNo.add(new OverTimeFrameNo(8));
-		overTimeFrameNo.add(new OverTimeFrameNo(9));
-		overTimeFrameNo.add(new OverTimeFrameNo(10));
 		
-		val holidayWorkTime = new ArrayList<TimeWithCalculation>();
-		holidayWorkTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		holidayWorkTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		holidayWorkTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		holidayWorkTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		holidayWorkTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		holidayWorkTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		holidayWorkTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		holidayWorkTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		holidayWorkTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		holidayWorkTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		
-		val holidayWorkTimeNo = new ArrayList<HolidayWorkFrameNo>();
-		holidayWorkTimeNo.add(new HolidayWorkFrameNo(1));
-		holidayWorkTimeNo.add(new HolidayWorkFrameNo(2));
-		holidayWorkTimeNo.add(new HolidayWorkFrameNo(3));
-		holidayWorkTimeNo.add(new HolidayWorkFrameNo(4));
-		holidayWorkTimeNo.add(new HolidayWorkFrameNo(5));
-		holidayWorkTimeNo.add(new HolidayWorkFrameNo(6));
-		holidayWorkTimeNo.add(new HolidayWorkFrameNo(7));
-		holidayWorkTimeNo.add(new HolidayWorkFrameNo(8));
-		holidayWorkTimeNo.add(new HolidayWorkFrameNo(9));
-		holidayWorkTimeNo.add(new HolidayWorkFrameNo(10));
-		
-		val bonusPayTime = new ArrayList<TimeWithCalculation>();
-		bonusPayTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		bonusPayTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		bonusPayTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		bonusPayTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		bonusPayTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		bonusPayTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		bonusPayTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		bonusPayTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		bonusPayTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		bonusPayTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		
-		val bonusPayNo = new ArrayList<Integer>();
-		bonusPayNo.add(1);
-		bonusPayNo.add(2);
-		bonusPayNo.add(3);
-		bonusPayNo.add(4);
-		bonusPayNo.add(5);
-		bonusPayNo.add(6);
-		bonusPayNo.add(7);
-		bonusPayNo.add(8);
-		bonusPayNo.add(9);
-		bonusPayNo.add(10);
-		
-		val specBonusPayTime = new ArrayList<TimeWithCalculation>();
-		specBonusPayTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		specBonusPayTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		specBonusPayTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		specBonusPayTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		specBonusPayTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		specBonusPayTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		specBonusPayTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		specBonusPayTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		specBonusPayTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		specBonusPayTime.add(TimeWithCalculation.sameTime(new AttendanceTime(0)));
-		
-		val specBonusPayNo = new ArrayList<Integer>();
-		specBonusPayNo.add(1);
-		specBonusPayNo.add(2);
-		specBonusPayNo.add(3);
-		specBonusPayNo.add(4);
-		specBonusPayNo.add(5);
-		specBonusPayNo.add(6);
-		specBonusPayNo.add(7);
-		specBonusPayNo.add(8);
-		specBonusPayNo.add(9);
-		specBonusPayNo.add(10);
-		
-		return new DailyAttendanceTimePubExport(overTime,
-												overTimeFrameNo,
-												holidayWorkTime,
-												holidayWorkTimeNo,
-												bonusPayTime,
-												bonusPayNo,
-												specBonusPayTime,
-												specBonusPayNo,
+		return new DailyAttendanceTimePubExport(overTimeFrames,
+												holidayWorkFrames,
+												bonusPays,
+												specBonusPays,
 												TimeWithCalculation.sameTime(new AttendanceTime(0)),
 												TimeWithCalculation.sameTime(new AttendanceTime(0))
 											);
