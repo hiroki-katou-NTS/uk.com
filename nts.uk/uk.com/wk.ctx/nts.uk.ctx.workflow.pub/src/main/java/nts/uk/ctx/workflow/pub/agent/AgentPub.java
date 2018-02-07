5f8bb6a6c811522fca27e9f978abd5d3f87aa6b3
NSVC\dudt <du_dt@nittsusystem-vn.com>
2018-02-07 19:28:31 +0700

    bo sung xu ly 代行者、期間から承認代行情報を取得する

--- nts.uk/uk.com/wk.ctx/nts.uk.ctx.workflow.pub/src/main/java/nts/uk/ctx/workflow/pub/agent/AgentPub.java
+++ nts.uk/uk.com/wk.ctx/nts.uk.ctx.workflow.pub/src/main/java/nts/uk/ctx/workflow/pub/agent/AgentPub.java
@@ -2,7 +2,11 @@ package nts.uk.ctx.workflow.pub.agent;
 
 import java.util.List;
 
+import nts.arc.time.GeneralDate;
+
 public interface AgentPub {
 	
 	AgentPubExport getApprovalAgencyInformation(String companyID, List<String> approverList);
+	
+	List<AgentDataPubExport> getBySidDate(String companyId, String employeeId, GeneralDate baseDate);
 }
