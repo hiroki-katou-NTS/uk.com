5de87f7545e35b739b63427084289ae3b7e93234
NWS_Tran Huyen Trang <trangth@newwave.vn>
2018-02-08 10:38:57 +0700

    [CCG001] Add QueryModel

---
+++ nts.uk/uk.com/query/nts.uk.query.model/src/main/java/nts/uk/query/model/QueryModel.java
@@ -0,0 +1,44 @@
+package nts.uk.query.model;
+
+import java.util.Optional;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import nts.arc.time.GeneralDate;
+
+@AllArgsConstructor
+@Getter
+
+//社員のソート用の規定情報
+public class QueryModel {
+	
+	/** The employee ID. */
+	private String employeeID; //社員ID
+	
+	/** The employee code. */
+	private String employeeCode; //社員コード
+	
+	/** The hire date. */
+	private Optional<GeneralDate> hireDate; //入社日
+	
+	/** The classification code. */
+	private Optional<String> classificationCode; //分類コード
+	
+	/** The name. */
+	private Optional<String> name; //氏名
+	
+	/** The job title code. */
+	private Optional<String> jobTitleCode; //職位コード
+	
+//	private Integer oderOfJobTitle; //職位の序列の並び順
+	
+	/** The workplace code. */
+	private Optional<String> workplaceCode; //職場の階層コード
+	
+	/** The department code. */
+	private Optional<String> departmentCode; //部門の階層コード
+	
+	/** The employment code. */
+	private Optional<String> employmentCode; //雇用コード
+	
+}
