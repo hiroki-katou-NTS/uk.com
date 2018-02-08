994380a5a3a28dda7375dd4f2ecfef9219b3b28f
NSVC\yennth <yen_nt@nittsusystem-vn.com>
2018-02-08 14:20:58 +0700

    KAF022: update server. update method by list

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.ws/src/main/java/nts/uk/ctx/at/request/ws/application/displayname/AppDispNameWebservice.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.ws/src/main/java/nts/uk/ctx/at/request/ws/application/displayname/AppDispNameWebservice.java
@@ -9,6 +9,8 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import nts.arc.layer.ws.WebService;
+import nts.uk.ctx.at.request.app.command.setting.company.displayname.AppDispNameCommand;
+import nts.uk.ctx.at.request.app.command.setting.company.displayname.UpdateAppDispNameCommandHandler;
 import nts.uk.ctx.at.request.app.find.setting.company.displayname.AppDispNameDto;
 import nts.uk.ctx.at.request.app.find.setting.company.displayname.AppDispNameFinder;
 import nts.uk.ctx.at.request.app.find.setting.company.displayname.HdAppDispNameDto;
@@ -21,6 +23,10 @@ public class AppDispNameWebservice extends WebService{
 	private AppDispNameFinder dispFinder;
 	@Inject
 	private HdAppDispNameFinder hdFinder; 
+	
+	@Inject
+	private UpdateAppDispNameCommandHandler updateHd;
+	
 	@POST
 	@Path("disp")
 	public List<AppDispNameDto> getAppSet(){
@@ -43,5 +49,11 @@ public class AppDispNameWebservice extends WebService{
 	@Path("hdapp/{hdAppType}")
 	public HdAppDispNameDto getHdAppDisp(@PathParam("hdAppType") int hdAppType){
 		 return hdFinder.findItem(hdAppType);
+	}
+	
+	@POST
+	@Path("update")
+	public void update(List<AppDispNameCommand> command){
+		 this.updateHd.handle(command);
 	}
 }
