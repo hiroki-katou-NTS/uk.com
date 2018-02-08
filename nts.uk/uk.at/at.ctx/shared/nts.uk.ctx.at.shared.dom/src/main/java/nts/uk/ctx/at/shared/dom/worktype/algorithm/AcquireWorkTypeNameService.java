b52984d2b71324a3df3d332107006ace5686a22b
NSVC\tanlv <tan_lv@nittsusystem-vn.com>
2018-02-08 10:01:08 +0700

    Request list #251: Update code

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/worktype/algorithm/AcquireWorkTypeNameService.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/worktype/algorithm/AcquireWorkTypeNameService.java
@@ -2,6 +2,7 @@ package nts.uk.ctx.at.shared.dom.worktype.algorithm;
 
 import java.util.Optional;
 
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import nts.uk.ctx.at.shared.dom.worktype.WorkType;
@@ -13,6 +14,7 @@ import nts.uk.shr.com.context.AppContexts;
  * @author tanlv
  *
  */
+@Stateless
 public class AcquireWorkTypeNameService {
 	@Inject
 	public WorkTypeRepository repository;
--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/worktype/algorithm/PublicHolidaysWorkTypeService.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/worktype/algorithm/PublicHolidaysWorkTypeService.java
@@ -3,6 +3,7 @@ package nts.uk.ctx.at.shared.dom.worktype.algorithm;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import nts.uk.ctx.at.shared.dom.worktype.WorkType;
@@ -16,6 +17,7 @@ import nts.uk.shr.com.context.AppContexts;
  * @author tanlv
  *
  */
+@Stateless
 public class PublicHolidaysWorkTypeService {
 	@Inject
 	public WorkTypeRepository workTypeRepository;
