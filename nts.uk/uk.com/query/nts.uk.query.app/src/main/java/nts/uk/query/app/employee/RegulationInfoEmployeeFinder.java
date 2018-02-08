ee0e1ae816d511d46dcab029aa0d88ef082a111d
nws-anhnm <anhnm@newwave.vn>
2018-02-08 16:13:06 +0700

    [ccg001] add RegulationInfoEmployee app + ws

---
+++ nts.uk/uk.com/query/nts.uk.query.app/src/main/java/nts/uk/query/app/employee/RegulationInfoEmployeeFinder.java
@@ -0,0 +1,35 @@
+/******************************************************************
+ * Copyright (c) 2017 Nittsu System to present.                   *
+ * All right reserved.                                            *
+ *****************************************************************/
+package nts.uk.query.app.employee;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import nts.uk.query.model.employee.EmployeeSearchQuery;
+import nts.uk.query.model.employee.RegulationInfoEmployee;
+import nts.uk.query.model.employee.RegulationInfoEmployeeRepository;
+
+/**
+ * The Class RegulationInfoEmployeeFinder.
+ */
+@Stateless
+public class RegulationInfoEmployeeFinder {
+
+	/** The repo. */
+	@Inject
+	private RegulationInfoEmployeeRepository repo;
+
+	/**
+	 * Find.
+	 *
+	 * @param query the query
+	 * @return the list
+	 */
+	public List<RegulationInfoEmployee> find(EmployeeSearchQuery query) {
+		return this.repo.find(query);
+	}
+}
