4845c9ff54d9a0e2d2beac4885b9ee988417ac1e
NSVN-PhamVanDan <dan_pv@nittsusystem-vn.com>
2018-02-22 10:32:21 +0700

    Add showColor field with categories which haven't data

--- nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.app/src/main/java/nts/uk/ctx/pereg/app/find/common/MappingFactory.java
+++ nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.app/src/main/java/nts/uk/ctx/pereg/app/find/common/MappingFactory.java
@@ -63,6 +63,7 @@ public class MappingFactory {
 		String recordId = peregDto.getDomainDto().getRecordId();
 		for (LayoutPersonInfoClsDto classItem : classItemList) {
 			for (Object item : classItem.getItems()) {
+				
 				LayoutPersonInfoValueDto valueItem = (LayoutPersonInfoValueDto) item;
 				Object value = itemCodeValueMap.get(valueItem.getItemCode());
 				if (valueItem.getItem() != null) {
@@ -74,6 +75,10 @@ public class MappingFactory {
 					}
 				}
 				valueItem.setValue(value);
+				
+				// update 2018/02/22 bug 87560
+				valueItem.setShowColor(false);
+				
 				boolean optionItemNoValue = itemCodeValueMap.containsKey(valueItem.getItemCode());
 				if (optionItemNoValue) {
 					valueItem.setRecordId(recordId);
@@ -131,6 +136,10 @@ public class MappingFactory {
 		for (LayoutPersonInfoClsDto classItem : classItemList) {
 			for (Object item : classItem.getItems()) {
 				LayoutPersonInfoValueDto valueItem = (LayoutPersonInfoValueDto) item;
+				
+				// update 2018/02/22 bug 87560
+				valueItem.setShowColor(false);
+				
 				// data
 				for (PersonOptionalDto dataItem : dataItems) {
 					if (valueItem.getItemCode().equals(dataItem.getItemCode())) {
@@ -149,6 +158,10 @@ public class MappingFactory {
 		for (LayoutPersonInfoClsDto classItem : classItemList) {
 			for (Object item : classItem.getItems()) {
 				LayoutPersonInfoValueDto valueItem = (LayoutPersonInfoValueDto) item;
+				
+				// update 2018/02/22 bug 87560
+				valueItem.setShowColor(false);
+				
 				// data
 				for (EmpOptionalDto dataItem : dataItems) {
 					if (valueItem.getItemCode().equals(dataItem.getItemCode())) {
