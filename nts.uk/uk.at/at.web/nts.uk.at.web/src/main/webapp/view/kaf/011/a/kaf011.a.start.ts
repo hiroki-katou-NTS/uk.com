7a8fa343a7b42f8b645714365c1bbdd5b3248577
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2018-02-23 10:09:42 +0700

    create kaf001 A Screen

---
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/011/a/kaf011.a.start.ts
@@ -0,0 +1,9 @@
+module kaf011.a.start {
+    let __viewContext: any = window['__viewContext'] || {};
+    __viewContext.ready(() => {
+        __viewContext['viewModel'] = new vm.ViewModel();
+
+        __viewContext.bind(__viewContext['viewModel']);
+
+    });
+}
