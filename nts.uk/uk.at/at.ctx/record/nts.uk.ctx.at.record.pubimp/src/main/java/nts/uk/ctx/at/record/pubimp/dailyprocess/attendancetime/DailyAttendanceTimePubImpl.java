646d7f55aad471a740c4c10cf6dd4b9b7994e8ea
NTS\keisuke_hoshina <keisuke_hoshina@nittsusystem.co.jp>
2018-02-23 09:00:53 +0900

    申請・スケから用計算窓口作成

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.pubimp/src/main/java/nts/uk/ctx/at/record/pubimp/dailyprocess/attendancetime/DailyAttendanceTimePubImpl.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.pubimp/src/main/java/nts/uk/ctx/at/record/pubimp/dailyprocess/attendancetime/DailyAttendanceTimePubImpl.java
@@ -1,27 +1,112 @@
 package nts.uk.ctx.at.record.pubimp.dailyprocess.attendancetime;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 
 import lombok.val;
+import nts.uk.ctx.at.record.dom.breakorgoout.BreakTimeSheet;
+import nts.uk.ctx.at.record.dom.breakorgoout.primitivevalue.BreakFrameNo;
 import nts.uk.ctx.at.record.dom.daily.TimeWithCalculation;
+import nts.uk.ctx.at.record.dom.dailyprocess.calc.IntegrationOfDaily;
+import nts.uk.ctx.at.record.dom.dailyprocess.calc.PrevisionalCalculationServiceImpl;
+import nts.uk.ctx.at.record.dom.dailyprocess.calc.ProvisionalCalculationService;
+import nts.uk.ctx.at.record.dom.worklocation.WorkLocationCD;
+import nts.uk.ctx.at.record.dom.worktime.WorkStamp;
+import nts.uk.ctx.at.record.dom.worktime.enums.StampSourceInfo;
 import nts.uk.ctx.at.record.pub.dailyprocess.attendancetime.DailyAttendanceTimePub;
 import nts.uk.ctx.at.record.pub.dailyprocess.attendancetime.DailyAttendanceTimePubExport;
 import nts.uk.ctx.at.record.pub.dailyprocess.attendancetime.DailyAttendanceTimePubImport;
 import nts.uk.ctx.at.shared.dom.common.time.AttendanceTime;
+import nts.uk.ctx.at.shared.dom.vacation.setting.compensatoryleave.OneDayTime;
 import nts.uk.ctx.at.shared.dom.workrule.outsideworktime.holidaywork.HolidayWorkFrameNo;
 import nts.uk.ctx.at.shared.dom.workrule.outsideworktime.overtime.overtimeframe.OverTimeFrameNo;
+import nts.uk.ctx.at.shared.dom.worktime.common.TimeZone;
+import nts.uk.shr.com.time.TimeWithDayAttr;
 
 @Stateless
 public class DailyAttendanceTimePubImpl implements DailyAttendanceTimePub{
 
+	@Inject
+	private ProvisionalCalculationService provisionalCalculationService;
 	/**
 	 * RequestList No23
 	 */
 	@Override
 	public DailyAttendanceTimePubExport calcDailyAttendance(DailyAttendanceTimePubImport imp) {
 
+		
+		//時間帯の作成
+		TimeZone timeZone = new TimeZone(new TimeWithDayAttr(imp.getWorkStartTime().valueAsMinutes()),
+										 new TimeWithDayAttr(imp.getWorkEndTime().valueAsMinutes()));
+		Map<Integer, TimeZone> timeZoneMap = new HashMap<Integer, TimeZone>();
+		timeZoneMap.put(1, timeZone);
+		
+		//休憩時間帯の作成
+		List<BreakTimeSheet> breakTimeSheets = new ArrayList<>();
+		//一時的な作成
+		WorkStamp attendance = new WorkStamp(new TimeWithDayAttr(imp.getBreakStartTime().valueAsMinutes()), new TimeWithDayAttr(imp.getBreakStartTime().valueAsMinutes()), new WorkLocationCD("01"), StampSourceInfo.GO_STRAIGHT);
+		WorkStamp leave = new WorkStamp(new TimeWithDayAttr(imp.getBreakEndTime().valueAsMinutes()), new TimeWithDayAttr(imp.getBreakEndTime().valueAsMinutes()), new WorkLocationCD("01"), StampSourceInfo.GO_STRAIGHT);
+		//-----------
+		breakTimeSheets.add(new BreakTimeSheet(new BreakFrameNo(1),attendance , leave, imp.getBreakEndTime().minusMinutes(imp.getBreakStartTime().valueAsMinutes())));
+		
+		val calculateResult = provisionalCalculationService.calculation(imp.getEmployeeid(),
+																		imp.getYmd(),
+																		timeZoneMap,
+																		imp.getWorkTypeCode(),
+																		imp.getWorkTimeCode(),
+																		breakTimeSheets,
+																		Collections.emptyList(),
+																		Collections.emptyList());
+		if(calculateResult.isPresent()) {
+			return isPresentValue(calculateResult.get());
+		}
+		else {
+			return notPresentValue();
+		}
+
+	}
+	
+	/**
+	 * 日別計算で値が作成できた時のクラス作成 	
+	 * @param integrationOfDaily 1日の実績(WORK)
+	 * @return RequestList No23 Output class
+	 */
+	private DailyAttendanceTimePubExport isPresentValue(IntegrationOfDaily integrationOfDaily) {
+		val overTimeFrames = new HashMap<OverTimeFrameNo,TimeWithCalculation>();
+		val holidayWorkFrames = new HashMap<HolidayWorkFrameNo,TimeWithCalculation>();
+		val bonusPays = new HashMap<Integer,TimeWithCalculation>();
+		val specBonusPays = new HashMap<Integer,TimeWithCalculation>();
+		for(int loopNumber = 1 ; loopNumber <=10 ; loopNumber++ ) {
+			//残業
+			overTimeFrames.put(new OverTimeFrameNo(loopNumber), integrationOfDaily.getAttendanceTimeOfDailyPerformance().getActualWorkingTimeOfDaily().getTotalWorkingTime().getExcessOfStatutoryTimeOfDaily().getOverTimeWork().get().getOverTimeWorkFrameTime().get(loopNumber-1).getOverTimeWork());
+			//休出
+			holidayWorkFrames.put(new HolidayWorkFrameNo(loopNumber), integrationOfDaily.getAttendanceTimeOfDailyPerformance().getActualWorkingTimeOfDaily().getTotalWorkingTime().getExcessOfStatutoryTimeOfDaily().getWorkHolidayTime().get().getHolidayWorkFrameTime().get(loopNumber-1).getHolidayWorkTime().get());
+			//加給
+			bonusPays.put(loopNumber, integrationOfDaily.getAttendanceTimeOfDailyPerformance().getActualWorkingTimeOfDaily().getTotalWorkingTime().getRaiseSalaryTimeOfDailyPerfor().getRaisingSalaryTimes().get(loopNumber-1).getBonusPayTime());
+			//特定日加給
+			specBonusPays.put(loopNumber, integrationOfDaily.getAttendanceTimeOfDailyPerformance().getActualWorkingTimeOfDaily().getTotalWorkingTime().getRaiseSalaryTimeOfDailyPerfor().getAutoCalRaisingSalarySettings().get(loopNumber-1).getBonusPayTime());
+			
+		}
+		return new DailyAttendanceTimePubExport(overTimeFrames,
+				holidayWorkFrames,
+				bonusPays,
+				specBonusPays,
+				integrationOfDaily.getAttendanceTimeOfDailyPerformance().getActualWorkingTimeOfDaily().getTotalWorkingTime().getExcessOfStatutoryTimeOfDaily().getOverTimeWork().get().getFlexTime().getNotMinusFlexTime(),
+				integrationOfDaily.getAttendanceTimeOfDailyPerformance().getActualWorkingTimeOfDaily().getTotalWorkingTime().getExcessOfStatutoryTimeOfDaily().getExcessOfStatutoryMidNightTime().getTime()
+			);
+	}
+
+	/**
+	 * 日別計算で値が作成できなかった時のクラス作成
+	 * @return　RequestList No23 Output class(all value zero)
+	 */
+	private DailyAttendanceTimePubExport notPresentValue() {
 		val overTimeFrames = new HashMap<OverTimeFrameNo,TimeWithCalculation>();
 		val holidayWorkFrames = new HashMap<HolidayWorkFrameNo,TimeWithCalculation>();
 		val bonusPays = new HashMap<Integer,TimeWithCalculation>();
@@ -37,8 +122,6 @@ public class DailyAttendanceTimePubImpl implements DailyAttendanceTimePub{
 			specBonusPays.put(loopNumber, TimeWithCalculation.sameTime(new AttendanceTime(0)));
 			
 		}
-		
-		
 		return new DailyAttendanceTimePubExport(overTimeFrames,
 												holidayWorkFrames,
 												bonusPays,
