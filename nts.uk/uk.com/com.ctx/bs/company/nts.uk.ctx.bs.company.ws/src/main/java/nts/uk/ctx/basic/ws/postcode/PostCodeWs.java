384867090d4ad4e9a80be573014edf62109c5c13
nws_tiendd <tiendd_int@newwave.vn>
2018-02-26 10:08:01 +0700

    Comment unimplement interface.

--- nts.uk/uk.com/com.ctx/bs/company/nts.uk.ctx.bs.company.ws/src/main/java/nts/uk/ctx/basic/ws/postcode/PostCodeWs.java
+++ nts.uk/uk.com/com.ctx/bs/company/nts.uk.ctx.bs.company.ws/src/main/java/nts/uk/ctx/basic/ws/postcode/PostCodeWs.java
@@ -1,15 +1,14 @@
 package nts.uk.ctx.basic.ws.postcode;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import javax.inject.Inject;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import nts.arc.layer.ws.WebService;
-import nts.uk.shr.find.postcode.IPostCodeFinder;
 import nts.uk.shr.find.postcode.PostCode;
 
 /**
@@ -18,8 +17,10 @@ import nts.uk.shr.find.postcode.PostCode;
 @Path("ctx/basic/postcode")
 @Produces("application/json")
 public class PostCodeWs extends WebService {
-	@Inject
-	private IPostCodeFinder finder;
+	
+	// TODO: Comment by TienDD. please add implement class if uncomment.
+//	@Inject
+//	private IPostCodeFinder finder;
 
 	/**
 	 * Find all.
@@ -31,7 +32,8 @@ public class PostCodeWs extends WebService {
 	@POST
 	@Path("find/{zipCode}")
 	public List<PostCode> findAll(@PathParam("zipCode") String zipCode) {
-		return this.finder.findPostCodeList(zipCode);
+		//return this.finder.findPostCodeList(zipCode);
+		return new ArrayList<PostCode>();
 	}
 
 }
