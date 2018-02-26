75f4daac380763ebe8ff2b47a1f7e78a6c1d239b
HopNT3SI <32725057+HopNT3SI@users.noreply.github.com>
2018-02-26 14:38:47 +0700

    Fibug #88275

--- nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.app/src/main/java/command/person/info/ValidateUtils.java
+++ nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.app/src/main/java/command/person/info/ValidateUtils.java
@@ -4,6 +4,9 @@ public class ValidateUtils {
 	private static final String JP_SPACE = "　";
 	
 	public static boolean validateName(String name){
+		if (name == null || name.trim().equals("")){
+			return true;
+		}
 		if (name.endsWith(JP_SPACE) || name.startsWith(JP_SPACE)){
 			return false;
 		}
