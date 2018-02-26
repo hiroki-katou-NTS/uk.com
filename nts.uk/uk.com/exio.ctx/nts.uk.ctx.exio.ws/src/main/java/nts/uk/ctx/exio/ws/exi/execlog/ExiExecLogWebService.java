4ac51229b838d2d6e145b9f091af6f2a4ea5f71a
BinhHX-3si <binh.hx@3si.vn>
2018-02-26 16:52:53 +0700

    Updated R

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.ws/src/main/java/nts/uk/ctx/exio/ws/exi/execlog/ExiExecLogWebService.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.ws/src/main/java/nts/uk/ctx/exio/ws/exi/execlog/ExiExecLogWebService.java
@@ -4,11 +4,22 @@
  *****************************************************************/
 package nts.uk.ctx.exio.ws.exi.execlog;
 
+import javax.inject.Inject;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import nts.arc.layer.ws.WebService;
+import nts.uk.ctx.exio.app.find.exi.execlog.ExacExeResultLogFinder;
+
 /**
  * The Class WorkplaceConfigInfoWebService
  */
-@Path(exio/ws/exi/execlog)
-@Produces(MediaType.APPLICATION_JSON)
+@Path("ctx/exio/ws/exi/execlog")
+@Produces("application/json")
 public class ExiExecLogWebService extends WebService {
-
+	@Inject
+	private ExacExeResultLogFinder exacExeResultLogFinder;
+	
+	
+	
 }
