241bd2e17bd3613c83869b33d1c4f2180c0465dd
3SI\anh.bd <anh.bd@3si.vn>
2018-02-28 18:01:28 +0700

    cmf001/a cmf001/l,m

---
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmf/001/a/cmf001.a.start.ts
@@ -0,0 +1,7 @@
+module nts.uk.com.view.cmf001.a {
+    __viewContext.ready(function() {
+        var screenModel = new viewmodel.ScreenModel();
+        //screenModel.startPage().done(function() {
+            __viewContext.bind(screenModel);
+    });
+}
