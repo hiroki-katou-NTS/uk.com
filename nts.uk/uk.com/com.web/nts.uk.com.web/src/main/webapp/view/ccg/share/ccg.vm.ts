25c10cb9fc77ed54b43863869ca611bd601369dc
nws-trangth <trangth@newwave.vn>
2018-03-02 10:23:22 +0700

    [CCG001] fix msg check future

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/share/ccg.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/share/ccg.vm.ts
@@ -766,9 +766,11 @@ module nts.uk.com.view.ccg.share.ccg {
              */
             private isNotFutureDate(acquiredBaseDate: string): boolean {
                 let self = this;
-                if (self.isFutureDate(moment.utc(acquiredBaseDate))) {
-                    nts.uk.ui.dialog.alertError({ messageId: "Msg_853" });
-                    return false;
+                if (self.showBaseDate){
+                    if (self.isFutureDate(moment.utc(acquiredBaseDate))) {
+                        nts.uk.ui.dialog.alertError({ messageId: "Msg_853" });
+                        return false;
+                    }
                 }
                 if (self.isFutureDate(self.periodEnd())) {
                     nts.uk.ui.dialog.alertError({ messageId: "Msg_860" });
