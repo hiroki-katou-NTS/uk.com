11a683f9dd95cc81adfe97cf8abfafe58911b173
ThuongDangXuan <thuong_dx@nittsusystem-vn.com>
2018-03-02 09:06:15 +0700

    update code domain for kal001 function

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/alarm/alarmlist/ExtractAlarmListService.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/alarm/alarmlist/ExtractAlarmListService.java
@@ -18,7 +18,7 @@ import nts.uk.ctx.at.function.dom.alarm.extraprocessstatus.AlarmListExtraProcess
 import nts.uk.shr.com.context.AppContexts;
 
 @Stateless
-public class CheckOutputAlarmListService {
+public class ExtractAlarmListService {
 
 	@Inject
 	private AlarmListExtraProcessStatusRepository alListExtraProcessStatusRepo;
@@ -26,18 +26,18 @@ public class CheckOutputAlarmListService {
 	@Inject
 	private AggregationProcessService aggregationProcessService;
 
-	public List<AlarmExtraValueWkReDto> checkOutputAlarmList(List<String> listEmployee, String checkPatternCode,
-			List<OutputScreenA> listOutputScreenA) {
+	public ExtractedAlarmDto extractAlarm(List<String> listEmployee, String checkPatternCode,
+			List<PeriodByAlarmCategory> periodByCategory) {		
 		String companyID = AppContexts.user().companyId();
-		String userLogin = AppContexts.user().userId();
+		String employeeId = AppContexts.user().employeeId();
 		
 		// ドメインモデル「アラームリスト抽出処理状況」をチェックする
 		Optional<AlarmListExtraProcessStatus> alarmListExtraProcessStatus = alListExtraProcessStatusRepo
 				.getAlListExtaProcessByEndDate(companyID);
 		// チェック条件に当てはまる場合 (When the check conditions apply)
-		if (!alarmListExtraProcessStatus.isPresent()) {
+		if (alarmListExtraProcessStatus.isPresent()) {
 			// 情報メッセージ(#Msg_993)を表示する
-			throw new BusinessException("Msg_993");
+			return new ExtractedAlarmDto(null, true, false);
 		}
 		
 		// チェック条件に当てはまらない場合(When it does not fit the check condition)
@@ -48,19 +48,19 @@ public class CheckOutputAlarmListService {
 		}
 		
 		// ドメインモデル「アラームリスト抽出処理状況」を作成する
-		AlarmListExtraProcessStatus alarmExtraProcessStatus = new AlarmListExtraProcessStatus(companyID, GeneralDate.today(), GeneralDateTime.now().seconds(),
-				userLogin, null, null);
+		AlarmListExtraProcessStatus alarmExtraProcessStatus = new AlarmListExtraProcessStatus(companyID, GeneralDate.today(), GeneralDateTime.now().minutes(),
+				employeeId, null, null);
 		this.alListExtraProcessStatusRepo.addAlListExtaProcess(alarmExtraProcessStatus);
 		
 		// 勤務実績のアラームリストの集計処理を行う
 		List<AlarmExtraValueWkReDto> listAlarmExtraValueWR = aggregationProcessService.processAlarmListWorkRecord(listEmployee,
-				checkPatternCode, listOutputScreenA);
+				checkPatternCode, periodByCategory);
 		// ドメインモデル「アラームリスト抽出処理状況」を更新する
-		alarmExtraProcessStatus.setEndDateAndEndTime(GeneralDate.today(), GeneralDateTime.now().seconds());
+		alarmExtraProcessStatus.setEndDateAndEndTime(GeneralDate.today(), GeneralDateTime.now().minutes());
 		this.alListExtraProcessStatusRepo.updateAlListExtaProcess(alarmExtraProcessStatus);
 
 		// 集計結果を確認する sort list
-		Comparator<AlarmExtraValueWkReDto> comparator = Comparator.comparing(AlarmExtraValueWkReDto::getWorkplaceID);
+		Comparator<AlarmExtraValueWkReDto> comparator = Comparator.comparing(AlarmExtraValueWkReDto::getWorkplaceName);
 		comparator = comparator.thenComparing(Comparator.comparing(AlarmExtraValueWkReDto::getEmployeeCode));
 		comparator = comparator.thenComparing(Comparator.comparing(AlarmExtraValueWkReDto::getAlarmValueDate));
 		comparator = comparator.thenComparing(Comparator.comparing(AlarmExtraValueWkReDto::getCategory));
@@ -70,11 +70,11 @@ public class CheckOutputAlarmListService {
 		// 集計データが無い場合
 		if (listAlarmExtraValueWR.isEmpty()) {
 			// 情報メッセージ(#Msg_835) を表示する
-			throw new BusinessException("Msg_835");
+			return  new ExtractedAlarmDto(null, false, true);
 		}
 		// 集計データがある場合
 		// B画面 ダイアログ「アラームリスト」を起動する
-		return sortedAlarmExtraValue;
+		return new ExtractedAlarmDto(sortedAlarmExtraValue, false, false);		
 
 	}
 
