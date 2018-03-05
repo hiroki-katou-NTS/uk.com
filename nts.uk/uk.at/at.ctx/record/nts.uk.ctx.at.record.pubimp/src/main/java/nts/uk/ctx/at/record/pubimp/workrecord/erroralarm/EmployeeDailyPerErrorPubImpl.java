690d9fd3d753f1bd61433f7d99c8a5bac753e14f
ThuongDangXuan <thuong_dx@nittsusystem-vn.com>
2018-03-05 13:39:14 +0700

    add pub and ac for EmployeeDailyPerError domain

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.pubimp/src/main/java/nts/uk/ctx/at/record/pubimp/workrecord/erroralarm/EmployeeDailyPerErrorPubImpl.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.pubimp/src/main/java/nts/uk/ctx/at/record/pubimp/workrecord/erroralarm/EmployeeDailyPerErrorPubImpl.java
@@ -1,13 +1,14 @@
 package nts.uk.ctx.at.record.pubimp.workrecord.erroralarm;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import nts.uk.ctx.at.record.dom.workrecord.erroralarm.EmployeeDailyPerError;
 import nts.uk.ctx.at.record.dom.workrecord.erroralarm.EmployeeDailyPerErrorRepository;
 import nts.uk.ctx.at.record.pub.workrecord.erroralarm.EmployeeDailyPerErrorPub;
 import nts.uk.ctx.at.record.pub.workrecord.erroralarm.EmployeeDailyPerErrorPubExport;
+import nts.uk.shr.com.context.AppContexts;
 import nts.uk.shr.com.time.calendar.period.DatePeriod;
 
 @Stateless
@@ -16,17 +17,15 @@ public class EmployeeDailyPerErrorPubImpl implements EmployeeDailyPerErrorPub {
 	private EmployeeDailyPerErrorRepository repo;
 
 	@Override
-	public Optional<EmployeeDailyPerErrorPubExport> getByErrorCode(String employeeId, DatePeriod datePeriod,
-			String errorCode) {
-		List<EmployeeDailyPerError> employeeDailyList = repo.findByPeriodOrderByYmd(employeeId, datePeriod);
-		Optional<EmployeeDailyPerError> employeeDailyOpt = employeeDailyList.stream()
-				.filter(e -> e.getErrorAlarmWorkRecordCode().v().equals(errorCode)).findFirst();
-		if (employeeDailyOpt.isPresent()) {
-			EmployeeDailyPerError e = employeeDailyOpt.get();
-			return Optional.of(new EmployeeDailyPerErrorPubExport(e.getCompanyID(), e.getEmployeeID(), e.getDate(),
-					e.getErrorAlarmWorkRecordCode().v(), e.getAttendanceItemList(), e.getErrorCancelAble()));
-		}
-		return Optional.empty();
+	public List<EmployeeDailyPerErrorPubExport> getByErrorCode(String employeeId, DatePeriod datePeriod,
+			List<String> errorCodes) {
+		String companyID = AppContexts.programId();
+		List<EmployeeDailyPerError> employeeDailyList = repo.findByPeriodOrderByYmd(employeeId, datePeriod).stream()
+				.filter(e -> errorCodes.contains(e.getErrorAlarmWorkRecordCode().v())
+						&& e.getCompanyID().equals(companyID)).collect(Collectors.toList());
+		
+		return employeeDailyList.stream().map(e ->new EmployeeDailyPerErrorPubExport(e.getCompanyID(), e.getEmployeeID(), e.getDate(),
+				e.getErrorAlarmWorkRecordCode().v(), e.getAttendanceItemList(), e.getErrorCancelAble())).collect(Collectors.toList());
 	}
 
 }
