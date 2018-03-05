1747d03c0481c41940537e0db7d4c04f1eceb6be
ThuongDangXuan <thuong_dx@nittsusystem-vn.com>
2018-03-05 20:08:01 +0700

    pub and ac for DailyRecordWork from at/record

---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.pubimp/src/main/java/nts/uk/ctx/at/record/pubimp/dailyperform/DailyRecordWorkPubImpl.java
@@ -0,0 +1,27 @@
+package nts.uk.ctx.at.record.pubimp.dailyperform;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import nts.arc.time.GeneralDate;
+import nts.uk.ctx.at.record.app.find.dailyperform.DailyRecordDto;
+import nts.uk.ctx.at.record.app.find.dailyperform.DailyRecordWorkFinder;
+import nts.uk.ctx.at.record.pub.dailyperform.DailyRecordWorkExport;
+import nts.uk.ctx.at.record.pub.dailyperform.DailyRecordWorkPub;
+import nts.uk.ctx.at.shared.dom.attendance.util.AttendanceItemUtil;
+
+@Stateless
+public class DailyRecordWorkPubImpl implements DailyRecordWorkPub{
+	@Inject
+	private DailyRecordWorkFinder fullFinder;
+	
+	
+	public DailyRecordWorkExport getByEmployee(String employeeId, GeneralDate baseDate, List<Integer> itemIds) {
+		DailyRecordWorkExport result = new DailyRecordWorkExport();
+		DailyRecordDto itemDtos = this.fullFinder.find(employeeId, baseDate);
+		result.setItems(AttendanceItemUtil.toItemValues(itemDtos, itemIds));
+		result.setEmployeeId(employeeId);
+		result.setDate(baseDate);
+		return result;
+	}
+}
