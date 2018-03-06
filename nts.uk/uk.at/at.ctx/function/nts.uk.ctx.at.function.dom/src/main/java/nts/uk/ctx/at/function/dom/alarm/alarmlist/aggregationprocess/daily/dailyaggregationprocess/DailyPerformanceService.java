d91e459867c424c6c099001933162f3cba67efcb
ThuongDangXuan <thuong_dx@nittsusystem-vn.com>
2018-03-06 17:07:04 +0700

    add service generate value when extract daily alarm

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/alarm/alarmlist/aggregationprocess/daily/dailyaggregationprocess/DailyPerformanceService.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/alarm/alarmlist/aggregationprocess/daily/dailyaggregationprocess/DailyPerformanceService.java
@@ -1,5 +1,6 @@
 package nts.uk.ctx.at.function.dom.alarm.alarmlist.aggregationprocess.daily.dailyaggregationprocess;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -11,6 +12,7 @@ import nts.arc.enums.EnumAdaptor;
 import nts.uk.ctx.at.function.dom.adapter.ErrorAlarmWorkRecordAdapter;
 import nts.uk.ctx.at.function.dom.adapter.ErrorAlarmWorkRecordAdapterDto;
 import nts.uk.ctx.at.function.dom.adapter.WorkRecordExtraConAdapter;
+import nts.uk.ctx.at.function.dom.adapter.dailyperform.DailyRecordWorkAdapter;
 import nts.uk.ctx.at.function.dom.adapter.eralworkrecorddto.ErrorAlarmConAdapterDto;
 import nts.uk.ctx.at.function.dom.adapter.workrecord.erroralarm.FuncEmployeeDailyPerErrorAdapter;
 import nts.uk.ctx.at.function.dom.adapter.workrecord.erroralarm.FuncEmployeeDailyPerErrorImport;
@@ -19,11 +21,12 @@ import nts.uk.ctx.at.function.dom.alarm.alarmdata.ValueExtractAlarm;
 import nts.uk.ctx.at.function.dom.alarm.alarmlist.FuncEmployeeSearchDto;
 import nts.uk.ctx.at.function.dom.alarm.alarmlist.PeriodByAlarmCategory;
 import nts.uk.ctx.at.function.dom.alarm.checkcondition.daily.DailyAlarmCondition;
+import nts.uk.ctx.at.shared.dom.attendance.util.item.ItemValue;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattendanceitem.adapter.DailyAttendanceItemNameAdapter;
 import nts.uk.shr.com.time.calendar.period.DatePeriod;
 
 @Stateless
-public class GenerateValueAlarmOfDailyPerformanceService {
+public class DailyPerformanceService {
 	// 社員の日別実績エラー一覧
 	@Inject
 	private FuncEmployeeDailyPerErrorAdapter employeeDailyAdapter;
@@ -32,18 +35,24 @@ public class GenerateValueAlarmOfDailyPerformanceService {
 	@Inject
 	private ErrorAlarmWorkRecordAdapter errorAlarmWorkRecordAdapter;
 
-	@Inject
-	private DailyAttendanceItemNameAdapter dailyAttendanceItemNameAdapter;
-
 	// 勤務実績のエラーアラームチェック
 	@Inject
 	private WorkRecordExtraConAdapter workRecordExtraConAdapter;
 	
+	// Get attendance item name 勤怠項目名
+	@Inject
+	private DailyAttendanceItemNameAdapter dailyAttendanceItemNameAdapter;
 	
-	public void AggregationProcess(DailyAlarmCondition dailyAlarmCondition, PeriodByAlarmCategory period,
+	// get attendance value　勤怠項目価値
+	@Inject
+	private DailyRecordWorkAdapter dailyRecordWorkAdapter;
+	
+	public List<ValueExtractAlarm> aggregationProcess(DailyAlarmCondition dailyAlarmCondition, PeriodByAlarmCategory period,
 			FuncEmployeeSearchDto employee, String companyID ) {
+		List<ValueExtractAlarm> valueExtractAlarmList= new ArrayList<>();
 		
 		List<String> listCode = dailyAlarmCondition.getErrorAlarmCode();
+		if(listCode ==null || listCode.isEmpty()) return null;
 		
 		// 勤務実績のエラーアラーム
 		Map<String, ErrorAlarmWorkRecordAdapterDto> errorAlarmWorkRecord = errorAlarmWorkRecordAdapter
@@ -64,18 +73,24 @@ public class GenerateValueAlarmOfDailyPerformanceService {
 		if (dailyAlarmCondition.isAddApplication()) {
 
 		} else {
-			
-			
-			
-			
-			for (FuncEmployeeDailyPerErrorImport eDaily : employeeDailyList) {
+			for (FuncEmployeeDailyPerErrorImport eDaily : employeeDailyList) {								
+				Map<Integer, String> attendanceNameMap =  dailyAttendanceItemNameAdapter.getDailyAttendanceItemNameAsMapName(eDaily.getAttendanceItemList());
+				List<ItemValue>  attendanceValueList = dailyRecordWorkAdapter.getByEmployeeList(employee.getId(), eDaily.getDate(), eDaily.getAttendanceItemList()).getItems();
+				
+				// アラーム値メッセージ caculate from 	attendance Name and attendance value 		
+				String messageContent ="";
+				
 				ValueExtractAlarm data = new ValueExtractAlarm(employee.getWorkplaceId(), employee.getId(),
 						eDaily.getDate(), EnumAdaptor.convertToValueName(AlarmCategory.DAILY).getLocalizedName(),
-						errorAlarmWorkRecord.get(eDaily.getErrorAlarmWorkRecordCode()).getCode(), "",
+						errorAlarmWorkRecord.get(eDaily.getErrorAlarmWorkRecordCode()).getName(), messageContent ,
 						workRecordExtraConMap.get(eDaily.getErrorAlarmWorkRecordCode()).getDisplayMessage());
+				
+				valueExtractAlarmList.add(data);
 			}
 
 		}
+		
+		return valueExtractAlarmList;
 		
 	}
 	
