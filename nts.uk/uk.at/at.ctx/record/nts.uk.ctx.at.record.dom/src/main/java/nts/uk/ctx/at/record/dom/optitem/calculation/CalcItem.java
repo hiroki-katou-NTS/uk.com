7bd77c4e7dd0dc31705cb59e849e3e51114bceed
NTS\keisuke_hoshina <keisuke_hoshina@nittsusystem.co.jp>
2018-03-06 11:32:54 +0900

    add class

---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/optitem/calculation/CalcItem.java
@@ -0,0 +1,34 @@
+package nts.uk.ctx.at.record.dom.optitem.calculation;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 計算項目
+ * @author keisuke_hoshina
+ *
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+public class CalcItem {
+	private int leftItemValue = 0;
+	private int rightItemValue = 0;
+	
+	/**
+	 * 項目順序を基に計算項目値を保持
+	 * @param dispOrder　項目順番
+	 * @param setValue　計算値
+	 */
+	public void setValueByOrder(SettingItemOrder dispOrder,int setValue) {
+		switch(dispOrder) {
+		case LEFT:
+			this.leftItemValue = setValue;
+		case RIGHT:
+			this.rightItemValue = setValue;
+		default:
+			throw new RuntimeException("unknown Item Order:"+dispOrder);
+		}
+	}
+}
