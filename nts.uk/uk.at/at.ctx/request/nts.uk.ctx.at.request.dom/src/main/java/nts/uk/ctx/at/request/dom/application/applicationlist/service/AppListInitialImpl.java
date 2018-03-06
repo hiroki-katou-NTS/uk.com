8728205c6d10423e3fff6a5a529a751dd93f57fc
NSVN-TranThiHoa <hoa_tt@nittsusystem-vn.com>
2018-03-06 14:10:31 +0700

    CMM045: doi ung req 23

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/applicationlist/service/AppListInitialImpl.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/applicationlist/service/AppListInitialImpl.java
@@ -577,56 +577,56 @@ public class AppListInitialImpl implements AppListInitialRepository{
 	public TimeResultOutput getAppListAchievementOverTime(String sID, GeneralDate date, List<OverTimeFrame> time) {
 		//Imported(申請承認)「勤務実績」を取得する - req #5
 		RecordWorkInfoImport record = recordWkpInfoAdapter.getRecordWorkInfo(sID, date);
-//		DailyAttendanceTimeCaculationImport cal = calTime.getCalculation(sID, date, record.getWorkTypeCode(), record.getWorkTimeCode(),record.getAttendanceStampTimeFirst(), record.getLeaveStampTimeFirst(), 0, 0);
+		DailyAttendanceTimeCaculationImport cal = calTime.getCalculation(sID, date, record.getWorkTypeCode(), record.getWorkTimeCode(),record.getAttendanceStampTimeFirst(), record.getLeaveStampTimeFirst(), 0, 0);
 		//Imported(申請承認)「計算残業時間」を取得する - req #23
 		boolean checkColor = false;
 		List<OverTimeFrame> lstFrameResult = new ArrayList<>();
-//		for(Map.Entry<Integer,TimeWithCalculationImport> entry : cal.getOverTime().entrySet()){
-//			for(OverTimeFrame i : time){
-//				if(i.getFrameNo() == entry.getKey()){
-//					int a =	entry.getValue().getCalTime();
-//					if(a < i.getApplicationTime()){
-//						checkColor = true;
-//					}
-//					OverTimeFrame frameRes = i;
-//					frameRes.setApplicationTime(a);
-//				}
-//			}
-//		}
-//		// TODO Auto-generated method stub
-//		//Imported(申請承認)「計算休出時間」を取得する - req #23
-//		for(Map.Entry<Integer,TimeWithCalculationImport> entry : cal.getHolidayWorkTime().entrySet()){
-//			for(OverTimeFrame i : time){
-//				if(i.getFrameNo() == entry.getKey()){
-//					int a =	entry.getValue().getCalTime();
-//					if(a < i.getApplicationTime()){
-//						checkColor = true;
-//					}
-//				}
-//			}
-//		}
-//		// TODO Auto-generated method stub
-//		//Imported(申請承認)「計算加給時間」を取得する - req #23
-//		for(Map.Entry<Integer,Integer> entry : cal.getBonusPayTime().entrySet()){
-//			for(OverTimeFrame i : time){
-//				if(i.getFrameNo() == entry.getKey()){
-//					int a =	entry.getValue().intValue();
-//					if(a < i.getApplicationTime()){
-//						checkColor = true;
-//					}
-//				}
-//			}
-//		}
-//		for(Map.Entry<Integer,Integer> entry : cal.getSpecBonusPayTime().entrySet()){
-//			for(OverTimeFrame i : time){
-//				if(i.getFrameNo() == entry.getKey()){
-//					int a =	entry.getValue().intValue();
-//					if(a < i.getApplicationTime()){
-//						checkColor = true;
-//					}
-//				}
-//			}
-//		}
+		for(Map.Entry<Integer,TimeWithCalculationImport> entry : cal.getOverTime().entrySet()){
+			for(OverTimeFrame i : time){
+				if(i.getFrameNo() == entry.getKey()){
+					int a =	entry.getValue().getCalTime();
+					if(a < i.getApplicationTime()){
+						checkColor = true;
+					}
+					OverTimeFrame frameRes = i;
+					frameRes.setApplicationTime(a);
+				}
+			}
+		}
+		// TODO Auto-generated method stub
+		//Imported(申請承認)「計算休出時間」を取得する - req #23
+		for(Map.Entry<Integer,TimeWithCalculationImport> entry : cal.getHolidayWorkTime().entrySet()){
+			for(OverTimeFrame i : time){
+				if(i.getFrameNo() == entry.getKey()){
+					int a =	entry.getValue().getCalTime();
+					if(a < i.getApplicationTime()){
+						checkColor = true;
+					}
+				}
+			}
+		}
+		// TODO Auto-generated method stub
+		//Imported(申請承認)「計算加給時間」を取得する - req #23
+		for(Map.Entry<Integer,Integer> entry : cal.getBonusPayTime().entrySet()){
+			for(OverTimeFrame i : time){
+				if(i.getFrameNo() == entry.getKey()){
+					int a =	entry.getValue().intValue();
+					if(a < i.getApplicationTime()){
+						checkColor = true;
+					}
+				}
+			}
+		}
+		for(Map.Entry<Integer,Integer> entry : cal.getSpecBonusPayTime().entrySet()){
+			for(OverTimeFrame i : time){
+				if(i.getFrameNo() == entry.getKey()){
+					int a =	entry.getValue().intValue();
+					if(a < i.getApplicationTime()){
+						checkColor = true;
+					}
+				}
+			}
+		}
 		return new TimeResultOutput(checkColor, lstFrameResult);
 	}
 	/**
