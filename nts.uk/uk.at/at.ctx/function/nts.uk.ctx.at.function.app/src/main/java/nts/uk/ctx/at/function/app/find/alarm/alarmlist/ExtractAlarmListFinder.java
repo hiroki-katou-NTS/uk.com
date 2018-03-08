090cf3299ae36eeb6a276000133d55191c010e8c
ThuongDangXuan <thuong_dx@nittsusystem-vn.com>
2018-03-08 15:42:31 +0700

    add api extract alarm

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.app/src/main/java/nts/uk/ctx/at/function/app/find/alarm/alarmlist/ExtractAlarmListFinder.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.app/src/main/java/nts/uk/ctx/at/function/app/find/alarm/alarmlist/ExtractAlarmListFinder.java
@@ -1,13 +1,9 @@
 package nts.uk.ctx.at.function.app.find.alarm.alarmlist;
 
-import java.util.List;
-
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import nts.uk.ctx.at.function.dom.alarm.alarmlist.ExtractAlarmListService;
 import nts.uk.ctx.at.function.dom.alarm.alarmlist.ExtractedAlarmDto;
-import nts.uk.ctx.at.function.dom.alarm.alarmlist.EmployeeSearchDto;
-import nts.uk.ctx.at.function.dom.alarm.alarmlist.PeriodByAlarmCategory;
 
 @Stateless
 public class ExtractAlarmListFinder {
@@ -15,8 +11,7 @@ public class ExtractAlarmListFinder {
 	@Inject
 	private ExtractAlarmListService extractAlarmListService;
 
-	public ExtractedAlarmDto checkOutputAlarmList(List<EmployeeSearchDto> listEmployee, String checkPatternCode,
-			List<PeriodByAlarmCategory> listPeriodByCategory) {
-		return this.extractAlarmListService.extractAlarm(listEmployee, checkPatternCode, listPeriodByCategory);
+	public ExtractedAlarmDto extractAlarm(ExtractAlarmQuery query) {
+		return this.extractAlarmListService.extractAlarm(query.getListEmployee(), query.getAlarmCode(), query.getListPeriodByCategory());
 	}
 }
