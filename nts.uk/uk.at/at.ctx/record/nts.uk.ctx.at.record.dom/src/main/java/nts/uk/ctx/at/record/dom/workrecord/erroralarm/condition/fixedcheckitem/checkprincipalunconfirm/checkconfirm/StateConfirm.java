adc3bef17fb50b0f3cd63ef66571b1ae4d584cbc
NSVN-TrieuKhacTu <tu_tk@nittsusystem-vn.com>
2018-03-09 18:12:09 +0700

    update code kal001

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/workrecord/erroralarm/condition/fixedcheckitem/checkprincipalunconfirm/checkconfirm/StateConfirm.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/workrecord/erroralarm/condition/fixedcheckitem/checkprincipalunconfirm/checkconfirm/StateConfirm.java
@@ -1,6 +1,5 @@
 package nts.uk.ctx.at.record.dom.workrecord.erroralarm.condition.fixedcheckitem.checkprincipalunconfirm.checkconfirm;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,9 +7,13 @@ import nts.arc.time.GeneralDate;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class StateConfirm {
 	GeneralDate date;
 	boolean state;
+	public StateConfirm(GeneralDate date, boolean state) {
+		super();
+		this.date = date;
+		this.state = state;
+	}
 
 }
