e25f79413b3c8ca22d5576cc73cad228d913acc7
haivh <haivh@newwave.vn>
2018-03-12 15:57:38 +0700

    fix inject CommandHandler

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/divergence/time/history/CompanyDivergenceReferenceTimeHistoryCommandHandler.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/divergence/time/history/CompanyDivergenceReferenceTimeHistoryCommandHandler.java
@@ -2,9 +2,12 @@ package nts.uk.ctx.at.record.app.command.divergence.time.history;
 
 import java.util.UUID;
 
+import javax.ejb.Stateless;
+
 import nts.arc.layer.app.command.CommandHandler;
 import nts.arc.layer.app.command.CommandHandlerContext;
 
+@Stateless
 public class CompanyDivergenceReferenceTimeHistoryCommandHandler extends CommandHandler<CompanyDivergenceReferenceTimeHistoryCommand>{
 
 	@Override
