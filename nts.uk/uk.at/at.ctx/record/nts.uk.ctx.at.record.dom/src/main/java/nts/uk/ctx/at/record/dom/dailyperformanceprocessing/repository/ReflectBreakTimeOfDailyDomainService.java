f02af203047b0878846da4931ae96fa42f509696
NSVC\dungdt <dung24391@gmail.com>
2018-03-12 15:32:03 +0700

    update KIF001

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/dailyperformanceprocessing/repository/ReflectBreakTimeOfDailyDomainService.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/dailyperformanceprocessing/repository/ReflectBreakTimeOfDailyDomainService.java
@@ -4,8 +4,9 @@ import java.util.List;
 
 import nts.uk.ctx.at.record.dom.dailyperformanceprocessing.output.StampReflectTimezoneOutput;
 import nts.uk.ctx.at.record.dom.workinformation.WorkInfoOfDailyPerformance;
+import nts.uk.ctx.at.record.dom.worktime.TimeLeavingWork;
 
 public interface ReflectBreakTimeOfDailyDomainService {
 		//就業時間帯の休憩時間帯を日別実績に反映する
-		public void reflectBreakTime(String companyId,List<StampReflectTimezoneOutput> lstStampReflectTimezone, WorkInfoOfDailyPerformance WorkInfo);
+		public void reflectBreakTime(String companyId,List<TimeLeavingWork> timeLeavingWorks, WorkInfoOfDailyPerformance WorkInfo);
 }
