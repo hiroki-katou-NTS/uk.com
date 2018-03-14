93aaa92b511616c10d137c9e0306b8210d53d98e
3SI\hiep.ld <hiep.ld@3si.vn>
2018-03-14 16:09:37 +0700

    cmf002/ nhung phan da lam

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmf/002/v1/cmf002.v1.start.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmf/002/v1/cmf002.v1.start.ts
@@ -1,4 +1,4 @@
-module nts.uk.com.view.cmf002.i {
+module nts.uk.com.view.cmf002.v1 {
     __viewContext.ready(function() {
         __viewContext['screenModel'] = new viewmodel.ScreenModel();
 //        __viewContext['screenModel'].start().done(function() {
