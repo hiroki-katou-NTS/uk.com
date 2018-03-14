9f8a8c4b75b7ccd3129368e1a557c2cf9caa8ccb
NSVC\hungnm <hungnm910@gmail.com>
2018-03-14 08:50:19 +0700

    kdw004: dom

---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/approvalmanagement/enums/ConfirmationOfManagerOrYouself.java
@@ -0,0 +1,27 @@
+/**
+ * 5:02:27 PM Mar 9, 2018
+ */
+package nts.uk.ctx.at.record.dom.approvalmanagement.enums;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * @author hungnm
+ *
+ */
+//上司・本人確認の挙動
+@AllArgsConstructor
+public enum ConfirmationOfManagerOrYouself {
+	
+	/* エラーがあっても確認できる */
+	CAN_CHECK(0, "Enum_ConfirmationOfManagerOrYouself_CanCheck"),
+	/* エラーを訂正するまでチェックできない */
+	CAN_NOT_CHECK(1, "Enum_ConfirmationOfManagerOrYouself_CanNotCheck"),
+	/* エラーを訂正するまで登録できない */
+	CAN_NOT_REGISTER(2, "Enum_ConfirmationOfManagerOrYouself_CanNotRegister");
+
+	public final int value;
+
+	public final String nameId;
+	
+}
