750cadef686573f31adaba86d81a478d1aebf84f
HopNT3SI <32725057+HopNT3SI@users.noreply.github.com>
2018-03-15 16:47:31 +0700

    Update primitive

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/remainingnumber/excessleave/OccurrenceUnit.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/remainingnumber/excessleave/OccurrenceUnit.java
@@ -1,8 +1,10 @@
 package nts.uk.ctx.at.record.dom.remainingnumber.excessleave;
 
-import nts.arc.primitive.TimeClockPrimitiveValue;
+import nts.arc.primitive.TimeDurationPrimitiveValue;
+import nts.arc.primitive.constraint.TimeRange;
 
-public class OccurrenceUnit extends TimeClockPrimitiveValue<OccurrenceUnit>{
+@TimeRange(min ="0:00", max="999:59")
+public class OccurrenceUnit extends TimeDurationPrimitiveValue<OccurrenceUnit>{
 
 	private static final long serialVersionUID = 1L;
 
