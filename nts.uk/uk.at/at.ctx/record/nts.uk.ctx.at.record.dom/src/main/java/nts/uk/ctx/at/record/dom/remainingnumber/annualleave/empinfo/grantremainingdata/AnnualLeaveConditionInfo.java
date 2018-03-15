409010e106e49673b63715311fac2ab91806433f
NSVN-PhamVanDan <dan_pv@nittsusystem-vn.com>
2018-03-15 16:48:08 +0700

    add comment name of variable

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/remainingnumber/annualleave/empinfo/grantremainingdata/AnnualLeaveConditionInfo.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/remainingnumber/annualleave/empinfo/grantremainingdata/AnnualLeaveConditionInfo.java
@@ -6,10 +6,19 @@ import nts.uk.ctx.at.record.dom.remainingnumber.base.YearDayNumber;
 @Getter
 public class AnnualLeaveConditionInfo {
 	
+	/**
+	 * 所定日数
+	 */
 	private YearDayNumber prescribedDays;
 	
+	/**
+	 * 控除日数
+	 */
 	private YearDayNumber deductedDays;
 	
+	/**
+	 * 労働日数
+	 */
 	private YearDayNumber workingDays;
 	
 }
