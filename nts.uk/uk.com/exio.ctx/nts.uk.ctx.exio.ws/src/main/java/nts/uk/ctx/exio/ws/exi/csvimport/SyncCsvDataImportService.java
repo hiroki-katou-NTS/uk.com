70a636be110d855c45c07f5319edc393058efcc4
hieunv3 <hieu.nv@3si.vn>
2018-03-15 17:00:33 +0700

    create code cmf001-q

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.ws/src/main/java/nts/uk/ctx/exio/ws/exi/csvimport/SyncCsvDataImportService.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.ws/src/main/java/nts/uk/ctx/exio/ws/exi/csvimport/SyncCsvDataImportService.java
@@ -7,28 +7,33 @@ import javax.ws.rs.Produces;
 
 import nts.arc.layer.ws.WebService;
 import nts.uk.ctx.exio.app.command.exi.csvimport.CsvImportDataCommand;
-import nts.uk.ctx.exio.app.command.exi.csvimport.SyncCsvImportDataCommandHandler;
+import nts.uk.ctx.exio.app.command.exi.csvimport.SyncCsvExecuteImportDataCommandHandler;
 import nts.uk.ctx.exio.app.command.exi.csvimport.CsvImportExecutionRespone;
+import nts.uk.ctx.exio.app.command.exi.csvimport.SyncCsvCheckImportDataCommandHandler;
 @Path("exio/exi/csvimport")
 @Produces("application/json")
 public class SyncCsvDataImportService extends WebService{
 	
 	@Inject
-	SyncCsvImportDataCommandHandler importHandler;
+	SyncCsvCheckImportDataCommandHandler checkImportHandler;
+
+	@Inject
+	SyncCsvExecuteImportDataCommandHandler excuteImportHandler;
+
+	@POST
+	@Path("check")
+	public CsvImportExecutionRespone csvChecktProcess(CsvImportDataCommand command){
+		CsvImportExecutionRespone response = new CsvImportExecutionRespone();
+		response.taskInfor = checkImportHandler.handle(command);
+		return response;
+	}
 	
 	@POST
 	@Path("execution")
 	public CsvImportExecutionRespone csvImportProcess(CsvImportDataCommand command){
 		CsvImportExecutionRespone response = new CsvImportExecutionRespone();
-		response.taskInfor = importHandler.handle(command);
+		response.taskInfor = excuteImportHandler.handle(command);
 		return response;
 	}
-	
-	@POST
-	@Path("check")
-	public CsvImportExecutionRespone csvChecktProcess(CsvImportDataCommand command){
-		CsvImportExecutionRespone response = new CsvImportExecutionRespone();
-		response.taskInfor = importHandler.handle(command);
-		return response;
-	}
+
 }
