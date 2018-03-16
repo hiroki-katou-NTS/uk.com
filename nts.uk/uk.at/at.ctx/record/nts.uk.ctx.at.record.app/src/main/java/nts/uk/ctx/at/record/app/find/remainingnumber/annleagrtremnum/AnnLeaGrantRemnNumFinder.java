31922b5ff81c7a9ca9a6edb77f27c9c9e51aa5ac
NSVN-PhamVanDan <dan_pv@nittsusystem-vn.com>
2018-03-16 15:41:57 +0700

    update finders

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/find/remainingnumber/annleagrtremnum/AnnLeaGrantRemnNumFinder.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/find/remainingnumber/annleagrtremnum/AnnLeaGrantRemnNumFinder.java
@@ -1,26 +1,24 @@
 package nts.uk.ctx.at.record.app.find.remainingnumber.annleagrtremnum;
 
-import java.util.Optional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import nts.uk.ctx.at.record.dom.remainingnumber.annualleave.empinfo.grantremainingdata.AnnLeaGrantRemDataRepository;
 import nts.uk.ctx.at.record.dom.remainingnumber.annualleave.empinfo.grantremainingdata.AnnualLeaveGrantRemainingData;
-import nts.uk.shr.pereg.app.find.PeregQuery;
 
 @Stateless
-public class AnnLeaGrantRemnNumFinder{
-	
+public class AnnLeaGrantRemnNumFinder {
+
 	@Inject
 	private AnnLeaGrantRemDataRepository annLeaDataRepo;
 
-	public AnnLeaGrantRemnNumDto getSingleData(PeregQuery query) {
-		Optional<AnnualLeaveGrantRemainingData> annLeaDataOpt = annLeaDataRepo.get(query.getEmployeeId());
-		if ( annLeaDataOpt.isPresent()) {
-			return AnnLeaGrantRemnNumDto.createFromDomain(annLeaDataOpt.get());
-		}
-		return null;
+	public List<AnnLeaGrantRemnNumDto> getListData(String employeeId) {
+		List<AnnualLeaveGrantRemainingData> annLeaDataList = annLeaDataRepo.find(employeeId);
+		return annLeaDataList.stream().map(domain -> AnnLeaGrantRemnNumDto.createFromDomain(domain))
+				.collect(Collectors.toList());
 	}
 
 }
--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/find/remainingnumber/rervleagrtremnum/ResvLeaGrantRemNumFinder.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/find/remainingnumber/rervleagrtremnum/ResvLeaGrantRemNumFinder.java
@@ -1,26 +1,24 @@
 package nts.uk.ctx.at.record.app.find.remainingnumber.rervleagrtremnum;
 
-import java.util.Optional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import nts.uk.ctx.at.record.dom.remainingnumber.reserveleave.empinfo.grantremainingdata.RervLeaGrantRemDataRepository;
 import nts.uk.ctx.at.record.dom.remainingnumber.reserveleave.empinfo.grantremainingdata.ReserveLeaveGrantRemainingData;
-import nts.uk.shr.pereg.app.find.PeregQuery;
 
 @Stateless
-public class ResvLeaGrantRemNumFinder{
-	
+public class ResvLeaGrantRemNumFinder {
+
 	@Inject
 	private RervLeaGrantRemDataRepository repository;
 
-	public ResvLeaGrantRemNumDto getSingleData(PeregQuery query) {
-		Optional<ReserveLeaveGrantRemainingData> dataOpt = repository.get(query.getEmployeeId());
-		if (dataOpt.isPresent()) {
-			return ResvLeaGrantRemNumDto.createFromDomain(dataOpt.get());
-		}
-		return null;
+	public List<ResvLeaGrantRemNumDto> getListData(String employeeId) {
+		List<ReserveLeaveGrantRemainingData> dataList = repository.find(employeeId);
+		return dataList.stream().map(domain -> ResvLeaGrantRemNumDto.createFromDomain(domain))
+				.collect(Collectors.toList());
 	}
 
 }
