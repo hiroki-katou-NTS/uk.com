9ee46ef0b3dfebbf6430eea2f7a5167f5e6f3b02
NSVN-PhamVanDan <dan_pv@nittsusystem-vn.com>
2018-03-19 10:46:49 +0700

    do with new enums

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/remainingnumber/nursingcareleavemanagement/info/UpperLimitSetting.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/remainingnumber/nursingcareleavemanagement/info/UpperLimitSetting.java
@@ -4,12 +4,16 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public enum UpperLimitSetting {
-	//個人情報を参照（本年度のみ利用）
-	PER_INFO_FISCAL_YEAR(1),
+	
+	//家族情報を参照
+	FAMILY_INFO(1),
+	
 	//個人情報を参照（毎年利用）
 	PER_INFO_EVERY_YEAR(2),
-	//家族情報を参照
-	FAMILY_INFO(3);
+	
+	//個人情報を参照（本年度のみ利用）
+	PER_INFO_FISCAL_YEAR(3);
+	
 	public final int value;
 	
 }
