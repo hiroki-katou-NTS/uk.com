5293dd41fbd853be9733d3c51c115c3ae89fccc9
NSVC\tanlv <tan_lv@nittsusystem-vn.com>
2018-03-19 15:11:34 +0700

    KTG002 - A: Update view

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ktg/002/a/ktg002.a.sevice.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ktg/002/a/ktg002.a.sevice.ts
@@ -1,38 +1,7 @@
-module nts.uk.com.view.ktg002.a {
-    import ajax = nts.uk.request.ajax;
-    import format = nts.uk.text.format;
+module nts.uk.com.view.ktg002.a.service {
 
-    export module service {
-        var paths = {
-            getListOfWorkPlaceFunctions: "auth/wplmanagementauthority/workplacefunction/getlistworkplacefunction",
-            getListOfWorkPlaceauthorities: "auth/wplmanagementauthority/workplaceauthority/getallWorkplaceauthoritybyid/{0}"
-        }
-        /**
-         * get getListOfFunctionPermission by roleId
-         */
-        export function getListOfDescriptionFunctionPermission(classification : number): JQueryPromise<Array<any>> {
-            
-            switch (classification) {
-            
-            case 1: // workplace
-                return ajax("com", paths.getListOfWorkPlaceFunctions);
-            default:
-                let dfd = $.Deferred();
-            
-                dfd.resolve([]);
-                return dfd.promise();
-            }
-
-        }
-        export function getListOfAviabilityFunctionPermission(roleId : string, classification : number): JQueryPromise<Array<any>> {
-            switch (classification) {
-            case 1: // workplace
-                return ajax("com", format(paths.getListOfWorkPlaceauthorities, roleId));
-            default:
-                break;
-            }
-            return [];
-        }
+    var paths = {
         
     }
+    
 }
