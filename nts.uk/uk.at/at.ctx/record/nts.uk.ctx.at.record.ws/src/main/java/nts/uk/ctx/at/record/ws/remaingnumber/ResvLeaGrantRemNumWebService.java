bb2a1948b527c635ce97e540a38860e392f1735e
Leu Thi Lan <lan-lt@nittsusystem-vn.com>
2018-03-20 17:51:35 +0700

    update get data ResvLeaGrantRemNumWebService

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.ws/src/main/java/nts/uk/ctx/at/record/ws/remaingnumber/ResvLeaGrantRemNumWebService.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.ws/src/main/java/nts/uk/ctx/at/record/ws/remaingnumber/ResvLeaGrantRemNumWebService.java
@@ -1,5 +1,6 @@
 package nts.uk.ctx.at.record.ws.remaingnumber;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -8,13 +9,16 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
+import nts.arc.enums.EnumAdaptor;
 import nts.arc.layer.ws.WebService;
+import nts.arc.time.GeneralDate;
 import nts.uk.ctx.at.record.app.command.remainingnumber.rervleagrtremnum.AddResvLeaCommandHandler;
 import nts.uk.ctx.at.record.app.command.remainingnumber.rervleagrtremnum.ResvLeaGrantRemNumCommand;
 import nts.uk.ctx.at.record.app.command.remainingnumber.rervleagrtremnum.UpdateResvLeaCommandHandler;
 import nts.uk.ctx.at.record.app.find.remainingnumber.rervleagrtremnum.ResvLeaGrantRemNumDto;
+import nts.uk.ctx.at.record.dom.remainingnumber.base.LeaveExpirationStatus;
 
-@Path("at/record/remainnumber/resvLea")
+@Path("record/remainnumber/resv-lea")
 @Produces("application/json")
 public class ResvLeaGrantRemNumWebService extends WebService{
 
@@ -29,9 +33,9 @@ public class ResvLeaGrantRemNumWebService extends WebService{
 	private UpdateResvLeaCommandHandler updateHandler;
 	
 	@POST
-	@Path("getResvLea/{empId}")
-	public List<ResvLeaGrantRemNumDto> findResvLeaGrantRemnNum(@PathParam("empId") String employeeId) {
-		return null;
+	@Path("get-resv-lea/{empId}")
+	public List<ResvLeaGrantRemNumDto> getAllOK(@PathParam("empId") String employeeId) {
+		return getData();
 	}
 	
 	@POST
@@ -44,6 +48,15 @@ public class ResvLeaGrantRemNumWebService extends WebService{
 	@Path("update")
 	public void update(ResvLeaGrantRemNumCommand command) {
 		updateHandler.handle(command);
+	}
+	
+	private List<ResvLeaGrantRemNumDto> getData(){
+		List<ResvLeaGrantRemNumDto> lst = new ArrayList<>();
+		for(int i = 1; i < 20; i++) {
+			lst.add(new ResvLeaGrantRemNumDto(GeneralDate.fromString("2018/10/" + i, "YYYY/MM/DD"), GeneralDate.fromString("2018/05/" + i, "YYYY/MM/DD"), 
+					EnumAdaptor.valueOf(i%2==0?1: 0, LeaveExpirationStatus.class), Double.parseDouble(i + ""), Double.parseDouble(i + ""), Double.parseDouble(i + ""), Double.parseDouble(i + "")));
+		}
+		return lst;
 	}
 	
 }
