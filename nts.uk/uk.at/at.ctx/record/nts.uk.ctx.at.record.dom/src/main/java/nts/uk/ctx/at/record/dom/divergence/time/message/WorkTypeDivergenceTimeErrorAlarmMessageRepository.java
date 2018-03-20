81f32af27815b1da3985eebcce675d83c71921cd
anhlt-nws <anhlt@newwave.vn>
2018-03-20 13:21:50 +0700

    Add screen I's repo interface

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/divergence/time/message/WorkTypeDivergenceTimeErrorAlarmMessageRepository.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/divergence/time/message/WorkTypeDivergenceTimeErrorAlarmMessageRepository.java
@@ -1,5 +1,32 @@
 package nts.uk.ctx.at.record.dom.divergence.time.message;
 
+/**
+ * The Interface WorkTypeDivergenceTimeErrorAlarmMessageRepository.
+ */
 public interface WorkTypeDivergenceTimeErrorAlarmMessageRepository {
 
+	/**
+	 * Gets the by divergence time no.
+	 *
+	 * @param divergenceTimeNo
+	 *            the divergence time no
+	 * @return the by divergence time no
+	 */
+	public WorkTypeDivergenceTimeErrorAlarmMessage getByDivergenceTimeNo(Integer divergenceTimeNo);
+
+	/**
+	 * Adds the.
+	 *
+	 * @param message
+	 *            the message
+	 */
+	public void add(WorkTypeDivergenceTimeErrorAlarmMessage message);
+
+	/**
+	 * Update.
+	 *
+	 * @param message
+	 *            the message
+	 */
+	public void update(WorkTypeDivergenceTimeErrorAlarmMessage message);
 }
