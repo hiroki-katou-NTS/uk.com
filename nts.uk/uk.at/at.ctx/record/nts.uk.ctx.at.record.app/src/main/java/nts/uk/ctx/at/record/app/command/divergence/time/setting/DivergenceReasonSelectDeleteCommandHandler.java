559982f5d9e9451ce7e4979d6d6979ddbf78b5b3
quangnt <quangnt@newwave.vn>
2018-03-20 16:15:21 +0700

    add,save,delete DivergenceReasonSelect

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/divergence/time/setting/DivergenceReasonSelectDeleteCommandHandler.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/divergence/time/setting/DivergenceReasonSelectDeleteCommandHandler.java
@@ -1,5 +1,37 @@
 package nts.uk.ctx.at.record.app.command.divergence.time.setting;
 
-public class DivergenceReasonSelectDeleteCommandHandler {
+import javax.ejb.Stateless;
+import javax.inject.Inject;
 
+import nts.arc.layer.app.command.CommandHandler;
+import nts.arc.layer.app.command.CommandHandlerContext;
+import nts.uk.ctx.at.record.dom.divergence.time.setting.DivergenceReasonSelect;
+import nts.uk.ctx.at.record.dom.divergence.time.setting.DivergenceReasonSelectRepository;
+
+/**
+ * The Class DivergenceReasonSelectAddCommandHandler.
+ */
+@Stateless
+public class DivergenceReasonSelectDeleteCommandHandler extends CommandHandler<DivergenceReasonSelectDeleteCommand>  {
+
+	/** The div reason select repo. */
+	@Inject
+	DivergenceReasonSelectRepository divReasonSelectRepo;
+
+	@Override
+	protected void handle(CommandHandlerContext<DivergenceReasonSelectDeleteCommand> context) {
+		//get command
+		
+		DivergenceReasonSelectDeleteCommand command = context.getCommand();
+		
+		// Convert to domain
+		
+		DivergenceReasonSelect domain = new DivergenceReasonSelect(command);
+		
+		//Delete
+		
+		this.divReasonSelectRepo.delete(domain);
+		
+	}
+	
 }
--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/divergence/time/setting/DivergenceReasonSelectSaveCommandHandler.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/divergence/time/setting/DivergenceReasonSelectSaveCommandHandler.java
@@ -1,5 +1,40 @@
 package nts.uk.ctx.at.record.app.command.divergence.time.setting;
 
-public class DivergenceReasonSelectSaveCommandHandler {
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import nts.arc.layer.app.command.CommandHandler;
+import nts.arc.layer.app.command.CommandHandlerContext;
+import nts.uk.ctx.at.record.dom.divergence.time.setting.DivergenceReasonSelect;
+import nts.uk.ctx.at.record.dom.divergence.time.setting.DivergenceReasonSelectRepository;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class DivergenceReasonSelectSaveCommandHandler.
+ */
+@Stateless
+public class DivergenceReasonSelectSaveCommandHandler extends CommandHandler<DivergenceReasonSelectSaveCommand> {
+
+	/** The div reason select repo. */
+	@Inject
+	DivergenceReasonSelectRepository divReasonSelectRepo;
+
+	/* (non-Javadoc)
+	 * @see nts.arc.layer.app.command.CommandHandler#handle(nts.arc.layer.app.command.CommandHandlerContext)
+	 */
+	@Override
+	protected void handle(CommandHandlerContext<DivergenceReasonSelectSaveCommand> context) {
+		// get command
+
+		DivergenceReasonSelectSaveCommand command = context.getCommand();
+
+		// convert to domain
+		DivergenceReasonSelect domain = new DivergenceReasonSelect(command);
+
+		// Save
+
+		this.divReasonSelectRepo.update(domain);
+
+	}
 
 }
