beb0b276ed5a771f0a5723575fa7973bbb23280c
sonnd <sonnd@newwave.vn>
2018-03-21 14:03:20 +0700

    add code webService company KMK004

---
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/kmk004/company/command/Kmk004ComDeleteCommandHandler.java
@@ -0,0 +1,40 @@
+/******************************************************************
+ * Copyright (c) 2017 Nittsu System to present.                   *
+ * All right reserved.                                            *
+ *****************************************************************/
+package nts.uk.screen.at.app.kmk004.company.command;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+import nts.arc.layer.app.command.CommandHandler;
+import nts.arc.layer.app.command.CommandHandlerContext;
+import nts.uk.ctx.at.shared.app.command.statutory.worktime.companyNew.DeleteComStatWorkTimeSetCommandHandler;
+
+/**
+ * The Class Kmk004ComDeleteCommandHandler.
+ */
+@Stateless
+@Transactional
+public class Kmk004ComDeleteCommandHandler extends CommandHandler<Kmk004ComDeleteCommand> {
+
+	/** The save com stat work time set command handler. */
+	@Inject
+	private DeleteComStatWorkTimeSetCommandHandler delCommand;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * nts.arc.layer.app.command.CommandHandler#handle(nts.arc.layer.app.command
+	 * .CommandHandlerContext)
+	 */
+	@Override
+	protected void handle(CommandHandlerContext<Kmk004ComDeleteCommand> context) {
+		Kmk004ComDeleteCommand deleteCommand = context.getCommand();
+		this.delCommand.handle(deleteCommand.getDeleteCommand());
+
+	}
+
+}
