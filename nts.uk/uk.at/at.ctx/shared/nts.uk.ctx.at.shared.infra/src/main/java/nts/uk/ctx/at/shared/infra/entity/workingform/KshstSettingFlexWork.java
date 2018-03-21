b14e9b0f4916a54c6247c2b1971cc6ec894dfdf0
nws-anhnm <anhnm@newwave.vn>
2018-03-21 15:47:09 +0700

    [KMK003] add domain & infra SettingFlexWork

---
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/entity/workingform/KshstSettingFlexWork.java
@@ -0,0 +1,52 @@
+/******************************************************************
+ * Copyright (c) 2017 Nittsu System to present.                   *
+ * All right reserved.                                            *
+ *****************************************************************/
+package nts.uk.ctx.at.shared.infra.entity.workingform;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+import nts.uk.shr.infra.data.entity.UkJpaEntity;
+
+/**
+ * The Class KshstSettingWorkFlex.
+ */
+@Getter
+@Setter
+@Entity
+@Table(name = "KSHST_SETTING_FLEX_WORK")
+public class KshstSettingFlexWork extends UkJpaEntity implements Serializable {
+
+	/** The Constant serialVersionUID. */
+	private static final long serialVersionUID = 1L;
+
+	/** The cid. */
+	@Id
+	private String cid;
+
+	/** The exclus ver. */
+	@Column(name = "EXCLUS_VER")
+	private int exclusVer;
+
+	/** The flex work managing. */
+	@Column(name = "FLEX_WORK_MANAGING")
+	private int flexWorkManaging;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see nts.arc.layer.infra.data.entity.JpaEntity#getKey()
+	 */
+	@Override
+	protected Object getKey() {
+		return this.cid;
+	}
+
+}
