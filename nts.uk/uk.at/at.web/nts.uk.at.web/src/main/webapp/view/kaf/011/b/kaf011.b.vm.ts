270e29f91b06a14b92be3574d03a5fcf8c485b32
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2018-03-21 10:28:23 +0700

    add KAF 011 B

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/011/b/kaf011.b.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/011/b/kaf011.b.vm.ts
@@ -3,7 +3,7 @@ module nts.uk.at.view.kaf011.b.viewmodel {
     import dialog = nts.uk.ui.dialog;
     import text = nts.uk.resource.getText;
     export class ViewModel extends kaf000.b.viewmodel.ScreenModel {
-
+        screenModeNew: KnockoutObservable<boolean> = ko.observable(false);
         update() {
 
         }
