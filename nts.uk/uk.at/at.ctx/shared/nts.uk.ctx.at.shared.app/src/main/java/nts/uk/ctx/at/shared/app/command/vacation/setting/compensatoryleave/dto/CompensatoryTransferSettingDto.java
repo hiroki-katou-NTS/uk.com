732c2d1a36d2ec2b7e3af7a77f71e1d18f61a2fe
NSVN-NguyenVanVu <vu_nv@nittsusystem-vn.com>
2018-03-23 10:33:15 +0700

    fix theo a Thành nws bảo để deploy cái

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.app/src/main/java/nts/uk/ctx/at/shared/app/command/vacation/setting/compensatoryleave/dto/CompensatoryTransferSettingDto.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.app/src/main/java/nts/uk/ctx/at/shared/app/command/vacation/setting/compensatoryleave/dto/CompensatoryTransferSettingDto.java
@@ -39,7 +39,8 @@ public class CompensatoryTransferSettingDto {
 	 * @return the transfer setting
 	 */
 	public SubHolTransferSet toDomain() {
-		return new SubHolTransferSet(new TransferSettingGetMementoImpl(this));
+//		return new SubHolTransferSet(new TransferSettingGetMementoImpl(this));
+		return new SubHolTransferSet(null);
 	}
 
 	/**
