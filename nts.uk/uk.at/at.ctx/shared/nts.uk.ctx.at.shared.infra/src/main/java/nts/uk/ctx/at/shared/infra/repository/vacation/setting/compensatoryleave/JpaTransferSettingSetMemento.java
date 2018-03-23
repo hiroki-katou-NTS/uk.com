b2f92c404d4debbd7af3a8eecf96dc31bffd95d5
NWS-ThanhNC <thanhnc@newwave.vn>
2018-03-23 10:30:32 +0700

    Replace TransferSetting by SubHolTransferSet.

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/repository/vacation/setting/compensatoryleave/JpaTransferSettingSetMemento.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/repository/vacation/setting/compensatoryleave/JpaTransferSettingSetMemento.java
@@ -4,122 +4,80 @@
  *****************************************************************/
 package nts.uk.ctx.at.shared.infra.repository.vacation.setting.compensatoryleave;
 
-import nts.uk.ctx.at.shared.dom.vacation.setting.compensatoryleave.OneDayTime;
-import nts.uk.ctx.at.shared.dom.vacation.setting.compensatoryleave.TransferSettingDivision;
-import nts.uk.ctx.at.shared.dom.vacation.setting.compensatoryleave.TransferSettingSetMemento;
+import nts.uk.ctx.at.shared.dom.worktime.common.DesignatedTime;
+import nts.uk.ctx.at.shared.dom.worktime.common.OneDayTime;
+import nts.uk.ctx.at.shared.dom.worktime.common.SubHolTransferSetAtr;
+import nts.uk.ctx.at.shared.dom.worktime.common.SubHolTransferSetSetMemento;
 import nts.uk.ctx.at.shared.infra.entity.vacation.setting.compensatoryleave.KocmtOccurrenceSet;
 
 /**
  * The Class JpaTransferSettingSetMemento.
  */
-public class JpaTransferSettingSetMemento implements TransferSettingSetMemento {
-    
-    /** The entity. */
-    private KocmtOccurrenceSet entity;
-    
-    /** The value true. */
-    private static int VALUE_TRUE = 1;
-    
-    /** The value false. */
-    private static int VALUE_FALSE = 0;
-    
-    /**
-     * Instantiates a new jpa transfer setting set memento.
-     *
-     * @param entity the entity
-     */
-    public JpaTransferSettingSetMemento(KocmtOccurrenceSet entity) {
-        this.entity = entity;
-    }
-    
-    /**
-     * Sets the certain time.
-     *
-     * @param certainTime the new certain time
-     */
-    /*
-     * (non-Javadoc)
-     * 
-     * @see nts.uk.ctx.at.shared.dom.vacation.setting.compensatoryleave.
-     * TransferSettingSetMemento#setCertainTime(nts.uk.ctx.at.shared.dom.
-     * vacation.setting.compensatoryleave.OneDayTime)
-     */
-    @Override
-    public void setCertainTime(OneDayTime certainTime) {
-        if (certainTime != null) {
-            this.entity.setCertainTime(certainTime.v().longValue());
-        }
-    }
+public class JpaTransferSettingSetMemento implements SubHolTransferSetSetMemento {
 
-    /**
-     * Sets the use division.
-     *
-     * @param useDivision the new use division
-     */
-    /*
-     * (non-Javadoc)
-     * 
-     * @see nts.uk.ctx.at.shared.dom.vacation.setting.compensatoryleave.
-     * TransferSettingSetMemento#setUseDivision(boolean)
-     */
-    @Override
-    public void setUseDivision(boolean useDivision) {
-        this.entity.setUseType(useDivision == true ? VALUE_TRUE : VALUE_FALSE);
-    }
+	/** The entity. */
+	private KocmtOccurrenceSet entity;
 
-    /**
-     * Sets the one day time.
-     *
-     * @param oneDayTime the new one day time
-     */
-    /*
-     * (non-Javadoc)
-     * 
-     * @see nts.uk.ctx.at.shared.dom.vacation.setting.compensatoryleave.
-     * TransferSettingSetMemento#setOneDayTime(nts.uk.ctx.at.shared.dom.vacation
-     * .setting.compensatoryleave.OneDayTime)
-     */
-    @Override
-    public void setOneDayTime(OneDayTime oneDayTime) {
-        if (oneDayTime != null) {
-            this.entity.setOneDayTime(oneDayTime.v().longValue());
-        }
-    }
+	/** The value true. */
+	private static int VALUE_TRUE = 1;
 
-    /**
-     * Sets the half day time.
-     *
-     * @param halfDayTime the new half day time
-     */
-    /*
-     * (non-Javadoc)
-     * 
-     * @see nts.uk.ctx.at.shared.dom.vacation.setting.compensatoryleave.
-     * TransferSettingSetMemento#setHalfDayTime(nts.uk.ctx.at.shared.dom.
-     * vacation.setting.compensatoryleave.OneDayTime)
-     */
-    @Override
-    public void setHalfDayTime(OneDayTime halfDayTime) {
-        if (halfDayTime != null) {
-            this.entity.setHalfDayTime(halfDayTime.v().longValue());
-        }
-    }
+	/** The value false. */
+	private static int VALUE_FALSE = 0;
 
-    /**
-     * Sets the transfer division.
-     *
-     * @param transferDivision the new transfer division
-     */
-    /*
-     * (non-Javadoc)
-     * 
-     * @see nts.uk.ctx.at.shared.dom.vacation.setting.compensatoryleave.
-     * TransferSettingSetMemento#setTransferDivision(nts.uk.ctx.at.shared.dom.
-     * vacation.setting.compensatoryleave.TransferSettingDivision)
-     */
-    @Override
-    public void setTransferDivision(TransferSettingDivision transferDivision) {
-        this.entity.setTransfType(transferDivision.value);
-    }
+	/**
+	 * Instantiates a new jpa transfer setting set memento.
+	 *
+	 * @param entity
+	 *            the entity
+	 */
+	public JpaTransferSettingSetMemento(KocmtOccurrenceSet entity) {
+		this.entity = entity;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see nts.uk.ctx.at.shared.dom.vacation.setting.compensatoryleave.
+	 * TransferSettingSetMemento#setCertainTime(nts.uk.ctx.at.shared.dom.
+	 * vacation.setting.compensatoryleave.OneDayTime)
+	 */
+	@Override
+	public void setCertainTime(OneDayTime certainTime) {
+		if (certainTime != null) {
+			this.entity.setCertainTime(certainTime.v().longValue());
+		}
+	}
+
+	/**
+	 * Sets the use division.
+	 *
+	 * @param useDivision
+	 *            the new use division
+	 */
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see nts.uk.ctx.at.shared.dom.vacation.setting.compensatoryleave.
+	 * TransferSettingSetMemento#setUseDivision(boolean)
+	 */
+	@Override
+	public void setUseDivision(boolean useDivision) {
+		this.entity.setUseType(useDivision == true ? VALUE_TRUE : VALUE_FALSE);
+	}
+
+	@Override
+	public void setDesignatedTime(DesignatedTime time) {
+		if (time.getOneDayTime() != null) {
+			this.entity.setOneDayTime(time.getOneDayTime().v().longValue());
+		}
+		if (time.getHalfDayTime() != null) {
+			this.entity.setHalfDayTime(time.getHalfDayTime().v().longValue());
+		}
+	}
+
+	@Override
+	public void setSubHolTransferSetAtr(SubHolTransferSetAtr atr) {
+		this.entity.setTransfType(atr.value);
+	}
 
 }
