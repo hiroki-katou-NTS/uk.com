cdfaea442ebbd30a5d3079e4d0c2ee2a5a07b209
NSVN-PhamVanDan <dan_pv@nittsusystem-vn.com>
2018-03-23 17:02:00 +0700

    get dataType with itemInfo to check visible

--- nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.app/src/main/java/nts/uk/ctx/pereg/app/find/roles/auth/item/PersonInfoItemDetailDto.java
+++ nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.app/src/main/java/nts/uk/ctx/pereg/app/find/roles/auth/item/PersonInfoItemDetailDto.java
@@ -3,8 +3,6 @@ package nts.uk.ctx.pereg.app.find.roles.auth.item;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import nts.arc.enums.EnumAdaptor;
-import nts.uk.ctx.pereg.dom.person.info.singleitem.DataTypeValue;
 import nts.uk.ctx.pereg.dom.roles.auth.item.PersonInfoItemDetail;
 
 @Data
@@ -33,12 +31,12 @@ public class PersonInfoItemDetailDto {
 
 	private boolean setting;
 	
-	private DataTypeValue dataType;
+	private int dataType;
 
 	public static PersonInfoItemDetailDto createDto(PersonInfoItemDetail itemDetail) {
 		return new PersonInfoItemDetailDto(itemDetail.getRoleId(), itemDetail.getPersonInfoCategoryAuthId(),
 				itemDetail.getPersonItemDefId(), itemDetail.getSelfAuthType(), itemDetail.getOtherPersonAuth(), itemDetail.getItemCd(),
 				itemDetail.getItemName(), itemDetail.getItemParentCd(), itemDetail.getAbolitionAtr(), itemDetail.getRequiredAtr(),
-				itemDetail.isSetting(), EnumAdaptor.valueOf(itemDetail.getDataType(), DataTypeValue.class));
+				itemDetail.isSetting(), itemDetail.getDataType());
 	}
 }
