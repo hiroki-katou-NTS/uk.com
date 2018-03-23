1a209d66cb74bf0eeaeccb90e4c5069cd09bffd8
NSVN-TruongVanLai <NSVN-TruongVanLai@users.noreply.github.com>
2018-03-23 16:27:56 +0700

    add @Stateless

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/remainingnumber/specialleavegrant/DeleteSpecialLeaCommandHandler.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/remainingnumber/specialleavegrant/DeleteSpecialLeaCommandHandler.java
@@ -1,11 +1,13 @@
 package nts.uk.ctx.at.record.app.command.remainingnumber.specialleavegrant;
 
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import nts.arc.layer.app.command.AsyncCommandHandler;
 import nts.arc.layer.app.command.CommandHandlerContext;
 import nts.uk.ctx.at.record.dom.remainingnumber.specialleave.empinfo.grantremainingdata.SpecialLeaveGrantRepository;
 
+@Stateless
 public class DeleteSpecialLeaCommandHandler extends AsyncCommandHandler<DeleteSpecialLeaCommand>{
 	
 	@Inject
