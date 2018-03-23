eebd5b98813b012bfaeea0635b1e99f0e3fb1c80
HoangDD <dangduyhoang3009@gmail.com>
2018-03-23 17:39:19 +0700

    [KMK013] Init screen K

---
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/013/k/kmk013.k.vm.ts
@@ -0,0 +1,26 @@
+module nts.uk.at.view.kmk013.k {
+    export module viewmodel {
+        export class ScreenModel {
+            startPage(): JQueryPromise<any> {
+                var self = this;
+                var dfd = $.Deferred();
+                dfd.resolve();
+                return dfd.promise();
+            }
+            
+            otSetBtn(): void {
+                nts.uk.request.jump("/view/kmk/010/a/index.xhtml");
+            }
+            
+            outOfOderSetBtn(): void {
+                nts.uk.request.jump("/view/kmk/013/q/index.xhtml");
+            }
+            
+            regLegalPrescribeTimeBtn(): void {
+                nts.uk.request.jump("/view/kmk/004/a/index.xhtml");
+            }
+            
+            
+        }
+    }
+}
