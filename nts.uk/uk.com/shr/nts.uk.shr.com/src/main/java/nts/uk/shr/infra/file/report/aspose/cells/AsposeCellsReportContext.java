93439ef1ce0ab1d481dbc5933e72e7b9798a41b9
m_kitahira <m_kitahira@nittsusystem.co.jp>
2018-03-26 13:19:17 +0900

    fix compile error

--- nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/infra/file/report/aspose/cells/AsposeCellsReportContext.java
+++ nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/infra/file/report/aspose/cells/AsposeCellsReportContext.java
@@ -14,6 +14,7 @@ import com.aspose.cells.PageSetup;
 import com.aspose.cells.SaveFormat;
 import com.aspose.cells.SaveOptions;
 import com.aspose.cells.TxtSaveOptions;
+import com.aspose.cells.TxtValueQuoteType;
 import com.aspose.cells.Workbook;
 import com.aspose.cells.WorkbookDesigner;
 import com.aspose.cells.WorksheetCollection;
