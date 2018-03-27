fb4ff93c3efcf0fad964ef1a8c4f931e4ed18ff1
3SI\hiep.ld <hiep.ld@3si.vn>
2018-03-27 17:25:36 +0700

    Revert "kdl 030, 034"
    This reverts commit f1458ae58858d2552596c1de68b94b37f60953f2.

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdl/034/a/kdl034.a.service.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdl/034/a/kdl034.a.service.ts
@@ -1,15 +1,11 @@
 module nts.uk.at.view.kdl034.a {
     export module service {
         var paths: any = {
-            remand: "at/request/application/remandapp",
-            getAppInfoByAppID: "at/request/application/getAppInfoByAppIdForRemand"
+            remand: "at/request/application/remandapp"
         }
         
         export function remand(command): JQueryPromise<any> {
             return nts.uk.request.ajax(paths.remand, command);
-        }
-        export function getAppInfoByAppID(appId): JQueryPromise<any> {
-            return nts.uk.request.ajax("at",paths.getAppInfoByAppID, appId);
         }
     }
 }
