fef0e2ac3527f446511c6c960adb8d8e740af121
HoangDD <dangduyhoang3009@gmail.com>
2018-03-28 10:56:24 +0700

    [KMK013] implement code screen Q

---
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/entity/workrecord/monthlyresults/roleopenperiod/KrcstRoleOfOpenPeriod_.java
@@ -0,0 +1,15 @@
+package nts.uk.ctx.at.shared.infra.entity.workrecord.monthlyresults.roleopenperiod;
+
+import java.math.BigDecimal;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-26T11:28:10")
+@StaticMetamodel(KrcstRoleOfOpenPeriod.class)
+public class KrcstRoleOfOpenPeriod_ { 
+	public static volatile SingularAttribute<KrcstRoleOfOpenPeriod, KrcstRoleOfOpenPeriodPK> krcstRoleOfOpenPeriodPK;
+    public static volatile SingularAttribute<KrcstRoleOfOpenPeriod, BigDecimal> roleOfOpenPeriod;
+
+}
