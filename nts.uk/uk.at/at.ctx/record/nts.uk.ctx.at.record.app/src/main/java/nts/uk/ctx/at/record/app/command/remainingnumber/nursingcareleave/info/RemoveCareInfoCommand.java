bea78ddb0c5c9bc2469a6a7c00a152bcbbf64859
NSVN-PhamXuanVinh <vinh_px@nittsusystem-vn.com>
2018-03-28 16:58:40 +0700

    update web service

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/remainingnumber/nursingcareleave/info/RemoveCareInfoCommand.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/remainingnumber/nursingcareleave/info/RemoveCareInfoCommand.java
@@ -3,7 +3,7 @@ package nts.uk.ctx.at.record.app.command.remainingnumber.nursingcareleave.info;
 import lombok.Value;
 
 @Value
-public class RemoveChildCareInfoCommand {
+public class RemoveCareInfoCommand {
 	
 	//社員ID
 	private String sId;
--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/remainingnumber/nursingcareleave/info/RemoveChildCareInfoCommandHandler.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/remainingnumber/nursingcareleave/info/RemoveChildCareInfoCommandHandler.java
@@ -6,22 +6,21 @@ import javax.inject.Inject;
 import nts.arc.enums.EnumAdaptor;
 import nts.arc.layer.app.command.AsyncCommandHandler;
 import nts.arc.layer.app.command.CommandHandlerContext;
-import nts.uk.ctx.at.record.app.command.remainingnumber.nursingcareleave.data.RemoveCareDataCommand;
 import nts.uk.ctx.at.record.dom.remainingnumber.nursingcareleavemanagement.basic.LeaveType;
-import nts.uk.ctx.at.record.dom.remainingnumber.nursingcareleavemanagement.data.NursCareLevRemainDataRepository;
-import nts.uk.ctx.at.record.dom.remainingnumber.nursingcareleavemanagement.data.NursingCareLeaveRemainingData;
+import nts.uk.ctx.at.record.dom.remainingnumber.nursingcareleavemanagement.info.NursCareLevRemainInfoRepository;
+import nts.uk.ctx.at.record.dom.remainingnumber.nursingcareleavemanagement.info.NursingCareLeaveRemainingInfo;
 import nts.uk.shr.com.context.AppContexts;
 
 @Stateless
-public class RemoveChildCareInfoCommandHandler extends AsyncCommandHandler<RemoveCareDataCommand> {
+public class RemoveChildCareInfoCommandHandler extends AsyncCommandHandler<RemoveCareInfoCommand> {
 
 	@Inject
-	private NursCareLevRemainDataRepository repo;
+	private NursCareLevRemainInfoRepository repo;
 
 	@Override
-	protected void handle(CommandHandlerContext<RemoveCareDataCommand> context) {
-		RemoveCareDataCommand data = context.getCommand();
-		NursingCareLeaveRemainingData removeData = new NursingCareLeaveRemainingData();
+	protected void handle(CommandHandlerContext<RemoveCareInfoCommand> context) {
+		RemoveCareInfoCommand data = context.getCommand();
+		NursingCareLeaveRemainingInfo removeData = new NursingCareLeaveRemainingInfo();
 		removeData.setLeaveType(EnumAdaptor.valueOf(2, LeaveType.class));
 		removeData.setSId(data.getSId());
 		repo.remove(removeData, AppContexts.user().companyId());
