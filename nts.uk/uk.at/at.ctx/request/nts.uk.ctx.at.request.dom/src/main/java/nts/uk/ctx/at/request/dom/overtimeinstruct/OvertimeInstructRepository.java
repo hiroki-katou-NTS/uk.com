01ce5aaa8c76beff202c2df2ba0f3fb6983b6ab5
NSVC\tanlv <tan_lv@nittsusystem-vn.com>
2018-03-28 15:48:59 +0700

    Request list 230

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/overtimeinstruct/OvertimeInstructRepository.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/overtimeinstruct/OvertimeInstructRepository.java
@@ -1,7 +1,17 @@
 package nts.uk.ctx.at.request.dom.overtimeinstruct;
 
+import java.util.List;
+
 import nts.arc.time.GeneralDate;
 
 public interface OvertimeInstructRepository {
 	public OverTimeInstruct getOvertimeInstruct(GeneralDate instructDate,String targetPerson);
+	
+	/**
+	 * For request list 230
+	 * @param sId
+	 * @param instructDate
+	 * @return
+	 */
+	public List<OverTimeInstruct> getAllOverTimeInstructBySId(String sId);
 }
