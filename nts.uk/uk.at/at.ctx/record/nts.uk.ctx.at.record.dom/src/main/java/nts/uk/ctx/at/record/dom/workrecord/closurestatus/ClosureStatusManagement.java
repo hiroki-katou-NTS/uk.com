3f5954879cefc5bc1e059c818f5359e5745e1076
Hung Tran <32724898+1capedbaldy@users.noreply.github.com>
2018-03-28 15:27:24 +0700

    kmw006: create domain & entity ClosureStatusManagement

---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/workrecord/closurestatus/ClosureStatusManagement.java
@@ -0,0 +1,48 @@
+package nts.uk.ctx.at.record.dom.workrecord.closurestatus;
+
+import lombok.Getter;
+import nts.arc.enums.EnumAdaptor;
+import nts.arc.layer.dom.AggregateRoot;
+import nts.arc.time.YearMonth;
+import nts.uk.ctx.at.shared.dom.workrule.closure.ClosureDate;
+import nts.uk.ctx.at.shared.dom.workrule.closure.ClosureId;
+import nts.uk.shr.com.time.calendar.period.DatePeriod;
+
+/**
+ * 
+ * @author HungTT - 締め状態管理
+ *
+ */
+
+@Getter
+public class ClosureStatusManagement extends AggregateRoot {
+
+	// 年月
+	private YearMonth yearMonth;
+
+	// 社員ID
+	private String employeeId;
+
+	// 締めID
+	private ClosureId closureId;
+
+	// 締め日
+	private ClosureDate closureDate;
+
+	// 期間
+	private DatePeriod period;
+
+	private ClosureProcessStatus status;
+
+	public ClosureStatusManagement(YearMonth yearMonth, String employeeId, int closureId, ClosureDate closureDate,
+			DatePeriod period, int status) {
+		super();
+		this.yearMonth = yearMonth;
+		this.employeeId = employeeId;
+		this.closureId = EnumAdaptor.valueOf(closureId, ClosureId.class);
+		this.closureDate = closureDate;
+		this.period = period;
+		this.status = EnumAdaptor.valueOf(status, ClosureProcessStatus.class);
+	}
+
+}
