0c9e9ee162d21a06a53b3d5c8f5a533516bc81bd
Vu Thang Loi <loi_vt@nittsusystem-vn.com>
2018-03-28 11:22:17 +0700

    update code overTime

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/overtime/OvertimeRepository.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/overtime/OvertimeRepository.java
@@ -25,4 +25,12 @@ public interface OvertimeRepository {
 	public void delete(String companyID, String appID);
 	
 	public Optional<AppOverTime> getAppOvertimeByDate(GeneralDate appDate, String employeeID, OverTimeAtr overTimeAtr);
+	/**
+	 * get Application Over Time and Frame
+	 * @author hoatt - new
+	 * @param companyID
+	 * @param appID
+	 * @return
+	 */
+	public Optional<AppOverTime> getAppOvertimeFrame(String companyID, String appID);
 }
