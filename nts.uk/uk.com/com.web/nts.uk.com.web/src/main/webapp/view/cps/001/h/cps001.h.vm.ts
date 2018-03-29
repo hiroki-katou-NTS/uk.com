6db3aecbce43b203573126d38ea68c8c9a19cbde
NSVN-PhamXuanVinh <vinh_px@nittsusystem-vn.com>
2018-03-29 16:39:39 +0700

    update cps 001 h generate deadline

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/001/h/cps001.h.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/001/h/cps001.h.vm.ts
@@ -17,114 +17,126 @@ module cps001.h.vm {
         columns: KnockoutObservableArray<NtsGridListColumn>;
         items: KnockoutObservableArray<IResvLeaGrantRemNum> = ko.observableArray([]);
         currentItem: KnockoutObservable<string> = ko.observable("");
-        leaveExpirationStatus:  KnockoutObservableArray<any>;
+        leaveExpirationStatus: KnockoutObservableArray<any>;
         resvLeaGrantRemNum: KnockoutObservable<ResvLeaGrantRemNum> = ko.observable(new ResvLeaGrantRemNum(<IResvLeaGrantRemNum>{}));
         enableRemoveBtn: KnockoutObservable<boolean> = ko.observable(true);
         isCreate: KnockoutObservable<boolean> = ko.observable(false);
         ckbAll: KnockoutObservable<boolean> = ko.observable(true);
-        
+        itemDefs: any = [];
+
         constructor() {
             let self = this;
-            self.ckbAll.subscribe((data) => {
-                service.getAll(data).done((data) => {
-                 if(data && data.length > 0){
-                    self.items(data);   
-                   self.currentItem(self.items()[0].id);
-                 }else{
-                     self.create();
-                 }
-            });  
-            });
-            self.currentItem.subscribe((id: string)=>{                 
-                    service.getByGrantDate(id).done((curItem) => {
-                        self.resvLeaGrantRemNum(new ResvLeaGrantRemNum(<IResvLeaGrantRemNum>curItem));
-                        if(curItem){
-                            self.enableRemoveBtn(true);
-                            self.isCreate(false);
-                            }
-                    });                 
+            self.ckbAll.subscribe((data) => {             
+                service.getAll(__viewContext.user.employeeId, data).done((data) => {
+                    if (data && data.length > 0) {
+                        self.items(data);
+                        self.currentItem(self.items()[0].id);
+                    } else {
+                        self.create();
+                    }
                 });
+            });
+            self.currentItem.subscribe((id: string) => {
+                  
+                service.getByGrantDate(id).done((curItem) => {
+                    self.resvLeaGrantRemNum(new ResvLeaGrantRemNum(<IResvLeaGrantRemNum>curItem));
+                    self.setDef();
+                    $("#grantDate").focus();
+                    if (curItem) {
+                        self.enableRemoveBtn(true);
+                        self.isCreate(false);
+                    }
+                });
+            });
             self.columns = ko.observableArray([
-               { headerText: "", key: 'id', hidden: true},
-               { headerText: text("CPS001_118"), key: 'grantDate', width: 100, isDateColumn: true, format: 'YYYY/MM/DD'},
-               { headerText: text("CPS001_119"), key: 'deadline', width: 100, isDateColumn: true, format: 'YYYY/MM/DD'}, 
-               { headerText: text("CPS001_120"), key: 'expirationStatus', width: 70}, 
-               { headerText: text("CPS001_121"), key: 'grantDays', width: 70},
-               { headerText: text("CPS001_130"), key: 'useDays', width: 70},
-               { headerText: text("CPS001_123"), key: 'overLimitDays', width: 70},
-               { headerText: text("CPS001_129"), key: 'remainingDays', width: 70}
-           ]); 
+                { headerText: "", key: 'id', hidden: true },
+                { headerText: text("CPS001_118"), key: 'grantDate', width: 100, isDateColumn: true, format: 'YYYY/MM/DD' },
+                { headerText: text("CPS001_119"), key: 'deadline', width: 100, isDateColumn: true, format: 'YYYY/MM/DD' },
+                { headerText: text("CPS001_120"), key: 'expirationStatus', width: 70 },
+                { headerText: text("CPS001_121"), key: 'grantDays', width: 70 },
+                { headerText: text("CPS001_130"), key: 'useDays', width: 70 },
+                { headerText: text("CPS001_123"), key: 'overLimitDays', width: 70 },
+                { headerText: text("CPS001_129"), key: 'remainingDays', width: 70 }
+            ]);
         }
-        load() :JQueryPromise<any>{
-            let self = this, dfd = $.Deferred();
-             service.getAll(self.ckbAll()).done((data) => {
-                 if(data && data.length > 0){
-                    self.items(data);       
-                 }else{
-                     self.create();
-                 }
-                 dfd.resolve();
-            });  
+        load(): JQueryPromise<any> {
+            let self = this, dfd = $.Deferred(),
+            sId = __viewContext.user.employeeId;
+            service.getAll(sId, self.ckbAll()).done((data) => {
+                if (data && data.length > 0) {
+                    self.items(data);
+                } else {
+                    self.create();
+                }
+                dfd.resolve();
+            });
             return dfd.promise();
         }
         start() {
-           let self  = this;
-           self.setDef();
-           self.load().done(()=>{
-               if(self.items().length > 0){
-                   self.currentItem(self.items()[0].id);
-               }
-           });
-        }
-        setDef(){
             let self = this;
-            service.getItemDef().done((data) => {
-                $("td[data-itemCode]").each(function(){ 
+            self.setDef();
+            self.load().done(() => {
+                if (self.items().length > 0) {
+                    self.currentItem(self.items()[0].id);
+                }
+            });
+        }
+        setDef() {
+            let self = this;
+            if(self.itemDefs.length > 0){
+                self.setItemValue(self.itemDefs);
+            }else{
+                service.getItemDef().done((data) => {
+                    self.itemDefs = data ;
+                    self.setItemValue(self.itemDefs);
+                });
+            }
+            
+        }
+        setItemValue(data: any){
+             $("td[data-itemCode]").each(function() {
                     let itemCodes = $(this).attr('data-itemCode');
-                    if(itemCodes){
+                    if (itemCodes) {
                         let itemCodeArray = itemCodes.split(" ");
                         _.forEach(itemCodeArray, (itemCode) => {
-                            let itemDef = _.find(data, (item)=>{
+                            let itemDef = _.find(data, (item) => {
                                 return item.itemCode == itemCode;
                             });
-                            if(itemDef){
-                                if(itemDef.display){
+                            if (itemDef) {
+                                if (itemDef.display) {
                                     $(this).children().first().html("<label>" + itemDef.itemName + "</label>");
                                 }
                             }
                         });
                     }
                 });
-                 
-                
-            });
-            }
-        
-        close(){
+        }
+
+        close() {
             close();
         }
-        
-        remove(){
+
+        remove() {
             let self = this;
             nts.uk.ui.dialog.confirm({ messageId: "Msg_18" }).ifYes(() => {
                 block();
-                let delItemIndex = _.findIndex(self.items(), (item)=>{
+                let delItemIndex = _.findIndex(self.items(), (item) => {
                     return item.id == self.resvLeaGrantRemNum().id();
                 });
                 let selectedId;
-                if(delItemIndex == self.items().length-1){
-                    if(self.items().length > 1){
-                        selectedId = self.items()[delItemIndex-1].id;
+                if (delItemIndex == self.items().length - 1) {
+                    if (self.items().length > 1) {
+                        selectedId = self.items()[delItemIndex - 1].id;
                     }
-                }else{
-                    selectedId = self.items()[delItemIndex+1].id;
+                } else {
+                    selectedId = self.items()[delItemIndex + 1].id;
                 }
                 service.remove(self.resvLeaGrantRemNum().id()).done(() => {
-                    self.load().done(()=>{
-                        if(self.items().length == 0){
-                             self.create();
-                        }else{
-                                self.currentItem(selectedId);
+                    self.load().done(() => {
+                        if (self.items().length == 0) {
+                            self.create();
+                        } else {
+                            self.currentItem(selectedId);
                         }
                     });
                     alert({ messageId: "Msg_16" });
@@ -134,79 +146,96 @@ module cps001.h.vm {
                 });
             });
         }
-        
-        register(){
+
+        register() {
             let self = this;
-            
+
             $("#grantDate").trigger("validate");
             $("#deadline").trigger("validate");
             $("#grantDays").trigger("validate");
             $("#useDays").trigger("validate");
             $("#overLimitDays").trigger("validate");
             $("#remainingDays").trigger("validate");
-            
+
             if (!$(".nts-input").ntsError("hasError")) {
-                let item = self.resvLeaGrantRemNum(), 
-                grantDate = moment.utc(item.grantDate(),"YYYY/MM/DD"),
-                deadline = moment.utc(item.deadline(),"YYYY/MM/DD");
-                if(grantDate > deadline){
+                let item = self.resvLeaGrantRemNum(),
+                    grantDate = moment.utc(item.grantDate(), "YYYY/MM/DD"),
+                    deadline = moment.utc(item.deadline(), "YYYY/MM/DD");
+                if ((new Date(deadline._d)) < (new Date(grantDate._d))) {
                     error({ messageId: "Msg_1023", messageParams: [] });
                     return;
                 }
-                if(self.isCreate()){
+                if (self.isCreate()) {
+                    let currItem = self.items();
                     service.create(grantDate, deadline, item.expirationStatus(),
-                    item.grantDays(), item.useDays(), item.overLimitDays(), item.remainingDays()).done(() => {
-                        self.load();
-                        alert({ messageId: "Msg_15" });
-                        unblock();
-                    }).fail((mes) => {
-                        unblock();
-                    });
-                }else{
-                   service.update(item.id(), grantDate, deadline, item.expirationStatus(),
-                    item.grantDays(), item.useDays(), item.overLimitDays(), item.remainingDays()).done(() => {
-                        self.load();
-                        alert({ messageId: "Msg_15" });
-                        unblock();
-                    }).fail((mes) => {
-                        unblock();
-                    }); 
+                        item.grantDays(), item.useDays(), item.overLimitDays(), item.remainingDays()).done(() => {
+                            self.load().done(() => {
+                                if (self.items().length > 0) {
+                                    self.currentItem(_.difference(self.items(), currItem)[0].id);
+                                }
+                                alert({ messageId: "Msg_15" });
+                                unblock();
+                            });
+                           
+                        }).fail((mes) => {
+                            unblock();
+                        });
+                } else {
+                    service.update(item.id(), grantDate, deadline, item.expirationStatus(),
+                        item.grantDays(), item.useDays(), item.overLimitDays(), item.remainingDays()).done(() => {
+                            self.load().done(() => {
+                                if (self.items().length > 0) {
+                                    self.currentItem(item.id());
+                                }
+                                alert({ messageId: "Msg_15" });
+                                unblock();
+                            });
+                           
+                        }).fail((mes) => {
+                            unblock();
+                        });
                 }
             }
-           
+
         }
-        
-        create(){
+
+        create() {
             let self = this;
             self.currentItem("-1");
             self.enableRemoveBtn(false);
             self.isCreate(true);
-            self.resvLeaGrantRemNum(new ResvLeaGrantRemNum(<IResvLeaGrantRemNum>{}));
         }
 
     }
-    class ResvLeaGrantRemNum{
-        id:  KnockoutObservable<string> = ko.observable("");
-        grantDate:  KnockoutObservable<string> = ko.observable("");
-        deadline:  KnockoutObservable<string> = ko.observable("");
-        expirationStatus:  KnockoutObservable<number> = ko.observable(1);
-        grantDays:  KnockoutObservable<string> = ko.observable("");
-        useDays:  KnockoutObservable<string> = ko.observable("");
-        overLimitDays:  KnockoutObservable<string> = ko.observable("");
-        remainingDays:  KnockoutObservable<string> = ko.observable("");       
-        constructor(data: IResvLeaGrantRemNum){
-            this.id(data && data.id || "");
-            this.grantDate(data && data.grantDate || "");
-            this.deadline(data && data.deadline || "");
-            this.expirationStatus(data == undefined ? 1 : (data.expirationStatus == undefined ? 1: data.expirationStatus));
-            this.grantDays(data &&data.grantDays || "");
-            this.useDays(data &&data.useDays || "");
-            this.overLimitDays(data &&data.overLimitDays || "");
-            this.remainingDays(data &&data.remainingDays || "");
+    class ResvLeaGrantRemNum {
+        id: KnockoutObservable<string> = ko.observable("");
+        grantDate: KnockoutObservable<string> = ko.observable("");
+        deadline: KnockoutObservable<string> = ko.observable("");
+        expirationStatus: KnockoutObservable<number> = ko.observable(1);
+        grantDays: KnockoutObservable<string> = ko.observable("");
+        useDays: KnockoutObservable<string> = ko.observable("");
+        overLimitDays: KnockoutObservable<string> = ko.observable("");
+        remainingDays: KnockoutObservable<string> = ko.observable("");
+        constructor(data: IResvLeaGrantRemNum) {
+            let self = this;
+            self.id(data && data.id || "");
+            self.grantDate(data && data.grantDate || "");
+            self.deadline(data && data.deadline || "");
+            self.expirationStatus(data == undefined ? 1 : (data.expirationStatus == undefined ? 1 : data.expirationStatus));
+            self.grantDays(data && data.grantDays || "");
+            self.useDays(data && data.useDays || "");
+            self.overLimitDays(data && data.overLimitDays || "");
+            self.remainingDays(data && data.remainingDays || "");
+            
+            self.grantDate.subscribe((data)=>{
+                    service.generateDeadline(moment.utc(data, "YYYY/MM/DD")).done((item) =>{
+                        self.deadline(item);
+                    });
+            });
         }
     }
-    
-    interface IResvLeaGrantRemNum{
+
+    interface IResvLeaGrantRemNum {
         id: string;
         grantDate: string;
         deadline: string;
@@ -216,5 +245,5 @@ module cps001.h.vm {
         overLimitDays: string;
         remainingDays: string;
     }
-  
+
 }
