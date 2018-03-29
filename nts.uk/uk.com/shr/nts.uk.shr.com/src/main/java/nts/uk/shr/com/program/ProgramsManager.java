952a46dfbb7a9fd6f76c832a58d7cbfad315601f
Vu Thang Loi <loi_vt@nittsusystem-vn.com>
2018-03-29 17:27:26 +0700

    update Kaf006

--- nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/com/program/ProgramsManager.java
+++ nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/com/program/ProgramsManager.java
@@ -1494,6 +1494,9 @@ public class ProgramsManager {
 	/** KAF005A */
 	public static final Program KAF005A = new Program(WebAppId.AT, ProgramIdConsts.KAF005A, "KAF005_1",
 			"/view/kaf/005/a/index.xhtml");
+	/** KAF006A */
+	public static final Program KAF006A = new Program(WebAppId.AT, ProgramIdConsts.KAF006A, "KAF006_1",
+			"/view/kaf/005/a/index.xhtml");
 	/** KAF007A */
 	public static final Program KAF007A = new Program(WebAppId.AT, ProgramIdConsts.KAF007A, null,
 			"/view/kaf/007/a/index.xhtml");
