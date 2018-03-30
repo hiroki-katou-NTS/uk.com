3be3ed7e6485e59dd1323f7ef61bf44e6f8f69b8
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2018-03-30 15:28:28 +0700

    package edit SPR fix SPR code upver at.request, bs.employee, shr.com

--- nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.pubimp/src/main/java/nts/uk/ctx/bs/employee/pubimp/employee/employeeindesignated/EmpInDesignatedPubImp.java
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.pubimp/src/main/java/nts/uk/ctx/bs/employee/pubimp/employee/employeeindesignated/EmpInDesignatedPubImp.java
@@ -9,6 +9,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import nts.arc.time.GeneralDate;
@@ -23,6 +24,7 @@ import nts.uk.ctx.bs.employee.pub.employment.statusemployee.StatusOfEmploymentPu
 /**
  * The Class EmpInDesignatedPubImp.
  */
+@Stateless
 public class EmpInDesignatedPubImp implements EmpInDesignatedPub {
 
 	/** The aff workplace history repo. */
