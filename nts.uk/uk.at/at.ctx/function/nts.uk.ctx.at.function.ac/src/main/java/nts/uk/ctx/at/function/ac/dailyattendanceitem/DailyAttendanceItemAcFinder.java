0c1d9ef3de008f1da405b7b67c874dc22749e977
ThuongDangXuan <thuong_dx@nittsusystem-vn.com>
2018-03-30 17:21:38 +0700

    add source daily attendance item adapter

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.ac/src/main/java/nts/uk/ctx/at/function/ac/dailyattendanceitem/DailyAttendanceItemAcFinder.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.ac/src/main/java/nts/uk/ctx/at/function/ac/dailyattendanceitem/DailyAttendanceItemAcFinder.java
@@ -6,15 +6,25 @@ import java.util.stream.Collectors;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+import nts.arc.time.GeneralDate;
 import nts.uk.ctx.at.function.dom.adapter.DailyAttendanceItemAdapter;
 import nts.uk.ctx.at.function.dom.adapter.DailyAttendanceItemAdapterDto;
+import nts.uk.ctx.at.function.dom.adapter.dailyattendanceitem.AttendanceItemValueImport;
+import nts.uk.ctx.at.function.dom.adapter.dailyattendanceitem.AttendanceResultImport;
+import nts.uk.ctx.at.record.pub.dailyattendanceitem.AttendanceItemService;
+import nts.uk.ctx.at.record.pub.dailyattendanceitem.AttendanceItemValue;
+import nts.uk.ctx.at.record.pub.dailyattendanceitem.AttendanceResult;
 import nts.uk.ctx.at.shared.pub.scherec.dailyattendanceitem.DailyAttendanceItemRecPub;
+import nts.uk.shr.com.time.calendar.period.DatePeriod;
 
 @Stateless
 public class DailyAttendanceItemAcFinder implements DailyAttendanceItemAdapter {
 
 	@Inject
 	private DailyAttendanceItemRecPub dailyAttendanceItemPub;
+	
+	@Inject
+	private AttendanceItemService attendanceItemService;
 
 	@Override
 	public List<DailyAttendanceItemAdapterDto> getDailyAttendanceItem(String companyId,
@@ -34,5 +44,25 @@ public class DailyAttendanceItemAcFinder implements DailyAttendanceItemAdapter {
 					f.getNameLineFeedPosition());
 		}).collect(Collectors.toList());
 	}
+
+	@Override
+	public AttendanceResultImport getValueOf(String employeeId, GeneralDate workingDate, List<Integer> itemIds) {
+		AttendanceResult  attendanceResult= attendanceItemService.getValueOf(employeeId, workingDate, itemIds);
+		return buildResultImport(attendanceResult);
+	}
+
+	@Override
+	public List<AttendanceResultImport> getValueOf(List<String> employeeIds, DatePeriod workingDate, List<Integer> itemIds) {
+		List<AttendanceResult> listAttendanceResult =  attendanceItemService.getValueOf(employeeIds, workingDate, itemIds);
+		return listAttendanceResult.stream().map( e -> buildResultImport(e)).collect(Collectors.toList());
+	}
 	
+	private AttendanceItemValueImport buildValueImport(AttendanceItemValue export) {
+		return  new AttendanceItemValueImport(export.getValueType(), export.getItemId(), export.getValue());
+	}
+	
+	private AttendanceResultImport buildResultImport(AttendanceResult export) {
+		return AttendanceResultImport.builder().employeeId(export.getEmployeeId()).workingDate(export.getWorkingDate())
+				.attendanceItems(export.getAttendanceItems().stream().map(value -> buildValueImport(value)).collect(Collectors.toList())).build();		
+	}
 }
