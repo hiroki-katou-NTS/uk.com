a92600caa3db9e2fa523c4f35aa98a2860b68a40
NTS\do_dt <du_dt@nittsusystem-vn.com>
2018-04-02 15:45:54 +0900

    勤務実績に反映: 事前申請の処理(休日出勤申請)

---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/dailyperformanceprocessing/appreflect/holidayworktime/PreHolidayWorktimeReflectService.java
@@ -0,0 +1,16 @@
+package nts.uk.ctx.at.record.dom.dailyperformanceprocessing.appreflect.holidayworktime;
+/**
+ * 勤務実績に反映
+ * 事前申請の処理(休日出勤申請)
+ * @author do_dt
+ *
+ */
+public interface PreHolidayWorktimeReflectService {
+	/**
+	 * 事前申請の処理(休日出勤申請)
+	 * @param holidayWorkPara
+	 * @return
+	 */
+	public boolean preHolidayWorktimeReflect(HolidayWorktimePara holidayWorkPara);
+	
+}
