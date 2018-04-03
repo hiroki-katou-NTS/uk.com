ba33cfdd26f130deb0989fe7a843a02b0f367708
NSVN-PhamVanThanh <thanh_pv@nittsusystem-vn.com>
2018-04-03 16:58:03 +0700

    Update KTG001, KTG002

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ktg/002/a/ktg002.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ktg/002/a/ktg002.a.vm.ts
@@ -1,14 +1,16 @@
 module nts.uk.at.view.ktg002.a.viewmodel {
     import block = nts.uk.ui.block;
     import windows = nts.uk.ui.windows;
+    import getText = nts.uk.resource.getText;
     export class ScreenModel {
 
-
+        text: KnockoutObservable<string>;
+        visible: KnockoutObservable<boolean>;
         constructor() {
             let self = this;
-
+            self.text = ko.observable("");
+            self.visible = ko.observable(false);
         }
-
         /**
          * startPage
          */
@@ -16,16 +18,20 @@ module nts.uk.at.view.ktg002.a.viewmodel {
             let self = this;
             let dfd = $.Deferred();
             service.getData().done((data) => {
-                console.log(data);
-            });
+                //console.log(data);
+                if(true){
+                    self.text = ko.observable(getText('KTG002_4'));
+                    self.visible = ko.observable(true);
+                }else{
+                    self.text = ko.observable(getText('KTG002_5'));
+                    self.visible = ko.observable(false);
+                }
             dfd.resolve();
+            });
             return dfd.promise();
         }
         dailyPerformanceConfirm() {
-            block.invisible();
-            windows.sub.modeless("/view/kdw/004/a/index.xhtml").onClosed(() => {
-                block.clear();
-            });
+            nts.uk.request.jump("/view/cmm/045/a/index.xhtml?a=1", {});
         }
     }
 }
