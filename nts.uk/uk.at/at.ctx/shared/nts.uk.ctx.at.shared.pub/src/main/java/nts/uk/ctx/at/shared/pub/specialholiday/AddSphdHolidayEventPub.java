1b64d7c54b30929e9c1a97068ba1816d05e3f53f
NSVN-BuiVanChinh <chinh_bv@nittsusystem-vn.com>
2018-04-06 15:26:51 +0700

    kmf004: remove event pub

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.pub/src/main/java/nts/uk/ctx/at/shared/pub/specialholiday/AddSphdHolidayEventPub.java
+++
@@ -1,9 +0,0 @@
-package nts.uk.ctx.at.shared.pub.specialholiday;
-
-public class AddSphdHolidayEventPub extends SphdHolidayEventPub{
-
-	public AddSphdHolidayEventPub(boolean isEffective, int specialHolidayCode, String specialHolidayName) {
-		super(isEffective, specialHolidayCode, specialHolidayName);
-	}
-
-}
