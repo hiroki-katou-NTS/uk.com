a0114776661b2ef59eba87bc38d26d0efbb2eff1
NSVN-NguyenXuanThanh <thanh_nx@nittsusystem-vn.com>
2018-04-13 10:14:44 +0700

    resolve conflict kdw003,004

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/003/a/kdw003.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/003/a/kdw003.a.vm.ts
@@ -148,7 +148,14 @@ module nts.uk.at.view.kdw003.a.viewmodel {
         
         initScreenSPR: any = 0;
         showDateRange: KnockoutObservable<any> = ko.observable(true);
-
+        flexShortage: KnockoutObservable<FlexShortage> = ko.observable(null);
+        optionNoOfHolidays: any = ko.mapping.fromJS({
+            width: "50",
+            grouplength: 3,
+            decimallength: 2
+        })
+        calcFlex: KnockoutObservable<CalcFlex> = ko.observable(null);
+        breakTimeDay: KnockoutObservable<BreakTimeDay> = ko.observable(null);
         constructor(dataShare:any) {
             var self = this;
             self.initLegendButton();
@@ -233,6 +240,9 @@ module nts.uk.at.view.kdw003.a.viewmodel {
                 self.shareObject().mapDataShare(dataShare.initParam, dataShare.extractionParam, dataShare.dataSPR);
                 self.showDateRange(self.shareObject().changePeriodAtr);
             }
+            
+            self.flexShortage.subscribe((val:any) => {
+            });
         }
          helps(event, data){
              var self = this;
@@ -368,6 +378,7 @@ module nts.uk.at.view.kdw003.a.viewmodel {
                 }: null,
                 displayFormat : _.isEmpty(self.shareObject()) ? 0 : self.shareObject().displayFormat,
                 initScreen: 0,
+                mode: _.isEmpty(self.shareObject()) ? 0 : self.shareObject().screenMode,
                 lstEmployee: [],
                 formatCodes: self.formatCodes(),
                 objectShare:  _.isEmpty(self.shareObject()) ? null : self.shareObject()
@@ -395,7 +406,8 @@ module nts.uk.at.view.kdw003.a.viewmodel {
                 
                 self.employmentCode(data.employmentCode);
                 self.lstAttendanceItem(data.lstControlDisplayItem.lstAttendanceItem);
-                self.showButton = ko.observable(new AuthorityDetailModel(data.authorityDto, data.lstControlDisplayItem.settingUnit));
+                let showCheckbox = _.isEmpty(self.shareObject()) ? data.showPrincipal : data.showSupervisor;
+                self.showButton = ko.observable(new AuthorityDetailModel(data.authorityDto, data.lstControlDisplayItem.settingUnit, showCheckbox));
                 self.referenceVacation(new ReferenceVacation(data.yearHolidaySettingDto == null ? false : data.yearHolidaySettingDto.manageAtr, data.substVacationDto == null ? false : data.substVacationDto.manageAtr, data.compensLeaveComDto == null ? false : data.compensLeaveComDto.manageAtr, data.com60HVacationDto == null ? false : data.com60HVacationDto.manageAtr, self.showButton()));
                 self.showTighProcess(data.identityProcessDto.useIdentityOfMonth);
                 // Fixed Header
@@ -431,6 +443,14 @@ module nts.uk.at.view.kdw003.a.viewmodel {
                 self.loadCcg001();
                 // no20
                 self.dPErrorDto(data.dperrorDto);
+                // flex
+                if (data.flexShortage != null) {
+                    self.breakTimeDay(data.flexShortage.breakTimeDay);
+                    self.calcFlex(new CalcFlex(data.flexShortage.value18, data.flexShortage.value21, data.flexShortage.value189, data.flexShortage.value190, data.flexShortage.value191));
+                    self.flexShortage(new FlexShortage(self, self.calcFlex()));
+                }else{
+                    $("#flex").remove();
+                }
                 nts.uk.ui.block.clear();
                 dfd.resolve();
             }).fail(function(error) {
@@ -457,7 +477,8 @@ module nts.uk.at.view.kdw003.a.viewmodel {
                             self.createSumColumn(data);
                             self.employmentCode(data.employmentCode);
                             self.lstAttendanceItem(data.lstControlDisplayItem.lstAttendanceItem);
-                            self.showButton(new AuthorityDetailModel(data.authorityDto, data.lstControlDisplayItem.settingUnit));
+                            let showCheckbox = _.isEmpty(self.shareObject()) ? data.showPrincipal : data.showSupervisor;
+                            self.showButton = ko.observable(new AuthorityDetailModel(data.authorityDto, data.lstControlDisplayItem.settingUnit, showCheckbox));
                             self.referenceVacation(new ReferenceVacation(data.yearHolidaySettingDto == null ? false : data.yearHolidaySettingDto.manageAtr, data.substVacationDto == null ? false : data.substVacationDto.manageAtr, data.compensLeaveComDto == null ? false : data.compensLeaveComDto.manageAtr, data.com60HVacationDto == null ? false : data.com60HVacationDto.manageAtr, self.showButton()));
                             // Fixed Header
                             self.fixHeaders(data.lstFixedHeader);
@@ -602,7 +623,7 @@ module nts.uk.at.view.kdw003.a.viewmodel {
                 });
                 if(!_.isEmpty(self.shareObject()) && self.shareObject().initClock != null){
                     let dataGout = new InfoCellEdit("", "31", String(self.shareObject().initClock.goOut), "INTEGER", "I_A_A_A_A1", self.shareObject().initClock.employeeId, self.shareObject().initClock.dateSpr.utc().toISOString(), 0);
-                    let dataLiveTime = new InfoCellEdit("", "41", String(self.shareObject().initClock.liveTime), "INTEGER", "I_A_A_A_A2", self.shareObject().initClock.employeeId, self.shareObject().initClock.dateSpr.utc().toISOString(), 0);
+                    let dataLiveTime = new InfoCellEdit("", "34", String(self.shareObject().initClock.liveTime), "INTEGER", "I_A_A_A_A2", self.shareObject().initClock.employeeId, self.shareObject().initClock.dateSpr.utc().toISOString(), 0);
                     dataChangeProcess.push(dataGout);
                     dataChangeProcess.push(dataLiveTime);
                 }
@@ -793,12 +814,14 @@ module nts.uk.at.view.kdw003.a.viewmodel {
                 $("#emp-component").css("display", "none");
                 $('#numberHoliday').hide();
                 $('#fixed-table').hide();
+                $('#flex').hide();
                // $("#content-grid").attr('style', 'top: 225px !IMPORTANT');
             } else {
                 $("#cbListDate").css("display", "none");
                 $("#emp-component").css("display", "none");
                 $('#numberHoliday').hide();
                 $('#fixed-table').hide();
+                $('#flex').hide();
                // $("#content-grid").attr('style', 'top: 180px !IMPORTANT');
             }
         }
@@ -828,6 +851,7 @@ module nts.uk.at.view.kdw003.a.viewmodel {
                     },
                     displayFormat : self.displayFormat(),
                     initScreen: 1,
+                     mode: _.isEmpty(self.shareObject()) ? 0 : self.shareObject().screenMode,
                     lstEmployee: lstEmployee,
                     formatCodes: self.formatCodes(),
                     objectShare: null
@@ -861,11 +885,9 @@ module nts.uk.at.view.kdw003.a.viewmodel {
                         self.errorModeHeader = [self.fixHeaders()[0], self.fixHeaders()[1], self.fixHeaders()[2], self.fixHeaders()[5], self.fixHeaders()[6], self.fixHeaders()[3]];
                     }
                     if (self.showSupervisor()) {
-                        if (self.showSupervisor()) {
-                            self.employeeModeHeader.push(self.fixHeaders()[8]);
-                            self.dateModeHeader.push(self.fixHeaders()[8]);
-                            self.errorModeHeader.push(self.fixHeaders()[8]);
-                        }
+                        self.employeeModeHeader.push(self.fixHeaders()[8]);
+                        self.dateModeHeader.push(self.fixHeaders()[8]);
+                        self.errorModeHeader.push(self.fixHeaders()[8]);
                     }
                     self.receiveData(data);
                     self.extraction();
@@ -1949,7 +1971,7 @@ module nts.uk.at.view.kdw003.a.viewmodel {
                 let param = {
                     typeDialog: typeGroup.split(":")[1],
                     param: {
-                        workTypeCode: val,
+                        workTypeCode: typeGroup.split(":")[1] == 4 ? columnKey.substring(4,columnKey.length) : val,
                         employmentCode: rowItemSelect.employmentCode,
                         workplaceId: rowItemSelect.workplaceId,
                         date: rowItemSelect.dateDetail,
@@ -1989,7 +2011,7 @@ module nts.uk.at.view.kdw003.a.viewmodel {
             //
             var self = this;
             let path: any = _.isEmpty(self.shareObject()) ? "" : self.shareObject().transitionDesScreen;         
-            nts.uk.request.jump(path);
+            nts.uk.request.jump("at", path);
         }
     }
     export class AuthorityDetailModel {
@@ -2010,7 +2032,7 @@ module nts.uk.at.view.kdw003.a.viewmodel {
         available19: KnockoutObservable<boolean> = ko.observable(true);
         available20: KnockoutObservable<boolean> = ko.observable(true);
         available21: KnockoutObservable<boolean> = ko.observable(true);
-        constructor(data: Array<DailyPerformanceAuthorityDto>, authority : any) {
+        constructor(data: Array<DailyPerformanceAuthorityDto>, authority : any, showCheckbox) {
             var self = this;
             if (!data) return;
             this.available1(self.checkAvailable(data, 1));
@@ -2024,10 +2046,13 @@ module nts.uk.at.view.kdw003.a.viewmodel {
             this.available7(self.checkAvailable(data, 7));
             this.available23(self.checkAvailable(data, 23));
             this.available25(self.checkAvailable(data, 25));
-            if(self.checkAvailable(data, 25)){
-                $("#btn-signAll").css("visibility", "visible"); 
-            }else{
+            if (self.checkAvailable(data, 25) && showCheckbox) {
+                $("#btn-signAll").css("visibility", "visible");
+                $("#btn-releaseAll").css("visibility", "visible");
+
+            } else {
                 $("#btn-signAll").css("visibility", "hidden");
+                $("#btn-releaseAll").css("visibility", "hidden");
             }
            // $("#btn-signAll").css("visibility", "hidden");
             this.available17(self.checkAvailable(data, 17));
@@ -2534,4 +2559,84 @@ module nts.uk.at.view.kdw003.a.viewmodel {
             this.goOut = data.goOut == undefined ? "" : data.goOut;
         }
     }
+    
+    class FlexShortage {
+        shortageTime: KnockoutObservable<any> = ko.observable();
+        nextMonthTransferredMoneyTime: KnockoutObservable<any> = ko.observable();
+        noOfHolidays: KnockoutObservable<any> = ko.observable();
+        absentDeductionTime: KnockoutObservable<any> = ko.observable();
+        parent: KnockoutObservable<any> = ko.observable();
+        constructor(parent: any, dataCalc: CalcFlex) {
+            let self = this;
+            let val18 = dataCalc.value18;
+            let val21 = dataCalc.value21;
+            let val189 = dataCalc.value189;
+            let val190 = dataCalc.value190;
+            let val191 = dataCalc.value191;
+            this.parent = ko.observable(parent);
+            self.shortageTime(nts.uk.resource.getText("KDW003_89", [self.convertToHours(Number(val191) + Number(val21) * (-1)), self.convertToHours(Number(val21) * (-1))]));
+            self.nextMonthTransferredMoneyTime(self.convertToHours(Number(val18) * (-1)));
+            self.noOfHolidays(Number(val189));
+            self.absentDeductionTime(Number(val190));
+            self.checkColor(self.nextMonthTransferredMoneyTime());
+            this.noOfHolidays.subscribe(val => {
+                    self.calc(self.parent().calcFlex(), self.parent().breakTimeDay());
+            });
+            this.absentDeductionTime.subscribe(val => {
+                    self.calc(self.parent().calcFlex(), self.parent().breakTimeDay());
+            });
+        }
+
+        calc(dataCalc: CalcFlex, breakTimeDay: BreakTimeDay){
+            let self = this;
+            let number189 = Number(self.noOfHolidays());
+            let val189 = Math.floor(number189) * Number(breakTimeDay.day) + (number189 - Math.floor(number189))/0.5 * breakTimeDay.am;
+            self.nextMonthTransferredMoneyTime(self.convertToHours((val189 + self.absentDeductionTime() - dataCalc.value191 + dataCalc.value21)));
+            self.checkColor(self.nextMonthTransferredMoneyTime());
+        }
+        
+        convertToHours(value: any): any {
+            let self = this;
+            let hours = String(Math.floor(value / 60));
+            let minutes = String(Math.abs(value) % 60);
+            if (Number(minutes) < 10) minutes = "0" + minutes;
+            return hours + ":" + minutes;
+        }
+        
+        checkColor(value : any){
+            if (value.indexOf("-") != -1) {
+                let hours = value.split("-")[1].split(":");
+                if (Number(hours[0]) > 15 || (Number(hours[0]) == 15 && hours[1] != "00")) {
+                    $("#next-month").attr('style', 'background-color: red !important');
+                } else {
+                    $("#next-month").attr('style', 'background-color: white !important');
+                }
+            } else {
+                $("#next-month").attr('style', 'background-color: white !important');
+            }
+        }
+    }
+    
+    class CalcFlex {
+        value18: any;
+        value21: any;
+        value189: any;
+        value190: any;
+        value191: any;
+        constructor(value18: any, value21: any, value189: any, value190: any, value191: any) {
+            this.value18 = value18;
+            this.value21 = value21;
+            this.value189 = value189;
+            this.value190 = value190;
+            this.value191 = value191;
+        }
+        
+    }
+    
+    interface BreakTimeDay {
+        day: any;
+        am: any;
+        pm: any;
+    }
+
 }
