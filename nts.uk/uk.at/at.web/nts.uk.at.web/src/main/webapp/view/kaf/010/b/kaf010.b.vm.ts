833c29dd5e84a1ac08058209b9304d6f966dc513
Vu Thang Loi <loi_vt@nittsusystem-vn.com>
2018-04-16 10:58:32 +0700

    update fixbug#91265

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/010/b/kaf010.b.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/010/b/kaf010.b.vm.ts
@@ -156,10 +156,17 @@ module nts.uk.at.view.kaf010.b {
                     if(res.messageId == 'Msg_426'){
                        dialog.alertError({messageId : res.messageId}).then(function(){
                             nts.uk.ui.block.clear();
+                           nts.uk.request.jump("/view/cmm/045/a/index.xhtml");
                     });
+                    }else if(res.messageId == 'Msg_423'){
+                        dialog.alertError({ messageId: res.messageId, messageParams: res.parameterIds })
+                            .then(function() {
+                                nts.uk.ui.block.clear();
+                                nts.uk.request.jump("/view/cmm/045/a/index.xhtml");
+                            });
                     }else{ 
                         nts.uk.ui.dialog.alertError(res.message).then(function(){
-                            nts.uk.request.jump("com", "/view/ccg/008/a/index.xhtml");
+                            nts.uk.request.jump("/view/cmm/045/a/index.xhtml");
                             nts.uk.ui.block.clear();
                         });
                     }
