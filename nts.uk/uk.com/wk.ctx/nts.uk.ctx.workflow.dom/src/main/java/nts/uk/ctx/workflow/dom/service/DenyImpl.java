59409b3d5dd5eb449c5025700e3088e406e1e532
NSVN-NguyenXuanThanh <thanh_nx@nittsusystem-vn.com>
2018-04-17 17:34:44 +0700

    cherry pick from kdw003: Hưng fix approve

--- nts.uk/uk.com/wk.ctx/nts.uk.ctx.workflow.dom/src/main/java/nts/uk/ctx/workflow/dom/service/DenyImpl.java
+++ nts.uk/uk.com/wk.ctx/nts.uk.ctx.workflow.dom/src/main/java/nts/uk/ctx/workflow/dom/service/DenyImpl.java
@@ -39,7 +39,7 @@ public class DenyImpl implements DenyService {
 	@Override
 	public Boolean doDeny(String companyID, String rootStateID, String employeeID, String memo) {
 		Boolean executedFlag = false;
-		Optional<ApprovalRootState> opApprovalRootState = approvalRootStateRepository.findEmploymentApp(rootStateID);
+		Optional<ApprovalRootState> opApprovalRootState = approvalRootStateRepository.findByID(rootStateID);
 		if(!opApprovalRootState.isPresent()){
 			throw new RuntimeException("状態：承認ルート取得失敗"+System.getProperty("line.separator")+"error: ApprovalRootState, ID: "+rootStateID);
 		}
