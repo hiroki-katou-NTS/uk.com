9fc949dd84ab237bc44f8feaa538aef1bf4dfbdd
NSVC\dudt <du_dt@nittsusystem-vn.com>
2018-04-17 16:25:33 +0700

    hoidaywork thay doi cach luu thong tin

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/dailyperformanceprocessing/appreflect/holidayworktime/HolidayWorkReflectProcessImpl.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/dailyperformanceprocessing/appreflect/holidayworktime/HolidayWorkReflectProcessImpl.java
@@ -8,7 +8,9 @@ import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import nts.arc.time.GeneralDate;
+import nts.uk.ctx.at.record.dom.actualworkinghours.AttendanceTimeOfDailyPerformance;
 import nts.uk.ctx.at.record.dom.dailyperformanceprocessing.appreflect.ScheAndRecordSameChangeFlg;
+import nts.uk.ctx.at.record.dom.dailyprocess.calc.IntegrationOfDaily;
 import nts.uk.ctx.at.record.dom.workinformation.service.reflectprocess.ReflectParameter;
 import nts.uk.ctx.at.record.dom.workinformation.service.reflectprocess.ScheWorkUpdateService;
 import nts.uk.ctx.at.shared.dom.worktime.service.WorkTimeIsFluidWork;
@@ -21,23 +23,25 @@ public class HolidayWorkReflectProcessImpl implements HolidayWorkReflectProcess{
 	@Inject
 	private ScheWorkUpdateService workUpdate;
 	@Override
-	public void updateScheWorkTimeType(String employeeId, GeneralDate baseDate, String workTypeCode,
-			String workTimeCode, boolean scheReflectFlg, ScheAndRecordSameChangeFlg scheAndRecordSameChangeFlg) {
+	public IntegrationOfDaily updateScheWorkTimeType(String employeeId, GeneralDate baseDate, String workTypeCode,
+			String workTimeCode, boolean scheReflectFlg, 
+			ScheAndRecordSameChangeFlg scheAndRecordSameChangeFlg,
+			IntegrationOfDaily dailyData) {
 		//ＩNPUT．勤務種類コードとＩNPUT．就業時間帯コードをチェックする
 		if(workTimeCode.isEmpty()
 				|| workTypeCode.isEmpty()) {
-			return;
+			return dailyData;
 		}
 		//予定勤種・就時を反映できるかチェックする
 		if(!this.checkScheWorkTimeReflect(employeeId, baseDate, workTimeCode, scheReflectFlg, scheAndRecordSameChangeFlg)) {
-			return;
+			return dailyData;
 		}
 		//予定勤種・就時の反映
 		ReflectParameter reflectInfo = new ReflectParameter(employeeId, 
 				baseDate, 
 				workTimeCode, 
 				workTypeCode); 
-		workUpdate.updateWorkTimeType(reflectInfo, true);
+		return workUpdate.updateWorkTimeTypeHoliwork(reflectInfo, true, dailyData);
 	}
 
 	@Override
@@ -60,7 +64,10 @@ public class HolidayWorkReflectProcessImpl implements HolidayWorkReflectProcess{
 	}
 
 	@Override
-	public void reflectWorkTimeFrame(String employeeId, GeneralDate baseDate, Map<Integer, Integer> mapWorkTimeFrame) {
+	public IntegrationOfDaily reflectWorkTimeFrame(String employeeId, 
+			GeneralDate baseDate, 
+			Map<Integer, Integer> mapWorkTimeFrame, 
+			IntegrationOfDaily dailyData) {
 		Map<Integer, Integer> tmp = new HashMap<>();
 		for(Map.Entry<Integer,Integer> entry : mapWorkTimeFrame.entrySet()){
 			//INPUT．休出時間のループ中の番をチェックする
@@ -70,7 +77,7 @@ public class HolidayWorkReflectProcessImpl implements HolidayWorkReflectProcess{
 			}
 		}
 		//事前休出時間の反映
-		workUpdate.updateWorkTimeFrame(employeeId, baseDate, tmp, true);
+		return workUpdate.updateWorkTimeFrame(employeeId, baseDate, tmp, true, dailyData);
 		
 	}
 
