d2a78d20b2acca30e4a707bf3080b840abfcd686
NSVC\dudt <du_dt@nittsusystem-vn.com>
2018-04-17 12:22:08 +0700

    thay doi cach tra du lieu ve

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/dailyperformanceprocessing/appreflect/overtime/PreOvertimeReflectService.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/dailyperformanceprocessing/appreflect/overtime/PreOvertimeReflectService.java
@@ -16,7 +16,7 @@ public interface PreOvertimeReflectService {
 	 * @param param
 	 * @return
 	 */
-	public ApplicationReflectOutput overtimeReflect(OvertimeParameter param);
+	public boolean overtimeReflect(OvertimeParameter param);
 	/**
 	 * 日別実績の修正からの計算
 	 * @param dailyData
