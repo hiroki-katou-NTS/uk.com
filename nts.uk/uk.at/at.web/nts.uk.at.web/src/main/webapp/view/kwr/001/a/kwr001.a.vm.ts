06f5bb67530c4064dba3b766a45701f78034ae25
YenNH <yennh@newwave.vn>
2018-04-19 15:54:31 +0700

    [KWR001][A][C][D] update UI screen d

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kwr/001/a/kwr001.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kwr/001/a/kwr001.a.vm.ts
@@ -1,29 +1,7 @@
 module nts.uk.at.view.kwr001.a {
     export module viewmodel {
         export class ScreenModel {
-            data: KnockoutObservable<number>;
             
-            constructor() {
-                var self = this;
-                self.data = ko.observable(1);
-            }
-            
-            public startPage(): JQueryPromise<void>  {
-                var dfd = $.Deferred<void>();
-                var self = this;
-                return dfd.promise();
-            }
-            openScreenD () {
-                var self = this;
-                nts.uk.ui.windows.setShared('KWR001_D', self.data(), true);
-                nts.uk.ui.windows.sub.modal('/view/kwr/001/d/index.xhtml').onClosed(function(): any {
-                    nts.uk.ui.windows.getShared('KWR001_D');
-                });
-            }
-            
-            saveData(): void {
-               
-            }
         }
     }
 }
