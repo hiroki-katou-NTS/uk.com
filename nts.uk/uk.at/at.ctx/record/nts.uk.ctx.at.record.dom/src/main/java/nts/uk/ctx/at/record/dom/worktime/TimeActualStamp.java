c652a43bc5723ce749bbfafaab76b88e3cb9843e
misaka mikoto <tin_dh@nittsusystem-vn.com>
2018-04-19 16:28:09 +0700

    resolve conflict TimeLeaveUpdateByWorkInfoChangeHandler

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/worktime/TimeActualStamp.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/worktime/TimeActualStamp.java
@@ -77,4 +77,9 @@ public class TimeActualStamp {
 		this.stamp = stamp == null ? Optional.empty() : stamp;
 		this.numberOfReflectionStamp = numberOfReflectionStamp;
 	}
+	
+	/** 「打刻」を削除する */
+	public void removeStamp() {
+		this.stamp = Optional.empty();
+	}
 }
