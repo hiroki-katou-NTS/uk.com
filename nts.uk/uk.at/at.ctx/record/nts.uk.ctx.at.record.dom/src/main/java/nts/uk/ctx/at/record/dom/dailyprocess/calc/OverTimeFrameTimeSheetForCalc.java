27acbcf17190df9199ec259bdf4079847b9108e8
NTS\keisuke_hoshina <keisuke_hoshina@nittsusystem.co.jp>
2018-04-19 20:00:27 +0900

    fixed miss merge

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/dailyprocess/calc/OverTimeFrameTimeSheetForCalc.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/dailyprocess/calc/OverTimeFrameTimeSheetForCalc.java
@@ -243,15 +243,6 @@ public class OverTimeFrameTimeSheetForCalc extends CalculationTimeSheet{
             /*振替処理   法定内基準時間を計算する*/
         	AttendanceTime ableRangeTime = new AttendanceTime(dailyUnit.getDailyTime().valueAsMinutes() - breakdownTimeDay.getPredetermineWorkTime());
         	if(ableRangeTime.greaterThan(0))
-<<<<<<< HEAD
-        		returnList.addAll(reclassified(ableRangeTime,
-        									   overTimeWorkFrameTimeSheetList.stream()
-        									   								 .filter(tc -> tc.getPayOrder().isPresent())
-        									   								 .sorted((first,second) -> first.getPayOrder().get().compareTo(second.getPayOrder().get()))
-        									   								 .collect(Collectors.toList()),
-        									   autoCalculationSet,
-        									   overTimeHourSetList));
-=======
         		return reclassified(ableRangeTime,overTimeWorkFrameTimeSheetList.stream()
         									   								    .filter(tc -> tc.getPayOrder().isPresent())
         									   								    .sorted((first,second) -> first.getPayOrder().get().compareTo(second.getPayOrder().isPresent()
@@ -262,7 +253,6 @@ public class OverTimeFrameTimeSheetForCalc extends CalculationTimeSheet{
         									   								    autoCalculationSet,
         									   								    overTimeHourSetList,
         									   								    holidayCalcMethodSet);
->>>>>>> b2afe03a3c... fixed bug
         }
         return overTimeWorkFrameTimeSheetList;
     }
