b27fe8ee5a33f2a5eb8a53be8125fe76aafe683e
NSVN-TrieuKhacTu <tu_tk@nittsusystem-vn.com>
2018-04-20 10:26:58 +0700

    resolve conflict MonPfmCorrectionFormatFinder.java

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.app/src/main/java/nts/uk/ctx/at/function/app/find/monthlycorrection/fixedformatmonthly/MonPfmCorrectionFormatFinder.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.app/src/main/java/nts/uk/ctx/at/function/app/find/monthlycorrection/fixedformatmonthly/MonPfmCorrectionFormatFinder.java
@@ -7,7 +7,6 @@ import java.util.stream.Collectors;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import javax.swing.plaf.basic.BasicTreeUI.CellEditorHandler;
 
 import nts.uk.ctx.at.function.dom.monthlycorrection.fixedformatmonthly.InitialDisplayMonthly;
 import nts.uk.ctx.at.function.dom.monthlycorrection.fixedformatmonthly.InitialDisplayMonthlyRepository;
@@ -16,47 +15,54 @@ import nts.uk.ctx.at.function.dom.monthlycorrection.fixedformatmonthly.MonPfmCor
 import nts.uk.shr.com.context.AppContexts;
 
 @Stateless
-public class MonPfmCorrectionFormatFinder  {
+public class MonPfmCorrectionFormatFinder {
 
 	@Inject
 	private MonPfmCorrectionFormatRepository repo;
-	
+
 	@Inject
 	private InitialDisplayMonthlyRepository initialDisplayMonthlyRepo;
-	
-	public List<MonPfmCorrectionFormatDto> getAllMonPfmCorrectionFormat(){
+
+	public List<MonPfmCorrectionFormatDto> getAllMonPfmCorrectionFormat() {
 		String companyID = AppContexts.user().companyId();
-		List<MonPfmCorrectionFormatDto> data = repo.getAllMonPfm(companyID).stream().map(c->MonPfmCorrectionFormatDto.fromDomain(c)).collect(Collectors.toList());
-		if(data.isEmpty())
+		List<MonPfmCorrectionFormatDto> data = repo.getAllMonPfm(companyID).stream().map(c -> MonPfmCorrectionFormatDto.fromDomain(c)).collect(Collectors.toList());
+		if (data.isEmpty())
 			return Collections.emptyList();
-		for(MonPfmCorrectionFormatDto monPfmCorrectionFormat : data) {
+		for (MonPfmCorrectionFormatDto monPfmCorrectionFormat : data) {
 			Optional<InitialDisplayMonthly> checkData = initialDisplayMonthlyRepo.getInitialDisplayMon(companyID, monPfmCorrectionFormat.getMonthlyPfmFormatCode());
-			if(checkData.isPresent()) {
+			if (checkData.isPresent()) {
 				monPfmCorrectionFormat.setSetFormatToDefault(false);
-			}else {
+			} else {
 				monPfmCorrectionFormat.setSetFormatToDefault(true);
 			}
-				
+
 		}
 		return data;
-		
+
 	}
-	
-	public MonPfmCorrectionFormatDto getMonPfmCorrectionFormat(String monthlyPfmFormatCode ){
+
+	public MonPfmCorrectionFormatDto getMonPfmCorrectionFormat(String monthlyPfmFormatCode) {
 		String companyID = AppContexts.user().companyId();
 		Optional<MonPfmCorrectionFormat> data = repo.getMonPfmCorrectionFormat(companyID, monthlyPfmFormatCode);
-		if(data.isPresent()) {
+		if (data.isPresent()) {
 			MonPfmCorrectionFormatDto dataUi = MonPfmCorrectionFormatDto.fromDomain(data.get());
-			Optional<InitialDisplayMonthly> checkData =  initialDisplayMonthlyRepo.getInitialDisplayMon(companyID, data.get().getMonthlyPfmFormatCode().v());
-			if(checkData.isPresent())
+			Optional<InitialDisplayMonthly> checkData = initialDisplayMonthlyRepo.getInitialDisplayMon(companyID, data.get().getMonthlyPfmFormatCode().v());
+			if (checkData.isPresent())
 				dataUi.setSetFormatToDefault(false);
 			else
 				dataUi.setSetFormatToDefault(true);
 			return dataUi;
-			
+
 		}
-			
 		return null;
-		
+
+	}
+	
+
+	public List<MonPfmCorrectionFormatDto> getMonPfmCorrectionFormat(String companyID, List<String> monthlyPfmFormatCode) {
+		return repo.getMonPfmCorrectionFormat(companyID, monthlyPfmFormatCode)
+				.stream()
+				.map(item -> MonPfmCorrectionFormatDto.fromDomain(item))
+				.collect(Collectors.toList());
 	}
 }
