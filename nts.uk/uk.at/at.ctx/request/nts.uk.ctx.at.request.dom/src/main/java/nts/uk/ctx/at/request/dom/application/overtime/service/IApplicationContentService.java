60de875003c9798dfbf445c0b632ac710500e246
3SI\hiep.ld <hiep.ld@3si.vn>
2018-04-20 09:06:23 +0700

    update mailcontent

---
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/overtime/service/IApplicationContentService.java
@@ -0,0 +1,7 @@
+package nts.uk.ctx.at.request.dom.application.overtime.service;
+
+import nts.uk.ctx.at.request.dom.application.Application_New;
+
+public interface IApplicationContentService {
+	String getApplicationContent(Application_New app);
+}
