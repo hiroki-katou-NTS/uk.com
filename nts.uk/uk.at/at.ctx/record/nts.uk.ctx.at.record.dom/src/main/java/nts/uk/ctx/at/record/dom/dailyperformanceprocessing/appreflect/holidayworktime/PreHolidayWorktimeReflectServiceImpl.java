cdd577e77d9e8c791aa5eb6714150c069654a7ee
NSVN-NguyenVanVu <vu_nv@nittsusystem-vn.com>
2018-04-20 17:04:46 +0700

    comment for deploy

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/dailyperformanceprocessing/appreflect/holidayworktime/PreHolidayWorktimeReflectServiceImpl.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/dailyperformanceprocessing/appreflect/holidayworktime/PreHolidayWorktimeReflectServiceImpl.java
@@ -36,51 +36,56 @@ public class PreHolidayWorktimeReflectServiceImpl implements PreHolidayWorktimeR
 	private WorkInformationRepository workRepository;
 	@Inject
 	private ScheWorkUpdateService scheWork;
+//	@Override
+//	public ApplicationReflectOutput preHolidayWorktimeReflect(HolidayWorktimePara holidayWorkPara) {		
+//		try {
+//			Optional<WorkInfoOfDailyPerformance> optDailyData = workRepository.find(holidayWorkPara.getEmployeeId(), holidayWorkPara.getBaseDate());
+//			if(!optDailyData.isPresent()) {
+//				return new ApplicationReflectOutput(EnumAdaptor.valueOf(holidayWorkPara.getHolidayWorkPara().getReflectedState().value, ReflectedStateRecord.class), 
+//						holidayWorkPara.getHolidayWorkPara().getReasonNotReflect() == null ? null : EnumAdaptor.valueOf(holidayWorkPara.getHolidayWorkPara().getReasonNotReflect().value, ReasonNotReflectRecord.class));
+//			}
+//			// 予定勤種・就時の反映
+//			holidayWorkProcess.updateScheWorkTimeType(holidayWorkPara.getEmployeeId(),
+//					holidayWorkPara.getBaseDate(), 
+//					holidayWorkPara.getHolidayWorkPara().getWorkTypeCode(), 
+//					holidayWorkPara.getHolidayWorkPara().getWorkTimeCode(), 
+//					holidayWorkPara.isScheReflectFlg(), 
+//					holidayWorkPara.getScheAndRecordSameChangeFlg());
+//			//勤種・就時の反映
+//			ReflectParameter reflectInfo = new ReflectParameter(holidayWorkPara.getEmployeeId(), 
+//					holidayWorkPara.getBaseDate(), 
+//					holidayWorkPara.getHolidayWorkPara().getWorkTimeCode(), 
+//					holidayWorkPara.getHolidayWorkPara().getWorkTypeCode()); 
+//			workUpdate.updateWorkTimeType(reflectInfo, false);
+//					
+//			//予定開始時刻の反映
+//			//予定終了時刻の反映
+//			TimeReflectPara timeData = new TimeReflectPara(holidayWorkPara.getEmployeeId(), 
+//					holidayWorkPara.getBaseDate(), 
+//					holidayWorkPara.getHolidayWorkPara().getStartTime(), 
+//					holidayWorkPara.getHolidayWorkPara().getEndTime(), 
+//					1, 
+//					true, 
+//					true);
+//			scheWork.updateScheStartEndTime(timeData);
+//			
+//			//事前休出時間の反映
+//			holidayWorkProcess.reflectWorkTimeFrame(holidayWorkPara.getEmployeeId(), holidayWorkPara.getBaseDate(), holidayWorkPara.getHolidayWorkPara().getMapWorkTimeFrame());
+//			//事前所定外深夜時間の反映
+//			workUpdate.updateTimeShiftNight(holidayWorkPara.getEmployeeId(), holidayWorkPara.getBaseDate(), holidayWorkPara.getHolidayWorkPara().getNightTime(), true);
+//			IntegrationOfDaily calculateData = calculate.calculate(overTimeService.calculateForAppReflect(holidayWorkPara.getEmployeeId(), holidayWorkPara.getBaseDate()));
+//			attendanceTime.updateFlush(calculateData.getAttendanceTimeOfDailyPerformance().get());
+//			return new ApplicationReflectOutput(ReflectedStateRecord.REFLECTED, ReasonNotReflectRecord.ACTUAL_CONFIRMED);
+//		} catch (Exception e) {
+//			return new ApplicationReflectOutput(EnumAdaptor.valueOf(holidayWorkPara.getHolidayWorkPara().getReflectedState().value, ReflectedStateRecord.class), 
+//					holidayWorkPara.getHolidayWorkPara().getReasonNotReflect() == null ? null : EnumAdaptor.valueOf(holidayWorkPara.getHolidayWorkPara().getReasonNotReflect().value, ReasonNotReflectRecord.class));
+//		}
+//		
+//	}
 	@Override
-	public ApplicationReflectOutput preHolidayWorktimeReflect(HolidayWorktimePara holidayWorkPara) {		
-		try {
-			Optional<WorkInfoOfDailyPerformance> optDailyData = workRepository.find(holidayWorkPara.getEmployeeId(), holidayWorkPara.getBaseDate());
-			if(!optDailyData.isPresent()) {
-				return new ApplicationReflectOutput(EnumAdaptor.valueOf(holidayWorkPara.getHolidayWorkPara().getReflectedState().value, ReflectedStateRecord.class), 
-						holidayWorkPara.getHolidayWorkPara().getReasonNotReflect() == null ? null : EnumAdaptor.valueOf(holidayWorkPara.getHolidayWorkPara().getReasonNotReflect().value, ReasonNotReflectRecord.class));
-			}
-			// 予定勤種・就時の反映
-			holidayWorkProcess.updateScheWorkTimeType(holidayWorkPara.getEmployeeId(),
-					holidayWorkPara.getBaseDate(), 
-					holidayWorkPara.getHolidayWorkPara().getWorkTypeCode(), 
-					holidayWorkPara.getHolidayWorkPara().getWorkTimeCode(), 
-					holidayWorkPara.isScheReflectFlg(), 
-					holidayWorkPara.getScheAndRecordSameChangeFlg());
-			//勤種・就時の反映
-			ReflectParameter reflectInfo = new ReflectParameter(holidayWorkPara.getEmployeeId(), 
-					holidayWorkPara.getBaseDate(), 
-					holidayWorkPara.getHolidayWorkPara().getWorkTimeCode(), 
-					holidayWorkPara.getHolidayWorkPara().getWorkTypeCode()); 
-			workUpdate.updateWorkTimeType(reflectInfo, false);
-					
-			//予定開始時刻の反映
-			//予定終了時刻の反映
-			TimeReflectPara timeData = new TimeReflectPara(holidayWorkPara.getEmployeeId(), 
-					holidayWorkPara.getBaseDate(), 
-					holidayWorkPara.getHolidayWorkPara().getStartTime(), 
-					holidayWorkPara.getHolidayWorkPara().getEndTime(), 
-					1, 
-					true, 
-					true);
-			scheWork.updateScheStartEndTime(timeData);
-			
-			//事前休出時間の反映
-			holidayWorkProcess.reflectWorkTimeFrame(holidayWorkPara.getEmployeeId(), holidayWorkPara.getBaseDate(), holidayWorkPara.getHolidayWorkPara().getMapWorkTimeFrame());
-			//事前所定外深夜時間の反映
-			workUpdate.updateTimeShiftNight(holidayWorkPara.getEmployeeId(), holidayWorkPara.getBaseDate(), holidayWorkPara.getHolidayWorkPara().getNightTime(), true);
-			IntegrationOfDaily calculateData = calculate.calculate(overTimeService.calculateForAppReflect(holidayWorkPara.getEmployeeId(), holidayWorkPara.getBaseDate()));
-			attendanceTime.updateFlush(calculateData.getAttendanceTimeOfDailyPerformance().get());
-			return new ApplicationReflectOutput(ReflectedStateRecord.REFLECTED, ReasonNotReflectRecord.ACTUAL_CONFIRMED);
-		} catch (Exception e) {
-			return new ApplicationReflectOutput(EnumAdaptor.valueOf(holidayWorkPara.getHolidayWorkPara().getReflectedState().value, ReflectedStateRecord.class), 
-					holidayWorkPara.getHolidayWorkPara().getReasonNotReflect() == null ? null : EnumAdaptor.valueOf(holidayWorkPara.getHolidayWorkPara().getReasonNotReflect().value, ReasonNotReflectRecord.class));
-		}
-		
+	public boolean preHolidayWorktimeReflect(HolidayWorktimePara holidayWorkPara) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
