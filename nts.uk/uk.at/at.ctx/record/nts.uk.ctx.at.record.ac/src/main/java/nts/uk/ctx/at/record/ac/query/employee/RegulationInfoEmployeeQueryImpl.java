279c431fd18d5dbc1ed262cc14484ca83573f336
NSVN-NguyenVanVu <vu_nv@nittsusystem-vn.com>
2018-04-23 15:21:40 +0700

    comment for Thanh để deploy

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.ac/src/main/java/nts/uk/ctx/at/record/ac/query/employee/RegulationInfoEmployeeQueryImpl.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.ac/src/main/java/nts/uk/ctx/at/record/ac/query/employee/RegulationInfoEmployeeQueryImpl.java
@@ -51,7 +51,7 @@ public class RegulationInfoEmployeeQueryImpl implements RegulationInfoEmployeeQu
 							.includeGoingOnLoan(queryX.getIncludeGoingOnLoan())
 							.systemType(2)
 //							.sortOrderNo(1)
-							.filterByClosure(false)
+//							.filterByClosure(false)
 							.includeRetirees(queryX.getIncludeRetirees()).build();
 	}
 }
