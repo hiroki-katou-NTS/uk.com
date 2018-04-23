580eb7a08b3dbed9a318f4f34d54952da2eef65a
3SI\hiep.ld <hiep.ld@3si.vn>
2018-04-23 09:39:17 +0700

    update get appcontent

---
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/common/service/application/IApplicationContentService.java
@@ -0,0 +1,8 @@
+package nts.uk.ctx.at.request.dom.application.common.service.application;
+
+import nts.uk.ctx.at.request.dom.application.Application_New;
+import nts.uk.ctx.at.request.dom.application.common.service.application.output.ApplicationForSendOutput;
+
+public interface IApplicationContentService {
+	String getApplicationContent(Application_New app); 
+}
