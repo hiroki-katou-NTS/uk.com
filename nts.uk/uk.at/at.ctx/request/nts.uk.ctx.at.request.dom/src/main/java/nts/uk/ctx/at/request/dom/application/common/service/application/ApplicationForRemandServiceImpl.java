ca0caec7e6c8e400d8847c665442cb6baa4ed27d
3SI\hiep.ld <hiep.ld@3si.vn>
2018-04-24 17:13:42 +0700

    kdl034_update job title

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/common/service/application/ApplicationForRemandServiceImpl.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/common/service/application/ApplicationForRemandServiceImpl.java
@@ -10,6 +10,8 @@ import javax.inject.Inject;
 import nts.uk.ctx.at.request.dom.application.ApplicationRepository_New;
 import nts.uk.ctx.at.request.dom.application.Application_New;
 import nts.uk.ctx.at.request.dom.application.common.adapter.bs.EmployeeRequestAdapter;
+import nts.uk.ctx.at.request.dom.application.common.adapter.bs.jobtitle.AtJobTitleAdapter;
+import nts.uk.ctx.at.request.dom.application.common.adapter.bs.jobtitle.dto.AffJobTitleHistoryImport;
 import nts.uk.ctx.at.request.dom.application.common.adapter.workflow.ApprovalRootStateAdapter;
 import nts.uk.ctx.at.request.dom.application.common.adapter.workflow.dto.ApprovalRootContentImport_New;
 import nts.uk.ctx.at.request.dom.application.common.service.application.output.ApplicationForRemandOutput;
@@ -28,30 +30,40 @@ public class ApplicationForRemandServiceImpl implements IApplicationForRemandSer
 	
 	@Inject
 	private EmployeeRequestAdapter employeeRequestAdapter;
+	
+	@Inject
+	private AtJobTitleAdapter jobTitleAdapter;
 	@Override
 	public ApplicationForRemandOutput getApplicationForRemand(String appID) {
 		String companyID = AppContexts.user().companyId();
 		Optional<Application_New> application_New = this.applicationRepository.findByID(companyID, appID);
-		ApprovalRootContentImport_New approvalRootContentImport = approvalRootStateAdapter
-				.getApprovalRootContent(companyID, null, null, null, appID, false);
-		String applicantPosition = "主任";
-		List<ApprovalFrameOutput> listApprovalFrame = new ArrayList<ApprovalFrameOutput>();
-		approvalRootContentImport.getApprovalRootState().getListApprovalPhaseState().forEach(x -> {
-			x.getListApprovalFrame().forEach(y -> {
-				List<DetailApproverOutput> listApprover = new ArrayList<DetailApproverOutput>();
-				y.getListApprover().forEach(z -> {
-					listApprover.add(new DetailApproverOutput(z.getApproverID(), z.getApproverName(), z.getRepresenterID(),
-							z.getRepresenterName(), "課長"));
+		if (application_New.isPresent()){
+			Application_New app = application_New.get();
+			ApprovalRootContentImport_New approvalRootContentImport = approvalRootStateAdapter
+					.getApprovalRootContent(companyID, null, null, null, appID, false);
+			Optional<AffJobTitleHistoryImport> jobTitle = jobTitleAdapter.getJobTitlebBySIDAndDate(app.getEmployeeID(), app.getAppDate());
+			String applicantPosition = jobTitle.isPresent() ? jobTitle.get().getJobTitleName() : "";
+			List<ApprovalFrameOutput> listApprovalFrame = new ArrayList<ApprovalFrameOutput>();
+			approvalRootContentImport.getApprovalRootState().getListApprovalPhaseState().forEach(x -> {
+				x.getListApprovalFrame().forEach(y -> {
+					List<DetailApproverOutput> listApprover = new ArrayList<DetailApproverOutput>();
+					y.getListApprover().forEach(z -> {
+						Optional<AffJobTitleHistoryImport> approverJobTitle = jobTitleAdapter.getJobTitlebBySIDAndDate(z.getApproverID(), app.getAppDate());
+						String approverPosition = approverJobTitle.isPresent() ? approverJobTitle.get().getJobTitleName() : "";
+						listApprover.add(new DetailApproverOutput(z.getApproverID(), z.getApproverName(), z.getRepresenterID(),
+								z.getRepresenterName(), approverPosition));
+					});
+					listApprovalFrame
+							.add(new ApprovalFrameOutput(y.getPhaseOrder(), y.getApprovalReason(), listApprover));
 				});
-				listApprovalFrame
-						.add(new ApprovalFrameOutput(y.getPhaseOrder(), y.getApprovalReason(), listApprover));
 			});
-		});
-		return new ApplicationForRemandOutput(appID, application_New.get().getVersion(),
-				approvalRootContentImport.getErrorFlag().value, applicantPosition,
-				application_New.isPresent() ? employeeRequestAdapter
-						.getEmployeeInfor(application_New.map(Application_New::getEmployeeID).orElse("")) : null,
-				listApprovalFrame);
+			return new ApplicationForRemandOutput(appID, application_New.get().getVersion(),
+					approvalRootContentImport.getErrorFlag().value, applicantPosition,
+					application_New.isPresent() ? employeeRequestAdapter
+							.getEmployeeInfor(application_New.map(Application_New::getEmployeeID).orElse("")) : null,
+					listApprovalFrame);
+		}
+		return null;
 	}
 	
 }
