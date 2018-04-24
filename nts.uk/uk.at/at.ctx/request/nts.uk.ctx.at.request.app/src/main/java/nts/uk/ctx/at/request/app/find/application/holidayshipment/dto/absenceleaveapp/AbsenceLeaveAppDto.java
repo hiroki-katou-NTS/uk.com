d98d932dc9c2f4625789046bc7b15dfbaec02031
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2018-04-24 15:09:32 +0700

    Change '振休振出申請' data type

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.app/src/main/java/nts/uk/ctx/at/request/app/find/application/holidayshipment/dto/absenceleaveapp/AbsenceLeaveAppDto.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.app/src/main/java/nts/uk/ctx/at/request/app/find/application/holidayshipment/dto/absenceleaveapp/AbsenceLeaveAppDto.java
@@ -1,81 +1,68 @@
 package nts.uk.ctx.at.request.app.find.application.holidayshipment.dto.absenceleaveapp;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import nts.arc.time.GeneralDate;
+import nts.uk.ctx.at.request.app.find.application.holidayshipment.HolidayShipmentAppDto;
+import nts.uk.ctx.at.request.app.find.application.holidayshipment.WorkingHoursDto;
 import nts.uk.ctx.at.request.dom.application.holidayshipment.absenceleaveapp.AbsenceLeaveApp;
+import nts.uk.ctx.at.request.dom.application.holidayshipment.absenceleaveapp.SubTargetDigestion;
 
 /**
  * @author sonnlb 振休申請Dto
  */
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class AbsenceLeaveAppDto extends HolidayShipmentAppDto {
 
-public class AbsenceLeaveAppDto {
-	/**
-	 * 申請ID
-	 */
-	private String appID;
-	/**
-	 * 勤務種類
-	 */
-	private String workTypeCD;
 	/**
 	 * 就業時間帯変更
 	 */
 	private int changeWorkHoursType;
 	/**
-	 * 就業時間帯
-	 */
-	private String workTimeCD;
-	/**
 	 * 勤務時間1
 	 */
-	private AbsenceLeaveWorkingHourDto wkTime1;
+	private WorkingHoursDto wkTime1;
 	/**
 	 * 勤務時間2
 	 */
-	private AbsenceLeaveWorkingHourDto wkTime2;
-	/**
-	 * 消化対象代休管理
-	 */
-	private List<SubTargetDigestionDto> subTargetDigestions;
+	private WorkingHoursDto wkTime2;
 	/**
 	 * 消化対象振休管理
 	 */
 	private List<SubDigestionDto> subDigestions;
 
-	/**
-	 * 基準日
-	 */
-	private GeneralDate appDate;
-
 	public static AbsenceLeaveAppDto fromDomain(AbsenceLeaveApp domain, GeneralDate appDate) {
-		AbsenceLeaveWorkingHourDto WorkTime1 = new AbsenceLeaveWorkingHourDto(domain.getWorkTime1().getStartTime().v(),
-				domain.getWorkTime1().getEndTime().v());
-		AbsenceLeaveWorkingHourDto WorkTime2 = new AbsenceLeaveWorkingHourDto(domain.getWorkTime2().getStartTime().v(),
-				domain.getWorkTime2().getEndTime().v());
-		List<SubTargetDigestionDto> subTargetDigestions = new ArrayList<SubTargetDigestionDto>();
-		domain.getSubTargetDigestions().forEach(x -> {
-			subTargetDigestions.add(new SubTargetDigestionDto(x.getAppID(), x.getHoursUsed(), x.getLeaveMngDataID(),
-					x.getBreakOutDate(), x.getRestState().value, x.getUnknownDate()));
-		});
 
-		List<SubDigestionDto> subDigestions = new ArrayList<SubDigestionDto>();
-		domain.getSubDigestions().forEach(x -> {
-			subDigestions.add(new SubDigestionDto(x.getAbsenceLeaveAppID(), x.getDaysUsedNo().value,
-					x.getPayoutMngDataID(), x.getPickUpState().value, x.getOccurrenceDate(), x.getUnknownDate()));
-		});
-		AbsenceLeaveAppDto result = new AbsenceLeaveAppDto(domain.getAppID(), domain.getWorkTypeCD(),
-				domain.getChangeWorkHoursType().value, domain.getWorkTimeCD().v(), WorkTime1, WorkTime2,
-				subTargetDigestions, subDigestions, appDate);
+		WorkingHoursDto WorkTime1 = WorkingHoursDto.createFromDomain(domain.getWorkTime1());
+
+		WorkingHoursDto WorkTime2 = WorkingHoursDto.createFromDomain(domain.getWorkTime2());
+
+		List<SubDigestionDto> subDigestions = domain.getSubDigestions().stream()
+				.map(x -> SubDigestionDto.createFromDomain(x)).collect(Collectors.toList());
+
+		AbsenceLeaveAppDto result = new AbsenceLeaveAppDto(domain.getAppID(), domain.getWorkTypeCD().v(),
+				domain.getChangeWorkHoursType().value, domain.getWorkTimeCD(), WorkTime1, WorkTime2,
+				domain.getSubTargetDigestions(), subDigestions, appDate);
 
 		return result;
+	}
+
+	public AbsenceLeaveAppDto(String appID, String workTypeCD, int changeWorkHoursType, String workTimeCD,
+			WorkingHoursDto workTime1, WorkingHoursDto workTime2, List<SubTargetDigestion> subTargetDigestions,
+			List<SubDigestionDto> subDigestions, GeneralDate appDate) {
+		super(appID, workTypeCD, appDate, subTargetDigestions, workTimeCD);
+		this.setChangeWorkHoursType(changeWorkHoursType);
+		this.setWkTime1(workTime1);
+		this.setWkTime2(workTime2);
+		this.setSubDigestions(subDigestions);
 	}
 
 }
