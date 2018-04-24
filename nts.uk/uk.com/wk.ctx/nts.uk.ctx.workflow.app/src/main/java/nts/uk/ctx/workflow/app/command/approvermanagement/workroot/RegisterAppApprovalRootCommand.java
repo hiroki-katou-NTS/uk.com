0a6ba26d0372ae13d58f7571353f2c64d04599cd
NTS\do_dt <du_dt@nittsusystem-vn.com>
2018-04-24 13:30:04 +0900

    CMM018: chuyen tu @Value -> @data lan 2

--- nts.uk/uk.com/wk.ctx/nts.uk.ctx.workflow.app/src/main/java/nts/uk/ctx/workflow/app/command/approvermanagement/workroot/RegisterAppApprovalRootCommand.java
+++ nts.uk/uk.com/wk.ctx/nts.uk.ctx.workflow.app/src/main/java/nts/uk/ctx/workflow/app/command/approvermanagement/workroot/RegisterAppApprovalRootCommand.java
@@ -2,9 +2,11 @@ package nts.uk.ctx.workflow.app.command.approvermanagement.workroot;
 
 import java.util.List;
 
-import lombok.Value;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Value
+@Data
+@NoArgsConstructor
 public class RegisterAppApprovalRootCommand {
 
 	/**就業ルート区分: 会社(0)　－　職場(1)　－　社員(2)*/
@@ -25,4 +27,20 @@ public class RegisterAppApprovalRootCommand {
 	private String endDate;
 	/**checkMode: まとめて登録モード(0), 申請個別登録モード(1)*/
 	private int checkMode;
+	public RegisterAppApprovalRootCommand(int rootType, String workpplaceId, String employeeId, boolean checkAddHist,
+			List<CompanyAppRootADto> root, AddHistoryDto addHist, List<AppType> lstAppType, String startDate,
+			String endDate, int checkMode) {
+		super();
+		this.rootType = rootType;
+		this.workpplaceId = workpplaceId;
+		this.employeeId = employeeId;
+		this.checkAddHist = checkAddHist;
+		this.root = root;
+		this.addHist = addHist;
+		this.lstAppType = lstAppType;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.checkMode = checkMode;
+	}
+	
 }
