ea22c941868e15e751320601cdfa5c59f9ab0306
NSVN-PhamVanThanh <thanh_pv@nittsusystem-vn.com>
2018-04-24 16:46:03 +0700

    Update KTG029

---
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/ktgwidget/find/DatePeriodDto.java
@@ -0,0 +1,20 @@
+package nts.uk.screen.at.app.ktgwidget.find;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import nts.arc.time.GeneralDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class DatePeriodDto {
+
+	private GeneralDate strCurrentMonth;
+	
+	private GeneralDate endCurrentMonth;
+	
+	private GeneralDate strNextMonth;
+	
+	private GeneralDate endNextMonth;
+}
