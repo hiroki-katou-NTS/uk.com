5f82325bc1a1469f20975376738062a9ba94b31c
thuyetnguyen <thuyetdinh9xhn@gmail.com>
2018-04-26 09:45:03 +0700

    Screen A & C

---
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmf/004/c/cmf004.c.vm.ts
@@ -0,0 +1,10 @@
+module nts.uk.com.view.cmf004.c {
+    export module viewmodel {
+        export class ScreenModel {
+            constructor() {
+            }
+            
+           
+        }
+    }
+}
