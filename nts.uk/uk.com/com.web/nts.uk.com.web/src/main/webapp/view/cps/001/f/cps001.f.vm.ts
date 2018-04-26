c94e51b2fc58bd8dfffef9beb6b6f4dca873d9fb
NSVN-TruongVanLai <NSVN-TruongVanLai@users.noreply.github.com>
2018-04-26 18:04:48 +0700

    fix bug 95032

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/001/f/cps001.f.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/001/f/cps001.f.vm.ts
@@ -39,12 +39,12 @@ module cps001.f.vm {
             self.fileId = ko.observable("");
             self.filename = ko.observable("");
             self.fileInfo = ko.observable(null);
-            self.accept = ko.observableArray([""]);
+            self.accept = ko.observableArray([]);
             self.textId = ko.observable("CPS001_71");
             self.asLink = ko.observable(true);
             self.enable = ko.observable(true);
             self.fileSize = ko.observable("");
-            self.stereoType = ko.observable("avatarfile");
+            self.stereoType = ko.observable("documentfile");
             self.uploadFinished = (fileInfo) => {
                 console.log("change");
                 console.log(fileInfo);
