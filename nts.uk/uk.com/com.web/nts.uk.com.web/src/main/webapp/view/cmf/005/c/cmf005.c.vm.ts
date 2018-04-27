15d28be97915e54dcba46ee8ec2cbc51bd6dca90
thuongtv <thuongtv@2nf.com.vn>
2018-04-27 17:49:54 +0700

    Modify System type enum

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmf/005/c/cmf005.c.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmf/005/c/cmf005.c.vm.ts
@@ -11,55 +11,44 @@ module nts.uk.com.view.cmf005.c.viewmodel {
 
     export class ScreenModel {
         //Dropdownlist contain System data
-        systemList: KnockoutObservableArray<SystemModel> =ko.observableArray([]);
+        systemList: KnockoutObservableArray<SystemModel> = ko.observableArray([]);
         systemName: KnockoutObservable<string>;
         currentSystemCode: KnockoutObservable<number>
         selectedSystemCode: KnockoutObservable<number>;
 
         //C5
-        listCategorys: KnockoutObservableArray<CategoryModel>;
+        listCategorysDelete: KnockoutObservableArray<CategoryModel> = ko.observableArray([]);
         swapColumns: KnockoutObservableArray<gridColumn>;
-        currentListCategorys: KnockoutObservableArray<any>;
+        listCategorysChose: KnockoutObservableArray<CategoryModel> = ko.observableArray([]);
 
         /* screen */
         constructor() {
-            //Set System data to dropdownlist
-            //            self.systemList = ko.observableArray([
-            //                new SystemModel(0, getText('CMF005_170')),
-            //                new SystemModel(1, getText('CMF005_171')),
-            //                new SystemModel(2, getText('CMF005_172')),
-            //                nel(3, getText('CMF005_173'))
-            //            ]);
-
-            self.systemName = ko.observable('');
-            self.currentSystemCode = ko.observable(0);
-            self.selectedSystemCode = ko.observable(0);
+            this.systemName = ko.observable('');
+            this.currentSystemCode = ko.observable(0);
+            this.selectedSystemCode = ko.observable(0);
 
             //C5
-            this.listCategorys = ko.observableArray([]);
-
             // set tam sau lay tu tang apdapter
             var array = [];
             for (var i = 1; i < 10; i++) {
                 array.push(new CategoryModel('0000' + i, 'cccccc' + i));
             }
-            this.listCategorys(array);
+            this.listCategorysDelete(array);
 
             this.swapColumns = ko.observableArray([
                 { headerText: getText('CMF005_19'), key: 'categoryId', width: 100 },
                 { headerText: getText('CMF005_20'), key: 'categoryName', width: 150 }
             ]);
 
-            this.currentListCategorys = ko.observableArray([]);
         }
 
         // init screen
         start(): JQueryPromise<any> {
             let self = this,
-                dfd = $.Deferred(),systemList = self.systemList;
+                dfd = $.Deferred(), systemList = self.systemList;
 
             service.getListSystemType().done((itemList: Array<SystemModel>) => {
-               
+
                 // in case number of RoleSet is greater then 0
                 if (itemList && itemList.length > 0) {
                     systemList(itemList);
@@ -71,37 +60,56 @@ module nts.uk.com.view.cmf005.c.viewmodel {
             return dfd.promise();
         }
 
-        //Load system type
-        loadSystemTypeList(): JQueryPromise<any> {
-            let self = this,
-                dfd = $.Deferred();
-
-            return dfd.resolve();
-        }
-
+      
         //Load category
         loadCategoryList(): JQueryPromise<any> {
             let self = this,
-                dfd = $.Deferred();
-
+                dfd = $.Deferred(), listCategorysDelete = self.listCategorysDelete,
+                listCategorysChose =self.listCategorysChose,
+                searchText='';
+            // get list category delete
+            let listCategoryId = [];
+            for (let categoryModel in listCategorysChose) {
+                console.log(categoryModel.categoryId); // "species"
+                listCategoryId.push(categoryModel.categoryId);
+            }
+            
+//            service.getCategorysDeletion(self.selectedSystemCode,searchText,listCategoryId).done((itemList: Array<CategoryModel>) => {
+//                console.log("aaaaaa:" + itemList.length);
+//                // in case number of RoleSet is greater then 0
+//                if (itemList && itemList.length > 0) {
+//                    listCategorysDelete(itemList);
+//                }
+//
+//            }).always(() => {
+//                dfd.resolve();
+//            });
             return dfd.resolve();
         }
 
-
-
         // Return code / name of selected line
-        selectConvertCode() {
-            //            var self = this;
-            //            let codeConvert = new model.AcceptanceCodeConvert("", "", 0);
+        selectedListCategory() {
+            var self = this;
+            let listCategoryChose = self.listCategorysChose;
+            let listCategorysDelete = self.listCategorysDelete;
+            
+            for (let categoryModel in listCategorysDelete) {
+                console.log("ố ố:"+categoryModel.categoryId); // "ố ố"
+                
+            }
+            console.log("systemList:" + self.systemList.length);
+            console.log("listCategorysDelete" + listCategorysDelete.length);
+            console.log("listCategoryChose:" + listCategoryChose.length);
+
             //            if (!_.isEqual(self.selectedConvertCode(), "")){
             //                codeConvert = _.find(ko.toJS(self.listConvertCode), (x: model.AcceptanceCodeConvert) => x.dispConvertCode == self.selectedConvertCode());
             //            }
             //            // 選択された行のコード/名称を返す
             //            setShared("cmf005kOutput", { selectedConvertCodeShared: codeConvert});
-            nts.uk.ui.windows.close();
+            //            nts.uk.ui.windows.close();
         }
         //Cancel and exit
-        cancelSelectConvertCode() {
+        cancelSelectCategory() {
             nts.uk.ui.windows.close();
         }
     }
