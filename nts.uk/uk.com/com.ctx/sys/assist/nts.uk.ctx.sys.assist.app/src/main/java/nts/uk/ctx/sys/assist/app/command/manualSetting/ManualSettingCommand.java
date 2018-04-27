add98da1e98163ae940780cf1cc69f0e6d4eb059
Nam Bean <nambean90@gmail.com>
2018-04-27 15:46:18 +0700

    Update server sau khi them List<TargetEmployees>, List<TargetCategory>

--- nts.uk/uk.com/com.ctx/sys/assist/nts.uk.ctx.sys.assist.app/src/main/java/nts/uk/ctx/sys/assist/app/command/manualSetting/ManualSettingCommand.java
+++ nts.uk/uk.com/com.ctx/sys/assist/nts.uk.ctx.sys.assist.app/src/main/java/nts/uk/ctx/sys/assist/app/command/manualSetting/ManualSettingCommand.java
@@ -3,17 +3,17 @@
  */
 package nts.uk.ctx.sys.assist.app.command.manualSetting;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
 
 import lombok.Value;
 import nts.arc.time.GeneralDate;
 import nts.arc.time.GeneralDateTime;
-import nts.uk.ctx.sys.assist.dom.storage.Explanation;
-import nts.uk.ctx.sys.assist.dom.storage.FileCompressionPassword;
 import nts.uk.ctx.sys.assist.dom.storage.ManualSetOfDataSave;
-import nts.uk.ctx.sys.assist.dom.storage.SaveSetName;
-import nts.uk.ctx.sys.assist.dom.storage.Year;
-import nts.uk.shr.com.enumcommon.NotUseAtr;
+import nts.uk.ctx.sys.assist.dom.storage.TargetCategory;
+import nts.uk.ctx.sys.assist.dom.storage.TargetEmployees;
+
 
 /**
  * @author nam.lh
@@ -41,11 +41,14 @@ public class ManualSettingCommand {
 	private int presenceOfEmployee;
 	private int identOfSurveyPre;
 	private String practitioner;
+	List<TargetEmployees> employees;
+	List<TargetCategory> category;
+	private int selectionTarget;
 
-	public ManualSetOfDataSave toDomain(String id) {
+	public ManualSetOfDataSave toDomain(String storeProcessingId) {
 		return new ManualSetOfDataSave(cid, storeProcessingId, systemType, passwordAvailability, saveSetName,
 				referenceDate, compressedPassword, executionDateAndTime, daySaveEndDate, daySaveStartDate,
 				monthSaveEndDate, monthSaveStartDate, suppleExplanation, endYear, startYear, presenceOfEmployee,
-				identOfSurveyPre, practitioner);
+				identOfSurveyPre, practitioner, employees, category);
 	}
 }
