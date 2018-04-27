a1d466b6bde6c6bb7270d694e9304b4bb33b5f27
YenNH <yennh@newwave.vn>
2018-04-27 17:40:20 +0700

    [KWR001][A] screen A

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/kwr/001/a/kwr001.a.service.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/kwr/001/a/kwr001.a.service.ts
@@ -1,5 +1,33 @@
 module nts.uk.at.view.kwr001.a {
     export module service {
+        var paths = {
+           checkScheduleBatchCorrectSettingSave: ""
+        }
         
+        /**
+         * call service check ScheduleBatchCorrectSetting save
+         */
+        export function checkScheduleBatchCorrectSettingSave(dto: model.ScheduleBatchCorrectSettingSave): JQueryPromise<void> {
+            return nts.uk.request.ajax('at', paths.checkScheduleBatchCorrectSettingSave, dto);
+        }
+        
+        
+        export module model {
+
+            export interface ScheduleBatchCorrectSettingSave {
+                worktypeCode: string;
+
+                employeeId: string;
+
+                endDate: Date;
+
+                startDate: Date;
+                
+                worktimeCode: string;
+
+                employeeIds: string[];
+            }
+            
+        }
     }
 }
--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/kwr/001/a/kwr001.a.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/kwr/001/a/kwr001.a.vm.ts
@@ -1,7 +1,14 @@
 module nts.uk.at.view.kwr001.a {
+    import ScheduleBatchCorrectSettingSave = service.model.ScheduleBatchCorrectSettingSave;
+    import Ccg001ReturnedData = nts.uk.com.view.ccg.share.ccg.service.model.Ccg001ReturnedData;
+    import EmployeeSearchDto = nts.uk.com.view.ccg.share.ccg.service.model.EmployeeSearchDto;
+    import GroupOption = nts.uk.com.view.ccg.share.ccg.service.model.GroupOption;
+    import ComponentOption = kcp.share.list.ComponentOption;
+    
     export module viewmodel {
         export class ScreenModel {
             data: KnockoutObservable<number>;
+            roundingRules: KnockoutObservable<any>;
             
             // A1_6
             enable: KnockoutObservable<boolean>;
@@ -11,7 +18,6 @@ module nts.uk.at.view.kwr001.a {
             endDateString: KnockoutObservable<string>;
             
             // switch button A6_2
-            roundingRules: KnockoutObservableArray<any>;
             selectedRuleCode: any;
             
             // dropdownlist A7_3
@@ -19,6 +25,97 @@ module nts.uk.at.view.kwr001.a {
             selectedCode: KnockoutObservable<string>;
             isEnable: KnockoutObservable<boolean>;
             isEditable: KnockoutObservable<boolean>;
+            
+            ccg001ComponentOption: GroupOption;
+            systemTypes: KnockoutObservableArray<any>;
+
+            // Options
+            isQuickSearchTab: KnockoutObservable<boolean>;
+            isAdvancedSearchTab: KnockoutObservable<boolean>;
+            isAllReferableEmployee: KnockoutObservable<boolean>;
+            isOnlyMe: KnockoutObservable<boolean>;
+            isEmployeeOfWorkplace: KnockoutObservable<boolean>;
+            isEmployeeWorkplaceFollow: KnockoutObservable<boolean>;
+            isMutipleCheck: KnockoutObservable<boolean>;
+            isSelectAllEmployee: KnockoutObservable<boolean>;
+            periodStartDate: KnockoutObservable<moment.Moment>;
+            periodEndDate: KnockoutObservable<moment.Moment>;
+            baseDate: KnockoutObservable<moment.Moment>;
+            selectedEmployee: KnockoutObservableArray<EmployeeSearchDto>;
+            showEmployment: KnockoutObservable<boolean>; // 雇用条件
+            showWorkplace: KnockoutObservable<boolean>; // 職場条件
+            showClassification: KnockoutObservable<boolean>; // 分類条件
+            showJobTitle: KnockoutObservable<boolean>; // 職位条件
+            showWorktype: KnockoutObservable<boolean>; // 勤種条件
+            inService: KnockoutObservable<boolean>; // 在職区分
+            leaveOfAbsence: KnockoutObservable<boolean>; // 休職区分
+            closed: KnockoutObservable<boolean>; // 休業区分
+            retirement: KnockoutObservable<boolean>; // 退職区分
+            systemType: KnockoutObservable<number>;
+            showClosure: KnockoutObservable<boolean>; // 就業締め日利用
+            showBaseDate: KnockoutObservable<boolean>; // 基準日利用
+            showAllClosure: KnockoutObservable<boolean>; // 全締め表示
+            showPeriod: KnockoutObservable<boolean>; // 対象期間利用
+            periodFormatYM: KnockoutObservable<boolean>; // 対象期間精度
+            
+            selectedImplementAtrCode: KnockoutObservable<number>;
+            checkReCreateAtrAllCase: KnockoutObservable<boolean>;
+            checkReCreateAtrOnlyUnConfirm: KnockoutObservable<boolean>;
+            checkProcessExecutionAtrRebuild: KnockoutObservable<boolean>;
+            checkProcessExecutionAtrReconfig: KnockoutObservable<boolean>;
+            checkCreateMethodAtrPersonalInfo: KnockoutObservable<boolean>;
+            checkCreateMethodAtrPatternSchedule: KnockoutObservable<boolean>;
+            checkCreateMethodAtrCopyPastSchedule: KnockoutObservable<boolean>;
+            resetWorkingHours: KnockoutObservable<boolean>;
+            resetDirectLineBounce: KnockoutObservable<boolean>;
+            resetMasterInfo: KnockoutObservable<boolean>;
+            resetTimeChildCare: KnockoutObservable<boolean>;
+            resetAbsentHolidayBusines: KnockoutObservable<boolean>;
+            resetTimeAssignment: KnockoutObservable<boolean>;
+            workTypeInfo: KnockoutObservable<string>;
+            workTypeCode: KnockoutObservable<string>;
+            workTimeInfo: KnockoutObservable<string>;
+            workTimeCode: KnockoutObservable<string>;
+            scheduleBatchCorrectSettingInfo: KnockoutObservable<ScheduleBatchCorrectSettingSave>;
+            
+            periodDate: KnockoutObservable<any>;
+            copyStartDate: KnockoutObservable<Date>;
+
+            lstLabelInfomation: KnockoutObservableArray<string>;
+            infoCreateMethod: KnockoutObservable<string>;
+            infoPeriodDate: KnockoutObservable<string>;
+            lengthEmployeeSelected: KnockoutObservable<string>;
+            
+            // Employee tab
+            lstPersonComponentOption: any;
+            selectedEmployeeCode: KnockoutObservableArray<string>;
+            employeeName: KnockoutObservable<string>;
+            employeeList: KnockoutObservableArray<UnitModel>;
+            alreadySettingPersonal: KnockoutObservableArray<UnitAlreadySettingModel>;
+            ccgcomponentPerson: GroupOption;
+            
+            // kcp005
+            bySelectedCode: KnockoutObservable<string>;
+            isAlreadySetting: KnockoutObservable<boolean>;
+            isDialog: KnockoutObservable<boolean>;
+            isShowNoSelectionItem: KnockoutObservable<boolean>;
+            isMultiSelect: KnockoutObservable<boolean>;
+            isShowWorkPlaceName: KnockoutObservable<boolean>;
+            isShowSelectAllButton: KnockoutObservable<boolean>;
+    
+            multiSelectedCode: KnockoutObservableArray<string>;
+            multiBySelectedCode: KnockoutObservableArray<string>;
+    
+            listComponentOption: any;
+            alreadySettingList: KnockoutObservableArray<UnitAlreadySettingModel>;
+    
+            hasSelectedEmp: KnockoutObservable<boolean>;
+    
+            selectionTypeList: KnockoutObservableArray<any>;
+            selectedType: KnockoutObservable<number>;
+            selectionOption: KnockoutObservableArray<any>;
+            selectedOption: KnockoutObservable<number>;
+            jsonData: KnockoutObservable<string>;
             
             constructor() {
                 var self = this;
@@ -52,10 +149,35 @@ module nts.uk.at.view.kwr001.a {
                     new ItemModel('1', nts.uk.resource.getText("#KWR001_38")),
                     new ItemModel('2', nts.uk.resource.getText("#KWR001_39"))
                 ]);
+                
+                // Initial listComponentOption
+                self.listComponentOption = {
+                    isShowAlreadySet: true,
+                    isMultiSelect: self.isMultiSelect(),
+                    listType: ListType.EMPLOYEE,
+                    employeeInputList: self.employeeList,
+                    selectType: self.selectedType(),
+                    selectedCode: self.bySelectedCode,
+                    isDialog: self.isDialog(),
+                    isShowNoSelectRow: self.isShowNoSelectionItem(),
+                    alreadySettingList: self.alreadySettingList,
+                    isShowWorkPlaceName: self.isShowWorkPlaceName(),
+                    isShowSelectAllButton: self.isShowSelectAllButton(),
+                    maxRows: 12
+                };
+                
+                // initial ccg options
+                self.setDefaultCcg001Option();
+                
+                // Init component.
+                self.reloadCcg001();
+                self.reloadComponent();
         
                 self.selectedCode = ko.observable('1');
                 self.isEnable = ko.observable(true);
                 self.isEditable = ko.observable(true);
+                self.periodDate = ko.observable({});
+                
             }
             
             public startPage(): JQueryPromise<void>  {
@@ -77,6 +199,179 @@ module nts.uk.at.view.kwr001.a {
                     nts.uk.ui.windows.getShared('KWR001_C');
                 });
             }
+            
+            /**
+             * Set default ccg001 options
+             */
+            public setDefaultCcg001Option(): void {
+                let self = this;
+                self.isQuickSearchTab = ko.observable(true);
+                self.isAdvancedSearchTab = ko.observable(true);
+                self.isAllReferableEmployee = ko.observable(true);
+                self.isOnlyMe = ko.observable(true);
+                self.isEmployeeOfWorkplace = ko.observable(true);
+                self.isEmployeeWorkplaceFollow = ko.observable(true);
+                self.isMutipleCheck = ko.observable(true);
+                self.isSelectAllEmployee = ko.observable(true);
+                self.baseDate = ko.observable(moment());
+                self.periodStartDate = ko.observable(moment());
+                self.periodEndDate = ko.observable(moment());
+                self.showEmployment = ko.observable(true); // 雇用条件
+                self.showWorkplace = ko.observable(true); // 職場条件
+                self.showClassification = ko.observable(true); // 分類条件
+                self.showJobTitle = ko.observable(true); // 職位条件
+                self.showWorktype = ko.observable(true); // 勤種条件
+                self.inService = ko.observable(true); // 在職区分
+                self.leaveOfAbsence = ko.observable(true); // 休職区分
+                self.closed = ko.observable(true); // 休業区分
+                self.retirement = ko.observable(true); // 退職区分
+                self.systemType = ko.observable(1);
+                self.showClosure = ko.observable(true); // 就業締め日利用
+                self.showBaseDate = ko.observable(true); // 基準日利用
+                self.showAllClosure = ko.observable(true); // 全締め表示
+                self.showPeriod = ko.observable(true); // 対象期間利用
+                self.periodFormatYM = ko.observable(false); // 対象期間精度
+            }
+
+            /**
+             * Reload component CCG001
+             */
+            public reloadCcg001(): void {
+                let self = this;
+                if ($('.ccg-sample-has-error').ntsError('hasError')) {
+                    return;
+                }
+                // clear ccg001 errors
+                $('#inp_baseDate').ntsError('clear');
+                $('#inp-period-startYMD').ntsError('clear');
+                $('#inp-period-endYMD').ntsError('clear');
+                $('#inp-period-startYM').ntsError('clear');
+                $('#inp-period-endYM').ntsError('clear');
+                $('#ccg001-partg-start').ntsError('clear');
+                $('#ccg001-partg-end').ntsError('clear');
+                
+                if (!self.showBaseDate() && !self.showClosure() && !self.showPeriod()){
+                    nts.uk.ui.dialog.alertError("Base Date or Closure or Period must be shown!" );
+                    return;
+                }
+                self.ccg001ComponentOption = {
+                    /** Common properties */
+                    systemType: self.systemType(), // システム区分
+                    showEmployeeSelection: self.isSelectAllEmployee(), // 検索タイプ
+                    showQuickSearchTab: self.isQuickSearchTab(), // クイック検索
+                    showAdvancedSearchTab: self.isAdvancedSearchTab(), // 詳細検索
+                    showBaseDate: self.showBaseDate(), // 基準日利用
+                    showClosure: self.showClosure(), // 就業締め日利用
+                    showAllClosure: self.showAllClosure(), // 全締め表示
+                    showPeriod: self.showPeriod(), // 対象期間利用
+                    periodFormatYM: self.periodFormatYM(), // 対象期間精度
+
+                    /** Required parameter */
+                    baseDate: self.baseDate().toISOString(), // 基準日
+                    periodStartDate: self.periodStartDate().toISOString(), // 対象期間開始日
+                    periodEndDate: self.periodEndDate().toISOString(), // 対象期間終了日
+                    inService: self.inService(), // 在職区分
+                    leaveOfAbsence: self.leaveOfAbsence(), // 休職区分
+                    closed: self.closed(), // 休業区分
+                    retirement: self.retirement(), // 退職区分
+
+                    /** Quick search tab options */
+                    showAllReferableEmployee: self.isAllReferableEmployee(), // 参照可能な社員すべて
+                    showOnlyMe: self.isOnlyMe(), // 自分だけ
+                    showSameWorkplace: self.isEmployeeOfWorkplace(), // 同じ職場の社員
+                    showSameWorkplaceAndChild: self.isEmployeeWorkplaceFollow(), // 同じ職場とその配下の社員
+
+                    /** Advanced search properties */
+                    showEmployment: self.showEmployment(), // 雇用条件
+                    showWorkplace: self.showWorkplace(), // 職場条件
+                    showClassification: self.showClassification(), // 分類条件
+                    showJobTitle: self.showJobTitle(), // 職位条件
+                    showWorktype: self.showWorktype(), // 勤種条件
+                    isMutipleCheck: self.isMutipleCheck(), // 選択モード
+
+                    /** Return data */
+                    returnDataFromCcg001: function(data: Ccg001ReturnedData) {
+                        self.selectedEmployee(data.listEmployee);
+                        self.applyKCP005ContentSearch(data.listEmployee);
+                    }
+                }
+
+                // Start component
+                $('#ccgcomponent').ntsGroupComponent(self.ccg001ComponentOption);
+            }
+            
+            /**
+            * apply ccg001 search data to kcp005
+            */
+            public applyKCP005ContentSearch(dataList: EmployeeSearchDto[]): void {
+                var self = this;
+                self.employeeList([]);
+                var employeeSearchs: UnitModel[] = [];
+                for (var employeeSearch of dataList) {
+                    var employee: UnitModel = {
+                        code: employeeSearch.employeeCode,
+                        name: employeeSearch.employeeName,
+                        workplaceName: employeeSearch.workplaceName
+                    };
+                    employeeSearchs.push(employee);
+                }
+                self.employeeList(employeeSearchs);
+                self.lstPersonComponentOption = {
+                    isShowAlreadySet: false,
+                    isMultiSelect: true,
+                    listType: ListType.EMPLOYEE,
+                    employeeInputList: self.employeeList,
+                    selectType: SelectType.SELECT_BY_SELECTED_CODE,
+                    selectedCode: self.selectedEmployeeCode,
+                    isDialog: false,
+                    isShowNoSelectRow: false,
+                    alreadySettingList: self.alreadySettingPersonal,
+                    isShowWorkPlaceName: true,
+                    isShowSelectAllButton: true,
+                    maxWidth: 550,
+                    maxRows: 15
+                };
+            }
+            // Reload component Method
+            private reloadComponent() {
+                var self = this;
+                self.listComponentOption.isShowAlreadySet = self.isAlreadySetting();
+                self.listComponentOption.listType = ListType.EMPLOYEE;
+                self.listComponentOption.employeeInputList = self.employeeList;
+                self.listComponentOption.isDialog = self.isDialog();
+                self.listComponentOption.isShowNoSelectRow = self.isShowNoSelectionItem();
+                self.listComponentOption.alreadySettingList = self.alreadySettingList;
+                self.listComponentOption.isMultiSelect = self.isMultiSelect();
+                self.listComponentOption.selectType = self.selectedType();
+                self.listComponentOption.isShowWorkPlaceName = self.isShowWorkPlaceName();
+                self.listComponentOption.isShowSelectAllButton = self.isShowSelectAllButton();
+    
+                $('#component-items-list').ntsListComponent(self.listComponentOption).done(function() {
+                    $('#component-items-list').focusComponent();
+                });
+                
+            }
+        }
+        
+        export class ListType {
+            static EMPLOYMENT = 1;
+            static Classification = 2;
+            static JOB_TITLE = 3;
+            static EMPLOYEE = 4;
+        }
+
+        export interface UnitModel {
+            code: string;
+            name?: string;
+            workplaceName?: string;
+            isAlreadySetting?: boolean;
+        }
+
+        export class SelectType {
+            static SELECT_BY_SELECTED_CODE = 1;
+            static SELECT_ALL = 2;
+            static SELECT_FIRST_ITEM = 3;
+            static NO_SELECT = 4;
         }
         
         class ItemModel {
