1665c503c595deda23e3cd6cf8764a093e5d345f
Vu Thang Loi <loi_vt@nittsusystem-vn.com>
2018-04-27 10:28:57 +0700

    update fix respond 133, fix requets list 190,191

--- nts.uk/uk.com/wk.ctx/nts.uk.ctx.workflow.infra/src/main/java/nts/uk/ctx/workflow/infra/entity/approverstatemanagement/WwfdtApproverStateNew.java
+++ nts.uk/uk.com/wk.ctx/nts.uk.ctx.workflow.infra/src/main/java/nts/uk/ctx/workflow/infra/entity/approverstatemanagement/WwfdtApproverStateNew.java
@@ -2,9 +2,6 @@ package nts.uk.ctx.workflow.infra.entity.approverstatemanagement;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.PrimaryKeyJoinColumn;
-import javax.persistence.PrimaryKeyJoinColumns;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -12,29 +9,16 @@ import lombok.Builder;
 import lombok.NoArgsConstructor;
 import nts.uk.ctx.workflow.dom.approverstatemanagement.ApproverState;
 import nts.uk.shr.infra.data.entity.UkJpaEntity;
-/**
- * 
- * @author Doan Duy Hung
- *
- */
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name="WWFDT_APPROVER_STATE")
 @Builder
-public class WwfdtApproverState extends UkJpaEntity {
-	
+public class WwfdtApproverStateNew extends UkJpaEntity {
 	@EmbeddedId
 	public WwfdpApproverStatePK wwfdpApproverStatePK;
 	
-	@ManyToOne
-	@PrimaryKeyJoinColumns({
-		@PrimaryKeyJoinColumn(name="ROOT_STATE_ID",referencedColumnName="ROOT_STATE_ID"),
-		@PrimaryKeyJoinColumn(name="PHASE_ORDER",referencedColumnName="PHASE_ORDER"),
-		@PrimaryKeyJoinColumn(name="FRAME_ORDER",referencedColumnName="FRAME_ORDER")
-	})
-	private WwfdtApprovalFrame wwfdtApprovalFrame;
-
 	@Override
 	protected Object getKey() {
 		return wwfdpApproverStatePK;
