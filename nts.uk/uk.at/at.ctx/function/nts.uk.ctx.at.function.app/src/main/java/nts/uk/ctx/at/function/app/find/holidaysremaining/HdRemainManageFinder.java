5471dd7b11467c6dc65ffe08276892cf2f8af14c
3S_dat.qt <dat.qt@3si.vn>
2018-05-02 12:52:25 +0700

    KDR001: fix merge error

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.app/src/main/java/nts/uk/ctx/at/function/app/find/holidaysremaining/HdRemainManageFinder.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.app/src/main/java/nts/uk/ctx/at/function/app/find/holidaysremaining/HdRemainManageFinder.java
@@ -1,4 +1,4 @@
-package nts.uk.ctx.at.function.app.find.holidaysremaining;
+*package nts.uk.ctx.at.function.app.find.holidaysremaining;
 
 //import java.util.Calendar;
 import java.util.List;
@@ -7,6 +7,11 @@ import java.util.stream.Collectors;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+
+import nts.arc.time.GeneralDate;
+import nts.arc.time.YearMonth;
+import nts.uk.ctx.at.function.dom.adapter.employment.EmploymentAdapter;
+import nts.uk.ctx.at.function.dom.adapter.employment.EmploymentHistoryImported;
 /*
 import nts.arc.time.GeneralDate;
 import nts.arc.time.YearMonth;
@@ -19,6 +24,12 @@ import nts.uk.ctx.at.function.dom.holidaysremaining.HolidaysRemainingManagement;
 import nts.uk.ctx.at.function.dom.holidaysremaining.PermissionOfEmploymentForm;
 import nts.uk.ctx.at.function.dom.holidaysremaining.repository.HolidaysRemainingManagementRepository;
 import nts.uk.ctx.at.function.dom.holidaysremaining.repository.PermissionOfEmploymentFormRepository;
+import nts.uk.ctx.at.shared.dom.holidaymanagement.publicholiday.configuration.PublicHolidaySettingRepository;
+import nts.uk.ctx.at.shared.dom.workrule.closure.Closure;
+import nts.uk.ctx.at.shared.dom.workrule.closure.ClosureEmployment;
+import nts.uk.ctx.at.shared.dom.workrule.closure.ClosureEmploymentRepository;
+import nts.uk.ctx.at.shared.dom.workrule.closure.ClosureRepository;
+import nts.uk.ctx.at.shared.dom.workrule.closure.service.ClosureService;
 /*import nts.uk.ctx.at.function.dom.holidaysremaining.repository.PermissionOfEmploymentFormRepository;
 import nts.uk.ctx.at.shared.dom.holidaymanagement.publicholiday.configuration.PublicHolidaySettingRepository;
 import nts.uk.ctx.at.shared.dom.workrule.closure.ClosureEmployment;
@@ -37,18 +48,17 @@ public class HdRemainManageFinder {
 
 	@Inject
 	private HolidaysRemainingManagementRepository hdRemainingManagementRepo;
-	/*
-	 * @Inject private ClosureEmploymentRepository closureEmploymentRepository;
-	 * 
-	 * @Inject private EmploymentAdapter employmentAdapter;
-	 * 
-	 * @Inject private PublicHolidaySettingRepository
-	 * publicHolidaySettingRepository;
-	 * 
-	 * @Inject private ClosureService closureService;
-	 * 
-	 * @Inject private ClosureRepository closureRepository;
-	 */
+	
+	@Inject private ClosureEmploymentRepository closureEmploymentRepository;
+	 
+	@Inject private EmploymentAdapter employmentAdapter;
+	 
+	@Inject private PublicHolidaySettingRepository	publicHolidaySettingRepository;
+
+	@Inject private ClosureService closureService;
+ 
+	@Inject private ClosureRepository closureRepository;
+	
 	@Inject
 	private PermissionOfEmploymentFormRepository permissionOfEmploymentFormRepository;
 
@@ -78,30 +88,30 @@ public class HdRemainManageFinder {
 	 * @return
 	 */
 	public DateHolidayRemainingDto getDate() {
-		DatePeriod datePeriod = null;
 		String companyId = AppContexts.user().companyId();
 		String employeeId = AppContexts.user().employeeId();
-		/*
-		 * GeneralDate baseDate = GeneralDate.today(); //
-		 * Imported（就業）「所属雇用履歴」を取得する Optional<EmploymentHistoryImported>
-		 * employmentHisOptional =
-		 * this.employmentAdapter.getEmpHistBySid(companyId, employeeId,
-		 * baseDate); if (employmentHisOptional.isPresent()) { //
-		 * 対応するドメインモデル「雇用に紐づく就業締め」を取得する (Lấy domain 「雇用に紐づく就業締め」)
-		 * Optional<ClosureEmployment> closureEmployment =
-		 * closureEmploymentRepository.findByEmploymentCD(companyId,
-		 * employmentHisOptional.get().getEmploymentCode()); //
-		 * アルゴリズム「当月の年月を取得する」を実行する (thực hiện thuật toán 「当月の年月を取得する」) YearMonth
-		 * ym = closureRepository.findById(companyId,
-		 * closureEmployment.get().getClosureId()).get().getClosureMonth().
-		 * getProcessingYm(); // 当月の期間を算出する (Tính thời gian của tháng này 当月)
-		 * datePeriod =
-		 * closureService.getClosurePeriod(closureEmployment.get().getClosureId(
-		 * ).intValue(), ym); } return new
-		 * DateHolidayRemainingDto(datePeriod.start().toString(),
-		 * datePeriod.end().toString());
-		 */
-		return null;
+		DateHolidayRemainingDto retDateHoliday = null;
+		GeneralDate baseDate = GeneralDate.today(); //
+		//Imported（就業）「所属雇用履歴」を取得する 
+		Optional<EmploymentHistoryImported> employmentHisOptional = this.employmentAdapter.getEmpHistBySid(companyId, employeeId, baseDate);
+		if (!employmentHisOptional.isPresent()) {
+			return retDateHoliday;
+		}
+		// 対応するドメインモデル「雇用に紐づく就業締め」を取得する (Lấy domain 「雇用に紐づく就業締め」)
+		Optional<ClosureEmployment> closureEmployment =	closureEmploymentRepository.findByEmploymentCD(companyId,
+				employmentHisOptional.get().getEmploymentCode()); //
+		//アルゴリズム「当月の年月を取得する」を実行する (thực hiện thuật toán 「当月の年月を取得する」)
+		if (!closureEmployment.isPresent()) {
+			return retDateHoliday;
+		}
+		
+		Optional<Closure> optClosure = closureRepository.findById(companyId, closureEmployment.get().getClosureId());
+		if (!optClosure.isPresent()) {
+			return retDateHoliday;
+		}
+		YearMonth ym = optClosure.get().getClosureMonth().getProcessingYm(); // 当月の期間を算出する (Tính thời gian của tháng này 当月)
+		DatePeriod datePeriod = closureService.getClosurePeriod(closureEmployment.get().getClosureId().intValue(), ym);
+		return new DateHolidayRemainingDto(datePeriod.start().toString(), datePeriod.end().toString());
 	}
 
 	/**
