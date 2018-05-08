8cf141ad78ffa15aee83d9f5080a05aaf57a22a6
Nam Bean <nambean90@gmail.com>
2018-05-08 16:11:59 +0700

    general csv

--- nts.uk/uk.com/com.ctx/sys/assist/nts.uk.ctx.sys.assist.dom/src/main/java/nts/uk/ctx/sys/assist/dom/storage/SaveTargetCsvRepository.java
+++ nts.uk/uk.com/com.ctx/sys/assist/nts.uk.ctx.sys.assist.dom/src/main/java/nts/uk/ctx/sys/assist/dom/storage/SaveTargetCsvRepository.java
@@ -3,10 +3,12 @@
  */
 package nts.uk.ctx.sys.assist.dom.storage;
 
+import java.util.List;
+
 /**
  * @author nam.lh
  *
  */
 public interface SaveTargetCsvRepository {
-
+	List<SaveTargetCsv> getSaveTargetCsvById(String storeProcessingId);
 }
