fb4c76a23f91788b71b5b3b8aa9af6d0f56b615e
NSVN-PhamVanDan <dan_pv@nittsusystem-vn.com>
2018-05-08 14:45:48 +0700

    create ws, app, infra

---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/find/stamp/stampcardedit/StampCardEditDto.java
@@ -0,0 +1,22 @@
+package nts.uk.ctx.at.record.app.find.stamp.stampcardedit;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import nts.uk.ctx.at.record.dom.stamp.card.stamcardedit.StampCardEditMethod;
+import nts.uk.ctx.at.record.dom.stamp.card.stamcardedit.StampCardEditing;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StampCardEditDto {
+
+	private int digitsNumber;
+
+	private StampCardEditMethod method;
+
+	public static StampCardEditDto createFromDomain(StampCardEditing domain) {
+		return new StampCardEditDto(domain.getDigitsNumber(), domain.getMethod());
+	}
+
+}
