200a60dbd1ff524280049bad24da748484486600
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2018-05-09 17:44:03 +0700

    SPR: fix service 1 menu code 5

--- nts.uk/uk.com/pub/nts.uk.pub.spr/src/main/java/nts/uk/pub/spr/login/paramcheck/LoginParamCheck.java
+++ nts.uk/uk.com/pub/nts.uk.pub.spr/src/main/java/nts/uk/pub/spr/login/paramcheck/LoginParamCheck.java
@@ -49,7 +49,7 @@ public interface LoginParamCheck {
 	 * パラメータチェック（日別実績の確認）
 	 * @param date
 	 */
-	public void checkParamConfirmDaily(String date);
+	public String checkParamConfirmDaily(String employeeCD, String date);
 	
 	/**
 	 * パラメータチェック（残業申請確認）
