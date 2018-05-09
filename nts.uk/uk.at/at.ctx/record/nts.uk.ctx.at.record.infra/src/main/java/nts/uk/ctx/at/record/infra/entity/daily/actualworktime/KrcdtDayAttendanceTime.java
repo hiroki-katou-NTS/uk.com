ecf45c8a48bd22211c16304d840cd3ba881b1069
NTS\tin_dh <tin_dh@nittsusystem.co.jp>
2018-05-09 13:38:17 +0900

    revert totalworktime

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.infra/src/main/java/nts/uk/ctx/at/record/infra/entity/daily/actualworktime/KrcdtDayAttendanceTime.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.infra/src/main/java/nts/uk/ctx/at/record/infra/entity/daily/actualworktime/KrcdtDayAttendanceTime.java
@@ -319,16 +319,14 @@ public class KrcdtDayAttendanceTime extends UkJpaEntity implements Serializable 
 						 DeductionTotalTime.of(TimeWithCalculation.sameTime(new AttendanceTime(0)),
 					 			   			   TimeWithCalculation.sameTime(new AttendanceTime(0)),
 					 			   			   TimeWithCalculation.sameTime(new AttendanceTime(0))),
-						 ChildCareAttribute.CARE
-						)
-//				,
-//				new HolidayOfDaily(new AbsenceOfDaily(new AttendanceTime(0)), 
-//								   new TimeDigestOfDaily(new AttendanceTime(0),new AttendanceTime(0)), 
-//								   new YearlyReservedOfDaily(new AttendanceTime(0)), 
-//								   new SubstituteHolidayOfDaily(new AttendanceTime(0), new AttendanceTime(0)), 
-//								   new OverSalaryOfDaily(new AttendanceTime(0), new AttendanceTime(0)), 
-//								   new SpecialHolidayOfDaily(new AttendanceTime(0), new AttendanceTime(0)), 
-//								   new AnnualOfDaily(new AttendanceTime(0), new AttendanceTime(0)))
+						 ChildCareAttribute.CARE),
+				new HolidayOfDaily(new AbsenceOfDaily(new AttendanceTime(0)), 
+								   new TimeDigestOfDaily(new AttendanceTime(0),new AttendanceTime(0)), 
+								   new YearlyReservedOfDaily(new AttendanceTime(0)), 
+								   new SubstituteHolidayOfDaily(new AttendanceTime(0), new AttendanceTime(0)), 
+								   new OverSalaryOfDaily(new AttendanceTime(0), new AttendanceTime(0)), 
+								   new SpecialHolidayOfDaily(new AttendanceTime(0), new AttendanceTime(0)), 
+								   new AnnualOfDaily(new AttendanceTime(0), new AttendanceTime(0)))
 				);
 
 		// 日別実績の勤務実績時間
