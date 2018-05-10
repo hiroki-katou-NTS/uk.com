28704b992af4fe2df1f753e1346135fc7947eb06
NSVN-PhamVanDan <dan_pv@nittsusystem-vn.com>
2018-05-10 10:14:23 +0700

    remove get personId

--- nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.app/src/main/java/nts/uk/ctx/pereg/app/find/processor/ItemDefFinder.java
+++ nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.app/src/main/java/nts/uk/ctx/pereg/app/find/processor/ItemDefFinder.java
@@ -9,7 +9,6 @@ import java.util.stream.Collectors;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
-import nts.uk.ctx.bs.employee.dom.employee.mgndata.EmployeeDataMngInfoRepository;
 import nts.uk.ctx.pereg.app.find.common.MappingFactory;
 import nts.uk.ctx.pereg.dom.person.additemdata.category.EmInfoCtgDataRepository;
 import nts.uk.ctx.pereg.dom.person.additemdata.category.EmpInfoCtgData;
@@ -48,9 +47,6 @@ public class ItemDefFinder {
 	private LayoutingProcessor layoutingProcessor;
 	
 	@Inject
-	private EmployeeDataMngInfoRepository empRepo;
-	
-	@Inject
 	private PerInfoCategoryRepositoty perInfoCtgRepo;
 
 	@Inject
@@ -72,9 +68,6 @@ public class ItemDefFinder {
 		// app context
 		String contractCd = AppContexts.user().contractCode();
 		String companyId = AppContexts.user().companyId();
-
-		// get Person ID
-		query.setPersonId(empRepo.findByEmpId(query.getEmployeeId()).get().getPersonId());
 
 		// get category 
 		PersonInfoCategory perInfoCtg = perInfoCtgRepo.getPerInfoCategoryByCtgCD(query.getCategoryCode(), companyId).get();
