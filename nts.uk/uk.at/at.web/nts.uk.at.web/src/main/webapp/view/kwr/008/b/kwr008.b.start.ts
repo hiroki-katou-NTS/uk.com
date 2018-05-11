26d08d6504d26320f8648f8720541a4bb33c1ff6
HaiTN <hai.tn@3si.vn>
2018-05-11 17:42:12 +0700

    fix UI

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kwr/008/b/kwr008.b.start.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kwr/008/b/kwr008.b.start.ts
@@ -3,7 +3,7 @@ module nts.uk.at.view.kwr008.b {
         let screenModel = new nts.uk.at.view.kwr008.b.viewmodel.ScreenModel();
         screenModel.startPage().done(function(){
             __viewContext.bind(screenModel);
-            $('#table-output-items').ntsFixedTable({ height: 363, width: 1200 });    
+            $('#table-output-items').ntsFixedTable({ height: 385, width: 1200 });    
         });
     }); 
 }
