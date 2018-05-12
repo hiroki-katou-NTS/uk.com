b04270cd636b5ddf3f1ed6d214fafd4c181e4e1e
NSVN-TruongVanLai <NSVN-TruongVanLai@users.noreply.github.com>
2018-05-12 15:24:06 +0700

    entity StampCard

---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.infra/src/main/java/nts/uk/ctx/at/record/infra/entity/stamp/stampcard/KrcmtStamcard.java
@@ -0,0 +1,34 @@
+package nts.uk.ctx.at.record.infra.entity.stamp.stampcard;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import nts.arc.time.GeneralDate;
+import nts.uk.shr.infra.data.entity.UkJpaEntity;
+
+//@Entity
+//@Table(name = "KRCMT_STAMP_CARD")
+public class KrcmtStamcard extends  UkJpaEntity{
+	
+	@Id
+	@Column(name = "STAMP_CARD_ID")
+	public String cardId;
+	
+	@Column(name = "SID")
+	public String sid;
+	
+	@Column(name = "STAMP_NUMBER")
+	public String stampNumber;
+
+	@Column(name = "REGISTER_DATE")
+    public GeneralDate registerDate;
+	
+
+	@Override
+	protected Object getKey() {
+		return cardId;
+	}
+
+}
