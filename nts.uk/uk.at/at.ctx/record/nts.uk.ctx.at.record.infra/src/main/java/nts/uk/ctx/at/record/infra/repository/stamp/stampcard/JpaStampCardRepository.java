61a21473c8e37aced0cce45058c6dcc597116147
NSVN-TruongVanLai <NSVN-TruongVanLai@users.noreply.github.com>
2018-05-12 16:52:18 +0700

    cmt chowf review table

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.infra/src/main/java/nts/uk/ctx/at/record/infra/repository/stamp/stampcard/JpaStampCardRepository.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.infra/src/main/java/nts/uk/ctx/at/record/infra/repository/stamp/stampcard/JpaStampCardRepository.java
@@ -1,5 +1,6 @@
 package nts.uk.ctx.at.record.infra.repository.stamp.stampcard;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -8,6 +9,8 @@ import java.util.stream.Collectors;
 import javax.ejb.Stateless;
 
 import nts.arc.layer.infra.data.JpaRepository;
+import nts.arc.time.GeneralDate;
+import nts.gul.text.IdentifierUtil;
 import nts.uk.ctx.at.record.dom.stamp.card.stampcard.StampCard;
 import nts.uk.ctx.at.record.dom.stamp.card.stampcard.StampCardRepository;
 import nts.uk.ctx.at.record.infra.entity.stamp.stampcard.KrcmtStamcard;
@@ -15,14 +18,14 @@ import nts.uk.ctx.at.record.infra.entity.stamp.stampcard.KrcmtStamcard;
 @Stateless
 public class JpaStampCardRepository  extends JpaRepository implements StampCardRepository{
 	
-	private String GET_ALL_BY_SID = "SELECT a FROM KrcmtStamcard a WHERE a.sid = :sid ";
+	//private String GET_ALL_BY_SID = "SELECT a FROM KrcmtStamcard a WHERE a.sid = :sid ";
 
-	private String GET_BY_CARD_ID = "SELECT a FROM KrcmtStamcard a WHERE a.cardId = :cardid";
+	//private String GET_BY_CARD_ID = "SELECT a FROM KrcmtStamcard a WHERE a.cardId = :cardid";
 
 
 	@Override
 	public List<StampCard> getListStampCard(String sid) {
-		List<KrcmtStamcard> entities = this.queryProxy()
+		/*List<KrcmtStamcard> entities = this.queryProxy()
 				.query(GET_ALL_BY_SID, KrcmtStamcard.class)
 				.setParameter("sid", sid).getList();
 		if(entities.isEmpty())
@@ -30,62 +33,69 @@ public class JpaStampCardRepository  extends JpaRepository implements StampCardR
 
 		return entities.stream()
 				.map(x -> StampCard.createFromJavaType(x.cardId, x.sid, x.stampNumber, x.registerDate))
-				.collect(Collectors.toList());
+				.collect(Collectors.toList());*/
+		
+		List<StampCard> mockResults = new ArrayList<>();
+		mockResults.add(StampCard.createFromJavaType(IdentifierUtil.randomUniqueId(), sid, "a0000001", GeneralDate.today()));
+		mockResults.add(StampCard.createFromJavaType(IdentifierUtil.randomUniqueId(), sid, "a0000002", GeneralDate.today()));
+		return mockResults;
 	}
 
 	@Override
 	public Optional<StampCard> getByStampCardId(String stampCardId) {
-		Optional<StampCard> entity = this.queryProxy()
+		return Optional.empty();
+		/*Optional<StampCard> entity = this.queryProxy()
 				.query(GET_BY_CARD_ID, KrcmtStamcard.class).setParameter("cardid", stampCardId)
 				.getSingle(x -> toDomain(x));
-		return entity;
+		return entity;*/
 	}
 
 	@Override
 	public void add(StampCard domain) {
-		this.commandProxy().insert(toEntity(domain));
+		//this.commandProxy().insert(toEntity(domain));
 	}
 
 
 	@Override
 	public void update(StampCard domain) {
-		Optional<KrcmtStamcard> entityOpt = this.queryProxy().find(domain.getStampCardId(),
+		/*Optional<KrcmtStamcard> entityOpt = this.queryProxy().find(domain.getStampCardId(),
 				KrcmtStamcard.class);
 		if (entityOpt.isPresent()) {
 			KrcmtStamcard entity = entityOpt.get();
 			updateDetail(entity, domain);
 			this.commandProxy().update(entity);
-		}
+		} */
 	}
 
 	@Override
 	public void delete(String stampCardId) {
-		Optional<KrcmtStamcard> entityOpt = this.queryProxy().find(stampCardId, KrcmtStamcard.class);
+		/*Optional<KrcmtStamcard> entityOpt = this.queryProxy().find(stampCardId, KrcmtStamcard.class);
 		if (entityOpt.isPresent()) {
 			KrcmtStamcard entity = entityOpt.get();
 			this.commandProxy().remove(entity);
-		}
+		}*/
 
 	}
 	
 	private StampCard toDomain(KrcmtStamcard e) {
-		return StampCard.createFromJavaType(e.cardId, e.sid, e.stampNumber, e.registerDate);
+		return null;
+		/*return StampCard.createFromJavaType(e.cardId, e.sid, e.stampNumber, e.registerDate);
 	}
 	
 	private KrcmtStamcard toEntity(StampCard domain) {
-		KrcmtStamcard entity = new KrcmtStamcard();
+		/*KrcmtStamcard entity = new KrcmtStamcard();
 		entity.cardId  = domain.getStampCardId();
 		entity.sid = domain.getEmployeeId();
 		entity.stampNumber = domain.getStampNumber().v();
 		entity.registerDate = domain.getRegisterDate();
-		return entity;
+		return entity;*/
 	}
 	
 	private void updateDetail(KrcmtStamcard entity, StampCard data) {
-		entity.cardId  = data.getStampCardId();
+		/*entity.cardId  = data.getStampCardId();
 		entity.sid = data.getEmployeeId();
 		entity.stampNumber = data.getStampNumber().v();
-		entity.registerDate = data.getRegisterDate();
+		entity.registerDate = data.getRegisterDate();*/
 	}
 
 }
