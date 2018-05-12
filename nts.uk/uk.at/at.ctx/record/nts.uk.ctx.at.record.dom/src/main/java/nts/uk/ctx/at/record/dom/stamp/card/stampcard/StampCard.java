53aceaeb44f3dbdcf1805a2ab2c5456841c14953
NSVN-PhamVanDan <dan_pv@nittsusystem-vn.com>
2018-05-12 09:36:19 +0700

    add full constructor and create method

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/stamp/card/stampcard/StampCard.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/stamp/card/stampcard/StampCard.java
@@ -8,27 +8,39 @@ import nts.arc.time.GeneralDate;
 @Getter
 @Setter
 public class StampCard extends AggregateRoot {
-	
+
 	// domain name: 打刻カード
-	
+
 	/**
 	 * 打刻カードID
 	 */
 	private String stampCardId;
-	
+
 	/**
 	 * 社員ID
 	 */
 	private String employeeId;
-	
+
 	/**
 	 * 番号
 	 */
 	private StampNumber stampNumber;
-	
+
 	/**
 	 * 登録日付
 	 */
 	private GeneralDate registerDate;
 
+	public static StampCard createFromJavaType(String stampCardId, String employeeId, String stampNumber,
+			GeneralDate registerDate) {
+		return new StampCard(stampCardId, employeeId, new StampNumber(stampNumber), registerDate);
+	}
+
+	public StampCard(String stampCardId, String employeeId, StampNumber stampNumber, GeneralDate registerDate) {
+		super();
+		this.stampCardId = stampCardId;
+		this.employeeId = employeeId;
+		this.stampNumber = stampNumber;
+		this.registerDate = registerDate;
+	}
 }
