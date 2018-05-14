6381c3829b893203d9967dec2f5bf40b84349fd0
anhlt-nws <anhlt@newwave.vn>
2018-05-14 15:41:27 +0700

    add metamodel

---
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.infra/src/main/java/nts/uk/ctx/at/function/infra/entity/attendancerecord/KfnstSealColumn_.java
@@ -0,0 +1,15 @@
+package nts.uk.ctx.at.function.infra.entity.attendancerecord;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+import nts.uk.ctx.at.function.infra.entity.attendancerecord.KfnstSealColumnPK;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-14T13:26:53")
+@StaticMetamodel(KfnstSealColumn.class)
+public class KfnstSealColumn_ { 
+
+    public static volatile SingularAttribute<KfnstSealColumn, Object> sealStampName;
+    public static volatile SingularAttribute<KfnstSealColumn, KfnstSealColumnPK> id;
+
+}
---
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.infra/src/main/java/nts/uk/ctx/at/function/infra/entity/attendancerecord/item/KfnstAttndRecItemPK_.java
@@ -0,0 +1,18 @@
+package nts.uk.ctx.at.function.infra.entity.attendancerecord.item;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-14T13:26:53")
+@StaticMetamodel(KfnstAttndRecItemPK.class)
+public class KfnstAttndRecItemPK_ { 
+
+    public static volatile SingularAttribute<KfnstAttndRecItemPK, Long> outputAtr;
+    public static volatile SingularAttribute<KfnstAttndRecItemPK, Long> exportCd;
+    public static volatile SingularAttribute<KfnstAttndRecItemPK, Long> timeItemId;
+    public static volatile SingularAttribute<KfnstAttndRecItemPK, Long> columnIndex;
+    public static volatile SingularAttribute<KfnstAttndRecItemPK, Long> position;
+    public static volatile SingularAttribute<KfnstAttndRecItemPK, String> cid;
+
+}
