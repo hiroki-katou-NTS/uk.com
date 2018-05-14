208d2577228995afe73971c3e2945071bf0abcea
TruongQuocPhong <phong_tq@nittsusystem-vn.com>
2018-05-14 09:23:30 +0700

    KAL004: update programIdConst for screen D and G

--- nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/com/program/ProgramIdConsts.java
+++ nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/com/program/ProgramIdConsts.java
@@ -685,7 +685,9 @@ public class ProgramIdConsts {
 
 	public static final String KAL004A = "KAL004A";
 	public static final String KAL004B = "KAL004B";
+	public static final String KAL004D = "KAL004D";
 	public static final String KAL004F = "KAL004F";
+	public static final String KAL004G = "KAL004G";
 	public static final String KAL002A = "KAL002A";
 	public static final String KAL002B = "KAL002B";
 	public static final String KDW004A = "KDW004A";
