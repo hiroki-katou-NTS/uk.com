0e62a7f24d0e3159073f2d2c7c3737dc59617381
3SI\sang.nv <sang.nv@3si.vn>
2018-05-15 17:20:49 +0700

    Update mock popup

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdm/001/test/kdm001.test.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdm/001/test/kdm001.test.vm.ts
@@ -3,17 +3,20 @@ module nts.uk.at.view.kdm001.test.viewmodel {
     export class ScreenModel {
         constructor() {
         }
-
+        openKDM001I(): void {
+            modal("/view/kdm/001/i/index.xhtml").onClosed(function() { });
+        }
+        openKDM001J(): void {
+            modal("/view/kdm/001/j/index.xhtml").onClosed(function() { });
+        }
+        openKDM001K(): void {
+            modal("/view/kdm/001/k/index.xhtml").onClosed(function() { });
+        }
+        openKDM001L(): void {
+            modal("/view/kdm/001/l/index.xhtml").onClosed(function() { });
+        }
         openKDM001M(): void {
             modal("/view/kdm/001/m/index.xhtml").onClosed(function() { });
-        }
-    }
-    export class ItemModel {
-        code: string;
-        name: string;
-        constructor(code: string, name: string) {
-            this.code = code;
-            this.name = name;
         }
     }
 }
