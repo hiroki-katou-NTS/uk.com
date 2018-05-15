f6bba6a7847fc15d12e06cbddeb2c9a34886569a
Nam Bean <nambean90@gmail.com>
2018-05-15 17:48:25 +0700

    update

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdm/001/test/kdm001.test.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdm/001/test/kdm001.test.vm.ts
@@ -7,6 +7,9 @@ module nts.uk.at.view.kdm001.test.viewmodel {
         openKDM001M(): void {
             modal("/view/kdm/001/m/index.xhtml").onClosed(function() { });
         }
+        openKDM001I(): void {
+            modal("/view/kdm/001/i/index.xhtml").onClosed(function() { });
+        }
     }
     export class ItemModel {
         code: string;
