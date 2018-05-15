e3c48c420779aef967b71e0f62e16a629a06b186
3SI\sang.nv <sang.nv@3si.vn>
2018-05-15 17:12:05 +0700

    Commit demo popup

---
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdm/001/test/kdm001.test.start.ts
@@ -0,0 +1,6 @@
+module nts.uk.at.view.kdm001.test {
+    __viewContext.ready(function() {
+        let screenModel = new viewmodel.ScreenModel();
+        __viewContext.bind(screenModel);
+    });
+}
