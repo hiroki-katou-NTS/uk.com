099d361d844ff8e48fb75ef7d00f926141daa1fd
thuyetnguyen <thuyetdinh9xhn@gmail.com>
2018-05-16 14:48:43 +0700

    Fix bug #118011 and source screen L

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdm/001/k/kdm001.k.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdm/001/k/kdm001.k.vm.ts
@@ -1,59 +1,61 @@
 module nts.uk.at.view.kdm001.k {
     export module viewmodel {
         export class ScreenModel {
-        items: KnockoutObservableArray<ItemModel>;
-        columns: KnockoutObservableArray<NtsGridListColumn>;
-        currentCode: KnockoutObservable<any>;
-        currentCodeList: KnockoutObservableArray<any>;
-        count: number = 100;
-        switchOptions: KnockoutObservableArray<any>;
-        workCode: KnockoutObservableArray<any>;
-        workPlaceName: KnockoutObservableArray<any>;
-        employeeCode: KnockoutObservableArray<any>;    
-        employeeName: KnockoutObservableArray<any>;
-        dateHoliday: KnockoutObservableArray<any>;
-        numberDay: KnockoutObservableArray<any>;      
-        constructor() {
-            var self = this;
-            self.items = ko.observableArray([]);
-            self.initScreen();
-            self.currentCode = ko.observable();
-            self.currentCodeList = ko.observableArray([]);
-            
-        }
-        
-        public initScreen(): void {
-            var self = this;
-            self.workCode = ko.observable('100');
-            self.workPlaceName = ko.observable('workPlaceName');
-            self.employeeCode = ko.observable('A000001');
-            self.employeeName = ko.observable('employeeName');
-            self.dateHoliday = ko.observable('2016/10/2');
-            self.numberDay = ko.observable('numberDay');
-            
-            for(let i = 1; i < 100; i++) {
-                self.items.push(new ItemModel('00' + i, "2010/1/10", "1.0F"));
+            items: KnockoutObservableArray<ItemModel>;
+            columns: KnockoutObservableArray<NtsGridListColumn>;
+            currentCode: KnockoutObservable<any>;
+            currentCodeList: KnockoutObservableArray<any>;
+
+            workCode: KnockoutObservable<string> = ko.observable('');
+            workPlaceName: KnockoutObservable<string> = ko.observable('');
+            employeeCode: KnockoutObservable<string> = ko.observable('');
+            employeeName: KnockoutObservable<string> = ko.observable('');
+            dateHoliday: KnockoutObservable<any> = ko.observable('');
+            numberDay: KnockoutObservable<any> = ko.observable('');
+            residualDay: KnockoutObservable<any> = ko.observable('');
+            constructor() {
+                var self = this;
+                self.columns = ko.observableArray([
+                    { headerText: 'コード', key: 'code', width: 100, hidden: true },
+                    { headerText: nts.uk.resource.getText("KDM001_95"), key: 'date', width: 100 },
+                    { headerText: nts.uk.resource.getText("KDM001_96"), key: 'useNumberDay', width: 100 }
+                ]);
+                self.items = ko.observableArray([]);
+                self.initScreen();
+                self.currentCode = ko.observable();
+                self.currentCodeList = ko.observableArray([]);
+
             }
-            
-            self.columns = ko.observableArray([
-                { headerText: 'コード', key: 'code', width: 100, hidden: true },
-                { headerText: nts.uk.resource.getText("KDM001_95"), key: 'date', width: 100 }, 
-                { headerText: nts.uk.resource.getText("KDM001_96"), key: 'useNumberDay', width: 100} 
-            ]); 
-            
+
+            public initScreen(): void {
+                var self = this;
+                self.workCode('100');
+                self.workPlaceName('営業部');
+                self.employeeCode('A000001');
+                self.employeeName('日通　太郎');
+                self.dateHoliday('2016/10/2');
+                self.numberDay('0.5日');
+                self.residualDay('0日');
+
+                for (let i = 1; i < 100; i++) {
+                    self.items.push(new ItemModel('00' + i, "2010/1/10", "1.0F"));
+                }
+
+
+
+            }
         }
-    }
-    
-    class ItemModel {
-        code: string;
-        date: string;
-        useNumberDay: string;
-        constructor(code: string,  date: string, useNumberDay?: string) {
-            this.code = code;
-            this.date = date;
-            this.useNumberDay = useNumberDay;
-           
+
+        class ItemModel {
+            code: string;
+            date: string;
+            useNumberDay: string;
+            constructor(code: string, date: string, useNumberDay?: string) {
+                this.code = code;
+                this.date = date;
+                this.useNumberDay = useNumberDay;
+
+            }
         }
-    }
     }
 }
---
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdm/001/l/kdm001.l.start.ts
@@ -0,0 +1,7 @@
+module nts.uk.at.view.kdm001.l {
+    __viewContext.ready(function() {
+        let screenModel = new nts.uk.at.view.kdm001.l.viewmodel.ScreenModel();
+            __viewContext.bind(screenModel);
+            
+    });
+}
