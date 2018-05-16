c262fb34253aa2883fbb880bc7a3aa075835cc11
HaiTN <hai.tn@3si.vn>
2018-05-16 11:29:15 +0700

    update primitive value theo dd

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/annualworkschedule/primitivevalue/ItemOutTblBookHeadingName.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/annualworkschedule/primitivevalue/ItemOutTblBookHeadingName.java
@@ -3,7 +3,7 @@ package nts.uk.ctx.at.function.dom.annualworkschedule.primitivevalue;
 import nts.arc.primitive.StringPrimitiveValue;
 import nts.arc.primitive.constraint.StringMaxLength;
 
-@StringMaxLength(20)
+@StringMaxLength(12)
 public class ItemOutTblBookHeadingName extends StringPrimitiveValue<ItemOutTblBookHeadingName> {
 
 	public ItemOutTblBookHeadingName(String rawValue) {
