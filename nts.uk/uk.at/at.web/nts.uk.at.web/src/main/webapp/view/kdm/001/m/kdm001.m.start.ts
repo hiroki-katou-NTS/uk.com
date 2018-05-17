9db75330bc4a96ae26c1af94828cb20196a5104d
3SI\sang.nv <sang.nv@3si.vn>
2018-05-17 09:38:10 +0700

    KDM001M: Set focus item

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdm/001/m/kdm001.m.start.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdm/001/m/kdm001.m.start.ts
@@ -2,5 +2,6 @@ module nts.uk.at.view.kdm001.m {
     __viewContext.ready(function() {
         let screenModel =  new nts.uk.at.view.kdm001.m.viewmodel.ScreenModel();
         __viewContext.bind(screenModel);
+        $('#M6_2').focus();
     });
 }
