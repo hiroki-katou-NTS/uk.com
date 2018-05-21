5a7eacd6528fea40d6f26812d2f4f1f55b6b4af4
thuyetnguyen <thuyetdinh9xhn@gmail.com>
2018-05-21 18:08:21 +0700

    add source Screen J V_2

---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/remainingnumber/dayoffmanagement/DayOffManagementData.java
@@ -0,0 +1,23 @@
+package nts.uk.ctx.at.record.dom.remainingnumber.dayoffmanagement;
+
+import java.util.List;
+
+import lombok.Getter;
+
+@Getter
+public class DayOffManagementData {
+	
+	private List<DaysOffMana> daysOffMana;
+	private String employeeId;
+	private String leaveId;
+	
+	public DayOffManagementData(List<DaysOffMana> daysOffMana, String employeeId, String leaveId) {
+		super();
+		this.daysOffMana = daysOffMana;
+		this.employeeId = employeeId;
+		this.leaveId = leaveId;
+	}
+	
+	
+	
+}
--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdm/001/j/kdm001.j.service.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdm/001/j/kdm001.j.service.ts
@@ -3,12 +3,19 @@ module nts.uk.at.view.kdm001.j {
         
         var paths = {
             getAll: "at/record/remainnumber/subhd/getAll/{0}",
+            update: "at/record/remainnumber/subhd/update",
         }
         /**
          * get list day off 
          */
         export function getAll(leaveId): JQueryPromise<any> {
              return nts.uk.request.ajax("at", paths.getAll, leaveId);
+        }
+        /**
+         * update
+         */
+        export function update(data): JQueryPromise<any> {
+             return nts.uk.request.ajax("at", paths.update, data);
         }
     }
 }
