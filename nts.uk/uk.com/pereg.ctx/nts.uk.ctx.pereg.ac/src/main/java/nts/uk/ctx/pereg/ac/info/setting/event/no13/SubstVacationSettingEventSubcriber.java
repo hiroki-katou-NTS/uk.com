5320356325ab838f3f87e4abd670037abc979c78
NSVN-TruongVanLai <NSVN-TruongVanLai@users.noreply.github.com>
2018-05-22 15:33:17 +0700

    update EventList13

--- nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.ac/src/main/java/nts/uk/ctx/pereg/ac/info/setting/event/no13/SubstVacationSettingEventSubcriber.java
+++ nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.ac/src/main/java/nts/uk/ctx/pereg/ac/info/setting/event/no13/SubstVacationSettingEventSubcriber.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import nts.arc.layer.dom.event.DomainEventSubscriber;
@@ -23,6 +24,7 @@ import nts.uk.shr.com.context.AppContexts;
  * @author laitv
  *
  */
+@Stateless
 public class SubstVacationSettingEventSubcriber implements DomainEventSubscriber<SubstVacationSettingDomainEvent> {
 
 	@Inject
@@ -33,7 +35,7 @@ public class SubstVacationSettingEventSubcriber implements DomainEventSubscriber
 
 	private static final List<String> ctgCodeLst = Arrays.asList(new String[] { "CS00035" });
 
-	private static final List<String> itemCdLst = Arrays.asList(new String[] { "IS00366", "IS00368" });
+	private static final List<String> itemCdLst = Arrays.asList(new String[] { "IS00368" });
 
 	@Override
 	public Class<SubstVacationSettingDomainEvent> subscribedToEventType() {
@@ -46,23 +48,28 @@ public class SubstVacationSettingEventSubcriber implements DomainEventSubscriber
 		boolean parameter = domainEvent.isParameter();
 		Optional<PersonInfoCategory> ctg35Opt = ctgRepo.getPerInfoCategoryByCtgCD("CS00035", companyId);
 		if (ctg35Opt.isPresent()) {
-			List<PersonInfoItemDefinition> lstItemDf = this.getItemLst();
-			if (parameter) {
-				lstItemDf.stream().map(c -> {
-					c.setIsAbolition(IsAbolition.NOT_ABOLITION);
-					return c;
-				}).collect(Collectors.toList());
-			} else {
-				lstItemDf.stream().map(c -> {
-					c.setIsAbolition(IsAbolition.ABOLITION);
-					return c;
-				}).collect(Collectors.toList());
-			}
-
-			if (lstItemDf.size() > 0)
-				this.itemRepo.updateAbolitionItem(lstItemDf);
+			updateAbolition(parameter);
 		}
 	}
+
+	private void updateAbolition(boolean parameter) {
+		List<PersonInfoItemDefinition> lstItemDf = this.getItemLst();
+		if (parameter) {
+			lstItemDf.stream().map(c -> {
+				c.setIsAbolition(IsAbolition.NOT_ABOLITION);
+				return c;
+			}).collect(Collectors.toList());
+		} else {
+			lstItemDf.stream().map(c -> {
+				c.setIsAbolition(IsAbolition.ABOLITION);
+				return c;
+			}).collect(Collectors.toList());
+		}
+
+		if (lstItemDf.size() > 0)
+			this.itemRepo.updateAbolitionItem(lstItemDf);
+	}
+	
 
 	private List<PersonInfoItemDefinition> getItemLst() {
 		String companyLogin = AppContexts.user().companyId();
