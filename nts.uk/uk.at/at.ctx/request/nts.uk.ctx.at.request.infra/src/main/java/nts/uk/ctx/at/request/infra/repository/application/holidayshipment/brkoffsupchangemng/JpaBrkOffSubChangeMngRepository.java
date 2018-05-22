3b785d9baa8a279d5b35a71c3daba87f5eb1956d
Vu Thang Loi <loi_vt@nittsusystem-vn.com>
2018-05-22 17:14:00 +0700

    kaf010: update call from kaf011 to kaf010

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.infra/src/main/java/nts/uk/ctx/at/request/infra/repository/application/holidayshipment/brkoffsupchangemng/JpaBrkOffSubChangeMngRepository.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.infra/src/main/java/nts/uk/ctx/at/request/infra/repository/application/holidayshipment/brkoffsupchangemng/JpaBrkOffSubChangeMngRepository.java
@@ -1,5 +1,7 @@
 package nts.uk.ctx.at.request.infra.repository.application.holidayshipment.brkoffsupchangemng;
 
+import java.util.Optional;
+
 import javax.ejb.Stateless;
 
 import nts.arc.layer.infra.data.JpaRepository;
@@ -10,11 +12,32 @@ import nts.uk.ctx.at.request.infra.entity.application.holidayshipment.brkoffsupc
 
 @Stateless
 public class JpaBrkOffSubChangeMngRepository extends JpaRepository implements BrkOffSupChangeMngRepository {
-
+	 private static final String FINDER_LEAVE_APPID = "Select e from KrqdtBrkOffSupChangeMng e where e.pk.leaveAppID = :holidayWorkAppID";
+	
 	@Override
 	public void insert(BrkOffSupChangeMng brkOffSupChangeMng) {
 		KrqdtBrkOffSupChangeMng entity = new KrqdtBrkOffSupChangeMng();
 		entity.setPk(new KrqdtBrkOffSupChangeMngPK(brkOffSupChangeMng.getRecAppID(), brkOffSupChangeMng.getAbsenceLeaveAppID()));
 		this.commandProxy().insert(entity);
+	}
+
+	@Override
+	public Optional<BrkOffSupChangeMng> findHolidayAppID(String holidayWorkAppID) {
+		Optional<KrqdtBrkOffSupChangeMng> opt = this.queryProxy().query(FINDER_LEAVE_APPID, KrqdtBrkOffSupChangeMng.class).setParameter("holidayWorkAppID", holidayWorkAppID).getSingle();
+		if(!opt.isPresent()){
+			return Optional.empty();
+		}
+		KrqdtBrkOffSupChangeMng krqdtBrkOffSupChangeMng = opt.get();
+		BrkOffSupChangeMng brkOffSupChangeMng = krqdtBrkOffSupChangeMng.toDomain();
+		return Optional.of(brkOffSupChangeMng);
+	}
+
+	@Override
+	public void remove(String leaveAppID, String absenceLeaveAppID) {
+		Optional<KrqdtBrkOffSupChangeMng> opt = this.queryProxy().find(new KrqdtBrkOffSupChangeMngPK(leaveAppID, absenceLeaveAppID), KrqdtBrkOffSupChangeMng.class);
+		if(!opt.isPresent()){
+			throw new RuntimeException("Not find KrqdtBrkOffSupChangeMng");
+		}
+		this.commandProxy().remove(KrqdtBrkOffSupChangeMng.class, new KrqdtBrkOffSupChangeMngPK(leaveAppID, absenceLeaveAppID));
 	}
 }
