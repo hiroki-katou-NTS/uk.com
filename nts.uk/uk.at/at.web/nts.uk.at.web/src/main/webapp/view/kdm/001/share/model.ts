112d83acb2cb85230549fd8befe8c71b8aa106de
ducnhientran <ducnhientran071094@gmail.com>
2018-05-22 17:59:15 +0700

    UI screen G&&H

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdm/001/share/model.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdm/001/share/model.ts
@@ -10,16 +10,28 @@ module nts.uk.at.view.kdm001.share.model {
     export function getNumberOfDays(): Array<ItemModel> {
         return [
             new model.ItemModel(1.0, getText('KDM001_127')),
-            new model.ItemModel(0.5, getText('KDM001_128')) 
+            new model.ItemModel(0.5, getText('KDM001_128'))
+        ];
+    }
+    export function getRequireDay(): Array<ItemModel> {
+        return [
+            new model.ItemModel(1.0, getText('KDM001_127')),
+            new model.ItemModel(0.5, getText('KDM001_128'))
         ];
     }
 
+    export function getremainDay(): Array<ItemModel> {
+        return [
+            new model.ItemModel(1.0, getText('KDM001_127')),
+            new model.ItemModel(0.5, getText('KDM001_128'))
+        ];
+    }
     export class ItemModel {
         code: any;
         name: string;
         constructor(code: any, name: string) {
-            this.code  = code;
-            this.name  = name;
+            this.code = code;
+            this.name = name;
         }
     }
 }
