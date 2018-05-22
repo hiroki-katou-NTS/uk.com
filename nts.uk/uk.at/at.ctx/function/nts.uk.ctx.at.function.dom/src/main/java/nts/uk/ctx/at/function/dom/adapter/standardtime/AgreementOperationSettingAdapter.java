8fe2faff1afee4277e3bea56860ecbfb156bd4cb
hieunv3 <hieu.nv@3si.vn>
2018-05-22 13:55:35 +0700

    KWR008-A: apply request list #108

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/adapter/standardtime/AgreementOperationSettingAdapter.java
+++
@@ -1,5 +0,0 @@
-package nts.uk.ctx.at.function.dom.adapter.standardtime;
-
-public interface AgreementOperationSettingAdapter {
-	public AgreementOperationSettingImport find();
-}
--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.pub/src/main/java/nts/uk/ctx/at/record/pub/standardtime/AgreementOperationSettingPub.java
+++
@@ -1,7 +0,0 @@
-package nts.uk.ctx.at.record.pub.standardtime;
-
-import java.util.Optional;
-
-public interface AgreementOperationSettingPub {
-	public Optional<AgreementOperationSettingDto> find();
-}
