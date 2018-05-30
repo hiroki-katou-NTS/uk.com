ab655f3f22e419e5f339da53fb1ba037b52b5b5c
Anh Trung <anhtrungbk55@gmail.com>
2018-05-30 10:32:59 +0700

    Thêm remainDays và stateAtr cho màn GH

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdm/001/a/kdm001.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdm/001/a/kdm001.a.vm.ts
@@ -123,6 +123,8 @@ module nts.uk.at.view.kdm001.a.viewmodel {
                 columns: [
                     { headerText: 'ID', key: 'id', dataType: 'string', width: '0px', hidden: true },
                     { headerText: 'linked', key: 'isLinked', dataType: 'string', width: '0px', hidden: true },
+                    { headerText: '', key: 'stateAtr', dataType: 'Number', width: '0px', hidden: true },
+                    { headerText: '', key: 'remainDays', dataType: 'Number', width: '0px', hidden: true },
                     { headerText: '', key: 'expiredDate', dataType: 'string', width: '0px', hidden: true },
                     { headerText: '', key: 'unknownDatePayout', dataType: 'string', width: '0px', hidden: true },
                     { headerText: '', key: 'unknownDateSub', dataType: 'string', width: '0px', hidden: true },
@@ -507,6 +509,7 @@ module nts.uk.at.view.kdm001.a.viewmodel {
         //add to check enable button
         isLinked: boolean;
         
+        //add to set '日' after day number
         occurredDaysText: string;
         requiredDaysText: string;
         unUsedDaysInGridText: string;
