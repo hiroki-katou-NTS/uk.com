bd8fac00eedf0134f2610db001b43446e1dcab6d
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2018-05-30 13:34:19 +0700

    fix bug #87840 KAF004 (control hiển thị ngày đăng kí đơn ở màn E và F bị sai)

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/lateorleaveearly/service/FactoryLateOrLeaveEarlyDefault.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/lateorleaveearly/service/FactoryLateOrLeaveEarlyDefault.java
@@ -15,9 +15,9 @@ public class FactoryLateOrLeaveEarlyDefault implements FactoryLateOrLeaveEarly {
 	public LateOrLeaveEarly buildLateOrLeaveEarly(Application_New application, int early1, int earlyTime1, int late1, int lateTime1, 
 			int early2, int earlyTime2, int late2, int lateTime2) {
 		return new LateOrLeaveEarly(application, 0, 
-				EnumAdaptor.valueOf(early1, Select.class), new TimeDay(early1), 
+				EnumAdaptor.valueOf(early1, Select.class), new TimeDay(earlyTime1), 
 				EnumAdaptor.valueOf(late1, Select.class), new TimeDay(lateTime1),  
-				EnumAdaptor.valueOf(early2, Select.class), new TimeDay(early2), 
+				EnumAdaptor.valueOf(early2, Select.class), new TimeDay(earlyTime2), 
 				EnumAdaptor.valueOf(late2, Select.class), new TimeDay(lateTime2));
 	}
 	
