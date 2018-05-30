30b6ae77d8862ce7196cfce44ea9a70d5b7c9c81
NSVC\tanlv <tan_lv@nittsusystem-vn.com>
2018-05-30 10:49:31 +0700

    Requestlist 210

---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.pubimp/src/main/java/nts/uk/ctx/at/record/pubimp/workrecord/remainingnumbermanagement/AnnualHolidayManagementPubImpl.java
@@ -0,0 +1,96 @@
+package nts.uk.ctx.at.record.pubimp.workrecord.remainingnumbermanagement;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import nts.arc.time.GeneralDate;
+import nts.uk.ctx.at.record.dom.remainingnumber.annualleave.empinfo.basicinfo.AnnLeaEmpBasicInfoRepository;
+import nts.uk.ctx.at.record.dom.remainingnumber.annualleave.empinfo.basicinfo.AnnualLeaveEmpBasicInfo;
+import nts.uk.ctx.at.record.pub.workrecord.remainingnumbermanagement.AnnualHolidayManagementPub;
+import nts.uk.ctx.at.record.pub.workrecord.remainingnumbermanagement.NextAnnualLeaveGrantExport;
+import nts.uk.ctx.at.shared.dom.workrule.closure.service.GetClosureStartForEmployee;
+import nts.uk.ctx.at.shared.dom.yearholidaygrant.GrantHdTblSet;
+import nts.uk.ctx.at.shared.dom.yearholidaygrant.LengthServiceRepository;
+import nts.uk.ctx.at.shared.dom.yearholidaygrant.YearHolidayRepository;
+import nts.uk.ctx.at.shared.dom.yearholidaygrant.export.GetNextAnnualLeaveGrant;
+import nts.uk.shr.com.time.calendar.period.DatePeriod;
+
+@Stateless
+public class AnnualHolidayManagementPubImpl implements AnnualHolidayManagementPub {
+	@Inject
+	AnnLeaEmpBasicInfoRepository annLeaEmpBasicInfoRepository;
+	
+	@Inject
+	YearHolidayRepository yearHolidayRepository;
+	
+	@Inject
+	GetNextAnnualLeaveGrant nextAnnualLeaveGrant;
+	
+	@Inject
+	LengthServiceRepository lengthServiceRepository;
+
+	/**
+	 * RequestList210
+	 * 次回年休付与日を取得する
+	 * 
+	 * @param cId
+	 * @param sId
+	 * @return
+	 */
+	@Override
+	public List<NextAnnualLeaveGrantExport> acquireNextHolidayGrantDate(String companyId, String employeeId) {
+		// ドメインモデル「年休社員基本情報」を取得
+		Optional<AnnualLeaveEmpBasicInfo> annualLeaveEmpBasicInfo = annLeaEmpBasicInfoRepository.get(employeeId);
+		
+		// 次回年休付与を計算
+		List<NextAnnualLeaveGrantExport> result = calculateNextHolidayGrant(companyId, employeeId, null, annualLeaveEmpBasicInfo);
+		
+		// 次回年休付与を返す
+		return result;
+	}
+
+	/**
+	 * 次回年休付与を計算
+	 * 
+	 * @param companyId
+	 * @param employeeId
+	 * @param period
+	 * @param annualLeaveEmpBasicInfo
+	 * @return
+	 */
+	private List<NextAnnualLeaveGrantExport> calculateNextHolidayGrant(String companyId, String employeeId, 
+			Optional<DatePeriod> period, Optional<AnnualLeaveEmpBasicInfo> annualLeaveEmpBasicInfo) {
+		boolean isSingleDay = false;
+		Optional<DatePeriod> periodDate = period;
+		
+		// Imported(就業)「社員」を取得する
+		// TODO: QA
+		
+		// パラメータ「期間」をチェック
+		if(!period.isPresent()) {
+			isSingleDay = true;
+			
+			// 社員に対応する締め開始日を取得する
+			GetClosureStartForEmployee closureStartForEmployee = new GetClosureStartForEmployee();
+			Optional<GeneralDate> closureStartDate = closureStartForEmployee.algorithm(employeeId);
+			
+			periodDate = Optional.ofNullable(new DatePeriod(GeneralDate.ymd(closureStartDate.get().year(), 
+					closureStartDate.get().month(), closureStartDate.get().day()), GeneralDate.ymd(9999, 12, 31)));
+		}
+		
+		// ドメインモデル「年休付与テーブル設定」を取得する
+		Optional<GrantHdTblSet> grantHdTblSet = yearHolidayRepository.findByCode(companyId, annualLeaveEmpBasicInfo.get().getGrantRule().getGrantTableCode().v());
+		
+		// 次回年休付与を取得する
+//		List<NextAnnualLeaveGrantExport> nextAnnualLeaveGrantData = nextAnnualLeaveGrant.algorithm(companyId, grantHdTblSet.get().getYearHolidayCode(), 
+//				entryDate, annualLeaveEmpBasicInfo.get().getGrantRule().getGrantStandardDate(), periodDate, isSingleDay);
+		
+		// List<次回年休付与>を返す
+//		return nextAnnualLeaveGrantData;
+		return Collections.emptyList();
+	}
+}
