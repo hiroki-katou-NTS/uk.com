a2616667ba481810f29bd8ccac05e4f246928af7
ducnhientran <ducnhientran071094@gmail.com>
2018-05-31 10:39:32 +0700

    Delete G&&H

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/remainingnumber/paymana/DeletePayoutManagementDataCommandHandler.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/remainingnumber/paymana/DeletePayoutManagementDataCommandHandler.java
@@ -6,22 +6,18 @@ import javax.inject.Inject;
 import nts.arc.layer.app.command.CommandHandler;
 import nts.arc.layer.app.command.CommandHandlerContext;
 import nts.uk.ctx.at.record.dom.remainingnumber.paymana.PayoutManagementDataRepository;
-import nts.uk.ctx.at.record.dom.remainingnumber.paymana.SubstitutionOfHDManaDataService;
 
 @Stateless
 public class DeletePayoutManagementDataCommandHandler extends CommandHandler<DeletePayoutManagementDataCommand> {
 
 	@Inject
 	private PayoutManagementDataRepository payoutManagementDataRepository;
-	
-	@Inject
-	private SubstitutionOfHDManaDataService substitutionOfHDManaDataService;
+
 
 	@Override
 	protected void handle(CommandHandlerContext<DeletePayoutManagementDataCommand> context) {
 		DeletePayoutManagementDataCommand command = context.getCommand();
 		payoutManagementDataRepository.delete(command.getPayoutId());
-		substitutionOfHDManaDataService.setToFree(command.getPayoutId());
 	}
 
 }
