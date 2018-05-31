11262c341859b2269b6c8dadf643837aabff77e3
3SI\sang.nv <sang.nv@3si.vn>
2018-05-31 16:59:22 +0700

    KDM001L: Update screen

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdm/001/l/kdm001.l.start.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdm/001/l/kdm001.l.start.ts
@@ -3,7 +3,7 @@ module nts.uk.at.view.kdm001.l {
         let screenModel = new nts.uk.at.view.kdm001.l.viewmodel.ScreenModel();
         screenModel.startPage().done(() => {
             __viewContext.bind(screenModel);
-            _.defer(() => {$('#L8_2 input').focus()});
+            _.defer(() => {$('#L8_2').focus()});
         });
     });
 }
