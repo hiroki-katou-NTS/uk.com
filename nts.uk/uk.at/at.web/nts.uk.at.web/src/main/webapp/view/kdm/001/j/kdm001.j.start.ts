0c4b5ed7771f6418a0a52c83aa112cf45ba2c7df
thuyetnguyen <thuyetdinh9xhn@gmail.com>
2018-05-31 16:08:58 +0700

    Fix bug focus

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdm/001/j/kdm001.j.start.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdm/001/j/kdm001.j.start.ts
@@ -2,7 +2,7 @@ module nts.uk.at.view.kdm001.j {
     __viewContext.ready(function() {
         let screenModel = new nts.uk.at.view.kdm001.j.viewmodel.ScreenModel();
             __viewContext.bind(screenModel);
-            $("#multi-list").focus();
+            $("#multi-list_container").focus();
             
     });
 }
--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdm/001/k/kdm001.k.start.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdm/001/k/kdm001.k.start.ts
@@ -2,6 +2,6 @@ module nts.uk.at.view.kdm001.k {
     __viewContext.ready(function() {
         let screenModel = new nts.uk.at.view.kdm001.k.viewmodel.ScreenModel();
             __viewContext.bind(screenModel);
-            $("#multi-list").focus();
+            $("#multi-list_container").focus();
     });
 }
