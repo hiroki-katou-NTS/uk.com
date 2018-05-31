02f872e9c419660e87f21a180446610e14dd98a5
NSVC\dungdt <dung24391@gmail.com>
2018-05-31 17:34:39 +0700

    fixbug #96001 kbt002

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/processexecution/executionlog/ExecutionTaskLog.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/processexecution/executionlog/ExecutionTaskLog.java
@@ -11,11 +11,27 @@ import lombok.Setter;
  */
 @Getter
 @Setter
-@AllArgsConstructor
 public class ExecutionTaskLog {
 	/* 更新処理 */
 	private ProcessExecutionTask procExecTask;
 	
 	/* 終了状態 */
 	private Optional<EndStatus> status;
+	
+	
+	private String execId;
+
+	public ExecutionTaskLog(ProcessExecutionTask procExecTask, Optional<EndStatus> status) {
+		super();
+		this.procExecTask = procExecTask;
+		this.status = status;
+	}
+
+	public ExecutionTaskLog(ProcessExecutionTask procExecTask, Optional<EndStatus> status, String execId) {
+		super();
+		this.procExecTask = procExecTask;
+		this.status = status;
+		this.execId = execId;
+	}
+	
 }
