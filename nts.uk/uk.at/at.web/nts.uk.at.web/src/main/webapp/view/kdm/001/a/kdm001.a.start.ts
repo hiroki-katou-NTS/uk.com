117759145d0922284cfe2ee3a55df74b26253a92
Anh Trung <anhtrungbk55@gmail.com>
2018-06-01 14:30:42 +0700

    Ghép màn A, B

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdm/001/a/kdm001.a.start.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdm/001/a/kdm001.a.start.ts
@@ -1,10 +1,24 @@
 module nts.uk.at.view.kdm001.a {
     __viewContext.ready(function() {
-        var screenModel = new nts.uk.at.view.kdm001.a.viewmodel.ScreenModel();
-        screenModel.startPage().done(function() {
-            __viewContext.bind(screenModel);
-            $('#ccgcomponentA').ntsGroupComponent(screenModel.ccgcomponent);
-            $('#emp-component').focus();
+        var viewmodelA = new nts.uk.at.view.kdm001.a.viewmodel.ScreenModel();
+        var viewmodelB = new nts.uk.at.view.kdm001.b.viewmodel.ScreenModel();
+        __viewContext.viewModel = {
+            viewmodelA: viewmodelA,
+            viewmodelB: viewmodelB
+        };
+        __viewContext.bind(__viewContext.viewModel);
+        
+        viewmodelA.startPage().done(function() {
+            $('#ccgcomponentA').ntsGroupComponent(viewmodelA.ccgcomponent);
+            $('#emp-componentA').focus();
+            nts.uk.ui.block.clear();
+            
+            $(".tab-b").click(function() {
+                viewmodelB.startPage().done(function() {
+                    $('#emp-component').focus();
+                    nts.uk.ui.block.clear();
+                });
+            });
         });
     });
 }
