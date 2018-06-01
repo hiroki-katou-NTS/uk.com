2b954573115b43358eb1b98d00a42d788b156db5
hoangtv_hn <hoangtv_hn@newwave.vn>
2018-06-01 21:08:16 +0700

    Revert "fix bug #95898"
    This reverts commit 2f2527f301e161ae2d94e2ebc120dc8872e1ac70.

--- nts.uk/uk.com/com.ctx/sys/gateway/nts.uk.ctx.sys.gateway.dom/src/main/java/nts/uk/ctx/sys/gateway/dom/singlesignon/WindowsAccount.java
+++ nts.uk/uk.com/com.ctx/sys/gateway/nts.uk.ctx.sys.gateway.dom/src/main/java/nts/uk/ctx/sys/gateway/dom/singlesignon/WindowsAccount.java
@@ -4,7 +4,6 @@
  *****************************************************************/
 package nts.uk.ctx.sys.gateway.dom.singlesignon;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -44,22 +43,18 @@ public class WindowsAccount extends AggregateRoot{
 		super.validate();
 		final Integer MAX_FREQUENCY = 1;
 
-		List<WindowsAccountInfo> lstCheck = new ArrayList<>(this.accountInfos);
 		// check duplicate account host name & user name
 		this.accountInfos.forEach(acc -> {
 			boolean isNameNotNull = !StringUtil.isNullOrEmpty(acc.getHostName().v(), true)
 					&& !StringUtil.isNullOrEmpty(acc.getUserName().v(), true);
 
-			lstCheck.remove(acc); // remove so it won't be duplicated with itself
-			boolean isDuplicated = Collections.frequency(lstCheck, acc) > MAX_FREQUENCY;
-			
+			boolean isDuplicated = Collections.frequency(this.accountInfos, acc) > MAX_FREQUENCY;
+
 			if (isNameNotNull && isDuplicated) {
 				BundledBusinessException exceptions = BundledBusinessException.newInstance();
 				exceptions.addMessage("Msg_616");
 				exceptions.throwExceptions();
 			}
-			
-			lstCheck.add(acc); // add it back to check duplicate with next element
 		});
 	}
 	/**
