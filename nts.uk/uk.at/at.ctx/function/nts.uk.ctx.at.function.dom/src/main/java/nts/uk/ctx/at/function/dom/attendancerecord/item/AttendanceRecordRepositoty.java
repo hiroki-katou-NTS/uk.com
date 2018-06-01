52df79580f7f215c23a9d47ca52067fa181a14db
Locph <locph@newwave.vn>
2018-06-01 10:51:47 +0700

    Screen B fix review;

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/attendancerecord/item/AttendanceRecordRepositoty.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/attendancerecord/item/AttendanceRecordRepositoty.java
@@ -2,12 +2,19 @@ package nts.uk.ctx.at.function.dom.attendancerecord.item;
 
 import nts.uk.ctx.at.function.dom.attendancerecord.export.setting.ExportSettingCode;
 
+
+/**
+ * The interface AttendanceRecordRepositoty.
+ *
+ * @author locph
+ */
 public interface AttendanceRecordRepositoty {
 
     /**
+     * Delete attendance record.
      *
-     * @param companyId
-     * @param exportSettingCode
+     * @param companyId         the company id
+     * @param exportSettingCode the export setting code
      */
     void deleteAttendanceRecord(String companyId, ExportSettingCode exportSettingCode);
 }
