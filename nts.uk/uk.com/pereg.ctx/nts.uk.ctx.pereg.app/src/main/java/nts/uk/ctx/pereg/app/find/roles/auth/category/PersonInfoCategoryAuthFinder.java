c1b988ac53f34a8612d01e781d38422952125bb2
Leu Thi Lan <lan-lt@nittsusystem-vn.com>
2018-06-01 10:22:01 +0700

    fix bug #96003

--- nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.app/src/main/java/nts/uk/ctx/pereg/app/find/roles/auth/category/PersonInfoCategoryAuthFinder.java
+++ nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.app/src/main/java/nts/uk/ctx/pereg/app/find/roles/auth/category/PersonInfoCategoryAuthFinder.java
@@ -1,14 +1,18 @@
 package nts.uk.ctx.pereg.app.find.roles.auth.category;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+import nts.uk.ctx.pereg.dom.person.info.item.PerInfoItemDefRepositoty;
 import nts.uk.ctx.pereg.dom.roles.auth.category.PersonInfoCategoryAuth;
 import nts.uk.ctx.pereg.dom.roles.auth.category.PersonInfoCategoryAuthRepository;
+import nts.uk.ctx.pereg.dom.roles.auth.category.PersonInfoCategoryDetail;
 import nts.uk.shr.com.context.AppContexts;
 
 /**
@@ -21,12 +25,25 @@ import nts.uk.shr.com.context.AppContexts;
 public class PersonInfoCategoryAuthFinder {
 	@Inject
 	private PersonInfoCategoryAuthRepository personCategoryAuthRepository;
+	
+	@Inject
+	private PerInfoItemDefRepositoty itemInfoRepo;
 
 	public List<PersonInfoCategoryDetailDto> getAllCategory(String roleId) {
-		return this.personCategoryAuthRepository
-				.getAllCategory(roleId, AppContexts.user().contractCode(), AppContexts.user().companyId()).stream()
-				.map(x -> PersonInfoCategoryDetailDto.fromDomain(x)).collect(Collectors.toList());
-
+		String contractCd = AppContexts.user().contractCode();
+		 List<PersonInfoCategoryDetail> ctgSourceLst = this.personCategoryAuthRepository
+					.getAllCategory(roleId, AppContexts.user().contractCode(), AppContexts.user().companyId());
+		 List<String> ctgLstId = ctgSourceLst.stream().map(c -> { return c.getCategoryId();}).collect(Collectors.toList());
+		 
+		Map<String, List<Object[]>> itemByCtgId  = this.itemInfoRepo.getAllPerInfoItemDefByListCategoryId(ctgLstId,contractCd);
+		 
+		List<PersonInfoCategoryDetailDto> ctgResultLst = new ArrayList<>();
+		for(PersonInfoCategoryDetail i : ctgSourceLst) {
+			List<Object[]>  item = itemByCtgId.get(i.getCategoryId());
+			if(item == null)  continue;
+			if(item.size() > 0)  ctgResultLst.add(PersonInfoCategoryDetailDto.fromDomain(i));
+		}
+		return ctgResultLst;
 	}
 	
 	public List<PersonInfoCategoryAuthDto> getAllCategoryAuth(String roleId) {
