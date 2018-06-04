2abede032025cda5e47c2212c53e65e010e4161e
Vu Thang Loi <loi_vt@nittsusystem-vn.com>
2018-06-04 16:02:19 +0700

    code import requestList228

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.ac/src/main/java/nts/uk/ctx/at/request/ac/bs/AtEmployeeAdapterImpl.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.ac/src/main/java/nts/uk/ctx/at/request/ac/bs/AtEmployeeAdapterImpl.java
@@ -8,6 +8,8 @@ import javax.inject.Inject;
 
 import nts.arc.time.GeneralDate;
 import nts.uk.ctx.at.request.dom.application.common.adapter.bs.AtEmployeeAdapter;
+import nts.uk.ctx.at.request.dom.application.common.adapter.bs.dto.EmployeeInforImport;
+import nts.uk.ctx.bs.employee.pub.employee.EmployeeInfoExport;
 import nts.uk.ctx.bs.employee.pub.employee.SyEmployeePub;
 /**
  * 
@@ -23,6 +25,17 @@ public class AtEmployeeAdapterImpl implements AtEmployeeAdapter{
 	public List<String> getListSid(String sId, GeneralDate baseDate) {
 		List<String> lstEmployeeId = syEmployeePub.GetListSid(sId, baseDate);
 		return lstEmployeeId == null ? new ArrayList<String>() : lstEmployeeId;
+	}
+	// Import RequestList228
+	@Override
+	public List<EmployeeInforImport> getEmployeeInfor(List<String> employeeIDs) {
+		List<EmployeeInforImport> result = new ArrayList<>();
+		List<EmployeeInfoExport> employeeInforExports = syEmployeePub.getByListSid(employeeIDs);
+		for(EmployeeInfoExport emp : employeeInforExports){
+			EmployeeInforImport empInfors = new EmployeeInforImport(emp.getSid(), emp.getScd(), emp.getBussinessName());
+			result.add(empInfors);
+		}
+		return result;
 	}
 
 }
--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/common/adapter/bs/AtEmployeeAdapter.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/common/adapter/bs/AtEmployeeAdapter.java
@@ -3,8 +3,15 @@ package nts.uk.ctx.at.request.dom.application.common.adapter.bs;
 import java.util.List;
 
 import nts.arc.time.GeneralDate;
+import nts.uk.ctx.at.request.dom.application.common.adapter.bs.dto.EmployeeInforImport;
 
 public interface AtEmployeeAdapter {
 
 	List<String> getListSid(String sId , GeneralDate baseDate);
+	/**
+	 * Import requestList228
+	 * @param employeeIDs
+	 * @return
+	 */
+	List<EmployeeInforImport> getEmployeeInfor(List<String> employeeIDs);
 }
