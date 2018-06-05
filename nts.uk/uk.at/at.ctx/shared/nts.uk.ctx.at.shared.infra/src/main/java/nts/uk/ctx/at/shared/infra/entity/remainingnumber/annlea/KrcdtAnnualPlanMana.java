11c4311045e9b615314eb45311688615261ab68b
HopNT3SI <32725057+HopNT3SI@users.noreply.github.com>
2018-06-05 10:47:41 +0700

    Fixbug has no primary key specified for chị Dự

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/entity/remainingnumber/annlea/KrcdtAnnualPlanMana.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/entity/remainingnumber/annlea/KrcdtAnnualPlanMana.java
@@ -1,6 +1,7 @@
 package nts.uk.ctx.at.shared.infra.entity.remainingnumber.annlea;
 
 import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -18,6 +19,7 @@ import nts.uk.shr.infra.data.entity.UkJpaEntity;
 @Table(name = "KRCDT_ANNUAL_PLAN_MANA")
 public class KrcdtAnnualPlanMana extends UkJpaEntity{
 	
+	@EmbeddedId
 	public KrcdtAnnualPlanManaPK pk;
 	/** 管理データ日数	 */
 	@Column(name = "USE_ANNUAL_DAYS")
