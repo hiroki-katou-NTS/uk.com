5f6ae96d926224ba91aa0def57ffe16818a34b4c
NSVC\dungdt <dung24391@gmail.com>
2018-06-06 08:35:44 +0700

    update next fire time kbt002

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.app/src/main/java/nts/uk/ctx/at/function/app/command/processexecution/SortingProcessScheduleJob.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.app/src/main/java/nts/uk/ctx/at/function/app/command/processexecution/SortingProcessScheduleJob.java
@@ -15,12 +15,10 @@ public class SortingProcessScheduleJob extends UkScheduledJob{
 		
 		String companyId = context.scheduletimeData().getString("companyId");
 		String execItemCd = context.scheduletimeData().getString("execItemCd");
-		//todo fixed 
-		String scheduleId ="lay tu kiban";
 		ScheduleExecuteCommand s = new ScheduleExecuteCommand();
 		s.setCompanyId(companyId);
 		s.setExecItemCd(execItemCd);
-		s.setScheduleId(scheduleId);
+		s.setNextDate(context.nextFireTime());
 		this.sortingProcessCommandHandler.handle(s);
 	}
 
