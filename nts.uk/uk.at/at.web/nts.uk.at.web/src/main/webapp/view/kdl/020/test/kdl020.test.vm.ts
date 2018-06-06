4ed4f1d83f8467884c5e8e6bfecb20c0d3fc958b
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2018-06-06 14:07:09 +0700

    update test KDL 020

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdl/020/test/kdl020.test.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdl/020/test/kdl020.test.vm.ts
@@ -10,17 +10,21 @@ module nts.uk.at.view.kdl020.test.screenModel {
     import setShared = nts.uk.ui.windows.setShared;
 
     export class ViewModel {
+
+        selectedCodeList = ko.observableArray(["c7cb93ce-d23b-4283-875e-a0bbb21b9d36",
+            "fe3b1f44-dbc8-44c0-ab32-f617f01f00a5",
+            "96c1e494-5cde-402c-8629-81b0dec7ac92",
+            "da1886cf-b80f-425c-af09-44a94a7643f2",
+            "ae69eb3f-4198-47e3-9f98-967d23c00997"]);
         constructor() {
             let self = this;
+
 
         }
 
         openKDL020Dialog() {
-            setShared('KDL020A_PARAM', ["c7cb93ce-d23b-4283-875e-a0bbb21b9d36",
-                "fe3b1f44-dbc8-44c0-ab32-f617f01f00a5",
-                "96c1e494-5cde-402c-8629-81b0dec7ac92",
-                "da1886cf-b80f-425c-af09-44a94a7643f2",
-                "ae69eb3f-4198-47e3-9f98-967d23c00997"])
+            let self = this;
+            setShared('KDL020A_PARAM', self.selectedCodeList());
             modal('/view/kdl/020/a/index.xhtml').onClosed(function(): any {
 
             });
