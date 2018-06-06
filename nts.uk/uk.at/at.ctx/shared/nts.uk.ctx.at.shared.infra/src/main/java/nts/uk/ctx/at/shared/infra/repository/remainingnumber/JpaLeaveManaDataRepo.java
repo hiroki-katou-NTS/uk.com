ee7d405e9a6338eb54f642fe33b1b250bbacec54
thuyetnguyen <thuyetdinh9xhn@gmail.com>
2018-06-06 11:05:36 +0700

    Fix bug #118974

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/repository/remainingnumber/JpaLeaveManaDataRepo.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/repository/remainingnumber/JpaLeaveManaDataRepo.java
@@ -151,6 +151,7 @@ public class JpaLeaveManaDataRepo extends JpaRepository implements LeaveManaData
 				.setParameter("leaveIDs", leaveIds).getList();
 		for(KrcmtLeaveManaData busItem: listListMana){
 			busItem.subHDAtr =  DigestionAtr.USED.value;
+			busItem.unUsedDays = 0.0;
 		}
 		this.commandProxy().updateAll(listListMana);
 	}
@@ -162,7 +163,7 @@ public class JpaLeaveManaDataRepo extends JpaRepository implements LeaveManaData
 				.setParameter("leaveIDs", leaveIds).getList();
 		for(KrcmtLeaveManaData busItem: listListMana){
 			busItem.subHDAtr =  DigestionAtr.UNUSED.value;
-			busItem.unUsedDays = 1.0;
+			busItem.unUsedDays = busItem.occurredDays;
 		}
 		this.commandProxy().updateAll(listListMana);
 	}
@@ -172,6 +173,7 @@ public class JpaLeaveManaDataRepo extends JpaRepository implements LeaveManaData
 	public void updateUnUseDayLeaveId(String leaveId,Double unUsedDay) {
 		KrcmtLeaveManaData leaveMana =  this.getEntityManager().find(KrcmtLeaveManaData.class, leaveId);
 		leaveMana.unUsedDays = unUsedDay;
+		leaveMana.subHDAtr = 1;
 		this.commandProxy().update(leaveMana);
 	}
 	
