1d571f19d973d292026e5cf34a7d4f7aa3f54be6
3SI\dat.lh <dat.lh@3si.vn>
2018-06-07 13:10:58 +0700

    ドメインモデル「３６協定運用設定」を取得する

---
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/adapter/standardtime/AgreementOperationSettingAdapter.java
@@ -0,0 +1,7 @@
+package nts.uk.ctx.at.function.dom.adapter.standardtime;
+
+import java.util.Optional;
+
+public interface AgreementOperationSettingAdapter {
+	Optional<AgreementOperationSettingImport> find(String cid);
+}
---
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/adapter/standardtime/ClosingDateAtrImport.java
@@ -0,0 +1,17 @@
+package nts.uk.ctx.at.function.dom.adapter.standardtime;
+
+public enum ClosingDateAtrImport {
+	/*
+	 * ３６協定締め日区分
+	 */
+	// 0: 勤怠の締め日と同じ
+	SAMEDATE(0),
+	// 1: 締め日を指定
+	DESIGNATEDATE(1);
+
+	public final int value;
+	
+	private ClosingDateAtrImport(int type) {
+		this.value = type;
+	}
+}
