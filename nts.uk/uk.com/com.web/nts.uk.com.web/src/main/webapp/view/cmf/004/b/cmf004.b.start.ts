467d681d7919766a0fc72ff1446fbebeee8bd903
3SI\sang.nv <sang.nv@3si.vn>
2018-06-11 13:50:41 +0700

    Remove CCG001

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmf/004/b/cmf004.b.start.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmf/004/b/cmf004.b.start.ts
@@ -3,7 +3,6 @@ module nts.uk.com.view.cmf004.b {
         __viewContext['screenModel'] = new viewmodel.ScreenModel();
         __viewContext['screenModel'].start().done(function(self) {
             __viewContext.bind(__viewContext['screenModel']);
-            $('#ccg001component').ntsGroupComponent(self.ccg001ComponentOption);
             $('#kcp005component').ntsListComponent(self.kcp005ComponentOption);
         });
     });
