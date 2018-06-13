6234852d40415d52e51625039ed3db9f7a579f65
Leu Thi Lan <lan-lt@nittsusystem-vn.com>
2018-06-13 11:03:48 +0700

    cps005- cps006: finder

--- nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.app/src/main/java/nts/uk/ctx/pereg/app/find/person/category/PerInfoCtgFinder.java
+++ nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.app/src/main/java/nts/uk/ctx/pereg/app/find/person/category/PerInfoCtgFinder.java
@@ -38,37 +38,38 @@ public class PerInfoCtgFinder {
 	};
 
 	public PerCtgInfoDto getDetailCtgInfo(String categoryId) {
-		String companyIdRoot = "000000000000-0000";
+		String companyIdRoot = AppContexts.user().zeroCompanyIdInContract();
 		String companyId = AppContexts.user().companyId();
 		String contractCd = companyId.substring(0, 12);
-		PerCtgInfoDto detailCtgInfo = new PerCtgInfoDto();
-		PersonInfoCategory perInfoCtg = this.perInfoByCompanyRepo
+		PerCtgInfoDto ctgDetail = new PerCtgInfoDto();
+		PersonInfoCategory ctg = this.perInfoByCompanyRepo
 				.getDetailCategoryInfo(companyId, categoryId, contractCd).orElse(null);
-		if (perInfoCtg != null) {
-			String categoryNameDefault = this.perInfoByCompanyRepo.getNameCategoryInfo(companyIdRoot,
-					perInfoCtg.getCategoryCode().toString());
-			detailCtgInfo.setCategoryName(perInfoCtg.getCategoryName().toString());
-			detailCtgInfo.setCategoryType(perInfoCtg.getCategoryType().value);
-			detailCtgInfo.setAbolition(perInfoCtg.getIsAbolition().value == 1 ? true : false);
-			detailCtgInfo.setPersonEmployeeType(perInfoCtg.getPersonEmployeeType().value);
-			if (!categoryNameDefault.equals("null")) {
-				detailCtgInfo.setCategoryNameDefault(categoryNameDefault);
+		if (ctg != null) {
+			String nameDefault = this.perInfoByCompanyRepo.getNameCategoryInfo(companyIdRoot,
+					ctg.getCategoryCode().toString());
+			ctgDetail.setCategoryName(ctg.getCategoryName().toString());
+			ctgDetail.setCategoryType(ctg.getCategoryType().value);
+			ctgDetail.setAbolition(ctg.getIsAbolition().value == 1 ? true : false);
+			ctgDetail.setPersonEmployeeType(ctg.getPersonEmployeeType().value);
+			ctgDetail.setCanAbolition(ctg.isCanAbolition());
+			if (!nameDefault.equals("null")) {
+				ctgDetail.setCategoryNameDefault(nameDefault);
 			}
 			List<PersonInfoItemDefDto> itemLst = this.pernfoItemDefRep
 					.getAllPerInfoItemDefByCategoryIdWithoutSetItem(categoryId, contractCd).stream()
 					.map(item -> PersonInfoItemDefDto.fromDomain(item)).collect(Collectors.toList());
 			if (itemLst.size() > 0) {
-				detailCtgInfo.setIsExistedItemLst(1);
+				ctgDetail.setIsExistedItemLst(1);
 
 			} else {
 
-				detailCtgInfo.setIsExistedItemLst(0);
+				ctgDetail.setIsExistedItemLst(0);
 			}
-			detailCtgInfo.setItemLst(itemLst);
-			detailCtgInfo.setSystemRequired(perInfoCtg.getIsFixed().value);
+			ctgDetail.setItemLst(itemLst);
+			ctgDetail.setSystemRequired(ctg.getIsFixed().value);
 
 		}
-		return detailCtgInfo;
+		return ctgDetail;
 	}
 
 	public List<PerInfoCtgDataDto> getAllCtgUsedByCompanyId(){
