b253ec908a7206873458fb98eb2189f0774f0402
3SI\sang.nv <sang.nv@3si.vn>
2018-06-13 11:06:07 +0700

    Update code CMF004I

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmf/004/i/cmf004.i.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmf/004/i/cmf004.i.vm.ts
@@ -1,55 +1,42 @@
-module nts.uk.com.view.cmf004.i {
+module nts.uk.com.view.cmf004.i.viewmodel {
+    export class ScreenModel {
+        elapsedTime     : KnockoutObservable<string> = ko.observable('');
+         //I2_1
+        statusProcess   : KnockoutObservable<string> = ko.observable('');
+        numberCategory  : KnockoutObservable<string> = ko.observable('');
+        employeeProcess : KnockoutObservable<string> = ko.observable('');
+        datetimeProcess : KnockoutObservable<string> = ko.observable('');
+        numberError     : KnockoutObservable<string> = ko.observable('');
+        //I4_1
+        code            : KnockoutObservable<string> = ko.observable('');
+        saveName        : KnockoutObservable<string> = ko.observable('');
+        recoveryName    : KnockoutObservable<string> = ko.observable('');
 
-    import getShared = nts.uk.ui.windows.getShared;
-    import getText = nts.uk.resource.getText;
-    export module viewmodel {
-        export class ScreenModel {
-            elapsedTime: KnockoutObservable<string>;
-             //I2_1
-            statusProcess: KnockoutObservable<string>;
-            numberCategory: KnockoutObservable<string>;
-            employeeProcess: KnockoutObservable<string>;
-            datetimeProcess: KnockoutObservable<string>;
-            numberError: KnockoutObservable<string>;
-            //I4_1
-            code: KnockoutObservable<string>;
-            saveName: KnockoutObservable<string>;
-            recoveryName: KnockoutObservable<string>;
-            // dialog mode
-            dialogMode: KnockoutObservable<string>;
-            
-            constructor() {
-                let self = this;
-                self.elapsedTime = ko.observable('00:05:12');
-                //init I2_1
-                self.statusProcess = ko.observable('テキスト');
-                self.numberCategory = ko.observable('19/33 テキスト');
-                self.employeeProcess = ko.observable('ID000000145');
-                self.datetimeProcess = ko.observable('2017/08/02');
-                self.numberError = ko.observable('2件');
-                //init I4_1
-                self.code = ko.observable('001');
-                self.saveName = ko.observable('保存セット名称 A');
-                self.recoveryName = ko.observable('保存セット名称 A_2016/06/15 13:15:20.zip');
-
-            }
-
-            start(): JQueryPromise<any> {
-                let self = this;
-                let dfd = $.Deferred();               
-                dfd.resolve(self);
-                return dfd.promise();
-            }
-
-            // close popup
-            public close(): void {
-                nts.uk.ui.windows.close();
-            }
-            
+        constructor() {
+            let self = this;
+            self.elapsedTime('00:05:12');
+            //init I2_1
+            self.statusProcess('テキスト');
+            self.numberCategory('19/33 テキスト');
+            self.employeeProcess('ID000000145');
+            self.datetimeProcess('2017/08/02');
+            self.numberError('2件');
+            //init I4_1
+            self.code('001');
+            self.saveName('保存セット名称 A');
+            self.recoveryName('保存セット名称 A_2016/06/15 13:15:20.zip');
         }
 
+        start(): JQueryPromise<any> {
+            let self = this;
+            let dfd = $.Deferred();
+            dfd.resolve(self);
+            return dfd.promise();
+        }
 
-
-
+        // close popup
+        close(): void {
+            nts.uk.ui.windows.close();
+        }
     }
 }
