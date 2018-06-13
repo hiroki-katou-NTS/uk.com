a4b033be936a2cf4ebe27bb371b10ff05e61668b
NSVN-TruongVanLai <NSVN-TruongVanLai@users.noreply.github.com>
2018-06-13 10:48:33 +0700

    fix bug #96124

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/shr/layout-control-ko-ext.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/shr/layout-control-ko-ext.ts
@@ -1826,6 +1826,7 @@ module nts.custombinding {
                     def.itemName = _.has(def, "itemName") && def.itemName || item.itemName;
                     def.itemDefId = _.has(def, "itemDefId") && def.itemDefId || item.id;
                     def.required = _.has(def, "required") && def.required || !!item.isRequired;
+                    //def.required = _.has(def, "required") ? (ko.isObservable(def.required) ? def.required : ko.observable(def.required)) : ko.observable(!!item.isRequired);
 
                     def.resourceId = _.has(def, "resourceId") && def.resourceId || undefined;
 
