8b4a31adcaee487ec703df6b5c771f8bd28f4945
TruongQuocPhong <phong_tq@nittsusystem-vn.com>
2018-06-14 14:55:42 +0700

    #fix bug 95648

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.app/src/main/java/nts/uk/ctx/at/schedule/app/find/shift/shiftcondition/shiftcondition/ShiftConditionFinder.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.app/src/main/java/nts/uk/ctx/at/schedule/app/find/shift/shiftcondition/shiftcondition/ShiftConditionFinder.java
@@ -1,18 +1,23 @@
 package nts.uk.ctx.at.schedule.app.find.shift.shiftcondition.shiftcondition;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+import nts.uk.ctx.at.schedule.dom.shift.shiftcondition.shiftcondition.ShiftCondition;
 import nts.uk.ctx.at.schedule.dom.shift.shiftcondition.shiftcondition.ShiftConditionRepository;
 import nts.uk.shr.com.context.AppContexts;
+import nts.uk.shr.com.i18n.TextResource;
 
 @Stateless
 public class ShiftConditionFinder {
 	@Inject
-	ShiftConditionRepository repo;
+	private ShiftConditionRepository repo;
 
 	/**
 	 * get all shift condition
@@ -23,5 +28,28 @@ public class ShiftConditionFinder {
 		String companyId = AppContexts.user().companyId();
 		return repo.getListShiftCondition(companyId).stream().map(domain -> ShiftConditionDto.fromDomain(domain))
 				.collect(Collectors.toList());
+	}
+	
+	public ItemConditionNames checkExistedItems(List<String> items) {
+		String companyId = AppContexts.user().companyId();
+		 List<Integer> itemNos = items.stream().map(item -> Integer.parseInt(item.replace("c", ""))).collect(Collectors.toList());
+		 Map<Integer, String> result = repo.getListShiftCondition(companyId).stream().collect(
+				 Collectors.toMap(ShiftCondition::getConditionNo, x->x.getConditionName().v()));
+		 StringBuilder nameList = new StringBuilder();
+		 String name = TextResource.localize("KSM011_75");
+		 ItemConditionNames conditionNames = new ItemConditionNames();
+		 IntStream.range(0, itemNos.size()).forEach(item ->{
+			 nameList.append(result.containsKey(itemNos.get(item)) ? result.get(itemNos.get(item)): name);
+			 if(item < itemNos.size() - 1)
+			 {
+				 nameList.append(", ");
+			 }
+				 
+			 
+		 });
+		 conditionNames.setExisted(!itemNos.stream().anyMatch(item -> !result.keySet().contains(item)));
+		 conditionNames.setItems(nameList.toString());
+		 return conditionNames;
+		
 	}
 }
