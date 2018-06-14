784ab586f8b13fc96739d3e2a5b6de2e091ca026
NSVN-NguyenXuanThanh <thanh_nx@nittsusystem-vn.com>
2018-06-14 16:08:49 +0700

    kdw/003/a

--- nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/dailyperformance/correction/datadialog/classification/AutomaticCalcAfterHours.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/dailyperformance/correction/datadialog/classification/AutomaticCalcAfterHours.java
@@ -15,13 +15,13 @@ public enum AutomaticCalcAfterHours {
 	// 申請または手入力
 	APPLYMANUALLYENTER(0, "申請または手入力"),
 	
-	/** The calculatemboss. */
-	// 打刻から計算する
-	CALCULATEMBOSS(2, "打刻から計算する"),
-
 	/** The timerecorder. */
 	// タイムレコーダーで選択
-	TIMERECORDER(1, "タイムレコーダーで選択");
+	TIMERECORDER(1, "タイムレコーダーで選択"),
+	
+	/** The calculatemboss. */
+	// 打刻から計算する
+	CALCULATEMBOSS(2, "打刻から計算する");
 
 	/** The value. */
 	public final int code;
