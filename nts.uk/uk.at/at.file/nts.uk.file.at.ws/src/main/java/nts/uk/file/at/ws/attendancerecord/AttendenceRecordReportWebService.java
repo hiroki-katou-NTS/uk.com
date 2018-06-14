2f5340d79239a6dfc90b27f10b8a5f65ccafe086
quangnt <quangnt@newwave.vn>
2018-06-14 14:31:19 +0700

    fix msg 1269, fix bug #2488

--- nts.uk/uk.at/at.file/nts.uk.file.at.ws/src/main/java/nts/uk/file/at/ws/attendancerecord/AttendenceRecordReportWebService.java
+++ nts.uk/uk.at/at.file/nts.uk.file.at.ws/src/main/java/nts/uk/file/at/ws/attendancerecord/AttendenceRecordReportWebService.java
@@ -6,16 +6,17 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 import nts.arc.layer.app.file.export.ExportServiceResult;
+import nts.arc.layer.ws.WebService;
 import nts.uk.file.at.app.export.attendancerecord.AttendanceRecordExportService;
 import nts.uk.file.at.app.export.attendancerecord.AttendanceRecordRequest;
 
 @Path("at/function/attendancerecord/report")
 @Produces("application/json")
-public class AttendenceRecordReportWebService {
-	
+public class AttendenceRecordReportWebService extends WebService {
+
 	@Inject
 	private AttendanceRecordExportService exportService;
-	
+
 	@POST
 	@Path("export")
 	public ExportServiceResult generate(AttendanceRecordRequest query) {
