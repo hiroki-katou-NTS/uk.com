961f5bc0f556aec197ccb39ad7c0b9d23c5b0d50
Shuichi Ishida <shu@silver.ocn.ne.jp>
2018-06-19 02:41:46 +0900

    update MonthlyAggregation（レスポンス改善）

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/worktime/common/subholtransferset/GetOverTimeAndTransferOrderImpl.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/worktime/common/subholtransferset/GetOverTimeAndTransferOrderImpl.java
@@ -2,6 +2,8 @@ package nts.uk.ctx.at.shared.dom.worktime.common.subholtransferset;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
@@ -10,6 +12,7 @@ import lombok.val;
 import nts.uk.ctx.at.shared.dom.worktime.algorithm.getcommonset.GetCommonSet;
 import nts.uk.ctx.at.shared.dom.worktime.common.CompensatoryOccurrenceDivision;
 import nts.uk.ctx.at.shared.dom.worktime.common.SubHolTransferSetAtr;
+import nts.uk.ctx.at.shared.dom.worktime.common.WorkTimezoneCommonSet;
 
 /**
  * 残業・振替の処理順序を取得する
@@ -26,17 +29,41 @@ public class GetOverTimeAndTransferOrderImpl implements GetOverTimeAndTransferOr
 	@Override
 	public List<OverTimeAndTransferAtr> get(String companyId, String workTimeCode, boolean reverse) {
 		
-		List<OverTimeAndTransferAtr> returnOrder = new ArrayList<>();
+		// 共通設定の取得
+		val workTimeCommonSetOpt = this.getCommonSet.get(companyId, workTimeCode);
+		
+		return this.getCommon(companyId, reverse, workTimeCommonSetOpt);
+	}
+	
+	/** 残業・振替の処理順序を取得する */
+	@Override
+	public List<OverTimeAndTransferAtr> get(String companyId, String workTimeCode, boolean reverse,
+			Map<String, WorkTimezoneCommonSet> workTimeCommonSetMap) {
+
+		return this.getCommon(companyId, reverse,
+				Optional.ofNullable(workTimeCommonSetMap.get(workTimeCode)));
+	}
+	
+	/**
+	 * 取得共通処理
+	 * @param companyId 会社ID
+	 * @param reverse 逆時系列にする
+	 * @param workTimeCommonSetOpt 就業時間帯：共通設定
+	 * @return 残業振替区分リスト　（処理順）
+	 */
+	private List<OverTimeAndTransferAtr> getCommon(String companyId, boolean reverse,
+			Optional<WorkTimezoneCommonSet> workTimeCommonSetOpt) {
+		
+		List<OverTimeAndTransferAtr> results = new ArrayList<>();
 		
 		// 共通設定の取得
-		val workTimezoneCommonSetOpt = this.getCommonSet.get(companyId, workTimeCode);
-		if (!workTimezoneCommonSetOpt.isPresent()){
-			returnOrder.add(OverTimeAndTransferAtr.OVER_TIME);
-			return returnOrder;
+		if (!workTimeCommonSetOpt.isPresent()){
+			results.add(OverTimeAndTransferAtr.OVER_TIME);
+			return results;
 		}
 		
 		// 代休振替設定を取得する
-		val subHolTimeSets = workTimezoneCommonSetOpt.get().getSubHolTimeSet();
+		val subHolTimeSets = workTimeCommonSetOpt.get().getSubHolTimeSet();
 		for (val subHolTimeSet : subHolTimeSets){
 			if (subHolTimeSet.getOriginAtr() != CompensatoryOccurrenceDivision.FromOverTime) continue;
 			val subHolTransferSet = subHolTimeSet.getSubHolTimeSet();
@@ -49,29 +76,29 @@ public class GetOverTimeAndTransferOrderImpl implements GetOverTimeAndTransferOr
 			if (transferSetAtr == SubHolTransferSetAtr.SPECIFIED_TIME_SUB_HOL) {
 				// 指定した時間を代休とする時
 				if (reverse){
-					returnOrder.add(OverTimeAndTransferAtr.OVER_TIME);
-					returnOrder.add(OverTimeAndTransferAtr.TRANSFER);
+					results.add(OverTimeAndTransferAtr.OVER_TIME);
+					results.add(OverTimeAndTransferAtr.TRANSFER);
 				}
 				else {
-					returnOrder.add(OverTimeAndTransferAtr.TRANSFER);
-					returnOrder.add(OverTimeAndTransferAtr.OVER_TIME);
+					results.add(OverTimeAndTransferAtr.TRANSFER);
+					results.add(OverTimeAndTransferAtr.OVER_TIME);
 				}
-				return returnOrder;
+				return results;
 			}
 			else {
 				// 一定時間を超えたら代休とする時
 				if (reverse){
-					returnOrder.add(OverTimeAndTransferAtr.TRANSFER);
-					returnOrder.add(OverTimeAndTransferAtr.OVER_TIME);
+					results.add(OverTimeAndTransferAtr.TRANSFER);
+					results.add(OverTimeAndTransferAtr.OVER_TIME);
 				}
 				else {
-					returnOrder.add(OverTimeAndTransferAtr.OVER_TIME);
-					returnOrder.add(OverTimeAndTransferAtr.TRANSFER);
+					results.add(OverTimeAndTransferAtr.OVER_TIME);
+					results.add(OverTimeAndTransferAtr.TRANSFER);
 				}
-				return returnOrder;
+				return results;
 			}
 		}
-		returnOrder.add(OverTimeAndTransferAtr.OVER_TIME);
-		return returnOrder;
+		results.add(OverTimeAndTransferAtr.OVER_TIME);
+		return results;
 	}
 }
