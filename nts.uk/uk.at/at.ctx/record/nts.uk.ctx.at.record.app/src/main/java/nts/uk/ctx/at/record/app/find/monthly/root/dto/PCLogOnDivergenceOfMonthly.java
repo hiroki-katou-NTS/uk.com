5d96e4ae3c2a485318e55d95d1b8dcf117eb4e97
NTS\tin_dh <tin_dh@nittsusystem.co.jp>
2018-06-18 16:37:27 +0900

    update for value type and mapping

---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/find/monthly/root/dto/PCLogOnDivergenceOfMonthly.java
@@ -0,0 +1,37 @@
+package nts.uk.ctx.at.record.app.find.monthly.root.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import nts.uk.ctx.at.record.dom.monthly.verticaltotal.workclock.pclogon.AggrPCLogonDivergence;
+import nts.uk.ctx.at.record.dom.monthly.verticaltotal.workclock.pclogon.PCLogonDivergenceOfMonthly;
+import nts.uk.ctx.at.shared.dom.attendance.util.ItemConst;
+import nts.uk.ctx.at.shared.dom.attendance.util.anno.AttendanceItemLayout;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+/** 月別実績のPCログオン時刻 + 月別実績のPCログオン乖離 */
+public class PCLogOnDivergenceOfMonthly implements ItemConst{
+
+	/** PCログオフ時刻: 集計PCログオン時刻 + PCログオフ乖離: 集計PCログオン乖離 */
+	@AttendanceItemLayout(jpPropertyName = LOGOFF, layout = LAYOUT_A)
+	private TotalPcLogonTime logOff;
+
+	/** PCログオン時刻: 集計PCログオン時刻 + PCログオン乖離: 集計PCログオン乖離 */
+	@AttendanceItemLayout(jpPropertyName = LOGON, layout = LAYOUT_B)
+	private TotalPcLogonTime logOn;
+	
+	public static PCLogOnDivergenceOfMonthly from(PCLogonDivergenceOfMonthly domain){
+		if(domain != null){
+			return new PCLogOnDivergenceOfMonthly(TotalPcLogonTime.from(domain.getLogoffDivergence()), 
+					TotalPcLogonTime.from(domain.getLogonDivergence()));
+		}
+		return null;
+	}
+	
+	public PCLogonDivergenceOfMonthly toDomain(){
+		return PCLogonDivergenceOfMonthly.of(logOn == null ? new AggrPCLogonDivergence() : logOn.toDomain(), 
+										logOff == null ? new AggrPCLogonDivergence() : logOff.toDomain());
+	}
+}
