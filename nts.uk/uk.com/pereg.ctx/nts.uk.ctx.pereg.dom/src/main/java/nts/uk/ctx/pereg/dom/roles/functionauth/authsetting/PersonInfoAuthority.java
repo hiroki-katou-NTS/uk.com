320333c457e763622b064a0d35c5c2c24a4c1129
NSVN-PhamVanDan <dan_pv@nittsusystem-vn.com>
2018-06-19 09:03:30 +0700

    edit domain with design

--- nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.dom/src/main/java/nts/uk/ctx/pereg/dom/roles/functionauth/authsetting/PersonInfoAuthority.java
+++ nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.dom/src/main/java/nts/uk/ctx/pereg/dom/roles/functionauth/authsetting/PersonInfoAuthority.java
@@ -1,47 +1,16 @@
 package nts.uk.ctx.pereg.dom.roles.functionauth.authsetting;
 
-import lombok.Getter;
-import nts.arc.layer.dom.AggregateRoot;
-import nts.uk.ctx.pereg.dom.roles.functionauth.primitive.AuthFunctionNumber;
+import nts.uk.shr.com.permit.AvailabilityPermissionBase;
+import nts.uk.shr.com.permit.RestoreAvailabilityPermission;
 
 /**
  * @author danpv
  * domain name: 個人情報の権限
  */
-@Getter
-public class PersonInfoAuthority extends AggregateRoot {
+public class PersonInfoAuthority extends AvailabilityPermissionBase {
 
-	/**
-	 * 会社ID
-	 */
-	private String companyId;
-
-	/**
-	 * ロールID
-	 */
-	private String roleId;
-
-	/**
-	 * 機能NO
-	 */
-	private AuthFunctionNumber functionNo;
-
-	/**
-	 * 利用できる
-	 */
-	private boolean available;
-
-	private PersonInfoAuthority(String companyId, String roleId, AuthFunctionNumber functionNo, boolean available) {
-		super();
-		this.companyId = companyId;
-		this.roleId = roleId;
-		this.functionNo = functionNo;
-		this.available = available;
-	}
-
-	public static PersonInfoAuthority createFromJavaType(String companyId, String roleId, int functionNo,
-			boolean available) {
-		return new PersonInfoAuthority(companyId, roleId, new AuthFunctionNumber(functionNo), available);
+	public PersonInfoAuthority(RestoreAvailabilityPermission restore) {
+		super(restore);
 	}
 
 }
--- nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.dom/src/main/java/nts/uk/ctx/pereg/dom/roles/functionauth/authsetting/PersonInfoAuthorityRepository.java
+++ nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.dom/src/main/java/nts/uk/ctx/pereg/dom/roles/functionauth/authsetting/PersonInfoAuthorityRepository.java
@@ -2,12 +2,10 @@ package nts.uk.ctx.pereg.dom.roles.functionauth.authsetting;
 
 import java.util.Map;
 
-public interface PersonInfoAuthorityRepository {
+import nts.uk.shr.com.permit.AvailabilityPermissionRepositoryBase;
+
+public interface PersonInfoAuthorityRepository extends AvailabilityPermissionRepositoryBase<PersonInfoAuthority>{
 	
 	Map<Integer, PersonInfoAuthority> getListOfRole(String companyId, String roleId);
 	
-	void add(PersonInfoAuthority auth);
-	
-	void update(PersonInfoAuthority auth);
-
 }
