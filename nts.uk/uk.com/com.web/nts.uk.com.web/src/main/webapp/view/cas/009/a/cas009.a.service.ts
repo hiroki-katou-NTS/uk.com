022cde1f4a3cc3d61b3d6df732431c012618675e
NSVN-PhamVanDan <dan_pv@nittsusystem-vn.com>
2018-06-20 15:12:40 +0700

    remove 2 paths

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cas/009/a/cas009.a.service.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cas/009/a/cas009.a.service.ts
@@ -5,9 +5,7 @@ module cas009.a {
         opt: () => ajax('com', 'ctx/sys/auth/role/get/enum/reference/range'),
         role: {
             'has': () => ajax('com', 'ctx/sys/auth/role/user/has/role/8'),
-            'get': (rids: any) => ajax('com', 'ctx/sys/auth/role/find/person/role', rids),
-            'save': (query: any) => ajax('com', 'ctx/sys/auth/role/save/person/infor', query),
-            'remove': (query: any) => ajax('com', 'ctx/sys/auth/role/remove/person/infor', query),
+            'get': (rids: any) => ajax('com', 'ctx/sys/auth/role/find/person/role', rids)
         },
         permision: {
             save: (command: any) => ajax('com', 'ctx/pereg/functions/auth/register', command),
