f94cb588f0d023460760f46abf096d39030a52fa
nws-anhnm <anhnm@newwave.vn>
2018-06-20 18:11:17 +0700

    [KWR006] add report data model

---
+++ nts.uk/uk.at/at.file/nts.uk.file.at.infra/src/main/java/nts/uk/file/at/infra/monthlyschedule/WorkplaceMonthlyReportData.java
@@ -0,0 +1,18 @@
+package nts.uk.file.at.infra.monthlyschedule;
+
+import lombok.Data;
+import nts.arc.time.GeneralDate;
+
+/**
+ * Instantiates a new daily report data.
+ * @author HoangNDH
+ */
+@Data
+public class WorkplaceMonthlyReportData {
+	
+	/** The date. */
+	public GeneralDate date;
+	
+	/** The lst workplace data. */
+	public MonthlyWorkplaceData lstWorkplaceData;
+}
