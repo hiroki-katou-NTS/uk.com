45ff8149efb27568da7637cfed9f94e039bd526e
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2018-06-21 19:24:27 +0700

    fix application version update error 2

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.infra/src/main/java/nts/uk/ctx/at/request/infra/repository/application/common/JpaApplicationRepository_New.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.infra/src/main/java/nts/uk/ctx/at/request/infra/repository/application/common/JpaApplicationRepository_New.java
@@ -27,6 +27,7 @@ public class JpaApplicationRepository_New extends JpaRepository implements Appli
 			+ "SET a.reversionReason = :reversionReason"
 			+ ", a.appReason = :appReason"
 			+ ", a.stateReflectionReal = :stateReflectionReal"
+			+ ", a.version = :version"
 			+ " WHERE a.krqdpApplicationPK.appID = :appID AND a.krqdpApplicationPK.companyID = :companyID";
 	private static final String SELECT_APP = "SELECT c FROM KrqdtApplication_New c "
 			+ "WHERE c.employeeID = :applicantSID "
@@ -92,6 +93,7 @@ public class JpaApplicationRepository_New extends JpaRepository implements Appli
 	@Override
 	public void update(Application_New application) {
 		this.getEntityManager().createQuery(UPDATE)
+			.setParameter("version", application.getVersion())
 			.setParameter("companyID", application.getCompanyID())
 			.setParameter("appID", application.getAppID())
 			.setParameter("reversionReason", application.getReversionReason().v())
@@ -105,6 +107,7 @@ public class JpaApplicationRepository_New extends JpaRepository implements Appli
 	public void updateWithVersion(Application_New application) {
 		KrqdtApplication_New krqdtApplication = this.queryProxy()
 			.find(new KrqdpApplicationPK_New(application.getCompanyID(), application.getAppID()), KrqdtApplication_New.class).get();
+		krqdtApplication.version = application.getVersion();
 		krqdtApplication.reversionReason = application.getReversionReason().v();
 		krqdtApplication.appReason = application.getAppReason().v();
 		krqdtApplication.stateReflectionReal = application.getReflectionInformation().getStateReflectionReal().value;
