1b41495eeac54945c24277c1d1375773d143ca82
NSVN-NguyenXuanThanh <thanh_nx@nittsusystem-vn.com>
2018-06-22 10:37:54 +0700

    fix bind data cell flex

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/003/a/kdw003.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/003/a/kdw003.a.vm.ts
@@ -3089,8 +3089,8 @@ module nts.uk.at.view.kdw003.a.viewmodel {
             self.messageRed(nts.uk.resource.getText('KDW003_80', [redConditionMessage]));
             self.messageNoForward(messageNotForward);
             
-            self.shortageTime("-"+nts.uk.resource.getText("KDW003_89", [self.convertToHours((Number(val191) + Number(val19))), "-"+self.convertToHours(Number(val19))]));
-            self.nextMonthTransferredMoneyTime("-"+nts.uk.resource.getText("KDW003_111", [self.convertToHours((Number(val18) + Number(val21)))]));
+            self.shortageTime(nts.uk.resource.getText("KDW003_89", [self.convertToHours((Number(val191) + Number(val19))), self.convertToHours(Number(val19))]));
+            self.nextMonthTransferredMoneyTime(nts.uk.resource.getText("KDW003_111", [self.convertToHours((Number(val18) + Number(val21)))]));
             self.noOfHolidays(Number(val189));
             self.absentDeductionTime(Number(val190));
             if (error) {
@@ -3170,12 +3170,14 @@ module nts.uk.at.view.kdw003.a.viewmodel {
 
     class CalcFlex {
         value18: any;
+        value19: any;
         value21: any;
         value189: any;
         value190: any;
         value191: any;
-        constructor(value18: any, value21: any, value189: any, value190: any, value191: any) {
+        constructor(value18: any, value19: any, value21: any, value189: any, value190: any, value191: any) {
             this.value18 = Number(value18.value);
+            this.value19 = Number(value19.value);
             this.value21 = Number(value21.value);
             this.value189 = Number(value189.value);
             this.value190 = Number(value190.value);
