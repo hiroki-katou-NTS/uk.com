e9a5b491d387bd404a8d368e8627f47319bd5bb4
hoangtv_hn <hoangtv_hn@newwave.vn>
2018-06-25 17:02:14 +0700

    [KWR006] create repository

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.infra/src/main/java/nts/uk/ctx/at/function/infra/repository/monthlyworkschedule/JpaOutputItemMonthlyWorkScheduleGetMemento.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.infra/src/main/java/nts/uk/ctx/at/function/infra/repository/monthlyworkschedule/JpaOutputItemMonthlyWorkScheduleGetMemento.java
@@ -1,47 +1,102 @@
 package nts.uk.ctx.at.function.infra.repository.monthlyworkschedule;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-import nts.uk.ctx.at.function.dom.dailyworkschedule.AttendanceItemsDisplay;
-import nts.uk.ctx.at.function.dom.dailyworkschedule.OutputItemSettingCode;
-import nts.uk.ctx.at.function.dom.dailyworkschedule.OutputItemSettingName;
 import nts.uk.ctx.at.function.dom.monthlyworkschedule.MonthlyAttendanceItemsDisplay;
 import nts.uk.ctx.at.function.dom.monthlyworkschedule.MonthlyOutputItemSettingCode;
 import nts.uk.ctx.at.function.dom.monthlyworkschedule.MonthlyOutputItemSettingName;
 import nts.uk.ctx.at.function.dom.monthlyworkschedule.OutputItemMonthlyWorkScheduleGetMemento;
 import nts.uk.ctx.at.function.dom.monthlyworkschedule.PrintSettingRemarksColumn;
+import nts.uk.ctx.at.function.infra.entity.monthlyworkschedule.KfnmtMonthlyWorkSche;
+import nts.uk.ctx.at.function.infra.entity.monthlyworkschedule.KfnmtMonthlyWorkSchePK;
+import nts.uk.shr.com.context.AppContexts;
 
-public class JpaOutputItemMonthlyWorkScheduleGetMemento implements OutputItemMonthlyWorkScheduleGetMemento{
+// TODO: Auto-generated Javadoc
+/**
+ * The Class JpaOutputItemMonthlyWorkScheduleGetMemento.
+ */
+public class JpaOutputItemMonthlyWorkScheduleGetMemento implements OutputItemMonthlyWorkScheduleGetMemento {
 
+	/** The kfnmt monthly work sche. */
+	private KfnmtMonthlyWorkSche kfnmtMonthlyWorkSche;
+
+	/**
+	 * Instantiates a new jpa output item monthly work schedule get memento.
+	 *
+	 * @param kfnmtMonthlyWorkSche
+	 *            the kfnmt monthly work sche
+	 */
+	public JpaOutputItemMonthlyWorkScheduleGetMemento(KfnmtMonthlyWorkSche kfnmtMonthlyWorkSche) {
+		this.kfnmtMonthlyWorkSche = kfnmtMonthlyWorkSche;
+		if (kfnmtMonthlyWorkSche.getId() == null) {
+			KfnmtMonthlyWorkSchePK key = new KfnmtMonthlyWorkSchePK();
+			key.setCid(AppContexts.user().companyId());
+			kfnmtMonthlyWorkSche.setId(key);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see nts.uk.ctx.at.function.dom.monthlyworkschedule.
+	 * OutputItemMonthlyWorkScheduleGetMemento#getCompanyID()
+	 */
 	@Override
 	public String getCompanyID() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.kfnmtMonthlyWorkSche.getId().getCid();
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see nts.uk.ctx.at.function.dom.monthlyworkschedule.
+	 * OutputItemMonthlyWorkScheduleGetMemento#getItemCode()
+	 */
 	@Override
 	public MonthlyOutputItemSettingCode getItemCode() {
 		// TODO Auto-generated method stub
-		return null;
+		return new MonthlyOutputItemSettingCode(this.kfnmtMonthlyWorkSche.getId().getItemCd());
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see nts.uk.ctx.at.function.dom.monthlyworkschedule.
+	 * OutputItemMonthlyWorkScheduleGetMemento#getItemName()
+	 */
 	@Override
 	public MonthlyOutputItemSettingName getItemName() {
 		// TODO Auto-generated method stub
-		return null;
+		return new MonthlyOutputItemSettingName(this.kfnmtMonthlyWorkSche.getItemName());
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see nts.uk.ctx.at.function.dom.monthlyworkschedule.
+	 * OutputItemMonthlyWorkScheduleGetMemento#getLstDisplayedAttendance()
+	 */
 	@Override
 	public List<MonthlyAttendanceItemsDisplay> getLstDisplayedAttendance() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.kfnmtMonthlyWorkSche.getLstKfnmtMonAttenDisplay().stream().map(entity -> {
+			return new MonthlyAttendanceItemsDisplay((int) entity.getId().getOrderNo(),
+					entity.getAtdDisplay().intValue());
+		}).collect(Collectors.toList());
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see nts.uk.ctx.at.function.dom.monthlyworkschedule.
+	 * OutputItemMonthlyWorkScheduleGetMemento#getPrintSettingRemarksColumn()
+	 */
 	@Override
 	public PrintSettingRemarksColumn getPrintSettingRemarksColumn() {
 		// TODO Auto-generated method stub
-		return null;
+		return PrintSettingRemarksColumn.valueOf(this.kfnmtMonthlyWorkSche.getIsPrint().intValue());
 	}
-
 
 }
