c40cae8b1261c276185bedc84e59109e58b75da0
3SI\hiep.ld <leduyhiep2612@gmail.com>
2018-06-27 09:13:22 +0700

    CMF004 update select server file + resolve error

--- nts.uk/uk.com/com.ctx/sys/assist/nts.uk.ctx.sys.assist.dom/src/main/java/nts/uk/ctx/sys/assist/dom/tablelist/TableListRepository.java
+++ nts.uk/uk.com/com.ctx/sys/assist/nts.uk.ctx.sys.assist.dom/src/main/java/nts/uk/ctx/sys/assist/dom/tablelist/TableListRepository.java
@@ -8,8 +8,6 @@ public interface TableListRepository {
 	void update(TableList domain);
 	void updateByStorageId(String storageId, String recoveryId);
 	void updateOldDataFieldByStorageId(String storageId, int oldDataValue);
-	Class<?> getTypeForTableName(String tableName);
-	String getFieldForColumnName(Class<?> tableType, String columnName);
 	List<TableList> getByOffsetAndNumber(String storeProcessingId, int offset, int number);
 	List<TableList> getByProcessingId(String storeProcessingId);
 	List<List<String>> getDataDynamic(TableList tableList);
