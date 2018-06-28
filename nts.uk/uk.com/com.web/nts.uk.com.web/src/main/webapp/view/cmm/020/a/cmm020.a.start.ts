81942aab9129f5c1450bacbdf742aae2def3a693
NWS-AnhHTH <anhhth@newwave.vn>
2018-06-28 16:23:57 +0700

    fix bug NUK-2553

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmm/020/a/cmm020.a.start.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmm/020/a/cmm020.a.start.ts
@@ -3,6 +3,12 @@ module nts.uk.com.view.cmm020.a {
         var screenModel = new viewmodel.ScreenModel();
         screenModel.start_page().done(function() {
             __viewContext.bind(screenModel);
+            var do_focus = function() {
+                var element = $("#single-list").find("tr").last();
+                element.focus();
+                screenModel.isFocus(true);
+            };
+            setTimeout(do_focus, 500);
         });
     });
 }
