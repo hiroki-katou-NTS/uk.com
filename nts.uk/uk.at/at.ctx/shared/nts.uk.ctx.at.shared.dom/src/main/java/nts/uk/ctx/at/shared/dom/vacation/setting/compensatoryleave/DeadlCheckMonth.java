a41183fc8e49dc6dc09deaf258e7f0893c3128c7
NWS-HoangNDH <hoangndh@newwave.vn>
2018-06-28 14:19:41 +0700

    [KMF001] Change nameId of DeadlCheckMonth enum

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/vacation/setting/compensatoryleave/DeadlCheckMonth.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/vacation/setting/compensatoryleave/DeadlCheckMonth.java
@@ -6,23 +6,23 @@ package nts.uk.ctx.at.shared.dom.vacation.setting.compensatoryleave;
  */
 public enum DeadlCheckMonth {
 	
-	/** The one month. */
-	ONE_MONTH(0,"1ヶ月前", "1ヶ月前"),
+	/** 1ヶ月前 */
+	ONE_MONTH(0,"Enum_DeadlCheckMonth_ONE_MONTH", "1ヶ月前"),
 	
-	/** The two month. */
-	TWO_MONTH(1,"2ヶ月前", "2ヶ月前"),
+	/** 2ヶ月前 */
+	TWO_MONTH(1,"Enum_DeadlCheckMonth_TWO_MONTH", "2ヶ月前"),
 	
-	/** The three month. */
-	THREE_MONTH(2,"3ヶ月前", "2ヶ月前"),
+	/** 3ヶ月前 */
+	THREE_MONTH(2,"Enum_DeadlCheckMonth_THREE_MONTH", "3ヶ月前"),
 	
-	/** The four month. */
-	FOUR_MONTH(3,"4ヶ月前", "3ヶ月前"),
+	/** 4ヶ月前 */
+	FOUR_MONTH(3,"Enum_DeadlCheckMonth_FOUR_MONTH", "4ヶ月前"),
 	
-	/** The five month. */
-	FIVE_MONTH(4,"5ヶ月前", "4ヶ月前"),
+	/** 5ヶ月前 */
+	FIVE_MONTH(4,"Enum_DeadlCheckMonth_FIVE_MONTH", "5ヶ月前"),
 	
-	/** The six month. */
-	SIX_MONTH(5,"6ヶ月前", "5ヶ月前");
+	/** 6ヶ月前 */
+	SIX_MONTH(5,"Enum_DeadlCheckMonth_SIX_MONTH", "6ヶ月前");
 	
 	/** The value. */
 	public int value;
