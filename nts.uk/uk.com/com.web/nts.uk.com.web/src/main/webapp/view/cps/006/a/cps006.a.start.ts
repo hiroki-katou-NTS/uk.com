a6aa7fc6a331cb4575308339d20953471a101fd3
Leu Thi Lan <lan_lt@nittsusystem-vn.com>
2018-06-28 14:49:27 +0700

    fix bug #87933

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/006/a/cps006.a.start.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/006/a/cps006.a.start.ts
@@ -8,23 +8,29 @@ module nts.uk.com.view.cps006.a {
 
 
 $(function() {
+
     $(document).on('click', '.search-btn', function(evt) {
-        let dataSoureFilter: Array<any> = $("#category_grid").igGrid("option", "dataSource");
-        if(dataSoureFilter.length > 0){
-            __viewContext["viewModel"].id(dataSoureFilter[0].id);
+        let dataSoureFilter: Array<any> = $("#category_grid").igGrid("option", "dataSource"),
+            vm: any = __viewContext["viewModel"];
+
+        if (dataSoureFilter.length > 0) {
+            vm.id(dataSoureFilter[0].id);
         }
-        __viewContext["viewModel"].isFiltered = true;
-        __viewContext["viewModel"].ctgLstFilter = dataSoureFilter;
+
+        vm.isFiltered = true;
+        vm.ctgLstFilter = dataSoureFilter;
     });
 
     $(document).on('click', '.clear-btn', function(evt) {
-        __viewContext["viewModel"].isFiltered = false;
-        __viewContext["viewModel"].categoryList.removeAll();
+        let vm: any = __viewContext["viewModel"];
+
+        vm.isFiltered = false;
+        vm.categoryList.removeAll();
+
         nts.uk.com.view.cps006.a.service.getAllCategory().done(function(data: Array<any>) {
             if (data.length > 0) {
-                if (__viewContext["viewModel"].isAbolished()) {
-
-                    __viewContext["viewModel"].categoryList(_.map(data, x => new nts.uk.com.view.cps006.a.viewmodel.CategoryInfo({
+                if (vm.isAbolished()) {
+                    vm.categoryList(_.map(data, x => new nts.uk.com.view.cps006.a.viewmodel.CategoryInfo({
                         id: x.id,
                         categoryCode: x.categoryCode,
                         categoryName: x.categoryName,
@@ -32,7 +38,7 @@ $(function() {
                         isAbolition: x.isAbolition
                     })));
                 } else {
-                    __viewContext["viewModel"].categoryList(_.map(_.filter(data, x => { return x.isAbolition == 0 }), x => new nts.uk.com.view.cps006.a.viewmodel.CategoryInfo({
+                    vm.categoryList(_.map(_.filter(data, x => { return x.isAbolition == 0 }), x => new nts.uk.com.view.cps006.a.viewmodel.CategoryInfo({
                         id: x.id,
                         categoryCode: x.categoryCode,
                         categoryName: x.categoryName,
@@ -43,9 +49,23 @@ $(function() {
                 }
             }
         });
-
-        $("#category_grid").igGrid("option", "dataSource", __viewContext["viewModel"].categoryList());
-
-
+        $("#category_grid").igGrid("option", "dataSource", vm.categoryList());
     });
+
+    $(document).on('keydown', 'input.ntsSearchBox.nts-editor.ntsSearchBox_Component', function(e) {
+        if (e.keyCode == 13) {
+            let dataSoureFilter: Array<any> = $("#category_grid").igGrid("option", "dataSource"),
+                vm: any = __viewContext["viewModel"];
+
+            if (dataSoureFilter.length > 0) {
+                vm.id(dataSoureFilter[0].id);
+                setTimeout(function() {
+                    $("#ctgName").focus();
+                }, 100);
+            }
+            vm.isFiltered = true;
+            vm.ctgLstFilter = dataSoureFilter;
+        }
+    })
+
 })
