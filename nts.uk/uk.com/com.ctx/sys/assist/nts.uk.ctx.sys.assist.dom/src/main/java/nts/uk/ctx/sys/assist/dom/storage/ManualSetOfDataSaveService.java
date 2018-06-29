6945236cf1316e465f493b99daeaf172f2c66d18
minh.nv <minh.nv@3si.vn>
2018-06-29 17:03:46 +0700

    Fix bug count category

--- nts.uk/uk.com/com.ctx/sys/assist/nts.uk.ctx.sys.assist.dom/src/main/java/nts/uk/ctx/sys/assist/dom/storage/ManualSetOfDataSaveService.java
+++ nts.uk/uk.com/com.ctx/sys/assist/nts.uk.ctx.sys.assist.dom/src/main/java/nts/uk/ctx/sys/assist/dom/storage/ManualSetOfDataSaveService.java
@@ -327,7 +327,7 @@ public class ManualSetOfDataSaveService extends ExportService<Object> {
 			// Get data from Manual Setting table
 			List<Map<String, Object>> dataSourceCsv = new ArrayList<>();
 			int offset = 0;
-			String categoryId = "";
+			List<String> categoryIds = new ArrayList<>();
 			while (true) {
 				// テーブル一覧の１行分を処理する
 				List<TableList> tableLists = repoTableList.getByOffsetAndNumber(storeProcessingId, offset,
@@ -343,8 +343,9 @@ public class ManualSetOfDataSaveService extends ExportService<Object> {
 					}
 					// テーブル一覧で次の処理行のカテゴリが異なる場合
 					// ドメインモデル「データ保存動作管理」を更新する
-					if (!tableList.getCategoryId().equals(categoryId)) {
-						categoryId = tableList.getCategoryId();
+					String categoryId = tableList.getCategoryId();
+					if (!categoryIds.contains(categoryId)) {
+						categoryIds.add(categoryId);
 						if (!repoDataSto.increaseCategoryCount(storeProcessingId)) {
 							return ResultState.INTERRUPTION;
 						}
