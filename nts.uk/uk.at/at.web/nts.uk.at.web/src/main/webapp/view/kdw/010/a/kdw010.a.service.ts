195118565061ca2890351322895a4b0d779a141e
NWS-TuanNT <tuannt@newwave.vn>
2018-06-29 17:38:15 +0700

    code UI, webservice,app A screen

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/010/a/kdw010.a.service.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/010/a/kdw010.a.service.ts
@@ -1,11 +1,27 @@
 module nts.uk.at.view.kdw010.a {
     export module service {
         var path: any = {
-            findContinuousHolCheckSet: "",
-            saveContinuousHolCheckSet: ""
+            findContinuousHolCheckSet: "at/record/erroralarm/otkcustomize/findContinuousHolCheckSet",
+            saveContinuousHolCheckSet: "at/record/erroralarm/otkcustomize/saveContinuousHolCheckSet",
+            findAllWorkTypeDto: "at/record/erroralarm/otkcustomize/findAllWorkTypeDto"
         }
+        /**
+         * find ContinuousHolCheckSet
+         */
         export function findContinuousHolCheckSet(): JQueryPromise<model.ContinuousHolCheckSet> {
             return nts.uk.request.ajax("at", path.findContinuousHolCheckSet);
+        }
+        /**
+         * save ContinuousHolCheckSet
+         */
+        export function saveContinuousHolCheckSet(continuousHolCheckSet: model.ContinuousHolCheckSet) {
+            return nts.uk.request.ajax("at", path.saveContinuousHolCheckSet, continuousHolCheckSet);
+        }
+        /**
+         * find all OtkWorkTypeDto
+         */
+        export function findAllWorkTypeDto(): JQueryPromise<Array<model.OtkWorkTypeDto>> {
+            return nts.uk.request.ajax("at", path.findAllWorkTypeDto);
         }
     }
     export module model {
@@ -15,14 +31,22 @@ module nts.uk.at.view.kdw010.a {
             useAtr: boolean;
             displayMessage: String;
             maxContinuousDays: number;
-            isUpdate: boolean;
-            constructor(targetWorkType:Array<String>,ignoreWorkType: Array<String>,useAtr: boolean, displayMessage: String,maxContinuousDays: number,isUpdate: boolean) {
+            updateMode: boolean;
+            constructor(targetWorkType: Array<String>, ignoreWorkType: Array<String>, useAtr: boolean, displayMessage: String, maxContinuousDays: number, updateMode: boolean) {
                 this.targetWorkType = targetWorkType;
                 this.ignoreWorkType = ignoreWorkType;
                 this.useAtr = useAtr;
                 this.displayMessage = displayMessage;
                 this.maxContinuousDays = maxContinuousDays;
-                this.isUpdate = isUpdate;
+                this.updateMode = updateMode;
+            }
+        }
+        export class OtkWorkTypeDto {
+            code: String;
+            name: String;
+            constructor(code: String, name: String) {
+                this.code = code;
+                this.name = name;
             }
         }
 
--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/010/a/kdw010.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/010/a/kdw010.a.vm.ts
@@ -1,14 +1,30 @@
 module nts.uk.at.view.kdw010.a {
     import blockUI = nts.uk.ui.block;
+    import getShared = nts.uk.ui.windows.getShared;
+    import setShared = nts.uk.ui.windows.setShared;
+    import modal = nts.uk.ui.windows.sub.modal;
+
+    const TARGET_TYPE = 1;
+    const IGNORE_TYPE = 2;
+    const JOIN_CHARACTOR = ", ";
     export module viewmodel {
         export class ScreenModel {
             useSet: KnockoutObservableArray<any>;
             selectUse: KnockoutObservable<number>;
             maxContinuousDay: KnockoutObservable<number>;
             enableState: KnockoutObservable<boolean>;
-            targetWorkTypeName: KnockoutObservable<string>;
-            ignoreWorkTypeName: KnockoutObservable<string>;
-            displayMessege: KnockoutObservable<string>;
+            displayMessege: KnockoutObservable<String>;
+            listWorkTypeDto: KnockoutObservableArray<model.OtkWorkTypeDto>;
+            joinNameTargetWorkType: KnockoutObservable<String>;
+            joinNameIgnoreWorkType: KnockoutObservable<String>;
+            targetType: number;
+            ignoreType: number;
+            selectedCodeTargetWorkType: KnockoutObservableArray<String>;
+            selectedCodeIgnoreWorkType: KnockoutObservableArray<String>;
+            targetWorkTypes: KnockoutObservableArray<model.OtkWorkTypeDto>;
+            ignoreWorkTypes: KnockoutObservableArray<model.OtkWorkTypeDto>;
+            updateMode: KnockoutObservable<boolean>;
+
 
             constructor() {
                 var self = this;
@@ -19,9 +35,17 @@ module nts.uk.at.view.kdw010.a {
                 self.selectUse = ko.observable(1);
                 self.maxContinuousDay = ko.observable(0);
                 self.enableState = ko.observable(true);
-                self.targetWorkTypeName = ko.observable('');
-                self.ignoreWorkTypeName = ko.observable('');
                 self.displayMessege = ko.observable('');
+                self.listWorkTypeDto = ko.observableArray([]);
+                self.joinNameTargetWorkType = ko.observable('');
+                self.joinNameIgnoreWorkType = ko.observable('');
+                self.targetType = TARGET_TYPE;
+                self.ignoreType = IGNORE_TYPE;
+                self.selectedCodeTargetWorkType = ko.observableArray([]);
+                self.selectedCodeIgnoreWorkType = ko.observableArray([]);
+                self.targetWorkTypes = ko.observableArray([]);
+                self.ignoreWorkTypes = ko.observableArray([]);
+                self.updateMode = ko.observable(false);
 
                 self.selectUse.subscribe(function(codeChanged) {
                     if (codeChanged == 1) {
@@ -30,16 +54,110 @@ module nts.uk.at.view.kdw010.a {
                         self.enableState(false);
                     }
                 });
+                self.targetWorkTypes.subscribe(function(newTargetWorkTypes: Array<model.OtkWorkTypeDto>) {
+                    self.joinNameTargetWorkType(self.joinNameWorkTypeMethod(newTargetWorkTypes));
+                    self.selectedCodeTargetWorkType(newTargetWorkTypes.map(e => e.code));
+                });
+                self.ignoreWorkTypes.subscribe(function(newIgnoreWorkTypes: Array<model.OtkWorkTypeDto>) {
+                    self.joinNameIgnoreWorkType(self.joinNameWorkTypeMethod(newIgnoreWorkTypes));
+                    self.selectedCodeIgnoreWorkType(newIgnoreWorkTypes.map(e => e.code));
+                });
 
             }
             startPage(): JQueryPromise<any> {
                 blockUI.invisible();
                 var self = this;
                 var dfd = $.Deferred();
-                blockUI.clear();
+
+                service.findContinuousHolCheckSet().done(function(continuousHolCheckSetDto: model.ContinuousHolCheckSet) {
+                    if (!_.isEmpty(continuousHolCheckSetDto)) {
+                        self.updateMode(true);
+                        self.maxContinuousDay(continuousHolCheckSetDto.maxContinuousDays);
+                        self.displayMessege(continuousHolCheckSetDto.displayMessage);
+                        var useSetNum = continuousHolCheckSetDto.useAtr ? 1 : 0;
+                        self.selectUse(useSetNum);
+                        service.findAllWorkTypeDto().done(function(listWorkType: Array<model.OtkWorkTypeDto>) {
+                            if (!_.isEmpty(listWorkType)) {
+                                self.listWorkTypeDto(listWorkType);
+                                //set list targetWorkType
+                                var targetWorkTypes = self.findWorkTypeDtoByCode(continuousHolCheckSetDto.targetWorkType);
+                                self.targetWorkTypes(targetWorkTypes);
+
+                                //set list ignoreWorkType
+                                var ignoreWorkTypes = self.findWorkTypeDtoByCode(continuousHolCheckSetDto.ignoreWorkType);
+                                self.ignoreWorkTypes(ignoreWorkTypes);
+
+                            }
+                            blockUI.clear();
+                        });
+                    }
+                });
+
                 dfd.resolve();
                 return dfd.promise();
             }
+            //find list workType by list code
+            findWorkTypeDtoByCode(codes: Array<String>): Array<model.OtkWorkTypeDto> {
+                var self = this;
+                var dataWorkType = self.listWorkTypeDto();
+                if (_.isEmpty(dataWorkType)) {
+                    return null;
+                }
+                var listWorkTypeResult: Array<model.OtkWorkTypeDto> = [];
+                _.forEach(codes, function(value) {
+                    var workType = dataWorkType.filter(e => e.code == value)[0];
+                    if (!_.isEmpty(workType)) listWorkTypeResult.push(workType);
+                });
+                return listWorkTypeResult;
+            }
+            //joint name of work type list
+            joinNameWorkTypeMethod(listWorkType: Array<model.OtkWorkTypeDto>): String {
+                var joinName: String = '';
+                if (!_.isEmpty(listWorkType)) {
+                    joinName = listWorkType[0].name;
+                    for (let i = 1; i < listWorkType.length; i++) {
+                        joinName += JOIN_CHARACTOR + listWorkType[i].name;
+                    }
+                }
+                return joinName;
+            }
+            //open dialog Kdl002
+            openKDL002Dialog(typeWorkType: any) {
+                var self = this;
+                setShared('KDL002_Multiple', true);
+                //all possible items
+                var posibleItems = self.listWorkTypeDto().map(e => e.code);
+                setShared('KDL002_AllItemObj', posibleItems);
+                //selected items
+                var selectedCode = typeWorkType == TARGET_TYPE ? self.selectedCodeTargetWorkType() : self.selectedCodeIgnoreWorkType();
+                setShared('KDL002_SelectedItemId', selectedCode);
+                modal('/view/kdl/002/a/index.xhtml').onClosed(function(): any {
+                    var newItemSelected = getShared('KDL002_SelectedNewItem');
+                    //defined target or ignore work type
+                    if (typeWorkType == TARGET_TYPE) {
+                        self.targetWorkTypes(newItemSelected);
+                    } else {
+                        self.ignoreWorkTypes(newItemSelected);
+                    }
+                });
+            }
+            //btn register click
+            registration() {
+                blockUI.invisible();
+                var self = this;
+                $('.nts-input').trigger("validate");
+                _.defer(() => {
+                    if (!$('.nts-editor').ntsError("hasError")) {
+                        var useAtr = self.selectUse() == 1 ? true : false;
+                        var continuousHolCheckSet: model.ContinuousHolCheckSet = new model.ContinuousHolCheckSet(self.selectedCodeTargetWorkType(), self.selectedCodeIgnoreWorkType(), useAtr, self.displayMessege(), self.maxContinuousDay(), self.updateMode());
+                        service.saveContinuousHolCheckSet(continuousHolCheckSet).done(function() {
+                            nts.uk.ui.dialog.info({ messageId: 'Msg_15' });
+                        });
+                    }
+                });
+                blockUI.clear();
+            }
+
         }
     }
 }
