601adeb27bbf3a6b8b3ee94553e58ec0bc0688a7
NSVN-PhamVanThanh <thanh_pv@nittsusystem-vn.com>
2018-07-03 10:11:26 +0700

    fix return null empHist for request lisst 203

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/vacation/setting/annualpaidleave/processten/AbsenceTenProcessImpl.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/vacation/setting/annualpaidleave/processten/AbsenceTenProcessImpl.java
@@ -93,7 +93,7 @@ public class AbsenceTenProcessImpl implements AbsenceTenProcess{
 		// アルゴリズム「社員所属雇用履歴を取得」を実行する(thực hiện xử lý 「社員所属雇用履歴を取得」)
 		Optional<BsEmploymentHistoryImport> empHistImport = employeeAdaptor.findEmploymentHistory(companyID, employeeID, baseDate);
 		if(!empHistImport.isPresent() || empHistImport.get().getEmploymentCode()==null){
-			throw new BusinessException("khong co employeeCode");
+			return null;
 		}
 		int isManageByTime = 0;
 		int digestiveUnit = 0;
