e17650395270382378a441305de4af01654fffdd
3SI\tam.nx <tamnguyenxuan25@gmail.com>
2018-07-03 09:05:56 +0700

    update category

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.app/src/main/java/nts/uk/ctx/exio/app/command/exo/categoryitemdata/AddCtgItemDataCommandHandler.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.app/src/main/java/nts/uk/ctx/exio/app/command/exo/categoryitemdata/AddCtgItemDataCommandHandler.java
@@ -1,26 +1,22 @@
 package nts.uk.ctx.exio.app.command.exo.categoryitemdata;
 
 import javax.ejb.Stateless;
-import javax.inject.Inject;
 import javax.transaction.Transactional;
 
 import nts.arc.layer.app.command.CommandHandler;
 import nts.arc.layer.app.command.CommandHandlerContext;
-import nts.uk.ctx.exio.dom.exo.categoryitemdata.CtgItemDataRepository;
-import nts.uk.ctx.exio.dom.exo.categoryitemdata.CtgItemData;
+
 
 @Stateless
 @Transactional
 public class AddCtgItemDataCommandHandler extends CommandHandler<CtgItemDataCommand>
 {
     
-    @Inject
-    private CtgItemDataRepository repository;
+ 
     
     @Override
     protected void handle(CommandHandlerContext<CtgItemDataCommand> context) {
-        CtgItemDataCommand addCommand = context.getCommand();
-        repository.add(new CtgItemData(addCommand.getTblAlias(), addCommand.getCategoryId(), addCommand.getDataType(), addCommand.getTableName(), addCommand.getFieldName(), addCommand.getPrimarykeyClassfication(), addCommand.getDateClassfication(), addCommand.getSpecialItem(), addCommand.getDisplayTableName(), addCommand.getDisplayClassfication(), addCommand.getItemNo(), addCommand.getItemName(), addCommand.getRequiredCategory(), addCommand.getSearchValueCd()));
+        
     
     }
 }
--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.app/src/main/java/nts/uk/ctx/exio/app/command/exo/categoryitemdata/UpdateCtgItemDataCommandHandler.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.app/src/main/java/nts/uk/ctx/exio/app/command/exo/categoryitemdata/UpdateCtgItemDataCommandHandler.java
@@ -1,13 +1,11 @@
 package nts.uk.ctx.exio.app.command.exo.categoryitemdata;
 
 import javax.ejb.Stateless;
-import javax.inject.Inject;
 import javax.transaction.Transactional;
 
 import nts.arc.layer.app.command.CommandHandler;
 import nts.arc.layer.app.command.CommandHandlerContext;
-import nts.uk.ctx.exio.dom.exo.categoryitemdata.CtgItemData;
-import nts.uk.ctx.exio.dom.exo.categoryitemdata.CtgItemDataRepository;
+
 
 
 @Stateless
@@ -15,13 +13,10 @@ import nts.uk.ctx.exio.dom.exo.categoryitemdata.CtgItemDataRepository;
 public class UpdateCtgItemDataCommandHandler extends CommandHandler<CtgItemDataCommand>
 {
     
-    @Inject
-    private CtgItemDataRepository repository;
+
     
     @Override
     protected void handle(CommandHandlerContext<CtgItemDataCommand> context) {
-        CtgItemDataCommand updateCommand = context.getCommand();
-        repository.update(new CtgItemData(updateCommand.getTblAlias(), updateCommand.getCategoryId(), updateCommand.getDataType(), updateCommand.getTableName(), updateCommand.getFieldName(), updateCommand.getPrimarykeyClassfication(), updateCommand.getDateClassfication(), updateCommand.getSpecialItem(), updateCommand.getDisplayTableName(), updateCommand.getDisplayClassfication(), updateCommand.getItemNo(), updateCommand.getItemName(), updateCommand.getRequiredCategory(), updateCommand.getSearchValueCd()));
-    
+        
     }
 }
