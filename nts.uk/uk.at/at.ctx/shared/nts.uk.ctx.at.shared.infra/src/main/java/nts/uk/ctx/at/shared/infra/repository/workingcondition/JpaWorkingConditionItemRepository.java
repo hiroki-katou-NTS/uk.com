4c991ef86507426c5677627cb3720a3f97e69b1a
NSVN-PhamVanDan <dan_pv@nittsusystem-vn.com>
2018-07-04 14:56:02 +0700

    fix bug getWorkingCondition list

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/repository/workingcondition/JpaWorkingConditionItemRepository.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/repository/workingcondition/JpaWorkingConditionItemRepository.java
@@ -409,7 +409,7 @@ public class JpaWorkingConditionItemRepository extends JpaRepository
 		}
 
 		// exclude select
-		return result.parallelStream().map(
+		return result.stream().map(
 				entity -> new WorkingConditionItem(new JpaWorkingConditionItemGetMemento(entity)))
 				.collect(Collectors.toList());
 	}
