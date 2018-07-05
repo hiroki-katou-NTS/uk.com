e6581911c5e5ee1b0c0030252a61b55edf70ec12
NSVC\hieult <hieultph03963@fpt.edu.vn>
2018-07-05 15:54:50 +0700

    lấy work place ID từ request list 478

--- nts.uk/uk.at/at.ctx/at.auth/nts.uk.ctx.at.auth.ac/src/main/java/WorkPlace/AuthWorkPlaceAdapterImpl.java
+++ nts.uk/uk.at/at.ctx/at.auth/nts.uk.ctx.at.auth.ac/src/main/java/WorkPlace/AuthWorkPlaceAdapterImpl.java
@@ -8,6 +8,8 @@ import javax.inject.Inject;
 import nts.arc.time.GeneralDate;
 import nts.uk.ctx.at.auth.dom.adapter.AuthWorkPlaceAdapter;
 import nts.uk.ctx.sys.auth.pub.employee.EmployeePublisher;
+import nts.uk.ctx.sys.auth.pub.workplace.WorkplaceListPub;
+
 
 @Stateless
 public class AuthWorkPlaceAdapterImpl implements AuthWorkPlaceAdapter{
@@ -15,9 +17,18 @@ public class AuthWorkPlaceAdapterImpl implements AuthWorkPlaceAdapter{
 	@Inject
 	private EmployeePublisher employeePublisher;
 
+	@Inject
+	private WorkplaceListPub  workplaceListPub ;
+	
 	@Override
 	public List<String> getListWorkPlaceID(String employeeID, GeneralDate referenceDate) {
 		List<String> listWorkPlaceID = employeePublisher.getListWorkPlaceID(employeeID, referenceDate);
+		return listWorkPlaceID;
+	}
+
+	@Override
+	public List<String> getWorkplaceListId(GeneralDate referenceDate, String employeeID, boolean referEmployee) {
+		List<String> listWorkPlaceID = workplaceListPub.getWorkplaceListId(referenceDate, employeeID, referEmployee);
 		return listWorkPlaceID;
 	}
 
