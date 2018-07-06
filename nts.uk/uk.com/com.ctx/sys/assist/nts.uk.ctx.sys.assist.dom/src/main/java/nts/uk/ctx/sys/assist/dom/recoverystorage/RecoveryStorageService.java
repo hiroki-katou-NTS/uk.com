161fe89e8606ad26f16313e9606b125d4d588585
ThuyetND_3S <thuyet.nd@3si.vn>
2018-07-06 09:18:49 +0700

    add source return error resultsSetting

--- nts.uk/uk.com/com.ctx/sys/assist/nts.uk.ctx.sys.assist.dom/src/main/java/nts/uk/ctx/sys/assist/dom/recoverystorage/RecoveryStorageService.java
+++ nts.uk/uk.com/com.ctx/sys/assist/nts.uk.ctx.sys.assist.dom/src/main/java/nts/uk/ctx/sys/assist/dom/recoverystorage/RecoveryStorageService.java
@@ -192,7 +192,8 @@ public class RecoveryStorageService {
 			List<String> resultsSetting = new ArrayList<>();
 			resultsSetting = this.settingDate(tableList);
 			if (resultsSetting.isEmpty()) {
-				return DataRecoveryOperatingCondition.ABNORMAL_TERMINATION;
+				continue;
+				//return DataRecoveryOperatingCondition.ABNORMAL_TERMINATION;
 			}
 
 			// 履歴区分の判別する - check history division
