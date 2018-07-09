425218c53179992e33496d76e28a45d0c7a1f3d7
anhlt-nws <anhlt@newwave.vn>
2018-07-09 16:02:57 +0700

    update new entity

--- nts.uk/uk.com/com.ctx/sys/assist/nts.uk.ctx.sys.assist.infra/src/main/java/nts/uk/ctx/sys/assist/infra/entity/mastercopy/SspmtMastercopyDataPK_.java
+++ nts.uk/uk.com/com.ctx/sys/assist/nts.uk.ctx.sys.assist.infra/src/main/java/nts/uk/ctx/sys/assist/infra/entity/mastercopy/SspmtMastercopyDataPK_.java
@@ -3,12 +3,12 @@ package nts.uk.ctx.sys.assist.infra.entity.mastercopy;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
-import nts.uk.ctx.sys.assist.infra.entity.mastercopy.SspmtMastercopyDataPK;
 
 @Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-07-09T15:57:05")
-@StaticMetamodel(SspmtMastercopyData.class)
-public class SspmtMastercopyData_ { 
+@StaticMetamodel(SspmtMastercopyDataPK.class)
+public class SspmtMastercopyDataPK_ { 
 
-    public static volatile SingularAttribute<SspmtMastercopyData, SspmtMastercopyDataPK> id;
+    public static volatile SingularAttribute<SspmtMastercopyDataPK, String> masterCopyTarget;
+    public static volatile SingularAttribute<SspmtMastercopyDataPK, String> masterCopyId;
 
 }
