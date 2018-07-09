ed8afc66b04e1465ba28bfde8d1ec4fcf0913a8c
thanhvc3si <thanh.vc@3si.com.vn>
2018-07-09 18:19:11 +0700

    create cmf002 n

---
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.app/src/main/java/nts/uk/ctx/exio/app/command/exo/awdataformat/SettingDataAWUpdateService.java
@@ -0,0 +1,28 @@
+package nts.uk.ctx.exio.app.command.exo.awdataformat;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+import nts.arc.layer.app.command.CommandHandler;
+import nts.arc.layer.app.command.CommandHandlerContext;
+import nts.uk.ctx.exio.dom.exo.dataformat.AwDataFormatSet;
+import nts.uk.ctx.exio.dom.exo.dataformat.AwDataFormatSetRepository;
+import nts.uk.ctx.exio.dom.exo.dataformat.ItemType;
+
+@Stateless
+@Transactional
+public class SettingDataAWUpdateService extends CommandHandler<AWDataFormatCommand> {
+
+	@Inject
+	AwDataFormatSetRepository awDataFormatSetRepository;
+
+	@Override
+	protected void handle(CommandHandlerContext<AWDataFormatCommand> context) {
+		AWDataFormatCommand command = context.getCommand();
+		AwDataFormatSet awDataFormatSet = new AwDataFormatSet(ItemType.CHARACTER.value, command.getCid(),
+				command.getClosedOutput(), command.getAbsenceOutput(), command.getFixedValue(),
+				command.getValueOfFixedValue(), command.getAtWorkOutput(), command.getRetirementOutput());
+		awDataFormatSetRepository.update(awDataFormatSet);
+	}
+}
--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.infra/src/main/java/nts/uk/ctx/exio/infra/repository/exo/dataformat/JpaAwDataFormatSetRepository.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.infra/src/main/java/nts/uk/ctx/exio/infra/repository/exo/dataformat/JpaAwDataFormatSetRepository.java
@@ -12,36 +12,36 @@ import nts.uk.ctx.exio.infra.entity.exo.dataformat.OiomtAwDataFormatSetPk;
 import nts.arc.layer.infra.data.JpaRepository;
 
 @Stateless
-public class JpaAwDataFormatSetRepository extends JpaRepository implements AwDataFormatSetRepository
-{
+public class JpaAwDataFormatSetRepository extends JpaRepository implements AwDataFormatSetRepository {
 
-    private static final String SELECT_ALL_QUERY_STRING = "SELECT f FROM OiomtAwDataFormatSet f";
-    private static final String SELECT_BY_KEY_STRING = SELECT_ALL_QUERY_STRING + " WHERE ";
+	private static final String SELECT_ALL_QUERY_STRING = "SELECT f FROM OiomtAwDataFormatSet f";
+	private static final String SELECT_BY_KEY_STRING = SELECT_ALL_QUERY_STRING + " WHERE ";
 
-    @Override
-    public List<AwDataFormatSet> getAllAwDataFormatSet(){
-        return this.queryProxy().query(SELECT_ALL_QUERY_STRING, OiomtAwDataFormatSet.class)
-                .getList(item -> item.toDomain());
-    }
+	@Override
+	public List<AwDataFormatSet> getAllAwDataFormatSet() {
+		return this.queryProxy().query(SELECT_ALL_QUERY_STRING, OiomtAwDataFormatSet.class)
+				.getList(item -> item.toDomain());
+	}
 
-    @Override
-    public Optional<AwDataFormatSet> getAwDataFormatSetById(){
-        return this.queryProxy().query(SELECT_BY_KEY_STRING, OiomtAwDataFormatSet.class)
-        .getSingle(c->c.toDomain());
-    }
+	@Override
+	public Optional<AwDataFormatSet> getAwDataFormatSetById(String cid) {
+		return this.queryProxy().query(SELECT_BY_KEY_STRING, OiomtAwDataFormatSet.class).setParameter("cid", cid)
+				.setParameter("cid", cid)
+				.getSingle(c -> c.toDomain());
+	}
 
-    @Override
-    public void add(AwDataFormatSet domain){
-        this.commandProxy().insert(OiomtAwDataFormatSet.toEntity(domain));
-    }
+	@Override
+	public void add(AwDataFormatSet domain) {
+		this.commandProxy().insert(OiomtAwDataFormatSet.toEntity(domain));
+	}
 
-    @Override
-    public void update(AwDataFormatSet domain){
-        this.commandProxy().update(OiomtAwDataFormatSet.toEntity(domain));
-    }
+	@Override
+	public void update(AwDataFormatSet domain) {
+		this.commandProxy().update(OiomtAwDataFormatSet.toEntity(domain));
+	}
 
-    @Override
-    public void remove(){
-        this.commandProxy().remove(OiomtAwDataFormatSet.class, new OiomtAwDataFormatSetPk()); 
-    }
+	@Override
+	public void remove() {
+		this.commandProxy().remove(OiomtAwDataFormatSet.class, new OiomtAwDataFormatSetPk());
+	}
 }
