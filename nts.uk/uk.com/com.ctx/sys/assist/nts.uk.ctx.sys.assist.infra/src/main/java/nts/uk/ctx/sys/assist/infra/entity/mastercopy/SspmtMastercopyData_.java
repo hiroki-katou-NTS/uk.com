c725e48581caf063cd2619260eca90c7dec1d55c
NWS-ThanhNC <thanhnc@newwave.vn>
2018-07-10 13:31:55 +0700

    Update MasterDataCopyEvent 初期値コピー

--- nts.uk/uk.com/com.ctx/sys/assist/nts.uk.ctx.sys.assist.infra/src/main/java/nts/uk/ctx/sys/assist/infra/entity/mastercopy/SspmtMastercopyData_.java
+++ nts.uk/uk.com/com.ctx/sys/assist/nts.uk.ctx.sys.assist.infra/src/main/java/nts/uk/ctx/sys/assist/infra/entity/mastercopy/SspmtMastercopyData_.java
@@ -3,12 +3,11 @@ package nts.uk.ctx.sys.assist.infra.entity.mastercopy;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
-import nts.uk.ctx.sys.assist.infra.entity.mastercopy.SspmtMastercopyDataPK;
 
 @Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-07-09T15:57:05")
 @StaticMetamodel(SspmtMastercopyData.class)
 public class SspmtMastercopyData_ { 
 
-    public static volatile SingularAttribute<SspmtMastercopyData, SspmtMastercopyDataPK> id;
+    public static volatile SingularAttribute<SspmtMastercopyData, SspmtMastercopyDataPK> sspmtMastercopyDataPK;
 
 }
