11144cec445647fccacbe584962b1105c8519d36
3si\minh.nv <minh.nv@3si.vn>
2018-07-11 09:56:30 +0700

    #120039

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/exo/execlog/ExecutionForm.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/exo/execlog/ExecutionForm.java
@@ -4,17 +4,13 @@ package nts.uk.ctx.exio.dom.exo.execlog;
  * 外部入出力実行形態
  */
 public enum ExecutionForm {
-	AUTOMATIC_EXECUTION(0, "自動実行"), 
-	MANUAL_EXECUTION(1, "手動実行");
+	AUTOMATIC_EXECUTION(0), // 自動実行
+	MANUAL_EXECUTION(1); // 手動実行
 
 	/** The value. */
 	public final int value;
 
-	/** The name id. */
-	public final String nameId;
-
-	private ExecutionForm(int value, String nameId) {
+	private ExecutionForm(int value) {
 		this.value = value;
-		this.nameId = nameId;
 	}
 }
