31070617f5292d312a9ca190118867499a31d5e1
DatLH_3S <dat.lh@3si.vn>
2018-07-12 10:29:56 +0700

    cmf002 X: update server

---
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/exo/condset/StandardAttr.java
@@ -0,0 +1,14 @@
+package nts.uk.ctx.exio.dom.exo.condset;
+
+public enum StandardAttr {
+	
+	USER(0),	
+	STANDARD(1);
+	
+	/** The value. */
+	public final int value;
+
+	private StandardAttr(int value) {
+		this.value = value;
+	}
+}
