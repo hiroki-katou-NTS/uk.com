2caecef825b4b57bbbd42ef21da3658ddb76ee7c
DatLH_3S <dat.lh@3si.vn>
2018-07-12 16:13:38 +0700

    cmf002 X: 起動する

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/exo/exechist/ExecHistService.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/exo/exechist/ExecHistService.java
@@ -1,19 +1,19 @@
 package nts.uk.ctx.exio.dom.exo.exechist;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+import nts.uk.ctx.exio.dom.exo.adapter.sys.auth.RoleAtr;
+import nts.uk.ctx.exio.dom.exo.adapter.sys.auth.RoleExportRepoAdapter;
+import nts.uk.ctx.exio.dom.exo.adapter.sys.auth.RoleImport;
 import nts.uk.ctx.exio.dom.exo.commonalgorithm.AcquisitionExternalOutputCategory;
 import nts.uk.ctx.exio.dom.exo.commonalgorithm.AcquisitionSettingList;
 import nts.uk.ctx.exio.dom.exo.condset.CondSet;
 import nts.uk.ctx.exio.dom.exo.condset.StandardAttr;
-import nts.uk.ctx.exio.dom.exo.condset.StdOutputCondSet;
 import nts.uk.shr.com.context.AppContexts;
 
 @Stateless
@@ -25,6 +25,9 @@ public class ExecHistService {
 	@Inject
 	private AcquisitionSettingList acquisitionSettingList;
 
+	@Inject
+	private RoleExportRepoAdapter roleExportRepoAdapter;
+
 	/**
 	 * 外部出力条件設定一覧
 	 */
@@ -32,13 +35,17 @@ public class ExecHistService {
 		CondSetAndCtg result = new CondSetAndCtg(new ArrayList<>(), new ArrayList<>());
 		String cid = AppContexts.user().companyId();
 		String employeeId = AppContexts.user().employeeId();
-		List<StdOutputCondSet> stdOutCondList;
+		// TODO roleId
+		String roleId = AppContexts.user().roles().forAttendance();
 		List<CondSet> condSetList = new ArrayList<>();
 		// imported(補助機能)「ロール」を取得する Get "role"
-		// TODO
+		Optional<RoleImport> roleOtp = roleExportRepoAdapter.findByRoleId(roleId);
+		if (!roleOtp.isPresent()) {
+			return result;
+		}
 		// ロール区分
 		// 担当権限の場合
-		if (true) {
+		if (RoleAtr.INCHARGE.equals(roleOtp.get().getAssignAtr())) {
 			// アルゴリズム「外部出力カテゴリ取得リスト」を実行する
 			result.setExOutCtgList(acquisitionExternalOutputCategory.getExternalOutputCategoryList());
 			// ドメインモデル「出力条件設定（ユーザ）」を取得する
@@ -48,7 +55,7 @@ public class ExecHistService {
 					Optional.empty());
 		}
 		// 一般権限の場合
-		else {
+		else if (RoleAtr.GENERAL.equals(roleOtp.get().getAssignAtr())) {
 			// アルゴリズム「外部出力取得設定一覧」を実行する
 			condSetList = acquisitionSettingList.getAcquisitionSettingList(cid, employeeId, StandardAttr.USER,
 					Optional.empty());
@@ -59,7 +66,6 @@ public class ExecHistService {
 		result.setCondSetList(condSetList);
 
 		// 取得した一覧の先頭に「すべて」を追加
-		// TODO
 		return result;
 	}
 
