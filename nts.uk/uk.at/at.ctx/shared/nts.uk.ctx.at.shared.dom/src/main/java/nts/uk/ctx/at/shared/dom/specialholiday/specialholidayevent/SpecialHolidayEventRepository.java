a1285c2b84d84c22eeb438c80c7f65fd30a0c34b
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2018-07-12 14:12:55 +0700

    update SpecialHolidayEvent entity

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/specialholiday/specialholidayevent/SpecialHolidayEventRepository.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/specialholiday/specialholidayevent/SpecialHolidayEventRepository.java
@@ -1,5 +1,26 @@
 package nts.uk.ctx.at.shared.dom.specialholiday.specialholidayevent;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface SpecialHolidayEventRepository {
+
+	/**
+	 * findBy CompanyId And No
+	 * 
+	 * @param companyId
+	 * @param sHsNo
+	 * @return
+	 */
+	List<SpecialHolidayEvent> findByCompanyIdAndNoLst(String companyId, List<Integer> sHsNo);
+
+	/**
+	 * findBy key
+	 * 
+	 * @param companyId
+	 * @param key
+	 * @return
+	 */
+	Optional<SpecialHolidayEvent> findByKey(String companyId, int eventNo);
 
 }
