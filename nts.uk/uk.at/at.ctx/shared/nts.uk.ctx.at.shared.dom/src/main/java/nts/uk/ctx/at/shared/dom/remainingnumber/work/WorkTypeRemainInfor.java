3d404395a179434004ae5cfd98e7db629bae462f
NTS_DoThiDu <du_dt@nittsusystem-vn.com>
2018-07-13 10:01:57 +0900

    fix bug 204

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/remainingnumber/work/WorkTypeRemainInfor.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/remainingnumber/work/WorkTypeRemainInfor.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import nts.uk.ctx.at.shared.dom.remainingnumber.interimremain.primitive.CreateAtr;
+import nts.uk.ctx.at.shared.dom.worktype.WorkTypeClassification;
 /**
  * 勤務種類別残数情報
  * @author do_dt
@@ -19,6 +20,7 @@ import nts.uk.ctx.at.shared.dom.remainingnumber.interimremain.primitive.CreateAt
 public class WorkTypeRemainInfor {
 	/**	勤務種類コード */
 	private String workTypeCode;
+	private WorkTypeClassification workTypeClass;
 	/**	作成元区分 */
 	private CreateAtr createData;
 	/**	発生使用明細 */
