8030913af8c4178689376286cda3fc3d3c7f5602
DatLH_3S <dat.lh@3si.vn>
2018-07-16 10:22:14 +0700

    KWR008: fix bug 97657

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.infra/src/main/java/nts/uk/ctx/at/function/infra/generator/annualworkschedule/JpaAnnualWorkScheduleRepository.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.infra/src/main/java/nts/uk/ctx/at/function/infra/generator/annualworkschedule/JpaAnnualWorkScheduleRepository.java
@@ -8,7 +8,6 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
