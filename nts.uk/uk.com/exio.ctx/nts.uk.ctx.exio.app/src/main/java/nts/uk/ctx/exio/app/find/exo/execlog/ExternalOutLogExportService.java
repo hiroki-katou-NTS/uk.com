f591797a0b3f71a7fa2fd1076f74d52d4e325c92
Thanh.TQ <thanh.tq@3si.vn>
2018-07-16 15:58:52 +0700

    fix wanring

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.app/src/main/java/nts/uk/ctx/exio/app/find/exo/execlog/ExternalOutLogExportService.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.app/src/main/java/nts/uk/ctx/exio/app/find/exo/execlog/ExternalOutLogExportService.java
@@ -1,9 +1,7 @@
 package nts.uk.ctx.exio.app.find.exo.execlog;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
