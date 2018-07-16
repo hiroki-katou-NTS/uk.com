c1059b863b63da144e6edba4ecc29ec39210564c
Shuichi Ishida <shu@silver.ocn.ne.jp>
2018-07-16 11:47:50 +0900

    update 期間中の積立年休残数を取得, add RequestList #201 , etc.

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/remainingnumber/reserveleave/empinfo/grantremainingdata/ReserveLeaveNumberInfo.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/remainingnumber/reserveleave/empinfo/grantremainingdata/ReserveLeaveNumberInfo.java
@@ -1,6 +1,7 @@
 package nts.uk.ctx.at.shared.dom.remainingnumber.reserveleave.empinfo.grantremainingdata;
 
 import lombok.Getter;
+import lombok.Setter;
 import nts.uk.ctx.at.shared.dom.remainingnumber.reserveleave.empinfo.grantremainingdata.daynumber.ReserveLeaveGrantDayNumber;
 import nts.uk.ctx.at.shared.dom.remainingnumber.reserveleave.empinfo.grantremainingdata.daynumber.ReserveLeaveRemainingDayNumber;
 import nts.uk.ctx.at.shared.dom.remainingnumber.reserveleave.empinfo.grantremainingdata.daynumber.ReserveLeaveUsedNumber;
@@ -21,6 +22,7 @@ public class ReserveLeaveNumberInfo {
 	/**
 	 * 残日数
 	 */
+	@Setter
 	private ReserveLeaveRemainingDayNumber remainingNumber;
 	
 	public ReserveLeaveNumberInfo(double grantDays, double usedDays,
@@ -30,4 +32,38 @@ public class ReserveLeaveNumberInfo {
 		this.remainingNumber = new ReserveLeaveRemainingDayNumber(remainDays);
 	}
 
+	/**
+	 * 使用数に日数を加算する
+	 * @param days　日数
+	 */
+	// 2018.7.15 add shuichi_ishida
+	public void addDaysToUsedNumber(double days){
+		double usedDays = this.usedNumber.getDays().v() + days;
+		Double overLimitDays = null;
+		if (this.usedNumber.getOverLimitDays().isPresent()) overLimitDays = this.usedNumber.getOverLimitDays().get().v();
+		this.usedNumber = new ReserveLeaveUsedNumber(usedDays, overLimitDays);
+	}
+	
+	/**
+	 * 残日数に日数を加算する
+	 * @param days　日数
+	 */
+	// 2018.7.15 add shuichi_ishida
+	public void addDaysToRemainingNumber(double days){
+		this.remainingNumber = new ReserveLeaveRemainingDayNumber(this.remainingNumber.v() + days);
+	}
+	
+	/**
+	 * 上限超過消滅日数に日数を加算する
+	 * @param days　日数
+	 */
+	// 2018.7.15 add shuichi_ishida
+	public void addDaysToOverLimitDays(double days){
+		double overLimitDays = 0.0;
+		if (this.usedNumber.getOverLimitDays().isPresent()) {
+			overLimitDays += this.usedNumber.getOverLimitDays().get().v();
+		}
+		overLimitDays += days;
+		this.usedNumber = new ReserveLeaveUsedNumber(this.usedNumber.getDays().v(), overLimitDays);
+	}
 }
