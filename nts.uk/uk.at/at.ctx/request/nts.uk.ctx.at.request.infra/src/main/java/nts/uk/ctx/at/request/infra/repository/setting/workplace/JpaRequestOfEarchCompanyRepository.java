9c111106c346c0a8e3b33bd8af60fa7560e88d84
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2018-07-16 17:20:37 +0700

    update behind code (view M-KAF022)

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.infra/src/main/java/nts/uk/ctx/at/request/infra/repository/setting/workplace/JpaRequestOfEarchCompanyRepository.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.infra/src/main/java/nts/uk/ctx/at/request/infra/repository/setting/workplace/JpaRequestOfEarchCompanyRepository.java
@@ -9,54 +9,52 @@ import nts.uk.ctx.at.request.dom.setting.workplace.ApprovalFunctionSetting;
 import nts.uk.ctx.at.request.dom.setting.workplace.RequestOfEachCompany;
 import nts.uk.ctx.at.request.dom.setting.workplace.RequestOfEachCompanyRepository;
 import nts.uk.ctx.at.request.infra.entity.setting.workplace.KrqstComAppConfig;
+
 /**
  * 
  * @author Doan Duy Hung
  *
  */
 @Stateless
-public class JpaRequestOfEarchCompanyRepository extends JpaRepository implements RequestOfEachCompanyRepository{
+public class JpaRequestOfEarchCompanyRepository extends JpaRepository implements RequestOfEachCompanyRepository {
 
-	private static final String FIND = "SELECT c "
-			+ "FROM KrqstComAppConfig c "
-			+ "WHERE c.companyId = :companyId ";
-	
+	private static final String FIND = "SELECT c " + "FROM KrqstComAppConfig c " + "WHERE c.companyId = :companyId ";
+
 	@Override
 	public Optional<RequestOfEachCompany> getRequestByCompany(String companyId) {
-		return this.queryProxy()
-				.query(FIND, KrqstComAppConfig.class)
-				.setParameter("companyId", companyId)
+		return this.queryProxy().query(FIND, KrqstComAppConfig.class).setParameter("companyId", companyId)
 				.getSingle(c -> c.toDomain());
 	}
-	
+
 	@Override
 	public Optional<ApprovalFunctionSetting> getFunctionSetting(String companyId, Integer appType) {
-		Optional<RequestOfEachCompany> requestOfEachCompany = this.queryProxy()
-				.query(FIND, KrqstComAppConfig.class)
-				.setParameter("companyId", companyId)
-				.getSingle(c -> c.toDomain());
-		if(!requestOfEachCompany.isPresent()){
+		Optional<RequestOfEachCompany> requestOfEachCompany = this.queryProxy().query(FIND, KrqstComAppConfig.class)
+				.setParameter("companyId", companyId).getSingle(c -> c.toDomain());
+		if (!requestOfEachCompany.isPresent()) {
 			return Optional.empty();
 		}
-		return requestOfEachCompany.get().getListApprovalFunctionSetting()
-				.stream().filter(x -> x.getAppUseSetting().getAppType().value == appType).findAny();
+		return requestOfEachCompany.get().getListApprovalFunctionSetting().stream()
+				.filter(x -> x.getAppUseSetting().getAppType().value == appType).findAny();
 	}
 
 	@Override
 	public void updateRequestOfEachCompany(RequestOfEachCompany company) {
 		KrqstComAppConfig entity = KrqstComAppConfig.toEntity(company);
 		Optional<KrqstComAppConfig> oldEntity = this.queryProxy().find(entity.companyId, KrqstComAppConfig.class);
-		oldEntity.get().selectOfApproversFlg = entity.selectOfApproversFlg;
-		oldEntity.get().krqstComAppConfigDetails = entity.krqstComAppConfigDetails;
-		this.commandProxy().update(oldEntity);
-		
+
+		oldEntity.ifPresent(_entity -> {
+			_entity.selectOfApproversFlg = entity.selectOfApproversFlg;
+			_entity.krqstComAppConfigDetails = entity.krqstComAppConfigDetails;
+			
+			this.commandProxy().update(_entity);
+		});
 	}
 
 	@Override
 	public void insertRequestOfEachCompany(RequestOfEachCompany company) {
 		KrqstComAppConfig entity = KrqstComAppConfig.toEntity(company);
 		this.commandProxy().insert(entity);
-		
+
 	}
 
 }
