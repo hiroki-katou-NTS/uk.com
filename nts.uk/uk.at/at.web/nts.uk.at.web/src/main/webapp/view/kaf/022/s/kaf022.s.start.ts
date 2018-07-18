9202192207bda48427e33f2cbad89f5b083dacc8
Nguyen Thi Hoang Yen <yen_nt@nittsusystem-vn.com>
2018-07-18 10:23:54 +0700

    KAF022: fix bug 97794

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/022/s/kaf022.s.start.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/022/s/kaf022.s.start.ts
@@ -3,9 +3,7 @@ module nts.uk.at.view.kaf022.s {
         let screenModel = new viewmodel.ScreenModel();
         screenModel.startPage().done(function() {
             __viewContext.bind(screenModel);
-//            setTimeout(function() {
-//                $("#button2").focus();
-//            }, 100);
+            $("#reason-temp").focus();
         });
     });
 }
