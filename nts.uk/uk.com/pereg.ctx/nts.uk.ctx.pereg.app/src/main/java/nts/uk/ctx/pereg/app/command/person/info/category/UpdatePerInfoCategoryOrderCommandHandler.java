674f27e3d7030d4aba89d82d200b638aeb241f94
Leu Thi Lan <lan-lt@nittsusystem-vn.com>
2018-07-18 18:44:47 +0700

    fix bug #97520

--- nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.app/src/main/java/nts/uk/ctx/pereg/app/command/person/info/category/UpdatePerInfoCategoryOrderCommandHandler.java
+++ nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.app/src/main/java/nts/uk/ctx/pereg/app/command/person/info/category/UpdatePerInfoCategoryOrderCommandHandler.java
@@ -9,9 +9,11 @@ import javax.inject.Inject;
 
 import nts.arc.layer.app.command.CommandHandler;
 import nts.arc.layer.app.command.CommandHandlerContext;
+import nts.uk.ctx.at.schedule.dom.plannedyearholiday.frame.NotUseAtr;
 import nts.uk.ctx.pereg.dom.person.info.category.PerInfoCtgByCompanyRepositoty;
 import nts.uk.ctx.pereg.dom.person.info.category.PersonInfoCtgOrder;
 import nts.uk.shr.com.context.AppContexts;
+import nts.uk.shr.com.system.config.InstalledProduct;
 
 /**
  * The class UpdatePerInfoCategoryOrderCommandHandler
@@ -28,13 +30,34 @@ public class UpdatePerInfoCategoryOrderCommandHandler extends CommandHandler<Lis
 	protected void handle(CommandHandlerContext<List<UpdatePerInfoCategoryOrderCommand>> context) {
 		List<UpdatePerInfoCategoryOrderCommand> update = context.getCommand();
 		String companyId = AppContexts.user().companyId();
+		String contractCode = AppContexts.user().contractCode();
+		int payroll = NotUseAtr.NOT_USE.value;
+		int personnel = NotUseAtr.NOT_USE.value;
+		int atttendance = NotUseAtr.NOT_USE.value;
+		List<InstalledProduct> installProduct = AppContexts.system().getInstalledProducts();
+		for (InstalledProduct productType : installProduct) {
+			switch (productType.getProductType()) {
+			case ATTENDANCE:
+				atttendance = NotUseAtr.USE.value;
+				break;
+			case PAYROLL:
+				payroll = NotUseAtr.USE.value;
+				break;
+			case PERSONNEL:
+				personnel = NotUseAtr.USE.value;
+				break;
+			default:
+				break;
+			}
+		}
 		if (update.size() == 0) {
 			return;
 		}
 		Map<String, Integer> map = update.stream().collect(Collectors.toMap(x -> x.getId(), x -> x.getOrder()));
 
 		// get all from database
-		List<PersonInfoCtgOrder> orders = this.perInfoCtgRepo.getOrderList(companyId);
+		List<PersonInfoCtgOrder> orders = this.perInfoCtgRepo.getOrderList(companyId, contractCode,
+				payroll, personnel, atttendance);
 		// update order
 		orders.forEach(order -> order.updateDisOrder(map.get(order.getCategoryId())));
 		this.perInfoCtgRepo.updatePerCtgOrder(orders);
