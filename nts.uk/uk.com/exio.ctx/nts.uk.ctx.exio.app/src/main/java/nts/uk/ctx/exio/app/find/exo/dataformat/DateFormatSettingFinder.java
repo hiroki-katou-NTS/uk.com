a0cfeeab826787b825effb8148223d9567de5f2d
congsonqs95 <congsonqs95@gmail.com>
2018-07-20 10:59:43 +0700

    [CMF002] Change repo for dateFormatSetting

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.app/src/main/java/nts/uk/ctx/exio/app/find/exo/dataformat/DateFormatSettingFinder.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.app/src/main/java/nts/uk/ctx/exio/app/find/exo/dataformat/DateFormatSettingFinder.java
@@ -5,19 +5,19 @@ import java.util.Optional;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+import nts.uk.ctx.exio.dom.exo.dataformat.init.DataFormatSettingRepository;
 import nts.uk.ctx.exio.dom.exo.dataformat.init.DateFormatSet;
-import nts.uk.ctx.exio.dom.exo.dataformat.init.DateFormatSetRepository;
 import nts.uk.shr.com.context.AppContexts;
 
 @Stateless
 public class DateFormatSettingFinder {
 
 	@Inject
-	private DateFormatSetRepository dateFormatSetRepository;
+	private DataFormatSettingRepository dataFormatSettingRepository;
 
 	public DateFormatSettingDTO getDateFormatSettingByCid() {
 		String cid = AppContexts.user().companyId();
-		Optional<DateFormatSet> dateFormatSetting = dateFormatSetRepository.getDateFormatSetByCid(cid);
+		Optional<DateFormatSet> dateFormatSetting = dataFormatSettingRepository.getDateFormatSetByCid(cid);
 		return dateFormatSetting.isPresent() ? DateFormatSettingDTO.fromDomain(dateFormatSetting.get()) : null;
 	}
 }
