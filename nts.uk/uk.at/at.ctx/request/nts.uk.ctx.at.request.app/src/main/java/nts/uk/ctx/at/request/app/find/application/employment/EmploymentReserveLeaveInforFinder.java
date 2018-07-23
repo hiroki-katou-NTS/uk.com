4c5cc4d032f5600e9e3191b37abe16331056410a
NSVN-TranThiHoa <hoa_tt@nittsusystem-vn.com>
2018-07-23 19:59:07 +0700

    KDL029: fix hiển thị data emp first + format date làm tròn sau dấu phẩy 1 chữ số

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.app/src/main/java/nts/uk/ctx/at/request/app/find/application/employment/EmploymentReserveLeaveInforFinder.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.app/src/main/java/nts/uk/ctx/at/request/app/find/application/employment/EmploymentReserveLeaveInforFinder.java
@@ -39,17 +39,17 @@ public class EmploymentReserveLeaveInforFinder {
 		if(param.getInputDate() != null){
 			GeneralDate referDate = GeneralDate.fromString(param.getInputDate(), "yyyy/MM/dd");
 			//3.積立年休取得日一覧の作成, 4.当月以降積休使用状況作成 - RequestList201
-			Optional<RsvLeaManagerImport> rsvLeaManaImport = rsvLeaManaApdater.getRsvLeaveManager(param.getListSID().get(0), referDate);
+			Optional<RsvLeaManagerImport> rsvLeaManaImport = rsvLeaManaApdater.getRsvLeaveManager(employeeInforImports.get(0).getSid(), referDate);
 			if(rsvLeaManaImport.isPresent()){
 				result.setRsvLeaManaImport(rsvLeaManaImport.get());
 			}else{
 //				List<RsvLeaGrantRemainingImport> lstRsv = new ArrayList<>();
+//				lstRsv.add(new RsvLeaGrantRemainingImport("2019/01/01","2019/05/05",3.1, 3.2,3.3));
 //				lstRsv.add(new RsvLeaGrantRemainingImport("2017/01/01","2017/05/05",1.1, 1.2,1.3));
 //				lstRsv.add(new RsvLeaGrantRemainingImport("2018/01/01","2018/05/05",2.1, 2.2,2.3));
-//				lstRsv.add(new RsvLeaGrantRemainingImport("2019/01/01","2019/05/05",3.1, 3.2,3.3));
 //				List<TmpRsvLeaveMngImport> lstTmp = new ArrayList<>();
-//				lstTmp.add(new TmpRsvLeaveMngImport("2017/03/03", "Loai1", 0.5));
 //				lstTmp.add(new TmpRsvLeaveMngImport("2018/03/03", "Loai2", 0.7));
+//				lstTmp.add(new TmpRsvLeaveMngImport("2017/03/03", "Loai1", 0.5));
 //				lstTmp.add(new TmpRsvLeaveMngImport("2019/03/03", "Loai3", 0.9));
 //				RsvLeaManagerImport dataF = new RsvLeaManagerImport(null, lstRsv,lstTmp);
 //				result.setRsvLeaManaImport(dataF);
