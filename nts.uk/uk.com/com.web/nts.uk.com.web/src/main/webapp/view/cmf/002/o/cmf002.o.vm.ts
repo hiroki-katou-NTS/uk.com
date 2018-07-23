783e67e109136fabcfd85bdd3431ddab3c237fa4
Anh Trung <trung.bv@3si.vn>
2018-07-23 18:14:59 +0700

    Commit màn R

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmf/002/o/cmf002.o.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmf/002/o/cmf002.o.vm.ts
@@ -42,6 +42,7 @@ module nts.uk.com.view.cmf002.o.viewmodel {
         isShowWorkPlaceName: KnockoutObservable<boolean>;
         isShowSelectAllButton: KnockoutObservable<boolean>;
         employeeList: KnockoutObservableArray<UnitModel> = ko.observableArray([]);
+        referenceDate: KnockoutObservable<string> = ko.observable('');
 
         constructor() {
             var self = this;
@@ -163,7 +164,7 @@ module nts.uk.com.view.cmf002.o.viewmodel {
             let self = this;
             self.next();
 
-            service.getExOutSummarySetting("conditionSetCd").done(res => {
+            service.getExOutSummarySetting(self.selectedConditionCd).done(res => {
                 self.listOutputCondition(res.ctgItemDataCustomList);
                 self.listOutputItem(res.ctdOutItemCustomList);
             }).fail(res => {
@@ -175,19 +176,26 @@ module nts.uk.com.view.cmf002.o.viewmodel {
         
         createExOutText() {
             let self = this;
-
+            
             //TODO set command
-            let command = new CreateExOutTextCommand();
+            let conditionSetCd = self.selectedConditionCd();
+            let userId = "";
+            let categoryId = "categoryId?????????????????????";
+            let startDate = self.periodDateValue().startDate;
+            let endDate = self.periodDateValue().endDate;
+            let referenceDate = self.referenceDate();
+            let standardType = true;
+            let sidList = "sidList????????????????????";
+            let command = new CreateExOutTextCommand(conditionSetCd, userId, categoryId, startDate, 
+                    endDate, referenceDate, standardType, sidList);
             service.createExOutText(command).done(res => {
                 let params = {
-                    storeProcessingId: res,
+                    processingId: res
                 };
 
                 setShared("CMF002_R_PARAMS", params);
                 nts.uk.ui.windows.sub.modal("/view/cmf/002/s/index.xhtml").onClosed(() => {
-                    //TODO
-                    //disable nut
-                    //$("").focus();
+                    $(".goback").focus();
                 });
             }).fail(res => {
                 console.log("createExOutText fail");
@@ -246,13 +254,13 @@ module nts.uk.com.view.cmf002.o.viewmodel {
 
 
                     self.applyKCP005ContentSearch(data.listEmployee);
-
+                    self.referenceDate(data.baseDate);
                 }
             }
             $('#component-items-list').ntsListComponent(self.listComponentOption);
             $('#com-ccg001').ntsGroupComponent(self.ccgcomponent);
         }
-    
+    }
 
     export class ListType {
         static EMPLOYMENT = 1;
