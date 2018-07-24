10fdcc8dd55e203c65260d2d6072084dade5ef58
NTS_DoThiDu <du_dt@nittsusystem-vn.com>
2018-07-24 20:05:57 +0900

    revert code nham

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/repository/specialholidaynew/JpaSpecialHolidayRepositoryNew.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/repository/specialholidaynew/JpaSpecialHolidayRepositoryNew.java
@@ -477,12 +477,12 @@ public class JpaSpecialHolidayRepositoryNew extends JpaRepository implements Spe
 	}
 
 	@Override
-	public Optional<Integer> findByAbsframeNo(String cid, int absFrameNo) {
+	public List<Integer> findByAbsframeNo(String cid, int absFrameNo) {
 		return this.queryProxy().query(QUEYRY_BY_ABSFRAMENO, KshstSphdAbsence.class)
 				.setParameter("companyId", cid)
 				.setParameter("absFameNo", absFrameNo)
-				.getSingle(c -> {
+				.getList().stream().map(c -> {
 					return c.pk.specialHolidayCode;
-				});
+				}).collect(Collectors.toList());
 	}
 }
