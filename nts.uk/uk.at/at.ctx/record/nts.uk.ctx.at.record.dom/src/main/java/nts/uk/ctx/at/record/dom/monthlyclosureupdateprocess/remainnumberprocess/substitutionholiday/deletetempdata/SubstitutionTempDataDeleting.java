db51f53aacbe7ee01c5cb6b9bcaf70163135fc56
Hung Tran <32724898+1capedbaldy@users.noreply.github.com>
2018-07-25 15:09:20 +0700

    kmw006 phase 2: update process

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/monthlyclosureupdateprocess/remainnumberprocess/substitutionholiday/deletetempdata/SubstitutionTempDataDeleting.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/monthlyclosureupdateprocess/remainnumberprocess/substitutionholiday/deletetempdata/SubstitutionTempDataDeleting.java
@@ -9,10 +9,8 @@ import javax.inject.Inject;
 import nts.gul.collection.CollectionUtil;
 import nts.uk.ctx.at.record.dom.monthlycommon.aggrperiod.AggrPeriodEachActualClosure;
 import nts.uk.ctx.at.shared.dom.remainingnumber.absencerecruitment.interim.InterimRecAbasMngRepository;
-import nts.uk.ctx.at.shared.dom.remainingnumber.absencerecruitment.interim.InterimRecAbsMng;
 import nts.uk.ctx.at.shared.dom.remainingnumber.interimremain.InterimRemain;
 import nts.uk.ctx.at.shared.dom.remainingnumber.interimremain.InterimRemainRepository;
-import nts.uk.ctx.at.shared.dom.remainingnumber.interimremain.primitive.DataManagementAtr;
 import nts.uk.ctx.at.shared.dom.remainingnumber.interimremain.primitive.RemainType;
 import nts.uk.shr.com.time.calendar.period.DatePeriod;
 
@@ -43,12 +41,7 @@ public class SubstitutionTempDataDeleting {
 				RemainType.PICKINGUP);
 		if (CollectionUtil.isEmpty(listTempRemain))
 			return;
-		List<InterimRecAbsMng> listRecAbsData = tempDataRepo.getRecByIdsMngAtr(
-				listTempRemain.stream().map(t -> t.getRemainManaID()).collect(Collectors.toList()),
-				DataManagementAtr.INTERIM);
-		if (CollectionUtil.isEmpty(listRecAbsData))
-			return;
-		List<String> listRecId = listRecAbsData.stream().map(r -> r.getRecruitmentMngId()).collect(Collectors.toList());
+		List<String> listRecId = listTempRemain.stream().map(r -> r.getRemainManaID()).collect(Collectors.toList());
 		tempDataRepo.deleteInterimRecMng(listRecId);
 	}
 
@@ -57,12 +50,7 @@ public class SubstitutionTempDataDeleting {
 		List<InterimRemain> listTempRemain = tempRemainRepo.getRemainBySidPriod(employeeId, period, RemainType.PAUSE);
 		if (CollectionUtil.isEmpty(listTempRemain))
 			return;
-		List<InterimRecAbsMng> listRecAbsData = tempDataRepo.getAbsByIdsMngAtr(
-				listTempRemain.stream().map(t -> t.getRemainManaID()).collect(Collectors.toList()),
-				DataManagementAtr.INTERIM);
-		if (CollectionUtil.isEmpty(listRecAbsData))
-			return;
-		List<String> listAbsId = listRecAbsData.stream().map(r -> r.getAbsenceMngId()).collect(Collectors.toList());
+		List<String> listAbsId = listTempRemain.stream().map(r -> r.getRemainManaID()).collect(Collectors.toList());
 		tempDataRepo.deleteInterimAbsMng(listAbsId);
 	}
 
