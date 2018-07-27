f3abe452b1f65e7ecbb3a88edfbe12bf5487bb85
NTS\keisuke_hoshina <keisuke_hoshina@nittsusystem.co.jp>
2018-07-27 10:36:41 +0900

    レスポンス対応(並列化処理対応) DB取得を計算外でやってもらう対応

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/dailyprocess/calc/CalculateDailyRecordServiceCenter.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/dailyprocess/calc/CalculateDailyRecordServiceCenter.java
@@ -18,6 +18,9 @@ public interface CalculateDailyRecordServiceCenter{
 	//計算
 	public List<IntegrationOfDaily> calculate(List<IntegrationOfDaily> integrationOfDaily);
 
+	//計算(会社共通のマスタを渡せる場合)
+	public List<IntegrationOfDaily> calculatePassCompanySetting(List<IntegrationOfDaily> integrationOfDaily,Optional<ManagePerCompanySet> companySet);
+	
 	//計算(就業計算と集計用)
 	public CalcStatus calculateForManageState(List<IntegrationOfDaily> integrationOfDaily,Optional<AsyncCommandHandlerContext> asyncContext, Optional<Consumer<ProcessState>> counter);
 	
