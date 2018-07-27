713a4ad7801269b9b02e22c9566c9c5d5fc44f9f
congsonqs95 <congsonqs95@gmail.com>
2018-07-27 17:27:58 +0700

    [CMF002] Fix bug screen i, k, v2

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmf/002/v2/cmf002.v2.start.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmf/002/v2/cmf002.v2.start.ts
@@ -3,6 +3,9 @@ module nts.uk.com.view.cmf002.v2 {
         __viewContext['screenModel'] = new viewmodel.ScreenModel();
         __viewContext['screenModel'].start().done(function() {
             __viewContext.bind(__viewContext['screenModel']);
+            _.defer(() => {
+                $('#V22_1').focus();
+            });
         });
     });
 }
