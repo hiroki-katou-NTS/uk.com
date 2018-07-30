c022b4db19fb317f7a2d57dc354e3f0fb94fa52e
3SI\sang.nv <sang.nv@3si.vn>
2018-07-30 18:34:42 +0700

    Update layout screen L,M

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmf/002/l/cmf002.l.start.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmf/002/l/cmf002.l.start.ts
@@ -4,7 +4,7 @@ module nts.uk.com.view.cmf002.l {
         __viewContext['screenModel'].start().done(function() {
             __viewContext.bind(__viewContext['screenModel']);
             _.defer(() => {
-                __viewContext['screenModel'].timeDataFormatSetting().fixedValue() == 1 ? $('#L10_1').focus() : $('#L2_1').focus();
+                __viewContext['screenModel'].timeDataFormatSetting().fixedValue() == 1 ? $('#L10_1_1').focus() : $('#L2_1').focus();
             });
         });
     });
