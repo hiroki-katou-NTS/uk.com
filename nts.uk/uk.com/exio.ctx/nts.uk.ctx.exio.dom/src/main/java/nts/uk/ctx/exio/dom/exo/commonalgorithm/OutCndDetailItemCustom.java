ea23770f1ca93c900aa6a834c13fa9491d06c6bd
Anh Trung <trung.bv@3si.vn>
2018-07-30 16:19:07 +0700

    Update màn R

---
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/exo/commonalgorithm/OutCndDetailItemCustom.java
@@ -0,0 +1,11 @@
+package nts.uk.ctx.exio.dom.exo.commonalgorithm;
+
+import lombok.Getter;
+import lombok.Setter;
+import nts.uk.ctx.exio.dom.exo.outcnddetail.OutCndDetailItem;
+
+@Getter
+@Setter
+public class OutCndDetailItemCustom extends OutCndDetailItem {
+	private String joinedSearchCodeList;
+}
