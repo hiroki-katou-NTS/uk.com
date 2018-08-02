e808090029e0ecd56f6960229b4f11d5146457b5
phuc.tc <phuc.tc@3si.vn>
2018-08-02 09:48:56 +0700

    [CMF002] fix bug screen P and Q

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.ws/src/main/java/nts/uk/ctx/exio/ws/exo/condset/StdOutConSetWebService.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.ws/src/main/java/nts/uk/ctx/exio/ws/exo/condset/StdOutConSetWebService.java
@@ -16,6 +16,7 @@ import nts.uk.ctx.exio.app.command.exo.condset.OutSetContentCommandHandler;
 import nts.uk.ctx.exio.app.command.exo.condset.RegisterStdOutputCondSetCommandHandler;
 import nts.uk.ctx.exio.app.command.exo.condset.RemoveStdOutputCondSetCommandHandler;
 import nts.uk.ctx.exio.app.command.exo.condset.StdOutputCondSetCommand;
+import nts.uk.ctx.exio.app.find.exo.category.ExOutCtgDto;
 import nts.uk.ctx.exio.app.find.exo.categoryitemdata.CtgItemDataDto;
 import nts.uk.ctx.exio.app.find.exo.categoryitemdata.CtgItemDataFinder;
 import nts.uk.ctx.exio.app.find.exo.condset.CondSetDto;
@@ -98,6 +99,12 @@ public class StdOutConSetWebService extends WebService {
 			@PathParam("cndSetCd") String cndSetCd) {
 		cndSetCd ="";
 		return stdOutputCondSetFinder.getConditionSetting(modeScreen,cndSetCd);
+		
+	}
+	@POST
+	@Path("getExOutCtgDto/{categoryId}")
+	public ExOutCtgDto getExOutCtgDto(@PathParam("categoryId") Integer categoryId) {
+		return stdOutputCondSetFinder.getExOutCtgDto(categoryId);
 		
 	}
 	
