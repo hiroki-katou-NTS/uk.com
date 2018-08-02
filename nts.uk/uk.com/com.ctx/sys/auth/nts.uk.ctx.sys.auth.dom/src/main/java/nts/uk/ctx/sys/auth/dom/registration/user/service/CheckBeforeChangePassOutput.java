a6b49dd3877041e4582493f6e53bc52869cd221a
NWS-AnhHTH <anhhth@newwave.vn>
2018-08-02 11:21:35 +0700

    - Fix comment

--- nts.uk/uk.com/com.ctx/sys/auth/nts.uk.ctx.sys.auth.dom/src/main/java/nts/uk/ctx/sys/auth/dom/registration/user/service/CheckBeforeChangePassOutput.java
+++ nts.uk/uk.com/com.ctx/sys/auth/nts.uk.ctx.sys.auth.dom/src/main/java/nts/uk/ctx/sys/auth/dom/registration/user/service/CheckBeforeChangePassOutput.java
@@ -6,9 +6,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import nts.uk.ctx.sys.auth.dom.registration.user.service.RegistrationUserServiceImp.PasswordMessageObject;
 
+/**
+ * Instantiates a new check before change pass output.
+ *
+ * @param error the error
+ * @param message the message
+ */
 @AllArgsConstructor
+
+/* (non-Javadoc)
+ * @see java.lang.Object#toString()
+ */
 @Data
 public class CheckBeforeChangePassOutput {
+	
+	/** The error. */
 	private boolean error;
+	
+	/** The message. */
 	private List<PasswordMessageObject> message;
 }
