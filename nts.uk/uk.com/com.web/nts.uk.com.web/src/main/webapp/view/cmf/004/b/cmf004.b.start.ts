0a9eea65974969fc39c31db2a3212464a4cdc8cc
HiepLD3S <hiep.ld@3si.vn>
2018-08-03 15:47:54 +0700

    Fix focus after select of datepicker

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmf/004/b/cmf004.b.start.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmf/004/b/cmf004.b.start.ts
@@ -1,13 +1,16 @@
 module nts.uk.com.view.cmf004.b {
     __viewContext.ready(function() {
         let screenModel = new viewmodel.ScreenModel();
-        screenModel.start().done(()=>{
+        screenModel.start().done(() => {
             __viewContext.bind(screenModel);
             $('#B5_1_horizontalScrollContainer').remove();
             $('#kcp005component').ntsListComponent(screenModel.kcp005ComponentOptionScreenG);
             $('#kcp005component1').ntsListComponent(screenModel.kcp005ComponentOptionScreenH);
-            
+
         });
         $('#B3_1').focus();
+        $('.nts-datepicker-wrapper input').on("change", (e) => {
+            e.currentTarget.focus();
+        });
     });
 }
