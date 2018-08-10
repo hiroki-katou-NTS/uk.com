b2fc0ca0d37d288f22ba301389f5a3c525c0a52f
anhlt-nws <anhlt@newwave.vn>
2018-08-10 14:56:11 +0700

    Update new entities

--- nts.uk/uk.com/com.ctx/sys/assist/nts.uk.ctx.sys.assist.infra/src/main/java/nts/uk/ctx/sys/assist/infra/entity/mastercopy/SspmtMastercopyData_.java
+++ nts.uk/uk.com/com.ctx/sys/assist/nts.uk.ctx.sys.assist.infra/src/main/java/nts/uk/ctx/sys/assist/infra/entity/mastercopy/SspmtMastercopyData_.java
@@ -1,13 +1,22 @@
 package nts.uk.ctx.sys.assist.infra.entity.mastercopy;
 
+import java.math.BigDecimal;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
+import nts.uk.ctx.sys.assist.infra.entity.mastercopy.SspmtMastercopyDataPK;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-07-09T15:57:05")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-08-10T14:54:53")
 @StaticMetamodel(SspmtMastercopyData.class)
 public class SspmtMastercopyData_ { 
 
-    public static volatile SingularAttribute<SspmtMastercopyData, SspmtMastercopyDataPK> sspmtMastercopyDataPK;
+    public static volatile SingularAttribute<SspmtMastercopyData, String> key1;
+    public static volatile SingularAttribute<SspmtMastercopyData, String> key2;
+    public static volatile SingularAttribute<SspmtMastercopyData, String> key5;
+    public static volatile SingularAttribute<SspmtMastercopyData, String> key3;
+    public static volatile SingularAttribute<SspmtMastercopyData, String> key4;
+    public static volatile SingularAttribute<SspmtMastercopyData, SspmtMastercopyDataPK> id;
+    public static volatile SingularAttribute<SspmtMastercopyData, BigDecimal> copyAtr;
+    public static volatile SingularAttribute<SspmtMastercopyData, String> tableName;
 
 }
