c55ed3a0e5be8e11a205af82f2684f15d0677a5b
anhlt <anhlt@newwave.vn>
2018-08-13 00:38:47 +0700

    Add interface & sample code

---
+++ nts.uk/uk.com/com.ctx/sys/assist/nts.uk.ctx.sys.assist.dom/src/main/java/nts/uk/ctx/sys/assist/dom/mastercopy/handler/DataCopyHandler.java
@@ -0,0 +1,12 @@
+package nts.uk.ctx.sys.assist.dom.mastercopy.handler;
+
+/**
+ * The Interface CopyHandler.
+ */
+public interface DataCopyHandler {
+	
+	/**
+	 * Handler.
+	 */
+	public void handler();
+}
---
+++ nts.uk/uk.com/com.ctx/sys/assist/nts.uk.ctx.sys.assist.infra/src/main/java/nts/uk/ctx/sys/assist/infra/repository/mastercopy/handler/KshstOvertimeFrameDataCopyHandler.java
@@ -0,0 +1,41 @@
+package nts.uk.ctx.sys.assist.infra.repository.mastercopy.handler;
+
+import lombok.Getter;
+import lombok.Setter;
+import nts.uk.ctx.sys.assist.dom.mastercopy.CopyMethod;
+import nts.uk.ctx.sys.assist.dom.mastercopy.handler.DataCopyHandler;
+
+/**
+ * The Class KshstOvertimeFrameDataCopyHandler.
+ */
+@Getter
+@Setter
+public class KshstOvertimeFrameDataCopyHandler implements DataCopyHandler {
+	
+	/** The copy method. */
+	private CopyMethod copyMethod;
+	
+	/** The company cd. */
+	private String companyCd;
+
+	/* (non-Javadoc)
+	 * @see nts.uk.ctx.sys.assist.dom.mastercopy.handler.DataCopyHandler#handler()
+	 */
+	@Override
+	public void handler() {
+		
+		// TODO: Get all company zero data
+		
+		switch (copyMethod) {
+			case REPLACE_ALL:
+				// Delete all old data
+			case ADD_NEW:
+				// Insert Data
+			case DO_NOTHING:
+				// Do nothing
+			default: 
+				break;
+		}
+	}
+
+}
