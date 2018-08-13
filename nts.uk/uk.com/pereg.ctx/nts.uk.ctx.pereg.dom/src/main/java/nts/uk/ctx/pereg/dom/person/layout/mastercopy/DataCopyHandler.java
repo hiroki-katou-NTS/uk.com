9ddfae88efa20f1f95fcc5cbbefd1d9608b83599
quangnt <quangnt@newwave.vn>
2018-08-13 17:45:17 +0700

    xử lý copy master data cho bảng PPEMT_NEW_LAYOUT, PPEMT_LAYOUT_ITEM_CLS, PPEMT_PINFO_ITEM_GROUP, PPEMT_PINFO_ITEM_DF_GROUP

---
+++ nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.dom/src/main/java/nts/uk/ctx/pereg/dom/person/layout/mastercopy/DataCopyHandler.java
@@ -0,0 +1,11 @@
+package nts.uk.ctx.pereg.dom.person.layout.mastercopy;
+
+/**
+ * The Interface DataCopyHandler.
+ */
+public interface DataCopyHandler {
+	/**
+	 * Do copy.
+	 */
+	public void doCopy();
+}
