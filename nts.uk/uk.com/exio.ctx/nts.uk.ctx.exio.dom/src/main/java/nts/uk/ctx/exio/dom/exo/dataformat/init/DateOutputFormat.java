cb69065e0754c46f12fdcfd9492792babb671717
Nguyen Xuan Tam <tamnx@3si.vn>
2018-08-13 16:10:02 +0700

    CMF002 Fixbug

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/exo/dataformat/init/DateOutputFormat.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/exo/dataformat/init/DateOutputFormat.java
@@ -6,12 +6,12 @@ package nts.uk.ctx.exio.dom.exo.dataformat.init;
  */
 public enum DateOutputFormat {
 	
-	JJYY_MM_DD(0, "JJYY/MM/DD"),
-	JJYYMMDD(1, "JJYYMMDD"),
-	YY_MM_DD(2, "YY/MM/DD"),
-	YYMMDD(3, "YYMMDD"),
-	YYYY_MM_DD(4, "YYYY/MM/DD"),
-	YYYYMMDD(5, "YYYYMMDD"),
+    YYYY_MM_DD(0, "YYYY/MM/DD"),
+    YYYYMMDD(1, "YYYYMMDD"),
+    YY_MM_DD(2, "YY/MM/DD"),
+    YYMMDD(3, "YYMMDD"),
+	JJYY_MM_DD(4, "JJYY/MM/DD"),
+	JJYYMMDD(5, "JJYYMMDD"),
 	DAY_OF_WEEK(6, "曜日");
 	
 	/** The value. */
