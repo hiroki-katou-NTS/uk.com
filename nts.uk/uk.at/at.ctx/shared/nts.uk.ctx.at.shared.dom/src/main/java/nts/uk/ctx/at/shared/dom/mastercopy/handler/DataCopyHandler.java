f34380dcc81614951812977dc8ced9fce986832d
linhtp-nws <linhtp@newwave.vn>
2018-08-13 17:08:44 +0700

    - Create copy KMFMT_RETENTION_YEARLY

---
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/mastercopy/handler/DataCopyHandler.java
@@ -0,0 +1,12 @@
+package nts.uk.ctx.at.shared.dom.mastercopy.handler;
+
+/**
+ * The Interface CopyHandler.
+ */
+public interface DataCopyHandler {
+	
+	/**
+	 * Do copy.
+	 */
+	public void doCopy();
+}
