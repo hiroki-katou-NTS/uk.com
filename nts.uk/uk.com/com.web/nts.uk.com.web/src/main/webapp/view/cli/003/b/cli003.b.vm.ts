1a33f561c6d538883a6091201885ddf7c85544ff
Nguyễn Văn Huân <huannv@2nf.com.vn>
2018-08-14 08:56:33 +0700

    fixbug datacorect

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cli/003/b/cli003.b.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cli/003/b/cli003.b.vm.ts
@@ -1111,7 +1111,7 @@ module nts.uk.com.view.cli003.b.viewmodel {
                     if (dataLogDisplaySetting) {
                         // function get logoutputItem by recordType and itemNo 
                         let dataOutPutItem = dataLogDisplaySetting.logSetOutputItems;
-                     
+                          paramLog.targetDataType= dataLogDisplaySetting.dataType;
                        
                         if (dataOutPutItem.length > 0) {
                             _.forEach(dataOutPutItem, function(dataItemNo: any) {
@@ -1542,6 +1542,10 @@ module nts.uk.com.view.cli003.b.viewmodel {
                             }
                             case ITEM_NO.ITEM_NO5: {
                                 self.columnsIgAllGrid.push(new IgGridColumnAllModel(item.itemName, "pcName", "string", ITEM_NO.ITEM_NO5));
+                                break;
+                            }
+                            case ITEM_NO.ITEM_NO6: {
+                                self.columnsIgAllGrid.push(new IgGridColumnAllModel(item.itemName, "account", "string", ITEM_NO.ITEM_NO6));
                                 break;
                             }
                             case ITEM_NO.ITEM_NO7: {
