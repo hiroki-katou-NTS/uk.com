7aa868f26cef6a416a9f5ed026b1f1f9d5117d9d
NWS-AnhHTH <anhhth@newwave.vn>
2018-08-14 19:36:45 +0700

    Implement repo execute copy process type 0

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.infra/src/main/java/nts/uk/ctx/at/request/infra/repository/mastercopy/handler/KrqstLateEarlyRequestDataCopyHandler.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.infra/src/main/java/nts/uk/ctx/at/request/infra/repository/mastercopy/handler/KrqstLateEarlyRequestDataCopyHandler.java
@@ -14,38 +14,38 @@ import nts.uk.ctx.at.request.dom.mastercopy.DataCopyHandler;
 import nts.uk.shr.com.context.AppContexts;
 
 /**
- * The Class KrqmtHdAppDispNameDataCopyHandler.
+ * The Class KrqstLateEarlyRequestDataCopyHandler.
  */
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class KrqmtHdAppDispNameDataCopyHandler implements DataCopyHandler {
-
+public class KrqstLateEarlyRequestDataCopyHandler implements DataCopyHandler {
+	
 	/** The entity manager. */
 	private EntityManager entityManager;
 	
 	/** The copy method. */
 	private CopyMethod copyMethod;
-
+	
 	/** The company Id. */
 	private String companyId;
-
+	
 	/** The insert query. */
-	private String INSERT_QUERY = "INSERT INTO KRQMT_HD_APP_DISP_NAME(CID ,HD_APP_TYPE ,DISP_NAME) VALUES (?, ?, ?);";
-	
-	/** The select by cid query. */
-	private String SELECT_BY_CID_QUERY = "SELECT CID, HD_APP_TYPE, DISP_NAME FROM KRQMT_HD_APP_DISP_NAME WHERE CID = ?";
-	
-	/** The delete by cid query. */
-	private String DELETE_BY_CID_QUERY = "DELETE FROM KRQMT_HD_APP_DISP_NAME WHERE CID = ?";
+	private String INSERT_QUERY = "INSERT INTO KRQST_LATE_EARLY_REQUESET(CID, SHOW_RESULT) VALUES (?, ?);";
 
+	/** The select by cid query. */
+	private String SELECT_BY_CID_QUERY = "SELECT CID, SHOW_RESULT FROM KRQST_LATE_EARLY_REQUESET WHERE CID = ?";
+
+	/** The delete by cid query. */
+	private String DELETE_BY_CID_QUERY = "DELETE FROM KRQST_LATE_EARLY_REQUESET WHERE CID = ?";
+	
 	/* (non-Javadoc)
 	 * @see nts.uk.ctx.at.request.dom.mastercopy.DataCopyHandler#doCopy()
 	 */
 	@Override
 	public void doCopy() {
-
+		
 		// Get all company zero data
 		Query selectQuery = this.entityManager.createNativeQuery(SELECT_BY_CID_QUERY)
 				.setParameter(1, AppContexts.user().zeroCompanyIdInContract());
@@ -64,9 +64,8 @@ public class KrqmtHdAppDispNameDataCopyHandler implements DataCopyHandler {
 				Query insertQuery = this.entityManager.createNativeQuery(insertQueryStr);
 				for (int i = 0, j = zeroCompanyDatas.length; i < j; i++) {
 					Object[] dataArr = (Object[]) zeroCompanyDatas[i];
-					insertQuery.setParameter(i * 3 + 1, this.companyId);
-					insertQuery.setParameter(i * 3 + 2, dataArr[1]);
-					insertQuery.setParameter(i * 3 + 3, dataArr[2]);
+					insertQuery.setParameter(i * 2 + 1, this.companyId);
+					insertQuery.setParameter(i * 2 + 2, dataArr[1]);
 				}
 				
 				// Run insert query
@@ -76,21 +75,20 @@ public class KrqmtHdAppDispNameDataCopyHandler implements DataCopyHandler {
 			default: 
 				break;
 		}
-
+		
 	}
 
 	/**
-	 * Instantiates a new krqmt hd app disp name data copy handler.
+	 * Instantiates a new krqst late early request data copy handler.
 	 *
 	 * @param entityManager the entity manager
 	 * @param copyMethod the copy method
 	 * @param companyId the company id
 	 */
-	public KrqmtHdAppDispNameDataCopyHandler(EntityManager entityManager, CopyMethod copyMethod, String companyId) {
+	public KrqstLateEarlyRequestDataCopyHandler(EntityManager entityManager, CopyMethod copyMethod, String companyId) {
 		super();
 		this.entityManager = entityManager;
 		this.copyMethod = copyMethod;
 		this.companyId = companyId;
 	}
-
 }
