77eb416862960de697f1ffcebafe6f7b64899465
NWS-TuanNT <tuannt@newwave.vn>
2018-08-15 08:37:15 +0700

    update copy data type 0

---
+++ nts.uk/uk.com/com.ctx/sys/assist/nts.uk.ctx.sys.assist.infra/src/main/java/nts/uk/ctx/sys/assist/infra/repository/mastercopy/handler/KrcstDvgcTimeDataCopyHandler.java
@@ -0,0 +1,100 @@
+package nts.uk.ctx.sys.assist.infra.repository.mastercopy.handler;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.apache.commons.lang3.StringUtils;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import nts.uk.ctx.sys.assist.dom.mastercopy.CopyMethod;
+import nts.uk.ctx.sys.assist.dom.mastercopy.handler.DataCopyHandler;
+import nts.uk.shr.com.context.AppContexts;
+
+/**
+ * The Class KshstOvertimeFrameDataCopyHandler.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class KrcstDvgcTimeDataCopyHandler implements DataCopyHandler {
+
+	private EntityManager entityManager;
+
+	/** The copy method. */
+	private CopyMethod copyMethod;
+
+	/** The company Id. */
+	private String companyId;
+
+	/** The insert query. */
+	private String INSERT_QUERY = "INSERT INTO KRCST_DVGC_TIME(CID ,NO,DVGC_REASON_INPUTED,DVGC_REASON_SELECTED,DVGC_TIME_NAME,DVGC_TIME_USE_SET,DVGC_TYPE,REASON_INPUT_CANCELED,REASON_SELECT_CANCELED) VALUES (?, ?, ?, ?, ?,?,?,?,?);";
+
+	/** The select by cid query. */
+	private String SELECT_BY_CID_QUERY = "SELECT CID ,NO,DVGC_REASON_INPUTED,DVGC_REASON_SELECTED,DVGC_TIME_NAME,DVGC_TIME_USE_SET,DVGC_TYPE,REASON_INPUT_CANCELED,REASON_SELECT_CANCELED FROM KRCST_DVGC_TIME WHERE CID = ?";
+
+	/** The delete by cid query. */
+	private String DELETE_BY_CID_QUERY = "DELETE FROM KRCST_DVGC_TIME WHERE CID = ?";
+	
+	private final int PARAMETER_QUANTITY = 9;
+
+	/**
+	 * Instantiates a new kshst overtime frame data copy handler.
+	 *
+	 * @param copyMethod
+	 *            the copy method
+	 * @param companyCd
+	 *            the company cd
+	 */
+	public KrcstDvgcTimeDataCopyHandler(EntityManager entityManager, CopyMethod copyMethod, String companyId) {
+		this.entityManager = entityManager;
+		this.copyMethod = copyMethod;
+		this.companyId = companyId;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see nts.uk.ctx.sys.assist.dom.mastercopy.handler.DataCopyHandler#doCopy()
+	 */
+	@Override
+	public void doCopy() {
+
+		// Get all company zero data
+		Query selectQuery = this.entityManager.createNativeQuery(SELECT_BY_CID_QUERY).setParameter(1,
+				AppContexts.user().zeroCompanyIdInContract());
+		Object[] zeroCompanyDatas = selectQuery.getResultList().toArray();
+
+		switch (copyMethod) {
+		case REPLACE_ALL:
+			Query deleteQuery = this.entityManager.createNativeQuery(DELETE_BY_CID_QUERY).setParameter(1,
+					this.companyId);
+			deleteQuery.executeUpdate();
+		case ADD_NEW:
+			String insertQueryStr = StringUtils.repeat(INSERT_QUERY, zeroCompanyDatas.length);
+			Query insertQuery = this.entityManager.createNativeQuery(insertQueryStr);
+			for (int i = 0, j = zeroCompanyDatas.length; i < j; i++) {
+				Object[] dataArr = (Object[]) zeroCompanyDatas[i];
+				insertQuery.setParameter(i * PARAMETER_QUANTITY + 1, this.companyId);
+				insertQuery.setParameter(i * PARAMETER_QUANTITY + 2, dataArr[1]);
+				insertQuery.setParameter(i * PARAMETER_QUANTITY + 3, dataArr[2]);
+				insertQuery.setParameter(i * PARAMETER_QUANTITY + 4, dataArr[3]);
+				insertQuery.setParameter(i * PARAMETER_QUANTITY + 5, dataArr[4]);
+				insertQuery.setParameter(i * PARAMETER_QUANTITY + 6, dataArr[5]);
+				insertQuery.setParameter(i * PARAMETER_QUANTITY + 7, dataArr[6]);
+				insertQuery.setParameter(i * PARAMETER_QUANTITY + 8, dataArr[7]);
+				insertQuery.setParameter(i * PARAMETER_QUANTITY + 9, dataArr[8]);
+			}
+			// Run insert query
+			if(!insertQueryStr.equals("")) insertQuery.executeUpdate();
+		case DO_NOTHING:
+			// Do nothing
+		default:
+			break;
+		}
+	}
+
+}
