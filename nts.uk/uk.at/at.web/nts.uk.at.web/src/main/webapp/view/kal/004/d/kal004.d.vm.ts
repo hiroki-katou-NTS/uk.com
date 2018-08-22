aa53978737e98403b4ccd9b30dd5b7ecc2b50cd0
MinhVV <minhvv@2nf.com.vn>
2018-08-22 14:59:02 +0700

    2NF_Nguyễn Văn Huân fix bug 99646

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kal/004/d/kal004.d.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kal/004/d/kal004.d.vm.ts
@@ -21,9 +21,7 @@ module nts.uk.com.view.kal004.d.viewmodel {
             self.endMonth = ko.observable(self.getParam.endMonth);
         }
         Decide(): any {
-            var self = this;
-            
-            if (self.checkPeriod()) {
+            var self = this;               
                let dataSetShare = self.getDataShare();
             nts.uk.ui.windows.setShared("extractionMonthly", dataSetShare);
 
@@ -36,7 +34,7 @@ module nts.uk.com.view.kal004.d.viewmodel {
 
                 self.closeDialog();
             }
-        }
+        
             }
         private validateSelectMonth(startMonth: number, endMonth: number):boolean {
             let self = this;
