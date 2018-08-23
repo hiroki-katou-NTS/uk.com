627b7f444c42a50d36854ceaf4b34b82a459f9ea
NTS_DoThiDu <du_dt@nittsusystem-vn.com>
2018-08-23 18:11:51 +0900

    fix loi 暫定データの登録

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/remainingnumber/algorithm/InterimRemainOffDateCreateDataImpl.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/remainingnumber/algorithm/InterimRemainOffDateCreateDataImpl.java
@@ -449,11 +449,11 @@ public class InterimRemainOffDateCreateDataImpl implements InterimRemainOffDateC
 		if(appInfor.getAppType() == ApplicationType.BREAK_TIME_APPLICATION) {
 			// 休日出勤申請から代休振替情報を作成する
 			DayoffTranferInfor tranferInforFromHoliday = this.tranferInforFromHolidayWork(cid, dayOffTimeIsUse, appInfor, remainInfor, createAtr);
-			outputData.setDayOffTranfer(Optional.of(tranferInforFromHoliday));
+			outputData.setDayOffTranfer(tranferInforFromHoliday == null ? Optional.empty() : Optional.of(tranferInforFromHoliday));
 		} else {
 			//休日出勤以外の申請から代休振替情報を作成する
 			DayoffTranferInfor tranferInforNotFromHoliday = this.transferInforFromNotHolidayWork(cid, dayOffTimeIsUse, appInfor, createInfo, remainInfor, createAtr);
-			outputData.setDayOffTranfer(Optional.of(tranferInforNotFromHoliday));
+			outputData.setDayOffTranfer(tranferInforNotFromHoliday == null ? Optional.empty() : Optional.of(tranferInforNotFromHoliday));
 		}
 		
 		return outputData;
@@ -508,13 +508,13 @@ public class InterimRemainOffDateCreateDataImpl implements InterimRemainOffDateC
 				//振替用就業時間帯コード：労働条件項目.区分別勤務.休日出勤時.就業時間帯コード
 				Optional<WorkingConditionItem> coditionInfo = conditionService.findWorkConditionByEmployee(sid, baseDate);
 				if(!coditionInfo.isPresent()) {
-					return null;
+					return "000";
 				} else {
 					coditionInfo.get().getWorkCategory().getHolidayWork().getWorkTimeCode();
 				}
 			}
 		}
-		return null;
+		return "000";
 	}
 
 	@Override
