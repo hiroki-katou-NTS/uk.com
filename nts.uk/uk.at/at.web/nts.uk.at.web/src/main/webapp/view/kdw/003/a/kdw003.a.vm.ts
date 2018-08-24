f5172439664f56d79637eb5270e61c0aa78030d5
Hung Tran <32724898+1capedbaldy@users.noreply.github.com>
2018-08-24 14:25:30 +0700

    kdw003: fix bug #99790

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/003/a/kdw003.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/003/a/kdw003.a.vm.ts
@@ -1015,72 +1015,62 @@ module nts.uk.at.view.kdw003.a.viewmodel {
                     }
                     dataParent["monthValue"] = self.valueUpdateMonth;
                 }
-
-                let checkDailyChange = (dataChangeProcess.length > 0 || dataCheckSign.length > 0 || dataCheckApproval.length > 0) && checkDataCare;
-                if (checkDailyChange || self.valueUpdateMonth != null) {
-                    let dfd = $.Deferred();
-                    service.calculation(dataParent).done((data) => {
-                        self.valueUpdateMonth = null;
-                        self.initScreenSPR = 1;
-                        self.clickFromExtract = false;
-                        self.showTextStyle = false;
-                        dataChange = {};
-                        if (_.isEmpty(data.resultError)) {
-                            self.lstDomainEdit = data.calculatedRows;
-                            let lstValue = data.resultValues;
-                            _.forEach(self.dpData, row => {
-                                let cellDatas = row.cellDatas;
-                                let rrow = _.find(lstValue, (r: any) => { return row.employeeId == r.employeeId && r.date == row.date });
-                                _.forEach(cellDatas, cell => {
-                                    let editedCell = _.find(dataChangeProcess, (item: any) => { return (item.rowId.indexOf(row.id) >= 0 && item.columnKey == cell.columnKey); });
-                                    if (editedCell == null || updateAll) {
-                                        let itemId = self.getItemIdFromColumnKey(cell.columnKey);
-                                        let itemValue = _.find(rrow.items, (i: any) => { return i.itemId == itemId });
-                                        if (itemValue)
-                                            $("#dpGrid").mGrid("updateCell", row.id, cell.columnKey, itemValue.value == null ? "" : itemValue.value);
-                                    } 
-                                });
+                let dfd = $.Deferred();
+                service.calculation(dataParent).done((data) => {
+                    self.valueUpdateMonth = null;
+                    self.initScreenSPR = 1;
+                    self.clickFromExtract = false;
+                    self.showTextStyle = false;
+                    dataChange = {};
+                    if (_.isEmpty(data.resultError)) {
+                        self.lstDomainEdit = data.calculatedRows;
+                        let lstValue = data.resultValues;
+                        _.forEach(self.dpData, row => {
+                            let cellDatas = row.cellDatas;
+                            let rrow = _.find(lstValue, (r: any) => { return row.employeeId == r.employeeId && r.date == row.date });
+                            _.forEach(cellDatas, cell => {
+                                let editedCell = _.find(dataChangeProcess, (item: any) => { return (item.rowId.indexOf(row.id) >= 0 && item.columnKey == cell.columnKey); });
+                                if (editedCell == null || updateAll) {
+                                    let itemId = self.getItemIdFromColumnKey(cell.columnKey);
+                                    let itemValue = _.find(rrow.items, (i: any) => { return i.itemId == itemId });
+                                    if (itemValue)
+                                        $("#dpGrid").mGrid("updateCell", "_" + row.id, cell.columnKey, itemValue.value == null ? "" : itemValue.value);
+                                } 
                             });
-                            self.flagCalculation = true;
-                            nts.uk.ui.block.clear();
-                        } else {
-                            nts.uk.ui.block.clear();
-                            if (data[0] != undefined) {
-                                self.listCareError(data[0])
-                                // nts.uk.ui.dialog.alertError({ messageId: "Msg_996" })
-                            }
-                            if (data[1] != undefined) {
-                                self.listCareInputError(data[1])
-                                // nts.uk.ui.dialog.alertError({ messageId: "Msg_1108" })
-                            }
-                            if (data[2] != undefined) {
-                                self.listCheckHolidays(data[2]);
-                                self.loadRowScreen(false);
-                            }
-
-                            if (data[3] != undefined) {
-                                self.listCheck28(data[3]);
-                            }
-
-                            if (data[4] != undefined) {
-                                self.listCheckDeviation = data[4];
-                            }
-                            self.showErrorDialog();
-                        }
-                        dfd.resolve();
-                    }).fail((data) => {
+                        });
+                        self.flagCalculation = true;
                         nts.uk.ui.block.clear();
-                        nts.uk.ui.dialog.alert(data.message);
-                        dfd.resolve();
-                    });
-                    dfd.promise();
-                } else {
-                    nts.uk.ui.block.clear();
-                    if (!checkDataCare) {
-                        // nts.uk.ui.dialog.alertError({ messageId: "Msg_996" })
+                    } else {
+                        nts.uk.ui.block.clear();
+                        if (data[0] != undefined) {
+                            self.listCareError(data[0])
+                            // nts.uk.ui.dialog.alertError({ messageId: "Msg_996" })
+                        }
+                        if (data[1] != undefined) {
+                            self.listCareInputError(data[1])
+                            // nts.uk.ui.dialog.alertError({ messageId: "Msg_1108" })
+                        }
+                        if (data[2] != undefined) {
+                            self.listCheckHolidays(data[2]);
+                            self.loadRowScreen(false);
+                        }
+
+                        if (data[3] != undefined) {
+                            self.listCheck28(data[3]);
+                        }
+
+                        if (data[4] != undefined) {
+                            self.listCheckDeviation = data[4];
+                        }
                         self.showErrorDialog();
                     }
-                }
+                    dfd.resolve();
+                }).fail((data) => {
+                    nts.uk.ui.block.clear();
+                    nts.uk.ui.dialog.alert(data.message);
+                    dfd.resolve();
+                });
+                dfd.promise();
             }
         }
 
