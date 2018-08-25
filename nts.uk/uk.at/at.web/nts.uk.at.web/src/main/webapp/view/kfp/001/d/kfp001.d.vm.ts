9dc2b03d7bc8fbd44b567fd929d67bb663aec15d
TruongQuocPhong <phong_tq@nittsusystem-vn.com>
2018-08-25 09:02:48 +0700

    #fix bug  99746

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kfp/001/d/kfp001.d.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kfp/001/d/kfp001.d.vm.ts
@@ -31,6 +31,16 @@ module nts.uk.at.view.kfp001.d {
                 let self = this;
                 $("#button-2D").focus();
                 let peopleCo = nts.uk.ui.windows.getShared("KFP001_DATAC_SELECT");
+                $(document).on('click', '[data-prevent-click="true"]', evt => {
+                    $.blockUI({
+                        message: null,
+                        overlayCSS: { opacity: 0 },
+                    });
+                }).ajaxStop(() => {
+                    setTimeout(() => {
+                        $.unblockUI();
+                    }, 500);
+                });
                 if (peopleCo != self.peopleNo()) {
                     self.peopleNo(peopleCo);
                 }
@@ -40,7 +50,7 @@ module nts.uk.at.view.kfp001.d {
 
                 let self = this;
                 nts.uk.ui.block.invisible();
-                
+
                 let listEmployeeId = _.map(_.filter(self.listEmp(), (v) => _.includes(self.listSelect(), v.employeeCode)), (item) => {
                     return item.employeeId;
                 });
@@ -104,13 +114,9 @@ module nts.uk.at.view.kfp001.d {
                     nts.uk.ui.windows.setShared("KFP001_DATAD", data);
                     nts.uk.ui.windows.setShared("KFP001_DATA_EXC", exc);
                     nts.uk.ui.windows.setShared("KFP001_DATAE", addAggrPeriodCommand);
-                    setTimeout(function() {
-                        nts.uk.ui.block.invisible();
-                        $("#button-2D").on('dblclick', (evt) => { evt.preventDefault() })
                         nts.uk.ui.windows.sub.modal('/view/kfp/001/e/index.xhtml').onClosed(() => {
                             nts.uk.ui.block.clear();
                         });
-                    }, 500);
                 }).fail(function(res) {
                     nts.uk.ui.dialog.alertError(res.message);
                 }).always(function() {
