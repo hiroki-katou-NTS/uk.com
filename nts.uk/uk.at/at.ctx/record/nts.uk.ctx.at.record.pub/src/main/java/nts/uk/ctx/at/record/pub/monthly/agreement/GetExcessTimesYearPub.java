501cfa37fbbb2137f93116719e59553c83d049ce
Shuichi Ishida <shu@silver.ocn.ne.jp>
2018-08-28 14:03:33 +0900

    update RequestList#458（年間超過回数の取得）

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.pub/src/main/java/nts/uk/ctx/at/record/pub/monthly/agreement/GetExcessTimesYearPub.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.pub/src/main/java/nts/uk/ctx/at/record/pub/monthly/agreement/GetExcessTimesYearPub.java
@@ -1,5 +1,6 @@
 package nts.uk.ctx.at.record.pub.monthly.agreement;
 
+import nts.uk.ctx.at.record.dom.monthly.agreement.export.AgreementExcessInfo;
 import nts.uk.ctx.at.shared.dom.common.Year;
 
 /**
@@ -15,5 +16,5 @@ public interface GetExcessTimesYearPub {
 	 * @return 年間超過回数
 	 */
 	// RequestList458
-	int algorithm(String employeeId, Year year);
+	AgreementExcessInfo algorithm(String employeeId, Year year);
 }
