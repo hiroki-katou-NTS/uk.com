b6c4ae763c312797d64c62b9333ece1c61cbdce5
Nguyen Xuan Tam <tamnx@3si.vn>
2018-08-29 11:57:01 +0700

    QMM011 code sever B screen

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.app/src/main/java/nts/uk/ctx/exio/app/find/monsalabonus/laborinsur/EmpInsurPreRateDto.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.app/src/main/java/nts/uk/ctx/exio/app/find/monsalabonus/laborinsur/EmpInsurPreRateDto.java
@@ -2,8 +2,6 @@ package nts.uk.ctx.exio.app.find.monsalabonus.laborinsur;
 
 import lombok.AllArgsConstructor;
 import lombok.Value;
-import nts.uk.ctx.exio.app.find.exo.condset.StdOutputCondSetDto;
-import nts.uk.ctx.exio.dom.exo.condset.StdOutputCondSet;
 import nts.uk.ctx.exio.dom.monsalabonus.laborinsur.EmpInsurPreRate;
 
 @AllArgsConstructor
