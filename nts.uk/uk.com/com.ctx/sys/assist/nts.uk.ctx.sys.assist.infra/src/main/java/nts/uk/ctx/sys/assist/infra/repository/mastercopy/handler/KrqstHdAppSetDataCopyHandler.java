bbb0b644932331c14e4878d8943441c38622e179
hoangnm1-nws <hoangnm1@newwave.vn>
2018-08-31 17:12:33 +0700

    [CMM001_copy_data] fixed copy data in KRQST_HD_APP_SET table

--- nts.uk/uk.com/com.ctx/sys/assist/nts.uk.ctx.sys.assist.infra/src/main/java/nts/uk/ctx/sys/assist/infra/repository/mastercopy/handler/KrqstHdAppSetDataCopyHandler.java
+++ nts.uk/uk.com/com.ctx/sys/assist/nts.uk.ctx.sys.assist.infra/src/main/java/nts/uk/ctx/sys/assist/infra/repository/mastercopy/handler/KrqstHdAppSetDataCopyHandler.java
@@ -20,7 +20,7 @@ import javax.persistence.Query;
 @Setter
 @NoArgsConstructor
 public class KrqstHdAppSetDataCopyHandler extends DataCopyHandler {
-	
+
 	/** The insert query. */
 	private final String INSERT_QUERY = "INSERT INTO KRQST_HD_APP_SET(CID, USE_60H_HOLIDAY, OBSTACLE_NAME, REGIS_SHORT_LOST_HD, HD_NAME, REGIS_LACK_PUBLIC_HD, CHANGE_WRK_HOURS, "
 			+ "CKUPER_LIMIT_HALFDAY_HD, ACTUAL_DISPLAY_ATR, WRK_HOURS_USE_ATR, PRIORITY_DIGESTION_ATR, YEAR_HD_NAME, "
@@ -37,85 +37,100 @@ public class KrqstHdAppSetDataCopyHandler extends DataCopyHandler {
 
 	/** The delete by cid query. */
 	private final String DELETE_BY_CID_QUERY = "DELETE FROM KRQST_HD_APP_SET WHERE CID = ?";
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see nts.uk.ctx.at.request.dom.mastercopy.DataCopyHandler#doCopy()
 	 */
 	@Override
 	public void doCopy() {
-		
+
 		// Get all company zero data
-		Query selectQuery = this.entityManager.createNativeQuery(SELECT_BY_CID_QUERY)
-				.setParameter(1, AppContexts.user().zeroCompanyIdInContract());
-		Object[] zeroCompanyDatas = selectQuery.getResultList().toArray();
-		
-		if(zeroCompanyDatas.length == 0)
+		Query selectQuery = this.entityManager.createNativeQuery(SELECT_BY_CID_QUERY).setParameter(1,
+				AppContexts.user().zeroCompanyIdInContract());
+		List<Object> zeroCompanyDatas = selectQuery.getResultList();
+
+		if (zeroCompanyDatas.isEmpty())
 			return;
 
 		switch (copyMethod) {
-			case REPLACE_ALL:
-				Query deleteQuery = this.entityManager.createNativeQuery(DELETE_BY_CID_QUERY)
-					.setParameter(1, this.companyId);
-				deleteQuery.executeUpdate();
-			case ADD_NEW:
-				if (copyMethod == CopyMethod.ADD_NEW) {
-					// get old data target by cid
-					Query selectQueryTarget = this.entityManager.createNativeQuery(SELECT_BY_CID_QUERY).setParameter(1,
-							this.companyId);
-					List<Object> oldDatas = selectQueryTarget.getResultList();
-					if(!oldDatas.isEmpty())
-						return;
+		case REPLACE_ALL:
+			Query deleteQuery = this.entityManager.createNativeQuery(DELETE_BY_CID_QUERY).setParameter(1,
+					this.companyId);
+			deleteQuery.executeUpdate();
+		case ADD_NEW:
+			if (copyMethod == CopyMethod.ADD_NEW) {
+				// get old data target by cid
+				Query selectQueryTarget = this.entityManager.createNativeQuery(SELECT_BY_CID_QUERY).setParameter(1,
+						this.companyId);
+				List<Object> oldDatas = selectQueryTarget.getResultList();
+				// ignore data existed
+				for (int i = 0; i < zeroCompanyDatas.size(); i++) {
+					Object[] dataAttr = (Object[]) zeroCompanyDatas.get(i);
+					for (int j = 0; j < oldDatas.size(); j++) {
+						Object[] targetAttr = (Object[]) oldDatas.get(j);
+						// compare keys and remove
+						if (dataAttr[1].equals(targetAttr[1])) {
+							zeroCompanyDatas.remove(i);
+							i -= 1;
+							break;
+						}
+					}
 				}
-				
-				String insertQueryStr = StringUtils.repeat(INSERT_QUERY, zeroCompanyDatas.length);
-				Query insertQuery = this.entityManager.createNativeQuery(insertQueryStr);
-				for (int i = 0, j = zeroCompanyDatas.length; i < j; i++) {
-					Object[] dataArr = (Object[]) zeroCompanyDatas[i];
-					insertQuery.setParameter(i * 27 + 1, this.companyId);
-					insertQuery.setParameter(i * 27 + 2, dataArr[1]);
-					insertQuery.setParameter(i * 27 + 3, dataArr[2]);
-					insertQuery.setParameter(i * 27 + 4, dataArr[3]);
-					insertQuery.setParameter(i * 27 + 5, dataArr[4]);
-					insertQuery.setParameter(i * 27 + 6, dataArr[5]);
-					insertQuery.setParameter(i * 27 + 7, dataArr[6]);
-					insertQuery.setParameter(i * 27 + 8, dataArr[7]);
-					insertQuery.setParameter(i * 27 + 9, dataArr[8]);
-					insertQuery.setParameter(i * 27 + 10, dataArr[9]);
-					insertQuery.setParameter(i * 27 + 11, dataArr[10]);
-					insertQuery.setParameter(i * 27 + 12, dataArr[11]);
-					insertQuery.setParameter(i * 27 + 13, dataArr[12]);
-					insertQuery.setParameter(i * 27 + 14, dataArr[13]);
-					insertQuery.setParameter(i * 27 + 15, dataArr[14]);
-					insertQuery.setParameter(i * 27 + 16, dataArr[15]);
-					insertQuery.setParameter(i * 27 + 17, dataArr[16]);
-					insertQuery.setParameter(i * 27 + 18, dataArr[17]);
-					insertQuery.setParameter(i * 27 + 19, dataArr[18]);
-					insertQuery.setParameter(i * 27 + 20, dataArr[19]);
-					insertQuery.setParameter(i * 27 + 21, dataArr[20]);
-					insertQuery.setParameter(i * 27 + 22, dataArr[21]);
-					insertQuery.setParameter(i * 27 + 23, dataArr[22]);
-					insertQuery.setParameter(i * 27 + 24, dataArr[23]);
-					insertQuery.setParameter(i * 27 + 25, dataArr[24]);
-					insertQuery.setParameter(i * 27 + 26, dataArr[25]);
-					insertQuery.setParameter(i * 27 + 27, dataArr[26]);
-				}
-				
-				// Run insert query
-				insertQuery.executeUpdate();
-			case DO_NOTHING:
-				// Do nothing
-			default: 
-				break;
+			}
+			String insertQueryStr = StringUtils.repeat(INSERT_QUERY, zeroCompanyDatas.size());
+			Query insertQuery = this.entityManager.createNativeQuery(insertQueryStr);
+			for (int i = 0, j = zeroCompanyDatas.size(); i < j; i++) {
+				Object[] dataArr = (Object[]) zeroCompanyDatas.get(i);
+				insertQuery.setParameter(i * 27 + 1, this.companyId);
+				insertQuery.setParameter(i * 27 + 2, dataArr[1]);
+				insertQuery.setParameter(i * 27 + 3, dataArr[2]);
+				insertQuery.setParameter(i * 27 + 4, dataArr[3]);
+				insertQuery.setParameter(i * 27 + 5, dataArr[4]);
+				insertQuery.setParameter(i * 27 + 6, dataArr[5]);
+				insertQuery.setParameter(i * 27 + 7, dataArr[6]);
+				insertQuery.setParameter(i * 27 + 8, dataArr[7]);
+				insertQuery.setParameter(i * 27 + 9, dataArr[8]);
+				insertQuery.setParameter(i * 27 + 10, dataArr[9]);
+				insertQuery.setParameter(i * 27 + 11, dataArr[10]);
+				insertQuery.setParameter(i * 27 + 12, dataArr[11]);
+				insertQuery.setParameter(i * 27 + 13, dataArr[12]);
+				insertQuery.setParameter(i * 27 + 14, dataArr[13]);
+				insertQuery.setParameter(i * 27 + 15, dataArr[14]);
+				insertQuery.setParameter(i * 27 + 16, dataArr[15]);
+				insertQuery.setParameter(i * 27 + 17, dataArr[16]);
+				insertQuery.setParameter(i * 27 + 18, dataArr[17]);
+				insertQuery.setParameter(i * 27 + 19, dataArr[18]);
+				insertQuery.setParameter(i * 27 + 20, dataArr[19]);
+				insertQuery.setParameter(i * 27 + 21, dataArr[20]);
+				insertQuery.setParameter(i * 27 + 22, dataArr[21]);
+				insertQuery.setParameter(i * 27 + 23, dataArr[22]);
+				insertQuery.setParameter(i * 27 + 24, dataArr[23]);
+				insertQuery.setParameter(i * 27 + 25, dataArr[24]);
+				insertQuery.setParameter(i * 27 + 26, dataArr[25]);
+				insertQuery.setParameter(i * 27 + 27, dataArr[26]);
+			}
+
+			// Run insert query
+			insertQuery.executeUpdate();
+		case DO_NOTHING:
+			// Do nothing
+		default:
+			break;
 		}
-		
+
 	}
-	
+
 	/**
 	 * Instantiates a new krqst hd app set data copy handler.
 	 *
-	 * @param entityManager the entity manager
-	 * @param copyMethod the copy method
-	 * @param companyId the company id
+	 * @param entityManager
+	 *            the entity manager
+	 * @param copyMethod
+	 *            the copy method
+	 * @param companyId
+	 *            the company id
 	 */
 	public KrqstHdAppSetDataCopyHandler(EntityManager entityManager, CopyMethod copyMethod, String companyId) {
 		super();
