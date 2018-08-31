b6a70529df8cbb1e0a35af35b2754204705e592a
tranphuc7686 <35018596+tranphuc7686@users.noreply.github.com>
2018-08-31 18:04:01 +0700

    [QMM011] code ui screen C

---
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.app/src/main/java/nts/uk/ctx/exio/app/find/monsalabonus/laborinsur/OccAccInsurBusFinder.java
@@ -0,0 +1,4 @@
+package nts.uk.ctx.exio.app.find.monsalabonus.laborinsur;
+
+public class OccAccInsurBusFinder {
+}
---
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.ws/src/main/java/nts/uk/ctx/exio/ws/monsalabonus/laborinsur/WorkersCompenInsurWebService.java
@@ -0,0 +1,4 @@
+package nts.uk.ctx.exio.ws.monsalabonus.laborinsur;
+
+public class WorkersCompenInsurWebService {
+}
