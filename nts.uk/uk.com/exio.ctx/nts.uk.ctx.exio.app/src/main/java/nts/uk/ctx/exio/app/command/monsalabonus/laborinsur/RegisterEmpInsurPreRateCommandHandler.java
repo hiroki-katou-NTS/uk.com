730cc8ba58b2e6e1f05d4d1b2c8b88ee1b4e8f44
Nguyen Xuan Tam <tamnx@3si.vn>
2018-09-05 17:13:58 +0700

    Qmm011 Update

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.app/src/main/java/nts/uk/ctx/exio/app/command/monsalabonus/laborinsur/RegisterEmpInsurPreRateCommandHandler.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.app/src/main/java/nts/uk/ctx/exio/app/command/monsalabonus/laborinsur/RegisterEmpInsurPreRateCommandHandler.java
@@ -1,6 +1,7 @@
 package nts.uk.ctx.exio.app.command.monsalabonus.laborinsur;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
@@ -8,20 +9,24 @@ import javax.transaction.Transactional;
 
 import nts.arc.layer.app.command.CommandHandler;
 import nts.arc.layer.app.command.CommandHandlerContext;
+import nts.uk.ctx.exio.dom.monsalabonus.laborinsur.EmpInsurBusBurRatio;
 import nts.uk.ctx.exio.dom.monsalabonus.laborinsur.EmpInsurBusBurRatioService;
 
 @Stateless
 @Transactional
-public class RegisterEmpInsurPreRateCommandHandler extends CommandHandler<EmpInsurBusBurRatioCommand> {
+public class RegisterEmpInsurPreRateCommandHandler extends CommandHandler<RegisterEmpInsurBusBurRatioCommand> {
 
     @Inject
     private EmpInsurBusBurRatioService empInsurBusBurRatioService;
 
 	@Override
-	protected void handle(CommandHandlerContext<EmpInsurBusBurRatioCommand> context) {
+	protected void handle(CommandHandlerContext<RegisterEmpInsurBusBurRatioCommand> context) {
 	    
-	    List<EmpInsurBusBurRatio> empInsurBusBurRatio = command.g
-	    empInsurBusBurRatioService.registerEmpInsurBusBurRatio();
+	    RegisterEmpInsurBusBurRatioCommand command = context.getCommand();
+	    List<EmpInsurBusBurRatio> listEmpInsurBusBurRatio  = command.getListEmpInsurPreRate().stream().map(item -> { 
+	        return new EmpInsurBusBurRatio(item.getHisId(), item.getEmpPreRateId(), item.getIndBdRatio(), item.getEmpContrRatio(), item.getPerFracClass(), item.getBusiOwFracClass());
+	        }).collect(Collectors.toList());
+	    empInsurBusBurRatioService.registerEmpInsurBusBurRatio(listEmpInsurBusBurRatio);
 	    
 	}
 }
