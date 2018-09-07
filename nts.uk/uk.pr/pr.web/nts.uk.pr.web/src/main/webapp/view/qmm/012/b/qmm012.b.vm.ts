b2cdf2e1dafd419a864e17010d6c5d4167727ad2
Anh Trung <trung.bv@3si.vn>
2018-09-07 18:26:46 +0700

    update UI màn B

--- nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/012/b/qmm012.b.vm.ts
+++ nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/012/b/qmm012.b.vm.ts
@@ -11,7 +11,26 @@ module nts.uk.pr.view.qmm012.b {
     export module viewModel {
         export class ScreenModel {
             
-            constructor() { 
+            // category comboBox
+            categoryList: KnockoutObservableArray<ItemModel>;
+            selectedCategory: KnockoutObservable<string>;
+            
+            // statement gridlist
+            statementList: KnockoutObservableArray<IStatementItem> = ko.observableArray([]);
+            statementSelected: KnockoutObservable<StatementItem> = ko.observable(null);
+            
+            constructor() {
+                let self = this;
+                
+                // category comboBox
+                self.categoryList = ko.observableArray([
+                    new model.ItemModel(model.CategoryAtr.PAYMENT_ITEM, getText('QMM012_3')),
+                    new model.ItemModel(model.CategoryAtr.DEDUCTION_ITEM, getText('QMM012_4')),
+                    new model.ItemModel(model.CategoryAtr.ATTEND_ITEM, getText('QMM012_5')),
+                    new model.ItemModel(model.CategoryAtr.REPORT_ITEM, getText('QMM012_6')),
+                    new model.ItemModel(model.CategoryAtr.OTHER_ITEM, getText('QMM012_7'))
+                ]);
+                self.selectedCategory = ko.observable(model.CategoryAtr.PAYMENT_ITEM);
 
             }//end constructor
             
@@ -21,6 +40,76 @@ module nts.uk.pr.view.qmm012.b {
                 deferred.resolve();
                 return deferred.promise();
             }
+            
+            public create(): void {
+                
+            }
+            
+            public register(): void {
+                
+            }
+            
+            public copy(): void {
+                
+            }
+            
+            public deleteItem(): void {
+                
+            }
+            
+            public outputExcel(): void {
+                
+            }
+            
+            public modifyLog(): void {
+                
+            }
+            
+            public registerPrintingName(): void {
+                
+            }
+            
         }
-    }
+        
+        class StatementItem {
+            cid: string;
+            categoryAtr: number;
+            itemNameCd: string;
+            salaryItemId: string;
+            defaultAtr: KnockoutObservable<number>;
+            valueAtr: KnockoutObservable<number>;
+            deprecatedAtr: KnockoutObservable<number>;
+            socialInsuaEditableAtr: KnockoutObservable<number>;
+            intergrateCd: KnockoutObservable<number>;
+            
+            constructor(item: IStatementItem) {
+                let self = this;
+                
+                if (item) {
+                    self.cid = item.cid;
+                    self.categoryAtr = item.categoryAtr;
+                    self.itemNameCd = item.itemNameCd;
+                    self.salaryItemId = item.salaryItemId;
+                    self.defaultAtr(item.defaultAtr);
+                    self.valueAtr(item.valueAtr);
+                    self.deprecatedAtr(item.deprecatedAtr);
+                    self.socialInsuaEditableAtr(item.socialInsuaEditableAtr);
+                    self.intergrateCd(item.intergrateCd);
+                    
+                }
+            }
+        }
+        
+        interface IStatementItem {
+            cid: string;
+            categoryAtr: number;
+            itemNameCd: string;
+            salaryItemId: string;
+            defaultAtr: number;
+            valueAtr: number;
+            deprecatedAtr: number;
+            socialInsuaEditableAtr: number;
+            intergrateCd: number;
+        }
+    }  
 }
--- nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/012/share/model.ts
+++ nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/012/share/model.ts
@@ -1,3 +1,24 @@
 module nts.uk.pr.view.qmm012.share.model {
+    export enum CategoryAtr {
+
+        PAYMENT_ITEM = 0,
+
+        DEDUCTION_ITEM = 1,
+
+        ATTEND_ITEM = 2,
+
+        REPORT_ITEM = 3,
+
+        OTHER_ITEM = 4
+    }
     
+    export class ItemModel {
+        code: string;
+        name: string;
+    
+        constructor(code: string, name: string) {
+            this.code = code;
+            this.name = name;
+        }
+    }
 }
