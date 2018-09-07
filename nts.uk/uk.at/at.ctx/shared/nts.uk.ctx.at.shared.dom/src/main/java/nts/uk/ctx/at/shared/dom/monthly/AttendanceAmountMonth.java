d70d8146e0831961fcd199d6a01395197b4fb295
m_kitahira <m_kitahira@nittsusystem.co.jp>
2018-09-07 14:53:19 +0900

    PrimitiveValue AttendanceAmountMonthをat.recordからat.sharedに移動（クラス名変更）

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/monthly/AttendanceAmountMonth.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/monthly/AttendanceAmountMonth.java
@@ -1,4 +1,4 @@
-package nts.uk.ctx.at.record.dom.monthly;
+package nts.uk.ctx.at.shared.dom.monthly;
 
 import nts.arc.primitive.IntegerPrimitiveValue;
 import nts.arc.primitive.constraint.IntegerRange;
@@ -8,7 +8,7 @@ import nts.arc.primitive.constraint.IntegerRange;
  * @author shuichu_ishida
  */
 @IntegerRange(min = 0, max = 99999999)
-public class AttendanceAmountMonthDom extends IntegerPrimitiveValue<AttendanceAmountMonthDom> {
+public class AttendanceAmountMonth extends IntegerPrimitiveValue<AttendanceAmountMonth> {
 
 	/** serialVersionUID */
 	private static final long serialVersionUID = 1L;
@@ -17,7 +17,7 @@ public class AttendanceAmountMonthDom extends IntegerPrimitiveValue<AttendanceAm
 	 * コンストラクタ
 	 * @param amount 金額
 	 */
-	public AttendanceAmountMonthDom(Integer amount){
+	public AttendanceAmountMonth(Integer amount){
 		
 		super(amount);
 	}
@@ -27,8 +27,8 @@ public class AttendanceAmountMonthDom extends IntegerPrimitiveValue<AttendanceAm
 	 * @param amount 金額
 	 * @return 加算後の勤怠月間金額
 	 */
-	public AttendanceAmountMonthDom addAmount(Integer amount){
+	public AttendanceAmountMonth addAmount(Integer amount){
 	
-		return new AttendanceAmountMonthDom(this.v() + amount);
+		return new AttendanceAmountMonth(this.v() + amount);
 	}
 }
