a9d8aed83e777365d911f484713d4d4d6314c5f7
Nam Hoang <nambean90@gmail.com>
2018-09-07 09:22:56 +0700

    move qmm005 b screen from com to pr

--- nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/005/b/qmm005.b.vm.ts
+++ nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/005/b/qmm005.b.vm.ts
@@ -1,7 +1,7 @@
-module nts.uk.com.view.qmm005.b.viewmodel {
+module nts.uk.pr.view.qmm005.b.viewmodel {
     import close = nts.uk.ui.windows.close;
     import getText = nts.uk.resource.getText;
-    import model = nts.uk.com.view.qmm005.share.model;
+    import model = nts.uk.pr.view.qmm005.share.model;
     import setShared = nts.uk.ui.windows.setShared;
     import getShared = nts.uk.ui.windows.getShared;
     import block = nts.uk.ui.block;
