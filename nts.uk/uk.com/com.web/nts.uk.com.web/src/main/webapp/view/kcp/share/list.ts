9f7e1685afc217284c3597e42add3b01a958e4cd
nws-dientx <dientx@newwave.vn>
2018-09-08 10:45:01 +0700

    fixbug #99861

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/kcp/share/list.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/kcp/share/list.ts
@@ -447,7 +447,11 @@ module kcp.share.list {
 
                 // setup event
                 self.initEvent();
-
+                
+                //re-set selectedCodes
+                const gridList = $('#' + self.componentGridId);
+                gridList.ntsGridList("setSelectedValue", self.selectedCodes());
+                
                 // set focus if parent screen has no focus
                 if (document.activeElement.tagName == 'BODY') {
                     _.defer(() => $('#' + self.searchBoxId + ' .ntsSearchBox').focus());
