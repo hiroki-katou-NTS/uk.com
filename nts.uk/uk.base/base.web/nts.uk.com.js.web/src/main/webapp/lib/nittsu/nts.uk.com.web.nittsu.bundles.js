3a744230dbdcc3386f1bbd58523c92020d751e06
NSVN-NguyenDacManh <manh_nd@nittsusystem.co.jp>
2018-09-08 17:18:11 +0900

    Update MGrid

--- nts.uk/uk.base/base.web/nts.uk.com.js.web/src/main/webapp/lib/nittsu/nts.uk.com.web.nittsu.bundles.js
+++ nts.uk/uk.base/base.web/nts.uk.com.js.web/src/main/webapp/lib/nittsu/nts.uk.com.web.nittsu.bundles.js
@@ -23745,7 +23745,9 @@ var nts;
                                 if (!ftPrint)
                                     ftPrint = true;
                             };
-                            setShtCellState($cell);
+                            if ($cell) {
+                                setShtCellState($cell);
+                            }
                             _.forEach(_.keys(_mafollicle[SheetDef]), function (s) {
                                 if (s === _currentSheet)
                                     return;
@@ -24625,21 +24627,27 @@ var nts;
                         if (reset) {
                             origDs[coord.rowIdx][coord.columnKey] = cellValue;
                         }
-                        var $cell, origVal = origDs[coord.rowIdx][coord.columnKey];
+                        var $cell, sumDone, origVal = origDs[coord.rowIdx][coord.columnKey];
                         var transe = function (sheet, zeroHidden, dirties, desc) {
                             var colour, before, after, total, calcCell = lch.cellAt(_$grid[0], coord.rowIdx, coord.columnKey, desc), sum = _summaries[coord.columnKey];
                             if (sum && sum.calculator === "Time" && calcCell) {
-                                after = moment.duration(cellValue);
-                                before = moment.duration($.data(calcCell, v.DATA));
-                                var diff = after.subtract(before);
-                                sum[_currentPage].add(diff);
+                                if (!sumDone) {
+                                    after = moment.duration(cellValue);
+                                    before = moment.duration($.data(calcCell, v.DATA));
+                                    var diff = after.subtract(before);
+                                    sum[_currentPage].add(diff);
+                                    sumDone = true;
+                                }
                                 sum[sheet].textContent = ti.momentToString(sum[_currentPage]);
                             }
                             else if (sum && sum.calculator === "Number" && calcCell) {
-                                after = parseFloat(cellValue);
-                                before = parseFloat($.data(calcCell, v.DATA));
-                                total = sum[_currentPage] + ((isNaN(after) ? 0 : after) - (isNaN(before) ? 0 : before));
-                                sum[_currentPage] = total;
+                                if (!sumDone) {
+                                    after = parseFloat(cellValue);
+                                    before = parseFloat($.data(calcCell, v.DATA));
+                                    total = sum[_currentPage] + ((isNaN(after) ? 0 : after) - (isNaN(before) ? 0 : before));
+                                    sum[_currentPage] = total;
+                                    sumDone = true;
+                                }
                                 sum[sheet].textContent = sum[_currentPage];
                             }
                             if (zeroHidden && ti.isZero(origVal)
@@ -24713,7 +24721,7 @@ var nts;
                             var t, formatted, disFormat, maf = _mafollicle[_currentPage][s];
                             if (maf && maf.desc) {
                                 t = transe(s, maf.zeroHidden, maf.dirties, maf.desc);
-                                if (!t || !t.c)
+                                if (!t || !t.c || _.find(_fixedColumns, function (fc) { return fc.key === coord.columnKey; }))
                                     return;
                                 formatted = format(column[0], cellValue);
                                 t.c.textContent = formatted;
@@ -25279,6 +25287,15 @@ var nts;
                             return;
                         var $sheetArea = v.createWrapper(top + ti.getScrollWidth() + SUM_HEIGHT + "px", 0, { width: parseFloat(width) + ti.getScrollWidth() + "px", height: gp.SHEET_HEIGHT + "px", containerClass: gp.SHEET_CLS });
                         $container.appendChild($sheetArea);
+                        var $scrollBar = document.createElement("ul");
+                        $scrollBar.classList.add("mgrid-sheet-scrollbar");
+                        $sheetArea.appendChild($scrollBar);
+                        var $up = document.createElement("li");
+                        $up.textContent = "▲";
+                        $scrollBar.appendChild($up);
+                        var $down = document.createElement("li");
+                        $down.textContent = "▼";
+                        $scrollBar.appendChild($down);
                         var $gridSheet = _prtDiv.cloneNode();
                         $gridSheet.classList.add("mgrid-sheet-nav");
                         $sheetArea.appendChild($gridSheet);
@@ -25305,6 +25322,13 @@ var nts;
                                 $btn.classList.add("ui-state-active");
                             });
                             $buttons.appendChild($btn);
+                        });
+                        var sheetNav = $($gridSheet);
+                        $up.addXEventListener(ssk.CLICK_EVT, function (evt) {
+                            sheetNav.scrollTop(sheetNav.scrollTop() - gp.SHEET_HEIGHT);
+                        });
+                        $down.addXEventListener(ssk.CLICK_EVT, function (evt) {
+                            sheetNav.scrollTop(sheetNav.scrollTop() + gp.SHEET_HEIGHT);
                         });
                     }
                     gp.imiSheets = imiSheets;
@@ -29244,11 +29268,9 @@ var nts;
                         }());
                         dist.Remote = Remote;
                     })(dist || (dist = {}));
-                    var start;
                     $.fn.ntsGrid = function (options) {
                         var self = this;
                         var $self = $(self);
-                        start = performance.now();
                         if (typeof options === "string") {
                             return functions.ntsAction($self, options, [].slice.call(arguments).slice(1));
                         }
@@ -32670,7 +32692,6 @@ var nts;
                                 // Load columns size
                                 columnSize.load($grid);
                                 utils.setChildrenTabIndex($grid, -1);
-                                console.log("grid: " + (performance.now() - start));
                             };
                         }
                         events.afterRendered = afterRendered;
