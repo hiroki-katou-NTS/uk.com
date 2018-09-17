adcc9ae74b915d565fe38a53b8fa3015d806311e
Nam Hoang <nambean90@gmail.com>
2018-09-17 17:54:09 +0700

    service A, F screen

--- nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/005/f/qmm005.f.vm.ts
+++ nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/005/f/qmm005.f.vm.ts
@@ -1,26 +1,25 @@
 module nts.uk.pr.view.qmm005.f.viewmodel {
     import model = nts.uk.pr.view.qmm005.share.model;
     import resource = nts.uk.resource;
+    import setShared = nts.uk.ui.windows.setShared;
+    import getShared = nts.uk.ui.windows.getShared;
+    import close = nts.uk.ui.windows.close;
     export class ScreenModel {
         itemsSwap: KnockoutObservableArray<ItemModel>;
         columns: KnockoutObservableArray<nts.uk.ui.NtsGridListColumn>;
         currentCodeListSwap: KnockoutObservableArray<any>;
-        test: KnockoutObservableArray<any>;
+        itemShared: any;
+
         constructor() {
             var self = this;
             self.itemsSwap = ko.observableArray([]);
-
-            var array = [];
-            for (var i = 0; i < 50; i++) {
-                array.push(new ItemModel(i, '基本給基本給'));
-            }
-            this.itemsSwap(array);
+            self.itemShared = getShared("QMM005_output_F");
+            this.itemsSwap(self.itemShared.employeeList);
             this.columns = ko.observableArray([
                 { headerText: resource.getText('QMM005_93'), key: 'code', width: 90 },
                 { headerText: resource.getText('QMM005_94'), key: 'name', width: 200  }
             ]);
             this.currentCodeListSwap = ko.observableArray([]);
-            this.test = ko.observableArray([]);
         }
 
         startPage(): JQueryPromise<any> {
@@ -28,21 +27,26 @@ module nts.uk.pr.view.qmm005.f.viewmodel {
             dfd.resolve();
             return dfd.promise();
         }
-        remove(){
+
+        remove() {
             this.itemsSwap.shift();
         }
 
-        decision(){
+        submit() {
+            var self = this;
+            setShared("QMM005F_outParams", {processCateNo: self.itemShared.processCateNo, returnList: self.currentCodeListSwap()});
+            close();
 
         }
 
-        cancel(){
-            nts.uk.ui.windows.close();
+        cancel() {
+            close();
         }
     }
     class ItemModel {
         code: number;
         name: string;
+
         constructor(code: number, name: string) {
             this.code = code;
             this.name = name;
