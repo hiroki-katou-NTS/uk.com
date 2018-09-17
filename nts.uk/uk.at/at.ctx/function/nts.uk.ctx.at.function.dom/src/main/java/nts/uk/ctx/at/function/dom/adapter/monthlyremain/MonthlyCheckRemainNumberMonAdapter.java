79f8ce700a5f2182e042d9e6c7352c34fe378676
dang dinh hoi <hoidd@2nf.com.vn>
2018-09-17 15:11:35 +0700

    //HoiDD #1000436 + fix lai 100284 (model.ts)

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/adapter/monthlyremain/MonthlyCheckRemainNumberMonAdapter.java
+++
@@ -1,9 +0,0 @@
-package nts.uk.ctx.at.function.dom.adapter.monthlyremain;
-
-import java.util.Optional;
-
-
-public interface MonthlyCheckRemainNumberMonAdapter {
-	
-	Optional<CheckRemainNumberMonImport> getByEralCheckID(String errorAlarmID);
-}
