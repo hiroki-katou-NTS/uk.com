f64c05c4bfbc1a43b348c83280c3d70b6ba9f20a
Anh Trung <trung.bv@3si.vn>
2018-09-18 16:08:22 +0700

    Commit màn B

--- nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/012/b/qmm012.b.service.ts
+++ nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/012/b/qmm012.b.service.ts
@@ -5,7 +5,12 @@ module nts.uk.pr.view.qmm012.b {
     export module service {
         
         let paths = {
+            getAllStatementItemData: "ctx/pr/core/wageprovision/statementitem/getAllStatementItemData/{0}/{1}"
         }
-       
+        
+        export function getAllStatementItemData(categoryAtr: number, isdisplayAbolition: boolean): JQueryPromise<any> {
+            let _path = format(paths.getAllStatementItemData, categoryAtr, isdisplayAbolition);
+            return ajax('pr', _path);
+        }
     }
 }
