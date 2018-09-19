6d913358c9852831838b2f2226f896092dd2b774
HiepLD3S <hiep.ld@3si.vn>
2018-09-19 19:18:27 +0700

    Update server B

--- nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.dom/src/main/java/nts/uk/ctx/core/dom/socialinsurance/healthinsurance/service/HealthInsuranceService.java
+++ nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.dom/src/main/java/nts/uk/ctx/core/dom/socialinsurance/healthinsurance/service/HealthInsuranceService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+import nts.arc.time.YearMonth;
 import nts.uk.ctx.core.dom.socialinsurance.AutoCalculationExecutionCls;
 import nts.uk.ctx.core.dom.socialinsurance.healthinsurance.BonusHealthInsuranceRate;
 import nts.uk.ctx.core.dom.socialinsurance.healthinsurance.BonusHealthInsuranceRateRepository;
@@ -17,6 +18,7 @@ import nts.uk.ctx.core.dom.socialinsurance.healthinsurance.HealthInsuranceStanda
 import nts.uk.ctx.core.dom.socialinsurance.healthinsurance.HealthInsuranceStandardMonthlyRepository;
 import nts.uk.shr.com.context.AppContexts;
 import nts.uk.shr.com.history.YearMonthHistoryItem;
+import nts.uk.shr.com.time.calendar.period.YearMonthPeriod;
 
 @Stateless
 public class HealthInsuranceService {
@@ -38,7 +40,7 @@ public class HealthInsuranceService {
 		HealthInsuranceFeeRateHistory welfarePensionHistory = null;
 		Optional<HealthInsuranceFeeRateHistory> opt_healthInsurance = healthInsuranceFeeRateHistoryRepository
 				.getHealthInsuranceFeeRateHistoryByCid(AppContexts.user().companyId(), officeCode);
-		calculationGradeFee(bonusHealthInsuranceRate, healthInsuranceMonthlyFee, yearMonthItem);
+		healthInsuranceMonthlyFee = calculationGradeFee(bonusHealthInsuranceRate, healthInsuranceMonthlyFee, yearMonthItem);
 		if (!opt_healthInsurance.isPresent()) {
 			welfarePensionHistory = new HealthInsuranceFeeRateHistory(AppContexts.user().companyId(), officeCode,
 					Arrays.asList(yearMonthItem));
@@ -72,10 +74,43 @@ public class HealthInsuranceService {
 		healthInsuranceMonthlyFeeRepository.update(healthInsuranceMonthlyFee);
 	}
 	
-	public void calculationGradeFee (BonusHealthInsuranceRate bonusHealthInsuranceRate, HealthInsuranceMonthlyFee healthInsuranceMonthlyFee, YearMonthHistoryItem yearMonthItem) {
+	public HealthInsuranceMonthlyFee calculationGradeFee (BonusHealthInsuranceRate bonusHealthInsuranceRate, HealthInsuranceMonthlyFee healthInsuranceMonthlyFee, YearMonthHistoryItem yearMonthItem) {
 		if (AutoCalculationExecutionCls.AUTO.equals(healthInsuranceMonthlyFee.getAutoCalculationCls())) {
 			Optional<HealthInsuranceStandardMonthly> healthInsuranceStandardMonthlyOptional = this.healthInsuranceStandardMonthlyRepository.getHealthInsuranceStandardMonthlyByStartYearMonth(yearMonthItem.start().v());
 			healthInsuranceMonthlyFee.algorithmMonthlyHealthInsurancePremiumCalculation(healthInsuranceStandardMonthlyOptional, bonusHealthInsuranceRate);
 		}
+		return healthInsuranceMonthlyFee;
+	}
+	
+	public void updateHistory (String officeCode, YearMonthHistoryItem yearMonth){
+		Optional<HealthInsuranceFeeRateHistory> opt_healthInsurance = healthInsuranceFeeRateHistoryRepository
+				.getHealthInsuranceFeeRateHistoryByCid(AppContexts.user().companyId(), officeCode);
+		if (!opt_healthInsurance.isPresent()) {
+			return;
+		}
+		HealthInsuranceFeeRateHistory healthInsurance = opt_healthInsurance.get();
+		Optional<YearMonthHistoryItem> currentSpan = healthInsurance.getHistory().stream().filter(item -> item.identifier().equals(yearMonth.identifier())).findFirst();
+		if (!currentSpan.isPresent()) return;
+		healthInsurance.changeSpan(currentSpan.get(), new YearMonthPeriod(yearMonth.start() , yearMonth.end()));
+		healthInsuranceFeeRateHistoryRepository.update(healthInsurance);
+	}
+	
+	public void deleteHistory (String officeCode, YearMonthHistoryItem yearMonth){
+		Optional<HealthInsuranceFeeRateHistory> opt_healthInsurance = healthInsuranceFeeRateHistoryRepository
+				.getHealthInsuranceFeeRateHistoryByCid(AppContexts.user().companyId(), officeCode);
+		if (!opt_healthInsurance.isPresent()) {
+			return;
+		}
+		HealthInsuranceFeeRateHistory healthInsurance = opt_healthInsurance.get();
+		if (healthInsurance.getHistory().size() == 0) return;
+		YearMonthHistoryItem lastestHistory = healthInsurance.getHistory().get(0);
+		healthInsurance.remove(lastestHistory);
+		if (healthInsurance.getHistory().size() > 0){
+			lastestHistory = healthInsurance.getHistory().get(0);
+			healthInsurance.changeSpan(healthInsurance.getHistory().get(0),  new YearMonthPeriod(lastestHistory.start(), new YearMonth(new Integer(999912))));
+		}
+		healthInsuranceFeeRateHistoryRepository.remove(healthInsurance);
+		bonusHealthInsuranceRateRepository.deleteByHistoryIds(Arrays.asList(yearMonth.identifier()));
+		healthInsuranceMonthlyFeeRepository.deleteByHistoryIds(Arrays.asList(yearMonth.identifier()));
 	}
 }
--- nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.ws/src/main/java/nts/uk/ctx/core/ws/socialinsurance/healthinsurance/HealthInsuranceWebservice.java
+++ nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.ws/src/main/java/nts/uk/ctx/core/ws/socialinsurance/healthinsurance/HealthInsuranceWebservice.java
@@ -1,19 +1,22 @@
 package nts.uk.ctx.core.ws.socialinsurance.healthinsurance;
 
-import nts.uk.ctx.core.app.command.socialinsurance.healthinsurance.AddHealthInsuranceCommandHandler;
-import nts.uk.ctx.core.app.command.socialinsurance.healthinsurance.command.AddHealthInsuranceCommand;
-import nts.uk.ctx.core.app.find.socialinsurance.healthinsurance.HealthInsuranceFeeRateFinder;
-import nts.uk.ctx.core.app.find.socialinsurance.healthinsurance.dto.HealthInsuranceDto;
-import nts.uk.ctx.core.app.find.socialinsurance.welfarepensioninsurance.dto.SocialInsuranceOfficeDto;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import java.util.List;
 
-@Path("ctx/pr/core/socialinsurance/healthinsurance")
+import nts.uk.ctx.core.app.command.socialinsurance.healthinsurance.AddHealthInsuranceCommandHandler;
+import nts.uk.ctx.core.app.command.socialinsurance.healthinsurance.DeleteHealthInsuranceCommandHandler;
+import nts.uk.ctx.core.app.command.socialinsurance.healthinsurance.UpdateHealthInsuranceFeeRateHistoryCommandHandler;
+import nts.uk.ctx.core.app.command.socialinsurance.healthinsurance.command.AddHealthInsuranceCommand;
+import nts.uk.ctx.core.app.find.socialinsurance.healthinsurance.HealthInsuranceFeeRateFinder;
+import nts.uk.ctx.core.app.find.socialinsurance.healthinsurance.dto.HealthInsuranceDto;
+import nts.uk.ctx.core.app.find.socialinsurance.welfarepensioninsurance.dto.SocialInsuranceOfficeDto;
+
+@Path("ctx/core/socialinsurance/healthinsurance")
 @Produces("application/json")
 public class HealthInsuranceWebservice {
 
@@ -22,6 +25,12 @@ public class HealthInsuranceWebservice {
 
     @Inject
     private AddHealthInsuranceCommandHandler addHealthInsuranceCommandHandler;
+    
+    @Inject
+    private UpdateHealthInsuranceFeeRateHistoryCommandHandler updateHealthInsuranceCommandHandler;
+    
+    @Inject
+    private DeleteHealthInsuranceCommandHandler deleteHealthInsuranceCommandHandler;
 
     @POST
     @Path("getByCompanyId")
@@ -42,7 +51,14 @@ public class HealthInsuranceWebservice {
     }
 
     @POST
-    @Path("update")
-    public void updateHealthInsuranceFeeRateByCompanyId() {
+    @Path("editHistory")
+    public void updateHealthInsuranceFeeRateByCompanyId(AddHealthInsuranceCommand command) {
+    	updateHealthInsuranceCommandHandler.handle(command);
+    }
+    
+    @POST
+    @Path("deleteHistory")
+    public void deleteHealthInsuranceFeeRateByCompanyId(AddHealthInsuranceCommand command) {
+    	deleteHealthInsuranceCommandHandler.handle(command);
     }
 }
