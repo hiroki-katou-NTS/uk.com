fe4e87ca0eea088c8f497b5691df5ce62496e209
3SI\sang.nv <sang.nv@3si.vn>
2018-09-20 10:49:52 +0700

    Update アルゴリズム「月額厚生年金保険料計算処理」を実行する

--- nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.dom/src/main/java/nts/uk/ctx/core/dom/socialinsurance/welfarepensioninsurance/EmployeesPensionMonthlyInsuranceFee.java
+++ nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.dom/src/main/java/nts/uk/ctx/core/dom/socialinsurance/welfarepensioninsurance/EmployeesPensionMonthlyInsuranceFee.java
@@ -86,43 +86,43 @@ public class EmployeesPensionMonthlyInsuranceFee extends AggregateRoot {
                 BigDecimal employeeFemaleExemptionInsurance = null;
 
                 //男子負担率
-                val maleContributionRate          = this.salaryEmployeesPensionInsuranceRate.getMaleContributionRate();
-                val maleIndividualBurdenRatio     = maleContributionRate.getIndividualBurdenRatio().v();                                                         //C3_9  男子給与個人負担率
-                val maleEmployeeContributionRatio = maleContributionRate.getEmployeeContributionRatio().v();                                                     //C3_11 男子給与事業主負担率
-                val maleIndividualExemptionRate   = maleContributionRate.getIndividualExemptionRate().map(PrimitiveValueBase::v).orElse(new BigDecimal(0)); //C4_19 男子給与個人免除保険料率
-                val maleEmployeeExemptionRate     = maleContributionRate.getEmployeeExemptionRate().map(PrimitiveValueBase::v).orElse(new BigDecimal(0));   //C4_21 男子給与事業主免除保険料率
+                val maleContributionRate                = this.salaryEmployeesPensionInsuranceRate.getMaleContributionRate();
+                val maleIndividualBurdenRatio           = maleContributionRate.getIndividualBurdenRatio().v();                                                             //C3_9  男子給与個人負担率
+                val maleEmployeeContributionRatio       = maleContributionRate.getEmployeeContributionRatio().v();                                                         //C3_11 男子給与事業主負担率
+                val maleIndividualExemptionRate         = maleContributionRate.getIndividualExemptionRate().map(PrimitiveValueBase::v).orElse(new BigDecimal(0));     //C4_19 男子給与個人免除保険料率
+                val maleEmployeeExemptionRate           = maleContributionRate.getEmployeeExemptionRate().map(PrimitiveValueBase::v).orElse(new BigDecimal(0));       //C4_21 男子給与事業主免除保険料率
 
                 //女子負担率
-                val femaleContributionRate          = this.salaryEmployeesPensionInsuranceRate.getFemaleContributionRate();
-                val femaleIndividualBurdenRatio     = femaleContributionRate.getIndividualBurdenRatio().v();                                                         //C3_18  女子給与個人負担率
-                val femaleEmployeeContributionRatio = femaleContributionRate.getEmployeeContributionRatio().v();                                                     //C3_20 女子給与事業主負担率
-                val femaleIndividualExemptionRate   = femaleContributionRate.getIndividualExemptionRate().map(PrimitiveValueBase::v).orElse(new BigDecimal(0)); //C4_38 女子給与個人免除保険料率
-                val femaleEmployeeExemptionRate     = femaleContributionRate.getEmployeeExemptionRate().map(PrimitiveValueBase::v).orElse(new BigDecimal(0));   //C4_40 女子給与事業主免除保険料率
+                val femaleContributionRate              = this.salaryEmployeesPensionInsuranceRate.getFemaleContributionRate();
+                val femaleIndividualBurdenRatio         = femaleContributionRate.getIndividualBurdenRatio().v();                                                            //C3_18  女子給与個人負担率
+                val femaleEmployeeContributionRatio     = femaleContributionRate.getEmployeeContributionRatio().v();                                                        //C3_20 女子給与事業主負担率
+                val femaleIndividualExemptionRate       = femaleContributionRate.getIndividualExemptionRate().map(PrimitiveValueBase::v).orElse(new BigDecimal(0));    //C4_38 女子給与個人免除保険料率
+                val femaleEmployeeExemptionRate         = femaleContributionRate.getEmployeeExemptionRate().map(PrimitiveValueBase::v).orElse(new BigDecimal(0));      //C4_40 女子給与事業主免除保険料率
 
-                val maleSalaryIndividualInsuranceRate   = maleIndividualBurdenRatio.subtract(maleIndividualExemptionRate);                       //C4_10 男子給与個人保険料率
-                val maleSalaryEmployerInsuranceRate     = maleEmployeeContributionRatio.subtract(maleEmployeeExemptionRate);                     //C4_12 男子給与事業主保険料率
-                val femaleSalaryIndividualInsuranceRate = femaleIndividualBurdenRatio.subtract(femaleIndividualExemptionRate);                   //C4_29 女子給与個人保険料率
-                val femaleSalaryEmployerInsuranceRate   = femaleEmployeeContributionRatio.subtract(femaleEmployeeExemptionRate);                 //C4_31 女子給与事業主保険料率
+                val maleSalaryIndividualInsuranceRate   = maleIndividualBurdenRatio.subtract(maleIndividualExemptionRate);                                                  //C4_10 男子給与個人保険料率
+                val maleSalaryEmployerInsuranceRate     = maleEmployeeContributionRatio.subtract(maleEmployeeExemptionRate);                                                //C4_12 男子給与事業主保険料率
+                val femaleSalaryIndividualInsuranceRate = femaleIndividualBurdenRatio.subtract(femaleIndividualExemptionRate);                                              //C4_29 女子給与個人保険料率
+                val femaleSalaryEmployerInsuranceRate   = femaleEmployeeContributionRatio.subtract(femaleEmployeeExemptionRate);                                            //C4_31 女子給与事業主保険料率
 
-                val personalFraction          = this.salaryEmployeesPensionInsuranceRate.getFractionClassification().getPersonalFraction();      //C5_8 給与個人端数区分
-                val businessOwnerFraction     = this.salaryEmployeesPensionInsuranceRate.getFractionClassification().getBusinessOwnerFraction(); //C5_10 給与事業主端数区分
-                val employeeShareAmountMethod = this.salaryEmployeesPensionInsuranceRate.getEmployeeShareAmountMethod();                         //事業主負担分計算方法
+                val personalFraction                    = this.salaryEmployeesPensionInsuranceRate.getFractionClassification().getPersonalFraction();                       //C5_8 給与個人端数区分
+                val businessOwnerFraction               = this.salaryEmployeesPensionInsuranceRate.getFractionClassification().getBusinessOwnerFraction();                  //C5_10 給与事業主端数区分
+                val employeeShareAmountMethod           = this.salaryEmployeesPensionInsuranceRate.getEmployeeShareAmountMethod();                                          //事業主負担分計算方法
 
                 //「厚生年金基金加入区分」で「無」を選択している場合
                 if (FundClassification.NOT_JOIN.equals(welfarePension.getFundClassification())) {
-                    insuredMaleInsurancePremium   = RoundCalculatedValue.calculation(standardMonthlyFee, maleIndividualBurdenRatio, personalFraction, RoundCalculatedValue.ROUND_1_AFTER_DOT);
-                    insuredFemaleInsurancePremium = RoundCalculatedValue.calculation(standardMonthlyFee, femaleIndividualBurdenRatio, personalFraction, RoundCalculatedValue.ROUND_1_AFTER_DOT);
+                    insuredMaleInsurancePremium      = RoundCalculatedValue.calculation(standardMonthlyFee, maleIndividualBurdenRatio, personalFraction, RoundCalculatedValue.ROUND_1_AFTER_DOT);
+                    insuredFemaleInsurancePremium    = RoundCalculatedValue.calculation(standardMonthlyFee, femaleIndividualBurdenRatio, personalFraction, RoundCalculatedValue.ROUND_1_AFTER_DOT);
                 } else {
-                    insuredMaleInsurancePremium       = RoundCalculatedValue.calculation(standardMonthlyFee, maleIndividualBurdenRatio.subtract(maleIndividualExemptionRate), personalFraction, RoundCalculatedValue.ROUND_1_AFTER_DOT);
-                    insuredMaleExemptionInsurance     = RoundCalculatedValue.calculation(standardMonthlyFee, maleIndividualExemptionRate, personalFraction, RoundCalculatedValue.ROUND_1_AFTER_DOT);
-                    insuredFemaleInsurancePremium     = RoundCalculatedValue.calculation(standardMonthlyFee, femaleIndividualBurdenRatio.subtract(femaleIndividualExemptionRate), personalFraction, RoundCalculatedValue.ROUND_1_AFTER_DOT);
-                    insuredFemaleExemptionInsurance   = RoundCalculatedValue.calculation(standardMonthlyFee, femaleIndividualExemptionRate, personalFraction, RoundCalculatedValue.ROUND_1_AFTER_DOT);
+                    insuredMaleInsurancePremium      = RoundCalculatedValue.calculation(standardMonthlyFee, maleIndividualBurdenRatio.subtract(maleIndividualExemptionRate), personalFraction, RoundCalculatedValue.ROUND_1_AFTER_DOT);
+                    insuredMaleExemptionInsurance    = RoundCalculatedValue.calculation(standardMonthlyFee, maleIndividualExemptionRate, personalFraction, RoundCalculatedValue.ROUND_1_AFTER_DOT);
+                    insuredFemaleInsurancePremium    = RoundCalculatedValue.calculation(standardMonthlyFee, femaleIndividualBurdenRatio.subtract(femaleIndividualExemptionRate), personalFraction, RoundCalculatedValue.ROUND_1_AFTER_DOT);
+                    insuredFemaleExemptionInsurance  = RoundCalculatedValue.calculation(standardMonthlyFee, femaleIndividualExemptionRate, personalFraction, RoundCalculatedValue.ROUND_1_AFTER_DOT);
                 }
 
                 //「厚生年金基金加入区分」で「無」を選択している場合,「事業主負担率を用いて計算する」が選択されている場合
                 if (FundClassification.NOT_JOIN.equals(welfarePension.getFundClassification()) && EmployeeShareAmountMethod.EMPLOYEE_CONTRIBUTION_RATIO.equals(employeeShareAmountMethod)) {
-                    employeeMaleInsurancePremium   = RoundCalculatedValue.calculation(standardMonthlyFee, maleEmployeeContributionRatio, businessOwnerFraction, RoundCalculatedValue.ROUND_1_AFTER_DOT);
-                    employeeFemaleInsurancePremium = RoundCalculatedValue.calculation(standardMonthlyFee, femaleEmployeeContributionRatio, businessOwnerFraction, RoundCalculatedValue.ROUND_1_AFTER_DOT);
+                    employeeMaleInsurancePremium     = RoundCalculatedValue.calculation(standardMonthlyFee, maleEmployeeContributionRatio, businessOwnerFraction, RoundCalculatedValue.ROUND_1_AFTER_DOT);
+                    employeeFemaleInsurancePremium   = RoundCalculatedValue.calculation(standardMonthlyFee, femaleEmployeeContributionRatio, businessOwnerFraction, RoundCalculatedValue.ROUND_1_AFTER_DOT);
                 }
                 //「厚生年金基金加入区分」で「無」を選択している場合,「全体の保険料から被保険者分を差し引く」が選択されている場合
                 else if (FundClassification.NOT_JOIN.equals(welfarePension.getFundClassification()) && EmployeeShareAmountMethod.SUBTRACT_INSURANCE_PREMIUM.equals(employeeShareAmountMethod)) {
