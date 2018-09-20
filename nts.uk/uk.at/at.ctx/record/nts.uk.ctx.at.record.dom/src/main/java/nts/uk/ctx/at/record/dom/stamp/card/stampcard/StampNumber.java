5f500a0cf33b64c3fcec999429fa17aa33701656
Pham Van dan <dan_pv@nittsusystem-vn.com>
2018-09-20 17:47:29 +0900

    fix bug 99545. mr.Lai remove CharType from primitive

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/stamp/card/stampcard/StampNumber.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/stamp/card/stampcard/StampNumber.java
@@ -1,12 +1,14 @@
 package nts.uk.ctx.at.record.dom.stamp.card.stampcard;
 
 import nts.arc.primitive.StringPrimitiveValue;
+import nts.arc.primitive.constraint.CharType;
+import nts.arc.primitive.constraint.StringCharType;
 import nts.arc.primitive.constraint.StringMaxLength;
 import nts.arc.primitive.constraint.StringRegEx;
 
 
 @StringMaxLength(20)
-//@StringCharType(CharType.ANY_HALF_WIDTH)
+@StringCharType(CharType.ANY_HALF_WIDTH)
 @StringRegEx("^[a-zA-Z0-9\\s#$%&()~:|{}*+?@'<>_/;\"\\\\\\[\\]\\`-]{1,20}$")
 public class StampNumber extends StringPrimitiveValue<StampNumber> {
 
