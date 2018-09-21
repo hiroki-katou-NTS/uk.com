7a01940d223683e0151b909192c8849ae5763c48
Trần Thanh Tài <tai.tt@3si.vn>
2018-09-21 08:50:10 +0700

    QMM011 Fix bug focus Screen E,F. + chỉnh sửa giao diện

--- nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/011/f/qmm011.f.start.ts
+++ nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/011/f/qmm011.f.start.ts
@@ -2,5 +2,6 @@ module nts.uk.pr.view.qmm011.f {
     __viewContext.ready(function() {
         var screenModel = new viewmodel.ScreenModel();
             __viewContext.bind(screenModel);
+            _.defer(() => {$('#F1_6').focus()});
     });
 }
