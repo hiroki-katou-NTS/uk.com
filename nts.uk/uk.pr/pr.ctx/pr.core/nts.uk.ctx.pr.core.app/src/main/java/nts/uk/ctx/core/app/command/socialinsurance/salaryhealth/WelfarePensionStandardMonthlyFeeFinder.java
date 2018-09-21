589b57d92d403db7e1b097f06267922fce9f3e69
ThuyetND_3S <thuyet.nd@3si.vn>
2018-09-21 09:25:44 +0700

    add stateless

--- nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.app/src/main/java/nts/uk/ctx/core/app/command/socialinsurance/salaryhealth/WelfarePensionStandardMonthlyFeeFinder.java
+++ nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.app/src/main/java/nts/uk/ctx/core/app/command/socialinsurance/salaryhealth/WelfarePensionStandardMonthlyFeeFinder.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 
@@ -23,6 +24,7 @@ import nts.uk.ctx.core.dom.socialinsurance.welfarepensioninsurance.WelfarePensio
 import nts.uk.ctx.core.dom.socialinsurance.welfarepensioninsurance.WelfarePensionStandardMonthlyFeeRepository;
 
 @Transactional
+@Stateless
 public class WelfarePensionStandardMonthlyFeeFinder {
 
 	@Inject
