835a31075ccdd68df608086656a49b64d23d644c
nws-anhnm <anhnm@newwave.vn>
2018-09-21 11:04:07 +0700

    [CCG001] fix bug dateRangePicker not synchronized

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/share/ccg.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/share/ccg.vm.ts
@@ -950,9 +950,10 @@ module nts.uk.com.view.ccg.share.ccg {
 
                 // synchronize period
                 if (self.dateRangeOfParentScreen) {
-                    const isSameDate = DateRangePickerModel.isSamePeriod(self.dateRangeOfParentScreen(), self.inputPeriod());
+                    const dateRangeOfParentScreen = _.clone(self.dateRangeOfParentScreen());
+                    const isSameDate = DateRangePickerModel.isSamePeriod(dateRangeOfParentScreen, self.inputPeriod());
                     if (!isSameDate) {
-                        self.inputPeriod(self.dateRangeOfParentScreen());
+                        self.inputPeriod(dateRangeOfParentScreen);
                     }
                 } else if (self.periodStartOfParentScreen) {
                     const isSameDate = moment.isMoment(self.periodStartOfParentScreen()) ?
@@ -1519,7 +1520,7 @@ module nts.uk.com.view.ccg.share.ccg {
                 // set period
                 if (options.dateRangePickerValue) {
                     self.dateRangeOfParentScreen = options.dateRangePickerValue;
-                    self.inputPeriod(self.dateRangeOfParentScreen());
+                    self.inputPeriod(_.clone(self.dateRangeOfParentScreen()));
                 }
                 else if (_.isFunction(options.periodStartDate) && _.isFunction(options.periodEndDate)) {
                     self.periodStartOfParentScreen = options.periodStartDate;
@@ -2070,7 +2071,7 @@ module nts.uk.com.view.ccg.share.ccg {
             }
 
             public static isSamePeriod(a: DateRangePickerModel, b: DateRangePickerModel): boolean {
-                return a.startDate == b.startDate && a.endDate == b.endDate
+                return a.startDate === b.startDate && a.endDate === b.endDate
             }
         }
 
