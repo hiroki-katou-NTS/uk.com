968fb0ceaa80a10836c11fe56324ea8d0352ab79
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2018-09-24 15:24:56 +0700

    fixbug: #100900 (disable button)

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/001/a/cm/cps-bdate-ko-comp.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cps/001/a/cm/cps-bdate-ko-comp.ts
@@ -5,11 +5,12 @@ module nts.custom.component {
         <div class="form-group">
             <div data-bind="ntsFormLabel: {text: text('CPS001_35')}"></div>
             <div tabindex="17" data-bind="ntsDatePicker: { enable: !!employeeId(), name: text('CPS001_35'), value: standardDate, dateFormat: 'YYYY/MM/DD'}"></div>
-            <button tabindex="18" data-bind="enable: !!employeeId(), click: function() { id.valueHasMutated() }, text: text('CPS001_37')"></button>
+            <button tabindex="18" data-bind="enable: !!employeeId() && !!enableBtn(), click: function() { id.valueHasMutated() }, text: text('CPS001_37')"></button>
         </div>`,
         viewModel: function(params: any) {
             $.extend(params, {
-                text: nts.uk.resource.getText
+                text: nts.uk.resource.getText,
+                enableBtn: ko.computed(() => !!(ko.toJS(params.standardDate) || '').match(/\d{4}(-|\/)\d{2}(-|\/)\d{2}/))
             });
 
             return params;
