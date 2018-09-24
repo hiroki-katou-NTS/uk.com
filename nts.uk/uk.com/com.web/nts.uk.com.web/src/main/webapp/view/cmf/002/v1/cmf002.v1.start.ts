588d0e921d829da3706c829693885e6b76545f7e
Nguyen Xuan Tam <tamnx@3si.vn>
2018-09-24 17:27:36 +0700

    Fixbug #123416

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmf/002/v1/cmf002.v1.start.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmf/002/v1/cmf002.v1.start.ts
@@ -3,6 +3,7 @@ module nts.uk.com.view.cmf002.v1 {
         __viewContext['screenModel'] = new viewmodel.ScreenModel();
         __viewContext['screenModel'].start().done(function() {
             __viewContext.bind(__viewContext['screenModel']);
+            _.defer(() => {$('#V3_2_container').focus()});
         });
     });
 }
