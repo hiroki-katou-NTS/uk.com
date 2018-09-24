68343aa8c523d9c674f6078e72ff6aae535e44f1
NWS-HoangNDH <hoangndh@newwave.vn>
2018-09-24 14:00:47 +0700

    [KWR006] Use new constants to avoid replacing file name

--- nts.uk/uk.at/at.file/nts.uk.file.at.infra/src/main/java/nts/uk/file/at/infra/schedule/daily/WorkScheOutputConstants.java
+++ nts.uk/uk.at/at.file/nts.uk.file.at.infra/src/main/java/nts/uk/file/at/infra/schedule/daily/WorkScheOutputConstants.java
@@ -41,7 +41,8 @@ public class WorkScheOutputConstants {
 	
 	// File/Sheet name
 	public static final String SHEET_FILE_NAME = "日別勤務表";
-	public static final String SHEET_FILE_NAME_MONTHLY = "sheet1";
+	public static final String SHEET_NAME_MONTHLY = "Sheet1";
+	public static final String FILE_NAME_MONTHLY = "月別勤務集計表";
 	
 	// Calculation text
 	public static final String TOTAL_EMPLOYEE = "個人計";
