8a0c28b82d35b74afc84b5f7ed175e6dca3ca8af
thanhvc3si <thanh.vc@3si.com.vn>
2018-09-26 18:05:07 +0700

    Fix bug cmf002

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmf/002/i/cmf002.i.start.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmf/002/i/cmf002.i.start.ts
@@ -4,9 +4,9 @@ module nts.uk.com.view.cmf002.i {
         __viewContext['screenModel'] = new viewmodel.ScreenModel();
         __viewContext['screenModel'].start().done(function() {
             __viewContext.bind(__viewContext['screenModel']);
-            _.defer(() => {
+            _.delay(() => {
                 __viewContext['screenModel'].numberDataFormatSetting().fixedValue() == model.NOT_USE_ATR.NOT_USE ? $('#I2_1').focus() : $('#I7_1').focus();
-            });
+            }, 1000);
         });
     });
 }
--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmf/002/j/cmf002.j.start.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmf/002/j/cmf002.j.start.ts
@@ -3,9 +3,9 @@ module nts.uk.com.view.cmf002.j {
     __viewContext['screenModel'] = new viewmodel.ScreenModel();
             __viewContext['screenModel'].start().done(function() {
                 __viewContext.bind(__viewContext['screenModel']);
-                _.defer(() => {
+                _.delay(() => {
                 __viewContext['screenModel'].characterDataFormatSetting().fixedValue() == 1 ? $('#J7_1').focus() : $('#J2_1').focus(); 
-            });
+            }, 1000);
             });
     });
 }
