fcb3b0983c0c8eb090cd73ba6f0238853f15ea88
Anh Trung <trung.bv@3si.vn>
2018-09-27 16:01:37 +0700

    Fix lỗi sort R7_1

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/exo/exoutsummaryservice/CtgItemDataCustom.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/exo/exoutsummaryservice/CtgItemDataCustom.java
@@ -4,10 +4,12 @@ import lombok.Getter;
 
 @Getter
 public class CtgItemDataCustom {
+	int seriNum;
 	String itemName;
 	String conditions;
 	
-	public CtgItemDataCustom(String itemName, String conditions) {
+	public CtgItemDataCustom(int seriNum, String itemName, String conditions) {
+		this.seriNum = seriNum;
 		this.itemName = itemName;
 		this.conditions = conditions;
 	}
