c35bf33af08f8774f0538754f3c9e831b462dd1a
NSVC\nampt <nam_pt@nittsusystem-vn.com>
2018-09-28 15:52:10 +0700

    update AppOvertimeFinder
    add thêm method

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/dailyperformanceprocessing/repository/ReflectBreakTimzoneImpl.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/dailyperformanceprocessing/repository/ReflectBreakTimzoneImpl.java
@@ -2,6 +2,7 @@ package nts.uk.ctx.at.record.dom.dailyperformanceprocessing.repository;
 
 import java.util.Optional;
 
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import nts.uk.ctx.at.record.dom.dailyperformanceprocessing.output.BreakTimeZoneSettingOutPut;
@@ -11,63 +12,64 @@ import nts.uk.ctx.at.shared.dom.schedule.basicschedule.WorkStyle;
 import nts.uk.ctx.at.shared.dom.worktime.worktimeset.WorkTimeSetting;
 import nts.uk.ctx.at.shared.dom.worktime.worktimeset.WorkTimeSettingRepository;
 
+@Stateless
 public class ReflectBreakTimzoneImpl implements BreakTimeZoneService {
+	
+	@Inject
+	private ReflectBreakTimeOfDailyDomainService reflectBreakTimeOfDailyDomainService;
 
-//	@Inject
-//	private ReflectBreakTimeOfDailyDomainServiceImpl reflectBreakTimeOfDailyDomainServiceImpl;
-//
-//	@Inject
-//	private WorkTimeSettingRepository workTimeSettingRepo;
+	@Inject
+	private WorkTimeSettingRepository workTimeSettingRepo;
 
 	@Override
 	public BreakTimeZoneSharedOutPut getBreakTimeZone(String companyId, String workTimeCode,
 			int weekdayHolidayClassification, WorkStyle checkWorkDay) {
 
-//		BreakTimeZoneSharedOutPut breakTimeZoneSharedOutPut = new BreakTimeZoneSharedOutPut();
-//
-//		BreakTimeZoneSettingOutPut breakTimeZoneSettingOutPut = new BreakTimeZoneSettingOutPut();
-//
-//		Optional<WorkTimeSetting> WorkTimeSettingOptional = this.workTimeSettingRepo.findByCode(companyId,
-//				workTimeCode);
-//		WorkTimeSetting workTimeSetting = WorkTimeSettingOptional.orElse(null);
-//
-//		// weekdayHolidayClassification :
-//		// 平日 : 0 , 休日 : 1
-//
-//		boolean checkReflect = false;
-//		if (workTimeSetting != null && workTimeSetting.getWorkTimeDivision().getWorkTimeDailyAtr().value == 0) {
-//
-//			switch (workTimeSetting.getWorkTimeDivision().getWorkTimeMethodSet().value) {
-//			case 0:// 固定勤務
-//				checkReflect = reflectBreakTimeOfDailyDomainServiceImpl.CheckBreakTimeFromFixedWorkSetting(companyId,
-//						weekdayHolidayClassification, workTimeCode, breakTimeZoneSettingOutPut, checkWorkDay);
-//				break;
-//			case 2:// 流動勤務
-//				checkReflect = reflectBreakTimeOfDailyDomainServiceImpl.confirmIntermissionTimeZone(companyId,
-//						weekdayHolidayClassification, workTimeCode, breakTimeZoneSettingOutPut);
-//
-//				break;
-//			case 1:// 時差勤務
-////				checkReflect = reflectBreakTimeOfDailyDomainServiceImpl.ConfirmInterTimezoneStaggeredWorkSetting(
-////						companyId, employeeID, processingDate, empCalAndSumExecLogID, weekdayHolidayClassification,
-////						null, // WorkInfo ,
-//				// breakTimeZoneSettingOutPut, checkWorkDay);
-//				break;
-//
-//			default:
-//				checkReflect = reflectBreakTimeOfDailyDomainServiceImpl.CheckBreakTimeFromFixedWorkSetting(companyId,
-//						weekdayHolidayClassification, workTimeCode, breakTimeZoneSettingOutPut, checkWorkDay);
-//				break;
-//			}
-//
-//		} else {
-//			checkReflect = reflectBreakTimeOfDailyDomainServiceImpl.confirmInterFlexWorkSetting(companyId,
-//					weekdayHolidayClassification, workTimeCode, breakTimeZoneSettingOutPut, checkWorkDay);
-//		}
-//
-//		breakTimeZoneSharedOutPut.setLstTimezone(breakTimeZoneSettingOutPut.getLstTimezone());
+		BreakTimeZoneSharedOutPut breakTimeZoneSharedOutPut = new BreakTimeZoneSharedOutPut();
 
-		return null;
+		BreakTimeZoneSettingOutPut breakTimeZoneSettingOutPut = new BreakTimeZoneSettingOutPut();
+
+		Optional<WorkTimeSetting> WorkTimeSettingOptional = this.workTimeSettingRepo.findByCode(companyId,
+				workTimeCode);
+		WorkTimeSetting workTimeSetting = WorkTimeSettingOptional.orElse(null);
+
+		// weekdayHolidayClassification :
+		// 平日 : 0 , 休日 : 1
+
+		boolean checkReflect = false;
+		if (workTimeSetting != null && workTimeSetting.getWorkTimeDivision().getWorkTimeDailyAtr().value == 0) {
+
+			switch (workTimeSetting.getWorkTimeDivision().getWorkTimeMethodSet().value) {
+			case 0:// 固定勤務
+				checkReflect = reflectBreakTimeOfDailyDomainService.CheckBreakTimeFromFixedWorkSetting(companyId,
+						weekdayHolidayClassification, workTimeCode, breakTimeZoneSettingOutPut, checkWorkDay);
+				break;
+			case 2:// 流動勤務
+				checkReflect = reflectBreakTimeOfDailyDomainService.confirmIntermissionTimeZone(companyId,
+						weekdayHolidayClassification, workTimeCode, breakTimeZoneSettingOutPut);
+
+				break;
+			case 1:// 時差勤務
+//				checkReflect = reflectBreakTimeOfDailyDomainServiceImpl.ConfirmInterTimezoneStaggeredWorkSetting(
+//						companyId, employeeID, processingDate, empCalAndSumExecLogID, weekdayHolidayClassification,
+//						null, // WorkInfo ,
+				// breakTimeZoneSettingOutPut, checkWorkDay);
+				break;
+
+			default:
+				checkReflect = reflectBreakTimeOfDailyDomainService.CheckBreakTimeFromFixedWorkSetting(companyId,
+						weekdayHolidayClassification, workTimeCode, breakTimeZoneSettingOutPut, checkWorkDay);
+				break;
+			}
+
+		} else {
+			checkReflect = reflectBreakTimeOfDailyDomainService.confirmInterFlexWorkSetting(companyId,
+					weekdayHolidayClassification, workTimeCode, breakTimeZoneSettingOutPut, checkWorkDay);
+		}
+
+		breakTimeZoneSharedOutPut.setLstTimezone(breakTimeZoneSettingOutPut.getLstTimezone());
+
+		return breakTimeZoneSharedOutPut;
 	}
 
 }
