2fb727f102de2316895035a101c1b0d3ca8bfae8
3SI\sang.nv <sang.nv@3si.vn>
2018-10-01 14:55:49 +0700

    Fixbug tabindex

--- nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/008/d/qmm008.d.start.ts
+++ nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/008/d/qmm008.d.start.ts
@@ -1,6 +1,24 @@
 module nts.uk.pr.view.qmm008.d {
-    __viewContext.ready(function() {
+    __viewContext.ready(function () {
         let screenModel = new nts.uk.pr.view.qmm008.d.viewmodel.ScreenModel();
-            __viewContext.bind(screenModel);
+        __viewContext.bind(screenModel);
+        setTimeout(function () {
+            $('#D4_23').on("keydown", function (e) {
+                let code = e.keyCode || e.which;
+                if (code === 9) {
+                    e.preventDefault();
+                    screenModel.selectedTab("tab-2");
+                    $('#D5_4').focus();
+                }
+            });
+
+            $('#D5_4').keydown(function (e) {
+                if (e.shiftKey && e.key === 'Tab') {
+                    e.preventDefault();
+                    screenModel.selectedTab("tab-1");
+                    $('#D4_23').focus();
+                }
+            });
+        }, 2000);
     });
 }
