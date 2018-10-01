c57ceb1f7a5c850b52fdbb9ec80fd2a3692004be
3SI\sang.nv <sang.nv@3si.vn>
2018-10-01 15:23:07 +0700

    Update tabindex

--- nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/008/d/qmm008.d.start.ts
+++ nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/008/d/qmm008.d.start.ts
@@ -3,9 +3,9 @@ module nts.uk.pr.view.qmm008.d {
         let screenModel = new nts.uk.pr.view.qmm008.d.viewmodel.ScreenModel();
         __viewContext.bind(screenModel);
         setTimeout(function () {
-            $('#D4_23').on("keydown", function (e) {
+            $('#D4_23').keydown(function (e) {
                 let code = e.keyCode || e.which;
-                if (code === 9) {
+                if (code === 9 && !e.shiftKey) {
                     e.preventDefault();
                     screenModel.selectedTab("tab-2");
                     $('#D5_4').focus();
