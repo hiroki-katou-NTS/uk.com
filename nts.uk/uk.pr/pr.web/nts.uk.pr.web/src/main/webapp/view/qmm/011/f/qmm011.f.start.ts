4ef9f5d002376fdbb4f916e76277851ddc43a48b
Nguyen Xuan Tam <tamnx@3si.vn>
2018-10-02 18:58:55 +0700

    QMM011 update last commit

--- nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/011/f/qmm011.f.start.ts
+++ nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/011/f/qmm011.f.start.ts
@@ -2,10 +2,6 @@ module nts.uk.pr.view.qmm011.f {
     __viewContext.ready(function() {
         var screenModel = new viewmodel.ScreenModel();
             __viewContext.bind(screenModel);
-        if(screenModel.canDelete()){
-            $('#F1_6').focus();
-        }else{
-            $('#F1_7').focus();
-        }
+            $('#F1_5').focus();
     });
 }
