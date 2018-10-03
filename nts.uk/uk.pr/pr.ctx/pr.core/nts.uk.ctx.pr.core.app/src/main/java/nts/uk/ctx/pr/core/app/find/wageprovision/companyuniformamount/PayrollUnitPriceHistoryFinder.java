4ab85593abdc262e713d8709ec2888afcf2534ce
tranphuc7686 <35018596+tranphuc7686@users.noreply.github.com>
2018-10-03 17:24:04 +0700

    [QMM007] Code server

--- nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.app/src/main/java/nts/uk/ctx/pr/core/app/find/wageprovision/companyuniformamount/PayrollUnitPriceHistoryFinder.java
+++ nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.app/src/main/java/nts/uk/ctx/pr/core/app/find/wageprovision/companyuniformamount/PayrollUnitPriceHistoryFinder.java
@@ -1,9 +1,12 @@
 package nts.uk.ctx.pr.core.app.find.wageprovision.companyuniformamount;
 
 
+import nts.uk.ctx.pr.core.wageprovision.companyuniformamount.PayrollUnitPriceHistory;
 import nts.uk.ctx.pr.core.wageprovision.companyuniformamount.PayrollUnitPriceHistoryRepository;
+import nts.uk.shr.com.context.AppContexts;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.ejb.Stateless;
@@ -22,6 +25,10 @@ public class PayrollUnitPriceHistoryFinder
     public List<PayrollUnitPriceHistoryDto> getAllPayrollUnitPriceHistory(){
         return finder.getAllPayrollUnitPriceHistory().stream().map(item -> PayrollUnitPriceHistoryDto.fromDomain(item))
                 .collect(Collectors.toList());
+    }
+    public PayrollUnitPriceHistoryDto getPayrollUnitPriceHis(String hisId){
+        String cId = AppContexts.user().companyId();
+        return PayrollUnitPriceHistoryDto.fromDomain(finder.getPayrollUnitPriceHistoryById(cId,hisId).get());
     }
 
 }
