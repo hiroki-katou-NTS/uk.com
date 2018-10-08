53e36376480f5bcc7e662976a9bd79824b54b414
HoaiDuong <hoaiduong1012@gmail.com>
2018-10-08 22:56:03 +0700

    commit

--- nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/040/a/qmm040.a.vm.ts
+++ nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/040/a/qmm040.a.vm.ts
@@ -1,8 +1,10 @@
 module nts.uk.pr.view.qmm040.a.viewmodel {
+    import getText = nts.uk.resource.getText;
     export class ScreenModel {
 
-        yearMonth:KnockoutObservable<number>=ko.observable(201804);
-
+        yearMonth: KnockoutObservable<number> = ko.observable(201804);
+        onTab: KnockoutObservable<number>=ko.observable(0);
+        titleTab:KnockoutObservable<string>=ko.observable(getText('QMM040_3'));
 
         constructor() {
             var self = this;
@@ -19,5 +21,38 @@ module nts.uk.pr.view.qmm040.a.viewmodel {
             dfd.resolve();
             return dfd.promise();
         }
+
+
+        onSelectTab(param) {
+            let self=this;
+
+            switch (param) {
+                case 0:
+                    //TODO
+                    self.onTab(0);
+                    self.titleTab(getText('QMM040_3'));
+                    break;
+                case 1:
+                    //TODO
+                    self.onTab(1);
+                    break;
+                case 2:
+                    //TODO
+                    self.onTab(2);
+                    break;
+                case 3:
+                    //TODO
+                    self.onTab(3);
+                    break;
+                default:
+                    //TODO
+                    self.onTab(0);
+                    break;
+            }
+        }
+
     }
+
+
+
 }
