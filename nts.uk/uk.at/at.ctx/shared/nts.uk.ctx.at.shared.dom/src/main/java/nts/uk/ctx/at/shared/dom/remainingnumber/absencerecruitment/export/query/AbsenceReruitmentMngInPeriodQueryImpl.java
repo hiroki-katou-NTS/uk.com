7de83dab8accd69195aee16c1ed456e0ba39dade
NTS_DoThiDu <du_dt@nittsusystem-vn.com>
2018-10-10 17:24:27 +0900

    check tam

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/remainingnumber/absencerecruitment/export/query/AbsenceReruitmentMngInPeriodQueryImpl.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/remainingnumber/absencerecruitment/export/query/AbsenceReruitmentMngInPeriodQueryImpl.java
@@ -730,8 +730,12 @@ public class AbsenceReruitmentMngInPeriodQueryImpl implements AbsenceReruitmentM
 		}
 		List<AbsRecDetailPara> lstOutputOfAbs = new ArrayList<>();
 		for (InterimAbsMng interimAbsMng : lstAbsMng) {
-			InterimRemain remainData = lstInterimMngOfAbs.stream().filter(x -> x.getRemainManaID().equals(interimAbsMng.getAbsenceMngId()))
-					.collect(Collectors.toList()).get(0);
+			List<InterimRemain> lstRemainData = lstInterimMngOfAbs.stream().filter(x -> x.getRemainManaID().equals(interimAbsMng.getAbsenceMngId()))
+					.collect(Collectors.toList());
+			if(lstRemainData.isEmpty()) {
+				continue;
+			}
+			InterimRemain remainData = lstRemainData.get(0);
 			//アルゴリズム「振出と紐付けをしない振休を取得する」を実行する
 			AbsRecDetailPara outputData = this.getNotTypeRec(interimAbsMng, remainData);
 			lstOutputOfAbs.add(outputData);
