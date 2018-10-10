4c994dd5a955fbd85e39c5fe8e80aaf830ecbf92
NSVC\hieult <hieultph03963@fpt.edu.vn>
2018-10-10 16:45:08 +0700

    Revert "Revert "create Event：権限管理の初期値登録""
    This reverts commit 465436e4f80dc2b3b122f3b905eefb08b90bb350.

--- nts.uk/uk.com/com.ctx/sys/portal/nts.uk.ctx.sys.portal.dom/src/main/java/nts/uk/ctx/sys/portal/dom/webmenu/webmenulinking/RoleByRoleTiesRepository.java
+++ nts.uk/uk.com/com.ctx/sys/portal/nts.uk.ctx.sys.portal.dom/src/main/java/nts/uk/ctx/sys/portal/dom/webmenu/webmenulinking/RoleByRoleTiesRepository.java
@@ -1,14 +1,19 @@
 package nts.uk.ctx.sys.portal.dom.webmenu.webmenulinking;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RoleByRoleTiesRepository {
-	
+
 	Optional<RoleByRoleTies> getRoleByRoleTiesById(String roleId);
-	
+
 	void insertRoleByRoleTies(RoleByRoleTies roleByRoleTies);
-	
+
 	void updateRoleByRoleTies(RoleByRoleTies roleByRoleTies);
-	
+
 	void deleteRoleByRoleTies(String roleId);
+
+	void insertAll(List<RoleByRoleTies> roleTies);
+
 }
+
