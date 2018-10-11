34ddd8c64ce9b17b9dac73235199117c4ce8d20e
thanh.hc <thanh.hc@3si.vn>
2018-10-11 13:56:46 +0700

    update screen A QMM039

--- nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/039/a/qmm039.a.service.ts
+++ nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/039/a/qmm039.a.service.ts
@@ -3,16 +3,14 @@ module nts.uk.pr.view.qmm039.a.service {
     import format = nts.uk.text.format;
 
     var paths = {
-        get: "#",
+        getInfoEmLogin: "workflow/approvermanagement/workroot/getInforPsLogin",
+        getWpName: "screen/com/kcp010/getLoginWorkPlace",
     }
 
-
-    // service 1
-    // domain PerProcessClsSet
-
-
-    export function get (param: string): JQueryPromise<any> {
-        let _path = format(paths.get, param);
-        return ajax(_path);
+    export function getInfoEmLogin(): JQueryPromise<any> {
+        return ajax("com", paths.getInfoEmLogin);
+    }
+    export function getWpName(): JQueryPromise<any> {
+        return ajax("com", paths.getWpName);
     }
 }
