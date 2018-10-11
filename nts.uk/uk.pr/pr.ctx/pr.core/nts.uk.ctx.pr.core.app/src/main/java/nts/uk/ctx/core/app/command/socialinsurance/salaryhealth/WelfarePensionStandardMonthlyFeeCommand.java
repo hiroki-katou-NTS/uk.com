a1fac4e7efd00fe0c9b22a146ed73289f7942bcb
3SI\sang.nv <sang.nv@3si.vn>
2018-10-11 11:44:35 +0700

    Fix deploy

--- nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.app/src/main/java/nts/uk/ctx/core/app/command/socialinsurance/salaryhealth/WelfarePensionStandardMonthlyFeeCommand.java
+++ nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.app/src/main/java/nts/uk/ctx/core/app/command/socialinsurance/salaryhealth/WelfarePensionStandardMonthlyFeeCommand.java
@@ -10,41 +10,38 @@ import javax.inject.Inject;
 
 import nts.arc.layer.app.command.CommandHandlerContext;
 import nts.arc.layer.app.command.CommandHandlerWithResult;
+import nts.uk.ctx.core.app.command.socialinsurance.salaryhealth.dto.CusWelfarePensionDto;
 import nts.uk.ctx.core.app.command.socialinsurance.salaryhealth.dto.UpdateCommandWelfare;
 import nts.uk.ctx.core.dom.socialinsurance.welfarepensioninsurance.*;
 
 @Stateless
 public class WelfarePensionStandardMonthlyFeeCommand extends CommandHandlerWithResult<UpdateCommandWelfare, List<String>> {
-	
-	@Inject
-	private EmployeesPensionMonthlyInsuranceFeeRepository employeesPensionMonthlyInsuranceFeeRepository;
 
-	@Inject
-	private WelfarePensionInsuranceClassificationRepository welfarePensionInsuranceClassificationRepository;
-	
-	@Override
-	protected List<String> handle(CommandHandlerContext<UpdateCommandWelfare> context) {
-		
-		List<String> response = new ArrayList<>();
-		UpdateCommandWelfare command = context.getCommand();
-		
-		// ドメインモデル「厚生年金保険月額保険料額」を更新する
-		Optional<EmployeesPensionMonthlyInsuranceFee> employeesPension = employeesPensionMonthlyInsuranceFeeRepository
-				.getEmployeesPensionMonthlyInsuranceFeeByHistoryId(command.getHistoryId());
-		Optional<WelfarePensionInsuranceClassification> data = welfarePensionInsuranceClassificationRepository.getWelfarePensionInsuranceClassificationById(command.getHistoryId());
-		if (employeesPension.isPresent()) {
-			List<GradeWelfarePensionInsurancePremium> dataUpdate = command.getCusWelfarePensions().stream()
-					.map(x -> x.fromToDomain(data)).collect(Collectors.toList());
-			employeesPension.get().updateGradeList(dataUpdate);
-			List<GradeWelfarePensionInsurancePremium> datacheck = employeesPension.get().getPensionInsurancePremium();
-			if(datacheck.isEmpty()) {
-				employeesPensionMonthlyInsuranceFeeRepository.insertWelfarePension(employeesPension.get());
-			} else {
-				employeesPensionMonthlyInsuranceFeeRepository.updateWelfarePension(employeesPension.get());
-			}
-		}
-		response.add("Msg_15");
-		return response;
-	} 
+    @Inject
+    private EmployeesPensionMonthlyInsuranceFeeRepository employeesPensionMonthlyInsuranceFeeRepository;
+
+    @Override
+    protected List<String> handle(CommandHandlerContext<UpdateCommandWelfare> context) {
+
+        List<String> response = new ArrayList<>();
+        UpdateCommandWelfare command = context.getCommand();
+
+        // ドメインモデル「厚生年金保険月額保険料額」を更新する
+        Optional<EmployeesPensionMonthlyInsuranceFee> employeesPension = employeesPensionMonthlyInsuranceFeeRepository
+                .getEmployeesPensionMonthlyInsuranceFeeByHistoryId(command.getHistoryId());
+        if (employeesPension.isPresent()) {
+            List<GradeWelfarePensionInsurancePremium> dataUpdate = command.getCusWelfarePensions().stream()
+                    .map(CusWelfarePensionDto::fromToDomain).collect(Collectors.toList());
+            employeesPension.get().updateGradeList(dataUpdate);
+            List<GradeWelfarePensionInsurancePremium> datacheck = employeesPension.get().getPensionInsurancePremium();
+            if (datacheck.isEmpty()) {
+                employeesPensionMonthlyInsuranceFeeRepository.insertWelfarePension(employeesPension.get());
+            } else {
+                employeesPensionMonthlyInsuranceFeeRepository.updateWelfarePension(employeesPension.get());
+            }
+        }
+        response.add("Msg_15");
+        return response;
+    }
 
 }
