92848a3a5e118c0b196ba35ce5a20d0c761ec199
ThuyetND_3S <thuyet.nd@3si.vn>
2018-10-15 11:11:00 +0700

    Fix bug screen F v2

--- nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.app/src/main/java/nts/uk/ctx/core/app/command/socialinsurance/salaryhealth/dto/CusWelfarePensionDto.java
+++ nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.app/src/main/java/nts/uk/ctx/core/app/command/socialinsurance/salaryhealth/dto/CusWelfarePensionDto.java
@@ -1,15 +1,17 @@
 package nts.uk.ctx.core.app.command.socialinsurance.salaryhealth.dto;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import nts.uk.ctx.core.dom.socialinsurance.welfarepensioninsurance.GradeWelfarePensionInsurancePremium;
+import nts.uk.ctx.core.dom.socialinsurance.welfarepensioninsurance.WelfarePensionInsuranceClassification;
 
 @Data
 @AllArgsConstructor
 public class CusWelfarePensionDto {
-	
+
 	private int welfarePensionGrade;
 	private long standardMonthlyFee;
 	private Long rewardMonthlyLowerLimit;
@@ -22,16 +24,24 @@ public class CusWelfarePensionDto {
 	private String emFemaleInsurancePremium;
 	private String inFemaleExemptionInsurance;
 	private String emFemaleExemptionInsurance;
-	
-	public GradeWelfarePensionInsurancePremium fromToDomain() {
-		return new GradeWelfarePensionInsurancePremium(this.welfarePensionGrade,
-				fromString(this.inFemaleInsurancePremium), fromString(this.inMaleInsurancePremium),
-				this.inFemaleExemptionInsurance != null ? fromString(this.inFemaleExemptionInsurance) : null, this.inMaleExemptionInsurance != null ? fromString(this.inMaleExemptionInsurance) : null,
-				fromString(this.emFemaleInsurancePremium), fromString(this.emMaleInsurancePremium),
-				this.emFemaleExemptionInsurance != null ? fromString(this.emFemaleExemptionInsurance) : null , this.emMaleExemptionInsurance != null ?fromString(this.emMaleExemptionInsurance) : null);
+
+	public static GradeWelfarePensionInsurancePremium fromToDomain(Optional<WelfarePensionInsuranceClassification> data, CusWelfarePensionDto dto) {
+		if(data.get().getFundClassification().value == 1) {
+			return new GradeWelfarePensionInsurancePremium(dto.welfarePensionGrade,
+					fromString(dto.getInFemaleInsurancePremium()).add(fromString(dto.inFemaleExemptionInsurance)), fromString(dto.inMaleInsurancePremium).add(fromString(dto.inMaleExemptionInsurance)),
+					dto.inFemaleExemptionInsurance != null ? fromString(dto.inFemaleExemptionInsurance) : null, dto.inMaleExemptionInsurance != null ? fromString(dto.inMaleExemptionInsurance) : null,
+					fromString(dto.emFemaleInsurancePremium).add(fromString(dto.emFemaleExemptionInsurance)), fromString(dto.emMaleInsurancePremium).add(fromString(dto.emMaleExemptionInsurance)),
+					dto.emFemaleExemptionInsurance != null ? fromString(dto.emFemaleExemptionInsurance) : null , dto.emMaleExemptionInsurance != null ?fromString(dto.emMaleExemptionInsurance) : null);
+		} else {
+			return new GradeWelfarePensionInsurancePremium(dto.welfarePensionGrade,
+					fromString(dto.inFemaleInsurancePremium), fromString(dto.inMaleInsurancePremium),
+					dto.inFemaleExemptionInsurance != null ? fromString(dto.inFemaleExemptionInsurance) : null, dto.inMaleExemptionInsurance != null ? fromString(dto.inMaleExemptionInsurance) : null,
+					fromString(dto.emFemaleInsurancePremium), fromString(dto.emMaleInsurancePremium),
+					dto.emFemaleExemptionInsurance != null ? fromString(dto.emFemaleExemptionInsurance) : null , dto.emMaleExemptionInsurance != null ?fromString(dto.emMaleExemptionInsurance) : null);
+		}
 	}
 
-	public BigDecimal fromString(String pa) {
+	public static BigDecimal fromString(String pa) {
 		return new BigDecimal(pa).setScale(2, BigDecimal.ROUND_HALF_EVEN);
 	}
 }
