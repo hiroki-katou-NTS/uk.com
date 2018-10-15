78ae793038c826afd0be6774cd7cb2a0f398c223
Thanh.TQ <thanhcoctn95@gmail.com>
2018-10-15 11:36:16 +0700

    QMM018_A

--- nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.ws/src/main/java/nts/uk/ctx/pr/core/ws/wageprovision/averagewagecalculationset/AverageWageCalculationSetService.java
+++ nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.ws/src/main/java/nts/uk/ctx/pr/core/ws/wageprovision/averagewagecalculationset/AverageWageCalculationSetService.java
@@ -1,6 +1,8 @@
 package nts.uk.ctx.pr.core.ws.wageprovision.averagewagecalculationset;
 
 import nts.arc.layer.ws.WebService;
+import nts.uk.ctx.pr.core.app.command.wageprovision.averagewagecalculationset.DataDisplayAverageCommand;
+import nts.uk.ctx.pr.core.app.command.wageprovision.averagewagecalculationset.UpdateAverageWageCalculationSetCommand;
 import nts.uk.ctx.pr.core.app.find.wageprovision.averagewagecalculationset.AverageWageCalculationSetFinder;
 import nts.uk.ctx.pr.core.app.find.wageprovision.averagewagecalculationset.DataDisplayAverageDto;
 import nts.uk.ctx.pr.core.app.find.wageprovision.averagewagecalculationset.StatementDto;
@@ -24,10 +26,19 @@ public class AverageWageCalculationSetService extends WebService {
     @Inject
     private AverageWageCalculationSetFinder finder;
 
+    @Inject
+    private UpdateAverageWageCalculationSetCommand update;
+
     @POST
     @Path("getStatemetItemData")
     public DataDisplayAverageDto getStatemetItem() {
         return this.finder.getStatemetItem();
+    }
+
+    @POST
+    @Path("registration")
+    public void registration(DataDisplayAverageCommand command) {
+        this.update.handle(command);
     }
 
     @POST
