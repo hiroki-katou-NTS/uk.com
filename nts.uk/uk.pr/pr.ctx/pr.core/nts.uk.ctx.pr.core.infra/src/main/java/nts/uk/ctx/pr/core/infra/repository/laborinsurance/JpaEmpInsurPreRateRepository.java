33f7489baea076bd4b53668c4ba9e83e74364fe4
Nguyen Xuan Tam <tamnx@3si.vn>
2018-10-17 17:24:56 +0700

    Update Domain QMM011

---
+++ nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.infra/src/main/java/nts/uk/ctx/pr/core/infra/repository/laborinsurance/JpaEmpInsurPreRateRepository.java
@@ -0,0 +1,72 @@
+package nts.uk.ctx.pr.core.infra.repository.laborinsurance;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.ejb.Stateless;
+
+import nts.arc.layer.infra.data.JpaRepository;
+import nts.uk.ctx.pr.core.dom.laborinsurance.EmpInsurBusBurRatio;
+import nts.uk.ctx.pr.core.dom.laborinsurance.EmpInsurBusBurRatioRepository;
+import nts.uk.ctx.pr.core.dom.laborinsurance.EmpInsurHis;
+import nts.uk.ctx.pr.core.dom.laborinsurance.EmpInsurPreRate;
+import nts.uk.ctx.pr.core.infra.entity.laborinsurance.QpbmtEmpInsurPreRate;
+import nts.uk.ctx.pr.core.infra.entity.laborinsurance.QpbmtEmpInsurPreRatePk;
+import nts.uk.shr.com.history.YearMonthHistoryItem;
+
+@Stateless
+public class JpaEmpInsurPreRateRepository extends JpaRepository implements EmpInsurBusBurRatioRepository {
+
+    private static final String SELECT_ALL_QUERY_STRING = "SELECT f FROM QpbmtEmpInsurPreRate f";
+    private static final String SELECT_BY_KEY_STRING = SELECT_ALL_QUERY_STRING + " WHERE  f.empInsurPreRatePk.cid =:cid AND  f.empInsurPreRatePk.historyId =:historyId ";
+    private static final String SELECT_BY_CID = SELECT_ALL_QUERY_STRING  + " WHERE  f.empInsurPreRatePk.cid =:cid ORDER BY f.startYearMonth";
+    private static final String DELETE_BY_HIS_ID = "DELETE FROM QpbmtEmpInsurBusBurRatio f "
+            + "WHERE f.empInsurBusBurRatioPk.cid =:cid AND f.empInsurBusBurRatioPk.hisId =:hisId";
+    
+    @Override
+    public List<EmpInsurBusBurRatio> getEmpInsurPreRateById(String cid, String historyId){
+    	Optional<QpbmtEmpInsurPreRate> empInsurPreRate = this.queryProxy().query(SELECT_BY_KEY_STRING, QpbmtEmpInsurPreRate.class)
+    			.setParameter("cid", cid)
+    			.setParameter("historyId", historyId)
+    			.getSingle();
+    	if(empInsurPreRate.isPresent()) {
+    		return empInsurPreRate.get().toDomain();
+    	}
+    	return new ArrayList<EmpInsurBusBurRatio>();
+    }
+    
+    @Override
+    public EmpInsurHis getEmpInsurHisByCid(String cid){
+    	List<YearMonthHistoryItem> item = this.queryProxy().query(SELECT_BY_CID, QpbmtEmpInsurPreRate.class)
+    			.setParameter("cid", cid)
+    			.getList(c -> c.toDomainYearMonth());
+    	EmpInsurHis empInsurHis = new EmpInsurHis(cid , item);
+    	return empInsurHis;	
+    }
+    
+    @Override
+	public void add(EmpInsurPreRate domain, String cId, YearMonthHistoryItem item) {
+		this.commandProxy().insert(QpbmtEmpInsurPreRate.toEntity(domain, cId, item));
+	}
+
+	@Override
+	public void update(EmpInsurPreRate domain, String cId, YearMonthHistoryItem item) {
+		this.commandProxy().update(QpbmtEmpInsurPreRate.toEntity(domain, cId, item));
+	}
+	
+	@Override
+	public void update(YearMonthHistoryItem item, String cId) {
+		Optional<QpbmtEmpInsurPreRate> empInsurPreRate = this.queryProxy().find(new QpbmtEmpInsurPreRatePk(cId, item.identifier()), QpbmtEmpInsurPreRate.class);
+		if (empInsurPreRate.isPresent()) {
+			empInsurPreRate.get().startYearMonth = item.start().v();
+			empInsurPreRate.get().endYearMonth = item.end().v();
+			this.commandProxy().update(empInsurPreRate);
+		}
+	}
+
+	@Override
+	public void remove(String cid, String historyId) {
+		this.getEntityManager().createQuery(DELETE_BY_HIS_ID, QpbmtEmpInsurPreRate.class).setParameter("cid", cid).setParameter("hisId", historyId).executeUpdate();
+	}
+}
--- nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.infra/src/main/java/nts/uk/ctx/pr/core/infra/repository/laborinsurance/JpaOccAccIsPrRateRepository.java
+++ nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.infra/src/main/java/nts/uk/ctx/pr/core/infra/repository/laborinsurance/JpaOccAccIsPrRateRepository.java
@@ -1,33 +1,54 @@
 package nts.uk.ctx.pr.core.infra.repository.laborinsurance;
 
-import nts.arc.enums.EnumAdaptor;
-import nts.arc.layer.infra.data.JpaRepository;
-import nts.uk.ctx.pr.core.dom.laborinsurance.*;
-import nts.uk.ctx.pr.core.infra.entity.laborinsurance.QpbmtOccAccIsPrRate;
-import nts.uk.ctx.pr.core.infra.entity.laborinsurance.QpbmtOccAccIsPrRatePk;
-
-
-import javax.ejb.Stateless;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.ejb.Stateless;
+
+import nts.arc.enums.EnumAdaptor;
+import nts.arc.layer.infra.data.JpaRepository;
+import nts.uk.ctx.pr.core.dom.laborinsurance.InsuPremiumFractionClassification;
+import nts.uk.ctx.pr.core.dom.laborinsurance.InsuranceRate;
+import nts.uk.ctx.pr.core.dom.laborinsurance.OccAccInsurBusiBurdenRatio;
+import nts.uk.ctx.pr.core.dom.laborinsurance.OccAccIsHis;
+import nts.uk.ctx.pr.core.dom.laborinsurance.OccAccIsPrRate;
+import nts.uk.ctx.pr.core.dom.laborinsurance.OccAccIsPrRateRepository;
+import nts.uk.ctx.pr.core.infra.entity.laborinsurance.QpbmtOccAccIsPrRate;
+import nts.uk.shr.com.history.YearMonthHistoryItem;
 
 @Stateless
 public class JpaOccAccIsPrRateRepository extends JpaRepository implements OccAccIsPrRateRepository {
 
-    private static final String SELECT_ALL_QUERY_STRING = "SELECT f FROM QpbmtOccAccIsPrRate f";
-    private static final String SELECT_BY_HIS_ID = SELECT_ALL_QUERY_STRING + " WHERE  f.occAccIsPrRatePk.hisId =:hisId";
-    private static final String DELETE_BY_HIS_ID = "DELETE FROM QpbmtOccAccIsPrRate f "
-            + "WHERE f.occAccIsPrRatePk.hisId =:hisId";
-
-
+	private static final String SELECT_ALL_QUERY_STRING = "SELECT f FROM QpbmtOccAccIsPrRate f";
+    private static final String SELECT_BY_HIS_ID = SELECT_ALL_QUERY_STRING + " WHERE  f.occAccIsPrRatePk.hisId =:hisId AND f.occAccIsPrRatePk.cid = :cid";
+    private static final String DELETE_BY_HIS_ID = "DELETE FROM QpbmtOccAccIsPrRate f"
+            + " WHERE f.occAccIsPrRatePk.cid =:cid AND f.occAccIsPrRatePk.hisId =:hisId";
+    private static final String UPDATE_BY_HIS_ID_AND_CID = "UPDATE FROM QpbmtOccAccIsPrRate f "
+    		+ " SET f.startYearMonth = :startYearMonth, f.endYearMonth = :endYearMonth"
+    		+ " WHERE f.occAccIsPrRatePk.hisId =:hisId AND f.occAccIsPrRatePk.cid =:cid";
+    private static final String SELECT_BY_CID =  "SELECT f.occAccIsPrRatePk.hisId, f.startYearMonth, f.endYearMonth FROM QpbmtOccAccIsPrRate f "
+   		+ " WHERE  f.occAccIsPrRatePk.cid =:cid"
+    		+ " GROUP BY f.occAccIsPrRatePk.hisId, f.startYearMonth, f.endYearMonth"
+    		+ " ORDER BY f.startYearMonth";
+   
     @Override
-    public OccAccIsPrRate getOccAccIsPrRateByHisId(String hisId) {
+    public OccAccIsPrRate getOccAccIsPrRateByHisId(String cid, String hisId) {
         List<QpbmtOccAccIsPrRate> occAccIsPrRateList = this.queryProxy().query(SELECT_BY_HIS_ID, QpbmtOccAccIsPrRate.class)
                 .setParameter("hisId", hisId)
                 .getList();
 
         return new OccAccIsPrRate(hisId,toDomain(occAccIsPrRateList));
     }
+    
+    @Override
+    public OccAccIsHis getOccAccIsHisByCid(String cid) {
+        List<QpbmtOccAccIsPrRate> occAccIsPrRateList = this.queryProxy().query(SELECT_BY_CID, QpbmtOccAccIsPrRate.class)
+                .setParameter("cid", cid)
+                .getList();
+
+        return new OccAccIsHis(cid ,QpbmtOccAccIsPrRate.toDomainHis(occAccIsPrRateList));
+    }
+    
     private List<OccAccInsurBusiBurdenRatio> toDomain(List<QpbmtOccAccIsPrRate> entities) {
 
         List<OccAccInsurBusiBurdenRatio> occAccInsurBusiBurdenRatio = new ArrayList<OccAccInsurBusiBurdenRatio>();
@@ -35,30 +56,33 @@ public class JpaOccAccIsPrRateRepository extends JpaRepository implements OccAcc
             return occAccInsurBusiBurdenRatio;
         }
         entities.forEach(entity -> {
-            occAccInsurBusiBurdenRatio.add( new OccAccInsurBusiBurdenRatio(entity.occAccIsPrRatePk.occAccInsurBusNo,
+            occAccInsurBusiBurdenRatio.add( new OccAccInsurBusiBurdenRatio(entity.occAccIsPrRatePk.occAccIsBusNo,
                     EnumAdaptor.valueOf(entity.fracClass,InsuPremiumFractionClassification.class),new InsuranceRate(entity.empConRatio)));
         });
         return occAccInsurBusiBurdenRatio;
     }
 
-    @Override
-    public void remove(int occAccInsurBusNo, String hisId){
-        this.commandProxy().remove(QpbmtOccAccIsPrRate.class, new QpbmtOccAccIsPrRatePk(occAccInsurBusNo, hisId));
-    }
-
 	@Override
-	public void remove(String hisId) {
-		this.getEntityManager().createQuery(DELETE_BY_HIS_ID, QpbmtOccAccIsPrRate.class).setParameter("hisId", hisId).executeUpdate();	
+	public void remove(String cid, String hisId) {
+		this.getEntityManager().createQuery(DELETE_BY_HIS_ID, QpbmtOccAccIsPrRate.class).setParameter("cid", cid).setParameter("hisId", hisId).executeUpdate();	
 	}
 
     @Override
-    public void add(List<OccAccInsurBusiBurdenRatio> domain,String hisId) {
-        this.commandProxy().insertAll(QpbmtOccAccIsPrRate.toEntity(domain,hisId));
+    public void add(List<OccAccInsurBusiBurdenRatio> domain, String cId, YearMonthHistoryItem yearMonthHistory) {
+        this.commandProxy().insertAll(QpbmtOccAccIsPrRate.toEntity(domain, cId, yearMonthHistory));
     }
 
     @Override
-    public void update(List<OccAccInsurBusiBurdenRatio> domain,String hisId) {
-        this.commandProxy().updateAll(QpbmtOccAccIsPrRate.toEntity(domain,hisId));
+    public void update(List<OccAccInsurBusiBurdenRatio> domain, String cId, YearMonthHistoryItem yearMonthHistory) {
+        this.commandProxy().updateAll(QpbmtOccAccIsPrRate.toEntity(domain, cId, yearMonthHistory));
     }
+
+	@Override
+	public void update(String cid, YearMonthHistoryItem item) {
+		this.getEntityManager().createQuery(UPDATE_BY_HIS_ID_AND_CID, QpbmtOccAccIsPrRate.class)
+		.setParameter("startYearMonth", item.start().v())
+		.setParameter("endYearMonth", item.end().v())
+		.setParameter("hisId", item.identifier()).setParameter("cid", cid).executeUpdate();	
+	}
 
 }
