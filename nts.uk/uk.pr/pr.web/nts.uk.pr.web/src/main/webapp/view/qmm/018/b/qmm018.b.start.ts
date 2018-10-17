d5df5e0c9913edf9c418986be4d32c08052448ca
Anh Trung <trung.bv@3si.vn>
2018-10-17 10:11:46 +0700

    Fix lỗi giao diện

--- nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/018/b/qmm018.b.start.ts
+++ nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/018/b/qmm018.b.start.ts
@@ -5,6 +5,23 @@ module nts.uk.pr.view.qmm018.b {
         screenModel.startPage().done(function() {
             __viewContext.bind(screenModel);
 
+            if (/Edge/.test(navigator.userAgent)) {
+                $("#swap-list-grid1_displayContainer").addClass('edgeList');
+                $("#swap-list-grid2_displayContainer").addClass('edgeList');
+                $("#swap-list-grid1_scrollContainer").addClass('edgeList');
+                $("#swap-list-grid2_scrollContainer").addClass('edgeList');
+            } else if (/Chrome/.test(navigator.userAgent)) {
+                $("#swap-list-grid1_displayContainer").addClass('chromeList');
+                $("#swap-list-grid2_displayContainer").addClass('chromeList');
+                $("#swap-list-grid1_scrollContainer").addClass('chromeList');
+                $("#swap-list-grid2_scrollContainer").addClass('chromeList');
+            } else {
+                $("#swap-list-grid1_displayContainer").addClass('ieList');
+                $("#swap-list-grid2_displayContainer").addClass('ieList');
+                $("#swap-list-grid1_scrollContainer").addClass('ieList');
+                $("#swap-list-grid2_scrollContainer").addClass('ieList');
+            }
+
             $("#swap-list-grid1_container").focus();
         });
     });
