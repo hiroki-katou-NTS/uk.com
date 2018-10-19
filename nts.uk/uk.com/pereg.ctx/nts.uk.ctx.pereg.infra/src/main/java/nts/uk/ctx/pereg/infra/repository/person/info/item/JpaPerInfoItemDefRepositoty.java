9131f238fd6fff2215c8b35ff3934485ca075a7c
NWS-ThanhNC <thanhnc@newwave.vn>
2018-10-19 16:20:56 +0700

    Fix SORT_BY_DISPORDER

--- nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.infra/src/main/java/nts/uk/ctx/pereg/infra/repository/person/info/item/JpaPerInfoItemDefRepositoty.java
+++ nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.infra/src/main/java/nts/uk/ctx/pereg/infra/repository/person/info/item/JpaPerInfoItemDefRepositoty.java
@@ -1140,7 +1140,9 @@ public class JpaPerInfoItemDefRepositoty extends JpaRepository implements PerInf
 			lstObj.addAll(this.queryProxy().query(SELECT_ALL_REQUIREDITEM_BY_LIST_CATEGORY_ID, Object[].class)
 				.setParameter("contractCd", contractCd).setParameter("lstPerInfoCategoryId", subList).getList());
 		});
-		lstObj.sort(SORT_BY_DISPORDER);
+		lstObj.sort( (o1, o2) -> {
+			return ((int) o1[32]) - ((int) o2[32]); // index 32 for [disporder] 
+		});
 		
 		// groupBy categoryId 
 		Map<String, List<Object[]>> perInfoItemDefByList = lstObj.stream().collect(Collectors.groupingBy(x -> String.valueOf(x[31])));
