b889ce445cad0b86f3067dcf58b7e24e649a57e8
ThuyetND_3S <thuyet.nd@3si.vn>
2018-10-22 17:55:22 +0700

    add source view screen A

---
+++ nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/035/a/qmm035.a.service.ts
@@ -0,0 +1,27 @@
+module nts.uk.pr.view.qmm035.a.service {
+    import ajax = nts.uk.request.ajax;
+    import format = nts.uk.text.format;
+    var paths = {
+        defaultData: "ctx/pr/core/socialinsurance/socialinsuranceoffice/start",
+        findByCode: "ctx/pr/core/socialinsurance/socialinsuranceoffice/findByCode/{0}",
+        update: "ctx/pr/core/socialinsurance/socialinsuranceoffice/update",
+        create: "ctx/pr/core/socialinsurance/socialinsuranceoffice/create",
+        deleteOffice: "ctx/pr/core/socialinsurance/socialinsuranceoffice/remove"
+    }
+    export function defaultData(): JQueryPromise<any> {
+        return ajax("pr",paths.defaultData);
+    }
+    export function findByCode(code : string) : JQueryPromise<any>{
+        let _path = format(paths.findByCode, code);
+        return ajax('pr', _path);
+    }
+    export function update(command) : JQueryPromise<any>{
+        return ajax('pr', paths.update, command);
+    }
+    export function create(command) : JQueryPromise<any>{
+        return ajax('pr', paths.create, command);
+    }
+    export function deleteOffice(command: any){
+        return ajax('pr', paths.deleteOffice, command);
+    }
+}
