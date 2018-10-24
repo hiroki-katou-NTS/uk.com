823168bc629e90cc4d9a902bd0ff3dc35620f4eb
HopNT <hop.nt@3si.vn>
2018-10-24 16:25:49 +0700

    Fix ZeroPaddedCode

--- nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.dom/src/main/java/nts/uk/ctx/core/dom/socialinsurance/socialinsuranceoffice/PrefectureCode.java
+++ nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.dom/src/main/java/nts/uk/ctx/core/dom/socialinsurance/socialinsuranceoffice/PrefectureCode.java
@@ -1,9 +1,9 @@
 package nts.uk.ctx.core.dom.socialinsurance.socialinsuranceoffice;
 
-import nts.arc.primitive.StringPrimitiveValue;
 import nts.arc.primitive.constraint.CharType;
 import nts.arc.primitive.constraint.StringCharType;
 import nts.arc.primitive.constraint.StringMaxLength;
+import nts.uk.shr.com.primitive.CodePrimitiveValue;
 import nts.uk.shr.com.primitive.ZeroPaddedCode;
 
 /**
@@ -12,7 +12,7 @@ import nts.uk.shr.com.primitive.ZeroPaddedCode;
 @StringMaxLength(2)
 @StringCharType(CharType.NUMERIC)
 @ZeroPaddedCode
-public class PrefectureCode extends StringPrimitiveValue<PrefectureCode> {
+public class PrefectureCode extends CodePrimitiveValue<PrefectureCode> {
     public PrefectureCode(String rawValue) {
         super(rawValue);
     }
