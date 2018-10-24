5caca80d1bddd0aa5508b69aee905fe20fbdfac6
nws-dientx <dientx@newwave.vn>
2018-10-24 13:29:46 +0700

    fix bug #102081

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cdl/002/a/cdl002.a.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cdl/002/a/cdl002.a.vm.ts
@@ -56,6 +56,13 @@ module nts.uk.com.view.cdl002.a {
                     isSelectAllAfterReload: false,
                     tabindex: 1
                 };
+                
+                if (params.selectedCodes.length > 0) {
+                    self.getClosureByEmployment(params.selectedCodes[0]).done((data: any) => {
+                        self.listComponentOption.selectedClosureId(data);
+                    });
+                }
+                
                 if (self.isMultiSelect()) {
                     self.listComponentOption.selectedCode = self.selectedMulEmployment;
                 }
@@ -70,6 +77,15 @@ module nts.uk.com.view.cdl002.a {
             closeDialog(): void {
                 setShared('CDL002Cancel', true);
                 nts.uk.ui.windows.close();
+            }
+            
+            getClosureByEmployment(employmentId: string): JQueryPromise<any> {
+                let dfd = $.Deferred<any>();
+
+                nts.uk.request.ajax('at', "ctx/at/shared/workrule/closure/getclosuretiedbyemployment/" + employmentId).done((closureId: any) => {
+                    dfd.resolve(closureId);
+                });
+                    return dfd.promise();
             }
 
             /**
