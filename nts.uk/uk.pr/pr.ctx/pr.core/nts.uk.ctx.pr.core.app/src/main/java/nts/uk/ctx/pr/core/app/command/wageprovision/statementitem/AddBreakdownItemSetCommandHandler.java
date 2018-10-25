2f206501b1a2b92e94c092926b7513074f7d8af5
Anh Trung <trung.bv@3si.vn>
2018-10-25 15:00:55 +0700

    Sửa zero padding màn I

--- nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.app/src/main/java/nts/uk/ctx/pr/core/app/command/wageprovision/statementitem/AddBreakdownItemSetCommandHandler.java
+++ nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.app/src/main/java/nts/uk/ctx/pr/core/app/command/wageprovision/statementitem/AddBreakdownItemSetCommandHandler.java
@@ -7,6 +7,7 @@ import javax.transaction.Transactional;
 import nts.arc.error.BusinessException;
 import nts.arc.layer.app.command.CommandHandler;
 import nts.arc.layer.app.command.CommandHandlerContext;
+import nts.uk.ctx.pr.core.dom.wageprovision.statementitem.breakdownitemset.BreakdownItemCode;
 import nts.uk.ctx.pr.core.dom.wageprovision.statementitem.breakdownitemset.BreakdownItemSet;
 import nts.uk.ctx.pr.core.dom.wageprovision.statementitem.breakdownitemset.BreakdownItemSetRepository;
 import nts.uk.shr.com.context.AppContexts;
@@ -21,12 +22,13 @@ public class AddBreakdownItemSetCommandHandler extends CommandHandler<BreakdownI
 	@Override
 	protected void handle(CommandHandlerContext<BreakdownItemSetCommand> context) {
 		String cid = AppContexts.user().companyId();
-
 		BreakdownItemSetCommand command = context.getCommand();
+		BreakdownItemCode code = new BreakdownItemCode(command.getBreakdownItemCode());
+
 		BreakdownItemSet breakdownItemSet = new BreakdownItemSet(cid, command.getCategoryAtr(), command.getItemNameCd(),
 				command.getBreakdownItemCode(), command.getBreakdownItemName());
         if(breakdownItemSetRepository.getBreakdownItemStById(
-        		cid, command.getCategoryAtr(), command.getItemNameCd(), command.getBreakdownItemCode()).isPresent()){
+        		cid, command.getCategoryAtr(), command.getItemNameCd(), code.v()).isPresent()){
         	throw new BusinessException("Msg_3");
         }
 		breakdownItemSetRepository.add(breakdownItemSet);
