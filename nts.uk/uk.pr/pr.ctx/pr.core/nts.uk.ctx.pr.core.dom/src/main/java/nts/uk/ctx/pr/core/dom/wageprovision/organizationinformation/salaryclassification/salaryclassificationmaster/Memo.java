f27baff452240e00c5e80fe9cca45761ac65fd02
hadhv-3si <viethadh@gmail.com>
2018-10-30 17:29:42 +0700

    CMM015 first commit

---
+++ nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.dom/src/main/java/nts/uk/ctx/pr/core/dom/wageprovision/organizationinformation/salaryclassification/salaryclassificationmaster/Memo.java
@@ -0,0 +1,19 @@
+package nts.uk.ctx.pr.core.dom.wageprovision.organizationinformation.salaryclassification.salaryclassificationmaster;
+
+import nts.arc.primitive.StringPrimitiveValue;
+import nts.arc.primitive.constraint.CharType;
+import nts.arc.primitive.constraint.StringCharType;
+import nts.arc.primitive.constraint.StringMaxLength;
+
+/**
+ * メモ
+ */
+
+@StringMaxLength(500)
+@StringCharType(CharType.ANY_HALF_WIDTH)
+public class Memo extends StringPrimitiveValue<Memo> {
+
+    public Memo(String rawValue) {
+        super(rawValue);
+    }
+}
---
+++ nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.dom/src/main/java/nts/uk/ctx/pr/core/dom/wageprovision/organizationinformation/salaryclassification/salaryclassificationmaster/SalaryClassificationName.java
@@ -0,0 +1,24 @@
+package nts.uk.ctx.pr.core.dom.wageprovision.organizationinformation.salaryclassification.salaryclassificationmaster;
+
+import nts.arc.primitive.StringPrimitiveValue;
+import nts.arc.primitive.constraint.CharType;
+import nts.arc.primitive.constraint.StringCharType;
+import nts.arc.primitive.constraint.StringMaxLength;
+
+/**
+* 給与分類名称
+*/
+
+@StringMaxLength(10)
+@StringCharType(CharType.ANY_HALF_WIDTH)
+public class SalaryClassificationName extends StringPrimitiveValue<SalaryClassificationName>
+{
+    
+    private static final long serialVersionUID = 1L;
+    
+    public SalaryClassificationName(String rawValue)
+    {
+         super(rawValue);
+    }
+    
+}
