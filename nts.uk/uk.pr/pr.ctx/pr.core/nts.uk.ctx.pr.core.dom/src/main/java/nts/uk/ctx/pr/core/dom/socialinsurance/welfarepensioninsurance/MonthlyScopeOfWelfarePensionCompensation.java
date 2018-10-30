c06d29c96870e074ffd5d9f5c1b32078e40c2e07
3SI\sang.nv <sang.nv@3si.vn>
2018-10-30 10:48:22 +0700

    Update code domain 厚生年金標準月額, 厚生年金報酬月額範囲

--- nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.dom/src/main/java/nts/uk/ctx/pr/core/dom/socialinsurance/welfarepensioninsurance/MonthlyScopeOfWelfarePensionCompensation.java
+++ nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.dom/src/main/java/nts/uk/ctx/pr/core/dom/socialinsurance/welfarepensioninsurance/MonthlyScopeOfWelfarePensionCompensation.java
@@ -3,6 +3,7 @@ package nts.uk.ctx.pr.core.dom.socialinsurance.welfarepensioninsurance;
 import lombok.Getter;
 import nts.arc.layer.dom.AggregateRoot;
 import nts.arc.time.YearMonth;
+import nts.uk.shr.com.history.YearMonthHistoryItem;
 import nts.uk.shr.com.time.calendar.period.YearMonthPeriod;
 
 import java.util.List;
@@ -16,7 +17,7 @@ public class MonthlyScopeOfWelfarePensionCompensation extends AggregateRoot {
     /**
      * 対象期間
      */
-    private YearMonthPeriod targetPeriod;
+    private YearMonthHistoryItem targetPeriod;
 
     /**
      * 等級毎報酬月額範囲
@@ -26,12 +27,13 @@ public class MonthlyScopeOfWelfarePensionCompensation extends AggregateRoot {
     /**
      * 厚生年金報酬月額範囲
      *
+     * @param historyId                                履歴ID
      * @param targetStartYm                            対象期間
      * @param targetEndYm                              対象期間
      * @param welfarePensionGradePerRewardMonthlyRange 等級毎報酬月額範囲
      */
-    public MonthlyScopeOfWelfarePensionCompensation(int targetStartYm, int targetEndYm, List<WelfarePensionGradePerRewardMonthlyRange> welfarePensionGradePerRewardMonthlyRange) {
-        this.targetPeriod = new YearMonthPeriod(new YearMonth(targetStartYm), new YearMonth(targetEndYm));
+    public MonthlyScopeOfWelfarePensionCompensation(String historyId, int targetStartYm, int targetEndYm, List<WelfarePensionGradePerRewardMonthlyRange> welfarePensionGradePerRewardMonthlyRange) {
+        this.targetPeriod                             = new YearMonthHistoryItem(historyId, new YearMonthPeriod(new YearMonth(targetStartYm), new YearMonth(targetEndYm)));
         this.welfarePensionGradePerRewardMonthlyRange = welfarePensionGradePerRewardMonthlyRange;
     }
 }
