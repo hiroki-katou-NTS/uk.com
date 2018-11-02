278a7feb051208265b63f11ea0523cef3d52b61c
NTS\keisuke_hoshina <keisuke_hoshina@nittsusystem.co.jp>
2018-11-02 16:36:30 +0900

    fixed bug #102501、#102407

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/dailyprocess/calc/CalculateDailyRecordServiceImpl.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/dailyprocess/calc/CalculateDailyRecordServiceImpl.java
@@ -50,6 +50,7 @@ import nts.uk.ctx.at.record.dom.dailyperformanceprocessing.repository.ReflectBre
 import nts.uk.ctx.at.record.dom.dailyprocess.calc.converter.CalcDefaultValue;
 import nts.uk.ctx.at.record.dom.dailyprocess.calc.converter.DailyRecordToAttendanceItemConverter;
 import nts.uk.ctx.at.record.dom.dailyprocess.calc.errorcheck.CalculationErrorCheckService;
+import nts.uk.ctx.at.record.dom.dailyprocess.calc.errorcheck.DailyRecordCreateErrorAlermService;
 import nts.uk.ctx.at.record.dom.dailyprocess.calc.ootsuka.OotsukaProcessService;
 //import nts.uk.ctx.at.record.dom.dailyprocess.calc.ootsuka.OotsukaProcessService;
 import nts.uk.ctx.at.record.dom.divergence.time.DivergenceTime;
@@ -235,6 +236,10 @@ public class CalculateDailyRecordServiceImpl implements CalculateDailyRecordServ
 	@Inject
 	private PersonnelCostSettingAdapter personnelCostSettingAdapter;
 	
+	@Inject
+	//日別作成側にあったエラーチェック処理
+	private DailyRecordCreateErrorAlermService dailyRecordCreateErrorAlermService;
+	
 	/**
 	 * 勤務情報を取得して計算
 	 * @param calculateOption
@@ -310,7 +315,11 @@ public class CalculateDailyRecordServiceImpl implements CalculateDailyRecordServ
 	private ManageCalcStateAndResult calcDailyAttendancePerformance(IntegrationOfDaily integrationOfDaily,
 															  ManagePerCompanySet companyCommonSetting, 
 															  ManagePerPersonDailySet personCommonSetting, DailyRecordToAttendanceItemConverter converter, Optional<WorkInfoOfDailyPerformance> yesterDayInfo, Optional<WorkInfoOfDailyPerformance> tomorrowDayInfo) {
-		
+		//打刻順序不正のチェック
+		//不正の場合、勤務情報の計算ステータス→未計算にしつつ、エラーチェックは行う必要有）
+		if(!dailyRecordCreateErrorAlermService.stampIncorrectOrderAlgorithm(integrationOfDaily).isEmpty()) {
+			return ManageCalcStateAndResult.failCalc(integrationOfDaily);
+		}
 		val copyCalcAtr = integrationOfDaily.getCalAttr();
 		//予定の時間帯
 		val schedule = createSchedule(integrationOfDaily,companyCommonSetting,personCommonSetting,converter,yesterDayInfo,tomorrowDayInfo);
