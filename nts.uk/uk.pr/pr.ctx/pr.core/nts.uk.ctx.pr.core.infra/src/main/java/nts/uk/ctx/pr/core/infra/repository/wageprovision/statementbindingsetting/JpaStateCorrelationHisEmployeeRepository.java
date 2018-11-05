4397bebe9df1afce40a608920008a9a608691056
TaiTT <tai.tt@3si.vn>
2018-11-05 18:08:47 +0700

    qmm020 SCREEN B SERVER

--- nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.infra/src/main/java/nts/uk/ctx/pr/core/infra/repository/wageprovision/statementbindingsetting/JpaStateCorrelationHisEmployeeRepository.java
+++ nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.infra/src/main/java/nts/uk/ctx/pr/core/infra/repository/wageprovision/statementbindingsetting/JpaStateCorrelationHisEmployeeRepository.java
@@ -22,7 +22,7 @@ public class JpaStateCorrelationHisEmployeeRepository extends JpaRepository impl
 
     private static final String SELECT_ALL_QUERY_STRING = "SELECT f FROM QpbmtStateCorHisEmp f";
     private static final String SELECT_BY_KEY_STRING = SELECT_ALL_QUERY_STRING + " WHERE  f.stateCorHisEmpPk.cid =:cid AND  f.stateCorHisEmpPk.hisId =:hisId ";
-    private static final String SELECT_BY_CID = SELECT_ALL_QUERY_STRING + " WHERE  f.stateCorHisEmpPk.cid =:cid ";
+    private static final String SELECT_BY_CID = SELECT_ALL_QUERY_STRING + " WHERE  f.stateCorHisEmpPk.cid =:cid ORDER BY f.endYearMonth DESC";
 
     @Override
     public Optional<StateCorrelationHisEmployee> getStateCorrelationHisEmployeeById(String cid, String hisId){
