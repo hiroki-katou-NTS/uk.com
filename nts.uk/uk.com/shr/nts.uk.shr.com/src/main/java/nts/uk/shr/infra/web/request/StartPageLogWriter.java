9a376de8ea7dc4690b68a92f49908c4f90f0d79c
NTS\tin_dh <tin_dh@nittsusystem.co.jp>
2018-11-07 16:58:51 +0900

    update for startpage log (provide for custom log)

--- nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/infra/web/request/StartPageLogWriter.java
+++ nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/infra/web/request/StartPageLogWriter.java
@@ -1,8 +1,6 @@
 package nts.uk.shr.infra.web.request;
 
 import java.io.IOException;
-import java.util.Optional;
-import java.util.function.Function;
 import java.util.stream.Stream;
 
 import javax.enterprise.inject.spi.CDI;
@@ -14,24 +12,11 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
-import nts.arc.time.GeneralDateTime;
-import nts.gul.text.IdentifierUtil;
 import nts.gul.text.StringUtil;
-import nts.uk.shr.com.context.AppContexts;
-import nts.uk.shr.com.context.LoginUserContext;
-import nts.uk.shr.com.context.RequestInfo;
 import nts.uk.shr.com.context.ScreenIdentifier;
-import nts.uk.shr.com.context.loginuser.role.DefaultLoginUserRoles;
-import nts.uk.shr.com.menu.ShareStandardMenuAdapter;
-import nts.uk.shr.com.security.audittrail.UserInfoAdaptorForLog;
-import nts.uk.shr.com.security.audittrail.basic.LogBasicInformation;
-import nts.uk.shr.com.security.audittrail.basic.LoginInformation;
-import nts.uk.shr.com.security.audittrail.correction.content.UserInfo;
-import nts.uk.shr.com.security.audittrail.start.StartPageLog;
-import nts.uk.shr.com.security.audittrail.start.StartPageLogStorageRepository;
-import nts.uk.shr.infra.application.auth.WindowsAccount;
+import nts.uk.shr.com.program.ProgramsManager;
 import nts.uk.shr.infra.web.util.FilterConst;
-import nts.uk.shr.infra.web.util.FilterHelper;
+import nts.uk.shr.infra.web.util.StartPageLogService;
 
 public class StartPageLogWriter implements Filter {
 
@@ -42,106 +27,50 @@ public class StartPageLogWriter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		
+
 		writeLog(request);
-		
+
 		chain.doFilter(request, response);
-	}
-
-	private void writeLog(ServletRequest request) {
-		HttpServletRequest httpRequest = (HttpServletRequest) request;
-		LoginUserContext context = AppContexts.user();
-		RequestInfo requseted = AppContexts.requestedWebApi();
-		WindowsAccount windowsAccount = AppContexts.windowsAccount();
-		String requestPagePath = httpRequest.getRequestURL().toString();
-		
-		boolean isStartFromMenu = isStartFromMenu(httpRequest);
-		
-		ScreenIdentifier targetPg = ScreenIdentifier.create(requestPagePath, httpRequest.getQueryString());
-		
-		if(StringUtil.isNullOrEmpty(targetPg.getProgramId(), true) || FilterHelper.isLoginPage(requestPagePath)){
-			return;
-		}
-		
-		ShareStandardMenuAdapter menuAdapter = CDI.current().select(ShareStandardMenuAdapter.class).get();
-		String comId = getValue(context, c -> c.companyId());
-		if(!menuAdapter.isEsistMenuWith(comId, targetPg.getScreenId(), targetPg.getProgramId(), targetPg.getQueryString())){
-			return;
-		}
-		
-		LogBasicInformation basic = new LogBasicInformation(
-				IdentifierUtil.randomUniqueId(), 
-				comId,
-				UserInfo.employee(
-						getValue(context, c -> c.userId()), 
-						getValue(context, c -> {
-							if(c.employeeId() == null){
-								return c.userId();
-							}
-							return c.employeeId();
-						}),
-						getValue(context, c -> {
-							UserInfoAdaptorForLog userAdapter = CDI.current().select(UserInfoAdaptorForLog.class).get();
-							if(context.isEmployee()){
-								return userAdapter.findByEmployeeId(c.employeeId()).getUserName();
-							}
-							return userAdapter.findByUserId(c.userId()).getUserName();
-						})), 
-				new LoginInformation(
-						getValue(requseted, c -> c.getRequestIpAddress()),
-						getValue(requseted, c -> c.getRequestPcName()), 
-						getValue(windowsAccount, c -> c.getUserName())),
-				GeneralDateTime.now(), 
-				getValue(context, c -> {
-					return getValue(c.roles(), role -> DefaultLoginUserRoles.cloneFrom(role));
-				}), targetPg, Optional.empty());
-		
-		saveLog(initLog(httpRequest, basic, isStartFromMenu));
-	}
-	
-	private boolean isStartFromMenu(HttpServletRequest httpRequest){
-		if(httpRequest.getCookies() == null){
-			return false;
-		}
-		return Stream.of(httpRequest.getCookies()).filter(c -> c.getName().equals(FilterConst.JUMP_FROM_MENU)).findFirst().isPresent();
-	}
-
-	private StartPageLog initLog(HttpServletRequest httpRequest, LogBasicInformation basic, boolean requestedFromMenu) {
-		
-		if(requestedFromMenu){
-			return StartPageLog.specialStarted(basic);
-		}
-		
-		return StartPageLog.pageStarted(getReferered(httpRequest), basic);
 	}
 
 	@Override
 	public void destroy() {
 	}
 
-	private void saveLog(StartPageLog log) {
-		StartPageLogStorageRepository logStorage = CDI.current().select(StartPageLogStorageRepository.class).get();
+	public void writeLog(ServletRequest request) {
+
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		String requestPagePath = httpRequest.getRequestURL().toString();
+
+		boolean isStartFromMenu = isStartFromMenu(httpRequest);
 		
-		logStorage.save(log);
+		ScreenIdentifier target = ScreenIdentifier.create(requestPagePath);
+		
+		if(ProgramsManager.KAF000B.getPId().equals(target.getProgramId() + target.getScreenId())){
+			return;
+		}
+
+		StartPageLogService logService = CDI.current().select(StartPageLogService.class).get();
+
+		logService.writeLog(isStartFromMenu ? null : getReferered(httpRequest), target);
 	}
 
-	private ScreenIdentifier getReferered(HttpServletRequest r) {
+	private boolean isStartFromMenu(HttpServletRequest httpRequest) {
+		if (httpRequest.getCookies() == null) {
+			return false;
+		}
+		return Stream.of(httpRequest.getCookies()).filter(c -> c.getName().equals(FilterConst.JUMP_FROM_MENU))
+				.findFirst().isPresent();
+	}
+
+	private String getReferered(HttpServletRequest r) {
 		String refereredPath = r.getHeader(FilterConst.REFERED_REQUEST);
-		
-		if(StringUtil.isNullOrEmpty(refereredPath, true)){
+
+		if (StringUtil.isNullOrEmpty(refereredPath, true)) {
 			return null;
 		}
-		
-		return ScreenIdentifier.create(refereredPath);
-	}
-	
-	private <U, T> T getValue(U source, Function<U, T> getter){
-		if(source != null){
-			return getter.apply(source);
-		}
-		
-		return null;
+
+		return refereredPath;
 	}
 
 }
-
