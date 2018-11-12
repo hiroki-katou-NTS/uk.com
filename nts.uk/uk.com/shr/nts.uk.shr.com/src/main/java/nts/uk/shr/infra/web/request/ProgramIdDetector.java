a124310880a383793e3be600dcaa9dcd5cb373a0
NTS\tin_dh <tin_dh@nittsusystem.co.jp>
2018-11-12 15:24:57 +0900

    Revert "Revert "update for write start page log""
    This reverts commit f3468510c73d9236e58580ba518d2d06e8f671de.

--- nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/infra/web/request/ProgramIdDetector.java
+++ nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/infra/web/request/ProgramIdDetector.java
@@ -1,6 +1,7 @@
 package nts.uk.shr.infra.web.request;
 
 import java.io.IOException;
+import java.util.stream.Stream;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.jboss.resteasy.plugins.providers.multipart.InputPart;
 
+import nts.gul.text.StringUtil;
 import nts.uk.shr.com.context.AppContextsConfig;
 import nts.uk.shr.com.context.RequestInfo;
 import nts.uk.shr.infra.web.util.FilterConst;
@@ -41,24 +43,46 @@ public class ProgramIdDetector implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		request.setAttribute(InputPart.DEFAULT_CHARSET_PROPERTY, FilterConst.DEFAULT_CHARSET_PROPERTY);
-		String requestPagePath = ((HttpServletRequest) request).getHeader(FilterConst.PG_PATH);
-		if (requestPagePath == null) {
-			chain.doFilter(request, response);
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		httpRequest.setAttribute(InputPart.DEFAULT_CHARSET_PROPERTY, FilterConst.DEFAULT_CHARSET_PROPERTY);
+		String target = httpRequest.getHeader(FilterConst.PG_PATH);
+		if (target == null) {
+			chain.doFilter(httpRequest, response);
 			return;
 		}
-		
-		String ip = FilterHelper.getClientIp((HttpServletRequest) request);
-		String pcName = FilterHelper.getPcName(ip);
-		
-		AppContextsConfig.setRequestedWebAPI(new RequestInfo(
-													requestPagePath, 
-													FilterHelper.detectWebapi(requestPagePath), 
-													ip, pcName));
-		
-		FilterHelper.detectProgram(requestPagePath).ifPresent(id -> AppContextsConfig.setProgramId(id));
 
-		chain.doFilter(request, response);
+		String ip = FilterHelper.getClientIp(httpRequest);
+		String pcName = FilterHelper.getPcName(ip);
+
+		AppContextsConfig.setRequestedWebAPI(new RequestInfo(target, FilterHelper.detectWebapi(target), ip, pcName));
+		boolean isStartFromMenu = isStartFromMenu(httpRequest);
+		if (!isStartFromMenu) {
+			String before = getReferered(httpRequest);
+			String webApi = FilterHelper.detectWebapi(before);
+
+			AppContextsConfig.setRequestedWebAPI(new RequestInfo(before, webApi, ip, pcName));
+		}
+		FilterHelper.detectProgram(target).ifPresent(id -> AppContextsConfig.setProgramId(id));
+
+		chain.doFilter(httpRequest, response);
+	}
+
+	private boolean isStartFromMenu(HttpServletRequest httpRequest) {
+		if (httpRequest.getCookies() == null) {
+			return false;
+		}
+		return Stream.of(httpRequest.getCookies()).filter(c -> c.getName().equals(FilterConst.JUMP_FROM_MENU))
+				.findFirst().isPresent();
+	}
+
+	private String getReferered(HttpServletRequest r) {
+		String refereredPath = r.getHeader(FilterConst.REFERED_REQUEST);
+
+		if (StringUtil.isNullOrEmpty(refereredPath, true)) {
+			return null;
+		}
+
+		return refereredPath;
 	}
 
 	/*
--- nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/infra/web/request/StartPageLogWriter.java
+++ nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/infra/web/request/StartPageLogWriter.java
@@ -1,7 +1,6 @@
 package nts.uk.shr.infra.web.request;
 
 import java.io.IOException;
-import java.util.stream.Stream;
 
 import javax.enterprise.inject.spi.CDI;
 import javax.servlet.Filter;
@@ -12,10 +11,8 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
-import nts.gul.text.StringUtil;
 import nts.uk.shr.com.context.ScreenIdentifier;
 import nts.uk.shr.com.program.ProgramsManager;
-import nts.uk.shr.infra.web.util.FilterConst;
 import nts.uk.shr.infra.web.util.StartPageLogService;
 
 public class StartPageLogWriter implements Filter {
@@ -42,35 +39,15 @@ public class StartPageLogWriter implements Filter {
 		HttpServletRequest httpRequest = (HttpServletRequest) request;
 		String requestPagePath = httpRequest.getRequestURL().toString();
 
-		boolean isStartFromMenu = isStartFromMenu(httpRequest);
-		
 		ScreenIdentifier target = ScreenIdentifier.create(requestPagePath);
-		
-		if(ProgramsManager.KAF000B.getPId().equals(target.getProgramId() + target.getScreenId())){
+
+		if (ProgramsManager.KAF000B.getPId().equals(target.getProgramId() + target.getScreenId())) {
 			return;
 		}
 
 		StartPageLogService logService = CDI.current().select(StartPageLogService.class).get();
 
-		logService.writeLog(isStartFromMenu ? null : getReferered(httpRequest), target);
-	}
-
-	private boolean isStartFromMenu(HttpServletRequest httpRequest) {
-		if (httpRequest.getCookies() == null) {
-			return false;
-		}
-		return Stream.of(httpRequest.getCookies()).filter(c -> c.getName().equals(FilterConst.JUMP_FROM_MENU))
-				.findFirst().isPresent();
-	}
-
-	private String getReferered(HttpServletRequest r) {
-		String refereredPath = r.getHeader(FilterConst.REFERED_REQUEST);
-
-		if (StringUtil.isNullOrEmpty(refereredPath, true)) {
-			return null;
-		}
-
-		return refereredPath;
+		logService.writeLog(target);
 	}
 
 }
