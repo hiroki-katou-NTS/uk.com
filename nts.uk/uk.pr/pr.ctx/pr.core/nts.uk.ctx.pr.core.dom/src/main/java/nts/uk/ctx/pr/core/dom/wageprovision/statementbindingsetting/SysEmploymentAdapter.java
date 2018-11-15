cd94506a4f35733f640748f452bfa3b91bdaa8e1
TaiTT <tai.tt@3si.vn>
2018-11-15 09:38:31 +0700

    QMM020 Revert code screen C

--- nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.dom/src/main/java/nts/uk/ctx/pr/core/dom/wageprovision/statementbindingsetting/SysEmploymentAdapter.java
+++
@@ -1,6 +0,0 @@
-package nts.uk.ctx.pr.core.dom.wageprovision.statementbindingsetting;
-import java.util.List;
-
-public interface SysEmploymentAdapter {
-    List<EmpCdNameImport> findAll(String companyId);
-}
