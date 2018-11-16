d321f7efdb42db2d31701b4e4d88230419caccf0
ThuyetND_3S <thuyetdinh9xhn@gmail.com>
2018-11-16 18:14:38 +0700

    Fix bug

--- nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/038/a/qmm038.a.vm.ts
+++ nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/038/a/qmm038.a.vm.ts
@@ -10,14 +10,14 @@ module nts.uk.pr.view.qmm038.a {
             baseDate: KnockoutObservable<any> = ko.observable(moment().format("YYYY/MM/DD"));
             giveCurrTreatYear: KnockoutObservable<any> = ko.observable(moment().format("YYYY/MM"));
             employeeIds: Array<any>;
-            numberValidator = new validation.NumberValidator(getText("QMM038_11"), "AverageWage", { required: true });
+            numberValidator = new validation.NumberValidator(getText("QMM038_11"), "AverageWage", {required: true});
             dataUpdate: Array<UpdateEmployee> = [];
 
             constructor() {
                 let self = this;
-                nts.uk.pr.view.qmm038.a.service.defaultData().done(function(response) {
-                    if(response[0] != null) {
-                        self.giveCurrTreatYear(response[0].substr(0,4) + "/" + response[0].substr(4));
+                nts.uk.pr.view.qmm038.a.service.defaultData().done(function (response) {
+                    if (response[0] != null) {
+                        self.giveCurrTreatYear(response[0].substr(0, 4) + "/" + response[0].substr(4));
                         self.baseDate(response[1]);
                         self.ccg001ComponentOption.baseDate = self.baseDate();
                     }
@@ -66,20 +66,25 @@ module nts.uk.pr.view.qmm038.a {
                      * Self-defined function: Return data from CCG001
                      * @param: data: the data return from CCG001
                      */
-                    returnDataFromCcg001: function(data: Ccg001ReturnedData) {
+                    returnDataFromCcg001: function (data: Ccg001ReturnedData) {
                         block.invisible();
                         nts.uk.ui.errors.clearAll();
                         self.employeeIds = data.listEmployee.map(item => item.employeeId);
-                        let command = { employeeIds: self.employeeIds, baseDate: self.baseDate(),giveCurrTreatYear: moment(self.giveCurrTreatYear()).format("YYYY/MM")};
-                        nts.uk.pr.view.qmm038.a.service.findByEmployee(command).done(function(response) {
+                        let command = {
+                            employeeIds: self.employeeIds,
+                            baseDate: self.baseDate(),
+                            giveCurrTreatYear: moment(self.giveCurrTreatYear()).format("YYYY/MM")
+                        };
+                        nts.uk.pr.view.qmm038.a.service.findByEmployee(command).done(function (response) {
                             self.statementItems = [];
                             self.statementItems = _.sortBy(response, ["employeeCode"]);
                             $("#gridStatement").ntsGrid("destroy");
-                            if(self.statementItems.length == 1) {
+                            if (self.statementItems.length == 1) {
                                 self.loadGridLimit();
                             } else {
                                 self.loadGrid();
                             }
+                            $('#gridStatement .nts-editor').addClass('ntsEditorCus');
                             block.clear();
                         });
 
@@ -91,7 +96,7 @@ module nts.uk.pr.view.qmm038.a {
                 self.loadGrid();
             }
 
-            loadGridLimit(){
+            loadGridLimit() {
                 let self = this;
                 $("#gridStatement").ntsGrid({
                     width: '807px',
@@ -124,13 +129,17 @@ module nts.uk.pr.view.qmm038.a {
 
                     ],
                     ntsControls: [
-                        {name: 'TextEditor', controlType: 'TextEditor', constraint: {valueType: 'String', required: false}}
+                        {
+                            name: 'TextEditor',
+                            controlType: 'TextEditor',
+                            constraint: {valueType: 'Integer', required: false, format: "Number_Separated"},
+                        }
                     ]
                 });
             }
 
 
-            loadGrid(){
+            loadGrid() {
                 let self = this;
                 $("#gridStatement").ntsGrid({
                     width: '807px',
@@ -145,7 +154,11 @@ module nts.uk.pr.view.qmm038.a {
                         {headerText: getText("QMM038_9"), key: 'departmentName', dataType: 'string', width: '150px'},
                         {headerText: getText("QMM038_10"), key: 'employmentName', dataType: 'string', width: '150px'},
                         {
-                            headerText: getText("QMM038_11"), key: 'averageWage', dataType: 'string', width: '207px',option: {
+                            headerText: getText("QMM038_11"),
+                            key: 'averageWage',
+                            dataType: 'string',
+                            width: '207px',
+                            option: {
                                 grouplength: 3
                             },
                             ntsControl: 'TextEditor'
@@ -165,7 +178,11 @@ module nts.uk.pr.view.qmm038.a {
 
                     ],
                     ntsControls: [
-                        {name: 'TextEditor', controlType: 'TextEditor', constraint: {valueType: 'String', required: false}, }
+                        {
+                            name: 'TextEditor',
+                            controlType: 'TextEditor',
+                            constraint: {valueType: 'Integer', required: false, format: "Number_Separated"}
+                        },
                     ]
                 });
             }
@@ -173,12 +190,16 @@ module nts.uk.pr.view.qmm038.a {
             findByEmployee() {
                 let self = this;
                 nts.uk.ui.errors.clearAll();
-                let command = { employeeIds: self.employeeIds, baseDate: self.baseDate(),giveCurrTreatYear: moment(self.giveCurrTreatYear()).format("YYYY/MM") };
-                nts.uk.pr.view.qmm038.a.service.findByEmployee(command).done(function(response) {
+                let command = {
+                    employeeIds: self.employeeIds,
+                    baseDate: self.baseDate(),
+                    giveCurrTreatYear: moment(self.giveCurrTreatYear()).format("YYYY/MM")
+                };
+                nts.uk.pr.view.qmm038.a.service.findByEmployee(command).done(function (response) {
                     self.statementItems = [];
                     self.statementItems = _.sortBy(response, ["employeeCode"]);
                     $("#gridStatement").ntsGrid("destroy");
-                    if(self.statementItems.length == 1) {
+                    if (self.statementItems.length == 1) {
                         self.loadGridLimit();
                     } else {
                         self.loadGrid();
@@ -193,22 +214,22 @@ module nts.uk.pr.view.qmm038.a {
                 let statementItems: Array<IDataScreen> = $("#gridStatement").igGrid("option", "dataSource")
 
                 self.validateForm(statementItems);
-                if(nts.uk.ui.errors.hasError()) {
+                if (nts.uk.ui.errors.hasError()) {
                     return;
                 }
                 block.invisible();
                 // update
                 _.forEach(statementItems, (item: IDataScreen) => {
                     self.dataUpdate = [];
-                    self.dataUpdate.push(new UpdateEmployee(item.employeeId,item.averageWage));
+                    self.dataUpdate.push(new UpdateEmployee(item.employeeId, item.averageWage));
                 })
                 let command = {
-                    employeeDtoList : self.dataUpdate,
-                    giveCurrTreatYear : moment(self.giveCurrTreatYear()).format("YYYY/MM")
+                    employeeDtoList: self.dataUpdate,
+                    giveCurrTreatYear: moment(self.giveCurrTreatYear()).format("YYYY/MM")
                 }
-                service.update(command).done(function(response) {
-                    if(response[0] == "Msg_15") {
-                        nts.uk.ui.dialog.info({ messageId: "Msg_15" }).then(function() {
+                service.update(command).done(function (response) {
+                    if (response[0] == "Msg_15") {
+                        nts.uk.ui.dialog.info({messageId: "Msg_15"}).then(function () {
                             $("#A2_3").focus();
                         });
                     }
@@ -225,11 +246,11 @@ module nts.uk.pr.view.qmm038.a {
                     check: any;
                 nts.uk.ui.errors.clearAll();
                 _.each(statementItems, (item: IDataScreen) => {
-                    if(item.averageWage == null) {
+                    if (item.averageWage == null) {
                         item.averageWage = "";
                     }
                     check = self.numberValidator.validate(item.averageWage.toString());
-                    if(!check.isValid) {
+                    if (!check.isValid) {
                         self.setErrorAverageWage(item.employeeCode, check.errorCode, check.errorMessage)
                     }
                 })
@@ -246,71 +267,75 @@ module nts.uk.pr.view.qmm038.a {
 
 
     }
-        export interface IDataScreen {
-            employeeId: string; // 基準日
-            employeeCode: string; // 社員コード
-            businessName: string; // ビジネスネーム
-            departmentName: string; // 所属部門
-            employmentName: string; // 所属雇用
-            averageWage: string; // 所属雇用
+
+    export interface IDataScreen {
+        employeeId: string; // 基準日
+        employeeCode: string; // 社員コード
+        businessName: string; // ビジネスネーム
+        departmentName: string; // 所属部門
+        employmentName: string; // 所属雇用
+        averageWage: string; // 所属雇用
+    }
+
+    // Note: Defining these interfaces are optional
+    export interface GroupOption {
+        /** Common properties */
+        showEmployeeSelection?: boolean; // 検索タイプ
+        systemType: number; // システム区分
+        showQuickSearchTab?: boolean; // クイック検索
+        showAdvancedSearchTab?: boolean; // 詳細検索
+        showBaseDate?: boolean; // 基準日利用
+        showClosure?: boolean; // 就業締め日利用
+        showAllClosure?: boolean; // 全締め表示
+        showPeriod?: boolean; // 対象期間利用
+        periodFormatYM?: boolean; // 対象期間精度
+        isInDialog?: boolean;
+
+        /** Required parameter */
+        baseDate?: string; // 基準日
+        periodStartDate?: string; // 対象期間開始日
+        periodEndDate?: string; // 対象期間終了日
+        inService: boolean; // 在職区分
+        leaveOfAbsence: boolean; // 休職区分
+        closed: boolean; // 休業区分
+        retirement: boolean; // 退職区分
+
+        /** Quick search tab options */
+        showAllReferableEmployee?: boolean; // 参照可能な社員すべて
+        showOnlyMe?: boolean; // 自分だけ
+        showSameWorkplace?: boolean; // 同じ職場の社員
+        showSameWorkplaceAndChild?: boolean; // 同じ職場とその配下の社員
+
+        /** Advanced search properties */
+        showEmployment?: boolean; // 雇用条件
+        showWorkplace?: boolean; // 職場条件
+        showClassification?: boolean; // 分類条件
+        showJobTitle?: boolean; // 職位条件
+        showWorktype?: boolean; // 勤種条件
+        isMutipleCheck?: boolean; // 選択モード
+        isTab2Lazy?: boolean;
+
+        /** Data returned */
+        returnDataFromCcg001: (data: Ccg001ReturnedData) => void;
+    }
+
+    export class UpdateEmployee {
+        employeeId: string;
+        averageWage: string;
+
+        constructor(employeeId: string, averageWage: string) {
+            this.employeeId = employeeId;
+            this.averageWage = averageWage;
         }
+    }
 
-        // Note: Defining these interfaces are optional
-        export interface GroupOption {
-            /** Common properties */
-            showEmployeeSelection?: boolean; // 検索タイプ
-            systemType: number; // システム区分
-            showQuickSearchTab?: boolean; // クイック検索
-            showAdvancedSearchTab?: boolean; // 詳細検索
-            showBaseDate?: boolean; // 基準日利用
-            showClosure?: boolean; // 就業締め日利用
-            showAllClosure?: boolean; // 全締め表示
-            showPeriod?: boolean; // 対象期間利用
-            periodFormatYM?: boolean; // 対象期間精度
-            isInDialog?: boolean;
-
-            /** Required parameter */
-            baseDate?: string; // 基準日
-            periodStartDate?: string; // 対象期間開始日
-            periodEndDate?: string; // 対象期間終了日
-            inService: boolean; // 在職区分
-            leaveOfAbsence: boolean; // 休職区分
-            closed: boolean; // 休業区分
-            retirement: boolean; // 退職区分
-
-            /** Quick search tab options */
-            showAllReferableEmployee?: boolean; // 参照可能な社員すべて
-            showOnlyMe?: boolean; // 自分だけ
-            showSameWorkplace?: boolean; // 同じ職場の社員
-            showSameWorkplaceAndChild?: boolean; // 同じ職場とその配下の社員
-
-            /** Advanced search properties */
-            showEmployment?: boolean; // 雇用条件
-            showWorkplace?: boolean; // 職場条件
-            showClassification?: boolean; // 分類条件
-            showJobTitle?: boolean; // 職位条件
-            showWorktype?: boolean; // 勤種条件
-            isMutipleCheck?: boolean; // 選択モード
-            isTab2Lazy?: boolean;
-
-            /** Data returned */
-            returnDataFromCcg001: (data: Ccg001ReturnedData) => void;
-        }
-        export class UpdateEmployee {
-            employeeId: string;
-            averageWage: string;
-            constructor(employeeId: string, averageWage: string) {
-                this.employeeId = employeeId;
-                this.averageWage = averageWage;
-            }
-        }
-        export interface Ccg001ReturnedData {
-            employeeId: string; // 基準日
-            employeeCode: string; // 社員コード
-            businessName: string; // ビジネスネーム
-            departmentName: string; // 所属部門
-            employmentName: string; // 所属雇用
-            averageWage: string; // 所属雇用
-        }
+    export interface Ccg001ReturnedData {
+        employeeId: string; // 基準日
+        employeeCode: string; // 社員コード
+        businessName: string; // ビジネスネーム
+        departmentName: string; // 所属部門
+        employmentName: string; // 所属雇用
+        averageWage: string; // 所属雇用
+    }
 
 }
