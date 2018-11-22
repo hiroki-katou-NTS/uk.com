3914cc5e3a941b95ab8e4057aacfe83f56e6e04e
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2018-11-22 12:17:53 +0700

    add Add Repository StopSetting

---
+++ nts.uk/uk.com/com.ctx/sys/gateway/nts.uk.ctx.sys.gateway.infra/src/main/java/nts/uk/ctx/sys/gateway/infra/repository/stopbycompany/JpaStopByCompanyRepository.java
@@ -0,0 +1,55 @@
+package nts.uk.ctx.sys.gateway.infra.repository.stopbycompany;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.ejb.Stateless;
+
+import nts.arc.layer.infra.data.JpaRepository;
+import nts.uk.ctx.sys.gateway.dom.stopbycompany.StopByCompany;
+import nts.uk.ctx.sys.gateway.dom.stopbycompany.StopByCompanyRepository;
+import nts.uk.ctx.sys.gateway.infra.entity.stopbycompany.SgwdtStopByCompany;
+import nts.uk.ctx.sys.gateway.infra.entity.stopbycompany.SgwdtStopByCompanyPK;
+
+@Stateless
+public class JpaStopByCompanyRepository extends JpaRepository implements StopByCompanyRepository {
+
+	private static final String FIND_BY_KEY = "SELECT s FROM SgwdtStopByCompany s WHERE s.pk.contractCd=:contractCd AND s.pk.companyCd";
+
+	private static final String FIND_BY_CONTRACTCD_AND_STATE = "SELECT s FROM SgwdtStopByCompany s WHERE s.pk.contractCd=:contractCd AND s.systemStatus=:systemStatus";
+
+	@Override
+	public void insert(StopByCompany domain) {
+		this.commandProxy().insert(toEntity(domain));
+	}
+
+	private SgwdtStopByCompany toEntity(StopByCompany domain) {
+		SgwdtStopByCompanyPK pk = new SgwdtStopByCompanyPK(domain.getContractCd(), domain.getCompanyCd());
+		return new SgwdtStopByCompany(pk, domain.getUsageStopMode().value, domain.getUsageStopMessage(),
+				domain.getSystemStatus().value, domain.getStopMessage());
+	}
+
+	@Override
+	public void update(StopByCompany domain) {
+		this.commandProxy().update(toEntity(domain));
+	}
+
+	@Override
+	public Optional<StopByCompany> findByKey(String contractCd, String companyCd) {
+		return this.queryProxy().query(FIND_BY_KEY, SgwdtStopByCompany.class).setParameter("contractCd", contractCd)
+				.setParameter("companyCd", companyCd).getSingle(x -> toDomain(x));
+	}
+
+	private StopByCompany toDomain(SgwdtStopByCompany entity) {
+		return StopByCompany.createFromJavaType(entity.pk.contractCd, entity.pk.companyCd, entity.usageStopMode,
+				entity.usageStopMessage, entity.systemStatus, entity.stopMessage);
+	}
+
+	@Override
+	public List<StopByCompany> findByContractCodeAndState(String contractCd, int systemStatus) {
+		return this.queryProxy().query(FIND_BY_CONTRACTCD_AND_STATE, SgwdtStopByCompany.class)
+				.setParameter("contractCd", contractCd).setParameter("systemStatus", systemStatus)
+				.getList(x -> toDomain(x));
+	}
+
+}
