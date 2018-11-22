c3dfe15df89fcd84cab66a1b8f45f9ffa92fe707
Hung Tran <32724898+1capedbaldy@users.noreply.github.com>
2018-11-22 15:46:32 +0700

    qmm016: update code

--- nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.app/src/main/java/nts/uk/ctx/pr/core/app/command/wageprovision/wagetable/ElementAttributeCommand.java
+++ nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.app/src/main/java/nts/uk/ctx/pr/core/app/command/wageprovision/wagetable/ElementAttributeCommand.java
@@ -17,7 +17,7 @@ public class ElementAttributeCommand {
     /**
      * 固定の要素
      */
-    private String fixedElement;
+    private Integer fixedElement;
 
     /**
      * 任意追加の要素
--- nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.infra/src/main/java/nts/uk/ctx/pr/core/infra/repository/wageprovision/wagetable/JpaQualificationGroupSettingRepository.java
+++ nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.infra/src/main/java/nts/uk/ctx/pr/core/infra/repository/wageprovision/wagetable/JpaQualificationGroupSettingRepository.java
@@ -1,12 +1,5 @@
 package nts.uk.ctx.pr.core.infra.repository.wageprovision.wagetable;
 
-import nts.arc.layer.infra.data.JpaRepository;
-import nts.uk.ctx.pr.core.dom.wageprovision.wagetable.QualificationGroupSetting;
-import nts.uk.ctx.pr.core.dom.wageprovision.wagetable.QualificationGroupSettingRepository;
-import nts.uk.ctx.pr.core.infra.entity.wageprovision.wagetable.QpbmtQualificationGroupSetting;
-import nts.uk.shr.com.context.AppContexts;
-
-import javax.ejb.Stateless;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -15,41 +8,60 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+import javax.ejb.Stateless;
+
+import nts.arc.layer.infra.data.JpaRepository;
+import nts.uk.ctx.pr.core.dom.wageprovision.wagetable.QualificationGroupSetting;
+import nts.uk.ctx.pr.core.dom.wageprovision.wagetable.QualificationGroupSettingRepository;
+import nts.uk.ctx.pr.core.infra.entity.wageprovision.wagetable.QpbmtQualificationGroupSetting;
+import nts.uk.shr.com.context.AppContexts;
+
 @Stateless
-public class JpaQualificationGroupSettingRepository extends JpaRepository implements QualificationGroupSettingRepository {
+public class JpaQualificationGroupSettingRepository extends JpaRepository
+		implements QualificationGroupSettingRepository {
 
-    private static final String FIND_BY_COMPANY = "SELECT a FROM QpbmtQualificationGroupSetting a WHERE a.qualificationGroupSettingPk.cid =:cid ORDER BY a.qualificationGroupSettingPk.qualificationGroupCode";
-    private static final String FIND_BY_COMPANY_AND_CODE = "SELECT a FROM QpbmtQualificationGroupSetting a WHERE a.qualificationGroupSettingPk.cid =:cid AND a.qualificationGroupSettingPk.qualificationGroupCode =:qualificationGroupCode";
-    private static final String DELETE_BY_COMPANY_AND_CODE = "DELETE FROM QpbmtQualificationGroupSetting a WHERE a.qualificationGroupSettingPk.cid =:cid AND a.qualificationGroupSettingPk.qualificationGroupCode =:qualificationGroupCode";
+	private static final String FIND_BY_COMPANY = "SELECT a FROM QpbmtQualificationGroupSetting a WHERE a.qualificationGroupSettingPk.cid =:cid ORDER BY a.qualificationGroupSettingPk.qualificationGroupCode";
+	private static final String FIND_BY_COMPANY_AND_CODE = "SELECT a FROM QpbmtQualificationGroupSetting a WHERE a.qualificationGroupSettingPk.cid =:cid AND a.qualificationGroupSettingPk.qualificationGroupCode =:qualificationGroupCode";
+	private static final String DELETE_BY_COMPANY_AND_CODE = "DELETE FROM QpbmtQualificationGroupSetting a WHERE a.qualificationGroupSettingPk.cid =:cid AND a.qualificationGroupSettingPk.qualificationGroupCode =:qualificationGroupCode";
 
-    @Override
-    public List<QualificationGroupSetting> getQualificationGroupSettingByCompanyID() {
-        return QpbmtQualificationGroupSetting.toDomain(this.queryProxy().query(FIND_BY_COMPANY, QpbmtQualificationGroupSetting.class).setParameter("cid", AppContexts.user().companyId()).getList().stream().filter(distinctByKey(item -> item.qualificationGroupSettingPk.qualificationGroupCode)).collect(Collectors.toList()));
-    }
+	@Override
+	public List<QualificationGroupSetting> getQualificationGroupSettingByCompanyID() {
+		return QpbmtQualificationGroupSetting
+				.toDomain(this.queryProxy().query(FIND_BY_COMPANY, QpbmtQualificationGroupSetting.class)
+						.setParameter("cid", AppContexts.user().companyId()).getList().stream()
+						.filter(distinctByKey(item -> item.pk.qualificationGroupCode)).collect(Collectors.toList()));
+	}
 
-    @Override
-    public Optional<QualificationGroupSetting> getQualificationGroupSettingById(String qualificationGroupCode) {
-        return QpbmtQualificationGroupSetting.toDomainForGroup(this.queryProxy().query(FIND_BY_COMPANY_AND_CODE, QpbmtQualificationGroupSetting.class).setParameter("cid", AppContexts.user().companyId()).setParameter("qualificationGroupCode", qualificationGroupCode).getList());
-    }
+	@Override
+	public Optional<QualificationGroupSetting> getQualificationGroupSettingById(String qualificationGroupCode) {
+		return QpbmtQualificationGroupSetting.toDomainForGroup(
+				this.queryProxy().query(FIND_BY_COMPANY_AND_CODE, QpbmtQualificationGroupSetting.class)
+						.setParameter("cid", AppContexts.user().companyId())
+						.setParameter("qualificationGroupCode", qualificationGroupCode).getList());
+	}
 
-    @Override
-    public void add(QualificationGroupSetting domain) {
-        this.commandProxy().insertAll(QpbmtQualificationGroupSetting.toEntity(domain));
-    }
+	@Override
+	public void add(QualificationGroupSetting domain) {
+		this.commandProxy().insertAll(QpbmtQualificationGroupSetting.toEntity(domain));
+	}
 
-    @Override
-    public void update(QualificationGroupSetting domain) {
-        this.getEntityManager().createQuery(DELETE_BY_COMPANY_AND_CODE, QpbmtQualificationGroupSetting.class).setParameter("cid", AppContexts.user().companyId()).setParameter("qualificationGroupCode", domain.getQualificationGroupCode().v()).executeUpdate();
-        this.commandProxy().insertAll(QpbmtQualificationGroupSetting.toEntity(domain));
-    }
+	@Override
+	public void update(QualificationGroupSetting domain) {
+		this.getEntityManager().createQuery(DELETE_BY_COMPANY_AND_CODE, QpbmtQualificationGroupSetting.class)
+				.setParameter("cid", AppContexts.user().companyId())
+				.setParameter("qualificationGroupCode", domain.getQualificationGroupCode().v()).executeUpdate();
+		this.commandProxy().insertAll(QpbmtQualificationGroupSetting.toEntity(domain));
+	}
 
-    @Override
-    public void remove(QualificationGroupSetting domain) {
-        this.getEntityManager().createQuery(DELETE_BY_COMPANY_AND_CODE, QpbmtQualificationGroupSetting.class).setParameter("cid", AppContexts.user().companyId()).setParameter("qualificationGroupCode", domain.getQualificationGroupCode().v()).executeUpdate();
-    }
+	@Override
+	public void remove(QualificationGroupSetting domain) {
+		this.getEntityManager().createQuery(DELETE_BY_COMPANY_AND_CODE, QpbmtQualificationGroupSetting.class)
+				.setParameter("cid", AppContexts.user().companyId())
+				.setParameter("qualificationGroupCode", domain.getQualificationGroupCode().v()).executeUpdate();
+	}
 
-    private static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
-        Set<Object> seen = ConcurrentHashMap.newKeySet();
-        return t -> seen.add(keyExtractor.apply(t));
-    }
+	private static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
+		Set<Object> seen = ConcurrentHashMap.newKeySet();
+		return t -> seen.add(keyExtractor.apply(t));
+	}
 }
