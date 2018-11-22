b00596f3a05e2f5d8d36ea37f3d44e1b16b552a1
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2018-11-22 12:17:31 +0700

    Add infra StopSetting

---
+++ nts.uk/uk.com/com.ctx/sys/gateway/nts.uk.ctx.sys.gateway.infra/src/main/java/nts/uk/ctx/sys/gateway/infra/entity/stopbysystem/SgwdtStopBySystem.java
@@ -0,0 +1,55 @@
+package nts.uk.ctx.sys.gateway.infra.entity.stopbysystem;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import nts.uk.shr.infra.data.entity.UkJpaEntity;
+
+/**
+ * システム全体の利用停止の設定.
+ * @author sonnlb
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "SGWST_SET_STOP_BY_SYSTEM")
+
+public class SgwdtStopBySystem extends UkJpaEntity implements Serializable {	
+    private static final long serialVersionUID = 1L;
+   
+    /** 契約コード */
+    @Id
+    @Column(name = "CONTRACT_CD")
+    public String contractCd;
+    
+    /** 利用停止モード  */
+    @Column(name = "USAGE_STOP_MODE_TYPE")
+    public Integer usageStopMode;
+    
+    /** 利用停止のメッセージ */
+    @Column(name = "USAGE_STOP_MESSAGE")
+    public String usageStopMessage;
+    
+    /** システム利用状態 */
+    @Column(name = "SYSTEM_STATUS_TYPE")
+    public int systemStatus;
+    
+    /** 停止予告のメッセージ */
+    @Column(name = "STOP_MESSAGE")
+    public String stopMessage;
+    
+	@Override
+	protected Object getKey() {
+		return this.contractCd;
+	}
+}
