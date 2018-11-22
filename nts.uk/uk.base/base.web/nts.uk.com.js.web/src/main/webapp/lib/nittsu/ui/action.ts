be6997aae741622f11f37d05b97e3f7ff2fa6349
misaka mikoto <tin_dh@nittsusystem-vn.com>
2018-11-22 10:09:22 +0900

    システム利用停止のメッセージ表示

--- nts.uk/uk.base/base.web/nts.uk.com.js.web/src/main/webapp/lib/nittsu/ui/action.ts
+++ nts.uk/uk.base/base.web/nts.uk.com.js.web/src/main/webapp/lib/nittsu/ui/action.ts
@@ -15,6 +15,42 @@ module nts.uk.ui.action {
             $('#functions-area-bottom').removeClass("disappear");
             $('#contents-area').removeClass("disappear");
         });
+        
+         ui.viewModelApplied.add(function () {
+            if(!nts.uk.util.isNullOrUndefined(__viewContext.program.operationMode) 
+                && (__viewContext.program.operationMode == 1 || __viewContext.program.operationMode == 2)){
+                let operationInfo = $("<div>", { 'class': 'operation-info-container marquee', 'id': 'operation-info' }),
+                    moving = $("<div>"), text = $("<label>"), text2 = $("<label>");
+                moving.append(text).append(text2);
+                operationInfo.append(moving).css({ right: ($("#manual").outerWidth() + 5) + "px" });
+                text.text(__viewContext.program.operationWarning);
+                text2.text(__viewContext.program.operationWarning);
+                $("#pg-area").append(operationInfo);
+                
+                operationInfo.hover(
+                    function() {
+                        moving.addClass( "animate-stopping" );
+                    }, function() {
+                        moving.removeClass( "animate-stopping" );
+                    }
+                );
+                
+                let limit = Math.floor(0 - moving.width()), current = limit, id = setInterval(running, 50);
+                moving.css({ "right": current + "px" });
+                operationInfo.data("animate-id", id);
+                function running() {
+                    if(moving.hasClass("animate-stopping")){
+                        return;
+                    }
+                    if (current >= 200) {
+                        current = limit;
+                    } else {
+                        current++;
+                    }
+                    moving.css({ "right": current + "px" });
+                }
+            }
+        });
 
     }
 }
