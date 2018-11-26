dce28d946bb5aaf4a39dca37560ed571fe463c77
Pham Van dan <dan_pv@nittsusystem-vn.com>
2018-11-26 10:14:10 +0900

    add validate() function to constraints

--- nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/com/validate/constraint/implement/NumericConstraint.java
+++ nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/com/validate/constraint/implement/NumericConstraint.java
@@ -1,10 +1,13 @@
 package nts.uk.shr.com.validate.constraint.implement;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 import lombok.Getter;
+import nts.gul.util.value.ValueWithType;
 import nts.uk.shr.com.validate.constraint.DataConstraint;
 import nts.uk.shr.com.validate.constraint.ValidatorType;
+import nts.uk.shr.com.validate.validator.ErrorIdFactory;
 
 @Getter
 public class NumericConstraint extends DataConstraint {
@@ -21,8 +24,8 @@ public class NumericConstraint extends DataConstraint {
 
 	private int decimalPart;
 
-	public NumericConstraint(int column, boolean minusAvailable, boolean money, BigDecimal min, BigDecimal max, int integerPart,
-			int decimalPart) {
+	public NumericConstraint(int column, boolean minusAvailable, boolean money, BigDecimal min, BigDecimal max,
+			int integerPart, int decimalPart) {
 		super(column, ValidatorType.NUMERIC);
 		this.minusAvailable = minusAvailable;
 		this.money = money;
@@ -40,6 +43,80 @@ public class NumericConstraint extends DataConstraint {
 		this.max = max;
 		this.integerPart = integerPart;
 		this.decimalPart = decimalPart;
+	}
+	
+	public Optional<String> validate(ValueWithType value) {
+		switch (value.getType()) {
+		case NUMBER:
+			return validateNumber(new BigDecimal(value.getValue().toString()));
+		case TEXT:
+			return validateString(value.getText());
+		default:
+			return	Optional.of(ErrorIdFactory.NumericType); 
+		}
+	}
+
+	public Optional<String> validateString(String stringValue) {
+
+		if (!isNumeric(stringValue)) {
+			return Optional.of(ErrorIdFactory.NumericType);
+		}
+
+		BigDecimal value = new BigDecimal(stringValue);
+
+		return validateNumber(value);
+	}
+	
+	private Optional<String> validateNumber(BigDecimal value) {
+
+		if (!this.minusAvailable && !validateMinus(value)) {
+			return Optional.of(ErrorIdFactory.MinusErrorId);
+		}
+
+		if (!validateMin(this.min, value)) {
+			return Optional.of(ErrorIdFactory.NumericMinErrorId);
+		}
+
+		if (!validateMax(this.max, value)) {
+			return Optional.of(ErrorIdFactory.NumericMaxErrorId);
+		}
+
+		if (!validateIntegerPart(this.integerPart, value)) {
+			return Optional.of(ErrorIdFactory.IntegerPartErrorId);
+		}
+
+		if (!validateDecimalPart(this.decimalPart, value)) {
+			return Optional.of(ErrorIdFactory.DecimalPartErrorId);
+		}
+
+		return Optional.empty();
+	}
+
+	private boolean validateMinus(BigDecimal value) {
+		return value.compareTo(BigDecimal.ZERO) >= 0;
+	}
+
+	private boolean validateMin(BigDecimal max, BigDecimal value) {
+		return value.compareTo(max) >= 0;
+	}
+
+	private boolean validateMax(BigDecimal max, BigDecimal value) {
+		return value.compareTo(max) <= 0;
+	}
+
+	private boolean validateIntegerPart(int integerPart, BigDecimal value) {
+		value = value.stripTrailingZeros();
+		int integerPartOfValue = value.precision() - value.scale();
+		return integerPartOfValue <= integerPart;
+	}
+
+	private boolean validateDecimalPart(int decimalPart, BigDecimal value) {
+		value = value.stripTrailingZeros();
+		return value.scale() <= decimalPart;
+	}
+
+	public boolean isNumeric(String strNum) {
+		return strNum.matches("-?\\d+(\\.\\d+)?");
 	}
 
 }
--- nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/com/validate/constraint/implement/TimeConstraint.java
+++ nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/com/validate/constraint/implement/TimeConstraint.java
@@ -1,20 +1,54 @@
 package nts.uk.shr.com.validate.constraint.implement;
 
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import lombok.Getter;
+import nts.gul.util.value.ValueWithType;
 import nts.uk.shr.com.validate.constraint.DataConstraint;
 import nts.uk.shr.com.validate.constraint.ValidatorType;
+import nts.uk.shr.com.validate.validator.ErrorIdFactory;
+import nts.uk.shr.com.validate.validator.TimeMinMaxValidator;
 
 @Getter
-public class TimeConstraint extends DataConstraint{
-	
+public class TimeConstraint extends DataConstraint {
+
+	private static final String TIME_FORMAT = "^[0-9]+:[0-5][0-9]$";
+
 	private int min;
-	
+
 	private int max;
-	
+
 	public TimeConstraint(int column, int min, int max) {
 		super(column, ValidatorType.TIME);
 		this.min = min;
 		this.max = max;
 	}
-	
+
+	public boolean validateTimeStyle(String value) {
+		Pattern pattern = Pattern.compile(TIME_FORMAT);
+		Matcher matcher = pattern.matcher(value);
+		return matcher.matches();
+	}
+
+	public Optional<String> validate(ValueWithType value) {
+		switch (value.getType()) {
+		case TEXT:
+			return validateString(value.getText());
+		default:
+			return Optional.of(ErrorIdFactory.TimeStyleErrorId);
+		}
+	}
+
+	private Optional<String> validateString(String value) {
+
+		// validate style
+		if (!validateTimeStyle(value)) {
+			return Optional.of(ErrorIdFactory.TimeStyleErrorId);
+		}
+
+		return TimeMinMaxValidator.validateMinMax(this.min, this.max, value);
+	}
+
 }
