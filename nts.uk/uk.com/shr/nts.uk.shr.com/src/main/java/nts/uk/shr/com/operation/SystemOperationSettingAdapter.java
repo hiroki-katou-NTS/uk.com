fe1ff619eceb75d4bc799496f8ddef099990ce66
Pham Van dan <dan_pv@nittsusystem-vn.com>
2018-11-27 15:25:05 +0900

    add stopUseConfirm

--- nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/com/operation/SystemOperationSettingAdapter.java
+++ nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/com/operation/SystemOperationSettingAdapter.java
@@ -1,6 +1,17 @@
 package nts.uk.shr.com.operation;
 
+import java.util.Optional;
+
 public interface SystemOperationSettingAdapter {
 
 	public SystemOperationSetting getSetting();
+	
+	/**
+	 * システム停止中の確認
+	 * @return
+	 * if stopping - return Optional with value
+	 * else return empty optional
+	 */
+	public Optional<String> stopUseConfirm();
+	
 }
