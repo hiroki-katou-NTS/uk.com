c1feb0b734c4588d2d0caa867e2a0e484ea9c7aa
NSVN-DoVanPhuong <phuong_dv@nittsusystem-vn.com>
2018-11-27 08:53:19 +0700

    Fix Warning

--- nts.uk/uk.com/com.ctx/bs/company/nts.uk.ctx.bs.company.pub/src/main/java/nts/uk/ctx/bs/company/pub/company/BeginOfMonthExport.java
+++ nts.uk/uk.com/com.ctx/bs/company/nts.uk.ctx.bs.company.pub/src/main/java/nts/uk/ctx/bs/company/pub/company/BeginOfMonthExport.java
@@ -1,7 +1,7 @@
 package nts.uk.ctx.bs.company.pub.company;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;-
+import lombok.Data;
 import lombok.NoArgsConstructor;
 //import nts.arc.time.GeneralDate;
 
