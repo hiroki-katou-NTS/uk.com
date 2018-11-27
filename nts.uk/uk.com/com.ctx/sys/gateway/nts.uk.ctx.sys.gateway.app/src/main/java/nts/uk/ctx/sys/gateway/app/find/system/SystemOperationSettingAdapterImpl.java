e332fa6b41632da24da58829d04b2926c8425385
Pham Van dan <dan_pv@nittsusystem-vn.com>
2018-11-27 15:41:31 +0900

    mock implements

--- nts.uk/uk.com/com.ctx/sys/gateway/nts.uk.ctx.sys.gateway.app/src/main/java/nts/uk/ctx/sys/gateway/app/find/system/SystemOperationSettingAdapterImpl.java
+++ nts.uk/uk.com/com.ctx/sys/gateway/nts.uk.ctx.sys.gateway.app/src/main/java/nts/uk/ctx/sys/gateway/app/find/system/SystemOperationSettingAdapterImpl.java
@@ -1,7 +1,10 @@
 package nts.uk.ctx.sys.gateway.app.find.system;
 
+import java.util.Optional;
+
 import javax.ejb.Stateless;
 
+import nts.uk.shr.com.context.AppContexts;
 import nts.uk.shr.com.operation.SystemOperationSetting;
 import nts.uk.shr.com.operation.SystemOperationSetting.SystemOperationMode;
 import nts.uk.shr.com.operation.SystemOperationSetting.SystemStopMode;
@@ -17,12 +20,21 @@ public class SystemOperationSettingAdapterImpl implements SystemOperationSetting
 		/** IF STOP ALL SYSTEM */
 		/** IF BEFORE STOP ALL SYSTEM AND COMPANY IS NOT STOP */
 		if(true){
-			return SystemOperationSetting.setting(SystemStopType.ALL_SYSTEM, SystemOperationMode.STOP, SystemStopMode.ADMIN_MODE, "test 1", "test 1"); 
+			return SystemOperationSetting.setting(SystemStopType.ALL_SYSTEM, SystemOperationMode.STOP, SystemStopMode.ADMIN_MODE, "test1", "test1"); 
 		}
 		
 		/** IF STOP COMPANY */
 		/** IF STOP COMPANY */
 		return SystemOperationSetting.setting(SystemStopType.COMPANY, SystemOperationMode.STOP, SystemStopMode.ADMIN_MODE, "test 1", "test 1");
+	}
+
+	@Override
+	public Optional<String> stopUseConfirm() {
+		String contractCode = AppContexts.user().contractCode();
+		if (contractCode.equals("000000000000")) {
+			return Optional.of("とりあえずサーバーに接続できません。ログインをしなおしてください。");
+		}
+		return Optional.empty();
 	}
 
 }
