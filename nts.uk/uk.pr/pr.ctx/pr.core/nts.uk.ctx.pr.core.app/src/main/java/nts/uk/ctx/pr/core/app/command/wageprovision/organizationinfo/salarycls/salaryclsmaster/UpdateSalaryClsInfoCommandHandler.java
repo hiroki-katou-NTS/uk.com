a597d3ec97e30d7bcfe50c4b23a289e5a606926a
3SI\sang.nv <sang.nv@3si.vn>
2018-11-28 16:58:04 +0700

    CMM015: Rename file & package

--- nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.app/src/main/java/nts/uk/ctx/pr/core/app/command/wageprovision/organizationinfo/salarycls/salaryclsmaster/UpdateSalaryClsInfoCommandHandler.java
+++ nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.app/src/main/java/nts/uk/ctx/pr/core/app/command/wageprovision/organizationinfo/salarycls/salaryclsmaster/UpdateSalaryClsInfoCommandHandler.java
@@ -1,4 +1,4 @@
-package nts.uk.ctx.pr.core.app.command.wageprovision.organizationinformation.salaryclassification.salaryclassificationmaster;
+package nts.uk.ctx.pr.core.app.command.wageprovision.organizationinfo.salarycls.salaryclsmaster;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
@@ -12,14 +12,14 @@ import nts.uk.shr.com.context.AppContexts;
 
 @Stateless
 @Transactional
-public class UpdateSalaryClassificationInformationCommandHandler extends CommandHandler<SalaryClassificationInformationCommand> {
+public class UpdateSalaryClsInfoCommandHandler extends CommandHandler<SalaryClsInfoCommand> {
 
     @Inject
     private SalaryClassificationInformationRepository repository;
 
     @Override
-    protected void handle(CommandHandlerContext<SalaryClassificationInformationCommand> context) {
-        SalaryClassificationInformationCommand command = context.getCommand();
+    protected void handle(CommandHandlerContext<SalaryClsInfoCommand> context) {
+        SalaryClsInfoCommand command = context.getCommand();
         repository.update(new SalaryClassificationInformation(
                 AppContexts.user().companyId(),
                 command.getSalaryClassificationCode(),
