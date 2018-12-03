ed63bdb15449b13bf9f6c3921656199ee2c7dc76
3SI\sang.nv <sang.nv@3si.vn>
2018-12-03 14:42:14 +0700

    Change position enum

--- nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.dom/src/main/java/nts/uk/ctx/pr/core/dom/wageprovision/wagetable/QualificationPaymentMethod.java
+++ nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.dom/src/main/java/nts/uk/ctx/pr/core/dom/wageprovision/wagetable/QualificationPaymentMethod.java
@@ -4,19 +4,22 @@ package nts.uk.ctx.pr.core.dom.wageprovision.wagetable;
  * 資格支払方法
  */
 public enum QualificationPaymentMethod {
-	
-	PAY_ONLY_ONE_HIGHEST_BENEFIT(0, "一番高い手当を1つだけ支給する"), 
-	
-	ADD_MULTIPLE_APPLICABLE_AMOUNT(1, "複数該当した金額を加算する");
+    ADD_MULTIPLE_APPLICABLE_AMOUNT(0, "複数該当した金額を加算する"),
+    PAY_ONLY_ONE_HIGHEST_BENEFIT(1, "一番高い手当を1つだけ支給する");
 
-	/** The value. */
-	public final int value;
 
-	/** The name id. */
-	public final String nameId;
+    /**
+     * The value.
+     */
+    public final int value;
 
-	private QualificationPaymentMethod(int value, String nameId) {
-		this.value = value;
-		this.nameId = nameId;
-	}
+    /**
+     * The name id.
+     */
+    public final String nameId;
+
+    private QualificationPaymentMethod(int value, String nameId) {
+        this.value  = value;
+        this.nameId = nameId;
+    }
 }
