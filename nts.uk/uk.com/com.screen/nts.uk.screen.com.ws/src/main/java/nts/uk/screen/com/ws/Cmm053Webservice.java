8634a9e8482ea11faac06824c62d0035abb1f2ab
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2018-12-03 15:49:45 +0700

    fix bug #102875 CMM053 (cập nhật xử lý EA #102875)

--- nts.uk/uk.com/com.screen/nts.uk.screen.com.ws/src/main/java/nts/uk/screen/com/ws/Cmm053Webservice.java
+++ nts.uk/uk.com/com.screen/nts.uk.screen.com.ws/src/main/java/nts/uk/screen/com/ws/Cmm053Webservice.java
@@ -6,6 +6,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
+import nts.uk.ctx.workflow.app.command.approvermanagement.workroot.HistoryCmm053Command;
 import nts.uk.ctx.workflow.app.find.approvermanagement.workroot.ManagerSettingDto;
 import nts.uk.screen.com.app.find.approvermanagement.workroot.SettingOfManagerFinder;
 
@@ -24,5 +25,11 @@ public class Cmm053Webservice {
 	@Path("find/settingOfManager/{employeeId}")
 	public ManagerSettingDto getPsAppRootBySettingOfManager(@PathParam("employeeId") String employeeId) {
 		return comFinder.getPsAppRootBySettingOfManager(employeeId);
+	}
+	
+	@POST
+	@Path("check-approval-setting")
+	public void checkApprovalSetting(HistoryCmm053Command command) {
+		this.comFinder.checkApprovalSetting(command);
 	}
 }
--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmm/053/a/cmm053.a.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmm/053/a/cmm053.a.vm.ts
@@ -128,7 +128,6 @@ module nts.uk.com.view.cmm053.a.viewmodel {
         start(): JQueryPromise<any> {
             let self = this;
             var dfd = $.Deferred();
-
             service.getInfoEmLogin().done(function(employeeInfo) {
                 service.getWpName().done(function(wpName) {
                     self.employeeInputList.push(new EmployeeKcp009(employeeInfo.sid,
@@ -193,7 +192,6 @@ module nts.uk.com.view.cmm053.a.viewmodel {
             let self = this;
             $('.nts-input').trigger("validate");
             if (!nts.uk.ui.errors.hasError()) {
-                block.invisible();
                 let startDate = new Date(self.settingManager().startDate());
                 let closingStartDate = new Date(self.settingManager().closingStartDate());
                 //開始日＜締めの開始日
@@ -201,7 +199,6 @@ module nts.uk.com.view.cmm053.a.viewmodel {
                     closingStartDate = nts.uk.time.formatDate(closingStartDate, 'yyyy/MM/dd');
                     //エラーメッセージ（Msg_1072）
                     dialog.alertError({ messageId: "Msg_1072", messageParams: [closingStartDate] });
-                    block.clear();
                 } else {
                     //入力承認者の承認権限をチェックする
                     if (!self.settingManager().hasAuthority()) {
@@ -212,21 +209,23 @@ module nts.uk.com.view.cmm053.a.viewmodel {
                         let command = ko.toJS(self.settingManager());
                         command.startDate = moment.utc(self.settingManager().startDate(), "YYYY/MM/DD").toISOString();
                         command.endDate = moment.utc(self.settingManager().endDate(), "YYYY/MM/DD").toISOString();
+                        //ドメインモデル「承認設定」．本人による承認をチェックする
+                        self.checkApprovalSetting(command).done(() => {
+                            if (self.screenMode() == EXECUTE_MODE.NEW_MODE) {
+                                self.callInsertHistoryService(command);
+                                return;
+                            }
 
-                        if (self.screenMode() == EXECUTE_MODE.NEW_MODE) {
-                            self.callInsertHistoryService(command);
-                            return;
-                        }
+                            if (self.screenMode() == EXECUTE_MODE.UPDATE_MODE && self.settingManager().hasHistory()) {
+                                self.callUpdateHistoryService(command);
+                                return;
+                            }
 
-                        if (self.screenMode() == EXECUTE_MODE.UPDATE_MODE && self.settingManager().hasHistory()) {
-                            self.callUpdateHistoryService(command);
-                            return;
-                        }
-
-                        if (self.screenMode() == EXECUTE_MODE.UPDATE_MODE && !self.settingManager().hasHistory()) {
-                            self.callInsertHistoryService(command);
-                            return;
-                        }
+                            if (self.screenMode() == EXECUTE_MODE.UPDATE_MODE && !self.settingManager().hasHistory()) {
+                                self.callInsertHistoryService(command);
+                                return;
+                            }
+                        });
                     }
                 }
             }
@@ -238,6 +237,22 @@ module nts.uk.com.view.cmm053.a.viewmodel {
         checkInputApproverAuthority(employeeCode: any){
             let self = this;
             self.getEmployeeByCode(employeeCode, APPROVER_TYPE.DEPARTMENT_APPROVER);
+        }
+        
+        checkApprovalSetting(command): JQueryPromise<any> {
+            let self = this,
+                dfd = $.Deferred();
+            block.invisible();
+            service.checkApprovalSetting(command).done(() => {
+                 block.clear();
+                dfd.resolve();
+            }).fail(error => {
+                dialog.alertError({ messageId: error.messageId });
+                 block.clear();
+                dfd.rejected();
+                })
+
+            return dfd.promise();
         }
 
         //削除する
@@ -266,6 +281,7 @@ module nts.uk.com.view.cmm053.a.viewmodel {
 
         callInsertHistoryService(command) {
             let self = this;
+            block.invisible();
             service.insertHistoryByManagerSetting(command).done(result => {
                 //情報メッセージMsg_15
                 dialog.info({ messageId: "Msg_15" }).then(() => {
@@ -283,12 +299,13 @@ module nts.uk.com.view.cmm053.a.viewmodel {
 
         callUpdateHistoryService(command) {
             let self = this;
+            block.invisible();
             service.updateHistoryByManagerSetting(command).done(result => {
                 //情報メッセージMsg_15
                 dialog.info({ messageId: "Msg_15" }).then(() => {
-                    self.initScreen().done(()=>{
+                    self.initScreen().done(() => {
                         self.isInitDepartment = false;
-                        self.isInitdailyApproval = false;    
+                        self.isInitdailyApproval = false;
                     });
                 });
             }).fail(error => {
