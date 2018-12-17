3ca0856264dc2c7ac4b9447437d54664a97658c6
NSVC\hieult <hieultph03963@fpt.edu.vn>
2018-12-17 13:35:17 +0700

    Create Finder

--- nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.app/src/main/java/nts/uk/ctx/pereg/app/find/person/setting/matrix/personinfomatrixitem/DisplayItemColumnSetFinder.java
+++ nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.app/src/main/java/nts/uk/ctx/pereg/app/find/person/setting/matrix/personinfomatrixitem/DisplayItemColumnSetFinder.java
@@ -1,14 +1,18 @@
 /**
  * 
  */
-package nts.uk.ctx.pereg.app.find.person.setting.matrix;
+package nts.uk.ctx.pereg.app.find.person.setting.matrix.personinfomatrixitem;
 
+import java.util.List;
 import java.util.Optional;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import nts.uk.ctx.pereg.dom.person.info.item.PerInfoItemDefRepositoty;
+import nts.uk.ctx.pereg.dom.person.setting.matrix.personinfomatrixitem.PersonInfoMatrixItem;
+import nts.uk.ctx.pereg.dom.person.setting.matrix.personinfomatrixitem.PersonInfoMatrixItemRepo;
+import nts.uk.ctx.sys.auth.app.find.user.UserDto;
 
 /**
  * @author hieult
@@ -19,6 +23,9 @@ public class DisplayItemColumnSetFinder {
 
 	@Inject
 	private PerInfoItemDefRepositoty perInfoItemDefRepositoty;
+	
+	@Inject
+	private PersonInfoMatrixItemRepo repo;
 	
 	public Optional<StatupProcessDto> findSetting(String pInfoCategoryID, int isAbolition , String parentCodeItem) {
 		/*
@@ -32,5 +39,8 @@ public class DisplayItemColumnSetFinder {
 		
 		return null;
 	}
-	
+	public List<PersonInfoMatrixItem> get(String pInfoCategoryID ){
+		List<PersonInfoMatrixItem> listData = repo.findByCategoryID(pInfoCategoryID);
+		return listData;
+	}
 }
---
+++ nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.app/src/main/java/nts/uk/ctx/pereg/app/find/person/setting/matrix/personinfomatrixitem/StatupProcessDto.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package nts.uk.ctx.pereg.app.find.person.setting.matrix.personinfomatrixitem;
+
+/**
+ * @author hieult
+ *
+ */
+public class StatupProcessDto {
+
+}
+
