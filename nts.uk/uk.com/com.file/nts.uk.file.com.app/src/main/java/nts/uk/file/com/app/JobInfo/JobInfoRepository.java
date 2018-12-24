dea074d1bc86984f6b3960ad25194e396f192ec4
Thanh.TQ <thanhcoctn95@gmail.com>
2018-12-24 13:27:32 +0700

    Update Design Cas013, Cas014

--- nts.uk/uk.com/com.file/nts.uk.file.com.app/src/main/java/nts/uk/file/com/app/JobInfo/JobInfoRepository.java
+++ nts.uk/uk.com/com.file/nts.uk.file.com.app/src/main/java/nts/uk/file/com/app/JobInfo/JobInfoRepository.java
@@ -7,5 +7,5 @@ import nts.uk.shr.infra.file.report.masterlist.data.MasterData;
 public interface JobInfoRepository {
 	List<MasterData> getDataRoleSetPosExport(String date);
 	
-	List<MasterData> getDataRoleSetEmpExport();
+	List<MasterData> getDataRoleSetEmpExport(String date);
 }
--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cas/013/m/cas013.m.service.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cas/013/m/cas013.m.service.ts
@@ -1,11 +1,11 @@
-module nts.uk.com.view.cas014.m {
+module nts.uk.com.view.cas013.m {
     export module service {
         // Service paths.
         var servicePath = {
             saveAsExcel: "file/at/personrole/saveAsExcel"
         }
         export function saveAsExcel(languageId: string, date: string): JQueryPromise<any> {
-                return nts.uk.request.exportFile('/masterlist/report/print', {domainId: "JobInfo", domainType: "CAS014ロールセットの付与", languageId: languageId, reportType: 0, baseDate: date});
+                return nts.uk.request.exportFile('/masterlist/report/print', {domainId: "Indivigrant", domainType: "CAS013担当ロールの付与", languageId: languageId, reportType: 0, baseDate: date});
         }
     }
 
