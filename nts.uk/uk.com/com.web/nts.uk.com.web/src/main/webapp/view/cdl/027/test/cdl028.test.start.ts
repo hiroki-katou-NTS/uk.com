6770b4878a5933d47809fb9cef696f6502baa750
datnv8 <dat.lh@3si.vn>
2018-12-24 19:34:20 +0700

    Revert "DatNV: coding cdl 028"
    This reverts commit 55ff2dc3cc0fe71fb723f263c293e070c6d64d09.

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cdl/027/test/cdl028.test.start.ts
+++
@@ -1,8 +0,0 @@
-module nts.uk.com.view.cdl028.test {
-    __viewContext.ready(function() {
-        let screenModel = new viewmodel.ScreenModel();
-        screenModel.startPage().done(function() {
-            __viewContext.bind(screenModel);
-        });
-    });
-}
