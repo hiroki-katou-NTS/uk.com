0fa9728f56c5507e522ed7accea9a9cba756da4d
Nguyễn Văn Huân <huannv@2nf.com.vn>
2019-01-04 11:38:10 +0700

    update kmk011

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/011/h/kmk011.h.service.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/011/h/kmk011.h.service.ts
@@ -17,7 +17,8 @@ module nts.uk.at.view.kmk011.h {
             return nts.uk.request.ajax("at", path.find);
         }
         export function saveAsExcel(param): JQueryPromise<any> {
-        return nts.uk.request.exportFile('/masterlist/report/print', {domainId: "Divergence", domainType: "KMK011乖離時間の登録",languageId: 'ja',baseDate:moment.utc(param).format(),mode:1, reportType: 0});
+            console.log('gtcontext:' +__viewContext);
+        return nts.uk.request.exportFile('/masterlist/report/print', {domainId: "Divergence", domainType: "KMK011"+__viewContext.program.programName,languageId: 'ja',baseDate:moment.utc(param).format(),mode:1, reportType: 0});
     }
     }
     export module model {
