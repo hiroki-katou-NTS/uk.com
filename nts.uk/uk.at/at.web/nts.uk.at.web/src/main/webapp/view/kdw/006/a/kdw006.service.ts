c626a20d2f082aa73ccf6ff69fcae24ed0d00b91
dang dinh hoi <hoidd@2nf.com.vn>
2019-01-08 16:44:58 +0700

    update programe name for KDW006,KML004,KDL024 and role

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/006/a/kdw006.service.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/006/a/kdw006.service.ts
@@ -4,15 +4,7 @@ module nts.uk.at.view.kdw006 {
         }
         //common excel
         export function saveAsExcelCommon(languageId: string): JQueryPromise<any> {
-            return nts.uk.request.exportFile('/masterlist/report/print', {domainId: "OperationSetting", domainType: "KDW006"+nts.uk.resource.getText("KDW006_86"), languageId: languageId, reportType: 0});
-        }
-        //daily excel
-        export function saveAsExcelDaily(languageId: string): JQueryPromise<any> {
-            return nts.uk.request.exportFile('/masterlist/report/print', {domainId: "DailyExport", domainType: "KDW006"+nts.uk.resource.getText("KDW006_86"), languageId: languageId, reportType: 0});
-        }
-        //Monthly excel
-        export function saveAsExcelMonthly(languageId: string): JQueryPromise<any> {
-            return nts.uk.request.exportFile('/masterlist/report/print', {domainId: "MonthlyExport", domainType: "KDW006"+nts.uk.resource.getText("KDW006_86"), languageId: languageId, reportType: 0});
+            return nts.uk.request.exportFile('/masterlist/report/print', {domainId: "OperationSetting", domainType: "KDW006"+__viewContext.program.programName, languageId: languageId, reportType: 0});
         }
     }
 }
