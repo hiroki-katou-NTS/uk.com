99a21fbb56d3657ff26006ec274bfef0009d0086
Lê Hữu Đạt <dat.lh@3si.vn>
2019-01-11 16:21:55 +0700

    Fix missing stateless

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/monthly/flex/CheckBeforeCalcFlexChangeService.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/monthly/flex/CheckBeforeCalcFlexChangeService.java
@@ -1,5 +1,8 @@
 package nts.uk.ctx.at.record.dom.monthly.flex;
 
+import javax.ejb.Stateless;
+
+@Stateless
 public interface CheckBeforeCalcFlexChangeService {
 	// 社員のフレックス繰越上限時間を求める
 	ConditionCalcResult getConditionCalcFlex(String companyId, CalcFlexChangeDto calc);
