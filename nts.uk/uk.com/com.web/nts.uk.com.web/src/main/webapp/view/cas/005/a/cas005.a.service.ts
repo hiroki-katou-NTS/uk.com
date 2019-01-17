60cec3227cff486fb77dc5a558b63b2bdcc21301
Lê Hữu Đạt <dat.lh@3si.vn>
2019-01-17 11:30:49 +0700

    #128881: Sửa tên file excel output

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cas/005/a/cas005.a.service.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cas/005/a/cas005.a.service.ts
@@ -53,8 +53,10 @@ module nts.uk.com.view.cas005.a {
             return nts.uk.request.ajax("at",paths.getEmploymentRoleById+"/"+roleId);    
         }
         export function exportExcel(): JQueryPromise<any> {
+            let program = nts.uk.ui._viewModel.kiban.programName().split(" ");
+            let domainType = program[1] != null ? "CAS005" + program[1] : "";
             let _params = { domainId: "RoleEmployment", 
-                        domainType: "CAS005ロールの登録（就業）", 
+                        domainType: domainType,
                         languageId: "ja", 
                         reportType: 0};
             return nts.uk.request.exportFile('/masterlist/report/print',_params);
