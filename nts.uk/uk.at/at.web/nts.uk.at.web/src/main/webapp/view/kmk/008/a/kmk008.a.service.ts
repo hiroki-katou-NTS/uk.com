f6496ba0b50f123ad0f55a0243039c33c1033e11
Nam.Lh3si <nam.lh@3si.vn>
2019-01-23 18:14:58 +0700

    fix ts kmk008

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/008/a/kmk008.a.service.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/008/a/kmk008.a.service.ts
@@ -2,23 +2,26 @@ module nts.uk.at.view.kmk008.a {
     export module service {
         export class Service {
             paths = {
-                saveAsExcel: "file/at/worktypereport/saveAsExcel",         
-             };
+                saveAsExcel: "file/at/worktypereport/saveAsExcel",
+            };
             constructor() { }
 
             public functionDemo(printType: number): JQueryPromise<any> {
                 return null;
-             }
-            saveAsExcel(languageId: string, startDate: any, endDate: any): JQueryPromise<any> {
-                let program = nts.uk.ui._viewModel.kiban.programName().split(" ");
-                let programName = program[1]!=null?program[1]:"";
-                return nts.uk.request.exportFile('/masterlist/report/print', { domainId: "RegisterTime",    
-                                                                               domainType: "KMK008"+programName, 
-                                                                               languageId: languageId, 
-                                                                               reportType: 0,
-                                                                               startDate: startDate,
-                                                                               endDate: endDate  });
             }
-}
-}
+
+            export saveAsExcel(languageId: string, startDate: any, endDate: any): JQueryPromise<any> {
+                let program = nts.uk.ui._viewModel.kiban.programName().split(" ");
+                let programName = program[1] != null ? program[1] : "";
+                return nts.uk.request.exportFile('/masterlist/report/print', {
+                    domainId: "RegisterTime",
+                    domainType: "KMK008" + programName,
+                    languageId: languageId,
+                    reportType: 0,
+                    startDate: startDate,
+                    endDate: endDate
+                });
+            }
+        }
     }
+}   
