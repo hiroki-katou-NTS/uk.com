195b443c7261ee2c93d003e19e30d290a08d9c29
2NF_MinhVu <minhvv@2nf.com.vn>
2019-01-28 15:34:08 +0700

    KMF002

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmf/002/e/kmf002.e.service.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmf/002/e/kmf002.e.service.ts
@@ -40,6 +40,20 @@ module nts.uk.at.view.kmf002.e {
             command.year = year;
             return nts.uk.request.ajax("at", path.remove, command);
         }
+        
+        export function saveAsExcel(mode: string, startDate: string, endDate: string): JQueryPromise<any> {
+            let program = nts.uk.ui._viewModel.kiban.programName().split(" ");
+            let programName = program[1] != null ? program[1] : "";
+            return nts.uk.request.exportFile('/masterlist/report/print',
+                {
+                    domainId: "HolidaySetting", domainType: "KMF002" + programName,
+                    languageId: 'ja', reportType: 0,
+                    mode: mode,
+                    startDate: moment.utc(startDate).format(),
+                    endDate: moment.utc(endDate).format()
+                });
+        }
+        
     }
     
     /**
