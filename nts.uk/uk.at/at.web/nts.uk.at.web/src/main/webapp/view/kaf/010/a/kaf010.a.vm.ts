34d4f1243a80a5c4a2ca5b54038c91ff9713e93d
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2019-02-18 17:46:13 +0700

    fix bug #106260

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/010/a/kaf010.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/010/a/kaf010.a.vm.ts
@@ -836,6 +836,7 @@ module nts.uk.at.view.kaf010.a.viewmodel {
         findBychangeAppDateData(data: any) {
             var self = this;
             let overtimeDto = data;
+            self.prePostSelected(overtimeDto.displayPrePostFlg);
             self.displayCaculationTime(overtimeDto.displayCaculationTime);
             self.displayPrePostFlg(data.displayPrePostFlg ? true : false);
             self.restTimeDisFlg(self.restTimeDisFlg());
