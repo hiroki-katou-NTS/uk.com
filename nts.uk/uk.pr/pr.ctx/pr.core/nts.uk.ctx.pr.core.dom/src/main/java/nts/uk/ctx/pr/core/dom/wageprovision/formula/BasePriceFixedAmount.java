d40593291d7903797db0cf96f2121740ad51d730
HaDHV-3S <ha.dhv@3si.vn>
2019-02-19 15:20:10 +0700

    QMM017 update primitive value

--- nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.dom/src/main/java/nts/uk/ctx/pr/core/dom/wageprovision/formula/BasePriceFixedAmount.java
+++ nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.dom/src/main/java/nts/uk/ctx/pr/core/dom/wageprovision/formula/BasePriceFixedAmount.java
@@ -1,26 +1,23 @@
 package nts.uk.ctx.pr.core.dom.wageprovision.formula;
 
 import nts.arc.primitive.DecimalPrimitiveValue;
+import nts.arc.primitive.LongPrimitiveValue;
 import nts.arc.primitive.StringPrimitiveValue;
-import nts.arc.primitive.constraint.DecimalMantissaMaxLength;
-import nts.arc.primitive.constraint.DecimalMaxValue;
-import nts.arc.primitive.constraint.DecimalMinValue;
+import nts.arc.primitive.constraint.*;
 
 import java.math.BigDecimal;
 
 /**
-* 基準金額固定額
-*/
-@DecimalMaxValue("999999999")
-@DecimalMinValue("0")
-@DecimalMantissaMaxLength(0)
-public class BasePriceFixedAmount extends DecimalPrimitiveValue<BaseItemFixedValue>
-{
+ * 基準金額固定額
+ */
+
+@LongMaxValue(9999999999L)
+@LongMinValue(-9999999999L)
+public class BasePriceFixedAmount extends LongPrimitiveValue<BasePriceFixedAmount> {
 
     private static final long serialVersionUID = 1L;
 
-    public BasePriceFixedAmount(BigDecimal rawValue)
-    {
+    public BasePriceFixedAmount(Long rawValue) {
         super(rawValue);
     }
 
