413ae44d210c7a076a644ec65f23500179b63240
Nguyen Hoang Son <son_hoang@nittsusystem-vn.com>
2019-02-20 18:36:49 +0700

    push class AgreementOneMonthTime

---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/standardtime/primitivevalue/AgreementOneMonthTime.java
@@ -0,0 +1,19 @@
+package nts.uk.ctx.at.record.dom.standardtime.primitivevalue;
+
+import nts.arc.primitive.TimeDurationPrimitiveValue;
+import nts.arc.primitive.constraint.TimeRange;
+
+/**
+ * 
+ * @author sonnh1
+ *
+ */
+@TimeRange(max = "744:00", min = "00:00")
+public class AgreementOneMonthTime extends TimeDurationPrimitiveValue<AgreementOneMonthTime> {
+	private static final long serialVersionUID = 1L;
+
+	public AgreementOneMonthTime(int rawValue) {
+		super(rawValue);
+	}
+
+}
