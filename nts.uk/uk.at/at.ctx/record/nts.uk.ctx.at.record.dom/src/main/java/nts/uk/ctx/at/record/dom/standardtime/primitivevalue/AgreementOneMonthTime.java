0b7f037e9d9009d997d55bab6354db2414aa4477
Nguyen Hoang Son <son_hoang@nittsusystem-vn.com>
2019-02-25 10:26:00 +0700

    KMK008: update css and primitive AgreementOneMonthTime, add comment code

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/standardtime/primitivevalue/AgreementOneMonthTime.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/standardtime/primitivevalue/AgreementOneMonthTime.java
@@ -4,10 +4,12 @@ import nts.arc.primitive.TimeDurationPrimitiveValue;
 import nts.arc.primitive.constraint.TimeRange;
 
 /**
+ * ３６協定１ヶ月時間
  * 
  * @author sonnh1
  *
  */
+
 @TimeRange(max = "744:00", min = "00:00")
 public class AgreementOneMonthTime extends TimeDurationPrimitiveValue<AgreementOneMonthTime> {
 	private static final long serialVersionUID = 1L;
