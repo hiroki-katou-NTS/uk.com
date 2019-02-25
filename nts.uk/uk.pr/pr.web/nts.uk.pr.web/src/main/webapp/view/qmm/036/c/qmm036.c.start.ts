d5f442aabd89e048c5dd40278ccab02010fbe6e7
3SI\HaDHV <ha.dhv@3si.vn>
2019-02-25 16:27:01 +0700

    QMM036 #125628

--- nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/036/c/qmm036.c.start.ts
+++ nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/036/c/qmm036.c.start.ts
@@ -3,6 +3,7 @@ module nts.uk.pr.view.qmm036.c {
         __viewContext['screenModel'] = new viewmodel.ScreenModel();
         __viewContext['screenModel'].startPage().done(function() {
             __viewContext.bind(__viewContext['screenModel']);
+            $('#C1_3').focus();
         });
     });
 }
