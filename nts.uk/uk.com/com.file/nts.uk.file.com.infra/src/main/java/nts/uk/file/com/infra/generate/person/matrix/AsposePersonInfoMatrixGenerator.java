60eb4ca692087f7602cbaba9548bf4b75d25e9d1
Leu Thi Lan <lan-lt@nittsusystem-vn.com>
2019-02-27 09:22:15 +0700

    fix bug in: check nếu có khoảng trắng thì lấy split[1] còn không thì split[0]

--- nts.uk/uk.com/com.file/nts.uk.file.com.infra/src/main/java/nts/uk/file/com/infra/generate/person/matrix/AsposePersonInfoMatrixGenerator.java
+++ nts.uk/uk.com/com.file/nts.uk.file.com.infra/src/main/java/nts/uk/file/com/infra/generate/person/matrix/AsposePersonInfoMatrixGenerator.java
@@ -264,7 +264,7 @@ public class AsposePersonInfoMatrixGenerator extends AsposeCellsReportGenerator 
 													String[] stringSplit = comboxObj.getOptionText().split(JP_SPACE);
 													String selCode = dataSource.getCategoryCode().equals("CS00016") || dataSource.getCategoryCode().equals("CS00017")?  (stringSplit.length > 0? stringSplit[0]: ""): comboxObj.getOptionValue();
 													selCodeCell.setValue(selCode);
-													selNameCell.setValue(stringSplit[1]);
+													selNameCell.setValue(stringSplit.length > 1? stringSplit[1]: stringSplit[0]);
 												}else {
 													
 													selCodeCell.setValue(comboxObj.getOptionValue());
