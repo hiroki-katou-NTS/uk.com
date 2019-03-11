817948ba0df3a74e79f8b097a153b68c72e256b3
NSVN-TruongVanLai <NSVN-TruongVanLai@users.noreply.github.com>
2019-03-11 10:46:35 +0700

    fix bug 105356(commit lan 3)

--- nts.uk/uk.com/com.ctx/sys/assist/nts.uk.ctx.sys.assist.dom/src/main/java/nts/uk/ctx/sys/assist/dom/datarestoration/common/CsvFileUtil.java
+++ nts.uk/uk.com/com.ctx/sys/assist/nts.uk.ctx.sys.assist.dom/src/main/java/nts/uk/ctx/sys/assist/dom/datarestoration/common/CsvFileUtil.java
@@ -1,24 +1,24 @@
 package nts.uk.ctx.sys.assist.dom.datarestoration.common;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 import org.apache.commons.csv.CSVFormat;
 
 import nts.arc.system.ServerSystemProperties;
 import nts.gul.csv.CSVParsedResult;
+import nts.gul.csv.CustomCsvReader;
 import nts.gul.csv.NtsCsvReader;
 import nts.gul.csv.NtsCsvRecord;
 
@@ -26,12 +26,12 @@ public class CsvFileUtil {
 
 	private static final String NEW_LINE_CHAR = "\r\n";
 	private static final String DATA_STORE_PATH = ServerSystemProperties.fileStoragePath();
-	private static final Charset CHARSET = StandardCharsets.UTF_8;
-	
+
 	static List<List<String>> getAllRecord(InputStream inputStream) {
 		// get csv reader
 		NtsCsvReader csvReader = NtsCsvReader.newReader().withNoHeader().skipEmptyLines(true)
-				.withChartSet(Charset.forName("Shift_JIS")).withFormat(CSVFormat.EXCEL.withRecordSeparator(NEW_LINE_CHAR));
+				.withChartSet(Charset.forName("Shift_JIS"))
+				.withFormat(CSVFormat.EXCEL.withRecordSeparator(NEW_LINE_CHAR));
 		List<List<String>> result = new ArrayList<>();
 		try {
 			CSVParsedResult csvParsedResult = csvReader.parse(inputStream);
@@ -57,11 +57,12 @@ public class CsvFileUtil {
 	public static List<List<String>> getAllRecord(String fileId, String fileName) {
 		// get csv reader
 		NtsCsvReader csvReader = NtsCsvReader.newReader().withNoHeader().skipEmptyLines(true)
-				.withChartSet(Charset.forName("Shift_JIS")).withFormat(CSVFormat.EXCEL.withRecordSeparator(NEW_LINE_CHAR));
+				.withChartSet(Charset.forName("Shift_JIS"))
+				.withFormat(CSVFormat.EXCEL.withRecordSeparator(NEW_LINE_CHAR));
 		List<List<String>> result = new ArrayList<>();
 		try {
 			InputStream inputStream = createInputStreamFromFile(fileId, fileName);
-			if (!Objects.isNull(inputStream)){
+			if (!Objects.isNull(inputStream)) {
 				CSVParsedResult csvParsedResult = csvReader.parse(inputStream);
 				List<NtsCsvRecord> allRecord = csvParsedResult.getRecords();
 				getDataCSV(result, allRecord);
@@ -71,35 +72,68 @@ public class CsvFileUtil {
 		}
 		return result;
 	}
-	
-	static List<String> getCsvHeader(String fileName, String fileId) {
+
+	public static List<String> getCsvHeader(String fileName, String fileId) {
 		// get csv reader
-		NtsCsvReader csvReader = NtsCsvReader.newReader().withNoHeader().skipEmptyLines(true)
-				.withChartSet(Charset.forName("Shift_JIS"))
-				.withFormat(CSVFormat.EXCEL.withRecordSeparator(NEW_LINE_CHAR));
-		List<String> data = new ArrayList<>();
+		NtsCsvReader csvReader = NtsCsvReader.newReader().skipEmptyLines(true)
+				.withChartSet(Charset.forName("Shift_JIS"));
 		try {
 			InputStream inputStream = createInputStreamFromFile(fileId, fileName);
 			if (!Objects.isNull(inputStream)) {
-				
-				CSVParsedResult csvParsedResult = csvReader.parse(inputStream);
-
-				int count = 0;
-				if (!csvParsedResult.getRecords().isEmpty()) {
-					NtsCsvRecord header = csvParsedResult.getRecords().get(0);
+				List<String> data = new ArrayList<>();
+				Consumer<CSVParsedResult> csvResult = (c) -> {
+					NtsCsvRecord header = c.getRecords().get(0);
 					for (int i = 0; i < header.columnLength(); i++) {
 						data.add((String) header.getColumn(i));
 					}
-				}
-				csvParsedResult = null;
-				inputStream.close();
+				};
+				Function<NtsCsvRecord, Boolean> customCheckRow = (row) -> {
+					return true;
+				};
+				CustomCsvReader csvCustomReader = csvReader.createCustomCsvReader(inputStream);
+				csvCustomReader.setNoHeader(true);
+				csvCustomReader.readByStep(2, 1, csvResult, customCheckRow);
+				return data;
 			}
-		} catch (IOException e) {
+		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
-		return data;
+		return new ArrayList<>();
 	}
-	
+
+	public static Set<String> getListSid(String fileId, String fileName) {
+		NtsCsvReader csvReader = NtsCsvReader.newReader().skipEmptyLines(true)
+				.withChartSet(Charset.forName("Shift_JIS"));
+		try {
+			InputStream inputStream = createInputStreamFromFile(fileId, fileName);
+			CustomCsvReader csvCustomReader = csvReader.createCustomCsvReader(inputStream);
+			csvCustomReader.setNoHeader(true);
+			if (!Objects.isNull(inputStream)) {
+				Set<String> listSid = new HashSet<>();
+				Consumer<CSVParsedResult> csvResult = (c) -> {
+					for (int i = 0; i < c.getRecords().size(); i++) {
+						if (c.getRecords().get(i).getColumn(1) != null) {
+							listSid.add(c.getRecords().get(i).getColumn(1).toString().trim());
+						}
+					}
+				};
+				
+				Function<NtsCsvRecord, Boolean> customCheckRow = (row) -> {
+					if (row.getColumn(1).equals("CMF003_H_SID") || row.getColumn(1).equals("") || row.getColumn(1).equals(" "))
+						return false;
+					return true;
+				};
+
+				csvCustomReader.readByStep(3000, csvResult,customCheckRow);
+				csvCustomReader.close();
+				return listSid;
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return new HashSet<>();
+	}
+
 	static InputStream createInputStreamFromFile(String fileId, String fileName) {
 		String filePath = getExtractDataStoragePath(fileId) + "//" + fileName + ".csv";
 		try {
@@ -109,7 +143,7 @@ public class CsvFileUtil {
 		}
 	}
 
-	public static String getExtractDataStoragePath(String fileId){
+	public static String getExtractDataStoragePath(String fileId) {
 		return DATA_STORE_PATH + "//packs//" + fileId;
 	}
 }
