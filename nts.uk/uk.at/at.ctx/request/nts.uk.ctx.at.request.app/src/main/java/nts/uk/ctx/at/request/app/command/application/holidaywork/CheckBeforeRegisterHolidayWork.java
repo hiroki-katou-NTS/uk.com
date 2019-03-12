6d54148cbc5da6f96d05b566b52c2d9866fb4763
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2019-03-12 15:40:09 +0700

    fix bug #106703

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.app/src/main/java/nts/uk/ctx/at/request/app/command/application/holidaywork/CheckBeforeRegisterHolidayWork.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.app/src/main/java/nts/uk/ctx/at/request/app/command/application/holidaywork/CheckBeforeRegisterHolidayWork.java
@@ -262,12 +262,12 @@ public class CheckBeforeRegisterHolidayWork {
 		return inputCommand.stream().filter(item -> {
 			Integer startTime = item.getStartTime() == null ? null : item.getStartTime().intValue();
 			Integer endTime = item.getEndTime() == null ? null : item.getEndTime().intValue();
-			int appTime = item.getApplicationTime() == null ? -1 : item.getApplicationTime().intValue();
-			return startTime != null || endTime != null || appTime != -1;
+			Integer appTime = item.getApplicationTime() == null ? null : item.getApplicationTime().intValue();
+			return startTime != null || endTime != null || appTime != null;
 		}).map(item -> {
 			Integer startTime = item.getStartTime() == null ? null : item.getStartTime().intValue();
 			Integer endTime = item.getEndTime() == null ? null : item.getEndTime().intValue();
-			int appTime = item.getApplicationTime() == null ? -1 : item.getApplicationTime().intValue();
+			Integer appTime = item.getApplicationTime() == null ? null : item.getApplicationTime().intValue();
 			return HolidayWorkInput.createSimpleFromJavaType(Cid, appId, attendanceId, item.getFrameNo(), startTime,
 					endTime, appTime);
 		}).collect(Collectors.toList());
