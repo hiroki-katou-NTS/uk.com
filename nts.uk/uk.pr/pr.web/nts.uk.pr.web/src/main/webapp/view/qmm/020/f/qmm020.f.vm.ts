606029fc5ac26437eaaa35f41c9ddd4a393ff5f5
Hung Tran <32724898+1capedbaldy@users.noreply.github.com>
2019-03-12 14:03:14 +0700

    qmm020: 126095

--- nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/020/f/qmm020.f.vm.ts
+++ nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qmm/020/f/qmm020.f.vm.ts
@@ -54,7 +54,7 @@ module nts.uk.pr.view.qmm020.f.viewmodel {
         loadGird() {
             let self = this;
             $("#F3_1").ntsGrid({
-                height: '343px',
+                height: '344px',
                 dataSource: self.listStateLinkSettingMaster(),
                 primaryKey: 'id',
                 virtualization: true,
