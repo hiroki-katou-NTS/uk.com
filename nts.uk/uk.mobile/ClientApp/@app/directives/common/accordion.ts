8d80f323c3b84d2b1f7af754ce517ccac798d0a7
nsvn-nguyenvanvuong <vuong_nv@nittsusystem-vn.com>
2019-03-13 10:17:59 +0700

    comment chi tiết các bước xử lý của accordion directive

--- nts.uk/uk.mobile/ClientApp/@app/directives/common/accordion.ts
+++ nts.uk/uk.mobile/ClientApp/@app/directives/common/accordion.ts
@@ -3,23 +3,30 @@ import { Vue } from '@app/provider';
 
 const accordion = {
     bind(container: HTMLElement) {
+        // Bắt sự kiện click vào element đặt directive
         container.addEventListener('click', (evt: MouseEvent) => {
             let title = evt.target as HTMLElement;
 
+            // Tìm title của các group, nếu thấy và đúng định dạng
             if (title && title.classList.contains('btn-link')) {
                 let card = title.closest('.card') as HTMLElement;
 
+                // tìm thẻ chứa cả title và body của một collapse
                 if (card) {
+                    // nếu thấy thì tìm tất cả các body khác
                     let collapse = card.querySelector('.collapse') as HTMLElement,
                         collapses = container.querySelectorAll('.collapse');
 
                     if (collapse) {
+                        // kiểm tra trạng thái đang đóng hay mở
                         let showOrHide = dom.hasClass(collapse, 'show');
 
+                        // đóng tất cả các body khác
                         [].slice.call(collapses).forEach((element: HTMLElement) => {
                             dom.removeClass(element, 'show');
                         });
 
+                        // nếu là trạng thái đóng thì mở lại body được click (bằng title)
                         if (!showOrHide) {
                             dom.addClass(collapse, 'show');
                         }
@@ -30,4 +37,5 @@ const accordion = {
     }
 };
 
+// khai báo directive là dạng global
 Vue.directive('nts-accordion', accordion);
