1bf280e8a6be03dc8f08f08480114a1f8aebae8a
NTS\tin_dh <tin_dh@nittsusystem.co.jp>
2019-03-25 11:36:09 +0900

    resolve conflict when cherry pick 2146918f4a677a1b0836589c275db36198b992c7

--- nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/infra/web/request/CorsPreflightFilter.java
+++ nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/infra/web/request/CorsPreflightFilter.java
@@ -43,6 +43,16 @@ public class CorsPreflightFilter implements Filter {
 			if (httpRequest.getMethod().equals(HttpMethod.OPTIONS)) {
 				return;
 			}
+		} else if (httpRequest.getHeader("origin") != null && httpRequest.getHeader("origin").contains("localhost:3000")) {
+			/** TODO: this code just for DEV mode, when deploy mobile in same wildfly with other web pack, need to remove this */
+			val httpResponse =(HttpServletResponse) response;
+			httpResponse.addHeader("Access-Control-Allow-Origin", "*");
+			httpResponse.addHeader("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, MOBILE, PG-Path, X-CSRF-TOKEN");
+			httpResponse.addHeader("Access-Control-Allow-Credentials", "true");
+			httpResponse.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+			if (httpRequest.getMethod().equals(HttpMethod.OPTIONS)) {
+				return;
+			}
 		}
 		
 		chain.doFilter(httpRequest, response);
