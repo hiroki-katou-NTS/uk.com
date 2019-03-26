ff005937dff1b60d40cb649ef2c43ef1aa195af6
Pham Van dan <dan_pv@nittsusystem-vn.com>
2019-03-26 18:02:40 +0900

    update test page on third time

--- nts.uk/uk.mobile/ClientApp/views/documents/controls/date-input/index.ts
+++ nts.uk/uk.mobile/ClientApp/views/documents/controls/date-input/index.ts
@@ -1,29 +1,20 @@
 import { Vue } from '@app/provider';
 import { component } from '@app/core/component';
-import { TimeInputType} from '@app/utils/time'
 
 @component({
     route: {
-        url: '/controls/time-component',
+        url: '/controls/date-input',
         parent: '/documents'
     },
     template: require('./index.html'),
     validations: {
-        time: {
-            minValue: -720,
-            maxValue: 4319
-        },
         date: {
             minValue: -720,
             maxValue: 4319
         }
     },
 })
-export class TimeComponentTest extends Vue {
-
-    time: number = 750;
-
-    date1: string = "2017-08-04";
+export class DateInputControl extends Vue {
 
     date: Date = new Date("2017-08-04");
 
