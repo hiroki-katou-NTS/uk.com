f2971928b664e58c760405b7554e96bad5e851fc
Pham Van dan <dan_pv@nittsusystem-vn.com>
2019-03-26 18:01:30 +0900

    update time-utils

--- nts.uk/uk.mobile/ClientApp/@app/utils/time.ts
+++ nts.uk/uk.mobile/ClientApp/@app/utils/time.ts
@@ -48,13 +48,17 @@ export const time = {
             }
             return newMinutes;
         }, 
-        computeTimePoint(value: number): TimePoint {
+        computeTimePoint(value: number): TimeWithDayPoint {
             var day = time.timewd.computeDay(value);
             var hour = time.timewd.computeHour(value);
             var minute = time.timewd.computeMinute(value);
             return {
                 day, hour, minute
             }
+        },
+        toString(value: number): string {
+            var timePoint = time.timewd.computeTimePoint(value);
+            return timePoint.day + '　' + time.leftpad(timePoint.hour) + ' ： ' + time.leftpad(timePoint.minute);
         }
     }, 
     timedr: {
@@ -95,8 +99,72 @@ export const time = {
             } else {
                 return newHour * 60 - newMinute;
             }
+        },
+        toString(value: number) {
+            var timePoint = time.timedr.computeTimePoint(value);
+
+            if (timePoint.hour == 0 && value < 0) {
+                return '-' + time.leftpad(timePoint.hour) + ' : ' + time.leftpad(timePoint.minute);
+            }  
+            
+            return time.leftpad(timePoint.hour) + ' : ' + time.leftpad(timePoint.minute);
         }
 
+    },
+    timept: {
+        computeHour(value: number): number {
+            if (value >= 0) {
+                return Math.floor(value / 60);
+            } else {
+                return - Math.floor((1440 + value) / 60);
+            }
+        },
+        computeMinute(value: number): number {
+            var hour = 0;
+            if (value >= 0) {
+                hour = Math.floor(value / 60);
+                return value - hour * 60;
+            } else {
+                var minutesInDay = 1440 + value;
+                hour = Math.floor(minutesInDay / 60);
+                return minutesInDay - hour * 60;
+            }
+        },
+        computeTimePoint(value: number): TimePoint {
+            var hour = 0;
+            var minute = 0;
+            if (value >= 0) {
+                hour = Math.floor(value / 60);
+                minute = value - hour * 60;
+                return { hour, minute }
+            } else {
+                var minutesInDay = 1440 + value;
+                hour = Math.floor(minutesInDay / 60);
+                minute = minutesInDay - hour * 60;
+                return { hour: -hour, minute};
+            }
+            
+        },
+        computeValue(newHour: number, newMinute: number): number {
+            if ( newHour >= 0 ) {
+                return newHour * 60 + newMinute;
+            } else {
+                return -1440 - newHour * 60 + newMinute;
+            }
+        },
+        toString(value: number) {
+            var timePoint = time.timept.computeTimePoint(value);
+            return time.leftpad(timePoint.hour) + ' : ' + time.leftpad(timePoint.minute);
+        }
+    },
+    leftpad(value: number) : string {
+        if ( value >= 0 && value < 10) {
+            return '0' + value;
+        } else if (value < 0 && value > -10){
+            return '-0' + (0 - value); 
+        }
+
+        return '' + value;
     }
 };
 
@@ -108,8 +176,13 @@ export enum DAYS {
 }
 
 
-export interface TimePoint {
+export interface TimeWithDayPoint {
     day: string;
+    hour: number;
+    minute: number;
+}
+
+export interface TimePoint {
     hour: number;
     minute: number;
 }
@@ -120,7 +193,7 @@ export interface TimeDurationPoint {
 }
 
 export enum TimeInputType {
-    TimeDuration = 'time-duration',
+    TimeWithDay = 'time-with-day',
     TimePoint = 'time-point',
-    TimeWithDay = 'time-with-day'
+    TimeDuration = 'time-duration'
 }
