11fe9c6b6a49b34e43003b6f98499d8526ff5c6f
nsvn-nguyenvanvuong <vuong_nv@nittsusystem-vn.com>
2019-03-26 15:41:40 +0700

    update focus directive document

--- nts.uk/uk.mobile/ClientApp/@app/directives/common/focus.ts
+++ nts.uk/uk.mobile/ClientApp/@app/directives/common/focus.ts
@@ -1,8 +1,8 @@
-const focus = {
+import { Vue } from '@app/provider';
+
+Vue.directive('focus', {
     // directive definition
     inserted: function (el: HTMLInputElement) {
         el.focus()
-    }
-};
-
-export { focus };
+    }    
+});
