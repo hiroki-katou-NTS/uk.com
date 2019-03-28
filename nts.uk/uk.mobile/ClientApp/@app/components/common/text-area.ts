1d263ef2d48b595783406c33f74a8bb045659ede
Pham Van dan <dan_pv@nittsusystem-vn.com>
2019-03-28 14:53:42 +0900

    edit textare. Use a new @component

--- nts.uk/uk.mobile/ClientApp/@app/components/common/text-area.ts
+++ nts.uk/uk.mobile/ClientApp/@app/components/common/text-area.ts
@@ -1,60 +1,68 @@
 
-import { component, Prop } from '@app/core/component';
+import { component, Prop, Emit } from '@app/core/component';
 import { Vue } from '@app/provider';
+import { InputComponent } from '@app/components/common/input';
 
 @component({
-  template: `
-    <span>
-      <textarea
-      class="form-control"
-      :value="value"
-      :disabled=!enable
-      :readonly=readonly
-      @blur="doWhenBlur"
-      @change="doWhenChanged"
-      @input="doWhenInput"
-      @keyup="doWhenKeyUp"
-      @keydown="doWhenKeyDown"
-      >
-      </textarea>
-    </span>
-    `,
+    template: `
+    <div class="form-group row">
+
+        <template v-if="showTitle">
+            <div v-bind:class="columns.title">
+                <v-label v-bind:constraint="constraint">{{ name | i18n }}</v-label>
+            </div>
+        </template>
+        <template v-else />
+
+        <div v-bind:class="columns.input">
+            <div class="input-group input-group-transparent">
+                <template>
+                    <template v-if="icons.before">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" v-bind:class="iconsClass.before">{{  !iconsClass.before ? icons.before : '' }}</span>
+                        </div>
+                    </template>
+                    <template v-else />
+                    <template v-if="icons.after">
+                        <div class="input-group-append">
+                            <span class="input-group-text" v-bind:class="iconsClass.after">{{ !iconsClass.after ? icons.after : ''}}</span>
+                        </div>
+                    </template>
+                    <template v-else />
+                    <textarea class="form-control"
+                        ref="input"
+                        v-validate="{
+                            always: !!errorsAlways,
+                            errors: (errors || errorsAlways || {})
+                        }"
+                        v-bind:disabled="disabled"
+                        v-bind:readonly="!editable"
+                        v-bind:value="rawValue"
+                        v-on:click="click()"
+                        v-on:keydown.13="click()"
+                        v-on:input="input()"
+                    />
+                </textarea>
+                </template>
+                
+                <v-errors v-for="(error, k) in (errors || errorsAlways || {})" v-bind:key="k" v-bind:data="error" v-bind:name="name" />
+            </div>
+        </div>
+
+    </div>`
 })
-export class TextArea extends Vue {
-  @Prop()
-  value : String;
+export class TextArea extends InputComponent {
 
-  @Prop({ default: true})
-  enable: Boolean;
+    type: string = 'text';
 
-  @Prop({ default: false })
-  readonly: Boolean;
+    get rawValue() {
+        return (this.value || '');
+    }
 
-  @Prop({ default: false })
-  immediate: Boolean;
-
-  @Prop()
-  option : Object;
-
-  doWhenBlur() {
-    console.log("Bluring Bluring Bluring");
-  }
-
-  doWhenChanged() {
-    console.log("Value was Changed");
-  }
-
-  doWhenInput(e) {
-    this.$emit('input', e.target.value);
-  }
-
-  doWhenKeyUp(e) {
-    console.log("You have just key up");
-  }
-
-  doWhenKeyDown(){
-    console.log("You have just key down");
-  }
+    @Emit()
+    input() {
+        return (<HTMLInputElement>this.$refs.input).value;
+    }
 
 }
 
