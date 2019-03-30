391f56a5f298f9a0c830df826f16928ed0e49916
NTS\keisuke_hoshina <keisuke_hoshina@nittsusystem.co.jp>
2019-03-30 11:13:12 +0900

    fixed bug All 0 判定の時に手修正された値を元に戻す 追加修正

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/dailyprocess/calc/CalculateDailyRecordServiceImpl.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/dailyprocess/calc/CalculateDailyRecordServiceImpl.java
@@ -326,17 +326,21 @@ public class CalculateDailyRecordServiceImpl implements CalculateDailyRecordServ
 			
 			DailyRecordToAttendanceItemConverter converterForAllZero = attendanceItemConvertFactory.createDailyConverter();
 			DailyRecordToAttendanceItemConverter beforDailyRecordDto = converterForAllZero.setData(integrationOfDaily);
-			integrationOfDaily.setAttendanceTimeOfDailyPerformance(peform);
+			val recordAllZeroValueIntegration = beforDailyRecordDto.toDomain();
+			recordAllZeroValueIntegration.setAttendanceTimeOfDailyPerformance(peform);
 			List<ItemValue> itemValueList = Collections.emptyList();
 			if (!attendanceItemIdList.isEmpty()) {
 
 				itemValueList = beforDailyRecordDto.convert(attendanceItemIdList);
 				DailyRecordToAttendanceItemConverter afterDailyRecordDto = converterForAllZero
-						.setData(integrationOfDaily);
+						.setData(recordAllZeroValueIntegration);
 				afterDailyRecordDto.merge(itemValueList);
 
 				// 手修正された項目の値を計算前に戻す
 				integrationOfDaily = afterDailyRecordDto.toDomain();
+			}
+			else {
+				integrationOfDaily = recordAllZeroValueIntegration;
 			}
 			
 			return ManageCalcStateAndResult.failCalc(integrationOfDaily);
@@ -433,17 +437,21 @@ public class CalculateDailyRecordServiceImpl implements CalculateDailyRecordServ
 			
 			DailyRecordToAttendanceItemConverter converter = attendanceItemConvertFactory.createDailyConverter();
 			DailyRecordToAttendanceItemConverter beforDailyRecordDto = converter.setData(integrationOfDaily);
-			integrationOfDaily.setAttendanceTimeOfDailyPerformance(peform);
+			val recordAllZeroValueIntegration = beforDailyRecordDto.toDomain();
+			recordAllZeroValueIntegration.setAttendanceTimeOfDailyPerformance(peform);
 			List<ItemValue> itemValueList = Collections.emptyList();
 			if (!attendanceItemIdList.isEmpty()) {
 
 				itemValueList = beforDailyRecordDto.convert(attendanceItemIdList);
 				DailyRecordToAttendanceItemConverter afterDailyRecordDto = converter
-						.setData(integrationOfDaily);
+						.setData(recordAllZeroValueIntegration);
 				afterDailyRecordDto.merge(itemValueList);
 
 				// 手修正された項目の値を計算前に戻す
 				integrationOfDaily = afterDailyRecordDto.toDomain();
+			}
+			else {
+				integrationOfDaily = recordAllZeroValueIntegration;
 			}
 			
 		
