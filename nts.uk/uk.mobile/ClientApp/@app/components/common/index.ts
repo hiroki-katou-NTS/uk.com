4babf3688ca23a03ca07cf8f1a955b1c9e76218b
Pham Van dan <dan_pv@nittsusystem-vn.com>
2019-04-01 09:31:40 +0900

    update path of export files

--- nts.uk/uk.mobile/ClientApp/@app/components/common/index.ts
+++ nts.uk/uk.mobile/ClientApp/@app/components/common/index.ts
@@ -2,9 +2,9 @@ import './label';
 import './input';
 import './form-check';
 
-import '@app/components/common/text-editor';
-import '@app/components/common/text-area';
-import '@app/components/common/time-editor';
+import './text-editor';
+import './text-area';
+import './time-editor';
 
-import '@app/components/common/date-input';
-import '@app/components/common/dropdown';
+import './date-input';
+import './dropdown';
