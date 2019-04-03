82c0f5ab9ac1d35cbccde5a1b68208f598d2065a
Pham Van dan <dan_pv@nittsusystem-vn.com>
2019-04-03 15:23:16 +0900

    update list of common components

--- nts.uk/uk.mobile/ClientApp/@app/components/common/index.ts
+++ nts.uk/uk.mobile/ClientApp/@app/components/common/index.ts
@@ -5,6 +5,10 @@ import './form-check';
 import './text-editor';
 import './text-area';
 import './time-editor';
+import './number-editor'
 
 import './date-input';
 import './dropdown';
+
+import './switch-button-renderless';
+import './switch-button';
