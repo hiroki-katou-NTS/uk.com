e463eae1527efdef4d199827b89b340c62c66b75
nsvn-nguyenvanvuong <vuong_nv@nittsusystem-vn.com>
2019-04-03 16:30:07 +0700

    update service get validators

--- nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/infra/web/request/CorsPreflightFilter.java
+++ nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/infra/web/request/CorsPreflightFilter.java
@@ -43,10 +43,17 @@ public class CorsPreflightFilter implements Filter {
 			if (httpRequest.getMethod().equals(HttpMethod.OPTIONS)) {
 				return;
 			}
-		} else if (httpRequest.getHeader("origin") != null && httpRequest.getHeader("origin").contains("localhost:3000")) {
+		} else if (httpRequest.getHeader("origin") != null && (httpRequest.getHeader("origin").contains("localhost:3000") || httpRequest.getHeader("origin").contains("0.0.0.0:3000"))) {
+			
 			/** TODO: this code just for DEV mode, when deploy mobile in same wildfly with other web pack, need to remove this */
 			val httpResponse =(HttpServletResponse) response;
-			httpResponse.addHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+			
+			if(httpRequest.getHeader("origin").contains("localhost:3000")) {
+				httpResponse.addHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+			} else {
+				httpResponse.addHeader("Access-Control-Allow-Origin", "http://0.0.0.0:3000");				
+			}
+			
 			httpResponse.addHeader("Access-Control-Allow-Headers", "X-Requested-With, origin, content-type, accept, authorization, PG-Path, X-CSRF-TOKEN, MOBILE");
 			httpResponse.addHeader("Access-Control-Allow-Credentials", "true");
 			httpResponse.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
