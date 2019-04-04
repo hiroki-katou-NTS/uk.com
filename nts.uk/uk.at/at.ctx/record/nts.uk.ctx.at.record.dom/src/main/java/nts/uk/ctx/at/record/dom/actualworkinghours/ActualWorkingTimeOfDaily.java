4771ab693963009b293b909f4a80c13c54d080ea
NSVN-NguyenXuanThanh <thanh_nx@nittsusystem-vn.com>
2019-04-04 18:02:28 +0700

    fix bug 107083v2

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/actualworkinghours/ActualWorkingTimeOfDaily.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/actualworkinghours/ActualWorkingTimeOfDaily.java
@@ -20,7 +20,6 @@ import nts.uk.ctx.at.record.dom.daily.breaktimegoout.BreakTimeOfDaily;
 import nts.uk.ctx.at.record.dom.dailyprocess.calc.CheckExcessAtr;
 import nts.uk.ctx.at.record.dom.dailyprocess.calc.IntegrationOfDaily;
 import nts.uk.ctx.at.record.dom.dailyprocess.calc.ManageReGetClass;
-import nts.uk.ctx.at.record.dom.dailyprocess.calc.OverTimeFrameTime;
 import nts.uk.ctx.at.record.dom.dailyprocess.calc.PredetermineTimeSetForCalc;
 import nts.uk.ctx.at.record.dom.dailyprocess.calc.VacationClass;
 import nts.uk.ctx.at.record.dom.dailyprocess.calc.converter.DailyRecordToAttendanceItemConverter;
@@ -35,6 +34,7 @@ import nts.uk.ctx.at.record.dom.workrecord.errorsetting.SystemFixedErrorAlarm;
 import nts.uk.ctx.at.shared.dom.calculation.holiday.kmk013_splitdomain.DeductLeaveEarly;
 import nts.uk.ctx.at.shared.dom.calculation.holiday.time.OverTimeFrame;
 import nts.uk.ctx.at.shared.dom.common.time.AttendanceTime;
+import nts.uk.ctx.at.shared.dom.common.time.AttendanceTimeOfExistMinus;
 import nts.uk.ctx.at.shared.dom.common.time.TimeSpanForCalc;
 import nts.uk.ctx.at.shared.dom.vacation.setting.compensatoryleave.CompensatoryOccurrenceSetting;
 import nts.uk.ctx.at.shared.dom.workingcondition.WorkingConditionItem;
@@ -293,9 +293,9 @@ public class ActualWorkingTimeOfDaily {
 			}
 			
 			nts.uk.ctx.at.record.dom.divergencetimeofdaily.DivergenceTime obj = new nts.uk.ctx.at.record.dom.divergencetimeofdaily.DivergenceTime(
-					new AttendanceTime(0),
-					new AttendanceTime(0),
-					new AttendanceTime(0),
+					new AttendanceTimeOfExistMinus(0),
+					new AttendanceTimeOfExistMinus(0),
+					new AttendanceTimeOfExistMinus(0),
 					div_index,
 					reasonContent == null ? null : new DivergenceReasonContent(reasonContent),
 					reasonCode == null ? null : new DiverdenceReasonCode(reasonCode));
@@ -323,9 +323,9 @@ public class ActualWorkingTimeOfDaily {
 											else {
 												totalTime = calcDivergenceNo8910(tdi,integrationOfDailyInDto,breakTimeSheets,calcResultOotsuka);
 											}
-											returnList.add(new nts.uk.ctx.at.record.dom.divergencetimeofdaily.DivergenceTime(new AttendanceTime(totalTime - deductionTime), 
+											returnList.add(new nts.uk.ctx.at.record.dom.divergencetimeofdaily.DivergenceTime(new AttendanceTimeOfExistMinus(totalTime - deductionTime), 
 													tdi.getDeductionTime(), 
-													new AttendanceTime(totalTime), 
+													new AttendanceTimeOfExistMinus(totalTime), 
 													tdi.getDivTimeId(), 
 											 		tdi.getDivReason(), 
 											 		tdi.getDivResonCode()));
