6202f49ae46b71be59096622f281f8a95c7c1ec0
nsvn-nguyenvanvuong <vuong_nv@nittsusystem-vn.com>
2019-04-10 11:24:42 +0700

    update floating button documents

--- nts.uk/uk.mobile/ClientApp/views/documents/directives/float-button/index.vue
+++ nts.uk/uk.mobile/ClientApp/views/documents/directives/float-button/index.vue
@@ -1,7 +1,26 @@
 <template>
-    <div class="documentsdirectivesfloat-button">
-        <h5>1. {{'sample' | i18n}}</h5>
-
-        <markdown />
+  <div class="documentsdirectivesfloat-button">
+    <div class="fixed-action-btn" v-float-action>
+      <ul>
+        <li class="btn-info">
+          <span>{{'vote_app' | i18n}}</span>
+          <i class="fas fa-star"></i>
+        </li>
+        <li class="btn-primary">
+          <span>{{'manager_users' | i18n}}</span>
+          <i class="fas fa-user"></i>
+        </li>
+        <li class="btn-success">
+          <span>{{'go_to_mail' | i18n}}</span>
+          <i class="fas fa-envelope"></i>
+        </li>
+        <li class="btn-warning">
+          <span>{{'go_to_cart' | i18n}}</span>
+          <i class="fas fa-shopping-cart"></i>
+        </li>
+      </ul>
     </div>
+
+    <markdown/>
+  </div>
 </template>
