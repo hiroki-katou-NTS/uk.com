b6f4dfc500231120f44566ba9d85289e933c2db6
nsvn-nguyenvanvuong <vuong_nv@nittsusystem-vn.com>
2019-04-10 11:05:41 +0700

    label documents

--- nts.uk/uk.mobile/ClientApp/views/documents/controls/label/content/vi.md
+++ nts.uk/uk.mobile/ClientApp/views/documents/controls/label/content/vi.md
@@ -1,31 +1,20 @@
-##### 2. Explaint
-> Sample quote
-
-- First item list
-- Second item list
-
-**HTML Code:**
+##### 2. Mã html
+> `nts-label` là view control được sử dụng để bind `name` và `constraint` của một `model` lên view để người sử dụng có thể nhận biết một cách trực quan các thông tin cần nhập vào `model` thông qua các input control.
+> <br />Có một số dạng `label` mà `nts-label` hỗ trợ như ví dụ dưới đây.
+-----
+###### 2.1 Không có ràng buộc
 ```html
-<div class="sample">
-    <span>Sample html code</span>
-</div>
+<nts-label>{{'not_required_label' | i18n}}</nts-label>
 ```
+###### 2.2 Có ràng buộc
 
-**Typescript code:**
-```typescript
-class ClassName {
-    constructor() {
-        // sample contructor
-    }
-
-    choose() {
-        // sample method
-    }
-}
+> Thêm `v-bind:constraint` vào control, giá trị nhận vào là một `object`, có thể lấy các `constraint` này trực tiếp từ `validations` được khai báo trên [`decorator`](/nts.uk.mobile.web/documents/component/basic).
+```html
+<nts-label v-bind:constraint="{required: true}">{{'required_label' | i18n}}</nts-label>
 ```
-
-##### 3. API
-
-id | name | content
-----|----|------
-id | name | content
+###### 2.3 Có ràng buộc (chế độ 1 dòng)
+> Để hiển thị một label có `name` & `constraint` dưới dạng `inline` (một dòng), chỉ cần thêm `class`: `control-label-inline` vào như một `attribute` của control theo ví dụ dưới đây.
+```html
+<nts-label v-bind:constraint="{required: true, min: 100, max: 1000}" class="control-label-inline">{{'sample_label' | i18n}}</nts-label>
+```
+###### 2.4 Tài liệu khác
--- nts.uk/uk.mobile/ClientApp/views/documents/controls/label/index.vue
+++ nts.uk/uk.mobile/ClientApp/views/documents/controls/label/index.vue
@@ -1,7 +1,47 @@
 <template>
-    <div class="documentscontrolslabel">
-        <h5>1. {{'sample' | i18n}}</h5>
-
-        <markdown />
+  <div class="documentscontrolslabel">
+    <h5>1. {{'sample' | i18n}}</h5>
+    <div class="row mb-2">
+      <div class="col-md-2">
+        <nts-label v-bind:constraint="{required: true}">{{'required_label' | i18n}}</nts-label>
+      </div>
+      <div class="col-md-2">
+        <nts-label>{{'not_required_label' | i18n}}</nts-label>
+      </div>
+      <div class="col-md-2">
+        <nts-label
+          v-bind:constraint="{required: true, min: 100, max: 1000}"
+        >{{'sample_label' | i18n}}</nts-label>
+      </div>
+      <div class="col-md-2">
+        <nts-label
+          v-bind:constraint="{required: false, min: 100, max: 1000}"
+        >{{'sample_label' | i18n}}</nts-label>
+      </div>
+      <div class="col-md-2">
+        <nts-label
+          v-bind:constraint="{required: true, min: 100, max: 1000}"
+          class="control-label-inline"
+        >{{'sample_label' | i18n}}</nts-label>
+      </div>
+      <div class="col-md-2">
+        <nts-label
+          v-bind:constraint="{required: false, min: 100, max: 1000}"
+          class="control-label-inline"
+        >{{'sample_label' | i18n}}</nts-label>
+      </div>
     </div>
+
+    <markdown/>
+    <router-link to="/documents/html/label">{{'raw_label_html' | i18n}}</router-link>
+    <br>
+    <br>
+    <br>
+    <blockquote>
+      <p>
+        Document write by:
+        <strong>Nguyen Van Vuong</strong>
+      </p>
+    </blockquote>
+  </div>
 </template>
