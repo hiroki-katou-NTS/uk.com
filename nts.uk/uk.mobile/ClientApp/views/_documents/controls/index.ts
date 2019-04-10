ef4dfa2211c2ac6d970218d42d883dbfd22bde01
nsvn-nguyenvanvuong <vuong_nv@nittsusystem-vn.com>
2019-04-10 11:21:41 +0700

    delete old controls documnent

--- nts.uk/uk.mobile/ClientApp/views/_documents/controls/index.ts
+++
@@ -1,9 +0,0 @@
-import './label';
-import './form-check';
-
-import './number-editor'
-import './text-editor';
-import './time-editor';
-import './textarea';
-import './date-input';
-import './dropdown';
