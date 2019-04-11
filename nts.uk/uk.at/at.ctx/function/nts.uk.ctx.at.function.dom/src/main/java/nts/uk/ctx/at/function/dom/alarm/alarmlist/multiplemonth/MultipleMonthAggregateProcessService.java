1d6190892a0b03e0745e908adc5ad0226ac6df63
NSVN-TrieuKhacTu <tu_tk@nittsusystem-vn.com>
2019-04-11 10:43:41 +0700

    fixbug kal001 : #107138

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/alarm/alarmlist/multiplemonth/MultipleMonthAggregateProcessService.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/alarm/alarmlist/multiplemonth/MultipleMonthAggregateProcessService.java
@@ -122,7 +122,7 @@ public class MultipleMonthAggregateProcessService {
 		parallelManager.forEach(CollectionUtil.partitionBySize(employees, 100), emps -> {
 			List<String> employeeIds = emps.stream().map(e -> e.getId()).collect(Collectors.toList());
 
-			Map<String, List<RegulationInfoEmployeeResult>> listTargetMap = erAlWorkRecordCheckAdapter.filterEmployees(new DatePeriod(period.end(),period.end()), 
+			Map<String, List<RegulationInfoEmployeeResult>> listTargetMap = erAlWorkRecordCheckAdapter.filterEmployees(period, 
 																														employeeIds, 
 																														multiMonthErAl.stream().map(c -> c.getExtractTargetCondition()).collect(Collectors.toList()));
 			
