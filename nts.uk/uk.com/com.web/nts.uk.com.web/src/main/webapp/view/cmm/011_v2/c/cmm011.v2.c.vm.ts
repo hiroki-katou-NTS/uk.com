46e9b05ab2c994d2d112dcb38d9b56b2e61d3468
An <duongvanan97x@gmail.com>
2019-04-17 16:07:44 +0700

    uppdate cmm011 screen c

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmm/011_v2/c/cmm011.v2.c.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmm/011_v2/c/cmm011.v2.c.vm.ts
@@ -33,13 +33,17 @@ module nts.uk.com.view.cmm011.v2.c.viewmodel {
             //Grid list item
             self.inputList = ko.observableArray([]);
 
-            self.inputList.push(new InputItems("targetID1", "A", "カラム1", "2018/04/01", "histryID1"));
-            self.inputList.push(new InputItems("targetID2", "B", "カラム2", "2018/04/02", "histryID2"));
+            self.inputList.push(new InputItems("001","targetID1", "A", "カラム1", "2018/04/01", "histryID1"));
+            self.inputList.push(new InputItems("002","targetID2", "A", "カラム2", "2018/04/02", "histryID2"));
+            _.sortBy(self.inputList(),[function (x) {
+                return -x.date;
+            }])
             //Grid list columns
             self.listColums = ko.observableArray([
-                {headerText: getText('CMM011-0_30'), key: 'code', width: 100},
+                {headerText: 'id' ,key: 'id', hidden: true},
+                {headerText: getText('CMM011-0_30'), key: 'code', width: 100, formatter: _.escape},
                 {headerText: getText('CMM011-0_31'), key: 'name', width: 100, formatter: _.escape},
-                {headerText: getText('CMM011-0_32'), key: 'date', width: 150, formatter: _.escape}
+                {headerText: getText('CMM011-0_32'), key: 'date', width: 150, format: 'YYYY/MM/DD', formatter: _.escape}
             ]);
             this.currentCode = ko.observable();
             //get param
@@ -63,7 +67,7 @@ module nts.uk.com.view.cmm011.v2.c.viewmodel {
             }
             else {
                 if (self.currentCode()) {
-                    var item = _.find(self.inputList(), x => x.code == self.currentCode());
+                    var item = _.find(self.inputList(), x => x.id == self.currentCode());
                     setShared("cmm001v2Output", {
                         targetID: item.targetID,
                         historyID: item.historyID
@@ -94,13 +98,15 @@ module nts.uk.com.view.cmm011.v2.c.viewmodel {
     }
 
     class InputItems {
+        id:string;
         targetID: string;
         code: string;
         name: string;
         date: string;
         historyID: string;
 
-        constructor(targetID: string, code: string, name: string, date: string, historyID: string) {
+        constructor(id:string, targetID: string, code: string, name: string, date: string, historyID: string) {
+            this.id=id;
             this.targetID = targetID;
             this.code = code;
             this.name = name;
