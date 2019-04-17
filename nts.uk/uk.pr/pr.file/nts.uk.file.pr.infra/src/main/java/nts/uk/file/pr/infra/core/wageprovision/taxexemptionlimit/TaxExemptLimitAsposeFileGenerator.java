6d44500d004ae4adac45d4a8419414bd431e25b3
Nguyen Xuan Tam <tamnguyenxuan25@gmail.com>
2019-04-17 08:45:25 +0700

    Export PDF

--- nts.uk/uk.pr/pr.file/nts.uk.file.pr.infra/src/main/java/nts/uk/file/pr/infra/core/wageprovision/taxexemptionlimit/TaxExemptLimitAsposeFileGenerator.java
+++ nts.uk/uk.pr/pr.file/nts.uk.file.pr.infra/src/main/java/nts/uk/file/pr/infra/core/wageprovision/taxexemptionlimit/TaxExemptLimitAsposeFileGenerator.java
@@ -26,7 +26,7 @@ public class TaxExemptLimitAsposeFileGenerator extends AsposeCellsReportGenerato
 
     private static final String TEMPLATE_FILE = "report/QMM023.xlsx";
 
-    private static final String REPORT_FILE_NAME = "QMM023-非課税限度額の登録.xlsx";
+    private static final String REPORT_FILE_NAME = "QMM023-非課税限度額の登録.pdf";
 
     private static final int COLUMN_START_1= 1;
     private static final int COLUMN_START_2 = 5;
@@ -96,7 +96,7 @@ public class TaxExemptLimitAsposeFileGenerator extends AsposeCellsReportGenerato
                 }
             }
             reportContext.processDesigner();
-            reportContext.saveAsExcel(this.createNewFile(fileContext, this.getReportName(REPORT_FILE_NAME)));
+            reportContext.saveAsPdf(this.createNewFile(fileContext, this.getReportName(REPORT_FILE_NAME)));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
