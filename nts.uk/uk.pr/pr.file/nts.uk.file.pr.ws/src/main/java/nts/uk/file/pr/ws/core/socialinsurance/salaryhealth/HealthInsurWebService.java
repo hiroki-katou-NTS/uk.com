b9dc8b23a7be441879f7b1c80f69fd3236666225
Phuc.TC_3S <35018596+tranphuc7686@users.noreply.github.com>
2019-04-17 18:04:43 +0700

    Update code

--- nts.uk/uk.pr/pr.file/nts.uk.file.pr.ws/src/main/java/nts/uk/file/pr/ws/core/socialinsurance/salaryhealth/HealthInsurWebService.java
+++ nts.uk/uk.pr/pr.file/nts.uk.file.pr.ws/src/main/java/nts/uk/file/pr/ws/core/socialinsurance/salaryhealth/HealthInsurWebService.java
@@ -1,6 +1,8 @@
 package nts.uk.file.pr.ws.core.socialinsurance.salaryhealth;
 
 import nts.arc.layer.app.file.export.ExportServiceResult;
+import nts.uk.ctx.pr.file.app.core.socialinsurance.salaryhealth_healthinsur.SalaryHealthInsurExportQuery;
+import nts.uk.ctx.pr.file.app.core.socialinsurance.salaryhealth_healthinsur.SalaryHealthInsurExportService;
 /*import nts.uk.ctx.pr.file.app.core.socialinsurance.salaryhealth_healthinsur.SalaryHealthInsurExportQuery;
 import nts.uk.ctx.pr.file.app.core.socialinsurance.salaryhealth_healthinsur.SalaryHealthInsurExportService;*/
 
@@ -12,12 +14,12 @@ import javax.ws.rs.Produces;
 @Path("file/core/socialinsurance/salaryhealth")
 @Produces("application/json")
 public class HealthInsurWebService {
-    /*@Inject
+    @Inject
     private SalaryHealthInsurExportService mSalaryHealthInsurExportService;
 
     @POST
     @Path("exportHealthInsur")
     public ExportServiceResult generate(SalaryHealthInsurExportQuery query) {
         return this.mSalaryHealthInsurExportService.start(query);
-    }*/
+    }
 }
