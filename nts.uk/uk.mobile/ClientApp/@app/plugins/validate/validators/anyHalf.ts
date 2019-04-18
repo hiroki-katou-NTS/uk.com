5183612d4eb30c598ab0f2d2e37e598be4fffc45
nsvn-nguyenvanvuong <vuong_nv@nittsusystem-vn.com>
2019-04-18 14:10:53 +0700

    remove not use validators

--- nts.uk/uk.mobile/ClientApp/@app/plugins/validate/validators/anyHalf.ts
+++
@@ -1,7 +0,0 @@
-import { text } from '@app/utils/text'
-
-const anyHalf = function (value: string, check: boolean) {
-    return value.length === text.countHalf(value) ? null : 'MsgB_4';
-};
-
-export { anyHalf };
