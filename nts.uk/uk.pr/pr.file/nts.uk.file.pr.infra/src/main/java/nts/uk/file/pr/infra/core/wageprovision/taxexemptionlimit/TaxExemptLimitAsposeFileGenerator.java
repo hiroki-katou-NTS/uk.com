d7fc7971c82edb7b691e91015f38fe1e90bd0881
Phuc.TC_3S <35018596+tranphuc7686@users.noreply.github.com>
2019-04-18 16:31:37 +0700

    Update code

--- nts.uk/uk.pr/pr.file/nts.uk.file.pr.infra/src/main/java/nts/uk/file/pr/infra/core/wageprovision/taxexemptionlimit/TaxExemptLimitAsposeFileGenerator.java
+++ nts.uk/uk.pr/pr.file/nts.uk.file.pr.infra/src/main/java/nts/uk/file/pr/infra/core/wageprovision/taxexemptionlimit/TaxExemptLimitAsposeFileGenerator.java
@@ -45,7 +45,7 @@ public class TaxExemptLimitAsposeFileGenerator extends AsposeCellsReportGenerato
             // Company name
             String companyName = this.company.getCurrentCompany().map(CompanyInfor::getCompanyName).orElse("");
             PageSetup pageSetup = ws.getPageSetup();
-            pageSetup.setHeader(0, "&8&\"MS ゴシック\"" + companyName);
+            pageSetup.setHeader(0, "&10&\"MS ゴシック\"" + companyName);
 
             // Output item name
             pageSetup.setHeader(1, "&16&\"MS ゴシック\"" + "非課税限度額の登録");
