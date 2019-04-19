1380cafa57927ca6212d00496ea734a85f4e431e
nsvn-nguyenvanvuong <vuong_nv@nittsusystem-vn.com>
2019-04-19 10:03:00 +0700

    update validators

--- nts.uk/uk.mobile/ClientApp/@app/plugins/validate/validators/charType.ts
+++ nts.uk/uk.mobile/ClientApp/@app/plugins/validate/validators/charType.ts
@@ -7,10 +7,11 @@ export const charType = function (value: string, type: 'Any' | 'Kana' | 'AnyHalf
     switch (type) {
         case "Any":
             break;
-        case "Kana":
-            if (!text.is.fullKatakanaReg(value)) {
-                return ["MsgB_7", cl];
+        case "AnyHalfWidth":
+            if (!text.is.halfAlphabet(value)) {
+                return ["MsgB_4", cl];
             }
+
             break;
         case "Numeric":
             if (!text.is.halfNumeric(value)) {
@@ -24,12 +25,13 @@ export const charType = function (value: string, type: 'Any' | 'Kana' | 'AnyHalf
             }
 
             break;
-        case "AnyHalfWidth":
-            if (!text.is.halfAlphabet(value)) {
-                return ["MsgB_4", cl];
+        case "Kana":
+            if (!text.is.fullKatakanaReg(value)) {
+                return ["MsgB_7", cl];
             }
+
             break;
     }
-    
+
     return null;
 }
--- nts.uk/uk.mobile/ClientApp/@app/plugins/validate/validators/index.ts
+++ nts.uk/uk.mobile/ClientApp/@app/plugins/validate/validators/index.ts
@@ -1,5 +1,4 @@
 export * from '@app/plugins/validate/validators/required';
-//export * from '@app/plugins/validate/validators/between';
 export * from '@app/plugins/validate/validators/min';
 export * from '@app/plugins/validate/validators/max';
 export * from '@app/plugins/validate/validators/mantissaMaxLength';
