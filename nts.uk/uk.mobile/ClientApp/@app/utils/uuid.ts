b1a2a16973b47b1a4315d2560ae2461f42bd0fbe
nsvn-nguyenvanvuong <vuong_nv@nittsusystem-vn.com>
2019-05-07 11:00:55 +0700

    add domid util

--- nts.uk/uk.mobile/ClientApp/@app/utils/uuid.ts
+++ nts.uk/uk.mobile/ClientApp/@app/utils/uuid.ts
@@ -7,6 +7,9 @@ const $uuid = {
             // tslint:disable-next-line: no-bitwise
             return ((c == 'x') ? r : (r & 0x3 | 0x8)).toString(16);
         });
+    },
+    get domId() {
+        return Math.random().toString(36).slice(2, 12);
     }
 };
 
