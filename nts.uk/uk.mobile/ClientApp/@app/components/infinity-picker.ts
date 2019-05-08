cba48bab6b9b2825db45de6ed2cb041ed99f1419
nsvn-nguyenvanvuong <vuong_nv@nittsusystem-vn.com>
2019-05-08 15:37:47 +0700

    update picker (change to infinity picker)

--- nts.uk/uk.mobile/ClientApp/@app/components/infinity-picker.ts
+++ nts.uk/uk.mobile/ClientApp/@app/components/infinity-picker.ts
@@ -6,7 +6,7 @@ interface IRange {
     new: number;
 }
 
-@component({
+export const InfinityPicker = {
     template: `<div class="ipkr-col" v-on:touchmove="preventScroll">
         <div class="ipkr-viewport"
                 v-on:touchstart="gearTouchStart"
@@ -21,252 +21,242 @@ interface IRange {
                 </ul>
             </div>
         </div>
-    </div>`
-})
-export class InfinityPickerComponent extends Vue {
-    private interval: number = 0;
-    private time: IRange = { old: 0, new: 0 };
-    private range: IRange = { old: 0, new: 0 };
-    private position: IRange = { old: 0, new: 0 };
-
-    @Prop({ default: () => undefined })
-    private readonly optionValue!: string | undefined;
-
-    @Prop({ default: () => undefined })
-    private readonly optionText!: string | undefined;
-
-    @Prop({ default: () => undefined })
-    private readonly value!: string | number;
-
-    @Prop({ default: () => [] })
-    private readonly dataSources!: Array<string | number | { [key: string]: any; }>;
-
-    public get $nindex() {
-        let self = this,
-            select = self.value,
-            dataSources = self.dataSources
-                .map((option) => self.optionValue ? option[self.optionValue] : option),
-            index = dataSources.indexOf(select),
-            mgt = self.$mtNumber,
-            move = Math.floor(mgt / 40),
-            nidex = index + move;
-
-        nidex = Math.floor(nidex % dataSources.length);
-
-        if (dataSources.length < 5) {
-            if (nidex < 0) {
-                nidex = 0;
-            } else if (nidex > dataSources.length - 1) {
-                nidex = dataSources.length - 1;
-            }
-        } else {
-            nidex = Math.abs(dataSources.length + nidex);
+    </div>`,
+    data: () => ({
+        interval: 0,
+        time: {
+            old: 0,
+            new: 0
+        },
+        range: {
+            old: 0,
+            new: 0
+        },
+        position: {
+            old: 0,
+            new: 0
         }
+    }),
+    props: ['optionValue', 'optionText', 'value', 'dataSources'],
+    computed: {
+        $nindex() {
+            let self = this,
+                select = self.value,
+                dataSources = self.dataSources
+                    .map((option) => self.optionValue ? option[self.optionValue] : option),
+                index = dataSources.indexOf(select),
+                mgt = self.$mtNumber,
+                move = Math.floor(mgt / 40),
+                nidex = index + move;
 
-        return Math.floor(nidex % dataSources.length);
-    }
+            nidex = Math.floor(nidex % dataSources.length);
 
-    public get items() {
-        let self = this,
-            nidex = self.$nindex,
-            dataSources = self.dataSources;
-
-        if (dataSources.length >= 5) {
-            let items: any[] = [];
-
-            for (let i = nidex - 2; i <= nidex + 2; i++) {
-                if (i < 0) {
-                    items.push(dataSources[Math.abs(dataSources.length + i)]);
-                } else if (i > dataSources.length - 1) {
-                    items.push(dataSources[Math.abs(dataSources.length - i)]);
-                } else {
-                    items.push(dataSources[i]);
+            if (dataSources.length < 5) {
+                if (nidex < 0) {
+                    nidex = 0;
+                } else if (nidex > dataSources.length - 1) {
+                    nidex = dataSources.length - 1;
                 }
+            } else {
+                nidex = Math.abs(dataSources.length + nidex);
             }
 
-            return items;
-        } else {
-            if (nidex === 0) {
-                let items: any[] = [undefined, undefined];
+            return Math.floor(nidex % dataSources.length);
+        },
+        items() {
+            let self = this,
+                nidex = self.$nindex,
+                dataSources = self.dataSources;
 
-                for (let i = 0; i <= 2; i++) {
-                    items.push(dataSources[i]);
-                }
-
-                return items;
-            } else if (nidex === dataSources.length - 1) {
-                let items: any[] = [undefined, undefined];
-
-                for (let i = nidex; i >= nidex - 2; i--) {
-                    items.unshift(dataSources[i]);
-                }
-
-                return items;
-            } else {
+            if (dataSources.length >= 5) {
                 let items: any[] = [];
 
                 for (let i = nidex - 2; i <= nidex + 2; i++) {
-                    if (i < 0 || i > dataSources.length - 1) {
-                        items.push(undefined);
+                    if (i < 0) {
+                        items.push(dataSources[Math.abs(dataSources.length + i)]);
+                    } else if (i > dataSources.length - 1) {
+                        items.push(dataSources[Math.abs(dataSources.length - i)]);
                     } else {
                         items.push(dataSources[i]);
                     }
                 }
 
                 return items;
-            }
-        }
-    }
-
-    get $mtNumber() {
-        let self = this,
-            rng = self.range,
-            mov: number = rng.new - rng.old,
-            amo: number = Math.abs(mov),
-            mup: boolean = rng.old > rng.new,
-            twt: boolean = (amo >= -60 && amo < -20) || (amo <= 60 && amo > 20);
-
-        if (mov >= -20 && mov <= 20) {
-            return 0;
-        }
-
-        if (mup) {
-            if (twt) {
-                return 40;
             } else {
-                return Math.floor(amo / 40) * 40;
-            }
-        } else {
-            if (twt) {
-                return -40;
-            } else {
-                return -Math.floor(amo / 40) * 40;
-            }
-        }
-    }
+                if (nidex === 0) {
+                    let items: any[] = [undefined, undefined];
 
-    get marginTop() {
-        return `${this.$mtNumber}px`;
-    }
+                    for (let i = 0; i <= 2; i++) {
+                        items.push(dataSources[i]);
+                    }
 
-    get transform() {
-        let self = this,
-            rng = self.range;
+                    return items;
+                } else if (nidex === dataSources.length - 1) {
+                    let items: any[] = [undefined, undefined];
 
-        return `translate3d(0px, ${rng.new - rng.old - 80}px, 0px)`;
-    }
+                    for (let i = nidex; i >= nidex - 2; i--) {
+                        items.unshift(dataSources[i]);
+                    }
 
-    public gearTouchStart(evt: TouchEvent) {
-        let self = this;
-
-        evt.preventDefault();
-        evt.stopPropagation();
-
-        clearInterval(self.interval);
-
-        // if continue touch, emit current select item
-        if (self.value !== self.dataSources[self.$nindex]) {
-            self.emitInput();
-        }
-
-        self.time.old = new Date().getTime();
-        self.position.old = Math.floor(evt.changedTouches[0].clientY);
-
-        self.range.old = self.range.new = Math.floor(evt.changedTouches[0].clientY);
-    }
-
-    public gearTouchMove(evt: TouchEvent) {
-        let self = this;
-
-        evt.preventDefault();
-        evt.stopPropagation();
-
-        self.time.new = new Date().getTime();
-        self.range.new = self.position.new = Math.floor(evt.changedTouches[0].clientY);
-
-        if (evt.targetTouches[0].screenY < 1) {
-            self.gearTouchEnd(evt);
-        }
-    }
-
-    public gearTouchEnd(evt: TouchEvent) {
-        let self = this,
-            time = self.time,
-            position = self.position;
-
-        evt.preventDefault();
-        evt.stopPropagation();
-
-        let gear: number = 0,
-            flag = (position.new - position.old) / (time.new - time.old);
-
-        if (Math.abs(flag) < 1) {
-            gear = 0;
-        } else if (Math.abs(flag) <= 2) {
-            gear = 20;
-        } else {
-            gear = 40;
-        }
-
-        if (self.dataSources.length >= 5) {
-            self.roleGear(gear);
-        } else {
-            self.emitInput();
-        }
-    }
-
-    private roleGear(gear: number) {
-        let self = this,
-            d: number = 0,
-            range: IRange = self.range,
-            position: IRange = self.position;
-
-        clearInterval(self.interval);
-
-        self.interval = setInterval(() => {
-            let rng = range.new - range.old,
-                speed = Math.floor(Math.abs(gear * Math.exp(-0.03 * d)));
-
-            if (gear !== 0) {
-                if (position.new > position.old) {
-                    range.new += speed < 1 ? 1 : speed;
+                    return items;
                 } else {
-                    range.new -= speed < 1 ? 1 : speed;
+                    let items: any[] = [];
+
+                    for (let i = nidex - 2; i <= nidex + 2; i++) {
+                        if (i < 0 || i > dataSources.length - 1) {
+                            items.push(undefined);
+                        } else {
+                            items.push(dataSources[i]);
+                        }
+                    }
+
+                    return items;
                 }
             }
+        },
+        $mtNumber() {
+            let self = this,
+                rng = self.range,
+                mov: number = rng.new - rng.old,
+                amo: number = Math.abs(mov),
+                mup: boolean = rng.old > rng.new,
+                twt: boolean = (amo >= -60 && amo < -20) || (amo <= 60 && amo > 20);
 
-            if (Math.abs(speed) < 1 && (gear === 0 || Math.abs(rng) % 40 === 0)) {
-                self.emitInput();
-
-                clearInterval(self.interval);
+            if (mov >= -20 && mov <= 20) {
+                return 0;
             }
 
-            d++;
-        }, 30);
-    }
+            if (mup) {
+                if (twt) {
+                    return 40;
+                } else {
+                    return Math.floor(amo / 40) * 40;
+                }
+            } else {
+                if (twt) {
+                    return -40;
+                } else {
+                    return -Math.floor(amo / 40) * 40;
+                }
+            }
+        },
+        marginTop() {
+            return `${this.$mtNumber}px`;
+        },
+        transform() {
+            let self = this,
+                rng = self.range;
 
-    private emitInput() {
-        let self = this,
-            index = self.$nindex,
-            ov = self.optionValue,
-            opt = self.dataSources[index];
+            return `translate3d(0px, ${rng.new - rng.old - 80}px, 0px)`;
+        }
+    },
+    methods: {
+        gearTouchStart(evt: TouchEvent) {
+            let self = this;
 
-        self.$emit('input', ov ? opt[ov] : opt);
-    }
+            evt.preventDefault();
+            evt.stopPropagation();
 
-    public preventScroll(evt: TouchEvent) {
-        evt.preventDefault();
-        evt.stopPropagation();
-    }
+            clearInterval(self.interval);
 
-    public created() {
+            // if continue touch, emit current select item
+            if (self.value !== self.dataSources[self.$nindex]) {
+                self.emitInput();
+            }
+
+            self.time.old = new Date().getTime();
+            self.position.old = Math.floor(evt.changedTouches[0].clientY);
+
+            self.range.old = self.range.new = Math.floor(evt.changedTouches[0].clientY);
+        },
+        gearTouchMove(evt: TouchEvent) {
+            let self = this;
+
+            evt.preventDefault();
+            evt.stopPropagation();
+
+            self.time.new = new Date().getTime();
+            self.range.new = self.position.new = Math.floor(evt.changedTouches[0].clientY);
+
+            if (evt.targetTouches[0].screenY < 1) {
+                self.gearTouchEnd(evt);
+            }
+        },
+        gearTouchEnd(evt: TouchEvent) {
+            let self = this,
+                time = self.time,
+                position = self.position;
+
+            evt.preventDefault();
+            evt.stopPropagation();
+
+            let gear: number = 0,
+                flag = (position.new - position.old) / (time.new - time.old);
+
+            if (Math.abs(flag) < 1) {
+                gear = 0;
+            } else if (Math.abs(flag) <= 2) {
+                gear = 20;
+            } else {
+                gear = 40;
+            }
+
+            if (self.dataSources.length >= 5) {
+                self.roleGear(gear);
+            } else {
+                self.emitInput();
+            }
+        },
+        roleGear(gear: number) {
+            let self = this,
+                d: number = 0,
+                range: IRange = self.range,
+                position: IRange = self.position;
+
+            clearInterval(self.interval);
+
+            self.interval = setInterval(() => {
+                let rng = range.new - range.old,
+                    speed = Math.floor(Math.abs(gear * Math.exp(-0.03 * d)));
+
+                if (gear !== 0) {
+                    if (position.new > position.old) {
+                        range.new += speed < 1 ? 1 : speed;
+                    } else {
+                        range.new -= speed < 1 ? 1 : speed;
+                    }
+                }
+                
+                if (Math.abs(speed) < 1 && (gear === 0 || Math.abs(rng) % 40 === 0)) {
+                    self.emitInput();
+
+                    clearInterval(self.interval);
+                }
+
+                d++;
+            }, 30);
+        },
+        emitInput() {
+            let self = this,
+                index = self.$nindex,
+                ov = self.optionValue,
+                opt = self.dataSources[index];
+
+            self.$emit('input', ov ? opt[ov] : opt);
+        },
+        preventScroll(evt: TouchEvent) {
+            evt.preventDefault();
+            evt.stopPropagation();
+        }
+    },
+    created() {
         let self = this,
             time: IRange = self.time,
             range: IRange = self.range,
             position: IRange = self.position;
 
         // reset position of gear
-        self.$on('input', () => {
+        self.$on('input', (value: any) => {
             time.new = 0;
             time.old = 0;
 
@@ -277,4 +267,4 @@ export class InfinityPickerComponent extends Vue {
             position.old = 0;
         });
     }
-}
+};
--- nts.uk/uk.mobile/ClientApp/@app/components/picker.ts
+++ nts.uk/uk.mobile/ClientApp/@app/components/picker.ts
@@ -1,46 +1,29 @@
-/*!
- * Copyright 2018
- */
-
 import { Vue } from '@app/provider';
 import { obj, dom } from '@app/utils';
 
+import { InfinityPicker } from './infinity-picker';
+
 export const MobilePicker = {
+    components: {
+        'ipkr': InfinityPicker
+    },
     template: `<transition name="picker-fade" v-on:after-leave="destroyPicker">
-        <div class="picker-container" v-show="show" v-on:touchmove="preventScroll">
-            <div class="pk-btn-groups">
-                <button class="btn btn-link" ref="close" v-on:click="close">{{'cancel' | i18n}}</button>
-                <div>
-                    <button class="btn btn-link mr-4" v-bind:disabled="!deleteAble" v-on:click="remove">{{'delete' | i18n}}</button>
-                    <button class="btn btn-link" v-on:click="finish">{{'accept' | i18n}}</button>
-                </div>
-            </div>
-            <div v-if="title" class="picker_title p-1">
-                {{ title | i18n }}:
-            </div>
-            <div v-else data-comment="Title of picker"></div>
-            <div class="picker_mask" v-on:touchmove="preventScroll">
-                <div class="picker_roll" v-on:touchmove="preventScroll">
-                <div class="picker-column" v-for="(cols, idx) in dataSources" v-bind:key="idx">
-                    <div
-                    class="gear"
-                    v-bind:id="idx"
-                    v-bind:ref="idx"
-                    v-on:touchstart="gearTouchStart"
-                    v-on:touchmove="gearTouchMove"
-                    v-on:touchend="gearTouchEnd"
-                    >
-                    <div
-                        class="tooth"
-                        v-for="(item, index) in cols"
-                        v-bind:key="index"
-                        v-bind:class="{ disabled: item.disabled }"
-                    >{{item[textField] | i18n}}</div>
-                    </div>
-                    <div class="area_grid"></div>
-                </div>
-                </div>
-            </div>
+        <div class="infinity-picker android centered" v-show="show" v-on:touchmove="preventScroll">
+        <div class="ipkr-caption" v-if="title">
+            <div class="ipkr-caption-item">{{title}}</div>
+        </div>
+        <div class="ipkr-content">
+            <ipkr v-for="(cols, idx) in dataSources" v-bind:key="idx"
+                v-model="selects[idx]"
+                v-bind:data-sources="cols"
+                v-bind:option-text="options.text"
+                v-bind:option-value="options.value"
+                v-on:input="value => onInput(value, idx)" />
+        </div>
+        <div class="ipkr-navbar">
+            <a class="ipkr-navbar-btn" ref="close" v-on:click="close">{{'cancel' | i18n}}</a>
+            <a class="ipkr-navbar-btn" v-on:click="finish">{{'accept' | i18n}}</a>
+        </div>
         </div>
     </transition>`,
     props: {
@@ -73,7 +56,6 @@ export const MobilePicker = {
             immediate: true,
             handler(show: boolean) {
                 let self = this,
-                    refs = self.$refs,
                     opts = self.options,
                     defaultData = self.value;
 
@@ -81,37 +63,21 @@ export const MobilePicker = {
                     if (self.$mask) {
                         self.$mask('hide');
                     }
+
                     dom.removeClass(document.body, 'modal-open');
                 } else {
                     if (self.$mask) {
                         self.$mask('show', 0.01);
                     }
 
-                    refs.close && refs.close.focus();
-
                     dom.addClass(document.body, 'modal-open');
 
-                    self.$nextTick(() => {
-                        obj.objectForEach(self.dataSources, (key: string, items: any[]) => {
-                            let position = 0,
-                                columns = refs[key][0] as HTMLElement;
-
-                            if (defaultData[key] !== undefined) {
-                                self.selects[key] = defaultData[key];
-                            } else {
-                                self.selects[key] = items[0][opts.value];
-                            }
-
-                            for (let i = 0, len = items.length; i < len; i++) {
-                                if (defaultData[key] && items[i][opts.value] == defaultData[key]) {
-                                    position = -(i * 2);
-                                    break;
-                                }
-                            }
-
-                            columns.style.transform = columns.style['-webkit-transform'] = 'translate3d(0,' + position + 'em,0)';
-                            columns.setAttribute('top', position + 'em');
-                        });
+                    obj.objectForEach(self.dataSources, (key: string, items: any[]) => {
+                        if (defaultData[key] !== undefined) {
+                            self.selects[key] = defaultData[key];
+                        } else {
+                            self.selects[key] = items[0][opts.value];
+                        }
                     });
                 }
             }
@@ -121,29 +87,11 @@ export const MobilePicker = {
             immediate: true,
             handler(dataSources: any[]) {
                 let self = this,
-                    refs = self.$refs,
                     opts = self.options;
 
                 obj.objectForEach(dataSources, (key: string, items: any[]) => {
                     if (!self.selects[key]) {
                         self.selects[key] = self.value[key] || items[0][opts.value];
-                    } else if (!items.filter((f) => f[opts.value] === self.selects[key]).length) {
-                        let pos = refs[key][0].getAttribute('top'),
-                            index = Math.abs(Number(pos ? pos : 2) / 2);
-
-                        if (index <= items.length - 1) {
-                            self.selects[key] = items[0][opts.value];
-                        } else {
-                            self.selects[key] = items[items.length - 1][opts.value];
-                        }
-                    }
-
-                    let keys = items.map((m) => m[opts.value]),
-                        index = keys.indexOf(self.selects[key]);
-
-                    if (refs[key] && refs[key][0]) {
-                        refs[key][0].setAttribute('top', -index * 2);
-
                     }
                 });
             }
@@ -154,19 +102,24 @@ export const MobilePicker = {
             handler(value: { [key: string]: any }) {
                 let self = this;
 
-                self.$emit('select', value);
+                self.$emit('select', self.toJS(value));
             }
         }
     },
     computed: {
-        textField() {
-            return this.options.text;
-        },
         deleteAble() {
             return !this.options.required && !!obj.keys(obj.cloneObject(this.value)).length;
         }
     },
     methods: {
+        onInput(value: any, idx: string) {
+            let self = this,
+                selects = self.toJS(self.selects);
+
+            selects[idx] = value;
+
+            Vue.set(self, 'selects', selects);
+        },
         close() {
             this.show = false;
             this.$emit('close');
@@ -194,144 +147,6 @@ export const MobilePicker = {
             this.$nextTick(() => {
                 this.selects = {};
             });
-        },
-        gearTouchStart(evt: TouchEvent) {
-            evt.preventDefault();
-            evt.stopPropagation();
-
-            let target = evt.target as HTMLElement;
-
-            while (true) {
-                if (!target.classList.contains('gear')) {
-                    target = target.parentElement;
-                } else {
-                    break;
-                }
-            }
-
-            let top = target.getAttribute('top');
-
-            clearInterval(target['int_' + target.id]);
-            target['o_t_' + target.id] = new Date().getTime();
-            target['old_' + target.id] = evt.targetTouches[0].screenY;
-
-            if (!top) {
-                target['o_d_' + target.id] = 0;
-            } else {
-                target['o_d_' + target.id] = parseFloat(top.replace(/em/g, ''));
-            }
-        },
-        gearTouchMove(evt: TouchEvent) {
-            evt.preventDefault();
-            evt.stopPropagation();
-
-            let target = evt.target as HTMLElement;
-
-            while (true) {
-                if (!target.classList.contains('gear')) {
-                    target = target.parentElement;
-                } else {
-                    break;
-                }
-            }
-
-            target['new_' + target.id] = evt.targetTouches[0].screenY;
-            target['n_t_' + target.id] = (new Date()).getTime();
-
-            let f = (target['new_' + target.id] - target['old_' + target.id]) * 30 / window.innerHeight;
-
-            target['pos_' + target.id] = target['o_d_' + target.id] + f;
-            target.style['-webkit-transform'] = 'translate3d(0,' + target['pos_' + target.id] + 'em,0)';
-            target.setAttribute('top', target['pos_' + target.id] + 'em');
-
-            if (evt.targetTouches[0].screenY < 1) {
-                this.gearTouchEnd(evt);
-            }
-        },
-        gearTouchEnd(evt: TouchEvent) {
-            evt.preventDefault();
-            evt.stopPropagation();
-
-            let target = evt.target as HTMLElement;
-
-            while (true) {
-                let cls: any = target.classList;
-
-                if (cls.contains('gear')) {
-                    break;
-                } else {
-                    target = target.parentElement;
-                }
-            }
-
-            let flag = (target['new_' + target.id] - target['old_' + target.id]) / (target['n_t_' + target.id] - target['o_t_' + target.id]);
-
-            if (Math.abs(flag) <= 0.2) {
-                target['spd_' + target.id] = flag < 0 ? -0.08 : 0.08;
-            } else if (Math.abs(flag) <= 0.5) {
-                target['spd_' + target.id] = flag < 0 ? -0.16 : 0.16;
-            } else {
-                target['spd_' + target.id] = flag / 2;
-            }
-
-            if (!target['pos_' + target.id]) {
-                target['pos_' + target.id] = 0;
-            }
-
-            this.rollGear(target);
-        },
-        rollGear(target: HTMLElement) {
-            let d = 0,
-                self = this,
-                stopGear = false;
-
-            clearInterval(target['int_' + target.id]);
-
-            target['int_' + target.id] = setInterval(function () {
-                let position = target['pos_' + target.id],
-                    speed = target['spd_' + target.id] * Math.exp(-0.03 * d),
-                    minTop = -(self.dataSources[target.id].length - 1) * 2;
-
-                position += speed;
-
-                if (Math.abs(speed) <= 0.1) {
-                    stopGear = true;
-                    position = Math.round(position / 2) * 2;
-                }
-
-                if (position > 0) {
-                    stopGear = true;
-                    position = 0;
-                }
-
-                if (position < minTop) {
-                    stopGear = true;
-                    position = minTop;
-                }
-
-                if (stopGear) {
-                    clearInterval(target['int_' + target.id]);
-
-                    self.setGear(target.id, Math.abs(position) / 2);
-                }
-
-                target['pos_' + target.id] = position;
-                target.style['-webkit-transform'] = 'translate3d(0,' + position + 'em,0)';
-                target.setAttribute('top', position + 'em');
-
-                d++;
-            }, 30);
-        },
-        setGear(key: string, index: number) {
-            let self = this,
-                opts = self.options,
-                items = self.dataSources[key],
-                selects = obj.cloneObject(self.selects);
-
-            index = Math.round(index);
-            selects[key] = items[index][opts.value];
-
-            Vue.set(self, 'selects', selects);
         },
         preventScroll(evt: TouchEvent) {
             evt.preventDefault();
