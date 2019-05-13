42bf7677216885a7f80ca9f30d882c9225a13fd2
nsvn-nguyenvanvuong <vuong_nv@nittsusystem-vn.com>
2019-05-13 17:30:04 +0700

    add modal option (accept VueConstructor<Vue>)

--- nts.uk/uk.mobile/ClientApp/declarations.d.ts
+++ nts.uk/uk.mobile/ClientApp/declarations.d.ts
@@ -101,6 +101,7 @@ declare module "vue/types/vue" {
         $goto(location: { name: string, params?: { [key: string]: any; } }, onComplete?: Function, onAbort?: ErrorHandler): void;
         $modal: {
             (name: string, params?: any, options?: IModalOptions): Promise<{}>;
+            (component: VueConstructor<Vue>, params?: any, options?: IModalOptions): Promise<{}>;
             (component: ComponentOptions<Vue>, params?: any, options?: IModalOptions): Promise<{}>;
             warn(msg: string): Promise<{}>;
             warn(resource: { messageId: string, messageParams?: string[] | { [key: string]: string } }): Promise<{}>;
