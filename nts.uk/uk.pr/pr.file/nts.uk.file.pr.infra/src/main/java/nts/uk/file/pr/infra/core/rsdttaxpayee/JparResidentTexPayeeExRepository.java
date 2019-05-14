17556b388d66ed80eba5923e32f659e0845efd22
Phuc.TC_3S <35018596+tranphuc7686@users.noreply.github.com>
2019-05-14 15:49:33 +0700

    Update code QMM003 SMAL

--- nts.uk/uk.pr/pr.file/nts.uk.file.pr.infra/src/main/java/nts/uk/file/pr/infra/core/rsdttaxpayee/JparResidentTexPayeeExRepository.java
+++ nts.uk/uk.pr/pr.file/nts.uk.file.pr.infra/src/main/java/nts/uk/file/pr/infra/core/rsdttaxpayee/JparResidentTexPayeeExRepository.java
@@ -28,7 +28,7 @@ public class JparResidentTexPayeeExRepository  extends JpaRepository implements 
         exportSQL.append("    ROOT.COMPILE_STATION_NAME ");
         exportSQL.append("FROM (SELECT * FROM QBTMT_RSDT_TAX_PAYEE PAYEE WHERE PAYEE.CID = ?cid) AS ROOT ");
         exportSQL.append("LEFT JOIN QBTMT_RSDT_TAX_PAYEE PAY ");
-        exportSQL.append("ON ROOT.REPORTS_CD = PAY.RESIDENT_TAX_CD ");
+        exportSQL.append("ON ROOT.REPORTS_CD = PAY.RESIDENT_TAX_CD AND ROOT.CID = PAY.CID ");
         exportSQL.append("ORDER BY ROOT.RESIDENT_TAX_CD ");
         try {
             resultQuery = this.getEntityManager().createNativeQuery(exportSQL.toString()).setParameter("cid", cid)
