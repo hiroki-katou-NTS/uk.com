5ff3b8b7a90ca12fb77537b7f50d47dd2aec2e20
Pham Van dan <dan_pv@nittsusystem-vn.com>
2019-06-03 09:32:08 +0900

    add document of validation by vietnamese

--- nts.uk/uk.mobile/ClientApp/views/documents/plugins/validation/index.ts
+++ nts.uk/uk.mobile/ClientApp/views/documents/plugins/validation/index.ts
@@ -15,79 +15,21 @@ import { component } from '@app/core/component';
         jp: require('./content/jp.md')
     },
     validations: {
-        model: {
-            username: {
-                required: true,
-                constraint: 'SampleIntMinMax'
-            },
-            password: {
-                required: true,
-                constraint: 'SampleStringKana'
-            },
-            items: {
-                name: {
-                    loop: true,
-                    required: true,
-                    constraint: 'SampleStringKana'
-                },
-                age: {
-                    loop: true,
-                    required: true,
-                    constraint: 'SampleStringAnyHalf'
-                }
-            }
+        textValue: {
+            required: true
+        },
+        numberValue: {
+            required: true
         }
     },
     constraints: [
-        'nts.uk.shr.sample.primitive.decimals.SampleDecimalMinMax',
-        'nts.uk.shr.sample.primitive.decimals.SampleDecimalRange',
-        'nts.uk.shr.sample.primitive.halfints.SampleHalfIntMinMax',
-        'nts.uk.shr.sample.primitive.halfints.SampleHalfIntRange',
-        'nts.uk.shr.sample.primitive.ints.SampleIntMinMax',
-        'nts.uk.shr.sample.primitive.ints.SampleIntRange',
-        'nts.uk.shr.sample.primitive.longs.SampleLongMinMax',
-        'nts.uk.shr.sample.primitive.longs.SampleLongRange',
-
-        'nts.uk.shr.sample.primitive.strings.SampleStringAlphaNumeric',
         'nts.uk.shr.sample.primitive.strings.SampleStringAny',
-        'nts.uk.shr.sample.primitive.strings.SampleStringAnyHalf',
-        'nts.uk.shr.sample.primitive.strings.SampleStringKana',
-        'nts.uk.shr.sample.primitive.strings.SampleStringNumeric',
-
-        'nts.uk.shr.sample.primitive.times.SampleClockMinMax',
-        'nts.uk.shr.sample.primitive.times.SampleClockRange',
-        'nts.uk.shr.sample.primitive.times.SampleDurationMinMax',
-        'nts.uk.shr.sample.primitive.times.SampleDurationRange'
+        'nts.uk.shr.sample.primitive.strings.SampleStringKana'
     ]
 })
 export class DocumentsPluginsValidationComponent extends Vue {
-    public model: {
-        username: number;
-        password: string;
-        items: any[];
-    } = {
-            username: 500,
-            password: 'password',
-            items: [{
-                name: 'a',
-                age: 100
-            }, {
-                name: 'b',
-                age: 100
-            }, {
-                name: 'c',
-                age: 100
-            }, {
-                name: 'd',
-                age: 100
-            }, {
-                name: 'e',
-                age: 100
-            }]
-        };
 
-    public created() {
-        $.set(window, '__', $);
-        $.set(window, 'vm', this);
-    }
+    public textValue: string = 'nittsu';
+    public numberValue: number = 1;
+
 }
--- nts.uk/uk.mobile/ClientApp/views/documents/plugins/validation/index.vue
+++ nts.uk/uk.mobile/ClientApp/views/documents/plugins/validation/index.vue
@@ -1,20 +1,10 @@
 <template>
   <div class="documentspluginsvalidation">
     <h5>1. {{'sample' | i18n}}</h5>
-    <div class="row justify-content-md-center">
+    <div class="row">
       <div class="col-md-4">
-        <nts-number-editor name="username" v-model="model.username"/>
-        <nts-input-password name="password" v-model="model.password"/>
-      </div>
-    </div>
-    <div class="row justify-content-md-center">
-      <div class="col-md-4">
-        <h3>Sample loop validators</h3>
-        <div v-for="(item, k) of model.items" v-bind:key="k">
-          <nts-text-editor name="username" v-model="model.items[k].name" v-bind:key="k"/>
-          <nts-text-editor name="age" v-model="model.items[k].age" v-bind:key="k"/>
-          <hr>
-        </div>
+        <nts-text-editor name="Text" v-model="textValue"/>
+        <nts-number-editor name="Number" v-model="numberValue"/>
       </div>
     </div>
     <markdown/>
