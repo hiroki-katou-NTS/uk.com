b1317456e00f45a2f5a41065085937fcc754c4f9
Leu Thi Lan <lan-lt@nittsusystem-vn.com>
2019-06-03 17:30:52 +0700

    fix bug #107718 ver 1, [IS00287] thêm thuật toán [アルゴリズム「社員の契約時間を取得する」を実行する]
    アルゴリズム「社員の契約時間を取得する」を実行する

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.pubimp/src/main/java/nts/uk/ctx/at/shared/pubimp/employee/ContractTimeEmployeePubImpl.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.pubimp/src/main/java/nts/uk/ctx/at/shared/pubimp/employee/ContractTimeEmployeePubImpl.java
@@ -6,15 +6,15 @@ package nts.uk.ctx.at.shared.pubimp.employee;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+
 import nts.arc.time.GeneralDate;
-import nts.uk.ctx.at.shared.dom.workingcondition.WorkingCondition;
-import nts.uk.ctx.at.shared.dom.workingcondition.WorkingConditionItem;
 import nts.uk.ctx.at.shared.dom.workingcondition.WorkingConditionItemRepository;
 import nts.uk.ctx.at.shared.dom.workingcondition.WorkingConditionRepository;
-import nts.uk.ctx.at.shared.pub.employee.ContractTimeEmployeePub;
 import nts.uk.ctx.at.shared.pub.employee.ContractTimeEmployeeExport;
+import nts.uk.ctx.at.shared.pub.employee.ContractTimeEmployeePub;
 import nts.uk.shr.com.history.DateHistoryItem;
 
 /**
@@ -32,35 +32,20 @@ public class ContractTimeEmployeePubImpl implements ContractTimeEmployeePub {
 	
 	@Override
 	public List<ContractTimeEmployeeExport> getData(List<String> listEmpID, GeneralDate baseDate) {
-		List<String> listHistID = new ArrayList<>();
-		List<ContractTimeEmployeeExport> data = new ArrayList<>();
 		//ドメインモデル「労働条件」をすべて取得する
 		//(Lấy all domain [WorkingCondition])
-		List<WorkingCondition> listWorkingCondition = workingConditionRepository.getBySids(listEmpID,baseDate);
-		List<List<DateHistoryItem>> lstDateHistoryItem = listWorkingCondition.stream().map(c->c.dateHistoryItem).collect(Collectors.toList());
-		for (List<DateHistoryItem> dateHistoryItem : lstDateHistoryItem) {
-			for(DateHistoryItem c :dateHistoryItem){
-				String histID = c.identifier();
-				listHistID.add(histID);
-			}
-			
-		}
+		List<List<DateHistoryItem>> listWorkingCondition = workingConditionRepository.getBySids(listEmpID, baseDate)
+				.stream().map(c -> c.dateHistoryItem).collect(Collectors.toList());
+		List<String> listHistID = listWorkingCondition.stream().flatMap(c -> c.stream())
+				.map(c -> c.identifier()).collect(Collectors.toList());
 		
 		//ドメインモデル「労働条件項目」を全て取得する
 		//(Lấy all domain [WorkingConditionItem])
-		List<WorkingConditionItem> listWorkingConditionItem = workingConditionItemRepository.getByListHistoryID(listHistID);
-		for (WorkingConditionItem workingConditionItem : listWorkingConditionItem) {
-			String employeeID = workingConditionItem.getEmployeeId();
-			int contractTime = workingConditionItem.getContractTime().v();
-			ContractTimeEmployeeExport a = new ContractTimeEmployeeExport(employeeID, contractTime);
-			data.add(a);
-		}
-		
-		return data;
+		if(listHistID.isEmpty()) return new ArrayList<>();
+		List<ContractTimeEmployeeExport> result = workingConditionItemRepository
+				.getByListHistoryID(listHistID).stream()
+				.map(c -> new ContractTimeEmployeeExport(c.getEmployeeId(), c.getContractTime().v()))
+				.collect(Collectors.toList());
+		return result;
 	}
-
-	
-	
-	
-
 }
--- nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.ac/src/main/java/nts/uk/ctx/pereg/ac/employee/ContractTimeEmployeeAC.java
+++ nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.ac/src/main/java/nts/uk/ctx/pereg/ac/employee/ContractTimeEmployeeAC.java
@@ -10,6 +10,7 @@ import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import nts.arc.time.GeneralDate;
+import nts.uk.ctx.at.shared.pub.employee.ContractTimeEmployeePub;
 import nts.uk.ctx.pereg.dom.adapter.ContractTimeEmployeeAdapter;
 import nts.uk.ctx.pereg.dom.adapter.ContractTimeEmployeeImport;
 
@@ -19,20 +20,17 @@ import nts.uk.ctx.pereg.dom.adapter.ContractTimeEmployeeImport;
  */
 @Stateless
 public class ContractTimeEmployeeAC implements ContractTimeEmployeeAdapter {
-//
-//	@Inject
-//	private ContractTimeEmployeePub contractTimeEmployeePub;
+
+	@Inject
+	private ContractTimeEmployeePub contractTimeEmployeePub;
 
 	/* (non-Javadoc)
 	 * @see nts.uk.ctx.pereg.dom.adapter.ContractTimeEmployeeAdapter#getData(java.util.List, nts.arc.time.GeneralDate)
 	 */
 	@Override
 	public List<ContractTimeEmployeeImport> getData(List<String> listEmpID, GeneralDate baseDate) {
-//		List<ContractTimeEmployeeExport> listEx = contractTimeEmployeePub.getData(listEmpID, baseDate);
-//		List<ContractTimeEmployeeImport> listIm = listEx.stream().map(x ->this.toImport(x)).collect(Collectors.toList());
-		return null;
-	}
-	private ContractTimeEmployeeImport toImport(){
-		return null;
+		List<ContractTimeEmployeeImport> listEx = contractTimeEmployeePub.getData(listEmpID, baseDate).stream()
+				.map(c -> new ContractTimeEmployeeImport(c.getEmployeeID(), c.getContractTime())).collect(Collectors.toList());
+		return listEx;
 	}
 }
