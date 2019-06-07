2fdf2a83c73aaff000ac410e54513f22415155a5
NSVN-Leu Thi Lan <lan_lt@nittsusystem-vn.com>
2019-06-07 10:57:38 +0700

    CPS003: check null

--- nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.app/src/main/java/nts/uk/ctx/pereg/app/find/common/MappingFactory.java
+++ nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.app/src/main/java/nts/uk/ctx/pereg/app/find/common/MappingFactory.java
@@ -91,7 +91,7 @@ public class MappingFactory {
 		if(peregDto != null) {
 			// map data
 			Map<String, Object> itemCodeValueMap = getFullDtoValue(peregDto);
-		String recordId = peregDto.getDomainDto().getRecordId();
+		String recordId = peregDto.getDomainDto() == null? null:  peregDto.getDomainDto().getRecordId();
 		for (LayoutPersonInfoClsDto classItem : classItemsOfCategory) {
 			for (LayoutPersonInfoValueDto valueItem : classItem.getItems()) {
 				Object value = getValue(itemCodeValueMap, valueItem);
@@ -121,7 +121,7 @@ public class MappingFactory {
 		if(peregDto != null) {
 			// map data
 			Map<String, Object> itemCodeValueMap = getFullDtoValue(peregDto);
-			String recordId = peregDto.getDomainDto().getRecordId();
+			String recordId = peregDto.getDomainDto() == null? null:  peregDto.getDomainDto().getRecordId();
 			for (LayoutPersonInfoClsDto classItem : classItemsOfCategory) {
 				for (LayoutPersonInfoValueDto valueItem : classItem.getItems()) {
 					
