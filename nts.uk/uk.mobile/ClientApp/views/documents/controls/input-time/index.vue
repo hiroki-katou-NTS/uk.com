616da4d78bd53b81fa6cfe0946fa8f4ed4028b4e
Pham Van dan <dan_pv@nittsusystem-vn.com>
2019-06-20 08:50:38 +0900

    create Vietnamese document of time-editor

--- nts.uk/uk.mobile/ClientApp/views/documents/controls/input-time/index.vue
+++ nts.uk/uk.mobile/ClientApp/views/documents/controls/input-time/index.vue
@@ -1,35 +1,16 @@
 <template>
-  <div class="documentscontrolsinput-time">
+<div class="documentscontrolsinput-time">
     <h5>1. {{'sample' | i18n}}</h5>
-
     <button v-on:click="validate" class="btn btn-secondary">Validate</button>
     <br/>
 
-    <nts-time-editor
-      v-model="timeWithDay"
-      name='日区分付き時刻　(Time-With-Day)'
-      time-input-type="time-with-day"
-    />
+    <nts-time-editor v-model="timeWithDay" name='日区分付き時刻　(Time-With-Day)' time-input-type="time-with-day" />
 
-    <nts-time-editor
-      v-model="timePoint"
-      name='時刻  (Time-Point)'
-      time-input-type="time-point"
-    />
+    <nts-time-editor v-model="timePoint" name='時刻  (Time-Point)' time-input-type="time-point" />
 
-    <nts-time-editor
-      v-model="timeDuration"
-      name='時間  (Time-Duration)'
-      time-input-type="time-duration"
-    />
+    <nts-time-editor v-model="timeDuration" name='時間  (Time-Duration)' time-input-type="time-duration" />
 
-    <markdown/>
+    <markdown />
 
-    <br>
-    <label>
-      Tạo bởi:
-      <b>Phạm Văn Dân</b>
-    </label>
-    <br>
-  </div>
+</div>
 </template>
