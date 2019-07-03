4a251699eecd34a73aa654a4aafde82dda03e992
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2019-07-03 14:31:01 +0700

    update require validator

--- nts.uk/uk.mobile/ClientApp/@app/plugins/validate/validators/required.ts
+++ nts.uk/uk.mobile/ClientApp/@app/plugins/validate/validators/required.ts
@@ -4,24 +4,25 @@ import { IRule } from 'declarations';
 const required = function (value: any, checkOr: boolean, rule: IRule) {
     let msg = false;
 
-    if (Array.isArray(value)) {
-        msg = value.length === 0;
-    } else if (value === undefined || value === null) {
-        msg = true;
-    } else if (value === false) {
-        msg = false;
-    } else if (value instanceof Date) {
-        // invalid date won't pass
-        msg = isNaN(value.getTime());
-    } else if (typeof value === 'object') {        
-        if ( rule.timeRange || rule.dateRange) {
-            msg = _.isNil(value.start) || _.isNil(value.end);
+    if (checkOr) {
+        if (Array.isArray(value)) {
+            msg = value.length === 0;
+        } else if (value === undefined || value === null) {
+            msg = true;
+        } else if (value === false) {
+            msg = false;
+        } else if (value instanceof Date) {
+            // invalid date won't pass
+            msg = isNaN(value.getTime());
+        } else if (typeof value === 'object') {
+            if (rule.timeRange || rule.dateRange) {
+                msg = _.isNil(value.start) || _.isNil(value.end);
+            } else {
+                msg = Object.keys(value).length === 0;
+            }
         } else {
-            msg = Object.keys(value).length === 0;
+            msg = String(value).length === 0;
         }
-
-    } else {
-        msg = String(value).length === 0;
     }
 
     return !msg ? null : 'MsgB_1';
