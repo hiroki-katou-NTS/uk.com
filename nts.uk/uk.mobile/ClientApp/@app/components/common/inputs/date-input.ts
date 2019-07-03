829b5f085272d8e3a9b0b78ac32a5cb63c3202fb
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2019-07-03 17:38:17 +0700

    move event & argument to self control (datepicker)

--- nts.uk/uk.mobile/ClientApp/@app/components/common/inputs/date-input.ts
+++ nts.uk/uk.mobile/ClientApp/@app/components/common/inputs/date-input.ts
@@ -1,7 +1,5 @@
-import { $ } from '@app/utils';
-import { Vue, moment } from '@app/provider';
+import { _, Vue, moment } from '@app/provider';
 import { input, InputComponent } from './input';
-import { DateHelper as helper} from '@app/components/controls/date-picker';
 
 @input()
 class DateComponent extends InputComponent {
@@ -11,42 +9,77 @@ class DateComponent extends InputComponent {
 
     public mounted() {
         this.icons.after = 'far fa-calendar-alt';
+
+        Object.assign(window, { moment });
+    }
+
+    get date() {
+        return moment(this.value || new Date()).date();
+    }
+
+    get month() {
+        return moment(this.value || new Date()).month() + 1;
+    }
+
+    get year() {
+        return moment(this.value || new Date()).year();
     }
 
     get rawValue() {
-       if ($.isNil(this.value)) {
-           return '';
-       }
+        if (_.isNil(this.value)) {
+            return '';
+        }
 
-       return this.computeRawValue(this.value);
+        return moment(this.value).format('YYYY-MM-DD');
     }
 
-    private computeRawValue(value: Date): string {
-        let year: number = value.getFullYear();
-        let month: number = value.getMonth() + 1;
-        let date: number = value.getDate();
-        let monthText = month < 10 ? '0' + month : month;
-        let dateText = date < 10 ? '0' + date : date;
-
-        return year + '-' + monthText + '-' + dateText;
+    private titlePicker(selecteds: { year: number; month: number; date: number; }) {
+        return this.$i18n('nts_date_format', [selecteds.year.toString(), selecteds.month.toString(), selecteds.date.toString()]);
     }
 
     public click() {
-        let selecteds = helper.computeSelecteds(this.value);
-        this.$picker(selecteds, 
-            helper.getDataSource(this.value),
-            helper.onSelect, {
-                title: `${selecteds.year}年${selecteds.month}月${selecteds.date}日`,
-                required: this.constraints && this.constraints.required
-            })
-            .then( (select: any) => {
-                if ( select === undefined) {
+        let self = this,
+            { year, month, date } = self,
+            daysInMonth = moment.utc(Date.UTC(year, month - 1)).daysInMonth();
+
+        self.$picker({
+            year,
+            month,
+            date
+        }, {
+                year: _.range(1900, 10000, 1).map((value: number) => ({ text: value.toString(), value })),
+                month: _.range(1, 13, 1).map((value: number) => ({ text: _.padStart(`${value}`, 2, '0'), value })),
+                date: _.range(1, daysInMonth + 1, 1).map((value) => ({ text: _.padStart(`${value}`, 2, '0'), value }))
+            }, (select: ISelected, pkr: IPicker) => {
+                if (_.isEmpty(pkr.selects)) {
+                    return;
+                }
+
+                let daysInMonth = moment.utc(Date.UTC(select.year, select.month - 1)).daysInMonth();
+
+                if (pkr.dataSources.date.length !== daysInMonth) {
+                    pkr.dataSources.date = _.range(1, daysInMonth + 1, 1).map((value) => ({ text: _.padStart(`${value}`, 2, '0'), value }));
+                }
+
+                if (pkr.selects.date > daysInMonth) {
+                    pkr.selects.date = daysInMonth;
+                }
+
+                let { year, month, date } = pkr.selects;
+
+                pkr.title = self.titlePicker({ year, month, date });
+            }, {
+                title: self.titlePicker({ year, month, date }),
+                required: self.constraints && self.constraints.required
+            }).then((select: { year: number; month: number; date: number; }) => {
+                if (select === undefined) {
 
                 } else if (select === null) {
-                    this.$emit('input', null);
+                    self.$emit('input', null);
                 } else {
-                    let momentValue = moment.utc(`${select.year}-${select.month}-${select.date}`, 'YYYY-MM-DD');
-                    this.$emit('input', momentValue.toDate());
+                    let $utc = Date.UTC(select.year, select.month - 1, select.date);
+
+                    self.$emit('input', moment.utc($utc).toDate());
                 }
             });
 
@@ -54,3 +87,17 @@ class DateComponent extends InputComponent {
 }
 
 Vue.component('nts-date-input', DateComponent);
+
+interface ISelected {
+    [key: string]: any;
+}
+
+interface IDataSource {
+    [key: string]: any[];
+}
+
+interface IPicker {
+    title: string;
+    dataSources: IDataSource;
+    selects: ISelected;
+}
