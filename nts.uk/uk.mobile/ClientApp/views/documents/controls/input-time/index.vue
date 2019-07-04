b4b8ca426940f452f73a0793a88b80470e829366
Pham Van dan <dan_pv@nittsusystem-vn.com>
2019-07-04 16:04:49 +0900

    edit document of time-input. display name with languages

--- nts.uk/uk.mobile/ClientApp/views/documents/controls/input-time/index.vue
+++ nts.uk/uk.mobile/ClientApp/views/documents/controls/input-time/index.vue
@@ -4,11 +4,11 @@
     <button v-on:click="validate" class="btn btn-secondary">Validate</button>
     <br/>
 
-    <nts-time-editor v-model="timeWithDay" name='日区分付き時刻　(Time-With-Day)' time-input-type="time-with-day" />
+    <nts-time-editor v-model="timeWithDay" name='time-with-day' time-input-type="time-with-day" />
 
-    <nts-time-editor v-model="timePoint" name='時刻  (Time-Point)' time-input-type="time-point" />
+    <nts-time-editor v-model="timePoint" name='time-point' time-input-type="time-point" />
 
-    <nts-time-editor v-model="timeDuration" name='時間  (Time-Duration)' time-input-type="time-duration" />
+    <nts-time-editor v-model="timeDuration" name='time-duration' time-input-type="time-duration" />
 
     <markdown />
 
