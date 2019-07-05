2d16eabc27d79c59b0ff3503066c52fbccd50926
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2019-07-05 17:44:23 +0700

    update range validate (msg_49)

--- nts.uk/uk.mobile/ClientApp/@app/plugins/validate/validators/dateRange.ts
+++ nts.uk/uk.mobile/ClientApp/@app/plugins/validate/validators/dateRange.ts
@@ -1,7 +1,7 @@
 import { _ } from '@app/provider';
 
 export const dateRange = function (value: { start: Number, end: Number }, checkOr: boolean) {
-    if (checkOr && !_.isNil(value) && value.end < value.start) {
+    if (checkOr && !_.isNil(value) && !_.isNil(value.end) && value.end < value.start) {
         return 'MsgB_49';
     }
 
