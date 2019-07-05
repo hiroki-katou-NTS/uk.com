f68934370171db820c51c4ee5f1c9e79124f97ca
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2019-07-05 14:02:04 +0700

    update v-focus

--- nts.uk/uk.mobile/ClientApp/@app/directives/common/focus.ts
+++ nts.uk/uk.mobile/ClientApp/@app/directives/common/focus.ts
@@ -3,6 +3,16 @@ import { Vue } from '@app/provider';
 Vue.directive('focus', {
     // directive definition
     inserted(el: HTMLInputElement) {
-        setTimeout(() => el.focus(), 25);
+        setTimeout(() => {
+            if (el.tabIndex > -1) {
+                el.focus();
+            } else {
+                let focusable = el.querySelector('a, input, select, button, textarea') as HTMLElement;
+
+                if (focusable) {
+                    focusable.focus();
+                }
+            }
+        }, 25);
     }
 });
