b1cabf41eb3b47d9ec356f1bd102bf2c8d83f874
nsvn-nguyenvanvuong <vuong_nv@nittsusystem-vn.com>
2019-07-15 14:14:22 +0700

    update sample tree views

--- nts.uk/uk.mobile/ClientApp/views/documents/htmls/list-tree/index.ts
+++ nts.uk/uk.mobile/ClientApp/views/documents/htmls/list-tree/index.ts
@@ -1,5 +1,5 @@
 import { obj, date } from '@app/utils';
-import { Vue } from '@app/provider';
+import { _, Vue } from '@app/provider';
 import { component } from '@app/core/component';
 
 @component({
@@ -20,84 +20,42 @@ export class DocumentsHtmlsListTreeComponent extends Vue {
     public selected: any = {};
     public selecteds: any[] = [];
 
-    public items: Array<any> = [{
-        id: 1,
-        text: 'Folder 1',
-        state: {
-            selected: false
-        },
-        children: [
-            {
-                id: 2,
-                text: 'Sub Folder 1.1',
-                state: {
-                    selected: false
-                },
-                children: [{
-                    id: 5,
-                    text: 'Sub Folder 1.1.1'
-                }, {
-                    id: 6,
-                    text: 'Sub Folder 1.1.2'
-                }]
-            },
-            {
-                id: 3,
-                text: 'Sub Folder 1.2',
-                state: {
-                    selected: false
-                },
-                children: [{
-                    id: 7,
-                    text: 'Sub Folder 1.2.1'
-                }, {
-                    id: 8,
-                    text: 'Sub Folder 1.2.2'
-                }]
-            }
-        ]
-    },
-    {
-        id: 4,
-        text: 'Folder 2',
-        state: {
-            selected: true
-        },
-        children: []
-    }];
+    public items: Array<any> = [];
 
     get flatten() {
-        return obj.hierarchy(this.items, 'children');
+        return obj.hierarchy(this.items, 'childs');
     }
 
     public addTopItem() {
         this.items.unshift({
-            id: Math.random() * 100, text: 'Add new item',
-            state: {
-                selected: false
-            },
-            children: [{
-                id: 7,
-                text: 'Sub Folder 1.2.1'
+            workplaceId: Math.random() * 100,
+            code: Math.random() * 100,
+            name: 'Add new item',
+            childs: [{
+                workplaceId: 7,
+                code: Math.random() * 100,
+                name: 'Sub Folder 1.2.1'
             }, {
-                id: 8,
-                text: 'Sub Folder 1.2.2'
+                workplaceId: 8,
+                code: Math.random() * 100,
+                name: 'Sub Folder 1.2.2'
             }]
         });
     }
 
     public addBottomItem() {
         this.items.push({
-            id: Math.random() * 100, text: 'Add new item',
-            state: {
-                selected: false
-            },
-            children: [{
-                id: 7,
-                text: 'Sub Folder 1.2.1'
+            workplaceId: Math.random() * 100,
+            code: Math.random() * 100,
+            name: 'Add new item',
+            childs: [{
+                workplaceId: 7,
+                code: Math.random() * 100,
+                name: 'Sub Folder 1.2.1'
             }, {
-                id: 8,
-                text: 'Sub Folder 1.2.2'
+                workplaceId: 8,
+                code: Math.random() * 100,
+                name: 'Sub Folder 1.2.2'
             }]
         });
     }
@@ -108,5 +66,40 @@ export class DocumentsHtmlsListTreeComponent extends Vue {
 
     public removeBottomItem() {
         this.items.pop();
+    }
+
+    public created() {
+        let items = require('./mock-data.json');
+
+        this.items = _.chain(items)
+            // sắp xếp đối tượng theo mã quan hệ
+            .orderBy((item) => item.hierarchyCode, 'asc')
+            // Lọc những đối tượng con
+            .map((item) => {
+                const childs = items.filter((f) => {
+                    // không phải là chính đối tương
+                    const notself = f.hierarchyCode !== item.hierarchyCode;
+                    // là những đối tượng con của đối tượng
+                    const ischilds = f.hierarchyCode.startsWith(item.hierarchyCode);
+
+                    return notself && ischilds;
+                })
+                    // dựng bản đồ quan hệ con - cha
+                    .map((child) => _.extend(child, { parent: item }));
+
+                // dựng bản đồ quan hệ cha - con
+                return _.extend(item, { childs });
+            })
+            // lọc những đối tượng con không hợp lệ (cháu, chắt)
+            .map((item) => {
+                item.childs = item.childs.filter((child) => child.parent.hierarchyCode == item.hierarchyCode);
+
+                return item;
+            })
+            // Lọc những đối tượng không có cha (là những đối tượng root)
+            .filter((item) => !item.parent)
+            // sắp xếp lại các đối tượng
+            // .orderBy((item) => item.code, 'asc')
+            .value();
     }
 }
