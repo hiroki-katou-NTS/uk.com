6f7501b8e3c952fe999418e7164442e0fe414c40
nsvn-nguyenvanvuong <vuong_nv@nittsusystem-vn.com>
2019-07-26 15:01:24 +0700

    exclude manifest file outside war

--- nts.uk/uk.mobile/webpack.package.war.plugin.js
+++ nts.uk/uk.mobile/webpack.package.war.plugin.js
@@ -17,14 +17,17 @@ PackageWarFile.prototype.apply = function (compiler) {
                 vdm = path.join(__dirname, 'wwwroot', 'nts.uk.mobile.web', 'dist', 'vendor.js.map'),
                 bdm = path.join(__dirname, 'wwwroot', 'nts.uk.mobile.web', 'dist', 'bundles.js.map'),
                 vdcm = path.join(__dirname, 'wwwroot', 'nts.uk.mobile.web', 'dist', 'vendor.css.map'),
-                bdcm = path.join(__dirname, 'wwwroot', 'nts.uk.mobile.web', 'dist', 'bundles.css.map');
+                bdcm = path.join(__dirname, 'wwwroot', 'nts.uk.mobile.web', 'dist', 'bundles.css.map'),
+                vmnf = path.join(__dirname, 'wwwroot', 'nts.uk.mobile.web', 'dist', 'vendor-manifest.json');
 
             // delete map file
             if (fs.existsSync(vdm)) { fs.unlinkSync(vdm); }
             if (fs.existsSync(bdm)) { fs.unlinkSync(bdm); }
-            
+
             if (fs.existsSync(vdcm)) { fs.unlinkSync(vdcm); }
             if (fs.existsSync(bdcm)) { fs.unlinkSync(bdcm); }
+
+            if (fs.existsSync(vmnf)) { fs.unlinkSync(vmnf); }
 
             // change version of resource
             const content = fs.readFileSync(idf, { encoding: 'utf-8' });
