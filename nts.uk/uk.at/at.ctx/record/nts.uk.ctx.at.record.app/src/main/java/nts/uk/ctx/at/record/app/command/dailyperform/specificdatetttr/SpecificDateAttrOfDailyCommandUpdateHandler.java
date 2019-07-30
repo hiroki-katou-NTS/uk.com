ebfb4913cf531270bf9c27774683768caddcfd70
NSVN-NguyenXuanThanh <thanh_nx@nittsusystem-vn.com>
2019-07-30 16:14:12 +0700

    fix bug 108505v5

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/dailyperform/specificdatetttr/SpecificDateAttrOfDailyCommandUpdateHandler.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/dailyperform/specificdatetttr/SpecificDateAttrOfDailyCommandUpdateHandler.java
@@ -4,20 +4,20 @@ import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import nts.arc.layer.app.command.CommandHandlerContext;
-import nts.uk.ctx.at.record.dom.raisesalarytime.repo.SpecificDateAttrOfDailyPerforRepo;
+import nts.uk.ctx.at.record.dom.daily.DailyRecordAdUpService;
 import nts.uk.ctx.at.shared.app.util.attendanceitem.CommandFacade;
 
 @Stateless
 public class SpecificDateAttrOfDailyCommandUpdateHandler extends CommandFacade<SpecificDateAttrOfDailyCommand> {
 
 	@Inject
-	private SpecificDateAttrOfDailyPerforRepo repo;
+	private DailyRecordAdUpService adUpRepo;
 
 	@Override
 	protected void handle(CommandHandlerContext<SpecificDateAttrOfDailyCommand> context) {
 		SpecificDateAttrOfDailyCommand command = context.getCommand();
 		if(command.getData().isPresent()){
-			repo.update(command.toDomain().get());
+			adUpRepo.adUpSpecificDate(command.toDomain());
 		}
 	}
 
