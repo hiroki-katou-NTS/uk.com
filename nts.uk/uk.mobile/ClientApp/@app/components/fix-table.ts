90c111c65f92b8dc357568af1eac99f1904391b7
Pham Van dan <dan_pv@nittsusystem-vn.com>
2019-07-31 15:06:21 +0900

    add 2 buttons to template.

--- nts.uk/uk.mobile/ClientApp/@app/components/fix-table.ts
+++ nts.uk/uk.mobile/ClientApp/@app/components/fix-table.ts
@@ -4,17 +4,19 @@ import { component, Prop } from '@app/core/component';
 @component({
     template: `<div class="table-container">
         <div class="table-header">
-            <table class="table table-bordered">
+            <button ref="previous" class="btn btn-secondary" v-on:click="previous">前項</button>
+            <button ref="next" class="btn btn-secondary" v-on:click="next">事項</button>
+            <table class="table table-bordered m-0">
                 <tbody></tbody>
             </table>
         </div>
         <div class="table-body">
-            <table class="table table-bordered">
+            <table class="table table-bordered m-0">
                 <slot/>
             </table>
         </div>
         <div class="table-footer">
-            <table class="table table-bordered">
+            <table class="table table-bordered m-0">
                 <tbody></tbody>
             </table>
         </div>
@@ -24,6 +26,9 @@ export class FixTableComponent extends Vue {
 
     @Prop({ default: 4 })
     public displayColumns: number;
+
+    @Prop({ default: 6}) 
+    public rowNumber: number;
 
     public startDisplayIndex: number = 1;
     public oldStartDisplayIndex: number = 1;
@@ -40,11 +45,11 @@ export class FixTableComponent extends Vue {
 
         this.moveTheadAndTfoot();
 
-        this.setStyleOfTableBody();
-
         this.hiddenColumns();
 
         this.addPrevNextButtons();
+
+        this.setStyleOfTableBody();
     }
 
     private moveTheadAndTfoot() {
@@ -53,13 +58,13 @@ export class FixTableComponent extends Vue {
     }
 
     private setStyleOfTableBody() {
-        let tableBodyDiv = this.$el.querySelector('.table-container .table-body') as HTMLDivElement;
+        this.$nextTick(() => {
+            let height: number = (this.bodyTable.tBodies[0].firstChild.firstChild as HTMLTableCellElement).clientHeight;
 
-        let heightOfARow: string = (this.bodyTable.tBodies[0].firstChild.firstChild as HTMLTableCellElement).style.height;
-        let height: any = heightOfARow.substring(0, heightOfARow.length - 3);
-
-        tableBodyDiv.style.height = `250px` ;
-        tableBodyDiv.style.overflowY = 'scroll';
+            let tableBodyDiv = this.$el.querySelector('.table-container .table-body') as HTMLDivElement;
+            tableBodyDiv.style.height = `${height * this.rowNumber}px` ;
+            tableBodyDiv.style.overflowY = 'scroll';
+        });
     }
 
     private hiddenColumns() {
@@ -122,50 +127,59 @@ export class FixTableComponent extends Vue {
         
     }
 
-    
-
     private addPrevNextButtons() {
-
-        let self = this;
-        
-        let prevButton = document.createElement('button');
-        prevButton.textContent = 'Prev';
-        prevButton.onclick = function() {
-            self.oldStartDisplayIndex = self.startDisplayIndex;
-            self.startDisplayIndex = self.startDisplayIndex - self.displayColumns;
-            
-            if (self.startDisplayIndex === 1) {
-                prevButton.disabled = true;
-            }
-
-            if (nextButton.disabled === true) {
-                nextButton.disabled = false;
-            }
-
-            self.changeDisplayColumn();
-        };
-
-        let nextButton = document.createElement('button');
-        nextButton.textContent = 'Next';
-        nextButton.onclick = function() {
-            self.oldStartDisplayIndex = self.startDisplayIndex;
-            self.startDisplayIndex = self.startDisplayIndex + self.displayColumns;
-            
-            let headerColumnLength = self.headerTable.tHead.firstChild.childNodes.length;
-            if (self.startDisplayIndex + self.displayColumns >= headerColumnLength) {
-                nextButton.disabled = true;
-            }
-
-            if (prevButton.disabled === true) {
-                prevButton.disabled = false;
-            }
-
-            self.changeDisplayColumn();
-        };
-
         let headerColumns = this.headerTable.tHead.firstChild.childNodes;
-        headerColumns[0].appendChild(prevButton);
-        headerColumns[headerColumns.length - 1].appendChild(nextButton);
+        headerColumns[0].appendChild(this.$refs.previous as Node);
+        headerColumns[headerColumns.length - 1].appendChild(this.$refs.next as Node);
+    }
+
+    public previous() {
+        let self = this;
+
+        self.oldStartDisplayIndex = self.startDisplayIndex;
+        self.startDisplayIndex = self.startDisplayIndex - self.displayColumns;
+        
+        if (self.startDisplayIndex === 1) {
+            (this.$refs.previous as HTMLButtonElement).disabled = true;
+        }
+
+        let nextButton = this.$refs.next as HTMLButtonElement;
+        if (nextButton.disabled === true) {
+            nextButton.disabled = false;
+        }
+
+        self.changeDisplayColumn();
+    }
+    
+    public next() {
+        let self = this;
+        self.oldStartDisplayIndex = self.startDisplayIndex;
+        self.startDisplayIndex = self.startDisplayIndex + self.displayColumns;
+        
+        let headerColumnLength = self.headerTable.tHead.firstChild.childNodes.length;
+        if (self.startDisplayIndex + self.displayColumns >= headerColumnLength) {
+            (self.$refs.next as HTMLButtonElement).disabled = true;
+        }
+
+        let prevButton = self.$refs.previous as HTMLButtonElement;
+        if (prevButton.disabled === true) {
+            prevButton.disabled = false;
+        }
+
+        self.changeDisplayColumn();
+    }
+    
+    public updated() {
+        let columns = this.bodyTable.tBodies[0].children[0].children;
+        let maps = Array.from(columns).map( 
+            (c) => c.clientWidth
+        );
+        
+        let headerColumns = this.headerTable.tHead.children[0].children;
+        for ( let i = 0; i < headerColumns.length; i++) {
+            (headerColumns[i] as HTMLTableCellElement).style.width = `${maps[i]}px`;
+        }
+
     }
 
 }
