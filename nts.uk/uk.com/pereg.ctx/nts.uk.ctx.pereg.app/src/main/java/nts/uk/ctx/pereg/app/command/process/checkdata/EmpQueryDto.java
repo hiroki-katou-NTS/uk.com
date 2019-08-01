f666b18928aa593dfd2df940843a690261857704
NSVN-TruongVanLai <NSVN-TruongVanLai@users.noreply.github.com>
2019-08-01 19:16:03 +0700

    remove filter CCG001

--- nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.app/src/main/java/nts/uk/ctx/pereg/app/command/process/checkdata/EmpQueryDto.java
+++ nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.app/src/main/java/nts/uk/ctx/pereg/app/command/process/checkdata/EmpQueryDto.java
@@ -132,7 +132,6 @@ public class EmpQueryDto implements Serializable {
 	public EmployeeSearchQuery toQueryModel(GeneralDateTime baseDate) {
 		return EmployeeSearchQuery.builder()
 				.baseDate(baseDate)
-				.systemType(CCG001SystemType.PERSONAL_INFORMATION.value)
 				.referenceRange(SearchReferenceRange.ALL_EMPLOYEE.value)
 				.filterByEmployment(false)
 				.employmentCodes(new ArrayList<>())
