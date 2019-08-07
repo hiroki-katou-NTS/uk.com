656cc77267d2cc562ac931b5da69174aac24d102
3SI\tam.nx <>
2019-08-07 15:50:22 +0700

    QSI013 - Code Client

---
+++ nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qsi/013/a/qsi013.a.service.ts
@@ -0,0 +1,9 @@
+module nts.uk.pr.view.qsi013.a.service {
+    import ajax = nts.uk.request.ajax;
+    import format = nts.uk.text.format;
+    let paths : any = {
+
+    };
+
+
+}
---
+++ nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qsi/013/b/qsi013.b.vm.ts
@@ -0,0 +1,125 @@
+module nts.uk.pr.view.qsi013.b.viewmodel {
+    import close = nts.uk.ui.windows.close;
+    import getText = nts.uk.resource.getText;
+    import setShared = nts.uk.ui.windows.setShared;
+    import getShared = nts.uk.ui.windows.getShared;
+    import model = qsi001.share.model;
+
+    export class ScreenModel {
+        ccg001ComponentOption: GroupOption;
+
+        constructor() {
+            let self = this;
+            this.loadCCG001();
+        }
+
+        close(){
+            nts.uk.ui.windows.close();
+        }
+
+        loadCCG001(){
+            let self = this;
+            self.ccg001ComponentOption = {
+                /** Common properties */
+                systemType: 1,
+                showEmployeeSelection: true,
+                showQuickSearchTab: false,
+                showAdvancedSearchTab: true,
+                showBaseDate: false,
+                showClosure: false,
+                showAllClosure: false,
+                showPeriod: false,
+                periodFormatYM: false,
+                tabindex: 5,
+                /** Required parameter */
+                baseDate: moment().toISOString(),
+                periodStartDate: moment().toISOString(),
+                periodEndDate: moment().toISOString(),
+                inService: true,
+                leaveOfAbsence: true,
+                closed: true,
+                retirement: true,
+
+                /** Quick search tab options */
+                showAllReferableEmployee: true,
+                showOnlyMe: true,
+                showSameWorkplace: true,
+                showSameWorkplaceAndChild: true,
+
+                /** Advanced search properties */
+                showEmployment: true,
+                showWorkplace: true,
+                showClassification: true,
+                showJobTitle: true,
+                showWorktype: true,
+                isMutipleCheck: true,
+                /**
+                 * Self-defined function: Return data from CCG001
+                 * @param: data: the data return from CCG001
+                 */
+                returnDataFromCcg001: function(data: Ccg001ReturnedData) {
+
+                }
+            }
+
+            $('#com-ccg001').ntsGroupComponent(self.ccg001ComponentOption);
+
+        }
+    }
+    export interface GroupOption {
+        /** Common properties */
+        showEmployeeSelection?: boolean; // 検索タイプ
+        systemType: number; // システム区分
+        showQuickSearchTab?: boolean; // クイック検索
+        showAdvancedSearchTab?: boolean; // 詳細検索
+        showBaseDate?: boolean; // 基準日利用
+        showClosure?: boolean; // 就業締め日利用
+        showAllClosure?: boolean; // 全締め表示
+        showPeriod?: boolean; // 対象期間利用
+        periodFormatYM?: boolean; // 対象期間精度
+        isInDialog?: boolean;
+        tabindex: number;
+
+        /** Required parameter */
+        baseDate?: string; // 基準日
+        periodStartDate?: string; // 対象期間開始日
+        periodEndDate?: string; // 対象期間終了日
+        inService: boolean; // 在職区分
+        leaveOfAbsence: boolean; // 休職区分
+        closed: boolean; // 休業区分
+        retirement: boolean; // 退職区分
+
+        /** Quick search tab options */
+        showAllReferableEmployee?: boolean; // 参照可能な社員すべて
+        showOnlyMe?: boolean; // 自分だけ
+        showSameWorkplace?: boolean; // 同じ職場の社員
+        showSameWorkplaceAndChild?: boolean; // 同じ職場とその配下の社員
+
+        /** Advanced search properties */
+        showEmployment?: boolean; // 雇用条件
+        showWorkplace?: boolean; // 職場条件
+        showClassification?: boolean; // 分類条件
+        showJobTitle?: boolean; // 職位条件
+        showWorktype?: boolean; // 勤種条件
+        isMutipleCheck?: boolean; // 選択モード
+        isTab2Lazy?: boolean;
+
+        /** Data returned */
+        returnDataFromCcg001: (data: Ccg001ReturnedData) => void;
+    }
+    export interface EmployeeSearchDto {
+        employeeId: string;
+        employeeCode: string;
+        employeeName: string;
+        workplaceId: string;
+        workplaceName: string;
+    }
+    export interface Ccg001ReturnedData {
+        baseDate: string; // 基準日
+        closureId?: number; // 締めID
+        periodStart: string; // 対象期間（開始)
+        periodEnd: string; // 対象期間（終了）
+        listEmployee: Array<EmployeeSearchDto>; // 検索結果
+    }
+
+}
