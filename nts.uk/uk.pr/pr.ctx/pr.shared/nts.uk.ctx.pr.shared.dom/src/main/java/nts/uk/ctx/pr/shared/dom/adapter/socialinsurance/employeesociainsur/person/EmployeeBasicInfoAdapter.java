252979a813d8f0bccd8c1926805214c24bfdd314
tai-tt <tai.tt@3si.vn>
2019-08-19 09:48:17 +0700

    [QSI001-ScreenB] Update code

--- nts.uk/uk.pr/pr.ctx/pr.shared/nts.uk.ctx.pr.shared.dom/src/main/java/nts/uk/ctx/pr/shared/dom/adapter/socialinsurance/employeesociainsur/person/EmployeeBasicInfoAdapter.java
+++
@@ -1,5 +0,0 @@
-package nts.uk.ctx.pr.shared.dom.adapter.socialinsurance.employeesociainsur.person;
-
-public interface EmployeeBasicInfoAdapter {
-    EmployeeBasicInfo getPersonInfo(String sID);
-}
