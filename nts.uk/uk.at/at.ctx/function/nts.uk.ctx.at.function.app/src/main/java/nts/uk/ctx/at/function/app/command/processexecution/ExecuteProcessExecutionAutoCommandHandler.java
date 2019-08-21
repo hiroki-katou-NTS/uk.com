c9bfba07366a06a7179c57ed3f8d20490f5b5df9
NSVN-TrieuKhacTu <tu_tk@nittsusystem-vn.com>
2019-08-21 10:00:02 +0700

    resolve conflict ExecuteProcessExecutionAutoCommandHandler.java

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.app/src/main/java/nts/uk/ctx/at/function/app/command/processexecution/ExecuteProcessExecutionAutoCommandHandler.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.app/src/main/java/nts/uk/ctx/at/function/app/command/processexecution/ExecuteProcessExecutionAutoCommandHandler.java
@@ -2891,16 +2891,18 @@ public class ExecuteProcessExecutionAutoCommandHandler extends AsyncCommandHandl
 		}
 		List<ErrMessageInfo> errMessageInfos = this.errMessageInfoRepository
 				.getAllErrMessageInfoByEmpID(empCalAndSumExeLog.getEmpCalAndSumExecLogID());
-		List<String> errorMessage = errMessageInfos.stream().map(error -> {
-			return error.getMessageError().v();
-		}).collect(Collectors.toList());
 
 		if ("日別作成".equals(typeExecution)) {
-			if(!errorMessage.isEmpty()) {
+
+			if (!errMessageInfos.stream()
+					.filter(c -> c.getExecutionContent().value == ExecutionContent.DAILY_CREATION.value)
+					.collect(Collectors.toList()).isEmpty()) {
 				throw new CreateDailyException(null);
 			}
 		} else {
-			if (isInterrupt) {
+			if (!errMessageInfos.stream()
+					.filter(c -> c.getExecutionContent().value == ExecutionContent.DAILY_CALCULATION.value)
+					.collect(Collectors.toList()).isEmpty()) {
 				throw new DailyCalculateException(null);
 			}
 		}
