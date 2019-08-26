92a4da9c16a864bc5d96d05f3347f90216038320
NTS_DoThiDu <du_dt@nittsusystem-vn.com>
2019-08-26 14:26:27 +0900

    fix bug 108829

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/appreflectprocess/service/workchange/WorkChangeReflectServiceScheImpl.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/appreflectprocess/service/workchange/WorkChangeReflectServiceScheImpl.java
@@ -1,6 +1,8 @@
 package nts.uk.ctx.at.schedule.dom.appreflectprocess.service.workchange;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
@@ -9,9 +11,21 @@ import nts.uk.ctx.at.schedule.dom.appreflectprocess.service.CommonReflectParamSc
 import nts.uk.ctx.at.schedule.dom.appreflectprocess.service.UpdateScheCommonAppRelect;
 import nts.uk.ctx.at.schedule.dom.schedule.basicschedule.BasicSchedule;
 import nts.uk.ctx.at.schedule.dom.schedule.basicschedule.BasicScheduleRepository;
+import nts.uk.ctx.at.schedule.dom.schedule.basicschedule.service.servicedto.TimeReflectScheDto;
+import nts.uk.ctx.at.schedule.dom.schedule.basicschedule.workscheduletimezone.BounceAtr;
+import nts.uk.ctx.at.schedule.dom.schedule.basicschedule.workscheduletimezone.WorkScheduleTimeZone;
+import nts.uk.ctx.at.schedule.dom.schedule.workschedulestate.ScheduleEditState;
 import nts.uk.ctx.at.schedule.dom.schedule.workschedulestate.WorkScheduleState;
 import nts.uk.ctx.at.schedule.dom.schedule.workschedulestate.WorkScheduleStateRepository;
+import nts.uk.ctx.at.shared.dom.schedule.basicschedule.BasicScheduleService;
+import nts.uk.ctx.at.shared.dom.schedule.basicschedule.WorkStyle;
+import nts.uk.ctx.at.shared.dom.worktime.predset.TimezoneUse;
+import nts.uk.ctx.at.shared.dom.worktime.predset.UseSetting;
+import nts.uk.ctx.at.shared.dom.worktime.worktimeset.WorkTimeSettingService;
+import nts.uk.ctx.at.shared.dom.worktime.worktimeset.internal.PredetermineTimeSetForCalc;
 import nts.uk.ctx.at.shared.dom.worktype.service.WorkTypeIsClosedService;
+import nts.uk.shr.com.context.AppContexts;
+import nts.uk.shr.com.time.TimeWithDayAttr;
 @Stateless
 public class WorkChangeReflectServiceScheImpl implements WorkChangeReflectServiceSche{
 	@Inject
@@ -22,6 +36,10 @@ public class WorkChangeReflectServiceScheImpl implements WorkChangeReflectServic
 	private WorkTypeIsClosedService workTypeRepo;
 	@Inject
 	private WorkScheduleStateRepository workScheReposi;
+	@Inject
+	private BasicScheduleService basicService;
+	@Inject
+	private WorkTimeSettingService workTimeService;
 	@Override
 	public void reflectWorkChange(WorkChangecommonReflectParamSche workchangeParam) {		
 		CommonReflectParamSche param = workchangeParam.getCommon();
@@ -35,7 +53,47 @@ public class WorkChangeReflectServiceScheImpl implements WorkChangeReflectServic
 				&& workTypeRepo.checkHoliday(scheData.getWorkTypeCode())) {
 			return;
 		}
-		updateSche.updateScheWorkTimeType(scheData, lstState, param.getWorktypeCode(), param.getWorkTimeCode());	
+		//勤務種類、就業時間帯を反映
+		updateSche.updateScheWorkTimeType(scheData, lstState, param.getWorktypeCode(), param.getWorkTimeCode());
+		List<WorkScheduleState> lstTimeState = lstState.stream().filter(x -> (x.getScheduleItemId() == 3 || x.getScheduleItemId() == 4)
+				&& x.getScheduleEditState() != ScheduleEditState.REFLECT_APPLICATION).collect(Collectors.toList());
+		if(param.getStartTime() == null && param.getEndTime() == null) {
+			WorkStyle checkworkDay = basicService.checkWorkDay(param.getWorktypeCode());
+			if(checkworkDay != WorkStyle.ONE_DAY_REST) {
+				List<WorkScheduleTimeZone> workScheduleTimeZones = scheData.getWorkScheduleTimeZones().stream()
+						.filter(x -> x.getScheduleCnt() == 1).collect(Collectors.toList());
+				if(workScheduleTimeZones.isEmpty()
+						|| (!workScheduleTimeZones.isEmpty() && lstTimeState.isEmpty())) {
+					//所定時間帯を取得する
+					PredetermineTimeSetForCalc getTimezone = workTimeService.getPredeterminedTimezone(AppContexts.user().companyId(), 
+							param.getWorkTimeCode(),
+							param.getWorktypeCode(),
+							1);
+					List<TimezoneUse> lstTimeZone = getTimezone.getTimezones().stream()
+							.filter(x -> x.getUseAtr() == UseSetting.USE && x.getWorkNo() == 1)
+							.collect(Collectors.toList());
+					if(!lstTimeZone.isEmpty()) {
+						TimezoneUse timeZone = lstTimeZone.get(0);
+						WorkScheduleTimeZone timeZoneData = new WorkScheduleTimeZone(1, new TimeWithDayAttr(timeZone.getStart().v()),
+								new TimeWithDayAttr(timeZone.getEnd().v()),
+								BounceAtr.DIRECT_BOUNCE);
+						workScheduleTimeZones.stream().forEach(x -> scheData.getWorkScheduleTimeZones().remove(x));
+						scheData.getWorkScheduleTimeZones().add(timeZoneData);
+					}
+				}
+			}
+		}
+		//出退勤時刻を反映
+		if(param.getStartTime() != null && param.getEndTime() != null) {
+			// 開始・終了時刻
+			updateSche.updateStartTimeRflect(new TimeReflectScheDto(param.getEmployeeId(),
+					param.getAppDate(),
+					param.getStartTime(),
+					param.getEndTime(),
+					1,
+					true,
+					true), scheData, lstState);
+		}
 		scheRepo.update(scheData);
 		workScheReposi.updateOrInsert(lstState);
 	}
