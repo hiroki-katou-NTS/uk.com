05682e16f9ba9d24c7c36b700f861c3187888b1d
Nguyễn Thị Hòa <hoaphucnguyen95@gmail.com>
2019-08-27 10:23:04 +0700

    hoant2 : update code file qsi013.vm.ts, model.ts

--- nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qsi/013/share/model.ts
+++ nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qsi/013/share/model.ts
@@ -11,4 +11,65 @@ module nts.uk.pr.view.qsi013.share.model {
         NEW = 0,
         UPDATE = 1
     }
+
+    export class ItemModel {
+        code: string;
+        name: string;
+
+        constructor(code: string, name: string) {
+            this.code = code;
+            this.name = name;
+        }
+    }
+
+    export enum ReasonsForLossHealthyIns {
+        BLANK = 0,
+        RETIREMENT = 4,
+        DEATH = 5,
+        ONLY_HEALTHY_INSURANCE = 7,
+        DISABILITY_AUTHORIZATION = 9
+    }
+
+    export enum ReasonsForLossPensionIns {
+        BLANK = 0,
+        RETIREMENT = 4,
+        DEATH = 5,
+        ONLY_HEALTHY_INSURANCE = 6
+    }
+
+    export interface EmployeeModel {
+        id: string;
+        code: string;
+        businessName: string;
+        depName?: string;
+        workplaceName?: string;
+    }
+
+    export class SystemType {
+        static EMPLOYMENT = 1;
+        static SALARY = 2;
+        static PERSONNEL = 3;
+        static ACCOUNTING = 4;
+        static OH = 6;
+    }
+
+    //for combo box
+    export function getCauseTypeHealthLossInfo(): Array<ItemModel> {
+        return [
+            new ItemModel(ReasonsForLossHealthyIns.BLANK.toString(), getText('Enum_ReasonsForLossHealthyIns_BLANK')),
+            new ItemModel(ReasonsForLossHealthyIns.RETIREMENT.toString(), getText('Enum_ReasonsForLossHealthyIns_RETIREMENT')),
+            new ItemModel(ReasonsForLossHealthyIns.DEATH.toString(), getText('Enum_ReasonsForLossHealthyIns_DEATH')),
+            new ItemModel(ReasonsForLossHealthyIns.ONLY_HEALTHY_INSURANCE.toString(), getText('Enum_ReasonsForLossHealthyIns_ONLY_HEALTHY_INSURANCE')),
+            new ItemModel(ReasonsForLossHealthyIns.DISABILITY_AUTHORIZATION.toString(), getText('Enum_ReasonsForLossHealthyIns_DISABILITY_AUTHORIZATION'))
+        ];
+    }
+
+    export function getCauseTypePensionLossInfo(): Array<ItemModel> {
+        return [
+            new ItemModel(ReasonsForLossPensionIns.BLANK.toString(), getText('Enum_ReasonsForLossPensionIns_BLANK')),
+            new ItemModel(ReasonsForLossPensionIns.RETIREMENT.toString(), getText('Enum_ReasonsForLossPensionIns_RETIREMENT')),
+            new ItemModel(ReasonsForLossPensionIns.DEATH.toString(), getText('Enum_ReasonsForLossPensionIns_DEATH')),
+            new ItemModel(ReasonsForLossPensionIns.ONLY_HEALTHY_INSURANCE.toString(), getText('Enum_ReasonsForLossPensionIns_ONLY_PENSION_INSURANCE'))
+        ];
+    }
 }
