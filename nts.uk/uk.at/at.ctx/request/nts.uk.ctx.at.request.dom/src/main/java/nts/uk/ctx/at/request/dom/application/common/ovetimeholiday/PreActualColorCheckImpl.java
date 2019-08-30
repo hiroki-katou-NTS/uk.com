d087a81b4ba56d8c1208aec63190ff3e5d9e62c0
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2019-08-30 15:07:10 +0700

    sửa lỗi hiển thị màu vượt quá cho KAFS05

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/common/ovetimeholiday/PreActualColorCheckImpl.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/common/ovetimeholiday/PreActualColorCheckImpl.java
@@ -389,15 +389,11 @@ public class PreActualColorCheckImpl implements PreActualColorCheck {
 		}
 		// 事前申請超過チェックをする必要があるかチェックする
 		if(preAppSetCheck==UseAtr.USE){
-			if(null != opAppBefore && opAppBefore.isPresent()){
-				// 事前申請超過チェック
-				if(overtimeColorCheck.appTime !=null && overtimeColorCheck.appTime > compareValue){
-					overtimeColorCheck.preAppError = PreActualError.PRE_ERROR.value;
-				} else {
-					overtimeColorCheck.preAppError = PreActualError.NO_ERROR.value;
-				}
-			} else {
+			// 事前申請超過チェック
+			if(overtimeColorCheck.appTime !=null && overtimeColorCheck.appTime > compareValue){
 				overtimeColorCheck.preAppError = PreActualError.PRE_ERROR.value;
+			} else {
+				overtimeColorCheck.preAppError = PreActualError.NO_ERROR.value;
 			}
 		}
 	}
@@ -419,23 +415,15 @@ public class PreActualColorCheckImpl implements PreActualColorCheck {
 		}
 		// 実績超過チェックをする必要があるかチェックする
 		if(actualSetCheck!=AppDateContradictionAtr.NOTCHECK){
-			if(!CollectionUtil.isEmpty(actualLst)){
-				// 実績時間チェック
-				if(overtimeColorCheck.appTime!=null && overtimeColorCheck.appTime > compareValue){
-					if(actualSetCheck==AppDateContradictionAtr.CHECKNOTREGISTER) {
-						overtimeColorCheck.actualError = PreActualError.ACTUAL_ERROR.value;
-					} else {
-						overtimeColorCheck.actualError = PreActualError.ACTUAL_ALARM.value;
-					}
-				} else {
-					overtimeColorCheck.actualError = PreActualError.NO_ERROR.value;
-				}
-			} else {
+			// 実績時間チェック
+			if(overtimeColorCheck.appTime!=null && overtimeColorCheck.appTime > compareValue){
 				if(actualSetCheck==AppDateContradictionAtr.CHECKNOTREGISTER) {
 					overtimeColorCheck.actualError = PreActualError.ACTUAL_ERROR.value;
 				} else {
 					overtimeColorCheck.actualError = PreActualError.ACTUAL_ALARM.value;
-				} 
+				}
+			} else {
+				overtimeColorCheck.actualError = PreActualError.NO_ERROR.value;
 			}
 		}
 	}
