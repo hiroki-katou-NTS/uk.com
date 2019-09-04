171ac1d73ea7dcc19803ca2f7f6c65b129c391cc
tai-tt <tai.tt@3si.vn>
2019-09-04 14:25:42 +0700

    [QSI001-SreenB] Update code

--- nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.ac/src/main/java/nts/uk/ctx/pr/core/ac/employee/person/PersonInfoImpl.java
+++ nts.uk/uk.pr/pr.ctx/pr.core/nts.uk.ctx.pr.core.ac/src/main/java/nts/uk/ctx/pr/core/ac/employee/person/PersonInfoImpl.java
@@ -12,7 +12,7 @@ import javax.inject.Inject;
 public class PersonInfoImpl implements PersonInfoAdapter {
 
     @Inject
-    IPersonInfoPub repo;
+    private IPersonInfoPub repo;
 
     @Override
     public PersonInfoExportAdapter getPersonInfo(String sID) {
