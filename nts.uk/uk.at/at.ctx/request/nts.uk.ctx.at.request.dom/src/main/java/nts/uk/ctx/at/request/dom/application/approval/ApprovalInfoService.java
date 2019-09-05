5a855267ca73ec566de628c2a5803a36e7f6f296
NSVN-TranThiHoa <hoa_tt@nittsusystem-vn.com>
2019-09-05 16:25:01 +0700

    CCGS33: スマホ対応

---
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/approval/ApprovalInfoService.java
@@ -0,0 +1,39 @@
+package nts.uk.ctx.at.request.dom.application.approval;
+
+import java.util.Optional;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import nts.uk.ctx.at.request.dom.application.ApplicationRepository_New;
+import nts.uk.ctx.at.request.dom.application.Application_New;
+import nts.uk.ctx.at.request.dom.application.common.adapter.workflow.ApprovalRootStateAdapter;
+
+@Stateless
+public class ApprovalInfoService implements ApprovalInfoInterface{
+	@Inject
+	private ApprovalRootStateAdapter apprRootStateAd;
+	@Inject
+	private ApplicationRepository_New appRepo;
+	/**
+	 * 利用者の判定
+	 * @param 会社ID companyId
+	 * @param ドメインモデル「申請」．ID appId
+	 * @param ログイン者の社員ID sId
+	 * @return
+	 */
+	@Override
+	public JudgmentUserAtr judgmentUser(String companyId, String appId, String sId){
+		//アルゴリズム「指定した社員が承認者であるかの判断」を実行する (Thực hiện [kiểm tra xem employee đã chỉ định có phải là approver ko])
+		Boolean	approverFlag = apprRootStateAd.judgmentTargetPersonIsApprover(companyId, appId, sId);
+		Optional<Application_New> app = appRepo.findByID(companyId, appId);
+		if(!app.isPresent()) return JudgmentUserAtr.OTHER;
+		String applicantID = app.get().getEmployeeID();
+		if(approverFlag){//承認者フラグがtrue TH co the approve
+			if(sId.equals(applicantID)) return JudgmentUserAtr.ALL;
+			return JudgmentUserAtr.APPROVER;
+		}
+		if(sId.equals(applicantID)) return JudgmentUserAtr.APPLICATION;
+		return JudgmentUserAtr.OTHER;
+	};
+}
--- nts.uk/uk.mobile/ClientApp/views/ccg/index.ts
+++ nts.uk/uk.mobile/ClientApp/views/ccg/index.ts
@@ -1,3 +1,4 @@
 
 import './007';
 import './008';
+import './033';
