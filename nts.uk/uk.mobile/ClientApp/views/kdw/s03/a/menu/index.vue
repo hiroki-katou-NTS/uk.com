a7dbba61d944545459b10993fe2637adf68dd132
ductm88 <zinzoo88@gmail.com>
2019-09-12 13:30:53 +0700

    create kdws03

--- nts.uk/uk.mobile/ClientApp/views/kdw/s03/a/menu/index.vue
+++ nts.uk/uk.mobile/ClientApp/views/kdw/s03/a/menu/index.vue
@@ -1,10 +1,18 @@
 <template>
-<div class="kdws03a">
-    <button type="button" class="btn btn-primary btn-block btn-lg">{{'KDWS03_24' | i18n}}</button>
-    <button type="button" class="btn btn-primary btn-block btn-lg">{{'KDWS03_25' | i18n}}</button>
-    <button type="button" class="btn btn-primary btn-block btn-lg" v-on:click="openKdws03c" v-bind:disabled="errorScreen">{{'KDWS03_30' | i18n}}</button>
-    <button type="button" class="btn btn-primary btn-block btn-lg">{{'KDWS03_31' | i18n}}</button>
-    <button type="button" class="btn btn-primary btn-block btn-lg">{{'KDWS03_32' | i18n}}</button>
-    <button type="button" class="btn btn-primary btn-block btn-lg">{{'KDWS03_33' | i18n}}</button>
-</div>
+    <div class="kdws03a">
+        <ul class="list-group border-0">
+            <li class="list-group-item border-0">
+                <button type="button" class="btn btn-primary w-100 btn-lg">{{'KDWS03_24' | i18n}}</button></li>
+            <li class="list-group-item border-0">
+                <button type="button" class="btn btn-primary w-100 btn-lg">{{'KDWS03_25' | i18n}}</button></li>
+            <li class="list-group-item border-0">
+                <button type="button" class="btn btn-primary w-100 btn-lg" v-on:click="openKdws03c" v-bind:disabled="errorScreen">{{'KDWS03_30' | i18n}}</button></li>
+            <li class="list-group-item border-0">
+                <button type="button" class="btn btn-primary w-100 btn-lg">{{'KDWS03_31' | i18n}}</button></li>
+            <li class="list-group-item border-0">
+                <button type="button" class="btn btn-primary w-100 btn-lg">{{'KDWS03_32' | i18n}}</button></li>
+            <li class="list-group-item border-0">
+                <button type="button" class="btn btn-primary w-100 btn-lg">{{'KDWS03_33' | i18n}}</button></li>
+        </ul>
+    </div>
 </template>
