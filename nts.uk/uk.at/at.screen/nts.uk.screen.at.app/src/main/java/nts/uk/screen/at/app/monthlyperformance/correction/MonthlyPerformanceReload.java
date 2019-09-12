1150ed56d92a83c0fa9c749a6a5a4eea00be7830
Nguyen Hoang Son <son_hoang@nittsusystem-vn.com>
2019-09-12 13:51:48 +0700

    fixbug 108905&108887: edit code base on new EAP

--- nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/monthlyperformance/correction/MonthlyPerformanceReload.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/monthlyperformance/correction/MonthlyPerformanceReload.java
@@ -357,7 +357,7 @@ public class MonthlyPerformanceReload {
 		Optional<StatusConfirmMonthDto> statusConfirmMonthDto = confirmStatusMonthly.getConfirmStatusMonthly(companyId, closureId, screenDto.getClosureDate().toDomain(), listEmployeeIds, YearMonth.of(yearMonth),monthlyResults);
 
 		//[No.587]月の実績の承認状況を取得する
-		Optional<ApprovalStatusMonth> approvalStatusMonth =  approvalStatusMonthly.getApprovalStatusMonthly(companyId, loginId, closureId, screenDto.getClosureDate().toDomain(), listEmployeeIds, YearMonth.of(yearMonth),monthlyResults);
+		Optional<ApprovalStatusMonth> approvalStatusMonth =  approvalStatusMonthly.getApprovalStatusMonthly(companyId, loginId, closureId, listEmployeeIds, YearMonth.of(yearMonth),monthlyResults);
 		
 		List<MPDataDto> lstData = new ArrayList<>(); // List all data
 		List<MPCellStateDto> lstCellState = new ArrayList<>(); // List cell state
