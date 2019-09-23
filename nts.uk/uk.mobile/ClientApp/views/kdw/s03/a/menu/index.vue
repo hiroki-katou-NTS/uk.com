7d44ea01c1fc8e34b61bd0b6a38f88d9a879e0bb
ductm88 <zinzoo88@gmail.com>
2019-09-23 14:23:31 +0700

    fix bug

--- nts.uk/uk.mobile/ClientApp/views/kdw/s03/a/menu/index.vue
+++ nts.uk/uk.mobile/ClientApp/views/kdw/s03/a/menu/index.vue
@@ -4,17 +4,17 @@
         </div>
         <ul class="list-group border-0">
             <li class="list-group-item border-0" v-if="params.allConfirmButtonDis">
-                <button type="button" class="btn btn-primary w-100 btn-lg" v-click:500="processConfirmAll('confirm')">{{'KDWS03_24' | i18n}}</button></li>
+                <button type="button" class="btn btn-primary w-100 btn-lg" v-on:click="processConfirmAll('confirm')">{{'KDWS03_24' | i18n}}</button></li>
             <li class="list-group-item border-0" v-if="params.allConfirmButtonDis">
-                <button type="button" class="btn btn-primary w-100 btn-lg" v-click:500="processConfirmAll('remove')">{{'KDWS03_25' | i18n}}</button></li>
+                <button type="button" class="btn btn-primary w-100 btn-lg" v-on:click="processConfirmAll('remove')">{{'KDWS03_25' | i18n}}</button></li>
             <li class="list-group-item border-0" v-if="params.errorReferButtonDis">
                 <button type="button" class="btn btn-primary w-100 btn-lg" v-click:500="openErrorList">{{'KDWS03_30' | i18n}}</button></li>
             <li class="list-group-item border-0" v-if="params.restReferButtonDis">
-                <button type="button" class="btn btn-primary w-100 btn-lg" v-click:500="openKdws03g(0)">{{'KDWS03_31' | i18n}}</button></li>
+                <button type="button" class="btn btn-primary w-100 btn-lg" v-on:click="openKdws03g(0)">{{'KDWS03_31' | i18n}}</button></li>
             <li class="list-group-item border-0" v-if="params.monthActualReferButtonDis">
                 <button type="button" class="btn btn-primary w-100 btn-lg" v-click:500="openKdws03f">{{'KDWS03_32' | i18n}}</button></li>
             <li class="list-group-item border-0" v-if="params.timeExcessReferButtonDis">
-                <button type="button" class="btn btn-primary w-100 btn-lg" v-click:500="openKdws03g(1)">{{'KDWS03_33' | i18n}}</button></li>
+                <button type="button" class="btn btn-primary w-100 btn-lg" v-on:click="openKdws03g(1)">{{'KDWS03_33' | i18n}}</button></li>
         </ul>
     </div>
 </template>
--- nts.uk/uk.mobile/ClientApp/views/kdw/s03/c/index.vue
+++ nts.uk/uk.mobile/ClientApp/views/kdw/s03/c/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="kdws03c">
         <div class="modal-header uk-bg-white-smoke pl-0 pr-0 rounded-0">
-            <div class="row uk-text-quote w-100 ml-0 mr-0" v-click:500="$close('NotCloseMenu')">
+            <div class="row uk-text-quote w-100 ml-0 mr-0" v-on:click="$close('NotCloseMenu')">
                 <div class="col-1 pl-1"><i class="fas fa-arrow-left"></i></div>
                 <div class="col-11 pl-0 pr-2"><span>{{'KDWS03_6' | i18n}}</span></div>
             </div>
@@ -13,7 +13,7 @@
     
             <div class="card-body">
                 <ul class="list-group list-group-selection">
-                    <li class="list-group-item" v-for="item in displayData" v-click:500="openErrorList(item.employeeId)">{{item.employeeName}}</li>
+                    <li class="list-group-item" v-for="item in displayData" v-on:click="openErrorList(item.employeeId)">{{item.employeeName}}</li>
                 </ul>
             </div>
         </div>
