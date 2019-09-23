9e3ca88bb2a55873e2c0b629dc37d13a26cbb2f1
ductm88 <zinzoo88@gmail.com>
2019-09-23 13:15:35 +0700

    create kdws03

--- nts.uk/uk.mobile/ClientApp/views/kdw/s03/a/index.vue
+++ nts.uk/uk.mobile/ClientApp/views/kdw/s03/a/index.vue
@@ -52,10 +52,10 @@
                         {{cell.value == '0:00' || cell.value == '0.0' || cell.value == '0' ? '' : cell.value}}
                     </td>
                     <td>
-                        <div style="text-align: right">
+                        <div style="text-align: right" v-on:click="openEdit(row.id)">
                             <span style="color: red" class="fa fa-exclamation-circle fa-lg" v-if="null != row.ERAL && row.ERAL.includes('ER')"></span>
                             <span style="color: red" class="fa fa-exclamation-triangle fa-lg uk-text-yellow" v-if="null != row.ERAL && !row.ERAL.includes('ER') && row.ERAL.includes('AL')"></span>
-                            <span class="pl-1" v-on:click="openEdit(row.id)" v-if="row.date != ''">></span>
+                            <span class="pl-1" v-if="row.date != ''">></span>
                         </div>
                     </td>
                 </tr>
@@ -83,10 +83,10 @@
                         {{cell.value == '0:00' || cell.value == '0.0' ? '' : cell.value}}
                     </td>
                     <td>
-                        <div style="text-align: right">
+                        <div style="text-align: right" v-on:click="openEdit(row.id)">
                             <span style="color: red" class="fa fa-exclamation-circle fa-lg" v-if="null != row.ERAL && row.ERAL.includes('ER')"></span>
                             <span style="color: red" class="fa fa-exclamation-triangle fa-lg" v-if="null != row.ERAL && !row.ERAL.includes('ER') && row.ERAL.includes('AL')"></span>
-                            <span class="pl-1" v-on:click="openEdit(row.id)" v-if="row.employeeNameDis != ''">></span>
+                            <span class="pl-1" v-if="row.employeeNameDis != ''">></span>
                         </div>
                     </td>
                 </tr>
