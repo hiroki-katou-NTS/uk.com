5b5c1c58c6ef27006d8cca418b0901c16a2e1b35
3SI\tam.nx <>
2019-09-29 20:58:54 +0700

    #8134

--- nts.uk/uk.pr/pr.file/nts.uk.file.pr.infra/src/main/java/nts/uk/file/pr/infra/core/socinsurnoticreset/NotificationOfLossInsCSVAposeFileGenerator.java
+++ nts.uk/uk.pr/pr.file/nts.uk.file.pr.infra/src/main/java/nts/uk/file/pr/infra/core/socinsurnoticreset/NotificationOfLossInsCSVAposeFileGenerator.java
@@ -129,18 +129,18 @@ public class NotificationOfLossInsCSVAposeFileGenerator extends AsposeCellsRepor
                 data.getBasicPenNumber().length() > 4 ? data.getBasicPenNumber().substring(4,data.getBasicPenNumber().length()) : "" : "");
         cells.get(startRow, 13).setValue(9);
         cells.get(startRow, 14).setValue(data.getEndDate().substring(0,4) + data.getEndDate().substring(5,7) + data.getEndDate().substring(8,10));
-        cells.get(startRow, 15).setValue(data.getCause() == null ? "" : data.getCause());
+        cells.get(startRow, 15).setValue(ins.getBusinessArrSymbol() == BussEsimateClass.HEAL_INSUR_OFF_ARR_SYMBOL ? data.getCause() == null ? "" : data.getCause() : data.getCause2() == null ? "": data.getCause2());
         cells.get(startRow, 16).setValue(9);
-        cells.get(startRow, 17).setValue(data.getCause() == null ? ""  : data.getCause() == 4 && data.getCause() == 5 ? data.getEndDate().substring(0,4) + data.getEndDate().substring(5,7) + data.getEndDate().substring(8,10) : "");
+        cells.get(startRow, 17).setValue(data.getCause() == null ? ""  : data.getCause() == 4 && data.getCause() == 5 ? ins.getBusinessArrSymbol() == BussEsimateClass.HEAL_INSUR_OFF_ARR_SYMBOL ? convertDate(data.getEndDate()) : convertDate(data.getEndDate2()) : "");
         cells.get(startRow, 18).setValue(data.getIsMoreEmp());
         cells.get(startRow, 19).setValue(data.getContinReemAfterRetirement());
-        cells.get(startRow, 20).setValue(data.getOtherReason());
-        cells.get(startRow, 21).setValue(Objects.toString(data.getCaInsurance(), ""));
-        cells.get(startRow, 22).setValue(Objects.toString(data.getCaInsurance(), ""));
-        cells.get(startRow, 23).setValue(data.getPercentOrMore());
+        cells.get(startRow, 20).setValue(ins.getBusinessArrSymbol() == BussEsimateClass.HEAL_INSUR_OFF_ARR_SYMBOL ? data.getOtherReason() : data.getOtherReason2());
+        cells.get(startRow, 21).setValue(Objects.toString(ins.getBusinessArrSymbol() == BussEsimateClass.HEAL_INSUR_OFF_ARR_SYMBOL ? data.getCaInsurance() : data.getCaInsurance2(), ""));
+        cells.get(startRow, 22).setValue(Objects.toString(ins.getBusinessArrSymbol() == BussEsimateClass.HEAL_INSUR_OFF_ARR_SYMBOL ? data.getCaInsurance() : data.getCaInsurance2(), ""));
+        cells.get(startRow, 23).setValue(data.getPercentOrMore().equals("1") ? 1 : "");
         cells.get(startRow, 24).setValue(data.getPercentOrMore().equals("1") ? 9 : "");
-        cells.get(startRow, 25).setValue(data.getCause() == null ? "" : data.getCause() == 1 ? convertDate(data.getEndDate()) : "");
-        cells.get(startRow, 26).setValue(data.getCause() == null ? "" : data.getCause() == 6 ? "" : 1);
+        cells.get(startRow, 25).setValue(ins.getBusinessArrSymbol() == BussEsimateClass.HEAL_INSUR_OFF_ARR_SYMBOL ? data.getCause() == null ? "" : data.getCause() == 1 ? convertDate(data.getEndDate()) : data.getCause2() == null ? data.getCause2() == 1 ? convertDate(data.getEndDate2()) : "" : "" : "");
+        cells.get(startRow, 26).setValue(data.getCause2() == null ? "" : data.getCause2() == 6 ? "" : 1);
 
     }
 
@@ -317,7 +317,7 @@ public class NotificationOfLossInsCSVAposeFileGenerator extends AsposeCellsRepor
         cells.get(startRow, 14).setValue(data.getEndDate().substring(0,4) + data.getEndDate().substring(5,7) + data.getEndDate().substring(8,10));
         cells.get(startRow, 15).setValue(data.getCause());
         cells.get(startRow, 16).setValue(9);
-        cells.get(startRow, 17).setValue(data.getCause() == 4 && data.getCause() == 5 ? data.getEndDate().substring(0,4) + data.getEndDate().substring(5,7) + data.getEndDate().substring(8,10) : "");
+        cells.get(startRow, 17).setValue(data.getCause() == 4 && data.getCause() == 5 ? convertDate(data.getEndDate()) : "");
         cells.get(startRow, 18).setValue(data.getIsMoreEmp());
         cells.get(startRow, 19).setValue(data.getContinReemAfterRetirement());
         cells.get(startRow, 20).setValue(data.getOtherReason());
