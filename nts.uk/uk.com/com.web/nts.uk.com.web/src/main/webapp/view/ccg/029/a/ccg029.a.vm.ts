78c3b4744a49bd2412063504c0824e4f07fce022
NSVN-PhamVanThanh <thanh_pv@nittsusystem-vn.com>
2019-10-04 13:34:54 +0700

    Ui done

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/029/a/ccg029.a.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/029/a/ccg029.a.vm.ts
@@ -9,17 +9,22 @@ module nts.uk.hr.view.jhc002.a.viewmodel {
         
         btnSelectionText : KnockoutObservable<string>;
         isShowfull : KnockoutObservable<boolean>;
+        callback: () => any;
+        input: Input;
         
-        
-        constructor() {
+        constructor(param: Input, callback: () => any) {
             var self = this;
+            //param
+            self.input = new Input(param);
+            self.callback = callback;
+            
+            //control 
             self.processingDate = ko.observable(moment(new Date()).format("YYYY/MM/DD"));
             self.keySearch = ko.observable("");
             self.employeeList = [];
             self.isShowfull = ko.observable(false);
             self.btnSelectionText = getText('CCG029_A1_5','部門');
-            
-             $(".searchTips-area").ntsPopup({
+            $(".searchTips-area").ntsPopup({
                 position: {
                     my: "left top",
                     at: "left bottom",
@@ -28,11 +33,9 @@ module nts.uk.hr.view.jhc002.a.viewmodel {
                 showOnStart: false,
                 dismissible: false
             });
-            
             $("#searchTipsBtn").click(function() {
                 $(".searchTips-area").ntsPopup("toggle");
             });
-            
         }
         startPage(): JQueryPromise<any> {
             var self = this;
@@ -72,19 +75,19 @@ module nts.uk.hr.view.jhc002.a.viewmodel {
                         headerText: 'id', key: 'id', hidden: true
                     },
                     {
-                        headerText: getText('CCG029_A1_25'), key: 'employeeCode', dataType: 'string', width: '100px'
+                        headerText: getText('CCG029_A1_24'), key: 'employeeCode', dataType: 'string', width: '100px'
                     },
                     {
-                        headerText: getText('CCG029_A1_26'), key: 'employeeName', dataType: 'string', width: '100px'
+                        headerText: getText('CCG029_A1_25'), key: 'employeeName', dataType: 'string', width: '100px'
                     },
                     {
-                        headerText: getText('CCG029_A1_27'), key: 'katakanaName', dataType: 'string',  width: '100px'
+                        headerText: getText('CCG029_A1_26'), key: 'katakanaName', dataType: 'string',  width: '100px'
                     },
                     {
-                        headerText: getText('CCG029_A1_28'), key: 'departmentCode', dataType: 'string',  width: '100px'
+                        headerText: (self.input.systemType == 1 ? getText('Com_Workplace') : getText('Com_Department')) + 'コード', key: 'departmentCode', dataType: 'string',  width: '100px'
                     },
                     {
-                        headerText: getText('CCG029_A1_29'), key: 'department', dataType: 'string', width: '100px'
+                        headerText: self.input.systemType == 1 ? getText('Com_Workplace') : getText('Com_Department'), key: 'department', dataType: 'string', width: '100px'
                     }
                 ],
                 dataSource: self.employeeList,
@@ -127,11 +130,36 @@ module nts.uk.hr.view.jhc002.a.viewmodel {
         
     }
     
+    class Input {
+        systemType: number; //システム区分（0：共通、1：就業、2：給与、3：人事）
+        includePreEmployee: boolean; //入社前社員を含める
+        includeRetirement: boolean; //退職者を含める
+        includeAbsence: boolean; //休職者を含める 
+        includeClosed: boolean; //休業者を含める
+        includeTransferEmployee: boolean; //出向社員を含める
+        includeAcceptanceTransferEmployee: boolean; //受入出向社員を含める
+        getPosition: boolean; //職位を取得する
+        getEmployment: boolean; //雇用を取得する
+        getPersonalFileManagert: boolean; //個人ファイル管理を取得する
+        
+        constructor(input: any) {
+            this.systemType = input ? input.systemType || 3 : 3;
+            this.includePreEmployee = input ? input.includePreEmployee || true: true;
+            this.includeRetirement = input ? input.includeRetirement || true: true;
+            this.includeAbsence = input ? input.includeAbsence || true: true;
+            this.includeClosed = input ? input.includeClosed || true: true;
+            this.includeTransferEmployee = input ? input.includeTransferEmployee || true: true;
+            this.includeAcceptanceTransferEmployee = input ? input.includeAcceptanceTransferEmployee || true: true;
+            this.getPosition = input ? input.getPosition || false: false;
+            this.getEmployment = input ? input.getEmployment || false: false;
+            this.getPersonalFileManagert = input ? input.getPersonalFileManagert || false: false;
+        }
+    }
+    
     class CareerType {
         id: string;
         code: string;
         name: string;
-        enable: KnockoutObservable<boolean> = ko.observable(false);
         constructor(obj: any) {
             this.id = obj.id
             this.code = obj.code;
