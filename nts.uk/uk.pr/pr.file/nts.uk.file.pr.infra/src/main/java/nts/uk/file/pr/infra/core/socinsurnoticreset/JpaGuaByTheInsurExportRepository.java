a24dc12fa8ae62c03816fdf62c5d0959f662091b
3SI\tam.nx <>
2019-10-07 16:11:17 +0700

    Update name for QSI001

--- nts.uk/uk.pr/pr.file/nts.uk.file.pr.infra/src/main/java/nts/uk/file/pr/infra/core/socinsurnoticreset/JpaGuaByTheInsurExportRepository.java
+++ nts.uk/uk.pr/pr.file/nts.uk.file.pr.infra/src/main/java/nts/uk/file/pr/infra/core/socinsurnoticreset/JpaGuaByTheInsurExportRepository.java
@@ -32,10 +32,10 @@ public class JpaGuaByTheInsurExportRepository extends JpaRepository implements G
         exportSQL.append("      WELFARE_PENSION_OFFICE_NUMBER_2,");
         exportSQL.append("      HEALTH_INSURANCE_OFFICE_NUMBER,");
         exportSQL.append("      WELFARE_PENSION_OFFICE_NUMBER,");
-        exportSQL.append("      BUSINESS_NAME,");
-        exportSQL.append("      BUSINESS_NAME_KANA,");
         exportSQL.append("      PERSON_NAME,");
         exportSQL.append("      PERSON_NAME_KANA,");
+        exportSQL.append("      TODOKEDE_FNAME,");
+        exportSQL.append("      TODOKEDE_FNAME_KANA,");
         exportSQL.append("      BIRTHDAY,");
         exportSQL.append(" 	    GENDER,");
         exportSQL.append("      COAL_MINER_ATR,");
