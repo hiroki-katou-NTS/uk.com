826d56ac86339c92d5f74f79952fb1058b4d5058
tranphuc7686@gmail.com <Tran Phuc>
2019-10-08 17:18:46 +0700

    Update focus

--- nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qsi/001/a/qsi001.a.start.ts
+++ nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qsi/001/a/qsi001.a.start.ts
@@ -2,5 +2,6 @@ module nts.uk.pr.view.qsi001.a {
     __viewContext.ready(function() {
         let  screenModel = new viewmodel.ScreenModel();
         __viewContext.bind(screenModel);
+        _.defer(() => {$('#A2_4').focus()});
     });
 }
