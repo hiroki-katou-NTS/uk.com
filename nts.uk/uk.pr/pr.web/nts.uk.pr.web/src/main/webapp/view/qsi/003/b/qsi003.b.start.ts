23a0f3f63e85e99ae5a9b6a1a40fec0fc3fff9eb
tai-tt <tai.tt@3si.vn>
2019-10-09 09:59:45 +0700

    [QSI002 - 3 ] Focus

--- nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qsi/003/b/qsi003.b.start.ts
+++ nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qsi/003/b/qsi003.b.start.ts
@@ -2,5 +2,6 @@ module nts.uk.pr.view.qsi003.b {
     __viewContext.ready(function() {
         let  screenModel = new viewmodel.ScreenModel();
         __viewContext.bind(screenModel);
+        $("#emp-component").focus();
     });
 }
