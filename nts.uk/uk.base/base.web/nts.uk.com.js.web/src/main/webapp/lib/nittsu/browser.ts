1ceeba71e0cc63db2b121a44aec6aaa664fcc7ea
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2019-10-11 09:17:59 +0700

    change size off messsage

--- nts.uk/uk.base/base.web/nts.uk.com.js.web/src/main/webapp/lib/nittsu/browser.ts
+++ nts.uk/uk.base/base.web/nts.uk.com.js.web/src/main/webapp/lib/nittsu/browser.ts
@@ -162,7 +162,7 @@ const b = nts.uk.util.browser;
 b.private.then(v => {
     if (v) {
         if (b.ios && b.version === 'Safari 10') {
-            document.body.innerHTML = '<div style="text-align: center; font-size: 5em; padding: 2em 1em;">現在ご利用の端末は、プライベートモードでは正常に動作しない可能性があります。\n通常モードで起動してください。</div>';
+            document.body.innerHTML = '<div style="text-align: center; font-size: 2em; padding: 2em 1em;">現在ご利用の端末は、プライベートモードでは正常に動作しない可能性があります。\n通常モードで起動してください。</div>';
         }
     }
 });
