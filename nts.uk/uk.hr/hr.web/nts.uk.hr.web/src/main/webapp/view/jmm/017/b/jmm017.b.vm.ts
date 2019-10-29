980122c0dea6f90d3357d4ad5cdb8d2ee535d4c1
NSVN-Leu Thi Lan <lan_lt@nittsusystem-vn.com>
2019-10-29 12:54:55 +0700

    JMM017: scroll ver 5 chưa hoàn hảo

--- nts.uk/uk.hr/hr.web/nts.uk.hr.web/src/main/webapp/view/jmm/017/b/jmm017.b.vm.ts
+++ nts.uk/uk.hr/hr.web/nts.uk.hr.web/src/main/webapp/view/jmm/017/b/jmm017.b.vm.ts
@@ -33,17 +33,48 @@ module nts.uk.hr.view.jmm017.b.viewmodel {
             self.useList = ko.observableArray([]);
             self.editTem = "<a href='#' onclick=\"editMsg({ id: '${guideMsgId}' })\">"+getText('JMM017_B422_23')+"</a>";
            
-            $("#grid").bind("iggridpagingpagesizechanging", function(c, v) {
-                let sidebarArea = $(".sidebar-html")[0].getBoundingClientRect().height, 
+//            $("#grid").bind("iggridpagingpagesizechanging", function(c, v) {
+//                let sidebarArea = $(".sidebar-html")[0].getBoundingClientRect().height,
+//                    objCalulator = self.caculator();
+//                $(".wrapScroll").css({ overflow: 'auto', height: objCalulator.contentAreaHeight + "px" });
+//                // số dòng hiện tại + 2 line header + height group + height của page
+//                let heightGrid = ($("#grid").igGrid("rows").length + 2) * 23 + 107 + 46;
+//                $("#grid").igGrid("option", "height", heightGrid);
+//                $("#grid_container").css("max-height", objCalulator.gridAreaHeight + 'px');
+//
+//            });
+//pageSizeChanged
+// $("#grid1").bind("iggridpagingpageindexchanged", handler);
+            
+            $("#grid").bind("iggridpagingpagesizechanged", function(c, v) {
+                let sidebarArea = $(".sidebar-html")[0].getBoundingClientRect().height,
                     objCalulator = self.caculator();
-                $(".wrapScroll").css({overflow: 'auto', height: objCalulator.contentAreaHeight +"px"});
-                $("#grid").igGrid("option", "height", objCalulator.gridAreaHeight);
+                $(".wrapScroll").css({ overflow: 'auto', height: objCalulator.contentAreaHeight + "px" });
+                // số dòng hiện tại + 2 line header + height group + height của page
+                let heightGrid = ($("#grid").igGrid("rows").length + 2) * 23 + 107 + 46;
+                $("#grid").igGrid("option", "height", heightGrid);
+                $("#grid_container").css("max-height", objCalulator.gridAreaHeight + 'px');
+
+            });
+            
+            $("#grid").bind("iggridpagingpageindexchanged", function(c, v) {
+                let sidebarArea = $(".sidebar-html")[0].getBoundingClientRect().height,
+                    objCalulator = self.caculator();
+                $(".wrapScroll").css({ overflow: 'auto', height: objCalulator.contentAreaHeight + "px" });
+                // số dòng hiện tại + 2 line header + height group + height của page
+                let heightGrid = ($("#grid").igGrid("rows").length + 2) * 23 + 107 + 46;
+                $("#grid").igGrid("option", "height", heightGrid);
+                $("#grid_container").css("max-height", objCalulator.gridAreaHeight + 'px');
+
             });
             
             $(window).resize(function() {
                 let objCalulator = self.caculator();
                 $(".wrapScroll").css({height: objCalulator.contentAreaHeight +"px"});
-                $("#grid").igGrid("option", "height", objCalulator.gridAreaHeight);
+                 // số dòng hiện tại + 2 line header + height group + height của page
+                let heightGrid = ($("#grid").igGrid("rows").length + 2)*23 + 107 + 46;
+                $("#grid").igGrid("option", "height", heightGrid);
+                $("#grid_container").css("max-height", objCalulator.gridAreaHeight + 'px'); 
             });
         }
         
@@ -84,24 +115,29 @@ module nts.uk.hr.view.jmm017.b.viewmodel {
                 useSet: self.useSet()==1?null:self.useSet()==2
             }
             new service.getGuideMessageList(param).done(function(data: any) {
-                let sidebarArea = $(".sidebar-html")[0].getBoundingClientRect().height, 
+                let sidebarArea = $(".sidebar-html")[0].getBoundingClientRect().height,
                     objCalulator = self.caculator();
-                $(".wrapScroll").css({overflow: "auto", height: objCalulator.contentAreaHeight +"px"});
-                $("#grid").igGrid("option", "height", objCalulator.gridAreaHeight);
+
+                //$("#grid").igGrid("option", "height", objCalulator.gridAreaHeight);
                 let groupByColumns = $("#grid").igGridGroupBy("groupByColumns");
                 self.guideMessageList = data;
                 //$('#gridContent').css( "height", "430px");
                 self.bindData();
-//                setTimeout(() => {
-                    $("#grid").igGridGroupBy("ungroupAll");
-                    _.forEach(groupByColumns, col => {
-                        $("#grid").igGridGroupBy("groupByColumn", col.key);
-                    });
-                    $("#grid").igGridPaging("pageIndex", 0);
-//                }, 1);
+                $("#grid").igGridGroupBy("ungroupAll");
+                _.forEach(groupByColumns, col => {
+                    $("#grid").igGridGroupBy("groupByColumn", col.key);
+                });
+                $("#grid").igGridPaging("pageIndex", 0);
+                // số dòng hiện tại + 2 line header + height group + height của page
+                let heightGrid = ($("#grid").igGrid("rows").length + 2) * 23 + 107 + 46;
+                $("#grid").igGrid("option", "height", heightGrid);
+                $(".wrapScroll").css({ overflow: "auto", height: objCalulator.contentAreaHeight + "px" });
+                $("#grid_container").css("max-height", objCalulator.gridAreaHeight + 'px');
+
             }).fail(function(errorInfor) {
                 error({ messageId: errorInfor.messageId });
-                let groupByColumns = $("#grid").igGridGroupBy("groupByColumns");
+                let groupByColumns = $("#grid").igGridGroupBy("groupByColumns"),
+                    objCalulator = self.caculator();
                 self.guideMessageList = [];
                 //$('#gridContent').css( "height", "200px");
                 self.bindData();
@@ -109,10 +145,25 @@ module nts.uk.hr.view.jmm017.b.viewmodel {
                 _.forEach(groupByColumns, col => {
                     $("#grid").igGridGroupBy("groupByColumn", col.key);
                 });
-                $("#grid").igGridPaging("pageIndex", 0);    
+                $("#grid").igGridPaging("pageIndex", 0);  
+                // số dòng hiện tại + 2 line header + height group + height của page
+                let heightGrid = ($("#grid").igGrid("rows").length + 2) * 23 + 107 + 46;
+                $("#grid").igGrid("option", "height", heightGrid);
+                $(".wrapScroll").css({ overflow: "auto", height: objCalulator.contentAreaHeight + "px" });
+                $("#grid_container").css("max-height", objCalulator.gridAreaHeight + 'px');
+
             }).always(function() {
                 block.clear();
             });
+        }
+        
+        setScroll() {
+            // số dòng hiện tại + 2 line header + height group + height của page
+            let heightGrid = ($("#grid").igGrid("rows").length + 2) * 23 + 107 + 46;
+            $("#grid").igGrid("option", "height", heightGrid);
+            $(".wrapScroll").css({ overflow: "auto", height: objCalulator.contentAreaHeight + "px" });
+            $("#grid_container").css("max-height", objCalulator.gridAreaHeight + 'px');
+
         }
         
         public reloadGrid(): void {
