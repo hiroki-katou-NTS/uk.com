36e6a1182f83829f5f6dd3d8662dc56362de7bba
3SI\tam.nx <>
2019-10-31 16:02:11 +0700

    #9753

--- nts.uk/uk.pr/pr.file/nts.uk.file.pr.infra/src/main/java/nts/uk/file/pr/infra/core/socinsurnoticreset/GuaByTheInsurCSVAposeFileGenerator.java
+++ nts.uk/uk.pr/pr.file/nts.uk.file.pr.infra/src/main/java/nts/uk/file/pr/infra/core/socinsurnoticreset/GuaByTheInsurCSVAposeFileGenerator.java
@@ -138,7 +138,7 @@ public class GuaByTheInsurCSVAposeFileGenerator extends AsposeCellsReportGenerat
                 columnStart = 0;
             }
             if(ins.getLineFeedCode().get() != LineFeedCode.DO_NOT_ADD) {
-                columnStart = columnStart * i;
+                columnStart = columnStart + columnStart - 1;
             }
             fillPensionEmployee(data, cells, infor, ins, startRow, startDate, endDate, company, columnStart);
 
@@ -298,8 +298,8 @@ public class GuaByTheInsurCSVAposeFileGenerator extends AsposeCellsReportGenerat
                 startRow++;
                 columnStart = 0;
             }
-            if(ins.getLineFeedCode().get() != LineFeedCode.DO_NOT_ADD) {
-                columnStart = columnStart * i;
+            if(ins.getLineFeedCode().get() == LineFeedCode.DO_NOT_ADD) {
+                columnStart = columnStart + columnStart - 1;
             }
             fillHealthInsAssociation(data, cells, infor, ins, startRow++, startDate, endDate, columnStart);
 
@@ -445,7 +445,7 @@ public class GuaByTheInsurCSVAposeFileGenerator extends AsposeCellsReportGenerat
                 startRow++;
             }
             if(ins.getLineFeedCode().get() == LineFeedCode.DO_NOT_ADD) {
-                columnStart = columnStart * i;
+                columnStart = columnStart + columnStart - 1;
             }
             fillEmpPensionFund(data, cells, infor, ins, startRow, startDate, endDate, company, columnStart);
         }
