d0a2d59ec26d74d96ced6209ca142923a6387b20
Nguyễn Thị Hòa <hoaphucnguyen95@gmail.com>
2019-11-05 11:10:23 +0700

    HoaNT2 : UPDATE SRC QSI014 ccgoo1

--- nts.uk/uk.pr/pr.file/nts.uk.file.pr.app/src/main/java/nts/uk/ctx/pr/file/app/core/socialinsurnoticreset/EmpAddChangeInfoExport.java
+++ nts.uk/uk.pr/pr.file/nts.uk.file.pr.app/src/main/java/nts/uk/ctx/pr/file/app/core/socialinsurnoticreset/EmpAddChangeInfoExport.java
@@ -14,6 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class EmpAddChangeInfoExport {
+    private String scd;
     private String empId;
     private String pId;
     private String companyId;
