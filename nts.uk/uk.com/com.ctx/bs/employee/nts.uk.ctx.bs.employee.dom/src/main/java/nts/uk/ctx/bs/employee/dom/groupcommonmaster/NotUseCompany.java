4ebc8da2f5174f956e0478d96683c1d6070999ad
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2019-11-05 15:02:45 +0700

    CMM023: ADD domain GroupCommonMaster (グループ会社共通マスタ)

---
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.dom/src/main/java/nts/uk/ctx/bs/employee/dom/groupcommonmaster/NotUseCompany.java
@@ -0,0 +1,16 @@
+package nts.uk.ctx.bs.employee.dom.groupcommonmaster;
+
+import lombok.Data;
+
+/**
+ * 共通項目使用しない会社リスト
+ * 
+ * @author sonnlb
+ *
+ */
+@Data
+public class NotUseCompany {
+
+	// 会社ID
+	private String companyId;
+}
