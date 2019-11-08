4451ccb239b8e58b08688886522caca8d09e12a1
NSVN-TruongVanLai <NSVN-TruongVanLai@users.noreply.github.com>
2019-11-08 14:55:58 +0700

    tạo root Medicalhistory

---
+++ nts.uk/uk.hr/hr.ctx/hr.shared/nts.uk.ctx.hr.shared.dom/src/main/java/nts/uk/ctx/hr/shared/dom/personalinfo/medicalhistory/Medicalhistory.java
@@ -0,0 +1,13 @@
+package nts.uk.ctx.hr.shared.dom.personalinfo.medicalhistory;
+
+import nts.arc.layer.dom.AggregateRoot;
+
+/**
+ * @author laitv
+ * domain 受診履歴
+
+ */
+public class Medicalhistory extends AggregateRoot{
+
+	
+}
