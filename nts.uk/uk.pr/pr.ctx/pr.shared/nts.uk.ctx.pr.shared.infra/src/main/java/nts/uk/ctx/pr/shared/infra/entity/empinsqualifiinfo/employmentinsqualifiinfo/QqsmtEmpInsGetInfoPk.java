aa5274469097f194f0c7ca592bec4db0643ca7f7
Hải Thanh Trần <hai.tt@3si.vn>
2019-11-12 17:49:44 +0700

    [QUI004]EmpInsLossInfo Entity

--- nts.uk/uk.pr/pr.ctx/pr.shared/nts.uk.ctx.pr.shared.infra/src/main/java/nts/uk/ctx/pr/shared/infra/entity/empinsqualifiinfo/employmentinsqualifiinfo/QqsmtEmpInsGetInfoPk.java
+++ nts.uk/uk.pr/pr.ctx/pr.shared/nts.uk.ctx.pr.shared.infra/src/main/java/nts/uk/ctx/pr/shared/infra/entity/empinsqualifiinfo/employmentinsqualifiinfo/QqsmtEmpInsGetInfoPk.java
@@ -1,8 +1,18 @@
 package nts.uk.ctx.pr.shared.infra.entity.empinsqualifiinfo.employmentinsqualifiinfo;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Basic;
 import javax.persistence.Column;
+import javax.persistence.Embeddable;
 import java.io.Serializable;
+
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Embeddable
 
 public class QqsmtEmpInsGetInfoPk implements Serializable {
     /**
