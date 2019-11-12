b1f41942cf4f4973e90c8c20d39a9e584184e1da
NSVN-TruongVanLai <NSVN-TruongVanLai@users.noreply.github.com>
2019-11-12 17:19:17 +0700

    update code test domain

--- nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.ws/src/main/java/nts/uk/ctx/bs/person/ws/test/ResidenceHistoryWS.java
+++ nts.uk/uk.com/com.ctx/bs/person/nts.uk.ctx.bs.person.ws/src/main/java/nts/uk/ctx/bs/person/ws/test/ResidenceHistoryWS.java
@@ -1,5 +1,6 @@
 package nts.uk.ctx.bs.person.ws.test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,28 +20,34 @@ public class ResidenceHistoryWS {
 
 	@Inject
 	private ForeignerResidenceHistoryInforItem domain;
-	
+
 	@POST
 	@Path("/get") // test service
-	public void testDomain(){
-		
-		List<String> listPID = Arrays.asList("ae7fe82e-a7bd-4ce3-adeb-5cd403a9d570",
-											 "8f9edce4-e135-4a1e-8dca-ad96abe405d6",
-											 "9787c06b-3c71-4508-8e06-c70ad41f042a",
-											 "62785783-4213-4a05-942b-c32a5ffc1d63",
-											 "4859993b-8065-4789-90d6-735e3b65626b",
-											 "aeaa869d-fe62-4eb2-ac03-2dde53322cb5");
-		GeneralDateTime baseDate = GeneralDateTime.now();
-		ListForeignerResidence result =  domain.getListForeignerResidenceHistoryInforItem(listPID, baseDate);
-		
-		domain.setListForeignerResidence(result);
-		
-		ForeignerResidenceHistoryInforItem mItem1 = domain.getForeignerResidenceHistoryInforItem("ae7fe82e-a7bd-4ce3-adeb-5cd403a9d570", baseDate);
-		
-		ForeignerResidenceHistoryInforItem mItem2 = domain.getForeignerResidenceHistoryInforItem("ae7fe82e-a7bd-4ce3-adeb-5cd403a9d599", baseDate);
-		
-		System.out.println("helllo" + mItem1 + mItem2);
+	public ListForeignerResidence testDomain(ParamTest param) {
+
+		List<String> listSid = param.listSid;
+		String sid = param.sid;
+		GeneralDateTime baseDate = GeneralDateTime.legacyDateTime(param.baseDate.date());
+
+		if (sid == null || sid == "") {
+			ListForeignerResidence result = domain.getListForeignerResidenceHistoryInforItem(listSid, baseDate);
+			return result;
+
+		} else {
+			ListForeignerResidence result = domain.getListForeignerResidenceHistoryInforItem(listSid, baseDate);
+			domain.setListForeignerResidence(result);
+			ForeignerResidenceHistoryInforItem mItem1 = domain.getForeignerResidenceHistoryInforItem(param.sid,
+					baseDate);
+
+			if (mItem1 == null) {
+				result.setListForeignerResidenceHistoryInforItem(new ArrayList<ForeignerResidenceHistoryInforItem>());
+			} else {
+				result.setListForeignerResidenceHistoryInforItem(new ArrayList<ForeignerResidenceHistoryInforItem>());
+				result.setListForeignerResidenceHistoryInforItem(Arrays.asList(mItem1));
+			}
+
+			return result;
+
+		}
 	}
-	
-	
 }
