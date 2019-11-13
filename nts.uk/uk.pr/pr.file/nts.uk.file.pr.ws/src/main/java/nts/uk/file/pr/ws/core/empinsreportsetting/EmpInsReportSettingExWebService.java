5c9845ae79ef242188be6e53b8fd5ca54564c626
tranphuc7686@gmail.com <Tran Phuc>
2019-11-13 17:05:26 +0700

    Update code

--- nts.uk/uk.pr/pr.file/nts.uk.file.pr.ws/src/main/java/nts/uk/file/pr/ws/core/empinsreportsetting/EmpInsReportSettingExWebService.java
+++ nts.uk/uk.pr/pr.file/nts.uk.file.pr.ws/src/main/java/nts/uk/file/pr/ws/core/empinsreportsetting/EmpInsReportSettingExWebService.java
@@ -1,10 +1,8 @@
 package nts.uk.file.pr.ws.core.empinsreportsetting;
 
 import nts.arc.layer.app.file.export.ExportServiceResult;
-import nts.uk.ctx.pr.file.app.core.empinsreportsetting.EmpInsReportSettingExportQuery;
-import nts.uk.ctx.pr.file.app.core.empinsreportsetting.EmpInsReportSettingPDFService;
-import nts.uk.ctx.pr.file.app.core.insurenamechangenoti.InsuredNameChangedNotiQuery;
-import nts.uk.ctx.pr.file.app.core.insurenamechangenoti.InsuredNameChangedNotiService;
+import nts.uk.ctx.pr.file.app.core.empinsqualifiinfo.empinsofficeinfo.NotifiOfChangInNameInsPerExportQuery;
+import nts.uk.ctx.pr.file.app.core.empinsqualifiinfo.empinsofficeinfo.NotifiOfChangInNameInsPerPDFService;
 
 import javax.inject.Inject;
 import javax.ws.rs.POST;
@@ -15,12 +13,12 @@ import javax.ws.rs.Produces;
 @Produces("application/json")
 public class EmpInsReportSettingExWebService {
     @Inject
-    private EmpInsReportSettingPDFService service;
+    private NotifiOfChangInNameInsPerPDFService service;
 
 
     @POST
     @Path("exportPDF")
-    public ExportServiceResult generate(EmpInsReportSettingExportQuery query) {
+    public ExportServiceResult generate(NotifiOfChangInNameInsPerExportQuery query) {
         return service.start(query);
     }
 }
