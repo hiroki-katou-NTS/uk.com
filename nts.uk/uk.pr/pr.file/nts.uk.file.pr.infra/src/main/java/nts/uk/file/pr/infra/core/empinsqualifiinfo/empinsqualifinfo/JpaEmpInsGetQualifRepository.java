5c3117a6d5fe4988c4748abfb1e6ae91276b9716
hadhv-3si <ha.dhv@3si.vn>
2019-11-13 19:09:46 +0700

    [QUI001] Coding export pdf

--- nts.uk/uk.pr/pr.file/nts.uk.file.pr.infra/src/main/java/nts/uk/file/pr/infra/core/empinsqualifiinfo/empinsqualifinfo/JpaEmpInsGetQualifRepository.java
+++ nts.uk/uk.pr/pr.file/nts.uk.file.pr.infra/src/main/java/nts/uk/file/pr/infra/core/empinsqualifiinfo/empinsqualifinfo/JpaEmpInsGetQualifRepository.java
@@ -1,7 +1,11 @@
 package nts.uk.file.pr.infra.core.empinsqualifiinfo.empinsqualifinfo;
 
+import nts.arc.layer.infra.data.JpaRepository;
+import nts.uk.ctx.pr.file.app.core.empinsqualifinfo.empinsqualifinfo.EmpInsGetQualifRepository;
+
 import javax.ejb.Stateless;
 
 @Stateless
-public class JpaEmpInsGetQualifRepository {
+public class JpaEmpInsGetQualifRepository extends JpaRepository implements EmpInsGetQualifRepository {
+
 }
