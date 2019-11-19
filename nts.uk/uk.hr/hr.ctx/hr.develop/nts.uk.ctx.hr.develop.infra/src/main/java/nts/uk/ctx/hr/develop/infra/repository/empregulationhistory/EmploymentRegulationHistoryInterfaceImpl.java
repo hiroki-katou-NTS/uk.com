839c8542229efd5d41345b05521b5a0eb7d92bbe
NSVN-PhamVanThanh <thanh_pv@nittsusystem-vn.com>
2019-11-19 11:28:43 +0700

    create domain 就業規則の履歴

---
+++ nts.uk/uk.hr/hr.ctx/hr.develop/nts.uk.ctx.hr.develop.infra/src/main/java/nts/uk/ctx/hr/develop/infra/repository/empregulationhistory/EmploymentRegulationHistoryInterfaceImpl.java
@@ -0,0 +1,26 @@
+package nts.uk.ctx.hr.develop.infra.repository.empregulationhistory;
+
+import java.util.Optional;
+
+import javax.ejb.Stateless;
+
+import nts.arc.layer.infra.data.JpaRepository;
+import nts.arc.time.GeneralDate;
+import nts.uk.ctx.hr.develop.dom.empregulationhistory.EmploymentRegulationHistoryInterface;
+
+@Stateless
+public class EmploymentRegulationHistoryInterfaceImpl extends JpaRepository implements EmploymentRegulationHistoryInterface {
+
+	private static final String SELECT_HIS_ID_BY_DATE = "SELECT c.historyId FROM JshmtEmpRegHistory c "
+			+ "WHERE c.startDate <= :baseDate  <= c.endDate "
+			+ "AND c.cId = :cId ";
+	
+	@Override
+	public Optional<String> getHistoryIdByDate(String cId, GeneralDate baseDate) {
+		return this.queryProxy().query(SELECT_HIS_ID_BY_DATE, String.class)
+			.setParameter("cId", cId)
+			.setParameter("baseDate", baseDate.toString())
+			.getSingle();
+	}
+
+}
