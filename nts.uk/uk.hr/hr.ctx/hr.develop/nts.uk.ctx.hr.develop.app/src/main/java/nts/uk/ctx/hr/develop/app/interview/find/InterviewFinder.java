332a1dcb3a2a324197f2873ab700004b7bbdc691
NSVC\hieult <hieultph03963@fpt.edu.vn>
2019-11-21 09:48:31 +0700

    Finder

--- nts.uk/uk.hr/hr.ctx/hr.develop/nts.uk.ctx.hr.develop.app/src/main/java/nts/uk/ctx/hr/develop/app/interview/find/InterviewFinder.java
+++ nts.uk/uk.hr/hr.ctx/hr.develop/nts.uk.ctx.hr.develop.app/src/main/java/nts/uk/ctx/hr/develop/app/interview/find/InterviewFinder.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+import nts.uk.ctx.hr.develop.dom.interview.InterviewRecord;
+import nts.uk.ctx.hr.develop.dom.interview.InterviewRecordRepository;
 import nts.uk.ctx.hr.develop.dom.interview.InterviewService;
 import nts.uk.ctx.hr.develop.dom.interview.dto.InterviewSummary;
 
@@ -13,10 +15,16 @@ public class InterviewFinder {
 
 	@Inject
 	private InterviewService interviewService;
+	@Inject
+	private InterviewRecordRepository interviewRecordRepository ;
 	
-	public InterviewSummary getInterviewContents(String companyID, int interviewCate, List<String> listEmployeeID,
+	public InterviewSummary getInterviewSummarry(String companyID, int interviewCate, List<String> listEmployeeID,
 			boolean getSubInterview, boolean getDepartment, boolean getPosition, boolean getEmployment)
 	{
 		return interviewService.getInterviewContents(companyID, interviewCate, listEmployeeID, getSubInterview, getDepartment, getPosition, getEmployment);
+	}
+	
+	public List<InterviewRecord> getInterviewContents( String companyID , int interviewCate ,List<String> listEmployeeID ){
+		return interviewRecordRepository.getInterviewContents(companyID, interviewCate, listEmployeeID);
 	}
 }
