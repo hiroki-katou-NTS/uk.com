031e305effc33aa80152b616dac91954c9b2c313
Tu Viet Van <tamnguyenxuan25@gmail.com>
2019-11-21 16:51:19 +0700

    Fix bug

--- nts.uk/uk.pr/pr.file/nts.uk.file.pr.infra/src/main/java/nts/uk/file/pr/infra/core/empinsqualifiinfo/empinsofficeinfo/NotifiOfChangInNameInsPerAposeFileGenerator.java
+++ nts.uk/uk.pr/pr.file/nts.uk.file.pr.infra/src/main/java/nts/uk/file/pr/infra/core/empinsqualifiinfo/empinsofficeinfo/NotifiOfChangInNameInsPerAposeFileGenerator.java
@@ -165,7 +165,7 @@ public class NotifiOfChangInNameInsPerAposeFileGenerator extends AsposePdfReport
                     String fullName = element.getFullName() != null ? element.getFullName() : "";
                     detachText(45, 466, fullName, 28, textBuilder);
                     //A1_10
-                    if (fullName.length() >= 29) {
+                    if (fullName.length() >= 28) {
                         detachText(45, 434, element.getFullName().substring(29, element.getFullName().length()), 12, textBuilder);
                     }
                 }
