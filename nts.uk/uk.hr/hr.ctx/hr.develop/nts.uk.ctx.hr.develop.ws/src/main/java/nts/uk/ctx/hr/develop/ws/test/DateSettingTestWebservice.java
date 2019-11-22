942b52743aee03d79ce7e90c5e6566ff1e96ea01
NSVN-DuongTuanAnh <anh_dt@nittsusystem-vn.com>
2019-11-22 13:50:42 +0700

    test webservice

---
+++ nts.uk/uk.hr/hr.ctx/hr.develop/nts.uk.ctx.hr.develop.ws/src/main/java/nts/uk/ctx/hr/develop/ws/test/DateSettingTestWebservice.java
@@ -0,0 +1,34 @@
+package nts.uk.ctx.hr.develop.ws.test;
+
+import javax.inject.Inject;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import nts.uk.ctx.hr.develop.app.sysoperationset.eventoperation.command.JMM018Cmd;
+import nts.uk.ctx.hr.develop.app.sysoperationset.eventoperation.commandhandler.UpdateEventMenuOperation;
+import nts.uk.ctx.hr.develop.app.sysoperationset.eventoperation.dto.JMM018Dto;
+import nts.uk.ctx.hr.develop.app.sysoperationset.eventoperation.find.JMM018Finder;
+
+@Path("hrtest/datesetting")
+@Produces(MediaType.APPLICATION_JSON)
+public class DateSettingTestWebservice {
+	@Inject
+	private JMM018Finder finder;
+	
+	@Inject
+	private UpdateEventMenuOperation eventMenu;
+	
+	@POST
+	@Path("/getAll")
+	public JMM018Dto getEventMenu(){
+		return finder.finderJMM018();
+	}
+	
+	@POST
+	@Path("/update-event-menu")
+	public void updateEventMenu(JMM018Cmd command){
+		eventMenu.handle(command);
+	}
+}
