2da65514e912a7a31300a5983b231f8378394112
thai_lh <thai.lh@3si.vn>
2019-11-25 16:17:52 +0700

    [QUI001] Create Domain 公共職業安定所

--- nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qui/001/a/qui001.a.vm.ts
+++ nts.uk/uk.pr/pr.web/nts.uk.pr.web/src/main/webapp/view/qui/001/a/qui001.a.vm.ts
@@ -166,7 +166,7 @@ module nts.uk.pr.view.qui001.a.viewmodel {
                 showAllClosure: true,
                 showPeriod: false,
                 periodFormatYM: false,
-                tabindex: 9,
+                tabindex: 7,
                 /** Required parameter */
                 baseDate: moment().toISOString(),
                 periodStartDate: moment().toISOString(),
