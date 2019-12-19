f57f73aa6026753505d745ea8416be1ec3a3c671
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2019-12-19 10:07:02 +0700

    Update Stateless

--- nts.uk/uk.hr/hr.ctx/hr.develop/nts.uk.ctx.hr.develop.ac/src/main/java/nts/uk/ctx/hr/develop/ac/workrule/closure/workday/WorkDayAdaptorImpl.java
+++ nts.uk/uk.hr/hr.ctx/hr.develop/nts.uk.ctx.hr.develop.ac/src/main/java/nts/uk/ctx/hr/develop/ac/workrule/closure/workday/WorkDayAdaptorImpl.java
@@ -3,12 +3,14 @@ package nts.uk.ctx.hr.develop.ac.workrule.closure.workday;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import nts.uk.ctx.at.shared.pub.workrule.closure.workday.IWorkDayPub;
 import nts.uk.ctx.hr.develop.dom.workrule.closure.workday.ClosureDateOfEmploymentImport;
 import nts.uk.ctx.hr.develop.dom.workrule.closure.workday.WorkDayAdaptor;
 
+@Stateless
 public class WorkDayAdaptorImpl implements WorkDayAdaptor {
 
 	@Inject
