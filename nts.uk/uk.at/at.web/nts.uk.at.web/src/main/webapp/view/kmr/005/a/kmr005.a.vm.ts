1d49c96e4946b52b892784e57efe96cf070c9eee
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2019-12-20 15:49:10 +0700

    bento: KMR005: sửa lỗi truyền list employee từ UI

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmr/005/a/kmr005.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmr/005/a/kmr005.a.vm.ts
@@ -171,7 +171,7 @@ module nts.uk.at.view.kmr005.a.viewmodel {
                 return;    
             }
             let param = {
-                empLst: _.map(self.employeeList(), (o) => o.id),
+                empLst: _(self.employeeList()).filter((o) => self.selectedEmployeeCode().includes(o.code)).map((o) => o.id).value(),
                 title: self.title(),
                 startDate: self.dateValue().startDate,
                 endDate: self.dateValue().endDate,
