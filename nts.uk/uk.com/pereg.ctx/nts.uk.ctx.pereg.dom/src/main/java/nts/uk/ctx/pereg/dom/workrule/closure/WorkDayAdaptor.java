0f0fcbf78da07187f9a23579bb6dce920a570d24
NSVN-PhamVanThanh <thanh_pv@nittsusystem-vn.com>
2019-12-21 10:53:00 +0700

    JMM018 Domain ThanhPV remove code tesst

--- nts.uk/uk.com/pereg.ctx/nts.uk.ctx.pereg.dom/src/main/java/nts/uk/ctx/pereg/dom/workrule/closure/WorkDayAdaptor.java
+++
@@ -1,7 +0,0 @@
-package nts.uk.ctx.pereg.dom.workrule.closure;
-
-import java.util.List;
-
-public interface WorkDayAdaptor {
-	List<ClosureDateOfEmploymentImport> getClosureDate(String companyId);
-}
