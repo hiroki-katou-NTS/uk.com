726a49629686e91dcb99e64e348ce34b55935e88
NSVN-TranThiHoa <hoa_tt@nittsusystem-vn.com>
2019-12-21 14:43:56 +0700

    CMM018_2次: domain HrApprovalRouteSetting fix entity

--- nts.uk/uk.hr/hr.ctx/hr.develop/nts.uk.ctx.hr.develop.infra/src/main/java/nts/uk/ctx/hr/develop/infra/entity/sysoperationset/businessrecognition/JcmmtRootArp.java
+++ nts.uk/uk.hr/hr.ctx/hr.develop/nts.uk.ctx.hr.develop.infra/src/main/java/nts/uk/ctx/hr/develop/infra/entity/sysoperationset/businessrecognition/JcmmtRootArp.java
@@ -1,8 +1,8 @@
 package nts.uk.ctx.hr.develop.infra.entity.sysoperationset.businessrecognition;
 
 import javax.persistence.Column;
-import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -19,7 +19,7 @@ import nts.uk.shr.infra.data.entity.UkJpaEntity;
 @NoArgsConstructor
 public class JcmmtRootArp extends UkJpaEntity {
 	
-	@EmbeddedId
+	@Id
 	@Column(name = "CID")
     public String cid;  //
 	
