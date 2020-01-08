3c77bab3f5afbc8ada7842ca5bc9808427fdddc1
NSVN-NguyenThiHoangYen <yen_nt@nittsusystem-vn.com>
2020-01-08 17:31:14 +0700

    JMM018_B: code server select history

--- nts.uk/uk.hr/hr.ctx/hr.develop/nts.uk.ctx.hr.develop.app/src/main/java/nts/uk/ctx/hr/develop/app/jmm018retire/command/SelectHistoryCommandHandler.java
+++ nts.uk/uk.hr/hr.ctx/hr.develop/nts.uk.ctx.hr.develop.app/src/main/java/nts/uk/ctx/hr/develop/app/jmm018retire/command/SelectHistoryCommandHandler.java
@@ -1,15 +1,34 @@
 package nts.uk.ctx.hr.develop.app.jmm018retire.command;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 
+import nts.arc.error.BusinessException;
+import nts.uk.ctx.hr.develop.app.jmm018retire.jmm0018bcfinder;
+import nts.uk.ctx.hr.develop.app.jmm018retire.dto.MandatoryRetirementDto;
+import nts.uk.ctx.hr.develop.app.jmm018retire.dto.RelateMasterDto;
 import nts.uk.ctx.hr.develop.app.jmm018retire.dto.StartDto;
 
 @Stateless
 public class SelectHistoryCommandHandler{
-
-	public void selcectHistory(StartDto startdto, String hist) {
+	
+	@Inject
+	private jmm0018bcfinder finder;
+	
+	public StartDto selcectHistory(StartDto startdto, String hist, boolean isLatestHis) {
 		if(startdto.getRelateMaster() == null) {
-			
+			throw new BusinessException("MsgJ_JMM018_16");
+		}else {
+			// アルゴリズム [就業規則の取得] を実行する (THực hiện thuật toán  [Lấy Quy tắc làm việc/Labor regulations] )
+			MandatoryRetirementDto madatory = finder.getLaborRegulation(hist);
+			if(madatory == null && isLatestHis == false) {
+				throw new BusinessException("MsgJ_JMM018_19");
+			}else if(madatory == null && isLatestHis == true) {
+				throw new BusinessException("MsgJ_JMM018_18");
+			}else {
+				RelateMasterDto relate = finder.getRelateMaster();
+				return new StartDto(relate, madatory);
+			}
 		}
 	}
 }
