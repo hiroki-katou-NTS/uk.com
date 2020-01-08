93bd130e8dd3ba803248131027b928426dfcb293
NSVN-PhamVanThanh <thanh_pv@nittsusystem-vn.com>
2020-01-08 19:31:03 +0700

    Update MandatoryRetirementRegulationWS.java

--- nts.uk/uk.hr/hr.ctx/hr.develop/nts.uk.ctx.hr.develop.ws/src/main/java/nts/uk/ctx/hr/develop/ws/announcement/mandatoryretirement/MandatoryRetirementRegulationWS.java
+++ nts.uk/uk.hr/hr.ctx/hr.develop/nts.uk.ctx.hr.develop.ws/src/main/java/nts/uk/ctx/hr/develop/ws/announcement/mandatoryretirement/MandatoryRetirementRegulationWS.java
@@ -1,35 +1,24 @@
 package nts.uk.ctx.hr.develop.ws.announcement.mandatoryretirement;
 
-import java.util.stream.Collectors;
-
 import javax.inject.Inject;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import nts.uk.ctx.hr.develop.app.announcement.mandatoryretirement.command.UpdateMandatoryRetirementRegulation;
 import nts.uk.ctx.hr.develop.app.announcement.mandatoryretirement.dto.MandatoryRetirementRegulationDto;
-import nts.uk.ctx.hr.develop.dom.announcement.mandatoryretirement.algorithm.mandatoryRetirementRegulation.MandatoryRetirementRegulationService;
 
 @Path("mandatoryRetirementRegulation")
 @Produces(MediaType.APPLICATION_JSON)
 public class MandatoryRetirementRegulationWS {
 
 	@Inject
-	private MandatoryRetirementRegulationService finder;
+	private UpdateMandatoryRetirementRegulation command;
 	
 	@POST
 	@Path("/update")
 	public void update(MandatoryRetirementRegulationDto param){
-		finder.updateMandatoryRetirementRegulation(
-				param.getCompanyId(), 
-				param.getHistoryId(), 
-				param.getReachedAgeTerm(), 
-				param.getPublicTerm().toDomain(), 
-				param.getRetireDateTerm().toDomain(), 
-				param.getPlanCourseApplyFlg(), 
-				param.getMandatoryRetireTerm().stream().map(c->c.toDomain()).collect(Collectors.toList()), 
-				param.getReferEvaluationTerm().stream().map(c->c.toDomain()).collect(Collectors.toList()), 
-				param.getPlanCourseApplyTerm().toDomain());
+		command.update(param);
 	}
 }
