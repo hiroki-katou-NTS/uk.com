eb7e9c4b0a6b16d7aacbdc697d3520be83cc022e
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2020-01-22 13:01:15 +0700

    CMM018_ver2: sửa xử lý remand

--- nts.uk/uk.com/wk.ctx/nts.uk.ctx.workflow.dom/src/main/java/nts/uk/ctx/workflow/dom/service/RemandImpl.java
+++ nts.uk/uk.com/wk.ctx/nts.uk.ctx.workflow.dom/src/main/java/nts/uk/ctx/workflow/dom/service/RemandImpl.java
@@ -46,11 +46,12 @@ public class RemandImpl implements RemandService {
 		List<ApprovalPhaseState> listUpperPhase = listApprovalPhase.stream().filter(x -> x.getPhaseOrder() >= order).collect(Collectors.toList());
 		listUpperPhase.forEach(approvalPhaseState -> {
 			approvalPhaseState.getListApprovalFrame().forEach(approvalFrame -> {
-//				approvalFrame.setApprovalAtr(ApprovalBehaviorAtr.UNAPPROVED);
-//				approvalFrame.setApproverID("");
-//				approvalFrame.setRepresenterID("");
-//				approvalFrame.setApprovalDate(null);
-//				approvalFrame.setApprovalReason("");
+				approvalFrame.getLstApproverInfo().forEach(approverInfor -> {
+					approverInfor.setApprovalAtr(ApprovalBehaviorAtr.UNAPPROVED);
+					approverInfor.setAgentID("");
+					approverInfor.setApprovalDate(null);
+					approverInfor.setApprovalReason("");
+				});
 			});
 			approvalPhaseState.setApprovalAtr(ApprovalBehaviorAtr.UNAPPROVED);
 		});
