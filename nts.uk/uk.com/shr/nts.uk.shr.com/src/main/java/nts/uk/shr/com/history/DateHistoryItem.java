44bed02808f22534f687c6c42ffbf820e81f20ae
m_kitahira <m_kitahira@nittsusystem.co.jp>
2020-01-31 14:21:25 +0900

    update DateHistoryItem 新しい履歴を作る

--- nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/com/history/DateHistoryItem.java
+++ nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/com/history/DateHistoryItem.java
@@ -1,6 +1,7 @@
 package nts.uk.shr.com.history;
 
 import nts.arc.time.GeneralDate;
+import nts.gul.text.IdentifierUtil;
 import nts.uk.shr.com.time.calendar.period.DatePeriod;
 
 /**
@@ -17,6 +18,15 @@ public class DateHistoryItem extends GeneralHistoryItem<DateHistoryItem, DatePer
 	 */
 	public DateHistoryItem(String historyId, DatePeriod period) {
 		super(historyId, period);
+	}
+	
+	/**
+	 * 新しい履歴を作る
+	 * @param period 期間
+	 * @return
+	 */
+	public static DateHistoryItem createNewHistory(DatePeriod period) {
+		return new DateHistoryItem(IdentifierUtil.randomUniqueId(), period);
 	}
 
 }
