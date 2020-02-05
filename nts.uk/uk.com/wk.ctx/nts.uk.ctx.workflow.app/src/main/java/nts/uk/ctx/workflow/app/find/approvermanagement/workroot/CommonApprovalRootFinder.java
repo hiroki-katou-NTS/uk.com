5b85b0fe3318697794a2c2dfb0a9111079e299fc
NSVN-TranThiHoa <hoa_tt@nittsusystem-vn.com>
2020-02-05 15:49:14 +0700

    CMM018_2次: isOverload (Check ls chồng chéo)

--- nts.uk/uk.com/wk.ctx/nts.uk.ctx.workflow.app/src/main/java/nts/uk/ctx/workflow/app/find/approvermanagement/workroot/CommonApprovalRootFinder.java
+++ nts.uk/uk.com/wk.ctx/nts.uk.ctx.workflow.app/src/main/java/nts/uk/ctx/workflow/app/find/approvermanagement/workroot/CommonApprovalRootFinder.java
@@ -428,6 +428,15 @@ public class CommonApprovalRootFinder {
 				&& date2.getStartDate().compareTo(date1.getEndDate()) <0){
 			return true;
 		}
+		/**
+		 * date 1.........|..............]..........
+		 * date 2....|....].........................
+		 * eDate2 = sDate1 or eDate1 = sDate2
+		 */
+		if(date2.getEndDate().compareTo(date1.getStartDate()) == 0
+				|| date2.getStartDate().compareTo(date1.getEndDate()) == 0){
+			return true;
+		}
 		return false;
 	}
 	/**
