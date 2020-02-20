5671ef38bf6c8b19c2f8829b6f1504e88ca77501
NSVN-PhamVanThanh <thanh_pv@nittsusystem-vn.com>
2020-02-20 11:02:38 +0700

    ThanhPV add noRankOrder into MenuOperation

--- nts.uk/uk.hr/hr.ctx/hr.develop/nts.uk.ctx.hr.develop.infra/src/main/java/nts/uk/ctx/hr/develop/infra/repository/sysoperationset/eventmanage/JpaMenuOperationRepository.java
+++ nts.uk/uk.hr/hr.ctx/hr.develop/nts.uk.ctx.hr.develop.infra/src/main/java/nts/uk/ctx/hr/develop/infra/repository/sysoperationset/eventmanage/JpaMenuOperationRepository.java
@@ -68,6 +68,6 @@ public class JpaMenuOperationRepository extends JpaRepository implements MenuOpe
 	 */
 	private MenuOperation convertToDomain(JcmctMenuOperation x){
 		return MenuOperation.createFromJavaType(x.jcmctMenuOperationPK.programId, x.useMenu, x.jcmctMenuOperationPK.companyId, 
-												x.useApproval, x.useNotice, x.ccd);
+												x.useApproval, x.useNotice, x.noRankOrder, x.ccd);
 	}
 }
