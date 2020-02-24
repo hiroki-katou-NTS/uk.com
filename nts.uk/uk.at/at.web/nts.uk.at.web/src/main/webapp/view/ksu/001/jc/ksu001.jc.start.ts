363c620872fefc2c1253b2dd75b1eaebc5fb8983
NSVC\hieult <hieultph03963@fpt.edu.vn>
2020-02-24 08:51:26 +0700

    Ui

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksu/001/jc/ksu001.jc.start.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksu/001/jc/ksu001.jc.start.ts
@@ -1,11 +1,10 @@
-module nts.uk.at.view.ksu001.ja {
+module nts.uk.at.view.ksu001.jb {
     import getShare = nts.uk.ui.windows.getShared;
 
     let __viewContext: any = window["__viewContext"] || {};
     __viewContext.ready(function() {
         var screenModel = new viewmodel.ScreenModel();
         __viewContext.bind(screenModel);
-        screenModel.init();
         $('input#textName').focus();
-    });
+    });   
 }
