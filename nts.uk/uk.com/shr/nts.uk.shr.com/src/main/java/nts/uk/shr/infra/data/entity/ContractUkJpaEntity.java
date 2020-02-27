8e485292641833fb12b354246914eba17df7b430
Hiroki Katou <hiroki_katou@nittsusystem.co.jp>
2020-02-27 19:04:13 +0900

    CONTRACT_CDの固定列挿入の、基盤吸収

---
+++ nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/infra/data/entity/ContractUkJpaEntity.java
@@ -0,0 +1,27 @@
+package nts.uk.shr.infra.data.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import lombok.Getter;
+import lombok.Setter;
+import nts.uk.shr.com.context.AppContexts;
+
+@Getter
+@Setter
+@MappedSuperclass
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+public abstract class ContractUkJpaEntity extends UkJpaEntity{
+	/* 契約コード */
+	@Column(name = "CONTRACT_CD")
+	public String contractCd;
+	
+	@PrePersist
+    private void setInsertingContractInfo() {
+		this.contractCd = AppContexts.user().contractCode();
+	}
+	
+
+}
