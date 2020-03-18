34c79de73e785a1f52ef3f54cf50e9bcf2963f3c
NSVN-TruongVanLai <NSVN-TruongVanLai@users.noreply.github.com>
2020-03-18 15:11:22 +0700

    fix bug #109922

--- nts.uk/uk.hr/hr.web/nts.uk.hr.web/src/main/webapp/view/jhn/001/a/jhn001.a.start.ts
+++ nts.uk/uk.hr/hr.web/nts.uk.hr.web/src/main/webapp/view/jhn/001/a/jhn001.a.start.ts
@@ -27,4 +27,25 @@ $(window).on('resize', () => {
 
         $content.css('height', `calc(100vh - ${bound.top + 20}px)`);
     }
+    
+    let layout = $('.ntsControl.layout-control.readonly').get(0),
+        B221 = $('#B221').get(0);
+    
+    let panelHeightResize = window.innerHeight - layout.getBoundingClientRect().top,
+        B221_panelResize = window.innerHeight - B221.getBoundingClientRect().top;
+    
+    if (panelHeightResize <= 215) {
+        $(".drag-panel").attr(`style`, `max-height: 100px !important;height: 100px !important;`);
+        $('#B221').attr(`style`, `max-height: 488px !important;height: 488px !important;` + `magin-bottom: 5px !important;`);
+        $("#B221_4").igGrid("option", "height", 368);
+    } else {
+        let heighGridCal = ((B221_panelResize - 93) % 23) == 0 ? B221_panelResize - 100 : Math.floor((B221_panelResize - 93) / 23) * 23,
+            heightGridSet = heighGridCal < 368 ? 368 : (heighGridCal - 23),
+            dragPanelSet = heighGridCal < 368 ? 100 : (panelHeightResize - 170);
+
+        $(".drag-panel").attr(`style`, `max-height: ` + dragPanelSet + `px !important;` + `height: ` + dragPanelSet + `px !important;`);
+        $("#B221").attr(`style`, `max-height: ` + (heightGridSet + 95) + `px !important;` + `height: ` + (heightGridSet + 95) + `px !important;` + `magin-bottom: 5px !important;`);
+        $("#B221_4").igGrid("option", "height", heightGridSet);
+    }
+   
 });
