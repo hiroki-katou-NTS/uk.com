73ed153aeb1d833bc8b1e47e42067ab43b6049dc
NSVN-TruongVanLai <NSVN-TruongVanLai@users.noreply.github.com>
2020-04-03 08:43:45 +0700

    update memo enum status

--- nts.uk/uk.hr/hr.ctx/hr.shared/nts.uk.ctx.hr.shared.dom/src/main/java/nts/uk/ctx/hr/shared/dom/databeforereflecting/common/Status.java
+++ nts.uk/uk.hr/hr.ctx/hr.shared/nts.uk.ctx.hr.shared.dom/src/main/java/nts/uk/ctx/hr/shared/dom/databeforereflecting/common/Status.java
@@ -7,18 +7,19 @@ import lombok.AllArgsConstructor;
 
 /**
  * ステータス
+ * path: UKDesign.ドメインモデル.NittsuSystem.UniversalK.人事.shared.個人情報反映前データ.個人情報反映前データ_共通.ステータス
  *
  */
 @AllArgsConstructor
 public enum Status {
-	Unregistered(0),
+	Unregistered(0), // 未登録
 
-	Registered(1),
+	Registered(1), // 登録済み/承認待ち
 	
-	WaitingReflection(2),
+	WaitingReflection(2), // 承認済み/反映待ち
 	
-	Reflected(3),
-	
-	Deny(4);
+	Reflected(3), // 反映済み
+	 
+	Deny(4); // 否認/差戻し
 	public final int value;
 }
