d1fce4267684cfd693f28659549f94179172398d
NSVN-NguyenXuanThanh <thanh_nx@nittsusystem-vn.com>
2020-04-11 13:25:08 +0900

    update param lock 584

---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/dailyperformanceprocessing/confirmationstatus/change/confirm/DailyLock.java
@@ -0,0 +1,66 @@
+package nts.uk.ctx.at.record.dom.dailyperformanceprocessing.confirmationstatus.change.confirm;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import nts.arc.time.GeneralDate;
+
+/**
+ * @author ThanhNX 日の実績のロック状態
+ */
+@AllArgsConstructor
+@Getter
+public class DailyLock {
+
+	/**
+	 * 社員ID
+	 */
+	private String employeeId;
+
+	/**
+	 * 対象日
+	 */
+	private GeneralDate date;
+
+	/**
+	 * 日別実績のロック
+	 */
+	private StatusLock lockDailyResult;
+
+	/**
+	 * 職場の就業確定
+	 */
+	private StatusLock lockWpl;
+
+	/**
+	 * 月別実績の承認
+	 */
+	private StatusLock lockApprovalMontｈ;
+
+	/**
+	 * 月別実績の確認
+	 */
+	private StatusLock lockConfirmMonth;
+
+	/**
+	 * 日別実績の承認
+	 */
+	private StatusLock lockApprovalDay;
+
+	/**
+	 * 日別実績の確認
+	 */
+	private StatusLock lockConfirmDay;
+
+	/**
+	 * 日別実績のロック
+	 */
+	private StatusLock lockPast;
+
+	public boolean lockData() {
+		return lockDailyResult.value == StatusLock.LOCK.value || lockWpl.value == StatusLock.LOCK.value
+				|| lockApprovalMontｈ.value == StatusLock.LOCK.value || lockConfirmMonth.value == StatusLock.LOCK.value
+				|| lockApprovalDay.value == StatusLock.LOCK.value || lockConfirmDay.value == StatusLock.LOCK.value
+				|| lockPast.value == StatusLock.LOCK.value;
+	}
+
+}
---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/dailyperformanceprocessing/confirmationstatus/change/confirm/StatusLock.java
@@ -0,0 +1,17 @@
+package nts.uk.ctx.at.record.dom.dailyperformanceprocessing.confirmationstatus.change.confirm;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * @author ThanhNX
+ * 
+ */
+@AllArgsConstructor
+public enum StatusLock {
+
+	LOCK(0),
+
+	UN_LOCK(1);
+
+	public final int value;
+}
