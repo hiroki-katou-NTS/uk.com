5bf43373264983eb710ff39a462dd8ea34e520d4
anhdt <duongtuananh4711@gmail.com>
2020-04-12 13:36:48 +0700

    add command screen C

--- nts.uk/uk.at/at.screen/nts.uk.screen.at.ws/src/main/java/nts/uk/screen/at/ws/stamp/personalengraving/StampPersonalEngravingWs.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.ws/src/main/java/nts/uk/screen/at/ws/stamp/personalengraving/StampPersonalEngravingWs.java
@@ -5,6 +5,8 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import nts.uk.ctx.at.record.app.command.stamp.card.stampcard.management.personalengraving.RegisterDailyIDentifyCommand;
+import nts.uk.ctx.at.record.app.command.stamp.card.stampcard.management.personalengraving.RegisterDailyIDentifyCommandHandler;
 import nts.uk.screen.at.app.stamp.personalengraving.query.StampResultConfirmDto;
 import nts.uk.screen.at.app.stamp.personalengraving.query.StampResultConfirmRequest;
 import nts.uk.screen.at.app.stamp.personalengraving.query.StampResultConfirmationQuery;
@@ -19,9 +21,18 @@ public class StampPersonalEngravingWs {
 	@Inject 
 	private StampResultConfirmationQuery stampResultConfirmationQuery;
 	
+	@Inject
+	private RegisterDailyIDentifyCommandHandler registerDailyIDentifyCommandHandler;
+	
 	@POST
 	@Path("startCScreen")
 	public StampResultConfirmDto startScreen(StampResultConfirmRequest param) throws InterruptedException {
 		return stampResultConfirmationQuery.getStampResultConfirm(param);
+	}
+	
+	@POST
+	@Path("registerDailyIdentify")
+	public void registerDailyIdentify(RegisterDailyIDentifyCommand	 param) throws InterruptedException {
+		registerDailyIDentifyCommandHandler.handle(param);
 	}
 }
