541ce67f7a62a62cb3ea5f61c340d661ebf115b2
NSVN - DuongTuanAnh <anh_dt@nittsusystem-vn.com>
2020-04-13 14:19:17 +0700

    show date time

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/find/stamp/management/personalengraving/dto/AttendanceOneDayDto.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/find/stamp/management/personalengraving/dto/AttendanceOneDayDto.java
@@ -10,17 +10,17 @@ import nts.uk.ctx.at.record.dom.workrecord.stampmanagement.stamp.domainservice.A
 @Data
 public class AttendanceOneDayDto {
 	private String date;
-	private String workIn1;
-	private String workOut1;
+	private Integer workIn1;
+	private Integer workOut1;
 
-	private String workIn2;
-	private String workOut2;
+	private Integer workIn2;
+	private Integer workOut2;
 	
 	public AttendanceOneDayDto (AttendanceOneDay domain) {
 		this.date = domain.getDate().toString();
-		this.workIn1 = new TimeActualStampDto(domain.getAttendance1()).getActualAfterRoundingTime();
-		this.workOut1 = new TimeActualStampDto(domain.getLeavingStamp1()).getActualAfterRoundingTime();
-		this.workIn1 = new TimeActualStampDto(domain.getAttendance2()).getActualAfterRoundingTime();
-		this.workOut2 = new TimeActualStampDto(domain.getLeavingStamp2()).getActualAfterRoundingTime();
+		this.workIn1 = new TimeActualStampDto(domain.getAttendance1()).getStampTimeWithDay();
+		this.workOut1 = new TimeActualStampDto(domain.getLeavingStamp1()).getStampTimeWithDay();
+		this.workIn2 = new TimeActualStampDto(domain.getAttendance2()).getStampTimeWithDay();
+		this.workOut2 = new TimeActualStampDto(domain.getLeavingStamp2()).getStampTimeWithDay();
 	}
 }
