89dc420e064d94421f0dc5fa3e120fa2b63d7635
NSVN - DuongTuanAnh <anh_dt@nittsusystem-vn.com>
2020-04-15 20:54:44 +0700

    screen T click jump

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdp/002/a/kdp002.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdp/002/a/kdp002.a.vm.ts
@@ -153,7 +153,11 @@ module nts.uk.at.view.kdp002.a {
                     if(res && res.dailyAttdErrorInfos && res.dailyAttdErrorInfos.length > 0) {
                         nts.uk.ui.windows.setShared('KDP010_2T', res, true);
                         nts.uk.ui.windows.sub.modal('/view/kdp/002/t/index.xhtml').onClosed(function (): any {
-                            
+                            let returnData =  nts.uk.ui.windows.getShared('KDP010_T');
+                            if(!returnData.isClose && returnData.errorDate) {
+                                console.log(returnData);
+                                nts.uk.request.jump("/view/kaf/005/a/index.xhtml?a=0", {appDate: returnData.errorDate});
+                            }
                         });
                     }
                 });
