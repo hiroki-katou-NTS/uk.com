baaf4546f85fb269da702ac1ddba4aeeb953a387
NSVN-TruongVanLai <NSVN-TruongVanLai@users.noreply.github.com>
2020-04-22 08:18:32 +0700

    打刻カード編集Repository

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/stamp/card/stamcardedit/StampCardEditingRepo.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/stamp/card/stamcardedit/StampCardEditingRepo.java
@@ -5,17 +5,30 @@ import java.util.Optional;
 /**
  * @author sonnlb
  * 
- *         打刻カード編集Repository
+ * 打刻カード編集Repository
  */
 public interface StampCardEditingRepo {
 
 	/**
 	 * 取得する(会社ID)
-	 * 
+
 	 * @param 会社ID
 	 *            companyId
 	 * @return 打刻カード編集 StampCardEditing
 	 */
 	Optional<StampCardEditing> get(String companyId);
+	
+	/**
+	 * [1] update(打刻カード編集)
+	 * @param domain
+	 */
+	void update(StampCardEditing domain);
+	
+	/**
+	 * [2] get
+	 * @param companyId
+	 * @return StampCardEditing
+	 */
+	StampCardEditing getStampCardEditing(String companyId);
 
 }
