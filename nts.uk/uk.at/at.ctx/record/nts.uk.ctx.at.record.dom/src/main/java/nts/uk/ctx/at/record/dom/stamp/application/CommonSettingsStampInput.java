b59f16b55c2b6259cfaf9eff531b7c37366b1f3b
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2020-04-27 13:32:55 +0700

    Create CommonSettingsStampInput.java

---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/stamp/application/CommonSettingsStampInput.java
@@ -0,0 +1,36 @@
+package nts.uk.ctx.at.record.dom.stamp.application;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * AR:	打刻入力の共通設定
+ * Path: UKDesign.ドメインモデル.NittsuSystem.UniversalK.就業.contexts.勤務実績.勤務実績.打刻管理.打刻設定.打刻入力の機能設定.打刻入力の共通設定
+ * @author chungnt
+ *
+ */
+
+@AllArgsConstructor
+public class CommonSettingsStampInput {
+	
+	/**
+	 * 	会社ID
+	 */
+	@Getter
+	private final String companyId;
+	
+	/**
+	 * 	職場選択を利用できる権限
+	 */
+	@Getter
+	private List<String> permission;
+	
+	/**
+	 * 	GoogleMap利用するか
+	 */
+	@Getter
+	private boolean googlemap;
+	
+}
