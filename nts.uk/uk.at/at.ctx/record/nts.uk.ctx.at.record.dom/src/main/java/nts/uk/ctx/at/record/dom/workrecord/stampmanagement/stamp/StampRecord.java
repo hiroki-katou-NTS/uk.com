1c6dc2f793e9c42b66a0a4c80d67fabe98d9137e
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2020-05-14 15:14:10 +0700

    Update StampRecord

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/workrecord/stampmanagement/stamp/StampRecord.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/workrecord/stampmanagement/stamp/StampRecord.java
@@ -2,10 +2,12 @@ package nts.uk.ctx.at.record.dom.workrecord.stampmanagement.stamp;
 
 import java.util.Optional;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import nts.arc.layer.dom.objecttype.DomainAggregate;
 import nts.arc.time.GeneralDateTime;
 import nts.uk.ctx.at.record.dom.employmentinfoterminal.EmpInfoTerminalCode;
+import nts.uk.ctx.at.record.dom.stamp.card.stampcard.ContractCode;
 import nts.uk.ctx.at.record.dom.stamp.card.stampcard.StampNumber;
 import nts.uk.ctx.at.record.dom.workrecord.stampmanagement.timestampsetting.prefortimestaminput.ReservationArt;
 
@@ -14,49 +16,40 @@ import nts.uk.ctx.at.record.dom.workrecord.stampmanagement.timestampsetting.pref
  * UKDesign.ドメインモデル.NittsuSystem.UniversalK.就業.contexts.勤務実績.勤務実績.打刻管理.打刻.打刻記録
  *         打刻記録
  */
+@AllArgsConstructor
+@Getter
 public class StampRecord implements DomainAggregate {
-
+																																									
+	/**
+	 * 	契約コード
+	 */
+	private final ContractCode contractCode;
 	/**
 	 * 打刻カード番号
 	 */
-	@Getter
 	private final StampNumber stampNumber;
 
 	/**
 	 * 打刻日時
 	 */
-	@Getter
 	private final GeneralDateTime stampDateTime;
 
 	/**
 	 * 打刻区分
 	 */
-	@Getter
 	private final boolean stampArt;
 
 	/**
 	 * 予約区分
 	 */
-	@Getter
 	private final ReservationArt revervationAtr;
 
 	/**
 	 * 就業情報端末コード
 	 */
-	@Getter
 	private final Optional<EmpInfoTerminalCode> empInfoTerCode;
 
-	public StampRecord(StampNumber stampNumber, GeneralDateTime stampDateTime, boolean stampArt,
-			ReservationArt revervationAtr, Optional<EmpInfoTerminalCode> empInfoTerCode) {
-		super();
-		this.stampNumber = stampNumber;
-		this.stampDateTime = stampDateTime;
-		this.stampArt = stampArt;
-		this.revervationAtr = revervationAtr;
-		this.empInfoTerCode = empInfoTerCode;
-	}
-
-	public String retriveKey() {
-		return this.getStampNumber()+ this.getStampDateTime().toString();
-	}
+	/**
+	 * [C-0] 打刻記録(契約コード, 打刻カード番号, 打刻日時, 打刻区分, 予約区分, 就業情報端末コード)
+	*/
 }
