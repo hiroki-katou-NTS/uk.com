27e43c4834452421268533251ea6cd766ce7e7a4
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2020-06-01 16:23:08 +0700

    Update DS_打刻カード番号を自動作成する

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/stamp/card/stampcard/AutoCreateStampCardNumberService.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/stamp/card/stampcard/AutoCreateStampCardNumberService.java
@@ -10,6 +10,7 @@ import nts.uk.ctx.at.record.dom.adapter.employee.EmployeeDataMngInfoImport;
 import nts.uk.ctx.at.record.dom.stamp.card.stamcardedit.StampCardEditing;
 import nts.uk.ctx.at.record.dom.workrecord.stampmanagement.stamp.Stamp;
 import nts.uk.ctx.at.record.dom.workrecord.stampmanagement.stamp.StampMeans;
+import nts.uk.ctx.at.record.dom.workrecord.stampmanagement.stamp.domainservice.StampDataReflectProcessService;
 
 /**
  * @author sonnlb
@@ -20,86 +21,86 @@ import nts.uk.ctx.at.record.dom.workrecord.stampmanagement.stamp.StampMeans;
 public class AutoCreateStampCardNumberService {
 
 	/**
-	 * 	[1] 作成する
+	 * [1] 作成する
+	 * 
 	 * @param 社員ID
-	 * employeeID
+	 *            employeeID
 	 * @param 打刻手段
-	 * stampMeans
-	 * @return 
-	 * @return 打刻カード作成結果
-	 * 	Optional<打刻カード作成結果>
+	 *            stampMeans
+	 * @return
+	 * @return 打刻カード作成結果 Optional<打刻カード作成結果>
 	 */
-	
-	public Optional<StampCardCreateResult> create(Require require, String employeeID, StampMeans stampMeans) {
-		
+
+	public static Optional<StampCardCreateResult> create(Require require, String employeeID, StampMeans stampMeans) {
+
 		// if not 打刻手段.打刻カードを自動作成するか()
 		if (!stampMeans.checkAutoCreateStamp()) {
 			return Optional.empty();
 		}
-		
+
 		// $打刻カード = [prv-1] 打刻カードを作成する(require, 社員ID)
 		Optional<StampCard> stampCard = createStampCard(require, employeeID);
-		
-		//	if $打刻カード.isEmpty
-		if(!stampCard.isPresent()){
+
+		// if $打刻カード.isEmpty
+		if (!stampCard.isPresent()) {
 			return Optional.empty();
 		}
-		
+
 		// $AtomTask = AtomTask: require.打刻カードを追加する($打刻カード)
-		
+
 		Optional<AtomTask> atomTask = Optional.of(AtomTask.of(() -> {
 			require.add(stampCard.get());
 		}));
-		
+
 		return Optional.ofNullable(new StampCardCreateResult(stampCard.get().getStampNumber().v(), atomTask));
 	}
-	
+
 	/**
 	 * [prv-1] 打刻カードを作成する
+	 * 
 	 * @param require
 	 * @param employeeID
 	 * @return
 	 */
-	private Optional<StampCard> createStampCard(Require require, String employeeID) {
-		//	$社員データ管理情報 = require.社員情報を取得する(社員ID)	
-		List<EmployeeDataMngInfoImport> empInfos =  require.findBySidNotDel(Arrays.asList(employeeID));
-		
-		//if $社員データ管理情報.isEmpty
+	private static Optional<StampCard> createStampCard(Require require, String employeeID) {
+		// $社員データ管理情報 = require.社員情報を取得する(社員ID)
+		List<EmployeeDataMngInfoImport> empInfos = require.findBySidNotDel(Arrays.asList(employeeID));
+
+		// if $社員データ管理情報.isEmpty
 		if (empInfos.isEmpty()) {
 			return Optional.empty();
 		}
-		
-		EmployeeDataMngInfoImport  empInfo = empInfos.get(0);
-		
-		//	$会社情報 = require.会社情報を取得する($社員データ管理情報.会社ID)
-		
+
+		EmployeeDataMngInfoImport empInfo = empInfos.get(0);
+
+		// $会社情報 = require.会社情報を取得する($社員データ管理情報.会社ID)
+
 		Optional<CompanyImport622> companyInfoOpt = require.getCompanyNotAbolitionByCid(empInfo.getCompanyId());
-		
-		
-		//	if $会社情報.isEmpty
-		
-		if(!companyInfoOpt.isPresent()){
+
+		// if $会社情報.isEmpty
+
+		if (!companyInfoOpt.isPresent()) {
 			return Optional.empty();
 		}
 		CompanyImport622 companyInfo = companyInfoOpt.get();
-		
+
 		// $打刻カード番号 = [prv-2] 打刻カード番号を作成する(require, $会社情報.契約コード, $会社情報.会社ID,
-		// $会社情報.会社コード, $社員データ管理情報.社員コード)	
-		Optional<String> stampCardNumberOpt = createStampCardNumber(require, companyInfo.getContractCd(), companyInfo.getCompanyId(),
-				companyInfo.getCompanyCode(), empInfo.getEmployeeCode());
-		
-		//	if $打刻カード番号.isEmpty
-		if(!stampCardNumberOpt.isPresent()){
+		// $会社情報.会社コード, $社員データ管理情報.社員コード)
+		Optional<String> stampCardNumberOpt = createStampCardNumber(require, companyInfo.getContractCd(),
+				companyInfo.getCompanyId(), companyInfo.getCompanyCode(), empInfo.getEmployeeCode());
+
+		// if $打刻カード番号.isEmpty
+		if (!stampCardNumberOpt.isPresent()) {
 			return Optional.empty();
 		}
-		
+
 		return Optional.of(new StampCard(companyInfo.getContractCd(), stampCardNumberOpt.get(), employeeID));
 	}
 
 	/**
 	 * [prv-2] 打刻カード番号を作成する
 	 */
-	private Optional<String> createStampCardNumber(Require require, String contractCode, String companyId,
+	private static Optional<String> createStampCardNumber(Require require, String contractCode, String companyId,
 			String companyCode, String employeeCode) {
 		// $打刻カード編集 = require.打刻カード編集を取得する(会社ID)
 		Optional<StampCardEditing> stampCardEditingOpt = require.get(companyId);
@@ -127,16 +128,20 @@ public class AutoCreateStampCardNumberService {
 		}
 		return Optional.ofNullable(stampCardNumberOpt.get());
 	}
-	
-	public static interface Require {
+
+	public static interface Require extends StampDataReflectProcessService.Require {
 		// [R-1] 社員情報を取得する
 		List<EmployeeDataMngInfoImport> findBySidNotDel(List<String> sid);
+
 		// [R-2] 会社情報を取得する
 		Optional<CompanyImport622> getCompanyNotAbolitionByCid(String cid);
+
 		// [R-3] 打刻カード編集を取得する
 		Optional<StampCardEditing> get(String companyId);
+
 		// [R-4] 打刻カードを取得する
 		Optional<Stamp> get(String contractCode, String stampNumber);
+
 		// [R-5] 打刻カードを追加する
 		void add(StampCard domain);
 	}
