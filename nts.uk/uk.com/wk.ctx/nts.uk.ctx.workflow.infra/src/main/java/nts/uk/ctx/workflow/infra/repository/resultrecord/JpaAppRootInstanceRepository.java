c39fcc6a85a09e4be2ba3758dfc85f6bbdcc2dd1
NSVN-PhongTQ <phong_tq@nittsusystem-vn.com>
2020-06-01 15:36:20 +0700

    # fix import Timestamp

--- nts.uk/uk.com/wk.ctx/nts.uk.ctx.workflow.infra/src/main/java/nts/uk/ctx/workflow/infra/repository/resultrecord/JpaAppRootInstanceRepository.java
+++ nts.uk/uk.com/wk.ctx/nts.uk.ctx.workflow.infra/src/main/java/nts/uk/ctx/workflow/infra/repository/resultrecord/JpaAppRootInstanceRepository.java
@@ -5,6 +5,7 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
