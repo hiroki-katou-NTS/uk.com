732e060b29121ad1e578fd13337187ea9546b6f9
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2020-06-01 16:45:05 +0700

    Update VO_打刻ページレイアウト

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/workrecord/stampmanagement/timestampsetting/prefortimestaminput/ButtonType.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/workrecord/stampmanagement/timestampsetting/prefortimestaminput/ButtonType.java
@@ -35,4 +35,29 @@ public class ButtonType implements DomainValue{
 	public boolean checkStampType(){
 		return this.getStampType().isPresent();	
 	}
+
+	/**
+	 * [2] 表示する打刻区分を取得する
+	 * 
+	 * @return string
+	 */
+	public String getStampTypeDisplay() {
+		// if @打刻種類.isEmpty
+		if (!stampType.isPresent()) {
+			// return @予約区分 ←EnumのNameを返す
+			return this.reservationArt.nameId;
+		}
+		// $表示する打刻種類 = @打刻種類.表示する打刻区分を作成する()
+
+		String stampTypeDisplay = stampType.get().createStampTypeDisplay();
+
+		// if @予約区分 == なし
+		if (this.reservationArt.equals(ReservationArt.NONE)) {
+			// return $表示する打刻種類
+			return stampTypeDisplay;
+		} else {
+			// $表示する打刻種類 + '+' +打刻記録.予約区分 ←EnumのName
+			return stampTypeDisplay + ReservationArt.CANCEL_RESERVATION.nameId;
+		}
+	}
 }
