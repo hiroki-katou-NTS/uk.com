2b8710c4c7746397c9e2a698dccc747a380a7b09
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2020-06-03 15:35:52 +0700

    Create KrcdtTopAlStamp

---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.infra/src/main/java/nts/uk/ctx/at/record/infra/entity/workrecord/stampmanagement/toppagealarm/KrcdtTopAlStampPk.java
@@ -0,0 +1,44 @@
+package nts.uk.ctx.at.record.infra.entity.workrecord.stampmanagement.toppagealarm;
+
+import java.io.Serializable;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import nts.arc.time.GeneralDateTime;
+
+/**
+ * 
+ * @author chungnt
+ *
+ */
+
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Embeddable
+public class KrcdtTopAlStampPk implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * 対象社員ID
+	 */
+	@Basic(optional = false)
+	@Column(name = "SID_TGT")
+	public String sid_tgt;
+	
+	/**
+	 *  実行完了日時
+	 *   */
+	@Basic (optional = false)
+	@Column (name = "FINISH_TIME")
+	public GeneralDateTime finishDateTime;
+
+}
