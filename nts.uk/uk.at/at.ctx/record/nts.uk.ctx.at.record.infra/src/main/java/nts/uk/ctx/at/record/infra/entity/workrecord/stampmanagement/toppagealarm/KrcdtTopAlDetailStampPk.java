b6c2408d466f0932c1562cbd059aee0897c24614
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2020-06-03 15:36:20 +0700

    Create KrcdtTopAlDetailStamp

---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.infra/src/main/java/nts/uk/ctx/at/record/infra/entity/workrecord/stampmanagement/toppagealarm/KrcdtTopAlDetailStampPk.java
@@ -0,0 +1,52 @@
+package nts.uk.ctx.at.record.infra.entity.workrecord.stampmanagement.toppagealarm;
+
+import java.io.Serializable;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import nts.arc.time.GeneralDateTime;
+
+/**
+ * 
+ * @author chungnt
+ *
+ */
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Embeddable
+@EqualsAndHashCode
+public class KrcdtTopAlDetailStampPk implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * 対象社員ID
+	 */
+	@Basic(optional = false)
+	@Column(name= "SID_TGT")
+	public String sid_tgt;
+	
+	/**
+	 * 実行完了日時
+	 */
+	@Basic(optional = false)
+	@Column(name= "FINISH_TIME")
+	public GeneralDateTime finish_time;
+	
+	/**
+	 * 連番	
+	 */
+	@Basic(optional = false)
+	@Column(name= "SERIAL_NO")
+	public String serial_no;
+
+}
