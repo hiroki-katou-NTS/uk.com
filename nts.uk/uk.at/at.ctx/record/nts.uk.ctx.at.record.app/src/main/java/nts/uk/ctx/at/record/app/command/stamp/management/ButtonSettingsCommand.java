1b3340dc54c4292304f08c654f4faf80f0d076e6
Vu Tien Lam <lam_vt@nittsusystem-vn.com>
2020-06-05 10:32:26 +0700

    update để build

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/stamp/management/ButtonSettingsCommand.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/stamp/management/ButtonSettingsCommand.java
@@ -70,7 +70,7 @@ public class ButtonSettingsCommand {
 					changeClockArt, changeCalArt);
 		}
 		
-		ButtonType buttonType = new ButtonType(reservationArt, stampType);
+		ButtonType buttonType = new ButtonType(reservationArt, Optional.of(stampType));
 
 		NotUseAtr usrArts = EnumAdaptor.valueOf(x.getUsrArt(), NotUseAtr.class);
 
