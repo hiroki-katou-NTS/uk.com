9887a950c04849b8d24d39c218332f391662c1a1
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2020-06-10 15:33:12 +0700

    Create StampCardTest

---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/test/java/nts/uk/ctx/at/record/dom/stamp/card/stampcard/StampCardTest.java
@@ -0,0 +1,21 @@
+package nts.uk.ctx.at.record.dom.stamp.card.stampcard;
+
+import org.junit.Test;
+
+import nts.arc.testing.assertion.NtsAssert;
+import nts.arc.time.GeneralDate;
+
+public class StampCardTest {
+
+	@Test
+	public void getter() {
+		StampCard stampCard = new StampCard("DUMMY", new ContractCode("DUMMY"), new StampNumber("DUMMY"), "DUMMY", GeneralDate.today());
+		NtsAssert.invokeGetters(stampCard);
+	}
+
+	@Test
+	public void test_StampCard_C1() {
+		StampCard stampCard = new StampCard("DUMMY", "DUMMY", "DUMMY");
+		NtsAssert.invokeGetters(stampCard);
+	}
+}
