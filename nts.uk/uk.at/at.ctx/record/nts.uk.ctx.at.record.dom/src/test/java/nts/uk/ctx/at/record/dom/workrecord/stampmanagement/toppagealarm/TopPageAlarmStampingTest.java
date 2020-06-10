d6437e0d94b6052a37385e17d066cca59833109e
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2020-06-10 16:25:30 +0700

    update TopPageAlarmStampingTest

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/test/java/nts/uk/ctx/at/record/dom/workrecord/stampmanagement/toppagealarm/TopPageAlarmStampingTest.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/test/java/nts/uk/ctx/at/record/dom/workrecord/stampmanagement/toppagealarm/TopPageAlarmStampingTest.java
@@ -1,25 +1,50 @@
 package nts.uk.ctx.at.record.dom.workrecord.stampmanagement.toppagealarm;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 
 import nts.arc.testing.assertion.NtsAssert;
+import nts.arc.time.GeneralDateTime;
 
 public class TopPageAlarmStampingTest {
 
 	@Test
 	public void getters() {
-		TopPageAlarmStamping topPageAlarmStamping = TopPageAlarmStampingHelper.getDefault();
-		NtsAssert.invokeGetters(topPageAlarmStamping);
+		List<TopPageArmDetail> list = new ArrayList<>();
+		List<String> lstsid = new ArrayList<>();
+		
+		list.add(new TopPageArmDetail("DUMMY", 1, "DUMMY"));
+		list.add(new TopPageArmDetail("DUMMY", 1, "DUMMY"));
+		list.add(new TopPageArmDetail("DUMMY", 1, "DUMMY"));
+		list.add(new TopPageArmDetail("DUMMY", 1, "DUMMY"));
+		
+		lstsid.add("DUMMY");
+		lstsid.add("DUMMY");
+		lstsid.add("DUMMY");
+		lstsid.add("DUMMY");
+		
+		TopPageArm topPageArm = new TopPageArm("DUMMY", GeneralDateTime.now(), ExistenceError.NO_ERROR, IsCancelled.NOT_CANCELLED, new ArrayList<>());
+		TopPageAlarmStamping alarmStamping = new TopPageAlarmStamping(list, topPageArm);
+		
+		NtsAssert.invokeGetters(alarmStamping);
 	}
 	
 	@Test
 	public void get_lsterror_empty() {
 		
-		TopPageAlarmStamping topPageAlarmStamping = TopPageAlarmStampingHelper.getListErrorNull();
+		List<TopPageArmDetail> list = new ArrayList<>();
+		List<String> lstsid = new ArrayList<>();
 		
-		assertThat(topPageAlarmStamping.lstTopPageDetail).isEmpty();
+		list.add(new TopPageArmDetail("DUMMY", 1, "DUMMY"));
+		list.add(new TopPageArmDetail("DUMMY", 2, "DUMMY"));
+		list.add(new TopPageArmDetail("DUMMY", 3, "DUMMY"));
+		list.add(new TopPageArmDetail("DUMMY", 4, "DUMMY"));
+		
+		TopPageAlarmStamping alarmStamping = new TopPageAlarmStamping(list, new TopPageArm(ExistenceError.HAVE_ERROR, lstsid));
+		
+//		assertThat(topPageAlarmStamping.lstTopPageDetail).isEmpty();
 	}
 
 }
