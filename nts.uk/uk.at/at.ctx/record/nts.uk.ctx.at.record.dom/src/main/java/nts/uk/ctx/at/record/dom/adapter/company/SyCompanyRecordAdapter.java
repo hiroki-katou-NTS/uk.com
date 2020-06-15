2a88ca33595ffb4af4af9c75f77cb758a0f72824
NSVN-TruongVanLai <NSVN-TruongVanLai@users.noreply.github.com>
2020-06-15 16:04:28 +0700

    KDP003.K

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/adapter/company/SyCompanyRecordAdapter.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/adapter/company/SyCompanyRecordAdapter.java
@@ -1,6 +1,7 @@
 package nts.uk.ctx.at.record.dom.adapter.company;
 
 import java.util.List;
+import java.util.Optional;
 
 import nts.arc.time.calendar.period.DatePeriod;
 
@@ -8,4 +9,6 @@ public interface SyCompanyRecordAdapter {
 	List<AffCompanyHistImport> getAffCompanyHistByEmployee(List<String> sid, DatePeriod datePeriod);
 	
 	List<StatusOfEmployeeExport> getListAffComHistByListSidAndPeriod(List<String> sid, DatePeriod datePeriod);
+	
+	List<CompanyImportForKDP003> get(String contractCd, Optional<String> cid, Boolean isAbolition );
 }
---
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdp/004/k/kdp003.k.service.ts
@@ -0,0 +1,4 @@
+module kdp003.k.service {
+    import ajax = nts.uk.request.ajax;
+    import format = nts.uk.text.format;
+}
