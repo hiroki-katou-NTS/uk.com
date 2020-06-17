ee8b7370098e20b21adafbf86c42f6a4c41ec711
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2020-06-17 15:05:57 +0700

    update view b and view c

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/kmp/001/view-b.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/kmp/001/view-b.ts
@@ -1,42 +1,89 @@
 /// <reference path="../../../lib/nittsu/viewcontext.d.ts" />
 
-
-
 const template = `<div class="sidebar-content-header">
 	<span class="title" data-bind= "text: $i18n('KMP001_2')"></span>
 	<button class="proceed" data-bind= "text: $i18n('KMP001_5')"></button>
 	<button class="danger" data-bind= "text: $i18n('KMP001_6')"></button>
 </div>
-<div>
-	<div style="width: 500px"
-		data-bind="ntsSearchBox: {
-			searchMode: 'filter',
-			targetKey: 'code',
-			comId: 'card-list', 
-			items: items,
-			selected: currentCode,
-			selectedKey: 'code',
-			fields: ['name', 'code'],
-			mode: 'igGrid'
-		}" />
-					
-	<table id="card-list"
-		data-bind="ntsGridList: {
-			height: 320,
-			options: items,
-			optionsValue: 'code',
-			columns: [
-	            { headerText: 'コード', prop: 'code', width: 150 },
-	            { headerText: '名称', prop: 'name', width: 130 },
-	            { headerText: '説明', prop: 'description', width: 50 }
-	        ],
-			multiple: false,
-			enable: true,
-			value: currentCode
-		}"></table>
+<div class="view-kmp">
+	<div class="float-left list-component">
+		<div style="width: 480px"
+			data-bind="ntsSearchBox: {
+				label: $i18n('KMP001_22'),
+				searchText: $i18n('KMP001_23'),
+				searchMode: 'filter',
+				targetKey: 'code',
+				comId: 'card-list', 
+				items: items,
+				selected: currentCode,
+				selectedKey: 'code',
+				fields: ['name', 'code','code1'],
+				mode: 'igGrid'
+				}"></div>
+		<div class="caret-right caret-background bg-green" style="padding: 10px;">
+			<table id="card-list" 
+				data-bind="ntsGridList: {
+					height: 300,
+					options: items,
+					optionsValue: 'code',
+					columns: [
+			            { headerText: $i18n('KMP001_22'), prop: 'code', width: 180 },
+			            { headerText: $i18n('KMP001_8'), prop: 'code1', width: 130 },
+			            { headerText: $i18n('KMP001_9'), prop: 'name', width: 150 }
+			        ],
+					multiple: false,
+					enable: true,
+					value: currentCode
+				}">
+			</table>
+		</div>
+	</div>
+	<div class="float-left model-component">
+		<table>
+			<tbody>
+				<tr>
+					<td class="label-column">
+						<div style="border: 0; font-size: 18px" data-bind="ntsFormLabel: { text: $i18n('KMP001_22') }"></div>
+					</td>
+					<td class="data">
+						<div>0000000000002</div>
+					</td>
+				</tr>
+				<tr>
+					<td class="label-column">
+						<div data-bind="ntsFormLabel: { text: $i18n('KMP001_9'), required: true }"></div>
+						<button data-bind="text: $i18n('KMP001_26')">Normal</button>
+					</td>
+					<td class="data">
+						<div>00000002</div>
+						<div style="margin-left: 10px">日通　社員2</div>
+					</td>
+				</tr>
+				<tr>
+					<td class="label-column">
+						<div style="border: 0; font-size: 18px" data-bind="ntsFormLabel: { text: $i18n('KMP001_20') }"></div>
+					</td>
+					<td class="data">
+						<div>2000/01/01</div>
+					</td>
+				</tr>
+				<tr>
+					<td class="label-column">
+						<div style="border: 0; font-size: 18px" data-bind="ntsFormLabel: { text: $i18n('KMP001_21') }"></div>
+					</td>
+					<td class="data">
+					<div>2000/01/01</div>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 </div>
-<div data-bind="text: currentCode"></div>
 `;
+
+interface Params {
+	
+}
 
 @component({
 	name: 'view-b',
@@ -46,20 +93,15 @@ class ViewBComponent extends ko.ViewModel {
 	public params!: Params;
 
 	public items: KnockoutObservableArray<any> = ko.observableArray([
-		{ code: '001', name: 'Abc', description: 'Nittsu', other1: 'xxz', other2: 'xxy' },
-		{ code: '002', name: 'Abc', description: 'Nittsu', other1: 'xxz', other2: 'xxy' },
-		{ code: '003', name: 'Abc', description: 'Nittsu', other1: 'xxz', other2: 'xxy' },
-		{ code: '004', name: 'Abc', description: 'Nittsu', other1: 'xxz', other2: 'xxy' }
+		{ code: '001', code1: '001',  name: 'Nittsu', startDate: '2000/01/01', endDate: '2000/01/01'},
+		{ code: '002', code1: '002',  name: 'Nittsu', startDate: '2000/01/01', endDate: '2000/01/01'},
+		{ code: '003', code1: '003', name: 'Nittsu', startDate: '2000/01/01', endDate: '2000/01/01' },
+		{ code: '004', code1: '004', name: 'Nittsu', startDate: '2000/01/01', endDate: '2000/01/01' },
+		{ code: '005', code1: '005', name: 'Nittsu', startDate: '2000/01/01', endDate: '2000/01/01' }
 	]);
 	public currentCode: KnockoutObservable<string> = ko.observable('');
 
 	created(params: Params) {
 		this.params = params;
-	}
-
-	mounted() {
-		const vm = this;
-
-		vm.items.push({ code: '005', name: 'Abc', description: 'Nittsu', other1: 'xxz', other2: 'xxy' });
 	}
 }
--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/kmp/001/view-c.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/kmp/001/view-c.ts
@@ -1,23 +1,146 @@
 /// <reference path="../../../lib/nittsu/viewcontext.d.ts" />
 
-//interface Params {
-//	model: KnockoutObservable<string>;
-//	tabs: KnockoutObservableArray<string>;
-//}
+const template = `<div class="sidebar-content-header">
+	<span class="title" data-bind= "text: $i18n('KMP001_3')"></span>
+	<button class="proceed" data-bind= "text: $i18n('KMP001_5')"></button>
+</div>
+<div class="view-kmp">
+	<div class="layout-date float-left list-component">
+		<table>
+			<tbody>
+				<tr>
+					<td>
+						<div data-bind="ntsFormLabel: { text: $i18n('KMP001_9'), required: true }"></div>
+					</td>
+					<td>
+						<div style="margin-left: 20px" id="daterangepicker" tabindex="1" data-bind="ntsDateRangePicker: { 
+							required: required,
+							enable: enable, 
+							showNextPrevious: true,
+							value: dateValue, 
+							maxRange: 'oneMonth'}"/>		
+					</td>
+					<td>
+						<button style="margin-left: 20px" class="caret-bottom" data-bind= "text: $i18n('KMP001_15')"'></button>
+					</td>
+				<tr>
+			</tbody>
+		</table>
+	</div>
+	<div class="float-left list-component caret-right caret-background bg-green" style="padding: 10px;">
+		<div style="width: 460px"
+			data-bind="ntsSearchBox: {
+				label: $i18n('KMP001_22'),
+				searchMode: 'filter',
+				targetKey: 'code',
+				comId: 'card-list', 
+				items: items,
+				selected: currentCode,
+				selectedKey: 'code',
+				fields: ['name', 'code','code1'],
+				mode: 'igGrid'
+				}"></div>
+		<div>
+			<table id="card-list" 
+				data-bind="ntsGridList: {
+					height: 300,
+					options: items,
+					optionsValue: 'code',
+					columns: [
+			            { headerText: $i18n('KMP001_22'), prop: 'code', width: 150 },
+			            { headerText: $i18n('KMP001_27'), prop: 'code1', width: 100 },
+			            { headerText: $i18n('KMP001_28'), prop: 'name', width: 80 },
+ 						{ headerText: $i18n('KMP001_29'), prop: 'name', width: 130 }
+			        ],
+					multiple: false,
+					enable: true,
+					value: currentCode
+				}">
+			</table>
+		</div>
+	</div>
+	<div class="float-left model-component">
+		<table>
+			<tbody>
+				<tr>
+					<td class="label-column">
+						<div style="border: 0; font-size: 18px" data-bind="ntsFormLabel: { text: $i18n('KMP001_22') }"></div>
+					</td>
+					<td class="data">
+						<div>0000000000004</div>
+					</td>
+				</tr>
+				<tr>
+					<td class="label-column">
+						<div data-bind="ntsFormLabel: { text: $i18n('KMP001_9'), required: true }"></div>
+						<button data-bind="text: $i18n('KMP001_26')">Normal</button>
+					</td>
+					<td class="data">
+						<div>00000002</div>
+						<div style="margin-left: 10px">日通　社員2</div>
+					</td>
+				</tr>
+				<tr>
+					<td class="label-column">
+						<div style="border: 0; font-size: 18px" data-bind="ntsFormLabel: { text: $i18n('KMP001_20') }"></div>
+					</td>
+				</tr>
+				<tr>
+					<td class="label-column">
+						<div style="border: 0; font-size: 18px" data-bind="ntsFormLabel: { text: $i18n('KMP001_21') }"></div>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
+`;
 
-const template =`<div class="sidebar-content-header">
-						<span class="title" data-bind= "text: $i18n('KMP001_3')"></span>
-						<button class="proceed" data-bind= "text: $i18n('KMP001_5')"></button>
-					</div>`;
+interface Params {
+	
+}
 
 @component({
 	name: 'view-c',
 	template
 })
-class ViewC extends ko.ViewModel {
-	public params!: Params;	
+class ViewCComponent extends ko.ViewModel {
+	public params!: Params;
+
+	public items: KnockoutObservableArray<any> = ko.observableArray([
+		{ code: '001', code1: '001',  name: 'Nittsu', startDate: '2000/01/01', endDate: '2000/01/01'},
+		{ code: '002', code1: '002',  name: 'Nittsu', startDate: '2000/01/01', endDate: '2000/01/01'},
+		{ code: '003', code1: '003', name: 'Nittsu', startDate: '2000/01/01', endDate: '2000/01/01' },
+		{ code: '004', code1: '004', name: 'Nittsu', startDate: '2000/01/01', endDate: '2000/01/01' },
+		{ code: '005', code1: '005', name: 'Nittsu', startDate: '2000/01/01', endDate: '2000/01/01' }
+	]);
+	public currentCode: KnockoutObservable<string> = ko.observable('');
+	
+		enable: KnockoutObservable<boolean>;
+        required: KnockoutObservable<boolean>;
+        dateValue: KnockoutObservable<any>;
+        startDateString: KnockoutObservable<string>;
+        endDateString: KnockoutObservable<string>;
 
 	created(params: Params) {
 		this.params = params;
+		
+		var self = this;
+            self.enable = ko.observable(true);
+            self.required = ko.observable(true);
+            
+            self.startDateString = ko.observable("");
+            self.endDateString = ko.observable("");
+            self.dateValue = ko.observable({});
+            
+            self.startDateString.subscribe(function(value){
+                self.dateValue().startDate = value;
+                self.dateValue.valueHasMutated();        
+            });
+            
+            self.endDateString.subscribe(function(value){
+                self.dateValue().endDate = value;   
+                self.dateValue.valueHasMutated();      
+            });
 	}
 }
