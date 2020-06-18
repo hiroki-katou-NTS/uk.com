3550fd9bfbbf7eb2d2e2ab88f6f4c82c28ac907b
NSVN - DuongTuanAnh <anh_dt@nittsusystem-vn.com>
2020-06-18 10:06:35 +0700

    repo 会社別シフトパレットの一覧を取得する

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.app/src/main/java/nts/uk/ctx/at/schedule/app/find/shift/shiftpalletsorg/ShiftPalletsOrgFinder.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.app/src/main/java/nts/uk/ctx/at/schedule/app/find/shift/shiftpalletsorg/ShiftPalletsOrgFinder.java
@@ -6,8 +6,11 @@ import java.util.stream.Collectors;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import nts.uk.ctx.at.schedule.dom.shift.management.ShiftPalletsOrg;
 import nts.uk.ctx.at.schedule.dom.shift.management.ShiftPalletsOrgRepository;
+import nts.uk.shr.com.context.AppContexts;
 
 /**
  * 
@@ -29,11 +32,22 @@ public class ShiftPalletsOrgFinder {
 	}
 	
 	// 会社別シフトパレットの一覧を取得する
-	public List<ShiftPalletsOrgDto> getByListPage(String workplaceId) {
-		// 0 = work place
-		List<ShiftPalletsOrg> shiftPalletsOrg = shiftPalletsOrgRepository.findbyWorkPlaceId(0, workplaceId);
-		List<ShiftPalletsOrgDto> result = shiftPalletsOrg.stream().map(c -> new ShiftPalletsOrgDto(c, workplaceId))
+	public List<PageDto> getByListPage(String workplaceId) {
+		String cid = AppContexts.user().companyId();
+		List<ShiftPalletsOrg> shiftPalletsOrg = shiftPalletsOrgRepository.findByCID(cid);
+		List<PageDto> result = shiftPalletsOrg.stream().map(c -> new PageDto(c))
 				.collect(Collectors.toList());
 		return result;
+	}
+	
+	@Getter
+	public class PageDto {
+		private int page;
+		private String name;
+		
+		public PageDto(ShiftPalletsOrg dom) {
+			this.page = dom.getPage();
+			this.name = dom.getShiftPallet().getDisplayInfor().getShiftPalletName().v();
+		}
 	}
 }
