304fa6a6f41006117cc61ef3b6839b7b8618ed9b
Hieult91 <king.of.throw.game@gmail.com>
2020-06-22 13:37:34 +0700

    Create Domain 勤務予定の表示設定

---
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/workschedule/displaysetting/WorkScheDisplaySetting.java
@@ -0,0 +1,30 @@
+package nts.uk.ctx.at.schedule.dom.workschedule.displaysetting;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import nts.arc.time.calendar.DateInMonth;
+
+/**
+ * 勤務予定の表示設定
+ * UKDesign.ドメインモデル."NittsuSystem.UniversalK".就業.contexts.勤務予定.表示設定
+ * @author HieuLT
+ *
+ */
+@AllArgsConstructor
+public class WorkScheDisplaySetting {
+	@Getter
+	/** 会社ID **/ 
+	private final String companyID;
+	
+	@Getter
+	/** 初期表示の月 */
+	private InitDispMonth initDispMonth;
+	
+	@Getter
+	/** 初期表示期間の終了日 : 一ヶ月間 **/
+	private DateInMonth endDay;
+
+	
+
+
+}
