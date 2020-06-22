74d1c82ce9009971c6cabf64bde9b92499d24030
NSVN - DuongTuanAnh <anh_dt@nittsusystem-vn.com>
2020-06-22 16:43:37 +0700

    DS [送る期間を取得する]

---
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/schedule/personal/correction/WorkShiftScheduleDto.java
@@ -0,0 +1,37 @@
+package nts.uk.screen.at.app.schedule.personal.correction;
+
+import java.util.Optional;
+
+import lombok.Data;
+import nts.arc.time.GeneralDate;
+import nts.uk.ctx.at.schedule.dom.workschedule.EditStateOfDailyAttd;
+
+/**
+ * @author anhdt
+ * 	勤務予定（シフト）dto
+ */
+@Data
+public class WorkShiftScheduleDto {
+	// シフトコード
+	private Optional<String> shiftCode;
+	// シフトの編集状態
+	private Optional<EditStateOfDailyAttd> editState;
+	// シフト名称
+	private Optional<String> shiftName;
+	// データがあるか
+	private boolean haveData;
+	// 出勤休日区分
+	private Optional<Integer> workHolidayClassification;
+	// 勤務予定が必要か
+	private boolean needToWork;
+	// 実績か
+	private boolean achievements;
+	// 年月日
+	private GeneralDate date;
+	// 応援か
+	private Integer supportCategory;
+	// 確定済みか
+	private boolean confirmed;
+	// 社員ID
+	private String employeeId;
+}
