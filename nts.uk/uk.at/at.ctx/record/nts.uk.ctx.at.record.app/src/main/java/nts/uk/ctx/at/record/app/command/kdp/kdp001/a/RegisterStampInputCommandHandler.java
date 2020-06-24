2ba42393fc1c5ea836dff23e9c0e954227178785
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2020-06-24 19:25:08 +0700

    KDP0001- Update Service

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/kdp/kdp001/a/RegisterStampInputCommandHandler.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/kdp/kdp001/a/RegisterStampInputCommandHandler.java
@@ -1,7 +1,6 @@
 package nts.uk.ctx.at.record.app.command.kdp.kdp001.a;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdp/001/a/viewmodel.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdp/001/a/viewmodel.ts
@@ -426,6 +426,20 @@ class KDP001AViewModel extends ko.ViewModel {
 			items = _.orderBy(items, ['stampDatetime'], ['desc']);
 
 			vm.stampDatas(items || []);
+			
+			if (items.length==1 && !vm.isScreenCD()) {
+
+					if (vm.screenMode() == 'a' || vm.screenMode() == 'b') {
+
+						$("#fixed-table").ntsFixedTable({ height: 53, width: 215 });
+					} else {
+
+						if (!vm.screenMode()) {
+
+							$("#fixed-table").ntsFixedTable({ height: 89, width: 280 });
+						}
+					}
+				}
 		}).always(() => {
 			vm.$blockui("clear");
 		});;
