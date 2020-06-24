1c43549d1b76deff3c6f50c08d2f1e4ed2374c75
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2020-06-24 15:40:41 +0700

    Update ScreenQuery PerformInitialStartup

--- nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/query/kmp/kmp001/a/PerformInitialStartup.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/query/kmp/kmp001/a/PerformInitialStartup.java
@@ -1,5 +1,26 @@
 package nts.uk.screen.at.app.query.kmp.kmp001.a;
 
+import java.util.Optional;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import nts.uk.ctx.at.record.dom.stamp.card.stamcardedit.StampCardEditing;
+import nts.uk.ctx.at.record.dom.stamp.card.stamcardedit.StampCardEditingRepo;
+import nts.uk.shr.com.context.AppContexts;
+
+@Stateless
 public class PerformInitialStartup {
 
+	@Inject
+	private StampCardEditingRepo stampCardEditRepo;
+
+	public PerformInitialStartupDto findDto() {
+
+		String cId = AppContexts.user().companyId();
+		Optional<StampCardEditing> stampCardEditingOpt = stampCardEditRepo.get(cId);
+
+		return stampCardEditingOpt.map(s -> new PerformInitialStartupDto(s.getDigitsNumber().v() == null ? 0 : s.getDigitsNumber().v(), s.getMethod().value))
+				.orElseThrow(RuntimeException :: new);
+	}
 }
--- nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/query/kmp/kmp001/a/PerformInitialStartupDto.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/query/kmp/kmp001/a/PerformInitialStartupDto.java
@@ -1,5 +1,25 @@
 package nts.uk.screen.at.app.query.kmp.kmp001.a;
 
-public class PerformInitialStartupDto {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+/**
+ * output of 初期起動を行う:
+ * 打刻カード桁数：打刻カード桁数
+ * 打刻カード編集方法：打刻カード編集方法
+ * @author chungnt
+ *
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class PerformInitialStartupDto {
+	
+	//打刻カード桁数
+	private int stampCardDigitNumber;
+	
+	//打刻カード編集方法
+	private int  stampCardEditMethod;
+	
 }
