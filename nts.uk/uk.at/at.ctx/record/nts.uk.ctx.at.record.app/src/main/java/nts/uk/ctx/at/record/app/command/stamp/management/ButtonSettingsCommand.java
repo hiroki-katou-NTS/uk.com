c6eb075973e9f7b2e8a56becc96bd87a5b16c197
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2020-06-25 14:13:24 +0700

    KDP010  - Fix bug không save được stampType= null

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/stamp/management/ButtonSettingsCommand.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/stamp/management/ButtonSettingsCommand.java
@@ -70,7 +70,7 @@ public class ButtonSettingsCommand {
 					changeClockArt, changeCalArt);
 		}
 		
-		ButtonType buttonType = new ButtonType(reservationArt, Optional.of(stampType));
+		ButtonType buttonType = new ButtonType(reservationArt, Optional.ofNullable(stampType));
 
 		NotUseAtr usrArts = EnumAdaptor.valueOf(x.getUsrArt(), NotUseAtr.class);
 
