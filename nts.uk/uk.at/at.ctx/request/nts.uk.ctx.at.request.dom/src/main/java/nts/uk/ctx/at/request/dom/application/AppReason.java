0616aeb3f387b832b3c413cbc11ac993fd628c7e
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2020-06-25 11:19:05 +0700

    refactor 4: sửa primitive AppReason 申請理由

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/AppReason.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/AppReason.java
@@ -3,6 +3,12 @@ package nts.uk.ctx.at.request.dom.application;
 import nts.arc.primitive.StringPrimitiveValue;
 import nts.arc.primitive.constraint.StringMaxLength;
 
+/**
+ * refactor 4
+ * 申請理由
+ * @author Doan Duy Hung
+ *
+ */
 @StringMaxLength(400)
 public class AppReason extends StringPrimitiveValue<AppReason> {
 
