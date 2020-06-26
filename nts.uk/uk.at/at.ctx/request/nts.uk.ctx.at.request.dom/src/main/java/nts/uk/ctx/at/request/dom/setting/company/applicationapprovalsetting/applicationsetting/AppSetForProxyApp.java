f603f8561a5016c10ea7f7e276fad19c0a140e98
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2020-06-26 14:05:22 +0700

    refactor 4: thêm class AppSetForProxyApp 代行申請で利用できる申請設定

---
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/setting/company/applicationapprovalsetting/applicationsetting/AppSetForProxyApp.java
@@ -0,0 +1,35 @@
+package nts.uk.ctx.at.request.dom.setting.company.applicationapprovalsetting.applicationsetting;
+
+import java.util.List;
+import java.util.Optional;
+
+import lombok.Getter;
+import nts.uk.ctx.at.request.dom.application.ApplicationType;
+import nts.uk.ctx.at.request.dom.application.overtime.OvertimeAppAtr;
+import nts.uk.ctx.at.request.dom.application.stamp.StampRequestMode;
+
+/**
+ * refactor 4
+ * UKDesign.ドメインモデル."NittsuSystem.UniversalK".就業.contexts.申請承認.設定.会社別.申請承認設定.申請設定.代行申請で利用できる申請設定
+ * @author Doan Duy Hung
+ *
+ */
+@Getter
+public class AppSetForProxyApp {
+	
+	/**
+	 * 申請種類
+	 */
+	private List<ApplicationType> appTypeLst;
+	
+	/**
+	 * 残業区分
+	 */
+	private Optional<OvertimeAppAtr> opOvertimeAppAtr;
+	
+	/**
+	 * 打刻申請モード
+	 */
+	private Optional<StampRequestMode> opStampRequestMode;
+	
+}
