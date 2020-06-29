c129ba25bd2fcdf161557955fc51e2350579c718
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2020-06-29 11:13:29 +0700

    refactor 4: thêm xử lý メールサーバを設定したかチェックする

--- nts.uk/uk.com/com.ctx/sys/env/nts.uk.ctx.sys.env.pub/src/main/java/nts/uk/ctx/sys/env/pub/mailserver/MailServerPub.java
+++ nts.uk/uk.com/com.ctx/sys/env/nts.uk.ctx.sys.env.pub/src/main/java/nts/uk/ctx/sys/env/pub/mailserver/MailServerPub.java
@@ -2,4 +2,11 @@ package nts.uk.ctx.sys.env.pub.mailserver;
 
 public interface MailServerPub {
 	boolean findBy(String companyId);
+	
+	/**
+	 * メールサーバを設定したかチェックする
+	 * @param companyID
+	 * @return
+	 */
+	public MailServerSetExport checkMailServerSet(String companyID); 
 }
