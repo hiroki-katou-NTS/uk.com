de18523c91c47a51f93d3f6bc3e50966d4b6468e
Hieult91 <king.of.throw.game@gmail.com>
2020-06-29 14:27:49 +0700

    update Domain Work Shcedule

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/workschedule/ScheManaStatuTempo.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/workschedule/ScheManaStatuTempo.java
@@ -1,5 +1,133 @@
 package nts.uk.ctx.at.schedule.dom.workschedule;
 
-public class ScheManaStatuTempo {
+import java.util.List;
+import java.util.Optional;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import nts.arc.time.GeneralDate;
+import nts.arc.time.calendar.period.DatePeriod;
+import nts.uk.ctx.at.shared.dom.adapter.employee.AffCompanyHistSharedImport;
+import nts.uk.ctx.at.shared.dom.adapter.employee.EmpEmployeeAdapter;
+import nts.uk.ctx.at.shared.dom.vacation.setting.compensatoryleave.EmploymentCode;
+import nts.uk.ctx.at.shared.dom.workingcondition.ManageAtr;
+import nts.uk.ctx.at.shared.dom.workingcondition.WorkingCondition;
+
+
+
+/**
+ * 社員の予定管理状態	
+ * UKDesign.ドメインモデル."NittsuSystem.UniversalK".就業.contexts.勤務予定.勤務予定.勤務予定
+ * @author HieuLt
+ *
+ */
+@Getter
+@RequiredArgsConstructor
+public class ScheManaStatuTempo {
+ 
+	/**	社員ID **/ 
+	private final String employeeID;
+	
+	/** 年月日 **/ 
+	private final GeneralDate date;
+	
+	/**	予定管理状態 **/
+	private final ScheManaStatus scheManaStatus;
+	
+	/** 休業枠NO**/
+	private final Optional<TempAbsenceFrameNo> optTempAbsenceFrameNo;
+	/** 雇用コード **/
+	private final Optional<EmploymentCode> optEmploymentCd;
+	
+	
+	/**
+	 * [C-1] 作成する	
+	 * @param require
+	 * @param employeeID
+	 * @param date
+	 * @return
+	 */
+	public ScheManaStatuTempo create(Require require, String employeeID, GeneralDate date) {
+		return null;
+	}
+	/**
+	 * [S-1] 在籍中か		
+	 * @param require
+	 * @param employeeID
+	 * @param date
+	 * @return
+	 */
+	private static boolean enrolled(Require require, String employeeID, GeneralDate date)
+			 {
+		return false;
+	}
+	/**
+	 * [S-2] 雇用コードを取得する			
+	 * @param require
+	 * @param employeeID
+	 * @param date
+	 * @return
+	 */
+	private static Optional<EmploymentCode> getEmplomentCd (Require require, String employeeID, GeneralDate date)
+	 {
+		return Optional.empty();
+	 }
+	
+	/**
+	 * [S-3] 予定管理区分を取得する	
+	 * @param require
+	 * @param employeeID
+	 * @param date
+	 * @return
+	 */
+	private static Optional<ManageAtr> getManageAtr(Require require, String employeeID, GeneralDate date){
+		return Optional.empty();
+	}
+	/**
+	 * [S-4] 休職中か		
+	 * @param require
+	 * @param employeeID
+	 * @param date
+	 * @return 
+	 */
+	private static boolean onLeave(Require require, String employeeID, GeneralDate date){
+		return false;
+	}
+	/**
+	 * [S-5] 休業枠NOを取得する	
+	 * @param require
+	 * @param employeeID
+	 * @param date
+	 * @return Optional<休職休業枠NO>
+	 */
+	private static Optional<TempAbsenceFrameNo> getTempAbsenceFrameNo(Require require, String employeeID, GeneralDate date){
+		return Optional.empty();
+	}
+	public static interface Require extends EmpEmployeeAdapter  {
+		
+		//@Inject
+		//private WorkingConditionRepository repo;
+		
+		/**
+		 * 	[R-1] 在籍期間を取得する( 社員ID, 年月日 ) : Optional
+		 * 	社員の所属会社履歴Adapter.期間を指定して在籍期間を取得する( list: 社員ID, 期間: 年月日 )		
+		 */ 
+		List<AffCompanyHistSharedImport> getAffCompanyHistByEmployee(List<String> sids, DatePeriod datePeriod);
+		
+		/** 																																					
+		 *[R-2] 労働条件履歴を取得する	
+		 * 	労働条件Repository.社員を指定して年月日時点の履歴項目を取得する( 会社ID, 社員ID, 年月日 )
+		 */
+			
+		//Optional<WorkingCondition>   repo.getBy getBySidAndStandardDate(String companyId, String employeeId, GeneralDate baseDate);
+		Optional<WorkingCondition> getBySidAndStandardDate(String companyId, String employeeId, GeneralDate baseDate);
+		
+		/**
+		 *  [R-3] 休職期間を取得する( 社員ID, 年月日 ) : Optional	
+		 * 	社員の休職履歴Adapter.期間を指定して休職期間を取得する( list: 社員ID, 期間: 年月日 )			
+		 */
+		
+		
+	}
+	
 }
--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/workschedule/domainservice/DailyResultAccordScheduleStatus.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/workschedule/domainservice/DailyResultAccordScheduleStatus.java
@@ -1,4 +1,15 @@
 package nts.uk.ctx.at.schedule.dom.workschedule.domainservice;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import nts.arc.time.GeneralDate;
+import nts.arc.time.calendar.period.DatePeriod;
+import nts.uk.ctx.at.schedule.dom.workschedule.ScheManaStatuTempo;
+import nts.uk.ctx.at.schedule.dom.workschedule.WorkSchedule;
+import nts.uk.ctx.at.shared.dom.dailyattdcal.dailyattendance.dailyattendancework.IntegrationOfDaily;
+
 /**
  * 予定管理状態に応じて日別実績を取得する	
  * UKDesign.ドメインモデル."NittsuSystem.UniversalK".就業.contexts.勤務予定.勤務予定.勤務予定
@@ -9,7 +20,30 @@ package nts.uk.ctx.at.schedule.dom.workschedule.domainservice;
 public class DailyResultAccordScheduleStatus {
 	
 	
-	/*public Map<ScheManaStatuTempo, Optional<WorkSchedule>>
-//日別勤怠(Work)
-	//IntegrationOfDaily --日別勤怠(Work)
-*/}
+	/**
+	 * 	[1] 取得する	
+	 * @param require
+	 * @param employeeID
+	 * @param datePeriod
+	 * @return Map<ScheManaStatuTempo, Optional<IntegrationOfDaily>> --- Map<社員の予定管理状態, Optional<日別勤怠(Work)>>
+	 */
+	public static Map<ScheManaStatuTempo , Optional<IntegrationOfDaily>> get(Require require, String employeeID , DatePeriod datePeriod ){
+		Map<ScheManaStatuTempo, Optional<IntegrationOfDaily>> map = new HashMap<>();
+		return map;
+	}
+
+	/**
+	 * 	[prv-1] 社員別に取得する	
+	 * @param employeeID
+	 * @param datePeriod
+	 * @return Map<ScheManaStatuTempo, Optional<IntegrationOfDaily>> --- Map<社員の予定管理状態, Optional<日別勤怠(Work)>>
+	 */
+	private Map<ScheManaStatuTempo , Optional<IntegrationOfDaily>> getByEmp(String employeeID , DatePeriod datePeriod){
+		Map<ScheManaStatuTempo, Optional<IntegrationOfDaily>> map = new HashMap<>();
+		return map;
+	}
+
+	public static interface Require  {
+		
+	}
+}
