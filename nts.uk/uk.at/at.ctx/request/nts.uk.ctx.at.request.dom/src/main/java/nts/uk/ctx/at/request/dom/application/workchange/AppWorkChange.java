e1f6bcccbf789e01592657c80e3011fd663e3d95
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2020-06-30 15:32:34 +0700

    refactor 4: thêm domain AppWorkChange 勤務変更申請

---
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/workchange/AppWorkChange.java
@@ -0,0 +1,58 @@
+package nts.uk.ctx.at.request.dom.application.workchange;
+
+import java.util.List;
+import java.util.Optional;
+
+import lombok.Getter;
+import nts.uk.ctx.at.request.dom.application.Application;
+import nts.uk.ctx.at.shared.dom.common.TimeZoneWithWorkNo;
+import nts.uk.ctx.at.shared.dom.worktime.common.WorkTimeCode;
+import nts.uk.ctx.at.shared.dom.worktype.WorkTypeCode;
+import nts.uk.shr.com.enumcommon.NotUseAtr;
+
+/**
+ * refactor 4
+ * UKDesign.ドメインモデル."NittsuSystem.UniversalK".就業.contexts.申請承認.申請.勤務変更申請.勤務変更申請
+ * @author Doan Duy Hung
+ *
+ */
+@Getter
+public class AppWorkChange extends Application {
+
+	/**
+	 * 直行区分
+	 */
+	private NotUseAtr straightGo;
+	
+	/**
+	 * 直帰区分
+	 */
+	private NotUseAtr straightBack;
+	
+	/**
+	 * 勤務種類コード
+	 */
+	private Optional<WorkTypeCode> opWorkTypeCD;
+	
+	/**
+	 * 就業時間帯コード
+	 */
+	private Optional<WorkTimeCode> opWorkTimeCD;
+	
+	/**
+	 * 勤務時間帯
+	 */
+	private List<TimeZoneWithWorkNo> timeZoneWithWorkNoLst;
+	
+	public AppWorkChange(NotUseAtr straightGo, NotUseAtr straightBack,
+		Optional<WorkTypeCode> opWorkTypeCD, Optional<WorkTimeCode> opWorkTimeCD,
+		List<TimeZoneWithWorkNo> timeZoneWithWorkNoLst, Application application) {
+		super(application);
+		this.straightGo = straightGo;
+		this.straightBack = straightBack;
+		this.opWorkTypeCD = opWorkTypeCD;
+		this.opWorkTimeCD = opWorkTimeCD;
+		this.timeZoneWithWorkNoLst = timeZoneWithWorkNoLst;
+	}
+	
+}
