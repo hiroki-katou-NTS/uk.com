43faee96b32698cce59f3dac5da4d0e552657efd
Hieult91 <king.of.throw.game@gmail.com>
2020-07-01 20:02:54 +0700

    update Domain Schedule Team & Schedule

---
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/employeeinfo/scheduleteam/ScheduleTeamRemarks.java
@@ -0,0 +1,26 @@
+package nts.uk.ctx.at.schedule.dom.employeeinfo.scheduleteam;
+
+import nts.arc.primitive.StringPrimitiveValue;
+import nts.arc.primitive.constraint.CharType;
+import nts.arc.primitive.constraint.StringCharType;
+import nts.arc.primitive.constraint.StringMaxLength;
+
+/**
+ * スケジュールチーム備考
+ * UKDesign.ドメインモデル."NittsuSystem.UniversalK".就業.contexts.勤務予定.社員情報.スケジュールチーム
+ * @author Hieult
+ *
+ */
+@StringMaxLength(12)
+@StringCharType(CharType.ANY_HALF_WIDTH)
+
+public class ScheduleTeamRemarks extends StringPrimitiveValue<ScheduleTeamRemarks>{
+
+
+	private static final long serialVersionUID = 1L;
+	
+	public ScheduleTeamRemarks(String rawValue) {
+		super(rawValue);
+	}
+
+}
