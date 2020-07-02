aa986cbd729482570a95069a8e74a8c098f4325a
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2020-07-02 16:33:11 +0700

    refactor 4: thêm root ApprovalListDisplaySetting 承認一覧表示設定

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/setting/company/applicationapprovalsetting/approvallistsetting/WeekNumberDays.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/setting/company/applicationapprovalsetting/approvallistsetting/WeekNumberDays.java
@@ -1,16 +1,17 @@
-package nts.uk.ctx.at.request.dom.setting.company.request.approvallistsetting;
+package nts.uk.ctx.at.request.dom.setting.company.applicationapprovalsetting.approvallistsetting;
 
 import nts.arc.primitive.IntegerPrimitiveValue;
 import nts.arc.primitive.constraint.IntegerRange;
 /**
- * 週間日数（警告前日数）
+ * refactor 4
+ * UKDesign.ドメインモデル."NittsuSystem.UniversalK".就業.contexts.申請承認.設定.会社別.申請承認設定.承認一覧設定.週間日数（警告前日数）
  * @author Doan Duy Hung
  *
  */
 @IntegerRange(max = 7, min = 0)
 public class WeekNumberDays extends IntegerPrimitiveValue<WeekNumberDays>{
 
-	public WeekNumberDays(Integer rawValue) {
+	public WeekNumberDays(int rawValue) {
 		super(rawValue);
 	}
 
