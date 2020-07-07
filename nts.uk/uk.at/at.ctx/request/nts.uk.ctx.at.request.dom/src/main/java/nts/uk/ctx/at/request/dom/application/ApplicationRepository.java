e8aaaac1eee4d2c038cbfa4a1319a248a8422939
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2020-07-07 09:53:20 +0700

    refactor 4: thêm class JpaApplicationRepository

---
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/ApplicationRepository.java
@@ -0,0 +1,18 @@
+package nts.uk.ctx.at.request.dom.application;
+
+import java.util.Optional;
+
+/**
+ * 
+ * @author Doan Duy Hung
+ *
+ */
+public interface ApplicationRepository {
+	
+	public Optional<Application> findByID(String companyID, String appID);
+	
+	public void insert(Application application);
+	
+	public void update(Application application);
+	
+}
