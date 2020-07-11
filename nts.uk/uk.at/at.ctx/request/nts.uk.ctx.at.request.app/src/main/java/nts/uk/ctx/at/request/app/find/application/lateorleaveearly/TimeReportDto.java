c91e38f935b86915524d696bc35f56a25e3ca3c9
NSVN-NguyenDinhHoang <61735854+NSVN-NguyenDinhHoang@users.noreply.github.com>
2020-07-11 16:20:39 +0700

    Fix Error Dto KAF004

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.app/src/main/java/nts/uk/ctx/at/request/app/find/application/lateorleaveearly/TimeReportDto.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.app/src/main/java/nts/uk/ctx/at/request/app/find/application/lateorleaveearly/TimeReportDto.java
@@ -3,24 +3,30 @@ package nts.uk.ctx.at.request.app.find.application.lateorleaveearly;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import nts.arc.enums.EnumAdaptor;
+import nts.uk.ctx.at.request.dom.application.lateorleaveearly.LateOrEarlyClassification;
 import nts.uk.ctx.at.request.dom.application.lateorleaveearly.TimeReport;
+import nts.uk.shr.com.time.TimeWithDayAttr;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-//遅刻早退時刻報告
+// 遅刻早退時刻報告
 public class TimeReportDto {
-//	勤務NO
+	// 勤務NO
 	private int workNo;
-//	区分
+	// 区分
 	private int lateOrEarlyClassification;
-//	時刻
+	// 時刻
 	// wait handle
 	private int timeWithDayAttr;
-	
-	public static TimeReportDto convertDto(TimeReport value) {
-		return new TimeReportDto(
-				value.getWorkNo(), 
-				value.getLateOrEarlyClassification().value, 
-				1);
+
+	public static TimeReportDto fromDomain(TimeReport value) {
+		return new TimeReportDto(value.getWorkNo(), value.getLateOrEarlyClassification().value, 1);
+	}
+
+	public TimeReport toDomain() {
+		return new TimeReport(workNo, EnumAdaptor.valueOf(lateOrEarlyClassification, LateOrEarlyClassification.class),
+				new TimeWithDayAttr(timeWithDayAttr));
 	}
 }
