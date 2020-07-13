2dbbddcdb0f4f7efe358493d9b238d3bc8f2f551
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2020-07-13 16:24:14 +0700

    refactor 4: KAF000A: update UI 13/07/2020

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf_ref/000/a/component5/kaf000.a.component5.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf_ref/000/a/component5/kaf000.a.component5.ts
@@ -4,14 +4,17 @@ module nts.uk.at.view.kaf000_ref.a.component5.viewmodel {
         name: 'kaf000-a-component5',
         template: '/nts.uk.at.web/view/kaf_ref/000/a/component5/index.html'
     })
-    class Kaf000AComponent2ViewModel extends ko.ViewModel {
+    class Kaf000AComponent5ViewModel extends ko.ViewModel {
         appType: number;
         appDispInfoStartupOutput: any;
-        opAppStandardReasonCD: KnockoutObservable<number> = ko.observable(1);
-        opAppReason: KnockoutObservable<string> = ko.observable("");
-        reasonTypeItemLst: KnockoutObservableArray<any> = ko.observableArray([]);
+        opAppStandardReasonCD: KnockoutObservable<number>;
+        opAppReason: KnockoutObservable<string>;
+        reasonTypeItemLst: KnockoutObservableArray<any>;
         created(params: any) {
             const vm = this;
+            vm.opAppStandardReasonCD = ko.observable(1);
+            vm.opAppReason = ko.observable("");
+            vm.reasonTypeItemLst = ko.observableArray([]);
             vm.reasonTypeItemLst([
                 {
                     appStandardReasonCD: 1,
