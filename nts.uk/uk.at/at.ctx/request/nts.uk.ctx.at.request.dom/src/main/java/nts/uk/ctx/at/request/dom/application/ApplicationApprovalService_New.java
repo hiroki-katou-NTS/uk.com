d4bdd41feaa89dd5a6c507152b0bdd3a1f85981a
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2020-07-16 16:49:59 +0700

    resolve conflict merge 16/07/2020

---
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/ApplicationApprovalService_New.java
@@ -0,0 +1,14 @@
+package nts.uk.ctx.at.request.dom.application;
+
+/**
+ * 
+ * @author Doan Duy Hung
+ *
+ */
+public interface ApplicationApprovalService_New {
+	
+	public void insert(Application application);
+	
+	public void delete(String companyID, String appID, int version, ApplicationType_Old appType);
+	
+}
