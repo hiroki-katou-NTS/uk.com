1cf0652f63398e809888dccd7f6683878f40ce84
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2020-07-17 10:06:34 +0700

    refactor 4: update xử lý 2-2.新規画面登録時承認反映情報の整理 17/07/2020

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/common/service/newscreen/RegisterAtApproveReflectionInfoService_New.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/common/service/newscreen/RegisterAtApproveReflectionInfoService_New.java
@@ -3,14 +3,17 @@ package nts.uk.ctx.at.request.dom.application.common.service.newscreen;
 import nts.uk.ctx.at.request.dom.application.Application;
 
 /**
- * 2-2.新規画面登録時承認反映情報の整理
+ * refactor 4
+ * UKDesign.UniversalK.就業.KAF_申請.共通アルゴリズム.2-2.新規画面登録時承認反映情報の整理(register: reflection info setting)
+ * @author Doan Duy Hung
+ *
  */
 public interface RegisterAtApproveReflectionInfoService_New {
 	/**
-	 * 新規画面登録時承認反映情報の整理
+	 * 2-2.新規画面登録時承認反映情報の整理
 	 * @param empID
 	 * @param application
 	 */
-	public void newScreenRegisterAtApproveInfoReflect(String SID, Application application);
+	public void newScreenRegisterAtApproveInfoReflect(String empID, Application application);
 
 }
