dc8ed44ccc4f15adfaa7e7506d3e285f9165d1cb
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2020-07-21 17:50:41 +0700

    Update card-list-component.ts

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmp/001/a/components/card-list-component.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmp/001/a/components/card-list-component.ts
@@ -7,15 +7,16 @@ module nts.uk.at.view.kmp001.a {
 	<!-- ko if: ko.unwrap(model.stampCard).length === 0 -->
 			<div
 				data-bind="ntsFormLabel: {
-					constraint: '10', 
+					constraint: $component.constraint, 
 					required: true, 
 					text: $i18n('KMP001_22') }">
 			</div>
 			<input 
 				data-bind="ntsTextEditor: {
 					value: ko.observable(''),
-					constraint: '10',
-					enabled: true
+					constraint: $component.constraint,
+					enabled: true, 
+					required: true
 				}"/>		
 	<!-- /ko -->
 	<!-- ko if: ko.unwrap(model.stampCard).length !== 0 -->
@@ -23,52 +24,55 @@ module nts.uk.at.view.kmp001.a {
 		<!-- ko if: $index() === $component.model.selectedStampCardIndex() -->
 			<div
 				data-bind="ntsFormLabel: {
-					constraint: '', 
+					constraint: $component.constraint, 
 					required: true, 
 					text: $i18n('KMP001_22') }">
 			</div>
 			<input 
 				data-bind="ntsTextEditor: {
-					value: stampNumber,
-					constraint: ''
+					value: stampNumber, 
+					required: true,
+					constraint: $component.constraint
 				}"/>
 		<!-- /ko -->
 	</div>
 	<!-- /ko -->
 	<table id="stampcard-list"></table>
 	`;
-	
+
 	const KMP001A_CARD_LIST = {
 		GET_STAMPCARDDIGIT: 'screen/pointCardNumber/getStampCardDigit'
 	};
-	
+
 	@component({
 		name: 'card-list-component',
 		template: templateCardList
 	})
 	export class CardListComponent extends ko.ViewModel {
 		model!: share.Model;
-		
+
 		public constraint: KnockoutObservable<string> = ko.observable('StampNumber');
 
 		created(params: any) {
 			const vm = this;
 
 			vm.model = params.model;
-			
+
 			vm.$ajax(KMP001A_CARD_LIST.GET_STAMPCARDDIGIT)
 				.then((data: any) => {
 					const ck = ko.toJS(vm.constraint);
-					const constraint = _.get(__viewContext.primitiveValueConstraints, ck);
 
-					if (constraint) {
-						_.extend(constraint, {
-							maxLength: data.stampCardDigitNumber
+					vm.$validate.constraint(ck)
+						.then((constraint) => {
+							if (constraint) {
+								_.extend(constraint, {
+									maxLength: data.stampCardDigitNumber
+								});
+								
+								vm.$validate.constraint(ck, constraint);
+								vm.constraint.valueHasMutated();
+							}
 						});
-
-						(nts.uk.ui.validation as any).writeConstraint(ck, constraint);
-						vm.constraint.valueHasMutated();
-					}
 				});
 		}
 
@@ -121,10 +125,10 @@ module nts.uk.at.view.kmp001.a {
 						$(vm.$el).find('.ui-iggrid-rowselector-header').html('').append($('<span>', { class: 'ui-iggrid-headertext', text: vm.$i18n('KMP001_31') }));
 					}
 				});
-				
-				/*if ($grid.data('igGrid') && $grid.data('igGridSelection') && $grid.igGrid('option', 'dataSource').length) {
-					$grid.igGridSelection("selectRow", 0);
-				}*/
+
+			/*if ($grid.data('igGrid') && $grid.data('igGridSelection') && $grid.igGrid('option', 'dataSource').length) {
+				$grid.igGridSelection("selectRow", 0);
+			}*/
 
 			ko.computed(() => {
 				const stampCard = ko.unwrap(vm.model.stampCard);
