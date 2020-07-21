ba87f1a8dba58ba8dcbe83e85d26a8f3965e96e4
NSVN-PhamTrongDuc <duc_pt@nittsusystem-vn.com>
2020-07-21 09:36:05 +0700

    update UI

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdl/014/a/kdl014.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdl/014/a/kdl014.a.vm.ts
@@ -20,46 +20,46 @@ module nts.uk.at.view.kdl014.a {
         
         constructor(){
             let self = this;
-            self.mode = 0;
+            self.mode = 1;
             
             if (self.mode == 1) {
                 self.columns = ko.observableArray([
                     { headerText: getText("KDL014_11"), key: 'code', hidden: true },
-                    { headerText: "<div style='text-align: center;'>"+getText("KDL014_12")+ "</div>" , key: 'name', width: 150},
+                    { headerText: "<div style='text-align: center;'>"+getText("KDL014_12")+ "</div>" , key: 'name', width: 100},
                     { headerText: "<div style='text-align: center;'>"+getText("KDL014_13")+ "</div>" , key: 'dateShow', width: 115},
                     { headerText: "<div style='text-align: center;'>"+getText("KDL014_14")+ "</div>" , key: 'time', width: 80},
-                    { headerText: "<div style='text-align: center;'>"+getText("KDL014_15")+ "</div>" , key: 'stampAtr', width: 100},
-                    { headerText: "<div style='text-align: center;'>"+getText("KDL014_16")+ "</div>" , key: 'workLocationName', width: 500},
-                    { headerText: "<div style='text-align: center;'>"+getText("KDL014_17")+ "</div>" , key: 'locationInfo', width: 150},
+                    { headerText: "<div style='text-align: center;'>"+getText("KDL014_15")+ "</div>" , key: 'stampAtr', width: 70},
+                    { headerText: "<div style='text-align: center;'>"+getText("KDL014_16")+ "</div>" , key: 'workLocationName', width: 200},
+                    { headerText: "<div style='text-align: center;'>"+getText("KDL014_17")+ "</div>" , key: 'locationInfo', width: 50},
                 ]);
             } else {
                 self.columns = ko.observableArray([
-                
-                { headerText: getText("KDL014_11"), key: 'code', hidden: true },
+
+                    { headerText: getText("KDL014_11"), key: 'code', hidden: true },
                     { headerText: getText("KDL014_12"), key: 'name', hidden: true },
-                    { headerText: "<div style='text-align: center;'>"+getText("KDL014_13")+ "</div>" , key: 'dateShow', width: 115},
-                    { headerText: "<div style='text-align: center;'>"+getText("KDL014_14")+ "</div>" , key: 'time', width: 80},
-                    { headerText: "<div style='text-align: center;'>"+getText("KDL014_15")+ "</div>" , key: 'stampAtr', width: 100},
-                    { headerText: "<div style='text-align: center;'>"+getText("KDL014_16")+ "</div>" , key: 'workLocationName', width: 500},
-                    { headerText: "<div style='text-align: center;'>"+getText("KDL014_17")+ "</div>" , key: 'locationInfo', width: 150},
+                    { headerText: "<div style='text-align: center;'>" + getText("KDL014_13") + "</div>", key: 'dateShow', width: 115 },
+                    { headerText: "<div style='text-align: center;'>" + getText("KDL014_14") + "</div>", key: 'time', width: 80 },
+                    { headerText: "<div style='text-align: center;'>" + getText("KDL014_15") + "</div>", key: 'stampAtr', width: 90 },
+                    { headerText: "<div style='text-align: center;'>" + getText("KDL014_16") + "</div>", key: 'workLocationName', width: 250 },
+                    { headerText: "<div style='text-align: center;'>" + getText("KDL014_17") + "</div>", key: 'locationInfo', width: 50 },
                 ]);
-               
-                self.employeeInputList.push({ id: 'ae7fe82e-a7bd-4ce3-adeb-5cd403a9d570', code: 'A000000000001', businessName: '日通　純一郎1', workplaceName: '名古屋支店', depName: 'Dep Name' },
-                { id: '8f9edce4-e135-4a1e-8dca-ad96abe405d6', code: 'A000000000002', businessName: '日通　純一郎2', workplaceName: '名古屋支店', depName: 'Dep Name' });
-                
-                $('#emp-component').ntsLoadListComponent({
-                    systemReference: SystemType.EMPLOYMENT,
-                    isDisplayOrganizationName: true,
-                    employeeInputList: self.employeeInputList,
-                    targetBtnText: getText('KCP009_3'),
-                    selectedItem: self.selectedItem,
-                    tabIndex: 1
-                });
-                
-                self.selectedItem.subscribe((newValue) => {
-                    self.filterGrid(newValue)
-                });
             }
+               
+            self.employeeInputList.push({ id: 'ae7fe82e-a7bd-4ce3-adeb-5cd403a9d570', code: 'A000000000001', businessName: '日通　純一郎1', workplaceName: '名古屋支店', depName: 'Dep Name' },
+                { id: '8f9edce4-e135-4a1e-8dca-ad96abe405d6', code: 'A000000000002', businessName: '日通　純一郎2', workplaceName: '名古屋支店', depName: 'Dep Name' });
+
+            $('#emp-component').ntsLoadListComponent({
+                systemReference: SystemType.EMPLOYMENT,
+                isDisplayOrganizationName: false,
+                employeeInputList: self.employeeInputList,
+                targetBtnText: getText('KCP009_3'),
+                selectedItem: self.selectedItem,
+                tabIndex: 1
+            });
+
+            self.selectedItem.subscribe((newValue) => {
+                self.filterGrid(newValue)
+            });
         }
         
         startPage(): JQueryPromise<any> {
@@ -69,7 +69,7 @@ module nts.uk.at.view.kdl014.a {
             let param = {
                 start: new Date('2019-07-13'),
                 end:new Date('2020-07-14'),
-                mode: 0, 
+                mode: 1, 
                 listEmp: [
                     {employeeId: 'ae7fe82e-a7bd-4ce3-adeb-5cd403a9d570', employeeCode: '', employeeName: '', affiliationName: ''},
                     {employeeId: '8f9edce4-e135-4a1e-8dca-ad96abe405d6', employeeCode: '', employeeName: '', affiliationName: ''}
@@ -80,7 +80,7 @@ module nts.uk.at.view.kdl014.a {
                 _.orderBy(data, ['name','stampDateTime'], ['asc','asc']);
                 console.log(data);
                 self.dataServer = data.listEmps;
-                self.selectedItem(self.employeeInputList()[0].id);
+                    self.selectedItem(self.employeeInputList()[0].id);
                 dfd.resolve();
             }).fail(function (res) {
                 error({ messageId: res.messageId });
@@ -170,9 +170,10 @@ module nts.uk.at.view.kdl014.a {
                 self.dateShow = "<span>"+ nts.uk.time.applyFormat("Short_YMDW", date) + "</span>"            
             }
             
-            if (param.locationInfo !== null) {
-                this.locationInfo = '<a href="https://www.google.co.jp/maps/place/' + param.locationInfo.latitude + ',' + param.locationInfo.longitude + '"><img src="../img/Mapアイコン画像.png" height="20" width="20"/></a>';
-            }
+//            if (param.locationInfo === null) {
+//                self.locationInfo = '<a href="https://www.google.co.jp/maps/place/' + param.locationInfo.latitude + ',' + param.locationInfo.longitude + '"><img src="../img/Mapアイコン画像.png" height="20" width="20"/></a>';
+//            }
+            self.locationInfo = '<div style="text-align: center;"><a href="https://www.google.co.jp/maps/place/35.165556, 136.905333"><img src="../img/Mapアイコン画像.png" height="20" width="20"/></a></div>';
         }
     }
     
