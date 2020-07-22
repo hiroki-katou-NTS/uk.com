10b6c8f91ec67b9131d35813aca51bcaf677e006
NSVN-PhamTrongDuc <duc_pt@nittsusystem-vn.com>
2020-07-22 13:45:01 +0700

    undo nameId to name StampMeans

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/workrecord/stampmanagement/stamp/StampMeans.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/workrecord/stampmanagement/stamp/StampMeans.java
@@ -39,7 +39,7 @@ public enum StampMeans {
 
 	public final int value;
 
-	public final String nameId;
+	public final String name;
 
 	/** The Constant values. */
 	private final static StampMeans[] values = StampMeans.values();
