2323f166f958fc0c9c8c6171389cdb4d16e41b71
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2020-07-23 17:43:46 +0700

    update define for dialog

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/lib/nittsu/viewcontext.d.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/lib/nittsu/viewcontext.d.ts
@@ -118,18 +118,22 @@ interface KnockoutStatic {
             }
             readonly $dialog: {
                 readonly info: {
+                    (message: string): JQueryDeferred<void>;
                     (options: { messageId: string; }): JQueryDeferred<void>;
                     (options: { messageId: string; messageParams: string[]; }): JQueryDeferred<void>;
                 };
                 readonly alert: {
+                    (message: string): JQueryDeferred<void>;
                     (options: { messageId: string; }): JQueryDeferred<void>;
                     (options: { messageId: string; messageParams: string[]; }): JQueryDeferred<void>;
                 };
                 readonly error: {
+                    (message: string): JQueryDeferred<void>;
                     (options: { messageId: string; }): JQueryDeferred<void>;
                     (options: { messageId: string; messageParams: string[]; }): JQueryDeferred<void>;
                 };
                 readonly confirm: {
+                    (message: string): JQueryDeferred<void>;
                     (options: { messageId: string; }): JQueryDeferred<'no' | 'yes' | 'cancel'>;
                     (options: { messageId: string; messageParams: string[]; }): JQueryDeferred<'no' | 'yes' | 'cancel'>;
                 };
