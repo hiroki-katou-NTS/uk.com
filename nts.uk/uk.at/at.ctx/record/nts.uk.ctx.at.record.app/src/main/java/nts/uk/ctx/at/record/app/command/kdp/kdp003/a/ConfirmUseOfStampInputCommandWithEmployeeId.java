977c28c20297510aef280bc0d0d890a052f458cb
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2020-07-23 11:38:37 +0700

    add ws

---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/kdp/kdp003/a/ConfirmUseOfStampInputCommandWithEmployeeId.java
@@ -0,0 +1,11 @@
+package nts.uk.ctx.at.record.app.command.kdp.kdp003.a;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import nts.uk.ctx.at.record.app.command.kdp.kdp001.a.ConfirmUseOfStampInputCommand;
+
+@Data
+@EqualsAndHashCode(callSuper=false)
+public class ConfirmUseOfStampInputCommandWithEmployeeId extends ConfirmUseOfStampInputCommand {
+	private String employeeId;
+}
