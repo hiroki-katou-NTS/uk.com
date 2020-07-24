64f00049e0877bffee9f591f053abd933723b65d
NSVN-NguyenDinhHoang <61735854+NSVN-NguyenDinhHoang@users.noreply.github.com>
2020-07-24 13:54:43 +0700

    Revert "Revert "Merge branch 'TeamD/Refactor4/Release_Build' into TeamD/Refactor4/Refactor""
    This reverts commit feb4284e41e6084d50c9c3a39ccd568f4415dbfc.

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf_ref/000/a/component1/kaf000.a.component1.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf_ref/000/a/component1/kaf000.a.component1.ts
@@ -1,5 +1,6 @@
 module nts.uk.at.view.kaf000_ref.a.component1.viewmodel {
-
+    import CommonProcess = nts.uk.at.view.kaf000_ref.shr.viewmodel.CommonProcess;
+    
     @component({
         name: 'kaf000-a-component1',
         template: '/nts.uk.at.web/view/kaf_ref/000/a/component1/index.html'
@@ -24,40 +25,7 @@ module nts.uk.at.view.kaf000_ref.a.component1.viewmodel {
             vm.displayDeadline = ko.observable(false);
             
             vm.appDispInfoStartupOutput.subscribe(value => {
-                vm.message(value.appDispInfoWithDateOutput.approvalFunctionSet.appUseSetLst[0].memo);
-                if(_.isEmpty(vm.message())) {
-                    vm.displayMsg(false);         
-                } else {
-                    vm.displayMsg(true);
-                }
-                
-                let advanceAppAcceptanceLimit = value.appDispInfoNoDateOutput.advanceAppAcceptanceLimit == 1;
-                let allowFutureDay = value.appDispInfoNoDateOutput.applicationSetting.receptionRestrictionSetting.afterhandRestriction.allowFutureDay;
-                let appDeadlineUseCategory = value.appDispInfoWithDateOutput.appDeadlineUseCategory == 1;
-                // 注意：申請表示情報(基準日関係なし)．事前申請の受付制限が利用しない && 申請表示情報．申請設定（基準日関係なし）．申請承認設定．申請設定．受付制限設定．事後の受付制限．未来日許可しないがfalse && 申請表示情報(基準日関係あり)．申請締め切り日利用区分が利用しない
-                if(!advanceAppAcceptanceLimit && !allowFutureDay && !appDeadlineUseCategory) {
-                    // 締め切りエリア全体に非表示
-                    vm.displayDeadline(false);        
-                } else {
-                    vm.displayDeadline(true);     
-                }
-                // {1}事前受付日
-                let prePart = "";
-                if(advanceAppAcceptanceLimit) {
-                    // ・申請表示情報(基準日関係なし)．事前受付時分がNull
-                    if(_.isNull(value.appDispInfoNoDateOutput.opAdvanceReceptionHours)) {
-                        prePart = vm.$i18n('KAF000_38', [value.appDispInfoNoDateOutput.opAdvanceReceptionDate]);        
-                    } 
-                    // ・申請表示情報(基準日関係なし)．事前受付時分がNullじゃない
-                    else {
-                        prePart = vm.$i18n('KAF000_41', [value.appDispInfoNoDateOutput.opAdvanceReceptionHours]);  
-                    }             
-                }
-                // {2}事後受付日
-                let postPart = vm.$i18n('KAF000_39', [moment(vm.$date.today()).format("YYYY/MM/DD")]);
-                // {3}締め切り期限日
-                let deadlinePart = vm.$i18n('KAF000_40', [value.appDispInfoWithDateOutput.opAppDeadline]);
-                vm.deadline(prePart + postPart + deadlinePart);
+                CommonProcess.initDeadlineMsg(value, vm);
             });
         }
     
--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf_ref/007/b/kaf007.b.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf_ref/007/b/kaf007.b.vm.ts
@@ -1,24 +1,51 @@
-module nts.uk.at.view.kaf007_ref.b.viewmodel {
-    import Kaf000BViewModel = nts.uk.at.view.kaf000_ref.b.viewmodel.Kaf000BViewModel;
+module nts.uk.at.view.kaf007_ref.c.viewmodel {
+    //import Kaf000BViewModel = nts.uk.at.view.kaf000_ref.b.viewmodel.Kaf000BViewModel;
+    import Application = nts.uk.at.view.kaf000_ref.shr.viewmodel.Application;
     import AppWorkChange = nts.uk.at.view.kaf007_ref.shr.viewmodel.AppWorkChange; 
     
     @component({
         name: 'kaf007-b',
         template: '/nts.uk.at.web/view/kaf_ref/007/b/index.html'
     })
-    class Kaf007BViewModel extends ko.ViewModel {
-        application: KnockoutObservable<Application>;
-        appWorkChange: KnockoutObservable<AppWorkChange>;
+    class Kaf007CViewModel extends ko.ViewModel {
         
-        created(params: any) {
+        appDispInfoStartupOutput: any;
+        
+        created(
+            params: { 
+                appDispInfoStartupOutput: any, 
+                event: (evt: () => void ) => void 
+            }
+        ) {
             const vm = this;
-            vm.application = params.application;
-            vm.appWorkChange = params.appWorkChange;
+            vm.appDispInfoStartupOutput = params.appDispInfoStartupOutput;
+            vm.application = ko.observable(new Application("", 1, [], 2, "", "", 0));
+            vm.appWorkChange = ko.observable(new AppWorkChange("001", "001", 100, 200));
+            
+            // gui event con ra viewmodel cha
+            // nhớ dùng bind(vm) để ngữ cảnh lúc thực thi
+            // luôn là component
+            params.event(vm.update.bind(vm));
         }
     
         mounted() {
             const vm = this;
-            vm.application().appDate(moment(new Date()).format("YYYY/MM/DD"));
         }
+        
+        // event update cần gọi lại ở button của view cha
+        update() {
+            const vm = this;
+            
+            console.log('update component', vm);    
+        }
+        
+        dispose() {
+            const vm = this;
+            
+        }
+    }
+    
+    const API = {
+        getWorkchangeByAppID_PC: "at/request/application/workchange/getWorkchangeByAppID_PC"
     }
 }
