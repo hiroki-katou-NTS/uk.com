d74a11a298803dd6c95701fa1b6eb9f7ff32ea2d
Hieult91 <king.of.throw.game@gmail.com>
2020-07-28 08:43:09 +0700

    Update Pub

--- nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.pubimp/src/main/java/nts/uk/ctx/bs/employee/pubimp/workplace/export/EmpOrganizationPubImpl.java
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.pubimp/src/main/java/nts/uk/ctx/bs/employee/pubimp/workplace/export/EmpOrganizationPubImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import nts.arc.time.GeneralDate;
@@ -20,6 +21,7 @@ import nts.uk.shr.com.context.AppContexts;
  * @author HieuLt
  *
  */
+@Stateless
 public class EmpOrganizationPubImpl implements EmpOrganizationPub {
 
 	@Override
