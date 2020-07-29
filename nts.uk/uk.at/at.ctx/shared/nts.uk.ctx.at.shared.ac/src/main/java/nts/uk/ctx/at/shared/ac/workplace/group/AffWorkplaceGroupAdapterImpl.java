5d0c824337b1a709e8283c3ef9e10cddd91d7d98
NSVN-PhongTQ <phong_tq@nittsusystem-vn.com>
2020-07-29 14:47:19 +0700

    # update server ksm 015

---
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.ac/src/main/java/nts/uk/ctx/at/shared/ac/workplace/group/AffWorkplaceGroupAdapterImpl.java
@@ -0,0 +1,23 @@
+package nts.uk.ctx.at.shared.ac.workplace.group;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import nts.uk.ctx.at.shared.dom.workrule.shiftmaster.AffWorkplaceAdapter;
+import nts.uk.ctx.bs.employee.pub.workplace.workplacegroup.WorkplaceGroupPublish;
+
+@Stateless
+public class AffWorkplaceGroupAdapterImpl implements AffWorkplaceAdapter{
+@Inject
+private WorkplaceGroupPublish groupPublish;
+
+@Override
+public List<String> getWKPID(String CID, String WKPGRPID) {
+	// TODO Auto-generated method stub
+	return groupPublish.getByWorkplaceGroupID(Arrays.asList(WKPGRPID)).stream().map(x->x.getWorkplaceId()).collect(Collectors.toList());
+}
+}
