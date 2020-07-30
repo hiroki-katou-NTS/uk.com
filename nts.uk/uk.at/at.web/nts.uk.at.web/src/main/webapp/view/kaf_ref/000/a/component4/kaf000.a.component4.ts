0e3e77c7456e033f4ce0896a326c51548407a05d
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2020-07-30 19:08:05 +0700

    refactor 4: update KAF000A UI: add validate

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf_ref/000/a/component4/kaf000.a.component4.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf_ref/000/a/component4/kaf000.a.component4.ts
@@ -1,3 +1,5 @@
+/// <reference path="../../../../../lib/nittsu/viewcontext.d.ts" />
+
 module nts.uk.at.view.kaf000_ref.a.component4.viewmodel {
     import AppType = nts.uk.at.view.kaf000_ref.shr.viewmodel.model.AppType;
     import CommonProcess = nts.uk.at.view.kaf000_ref.shr.viewmodel.CommonProcess;
@@ -9,21 +11,19 @@ module nts.uk.at.view.kaf000_ref.a.component4.viewmodel {
     class Kaf000AComponent4ViewModel extends ko.ViewModel {
         appDate: KnockoutObservable<string>;
         appDispInfoStartupOutput: any;
-        dispSingleDate: KnockoutObservable<boolean>;
-        checkBoxValue: KnockoutObservable<boolean>;
-        checkBoxText: KnockoutObservable<string>;
+        dispSingleDate: KnockoutObservable<boolean> = ko.observable(true);
+        checkBoxValue: KnockoutObservable<boolean> = ko.observable(false);
+
         created(params: any) {
             const vm = this;
             vm.appDate = params.application().appDate;
             vm.appDispInfoStartupOutput = params.appDispInfoStartupOutput;
-            vm.dispSingleDate = ko.observable(true);
-            vm.checkBoxValue = ko.observable(false);
-            vm.checkBoxText = ko.observable(vm.$i18n('KAF000_43'));
-            
+            params.component4ValidateEvent(vm.validate.bind(vm));
+
             vm.appDispInfoStartupOutput.subscribe(value => {
-                let appType = value.appDispInfoNoDateOutput.applicationSetting.appTypeSetting.appType;
+                /*let appType = value.appDispInfoNoDateOutput.applicationSetting.appTypeSetting.appType;
                 let conditionA8 = (appType == AppType.ABSENCE_APPLICATION || appType == AppType.WORK_CHANGE_APPLICATION || appType == AppType.BUSINESS_TRIP_APPLICATION);
-                let conditionA9 = !conditionA8; 
+                let conditionA9 = !conditionA8;*/ 
             });
             
             vm.appDate.subscribe(value => {
@@ -55,6 +55,11 @@ module nts.uk.at.view.kaf000_ref.a.component4.viewmodel {
                  
             });
         }
+
+		validate() {
+			const vm = this;
+			return vm.$validate("#kaf000-a-component4-singleDate")	
+		}
     }
     
     const API = {
