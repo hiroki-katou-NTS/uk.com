84e9998fcc0a2ba21b4bfbc8a189579990acd1cd
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2020-07-30 14:15:53 +0700

    KDPS01 - Add Screen C

---
+++ nts.uk/uk.mobile/ClientApp/views/kdp/s01/c/index.ts
@@ -0,0 +1,54 @@
+import { component, Prop } from '@app/core/component';
+import { _, Vue } from '@app/provider';
+
+const servicePath = {
+    getAllWkType: 'at/share/worktype/get_not_remove_work_type',
+    checkWorkTimeSettingNeeded: 'at/schedule/basicschedule/isWorkTimeSettingNeeded/'
+};
+
+@component({
+    name: 'kdpS01c',
+    route: '/kdp/s01/c',
+    style: require('./style.scss'),
+    template: require('./index.vue'),
+    resource: require('./resources.json'),
+    constraints: []
+})
+
+
+export class KdpS01CComponent extends Vue {
+    @Prop({ default: () => ({}) })
+    public params!: any;
+    public screenData: IScreenData = {
+        employeeCode: '000001', employeeName: '日通　太郎', date: '2018/03/10（火）',
+        time: '17：58', stampTypeText: '出勤', workName: '勤務種類の名称', localtion: '000　本社',
+         workTime: '就業時間帯の名称'
+    };
+
+
+    public created() {
+
+
+
+    }
+
+    public login() {
+
+
+    }
+
+    public mounted() {
+    }
+}
+
+interface IScreenData {
+    employeeCode: string;
+    employeeName: string;
+    date: string;
+    time: string;
+    stampTypeText: string;
+    localtion: string;
+    workName: string;
+    workTime: string;
+}
+
---
+++ nts.uk/uk.mobile/ClientApp/views/kdp/s01/c/index.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="row ml-1 title">
+      <label>{{ "KDPS01_26" | i18n }}</label>
+    </div>
+    <div class="col-12 value">
+      <label>{{ screenData.employeeCode }} {{ screenData.employeeName }}</label>
+    </div>
+
+    <div class="row ml-1 title">
+      <label>{{ "KDPS01_27" | i18n }}</label>
+    </div>
+    <div class="col-12 value">
+      <label>{{ screenData.date }}</label>
+      <label class="stamp-time font-weight-bold ml-4">{{
+        screenData.time
+      }}</label>
+    </div>
+
+    <div class="row ml-1 title">
+      <label>{{ "KDPS01_28" | i18n }}</label>
+    </div>
+    <div class="col-12 value text-center uk-text-blue">
+      <label class="stamp-text font-weight-bold">{{
+        screenData.stampTypeText
+      }}</label>
+    </div>
+
+    <div class="row ml-1 title uk-text-over-time">
+      <label>{{ "KDPS01_30" | i18n }}</label>
+    </div>
+    <div class="col-12 value">
+      <label>{{ screenData.localtion }}</label>
+    </div>
+    <div class="row ml-1 title uk-text-over-time">
+      <label>{{ "KDPS01_30" | i18n }}</label>
+    </div>
+    <div class="row ml-1 title uk-text-over-time">
+      <label>{{ "KDPS01_30" | i18n }}</label>
+    </div>
+    <div class="row ml-1 title uk-text-over-time">
+      <label>{{ "KDPS01_44" | i18n }}</label>
+    </div>
+    <div class="card-header">{{ "KDPS01_43" | i18n }}</div>
+    <div class="col-12 value">
+      <label>{{ screenData.workTypeName}}</label>
+    </div>
+    <div class="col-12 value">
+      <label>{{ screenData.workTimeName }}</label>
+    </div>
+    <div class="col-12 value">
+      <label>{{ screenData.employeeCode }} {{ screenData.employeeName }}</label>
+    </div>
+    <div class="col-12 value">
+      <label>{{ screenData.employeeCode }} {{ screenData.employeeName }}</label>
+    </div>
+    <div class="col-12 value">
+      <label>{{ screenData.employeeCode }} {{ screenData.employeeName }}</label>
+    </div>
+    <div class="col-12 value">
+      <label>{{ screenData.employeeCode }} {{ screenData.employeeName }}</label>
+    </div>
+    <div class="col-12 value">
+      <label>{{ screenData.employeeCode }} {{ screenData.employeeName }}</label>
+    </div>
+    <div class="col-12 value">
+      <label>{{ screenData.employeeCode }} {{ screenData.employeeName }}</label>
+    </div>
+    <div class="col-12 value">
+      <label>{{ "KDPS01_48" | i18n }}</label>
+    </div>
+
+    <button type="button" class="col-12 btn btn-outline-secondary">
+      {{ "KDPS01_37" | i18n }}
+    </button>
+  </div>
+</template>
