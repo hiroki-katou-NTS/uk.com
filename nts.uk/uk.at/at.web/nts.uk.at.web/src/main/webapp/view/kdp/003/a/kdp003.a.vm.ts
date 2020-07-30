2a25c00cff713ee20d178a7c352091e88620cf66
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2020-07-30 20:04:16 +0700

    update ui kdp003

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdp/003/a/kdp003.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdp/003/a/kdp003.a.vm.ts
@@ -12,7 +12,8 @@ module nts.uk.at.kdp003.a {
 		LOGIN_ADMIN: 'ctx/sys/gateway/kdp/login/adminmode',
 		LOGIN_EMPLOYEE: 'ctx/sys/gateway/kdp/login/employeemode',
 		COMPANIES: '/ctx/sys/gateway/kdp/login/getLogginSetting',
-		FINGER_STAMP_SETTING: 'at/record/stamp/finger/get-finger-stamp-setting'
+		FINGER_STAMP_SETTING: 'at/record/stamp/finger/get-finger-stamp-setting',
+		CONFIRM_STAMP_INPUT: '/at/record/stamp/employment/system/confirm-use-of-stamp-input'
 	};
 
 	const DIALOG = {
@@ -25,6 +26,8 @@ module nts.uk.at.kdp003.a {
 	};
 
 	const KDP003_SAVE_DATA = 'KDP003_DATA';
+
+	type COMPANIES = f.CompanyItem[];
 
 	@bean()
 	export class ViewModel extends ko.ViewModel {
@@ -47,7 +50,7 @@ module nts.uk.at.kdp003.a {
 		employeeData: EmployeeListParam = {
 			employees: ko.observableArray([]),
 			selectedId: ko.observable(undefined),
-			employeeAuthcUseArt: ko.observable(true)
+			employeeAuthcUseArt: ko.observable(false)
 		};
 
 		// data option for tabs button A5
@@ -69,149 +72,182 @@ module nts.uk.at.kdp003.a {
 		mounted() {
 			const vm = this;
 
-			vm.$ajax('at', API.COMPANIES)
-				.then((data: f.CompanyItem[]) => {
-					// UI[F2]  打刻使用可能会社の取得と判断 
-					if (data.every(e => e.fingerAuthStamp === false)) {
-						vm.showClockButton.setting(false);
-					} else {
-						vm.showClockButton.setting(true);
+			vm.$window.storage(KDP003_SAVE_DATA)
+				.then((data: undefined | StorageData) => {
+					if (data === undefined) {
+						return vm.$window.modal('at', DIALOG.F, { mode: 'admin' }) as JQueryDeferred<f.TimeStampLoginData>;
 					}
+					// data login by storage
+					const {
+						CCD,
+						CID,
+						PWD,
+						SCD,
+						SID
+					} = data as StorageData;
 
-					return vm.$window.storage(KDP003_SAVE_DATA);
+					const loginData: f.ModelData = {
+						contractCode: '000000000000',
+						companyCode: CCD,
+						companyId: CID,
+						employeeCode: SCD,
+						employeeId: SID,
+						password: PWD,
+						passwordInvalid: false,
+						isAdminMode: true,
+						runtimeEnvironmentCreate: true
+					};
+
+					// auto login by storage data of preview login
+					// <<ScreenQuery>> 打刻管理者でログインする
+					return vm.$ajax('at', API.COMPANIES)
+						.then((data: f.CompanyItem[]) => {
+							if (_.every(data, d => d.selectUseOfName === false)) {
+								// note: ログイン失敗(打刻会社一覧が取得できない場合)
+								vm.message({ messageId: 'Msg_1527' });
+
+								// UI[F2]  打刻使用可能会社の取得と判断 
+								vm.showClockButton.setting(false);
+
+								return null;
+							}
+
+							vm.showClockButton.setting(true);
+
+							return vm.$ajax('at', API.LOGIN_ADMIN, loginData);
+						}) as JQueryDeferred<undefined | f.TimeStampLoginData>;
 				})
-				// get storage save preview login data
-				.then((data: StorageData) => {
-					if (!data) {
-						// <<ScreenQuery>> 打刻管理者でログインする
-						const showLoginDialog = () => vm.$window
-							.modal('at', DIALOG.F, { mode: 'admin' });
+				.then((data: null | undefined | f.TimeStampLoginData) => {
+					if (data === null) {
+						return false;
+					} else if (data === undefined) {
+						// note: ログイン失敗(キャンセル)
+						vm.message({ messageId: 'Msg_1647' });
 
-						// if not has storage (first login)
-						// open f dialog and login
-						return vm.$blockui('show').then(showLoginDialog);
+						return false;
 					} else {
-						// if data login exist (next login)
-						// return exist data get from storage
-						return data;
-					}
-				})
-				.then((data: f.TimeStampLoginData | StorageData) => {
-					if (_.has(data, 'em')) {
-						// data login by f dialog
-						const loginData = data as f.TimeStampLoginData;
+						// note: 利用不可モードで表示
+						if (!data.msgErrorId && !data.errorMessage) {
+							const { em } = data;
 
-						if (!loginData.msgErrorId && !loginData.errorMessage) {
-							// storage successful login data
-							const storageData: StorageData = {
-								CID: loginData.em.companyId,
-								CCD: loginData.em.companyCode,
-								PWD: loginData.em.password,
-								SCD: loginData.em.employeeCode,
-								SID: loginData.em.employeeId,
-								WKLOC_CD: '',
-								WKPID: []
-							};
+							return vm.$window
+								.storage(KDP003_SAVE_DATA)
+								.then((storage: undefined | StorageData) => {
+									// storage successful login data
+									const storageData: StorageData = {
+										CID: em.companyId,
+										CCD: em.companyCode,
+										PWD: em.password,
+										SCD: em.employeeCode,
+										SID: em.employeeId,
+										WKLOC_CD: storage.WKLOC_CD,
+										WKPID: storage.WKPID
+									};
 
-							vm.$window.storage(KDP003_SAVE_DATA, storageData);
+									return vm.$window.storage(KDP003_SAVE_DATA, storageData);
+								})
+								.then(() => true);
 						} else {
-							if (!loginData.msgErrorId) {
-								vm.message(loginData.errorMessage);
+							// note: ログイン失敗(打刻会社一覧が取得できない場合)
+							if (!data.msgErrorId) {
+								vm.message(data.errorMessage);
 							} else {
-								vm.message({ messageId: loginData.msgErrorId });
+								vm.message({ messageId: data.msgErrorId });
 							}
+
+							return false;
 						}
-
-						return loginData;
-					} else {
-						// data login by storage
-						const {
-							CCD,
-							CID,
-							PWD,
-							SCD,
-							SID
-						} = data as StorageData;
-
-						const loginData: f.ModelData = {
-							contractCode: '000000000000',
-							companyCode: CCD,
-							companyId: CID,
-							employeeCode: SCD,
-							employeeId: SID,
-							password: PWD,
-							passwordInvalid: false,
-							isAdminMode: true,
-							runtimeEnvironmentCreate: true
-						};
-
-						// auto login by storage data of preview login
-						// <<ScreenQuery>> 打刻管理者でログインする
-						return vm.$ajax('at', API.LOGIN_ADMIN, loginData).fail(() => false);
 					}
 				})
-				// check storage data
-				.then((state: false | f.TimeStampLoginData) => {
-					if (state === false || state.msgErrorId || state.errorMessage) {
-						if (state !== false) {
-							if (state.msgErrorId) {
-								vm.message({
-									messageId: state.msgErrorId
-								});
-							} else {
-								vm.message(state.errorMessage);
-							}
-						}
-
-						return false;
-					} else {
-						return vm.$window.storage(KDP003_SAVE_DATA)
-							.then((data: StorageData) => {
-								if (data && data.WKPID.length) {
-									return { selectedId: data.WKPID };
-								}
-
-								// if not exist workplaceID
-								return vm.$window.modal('at', DIALOG.K);
-							});
-					}
-				})
-				.then((data: false | k.Return) => {
+				.then((data: boolean) => {
 					if (data === false) {
-						return false;
-					} else {
-						return vm.$window.storage(KDP003_SAVE_DATA)
-							// update workplaceId to storage
-							.then((storage: StorageData) => {
-								if (storage) {
-									if (_.isArray(data.selectedId)) {
-										storage.WKPID = data.selectedId;
-									} else {
-										storage.WKPID = [data.selectedId];
-									}
-								}
-
-								// return data from storage
-								return vm.$window.storage(KDP003_SAVE_DATA, storage);
-							});
+						return null;
 					}
+
+					return vm.$window
+						.storage(KDP003_SAVE_DATA)
+						.then((data: StorageData) => {
+							if (data.WKPID.length) {
+								return { selectedId: data.WKPID };
+							}
+
+							// if not exist workplaceID
+							return vm.$window.modal('at', DIALOG.K);
+						}) as JQueryDeferred<k.Return>;
+				})
+				.then((data: null | k.Return) => {
+					if (data === null) {
+						if (ko.unwrap(vm.message) === undefined) {
+							vm.message({ messageId: 'Msg_1647' });
+						}
+
+						return false;
+					}
+
+					return vm.$window
+						.storage(KDP003_SAVE_DATA)
+						// update workplaceId to storage
+						.then((storage: StorageData) => {
+							if (storage) {
+								if (_.isArray(data.selectedId)) {
+									storage.WKPID = data.selectedId;
+								} else {
+									storage.WKPID = [data.selectedId];
+								}
+							}
+
+							// return data from storage
+							return vm.$window.storage(KDP003_SAVE_DATA, storage);
+						});
 				})
 				.then((data: false | StorageData) => {
-					// if login and storage data success
-					if (data) {
-						return vm.loadData(data);
+					if (data !== false) {
+						const params: f.CommanStampInput = {
+							companyId: data.CID,
+							employeeCode: data.SCD,
+							employeeId: data.SID,
+							stampMeans: StampMeans.NAME_SELECTION
+						};
+
+						return vm.$ajax('at', API.CONFIRM_STAMP_INPUT, params)
+							.then((data: f.ConfirmStampInput) => {
+								if (data.used === CanEngravingUsed.ENGTAVING_FUNCTION_CANNOT_USED) {
+									vm.message({
+										messageId: 'Msg_1645',
+										messageParams: [vm.$i18n('KDP002_2')]
+									});
+
+									return false;
+								}
+
+								return data;
+							}) as JQueryDeferred<false | StorageData>;
 					}
+
+					return false;
+				})
+				.then((data: false | StorageData) => {
+					if (data === false) {
+						return false;
+					}
+
+					// if message is not set, clear it (show ui)
+					if (ko.unwrap(vm.message) === undefined) {
+						vm.message(null);
+					}
+					
+					// login success and datas are valid
+					return vm.loadData(data);
 				})
 				// show message from login data (return by f dialog)
 				.fail((message: { messageId: string }) => {
 					vm.message(message);
 					vm.$dialog.error(message);
-				})
-				.always(() => vm.$blockui('clear'));
+				});
 		}
 
 		// ※画面起動「※起動1」より再度実行(UI処理[A5])
-		// <<ScreenQuery>> 打刻入力(氏名選択)の設定を取得する 
+		// <<ScreenQuery>> 打刻入力(氏名選択)の設定を取得する
 		loadData(data: StorageData) {
 			const vm = this;
 			const clearState = () => {
@@ -262,37 +298,40 @@ module nts.uk.at.kdp003.a {
 
 			vm.$window.storage(KDP003_SAVE_DATA)
 				.then((data: StorageData) => {
-					return vm.$window
-						.modal('at', DIALOG.F, {
-							mode: 'admin',
-							companyId: (data || {}).CID
-						});
+					const mode = 'admin';
+					const companyId = (data || {}).CID;
+
+					return vm.$window.modal('at', DIALOG.F, { mode, companyId });
 				})
 				.then((data: f.TimeStampLoginData) => {
 					if (data) {
-						// update or save login data to storage
-						vm.$window.storage(KDP003_SAVE_DATA)
-							.then((storage: StorageData) => {
-								if (storage) {
-									storage.CCD = data.em.companyCode;
-									storage.CID = data.em.companyId;
-									storage.PWD = data.em.password;
-									storage.SCD = data.em.employeeCode;
-									storage.SID = data.em.employeeId;
-								} else {
-									storage = {
-										CCD: data.em.companyCode,
-										CID: data.em.companyId,
-										PWD: data.em.password,
-										SCD: data.em.employeeCode,
-										SID: data.em.employeeId,
-										WKLOC_CD: '',
-										WKPID: []
-									};
-								}
+						const { em } = data;
 
-								vm.$window.storage(KDP003_SAVE_DATA, storage);
-							});
+						if (em) {
+							// update or save login data to storage
+							vm.$window.storage(KDP003_SAVE_DATA)
+								.then((storage: StorageData) => {
+									if (storage) {
+										storage.CCD = em.companyCode;
+										storage.CID = em.companyId;
+										storage.PWD = em.password;
+										storage.SCD = em.employeeCode;
+										storage.SID = em.employeeId;
+									} else {
+										storage = {
+											CCD: em.companyCode,
+											CID: em.companyId,
+											PWD: em.password,
+											SCD: em.employeeCode,
+											SID: em.employeeId,
+											WKLOC_CD: '',
+											WKPID: []
+										};
+									}
+
+									vm.$window.storage(KDP003_SAVE_DATA, storage);
+								});
+						}
 
 						return data;
 					}
@@ -302,7 +341,6 @@ module nts.uk.at.kdp003.a {
 				// check storage data
 				.then((state: false | f.TimeStampLoginData) => {
 					if (state === false || !!state.msgErrorId || !!state.errorMessage) {
-						debugger;
 						if (state !== false) {
 							if (state.msgErrorId) {
 								vm.message({
@@ -311,7 +349,12 @@ module nts.uk.at.kdp003.a {
 							} else {
 								vm.message(state.errorMessage);
 							}
+						} else {
+							vm.message({
+								messageId: 'Msg_1647'
+							});
 						}
+
 						return false;
 					} else {
 						return vm.$window.modal('at', DIALOG.K);
@@ -445,11 +488,13 @@ module nts.uk.at.kdp003.a {
 									const { resultDisplayTime } = stampSetting;
 
 									return storage('resultDisplayTime', resultDisplayTime)
-										.then(() => storage('infoEmpToScreenB', { employeeId }))
+										.then(() => storage('infoEmpToScreenB', employeeInfo))
 										.then(() => modal('at', DIALOG.KDP002_B)) as JQueryDeferred<any>;
 								}
 							}
 						}
+
+						return data;
 					}
 
 					return null;
@@ -515,5 +560,30 @@ module nts.uk.at.kdp003.a {
 		passwordRequiredArt: boolean;
 		resultDisplayTime: number;
 		textColor: string;
+	}
+
+
+
+	enum StampMeans {
+		NAME_SELECTION = 0, // 0:氏名選択
+		FINGER_AUTHC = 1,   // 1:指認証打刻
+		IC_CARD = 2,        // 2:ICカード打刻
+		INDIVITION = 3,     //  3:個人打刻
+		PORTAL = 4,         // 4:ポータル打刻
+		SMART_PHONE = 5,    // 5:スマホ打刻
+		TIME_CLOCK = 6,     // 6:タイムレコーダー打刻
+		TEXT = 7,           // 7:テキスト受入
+		RICOH_COPIER = 8    // 8:リコー複写機打刻
+	}
+
+	enum CanEngravingUsed {
+		// 0 利用できる
+		AVAILABLE = 0,
+		// 1 打刻オプション未購入
+		NOT_PURCHASED_STAMPING_OPTION = 1,
+		// 2 打刻機能利用不可
+		ENGTAVING_FUNCTION_CANNOT_USED = 2,
+		// 3 打刻カード未登録
+		UNREGISTERED_STAMP_CARD = 3
 	}
 }
