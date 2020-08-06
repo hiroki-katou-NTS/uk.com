0c3e58a52825e84cb0fc37c79723d9bcffbb4919
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2020-08-06 15:07:12 +0700

    Create method toEntity

--- nts.uk/uk.hr/hr.ctx/hr.shared/nts.uk.ctx.hr.shared.infra/src/main/java/nts/uk/ctx/hr/shared/infra/entity/personalinformation/PpedtData.java
+++ nts.uk/uk.hr/hr.ctx/hr.shared/nts.uk.ctx.hr.shared.infra/src/main/java/nts/uk/ctx/hr/shared/infra/entity/personalinformation/PpedtData.java
@@ -87,11 +87,11 @@ public class PpedtData extends UkJpaEntity implements Serializable {
 	
 	@Column(name = "RPT_LAYOUT_ID")
 	@Basic(optional = true)
-	public int rptLa;
+	public int rptLayoutId;
 	
 	@Column(name = "RPTID")
 	@Basic(optional = true)
-	public int rptId;
+	public BigInteger rptId;
 	
 	@Column(name = "RPT_NUMBER")
 	@Basic(optional = true)
@@ -857,128 +857,225 @@ public class PpedtData extends UkJpaEntity implements Serializable {
 	@Basic(optional = true)
 	public String selectName20;
 	
-	@Column(name = "SELECT_ID_21")
-	@Basic(optional = true)
-	public BigInteger selectId21;
-	
-	@Column(name = "SELECT_CODE_21")
-	@Basic(optional = true)
-	public String selectCode21;
-	
-	@Column(name = "SELECT_NAME_21")
-	@Basic(optional = true)
-	public String selectName21;
-	
-	@Column(name = "SELECT_ID_22")
-	@Basic(optional = true)
-	public BigInteger selectId22;
-	
-	@Column(name = "SELECT_CODE_22")
-	@Basic(optional = true)
-	public String selectCode22;
-	
-	@Column(name = "SELECT_NAME_22")
-	@Basic(optional = true)
-	public String selectName22;
-	
-	@Column(name = "SELECT_ID_23")
-	@Basic(optional = true)
-	public BigInteger selectId23;
-	
-	@Column(name = "SELECT_CODE_23")
-	@Basic(optional = true)
-	public String selectCode23;
-	
-	@Column(name = "SELECT_NAME_23")
-	@Basic(optional = true)
-	public String selectName23;
-	
-	@Column(name = "SELECT_ID_24")
-	@Basic(optional = true)
-	public BigInteger selectId24;
-	
-	@Column(name = "SELECT_CODE_24")
-	@Basic(optional = true)
-	public String selectCode24;
-	
-	@Column(name = "SELECT_NAME_24")
-	@Basic(optional = true)
-	public String selectName24;
-	
-	@Column(name = "SELECT_ID_25")
-	@Basic(optional = true)
-	public BigInteger selectId25;
-	
-	@Column(name = "SELECT_CODE_25")
-	@Basic(optional = true)
-	public String selectCode25;
-	
-	@Column(name = "SELECT_NAME_25")
-	@Basic(optional = true)
-	public String selectName25;
-	
-	@Column(name = "SELECT_ID_26")
-	@Basic(optional = true)
-	public BigInteger selectId26;
-	
-	@Column(name = "SELECT_CODE_26")
-	@Basic(optional = true)
-	public String selectCode26;
-	
-	@Column(name = "SELECT_NAME_26")
-	@Basic(optional = true)
-	public String selectName26;
-	
-	@Column(name = "SELECT_ID_27")
-	@Basic(optional = true)
-	public BigInteger selectId27;
-	
-	@Column(name = "SELECT_CODE_27")
-	@Basic(optional = true)
-	public String selectCode27;
-	
-	@Column(name = "SELECT_NAME_27")
-	@Basic(optional = true)
-	public String selectName27;
-	
-	@Column(name = "SELECT_ID_28")
-	@Basic(optional = true)
-	public BigInteger selectId28;
-	
-	@Column(name = "SELECT_CODE_28")
-	@Basic(optional = true)
-	public String selectCode28;
-	
-	@Column(name = "SELECT_NAME_28")
-	@Basic(optional = true)
-	public String selectName28;
-	
-	@Column(name = "SELECT_ID_29")
-	@Basic(optional = true)
-	public BigInteger selectId29;
-	
-	@Column(name = "SELECT_CODE_29")
-	@Basic(optional = true)
-	public String selectCode29;
-	
-	@Column(name = "SELECT_NAME_29")
-	@Basic(optional = true)
-	public String selectName29;
-	
-	@Column(name = "SELECT_ID_30")
-	@Basic(optional = true)
-	public BigInteger selectId30;
-	
-	@Column(name = "SELECT_CODE_30")
-	@Basic(optional = true)
-	public String selectCode30;
-	
-	@Column(name = "SELECT_NAME_30")
-	@Basic(optional = true)
-	public String selectName30;
-	
 	@Override
 	protected Object getKey() {
 		return hisId;
+	}
+	
+	public PpedtData toEntity(PersonalInformation domain) {
+		PpedtData entity = new PpedtData();
+		
+		entity.hisId = domain.getHistId();
+		entity.contractCd = domain.getContractCd();
+		entity.cId = domain.getCid().map(m -> m).orElse(null);
+		entity.ccd = domain.getCompanyCode().map(m -> m).orElse(null);
+		entity.pId = domain.getPId();
+		entity.sid = domain.getScd().map(m -> m).orElse(null);
+		entity.scd = domain.getScd().map(m -> m).orElse(null);
+		entity.personName = domain.getPersonName().map(m -> m).orElse(null);
+		entity.workId = domain.getWorkId();
+		entity.workName = domain.getWorkName().map(m -> m).orElse(null);
+		entity.startDate = domain.getStartDate();
+		entity.endDate = domain.getEndDate();
+		entity.releaseDate = domain.getReleaseDate().map(m -> m).orElse(null);
+		entity.requestFlg = domain.getRequestFlg();
+		entity.rptLayoutId = domain.getRptLayoutId();
+		entity.rptId = BigInteger.valueOf(domain.getRptId());
+		entity.rptNumber = domain.getRptNumber().map(m -> m).orElse(null);
+		
+		entity.str01 = domain.getStr01().map(m -> m).orElse(null);
+		entity.str02 = domain.getStr02().map(m -> m).orElse(null);
+		entity.str03 = domain.getStr03().map(m -> m).orElse(null);
+		entity.str04 = domain.getStr04().map(m -> m).orElse(null);
+		entity.str05 = domain.getStr05().map(m -> m).orElse(null);
+		entity.str06 = domain.getStr06().map(m -> m).orElse(null);
+		entity.str07 = domain.getStr07().map(m -> m).orElse(null);
+		entity.str08 = domain.getStr08().map(m -> m).orElse(null);
+		entity.str09 = domain.getStr09().map(m -> m).orElse(null);
+		entity.str10 = domain.getStr10().map(m -> m).orElse(null);
+		entity.str11 = domain.getStr11().map(m -> m).orElse(null);
+		entity.str12 = domain.getStr12().map(m -> m).orElse(null);
+		entity.str13 = domain.getStr13().map(m -> m).orElse(null);
+		entity.str14 = domain.getStr14().map(m -> m).orElse(null);
+		entity.str15 = domain.getStr15().map(m -> m).orElse(null);
+		entity.str16 = domain.getStr16().map(m -> m).orElse(null);
+		entity.str17 = domain.getStr17().map(m -> m).orElse(null);
+		entity.str18 = domain.getStr18().map(m -> m).orElse(null);
+		entity.str19 = domain.getStr19().map(m -> m).orElse(null);
+		entity.str20 = domain.getStr20().map(m -> m).orElse(null);
+		entity.str21 = domain.getStr21().map(m -> m).orElse(null);
+		entity.str22 = domain.getStr22().map(m -> m).orElse(null);
+		entity.str23 = domain.getStr23().map(m -> m).orElse(null);
+		entity.str24 = domain.getStr24().map(m -> m).orElse(null);
+		entity.str25 = domain.getStr25().map(m -> m).orElse(null);
+		entity.str26 = domain.getStr26().map(m -> m).orElse(null);
+		entity.str27 = domain.getStr27().map(m -> m).orElse(null);
+		entity.str28 = domain.getStr28().map(m -> m).orElse(null);
+		entity.str29 = domain.getStr29().map(m -> m).orElse(null);
+		entity.str30 = domain.getStr30().map(m -> m).orElse(null);
+		entity.str31 = domain.getStr31().map(m -> m).orElse(null);
+		entity.str32 = domain.getStr32().map(m -> m).orElse(null);
+		entity.str33 = domain.getStr33().map(m -> m).orElse(null);
+		entity.str34 = domain.getStr34().map(m -> m).orElse(null);
+		entity.str35 = domain.getStr35().map(m -> m).orElse(null);
+		entity.str36 = domain.getStr36().map(m -> m).orElse(null);
+		entity.str37 = domain.getStr37().map(m -> m).orElse(null);
+		entity.str38 = domain.getStr38().map(m -> m).orElse(null);
+		entity.str39 = domain.getStr39().map(m -> m).orElse(null);
+		entity.str40 = domain.getStr40().map(m -> m).orElse(null);
+		entity.str41 = domain.getStr41().map(m -> m).orElse(null);
+		entity.str42 = domain.getStr42().map(m -> m).orElse(null);
+		entity.str43 = domain.getStr43().map(m -> m).orElse(null);
+		entity.str44 = domain.getStr44().map(m -> m).orElse(null);
+		entity.str45 = domain.getStr45().map(m -> m).orElse(null);
+		entity.str46 = domain.getStr46().map(m -> m).orElse(null);
+		entity.str47 = domain.getStr47().map(m -> m).orElse(null);
+		entity.str48 = domain.getStr48().map(m -> m).orElse(null);
+		entity.str49 = domain.getStr49().map(m -> m).orElse(null);
+		entity.str50 = domain.getStr50().map(m -> m).orElse(null);
+		entity.str51 = domain.getStr51().map(m -> m).orElse(null);
+		entity.str52 = domain.getStr52().map(m -> m).orElse(null);
+		entity.str53 = domain.getStr53().map(m -> m).orElse(null);
+		entity.str54 = domain.getStr54().map(m -> m).orElse(null);
+		entity.str55 = domain.getStr55().map(m -> m).orElse(null);
+		entity.str56 = domain.getStr56().map(m -> m).orElse(null);
+		entity.str57 = domain.getStr57().map(m -> m).orElse(null);
+		entity.str58 = domain.getStr58().map(m -> m).orElse(null);
+		entity.str59 = domain.getStr59().map(m -> m).orElse(null);
+		entity.str60 = domain.getStr60().map(m -> m).orElse(null);
+		
+		entity.date01 = domain.getDate01().map(m -> m).orElse(null);
+		entity.date02 = domain.getDate02().map(m -> m).orElse(null);
+		entity.date03 = domain.getDate03().map(m -> m).orElse(null);
+		entity.date04 = domain.getDate04().map(m -> m).orElse(null);
+		entity.date05 = domain.getDate05().map(m -> m).orElse(null);
+		entity.date06 = domain.getDate06().map(m -> m).orElse(null);
+		entity.date07 = domain.getDate07().map(m -> m).orElse(null);
+		entity.date08 = domain.getDate08().map(m -> m).orElse(null);
+		entity.date09 = domain.getDate09().map(m -> m).orElse(null);
+		entity.date10 = domain.getDate10().map(m -> m).orElse(null);
+		
+		entity.int01 = domain.getInt01();
+		entity.int02 = domain.getInt02();
+		entity.int03 = domain.getInt03();
+		entity.int04 = domain.getInt04();
+		entity.int05 = domain.getInt05();
+		entity.int06 = domain.getInt06();
+		entity.int07 = domain.getInt07();
+		entity.int08 = domain.getInt08();
+		entity.int09 = domain.getInt09();
+		entity.int10 = domain.getInt10();
+		entity.int11 = domain.getInt11();
+		entity.int12 = domain.getInt12();
+		entity.int13 = domain.getInt13();
+		entity.int14 = domain.getInt14();
+		entity.int15 = domain.getInt15();
+		entity.int16 = domain.getInt16();
+		entity.int17 = domain.getInt17();
+		entity.int18 = domain.getInt18();
+		entity.int19 = domain.getInt19();
+		entity.int20 = domain.getInt20();
+		entity.int21 = domain.getInt21();
+		entity.int22 = domain.getInt22();
+		entity.int23 = domain.getInt23();
+		entity.int24 = domain.getInt24();
+		entity.int25 = domain.getInt25();
+		entity.int26 = domain.getInt26();
+		entity.int27 = domain.getInt27();
+		entity.int28 = domain.getInt28();
+		entity.int29 = domain.getInt29();
+		entity.int30 = domain.getInt30();
+		
+		entity.number01 = BigDecimal.valueOf(domain.getNumber01());
+		entity.number02 = BigDecimal.valueOf(domain.getNumber02());
+		entity.number03 = BigDecimal.valueOf(domain.getNumber03());
+		entity.number04 = BigDecimal.valueOf(domain.getNumber04());
+		entity.number05 = BigDecimal.valueOf(domain.getNumber05());
+		entity.number06 = BigDecimal.valueOf(domain.getNumber06());
+		entity.number07 = BigDecimal.valueOf(domain.getNumber07());
+		entity.number08 = BigDecimal.valueOf(domain.getNumber08());
+		entity.number09 = BigDecimal.valueOf(domain.getNumber09());
+		entity.number10 = BigDecimal.valueOf(domain.getNumber10());
+		entity.number11 = BigDecimal.valueOf(domain.getNumber11());
+		entity.number12 = BigDecimal.valueOf(domain.getNumber12());
+		entity.number13 = BigDecimal.valueOf(domain.getNumber13());
+		entity.number14 = BigDecimal.valueOf(domain.getNumber14());
+		entity.number15 = BigDecimal.valueOf(domain.getNumber15());
+		entity.number16 = BigDecimal.valueOf(domain.getNumber16());
+		entity.number17 = BigDecimal.valueOf(domain.getNumber17());
+		entity.number18 = BigDecimal.valueOf(domain.getNumber18());
+		entity.number19 = BigDecimal.valueOf(domain.getNumber19());
+		entity.number20 = BigDecimal.valueOf(domain.getNumber20());
+		entity.number21 = BigDecimal.valueOf(domain.getNumber21());
+		entity.number22 = BigDecimal.valueOf(domain.getNumber22());
+		entity.number23 = BigDecimal.valueOf(domain.getNumber23());
+		entity.number24 = BigDecimal.valueOf(domain.getNumber24());
+		entity.number25 = BigDecimal.valueOf(domain.getNumber25());
+		entity.number26 = BigDecimal.valueOf(domain.getNumber26());
+		entity.number27 = BigDecimal.valueOf(domain.getNumber27());
+		entity.number28 = BigDecimal.valueOf(domain.getNumber28());
+		entity.number29 = BigDecimal.valueOf(domain.getNumber29());
+		entity.number30 = BigDecimal.valueOf(domain.getNumber30());
+		
+		entity.selectId01 = BigInteger.valueOf(domain.getSelectId01());
+		entity.selectCode01 = domain.getSelectCode01().map(m -> m).orElse(null);
+		entity.selectName01 = domain.getSelectName01().map(m -> m).orElse(null);
+		entity.selectId02 = BigInteger.valueOf(domain.getSelectId02());
+		entity.selectCode02 = domain.getSelectCode02().map(m -> m).orElse(null);
+		entity.selectName02 = domain.getSelectName02().map(m -> m).orElse(null);
+		entity.selectId03 = BigInteger.valueOf(domain.getSelectId03());
+		entity.selectCode03 = domain.getSelectCode03().map(m -> m).orElse(null);
+		entity.selectName03 = domain.getSelectName03().map(m -> m).orElse(null);
+		entity.selectId04 = BigInteger.valueOf(domain.getSelectId04());
+		entity.selectCode04 = domain.getSelectCode04().map(m -> m).orElse(null);
+		entity.selectName04 = domain.getSelectName04().map(m -> m).orElse(null);
+		entity.selectId05 = BigInteger.valueOf(domain.getSelectId05());
+		entity.selectCode05 = domain.getSelectCode05().map(m -> m).orElse(null);
+		entity.selectName05 = domain.getSelectName05().map(m -> m).orElse(null);
+		entity.selectId06 = BigInteger.valueOf(domain.getSelectId06());
+		entity.selectCode06 = domain.getSelectCode06().map(m -> m).orElse(null);
+		entity.selectName06 = domain.getSelectName06().map(m -> m).orElse(null);
+		entity.selectId07 = BigInteger.valueOf(domain.getSelectId07());
+		entity.selectCode07 = domain.getSelectCode07().map(m -> m).orElse(null);
+		entity.selectName07 = domain.getSelectName07().map(m -> m).orElse(null);
+		entity.selectId08 = BigInteger.valueOf(domain.getSelectId08());
+		entity.selectCode08 = domain.getSelectCode08().map(m -> m).orElse(null);
+		entity.selectName08 = domain.getSelectName08().map(m -> m).orElse(null);
+		entity.selectId09 = BigInteger.valueOf(domain.getSelectId09());
+		entity.selectCode09 = domain.getSelectCode09().map(m -> m).orElse(null);
+		entity.selectName09 = domain.getSelectName09().map(m -> m).orElse(null);
+		entity.selectId10 = BigInteger.valueOf(domain.getSelectId10());
+		entity.selectCode10 = domain.getSelectCode10().map(m -> m).orElse(null);
+		entity.selectName10 = domain.getSelectName10().map(m -> m).orElse(null);
+		entity.selectId11 = BigInteger.valueOf(domain.getSelectId11());
+		entity.selectCode11 = domain.getSelectCode01().map(m -> m).orElse(null);
+		entity.selectName11 = domain.getSelectName01().map(m -> m).orElse(null);
+		entity.selectId12 = BigInteger.valueOf(domain.getSelectId12());
+		entity.selectCode12 = domain.getSelectCode02().map(m -> m).orElse(null);
+		entity.selectName12 = domain.getSelectName02().map(m -> m).orElse(null);
+		entity.selectId13 = BigInteger.valueOf(domain.getSelectId13());
+		entity.selectCode13 = domain.getSelectCode03().map(m -> m).orElse(null);
+		entity.selectName13 = domain.getSelectName03().map(m -> m).orElse(null);
+		entity.selectId14 = BigInteger.valueOf(domain.getSelectId14());
+		entity.selectCode14 = domain.getSelectCode04().map(m -> m).orElse(null);
+		entity.selectName14 = domain.getSelectName04().map(m -> m).orElse(null);
+		entity.selectId15 = BigInteger.valueOf(domain.getSelectId15());
+		entity.selectCode15 = domain.getSelectCode05().map(m -> m).orElse(null);
+		entity.selectName15 = domain.getSelectName05().map(m -> m).orElse(null);
+		entity.selectId16 = BigInteger.valueOf(domain.getSelectId16());
+		entity.selectCode16 = domain.getSelectCode06().map(m -> m).orElse(null);
+		entity.selectName16 = domain.getSelectName06().map(m -> m).orElse(null);
+		entity.selectId17 = BigInteger.valueOf(domain.getSelectId17());
+		entity.selectCode17 = domain.getSelectCode07().map(m -> m).orElse(null);
+		entity.selectName17 = domain.getSelectName07().map(m -> m).orElse(null);
+		entity.selectId18 = BigInteger.valueOf(domain.getSelectId18());
+		entity.selectCode18 = domain.getSelectCode08().map(m -> m).orElse(null);
+		entity.selectName18 = domain.getSelectName08().map(m -> m).orElse(null);
+		entity.selectId19 = BigInteger.valueOf(domain.getSelectId19());
+		entity.selectCode19 = domain.getSelectCode09().map(m -> m).orElse(null);
+		entity.selectName19 = domain.getSelectName09().map(m -> m).orElse(null);
+		entity.selectId20 = BigInteger.valueOf(domain.getSelectId20());
+		
+		return entity;
 	}
 }
