cb47dc7e48f26ecf7ff7c2f635be5b5ea1d2ad5a
Dat.nk <datnkph@gmail.com>
2020-08-13 17:44:12 +0700

    Fix Bug: #28483, #28677, #28686
    (cherry picked from commit 9c3d9a032ec6303e3c58d0bb405f264e9fabc40b)

--- nts.uk/uk.at/at.screen/nts.uk.screen.at.infra/src/main/java/nts/uk/screen/at/infra/shift/sixmonthscalendar/SixMonthsCalendarRepositoryImpl.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.infra/src/main/java/nts/uk/screen/at/infra/shift/sixmonthscalendar/SixMonthsCalendarRepositoryImpl.java
@@ -44,6 +44,7 @@ public class SixMonthsCalendarRepositoryImpl extends JpaRepository implements Si
                 "(c.ksmmtCalendarClassPK.companyId, c.ksmmtCalendarClassPK.classId, c.ksmmtCalendarClassPK.date , c.workingDayAtr) ");
         stringBuilder.append("FROM KsmmtCalendarClass c ");
         stringBuilder.append("WHERE c.ksmmtCalendarClassPK.classId = :classId ");
+        stringBuilder.append("AND c.ksmmtCalendarClassPK.companyId = :companyId ");
         stringBuilder.append("AND c.ksmmtCalendarClassPK.date >= :startDate AND c.ksmmtCalendarClassPK.date <= :endDate ");
         stringBuilder.append("AND c.workingDayAtr != 0 ");
         SELECT_SIXMONTHS_CLASSIFICATION = stringBuilder.toString();
@@ -68,8 +69,9 @@ public class SixMonthsCalendarRepositoryImpl extends JpaRepository implements Si
     }
 
     @Override
-    public List<SixMonthsCalendarClassScreenDto> getSixMonthsCalendarClassByYearMonth(String classId, DatePeriod yearMonth) {
+    public List<SixMonthsCalendarClassScreenDto> getSixMonthsCalendarClassByYearMonth(String conpanyId, String classId, DatePeriod yearMonth) {
         return this.queryProxy().query(SELECT_SIXMONTHS_CLASSIFICATION, SixMonthsCalendarClassScreenDto.class)
+                .setParameter("companyId", conpanyId)
                 .setParameter("classId", classId)
                 .setParameter("startDate", yearMonth.start())
                 .setParameter("endDate", yearMonth.end())
