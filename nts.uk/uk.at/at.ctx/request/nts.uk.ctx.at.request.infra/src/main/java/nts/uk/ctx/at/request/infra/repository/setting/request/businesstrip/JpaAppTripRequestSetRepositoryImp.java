f73d02ddc440fac601f292ae40c220271cb35d95
haitt3si <hai.tt@3si.vn>
2020-08-14 13:19:27 +0700

    Domain 出張申請設定

---
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.infra/src/main/java/nts/uk/ctx/at/request/infra/repository/setting/request/businesstrip/JpaAppTripRequestSetRepositoryImp.java
@@ -0,0 +1,37 @@
+package nts.uk.ctx.at.request.infra.repository.setting.request.businesstrip;
+
+import nts.arc.layer.infra.data.JpaRepository;
+import nts.uk.ctx.at.request.dom.setting.request.application.businesstrip.AppTripRequestSet;
+import nts.uk.ctx.at.request.dom.setting.request.application.businesstrip.AppTripRequestSetRepository;
+import nts.uk.ctx.at.request.infra.entity.setting.request.businesstrip.KrqstAppTripRequestSet;
+
+import java.util.Optional;
+
+public class JpaAppTripRequestSetRepositoryImp extends JpaRepository implements AppTripRequestSetRepository {
+
+    private static final String SELECT_ALL = "SELECT * FROM KrqstAppTripRequestSet";
+    private static final String SELECT_BY_ID = SELECT_ALL + " WHERE companyId = :cid";
+
+    @Override
+    public Optional<AppTripRequestSet> findById(String cid) {
+        Optional<AppTripRequestSet> appTripRequestSet = this.queryProxy().query(SELECT_BY_ID, KrqstAppTripRequestSet.class)
+                .setParameter("cid", cid).getSingle(i -> i.toDomain());
+        return appTripRequestSet;
+    }
+
+    @Override
+    public void add(AppTripRequestSet domain) {
+
+    }
+
+    @Override
+    public void update(AppTripRequestSet domain) {
+
+    }
+
+    @Override
+    public void remove(AppTripRequestSet domain) {
+
+    }
+
+}
