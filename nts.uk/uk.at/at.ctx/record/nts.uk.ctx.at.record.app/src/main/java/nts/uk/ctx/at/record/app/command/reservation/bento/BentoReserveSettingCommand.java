c280fa5e70f97fc889813bd51eda3d08372025a1
quang.nh1 <huquang.nhq@gmail.com>
2020-08-14 17:43:08 +0700

    fix bug ut

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/reservation/bento/BentoReserveSettingCommand.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/reservation/bento/BentoReserveSettingCommand.java
@@ -1,28 +1,47 @@
 package nts.uk.ctx.at.record.app.command.reservation.bento;
 
 import lombok.Value;
+import nts.uk.ctx.at.record.dom.reservation.bento.BentoReservationTime;
+import nts.uk.ctx.at.record.dom.reservation.bento.rules.BentoReservationTimeName;
 import nts.uk.ctx.at.record.dom.reservation.bentomenu.closingtime.BentoReservationClosingTime;
-import nts.uk.ctx.at.record.dom.reservation.reservationsetting.Achievements;
-import nts.uk.ctx.at.record.dom.reservation.reservationsetting.CorrectionContent;
-import nts.uk.ctx.at.record.dom.reservation.reservationsetting.OperationDistinction;
+import nts.uk.ctx.at.record.dom.reservation.bentomenu.closingtime.ReservationClosingTime;
+import nts.uk.ctx.at.record.dom.reservation.reservationsetting.*;
+
+import java.util.Optional;
 
 @Value
 public class BentoReserveSettingCommand {
 
     /** param : 予約の運用区別
      */
-    private OperationDistinction operationDistinction;
+    private int operationDistinction;
 
-    /** param : 実績集計
-     */
-    private Achievements achievements;
+    private int referenceTime;
+    private int dailyResults;
+    private int monthlyResults;
 
-    /** param : 予約修正内容
-     */
-    private CorrectionContent correctionContent;
+    private int contentChangeDeadline;
+    private int contentChangeDeadlineDay;
+    private int orderedData;
+    private int orderDeadline;
 
-    /** param : 予約締め時刻
-     */
-    private BentoReservationClosingTime closingTime;
+    private String name1;
+    private int end1;
+    private int start1;
+    private String name2;
+    private int end2;
+    private int start2;
+
+//    /** param : 実績集計
+//     */
+//    private AchievementValueObject achievements;
+//
+//    /** param : 予約修正内容
+//     */
+//    private CorrectionContentValueObject correctionContent;
+//
+//    /** param : 予約締め時刻
+//     */
+//    private ClosingTimeValueObject closingTime;
 
 }
