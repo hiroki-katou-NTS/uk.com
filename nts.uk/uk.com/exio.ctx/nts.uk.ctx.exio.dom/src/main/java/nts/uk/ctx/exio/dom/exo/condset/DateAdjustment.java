6e49c6997336db463d83e1b4596057da372a5316
nws-tungvd <tungvd@newwave.vn>
2020-08-17 17:27:27 +0700

    Fix primitive value

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/exo/condset/DateAdjustment.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/exo/condset/DateAdjustment.java
@@ -1,14 +1,12 @@
 package nts.uk.ctx.exio.dom.exo.condset;
 
 import nts.arc.primitive.IntegerPrimitiveValue;
-import nts.arc.primitive.constraint.IntegerMaxValue;
-import nts.arc.primitive.constraint.IntegerMinValue;
+import nts.arc.primitive.constraint.IntegerRange;
 
 /*
  * 日付調整
  */
-@IntegerMaxValue(31)
-@IntegerMinValue(-31)
+@IntegerRange(min = -31, max = 31)
 public class DateAdjustment extends IntegerPrimitiveValue<DateAdjustment> {
 
 	private static final long serialVersionUID = 1L;
