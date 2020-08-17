29f026bcd58d03b4b705f99e0becd533e607fb6f
Hop.NT <hop.nt@3si.vn>
2020-08-17 11:10:20 +0700

    Domain service  弁当予約を強制予約する

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/reservation/bento/CancelBentoReservationService.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/reservation/bento/CancelBentoReservationService.java
@@ -14,18 +14,19 @@ import java.util.stream.Collectors;
  */
 public class CancelBentoReservationService {
     public static AtomTask reserve(Require require, ReservationRegisterInfo registerInfor,
-                                   ReservationDate reservationDate, Optional<WorkLocationCode> workLocationCode)
+                                   ReservationDate reservationDate, GeneralDateTime reservationDatetime, Optional<WorkLocationCode> workLocationCode)
     {
         // $メニュー = require.弁当メニューを取得する(対象日.年月日,@勤務場所コード)
         val bentomenu = require.getBentoMenu(reservationDate,workLocationCode);
        // $メニュー.予約受付チェック(登録情報, 対象日)
-        bentomenu.receptionCheck(GeneralDateTime.now(),reservationDate);
+        bentomenu.receptionCheck(reservationDatetime,reservationDate);
         // $弁当予約 = require.弁当予約を取得する(登録情報, 対象日, 枠番)
         val bentoReservation = require.getBentoReservation(registerInfor,reservationDate,
                 bentomenu.getMenu().stream().map(i->i.getFrameNo()).collect(Collectors.toList()));
         // $弁当予約.取消可能かチェックする()
         bentoReservation.checkCancelPossible();
         return AtomTask.of(()->{
+            // require.弁当予約を取り消す(弁当予約)
             require.cancelOrder(bentoReservation);
         });
     }
