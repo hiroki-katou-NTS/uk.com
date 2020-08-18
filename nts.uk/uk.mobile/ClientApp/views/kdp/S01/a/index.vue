403dd973fadac67dbdf3077746fab2b505acdc8f
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2020-08-18 11:22:43 +0700

    KDPS01 - fix bug #110969 Lần thứ 2 khi click button A3, click 2 lần mới A3 mới mở được màn S

--- nts.uk/uk.mobile/ClientApp/views/kdp/S01/a/index.vue
+++ nts.uk/uk.mobile/ClientApp/views/kdp/S01/a/index.vue
@@ -1,30 +1,38 @@
 <template>
   <div>
-    <div id="clock"
-    v-bind:style="{
-          'background-color': setting.displaySettingsStampScreen.settingDateTimeColor.backgroundColor,
-          'color': setting.displaySettingsStampScreen.settingDateTimeColor.textColor
-        }">
-      <div class="col-12 pr-1 pt-2 font-weight-bold">
+    <div
+      id="clock"
+      v-bind:style="{
+        'background-color':
+          setting.displaySettingsStampScreen.settingDateTimeColor
+            .backgroundColor,
+        color: setting.displaySettingsStampScreen.settingDateTimeColor.textColor
+      }"
+    >
+      <div class="col-12 pr-1 pt-2 font-weight-bold clearfix">
         <span id="date">
           {{ $dt.now | date("YYYY年 MM月 DD日（ddd）") }}
         </span>
-        <button  v-click:500="openDialogS" type="button" class="float-right btn btn-success">
+        <button
+          v-on:click="openDialogS"
+          type="button"
+          class="float-right btn btn-success"
+        >
           {{ "KDPS01_25" | i18n }}
         </button>
       </div>
-      <div class="col-12 pt-2 px-5 font-weight-bold">
+      <div class="col-12 px-5 font-weight-bold">
         <span id="time">{{ $dt.now | date("HH:mm") }}</span>
       </div>
     </div>
     <div>
-      <pre 
+      <pre
         class="pl-4 pt-2"
         style="min-height: 4rem;"
         v-bind:style="{
-          'color': setting.stampPageComment.commentColor
+          color: setting.stampPageComment.commentColor
         }"
-      >{{ setting.stampPageComment.pageComment }}</pre
+        >{{ setting.stampPageComment.pageComment }}</pre
       >
     </div>
     <div class="d-flex flex-wrap">
@@ -40,7 +48,7 @@
             color: button.buttonDisSet.buttonNameSet.textColor,
             'background-color': button.buttonDisSet.backGroundColor
           }"
-          v-click:500="()=>stampClick(button)"
+          v-click:500="() => stampClick(button)"
           class="col-12 btn-stamp btn btn-secondary "
         >
           {{ button.buttonDisSet.buttonNameSet.buttonName }}
