1c8ddf5ce86bcb4dd5b1055c04011d902fe298bb
chinh.hm <chinh.hm@3si.vn>
2020-08-18 15:15:40 +0700

    ScreenQuery KSC001-d

--- nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/ksc001/d/GetEligibleEmployeesScreenQuery.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/ksc001/d/GetEligibleEmployeesScreenQuery.java
@@ -1,8 +1,11 @@
 package nts.uk.screen.at.app.ksc001.d;
 
 import lombok.val;
+import nts.arc.time.calendar.period.DatePeriod;
+import nts.uk.ctx.at.shared.dom.employmentrules.organizationmanagement.ConditionEmployee;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,16 +14,21 @@ import java.util.List;
  */
 @Stateless
 public class GetEligibleEmployeesScreenQuery {
-
-
+    @Inject
+    private ConditionEmployee.Require require;
     public List<String> getListEmployeeId(ConditionDto conditionDto){
-        val listId = conditionDto.getListEmployeeId();
         List<String> rs = new ArrayList<>();
-        listId.forEach(e ->{
-            if(true){
-                rs.add(e);
-            }
-        });
+        if(conditionDto!= null){
+            val check = new ConditionEmployee(conditionDto.isTransfer(),conditionDto.isLeaveOfAbsence(),
+                    conditionDto.isShortWorkingHours(),conditionDto.isChangedWorkingConditions());
+            val listId = conditionDto.getListEmployeeId();
+            val period = new DatePeriod(conditionDto.getStartDate(),conditionDto.getEndDate());
+            listId.forEach(e ->{
+                if(check.CheckEmployeesIsEligible(require,e,period)){
+                    rs.add(e);
+                }
+            });
+        }
         return rs;
     }
 }
