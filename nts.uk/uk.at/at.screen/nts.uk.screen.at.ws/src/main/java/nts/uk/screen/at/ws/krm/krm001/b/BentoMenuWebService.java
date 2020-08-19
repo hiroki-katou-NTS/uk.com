4818eef92eb46410b22e578dcf192b06f11a5ed1
quang.nh1 <huquang.nhq@gmail.com>
2020-08-19 16:51:32 +0700

    #27355 command đăng ký bento

--- nts.uk/uk.at/at.screen/nts.uk.screen.at.ws/src/main/java/nts/uk/screen/at/ws/krm/krm001/b/BentoMenuWebService.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.ws/src/main/java/nts/uk/screen/at/ws/krm/krm001/b/BentoMenuWebService.java
@@ -1,6 +1,7 @@
 package nts.uk.screen.at.ws.krm.krm001.b;
 
 import nts.arc.layer.ws.WebService;
+import nts.uk.screen.at.app.reservation.BentoJoinReservationSetting;
 import nts.uk.screen.at.app.reservation.BentoMenuDto;
 import nts.uk.screen.at.app.reservation.BentoMenuJoinBentoSettingDto;
 import nts.uk.screen.at.app.reservation.BentoMenuSetScreenProcessor;
@@ -8,8 +9,10 @@ import nts.uk.screen.at.app.reservation.BentoMenuSetScreenProcessor;
 import javax.inject.Inject;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("screen/at/record/reservation/bento_menu")
 @Produces(MediaType.APPLICATION_JSON)
@@ -25,8 +28,8 @@ public class BentoMenuWebService extends WebService{
     }
 
     @POST
-    @Path("getBentoMenuByHist")
-    public BentoMenuDto getBentoMenu() {
-        return this.bentoMenuSetScreenProcessor.getBentoMenuByHist();
+    @Path("getBentoMenuByHist/{histId}")
+    public List<BentoJoinReservationSetting> getBentoMenu(@PathParam("histId") String histId) {
+        return this.bentoMenuSetScreenProcessor.getBentoMenuByHist(histId);
     }
 }
