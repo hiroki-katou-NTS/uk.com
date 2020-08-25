f399e705f50fb7a226aee95adfab733b9884b7e4
Tran Ba Quy <quy_tb@nittsusystem-vn.com>
2020-08-25 17:01:51 +0700

    update ksu001u calendar

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksu/001/la/test/vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksu/001/la/test/vm.ts
@@ -20,10 +20,6 @@ module test {
                 nts.uk.ui.windows.setShared("baseDate", moment(self.date()).format('YYYY/MM/DD'));
                 self.currentScreen = nts.uk.ui.windows.sub.modal("/view/ksu/001/la/index.xhtml");
             }   
-            // public openDialogU(): void {
-            //     let self = this;
-            //     self.currentScreen = nts.uk.ui.windows.sub.modeless("/view/ksu/001/u/index.xhtml");
-            // }   
         }
     }    
 }
