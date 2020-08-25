6ab00bb3a3c041467927a185558c554e34b576bd
Tran Ba Quy <quy_tb@nittsusystem-vn.com>
2020-08-25 14:08:09 +0700

    add màn hình test cho ksu001u

---
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksu/001/u/test/ksu001.test.start.ts
@@ -0,0 +1,8 @@
+module nts.uk.at.view.ksu001.u.test {
+    __viewContext.ready(function() {
+        var screenModel = new viewmodel.ScreenModel();
+        screenModel.startPage().done(function() {
+            __viewContext.bind(screenModel);
+        });
+    });
+}
