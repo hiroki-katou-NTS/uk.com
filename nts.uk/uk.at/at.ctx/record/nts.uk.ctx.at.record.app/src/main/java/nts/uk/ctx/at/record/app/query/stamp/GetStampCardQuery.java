ea986c441a0f6435783df39cef8f19ce85c5c636
Lê Hữu Đạt <dat.lh@3si.vn>
2020-08-27 11:00:06 +0700

    fix kmr002, kmr003, get StampNumber by List<社員ID＞

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/query/stamp/GetStampCardQuery.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/query/stamp/GetStampCardQuery.java
@@ -2,13 +2,11 @@ package nts.uk.ctx.at.record.app.query.stamp;
 
 import nts.uk.ctx.at.record.dom.stamp.card.stampcard.StampCard;
 import nts.uk.ctx.at.record.dom.stamp.card.stampcard.StampCardRepository;
+import nts.uk.ctx.at.record.dom.stamp.card.stampcard.StampNumber;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Stateless
@@ -19,19 +17,20 @@ public class GetStampCardQuery {
 
     /**
      * List<社員ID＞から打刻カードを全て取得する
-     * 
+     *
+     * @param empIds List<社員ID＞
+     * @return return Map＜社員ID、打刻カード番号＞
      * @author Le Huu Dat
      */
-    public List<StampCard> getStampCardBy(List<String> empIds) {
+    public Map<String, StampNumber> getStampNumberBy(List<String> empIds) {
         List<StampCard> stampCardAll = stampCardRepo.getLstStampCardByLstSid(empIds);
-        List<StampCard> stampCards = new ArrayList<>();
+        Map<String, StampNumber> stampCards = new HashMap<>();
         Map<String, List<StampCard>> stampCardMap = stampCardAll.stream().collect(
                 Collectors.groupingBy(StampCard::getEmployeeId, Collectors.toList()));
         for (Map.Entry<String, List<StampCard>> entry : stampCardMap.entrySet()) {
             StampCard item = entry.getValue().stream()
-                    .sorted(Comparator.comparing(StampCard::getRegisterDate, Comparator.naturalOrder()).reversed())
-                    .findFirst().get();
-            stampCards.add(item);
+                    .max(Comparator.comparing(StampCard::getRegisterDate, Comparator.naturalOrder())).get();
+            stampCards.put(entry.getKey(), item.getStampNumber());
         }
         return stampCards;
     }
