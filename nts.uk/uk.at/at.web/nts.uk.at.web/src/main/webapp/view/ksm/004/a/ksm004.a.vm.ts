ceada3fc7e773ba2b5c96ca31cbd31b0469ad514
Dat.nk <datnkph@gmail.com>
2020-08-28 10:16:18 +0700

    fix bug kms004

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksm/004/a/ksm004.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksm/004/a/ksm004.a.vm.ts
@@ -886,11 +886,11 @@ module nts.uk.at.view.ksm004.a {
                 change Style when change selected Working Day
             */
             changeWorkingDayAtr(value){
-                $('.panel-frame-btn').css("background-color","unset");
+                $('.panel-frame-btn').css("background-color","white");
                 const self = this;
                 if(value!=null) {
                     if (value == 1) {
-                        $('.button-sqr1').css("background-color","rgb(72 147 224 / 40%)");
+                        $('.button-sqr1').css("background-color","rgb(155,194,230)");
                     } else {
                         $('.button-sqr'+value).css("background","lightgoldenrodyellow");
                     }
