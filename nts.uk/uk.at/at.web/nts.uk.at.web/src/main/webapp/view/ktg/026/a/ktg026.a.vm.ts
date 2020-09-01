9081c736fb835ae3104ede33a8013a7c17ae4e48
nws-dungdv <dungdv@newwave.vn>
2020-09-01 17:52:51 +0700

    UI screen

---
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ktg/026/a/ktg026.a.vm.ts
@@ -0,0 +1,20 @@
+/// <reference path='../../../../lib/nittsu/viewcontext.d.ts' />
+module nts.uk.at.view.ktg029.a.viewmodel {
+  import block = nts.uk.ui.block;
+  import getText = nts.uk.resource.getText;
+  import info = nts.uk.ui.dialog.info;
+
+  @bean()
+  export class ViewModel extends ko.ViewModel {
+    year: KnockoutObservable<string> = ko.observable('');
+    
+    created() {
+
+    }
+
+    mounted() {
+
+    }
+  }
+}
+
