cce14133ba0fea116316c32741b34db632e969df
manhb <manhb@newwave.vn>
2020-09-01 11:49:58 +0700

    implement CCG007 & CCGS007

--- nts.uk/uk.mobile/ClientApp/views/ccg/007/a/index.ts
+++ nts.uk/uk.mobile/ClientApp/views/ccg/007/a/index.ts
@@ -1,10 +1,59 @@
 import { Vue } from '@app/provider';
-import { component } from '@app/core/component';
+import { component, Prop } from '@app/core/component';
+import { storage, auth } from '@app/utils';
+import { NavMenu, SideMenu } from '@app/services';
 
 @component({
     route: '/ccg/007/a',
     template: require('./index.vue'),
+    validations: {
+        model: {
+            contractCode: {
+                required: true
+            },
+            password: {
+                required: true
+            }
+        }
+    },
     name: 'ccg007a'
 })
 export class Ccg007AComponent extends Vue {
+
+    @Prop({ default: () => ({}) })
+    public params!: any;
+
+    public model = {
+        contractCode: '',
+        password: ''
+    };
+
+    public mounted() {
+        this.$nextTick(() => {
+            this.$parent.$el.querySelector('.container-fluid').classList.add('uk-bg-process');
+        });
+    }
+
+    public authContract() {
+        this.$validate();
+        if (!this.$valid) {
+            return;
+        }
+        this.$http.post('ctx/sys/gateway/login/submitcontract', this.model).then((response) => {
+            console.log('success', response);
+
+            return;
+            storage.local.setItem('contract', { code: this.model.contractCode, password: this.model.password });
+            this.$router.go(-1);
+        }).catch((error) => {
+            console.log('failed', error);
+
+            return;
+        });
+    }
+
+    public beforeDestroy() {
+        this.$parent.$el.querySelector('.container-fluid').classList.remove('uk-bg-process');
+    }
+
 }
--- nts.uk/uk.mobile/ClientApp/views/ccg/007/a/index.vue
+++ nts.uk/uk.mobile/ClientApp/views/ccg/007/a/index.vue
@@ -1,5 +1,40 @@
 <template>
-    <div id="contract-confirm">
-        <div class="text-center page-title">契約認証</div>
+  <div id="contract-confirm" style="width: 100%; height: 100%">
+    <div class="text-center page-title uk-bg-olive">{{'CCGS07_50' | i18n}}</div>
+    <div class="text-left page-info mt-3 uk-text-dark-gray">
+      <div>{{ 'CCGS07_51' | i18n }}</div>
     </div>
+    <fieldset class="login-form mt-3">
+      <!-- <form v-submit="login"> -->
+      <nts-text-editor
+        id="contractCode"
+        name="CCGS07_52"
+        v-bind:tabindex="1"
+        v-model="model.contractCode"
+        v-bind:show-title="true"
+        v-bind:columns="{ title: 'col-lg-4', input: 'col-lg-8' }"
+        v-bind:constraint="validations.model.contractCode"
+      />
+      <nts-input-password
+        id="password"
+        name="CCGS07_53"
+        v-bind:tabindex="2"
+        v-model="model.password"
+        v-bind:show-title="true"
+        v-bind:columns="{ title: 'col-lg-4', input: 'col-lg-8' }"
+        v-bind:constraint="validations.model.password"
+      />
+      <!-- </form> -->
+      <div class="text-left page-info mt-3 uk-text-dark-gray">
+        <div>{{ 'CCGS07_54' | i18n }}</div>
+      </div>
+      <div class="mt-3">
+        <button
+          v-bind:tabindex="3"
+          class="btn btn-primary btn-block"
+          v-click:500="authContract"
+        >{{'CCGS07_55' | i18n}}</button>
+      </div>
+    </fieldset>
+  </div>
 </template>
