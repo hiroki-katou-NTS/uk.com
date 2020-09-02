c53bb63f7778097e1be71e0ccfe12befb496e602
Hop.NT <hop.nt@3si.vn>
2020-09-02 23:09:08 +0700

    refactor package

--- nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/ksm005/find/WeeklyWorkDto.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/ksm005/find/WeeklyWorkDto.java
@@ -2,7 +2,7 @@ package nts.uk.screen.at.app.ksm005.find;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import nts.uk.ctx.at.schedule.dom.shift.WeeklyWorkDay.WorkdayPatternItem;
+import nts.uk.ctx.at.schedule.dom.shift.weeklywrkday.WorkdayPatternItem;
 
 import java.util.List;
 
