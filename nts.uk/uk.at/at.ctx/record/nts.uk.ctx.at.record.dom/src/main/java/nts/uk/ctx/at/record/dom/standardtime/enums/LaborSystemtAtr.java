6e0558fd3db968f5fb7cdf4f6767519607f0b3d5
chinh.hm <chinh.hm@3si.vn>
2020-09-03 12:00:16 +0700

    update domain

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/standardtime/enums/LaborSystemtAtr.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/standardtime/enums/LaborSystemtAtr.java
@@ -1,9 +1,9 @@
 package nts.uk.ctx.at.record.dom.standardtime.enums;
 
+import nts.uk.ctx.at.shared.dom.workingcondition.WorkingSystem;
+
 /**
- * 
- * @author nampt
- *
+ *Enum : ３６協定労働制
  */
 public enum LaborSystemtAtr {
 	
@@ -21,5 +21,45 @@ public enum LaborSystemtAtr {
 		this.value = type;
 	}
 
-	
+	/**
+	 * Value of.
+	 *
+	 * @param value
+	 *            the value
+	 * @return the rounding
+	 */
+	public static LaborSystemtAtr valueOf(Integer value) {
+		// Invalid object.
+		if (value == null) {
+			return null;
+		}
+
+		// Find value.
+		for (LaborSystemtAtr val : LaborSystemtAtr.values()) {
+			if (val.value == value) {
+				return val;
+			}
+		}
+
+		// Not found.
+		return null;
+	}
+
+	/**
+	 * GENERAL_LABOR_SYSTEM
+	 *
+	 */
+	public boolean isGeneralLaborSystem(){
+		return this.equals(GENERAL_LABOR_SYSTEM);
+	}
+
+
+	/**
+	 * DEFORMATION_WORKING_TIME_SYSTEM(1);
+	 *
+	 */
+	public boolean isDefformationWorrkingTimeSystem(){
+		return this.equals(DEFORMATION_WORKING_TIME_SYSTEM);
+	}
+
 }
