53bb56cd327be52741f08e18e4bdaba5276d72cc
khai.dh <khai.dh@3si.vn>
2020-09-04 13:13:43 +0700

    domain 36

---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/monthly/agreement/approver/Approver36AgrByWorkplaceRepo.java
@@ -0,0 +1,79 @@
+package nts.uk.ctx.at.record.dom.monthly.agreement.approver;
+
+import nts.arc.time.GeneralDate;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * 職場別の承認者（36協定）Repository
+ *
+ * Table: KRCMT_36AGR_APV_WKP
+ *
+ * @author khai.dh
+ */
+
+public interface Approver36AgrByWorkplaceRepo {
+
+	/**
+	 * [1] Insert(職場別の承認者（36協定）)
+	 */
+	public void insert(Approver36AgrByWorkplace domain);
+
+	/**
+	 * [2] Update(職場別の承認者（36協定）)
+	 */
+	public void update(Approver36AgrByWorkplace domain);
+
+	/**
+	 * [3] Delete(職場別の承認者（36協定）)
+	 */
+	public void delete(Approver36AgrByWorkplace domain);
+
+	/**
+	 * [4] get
+	 * 指定職場の全ての職場別の承認者（36協定）を取得する
+	 *
+	 * AggregateRoot
+	 * 		職場別の承認者（36協定） m
+	 * Where
+	 * 		m.職場ID = 職場ID
+	 */
+	public List<Approver36AgrByWorkplace> getByWorkplaceId(String workplaceId);
+
+	/**
+	 * [5] 指定日以降の履歴を取得する
+	 * 指定日以降の職場別の承認者（36協定）を取得する
+	 *
+	 * AggregateRoot
+	 * 		職場別の承認者（36協定） m
+	 * Where
+	 * 		m.職場ID = 職場ID
+	 * 		m.期間.開始日 >= 年月日
+	 */
+	public List<Approver36AgrByWorkplace> getByWorkplaceIdFromDate(String workplaceId, GeneralDate date);
+
+	/**
+	 * [6] 指定終了日の履歴を取得する
+	 * 指定日する終了日の職場別の承認者（36協定）の履歴を取得する
+	 *
+	 * AggregateRoot
+	 * 		職場別の承認者（36協定） m
+	 * Where
+	 * 		m.職場ID = 職場ID
+	 * 		m.期間.終了日 = 終了日 TODO https://pro.insight.3si.vn/issues/30261
+	 */
+	public Optional<Approver36AgrByWorkplace> getByWorkplaceIdToDate(String workplaceId, GeneralDate date);
+
+	/**
+	 * [7] get
+	 * 基準日時点に指定の職場の承認者を取得する
+	 *
+	 * AggregateRoot
+	 * 		職場別の承認者（36協定） m
+	 * Where
+	 * 		m.職場ID = 職場ID
+	 * 		m.期間.開始日 <= 基準日 <= m.期間.終了日
+	 */
+	public Optional<Approver36AgrByWorkplace> getByWorkplaceIdAndDate(String workplaceId, GeneralDate refDate);
+}
---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.infra/src/main/java/nts/uk/ctx/at/record/infra/entity/monthly/agreement/Krcmt36AgrApvWkp.java
@@ -0,0 +1,131 @@
+package nts.uk.ctx.at.record.infra.entity.monthly.agreement;
+
+import lombok.NoArgsConstructor;
+import nts.arc.layer.infra.data.entity.type.GeneralDateToDBConverter;
+import nts.arc.time.GeneralDate;
+import nts.arc.time.calendar.period.DatePeriod;
+import nts.gul.text.StringUtil;
+import nts.uk.ctx.at.record.dom.monthly.agreement.approver.Approver36AgrByWorkplace;
+import nts.uk.shr.infra.data.entity.UkJpaEntity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * エンティティ：職場別の承認者（36協定）
+ *
+ * @author khai.dh
+ */
+@Entity
+@Table(name = "KRCMT_36AGR_APV_WKP")
+@NoArgsConstructor
+public class Krcmt36AgrApvWkp extends UkJpaEntity implements Serializable {
+
+	/**
+	 * serialVersionUID
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@EmbeddedId
+	public Krcmt36AgrApvWkpPK PK;
+
+	@Basic(optional = false)
+	@NotNull
+	@Column(name = "CID")
+	public String companyID;
+
+	@Basic(optional = false)
+	@NotNull
+	@Column(name = "END_DATE")
+	@Convert(converter = GeneralDateToDBConverter.class)
+	public GeneralDate endDate;
+
+	@Basic(optional = false)
+	@NotNull
+	@Column(name = "APV_SID_1")
+	public String approverSid1;
+
+	@Column(name = "APV_SID_2")
+	public String approverSid2;
+
+	@Column(name = "APV_SID_3")
+	public String approverSid3;
+
+	@Column(name = "APV_SID_4")
+	public String approverSid4;
+
+	@Column(name = "APV_SID_5")
+	public String approverSid5;
+
+	@Column(name = "CFM_SID_1")
+	public String confirmerSid1;
+
+	@Column(name = "CFM_SID_2")
+	public String confirmerSid2;
+
+	@Column(name = "CFM_SID_3")
+	public String confirmerSid3;
+
+	@Column(name = "CFM_SID_4")
+	public String confirmerSid4;
+
+	@Column(name = "CFM_SID_5")
+	public String confirmerSid5;
+
+	@Override
+	protected Object getKey() {
+		return this.PK;
+	}
+
+	public Approver36AgrByWorkplace toDomain() {
+		List<String> approverIds = new ArrayList<>();
+		approverIds.add(this.approverSid1);
+		if (!StringUtil.isNullOrEmpty(this.approverSid2, true)) approverIds.add(this.approverSid2);
+		if (!StringUtil.isNullOrEmpty(this.approverSid3, true)) approverIds.add(this.approverSid3);
+		if (!StringUtil.isNullOrEmpty(this.approverSid4, true)) approverIds.add(this.approverSid4);
+		if (!StringUtil.isNullOrEmpty(this.approverSid5, true)) approverIds.add(this.approverSid5);
+
+		List<String> confirmerIds = new ArrayList<>();
+		if (!StringUtil.isNullOrEmpty(this.confirmerSid1, true)) confirmerIds.add(this.confirmerSid1);
+		if (!StringUtil.isNullOrEmpty(this.confirmerSid2, true)) confirmerIds.add(this.confirmerSid2);
+		if (!StringUtil.isNullOrEmpty(this.confirmerSid3, true)) confirmerIds.add(this.confirmerSid3);
+		if (!StringUtil.isNullOrEmpty(this.confirmerSid4, true)) confirmerIds.add(this.confirmerSid4);
+		if (!StringUtil.isNullOrEmpty(this.confirmerSid5, true)) confirmerIds.add(this.confirmerSid5);
+
+		return new Approver36AgrByWorkplace(
+				this.companyID,
+				this.PK.workplaceID,
+				new DatePeriod(this.PK.startDate, this.endDate),
+				approverIds,
+				confirmerIds
+		);
+	}
+
+	public void fromDomainForPersist(Approver36AgrByWorkplace domain) {
+		this.PK = new Krcmt36AgrApvWkpPK(domain.getWorkplaceId(), domain.getPeriod().start());
+		this.fromDomainForUpdate(domain);
+	}
+
+	public void fromDomainForUpdate(Approver36AgrByWorkplace domain) {
+
+		this.companyID = domain.getCompanyId();
+		this.endDate = domain.getPeriod().end();
+
+		List<String> approverIds = domain.getApproverIds();
+		this.approverSid1 = approverIds.get(0);
+		if (approverIds.size() > 1) this.approverSid2 = approverIds.get(1);
+		if (approverIds.size() > 2) this.approverSid3 = approverIds.get(2);
+		if (approverIds.size() > 3) this.approverSid4 = approverIds.get(3);
+		if (approverIds.size() > 4) this.approverSid5 = approverIds.get(4);
+
+		List<String> confirmerIds = domain.getConfirmerIds();
+		if (confirmerIds.size() > 0) this.confirmerSid1 = confirmerIds.get(0);
+		if (confirmerIds.size() > 1) this.confirmerSid2 = confirmerIds.get(1);
+		if (confirmerIds.size() > 2) this.confirmerSid3 = confirmerIds.get(2);
+		if (confirmerIds.size() > 3) this.confirmerSid4 = confirmerIds.get(3);
+		if (confirmerIds.size() > 4) this.confirmerSid5 = confirmerIds.get(4);
+	}
+}
