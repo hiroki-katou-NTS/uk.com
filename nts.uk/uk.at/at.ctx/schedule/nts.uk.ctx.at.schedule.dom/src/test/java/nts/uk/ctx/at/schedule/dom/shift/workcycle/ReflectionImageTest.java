2d24da40696822daebe5198c1c55a54a3a94cf87
quang.nh1 <huquang.nhq@gmail.com>
2020-09-08 16:36:24 +0700

    #30621 fix review junit

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/test/java/nts/uk/ctx/at/schedule/dom/shift/workcycle/ReflectionImageTest.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/test/java/nts/uk/ctx/at/schedule/dom/shift/workcycle/ReflectionImageTest.java
@@ -4,15 +4,11 @@ import lombok.val;
 import mockit.integration.junit4.JMockit;
 import nts.arc.testing.assertion.NtsAssert;
 import nts.arc.time.GeneralDate;
-import nts.uk.ctx.at.schedule.dom.shift.workcycle.domainservice.RefImageEachDay;
 import nts.uk.ctx.at.schedule.dom.shift.workcycle.domainservice.ReflectionImage;
 import nts.uk.ctx.at.schedule.dom.shift.workcycle.domainservice.WorkCreateMethod;
 import nts.uk.ctx.at.shared.dom.WorkInformation;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.util.HashMap;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -29,93 +25,96 @@ public class ReflectionImageTest {
 
     @Test
     public void addByWeeklyWorkingTest_1() {
+        WorkInformation workInformation = new WorkInformation("workTimeCode", "workTypeCode");
         ReflectionImage target = ReflectionImage.create();
-        target.addByWeeklyWorking(GeneralDate.today(), null);
+        target.addByWeeklyWorking(GeneralDate.today(), workInformation);
 
         val result = target.getListRefOrdByDate().get(0);
 
-        Assert.assertEquals(GeneralDate.today(),result.getDate());
-        Assert.assertEquals(WorkCreateMethod.WEEKLY_WORK,result.getWorkCreateMethod());
-        Assert.assertEquals(null,result.getWorkInformation());
+        assertThat(GeneralDate.today()).isEqualByComparingTo(result.getDate());
+        assertThat(WorkCreateMethod.WEEKLY_WORK).isEqualByComparingTo(result.getWorkCreateMethod());
+        assertThat(workInformation).isEqualTo(result.getWorkInformation());
     }
 
     @Test
     public void addByWeeklyWorkingTest_2() {
         WorkInformation workCycleWorkInfor = new WorkInformation("workCcle-workTimeCode", "workCcle-workTypeCode");
-        WorkInformation weeklWorkInfor = new WorkInformation("weekly-workTimeCode", "weekly-workTypeCode");
+        WorkInformation workInformation = new WorkInformation("weekly-workTimeCode", "weekly-workTypeCode");
 
         ReflectionImage target = ReflectionImage.create();
         target.addInWorkCycle(GeneralDate.today(),workCycleWorkInfor);
-
-        target.addByWeeklyWorking(GeneralDate.today(),weeklWorkInfor);
-
-        val result = target.getListRefOrdByDate().get(0);
-
-        Assert.assertEquals(GeneralDate.today(),result.getDate());
-        Assert.assertEquals(WorkCreateMethod.WEEKLY_WORK,result.getWorkCreateMethod());
-        Assert.assertEquals(weeklWorkInfor,result.getWorkInformation());
-    }
-
-    @Test
-    public void addByWeeklyWorkingTest_3() {
-        WorkInformation workInformation = new WorkInformation("workTimeCode", "workTypeCode");
-
-        ReflectionImage target = ReflectionImage.create();
-        target.addHolidays(GeneralDate.today(), workInformation);
 
         target.addByWeeklyWorking(GeneralDate.today(),workInformation);
 
         val result = target.getListRefOrdByDate().get(0);
 
-        Assert.assertEquals(GeneralDate.today(),result.getDate());
-        Assert.assertEquals(WorkCreateMethod.PUB_HOLIDAY,result.getWorkCreateMethod());
-        Assert.assertEquals(workInformation,result.getWorkInformation());
+        assertThat(GeneralDate.today()).isEqualByComparingTo(result.getDate());
+        assertThat(WorkCreateMethod.WEEKLY_WORK).isEqualByComparingTo(result.getWorkCreateMethod());
+        assertThat(workInformation).isEqualTo(result.getWorkInformation());
+    }
 
+    @Test
+    public void addByWeeklyWorkingTest_3() {
+        WorkInformation workHolidayWorkInfor = new WorkInformation("workCcle-workTimeCode", "workCcle-workTypeCode");
+        WorkInformation workInformation = new WorkInformation("weekly-workTimeCode", "weekly-workTypeCode");
+
+        ReflectionImage target = ReflectionImage.create();
+        target.addHolidays(GeneralDate.today(), workHolidayWorkInfor);
+
+        target.addByWeeklyWorking(GeneralDate.today(),workInformation);
+
+        val result = target.getListRefOrdByDate().get(0);
+
+        assertThat(GeneralDate.today()).isEqualByComparingTo(result.getDate());
+        assertThat(WorkCreateMethod.PUB_HOLIDAY).isEqualByComparingTo(result.getWorkCreateMethod());
+        assertThat(workHolidayWorkInfor).isEqualTo(result.getWorkInformation());
     }
 
     @Test
     public void addHolidaysTest_1() {
+        WorkInformation workInformation = new WorkInformation("workTimeCode", "workTypeCode");
         ReflectionImage target = ReflectionImage.create();
-        target.addHolidays(GeneralDate.today(), null);
+        target.addHolidays(GeneralDate.today(), workInformation);
 
         val result = target.getListRefOrdByDate().get(0);
 
-        Assert.assertEquals(GeneralDate.today(),result.getDate());
-        Assert.assertEquals(WorkCreateMethod.PUB_HOLIDAY,result.getWorkCreateMethod());
-        Assert.assertEquals(null,result.getWorkInformation());
+        assertThat(GeneralDate.today()).isEqualByComparingTo(result.getDate());
+        assertThat(WorkCreateMethod.PUB_HOLIDAY).isEqualByComparingTo(result.getWorkCreateMethod());
+        assertThat(workInformation).isEqualTo(result.getWorkInformation());
     }
 
     @Test
     public void addHolidaysTest_2() {
         WorkInformation workCycleWorkInfor = new WorkInformation("workCcle-workTimeCode", "workCcle-workTypeCode");
-        WorkInformation weeklWorkInfor = new WorkInformation("weekly-workTimeCode", "weekly-workTypeCode");
+        WorkInformation workInformation = new WorkInformation("weekly-workTimeCode", "weekly-workTypeCode");
 
         ReflectionImage target = ReflectionImage.create();
         target.addInWorkCycle(GeneralDate.today(),workCycleWorkInfor);
 
-        target.addHolidays(GeneralDate.today(),weeklWorkInfor);
+        target.addHolidays(GeneralDate.today(),workInformation);
 
         val result = target.getListRefOrdByDate().get(0);
 
-        Assert.assertEquals(GeneralDate.today(),result.getDate());
-        Assert.assertEquals(WorkCreateMethod.PUB_HOLIDAY,result.getWorkCreateMethod());
-        Assert.assertEquals(weeklWorkInfor,result.getWorkInformation());
+        assertThat(GeneralDate.today()).isEqualByComparingTo(result.getDate());
+        assertThat(WorkCreateMethod.PUB_HOLIDAY).isEqualByComparingTo(result.getWorkCreateMethod());
+        assertThat(workInformation).isEqualTo(result.getWorkInformation());
     }
 
     @Test
     public void addHolidaysTest_3() {
-        WorkInformation workInformation = new WorkInformation("workTimeCode", "workTypeCode");
+        WorkInformation workHolidayWorkInfor = new WorkInformation("holiday-workTimeCode", "holiday-workTypeCode");
+        WorkInformation workInformation = new WorkInformation("weekly-workTimeCode", "weekly-workTypeCode");
 
         ReflectionImage target = ReflectionImage.create();
         target.addByWeeklyWorking(GeneralDate.today(),workInformation);
         
-        target.addHolidays(GeneralDate.today(), workInformation);
+        target.addHolidays(GeneralDate.today(), workHolidayWorkInfor);
 
         val result = target.getListRefOrdByDate().get(0);
 
-        Assert.assertEquals(GeneralDate.today(),result.getDate());
-        Assert.assertEquals(WorkCreateMethod.WEEKLY_WORK,result.getWorkCreateMethod());
-        Assert.assertEquals(workInformation,result.getWorkInformation());
+        assertThat(GeneralDate.today()).isEqualByComparingTo(result.getDate());
+        assertThat(WorkCreateMethod.WEEKLY_WORK).isEqualByComparingTo(result.getWorkCreateMethod());
+        assertThat(workInformation).isEqualTo(result.getWorkInformation());
     }
 
     @Test
@@ -132,14 +131,14 @@ public class ReflectionImageTest {
         WorkInformation workInformation = new WorkInformation("workTimeCode2", "workTypeCode2");
 
         ReflectionImage target = ReflectionImage.create();
-        target.addHolidays(GeneralDate.max(),holidayWorkInfo);
+        target.addInWorkCycle(GeneralDate.max(),holidayWorkInfo);
 
         val result = target.getListRefOrdByDate().get(0);
 
         assertThat(target.addInWorkCycle(GeneralDate.today(),workInformation)).isTrue();
-        Assert.assertEquals(GeneralDate.max(),result.getDate());
-        Assert.assertEquals(WorkCreateMethod.PUB_HOLIDAY,result.getWorkCreateMethod());
-        Assert.assertEquals(holidayWorkInfo,result.getWorkInformation());
+        assertThat(GeneralDate.max()).isEqualByComparingTo(result.getDate());
+        assertThat(WorkCreateMethod.WORK_CYCLE).isEqualByComparingTo(result.getWorkCreateMethod());
+        assertThat(holidayWorkInfo).isEqualTo(result.getWorkInformation());
     }
 
     @Test
@@ -153,10 +152,11 @@ public class ReflectionImageTest {
         target.addHolidays(GeneralDate.ymd(2020,9,3),workInformation);
 
         val result = target.getListRefOrdByDate();
-        Assert.assertEquals(result.get(0).getDate(),GeneralDate.ymd(2020,9,1));
-        Assert.assertEquals(result.get(1).getDate(),GeneralDate.ymd(2020,9,2));
-        Assert.assertEquals(result.get(2).getDate(),GeneralDate.ymd(2020,9,3));
-        Assert.assertEquals(result.get(3).getDate(),GeneralDate.ymd(2020,9,5));
+
+        assertThat(result.get(0).getDate()).isEqualByComparingTo(GeneralDate.ymd(2020,9,1));
+        assertThat(result.get(1).getDate()).isEqualByComparingTo(GeneralDate.ymd(2020,9,2));
+        assertThat(result.get(2).getDate()).isEqualByComparingTo(GeneralDate.ymd(2020,9,3));
+        assertThat(result.get(3).getDate()).isEqualByComparingTo(GeneralDate.ymd(2020,9,5));
     }
 
 
