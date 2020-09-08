413866d44c9b357e0b256e02f2edbd34ed16b6a6
nws-dongnt <dongnt@newwave.vn>
2020-09-08 09:39:54 +0700

    Update

--- nts.uk/uk.com/com.ctx/sys/portal/nts.uk.ctx.sys.portal.ws/src/main/java/nts/uk/ctx/sys/portal/ws/logsettings/LogSettingService.java
+++ nts.uk/uk.com/com.ctx/sys/portal/nts.uk.ctx.sys.portal.ws/src/main/java/nts/uk/ctx/sys/portal/ws/logsettings/LogSettingService.java
@@ -8,6 +8,7 @@ import javax.ws.rs.Produces;
 import nts.arc.layer.ws.WebService;
 import nts.uk.ctx.sys.portal.app.find.logsettings.LogSettingDto;
 import nts.uk.ctx.sys.portal.app.find.logsettings.LogSettingFinder;
+import nts.uk.ctx.sys.portal.dom.logsettings.LogSetting;
 
 
 @Path("sys/portal/logsettings")
@@ -21,6 +22,12 @@ public class LogSettingService extends WebService {
 	@Path("findBySystem/{systemType}")
 	public List<LogSettingDto> findBySystem(int systemType) {
 		return this.logSettingFinder.findBySystem(systemType);
+	}
+	
+	@POST
+	@Path("update")
+	public void updateLogSetting(List<LogSetting> logSettings) {
+		this.logSettingFinder.updateLogsetting(logSettings);
 	}
 
 }
