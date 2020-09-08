98060df7dc78b9474edda2f27ade64ba62a756a2
nws-longdh <longdh@newwave.vn>
2020-09-08 09:40:31 +0700

    update api

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cli/002/a/cli002.a.service.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cli/002/a/cli002.a.service.ts
@@ -2,6 +2,7 @@ module nts.uk.com.view.cli002.a.service {
   import ajax = nts.uk.request.ajax;
     var servicePath: any = {
       findBySystem: "sys/portal/pginfomation/findBySystem",
+      updateLogSetting: "sys/portal/logsettings/update"
     }
 
     export function findBySystem(systemType: number): JQueryPromise<any> {
