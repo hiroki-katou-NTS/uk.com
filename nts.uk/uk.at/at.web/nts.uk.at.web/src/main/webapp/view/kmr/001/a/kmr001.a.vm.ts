4e163ce7363ec9c0e6627c892a8d81f10d4c50b1
Dat.nk <datnkph@gmail.com>
2020-09-08 15:07:17 +0700

    clean code man kmr001.a,b

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmr/001/a/kmr001.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmr/001/a/kmr001.a.vm.ts
@@ -1,26 +1,7 @@
 /// <reference path="../../../../lib/nittsu/viewcontext.d.ts" />
 
 module nts.uk.at.kmr001.a {
-	const API = {
-	};
-	const PATH = {
-		REDIRECT : '/view/ccg/008/a/index.xhtml'
-	}
-
 	@bean()
-	export class KMR001AViewModel extends ko.ViewModel {
-
-
-		created() {
-
-		}
-
-		setting() {
-
-		}
-
-		company() {
-		}
-
+	export class ViewModel extends ko.ViewModel {
 	}
 }
