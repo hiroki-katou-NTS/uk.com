0e3ec7f736239f239b14e822c1170c65a3606200
NSVN-NguyenDinhHoang <61735854+NSVN-NguyenDinhHoang@users.noreply.github.com>
2020-09-10 12:32:09 +0700

    Refactor4: requestList4

---
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.pub/src/main/java/nts/uk/ctx/at/schedule/pub/schedule/basicschedule/ScWorkScheduleExport_New.java
@@ -0,0 +1,39 @@
+package nts.uk.ctx.at.schedule.pub.schedule.basicschedule;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import nts.arc.time.GeneralDate;
+import nts.uk.shr.com.time.TimeWithDayAttr;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ScWorkScheduleExport_New {
+	// BasicSchedule
+	// 社員ID
+	private String employeeId;
+	// 年月日
+	private GeneralDate date;
+	// 勤務種類コード
+	private String workTypeCode;
+
+	// 就業時間帯コード
+	private String workTimeCode;
+	// 開始時刻1
+	private TimeWithDayAttr scheduleStartClock1;
+	// 終了時刻1
+	private TimeWithDayAttr scheduleEndClock1;
+	// 開始時刻2
+	private TimeWithDayAttr scheduleStartClock2;
+	// 終了時刻2
+	private TimeWithDayAttr scheduleEndClock2;
+//	育児時間
+	private Integer childTime;
+//	短時間勤務時間帯
+	private List<ShortWorkingTimeSheetExport> listShortWorkingTimeSheetExport = new ArrayList<>();
+		
+}
