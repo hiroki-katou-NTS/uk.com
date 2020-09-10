eee8785fe4afb86cad12eb63f6f6b7b4fa787abd
NTS-NguyenXuanThanh <thanh_nx@nittsusystem.co.jp>
2020-09-10 09:18:47 +0900

    fix error 勤怠ルールの補正処理(自動打刻セットする)v3

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/dailyattdcal/dailywork/functionalgorithm/setautomatic/RoundAttendanceTime.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/dailyattdcal/dailywork/functionalgorithm/setautomatic/RoundAttendanceTime.java
@@ -7,11 +7,22 @@ import java.util.Optional;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+import lombok.AllArgsConstructor;
 import nts.uk.ctx.at.shared.dom.dailyattdcal.dailywork.functionalgorithm.setautomatic.AddTimeZoneToAttendance.AttLeavAtr;
 import nts.uk.ctx.at.shared.dom.worktime.algorithm.getcommonset.GetCommonSet;
 import nts.uk.ctx.at.shared.dom.worktime.common.RoundingSet;
 import nts.uk.ctx.at.shared.dom.worktime.common.Superiority;
 import nts.uk.ctx.at.shared.dom.worktime.common.WorkTimezoneCommonSet;
+import nts.uk.ctx.at.shared.dom.worktime.difftimeset.DiffTimeWorkSetting;
+import nts.uk.ctx.at.shared.dom.worktime.difftimeset.DiffTimeWorkSettingRepository;
+import nts.uk.ctx.at.shared.dom.worktime.fixedset.FixedWorkSetting;
+import nts.uk.ctx.at.shared.dom.worktime.fixedset.FixedWorkSettingRepository;
+import nts.uk.ctx.at.shared.dom.worktime.flexset.FlexWorkSetting;
+import nts.uk.ctx.at.shared.dom.worktime.flexset.FlexWorkSettingRepository;
+import nts.uk.ctx.at.shared.dom.worktime.flowset.FlowWorkSetting;
+import nts.uk.ctx.at.shared.dom.worktime.flowset.FlowWorkSettingRepository;
+import nts.uk.ctx.at.shared.dom.worktime.worktimeset.WorkTimeSetting;
+import nts.uk.ctx.at.shared.dom.worktime.worktimeset.WorkTimeSettingRepository;
 
 /**
  * @author thanh_nx
@@ -20,14 +31,28 @@ import nts.uk.ctx.at.shared.dom.worktime.common.WorkTimezoneCommonSet;
  */
 @Stateless
 public class RoundAttendanceTime {
+	
+	@Inject
+	private FlowWorkSettingRepository flowWorkSettingRepo;
 
 	@Inject
-	private GetCommonSet getCommonSet;
+	private DiffTimeWorkSettingRepository diffTimeWorkSettingRepo;
+
+	@Inject
+	private FlexWorkSettingRepository flexWorkSettingRepo;
+
+	@Inject
+	private FixedWorkSettingRepository fixedWorkSettingRepo;
+
+	@Inject
+	private WorkTimeSettingRepository workTimeSettingRepo;
 
 	public int process(String companyId, int time, String workTimeCode, AttLeavAtr atr) {
 
 		// クラス「就業時間帯の共通設定」を確認する (Xác nhận class 「就業時間帯の共通設定」)
-		Optional<WorkTimezoneCommonSet> commonSet = Optional.empty();// getCommonSet.get(companyId, workTimeCode);
+		RequireImpl impl = new RequireImpl(flowWorkSettingRepo, diffTimeWorkSettingRepo, flexWorkSettingRepo,
+				fixedWorkSettingRepo, workTimeSettingRepo);
+		Optional<WorkTimezoneCommonSet> commonSet = GetCommonSet.workTimezoneCommonSet(impl, companyId, workTimeCode);
 
 		if (!commonSet.isPresent())
 			return time;
@@ -60,5 +85,45 @@ public class RoundAttendanceTime {
 			}
 		}
 		return result.multiply(new BigDecimal(roundingTimeUnit)).intValue();
+	}
+
+	@AllArgsConstructor
+	public class RequireImpl implements GetCommonSet.RequireM3 {
+
+		private final FlowWorkSettingRepository flowWorkSettingRepo;
+
+		private final DiffTimeWorkSettingRepository diffTimeWorkSettingRepo;
+
+		private final FlexWorkSettingRepository flexWorkSettingRepo;
+
+		private final FixedWorkSettingRepository fixedWorkSettingRepo;
+
+		private final WorkTimeSettingRepository workTimeSettingRepo;
+
+		@Override
+		public Optional<FlowWorkSetting> flowWorkSetting(String companyId, String workTimeCode) {
+			return flowWorkSettingRepo.find(companyId, workTimeCode);
+		}
+
+		@Override
+		public Optional<FlexWorkSetting> flexWorkSetting(String companyId, String workTimeCode) {
+			return flexWorkSettingRepo.find(companyId, workTimeCode);
+		}
+
+		@Override
+		public Optional<FixedWorkSetting> fixedWorkSetting(String companyId, String workTimeCode) {
+			return fixedWorkSettingRepo.findByKey(companyId, workTimeCode);
+		}
+
+		@Override
+		public Optional<DiffTimeWorkSetting> diffTimeWorkSetting(String companyId, String workTimeCode) {
+			return diffTimeWorkSettingRepo.find(companyId, workTimeCode);
+		}
+
+		@Override
+		public Optional<WorkTimeSetting> workTimeSetting(String companyId, String workTimeCode) {
+			return workTimeSettingRepo.findByCode(companyId, workTimeCode);
+		}
+
 	}
 }
