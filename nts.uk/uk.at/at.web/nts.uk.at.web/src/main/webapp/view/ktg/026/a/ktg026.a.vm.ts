05bb368abcac32b6c12822226b0ee24464a37078
nws-dungdv <dungdv@newwave.vn>
2020-09-11 15:24:57 +0700

    Update UI

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ktg/026/a/ktg026.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ktg/026/a/ktg026.a.vm.ts
@@ -9,14 +9,25 @@ module nts.uk.at.view.ktg029.a.viewmodel {
     year: KnockoutObservable<string> = ko.observable('');
     width: KnockoutObservable<number> = ko.observable(60);
     width2: KnockoutObservable<number> = ko.observable(70);
+    textA1_5: KnockoutComputed<string>;
     
     created() {
-
+      const vm = this;
+      vm.year = ko.observable('2020');
+      vm.textA1_5 = ko.computed(() => {
+        return vm.$i18n('KTG027_6', [vm.year(), '2回']);
+      });
     }
 
     mounted() {
-
+      const vm = this;
     }
+
+
+    // textA1_5(): string {
+    //   const vm = this;
+    //   return getText('KTG027_6', ['2020', 'abc']);
+    // }
   }
 }
 
