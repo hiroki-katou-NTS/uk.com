03f28283c4556066de9b40978509c2fd3c10fd9b
hieutt <hieutt@newwave.vn>
2020-09-11 11:15:48 +0700

    fake UI

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ktg/027/a/ktg027.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ktg/027/a/ktg027.a.vm.ts
@@ -17,8 +17,8 @@ module nts.uk.at.view.ktg027.a.viewmodel {
         displayEr: KnockoutObservable<boolean>;
         msg: KnockoutObservable<String>;
         year: KnockoutObservable<string> = ko.observable('');
-        width: KnockoutObservable<string> = ko.observable('60px');
-        width2: KnockoutObservable<string> = ko.observable('100px');
+        width: KnockoutObservable<string> = ko.observable('90px');
+        width2: KnockoutObservable<string> = ko.observable('30px');
         constructor() {
             var self = this;
             var today = moment();
