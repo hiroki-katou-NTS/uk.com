a93de77af3ed290daed6ff9554325813946410d9
chinh.hm <chinh.hm@3si.vn>
2020-09-15 13:56:21 +0700

    update Screen A case start CMM024

---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/adapter/personempbasic/PersonEmpBasicInfoDto.java
@@ -0,0 +1,35 @@
+package nts.uk.ctx.at.record.dom.adapter.personempbasic;
+
+import lombok.Getter;
+import lombok.Setter;
+import nts.arc.time.GeneralDate;
+
+import javax.ejb.Stateless;
+
+@Getter
+@Setter
+public class PersonEmpBasicInfoDto {
+    //個人ID
+    private String personId;
+
+    //社員ID
+    private String employeeId;
+
+    //ビジネスネーム
+    private String businessName;
+
+    //性別
+    private int gender;
+
+    //生年月日
+    private GeneralDate birthday;
+
+    //社員コード
+    private String employeeCode;
+
+    //入社年月日
+    private GeneralDate jobEntryDate;
+
+    //退職年月日
+    private GeneralDate retirementDate;
+}
