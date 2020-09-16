bbca7ceb05631510541bf94ada36b0f817a40bac
manhb <manhb@newwave.vn>
2020-09-16 17:35:50 +0700

    fix label bugs

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/007/a/ccg007.a.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/007/a/ccg007.a.vm.ts
@@ -5,10 +5,15 @@ module nts.uk.pr.view.ccg007.a {
         export class ScreenModel {
             contractCode: KnockoutObservable<string>;
             password: KnockoutObservable<string>;
+            bottomText1: KnockoutObservable<string>;
+            bottomText2: KnockoutObservable<string>;
             constructor() {
                 var self = this;
                 self.contractCode = ko.observable('');
                 self.password = ko.observable('');
+                let bottomLabels = nts.uk.resource.getText("CCG007_11").split('|');
+                self.bottomText1 = ko.observable(bottomLabels[0]);
+                self.bottomText2 = ko.observable(bottomLabels[1]);
             }
             start(): JQueryPromise<void> {
                 var self = this;
@@ -20,18 +25,18 @@ module nts.uk.pr.view.ccg007.a {
                 var self = this;
                 blockUI.invisible();
                 if (!nts.uk.ui.errors.hasError()) {
-                    service.submitForm({ contractCode: _.escape(self.contractCode()), password: _.escape(self.password()) }).done(function() {
+                    service.submitForm({ contractCode: _.escape(self.contractCode()), password: _.escape(self.password()) }).done(function () {
                         nts.uk.characteristics.remove("contractInfo");
                         nts.uk.characteristics.save("contractInfo", { contractCode: _.escape(self.contractCode()), contractPassword: _.escape(self.password()) });
                         nts.uk.ui.windows.setShared('contractCode', _.escape(self.contractCode()));
                         nts.uk.ui.windows.setShared('contractPassword', _.escape(self.password()));
                         nts.uk.ui.windows.setShared('isSubmit', true);
                         nts.uk.ui.windows.close();
-                    }).fail(function(res:any) {
+                    }).fail(function (res: any) {
                         nts.uk.ui.dialog.alertError({ messageId: res.messageId, messageParams: res.parameterIds });
-                        }).always(() => {
-                            blockUI.clear();
-                        });
+                    }).always(() => {
+                        blockUI.clear();
+                    });
                 }
             }
         }
