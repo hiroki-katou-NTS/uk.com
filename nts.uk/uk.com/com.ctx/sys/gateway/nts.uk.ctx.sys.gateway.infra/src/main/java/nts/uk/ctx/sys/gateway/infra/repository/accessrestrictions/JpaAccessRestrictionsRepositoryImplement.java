244bb65c995b18b2447f2da69a59111dbba84577
NSVN-PhamVanThanh <thanh_pv@nittsusystem-vn.com>
2020-09-18 18:11:34 +0700

    Update JpaAccessRestrictionsRepositoryImplement.java

--- nts.uk/uk.com/com.ctx/sys/gateway/nts.uk.ctx.sys.gateway.infra/src/main/java/nts/uk/ctx/sys/gateway/infra/repository/accessrestrictions/JpaAccessRestrictionsRepositoryImplement.java
+++ nts.uk/uk.com/com.ctx/sys/gateway/nts.uk.ctx.sys.gateway.infra/src/main/java/nts/uk/ctx/sys/gateway/infra/repository/accessrestrictions/JpaAccessRestrictionsRepositoryImplement.java
@@ -2,12 +2,15 @@ package nts.uk.ctx.sys.gateway.infra.repository.accessrestrictions;
 
 import java.util.Optional;
 
+import javax.ejb.Stateless;
+
 import nts.arc.layer.infra.data.JpaRepository;
 import nts.uk.ctx.sys.gateway.dom.accessrestrictions.AccessRestrictions;
 import nts.uk.ctx.sys.gateway.dom.accessrestrictions.AccessRestrictionsRepository;
 import nts.uk.ctx.sys.gateway.dom.login.ContractCode;
 import nts.uk.ctx.sys.gateway.infra.entity.accessrestrictions.SgwmtAccess;
 
+@Stateless
 public class JpaAccessRestrictionsRepositoryImplement extends JpaRepository implements AccessRestrictionsRepository {
 
 	@Override
