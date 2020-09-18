4db5ca1bceebcf04a0993bf5eddce912c2c14d85
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2020-09-18 08:40:57 +0700

    Switch

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksu/002/component/title-date.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksu/002/component/title-date.ts
@@ -6,12 +6,38 @@ module nts.uk.ui.at.ksu002.a {
 		<div class="title-date">
 			<div class="backgroud" >
 				<div class="content">
-					<div data-bind="ntsDatePicker: { value: yearMonth, dateFormat: 'yearmonth' , valueFormat: 'YYYYMM',  fiscalMonthsMode: true, defaultClass: 'round-orange' }">
-					</div>
-					<span class="label-1" class="label" data-bind="i18n: 'KSU002_23'"></span>
-					<span class="label-2" class="label" data-bind="i18n: 'KSU002_7'"></span>
-					<div>Chung dep trai
-					</div>
+					<table>
+						<tbody>
+							<tr>
+								<td>
+									<div data-bind="ntsDatePicker: { 
+										value: yearMonth,
+										dateFormat: 'yearmonth' ,
+									 	valueFormat: 'YYYYMM',
+								  		fiscalMonthsMode: true,
+						 				defaultClass: 'round-orange',
+										showJumpButtons: true  }">
+									</div>
+									<span class="label-1" class="label" data-bind="i18n: 'KSU002_23'"></span>
+									<span class="label-2" class="label" data-bind="i18n: 'KSU002_7'"></span>
+								</td>
+								<td>
+									<div data-bind="ntsDatePicker: { value: yearMonth, dateFormat: 'yearmonth' , valueFormat: 'YYYYMM',  fiscalMonthsMode: true, defaultClass: 'round-orange' }">
+									</div>
+									<span class="label-1" class="label" data-bind="i18n: 'KSU002_6'"></span>
+									<span class="label-2" class="label" data-bind="i18n: 'KSU002_7'"></span>
+								</td>
+								<td>
+									<div class="cf" data-bind="ntsSwitchButton: {
+										$i18n: 'KSU002_6',
+										options: roundingRules,
+										optionsValue: 'code',
+										optionsText: 'name',
+										value: selectedRuleCode }"></div>
+								</td>
+							</tr>
+						</tbody>
+					</table>
 				</div>
 			</div>
 		</div>
@@ -21,7 +47,7 @@ module nts.uk.ui.at.ksu002.a {
 				background: #edfac2;
 				padding: 6px;
 				margin: 10px;
-				width: 500px;
+				width: 615px;
 			}
 			
 			.title-date .backgroud .content .label-1 {
@@ -30,6 +56,7 @@ module nts.uk.ui.at.ksu002.a {
 			
 			.title-date .backgroud .content .label-2 {
 				margin-left: -6px;
+				margin-right: 12px;
 			}
 			
         </style>
@@ -46,14 +73,36 @@ module nts.uk.ui.at.ksu002.a {
 	})
 	export class TitleDateComponent extends ko.ViewModel {
 
-		public yearMonth: KnockoutObservable<number> = ko.observable(-1);
+		public yearMonth: KnockoutObservable<number> = ko.observable(1);
+		public selectedRuleCode: KnockoutObservable<number> = ko.observable(-1);
+		roundingRules: KnockoutObservableArray<any>;
 
 		created(params: Params) {
 			const vm = this;
+
+			vm.roundingRules = ko.observableArray([
+				{ code: 1, name: vm.$i18n('KSU002_8') },
+				{ code: 2, name: vm.$i18n('KSU002_9') }
+			]);
 			
+			vm.selectedRuleCode(1);
 		}
 
 		mounted() {
 		}
 	}
 }
+
+class SwitchType {
+	code: number;
+	name: string;
+
+	constructor(code: number, name: string) {
+		const mock = new ko.ViewModel();
+
+		if (name) {
+			this.name = mock.$i18n(name);
+		}
+	}
+}
+
