dae00e268899bbc08492ae659ec3a459cb095670
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2020-09-21 09:29:52 +0700

    update template

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksu/002/a/ksu.002a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksu/002/a/ksu.002a.vm.ts
@@ -1,14 +1,16 @@
 /// <reference path="../../../../lib/nittsu/viewcontext.d.ts" />
 
 module nts.uk.ui.at.ksu002.a {
-    @bean()
-    export class ViewModel extends ko.ViewModel {
-        created() {
+	@bean()
+	export class ViewModel extends ko.ViewModel {
+		baseDate = ko.observable({ begin: new Date(2020, 8, 14), finish: new Date(2020, 9, 4) });
+		
+		created() {
 
-        }
+		}
 
-        mounted() {
+		mounted() {
 
-        }
-    }       
+		}
+	}
 }
--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksu/002/component/title-date.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksu/002/component/title-date.ts
@@ -3,44 +3,33 @@
 module nts.uk.ui.at.ksu002.a {
 
 	const template = `
-		<div class="title-date">
-			<div class="backgroud" >
-				<div class="content">
-					<table>
-						<tbody>
-							<tr>
-								<td>
-									<div data-bind="ntsDatePicker: { 
-										value: yearMonth,
-										dateFormat: 'yearmonth' ,
-									 	valueFormat: 'YYYYMM',
-								  		fiscalMonthsMode: true,
-						 				defaultClass: 'round-orange',
-										showJumpButtons: true  }">
-									</div>
-									<span class="label-1" class="label" data-bind="i18n: 'KSU002_23'"></span>
-									<span class="label-2" class="label" data-bind="i18n: 'KSU002_7'"></span>
-								</td>
-								<td>
-									<div data-bind="ntsDatePicker: { value: yearMonth, dateFormat: 'yearmonth' , valueFormat: 'YYYYMM',  fiscalMonthsMode: true, defaultClass: 'round-orange' }">
-									</div>
-									<span class="label-1" class="label" data-bind="i18n: 'KSU002_6'"></span>
-									<span class="label-2" class="label" data-bind="i18n: 'KSU002_7'"></span>
-								</td>
-								<td>
-									<div class="cf" data-bind="ntsSwitchButton: {
-										$i18n: 'KSU002_6',
-										options: roundingRules,
-										optionsValue: 'code',
-										optionsText: 'name',
-										value: selectedRuleCode }"></div>
-								</td>
-							</tr>
-						</tbody>
-					</table>
+		<div class="backgroud">
+			<div class="content">
+				<div data-bind="ntsDatePicker: { 
+					value: yearMonth,
+					dateFormat: 'yearmonth' ,
+				 	valueFormat: 'YYYYMM',
+			  		fiscalMonthsMode: true,
+	 				defaultClass: 'round-orange',
+					showJumpButtons: true  }">
 				</div>
+				
+				<span class="label-1" class="label" data-bind="i18n: 'KSU002_23'"></span>
+				<span class="label-2" class="label" data-bind="i18n: 'KSU002_7'"></span>
+				
+								<div data-bind="ntsDatePicker: { value: yearMonth, dateFormat: 'yearmonth' , valueFormat: 'YYYYMM',  fiscalMonthsMode: true, defaultClass: 'round-orange' }">
+								</div>
+								<span class="label-1" class="label" data-bind="i18n: 'KSU002_6'"></span>
+								<span class="label-2" class="label" data-bind="i18n: 'KSU002_7'"></span>
+								
+								<div class="cf" data-bind="ntsSwitchButton: {
+									$i18n: 'KSU002_6',
+									options: roundingRules,
+									optionsValue: 'code',
+									optionsText: 'name',
+									value: selectedRuleCode }"></div>
 			</div>
-		</div>
+		</div> 
 		<style type="text/css" rel="stylesheet">
             .title-date .backgroud {
 				border: 1px solid #cccccc;
@@ -66,9 +55,31 @@ module nts.uk.ui.at.ksu002.a {
 	interface Params {
 
 	}
+	
+	const COMPONENT_NAME = 'title-date';
+	
+    @handler({
+        bindingName: COMPONENT_NAME,
+        validatable: true,
+        virtual: false
+    })
+    export class TitleDateComponentBindingHandler implements KnockoutBindingHandler {
+        init(element: any, valueAccessor: () => any, allBindingsAccessor: KnockoutAllBindingsAccessor, viewModel: any, bindingContext: KnockoutBindingContext): void | { controlsDescendantBindings: boolean; } {
+            const name = COMPONENT_NAME;
+            const baseDate = valueAccessor();
+            const mode = allBindingsAccessor.get('mode');
+
+            element.classList.add('cf');
+            element.classList.add('title-date');
+
+            ko.applyBindingsToNode(element, { component: { name: name, params: { mode: mode, baseDate: baseDate } } }, bindingContext);
+
+            return { controlsDescendantBindings: true };
+        }
+    }
 
 	@component({
-		name: 'title-date',
+		name: COMPONENT_NAME,
 		template
 	})
 	export class TitleDateComponent extends ko.ViewModel {
