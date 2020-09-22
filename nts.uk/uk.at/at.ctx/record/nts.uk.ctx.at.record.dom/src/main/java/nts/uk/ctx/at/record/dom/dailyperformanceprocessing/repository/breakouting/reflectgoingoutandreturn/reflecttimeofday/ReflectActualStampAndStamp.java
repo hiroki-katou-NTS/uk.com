3a70382184a203ecb122e31fe0aff6c59abe448c
NSVN-TrieuKhacTu <tu_tk@nittsusystem-vn.com>
2020-09-22 09:59:02 +0700

    move class to package scherec ver 1.1

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/dailyperformanceprocessing/repository/breakouting/reflectgoingoutandreturn/reflecttimeofday/ReflectActualStampAndStamp.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/dailyperformanceprocessing/repository/breakouting/reflectgoingoutandreturn/reflecttimeofday/ReflectActualStampAndStamp.java
@@ -9,9 +9,9 @@ import javax.inject.Inject;
 
 import nts.arc.time.GeneralDate;
 import nts.uk.ctx.at.record.dom.dailyperformanceprocessing.repository.breakouting.reflectgoingoutandreturn.TimeFrame;
-import nts.uk.ctx.at.record.dom.dailyperformanceprocessing.repository.reflectattdclock.AttendanceAtr;
 import nts.uk.ctx.at.record.dom.require.RecordDomRequireService;
 import nts.uk.ctx.at.record.dom.workrecord.stampmanagement.stamp.Stamp;
+import nts.uk.ctx.at.record.dom.workrecord.stampmanagement.timestampsetting.prefortimestaminput.ChangeClockArt;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.common.TimeActualStamp;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.common.timestamp.EngravingMethod;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.common.timestamp.ReasonTimeChange;
@@ -64,14 +64,15 @@ public class ReflectActualStampAndStamp {
 			TimeWithDayAttr timeWithDayAttr = TimeWithDayAttr.convertToTimeWithDayAttr(ymd,
 					stamp.getStampDateTime().toDate(), stamp.getStampDateTime().clockHourMinute().v());
 			//時刻を丸める
-			if(timeActualStamp.getActualStamp().isPresent() && workTimeCode !=null ) {
-				this.roundStamp(workTimeCode.v(), timeWithDayAttr, AttendanceAtr.TEMPORARY);
+			TimeWithDayAttr afterRouding = timeWithDayAttr;
+			if(workTimeCode !=null ) {
+				afterRouding = this.roundStamp(workTimeCode.v(), timeWithDayAttr,stamp.getType().getChangeClockArt());
 			}
 			//実打刻コピーする
 			TimeActualStamp timeStampCopy = new TimeActualStamp(timeActualStamp.getActualStamp(), timeActualStamp.getStamp(), timeActualStamp.getNumberOfReflectionStamp(), timeActualStamp.getOvertimeDeclaration(), timeActualStamp.getTimeVacation());
 			//打刻方法を打刻元情報に変換する
 			WorkTimeInformation timeDay = new WorkTimeInformation(new ReasonTimeChange(TimeChangeMeans.REAL_STAMP, EngravingMethod.TIME_RECORD_ID_INPUT), timeWithDayAttr);
-			WorkStamp workStamp = new WorkStamp(timeWithDayAttr, timeDay, stamp.getRefActualResults().getWorkLocationCD());
+			WorkStamp workStamp = new WorkStamp(afterRouding, timeDay, stamp.getRefActualResults().getWorkLocationCD());
 			timeStampCopy.setActualStamp(Optional.of(workStamp));
 			//時間帯枠（Temporary）。理由←外出理由
 			timeFrame.setGoOutReason(stamp.getType().getGoOutArt());
@@ -98,12 +99,12 @@ public class ReflectActualStampAndStamp {
 	 * @param attendanceAtr
 	 * @param actualStampAtr
 	 */
-	public void roundStamp(String workTimeCode, TimeWithDayAttr timeWithDayAttr, AttendanceAtr attendanceAtr) {
+	public TimeWithDayAttr roundStamp(String workTimeCode, TimeWithDayAttr timeWithDayAttr, ChangeClockArt changeClockArt) {
 		String companyId = AppContexts.user().companyId();
 		// ドメインモデル「丸め設定」を取得する (Lấy 「丸め設定」)
 		RoundingSet roudingTime = workTimeCode != null
 				? this.getRoudingTime(companyId, workTimeCode,
-						attendanceAtr == AttendanceAtr.LEAVING_WORK ? Superiority.OFFICE_WORK : Superiority.ATTENDANCE)
+						changeClockArt == ChangeClockArt.GO_OUT ? Superiority.GO_OUT : Superiority.TURN_BACK)
 				: null;
 
 		InstantRounding instantRounding = null;
@@ -129,8 +130,9 @@ public class ReflectActualStampAndStamp {
 				timeChange = (modTimeOfDay == 0) ? numberMinuteTimeOfDay
 						: numberMinuteTimeOfDay - modTimeOfDay + blockTime;
 			}
-			timeWithDayAttr = new TimeWithDayAttr(timeChange);
+			return new TimeWithDayAttr(timeChange);
 		} // end : nếu time khác giá trị default
+		return timeWithDayAttr;
 	}
 	
 	private RoundingSet getRoudingTime(String companyId, String workTimeCode, Superiority superiority) {
