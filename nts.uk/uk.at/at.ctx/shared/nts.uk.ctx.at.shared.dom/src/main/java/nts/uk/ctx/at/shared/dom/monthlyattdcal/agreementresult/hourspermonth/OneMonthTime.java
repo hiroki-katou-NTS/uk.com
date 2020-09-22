b3d00ca31f445d4682fab3a08bb4ad7a5286c21d
quang.nh1 <huyquang.nhq@gmail.com>
2020-09-22 17:03:31 +0700

    #30160 update method 1
    (cherry picked from commit 9906d9a6047bf0b0a1c12de2640663cc2c08c00a)

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/monthlyattdcal/agreementresult/hourspermonth/OneMonthTime.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/monthlyattdcal/agreementresult/hourspermonth/OneMonthTime.java
@@ -3,14 +3,17 @@ package nts.uk.ctx.at.shared.dom.monthlyattdcal.agreementresult.hourspermonth;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import nts.arc.enums.EnumAdaptor;
 import nts.arc.error.BusinessException;
+import nts.uk.ctx.at.shared.dom.common.time.AttendanceTimeMonth;
 import nts.uk.ctx.at.shared.dom.monthlyattdcal.agreementresult.AgreementOneMonthTime;
+import nts.uk.ctx.at.shared.dom.monthlyattdcal.agreementresult.OverState;
 import org.apache.commons.lang3.tuple.Pair;
 import org.eclipse.persistence.internal.xr.ValueObject;
 
 /**
  * @author quang.nh1
- * 	１ヶ月時間
+ * １ヶ月時間
  */
 @Getter
 @AllArgsConstructor
@@ -37,10 +40,25 @@ public class OneMonthTime extends ValueObject {
         return new OneMonthTime(errorTimeInMonth, upperLimitTime);
     }
 
-    //todo tobe continue [1] エラーチェック
+    /**
+     * [1] エラーチェック
+     */
+    public OverState errorCheck(AttendanceTimeMonth applicationTime) {
+        int value;
+        if (applicationTime.lessThanOrEqualTo(errorTimeInMonth.getAlarmTime().v())) {
+            value = 0;
+        } else if (applicationTime.lessThanOrEqualTo(errorTimeInMonth.getErrorTime().v())) {
+            value = 1;
+        } else if (applicationTime.lessThanOrEqualTo(upperLimitTime.v())) {
+            value = 2;
+        } else {
+            value = 3;
+        }
+        return EnumAdaptor.valueOf(value, OverState.class);
+    }
 
     /**
-     *[2] エラー時間を超えているか
+     * [2] エラー時間を超えているか
      */
     public Pair<Boolean, AgreementOneMonthTime> checkErrorTimeExceeded(AgreementOneMonthTime applicationTime) {
         Pair<Boolean, AgreementOneMonthTime> reusult = errorTimeInMonth.checkErrorTimeExceeded(applicationTime);
@@ -48,7 +66,7 @@ public class OneMonthTime extends ValueObject {
     }
 
     /**
-     *[3] アラーム時間を計算する
+     * [3] アラーム時間を計算する
      */
     public AgreementOneMonthTime calculateAlarmTime(AgreementOneMonthTime applicationTime) {
         return errorTimeInMonth.calculateAlarmTime(applicationTime);
