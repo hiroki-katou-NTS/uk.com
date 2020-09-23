807ef44b1477d8ba49ca9e214936875119bca57d
khaidh <khai.dh@3si.vn>
2020-09-23 18:22:27 +0700

    #30162
    (cherry picked from commit 8d565c7a5d291c805280b5ae3ba1ea35692a5ed7)

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/monthly/agreement/approver/AnnualAppContent.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/monthly/agreement/approver/AnnualAppContent.java
@@ -3,9 +3,8 @@ package nts.uk.ctx.at.record.dom.monthly.agreement.approver;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import nts.uk.ctx.at.record.dom.monthly.agreement.monthlyresult.specialprovision.SpecialProvisionsOfAgreement;
+import nts.uk.ctx.at.record.dom.monthly.agreement.monthlyresult.specialprovision.ReasonsForAgreement;
 import nts.uk.ctx.at.shared.dom.common.Year;
-import nts.uk.ctx.at.shared.dom.monthlyattdcal.agreementresult.AgreementOneMonthTime;
 import nts.uk.ctx.at.shared.dom.monthlyattdcal.agreementresult.AgreementOneYearTime;
 import org.eclipse.persistence.internal.xr.ValueObject;
 
@@ -20,7 +19,7 @@ public class AnnualAppContent extends ValueObject {
 	// 対象者
 	private String applicant;
 
-	// 年月度
+	// 年度
 	private Year year;
 
 	// エラー時間
@@ -31,5 +30,5 @@ public class AnnualAppContent extends ValueObject {
 	private AgreementOneYearTime alarmTime;
 
 	// 申請理由 : 36協定申請理由
-	private SpecialProvisionsOfAgreement reason;
+	private ReasonsForAgreement reason;
 }
--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/monthly/agreement/approver/AnnualAppCreate.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/monthly/agreement/approver/AnnualAppCreate.java
@@ -3,13 +3,13 @@ package nts.uk.ctx.at.record.dom.monthly.agreement.approver;
 import lombok.val;
 import nts.arc.task.tran.AtomTask;
 import nts.arc.time.GeneralDate;
+import nts.uk.ctx.at.record.dom.monthly.agreement.monthlyresult.specialprovision.*;
 import nts.uk.ctx.at.record.dom.monthly.agreement.monthlyresult.specialprovision.ScreenDisplayInfo;
-import nts.uk.ctx.at.record.dom.monthly.agreement.monthlyresult.specialprovision.SpecialProvisionsOfAgreement;
 import nts.uk.ctx.at.record.dom.standardtime.repository.AgreementDomainService;
+import nts.uk.ctx.at.shared.dom.monthlyattdcal.agreementresult.hoursperyear.ErrorTimeInYear;
 import nts.uk.ctx.at.shared.dom.workingcondition.WorkingSystem;
 
 import javax.ejb.Stateless;
-import javax.inject.Inject;
 import java.util.List;
 import java.util.Optional;
 
@@ -76,9 +76,9 @@ public class AnnualAppCreate {
 
 		// 申請内容.アラーム時間
 		annualAppContent.setAlarmTime(oneYear.getBasicSetting().calculateAlarmTime(annualAppContent.getErrTime()));
-//
+
 		// $申請
-		val app = createAnnualApp(
+		val app = this.createAnnualApp(
 				empId,
 				annualAppContent,
 				optApproverItem.get().getApproverList(),
@@ -102,33 +102,43 @@ public class AnnualAppCreate {
 	 * [prv-1] 年間の申請を作成する
 	 * 年間の36協定特別条項の適用申請を作成する
 	 *
-	 * @param empId             社員ID
-	 * @param annualAppContent  年間の申請内容
+	 * @param applicantId       申請者
+	 * @param annualAppContent  年間の 申請内容
 	 * @param approverList      承認者リスト
 	 * @param confirmerList     確認者リスト
 	 * @param screenDisplayInfo 画面表示情報
 	 * @return 36協定特別条項の適用申請
 	 */
-	private SpecialProvisionsOfAgreement createAnnualApp(String empId,
-														 AnnualAppContent annualAppContent,
-														 List<String> approverList,
-														 List<String> confirmerList,
-														 ScreenDisplayInfo screenDisplayInfo) {
+	private SpecialProvisionsOfAgreement createAnnualApp(
+			String applicantId,
+			AnnualAppContent annualAppContent,
+			List<String> approverList,
+			List<String> confirmerList,
+			ScreenDisplayInfo screenDisplayInfo) {
 
-//		$エラーアラーム = 1年間のエラーアラーム時間#1年間のエラーアラーム時間(申請内容.エラー時間
-//				,申請内容.アラーム時間)
+		// $エラーアラーム
+		val errorArlarmTime = new ErrorTimeInYear(annualAppContent.getErrTime(), annualAppContent.getAlarmTime());
 
-//
-//		$１年間時間 = １年間時間#１年間時間(申請内容.年度,$エラーアラーム)
-//
-//		$申請時間 = 申請時間#申請時間(３６協定申請種類.1年間,Optional.empty,$１年間時間)
-//
-//		return 36協定特別条項の適用申請#新規申請作成(申請者,申請内容.対象者,$申請時間,申請内容.申請理由
-//				,承認者リスト,確認者リスト,画面表示情報)
+		// $１年間時間
+		val oneYearTime = new OneYearTime(errorArlarmTime, annualAppContent.getYear());
 
-		return null;
+		// $申請時間
+		val appTime = new ApplicationTime(
+				TypeAgreementApplication.ONE_YEAR,
+				Optional.empty(),
+				Optional.of(oneYearTime)
+		);
+
+		return SpecialProvisionsOfAgreement.create(
+				applicantId,
+				annualAppContent.getApplicant(),
+				appTime,
+				annualAppContent.getReason(),
+				approverList,
+				confirmerList,
+				screenDisplayInfo
+		);
 	}
-
 
 	public interface Require extends GettingApproverDomainService.Require, AgreementDomainService.RequireM3 {
 		/**
