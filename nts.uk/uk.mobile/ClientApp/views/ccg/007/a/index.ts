73480f4b28fbc5434d4cb96596fabb921070c085
nws-dungdv <dungdv@newwave.vn>
2020-09-23 18:59:14 +0700

    fix bug

--- nts.uk/uk.mobile/ClientApp/views/ccg/007/a/index.ts
+++ nts.uk/uk.mobile/ClientApp/views/ccg/007/a/index.ts
@@ -1,7 +1,6 @@
 import { Vue } from '@app/provider';
 import { component, Prop } from '@app/core/component';
 import { storage, auth } from '@app/utils';
-import { NavMenu, SideMenu } from '@app/services';
 
 @component({
     route: '/ccg/007/a',
@@ -33,6 +32,12 @@ export class Ccg007AComponent extends Vue {
         const vm = this;
         vm.$nextTick(() => {
             vm.$parent.$el.querySelector('.container-fluid').classList.add('uk-bg-process');
+            const contract = storage.local.getItem('contract') as any;
+            if (!!contract && contract.code !== null && contract.password !== null) {
+                vm.model.contractCode = contract.code;
+                vm.model.password = contract.password;
+                vm.$goto('ccg007b');
+            }
         });
     }
 
