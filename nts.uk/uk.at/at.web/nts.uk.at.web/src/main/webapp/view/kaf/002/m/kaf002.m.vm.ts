469efa94a3b809f595d33321c81b24a99d0b851c
NSVN-NguyenDinhHoang <61735854+NSVN-NguyenDinhHoang@users.noreply.github.com>
2020-09-23 20:46:54 +0700

    Refactor4: resize KAF002 M

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/002/m/kaf002.m.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/002/m/kaf002.m.vm.ts
@@ -391,7 +391,7 @@ module nts.uk.at.view.kaf002_ref.m.viewmodel {
             });
 
             let optionGrid = {
-                    width: (((!self.isVisibleComlumn && !ko.toJS(self.isPreAtr)) || ko.toJS(self.isPreAtr))) ? '420px' : '517px',
+                    width: (((!self.isVisibleComlumn && !ko.toJS(self.isPreAtr)) || ko.toJS(self.isPreAtr))) ? '430px' : '527px',
                     height: isChrome ? '330px' : (ko.toJS(self.isPreAtr) ? '300px' : '320px'),
                     dataSource: dataSource,
                     primaryKey: 'id',
@@ -448,7 +448,7 @@ module nts.uk.at.view.kaf002_ref.m.viewmodel {
                                new ItemModel('2', '有償'),
                                new ItemModel('3', '組合')];
             let option2 = {
-              width: (((!self.isVisibleComlumn && !ko.toJS(self.isPreAtr)) || ko.toJS(self.isPreAtr))) ? '555px' : '655px',
+              width: (((!self.isVisibleComlumn && !ko.toJS(self.isPreAtr)) || ko.toJS(self.isPreAtr))) ? '565px' : '665px',
               height: isChrome ? '330px' : (ko.toJS(self.isPreAtr) ? '300px' : '320px'),
               dataSource: dataSource,
               primaryKey: 'id',
