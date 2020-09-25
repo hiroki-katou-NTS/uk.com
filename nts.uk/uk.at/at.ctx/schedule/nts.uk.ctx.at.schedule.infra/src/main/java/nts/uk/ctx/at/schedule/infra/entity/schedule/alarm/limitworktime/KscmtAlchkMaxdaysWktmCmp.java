3d21e6072b61d17b2d07aceae8e2fb59ec9c9b21
NTS-PhamVanDan <dan_pv@nittsusystem-vn.com>
2020-09-25 18:19:12 +0900

    add entities

---
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.infra/src/main/java/nts/uk/ctx/at/schedule/infra/entity/schedule/alarm/limitworktime/KscmtAlchkMaxdaysWktmCmp.java
@@ -0,0 +1,62 @@
+package nts.uk.ctx.at.schedule.infra.entity.schedule.alarm.limitworktime;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import nts.arc.layer.infra.data.jdbc.NtsResultSet.NtsResultRecord;
+import nts.arc.layer.infra.data.jdbc.map.JpaEntityMapper;
+import nts.uk.ctx.at.schedule.dom.schedule.alarm.continuouswork.limitworktime.MaxDay;
+import nts.uk.ctx.at.schedule.dom.schedule.alarm.continuouswork.limitworktime.MaxDayOfWorkTime;
+import nts.uk.ctx.at.schedule.dom.schedule.alarm.continuouswork.limitworktime.MaxDayOfWorkTimeCompany;
+import nts.uk.ctx.at.schedule.dom.schedule.alarm.continuouswork.limitworktime.WorkTimeMaximumCode;
+import nts.uk.ctx.at.schedule.dom.schedule.alarm.continuouswork.limitworktime.WorkTimeMaximumName;
+import nts.uk.shr.infra.data.entity.ContractUkJpaEntity;
+
+@Entity
+@Table(name = "KSCMT_ALCHK_MAXDAYS_WKTM_CMP")
+@AllArgsConstructor
+public class KscmtAlchkMaxdaysWktmCmp extends ContractUkJpaEntity {
+	
+	public static final Function<NtsResultRecord, KscmtAlchkMaxdaysWktmCmp> mapper = 
+			s -> new JpaEntityMapper<>(KscmtAlchkMaxdaysWktmCmp.class).toEntity(s);
+	
+	@EmbeddedId
+	public KscmtAlchkMaxdaysWktmCmpPk pk;
+	
+	@Column(name = "NAME")
+	public String name;
+	
+	@Column(name = "MAX_DAYS")
+	public int maxDays;
+	
+	@Override
+	protected Object getKey() {
+		return pk;
+	}
+	
+	public static KscmtAlchkMaxdaysWktmCmp fromDomain(String companyId, MaxDayOfWorkTimeCompany domain) {
+		
+		return new KscmtAlchkMaxdaysWktmCmp(
+				new KscmtAlchkMaxdaysWktmCmpPk( companyId, domain.getCode().v()), 
+				domain.getName().v(), 
+				domain.getMaxDayOfWorkTime().getMaxDay().v());
+	}
+	
+	public MaxDayOfWorkTimeCompany toDomain(List<KscmtAlchkMaxdaysWktmCmpDtl> dtlList) {
+		
+		return new MaxDayOfWorkTimeCompany(
+				new WorkTimeMaximumCode(this.pk.code), 
+				new WorkTimeMaximumName(this.name), 
+				new MaxDayOfWorkTime(
+						dtlList.stream().map( dtl -> dtl.toWorkTimeCode()).collect(Collectors.toList()), 
+						new MaxDay(this.maxDays)));
+	}
+
+}
