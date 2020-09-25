73379505d842f89299f8a72160d9b7d16cb77ef7
leulan <lan-lt@nittsusystem-vn.com>
2020-09-25 15:39:19 +0900

    [同時出勤指定]のUnitを追加しました。

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/test/java/nts/uk/ctx/at/schedule/dom/schedule/alarm/simultaneousattendance/designation/SimultaneousAttendanceDesignationTest.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/test/java/nts/uk/ctx/at/schedule/dom/schedule/alarm/simultaneousattendance/designation/SimultaneousAttendanceDesignationTest.java
@@ -1,7 +1,9 @@
 package nts.uk.ctx.at.schedule.dom.schedule.alarm.simultaneousattendance.designation;
 
-import java.util.ArrayList;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.Arrays;
+import java.util.Collections;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,11 +20,11 @@ public class SimultaneousAttendanceDesignationTest {
 		val simultaneousAttendanceDesignation = SimultaneousAttendanceDesignation.create(
 				"517ef7f8-77d0-4eb0-b539-05e03a23f9e0",
 				Arrays.asList(
-						"517ef7f8-77d0-4eb0-b539-05e03a23f9e1", 
-						"517ef7f8-77d0-4eb0-b539-05e03a23f9e2",
-						"517ef7f8-77d0-4eb0-b539-05e03a23f9e3", 
-						"517ef7f8-77d0-4eb0-b539-05e03a23f9e4",
-						"517ef7f8-77d0-4eb0-b539-05e03a23f9e5"));
+						"EMPLOYEE_1", 
+						"EMPLOYEE_2",
+						"EMPLOYEE_3", 
+						"EMPLOYEE_4",
+						"EMPLOYEE_5"));
 		
 		NtsAssert.invokeGetters(simultaneousAttendanceDesignation);
 		
@@ -36,7 +38,7 @@ public class SimultaneousAttendanceDesignationTest {
 	public void check_inv1_emptyList() {
 		NtsAssert.businessException("Msg_1881", ()-> {
 			SimultaneousAttendanceDesignation.create(
-					"517ef7f8-77d0-4eb0-b539-05e03a23f9e0", new ArrayList<>());
+					"EMPLOYEE_1", Collections.emptyList());
 		});
 	}
 	
@@ -47,23 +49,91 @@ public class SimultaneousAttendanceDesignationTest {
 	 */
 	@Test
 	public void check_inv1_sizeAfter10() {
+		
+		/** ケース: 同時に出勤すべき社員の候補のsize <= 10 -> success*/
+		SimultaneousAttendanceDesignation.create(
+				"EMPLOYEE_0",
+				Arrays.asList(
+						"EMPLOYEE_1", 
+						"EMPLOYEE_2",
+						"EMPLOYEE_3", 
+						"EMPLOYEE_4",
+						"EMPLOYEE_5",
+						"EMPLOYEE_6", 
+						"EMPLOYEE_7",
+						"EMPLOYEE_8",
+						"EMPLOYEE_9",
+						"EMPLOYEE_10"
+						));
+		
+		/** ケース: 同時に出勤すべき社員の候補のsize > 10 -> Msg_1881*/
 		NtsAssert.businessException("Msg_1881", ()-> {
 			SimultaneousAttendanceDesignation.create(
 					"517ef7f8-77d0-4eb0-b539-05e03a23f9e0",
 					Arrays.asList(
-							"517ef7f8-77d0-4eb0-b539-05e03a23f9e1", 
-							"517ef7f8-77d0-4eb0-b539-05e03a23f9e2",
-							"517ef7f8-77d0-4eb0-b539-05e03a23f9e3", 
-							"517ef7f8-77d0-4eb0-b539-05e03a23f9e4",
-							"517ef7f8-77d0-4eb0-b539-05e03a23f9e5",
-							"517ef7f8-77d0-4eb0-b539-05e03a23f9e6", 
-							"517ef7f8-77d0-4eb0-b539-05e03a23f9e7",
-							"517ef7f8-77d0-4eb0-b539-05e03a23f9e8",
-							"517ef7f8-77d0-4eb0-b539-05e03a23f9e9",
-							"517ef7f8-77d0-4eb0-b539-05e03a23f910",
-							"517ef7f8-77d0-4eb0-b539-05e03a23f911"
+							"EMPLOYEE_1", 
+							"EMPLOYEE_2",
+							"EMPLOYEE_3", 
+							"EMPLOYEE_4",
+							"EMPLOYEE_5",
+							"EMPLOYEE_6", 
+							"EMPLOYEE_7",
+							"EMPLOYEE_8",
+							"EMPLOYEE_9",
+							"EMPLOYEE_10",
+							"EMPLOYEE_11"
 							));
 		});
+	}
+	
+	/**
+	 * inv-2 not 同時に出勤すべき社員の候補.contains( 社員ID )																	
+	 * ケース: 同時に出勤すべき社員の候補の中に社員IDがいる -> Msg_1882
+	 */
+	@Test
+	public void check_inv2_checkEmpMustWorkTogetherLstContainsEmployeeId() {
+		
+		/** ケース: 同時に出勤すべき社員の候補のsize <= 10 -> success*/
+		SimultaneousAttendanceDesignation.create(
+				"EMPLOYEE_1",
+				Arrays.asList(
+						"EMPLOYEE_2", 
+						"EMPLOYEE_3",
+						"EMPLOYEE_4"
+						));
+		
+		/** ケース: 同時に出勤すべき社員の候補.contains( 社員ID ) == true -> Msg_1882*/
+		NtsAssert.businessException("Msg_1881", ()-> {
+			SimultaneousAttendanceDesignation.create(
+					"EMPLOYEE_1",
+					Arrays.asList(
+							"EMPLOYEE_1", 
+							"EMPLOYEE_2"
+							));
+		});
+	}
+	/**
+	 * 同時出勤指定を作成する：成功(success)
+	 * 
+	 */
+	@Test
+	public void create_SimultaneousAttendanceDesignation_success() {
+		val employeeIds = Arrays.asList(
+				"EMPLOYEE_1", 
+				"EMPLOYEE_2",
+				"EMPLOYEE_3", 
+				"EMPLOYEE_4",
+				"EMPLOYEE_5",
+				"EMPLOYEE_6", 
+				"EMPLOYEE_7",
+				"EMPLOYEE_8",
+				"EMPLOYEE_9"
+				);
+		val simulAttDesign =  SimultaneousAttendanceDesignation.create("EMPLOYEE_0", employeeIds);
+		
+		assertThat(simulAttDesign.getSid()).isEqualTo("EMPLOYEE_0");
+		assertThat(simulAttDesign.getEmpMustWorkTogetherLst()).containsExactlyInAnyOrderElementsOf(employeeIds);
+		
 	}
 	
 }
