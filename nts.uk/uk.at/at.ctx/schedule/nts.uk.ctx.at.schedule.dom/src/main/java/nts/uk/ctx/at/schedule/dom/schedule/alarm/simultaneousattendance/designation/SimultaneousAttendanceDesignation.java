dfec1b299bccf9961990a23a60839df62ac82cee
leulan <lan-lt@nittsusystem-vn.com>
2020-09-28 13:22:48 +0900

    ドメイン「同時出勤指定」: commentを追加しました。

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/schedule/alarm/simultaneousattendance/designation/SimultaneousAttendanceDesignation.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/schedule/alarm/simultaneousattendance/designation/SimultaneousAttendanceDesignation.java
@@ -8,23 +8,24 @@ import nts.arc.error.BusinessException;
 import nts.arc.layer.dom.objecttype.DomainAggregate;
 /**
  * 同時出勤指定
+ * UKDesign.ドメインモデル.NittsuSystem.UniversalK.就業.contexts.勤務予定.勤務予定.勤務予定のアラームチェック.同時出勤.同時出勤指定.同時出勤指定
  * @author lan_lt
  *
  */
 @Getter
 @AllArgsConstructor
 public class SimultaneousAttendanceDesignation implements DomainAggregate{
-	//社員ID
+	/** 社員ID */
 	private final String sid;
 	
-	//同時に出勤すべき社員の候補
+	/** 同時に出勤すべき社員の候補 */
 	private List<String> empMustWorkTogetherLst;
 	
 	/**
 	 * 
-	 * [C-1] 作成する
-	 * @param sid
-	 * @param empMustWorkTogetherLst
+	 * 作成する
+	 * @param sid 社員ID
+	 * @param empMustWorkTogetherLst 同時に出勤すべき社員の候補
 	 * @return
 	 */
 	public  static SimultaneousAttendanceDesignation create(String sid, List<String> empMustWorkTogetherLst) {
