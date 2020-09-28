954af1f7f0605cbc3d8d214b8b1a932a0e013878
NguyenMauAnh <twas.horus@gmail.com>
2020-09-28 11:29:31 +0700

    refactor 4: KAf009: fix bug an component worktype worktime ko dang ky duoc

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/009/a/kaf009.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/009/a/kaf009.a.vm.ts
@@ -145,7 +145,7 @@ module nts.uk.at.view.kaf009_ref.a.viewmodel {
             vm.applicationTest.opAppStandardReasonCD = application.opAppStandardReasonCD;
             vm.applicationTest.opReversionReason = application.opReversionReason;
             if (vm.model) {
-                if ((vm.model.checkbox3() == true || vm.model.checkbox3() == null) && !vm.model.workTypeCode()) {
+                if ((vm.model.checkbox3() == true || vm.model.checkbox3() == null) && !vm.model.workTypeCode() && vm.dataFetch().goBackReflect().reflectApplication !== 0) {
                    // $('#workSelect').focus();
 					let el = document.getElementById('workSelect');
 	                if (el) {
