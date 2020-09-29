ceb7140418ff6d227096873221db3e16cd6bfd65
NTS-PhamVanDan <dan_pv@nittsusystem-vn.com>
2020-09-29 16:19:32 +0900

    create domain service

---
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/shift/management/shifttable/GetShfitTableRuleForOrganizationService.java
@@ -0,0 +1,40 @@
+package nts.uk.ctx.at.schedule.dom.shift.management.shifttable;
+
+import java.util.Optional;
+
+import nts.uk.ctx.at.shared.dom.workrule.organizationmanagement.workplace.TargetOrgIdenInfor;
+
+/**
+ * 組織のシフト表のルールを取得する
+ * UKDesign.ドメインモデル.NittsuSystem.UniversalK.就業.contexts.勤務予定.シフト管理.シフト勤務.シフト表.組織のシフト表のルールを取得する
+ * @author dan_pv
+ *
+ */
+public class GetShfitTableRuleForOrganizationService {
+	
+	public static Optional<ShiftTableRule> get(Require require, TargetOrgIdenInfor targetOrg) {
+		
+		// get shift table rule of organization
+		Optional<ShiftTableRuleForOrganization> orgShiftTable = require.getOrganizationShiftTable(targetOrg);
+		if (orgShiftTable.isPresent()) {
+			return Optional.of(orgShiftTable.get().getShiftTableRule());
+		}
+		
+		// get shift table rule of company
+		Optional<ShiftTableRuleForCompany> comShiftTable = require.getCompanyShiftTable();
+		if (comShiftTable.isPresent()) {
+			return Optional.of(comShiftTable.get().getShiftTableRule());
+		}
+		
+		return Optional.empty();
+	}
+	
+	public static interface Require {
+		
+		Optional<ShiftTableRuleForOrganization> getOrganizationShiftTable(TargetOrgIdenInfor targetOrg);
+		
+		Optional<ShiftTableRuleForCompany> getCompanyShiftTable();
+		
+	}
+
+}
