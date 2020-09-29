70d4eca6ea7e3436822bcd504ac7b7c73558981c
leulan <lan-lt@nittsusystem-vn.com>
2020-09-29 17:22:34 +0900

    勤務予定のアラームチェック条件 remove method create

--- nts.uk/uk.at/at.ctx/schedulealarm/nts.uk.ctx.at.schedulealarm.dom/src/main/java/nts/uk/ctx/at/schedulealarm/dom/alarmcheck/AlarmCheckConditionScheduleOrder.java
+++ nts.uk/uk.at/at.ctx/schedulealarm/nts.uk.ctx.at.schedulealarm.dom/src/main/java/nts/uk/ctx/at/schedulealarm/dom/alarmcheck/AlarmCheckConditionScheduleOrder.java
@@ -1,11 +1,9 @@
 package nts.uk.ctx.at.schedulealarm.dom.alarmcheck;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import nts.arc.error.BusinessException;
 import nts.arc.layer.dom.objecttype.DomainAggregate;
 
 /**
@@ -21,18 +19,18 @@ public class AlarmCheckConditionScheduleOrder implements DomainAggregate{
 	/** 優先順リスト */
 	private List<AlarmCheckConditionCode> codes;
 
-	public static AlarmCheckConditionScheduleOrder create(List<AlarmCheckConditionCode> codes) {
-		// inv-2 @優先順リスト.size() > 0
-		if (codes.isEmpty()) {
-			throw new BusinessException("Msg_XXX");
-		}
-
-		// inv-1 @優先順リストの勤務予定のアラームチェック条件コードが重複しないこと
-		List<String> lstElement = codes.stream().map(c -> c.toString()).distinct().collect(Collectors.toList());
-		if (lstElement.size() < codes.size()) {
-			throw new BusinessException("Msg_XXX");
-		}
-
-		return new AlarmCheckConditionScheduleOrder(codes);
-	}
+//	public static AlarmCheckConditionScheduleOrder create(List<AlarmCheckConditionCode> codes) {
+//		// inv-2 @優先順リスト.size() > 0
+//		if (codes.isEmpty()) {
+//			throw new BusinessException("Msg_XXX");
+//		}
+//
+//		// inv-1 @優先順リストの勤務予定のアラームチェック条件コードが重複しないこと
+//		List<String> lstElement = codes.stream().map(c -> c.toString()).distinct().collect(Collectors.toList());
+//		if (lstElement.size() < codes.size()) {
+//			throw new BusinessException("Msg_XXX");
+//		}
+//
+//		return new AlarmCheckConditionScheduleOrder(codes);
+//	}
 }
