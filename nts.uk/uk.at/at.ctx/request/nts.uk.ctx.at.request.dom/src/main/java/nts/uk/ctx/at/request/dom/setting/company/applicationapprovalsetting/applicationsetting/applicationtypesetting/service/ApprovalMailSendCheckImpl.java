be749d8bb3545d55a7e05dd1116b4f63bbc8f1b5
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2020-10-01 17:20:12 +0700

    refactor 4: QA #111260: update xử lý 承認する 01/10/2020

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/setting/company/applicationapprovalsetting/applicationsetting/applicationtypesetting/service/ApprovalMailSendCheckImpl.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/setting/company/applicationapprovalsetting/applicationsetting/applicationtypesetting/service/ApprovalMailSendCheckImpl.java
@@ -1,12 +1,8 @@
 package nts.uk.ctx.at.request.dom.setting.company.applicationapprovalsetting.applicationsetting.applicationtypesetting.service;
 
 import javax.ejb.Stateless;
-import javax.inject.Inject;
 
 import nts.uk.ctx.at.request.dom.application.Application;
-import nts.uk.ctx.at.request.dom.application.common.service.other.OtherCommonAlgorithm;
-import nts.uk.ctx.at.request.dom.application.common.service.other.output.MailResult;
-import nts.uk.ctx.at.request.dom.application.common.service.other.output.ProcessResult;
 import nts.uk.ctx.at.request.dom.setting.company.applicationapprovalsetting.applicationsetting.applicationtypesetting.AppTypeSetting;
 
 /**
@@ -15,27 +11,18 @@ import nts.uk.ctx.at.request.dom.setting.company.applicationapprovalsetting.appl
  */
 @Stateless
 public class ApprovalMailSendCheckImpl implements ApprovalMailSendCheck {
-	
-	@Inject
-	private OtherCommonAlgorithm otherCommonAlgorithm;
 
 	@Override
-	public ProcessResult sendMail(AppTypeSetting appTypeSetting, Application application, Boolean allApprovalFlg) {
-		ProcessResult processResult = new ProcessResult();
-		processResult.setProcessDone(true);
+	public String sendMail(AppTypeSetting appTypeSetting, Application application, Boolean allApprovalFlg) {
 		// ドメインモデル「申請種類別設定」．承認処理時に自動でメールを送信するをチェックする(check domain 「申請種類別設定」．承認処理時に自動でメールを送信する)
 		if(!appTypeSetting.isSendMailWhenApproval()) {
-			return processResult;
+			return "";
 		}
 		// アルゴリズム「承認全体が完了したか」の実行結果をチェックする
 		if(!allApprovalFlg) {
-			return processResult;
+			return "";
 		}
-		// 申請者本人にメール送信する(gửi mail cho người viết đơn)
-		MailResult applicantResult = otherCommonAlgorithm.sendMailApplicantApprove(application);
-		processResult.setAutoSuccessMail(applicantResult.getSuccessList());
-		processResult.setAutoFailMail(applicantResult.getFailList());
-		processResult.setAutoFailServer(applicantResult.getFailServerList());
-		return processResult;
+		// 申請者本人をメール送信先リストに追加して、返す 
+		return application.getEmployeeID();
 	}
 }
