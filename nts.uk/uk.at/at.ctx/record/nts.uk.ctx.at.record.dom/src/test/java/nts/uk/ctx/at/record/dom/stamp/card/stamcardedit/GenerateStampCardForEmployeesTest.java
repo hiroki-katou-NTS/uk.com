7d3e934fd9913275280d036f787b18439ac3a133
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2020-10-01 10:29:27 +0700

    Revert "Pick Create GenerateStampCardForEmployeesTest"
    This reverts commit 828884cd10877e3a06dc8cec12fff1f3bb1085cd.

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/test/java/nts/uk/ctx/at/record/dom/stamp/card/stamcardedit/GenerateStampCardForEmployeesTest.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/test/java/nts/uk/ctx/at/record/dom/stamp/card/stamcardedit/GenerateStampCardForEmployeesTest.java
@@ -4,10 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.List;
-<<<<<<< HEAD
 import java.util.Optional;
-=======
->>>>>>> ada8a756ed3... Create GenerateStampCardForEmployeesTest
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,10 +15,7 @@ import mockit.integration.junit4.JMockit;
 import nts.arc.enums.EnumAdaptor;
 import nts.arc.testing.assertion.NtsAssert;
 import nts.uk.ctx.at.record.dom.stamp.card.stamcardedit.GenerateStampCardForEmployees.Require;
-<<<<<<< HEAD
 import nts.uk.ctx.at.record.dom.stamp.card.stampcard.StampCard;
-=======
->>>>>>> ada8a756ed3... Create GenerateStampCardForEmployeesTest
 
 /**
  * 
@@ -35,7 +29,6 @@ public class GenerateStampCardForEmployeesTest {
 	@Injectable
 	private Require require;
 	
-<<<<<<< HEAD
 	private String companyId = "000001";
 	private String contractCd = "000000";
 	private String companyCd = "0001";
@@ -98,48 +91,24 @@ public class GenerateStampCardForEmployeesTest {
 	
 	@Test
 	public void test2() {
-=======
-	private String companyId = "000000000000-0001";
-	private String contractCd = "000000000000";
-	private String companyCd = "0001";
-
-	@Test
-	public void testreturnEmpty() {
-		List<TargetPerson> persons = new ArrayList<>();
-		List<ImprintedCardGenerationResult> list = GenerateStampCardForEmployees.generate(require, contractCd, companyCd, EnumAdaptor.valueOf(0, MakeEmbossedCard.class), persons);
-		
-		assertThat(list).isEmpty();
-	}
-	
-	@Test
-	public void testMsg_1756() {
->>>>>>> ada8a756ed3... Create GenerateStampCardForEmployeesTest
 		List<TargetPerson> persons = new ArrayList<>();
 		persons.add(new TargetPerson("000002", "8f9edce4-e135-4a1e-8dca-ad96abe405d6"));
 		
 		new Expectations() {
 			{
 				require.get(companyId);
-<<<<<<< HEAD
 				result = new StampCardEditing(companyId, new StampCardDigitNumber(20), StampCardEditMethod.AfterSpace);
 				
 				require.getByCardNoAndContractCode("DUMMY", contractCd);
 				result = Optional.of(new StampCard(contractCd, "0123456", sid));
-=======
-				// result = new StampCardEditing(companyCd, new StampCardDigitNumber(10), EnumAdaptor.valueOf(1, StampCardEditMethod.class));
->>>>>>> ada8a756ed3... Create GenerateStampCardForEmployeesTest
 				
 			}
 		};
 		
-<<<<<<< HEAD
 
 		List<ImprintedCardGenerationResult> cardGenerationResults = GenerateStampCardForEmployees.generate(require,
 				contractCd, companyCd, EnumAdaptor.valueOf(0, MakeEmbossedCard.class), persons, companyId,sid);
 		
 		assertThat(cardGenerationResults.get(0).getCardNumber().getStampNumber().v()).isEqualTo("000002              ");
-=======
-		NtsAssert.businessException("Msg_1756", () -> GenerateStampCardForEmployees.generate(require, contractCd, companyCd, EnumAdaptor.valueOf(1, MakeEmbossedCard.class), persons));
->>>>>>> ada8a756ed3... Create GenerateStampCardForEmployeesTest
 	}
 }
