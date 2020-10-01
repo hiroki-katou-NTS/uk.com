5ef62128eddeeac3610a05d85537abaec11b7afd
NTS-PhamVanDan <dan_pv@nittsusystem-vn.com>
2020-10-01 13:20:35 +0900

    add EA path to Class files

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/shift/management/workexpect/AssignmentMethod.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/shift/management/workexpect/AssignmentMethod.java
@@ -3,6 +3,12 @@ package nts.uk.ctx.at.schedule.dom.shift.management.workexpect;
 import lombok.RequiredArgsConstructor;
 import nts.arc.enums.EnumAdaptor;
 
+/**
+ * 勤務希望の指定方法
+ * UKDesign.ドメインモデル.NittsuSystem.UniversalK.就業.contexts.勤務予定.シフト管理.シフト勤務.勤務希望.勤務希望の指定方法
+ * @author dan_pv
+ *
+ */
 @RequiredArgsConstructor
 public enum AssignmentMethod {
 
--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/shift/management/workexpect/WorkExpectDisplayInfo.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/shift/management/workexpect/WorkExpectDisplayInfo.java
@@ -5,6 +5,12 @@ import java.util.List;
 import lombok.Value;
 import nts.uk.ctx.at.shared.dom.common.time.TimeSpanForCalc;
 
+/**
+ * 勤務希望の表示情報
+ * UKDesign.ドメインモデル.NittsuSystem.UniversalK.就業.contexts.勤務予定.シフト管理.シフト勤務.勤務希望.勤務希望の表示情報
+ * @author dan_pv
+ *
+ */
 @Value
 public class WorkExpectDisplayInfo {
 	
