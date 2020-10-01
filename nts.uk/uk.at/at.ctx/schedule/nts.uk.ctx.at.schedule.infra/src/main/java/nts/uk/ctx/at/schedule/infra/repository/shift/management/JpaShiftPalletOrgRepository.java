a667557d7dd27141676f36ebac593ea2069ea6a4
NSVN-HieuLT <king.of.throw.game@gmail.com>
2020-10-01 15:45:39 +0700

    fixbug KSU 001 J

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.infra/src/main/java/nts/uk/ctx/at/schedule/infra/repository/shift/management/JpaShiftPalletOrgRepository.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.infra/src/main/java/nts/uk/ctx/at/schedule/infra/repository/shift/management/JpaShiftPalletOrgRepository.java
@@ -56,7 +56,7 @@ public class JpaShiftPalletOrgRepository extends JpaRepository implements ShiftP
 		builderString.append(
 				" FROM KSCMT_PALETTE_ORG a  JOIN KSCMT_PALETTE_ORG_COMBI b ON a.TARGET_UNIT = b.TARGET_UNIT AND a.TARGET_ID = b.TARGET_ID AND a.PAGE = b.PAGE ");
 		builderString.append(
-				" JOIN KSCMT_PALETTE_ORG_COMBI_DTL c ON a.TARGET_UNIT = c.TARGET_UNIT AND a.TARGET_ID = c.TARGET_ID AND a.PAGE = c.PAGE ");
+				" JOIN KSCMT_PALETTE_ORG_COMBI_DTL c ON b.TARGET_UNIT = c.TARGET_UNIT AND b.TARGET_ID = c.TARGET_ID AND b.PAGE = c.PAGE AND b.POSITION = c.POSITION ");
 		SELECT = builderString.toString();
 
 		builderString = new StringBuilder();
