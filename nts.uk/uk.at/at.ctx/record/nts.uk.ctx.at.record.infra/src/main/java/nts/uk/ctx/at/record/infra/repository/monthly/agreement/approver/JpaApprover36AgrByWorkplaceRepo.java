5bba5627da2061de843e517f95fe5088556ea5bc
chinh.hm <chinh.hm@3si.vn>
2020-10-02 08:34:23 +0700

    update repo 会社別の承認者（36協定） cmm024
    (cherry picked from commit f11dd4f1812db8751b28f75ff5f27b9c30e63f5c)

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.infra/src/main/java/nts/uk/ctx/at/record/infra/repository/monthly/agreement/approver/JpaApprover36AgrByWorkplaceRepo.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.infra/src/main/java/nts/uk/ctx/at/record/infra/repository/monthly/agreement/approver/JpaApprover36AgrByWorkplaceRepo.java
@@ -44,22 +44,10 @@ public class JpaApprover36AgrByWorkplaceRepo extends JpaRepository implements Ap
 		val pk = new Krcmt36AgrApvWkpPK(domain.getWorkplaceId(),startDateBeforeChange);
 		Optional<Krcmt36AgrApvWkp> findResult = this.queryProxy().find(domainData.pk, Krcmt36AgrApvWkp.class);
 		if (findResult.isPresent()) {
-			Krcmt36AgrApvWkp target = findResult.get();
-			target.endDate = domainData.endDate;
 
-			target.approverSid1 = domainData.approverSid1;
-			target.approverSid2 = domainData.approverSid2;
-			target.approverSid3 = domainData.approverSid3;
-			target.approverSid4 = domainData.approverSid4;
-			target.approverSid5 = domainData.approverSid5;
-
-			target.confirmerSid1 = domainData.confirmerSid1;
-			target.confirmerSid2 = domainData.confirmerSid2;
-			target.confirmerSid3 = domainData.confirmerSid3;
-			target.confirmerSid4 = domainData.confirmerSid4;
-			target.confirmerSid5 = domainData.confirmerSid5;
-
-			this.commandProxy().update(target);
+			this.commandProxy().remove(Krcmt36AgrApvWkp.class,pk);
+			this.getEntityManager().flush();
+			this.commandProxy().insert(domainData);
 		} else {
 			this.commandProxy().insert(domainData);
 		}
