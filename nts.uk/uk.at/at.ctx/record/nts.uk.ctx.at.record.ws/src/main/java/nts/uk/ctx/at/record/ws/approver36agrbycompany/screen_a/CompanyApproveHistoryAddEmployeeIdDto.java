de426411daae0b4045bc85b7dbe9cb8b5ad14364
chinh.hm <chinh.hm@3si.vn>
2020-10-05 11:25:38 +0700

    update cmm024-a

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.ws/src/main/java/nts/uk/ctx/at/record/ws/approver36agrbycompany/screen_a/CompanyApproveHistoryAddEmployeeIdDto.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.ws/src/main/java/nts/uk/ctx/at/record/ws/approver36agrbycompany/screen_a/CompanyApproveHistoryAddEmployeeIdDto.java
@@ -29,7 +29,7 @@ public class CompanyApproveHistoryAddEmployeeIdDto {
     /**
      * 承認者リスト
      */
-    private List<String> approveList;
+    private List<String> approvedList;
     /**
      * 確認者リスト
      */
