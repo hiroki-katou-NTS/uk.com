3368cd397f0cf903a370d286bbc1b731620fe054
chinh.hm <chinh.hm@3si.vn>
2020-10-05 13:34:53 +0700

    update junit cmm024

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/test/java/nts/uk/ctx/at/record/dom/monthly/agreement/approver/DeleteWorkplaceApproverHistoryDomainServiceTest.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/test/java/nts/uk/ctx/at/record/dom/monthly/agreement/approver/DeleteWorkplaceApproverHistoryDomainServiceTest.java
@@ -24,9 +24,11 @@ public class DeleteWorkplaceApproverHistoryDomainServiceTest {
 
     @Injectable
     DeleteWorkplaceApproverHistoryDomainService.Require require;
-    private static String workplaceId = "wid";
-    private   static List<String> approverList =  Arrays.asList("abc","cba","efg");
-    private   static List<String> confirmerList =  Arrays.asList("abc","cba","efg");
+
+    private static String workplaceId = "000000000000-0003";
+    private   static List<String> approverList =  Arrays.asList("7AB520B7-CF55-4068-8DEF-C4DF52C35C2E","7AD2CE3D-3AE8-4D90-B556-867596A830B7","7AB52E44-06B2-476D-B04C-0D35F712C00F");
+    private   static List<String> confirmerList =  Arrays.asList("7AB52E44-06B2-476D-B04C-0D35F712C00F","7AD2CE3D-3AE8-4D90-B556-867596A830B7","7AB52E44-06B2-476D-B04C-0D35F712C00F");
+
     @Test
     public void test_01(){
         val deleteItem = Approver36AgrByWorkplace.create(
@@ -53,6 +55,8 @@ public class DeleteWorkplaceApproverHistoryDomainServiceTest {
                 any -> require.deleteHistory(any.get()),
                 any -> require.changeLatestHistory(any.get(),preVHistoryItem.getPeriod().start())
         );
+        assertThat(preVHistoryItem.getPeriod().end()).isEqualTo(GeneralDate.max() );
+
     }
     @Test
     public void test_02(){
@@ -71,32 +75,5 @@ public class DeleteWorkplaceApproverHistoryDomainServiceTest {
                 () -> DeleteWorkplaceApproverHistoryDomainService.changeHistory(require, deleteItem),
                 any -> require.deleteHistory(any.get())
         );
-    }
-    @Test
-    public void test_03(){
-        val deleteItem = Approver36AgrByWorkplace.create(
-                workplaceId,
-                new DatePeriod(GeneralDate.today(),GeneralDate.max()),
-                approverList,
-                confirmerList
-
-        );
-        val preVHistoryItem = Approver36AgrByWorkplace.create(
-                workplaceId,
-                new DatePeriod(deleteItem.getPeriod().start().addDays(-5),deleteItem.getPeriod().start().addDays(-1)),
-                approverList,
-                confirmerList
-
-        );
-        new Expectations(){{
-            require.getLastHistory(workplaceId,deleteItem.getPeriod().start().addDays(-1));
-            result = Optional.of(preVHistoryItem);
-        }};
-
-        NtsAssert.atomTask(
-                () -> DeleteWorkplaceApproverHistoryDomainService.changeHistory(require, deleteItem),
-                any -> require.deleteHistory(any.get())
-        );
-        assertThat(preVHistoryItem.getPeriod().end()).isEqualTo(GeneralDate.max() );
     }
 }
