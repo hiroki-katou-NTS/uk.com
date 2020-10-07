21d1957279bad20ba76c4bdc8e756229736c812f
Hop.NT <hop.nt@3si.vn>
2020-10-07 11:51:08 +0700

    KSM003: fixbug AT #111842

--- nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/ksm003/find/GetWorkCycle.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/ksm003/find/GetWorkCycle.java
@@ -2,11 +2,15 @@ package nts.uk.screen.at.app.ksm003.find;
 
 import lombok.val;
 import nts.uk.ctx.at.schedule.dom.shift.workcycle.WorkCycle;
-import nts.uk.ctx.at.schedule.dom.shift.workcycle.WorkCycleInfo;
+import nts.uk.ctx.at.shared.dom.worktime.worktimeset.WorkTimeSetting;
+import nts.uk.ctx.at.shared.dom.worktime.worktimeset.WorkTimeSettingRepository;
+import nts.uk.ctx.at.shared.dom.worktype.WorkType;
+import nts.uk.ctx.at.shared.dom.worktype.WorkTypeRepository;
 import nts.uk.shr.com.context.AppContexts;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -15,7 +19,13 @@ import java.util.stream.Collectors;
 public class GetWorkCycle {
 
     @Inject
-    WorkCycleQueryRepository workCycleQueryRepository;
+    private WorkCycleQueryRepository workCycleQueryRepository;
+
+    @Inject
+    private WorkTypeRepository workTypeRepo;
+
+    @Inject
+    private WorkTimeSettingRepository workTimeSettingRepository;
 
     /**
      * 勤務サイクル一覧を取得する
@@ -29,16 +39,30 @@ public class GetWorkCycle {
 
     /**
      * 勤務サイクルの明細を表示する
-     * @param code
+     * @param patternCode
      * @return
      */
     public WorkCycleDto getWorkCycleInfo(String patternCode) {
         String cid = AppContexts.user().companyId();
         Optional<WorkCycle> workingCycle = workCycleQueryRepository.getByCidAndCode(cid, patternCode);
-        if (workingCycle.isPresent()) {
-            return WorkCycleDto.createFromDomain(workingCycle.get());
+        if (!workingCycle.isPresent()) {
+            return null;
         }
-        return null;
+
+        List<WorkType> workTypes = new ArrayList<>();
+        val listWrkTypes = workingCycle.get().getInfos().stream().map(i->i.getWorkInformation().getWorkTypeCode().v()).collect(Collectors.toList());
+        if (!listWrkTypes.isEmpty()) {
+            workTypes = this.workTypeRepo.findNotDeprecatedByListCode(cid, listWrkTypes);
+        }
+
+        List<WorkTimeSetting> workTimeItems = new ArrayList<>();
+        val listWrkTimes = workingCycle.get().getInfos().stream().map(i->i.getWorkInformation().getWorkTimeCodeNotNull()).collect(Collectors.toList());
+        val listWrkTimeStr = listWrkTimes.stream().filter(i->i.isPresent()).map(i->i.get().v()).collect(Collectors.toList());
+        if (listWrkTimeStr.isEmpty()) {
+            workTimeItems = this.workTimeSettingRepository.findByCodes(cid, listWrkTimeStr);
+        }
+
+        return WorkCycleDto.createFromDomain(workingCycle.get(),workTypes,workTimeItems);
     }
 
 }
--- nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/ksm003/find/WorkCyleInfoDto.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/ksm003/find/WorkCyleInfoDto.java
@@ -3,6 +3,12 @@ package nts.uk.screen.at.app.ksm003.find;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.val;
+import nts.uk.ctx.at.shared.dom.worktime.worktimeset.WorkTimeSetting;
+import nts.uk.ctx.at.shared.dom.worktype.WorkType;
+import nts.uk.shr.com.i18n.TextResource;
+
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -11,10 +17,31 @@ public class WorkCyleInfoDto {
 
     private String typeCode;
 
+    private String typeName;
+
     private String timeCode;
+
+    private String timeName;
 
     private int days;
 
     private int dispOrder;
 
+    public WorkCyleInfoDto(String typeCode, String timeCode, int days, int dispOrder, List<WorkType> workTypes, List<WorkTimeSetting> workTimeItems) {
+        this.typeCode = typeCode;
+        this.timeCode = timeCode;
+        this.days = days;
+        this.dispOrder = dispOrder;
+
+        // Get WorkType
+        val workType = workTypes.stream().filter(i -> i.getWorkTypeCode().v().equals(typeCode)).findFirst();
+        this.typeName = workType.isPresent() ? workType.get().getName().v() : TextResource.localize("KSM003_2");
+
+        if (timeCode != null) {
+            // Get WorkTime
+            val workTime = workTimeItems.stream().filter(i -> i.getWorktimeCode().v().equals(timeCode)).findFirst();
+            this.timeCode = workTime.isPresent() ? workTime.get().getWorkTimeDisplayName().getWorkTimeName().v() : TextResource.localize("KSM003_2");
+        }
+
+    }
 }
