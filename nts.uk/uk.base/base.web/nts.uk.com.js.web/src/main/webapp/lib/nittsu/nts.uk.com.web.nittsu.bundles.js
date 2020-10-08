b73e336b5bd8b702c12c56e11a7612aa6e30f9ee
NSVN-TruongVanLai <NSVN-TruongVanLai@users.noreply.github.com>
2020-10-08 13:15:06 +0700

    bundles.js

--- nts.uk/uk.base/base.web/nts.uk.com.js.web/src/main/webapp/lib/nittsu/nts.uk.com.web.nittsu.bundles.js
+++ nts.uk/uk.base/base.web/nts.uk.com.js.web/src/main/webapp/lib/nittsu/nts.uk.com.web.nittsu.bundles.js
@@ -14993,10 +14993,24 @@ var nts;
                         var $grid = $container.find("." + BODY_PRF + DETAIL);
                         if (mode !== DETERMINE) {
                             if (exTable.updateMode !== DETERMINE) {
+                                // Keep states while switching to new mode
+                                if (exTable.updateMode === EDIT) {
+                                    var editor = $container.data(update.EDITOR);
+                                    var inputSelecting = $grid.data(internal.INPUT_SELECTING);
+                                    if (editor) {
+                                        update.outsideClick($container[0], null, true);
+                                    }
+                                    else if (inputSelecting) {
+                                        selection.clearInnerCell($grid[0], inputSelecting.rowIdx, inputSelecting.columnKey, inputSelecting.innerIdx);
+                                    }
+                                }
+                                else if (exTable.updateMode === COPY_PASTE) {
+                                    selection.clearAll($grid[0]);
+                                }
                                 exTable.setUpdateMode(mode);
-                                exTable.modifications = {};
-                                render.begin($grid[0], _.cloneDeep(helper.getOrigDS($grid[0])), exTable.detailContent);
-                                selection.tickRows($container.find("." + BODY_PRF + LEFTMOST)[0], true);
+                                //                    exTable.modifications = {};
+                                //                    render.begin($grid[0], _.cloneDeep(helper.getOrigDS($grid[0])), exTable.detailContent);
+                                //                    selection.tickRows($container.find("." + BODY_PRF + LEFTMOST)[0], true);
                             }
                             else {
                                 exTable.setUpdateMode(mode);
