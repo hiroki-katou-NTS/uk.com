11e7ffad637fb40432f41dc69feadfadc3243698
haitt3si <hai.tt@3si.vn>
2020-10-08 17:19:06 +0700

    [KAF001] Refactor code
    (cherry picked from commit 867d2531d41b0ae1c941edb07f90a414a4d88dc6)

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.ws/src/main/java/nts/uk/ctx/at/request/ws/application/proxy/ProxyApplicationWebservice.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.ws/src/main/java/nts/uk/ctx/at/request/ws/application/proxy/ProxyApplicationWebservice.java
@@ -6,8 +6,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 import nts.arc.layer.ws.WebService;
+import nts.uk.ctx.at.request.app.find.application.proxy.CheckEmployeeParam;
 import nts.uk.ctx.at.request.app.find.application.proxy.ProxyApplicationFinder;
 import nts.uk.ctx.at.request.app.find.application.proxy.ProxyParamFind;
+import nts.uk.ctx.at.request.dom.application.common.adapter.application.dto.AtStandardMenuNameImport;
+
+import java.util.List;
 
 /**
  * @author sang.nv
@@ -24,5 +28,17 @@ public class ProxyApplicationWebservice extends WebService {
 	@Path("find")
 	public void selectApplicationByType(ProxyParamFind proxyParamFind) {
 		this.proxyApplicationFinder.selectApplicationByType(proxyParamFind);
+	}
+
+	@POST
+	@Path("checkValid")
+	public void checkEmployeeSelected(CheckEmployeeParam param) {
+		this.proxyApplicationFinder.checkEmployeeSelected(param);
+	}
+
+	@POST
+	@Path("findName")
+	public List<AtStandardMenuNameImport> getAppDispName() {
+		return this.proxyApplicationFinder.startProxyApplication();
 	}
 }
