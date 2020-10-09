059995c2b5da43a20593e1fc9af162af35a66961
NguyenMauAnh <twas.horus@gmail.com>
2020-10-09 14:03:26 +0700

    refactor 5: KAf007: Update hien thi thoi gian khoi dong

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/007/b/kaf007.b.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/007/b/kaf007.b.vm.ts
@@ -109,10 +109,10 @@ module nts.uk.at.view.kaf007_ref.c.viewmodel {
             var time1 = _.filter(lstTimezone, ['workNo', 1]);
             var time2 = _.filter(lstTimezone, ['workNo', 2]);
 
-            vm.appWorkChange.startTime1((time1.length > 0 && time1[0].useAtr === true) ? time1[0].start : null);
-			vm.appWorkChange.endTime1((time1.length > 0 && time1[0].useAtr === true) ? time1[0].end : null);
-			vm.appWorkChange.startTime2((time2.length > 0 && time2[0].useAtr === true) ? time2[0].start : null);
-			vm.appWorkChange.endTime2((time2.length > 0 && time2[0].useAtr === true) ? time2[0].end : null);
+            vm.appWorkChange.startTime1((time1.length > 0) ? time1[0].timeZone.startTime : null);
+			vm.appWorkChange.endTime1((time1.length > 0) ? time1[0].timeZone.endTime : null);
+			vm.appWorkChange.startTime2((time2.length > 0) ? time2[0].timeZone.startTime : null);
+			vm.appWorkChange.endTime2((time2.length > 0) ? time2[0].timeZone.endTime : null);
             vm.isStraightGo(appWorkChangeParam.straightGo == 1);
             vm.isStraightBack(appWorkChangeParam.straightBack == 1);
         }
