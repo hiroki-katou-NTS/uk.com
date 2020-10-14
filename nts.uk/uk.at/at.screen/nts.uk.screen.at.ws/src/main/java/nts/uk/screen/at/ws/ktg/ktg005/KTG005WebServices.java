caf1b1d0674ed628367239aa73fc7d91379e2195
Son <choiaog@gmail.com>
2020-10-14 16:40:49 +0700

    KTG005- Add Server

---
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.ws/src/main/java/nts/uk/screen/at/ws/ktg/ktg005/KTG005WebServices.java
@@ -0,0 +1,17 @@
+package nts.uk.screen.at.ws.ktg.ktg005;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import nts.arc.layer.ws.WebService;
+
+/**
+ * 
+ * @author sonnlb
+ *
+ */
+@Path("screen/at/schedule/basicschedule")
+@Produces("application/json")
+public class KTG005WebServices extends WebService {
+
+}
