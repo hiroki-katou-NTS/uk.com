3f8494294ba44b1005b6ee4a3623af89e6cb520f
NguyenMauAnh <twas.horus@gmail.com>
2020-10-15 17:07:55 +0700

    refactor 5: KAF007: Update folder app7

--- nts.uk/uk.mobile/ClientApp/views/cmm/s45/shr/components/app7/app71/index.ts
+++ nts.uk/uk.mobile/ClientApp/views/cmm/s45/shr/components/app7/app71/index.ts
@@ -3,7 +3,7 @@ import { component } from '@app/core/component';
 
 @component({
     name: 'cmms45shrcomponentsapp71',
-    route: '/cmm/s45/shr/components/app71',
+    route: '/cmm/s45/shr/components/app7/app71',
     style: require('./style.scss'),
     template: require('./index.vue'),
     resource: require('./resources.json'),
--- nts.uk/uk.mobile/ClientApp/views/cmm/s45/shr/components/index.ts
+++ nts.uk/uk.mobile/ClientApp/views/cmm/s45/shr/components/index.ts
@@ -3,5 +3,4 @@ export * from './app2';
 export * from './app3';
 export * from './app4';
 export * from './app5';
-export * from  './app70';
-export * from  './app71';
+export * from './app7';
