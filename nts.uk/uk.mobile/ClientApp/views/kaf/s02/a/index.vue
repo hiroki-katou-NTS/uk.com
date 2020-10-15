4afe2a86593b7480b913dc632551117dd9867110
NguyenMauAnh <twas.horus@gmail.com>
2020-10-15 16:13:39 +0700

    refactor 5: KAF007: Update xu ly update

--- nts.uk/uk.mobile/ClientApp/views/kaf/s02/a/index.vue
+++ nts.uk/uk.mobile/ClientApp/views/kaf/s02/a/index.vue
@@ -38,7 +38,7 @@
               <div class="col-6 text-right" style="font-size: 90%" v-if="condition2"><kafs00subp3 v-bind:params="itemWH.actualHours" /></div>
             </div>
             <!-- A3_3 -->
-            <div style="color: red" v-if="condition2 && itemWH.errorMsg !== null && condition1(itemWH)">{{ itemWH.errorMsg }}</div>
+            <div style="color: red; font-size: 90%" v-if="condition2 && itemWH.errorMsg !== null && condition1(itemWH)">{{ itemWH.errorMsg }}</div>
             <!-- A3_1 -->
             <div class="card-body" v-if="condition1(itemWH)">
               <nts-time-range-input class="mb-1" v-model="itemWH.workHours" v-bind:showTile="false" v-bind:disabled="checkboxWH.filter((x) => x === itemWH.frame).length > 0" />
@@ -64,7 +64,7 @@
               <div class="col-6 text-right" style="font-size: 90%" v-if="condition2"><kafs00subp3 v-bind:params="itemTH.actualHours" /></div>
             </div>
             <!-- A3_3 -->
-            <div style="color: red" v-if="condition2 && condition12">{{ itemTH.errorMsg }}</div>
+            <div style="color: red; font-size: 90%" v-if="condition2 && condition12">{{ itemTH.errorMsg }}</div>
             <!-- A3_1 -->
             <div class="card-body">
               <nts-time-range-input class="mb-1" v-model="itemTH.workHours" v-bind:showTile="false" v-bind:disabled="checkboxTH.filter((x) => x === itemTH.frame).length > 0" />
@@ -99,7 +99,7 @@
             <div class="col-6 text-right" style="font-size: 90%" v-if="condition2"><kafs00subp3 v-bind:params="itemGH.actualHours" /></div>
           </div>
           <!-- A4_3 -->
-          <div style="color: red" v-if="condition2 && condition12">{{ itemGH.errorMsg }}</div>
+          <div style="color: red; font-size: 90%" v-if="condition2 && condition12">{{ itemGH.errorMsg }}</div>
           <!-- A4_1 -->
           <div class="card-body">
             <nts-time-range-input v-model="itemGH.hours" v-bind:showTile="false" v-bind:disabled="checkboxGH.filter((x) => x === itemGH.frame).length > 0" />
@@ -155,7 +155,7 @@
             <div class="col-6 text-right" style="font-size: 90%" v-if="condition2"><kafs00subp3 v-bind:params="itemBH.actualHours" /></div>
           </div>
           <!-- A5_3 -->
-          <div style="color: red" v-if="condition2 && condition12">{{ itemBH.errorMsg }}</div>
+          <div style="color: red; font-size: 90%" v-if="condition2 && condition12">{{ itemBH.errorMsg }}</div>
           <!-- A5_1 -->
           <div class="card-body">
             <nts-time-range-input class="mb-1" v-model="itemBH.workHours" v-bind:showTile="false" v-bind:disabled="checkboxBH.filter((x) => x === itemBH.frame).length > 0" />
@@ -202,7 +202,7 @@
             <div class="col-6 text-right" style="font-size: 90%" v-if="condition2"><kafs00subp3 v-bind:params="itemCH.actualHours" /></div>
           </div>
           <!-- A6_3 -->
-          <div style="color: red" v-if="condition2 && condition12">{{ itemCH.errorMsg }}</div>
+          <div style="color: red; font-size: 90%" v-if="condition2 && condition12">{{ itemCH.errorMsg }}</div>
           <!-- A6_1 -->
           <div class="card-body">
             <nts-time-range-input class="mb-1" v-model="itemCH.workHour" v-bind:showTile="false" v-bind:disabled="checkboxCH.filter((x) => x === itemCH.frame).length > 0" />
@@ -235,7 +235,7 @@
             <div class="col-6 text-right" style="font-size: 90%" v-if="condition2"><kafs00subp3 v-bind:params="itemLH.actualHours" /></div>
           </div>
           <!-- A7_3 -->
-          <div style="color: red" v-if="condition2">{{ itemLH.errorMsg }}</div>
+          <div style="color: red; font-size: 90%" v-if="condition2">{{ itemLH.errorMsg }}</div>
           <!-- A7_1 -->
           <div class="card-body">
             <nts-time-range-input class="mb-1" v-model="itemLH.workHours" v-bind:showTile="false" v-bind:disabled="checkboxLH.filter((x) => x === itemLH.frame).length > 0" />
