26d9373dde4ec498b66af23abb8feedd5de14e7b
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2020-10-18 12:04:00 +0700

    refactor 5: KAF018D: update UI 18/10/2020

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/018/d/kaf018.d.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/018/d/kaf018.d.vm.ts
@@ -2,15 +2,85 @@
 
 module nts.uk.at.view.kaf018.d.viewmodel {
 
-    @bean()
-    class Kaf018DViewModel extends ko.ViewModel {
+	@bean()
+	class Kaf018DViewModel extends ko.ViewModel {
+		closureName: string = '対象締め';
+		processingYm: string = '対象締め';
+		dataSource: any = [];
+		headers: any = [];
+		columns: any = [];
 		
-        created() {
-	
-        }
-    }
+		created(params: any) {
+			const vm = this;
+			for(let i = 1; i <= 20; i++) {
+				vm.dataSource.push({ wkpName: 'wkpName' + i, appInfo: 'x' });
+			}
+			vm.columns.push(
+				{ 
+					headerText: vm.$i18n('KAF018_373'), 
+					key: 'wkpName',
+					headerCssClass: 'kaf018-d-header-wkpName',
+					columnCssClass: 'kaf018-d-column-wkpName'
+				}
+			);
+			for(let i = 1; i <= 31; i++) {
+				vm.headers.push(moment.utc().day(i));
+			}
+			_.forEach(vm.headers, (dateloop) => {
+				vm.columns.push(
+					{ 
+						headerText: moment(dateloop).date(),
+						headerCssClass: 'kaf018-d-header-date',
+						group: [
+							{ 
+								headerText: moment(dateloop).format('ddd'), 
+								key: 'appInfo',
+								width: '28px',
+								headerCssClass: 'kaf018-d-header-date',
+								columnCssClass: 'kaf018-d-column-date'
+							}
+						]
+					}
+				);
+			});
+			vm.createMGrid();
+		}
+		
+		createMGrid() {
+			const vm = this;
+			$("#dpGrid").ntsGrid({
+				height: 508,
+				width: screen.availWidth - 70,
+				dataSource: vm.dataSource,
+				virtualization: true,
+				virtualizationMode: 'continuous',
+				columns: vm.columns,
+				features: [
+					{
+						name: 'MultiColumnHeaders'
+					}
+				],
+			});
+		}
+		
+		formatSubHeader(value: any) {
+			const vm = this;
+			return value+1;
+		}
 
-    const API = {
+		getTargetDate(): string {
+			const vm = this;
+			return "";
+		}
+
+		
+		close() {
+			const vm = this;
+			vm.$window.close({});
+		}
+	}
+
+	const API = {
 	
-    }
+	}
 }
