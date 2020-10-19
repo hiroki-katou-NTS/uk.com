cbc7b9c75db65434b738f7b892be6386cb3ada84
masaakijinno <fairvancs@gmail.com>
2020-10-19 10:47:34 +0900

    年休１日に相当する時間年休時間を取得する

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/vacation/setting/annualpaidleave/AnnualTimePerDay.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/vacation/setting/annualpaidleave/AnnualTimePerDay.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import nts.uk.ctx.at.shared.dom.vacation.setting.ManageDistinct;
 import nts.uk.ctx.at.shared.dom.vacation.setting.TimeAnnualRoundProcesCla;
 import nts.uk.ctx.at.shared.dom.vacation.setting.TimeDigestiveUnit;
+import nts.uk.ctx.at.shared.dom.workingcondition.LaborContractTime;
 
 /**
  * 時間年休一日の時間
@@ -19,7 +20,7 @@ public class AnnualTimePerDay {
 	private AnnualTimePerDayRefer annualTimePerDayRefer;
 	
 	/** 全社一律の時間 */
-	private LeaveTime leaveTime;
+	private LaborContractTime laborContractTime;
 	
 	/** 契約時間丸め */
 	private ContractTimeRound contractTimeRound;
