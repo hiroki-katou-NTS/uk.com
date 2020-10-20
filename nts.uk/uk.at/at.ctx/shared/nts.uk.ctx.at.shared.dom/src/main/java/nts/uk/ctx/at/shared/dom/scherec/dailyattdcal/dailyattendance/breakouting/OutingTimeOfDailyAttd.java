4a864f9f111ec7382d2e5b6c9fa7e6e19b9c85d8
leulan <lan-lt@nittsusystem-vn.com>
2020-10-20 11:38:31 +0900

    「日別勤怠の外出時間帯」：外出理由を指定して時間帯を取得する

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/scherec/dailyattdcal/dailyattendance/breakouting/OutingTimeOfDailyAttd.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/scherec/dailyattdcal/dailyattendance/breakouting/OutingTimeOfDailyAttd.java
@@ -7,6 +7,9 @@ import java.util.stream.Collectors;
 
 import lombok.Getter;
 import lombok.Setter;
+import nts.uk.ctx.at.shared.dom.common.time.TimeSpanForCalc;
+import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.common.TimeActualStamp;
+import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.common.timestamp.WorkStamp;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailycalprocess.calculation.timezone.deductiontime.DeductionAtr;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailycalprocess.calculation.timezone.deductiontime.DeductionClassification;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailycalprocess.calculation.timezone.deductiontime.TimeSheetOfDeductionItem;
@@ -14,6 +17,7 @@ import nts.uk.ctx.at.shared.dom.worktime.flowset.FlowFixedRestSet;
 import nts.uk.ctx.at.shared.dom.worktime.flowset.FlowWorkRestSettingDetail;
 import nts.uk.ctx.at.shared.dom.worktime.flowset.FlowWorkRestTimezone;
 import nts.uk.ctx.at.shared.dom.worktime.worktimeset.WorkTimeMethodSet;
+import nts.uk.shr.com.time.TimeWithDayAttr;
 
 /**
  * 日別勤怠の外出時間帯
@@ -103,4 +107,46 @@ public class OutingTimeOfDailyAttd {
 									.map(tc -> tc.toTimeSheetOfDeductionItem())
 									.collect(Collectors.toList());
 	}
+	/**
+	 * 外出理由を指定して時間帯を取得する
+	 * @param goOutReason
+	 */
+	public List<TimeSpanForCalc> getTimeZoneByGoOutReason() {
+		
+		return this.outingTimeSheets.stream()
+				.filter(c -> {  return greateThan(c.getComeBack(), c.getGoOut()) == true; })
+				.map(c -> {
+								return new TimeSpanForCalc(c.getGoOut().get().getStamp().get().getTimeDay().getTimeWithDay().get(),
+										c.getComeBack().get().getStamp().get().getTimeDay().getTimeWithDay().get());
+				}).collect(Collectors.toList());
+
+	}
+	
+	private boolean greateThan(Optional<TimeActualStamp> comBackStamp, Optional<TimeActualStamp> goOutStamp) {
+		
+		if(comBackStamp.isPresent() && goOutStamp.isPresent()) {
+			return greateThanWorkStamp(comBackStamp.get().getStamp(), goOutStamp.get().getStamp());
+		}
+		return false;
+	}
+	
+	private boolean greateThanWorkStamp(Optional<WorkStamp> wkStampComeback, Optional<WorkStamp> wkStampGoOut){
+		if(wkStampComeback.isPresent() && wkStampGoOut.isPresent()) {
+			return greateThanTimeDay(wkStampComeback.get().getTimeDay().getTimeWithDay(), 
+					wkStampGoOut.get().getTimeDay().getTimeWithDay());
+		}
+		return false;
+	}
+	
+	private boolean greateThanTimeDay(Optional<TimeWithDayAttr> time, Optional<TimeWithDayAttr> time1) {
+	
+		if(time.isPresent() && time1.isPresent()) {
+			return time.get().greaterThan(time1.get());
+		}
+		
+		return false;
+	}
+	
+	
+	
 }
