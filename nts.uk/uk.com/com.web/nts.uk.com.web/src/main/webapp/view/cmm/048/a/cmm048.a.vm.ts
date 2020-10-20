14bbdd5d9cf8df19e52f38547139b48469aab168
nws-dongnt <dongnt@newwave.vn>
2020-10-20 10:05:01 +0700

    update interface DTO

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmm/048/a/cmm048.a.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmm/048/a/cmm048.a.vm.ts
@@ -281,32 +281,603 @@ module nts.uk.com.view.cmm048.a {
     alphabetDigit: number;
   }
 
+  /**
+   * Dto ユーザー情報の使用方法
+   */
+  interface UserInfoUseMethodDto {
+    /**
+    * お知らせの利用
+    */
+    useOfNotice: number;
 
-  interface UserInfoUseMethodDto { }
+    /**
+     * パスワードの利用
+     */
+    useOfPassword: number;
 
+    /**
+     * プロフィールの利用
+     */
+    useOfProfile: number;
 
-  interface UserDto { }
+    /**
+     * 言語の利用
+     */
+    useOfLanguage: number;
 
+    /**
+     * 会社ID
+     */
+    companyId: string;
 
-  interface PersonDto { }
+    /**
+     * メール送信先機能
+     */
+    emailDestinationFunctions: EmailDestinationFunctionDto[];
 
+    /**
+     * 連絡先情報の設定
+     */
+    settingContactInformation: SettingContactInformationDto;
+  }
 
-  interface PersonalContactDto { }
+  /**
+   * Dto メール送信先機能
+   */
+  interface EmailDestinationFunctionDto {
+    /**
+     * メール分類
+     */
+    emailClassification: number;
 
+    /**
+     * 機能ID
+     */
+    functionIds: number[];
+  }
 
-  interface EmployeeDataMngInfoDto { }
+  /**
+ * Dto 連絡先情報の設定
+ */
+  interface SettingContactInformationDto {
+    /**
+   * ダイヤルイン番号
+   */
+    dialInNumber: ContactSettingDto;
 
+    /**
+     * 会社メールアドレス
+     */
+    companyEmailAddress: ContactSettingDto;
 
-  interface EmployeeContactDto { }
+    /**
+     * 会社携帯メールアドレス
+     */
+    companyMobileEmailAddress: ContactSettingDto;
 
+    /**
+     * 個人メールアドレス
+     */
+    personalEmailAddress: ContactSettingDto;
 
-  interface PasswordChangeLogDto { }
+    /**
+     * 個人携帯メールアドレス
+     */
+    personalMobileEmailAddress: ContactSettingDto;
 
+    /**
+     * 内線番号
+     */
+    extensionNumber: ContactSettingDto;
 
-  interface AnniversaryNoticeDto { }
+    /**
+     * 携帯電話（会社用）
+     */
+    companyMobilePhone: ContactSettingDto;
 
+    /**
+     * 携帯電話（個人用）
+     */
+    personalMobilePhone: ContactSettingDto;
 
-  interface UserAvatarDto { }
+    /**
+     * 緊急電話番号1
+     */
+    emergencyNumber1: ContactSettingDto;
+
+    /**
+     * 緊急電話番号2
+     */
+    emergencyNumber2: ContactSettingDto;
+
+    /**
+     * 他の連絡先
+     */
+    otherContacts: OtherContactDto[];
+  }
+
+  /**
+ * Dto 連絡先の設定
+ */
+  interface ContactSettingDto {
+    /**
+   * 連絡先利用設定
+   */
+    contactUsageSetting: number;
+
+    /**
+     * 更新可能
+     */
+    updatable: number;
+  }
+
+  /**
+   * Dto 他の連絡先
+   */
+  interface OtherContactDto {
+    /**
+   * NO
+   */
+    no: number;
+
+    /**
+     * 連絡先利用設定
+     */
+    contactUsageSetting: number;
+
+    /**
+     * 連絡先名
+     */
+    contactName: string;
+  }
+
+  /**
+ * Dto ユーザ
+ */
+  interface UserDto {
+    /**
+    * ユーザID
+    */
+    userId: string;
+
+    /**
+     * デフォルトユーザ
+     */
+    defaultUser: boolean;
+
+    /**
+     * パスワード
+     */
+    password: string;
+
+    /**
+     * ログインID
+     */
+    loginID: string;
+
+    /**
+     * 契約コード
+     */
+    contractCode: string;
+
+    /**
+     * 有効期限
+     */
+    expirationDate: string;
+
+    /**
+     * 特別利用者
+     */
+    specialUser: number;
+
+    /**
+     * 複数会社を兼務する
+     */
+    multiCompanyConcurrent: number;
+
+    /**
+     * メールアドレス
+     */
+    mailAddress: string;
+
+    /**
+     * ユーザ名
+     */
+    userName: string;
+
+    /**
+     * 紐付け先個人ID
+     */
+    associatedPersonID: string;
+
+    /**
+     * パスワード状態
+     */
+    passStatus: number;
+
+    /**
+     * 言語
+     */
+    language: number;
+  }
+
+  /**
+   * Dto 個人
+   */
+  interface PersonDto {
+    /**
+     * 個人ID
+     */
+    personId: string;
+
+    /**
+     * 生年月日
+     */
+
+    birthDate: string;
+
+    /**
+     * 血液型
+     */
+    bloodType: number;
+
+    /**
+     * 性別
+     */
+    gender: number;
+
+    /**
+     * 個人名グループ
+     */
+    personNameGroup: PersonNameGroupDto;
+  }
+
+  /**
+ * Dto 個人名グループ
+ */
+  interface PersonNameGroupDto {
+    /**
+    * ビジネスネーム
+    */
+    businessName: string;
+
+    /**
+     * ビジネスネームカナ
+     */
+    businessNameKana: string;
+
+    /**
+     * ビジネスネームその他
+     */
+    businessOtherName: string;
+
+    /**
+     * ビジネスネーム英語
+     */
+    businessEnglishName: string;
+
+    /**
+     * 個人名
+     */
+    personName: FullNameSetDto;
+
+    /**
+     * 個人名多言語
+     */
+    PersonalNameMultilingual: FullNameSetDto;
+
+    /**
+     * 個人名ローマ字
+     */
+    personRomanji: FullNameSetDto;
+
+    /**
+     * 個人届出名称
+     */
+    todokedeFullName: FullNameSetDto;
+
+    /**
+     * 個人旧氏名
+     */
+    oldName: FullNameSetDto;
+  }
+
+  interface FullNameSetDto {
+    /**
+   * 氏名
+   */
+    fullName: string;
+
+    /**
+     * 氏名カナ
+     */
+    fullNameKana: string;
+  }
+
+  /**
+   * Dto 個人連絡先
+   */
+  interface PersonalContactDto {
+    /**
+   * 個人ID
+   */
+    personalId: string;
+
+    /**
+     * メールアドレス
+     */
+    mailAddress: string;
+
+    /**
+     * メールアドレスが在席照会に表示するか
+     */
+    isMailAddressDisplay: boolean;
+
+    /**
+     * 携帯メールアドレス
+     */
+    mobileEmailAddress: string;
+
+    /**
+     * 携帯メールアドレスが在席照会に表示するか
+     */
+    isMobileEmailAddressDisplay: boolean;
+
+    /**
+     * 携帯電話番号
+     */
+    phoneNumber: string;
+
+    /**
+     * 携帯電話番号が在席照会に表示するか
+     */
+    isPhoneNumberDisplay: boolean;
+
+    /**
+     * 緊急連絡先１
+     */
+    emergencyContact1: EmergencyContactDto;
+
+    /**
+     * 緊急連絡先１が在席照会に表示するか
+     */
+    isEmergencyContact1Display: boolean;
+
+    /**
+     * 緊急連絡先２
+     */
+    emergencyContact2: EmergencyContactDto;
+
+    /**
+     * 緊急連絡先２が在席照会に表示するか
+     */
+    isEmergencyContact2Display: boolean;
+
+    /**
+     * 他の連絡先
+     */
+    otherContacts: OtherContactDtoPs[];
+  }
+
+  /**
+   * Dto 緊急連絡先
+   */
+  interface EmergencyContactDto {
+    /**
+      * メモ
+      */
+    remark: string;
+
+    /**
+     * 連絡先名
+     */
+    contactName: string;
+
+    /**
+     * 電話番号
+     */
+    phoneNumber: string;
+  }
+
+  /**
+   * Dto 他の連絡先
+   */
+  interface OtherContactDtoPs {
+    /**
+    * NO
+    */
+    otherContactNo: number;
+
+    /**
+     * 在席照会に表示するか
+     */
+    isDisplay: boolean;
+
+    /**
+     * 連絡先のアドレス
+     */
+    address: string;
+  }
+
+  /**
+ * Dto 社員データ管理情報
+ */
+  interface EmployeeDataMngInfoDto {
+    /**
+    * 会社ID
+    */
+    companyId: string;
+
+    /**
+     * 個人ID
+     */
+    personId: string;
+
+    /**
+     * 社員ID
+     */
+    employeeId: string;
+
+    /**
+     * 社員コード
+     */
+    employeeCode: string;
+
+    /**
+     * 削除状況
+     */
+    deletedStatus: number;
+
+    /**
+     * 一時削除日時
+     */
+    deleteDateTemporary: string;
+
+    /**
+     * 削除理由
+     */
+    removeReason: string;
+
+    /**
+     * 外部コード
+     */
+    externalCode: string;
+  }
+
+  /**
+   * Dto 社員連絡先
+   */
+  interface EmployeeContactDto {
+    /**
+   * 社員ID
+   */
+    employeeId: string;
+
+    /**
+     * メールアドレス
+     */
+    mailAddress: string;
+
+    /**
+     * メールアドレスが在席照会に表示するか
+     */
+    isMailAddressDisplay: boolean;
+
+    /**
+     * 座席ダイヤルイン
+     */
+    seatDialIn: string;
+
+    /**
+     * 座席ダイヤルインが在席照会に表示するか
+     */
+    isSeatDialInDisplay: boolean;
+
+    /**
+     * 座席内線番号
+     */
+    seatExtensionNumber: string;
+
+    /**
+     * 座席内線番号が在席照会に表示するか
+     */
+    isSeatExtensionNumberDisplay: boolean;
+
+    /**
+     * 携帯メールアドレス
+     */
+    mobileMailAddress: string;
+
+    /**
+     * 携帯メールアドレスが在席照会に表示するか
+     */
+    isMobileMailAddressDisplay: boolean;
+
+    /**
+     * 携帯電話番号
+     */
+    cellPhoneNumber: string;
+
+    /**
+     * 携帯電話番号が在席照会に表示するか
+     */
+    isCellPhoneNumberDisplay: boolean;
+  }
+
+  /**
+   * Dto パスワード変更ログ
+   */
+  interface PasswordChangeLogDto {
+    /**
+   * ログID
+   */
+    logId: string;
+
+    /**
+     * ユーザID
+     */
+    userId: string;
+
+    /**
+     * 変更日時
+     */
+    modifiedDate: string;
+
+    /**
+     * パスワード
+     */
+    password: string;
+  }
+
+  /**
+   * Dto 個人の記念日情報
+   */
+  interface AnniversaryNoticeDto {
+    /**
+    * 個人ID
+    */
+    personalId: string;
+
+    /**
+     * 日数前の通知
+     */
+    noticeDay: number;
+
+    /**
+     * 最後見た記念日
+     */
+    seenDate: string;
+
+    /**
+     * 記念日
+     */
+    anniversary: string;
+
+    /**
+     * 記念日のタイトル
+     */
+    anniversaryTitle: string;
+
+    /**
+     * 記念日の内容
+     */
+    notificationMessage: string;
+  }
+
+  /**
+   * Dto 個人の顔写真
+   */
+  interface UserAvatarDto {
+    /**
+   * 個人ID
+   */
+    personalId: string;
+
+    /**
+     * 顔写真ファイルID
+     */
+    fileId: string;
+  }
 
 
 }
