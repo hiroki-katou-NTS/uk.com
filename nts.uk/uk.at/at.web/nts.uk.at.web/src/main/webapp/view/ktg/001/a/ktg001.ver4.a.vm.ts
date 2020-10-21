a961715e68a819845424a70e6721759d13586802
Thieu Thanh Tu <tu_tt@nittsusystem-vn.com>
2020-10-21 08:57:55 +0700

    update code màn A

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ktg/001/a/ktg001.ver4.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ktg/001/a/ktg001.ver4.a.vm.ts
@@ -65,29 +65,29 @@ module nts.uk.at.view.ktg001.a {
 	@bean()
 	class ViewModel extends ko.ViewModel {
 
-		title: KnockoutObservable<string> = ko.observable('あなたの承認状況');
-		appText: KnockoutObservable<string> = ko.observable('あり');
-		dayText: KnockoutObservable<string> = ko.observable('あり');
-		monText: KnockoutObservable<string> = ko.observable('あり');
-		aggrText: KnockoutObservable<string> = ko.observable('あり');
+		title: KnockoutObservable<string> = ko.observable('');
+		appText: KnockoutObservable<string> = ko.observable('');
+		dayText: KnockoutObservable<string> = ko.observable('');
+		monText: KnockoutObservable<string> = ko.observable('');
+		aggrText: KnockoutObservable<string> = ko.observable('');
 		selectedSwitch: KnockoutObservable<number> = ko.observable(1);
-		
-		appRowVisible: KnockoutObservable<Boolean> = ko.observable(true);
-		dayRowVisible: KnockoutObservable<Boolean> = ko.observable(true);
-		monRowVisible: KnockoutObservable<Boolean> = ko.observable(true);
-		aggrRowVisible: KnockoutObservable<Boolean> = ko.observable(true);
-		
-		appIconVisible: KnockoutObservable<Boolean> = ko.observable(true);
-		dayIconVisible: KnockoutObservable<Boolean> = ko.observable(true);
-		monIconVisible: KnockoutObservable<Boolean> = ko.observable(true);
-		aggrIconVisible: KnockoutObservable<Boolean> = ko.observable(true);
+
+		appRowVisible: KnockoutObservable<Boolean> = ko.observable(false);
+		dayRowVisible: KnockoutObservable<Boolean> = ko.observable(false);
+		monRowVisible: KnockoutObservable<Boolean> = ko.observable(false);
+		aggrRowVisible: KnockoutObservable<Boolean> = ko.observable(false);
+
+		appIconVisible: KnockoutObservable<Boolean> = ko.observable(false);
+		dayIconVisible: KnockoutObservable<Boolean> = ko.observable(false);
+		monIconVisible: KnockoutObservable<Boolean> = ko.observable(false);
+		aggrIconVisible: KnockoutObservable<Boolean> = ko.observable(false);
 
 		created() {
 
 		}
-		
+
 		mounted() {
-			/*let vm = this;
+			let vm = this;
 			let cacheCcg008 = windows.getShared("cache");
 			let closureId = 1;
 
@@ -102,9 +102,9 @@ module nts.uk.at.view.ktg001.a {
 			} else {
 				vm.selectedSwitch(cacheCcg008.currentOrNextMonth);
 				closureId = cacheCcg008.closureId;
-			}*/
-			
-			/*vm.$ajax(KTG001_API.GET_APPROVED_DATA_EXCECUTION, param).done((data: IResponse) => {
+			}
+
+			vm.$ajax(KTG001_API.GET_APPROVED_DATA_EXCECUTION, param).done((data: IResponse) => {
 				if (data) {
 					let approvedDataExecution = data.approvedDataExecutionResultDto;
 					let approvalProcessingUse = data.approvalProcessingUseSetting;
@@ -114,32 +114,32 @@ module nts.uk.at.view.ktg001.a {
 					approvedDataExecution.approvedAppStatusDetailedSettings.forEach(i => {
 						if (i.item == ApprovedApplicationStatusItem.APPLICATION_DATA && i.displayType == NotUseAtr.USE) {
 							vm.appRowVisible(true);
-							vm.appText = approvedDataExecution.appDisplayAtr == true ? ko.observable(vm.$i18n('KTG001_5')) : ko.observable(vm.$i18n('KTG001_6'));
+							vm.appText(approvedDataExecution.appDisplayAtr == true ? vm.$i18n('KTG001_5') : vm.$i18n('KTG001_6'));
 							vm.appIconVisible(approvedDataExecution.appDisplayAtr);
 						}
 
 						if (i.item == ApprovedApplicationStatusItem.DAILY_PERFORMANCE_DATA && i.displayType == NotUseAtr.USE && approvalProcessingUse.useDayApproverConfirm == true) {
 							vm.dayRowVisible(true);
-							vm.dayText = approvedDataExecution.dayDisplayAtr == true ? ko.observable(vm.$i18n('KTG001_5')) : ko.observable(vm.$i18n('KTG001_6'));
+							vm.dayText(approvedDataExecution.dayDisplayAtr == true ? vm.$i18n('KTG001_5') : vm.$i18n('KTG001_6'));
 							vm.dayIconVisible(approvedDataExecution.dayDisplayAtr);
 						}
 
 						if (i.item == ApprovedApplicationStatusItem.MONTHLY_RESULT_DATA && i.displayType == NotUseAtr.USE && approvalProcessingUse.useMonthApproverConfirm == true) {
 							vm.monRowVisible(true);
-							vm.monText = approvedDataExecution.monthDisplayAtr == true ? ko.observable(vm.$i18n('KTG001_5')) : ko.observable(vm.$i18n('KTG001_6'));
+							vm.monText(approvedDataExecution.monthDisplayAtr == true ? vm.$i18n('KTG001_5') : vm.$i18n('KTG001_6'));
 							vm.monIconVisible(approvedDataExecution.monthDisplayAtr);
 						}
 
 						if (i.item == ApprovedApplicationStatusItem.AGREEMENT_APPLICATION_DATA && i.displayType == NotUseAtr.USE) {
 							vm.aggrRowVisible(true);
-							vm.aggrText = approvedDataExecution.aggrDisplayAtr == true ? ko.observable(vm.$i18n('KTG001_5')) : ko.observable(vm.$i18n('KTG001_6'));
+							vm.aggrText(approvedDataExecution.aggrDisplayAtr == true ? vm.$i18n('KTG001_5') : vm.$i18n('KTG001_6'));
 							vm.aggrIconVisible(approvedDataExecution.aggrDisplayAtr);
 						}
 
 					})
 				}
 
-			}).always(() => vm.$blockui("clear"));*/
+			}).always(() => vm.$blockui("clear"));
 
 		}
 
@@ -157,9 +157,11 @@ module nts.uk.at.view.ktg001.a {
 
 		aggrementApproval() {
 		}
+
 		
-		setting(){
-			nts.uk.ui.windows.sub.modal('at','/view/ktg/001/b/index_ver4.xhtml');
+		setting() {
+			//windows.top.location = windows.location.origin + '/nts.uk.at.web/view/ktg/001/b/index_ver4.xhtml';
+			nts.uk.ui.windows.sub.modal('at', '/view/ktg/001/b/index_ver4.xhtml');
 		}
 
 
