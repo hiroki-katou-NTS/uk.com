a41d04c3a7489fc51e05cb21b892308d79a641e4
nws-ductm <ductm@newwave.vn>
2020-10-22 17:46:46 +0700

    Update primitive

--- nts.uk/uk.com/com.ctx/sys/portal/nts.uk.ctx.sys.portal.dom/src/main/java/nts/uk/ctx/sys/portal/dom/flowmenu/LabelContent.java
+++ nts.uk/uk.com/com.ctx/sys/portal/nts.uk.ctx.sys.portal.dom/src/main/java/nts/uk/ctx/sys/portal/dom/flowmenu/LabelContent.java
@@ -6,7 +6,7 @@ import nts.arc.primitive.constraint.StringMaxLength;
 /**
  * UKDesign.ドメインモデル.NittsuSystem.UniversalK.システム.ポータル.トップページの部品.フローメニュー作成.ラベル内容
  */
-@StringMaxLength(100)
+@StringMaxLength(200)
 public class LabelContent extends StringPrimitiveValue<LabelContent> {
 
 	private static final long serialVersionUID = 1L;
