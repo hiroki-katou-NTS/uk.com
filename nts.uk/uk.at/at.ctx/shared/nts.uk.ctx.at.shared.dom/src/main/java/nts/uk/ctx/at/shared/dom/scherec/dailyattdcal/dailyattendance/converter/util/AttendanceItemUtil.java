12111ce442fc14ba1e6658f688fb4faffd47852a
NTS-NguyenXuanThanh <thanh_nx@nittsusystem.co.jp>
2020-10-22 18:05:16 +0900

    fix bug  kdw/003 error register
    (cherry picked from commit 610889d478e09a14e17351b5f6ab9692d861e917)

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/scherec/dailyattdcal/dailyattendance/converter/util/AttendanceItemUtil.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/scherec/dailyattdcal/dailyattendance/converter/util/AttendanceItemUtil.java
@@ -404,6 +404,7 @@ public class AttendanceItemUtil implements ItemConst {
 						.orElse(null);
 
 				if (itemValue != null) {
+					itemValue.valueType(valueAnno.type());
 
 					if (!valueAnno.setValueWith().isEmpty()) {
 						callSetMethod(attendanceItems, valueAnno, itemValue);
