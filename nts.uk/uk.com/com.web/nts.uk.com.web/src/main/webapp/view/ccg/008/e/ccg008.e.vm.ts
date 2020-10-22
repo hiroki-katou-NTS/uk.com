2791b74164f243fb5a0d63e7fe35783c8e17598f
hieutt <hieutt@newwave.vn>
2020-10-22 15:18:17 +0700

    Coding UI screen A and E

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/008/e/ccg008.e.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/008/e/ccg008.e.vm.ts
@@ -2,8 +2,19 @@
 module nts.uk.at.view.kdl048.screenModel {
   @bean()
   export class ViewModel extends ko.ViewModel {
+    itemListCb: KnockoutObservableArray<ItemModel> = ko.observableArray([]);
+    selectedCode: KnockoutObservable<string> = ko.observable('');
+    isEnable: KnockoutObservable<boolean> = ko.observable(true);
+    isEditable: KnockoutObservable<boolean> = ko.observable(false);
+    isRequired: KnockoutObservable<boolean> = ko.observable(false);
+    selectFirstIfNull: KnockoutObservable<boolean> = ko.observable(false); 
     created(){
-
+      const vm = this;
+      vm.itemListCb = ko.observableArray([
+        new ItemModel('1', ''),
+        new ItemModel('2', ''),
+        new ItemModel('3', '')
+    ]);
     }
     mounted(){
 
@@ -13,6 +24,16 @@ module nts.uk.at.view.kdl048.screenModel {
     }
     onClickCancel(){
       this.$window.close()
+    }
+  }
+
+  export class ItemModel {
+    code: string;
+    name: string;
+
+    constructor(code: string, name: string) {
+        this.code = code;
+        this.name = name;
     }
   }
 }
