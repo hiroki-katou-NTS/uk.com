cbf9265a2368d8b87c8ac7c03f686245004512d4
Lê Hữu Đạt <dat.lh@3si.vn>
2020-10-22 16:15:19 +0700

    update domain service AppApproval, JpaAgreementMonthSettingRepository

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.infra/src/main/java/nts/uk/ctx/at/record/infra/repository/standardtime/JpaAgreementMonthSettingRepository.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.infra/src/main/java/nts/uk/ctx/at/record/infra/repository/standardtime/JpaAgreementMonthSettingRepository.java
@@ -184,7 +184,7 @@ public class JpaAgreementMonthSettingRepository extends JpaRepository implements
 		return this.queryProxy()
 				.query(FIND_BY_KEY, KmkmtAgreementMonthSet.class)
 				.setParameter("employeeId", employeeId)
-				.setParameter("yearMonth", yearMonth).getSingle(x -> toDomain(x));
+				.setParameter("yearmonthValue", yearMonth.v()).getSingle(x -> toDomain(x));
 	}
 
 	private static AgreementMonthSetting toDomain(KmkmtAgreementMonthSet kmkmtAgreementMonthSet) {
