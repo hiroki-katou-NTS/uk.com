0d43bdc99d04e80a69ddb3fae5096fc9adb2958c
NSC-hirokoMiura <hiroko_miura@nittsusystem.co.jp>
2020-10-27 13:13:10 +0900

    不要なimportを削除

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/test/java/nts/uk/ctx/at/schedule/dom/shift/management/schedulecounter/laborcostandtime/WorkplaceCounterLaborCostAndTimeTest.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/test/java/nts/uk/ctx/at/schedule/dom/shift/management/schedulecounter/laborcostandtime/WorkplaceCounterLaborCostAndTimeTest.java
@@ -8,9 +8,6 @@ import java.util.Map;
 import org.junit.Test;
 
 import nts.arc.testing.assertion.NtsAssert;
-import nts.uk.ctx.at.schedule.dom.shift.management.schedulecounter.laborcostandtime.LaborCostAndTime;
-import nts.uk.ctx.at.schedule.dom.shift.management.schedulecounter.laborcostandtime.LaborCostAndTimeType;
-import nts.uk.ctx.at.schedule.dom.shift.management.schedulecounter.laborcostandtime.WorkplaceCounterLaborCostAndTime;
 import nts.uk.shr.com.enumcommon.NotUseAtr;
 
 public class WorkplaceCounterLaborCostAndTimeTest {
