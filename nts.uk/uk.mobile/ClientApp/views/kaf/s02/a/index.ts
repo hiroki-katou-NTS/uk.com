7cb6473f0c2102cd733f3ab25eda41743c47a4dd
NguyenMauAnh <twas.horus@gmail.com>
2020-10-28 23:28:00 +0700

    refactor 5: KAFS02: Fix hien thi truong hop co data man chinh sua

--- nts.uk/uk.mobile/ClientApp/views/kaf/s02/a/index.ts
+++ nts.uk/uk.mobile/ClientApp/views/kaf/s02/a/index.ts
@@ -293,6 +293,26 @@ export class KafS02AComponent extends KafS00ShrComponent {
         self.$auth.user.then((usr: any) => {
             self.user = usr;
         }).then(() => {
+
+            if (!self.mode && !self.condition5) {
+                self.workHourLst = [];
+            }
+            if (!self.mode && !self.condition4) {
+                self.tempWorkHourLst = [];
+            }
+            if (!self.mode && !self.condition6) {
+                self.goOutLst = [];
+            }
+            if (!self.mode && !self.condition7) {
+                self.breakLst = [];
+            }
+            if (!self.mode && !self.condition8) {
+                self.childCareLst = [];
+            }
+            if (!self.mode && !self.condition9) {
+                self.longTermLst = [];
+            }
+
             self.bindData(self.data);
             self.fetchErrorLst(self.data.errorListOptional);
             self.mappingApplication();
