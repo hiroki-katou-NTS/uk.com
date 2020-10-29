5925ff2e78b95060e65abe724b4e1e1769a16ccd
Thoang Le <thoang.lc@3si.vn>
2020-10-29 10:42:34 +0700

    updat code screen  A - KAL-013

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kal/013/a/kal013.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kal/013/a/kal013.a.vm.ts
@@ -16,6 +16,8 @@ module nts.uk.at.view.kal013.a {
     currentAlarm: KnockoutObservable<any>;
     tabs: KnockoutObservableArray<nts.uk.ui.NtsTabPanelModel>;
 
+    alarmArrangeList: KnockoutObservableArray<AlarmDto> = ko.observableArray([]);
+
     constructor(params: any) {
       super();
       const vm = this;
@@ -24,10 +26,11 @@ module nts.uk.at.view.kal013.a {
       vm.alarmListItems.push( new Alarm('002', '文字列') );
 
       vm.alarmListItems(_.orderBy(vm.alarmListItems(), 'code', 'asc'));
-      vm.selectedAlarm(vm.findAlarmSelected('001'));
+      vm.selectedAlarm(vm.findItemSelected('001', vm.alarmListItems()));
 
       vm.categoryList.push( new Alarm('001', 'アラームリストのカテゴリ') );
       vm.categoryList.push( new Alarm('002', 'ムリストのカテゴリ') );
+      vm.selectedCategory(vm.findItemSelected('001', vm.categoryList()));
 
       vm.currentAlarm = ko.observable({
         code: ko.observable('001'),
@@ -35,12 +38,20 @@ module nts.uk.at.view.kal013.a {
       });
 
       vm.tabs = ko.observableArray([
-        {id: 'tab-1', title: vm.$i18n('KAL013_15'), content: '.tab-content-1', enable: ko.observable(true), visible: ko.observable(true)}        
+        { 
+          id: 'tab-1', 
+          title: vm.$i18n('KAL013_15'), 
+          content: '.tab-content-1', 
+          enable: ko.observable(true), 
+          visible: ko.observable(true)
+        }
       ]);
 
       vm.selectedAll.subscribe((newValue) => {
         if( newValue === null ) return;
       });
+
+      vm.getAlarmArrangeList();
     }
 
     created(params: any) {
@@ -49,18 +60,29 @@ module nts.uk.at.view.kal013.a {
 
     mounted() {
       const vm = this;
+
+      
+      $("#fixed-table").ntsFixedTable({ height: 350 });
     }
 
-    findAlarmSelected( code: string ) {
+    findItemSelected( code: string, seachArr: Array<any> ) : any {
       const vm = this;
-      let findAlarm = _.find(vm.alarmListItems(), ['code', code]);
-      return findAlarm;
+      if( !_.isArray(seachArr)) return null;
+      let found = _.find(seachArr, ['code', code]);
+      return ( !_.isNil(found)) ? found : null;
     }
 
     openScreenB() {
       const vm = this;
       //vm.$window.storage('');
       vm.$window.modal('/view/kal/013/b/index.xhtml').then(() => {});
+    }
+
+    getAlarmArrangeList() {
+      const vm = this;
+      for( let i = 0; i< 20; i++ ) {
+        vm.alarmArrangeList.push( new AlarmDto(false, true, '名称 ' + (i + 1), '表示するメッセージ ' + (i + 1)));
+      }
     }
   }
 
@@ -80,6 +102,19 @@ module nts.uk.at.view.kal013.a {
     constructor( code?: string,  name?: string) {
       this.code = code;
       this.name = name;
+    }
+  }
+
+  export class  AlarmDto {
+    isChecked: KnockoutObservable<boolean> = ko.observable(false);    
+    register: KnockoutObservable<boolean> = ko.observable(false); 
+    name: KnockoutObservable<string> = ko.observable(null); 
+    message: KnockoutObservable<string> = ko.observable(null); 
+    constructor( isChecked?: boolean, register?:boolean, name?:string, message?:string) {
+      this.isChecked(isChecked);
+      this.register(register);
+      this.name(name);
+      this.message(message);
     }
   }
 }
