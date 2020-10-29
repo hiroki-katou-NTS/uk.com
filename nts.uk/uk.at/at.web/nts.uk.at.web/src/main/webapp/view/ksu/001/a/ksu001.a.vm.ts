bdb1f42a50687c806b06b08efb245c994b1ddca0
NSVN-TruongVanLai <NSVN-TruongVanLai@users.noreply.github.com>
2020-10-29 17:58:38 +0700

    update event open dialogS

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksu/001/a/ksu001.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksu/001/a/ksu001.a.vm.ts
@@ -3049,11 +3049,13 @@ module nts.uk.at.view.ksu001.a.viewmodel {
             let self = this;
             //hiện giờ truyền sang workplaceId va tất cả emmployee . Sau này sửa truyền list employee theo workplace id
             setShared("baseDate", ko.observable(self.dateTimeAfter()));
+            
+            let listEmpId = _.map(self.listEmpData, 'id');
 
             // listEmpData : {id : '' , code : '', name : ''}
-            setShared('dataShareDialogG', {
-                endDate: moment(self.dtAft()).format('YYYY/MM/DD'),
-                listEmp: self.listEmpData
+            setShared('KSU001S', {
+                date: self.dtAft(),
+                listEmp: { listEmpId: listEmpId }
             });
 
             $('#A1_12_1').ntsPopup('hide');
