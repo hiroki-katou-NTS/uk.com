db868bb75be865b5ed40bc8abf55e2f69d105f0a
3SI-L129W-RAFIQ\rafiqul.islam <rafiqul.islam@3si.vn>
2020-10-30 18:19:13 +0700

    KAL014 A UI design done

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kal/014/a/kal014.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kal/014/a/kal014.a.vm.ts
@@ -5,22 +5,77 @@ module nts.uk.at.ksm008.a {
     @bean()
     export class KSM008AViewModel extends ko.ViewModel {
 
-        backButon: string = "#";
+        backButon: string = "/view/kal/013/a/index.xhtml";
         gridItems: KnockoutObservableArray<GridItem> = ko.observableArray([]);
         currentCode: KnockoutObservable<string> = ko.observable(null);
         alarmPattern: AlarmPattern = new AlarmPattern('', '');
         tabs: KnockoutObservableArray<nts.uk.ui.NtsTabPanelModel>;
         selectedTab: KnockoutObservable<string>;
+        isNewMode: KnockoutObservable<boolean>;
+        itemsSwap: KnockoutObservableArray<ItemModel>;
+        tableItems: KnockoutObservableArray<TableItem>;
+        columns: KnockoutObservableArray<nts.uk.ui.NtsGridListColumn>;
+        currentCodeListSwap: KnockoutObservableArray<any>;
+        test: KnockoutObservableArray<any>;
+        roundingRules: KnockoutObservableArray<any>;
+        selectedRuleCode: any;
+        clickTableItem = function (item: any) {
+            const vm = this;
+            console.log(item);
+        }
 
         constructor(props: any) {
             super();
-            const vm  = this;
+            const vm = this;
             vm.tabs = ko.observableArray([
-                {id: 'tab-1', title: getText('KAL014_17'), content: '.tab-content-1', enable: ko.observable(true), visible: ko.observable(true)},
-                {id: 'tab-2', title: getText('KAL014_18'), content: '.tab-content-2', enable: ko.observable(true), visible: ko.observable(true)},
-                {id: 'tab-3', title: getText('KAL014_19'), content: '.tab-content-3', enable: ko.observable(true), visible: ko.observable(true)}
+                {
+                    id: 'tab-1',
+                    title: getText('KAL014_17'),
+                    content: '.tab-content-1',
+                    enable: ko.observable(true),
+                    visible: ko.observable(true)
+                },
+                {
+                    id: 'tab-2',
+                    title: getText('KAL014_18'),
+                    content: '.tab-content-2',
+                    enable: ko.observable(true),
+                    visible: ko.observable(true)
+                },
+                {
+                    id: 'tab-3',
+                    title: getText('KAL014_19'),
+                    content: '.tab-content-3',
+                    enable: ko.observable(true),
+                    visible: ko.observable(true)
+                }
             ]);
             vm.selectedTab = ko.observable('tab-1');
+            vm.itemsSwap = ko.observableArray([]);
+            vm.tableItems = ko.observableArray([]);
+            var array = [];
+            for (var i = 0; i < 100; i++) {
+                array.push(new TableItem("マスタチェック(日別) " + i, "当月の締め開始日　～　当月の締め終了日 " + i));
+            }
+            this.tableItems(array);
+            array = [];
+            for (var i = 0; i < 100; i++) {
+                array.push(new ItemModel(i, '基本給', "カテゴリ"));
+            }
+            vm.itemsSwap(array);
+            vm.columns = ko.observableArray([
+                {headerText: getText('KAL014_22'), key: 'category', width: 70},
+                {headerText: getText('KAL014_23'), key: 'code', width: 70},
+                {headerText: getText('KAL014_24'), key: 'name', width: 180}
+            ]);
+            vm.currentCodeListSwap = ko.observableArray([]);
+            vm.test = ko.observableArray([]);
+            vm.roundingRules = ko.observableArray([
+                {code: '1', name: getText('KAL014_30')},
+                {code: '2', name: getText('KAL014_31')}
+            ]);
+            vm.selectedRuleCode = ko.observable(1);
+            vm.isNewMode=ko.observable(true);
         }
 
         created() {
@@ -34,13 +89,60 @@ module nts.uk.at.ksm008.a {
 
         }
 
+        remove() {
+            this.itemsSwap.shift();
+        }
+
         clickNewButton() {
+            const vm = this;
+            vm.isNewMode(true);
+            vm.$errors("clear", ".nts-editor", "button").then(() => {
+                $("#A3_2").focus();
+                vm.currentCode("");
+                vm.cleanInput();
+            });
+        }
+
+        cleanInput(){
+            const vm = this;
+            vm.alarmPattern.code("");
+            vm.alarmPattern.name("");
         }
 
         clickRegister() {
+            const vm=this;
+            vm.isNewMode(false);
         }
 
         clickDeleteButton() {
+        }
+
+        clickConfiguration() {
+            alert("Configuration");
+        }
+    }
+
+    class ItemModel {
+        code: number;
+        name: string;
+        category: string;
+        deletable: boolean;
+
+        constructor(code: number, name: string, category: string) {
+            this.code = code;
+            this.name = name;
+            this.category = category;
+            this.deletable = code % 3 === 0;
+        }
+    }
+
+    class TableItem {
+        category: string;
+        extractionPeriod: string;
+
+        constructor(category: string, extractionPeriod: string) {
+            this.category = category;
+            this.extractionPeriod = extractionPeriod;
         }
     }
 
@@ -54,7 +156,7 @@ module nts.uk.at.ksm008.a {
         }
     }
 
-    class AlarmPattern{
+    class AlarmPattern {
         /** コード */
         code: KnockoutObservable<string>;
         /** 名称 */
