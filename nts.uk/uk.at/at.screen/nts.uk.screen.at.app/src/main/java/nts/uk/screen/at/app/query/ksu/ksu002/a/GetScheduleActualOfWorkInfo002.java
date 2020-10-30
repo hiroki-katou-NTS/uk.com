ff8ac4563b846ec30f6dbaa6250c1b0ad82e2da0
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2020-10-30 14:30:18 +0700

    refactor & fix code

--- nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/query/ksu/ksu002/a/GetScheduleActualOfWorkInfo002.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/query/ksu/ksu002/a/GetScheduleActualOfWorkInfo002.java
@@ -12,52 +12,35 @@ import nts.uk.screen.at.app.query.ksu.ksu002.a.dto.WorkScheduleWorkInforDto;
 import nts.uk.screen.at.app.query.ksu.ksu002.a.input.DisplayInWorkInfoInput;
 
 /**
- * 
- * @author chungnt ScreenQuery : 予定・実績を取得する
- *
+ * @author chungnt 
+ * ScreenQuery: 予定・実績を取得する
  */
 
 @Stateless
 public class GetScheduleActualOfWorkInfo002 {
-
-	@Inject
-	private GetScheduleOfWorkInfo002 getScheduleOfWorkInfo002;
 	@Inject
 	private GetWorkRecord getWorkRecord;
 
-	public List<WorkScheduleWorkInforDto> getDataScheduleAndAactualOfWorkInfo(DisplayInWorkInfoInput param) {
+	@Inject
+	private GetScheduleOfWorkInfo002 getScheduleOfWorkInfo002;
 
+	public List<WorkScheduleWorkInforDto> getDataScheduleAndAactualOfWorkInfo(DisplayInWorkInfoInput param) {
 		// lay data Schedule
 		List<WorkScheduleWorkInforDto> listDataSchedule = getScheduleOfWorkInfo002.getDataScheduleOfWorkInfo(param);
 
 		if (param.getActualData()) {
 			// lay data Daily
 			List<WorkScheduleWorkInforDto> listDataDaily = getWorkRecord.get(param);
-			// merge
-			List<WorkScheduleWorkInforDto> listToRemove = new ArrayList<WorkScheduleWorkInforDto>();
-			List<WorkScheduleWorkInforDto> listToAdd = new ArrayList<WorkScheduleWorkInforDto>();
 			
-			for (WorkScheduleWorkInforDto dataSchedule : listDataSchedule) {
-				String sid = dataSchedule.employeeId;
-				GeneralDate date = dataSchedule.date;
-				
-				Optional<WorkScheduleWorkInforDto> dataDaily = listDataDaily.stream().filter(data -> {
-					if (data.employeeId.equals(sid) && data.date.equals(date))
-						return true;
-					return false;
-				}).findFirst();
-				
-				if (dataDaily.isPresent()) {
-					listToRemove.add(dataSchedule);
-					listToAdd.add(dataDaily.get());
+			for (WorkScheduleWorkInforDto ds : listDataSchedule) {				
+				if (!listDataDaily.stream().anyMatch(c -> c.employeeId.equals(ds.employeeId) && c.date.equals(ds.date))) {
+					listDataDaily.add(ds);
 				}
 			}
 			
-			listDataSchedule.removeAll(listToRemove);
-			listDataSchedule.addAll(listToAdd);
+			return listDataDaily;
 		}
 
 		return listDataSchedule;
 	}
-
 }
