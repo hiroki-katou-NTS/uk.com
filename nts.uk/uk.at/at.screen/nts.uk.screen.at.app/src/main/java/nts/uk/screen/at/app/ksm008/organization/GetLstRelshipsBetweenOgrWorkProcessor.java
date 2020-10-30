a42a92da692619da57c19edc766f3d63694264aa
quang.nh1 <huyquang.nhq@gmail.com>
2020-10-30 12:55:59 +0700

    fix ut ksm008

--- nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/ksm008/organization/GetLstRelshipsBetweenOgrWorkProcessor.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/ksm008/organization/GetLstRelshipsBetweenOgrWorkProcessor.java
@@ -8,12 +8,13 @@ import nts.uk.ctx.at.shared.dom.workrule.organizationmanagement.workplace.Target
 import nts.uk.ctx.at.shared.dom.workrule.organizationmanagement.workplace.TargetOrganizationUnit;
 import nts.uk.ctx.at.shared.dom.worktime.worktimeset.WorkTimeSetting;
 import nts.uk.ctx.at.shared.dom.worktime.worktimeset.WorkTimeSettingRepository;
-import nts.uk.screen.at.app.ksm008.company.WorkingHoursDto;
+import nts.uk.screen.at.app.ksm008.company.WorkingHoursAndWorkMethodDto;
 import nts.uk.shr.com.context.AppContexts;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -30,23 +31,34 @@ public class GetLstRelshipsBetweenOgrWorkProcessor {
     @Inject
     private WorkTimeSettingRepository workTimeSettingRepository;
 
-    public List<WorkingHoursDto> getLstRelshipsBetweenOgrWork(RequestRelshipPrams requestPrams) {
+    public List<WorkingHoursAndWorkMethodDto> getLstRelshipsBetweenOgrWork(RequestRelshipPrams requestPrams) {
 
-        TargetOrgIdenInfor targetOrgIdenInfor = new TargetOrgIdenInfor(TargetOrganizationUnit.valueOf(requestPrams.getUnit()), Optional.of(requestPrams.getWorkplaceId()),Optional.of(requestPrams.getWorkplaceGroupId()));
+        TargetOrgIdenInfor targetOrgIdenInfor = new TargetOrgIdenInfor(TargetOrganizationUnit.valueOf(requestPrams.getUnit()), Optional.ofNullable(requestPrams.getWorkplaceId()),Optional.ofNullable(requestPrams.getWorkplaceGroupId()));
 
         //2:組織の勤務方法の関係性リストを取得する(会社ID, 対象組織情報): List<組織の勤務方法の関係性>
         List<WorkMethodRelationshipOrganization> organizations = workMethodRelationshipOrgRepo.getAll(AppContexts.user().companyId(),targetOrgIdenInfor);
-        List<String> listCodes = organizations.stream().
-                filter(x -> x.getWorkMethodRelationship().getPrevWorkMethod().getWorkMethodClassification() == WorkMethodClassfication.ATTENDANCE).
-                map(x -> ((WorkMethodAttendance)x.getWorkMethodRelationship().getPrevWorkMethod()).getWorkTimeCode().v()).collect(Collectors.toList());
-        List<WorkingHoursDto> detailDtos = new ArrayList<>();
-        List<WorkTimeSetting> workTimeSettings = workTimeSettingRepository.findByCodes(AppContexts.user().companyId(), listCodes);
-        workTimeSettings.forEach(x -> detailDtos.add(new WorkingHoursDto(
-                x.getWorktimeCode().v(),
-                x.getWorkTimeDisplayName().getWorkTimeName().v()
-        )));
 
-        return detailDtos;
+        HashMap<String, Integer> workHourCodeAndMethods = new HashMap<>();
+
+        organizations.stream().
+            filter(x -> x.getWorkMethodRelationship().getPrevWorkMethod().getWorkMethodClassification() == WorkMethodClassfication.ATTENDANCE).
+            map(x -> workHourCodeAndMethods.put(((WorkMethodAttendance) x.getWorkMethodRelationship().getPrevWorkMethod()).getWorkTimeCode().v(),
+                x.getWorkMethodRelationship().getPrevWorkMethod().getWorkMethodClassification().value)).collect(Collectors.toList());
+
+        List<WorkingHoursAndWorkMethodDto> workingHoursDtos = new ArrayList<>();
+        if (workHourCodeAndMethods.entrySet().size() > 0){
+            List<WorkTimeSetting> workTimeSettingList = workTimeSettingRepository.findByCodes(AppContexts.user().companyId(),
+                workHourCodeAndMethods.entrySet().stream().map(x -> x.getKey()).collect(Collectors.toList()));
+            workTimeSettingList.forEach(x -> {
+                workingHoursDtos.add(new WorkingHoursAndWorkMethodDto(
+                    x.getWorktimeCode().v(),
+                    x.getWorkTimeDisplayName().getWorkTimeName().v(),
+                    workHourCodeAndMethods.getOrDefault(x.getWorktimeCode().v(), 0)
+                ));
+            });
+        }
+
+        return workingHoursDtos;
     }
 
 }
