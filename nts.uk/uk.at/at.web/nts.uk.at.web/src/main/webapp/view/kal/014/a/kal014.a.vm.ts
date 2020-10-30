1bc12777493ca9bbddfc4d77ae1a3b1514d7220f
3SI-L129W-RAFIQ\rafiqul.islam <rafiqul.islam@3si.vn>
2020-10-30 09:19:25 +0700

    UI mock up KAL014 A

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kal/014/a/kal014.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kal/014/a/kal014.a.vm.ts
@@ -1,5 +1,5 @@
 module nts.uk.at.ksm008.a {
-
+    import getText = nts.uk.resource.getText;
     const PATH_API = {}
 
     @bean()
@@ -8,10 +8,19 @@ module nts.uk.at.ksm008.a {
         backButon: string = "#";
         gridItems: KnockoutObservableArray<GridItem> = ko.observableArray([]);
         currentCode: KnockoutObservable<string> = ko.observable(null);
+        alarmPattern: AlarmPattern = new AlarmPattern('', '');
+        tabs: KnockoutObservableArray<nts.uk.ui.NtsTabPanelModel>;
+        selectedTab: KnockoutObservable<string>;
 
         constructor(props: any) {
             super();
-
+            const vm  = this;
+            vm.tabs = ko.observableArray([
+                {id: 'tab-1', title: getText('KAL014_17'), content: '.tab-content-1', enable: ko.observable(true), visible: ko.observable(true)},
+                {id: 'tab-2', title: getText('KAL014_18'), content: '.tab-content-2', enable: ko.observable(true), visible: ko.observable(true)},
+                {id: 'tab-3', title: getText('KAL014_19'), content: '.tab-content-3', enable: ko.observable(true), visible: ko.observable(true)}
+            ]);
+            vm.selectedTab = ko.observable('tab-1');
         }
 
         created() {
@@ -43,5 +52,17 @@ module nts.uk.at.ksm008.a {
             this.code = code;
             this.name = name;
         }
+    }
+
+    class AlarmPattern{
+        /** コード */
+        code: KnockoutObservable<string>;
+        /** 名称 */
+        name: KnockoutObservable<string>;
+
+        constructor(code: string, name: string) {
+            this.code = ko.observable(code);
+            this.name = ko.observable(name);
+        };
     }
 }
