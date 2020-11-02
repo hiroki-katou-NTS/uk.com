c56ea6f81b1792b6fbeb85b71c7269cc84569933
Hop.NT <hop.nt@3si.vn>
2020-11-02 16:53:59 +0700

    using $vm

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/008/b/kmk008.b.start.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/008/b/kmk008.b.start.ts
@@ -2,12 +2,6 @@ module nts.uk.at.view.kmk008.b {
     __viewContext.ready(function() {
         let screenModels = new kmk008.b.viewmodel.ScreenModel();
 
-		const vm = screenModels;
-		_.extend(window, {vm});
-
-		// const kmk008b = nts.uk.at.view.kmk008.b;
-		// _.extend(window, {kmk008b});
-
         screenModels.startPage().done(function() {
             __viewContext.bind(screenModels);
         });
