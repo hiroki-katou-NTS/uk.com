34168d5cf59b09a2f76f8dfd9f0b6a93e5e334b5
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2020-11-03 17:07:30 +0700

    KMK004 - Add screen R

---
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/004/r/kmk004.r.vm.ts
@@ -0,0 +1,24 @@
+/// <reference path="../../../../lib/nittsu/viewcontext.d.ts" />
+
+module nts.uk.at.kmk004.r {
+	
+
+	const API = {
+		
+		REGISTER: '/at/record/stamp/employment/system/register-stamp-input'
+	};
+
+	@bean()
+	export class ViewModel extends ko.ViewModel {
+		
+
+		created() {
+			
+		}
+
+		mounted() {
+			
+		}
+	}
+	
+}
