c8dd7c52b6c311bc9fb7d64834c32e5f055f47fa
Thieu Thanh Tu <tu_tt@nittsusystem-vn.com>
2020-11-03 16:25:11 +0700

    change return data

--- nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/command/ktg/ktg001/ApproveStatusSettingCommandHandler.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/command/ktg/ktg001/ApproveStatusSettingCommandHandler.java
@@ -1,5 +1,6 @@
 package nts.uk.screen.at.app.command.ktg.ktg001;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,9 +9,11 @@ import javax.inject.Inject;
 
 import nts.arc.enums.EnumAdaptor;
 import nts.uk.ctx.sys.portal.dom.toppagepart.TopPagePartName;
+import nts.uk.ctx.sys.portal.dom.toppagepart.standardwidget.ApplicationStatusDetailedSetting;
 import nts.uk.ctx.sys.portal.dom.toppagepart.standardwidget.ApproveWidgetRepository;
 import nts.uk.ctx.sys.portal.dom.toppagepart.standardwidget.ApprovedAppStatusDetailedSetting;
 import nts.uk.ctx.sys.portal.dom.toppagepart.standardwidget.ApprovedApplicationStatusItem;
+import nts.uk.ctx.sys.portal.dom.toppagepart.standardwidget.DetailedWorkStatusSetting;
 import nts.uk.ctx.sys.portal.dom.toppagepart.standardwidget.StandardWidget;
 import nts.uk.ctx.sys.portal.dom.toppagepart.standardwidget.StandardWidgetType;
 import nts.uk.shr.com.context.AppContexts;
@@ -30,7 +33,7 @@ public class ApproveStatusSettingCommandHandler {
 				.map(m-> new ApprovedAppStatusDetailedSetting(EnumAdaptor.valueOf(m.getDisplayType(), NotUseAtr.class), 
 						EnumAdaptor.valueOf(m.getItem(), ApprovedApplicationStatusItem.class))).collect(Collectors.toList());
 		
-		StandardWidget standardWidget = new StandardWidget(companyId, "", null, new TopPagePartName(param.topPagePartName), null, null, null, settings, StandardWidgetType.APPROVE_STATUS, null );
+		StandardWidget standardWidget = new StandardWidget(companyId, "", null, new TopPagePartName(param.topPagePartName), null, null, new ArrayList<DetailedWorkStatusSetting>(), settings, StandardWidgetType.APPROVE_STATUS, new ArrayList<ApplicationStatusDetailedSetting>() );
 		
 		approveWidgetRepository.updateApproveStatus(standardWidget, companyId);
 	}
