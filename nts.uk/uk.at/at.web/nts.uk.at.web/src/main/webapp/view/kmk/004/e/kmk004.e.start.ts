3dd0b88b6ffa1d029e590854ddcd74308451653e
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2020-11-03 09:03:30 +0700

    Remove KMK004

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/004/e/kmk004.e.start.ts
+++
@@ -1,9 +0,0 @@
-module nts.uk.at.view.kmk004.e {
-    __viewContext.ready(function() {
-        var screenModel = new viewmodel.ScreenModel();
-        screenModel.startPage().done(function() {
-            __viewContext.bind(screenModel);
-            $('#checkbox-manage-display').focus();
-        });
-    });
-}
--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/004/h/kmk004.h.start.ts
+++
@@ -1,9 +0,0 @@
-module nts.uk.at.view.kmk004.h {
-    __viewContext.ready(function() {
-        var screenModel = new viewmodel.ScreenModel();
-        __viewContext.bind(screenModel);
-        _.defer(() => {
-            $('#combo-box').focus();
-        });
-    });
-}
