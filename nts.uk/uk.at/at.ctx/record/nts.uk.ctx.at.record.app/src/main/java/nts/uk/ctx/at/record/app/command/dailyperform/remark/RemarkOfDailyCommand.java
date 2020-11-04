d13345bf78fe963a9560d836ee911da06aef0225
NSC-DaikiIchioka <daiki_ichioka@nittsusystem.co.jp>
2020-11-04 17:22:00 +0900

    fix bug：日別修正で備考を入力して確定すると落ちる問題

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/dailyperform/remark/RemarkOfDailyCommand.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/dailyperform/remark/RemarkOfDailyCommand.java
@@ -8,6 +8,7 @@ import nts.uk.ctx.at.record.app.find.dailyperform.remark.dto.RemarksOfDailyDto;
 import nts.uk.ctx.at.record.dom.daily.remarks.RemarksOfDailyPerform;
 import nts.uk.ctx.at.shared.app.util.attendanceitem.DailyWorkCommonCommand;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.converter.util.item.ConvertibleAttendanceItem;
+import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.remarks.RemarksOfDailyAttd;
 
 public class RemarkOfDailyCommand extends DailyWorkCommonCommand {
 
@@ -24,7 +25,7 @@ public class RemarkOfDailyCommand extends DailyWorkCommonCommand {
 	@Override
 	public void updateData(Object data) {
 		if(data != null){
-			RemarksOfDailyPerform d = (RemarksOfDailyPerform) data;
+			RemarksOfDailyPerform d = new RemarksOfDailyPerform(getEmployeeId(), getWorkDate(), (RemarksOfDailyAttd) data);
 			this.data.removeIf(br -> br.getRemarks().getRemarkNo() == d.getRemarks().getRemarkNo());
 			this.data.add(d);
 		}
