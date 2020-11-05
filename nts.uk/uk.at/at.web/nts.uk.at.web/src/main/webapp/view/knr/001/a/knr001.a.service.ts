7182897f8368cf0ce4036f57112729c70a975d16
NSVC\xuannt <xuan_nt@nitsusystem-vn.com>
2020-11-05 11:42:07 +0700

    KNR001 export button

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/knr/001/a/knr001.a.service.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/knr/001/a/knr001.a.service.ts
@@ -4,10 +4,12 @@ module nts.uk.at.view.knr001.a.service {
 
     let paths: any = {
         getAll: "screen/at/empInfoTerminal/getAll",
-        getDetails: "screen/at/empInfoTerminal/getDetails/{0}/{1}",
+        getDetails: "screen/at/empInfoTerminal/getDetails",
         register: "at/record/empinfoterminal/register",
         update: "at/record/empinfoterminal/update",
-        delete: "at/record/empinfoterminal/delete"
+        delete: "at/record/empinfoterminal/delete",
+        knrExport: "file/empInfoTerminal/report/export",
+        getModel: "screen/at/empInfoTerminal/getModel"
     };
 
     /**
@@ -20,44 +22,41 @@ module nts.uk.at.view.knr001.a.service {
     /**
     * Get Details
     */
-    export function getDetails(empInfoTerCode, workLocationCD): JQueryPromise<any> {
-        return ajax(format(paths.getDetails, empInfoTerCode, workLocationCD));
+    export function getDetails(empInfoTerCode: any): JQueryPromise<any> {
+        return ajax(paths.getDetails+ "/" + empInfoTerCode);
     }
 
    /**
     * Register
     */
-    export function register(data: any) {
+    export function register(data: any): JQueryPromise<any> {
         return ajax(paths.register, data);
     }
 
     /**
     * Update
     */
-    export function update(data: any) {
+    export function update(data: any): JQueryPromise<any> {
         return ajax(paths.update, data);
     }
     /**
     * Remove
     */
-    export function removeEmpInfoTer(code) {
+    export function removeEmpInfoTer(code): JQueryPromise<any> {
         return ajax(format(paths.getDetails, code));
     }
 
-
-
-    //saveAsExcel
-        
-    export function saveAsExcel(languageId: String): JQueryPromise<any> {
-        let program = nts.uk.ui._viewModel.kiban.programName().split(" ");
-        let domainType = "KNR001";
-        if (program.length > 1) {
-            program.shift();
-            domainType = domainType + program.join(" ");
-        }
-        return nts.uk.request.exportFile('/masterlist/report/print', {
-            domainId: "",
-            domainType: domainType, languageId: languageId, reportType: 0
-        });
+    /**
+    * Save As Excel
+    */    
+    export function knrExport(): JQueryPromise<any> {
+        return nts.uk.request.exportFile(paths.knrExport);
+    };
+    /**
+     * Get 機種
+     */
+    export function getModel(data: any): JQueryPromise<any> {
+        return ajax(paths.getModel, data);
     }
+
 }
