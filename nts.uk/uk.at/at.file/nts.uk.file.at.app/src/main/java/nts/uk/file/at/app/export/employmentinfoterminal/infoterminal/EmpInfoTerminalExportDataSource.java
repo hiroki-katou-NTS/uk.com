c1daccb866653f047a74632c091012d24fbb60ec
huylq <huy_lq@nittsusystem-vn.com>
2020-11-05 10:19:13 +0700

    KNR001 print

--- nts.uk/uk.at/at.file/nts.uk.file.at.app/src/main/java/nts/uk/file/at/app/export/employmentinfoterminal/infoterminal/EmpInfoTerminalExportDataSource.java
+++ nts.uk/uk.at/at.file/nts.uk.file.at.app/src/main/java/nts/uk/file/at/app/export/employmentinfoterminal/infoterminal/EmpInfoTerminalExportDataSource.java
@@ -4,13 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import nts.uk.ctx.at.record.dom.employmentinfoterminal.infoterminal.EmpInfoTerminal;
+import nts.uk.ctx.at.record.dom.worklocation.WorkLocation;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class EmpInfoTerminalExportDataSource {
 	
-	private int empInfoTerNo;
+	private String empInfoTerNo;
 	private String empInfoTerName;
 	private String modelEmpInfoTer;
 	private String macAddress;
@@ -19,15 +20,14 @@ public class EmpInfoTerminalExportDataSource {
 	private String workLocationCd;
 	private String workLocationName;
 	private int intervalTimeCondition;
-	private int outSupportAtr;
+	private String outSupportAtr;
 	private String outAtr;
 	private String outReplaceAtr;
 	private String outReasonAtr;
-	private int entranceExitAtr;
+	private String entranceExitAtr;
 	private String empInfoTerMemo;
 	
 	public static EmpInfoTerminalExportDataSource convertToDatasource(EmpInfoTerminal empInfoTerminal, WorkLocation workLocation) {
-		// not check Optional yet
 		String outAtr = "置き換えなし";
 		switch(empInfoTerminal.getCreateStampInfo().getOutPlaceConvert().getReplace()) {
 		case USE:
@@ -37,15 +37,20 @@ public class EmpInfoTerminalExportDataSource {
 			default:
 				break;
 		}
-		return new EmpInfoTerminalExportDataSource(empInfoTerminal.getEmpInfoTerCode().v(), empInfoTerminal.getEmpInfoTerName().v(),
-				empInfoTerminal.getModelEmpInfoTer().name(), empInfoTerminal.getMacAddress().v(), empInfoTerminal.getIpAddress().isPresent()?empInfoTerminal.getIpAddress().get().v():"",
-				empInfoTerminal.getTerSerialNo().isPresent()?empInfoTerminal.getTerSerialNo().get().v():"", empInfoTerminal.getCreateStampInfo().getWorkLocationCd().get().v(), workLocation.workLocationName,
-				empInfoTerminal.getIntervalTime().v(), empInfoTerminal.getCreateStampInfo().getConvertEmbCate().getOutSupport().value, outAtr, 
-				empInfoTerminal.getCreateStampInfo().getOutPlaceConvert().getReplace().nameId, empInfoTerminal.getCreateStampInfo().getOutPlaceConvert().getGoOutReason().get().nameId, 
-				empInfoTerminal.getCreateStampInfo().getConvertEmbCate().getEntranceExit().value, empInfoTerminal.getEmpInfoTerMemo().isPresent()?empInfoTerminal.getEmpInfoTerMemo().get().v():"");
-	}
-	
-	class WorkLocation{
-		String workLocationName;
+		
+		String empInfoTerNo = "0000" + empInfoTerminal.getEmpInfoTerCode().v();
+		empInfoTerNo = empInfoTerNo.substring(empInfoTerNo.length()-4);
+		
+		return new EmpInfoTerminalExportDataSource(empInfoTerNo, empInfoTerminal.getEmpInfoTerName().v(),
+				empInfoTerminal.getModelEmpInfoTer().name(), empInfoTerminal.getMacAddress().v(), 
+				empInfoTerminal.getIpAddress().isPresent()?empInfoTerminal.getIpAddress().get().v():"",
+				empInfoTerminal.getTerSerialNo().isPresent()?empInfoTerminal.getTerSerialNo().get().v():"", 
+				workLocation!=null?empInfoTerminal.getCreateStampInfo().getWorkLocationCd().get().v():"", 
+				workLocation!=null?workLocation.getWorkLocationName().v():"",
+				empInfoTerminal.getIntervalTime().v(), empInfoTerminal.getCreateStampInfo().getConvertEmbCate().getOutSupport().value==1?"〇":"ー", 
+				outAtr, empInfoTerminal.getCreateStampInfo().getOutPlaceConvert().getReplace().nameId, 
+				empInfoTerminal.getCreateStampInfo().getOutPlaceConvert().getGoOutReason().isPresent()?empInfoTerminal.getCreateStampInfo().getOutPlaceConvert().getGoOutReason().get().nameId:"", 
+				empInfoTerminal.getCreateStampInfo().getConvertEmbCate().getEntranceExit().value==1?"〇":"ー", 
+				empInfoTerminal.getEmpInfoTerMemo().isPresent()?empInfoTerminal.getEmpInfoTerMemo().get().v():"");
 	}
 }
