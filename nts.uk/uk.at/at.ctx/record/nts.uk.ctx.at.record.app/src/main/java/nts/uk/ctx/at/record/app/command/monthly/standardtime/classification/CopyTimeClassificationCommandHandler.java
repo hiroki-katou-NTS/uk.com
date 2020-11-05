d87fee97e53a2d7c926db82707aa5de9d95452ab
khaidh <khai.dh@3si.vn>
2020-11-05 18:46:27 +0700

    35402

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/monthly/standardtime/classification/CopyTimeClassificationCommandHandler.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/monthly/standardtime/classification/CopyTimeClassificationCommandHandler.java
@@ -1,18 +1,18 @@
 package nts.uk.ctx.at.record.app.command.monthly.standardtime.classification;
 
+import lombok.val;
 import nts.arc.enums.EnumAdaptor;
 import nts.arc.layer.app.command.CommandHandler;
 import nts.arc.layer.app.command.CommandHandlerContext;
+import nts.gul.collection.CollectionUtil;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.affiliationinfor.ClassificationCode;
 import nts.uk.ctx.at.shared.dom.scherec.monthlyattdcal.agreement.management.AgreementTimeOfClassification;
 import nts.uk.ctx.at.shared.dom.scherec.monthlyattdcal.agreement.management.Classification36AgreementTimeRepository;
 import nts.uk.ctx.at.shared.dom.scherec.monthlyattdcal.agreement.management.enums.LaborSystemtAtr;
-import nts.uk.ctx.at.shared.dom.scherec.monthlyattdcal.agreement.management.timesetting.BasicAgreementSetting;
 import nts.uk.shr.com.context.AppContexts;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import java.util.Optional;
 
 @Stateless
 public class CopyTimeClassificationCommandHandler extends CommandHandler<CopyTimeClassificationCommand> {
@@ -22,31 +22,33 @@ public class CopyTimeClassificationCommandHandler extends CommandHandler<CopyTim
 
     @Override
     protected void handle(CommandHandlerContext<CopyTimeClassificationCommand> context) {
-
         CopyTimeClassificationCommand command = context.getCommand();
 
-        if (command.getClassificationCdTarget().equals(command.getClassificationCdSource())) {
-        	return;
+		String cid = AppContexts.user().companyId();
+		val laborSystemAtr = EnumAdaptor.valueOf(command.getLaborSystemAtr(), LaborSystemtAtr.class);
+
+		// 1: get(会社ID,雇用コード) : ３６協定基本設定
+        val source = repo.getByCidAndClassificationCode(cid, command.getClassificationCdSource(), laborSystemAtr);
+
+		if(!source.isPresent()) return;
+		if (CollectionUtil.isEmpty(command.getClassificationCdTarget())) return;
+
+		val basicAgreementSetting = source.get().getSetting();
+		for (val targetCd : command.getClassificationCdTarget()) {
+			if (targetCd.equals(command.getClassificationCdSource())) continue;
+
+			// 2: delete(会社ID,雇用コード)
+			val existTarget = repo.getByCidAndClassificationCode(cid, targetCd, laborSystemAtr);
+			existTarget.ifPresent(x -> repo.delete(x));
+
+			// 3: insert(会社ID,雇用コード,３６協定労働制,３６協定基本設定)
+			val newTarget = new AgreementTimeOfClassification(
+					cid,
+					laborSystemAtr,
+					new ClassificationCode(targetCd),
+					basicAgreementSetting
+			);
+			repo.insert(newTarget);
 		}
-
-        //1: get(会社ID,雇用コード) : ３６協定基本設定
-        Optional<AgreementTimeOfClassification> timeOfClassification =  repo.getByCidAndClassificationCode(AppContexts.user().companyId(),
-                command.getClassificationCdSource(),EnumAdaptor.valueOf(command.getLaborSystemAtr(),LaborSystemtAtr.class));
-
-        if(timeOfClassification.isPresent()){
-            BasicAgreementSetting basicAgreementSetting = timeOfClassification.get().getSetting();
-
-            Optional<AgreementTimeOfClassification> timeOfClassificationCoppy =  repo.getByCidAndClassificationCode(AppContexts.user().companyId(),
-                    command.getClassificationCdTarget(),EnumAdaptor.valueOf(command.getLaborSystemAtr(),LaborSystemtAtr.class));
-
-            //2: delete(会社ID,雇用コード)
-            timeOfClassificationCoppy.ifPresent(x -> repo.delete(x));
-
-            //3: insert(会社ID,雇用コード,３６協定労働制,３６協定基本設定)
-            AgreementTimeOfClassification agreementTimeOfClassification = new AgreementTimeOfClassification(AppContexts.user().companyId(),
-                    EnumAdaptor.valueOf(command.getLaborSystemAtr(), LaborSystemtAtr.class),new ClassificationCode(command.getClassificationCdTarget()),basicAgreementSetting);
-				repo.insert(agreementTimeOfClassification);
-			}
-
     }
 }
--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/008/d/kmk008.d.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/008/d/kmk008.d.vm.ts
@@ -238,32 +238,23 @@ module nts.uk.at.view.kmk008.d {
 				});
 			}
 
-			callCopySettingAPI(data:any): JQueryPromise<any> {
+			callCopySettingAPI(cds: string[]): JQueryPromise<any> {
 				let self = this;
-				let promises:any = [];
 
-				_.forEach(data, workplaceIdTarget => {
-					let dfd = $.Deferred();
-					let command = {
-						workplaceIdTarget: workplaceIdTarget,
-						workplaceIdSource: self.selectedCode(),
-						laborSystemAtr: self.laborSystemAtr
-					};
+				let dfd = $.Deferred();
+				let command = {
+					workplaceIdTarget: cds,
+					workplaceIdSource: self.selectedCode(),
+					laborSystemAtr: self.laborSystemAtr
+				};
 
-					if (command.workplaceIdTarget == command.workplaceIdSource) {
-						dfd.resolve();
-						return;
-					}
-
-					new service.Service().copySetting(command).done((result) => {
-						dfd.resolve(result);
-					}).fail((error:any) => {
-						dfd.reject(error);
-					});
-					promises.push(dfd);
+				new service.Service().copySetting(command).done((result) => {
+					dfd.resolve(result);
+				}).fail((error:any) => {
+					dfd.reject(error);
 				});
 
-				return $.when.apply(undefined, promises).promise();
+				return dfd.promise();
 			}
         }
 
