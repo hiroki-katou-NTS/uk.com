c40c1570a3120a9ed488d5d71dc1e871a08e320c
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2020-11-06 13:22:58 +0700

    KMK004 - update UI screen  Q

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/004/q/kmk004.q.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/004/q/kmk004.q.vm.ts
@@ -1,24 +1,49 @@
 /// <reference path="../../../../lib/nittsu/viewcontext.d.ts" />
 
 module nts.uk.at.kmk004.q {
-	
+
 
 	const API = {
-		
-		REGISTER: '/at/record/stamp/employment/system/register-stamp-input'
+
 	};
 
 	@bean()
 	export class ViewModel extends ko.ViewModel {
-		
 
-		created() {
-			
+		year: KnockoutObservable<number> = ko.observable(parseInt(moment().format("YYYY")));
+
+		updateMode: KnockoutObservable<boolean> = ko.observable(false);
+
+		register() {
+
+		}
+
+		close() {
+			let vm = this;
+			vm.$window.close();
+		}
+
+		created(params?: IParams) {
+			let vm = this;
+			if (params) {
+
+				vm.year(_.max(params.years) + 1);
+				vm.updateMode(true);
+
+			} else {
+				vm.updateMode(false);
+			}
 		}
 
 		mounted() {
-			
+
 		}
 	}
-	
+
+	interface IParams {
+		years: Array<number>;
+		workTypeMode: 'REGULAR_WORK' | 'FLEX' | 'UNUSUAL_WORK';
+		screenMode: 'COMPANY' | 'WORK_PLACE' | 'EMPLOYMENT' | 'EMPLOYEE';
+	}
+
 }
