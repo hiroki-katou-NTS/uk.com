13e18856a4ca012720a79be0b3b0516550b61c93
nws-dongnt <dongnt@newwave.vn>
2020-11-10 13:11:19 +0700

    clean code

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cli/003/f/cli003.f.service.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cli/003/f/cli003.f.service.ts
@@ -3,7 +3,7 @@ module nts.uk.com.view.cli003.f {
 
     export module service {
 
-        var paths = {
+        const paths = {
             getLogOutputItemsByRecordTypeItemNos: "ctx/sys/log/app/get-log-output-item-by-record-type-item-no-list",
             getLogOutputItemsByRecordType: "ctx/sys/log/app/get-log-output-item-by-record-type",
             getLogBasicInfoByModifyDate: "ctx/sys/log/record-reference/get-log-basic-info-by-modify-date",
@@ -11,7 +11,7 @@ module nts.uk.com.view.cli003.f {
             getLogSettingsBySystem: "sys/portal/logsettings/findBySystem",
             getLogDataResults: "ctx/sys/assist/app/getLogDataResults",
             exportCsvForDataResult: "ctx/sys/assist/app/export-csv",
-        }
+        };
 
         export function getLogOutputItemsByRecordTypeItemNos(paramOutputItem): JQueryPromise<any> {
             return ajax('com', paths.getLogOutputItemsByRecordTypeItemNos, paramOutputItem);
@@ -19,24 +19,26 @@ module nts.uk.com.view.cli003.f {
 
         export function getLogOutputItemsByRecordType(recordType: string): JQueryPromise<any> {
             return ajax('com', paths.getLogOutputItemsByRecordType, recordType);
-        };
+        }
 
         export function getLogBasicInfoByModifyDate(paramLog): JQueryPromise<any> {
             return ajax('com', paths.getLogBasicInfoByModifyDate, paramLog);
-        };
+        }
 
         export function logSettingExportCsv(params): JQueryPromise<any> {
             return nts.uk.request.exportFile(paths.logSettingExportCsv, params);
-        };
+        }
+
         export function getLogSettingsBySystem(systemType: number): JQueryPromise<any> {
             return ajax(`${paths.getLogSettingsBySystem}/${systemType}`);
-        };
+        }
+
         export function getLogDataResults(logDataParams: any): JQueryPromise<any> {
             return ajax('com', paths.getLogDataResults, logDataParams);
-        };
+        }
 
         export function exportCsvForDataResult(params): JQueryPromise<any> {
             return nts.uk.request.exportFile(paths.exportCsvForDataResult, params);
-        };
+        }
     }
 }
