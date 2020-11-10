ea273c2c290017a817f59f85e2ed3a91ce0d478d
quang.nh1 <huyquang.nhq@gmail.com>
2020-11-10 09:49:52 +0700

    update domain alarm check

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/alarmworkplace/StartDate.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/alarmworkplace/StartDate.java
@@ -2,7 +2,7 @@ package nts.uk.ctx.at.function.dom.alarmworkplace;
 
 import nts.uk.ctx.at.function.dom.alarm.extractionrange.daily.StartSpecify;
 
-public interface StartDate extends ExtractionPeriod{
+public interface StartDate{
 
     StartSpecify getStartSpecify();
 
