187afa3bdbd76bcc0e7aa05670f61aa8ebb512bc
NSVN-NguyenDinhHoang <61735854+NSVN-NguyenDinhHoang@users.noreply.github.com>
2020-11-10 13:57:02 +0700

    Refactor5: Update 02_更新登録 B KAF005

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/overtime/service/OverTimeRegisterService.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/overtime/service/OverTimeRegisterService.java
@@ -6,10 +6,27 @@ import nts.uk.ctx.at.request.dom.application.common.adapter.workflow.dto.Approva
 import nts.uk.ctx.at.request.dom.application.overtime.AppOverTime;
 
 public interface OverTimeRegisterService {
+	/**
+	 * Refactor5
+	 * 
+	 * @param companyId
+	 * @param appOverTime
+	 * @param lstApproval
+	 * @param mailServerSet
+	 */
 	public void register(
 			String companyId,
 			AppOverTime appOverTime,
 			List<ApprovalPhaseStateImport_New> lstApproval,
 			Boolean mailServerSet);
-	
+	/**
+	 * Refactor5 02_更新登録
+	 * UKDesign.UniversalK.就業.KAF_申請.KAF005_残業申請.B：残業申請（詳細・照会）.アルゴリズム.02_更新登録
+	 * @param companyId
+	 * @param appOverTime
+	 */
+	public void update(
+			String companyId,
+			AppOverTime appOverTime
+			);
 }
--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/overtime/service/OverTimeRegisterServiceImpl.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/overtime/service/OverTimeRegisterServiceImpl.java
@@ -7,13 +7,21 @@ import javax.inject.Inject;
 
 import nts.uk.ctx.at.request.dom.application.Application;
 import nts.uk.ctx.at.request.dom.application.ApplicationApprovalService;
+import nts.uk.ctx.at.request.dom.application.ApplicationRepository;
 import nts.uk.ctx.at.request.dom.application.common.adapter.workflow.dto.ApprovalPhaseStateImport_New;
+import nts.uk.ctx.at.request.dom.application.common.service.detailscreen.after.DetailAfterUpdate;
 import nts.uk.ctx.at.request.dom.application.common.service.newscreen.RegisterAtApproveReflectionInfoService;
 import nts.uk.ctx.at.request.dom.application.overtime.AppOverTime;
 import nts.uk.ctx.at.request.dom.application.overtime.AppOverTimeRepository;
 
 @Stateless
 public class OverTimeRegisterServiceImpl implements OverTimeRegisterService {
+	
+	@Inject
+	private DetailAfterUpdate detailAfterUpdate;
+	
+	@Inject
+	private ApplicationRepository appUpdateRepository;
 	
 	@Inject
 	ApplicationApprovalService appRepository;
@@ -41,6 +49,22 @@ public class OverTimeRegisterServiceImpl implements OverTimeRegisterService {
 		
 		// 2-3.新規画面登録後の処理を実行 #112628
 		return;
+	}
+
+	@Override
+	public void update(String companyId, AppOverTime appOverTime) {
+		Application application = (Application) appOverTime;
+		// ドメインモデル「残業申請」を更新する
+		appUpdateRepository.update(application);
+		appOverTimeRepository.update(appOverTime);
+		
+		// 暫定データの登録
+		
+		// アルゴリズム「4-2.詳細画面登録後の処理」を実行する
+		detailAfterUpdate.processAfterDetailScreenRegistration(
+				companyId,
+				application.getAppID(),
+				null); //#112628
 	}
 	
 
