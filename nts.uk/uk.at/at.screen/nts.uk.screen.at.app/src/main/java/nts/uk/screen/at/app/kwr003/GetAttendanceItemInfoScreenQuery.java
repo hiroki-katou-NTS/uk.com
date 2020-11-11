9a312d2a47a43ca44106c44d7fa72c8aad89baec
chinh.hm <chinh.hm@3si.vn>
2020-11-11 17:29:46 +0700

    update kwr003

--- nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/kwr003/GetAttendanceItemInfoScreenQuery.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/kwr003/GetAttendanceItemInfoScreenQuery.java
@@ -1,107 +1,19 @@
 package nts.uk.screen.at.app.kwr003;
 
-
-import lombok.val;
-import nts.uk.ctx.at.function.dom.outputitemsofworkstatustable.enums.CommonAttributesOfForms;
+import nts.arc.enums.EnumAdaptor;
 import nts.uk.ctx.at.function.dom.outputitemsofworkstatustable.enums.DailyMonthlyClassification;
-import nts.uk.ctx.at.shared.dom.monthlyattditem.MonthlyAttendanceItemAtr;
-import nts.uk.ctx.at.shared.dom.scherec.dailyattendanceitem.enums.DailyAttendanceAtr;
-import nts.uk.ctx.at.shared.dom.scherec.dailyattendanceitem.enums.TypesMasterRelatedDailyAttendanceItem;
-import nts.uk.ctx.at.shared.dom.scherec.dailyattendanceitem.service.CompanyDailyItemService;
-import nts.uk.ctx.at.shared.dom.scherec.monthlyattendanceitem.service.CompanyMonthlyItemService;
-import nts.uk.shr.com.context.AppContexts;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import java.util.ArrayList;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
-/**
- * ScreenQuery : 勤怠項目情報を取得する
- *
- * @author chinh.hm
- */
 @Stateless
 public class GetAttendanceItemInfoScreenQuery {
-
     @Inject
-    private CompanyMonthlyItemService monthlyItemService;
-    @Inject
-    private CompanyDailyItemService dailyItemService;
+    private GetAttendanceItemInfo getAttendanceItemInfo;
 
-
-    public AttendanceItemInfoDto getAttendanceItemInfo(DailyMonthlyClassification classification, int formNumberDisplay) {
-        // ①　=　List<勤怠項目ID>// TODO   QA.
-        val listAttendanceItemId = new ArrayList<Integer>();
-        //  （2:月次）
-        val listAttdanceIdOfMonthly = new ArrayList<Integer>();
-        //  （1:日次）
-        val listAttdanceIdOfDaily = new ArrayList<Integer>();
-
-        val cid = AppContexts.user().companyId();
-        val roleId = Optional.of(AppContexts.user().roles().forAttendance()); // TODO QA
-        // 勤怠項目の種類　（2:月次）
-        val itemMonthlys = monthlyItemService.getMonthlyItems(cid, roleId, listAttdanceIdOfMonthly, null);
-        //勤怠項目の種類　（1:日次）
-        val itemDailys = dailyItemService.getDailyItems(cid, roleId, listAttdanceIdOfDaily, null);
-
-        val listDailyAttributes = itemDailys.stream().map(e -> new AttItemDto(
-                e.getAttendanceItemId(),
-                e.getAttendanceItemName(),
-                e.getAttendanceItemDisplayNumber(),
-                e.getTypeOfAttendanceItem(),
-                convertDailyToAttForms(e.getTypeOfAttendanceItem(), 0)))
-                .collect(Collectors.toCollection(ArrayList::new));
-        val listMonthlyAttributes = itemMonthlys.stream().map(e -> new AttItemDto(
-                e.getAttendanceItemId(),
-                e.getAttendanceItemName(),
-                e.getAttendanceItemDisplayNumber(),
-                e.getTypeOfAttendanceItem(),
-                convertMonthlyToAttForms(e.getTypeOfAttendanceItem())))
-                .collect(Collectors.toCollection(ArrayList::new));
-
-        return new AttendanceItemInfoDto(listDailyAttributes, listMonthlyAttributes);
-
+    public AttendanceItemInfoDto getInfo( int formNumberDisplay) {
+        int DAILY = 1;
+        DailyMonthlyClassification classification = EnumAdaptor.valueOf(DAILY,DailyMonthlyClassification.class);
+        return getAttendanceItemInfo.getAttendanceItemInfo(classification,formNumberDisplay);
     }
-
-    private int convertDailyToAttForms(Integer typeOfAttendanceItem, int masterType) {
-        if (typeOfAttendanceItem == DailyAttendanceAtr.Code.value &&
-                masterType == TypesMasterRelatedDailyAttendanceItem.WORK_TYPE.value) {
-            return CommonAttributesOfForms.WORK_TYPE.value;
-        }
-        if (typeOfAttendanceItem == DailyAttendanceAtr.Code.value &&
-                masterType == TypesMasterRelatedDailyAttendanceItem.WORKING_HOURS.value) {
-            return CommonAttributesOfForms.WORKING_HOURS.value;
-        }
-        if (typeOfAttendanceItem == DailyAttendanceAtr.NumberOfTime.value) {
-            return CommonAttributesOfForms.NUMBER_OF_TIMES.value;
-        }
-        if (typeOfAttendanceItem == DailyAttendanceAtr.AmountOfMoney.value) {
-            return CommonAttributesOfForms.AMOUNT_OF_MONEY.value;
-        }
-        if (typeOfAttendanceItem == DailyAttendanceAtr.Time.value) {
-            return CommonAttributesOfForms.TIME.value;
-        }
-        return 0;
-    }
-
-    private int convertMonthlyToAttForms(Integer typeOfAttendanceItem) {
-        if (typeOfAttendanceItem == MonthlyAttendanceItemAtr.TIME.value) {
-
-            return CommonAttributesOfForms.TIME.value;
-        }
-        if (typeOfAttendanceItem == MonthlyAttendanceItemAtr.NUMBER.value) {
-            return CommonAttributesOfForms.NUMBER_OF_TIMES.value;
-        }
-        if (typeOfAttendanceItem == MonthlyAttendanceItemAtr.DAYS.value) {
-            return CommonAttributesOfForms.DAYS.value;
-        }
-        if (typeOfAttendanceItem == MonthlyAttendanceItemAtr.AMOUNT.value) {
-            return CommonAttributesOfForms.AMOUNT_OF_MONEY.value;
-        }
-
-        return 0;
-    }
-
 }
--- nts.uk/uk.at/at.screen/nts.uk.screen.at.ws/src/main/java/nts/uk/screen/at/ws/kwr003/GetAttendanceItemInfoWebService.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.ws/src/main/java/nts/uk/screen/at/ws/kwr003/GetAttendanceItemInfoWebService.java
@@ -1,10 +1,7 @@
 package nts.uk.screen.at.ws.kwr003;
 
 
-import lombok.val;
-import nts.arc.enums.EnumAdaptor;
 import nts.arc.layer.ws.WebService;
-import nts.uk.ctx.at.function.dom.outputitemsofworkstatustable.enums.DailyMonthlyClassification;
 import nts.uk.screen.at.app.kwr003.AttendanceItemInfoDto;
 import nts.uk.screen.at.app.kwr003.GetAttendanceItemInfoScreenQuery;
 
@@ -13,7 +10,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-@Path("at/screen/kwr003/a")
+@Path("at/screen/kwr/003/b")
 @Produces("application/json")
 public class GetAttendanceItemInfoWebService extends WebService {
     @Inject
@@ -22,7 +19,6 @@ public class GetAttendanceItemInfoWebService extends WebService {
     @POST
     @Path("getinfor")
     public AttendanceItemInfoDto getAttendanceItemInfo(int classification, int formNumberDisplay) {
-        val classic = EnumAdaptor.valueOf(classification, DailyMonthlyClassification.class);
-        return query.getAttendanceItemInfo(classic, formNumberDisplay);
+        return query.getInfo(formNumberDisplay);
     }
 }
