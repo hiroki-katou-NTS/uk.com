757f24a0b080717849ea1b9cf095fa4ded24fc6d
khaidh <khai.dh@3si.vn>
2020-11-11 15:01:35 +0700

    KWR005

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/workledgeroutputitem/CreateWorkLedgerDisplayContentDomainService.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/workledgeroutputitem/CreateWorkLedgerDisplayContentDomainService.java
@@ -1,9 +1,12 @@
 package nts.uk.ctx.at.function.dom.workledgeroutputitem;
 
+import lombok.val;
 import nts.arc.time.calendar.period.DatePeriod;
+import nts.uk.ctx.at.function.dom.outputitemsofworkstatustable.dto.StatusOfEmployee;
 import nts.uk.ctx.at.shared.dom.adapter.employee.EmployeeBasicInfoImport;
 import nts.uk.ctx.at.shared.dom.adapter.workplace.config.info.WorkplaceInfor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -29,13 +32,28 @@ public class CreateWorkLedgerDisplayContentDomainService {
 			WorkLedgerOutputSetting workLedgerOutputSetting,
 			List<WorkplaceInfor> workplaceInfoList) {
 
+		// ① = <call> 社員の指定期間中の所属期間を取得する
+		List<String> empIdList = new ArrayList<>();
+		if(empInfoList != null && empInfoList.size() > 0) {
+			for (val item: empInfoList) {
+				empIdList.add(item.getSid());
+			}
+		}
+		List<StatusOfEmployee> empStatusList = require.getAffiliateEmpListDuringPeriod(datePeriod, empIdList);
+
 		//		for (val empInfo: empInfoList) {
 		//			// AffCom	panyHist x =
 		//		}
+
+
+
 		return null;
 	}
 
 	public interface Require {
-
+		/**
+		 * 社員の指定期間中の所属期間を取得する
+		 */
+		List<StatusOfEmployee> getAffiliateEmpListDuringPeriod(DatePeriod datePeriod, List<String> empIdList);
 	}
 }
