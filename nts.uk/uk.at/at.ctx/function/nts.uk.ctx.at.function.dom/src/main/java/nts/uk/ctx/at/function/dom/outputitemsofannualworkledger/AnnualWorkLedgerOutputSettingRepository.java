e41ce8d048d21369c57a231c7c197a480cba729d
minh.nv <minh.nv@3si.vn>
2020-11-12 17:15:15 +0700

    KWR004 report performance

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/outputitemsofannualworkledger/AnnualWorkLedgerOutputSettingRepository.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/outputitemsofannualworkledger/AnnualWorkLedgerOutputSettingRepository.java
@@ -11,18 +11,23 @@ import java.util.Optional;
 
 /**
  * Repository: 年間勤務台帳の出力項目
+ *
  * @author : chinh.hm
  */
 public interface AnnualWorkLedgerOutputSettingRepository {
     // 	[1]	定型選択の出力設定一覧を取得する
-    public List<AnnualWorkLedgerOutputSetting> getAListOfOutputSettings(String cid,SettingClassificationCommon classificationCommon);
+    public List<AnnualWorkLedgerOutputSetting> getAListOfOutputSettings(String cid, SettingClassificationCommon classificationCommon);
+
     // 	[2]	自由設定の出力項設定一覧を取得する
-    public List<AnnualWorkLedgerOutputSetting>getTheFreeSettingOutputItemList(String cid,SettingClassificationCommon classificationCommon,String employeeId);
+    public List<AnnualWorkLedgerOutputSetting> getTheFreeSettingOutputItemList(String cid, SettingClassificationCommon classificationCommon, String employeeId);
+
     // 	[3]	出力設定の詳細を取得する
     Optional<AnnualWorkLedgerOutputSetting> getDetailsOfTheOutputSettings(String cid, String settingId);
+
     //	[4]	定型選択を新規作成する
     void createNew(String cid, AnnualWorkLedgerOutputSetting outputSetting, List<DailyOutputItemsAnnualWorkLedger> outputItemsOfTheDayList,
                    List<OutputItem> outputItemList, List<OutputItemDetailSelectionAttendanceItem> attendanceItemList);
+
     // 	[5]	自由設定を新規作成する
     // 	[6]	定型選択を更新する
     void update(String cid, String settingId, AnnualWorkLedgerOutputSetting outputSetting, List<DailyOutputItemsAnnualWorkLedger> outputItemsOfTheDayList, List<OutputItem> outputItemList,
@@ -30,12 +35,15 @@ public interface AnnualWorkLedgerOutputSettingRepository {
 
     //	[8]	設定の詳細を削除する
     void deleteSettingDetail(String settingId);
+
     //  [9]	設定の詳細を複製する
     void duplicateConfigurationDetail(String cid, String optionalEmployeeId,
-                                      String replicationSourceSettingsId,String destinationSettingId,
-                                      OutputItemSettingCode outputItemSettingCode,OutputItemSettingName outputItemSettingName);
+                                      String replicationSourceSettingsId, String destinationSettingId,
+                                      OutputItemSettingCode outputItemSettingCode, OutputItemSettingName outputItemSettingName);
+
     // 	[10]  exist(コード、ログイン会社ID)
-    boolean exist(OutputItemSettingCode code,String cid);
+    boolean exist(OutputItemSettingCode code, String cid);
+
     //  [11]  exist(コード、ログイン会社ID、ログイン社員ID)
-    boolean  exist(OutputItemSettingCode code,String cid,String employeeId);
+    boolean exist(OutputItemSettingCode code, String cid, String employeeId);
 }
