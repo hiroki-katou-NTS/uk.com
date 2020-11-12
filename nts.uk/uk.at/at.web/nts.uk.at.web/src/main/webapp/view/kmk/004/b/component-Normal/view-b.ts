ec0f4b33ae79d020bb6d9bad5e717a9b9a2124ea
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2020-11-12 09:45:08 +0700

    KMK004 Update Ui view B

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/004/b/component-Normal/view-b.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/004/b/component-Normal/view-b.ts
@@ -27,12 +27,75 @@ module nts.uk.at.view.kmk004.b {
 			<div data-bind="ntsFormLabel: {}, i18n: 'KMK004_232'"></div>
 		</div>
 		<div class="content">
-			<button data-bind="i18n: 'KMK004_233'"></button>
 			<div>
-				
+				<button data-bind="i18n: 'KMK004_233'"></button>
+			</div>
+			<div class= "data">
+				<div class= "box-year" data-bind="component: {
+					name: 'box-year',
+					params:{
+					}
+				}"></div>
+				<div class= "time-work" data-bind="component: {
+					name: 'time-work',
+					params:{
+					}
+				}"></div>
 			</div>
 		</div>
 	</div>
+	<style type="text/css" rel="stylesheet">
+		.view-b {
+			padding: 10px;
+		}
+		
+		.view-b .header-b {
+			width: 435px;
+		}
+		
+		.view-b .header-b ~ div {
+			font-size: 20px;
+		}
+		
+		.view-b .header-b hr {
+			margin-top: 4px;
+		}
+		
+		.view-b .header-b .header_title {
+			margin-top: 15px;
+		}
+		
+		.view-b .header-b .header_content {
+			margin-top: 15px;
+			margin-bottom: 15px;
+		}
+		
+		.view-b .content {
+			text-align: left;
+			margin-top: 15px;
+		}
+		
+		.view-b .content ~ div {
+			margin-top: 15px;
+			float: left;
+		}
+
+		.view-b .content .data {
+			margin-top: 20px;
+			float: left;
+			display: flex;
+		}
+
+		.view-b .content .data .box-year {
+			display: inline-block;
+		}
+
+		.view-b .content .data .time-work {
+			display: inline-block;
+			margin-left: 100px;
+		}
+    </style>
+    <style type="text/css" rel="stylesheet" data-bind="html: $component.style"></style>
 	`;
 
 	interface Params {
