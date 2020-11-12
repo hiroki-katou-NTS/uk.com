91c8f51b1c095d719b58981f2e0227ad81731c0a
NTS-PhamVanDan <dan_pv@nittsusystem-vn.com>
2020-11-12 13:56:42 +0900

    日別勤怠の勤務情報のUnitTest

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/test/java/nts/uk/ctx/at/shared/dom/dailyattdcal/dailyattendance/workinfomation/WorkInfoOfDailyAttendanceHelper.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/test/java/nts/uk/ctx/at/shared/dom/dailyattdcal/dailyattendance/workinfomation/WorkInfoOfDailyAttendanceHelper.java
@@ -1,12 +1,17 @@
 package nts.uk.ctx.at.shared.dom.dailyattdcal.dailyattendance.workinfomation;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import nts.uk.ctx.at.shared.dom.WorkInfoAndTimeZone;
 import nts.uk.ctx.at.shared.dom.WorkInformation;
 import nts.uk.ctx.at.shared.dom.holidaymanagement.publicholiday.configuration.DayOfWeek;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.workinfomation.CalculationState;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.workinfomation.NotUseAttribute;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.workinfomation.WorkInfoOfDailyAttendance;
+import nts.uk.ctx.at.shared.dom.worktime.common.TimeZone;
+import nts.uk.ctx.at.shared.dom.worktime.worktimeset.WorkTimeSetting;
+import nts.uk.ctx.at.shared.dom.worktype.WorkType;
 
 public class WorkInfoOfDailyAttendanceHelper {
 	public static WorkInfoOfDailyAttendance getWorkInfoOfDailyAttendanceDefault() {
@@ -19,5 +24,9 @@ public class WorkInfoOfDailyAttendanceHelper {
 	public static WorkInfoOfDailyAttendance getData(WorkInformation recordInfo) {
 		return new WorkInfoOfDailyAttendance(recordInfo, recordInfo, CalculationState.Calculated, NotUseAttribute.Not_use, NotUseAttribute.Not_use,
 				DayOfWeek.FRIDAY, new ArrayList<>());
+	}
+	
+	public static WorkInfoAndTimeZone createWorkInfoAndTimeZone(List<TimeZone> listTimeZone) {
+		return new WorkInfoAndTimeZone(new WorkType(), new WorkTimeSetting(), listTimeZone);
 	}
 }
--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/test/java/nts/uk/ctx/at/shared/dom/dailyattdcal/dailyattendance/workinfomation/WorkInfoOfDailyAttendanceTest.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/test/java/nts/uk/ctx/at/shared/dom/dailyattdcal/dailyattendance/workinfomation/WorkInfoOfDailyAttendanceTest.java
@@ -1,21 +1,33 @@
 package nts.uk.ctx.at.shared.dom.dailyattdcal.dailyattendance.workinfomation;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import mockit.Expectations;
 import mockit.Injectable;
 import mockit.Mock;
 import mockit.MockUp;
 import mockit.integration.junit4.JMockit;
 import nts.arc.testing.assertion.NtsAssert;
 import nts.uk.ctx.at.shared.dom.WorkInformation;
+import nts.uk.ctx.at.shared.dom.holidaymanagement.publicholiday.configuration.DayOfWeek;
 import nts.uk.ctx.at.shared.dom.schedule.basicschedule.WorkStyle;
+import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.workinfomation.CalculationState;
+import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.workinfomation.NotUseAttribute;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.workinfomation.WorkInfoOfDailyAttendance;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.workinfomation.WorkInfoOfDailyAttendance.Require;
+import nts.uk.ctx.at.shared.dom.worktime.common.TimeZone;
+import nts.uk.ctx.at.shared.dom.worktime.common.WorkTimeCode;
+import nts.uk.ctx.at.shared.dom.worktype.WorkTypeCode;
+import nts.uk.shr.com.time.TimeWithDayAttr;
 
 @RunWith(JMockit.class)
 public class WorkInfoOfDailyAttendanceTest {
@@ -106,6 +118,70 @@ public class WorkInfoOfDailyAttendanceTest {
 		};
 		Optional<WorkStyle> workstyle = workInfoOfDailyAttendance.getWorkStyle(require);
 		assertThat(workstyle.get().equals(WorkStyle.ONE_DAY_WORK)).isTrue();
+	}
+	
+	@Test
+	public void testCreate_scheduleTimeSheets_empty() {
+		
+		// Arrange
+		WorkInformation scheduleInfo = new WorkInformation(new WorkTypeCode("a01"), new WorkTimeCode("b01"));
+		WorkInformation recordInfo = new WorkInformation(new WorkTypeCode("a01"), new WorkTimeCode("b01"));
+		
+		new Expectations(scheduleInfo) { {
+			scheduleInfo.getWorkInfoAndTimeZone(require);
+			result = Optional.of(WorkInfoOfDailyAttendanceHelper.createWorkInfoAndTimeZone(Collections.emptyList()));
+		}};
+		
+		// Action
+		WorkInfoOfDailyAttendance target = WorkInfoOfDailyAttendance.create(require, recordInfo, scheduleInfo, 
+				CalculationState.No_Calculated, NotUseAttribute.Not_use, NotUseAttribute.Not_use, DayOfWeek.MONDAY);
+		
+		// Assert
+		assertThat(target.getRecordInfo()).isEqualTo(recordInfo);
+		assertThat(target.getScheduleInfo()).isEqualTo(scheduleInfo);
+		assertThat(target.getCalculationState()).isEqualTo(CalculationState.No_Calculated);
+		assertThat(target.getBackStraightAtr()).isEqualTo(NotUseAttribute.Not_use);
+		assertThat(target.getGoStraightAtr()).isEqualTo(NotUseAttribute.Not_use);
+		assertThat(target.getDayOfWeek()).isEqualTo(DayOfWeek.MONDAY);
+		assertThat(target.getScheduleTimeSheets()).isEmpty(); // emptyList
+	}
+	
+	@Test
+	public void testCreate_scheduleTimeSheets_not_empty() {
+		
+		// Arrange
+		WorkInformation scheduleInfo = new WorkInformation(new WorkTypeCode("a01"), new WorkTimeCode("b01"));
+		WorkInformation recordInfo = new WorkInformation(new WorkTypeCode("a01"), new WorkTimeCode("b01"));
+		
+		List<TimeZone> listTimeZone = Arrays.asList(
+				new TimeZone(new TimeWithDayAttr(10), new TimeWithDayAttr(20)),
+				new TimeZone(new TimeWithDayAttr(30), new TimeWithDayAttr(40))
+				);
+		
+		new Expectations(scheduleInfo) {{
+			scheduleInfo.getWorkInfoAndTimeZone(require);
+			result = Optional.of(WorkInfoOfDailyAttendanceHelper.createWorkInfoAndTimeZone(listTimeZone));
+		}};
+		
+		// Action
+		WorkInfoOfDailyAttendance target = WorkInfoOfDailyAttendance.create(require, recordInfo, scheduleInfo, 
+				CalculationState.No_Calculated, NotUseAttribute.Not_use, NotUseAttribute.Not_use, DayOfWeek.MONDAY);
+		
+		// Assert
+		assertThat(target.getRecordInfo()).isEqualTo(recordInfo);
+		assertThat(target.getScheduleInfo()).isEqualTo(scheduleInfo);
+		assertThat(target.getCalculationState()).isEqualTo(CalculationState.No_Calculated);
+		assertThat(target.getBackStraightAtr()).isEqualTo(NotUseAttribute.Not_use);
+		assertThat(target.getGoStraightAtr()).isEqualTo(NotUseAttribute.Not_use);
+		assertThat(target.getDayOfWeek()).isEqualTo(DayOfWeek.MONDAY);
+		assertThat(target.getScheduleTimeSheets())
+			.extracting(
+				e -> e.getWorkNo().v(),
+				e -> e.getAttendance().v(),
+				e -> e.getLeaveWork().v())
+			.containsExactly(
+				tuple(1, 10, 20),
+				tuple(2, 30, 40));
 	}
 
 }
