244b4b6236b0fe17afaa10dddf0a509d680be5fd
nws-longdh <longdh@newwave.vn>
2020-11-13 10:37:58 +0700

    delete old domain cmm049

--- nts.uk/uk.com/com.ctx/sys/env/nts.uk.ctx.sys.env.pubimp/src/main/java/nts/uk/ctx/sys/env/pubimp/maildestination/MailDestinationPubImpl.java
+++ nts.uk/uk.com/com.ctx/sys/env/nts.uk.ctx.sys.env.pubimp/src/main/java/nts/uk/ctx/sys/env/pubimp/maildestination/MailDestinationPubImpl.java
@@ -4,31 +4,19 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
-import nts.gul.collection.CollectionUtil;
 import nts.uk.ctx.sys.env.dom.contact.EmployeeContactAdapter;
 import nts.uk.ctx.sys.env.dom.contact.EmployeeContactObjectImport;
 import nts.uk.ctx.sys.env.dom.contact.PersonContactObjectOfEmployeeImport;
-import nts.uk.ctx.sys.env.dom.mailnoticeset.company.ContactSetting;
 import nts.uk.ctx.sys.env.dom.mailnoticeset.company.EmailClassification;
 import nts.uk.ctx.sys.env.dom.mailnoticeset.company.EmailDestinationFunction;
-import nts.uk.ctx.sys.env.dom.mailnoticeset.company.MailDestinationFunction;
-import nts.uk.ctx.sys.env.dom.mailnoticeset.company.MailDestinationFunctionRepository;
-import nts.uk.ctx.sys.env.dom.mailnoticeset.company.SettingUseSendMail;
-import nts.uk.ctx.sys.env.dom.mailnoticeset.company.UserInfoUseMethod;
-import nts.uk.ctx.sys.env.dom.mailnoticeset.company.UserInfoUseMethodRepository;
 import nts.uk.ctx.sys.env.dom.mailnoticeset.company.UserInformationUseMethod;
 import nts.uk.ctx.sys.env.dom.mailnoticeset.company.UserInformationUseMethodRepository;
-import nts.uk.ctx.sys.env.dom.mailnoticeset.employee.UseContactSetting;
-import nts.uk.ctx.sys.env.dom.mailnoticeset.employee.UseContactSettingRepository;
-import nts.uk.ctx.sys.env.dom.mailnoticeset.employee.UserInfoItem;
 import nts.uk.ctx.sys.env.pub.maildestination.IMailDestinationPub;
 import nts.uk.ctx.sys.env.pub.maildestination.MailDestination;
-import nts.uk.shr.com.enumcommon.NotUseAtr;
 
 /**
  * @author sonnlb
@@ -41,8 +29,6 @@ public class MailDestinationPubImpl implements IMailDestinationPub {
 	private UserInformationUseMethodRepository userInformationUseMethodRepository;
 	@Inject
 	private EmployeeContactAdapter empContactAdapter;
-	@Inject
-	private UseContactSettingRepository useContactRepo;
 
 	@Override
 	public List<MailDestination> getEmpEmailAddress(String cID, List<String> sIDs, Integer functionID) {
@@ -53,23 +39,25 @@ public class MailDestinationPubImpl implements IMailDestinationPub {
 		 */
 		sIDs.forEach(sID -> {
 			emailAddress.add(new MailDestination(sID, Collections.emptyList()));
-		});		
+		});
 		/**
 		 * ドメインモデル「ユーザー情報の使用方法」を取得する
 		 */
-		Optional<UserInformationUseMethod> userInformationUseMethodOpt = userInformationUseMethodRepository.findByCId(cID);
-		
+		Optional<UserInformationUseMethod> userInformationUseMethodOpt = userInformationUseMethodRepository
+				.findByCId(cID);
+
 		/**
 		 * 取得件数０件
 		 */
-		if (!userInformationUseMethodOpt.isPresent() || !userInformationUseMethodOpt.get().getSettingContactInformation().isPresent()) {
+		if (!userInformationUseMethodOpt.isPresent()
+				|| !userInformationUseMethodOpt.get().getSettingContactInformation().isPresent()) {
 			return emailAddress;
 		}
 		/**
 		 * 取得件数１件以上
 		 */
 		UserInformationUseMethod userInformationUseMethod = userInformationUseMethodOpt.get();
-		
+
 		/**
 		 * 取得した「ユーザー情報の使用方法」kから、使えるメールの判断する
 		 */
@@ -80,221 +68,129 @@ public class MailDestinationPubImpl implements IMailDestinationPub {
 		if (!userInformationUseMethod.getEmailDestinationFunctions().isEmpty()) {
 			for (EmailDestinationFunction e : userInformationUseMethod.getEmailDestinationFunctions()) {
 				if (e.getEmailClassification() == EmailClassification.COMPANY_EMAIL_ADDRESS
-						&& userInformationUseMethod.getSettingContactInformation().get().getCompanyEmailAddress().getContactUsageSetting().value != 0) {
+						&& userInformationUseMethod.getSettingContactInformation().get().getCompanyEmailAddress()
+								.getContactUsageSetting().value != 0) {
 					companyEmailAddressFlag = true;
 				}
 				if (e.getEmailClassification() == EmailClassification.PERSONAL_EMAIL_ADDRESS
-						&& userInformationUseMethod.getSettingContactInformation().get().getPersonalEmailAddress().getContactUsageSetting().value != 0) {
+						&& userInformationUseMethod.getSettingContactInformation().get().getPersonalEmailAddress()
+								.getContactUsageSetting().value != 0) {
 					personalEmailAddressFlag = true;
 				}
 				if (e.getEmailClassification() == EmailClassification.COMPANY_MOBILE_EMAIL_ADDRESS
-						&& userInformationUseMethod.getSettingContactInformation().get().getCompanyMobileEmailAddress().getContactUsageSetting().value != 0) {
+						&& userInformationUseMethod.getSettingContactInformation().get().getCompanyMobileEmailAddress()
+								.getContactUsageSetting().value != 0) {
 					companyMobileEmailAddressFlag = true;
 				}
 				if (e.getEmailClassification() == EmailClassification.PERSONAL_MOBILE_EMAIL_ADDRESS
-						&& userInformationUseMethod.getSettingContactInformation().get().getPersonalMobileEmailAddress().getContactUsageSetting().value != 0) {
+						&& userInformationUseMethod.getSettingContactInformation().get().getPersonalMobileEmailAddress()
+								.getContactUsageSetting().value != 0) {
 					personalMobileEmailAddressFlag = true;
 				}
 			}
 		}
-		
+
 		/**
-		 * 「会社メールアドレスFlag」と「会社携帯メールアドレスFlag」をチェックする
-		 */
-		/**
-		 * 両方がfalse場合
+		 * 「会社メールアドレスFlag」と「会社携帯メールアドレスFlag」をチェックする 両方がfalse場合
 		 */
 		if (!companyEmailAddressFlag && !companyMobileEmailAddressFlag) {
 			/**
 			 * 「個人メールアドレスFlag」と「個人携帯メールアドレスFlag」をチェックする
 			 */
 			if (!personalEmailAddressFlag && !personalMobileEmailAddressFlag) {
-				
+				/**
+				 * パラメータ.社員ID(List)をメール送信一覧を渡す
+				 */
+				return emailAddress;
 			} else {
 				/**
-				 * その他
+				 * その他 Imported(環境)「個人連絡先」を取得する RequestList 420
 				 */
-				/**
-				 * Imported(環境)「個人連絡先」を取得する
-				 */
-				// Imported(環境)「個人連絡先」を取得する RequestList 420
 				List<PersonContactObjectOfEmployeeImport> personContacts = empContactAdapter.getListOfEmployees(sIDs);
+
+				if (personalEmailAddressFlag && userInformationUseMethod.getSettingContactInformation().get()
+						.getPersonalEmailAddress().getContactUsageSetting().value != 0) {
+					sIDs.forEach(sID -> {
+						Optional<MailDestination> mailDestinationOpt = emailAddress.stream()
+								.filter(mailDestination -> mailDestination.getEmployeeID().equals(sID)).findFirst();
+
+						mailDestinationOpt.ifPresent(mailDestination -> {
+							Optional<PersonContactObjectOfEmployeeImport> perContactOpt = personContacts.stream()
+									.filter(perContact -> perContact.getPersonId().equals(sID)).findFirst();
+
+							perContactOpt.ifPresent(item -> {
+								if (item.getIsMailAddressDisplay()) {
+									mailDestination.addOutGoingMails(item.getMailAdress());
+								}
+							});
+						});
+					});
+				}
+				
+				if (personalMobileEmailAddressFlag && userInformationUseMethod.getSettingContactInformation().get()
+						.getPersonalMobileEmailAddress().getContactUsageSetting().value != 0) {
+					sIDs.forEach(sID -> {
+						Optional<MailDestination> mailDestinationOpt = emailAddress.stream()
+								.filter(mailDestination -> mailDestination.getEmployeeID().equals(sID)).findFirst();
+
+						mailDestinationOpt.ifPresent(mailDestination -> {
+							Optional<PersonContactObjectOfEmployeeImport> perContactOpt = personContacts.stream()
+									.filter(perContact -> perContact.getPersonId().equals(sID)).findFirst();
+
+							perContactOpt.ifPresent(item -> {
+								if (item.getIsMobileEmailAddressDisplay()) {
+									mailDestination.addOutGoingMails(item.getMobileMailAdress());
+								}
+							});
+						});
+					});
+				}
 			}
 		} else {
 			/**
-			 * その他
-			 */
-			/**
-			 * Imported(環境)「社員連絡先」を取得する RequestList 378
+			 * その他 Imported(環境)「社員連絡先」を取得する RequestList 378
 			 */
 			List<EmployeeContactObjectImport> empContacts = empContactAdapter.getList(sIDs);
-			boolean displayEmail = empContacts.stream().allMatch(x -> !x.getMailAddress().isEmpty());
-			boolean displayPhoneEmail = empContacts.stream().allMatch(x -> !x.getPhoneMailAddress().isEmpty());
-			if(companyEmailAddressFlag) {
-				if(userInformationUseMethod.getSettingContactInformation().get().getCompanyEmailAddress().getContactUsageSetting().value == 1
-						|| (userInformationUseMethod.getSettingContactInformation().get().getCompanyEmailAddress().getContactUsageSetting().value == 2
-						&& true)) {
-//					empContacts.stream().filter(x -> x.getSid().equ)
-				}
+
+			if (companyEmailAddressFlag && userInformationUseMethod.getSettingContactInformation().get()
+					.getCompanyEmailAddress().getContactUsageSetting().value != 0) {
+				sIDs.forEach(sID -> {
+					Optional<MailDestination> mailDestinationOpt = emailAddress.stream()
+							.filter(mailDestination -> mailDestination.getEmployeeID().equals(sID)).findFirst();
+
+					mailDestinationOpt.ifPresent(mailDestination -> {
+						Optional<EmployeeContactObjectImport> empContactOpt = empContacts.stream()
+								.filter(empContact -> empContact.getSid().equals(sID)).findFirst();
+
+						empContactOpt.ifPresent(item -> {
+							if (item.getIsMailAddressDisplay()) {
+								mailDestination.addOutGoingMails(item.getMailAddress());
+							}
+						});
+					});
+				});
+			}
+
+			if (companyMobileEmailAddressFlag && userInformationUseMethod.getSettingContactInformation().get()
+					.getCompanyMobileEmailAddress().getContactUsageSetting().value != 0) {
+				sIDs.forEach(sID -> {
+					Optional<MailDestination> mailDestinationOpt = emailAddress.stream()
+							.filter(mailDestination -> mailDestination.getEmployeeID().equals(sID)).findFirst();
+
+					mailDestinationOpt.ifPresent(mailDestination -> {
+						Optional<EmployeeContactObjectImport> empContactOpt = empContacts.stream()
+								.filter(empContact -> empContact.getSid().equals(sID)).findFirst();
+
+						empContactOpt.ifPresent(item -> {
+							if (item.getIsMobileMailAddressDisplay()) {
+								mailDestination.addOutGoingMails(item.getPhoneMailAddress());
+							}
+						});
+					});
+				});
 			}
 		}
 
 		return emailAddress;
 	}
-
-	private void setEmail(UserInfoUseMethod useInfoMethod, List<MailDestination> emailAddress, List<String> sIDs,
-			String cID) {
-
-		boolean isCompanyEmail = useInfoMethod.getSettingItem().equals(UserInfoItem.COMPANY_PC_MAIL)
-				|| useInfoMethod.getSettingItem().equals(UserInfoItem.COMPANY_MOBILE_MAIL);
-		if (isCompanyEmail) {
-			// Imported(環境)「社員連絡先」を取得する RequestList 378
-			List<EmployeeContactObjectImport> empContacts = empContactAdapter.getList(sIDs);
-
-			setCompanyMail(useInfoMethod, emailAddress, sIDs, empContacts, cID, useInfoMethod.getSettingItem());
-
-		}
-
-		boolean isPersonEmail = useInfoMethod.getSettingItem().equals(UserInfoItem.PERSONAL_PC_MAIL)
-				|| useInfoMethod.getSettingItem().equals(UserInfoItem.PERSONAL_MOBILE_MAIL);
-
-		if (isPersonEmail) {
-			// Imported(環境)「個人連絡先」を取得する RequestList 420
-			List<PersonContactObjectOfEmployeeImport> personContacts = empContactAdapter.getListOfEmployees(sIDs);
-
-			setPersonMail(useInfoMethod, emailAddress, sIDs, personContacts, cID, useInfoMethod.getSettingItem());
-		}
-
-	}
-
-	private void setPersonMail(UserInfoUseMethod useInfoMethod, List<MailDestination> emailAddress, List<String> sIDs,
-			List<PersonContactObjectOfEmployeeImport> personContacts, String cID, UserInfoItem userInfoItem) {
-		boolean isPersonSelectAble = useInfoMethod.getSettingUseMail().get()
-				.equals(SettingUseSendMail.PERSONAL_SELECTABLE);
-
-		if (isPersonSelectAble) {
-			// ドメインモデル「連絡先使用設定」を取得する
-			List<UseContactSetting> useContacts = useContactRepo.findByListEmployeeId(sIDs, cID).stream()
-					.filter(x -> x.getSettingItem().equals(userInfoItem)).filter(x -> x.isUseMailSetting())
-					.collect(Collectors.toList());
-
-			boolean isUseContactsNotEmpty = !CollectionUtil.isEmpty(useContacts);
-
-			if (isUseContactsNotEmpty) {
-				addEmailFromPerContact(emailAddress, sIDs, personContacts,userInfoItem);
-
-			}
-		}
-
-		boolean isUse = useInfoMethod.getSettingUseMail().get().equals(SettingUseSendMail.USE);
-
-		if (isUse) {
-			addEmailFromPerContact(emailAddress, sIDs, personContacts, userInfoItem);
-		}
-
-	}
-
-	private void addEmailFromPerContact(List<MailDestination> emailAddress, List<String> sIDs,
-			List<PersonContactObjectOfEmployeeImport> personContacts, UserInfoItem userInfoItem) {
-		sIDs.forEach(sID -> {
-
-			Optional<MailDestination> mailDestinationOpt = emailAddress.stream()
-					.filter(mailDestination -> mailDestination.getEmployeeID().equals(sID)).findFirst();
-			mailDestinationOpt.ifPresent(mailDestination -> {
-				Optional<PersonContactObjectOfEmployeeImport> perContactOpt = personContacts.stream()
-						.filter(perContact -> perContact.getEmployeeId().equals(sID)).findFirst();
-
-				perContactOpt.ifPresent(item -> {
-					if(userInfoItem.equals(UserInfoItem.PERSONAL_PC_MAIL)){
-					mailDestination.addOutGoingMails(item.getMailAdress());
-					}
-					if (userInfoItem.equals(UserInfoItem.PERSONAL_MOBILE_MAIL)) {
-						mailDestination.addOutGoingMails(item.getMobileMailAdress());
-					}
-				});
-
-			});
-
-		});
-
-	}
-	
-	private void setCompanyMail(UserInfoUseMethod useInfoMethod, List<MailDestination> emailAddress, List<String> sIDs,
-			List<EmployeeContactObjectImport> empContacts, String cID, UserInfoItem userInfoItem) {
-		boolean isPersonSelectAble = useInfoMethod.getSettingUseMail().get()
-				.equals(SettingUseSendMail.PERSONAL_SELECTABLE);
-		
-		if (isPersonSelectAble) {
-			// ドメインモデル「連絡先使用設定」を取得する
-			List<UseContactSetting> useContacts = useContactRepo.findByListEmployeeId(sIDs, cID).stream()
-					.filter(x -> x.getSettingItem().equals(userInfoItem)).filter(x -> x.isUseMailSetting())
-					.collect(Collectors.toList());
-
-			boolean isUseContactsNotEmpty = !CollectionUtil.isEmpty(useContacts);
-
-			if (isUseContactsNotEmpty) {
-				addEmailFromEmpContact(emailAddress, sIDs, empContacts, userInfoItem);
-
-			}
-		}
-
-		boolean isUse = useInfoMethod.getSettingUseMail().get().equals(SettingUseSendMail.USE);
-
-		if (isUse) {
-			addEmailFromEmpContact(emailAddress, sIDs, empContacts, userInfoItem);
-		}
-
-	}
-
-	private void addEmailFromEmpContact(List<MailDestination> emailAddress, List<String> sIDs,
-			List<EmployeeContactObjectImport> empContacts, UserInfoItem userInfoItem) {
-		sIDs.forEach(sID -> {
-
-			Optional<MailDestination> mailDestinationOpt = emailAddress.stream()
-					.filter(mailDestination -> mailDestination.getEmployeeID().equals(sID)).findFirst();
-
-			mailDestinationOpt.ifPresent(mailDestination -> {
-				Optional<EmployeeContactObjectImport> empContactOpt = empContacts.stream()
-						.filter(empContact -> empContact.getSid().equals(sID)).findFirst();
-
-				empContactOpt.ifPresent(item -> {
-					if (userInfoItem.equals(UserInfoItem.COMPANY_PC_MAIL)) {
-						mailDestination.addOutGoingMails(item.getMailAddress());
-					}
-					
-					if (userInfoItem.equals(UserInfoItem.COMPANY_MOBILE_MAIL)) {
-						mailDestination.addOutGoingMails(item.getPhoneMailAddress());
-					}
-				});
-
-			});
-
-		});
-
-	}
-
-	private boolean checkUseInfoMethod(Optional<UserInfoUseMethod> useInfoMethodOpt) {
-
-		if (!useInfoMethodOpt.isPresent()) {
-
-			return false;
-		}
-
-		UserInfoUseMethod useInfoMethod = useInfoMethodOpt.get();
-
-		if (!useInfoMethod.getSettingUseMail().isPresent()) {
-			return false;
-		}
-
-		SettingUseSendMail settingUseSendMail = useInfoMethod.getSettingUseMail().get();
-
-		if (!(settingUseSendMail.equals(SettingUseSendMail.USE)
-				|| settingUseSendMail.equals(SettingUseSendMail.PERSONAL_SELECTABLE))) {
-
-			return false;
-
-		}
-		return true;
-	}
-
 }
