9b729354b9c8106e1c83015bbdc5414e9b768c9f
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2020-11-13 17:44:41 +0700

    Update UI ViewB

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/004/b/component-Normal/view-c.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/004/b/component-Normal/view-c.ts
@@ -2,7 +2,38 @@
 
 module nts.uk.at.view.kmk004.b {
 	const template = `
-		<p>viewc</p>
+		<div class="sidebar-content-header">
+			<div class="title" data-bind="i18n: 'Com_Workplace'"></div>
+			<a class="goback" data-bind="ntsLinkButton: { jump: '/view/kmk/004/a/index.xhtml' },i18n: 'KMK004_224'"></a>
+			<button class="proceed" data-bind="i18n: 'KMK004_225'"></button>
+			<button data-bind="i18n: 'KMK004_226'"></button>
+			<button class="danger" data-bind="i18n: 'KMK004_227'"></button>
+		</div>
+		<div class="view-c-kmk004">
+			<div class="left-content">
+				<div data-bind="component: {
+					name: 'kcp004',
+					params:{
+					}
+				}"></div>
+			</div>
+			<div class="right-content">
+				<div>
+					<p data-bind="i18n: 'KMK004_228'"></p>
+					<hr></hr>
+					<div data-bind="i18n: ''"></div>
+					<div>
+						<div data-bind="ntsFormLabel: {}, i18n: 'KMK004_229'"></div>
+						<!-- ko if: modeCheckSetting -->
+							<button data-bind="i18n: 'KMK004_239'"></button>
+						<!-- /ko -->
+						<!-- ko if: !modeCheckSetting -->
+							<button data-bind="i18n: 'KMK004_238'"></button>
+						<!-- /ko -->
+					</div>
+				</div>
+			</div>
+		<div>
 	`;
 
 	interface Params {
@@ -15,6 +46,8 @@ module nts.uk.at.view.kmk004.b {
     })
     
 	export class ViewCComponent extends ko.ViewModel {
+
+		public modeCheckSetting: KnockoutObservable<boolean> = ko.observable(true);
 		
 		created(params: Params) {
 
