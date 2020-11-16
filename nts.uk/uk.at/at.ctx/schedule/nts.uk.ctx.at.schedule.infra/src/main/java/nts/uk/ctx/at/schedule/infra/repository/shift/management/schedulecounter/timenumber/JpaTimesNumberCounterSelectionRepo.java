55d84e9a5f76d48555a5500d1fff2dbbcee4cafb
quang.nh1 <huyquang.nhq@gmail.com>
2020-11-16 17:03:58 +0700

    36345 fix bug kml002

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.infra/src/main/java/nts/uk/ctx/at/schedule/infra/repository/shift/management/schedulecounter/timenumber/JpaTimesNumberCounterSelectionRepo.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.infra/src/main/java/nts/uk/ctx/at/schedule/infra/repository/shift/management/schedulecounter/timenumber/JpaTimesNumberCounterSelectionRepo.java
@@ -38,7 +38,7 @@ public class JpaTimesNumberCounterSelectionRepo extends JpaRepository implements
         builderString = new StringBuilder();
         builderString.append(SELECT);
         builderString.append(" JOIN KshstTotalSubjects b  ");
-        builderString.append(" ON a.pk.timeNo = b.kshstTotalSubjectsPK.totalTimesNo ");
+        builderString.append(" ON a.pk.timeNo = b.kshstTotalSubjectsPK.totalTimesNo AND a.pk.companyId = b.kshstTotalSubjectsPK.cid ");
         builderString.append(" WHERE a.pk.companyId = :companyId ");
         builderString.append(" AND a.pk.countType = :type ");
         FIND_BY_CID_AND_TYPE = builderString.toString();
