e64d7e7d04a3571602d9de643dfa60a4a95b42d8
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2020-11-17 14:01:35 +0700

    refactor 5: merge pj/at/release_ver4 to TeamD/Refactor5/Release_1

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.ws/src/main/java/nts/uk/ctx/at/request/ws/application/common/ApplicationWebservice.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.ws/src/main/java/nts/uk/ctx/at/request/ws/application/common/ApplicationWebservice.java
@@ -1,7 +1,6 @@
 package nts.uk.ctx.at.request.ws.application.common;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
