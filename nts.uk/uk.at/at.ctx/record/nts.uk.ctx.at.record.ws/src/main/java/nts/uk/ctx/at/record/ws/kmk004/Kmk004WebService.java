b731c149c3ea76de5a98adbedaa1b74542184a75
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2020-11-17 08:51:44 +0700

    Update ws to CommandHandler

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.ws/src/main/java/nts/uk/ctx/at/record/ws/kmk004/Kmk004WebService.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.ws/src/main/java/nts/uk/ctx/at/record/ws/kmk004/Kmk004WebService.java
@@ -6,8 +6,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 import nts.arc.layer.ws.WebService;
-import nts.uk.ctx.at.record.app.command.kmk004.s.UpdateUsageUnitSettings;
-import nts.uk.ctx.at.record.app.command.kmk004.s.UpdateUsageUnitSettingsInput;
+import nts.uk.ctx.at.record.app.command.kmk004.s.UsageUnitSettingCommandHandler;
+import nts.uk.ctx.at.record.app.command.kmk004.s.UsageUnitSettingCommand;
 
 /**
  * 
@@ -19,12 +19,11 @@ import nts.uk.ctx.at.record.app.command.kmk004.s.UpdateUsageUnitSettingsInput;
 public class Kmk004WebService extends WebService {
 
 	@Inject
-	private UpdateUsageUnitSettings unitSettings;
-	
+	private UsageUnitSettingCommandHandler unitSettings;
 	
 	@POST
 	@Path("update-setting")
-	public void saveStampCardViewA(UpdateUsageUnitSettingsInput command) {
-		unitSettings.updateUsageSetting(command);
+	public void saveStampCardViewA(UsageUnitSettingCommand command) {
+		unitSettings.handle(command);
 	}
 }
