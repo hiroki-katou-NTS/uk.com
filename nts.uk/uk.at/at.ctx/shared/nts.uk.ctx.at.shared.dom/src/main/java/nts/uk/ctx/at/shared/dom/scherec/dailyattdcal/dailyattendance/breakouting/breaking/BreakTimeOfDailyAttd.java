5e01ed996395d6702ef4b83a9c88f04c6c59eb25
NTS-PhamVanDan <dan_pv@nittsusystem-vn.com>
2020-11-19 16:38:31 +0900

    add getBreakTimeWithNumber

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/scherec/dailyattdcal/dailyattendance/breakouting/breaking/BreakTimeOfDailyAttd.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/scherec/dailyattdcal/dailyattendance/breakouting/breaking/BreakTimeOfDailyAttd.java
@@ -1,12 +1,14 @@
 package nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.breakouting.breaking;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import nts.arc.layer.dom.objecttype.DomainObject;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailycalprocess.calculation.timezone.deductiontime.TimeSheetOfDeductionItem;
+import nts.uk.shr.com.time.TimeWithDayAttr;
 
 /**
  * 日別勤怠の休憩時間帯
@@ -33,6 +35,23 @@ public class BreakTimeOfDailyAttd implements DomainObject {
 	 */
 	public List<TimeSheetOfDeductionItem> changeAllTimeSheetToDeductionItem(){
 		return this.breakTimeSheets.stream().map(tc -> tc.toTimeSheetOfDeductionItem()).collect(Collectors.toList());
+	}
+	
+	/**
+	 * 休憩時刻を取る
+	 * @param number 休憩時間帯NO　1~10
+	 * @param startTime 開始時刻か
+	 * @return
+	 */
+	public Optional<TimeWithDayAttr> getBreakTimeWithNumber(int number, boolean startTime) {
+		
+		if ( this.breakTimeSheets.size() < number ) return Optional.empty(); 
+		
+		TimeWithDayAttr timeWithDay = startTime ? 
+				this.breakTimeSheets.get(number - 1).getStartTime() :
+				this.breakTimeSheets.get(number - 1).getEndTime();
+		
+		return Optional.of(timeWithDay);
 	}
 	
 }
