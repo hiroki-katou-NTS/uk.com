66e271853c4f72be3232bed7f1c5fb39bbcd0568
NWS_LienPTK <lienptk@newwave.vn>
2020-11-19 09:16:29 +0700

    fix comment source for daily + monthly used attendance

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/scherec/monthlyattendanceitem/MonthlyAttendanceItemUsedRepository.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/scherec/monthlyattendanceitem/MonthlyAttendanceItemUsedRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 public interface MonthlyAttendanceItemUsedRepository {
 	
 	/**
-	 * 	月次の勤怠項目が利用できる帳票
+	 * UKDesign.ドメインモデル.NittsuSystem.UniversalK.就業.contexts.就業機能.勤怠項目が利用できる帳票.Repository.帳票用の月次勤怠項目の取得Repository
 	 *
 	 * @param companyId 会社ID
 	 * @param reportId 帳票ID（勤怠項目が利用できる帳票）
