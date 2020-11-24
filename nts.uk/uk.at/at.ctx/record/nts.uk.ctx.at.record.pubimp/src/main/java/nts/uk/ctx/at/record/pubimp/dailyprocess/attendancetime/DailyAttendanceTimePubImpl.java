3760e8b6bdf9ca8cb0f21f1232065d57e0be7c9b
NSVN-TrieuKhacTu <tu_tk@nittsusystem-vn.com>
2020-11-24 11:43:53 +0700

    fixbug : #112458, #112757

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.pubimp/src/main/java/nts/uk/ctx/at/record/pubimp/dailyprocess/attendancetime/DailyAttendanceTimePubImpl.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.pubimp/src/main/java/nts/uk/ctx/at/record/pubimp/dailyprocess/attendancetime/DailyAttendanceTimePubImpl.java
@@ -24,7 +24,9 @@ import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.breakgoout.
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.breakouting.breaking.BreakTimeSheet;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.common.TimeWithCalculation;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.dailyattendancework.IntegrationOfDaily;
+import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.holidayworktime.HolidayWorkMidNightTime;
 import nts.uk.ctx.at.shared.dom.workrule.outsideworktime.holidaywork.HolidayWorkFrameNo;
+import nts.uk.ctx.at.shared.dom.workrule.outsideworktime.holidaywork.StaturoryAtrOfHolidayWork;
 import nts.uk.ctx.at.shared.dom.workrule.outsideworktime.overtime.overtimeframe.OverTimeFrameNo;
 import nts.uk.ctx.at.shared.dom.worktime.common.TimeZone;
 import nts.uk.shr.com.time.TimeWithDayAttr;
@@ -76,9 +78,11 @@ public class DailyAttendanceTimePubImpl implements DailyAttendanceTimePub{
 			return Collections.emptyList();
 		
 		//時間帯の作成
-		TimeZone timeZone = imp.getLstTimeZone().get(0);
 		Map<Integer, TimeZone> timeZoneMap = new HashMap<Integer, TimeZone>();
-		timeZoneMap.put(1, timeZone);
+		for(int i = 0;i<imp.getLstTimeZone().size();i++) {
+			TimeZone timeZone = imp.getLstTimeZone().get(i+1);
+			timeZoneMap.put(i+1, timeZone);
+		}
 		
 		//休憩時間帯の作成
 		List<BreakTimeSheet> breakTimeSheets = new ArrayList<>();
@@ -115,17 +119,43 @@ public class DailyAttendanceTimePubImpl implements DailyAttendanceTimePub{
 		val specBonusPays = new HashMap<Integer,AttendanceTime>();
 		TimeWithCalculation flexTime = TimeWithCalculation.sameTime(new AttendanceTime(0));
 		TimeWithCalculation excessTimeMidNightTime = TimeWithCalculation.sameTime(new AttendanceTime(0));
-		
+		//計算合計外深夜時間
+		AttendanceTime timeOutSideMidnight = new AttendanceTime(0);
+		//計算残業深夜時間
+		AttendanceTime calOvertimeMidnight = new AttendanceTime(0);
+		//計算休出深夜時間
+		Map<StaturoryAtrOfHolidayWork,AttendanceTime> calHolidayMidnight  = new HashMap<StaturoryAtrOfHolidayWork,AttendanceTime>();
 		
 		if(integrationOfDaily.getAttendanceTimeOfDailyPerformance().isPresent()) {
 			if(integrationOfDaily.getAttendanceTimeOfDailyPerformance().get().getActualWorkingTimeOfDaily() != null){
 				if(integrationOfDaily.getAttendanceTimeOfDailyPerformance().get().getActualWorkingTimeOfDaily().getTotalWorkingTime()!= null) {
 					if(integrationOfDaily.getAttendanceTimeOfDailyPerformance().get().getActualWorkingTimeOfDaily().getTotalWorkingTime().getExcessOfStatutoryTimeOfDaily() != null) {
-						if(integrationOfDaily.getAttendanceTimeOfDailyPerformance().get().getActualWorkingTimeOfDaily().getTotalWorkingTime().getExcessOfStatutoryTimeOfDaily().getExcessOfStatutoryMidNightTime() != null)
+						if(integrationOfDaily.getAttendanceTimeOfDailyPerformance().get().getActualWorkingTimeOfDaily().getTotalWorkingTime().getExcessOfStatutoryTimeOfDaily().getExcessOfStatutoryMidNightTime() != null) {
 							 excessTimeMidNightTime = TimeWithCalculation.convertFromTimeDivergence(integrationOfDaily.getAttendanceTimeOfDailyPerformance().get().getActualWorkingTimeOfDaily().getTotalWorkingTime().getExcessOfStatutoryTimeOfDaily().getExcessOfStatutoryMidNightTime().getTime());
+							 //日別実績の勤怠時間．勤怠時間．勤務時間．総労働時間．所定外時間．所定外深夜時間．時間
+							 timeOutSideMidnight = excessTimeMidNightTime.getCalcTime();
+						}
 						if(integrationOfDaily.getAttendanceTimeOfDailyPerformance().get().getActualWorkingTimeOfDaily().getTotalWorkingTime().getExcessOfStatutoryTimeOfDaily().getOverTimeWork().isPresent()) {
 							flexTime = integrationOfDaily.getAttendanceTimeOfDailyPerformance().get().getActualWorkingTimeOfDaily().getTotalWorkingTime().getExcessOfStatutoryTimeOfDaily().getOverTimeWork().get().getFlexTime().getNotMinusFlexTime();
+							if(integrationOfDaily.getAttendanceTimeOfDailyPerformance().get().getActualWorkingTimeOfDaily().getTotalWorkingTime().getExcessOfStatutoryTimeOfDaily().getOverTimeWork().get().getExcessOverTimeWorkMidNightTime().isPresent()) {
+								//日別実績の勤怠時間．勤怠時間．勤務時間．総労働時間．所定外時間．残業時間．所定外深夜時間．時間
+								calOvertimeMidnight = integrationOfDaily.getAttendanceTimeOfDailyPerformance().get().getActualWorkingTimeOfDaily().getTotalWorkingTime().getExcessOfStatutoryTimeOfDaily().getOverTimeWork().get().getExcessOverTimeWorkMidNightTime().get().getTime().getCalcTime();
+							}
 						}
+						if(integrationOfDaily.getAttendanceTimeOfDailyPerformance().get().getActualWorkingTimeOfDaily().getTotalWorkingTime().getExcessOfStatutoryTimeOfDaily().getWorkHolidayTime().isPresent()) {
+							if(integrationOfDaily.getAttendanceTimeOfDailyPerformance().get().getActualWorkingTimeOfDaily().getTotalWorkingTime().getExcessOfStatutoryTimeOfDaily().getWorkHolidayTime().get().getHolidayMidNightWork().isPresent()) {
+								if(integrationOfDaily.getAttendanceTimeOfDailyPerformance().get().getActualWorkingTimeOfDaily().getTotalWorkingTime().getExcessOfStatutoryTimeOfDaily().getWorkHolidayTime().get().getHolidayMidNightWork().isPresent()) {
+									List<HolidayWorkMidNightTime> holidayWorkMidNightTimes = integrationOfDaily.getAttendanceTimeOfDailyPerformance().get().getActualWorkingTimeOfDaily().getTotalWorkingTime().getExcessOfStatutoryTimeOfDaily().getWorkHolidayTime().get().getHolidayMidNightWork().get().getHolidayWorkMidNightTime();
+									for(HolidayWorkMidNightTime hw : holidayWorkMidNightTimes) {
+										calHolidayMidnight.put(hw.getStatutoryAtr(), hw.getTime().getCalcTime());
+									}
+									
+								}
+								
+							}
+						}
+						
+						
 					}
 				}
 			}
@@ -181,7 +211,10 @@ public class DailyAttendanceTimePubImpl implements DailyAttendanceTimePub{
 				bonusPays,
 				specBonusPays,
 				flexTime,
-				excessTimeMidNightTime
+				excessTimeMidNightTime,
+				timeOutSideMidnight,
+				calOvertimeMidnight,
+				calHolidayMidnight
 			);
 	}
 
@@ -194,6 +227,12 @@ public class DailyAttendanceTimePubImpl implements DailyAttendanceTimePub{
 		val holidayWorkFrames = new HashMap<HolidayWorkFrameNo,TimeWithCalculation>();
 		val bonusPays = new HashMap<Integer,AttendanceTime>();
 		val specBonusPays = new HashMap<Integer,AttendanceTime>();
+		//計算合計外深夜時間
+		AttendanceTime timeOutSideMidnight = new AttendanceTime(0);
+		// 計算残業深夜時間
+		AttendanceTime calOvertimeMidnight = new AttendanceTime(0);
+		// 計算休出深夜時間
+		Map<StaturoryAtrOfHolidayWork, AttendanceTime> calHolidayMidnight  = new HashMap<StaturoryAtrOfHolidayWork,AttendanceTime>();
 		for(int loopNumber = 1 ; loopNumber <=10 ; loopNumber++ ) {
 			//残業
 			overTimeFrames.put(new OverTimeFrameNo(loopNumber), TimeWithCalculation.sameTime(new AttendanceTime(0)));
@@ -209,7 +248,10 @@ public class DailyAttendanceTimePubImpl implements DailyAttendanceTimePub{
 												bonusPays,
 												specBonusPays,
 												TimeWithCalculation.sameTime(new AttendanceTime(0)),
-												TimeWithCalculation.sameTime(new AttendanceTime(0))
+												TimeWithCalculation.sameTime(new AttendanceTime(0)),
+												timeOutSideMidnight,
+												calOvertimeMidnight,
+												calHolidayMidnight
 											);
 	}
 	
