03eb6e4d8c4303bbf76dfa804572b8f227de9e64
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2020-11-26 17:06:03 +0700

    Update Find By Cid

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/scherec/monthlyattdcal/aggr/calcmethod/calcmethod/other/wkp/WkpRegulaMonthActCalSetRepo.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/scherec/monthlyattdcal/aggr/calcmethod/calcmethod/other/wkp/WkpRegulaMonthActCalSetRepo.java
@@ -1,5 +1,6 @@
 package nts.uk.ctx.at.shared.dom.scherec.monthlyattdcal.aggr.calcmethod.calcmethod.other.wkp;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -15,6 +16,13 @@ public interface WkpRegulaMonthActCalSetRepo {
 	 * @return the optional
 	 */
 	Optional<WkpRegulaMonthActCalSet> find(String cid, String wkpId);
+	
+	/**
+	 * Find by Cid
+	 * @param cid
+	 * @return
+	 */
+	List<WkpRegulaMonthActCalSet> findWkpRegulaMonthAll(String cid);
 
 	/**
 	 * Adds the.
