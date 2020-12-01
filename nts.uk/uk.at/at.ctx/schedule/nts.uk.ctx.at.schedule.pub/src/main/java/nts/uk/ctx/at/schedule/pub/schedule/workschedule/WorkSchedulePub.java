6bf418ba52bc894741b3b2029c7869eadae74c2f
quang.nh1 <huyquang.nhq@gmail.com>
2020-12-01 16:21:06 +0700

    add algorithm kal011

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.pub/src/main/java/nts/uk/ctx/at/schedule/pub/schedule/workschedule/WorkSchedulePub.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.pub/src/main/java/nts/uk/ctx/at/schedule/pub/schedule/workschedule/WorkSchedulePub.java
@@ -1,8 +1,11 @@
 package nts.uk.ctx.at.schedule.pub.schedule.workschedule;
 
+import java.util.List;
 import java.util.Optional;
 
 import nts.arc.time.GeneralDate;
+import nts.arc.time.calendar.period.DatePeriod;
+
 /**
  * 
  * @author tutk
@@ -10,4 +13,9 @@ import nts.arc.time.GeneralDate;
  */
 public interface WorkSchedulePub {
 	public Optional<WorkScheduleExport> get(String employeeID , GeneralDate ymd);
+
+	/**
+	 * 社員ID(List)、期間を設定して勤務予定を取得する
+	 */
+	public List<WorkScheduleExport> getList(List<String> sids, DatePeriod period);
 }
