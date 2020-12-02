0aee0487647ee2787ff2ef6920d1d4c18961deed
NSC-LanLT <lan-lt@nittsusystem.co.jp>
2020-12-02 13:36:51 +0900

    [雇用の目安金額]のコード

---
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/shift/management/schedulecounter/estimate/EmploymentEstimateAmount.java
@@ -0,0 +1,22 @@
+package nts.uk.ctx.at.schedule.dom.shift.management.schedulecounter.estimate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import nts.arc.layer.dom.objecttype.DomainAggregate;
+import nts.uk.ctx.at.shared.dom.vacation.setting.compensatoryleave.EmploymentCode;
+/**
+ * 雇用の目安金額
+ * @author lan_lt
+ *
+ */
+@AllArgsConstructor
+@Getter
+public class EmploymentEstimateAmount implements  DomainAggregate{
+	
+	/** 雇用コード*/
+	private final  EmploymentCode employmentCode;
+	
+	/**　目安金額詳細　*/
+	private EstimatePriceDetail detail;
+
+}
