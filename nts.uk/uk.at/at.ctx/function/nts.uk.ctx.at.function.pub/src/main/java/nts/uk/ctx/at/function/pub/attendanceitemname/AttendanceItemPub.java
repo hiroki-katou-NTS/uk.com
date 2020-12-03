a400f75d8769c3a81bf386cd7d78e8180e873a24
quang.nh1 <huyquang.nhq@gmail.com>
2020-12-03 09:17:12 +0700

    add algorithm kal011

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.pub/src/main/java/nts/uk/ctx/at/function/pub/attendanceitemname/AttendanceItemPub.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.pub/src/main/java/nts/uk/ctx/at/function/pub/attendanceitemname/AttendanceItemPub.java
@@ -5,4 +5,20 @@ import java.util.List;
 public interface AttendanceItemPub {
 
 	List<AttendanceItemExport> getAttendanceItemName(List<Integer> dailyAttendanceItemIds, int typeOfAttendanceItem);
+
+	/**
+	 * スケジュールの勤怠項目を取得する
+	 * @param typeOfAttendanceItem
+	 * @return List<AttendanceItemExport>
+	 */
+	List<AttendanceItemExport> getAttendanceItemName(int typeOfAttendanceItem);
+
+	/**
+	 * 日次の勤怠項目を取得する
+	 * @param typeOfAttendanceItem
+	 * @param cid
+	 * @return List<AttendanceItemExport>
+	 */
+	List<AttendanceItemExport> getByAttendenceIds(String cid, int typeOfAttendanceItem);
+
 }
--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.pubimp/src/main/java/nts/uk/ctx/at/function/pubimp/attendanceitemname/AttendanceItemPubImpl.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.pubimp/src/main/java/nts/uk/ctx/at/function/pubimp/attendanceitemname/AttendanceItemPubImpl.java
@@ -1,12 +1,17 @@
 package nts.uk.ctx.at.function.pubimp.attendanceitemname;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+import nts.arc.enums.EnumAdaptor;
+import nts.uk.ctx.at.function.dom.attendanceitemframelinking.enums.TypeOfItem;
 import nts.uk.ctx.at.function.dom.attendanceitemname.service.AttendanceItemNameDomainService;
+import nts.uk.ctx.at.function.dom.attendancetype.AttendanceType;
+import nts.uk.ctx.at.function.dom.attendancetype.AttendanceTypeRepository;
 import nts.uk.ctx.at.function.pub.attendanceitemname.AttendanceItemExport;
 import nts.uk.ctx.at.function.pub.attendanceitemname.AttendanceItemPub;
 
@@ -16,10 +21,30 @@ public class AttendanceItemPubImpl implements AttendanceItemPub{
 	@Inject
 	private AttendanceItemNameDomainService attendanceItemNameDomainService;
 
+	@Inject
+	private AttendanceTypeRepository attendanceTypeRepository;
+
 	@Override
 	public List<AttendanceItemExport> getAttendanceItemName(List<Integer> dailyAttendanceItemIds,
 			int typeOfAttendanceItem) {
 		return this.attendanceItemNameDomainService.getNameOfAttendanceItem(dailyAttendanceItemIds, typeOfAttendanceItem).stream().map(item -> {
+			return new AttendanceItemExport(item.getAttendanceItemId(), item.getAttendanceItemName(), item.getAttendanceItemDisplayNumber());
+		}).collect(Collectors.toList());
+	}
+
+	@Override
+	public List<AttendanceItemExport> getAttendanceItemName(int typeOfAttendanceItem) {
+		return this.attendanceItemNameDomainService.getNameOfAttendanceItem(EnumAdaptor.valueOf(typeOfAttendanceItem, TypeOfItem.class)).stream().map(item -> {
+			return new AttendanceItemExport(item.getAttendanceItemId(), item.getAttendanceItemName(), item.getAttendanceItemDisplayNumber());
+		}).collect(Collectors.toList());
+	}
+
+	@Override
+	public List<AttendanceItemExport> getByAttendenceIds(String cid,int typeOfAttendanceItem) {
+		List<AttendanceType> attendanceTypes = attendanceTypeRepository.getItemByAtrandType(cid,typeOfAttendanceItem);
+		List<Integer> attendanceItemIds = attendanceTypes.stream().map(AttendanceType::getAttendanceItemId).collect(Collectors.toList());
+
+		return this.attendanceItemNameDomainService.getNameOfAttendanceItem(attendanceItemIds, typeOfAttendanceItem).stream().map(item -> {
 			return new AttendanceItemExport(item.getAttendanceItemId(), item.getAttendanceItemName(), item.getAttendanceItemDisplayNumber());
 		}).collect(Collectors.toList());
 	}
