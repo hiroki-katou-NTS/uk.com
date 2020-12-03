ed70f1d2a7fe1a89f49e8b3ae5f454522bd7a7eb
nws-dungdv <dungdv@newwave.vn>
2020-12-03 16:01:21 +0700

    fix getmemento, fix isnewMsg function

--- nts.uk/uk.com/com.ctx/sys/portal/nts.uk.ctx.sys.portal.ac/src/main/java/nts/uk/ctx/sys/portal/ac/notice/MessageNoticeAdapterImpl.java
+++ nts.uk/uk.com/com.ctx/sys/portal/nts.uk.ctx.sys.portal.ac/src/main/java/nts/uk/ctx/sys/portal/ac/notice/MessageNoticeAdapterImpl.java
@@ -2,7 +2,6 @@ package nts.uk.ctx.sys.portal.ac.notice;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
