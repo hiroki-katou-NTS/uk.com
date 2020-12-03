165473e6972a99a088603092267a12f41b1fd3d1
chaunhatquang <quang.cn@3si.vn>
2020-12-03 15:34:29 +0700

    kafs3536 fix bug UI

--- nts.uk/uk.mobile/ClientApp/views/kdl/s35/index.vue
+++ nts.uk/uk.mobile/ClientApp/views/kdl/s35/index.vue
@@ -11,41 +11,41 @@
       <!-- A1_2 -->
       <div class="uk-bg-headline py-3">
         <!-- A1_3 -->
-        <span>{{ "KDLS35_2" | i18n }}}</span>
+        <span>{{ "KDLS35_2" | i18n }}</span>
       </div>
     </div>
     <div class="modal-component modal-body m-n3">
       <!-- A2_1 -->
       <div class="row">
-        <div class="col-6">
+        <div class="col-5">
           <nts-label>{{ "KDLS35_3" | i18n }}</nts-label>
         </div>
         <!-- A2_2 -->
-        <label v-if="startDate === endDate" class="col-6 pt-2">{{
+        <label v-if="startDate === endDate" class="col-7 pt-2">{{
           startDate | i18n
         }}</label>
-        <label v-else class="col-6 pt-2">{{
+        <label v-else class="col-7 pt-2">{{
           "KDLS35_14" | i18n([startDate, endDate])
         }}</label>
       </div>
       <!-- A3_1 -->
       <div class="row pt-1">
-        <div class="col-6">
+        <div class="col-5">
           <nts-label>{{ "KDLS35_4" | i18n }}</nts-label>
         </div>
         <!-- A3_2 -->
-        <label class="col-6 pt-2">{{
+        <label class="col-7 pt-2">{{
           "KDLS35_5" | i18n(requiredNumberOfDays)
         }}</label>
       </div>
       <div class="row pt-1">
-        <div class="col-6">
+        <div class="col-5">
           <!-- A4_1 -->
           <fa-font icon="fas fa-exclamation-triangle" />
           <!-- A4_2 -->
           <label>{{ "KDLS35_6" | i18n }}</label>
         </div>
-        <div class="col-6">
+        <div class="col-7">
           <!-- A4_3 -->
           <fa-font icon="fas fa-calendar-check" />
           <!-- A4_4 -->
@@ -53,10 +53,10 @@
         </div>
       </div>
       <!-- A5_1 -->
-      <table class="table">
+      <table class="table table-responsive">
         <thead class="text-center">
           <tr>
-            <th scope="col">#</th>
+            <th scope="col"></th>
             <!-- A5_2 -->
             <th scope="col">{{ "KDLS35_8" | i18n }}</th>
             <!-- A5_3 -->
@@ -78,7 +78,7 @@
               </nts-checkbox>
             </td>
             <!-- A5_7 -->
-            <td>
+            <td v-on:click="() => item.checked = !item.checked">
               <fa-font 
               v-if="item.dataType == 1" 
               icon="fas fa-calendar-check" 
@@ -93,15 +93,15 @@
               />
             </td>
             <!-- A5_8 -->
-            <td>
+            <td v-on:click="() => item.checked = !item.checked">
               {{ item.substituteWorkDate | date("YY/MM/DD(ddd)") }}
             </td>
             <!-- A5_9 -->
-            <td>
+            <td v-on:click="() => item.checked = !item.checked">
               <span>{{ "KDLS35_5" | i18n(item.remainingNumber) }}</span>
             </td>
             <!-- A5_10 -->
-            <td>
+            <td v-on:click="() => item.checked = !item.checked">
               <span>{{ item.expirationDate | date("YY/MM/DD(ddd)") }}</span>
             </td>
           </tr>
