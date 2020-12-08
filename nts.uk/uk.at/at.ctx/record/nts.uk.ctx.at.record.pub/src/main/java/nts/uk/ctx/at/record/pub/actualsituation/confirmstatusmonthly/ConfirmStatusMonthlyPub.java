d49c54f1eca5629b7ded34c81d93a6a5eb1934b1
NSVN-TrieuKhacTu <tu_tk@nittsusystem-vn.com>
2020-12-08 10:54:11 +0700

    add name RQ586
    (cherry picked from commit 9b0a9002ab5cff9bd489b5cf2c536360ce7ab5ba)

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.pub/src/main/java/nts/uk/ctx/at/record/pub/actualsituation/confirmstatusmonthly/ConfirmStatusMonthlyPub.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.pub/src/main/java/nts/uk/ctx/at/record/pub/actualsituation/confirmstatusmonthly/ConfirmStatusMonthlyPub.java
@@ -6,5 +6,6 @@ import java.util.Optional;
 import nts.arc.time.YearMonth;
 
 public interface ConfirmStatusMonthlyPub {
+	// RQ586 : 月の実績の確認状況を取得する
 	public Optional<StatusConfirmMonthEx> getConfirmStatusMonthly(String companyId, List<String> listEmployeeId, YearMonth yearmonthInput, Integer clsId, boolean clearState);
 }
