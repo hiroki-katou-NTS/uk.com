2fb3ae783f35c893c6caa4eeb1bc8208f451d2e8
NSVN-NguyenDinhHoang <61735854+NSVN-NguyenDinhHoang@users.noreply.github.com>
2020-12-09 09:28:53 +0700

    Refactor5: Update bind workinfo KAFS05

--- nts.uk/uk.mobile/ClientApp/views/kaf/s05/step1/index.ts
+++ nts.uk/uk.mobile/ClientApp/views/kaf/s05/step1/index.ts
@@ -1,4 +1,4 @@
-import { Vue } from '@app/provider';
+import { _, Vue } from '@app/provider';
 import { component } from '@app/core/component';
 import { KafS00SubP3Component } from 'views/kaf/s00/sub/p3';
 import { KafS00SubP1Component } from 'views/kaf/s00/sub/p1';
@@ -23,11 +23,11 @@ import { KafS00AComponent, KafS00BComponent, KafS00CComponent } from 'views/kaf/
 export class KafS05Step1Component extends Vue {
     public title: string = 'KafS05Step1';
 
-    public model: Model;
+    public workInfo: WorkInfo = {} as WorkInfo;
 
-    public valueWorkHours1: { start: number, end: number } = null;
+    public workHours1: { start: number, end: number } = null;
 
-    public valueWorkHours2: { start: number, end: number } = null;
+    public workHours2?: { start: number, end: number } = null;
 
     public kafS00P1Params1: any = {
         preAppDisp: false,
@@ -54,20 +54,34 @@ export class KafS05Step1Component extends Vue {
     public loadData(object?: any) {
         const self = this;
         console.log('loadData');
-        let workType = {} as Work;
-        workType.code = '001';
-        workType.name = 'name';
-        let workTime = {} as Work;
-        workTime.code = '001';
-        workTime.name = 'time';
-        let work = {} as WorkInfo;
-        work.workType = workType;
-        work.workTime = workTime;
+        if (!_.isNil(object)) {
+            let workType = {} as Work;
+            workType.code = '001';
+            workType.name = 'name';
 
-        self.model = {
+            let workTime = {} as Work;
+            workTime.code = '001';
+            workTime.name = 'name';
+            workTime.time = '12:00 ~ 20:00';
+            self.workInfo.workType = workType;
+            self.workInfo.workTime = workTime;
 
-        } as Model;
-        self.model.work = work;
+            let workInfo = {} as WorkInfo;
+            workInfo.workType = workType;
+            workInfo.workTime = workTime;
+            self.workInfo = workInfo;
+
+            return;
+        } 
+        self.workInfo.workType = {
+            code: '',
+            name: ''
+        } as Work;
+        self.workInfo.workTime = {
+            code: '',
+            name: '',
+            time: '12:00 ~ 20:00'
+        } as Work;
 
     }
 
@@ -81,7 +95,4 @@ interface Work {
     code: string;
     name: string;
     time?: string;
-}
-interface Model {
-    work: WorkInfo;
 }
