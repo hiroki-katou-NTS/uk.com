11b488c97312c1275080edabb8e635c772ed0edd
NSVC\dungbn <dung_bn@nittsusystem-vn.com>
2020-12-09 11:47:00 +0700

    KNR002: Update repository

---
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/query/knr/knr002/a/EmpInfoTerminalDto.java
@@ -0,0 +1,23 @@
+package nts.uk.screen.at.app.query.knr.knr002.a;
+
+import java.util.Optional;
+
+import lombok.Setter;
+
+//@AllArgsConstructor
+//@Getter
+//@Setter
+public class EmpInfoTerminalDto {
+
+	private String empInfoTerCode;
+	
+	private String empInfoTerName;
+	
+	private int modelEmpInfoTer;
+	
+	private String workLocationCd;
+	
+	private String workLocationName;
+	
+//	private String 
+}
---
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/query/knr/knr002/a/GetListInfoOfEmpInfoTerminal.java
@@ -0,0 +1,14 @@
+package nts.uk.screen.at.app.query.knr.knr002.a;
+
+import javax.ejb.Stateless;
+
+/**
+ * 
+ * @author dungbn
+ * UKDesign.UniversalK.就業.KNR_就業情報端末.KNR002_就業情報端末の監視.Ａ：就業情報端末の監視.メニュー別OCD.Ａ：就業情報端末の監視画面を起動する
+ *
+ */
+@Stateless
+public class GetListInfoOfEmpInfoTerminal {
+
+}
