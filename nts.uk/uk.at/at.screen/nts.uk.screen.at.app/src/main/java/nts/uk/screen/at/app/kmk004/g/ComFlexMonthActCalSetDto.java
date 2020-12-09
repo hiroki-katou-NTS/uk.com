530a13f3173b2311de0b2d6d9fda9304af14efdc
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2020-12-09 09:21:25 +0700

    KMK004 - Add DisplayInitialScreen (会社別法定労働時間の登録（フレックス勤務）の初期画面を表示する)

---
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/kmk004/g/ComFlexMonthActCalSetDto.java
@@ -0,0 +1,18 @@
+package nts.uk.screen.at.app.kmk004.g;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * 
+ * @author sonnlb
+ *
+ *         会社別フレックス勤務集計方法
+ */
+@AllArgsConstructor
+@Data
+public class ComFlexMonthActCalSetDto {
+
+	/** 所定労動時間使用区分 */
+	private boolean withinTimeUsageAttr;
+}
---
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/kmk004/g/DisplayFlexBasicSettingByCompanyDto.java
@@ -0,0 +1,21 @@
+package nts.uk.screen.at.app.kmk004.g;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 
+ * @author sonnlb
+ *
+ *         会社別基本設定（フレックス勤務）を表示する
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DisplayFlexBasicSettingByCompanyDto {
+	// フレックス勤務所定労働時間取得
+	private GetFlexPredWorkTimeDto getFlexPredWorkTime;
+	// 会社別フレックス勤務集計方法
+	private ComFlexMonthActCalSetDto ComFlexMonthActCalSet;
+}
