5c7f7e0acb4b95f1af394f02f6ff5a06dd44ac2d
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2020-12-10 17:09:02 +0700

    KMK004 - Add DisplayInitialFlexScreenByEmployee(社員別法定労働時間の登録（フレックス勤務）の初期画面を表示する)

---
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/kmk004/j/DisplayInitialFlexScreenByEmployee.java
@@ -0,0 +1,37 @@
+package nts.uk.screen.at.app.kmk004.j;
+
+import java.util.Optional;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import nts.uk.ctx.at.shared.dom.scherec.statutory.worktime.flex.GetFlexPredWorkTime;
+import nts.uk.ctx.at.shared.dom.scherec.statutory.worktime.flex.GetFlexPredWorkTimeRepository;
+import nts.uk.screen.at.app.kmk004.g.GetFlexPredWorkTimeDto;
+import nts.uk.shr.com.context.AppContexts;
+
+/**
+ * 
+ * @author sonnlb
+ *
+ *         UKDesign.UniversalK.就業.KDW_日別実績.KMK_計算マスタ.KMK004_法定労働時間の登録（New）.J：社員別法定労働時間の登録（フレックス勤務）.メニュー別OCD.社員別法定労働時間の登録（フレックス勤務）の初期画面を表示する
+ */
+@Stateless
+public class DisplayInitialFlexScreenByEmployee {
+
+	@Inject
+	private GetFlexPredWorkTimeRepository getFlexPredWorkTimeRepo;
+
+	public GetFlexPredWorkTimeDto displayInitialFlexScreenByEmployee() {
+		// フレックス勤務所定労働時間取得
+		// 1. ログイン会社ID
+		Optional<GetFlexPredWorkTime> getFlexOpt = this.getFlexPredWorkTimeRepo.find(AppContexts.user().companyId());
+
+		if (getFlexOpt.isPresent()) {
+			return GetFlexPredWorkTimeDto.fromDomain(getFlexOpt.get());
+
+		}
+		return null;
+	}
+
+}
