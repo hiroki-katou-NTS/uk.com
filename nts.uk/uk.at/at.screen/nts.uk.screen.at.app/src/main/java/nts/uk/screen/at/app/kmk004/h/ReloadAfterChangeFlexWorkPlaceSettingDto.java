51b4207f1b59afe9267e368db93f74faa75b89f2
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2020-12-10 10:34:24 +0700

    MKM004 -Add ReloadAfterChangeFlexWorkPlaceSetting (職場別基本設定（フレックス勤務）を作成・変更・削除した時)

---
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/kmk004/h/ReloadAfterChangeFlexWorkPlaceSettingDto.java
@@ -0,0 +1,23 @@
+package nts.uk.screen.at.app.kmk004.h;
+
+import java.util.List;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import nts.uk.screen.at.app.query.kmk004.common.WorkplaceIdDto;
+
+/**
+ * 
+ * @author sonnlb
+ *
+ *         職場別基本設定（フレックス勤務）を作成・変更・削除した時
+ */
+@NoArgsConstructor
+@Data
+public class ReloadAfterChangeFlexWorkPlaceSettingDto {
+	
+	// 職場別基本設定（フレックス勤務）を表示する
+	DisplayFlexBasicSettingByWokPlaceDto displayFlexBasicSettingByWokPlace;
+	
+	List<WorkplaceIdDto> wkpIds;
+}
