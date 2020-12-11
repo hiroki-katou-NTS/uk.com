2fd438cb6ee95f0f7f335bac655a47cd89f1ad33
NSVN-NguyenDinhHoang <61735854+NSVN-NguyenDinhHoang@users.noreply.github.com>
2020-12-11 13:45:52 +0700

    Refactor5: fix exception KAF005

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/overtime/OverTimeShiftNight.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.dom/src/main/java/nts/uk/ctx/at/request/dom/application/overtime/OverTimeShiftNight.java
@@ -1,5 +1,6 @@
 package nts.uk.ctx.at.request.dom.application.overtime;
 
+import java.util.Collections;
 import java.util.List;
 
 import lombok.AllArgsConstructor;
@@ -18,7 +19,7 @@ import nts.uk.ctx.at.shared.dom.common.time.AttendanceTime;
 //申請就業外深夜時間
 public class OverTimeShiftNight {
 	// 休出深夜時間
-	private List<HolidayMidNightTime> midNightHolidayTimes;
+	private List<HolidayMidNightTime> midNightHolidayTimes = Collections.emptyList();
 	// 合計外深夜時間
 	private AttendanceTime midNightOutSide;
 	// 残業深夜時間
