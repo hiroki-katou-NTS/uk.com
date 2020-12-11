6c091466b618d9656a83b55ece93f6639c722ed2
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2020-12-11 15:50:49 +0700

    KMK004 - Update Finder Screen K

--- nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/kmk004/g/ComFlexMonthActCalSetDto.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/kmk004/g/ComFlexMonthActCalSetDto.java
@@ -1,7 +1,11 @@
 package nts.uk.screen.at.app.kmk004.g;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import nts.uk.ctx.at.shared.dom.scherec.monthlyattdcal.aggr.calcmethod.calcmethod.flex.com.ComFlexMonthActCalSet;
+import nts.uk.screen.at.app.kmk004.h.AggregateTimeSettingDto;
+import nts.uk.screen.at.app.kmk004.h.FlexMonthWorkTimeAggrSetDto;
+import nts.uk.screen.at.app.kmk004.h.FlexTimeHandleDto;
+import nts.uk.screen.at.app.kmk004.h.ShortageFlexSettingDto;
 
 /**
  * 
@@ -9,10 +13,26 @@ import lombok.Data;
  *
  *         会社別フレックス勤務集計方法
  */
-@AllArgsConstructor
 @Data
-public class ComFlexMonthActCalSetDto {
+public class ComFlexMonthActCalSetDto extends FlexMonthWorkTimeAggrSetDto {
 
 	/** 所定労動時間使用区分 */
 	private boolean withinTimeUsageAttr;
+
+	public ComFlexMonthActCalSetDto(boolean withinTimeUsageAttr, String comId, int aggrMethod,
+			ShortageFlexSettingDto insufficSet, AggregateTimeSettingDto legalAggrSet,
+			FlexTimeHandleDto flexTimeHandle) {
+		super(comId, aggrMethod, insufficSet, legalAggrSet, flexTimeHandle);
+		this.withinTimeUsageAttr = withinTimeUsageAttr;
+	}
+
+	public static ComFlexMonthActCalSetDto fromDomain(ComFlexMonthActCalSet domain) {
+
+		return new ComFlexMonthActCalSetDto(domain.isWithinTimeUsageAttr(), domain.getComId(),
+				domain.getAggrMethod().value, ShortageFlexSettingDto.fromDomain(domain.getInsufficSet()),
+				AggregateTimeSettingDto.fromDomain(domain.getLegalAggrSet()),
+				FlexTimeHandleDto.fromDomain(domain.getFlexTimeHandle()));
+
+	}
+
 }
---
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/kmk004/k/DisplayInitialFlexBasicSettingByEmployee.java
@@ -0,0 +1,39 @@
+package nts.uk.screen.at.app.kmk004.k;
+
+import java.util.Optional;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import nts.uk.ctx.at.shared.dom.scherec.monthlyattdcal.aggr.calcmethod.calcmethod.flex.sha.ShaFlexMonthActCalSet;
+import nts.uk.ctx.at.shared.dom.scherec.monthlyattdcal.aggr.calcmethod.calcmethod.flex.sha.ShaFlexMonthActCalSetRepo;
+import nts.uk.screen.at.app.kmk004.j.ShaFlexMonthActCalSetDto;
+import nts.uk.shr.com.context.AppContexts;
+
+/**
+ * 
+ * @author sonnlb
+ *
+ *         UKDesign.UniversalK.就業.KDW_日別実績.KMK_計算マスタ.KMK004_法定労働時間の登録（New）.K：基本設定（フレックス勤務）.メニュー別OCD.職場別基本設定（フレックス勤務）の初期画面を表示する
+ */
+@Stateless
+public class DisplayInitialFlexBasicSettingByEmployee {
+	@Inject
+	private ShaFlexMonthActCalSetRepo shaFlexMonthActCalSetRepo;
+
+	public ShaFlexMonthActCalSetDto displayInitialFlexBasicSettingByEmployee(String sId) {
+
+		// 社員別フレックス勤務集計方法
+
+		Optional<ShaFlexMonthActCalSet> ShaFlexOpt = this.shaFlexMonthActCalSetRepo.find(AppContexts.user().companyId(),
+				sId);
+
+		if (ShaFlexOpt.isPresent()) {
+			return ShaFlexMonthActCalSetDto.fromDomain(ShaFlexOpt.get());
+		}
+
+		return null;
+
+	}
+
+}
