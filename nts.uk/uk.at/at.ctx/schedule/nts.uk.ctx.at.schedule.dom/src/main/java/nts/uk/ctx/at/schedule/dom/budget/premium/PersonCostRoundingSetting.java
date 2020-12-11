0c39ea964b634be597bfbee58c79261ed61ac95b
chinh.hm <chinh.hm@3si.vn>
2020-12-11 13:59:41 +0700

    update kml 001

---
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/budget/premium/PersonCostRoundingSetting.java
@@ -0,0 +1,10 @@
+package nts.uk.ctx.at.schedule.dom.budget.premium;
+
+import org.eclipse.persistence.internal.xr.ValueObject;
+
+/**
+ * VO: 人件費丸め設定
+ */
+public class PersonCostRoundingSetting extends ValueObject {
+    //1: 単価＊割増率の丸め:人件費単価丸め
+}
---
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/budget/premium/UnitPriceRoundingSetting.java
@@ -0,0 +1,11 @@
+package nts.uk.ctx.at.schedule.dom.budget.premium;
+
+import org.eclipse.persistence.internal.xr.ValueObject;
+
+
+/**
+ * VO: 人件費単価丸め
+ */
+public class UnitPriceRoundingSetting extends ValueObject{
+    //1 : 端数処理 :人件費単価端数処理
+}
