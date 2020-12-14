7f9af955f19cfa73c22266244ed54c58ec6a74cb
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2020-12-14 13:48:01 +0700

    Create API get work Time

--- nts.uk/uk.at/at.screen/nts.uk.screen.at.ws/src/main/java/nts/uk/screen/at/ws/kmk/kmk004/c/Kmk004CWebSevice.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.ws/src/main/java/nts/uk/screen/at/ws/kmk/kmk004/c/Kmk004CWebSevice.java
@@ -12,6 +12,8 @@ import nts.arc.layer.ws.WebService;
 import nts.uk.ctx.at.shared.dom.scherec.statutory.worktime.monunit.MonthlyWorkTimeSet.LaborWorkTypeAttr;
 import nts.uk.screen.at.app.kmk004.c.BasicSettingsForWorkplace;
 import nts.uk.screen.at.app.query.kmk004.b.DisplayBasicSettingsDto;
+import nts.uk.screen.at.app.query.kmk004.common.DisplayMonthlyWorkingDto;
+import nts.uk.screen.at.app.query.kmk004.common.MonthlyWorkingHoursByWorkplace;
 import nts.uk.screen.at.app.query.kmk004.common.WorkplaceIdDto;
 import nts.uk.screen.at.app.query.kmk004.common.WorkplaceList;
 import nts.uk.screen.at.app.query.kmk004.common.YearDto;
@@ -35,24 +37,33 @@ public class Kmk004CWebSevice extends WebService{
 	@Inject
 	private YearlyListByWorkplace getYears;
 	
+	@Inject
+	private MonthlyWorkingHoursByWorkplace getWorkTimes;
+	
 	
 	@POST
-	@Path("viewc/wpl/getWorkPlaceId")
+	@Path("viewc/wkp/getWorkPlaceId")
 	public List<WorkplaceIdDto> getWorkPlaceId() {
 		return this.workplaceList.get(LaborWorkTypeAttr.REGULAR_LABOR);
 	}
 	
 	
 	@POST
-	@Path("viewc/wpl/getBaseSetting/{wkpId}")
+	@Path("viewc/wkp/getBaseSetting/{wkpId}")
 	public DisplayBasicSettingsDto getBaseSetting(@PathParam("wkpId") String wkpId) {
 		return this.baseSetting.getSetting(wkpId);
 	}
 	
 	@POST
-	@Path("viewc/wpl/getYears/{wkpId}")
+	@Path("viewc/wkp/getYears/{wkpId}")
 	public List<YearDto> getYears(@PathParam("wkpId") String wkpId) {
 		return this.getYears.get(wkpId, LaborWorkTypeAttr.REGULAR_LABOR);
+	}
+	
+	@POST
+	@Path("viewc/wkp/getWorkTimes/{wkpId}/{year}")
+	public List<DisplayMonthlyWorkingDto> getWorkTimes(@PathParam("wkpId") String wkpId, @PathParam("year") int year) {
+		return this.getWorkTimes.get(wkpId, LaborWorkTypeAttr.REGULAR_LABOR, year);
 	}
 	
 }
