2cdddb5ec9a5277863202e85425c5b73d7804e66
chinh.hm <chinh.hm@3si.vn>
2020-12-14 08:48:43 +0700

    update kml 001

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/budget/premium/HowToSetUnitPrice.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/budget/premium/HowToSetUnitPrice.java
@@ -6,6 +6,17 @@ package nts.uk.ctx.at.schedule.dom.budget.premium;
  */
 public enum HowToSetUnitPrice {
 
-    // 割増率を設定する
+    //1: 割増率を設定する
+    SET_PREMIUM_RATE(0),
+
+    //2: 単価を設定する
+    SET_UNIT_PRICE(1);
+
+    public final int value;
+
+    HowToSetUnitPrice(int value) {
+        this.value = value;
+    }
+
 
 }
