1a661e60cd1970cb9972fd0c51f311700f19da00
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2020-12-14 18:20:18 +0700

    Create API get Setting view E

--- nts.uk/uk.at/at.screen/nts.uk.screen.at.ws/src/main/java/nts/uk/screen/at/ws/kmk/kmk004/e/Kmk004EWebSevice.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.ws/src/main/java/nts/uk/screen/at/ws/kmk/kmk004/e/Kmk004EWebSevice.java
@@ -5,10 +5,13 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import nts.arc.layer.ws.WebService;
 import nts.uk.ctx.at.shared.dom.scherec.statutory.worktime.monunit.MonthlyWorkTimeSet.LaborWorkTypeAttr;
+import nts.uk.screen.at.app.kmk004.e.BasicSettingsForEmployee;
+import nts.uk.screen.at.app.query.kmk004.b.DisplayBasicSettingsDto;
 import nts.uk.screen.at.app.query.kmk004.common.EmployeeIdDto;
 import nts.uk.screen.at.app.query.kmk004.common.EmployeeList;
 
@@ -25,11 +28,20 @@ public class Kmk004EWebSevice extends WebService {
 	@Inject
 	private EmployeeList employeeList;
 	
+	@Inject
+	private BasicSettingsForEmployee basicSettings;
+	
 	
 	@POST
-	@Path("viewd/sha/getEmployeeId")
+	@Path("viewe/sha/getEmployeeId")
 	public List<EmployeeIdDto> getEmployeeId() {
 		return this.employeeList.get(LaborWorkTypeAttr.REGULAR_LABOR);
+	}
+	
+	@POST
+	@Path("viewe/sha/getBaseSetting/{sid}")
+	public DisplayBasicSettingsDto getBaseSetting(@PathParam("sid") String sid) {
+		return this.basicSettings.getSetting(sid);
 	}
 	
 }
