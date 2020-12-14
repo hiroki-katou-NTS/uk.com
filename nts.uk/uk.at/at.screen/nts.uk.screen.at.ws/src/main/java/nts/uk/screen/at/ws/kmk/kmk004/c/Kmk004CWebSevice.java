1a67b27a435335d398ca249cdc77604a43223ac8
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2020-12-14 14:29:37 +0700

    Create API save workTime in view C

--- nts.uk/uk.at/at.screen/nts.uk.screen.at.ws/src/main/java/nts/uk/screen/at/ws/kmk/kmk004/c/Kmk004CWebSevice.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.ws/src/main/java/nts/uk/screen/at/ws/kmk/kmk004/c/Kmk004CWebSevice.java
@@ -1,6 +1,7 @@
 package nts.uk.screen.at.ws.kmk.kmk004.c;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 import javax.ws.rs.POST;
@@ -10,6 +11,10 @@ import javax.ws.rs.Produces;
 
 import nts.arc.layer.ws.WebService;
 import nts.uk.ctx.at.shared.dom.scherec.statutory.worktime.monunit.MonthlyWorkTimeSet.LaborWorkTypeAttr;
+import nts.uk.screen.at.app.command.kmk.kmk004.MonthlyLaborTimeCommand;
+import nts.uk.screen.at.app.command.kmk.kmk004.monthlyworktimesetwkp.MonthlyWorkTimeSetWkpCommand;
+import nts.uk.screen.at.app.command.kmk.kmk004.monthlyworktimesetwkp.SaveMonthlyWorkTimeSetWkpCommand;
+import nts.uk.screen.at.app.command.kmk.kmk004.monthlyworktimesetwkp.SaveMonthlyWorkTimeSetWkpCommandHandler;
 import nts.uk.screen.at.app.kmk004.c.BasicSettingsForWorkplace;
 import nts.uk.screen.at.app.query.kmk004.b.DisplayBasicSettingsDto;
 import nts.uk.screen.at.app.query.kmk004.common.DisplayMonthlyWorkingDto;
@@ -40,6 +45,9 @@ public class Kmk004CWebSevice extends WebService{
 	@Inject
 	private MonthlyWorkingHoursByWorkplace getWorkTimes;
 	
+	@Inject
+	private SaveMonthlyWorkTimeSetWkpCommandHandler saveMonthlyWorkTimeSetWkpCommandHandler;
+	
 	
 	@POST
 	@Path("viewc/wkp/getWorkPlaceId")
@@ -64,6 +72,18 @@ public class Kmk004CWebSevice extends WebService{
 	@Path("viewc/wkp/getWorkTimes/{wkpId}/{year}")
 	public List<DisplayMonthlyWorkingDto> getWorkTimes(@PathParam("wkpId") String wkpId, @PathParam("year") int year) {
 		return this.getWorkTimes.get(wkpId, LaborWorkTypeAttr.REGULAR_LABOR, year);
+	}
+	
+	@POST
+	@Path("viewc/wkp/monthlyWorkTime/add")
+	public void getWorkTimes(WorkTimeInputViewC input) {
+		List<MonthlyWorkTimeSetWkpCommand> result = input.laborTime.stream().map(m -> {
+			return new MonthlyWorkTimeSetWkpCommand(input.workPlaceId, 0, input.year,
+					new MonthlyLaborTimeCommand(m, null, null));
+		}).collect(Collectors.toList());
+
+		SaveMonthlyWorkTimeSetWkpCommand command = new SaveMonthlyWorkTimeSetWkpCommand(result);
+		this.saveMonthlyWorkTimeSetWkpCommandHandler.handle(command);
 	}
 	
 }
