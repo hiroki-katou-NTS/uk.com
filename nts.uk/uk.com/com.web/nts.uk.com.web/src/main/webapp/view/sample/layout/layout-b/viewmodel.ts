9baf26fa8c3290e8d34bed37734c328a94a86cfc
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2020-12-16 16:25:22 +0700

    update layout

---
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/sample/layout/layout-b/viewmodel.ts
@@ -0,0 +1,9 @@
+/// <reference path="../../../../lib/nittsu/viewcontext.d.ts" />
+
+module nts.uk.ui.sample.layout.b {
+
+    @bean()
+    export class ViewModel extends ko.ViewModel {
+
+    }
+}
---
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/sample/layout/simple/viewmodel.ts
@@ -0,0 +1,9 @@
+/// <reference path="../../../../lib/nittsu/viewcontext.d.ts" />
+
+module nts.uk.ui.sample.layout.simple {
+
+    @bean()
+    export class ViewModel extends ko.ViewModel {
+
+    }
+}
