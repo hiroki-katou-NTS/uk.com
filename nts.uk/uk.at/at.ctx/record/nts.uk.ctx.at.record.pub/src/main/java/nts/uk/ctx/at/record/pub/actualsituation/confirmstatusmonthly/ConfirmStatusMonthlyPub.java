33dfc72c15316c2b46c194ef07ff6c676d5a9c5a
NSVN-TrieuKhacTu <tu_tk@nittsusystem-vn.com>
2020-12-17 13:05:58 +0700

    create pub to class   ConfirmStatusMonthly ver 3

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.pub/src/main/java/nts/uk/ctx/at/record/pub/actualsituation/confirmstatusmonthly/ConfirmStatusMonthlyPub.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.pub/src/main/java/nts/uk/ctx/at/record/pub/actualsituation/confirmstatusmonthly/ConfirmStatusMonthlyPub.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import nts.arc.time.YearMonth;
+import nts.uk.ctx.at.record.dom.workrecord.actualsituation.confirmstatusmonthly.MonthlyPerformaceLockStatus;
 
 public interface ConfirmStatusMonthlyPub {
-	public Optional<StatusConfirmMonthEx> getConfirmStatusMonthly(String companyId, List<String> listEmployeeId, YearMonth yearmonthInput, Integer clsId, boolean clearState);
+	public Optional<StatusConfirmMonthEx> getConfirmStatusMonthly(String companyId, List<String> listEmployeeId,
+			YearMonth yearmonthInput, Integer clsId, Optional<MonthlyPerformaceLockStatus> lockData);
 }
