4e81d4d8640f43056b819e23c901fabd92d7a634
NSVN-NguyenDinhHoang <61735854+NSVN-NguyenDinhHoang@users.noreply.github.com>
2020-12-17 16:32:52 +0700

    Refactor5: update step 2 KAFS05

--- nts.uk/uk.mobile/ClientApp/views/kaf/s05/step2/index.vue
+++ nts.uk/uk.mobile/ClientApp/views/kaf/s05/step2/index.vue
@@ -1,5 +1,84 @@
 <template>
 <div class="kafs05step2">
+    <!-- Errro-->
+    
+
+    <!-- OverTime -->
+    <div v-if="true" class="card card-label">
+      <div class="card-header uk-bg-accordion mt-2 mb-n2">
+        <span>{{ "KAFS05_70" | i18n }}</span>
+        <span class="badge badge-warning">必須</span>
+      </div>
+      <div v-for="(item, index) in overTimes"
+          v-bind:key="index"
+          :value="index"
+      >
+        <div class="card-body">
+          <div class="row mt-3">
+            <div class="col-3">{{item.title}}</div>
+            <div class="col-5">
+              <kafs00subp1 
+                    v-bind:params=item.preApp
+                    />
+            </div>
+            <div class="col-3.5">
+              <kafs00subp1 
+                    v-bind:params=item.actualApp
+                    />
+            </div>    
+          </div>
+          <div class="card-body"> 
+              <nts-time-editor
+                v-model= item.applicationTime
+                name=""
+                require= true
+                showTitle= true
+                time-input-type="time-duration"
+              />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- HolidayTime -->
+
+    <div v-if="true" class="card card-label">
+      <div class="card-header uk-bg-accordion mt-2 mb-n2">
+        <span>{{ "KAFS05_73" | i18n }}</span>
+        <span class="badge badge-warning">必須</span>
+      </div>
+      <div v-for="(item, index) in holidayTimes"
+          v-bind:key="index"
+          :value="index"
+      >
+        <div class="card-body">
+          <div class="row mt-3">
+            <div class="col-3">{{item.title}}</div>
+            <div class="col-5">
+              <kafs00subp1 
+                    v-bind:params=item.preApp
+                    />
+            </div>
+            <div class="col-3.5">
+              <kafs00subp1 
+                    v-bind:params=item.actualApp
+                    />
+            </div>    
+          </div>
+          <div class="card-body"> 
+              <nts-time-editor
+                v-model= item.applicationTime
+                name=""
+                require= true
+                showTitle= true
+                time-input-type="time-duration"
+              />
+          </div>
+        </div>
+      </div>
+    </div>
+
+
     <div class="card card-label">
         <!-- A2_C1_1 -->
         <button
