8c25df7f72ded99b3e776d24a221a0942e92f0d2
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2020-12-18 15:21:31 +0700

    Update WS update Emp view F

--- nts.uk/uk.at/at.screen/nts.uk.screen.at.ws/src/main/java/nts/uk/screen/at/ws/kmk/kmk004/f/Kmk004FWebSevice.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.ws/src/main/java/nts/uk/screen/at/ws/kmk/kmk004/f/Kmk004FWebSevice.java
@@ -6,8 +6,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 import nts.arc.layer.ws.WebService;
+import nts.uk.screen.at.app.command.kmk.kmk004.f.DeleteByIdHandler;
 import nts.uk.screen.at.app.command.kmk.kmk004.f.DeleteSettingsForCompany;
+import nts.uk.screen.at.app.command.kmk.kmk004.f.DeleteSettingsForEmp;
+import nts.uk.screen.at.app.command.kmk.kmk004.f.UpdateSettingsByIdHandler;
 import nts.uk.screen.at.app.command.kmk.kmk004.f.UpdateSettingsForCompany;
+import nts.uk.screen.at.app.command.kmk.kmk004.f.UpdateSettingsForEmp;
 import nts.uk.screen.at.app.command.kmk.kmk004.f.UpdateSettingsHandler;
 
 /**
@@ -26,6 +30,12 @@ public class Kmk004FWebSevice extends WebService{
 	@Inject
 	private DeleteSettingsForCompany deleteByCom;
 	
+	@Inject
+	private UpdateSettingsForEmp updateByEmp;
+	
+	@Inject
+	private DeleteSettingsForEmp deleteByEmp;
+	
 	@POST
 	@Path("viewf/com/setting/update")
 	public void addOrUpdateCom(UpdateSettingsHandler command) {
@@ -36,5 +46,17 @@ public class Kmk004FWebSevice extends WebService{
 	@Path("viewf/com/setting/delete")
 	public void DeleteCom() {
 		this.deleteByCom.delete();;
+	}
+	
+	@POST
+	@Path("viewf/emp/setting/update")
+	public void addOrUpdateEmp(UpdateSettingsByIdHandler command) {
+		this.updateByEmp.handle(command);;
+	}
+	
+	@POST
+	@Path("viewf/emp/setting/delete")
+	public void DeleteEmp(DeleteByIdHandler command) {
+		this.deleteByEmp.handle(command);;
 	}
 }
