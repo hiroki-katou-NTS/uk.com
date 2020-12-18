11352c689f26d3811126f378a45267ed4974dd0e
NSC-LanLT <lan-lt@nittsusystem.co.jp>
2020-12-18 10:30:59 +0900

    作業枠NO: @IntegerRange(min = 1, max = 5)を修正しました。

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/scherec/taskmanagement/taskframe/TaskFrameNo.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/scherec/taskmanagement/taskframe/TaskFrameNo.java
@@ -1,16 +1,14 @@
 package nts.uk.ctx.at.shared.dom.scherec.taskmanagement.taskframe;
 
 import nts.arc.primitive.IntegerPrimitiveValue;
-import nts.arc.primitive.constraint.IntegerMaxValue;
-import nts.arc.primitive.constraint.IntegerMinValue;
+import nts.arc.primitive.constraint.IntegerRange;
 /**
  * 作業枠NO
  * UKDesign.ドメインモデル.NittsuSystem.UniversalK.就業.shared(勤務予定、勤務実績).作業管理.作業枠.作業枠NO
  * @author lan_lt
  *
  */
-@IntegerMinValue(1)
-@IntegerMaxValue(5)
+@IntegerRange(min = 1, max = 5)
 public class TaskFrameNo extends IntegerPrimitiveValue<TaskFrameNo> {
 
 	/** serialVersionUID  */
