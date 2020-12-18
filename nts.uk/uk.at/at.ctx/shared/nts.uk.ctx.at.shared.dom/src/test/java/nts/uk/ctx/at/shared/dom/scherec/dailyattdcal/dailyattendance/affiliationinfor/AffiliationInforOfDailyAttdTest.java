1e887b26d9bc41411f074bd3c8678212d6f6f5eb
NTS-PhamVanDan <dan_pv@nittsusystem-vn.com>
2020-12-18 10:17:02 +0900

    update AffiliationInforOfDailyAttdTest, add private unit test

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/test/java/nts/uk/ctx/at/shared/dom/scherec/dailyattdcal/dailyattendance/affiliationinfor/AffiliationInforOfDailyAttdTest.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/test/java/nts/uk/ctx/at/shared/dom/scherec/dailyattdcal/dailyattendance/affiliationinfor/AffiliationInforOfDailyAttdTest.java
@@ -15,13 +15,121 @@ import nts.uk.ctx.at.shared.dom.adapter.employee.SClsHistImport;
 import nts.uk.ctx.at.shared.dom.adapter.employment.SharedSyEmploymentImport;
 import nts.uk.ctx.at.shared.dom.adapter.jobtitle.SharedAffJobTitleHisImport;
 import nts.uk.ctx.at.shared.dom.adapter.workplace.SharedAffWorkPlaceHisImport;
+import nts.uk.ctx.at.shared.dom.employeeworkway.businesstype.employee.BusinessTypeOfEmployee;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.bonuspay.primitives.BonusPaySettingCode;
+import nts.uk.ctx.at.shared.dom.vacation.setting.compensatoryleave.EmploymentCode;
 import nts.uk.ctx.at.shared.dom.workingcondition.WorkingConditionItem;
+import nts.uk.ctx.at.shared.dom.workrule.businesstype.BusinessTypeCode;
 
 public class AffiliationInforOfDailyAttdTest {
 	
 	@Injectable
 	AffiliationInforOfDailyAttd.Require require;
+	
+	@Test
+	public void testGetEmploymentCode() {
+		new Expectations() {{
+			require.getAffEmploymentHistory( anyString, (GeneralDate) any);
+			result = new SharedSyEmploymentImport("empId", "E01", "正社員", 
+						new DatePeriod(
+							GeneralDate.ymd(2020, 1, 1), 
+							GeneralDate.ymd(2020, 12, 31)));
+		}};
+		
+		EmploymentCode result = NtsAssert.Invoke.staticMethod(AffiliationInforOfDailyAttd.class, "getEmploymentCode", 
+				require, "empId", GeneralDate.today());
+		
+		assertThat( result.v() ).isEqualTo( "E01" );
+	}
+	
+	@Test
+	public void testGetJobTitleId() {
+		new Expectations() {{
+			require.getAffJobTitleHistory( anyString, (GeneralDate) any);
+			result = new SharedAffJobTitleHisImport(
+						"empId", 
+						"job-id", 
+						new DatePeriod(
+							GeneralDate.ymd(2020, 1, 1), 
+							GeneralDate.ymd(2020, 12, 31)), 
+						"job-name");
+		}};
+		
+		String result = NtsAssert.Invoke.staticMethod(AffiliationInforOfDailyAttd.class, "getJobTitleId", 
+				require, "empId", GeneralDate.today());
+		
+		assertThat( result ).isEqualTo( "job-id" );
+	}
+	
+	@Test
+	public void testGetWorkplaceId() {
+		new Expectations() {{
+			require.getAffWorkplaceHistory( anyString, (GeneralDate) any);
+			result = new SharedAffWorkPlaceHisImport(
+						new DatePeriod(
+								GeneralDate.ymd(2020, 1, 1), 
+								GeneralDate.ymd(2020, 12, 31)), 
+						"empId",
+						"wpl-id", 
+						"wpl-code", 
+						"wpl-name", 
+						"wpl-displayName");
+		}};
+		
+		String result = NtsAssert.Invoke.staticMethod(AffiliationInforOfDailyAttd.class, "getWorkplaceId", 
+				require, "empId", GeneralDate.today());
+		
+		assertThat( result ).isEqualTo( "wpl-id" );
+	}
+	
+	@Test
+	public void testGetClassificationCode() {
+		new Expectations() {{
+			require.getClassificationHistory( anyString, (GeneralDate) any);
+			result = new SClsHistImport(
+						new DatePeriod(
+								GeneralDate.ymd(2020, 1, 1), 
+								GeneralDate.ymd(2020, 12, 31)), 
+						"empId", 
+						"class-code", 
+						"class-name");
+		}};
+		
+		ClassificationCode result = NtsAssert.Invoke.staticMethod(AffiliationInforOfDailyAttd.class, "getClassificationCode", 
+				require, "empId", GeneralDate.today());
+		
+		assertThat( result.v() ).isEqualTo( "class-code" );
+	}
+	
+	@Test
+	public void testGetBusinessTypeCode_empty() {
+		
+		new Expectations() {{
+			require.getBusinessType(anyString, (GeneralDate) any);
+			//result = empty
+		}};
+		
+		Optional<BusinessTypeCode> result = NtsAssert.Invoke.staticMethod(AffiliationInforOfDailyAttd.class, "getBusinessTypeCode", 
+				require, "empId", GeneralDate.today());
+		
+		assertThat( result ).isEmpty();
+	}
+	
+	@Test
+	public void testGetBusinessTypeCode_ok() {
+		
+		new Expectations() {{
+			require.getBusinessType(anyString, (GeneralDate) any);
+			result = Optional.of(
+						new BusinessTypeOfEmployee(
+								new BusinessTypeCode("001"), "histId", "empId"));
+		}};
+		
+		Optional<BusinessTypeCode> result = NtsAssert.Invoke.staticMethod(AffiliationInforOfDailyAttd.class, "getBusinessTypeCode", 
+				require, "empId", GeneralDate.today());
+		
+		assertThat( result.get().v() ).isEqualTo( "001" );
+	}
 	
 	@Test
 	public void testGetBonusPaySettingCode_exception() {
@@ -124,6 +232,8 @@ public class AffiliationInforOfDailyAttdTest {
 		assertThat( result.getBonusPaySettingCode().get().v() ).isEqualTo( "001" );
 		
 	}
+	
+	
 	
 	static class Helper {
 		
