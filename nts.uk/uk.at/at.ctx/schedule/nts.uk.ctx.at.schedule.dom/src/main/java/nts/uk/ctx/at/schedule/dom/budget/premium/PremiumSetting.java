0830a719ade9b04b79c8b563e2b25823d0d07d58
chinh.hm <chinh.hm@3si.vn>
2020-12-21 11:25:48 +0700

    update kml 001

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/budget/premium/PremiumSetting.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/budget/premium/PremiumSetting.java
@@ -5,6 +5,8 @@ import java.util.List;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
 import nts.arc.validate.Validatable;
+import nts.uk.ctx.at.schedule.dom.schedule.basicschedule.personalfee.ExtraTimeItemNo;
+
 /**
  * 割増設定
  * @author Doan Duy Hung
@@ -17,26 +19,23 @@ public class PremiumSetting implements Validatable{
 	private String companyID;
 	
 	private String historyID;
-	
-	private Integer displayNumber;
-	
+
+	private ExtraTimeItemNo iD;
+
     private PremiumRate rate;
-    
-    private PremiumName name;
-    
-    private UseAttribute useAtr;
+
+    private UnitPrice unitPrice;
     
     private List<Integer> attendanceItems;
 
-    public PremiumSetting(String companyID, String historyID, Integer displayNumber, PremiumRate rate, PremiumName name,
+    public PremiumSetting(String companyID, String historyID, ExtraTimeItemNo iD, PremiumRate rate, UnitPrice unitPrice,
 			UseAttribute useAtr, List<Integer> attendanceItems) {
 		super();
 		this.companyID = companyID;
 		this.historyID = historyID;
-		this.displayNumber = displayNumber;
+		this.iD = iD;
 		this.rate = rate;
-		this.name = name;
-		this.useAtr = useAtr;
+		this.unitPrice = unitPrice;
 		this.attendanceItems = attendanceItems;
 	}
 
