4b02b60c97b99efb4edfc01d4fb4db05a4c2994e
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2020-12-22 19:40:06 +0700

    update tab controls

--- nts.uk/uk.base/base.web/nts.uk.com.js.web/src/main/webapp/lib/nittsu/ui/ko-ext/tabpanel-ko-ext.ts
+++ nts.uk/uk.base/base.web/nts.uk.com.js.web/src/main/webapp/lib/nittsu/ui/ko-ext/tabpanel-ko-ext.ts
@@ -1,128 +1,116 @@
 /// <reference path="../../reference.ts"/>
 
 module nts.uk.ui.koExtentions {
+    export module tabpanel {
 
-    /**
-	 * TabPanel Binding Handler
-	 */
-    class TabPanelBindingHandler implements KnockoutBindingHandler {
-        /**
-		 * Constructor.
-		 */
-        constructor() {
+        type TABPANEL_PARAMS = {
+            direction: 'horizontal' | 'vertical' | KnockoutObservable<'horizontal' | 'vertical'>;
+            active: string | KnockoutObservable<string>;
+            dataSource: TabModel[] | KnockoutObservableArray<TabModel>;
+        };
+
+        interface TabModel {
+            id: string;
+            icon: string;
+            title: string;
+            content: string;
+            enable: boolean;
+            visible: boolean;
         }
 
-        /**
-		 * Init.
-		 */
-        init(element: any, valueAccessor: () => any, allBindingsAccessor: () => any, viewModel: any, bindingContext: KnockoutBindingContext): void {
-            // Get data.
-            var data = valueAccessor();
-            var tabs: Array<any> = ko.unwrap(data.dataSource);
-            var direction: string = ko.unwrap(data.direction || "horizontal");
+        @handler({
+            bindingName: 'ntsTabPanel',
+            validatable: true,
+            virtual: false
+        })
+        export class TabPanelBindingHandler implements KnockoutBindingHandler {
+            init(element: HTMLElement): void {
+                element.classList.add('tabs-panel');
+                element.removeAttribute('data-bind');
 
-            // Container.
-            var container = $(element);
-            if (nts.uk.util.isNullOrUndefined(container.attr("tabindex")))
-                container.attr("tabindex", "0");
-            
-            container.data("tabindex", container.attr("tabindex"));
-            // Create title.
-            container.prepend('<ul></ul>');
-            var titleContainer = container.children('ul');
-            for (var i = 0; i < tabs.length; i++) {
-                var id = tabs[i].id;
-                var title = tabs[i].title;
-                titleContainer.append('<li><a href="#' + id + '">' + title + '</a></li>');
+                const childs = $(element).children();
 
-                // Wrap content.
-                var content = tabs[i].content;
-                container.children(content).wrap('<div id="' + id + '"></div>')
-            }
-            container.bind("parentactived", function(evt, dataX) {
-                dataX.child.find("div[role='tabpanel'][aria-hidden='false']:first").removeClass("disappear");
-//                data.active.valueHasMutated();
-            });
-            
-            container.bind("change-tab", function(e, newTabId) {
-                data.active(newTabId);
-                
-                // nested tabの場合にpropagationすると困る。tabIdは別なので。
-                e.stopPropagation();
-            });
-            
-            container.tabs({
-                create: function(event: Event, ui: any) {
-                    container.find('.ui-tabs-panel').addClass('disappear');
-                    ui.panel.removeClass('disappear'); 	
-                },
-                activate: function(evt: Event, ui: any) {
-                    data.active(ui.newPanel[0].id);
-                    container.find('.ui-tabs-panel').addClass('disappear');
-                    ui.newPanel.removeClass('disappear');
-                    container.children('ul').children('.ui-tabs-active').addClass('active');
-                    container.children('ul').children('li').not('.ui-tabs-active').removeClass('active');
-                    container.children('ul').children('.ui-state-disabled').addClass('disabled');
-                    container.children('ul').children('li').not('.ui-state-disabled').removeClass('disabled');
-                    let child = ui.newPanel.children().find(".ui-tabs:first");
-                    child.trigger("parentactived", {child: child});
-                }
-            }).addClass(direction);
-        }
+                const tablist = $('<div>', { class: 'tabs-list' });
 
-        /**
-		 * Update
-		 */
-        update(element: any, valueAccessor: () => any, allBindingsAccessor: () => any, viewModel: any, bindingContext: KnockoutBindingContext): void {
-            // Get data.
-            var data = valueAccessor();
-            // Get tab list.
-            var tabs: Array<any> = ko.unwrap(data.dataSource);
-            // Container.
-            var container = $(element);
-            // Select tab.
-            var activeTab = _.find(tabs, function(tab) {
-                return tab.id == data.active();
-            });
-            var indexActive = tabs.indexOf(activeTab);
-            let oldIndexActive = container.tabs("option", "active");
-            if (oldIndexActive !== indexActive) {
-                container.tabs("option", "active", indexActive);
+                tablist
+                    .appendTo(element);
+
+                const wrapper = $('<div>', { class: 'tabs-content' });
+
+                wrapper
+                    .appendTo(element).append(childs);
             }
 
-            if (!activeTab.enable() || !activeTab.visible()) {
-                let firstActiveTab = _.find(tabs, function(tab) {
-                    return tab.enable() && tab.visible();
-                });
-                if (!nts.uk.util.isNullOrUndefined(firstActiveTab)) {
-                    data.active(firstActiveTab.id);
-                    var firstIndexActive = tabs.indexOf(firstActiveTab);
-                    container.tabs("option", "active", firstIndexActive);
+            update(element: HTMLElement, valueAccessor: () => TABPANEL_PARAMS, allBindingsAccessor: () => any, viewModel: nts.uk.ui.vm.ViewModel, bindingContext: KnockoutBindingContext & { $vm: nts.uk.ui.vm.ViewModel }): void {
+                const mvm = new ko.ViewModel();
+                const data = valueAccessor();
+                const active = ko.unwrap<string>(data.active);
+                const dataSource = ko.unwrap<TabModel[]>(data.dataSource);
+                const direction = ko.unwrap<'vertical' | 'horizontal'>(data.direction) || 'horizontal';
+
+                const $tabs = $(element).find('.tabs-list').get(0);
+                const $contents = $(element).find('.tabs-content').get(0);
+
+                element.classList.remove('vertical');
+                element.classList.remove('horizontal');
+
+                element.classList.add(direction);
+
+                if ($tabs) {
+                    $tabs.innerHTML = '';
+
+                    const contents = dataSource.map(({ content }) => content).join(', ');
+
+                    $(element)
+                        .find(contents)
+                        .addClass('hidden');
+
+                    _.each(dataSource, (tab: TabModel) => {
+                        if (ko.unwrap<boolean>(tab.visible)) {
+                            const btn = $('<button>', {
+                                id: tab.id || '',
+                                text: mvm.$i18n(tab.title),
+                                disabled: !ko.unwrap<boolean>(tab.enable),
+                                class: direction === 'vertical' ? 'link icon' : ''
+                            })
+                                .appendTo($tabs)
+                                .on('click', () => {
+                                    if (direction === 'horizontal') {
+                                        if (ko.isObservable(data.active)) {
+                                            data.active(tab.id);
+                                        }
+                                    }
+                                });
+
+                            if (direction === 'vertical') {
+                                ko.applyBindingsToNode(btn.get(0), {
+                                    'btn-link': tab.title,
+                                    icon: tab.icon || 'PEOPLES',
+                                    width: 40,
+                                    height: 32
+                                });
+                            }
+
+                            if (active === tab.id) {
+                                btn
+                                    .addClass('active');
+
+                                $(element)
+                                    .find(tab.content)
+                                    .removeClass('hidden');
+                            }
+                        }
+                    });
+
+                    if ($contents) {
+                        if (direction === 'horizontal') {
+                            $contents.style.minHeight = '';
+                        } else {
+                            $contents.style.minHeight = `${$tabs.offsetHeight}px`;
+                        }
+                    }
                 }
             }
-
-            // Disable & visible tab.
-            tabs.forEach(tab => {
-                if (tab.enable()) {
-                    container.tabs("enable", '#' + tab.id);
-                    container.children('#' + tab.id).children('div').show();
-                    container.children('ul').children('li[aria-controls="' + tab.id + '"]').removeClass('disabled');
-                } else {
-                    container.tabs("disable", '#' + tab.id);
-                    container.children('#' + tab.id).children('div').hide();
-                    container.children('ul').children('li[aria-controls="' + tab.id + '"]').addClass('disabled');
-                }
-                if (!tab.visible()) {
-                    container.children('ul').children('li[aria-controls="' + tab.id + '"]').hide();
-                } else {
-                    container.children('ul').children('li[aria-controls="' + tab.id + '"]').show();
-                }
-            });
-		
-		    container.attr('tabindex', container.data("tabindex"));	
-            _.defer(() => { container.children('ul').children('li').attr("tabindex", container.data("tabindex")); });
         }
     }
-
-    ko.bindingHandlers['ntsTabPanel'] = new TabPanelBindingHandler();
 }
