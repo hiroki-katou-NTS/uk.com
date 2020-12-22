d9bb30ac5043a3fa316ac9d5083743eca9576ee4
chinh.hm <chinh.hm@3si.vn>
2020-12-22 11:34:34 +0700

    update kml 001

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.infra/src/main/java/nts/uk/ctx/at/schedule/infra/entity/budget/premium/KscmtPerCostPremiRate.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.infra/src/main/java/nts/uk/ctx/at/schedule/infra/entity/budget/premium/KscmtPerCostPremiRate.java
@@ -1,4 +1,40 @@
 package nts.uk.ctx.at.schedule.infra.entity.budget.premium;
 
-public class KscmtPerCostPremiRate {
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import nts.arc.time.GeneralDate;
+import nts.uk.shr.infra.data.entity.UkJpaEntity;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "KSCMT_PER_COST_PREMI_RATE")
+@AllArgsConstructor
+@NoArgsConstructor
+public class KscmtPerCostPremiRate extends UkJpaEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @EmbeddedId
+    public KscmtPerCostPremiRatePk pk;
+
+    @Column(name = "PREMIUM_RATE")
+    public int premiumRate;
+
+    @Column(name = "UNIT_PRICE")
+    public int unitPrice;
+
+    @Override
+    protected Object getKey() {
+        return pk;
+    }
 }
