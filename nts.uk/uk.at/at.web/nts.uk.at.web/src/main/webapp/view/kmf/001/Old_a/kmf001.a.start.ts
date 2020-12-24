c557fad4a27bc4e44ddee9eb56825c0e23f5969c
NSVN-HieuLT <king.of.throw.game@gmail.com>
2020-12-24 11:42:58 +0700

    update 001

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmf/001/Old_a/kmf001.a.start.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmf/001/Old_a/kmf001.a.start.ts
@@ -1,4 +1,4 @@
-module nts.uk.at.view.kmf001.a {
+module nts.uk.pr.view.kmf001.a {
     __viewContext.ready(function() {
         var screenModel = new viewmodel.ScreenModel();
         screenModel.startPage().done(function() {
