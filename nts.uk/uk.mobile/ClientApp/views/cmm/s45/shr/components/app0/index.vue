763008c2ee0c8ef3cdb3a39b11091a470456dcac
NSVN-NguyenDinhHoang <61735854+NSVN-NguyenDinhHoang@users.noreply.github.com>
2020-12-30 13:30:10 +0700

    Refactor5: fix css KAFS05 CMMS45C

--- nts.uk/uk.mobile/ClientApp/views/cmm/s45/shr/components/app0/index.vue
+++ nts.uk/uk.mobile/ClientApp/views/cmm/s45/shr/components/app0/index.vue
@@ -121,7 +121,7 @@
     v-for="(item, index) in overTimes"
     v-bind:key="index"
     :value="index"
-    >
+    >   
         <div v-if="c4" class="row mt-1 mb-1">
             <!-- B5_2_1 -->
             <div class="col pl-4">
@@ -147,6 +147,7 @@
                 </div>  
             </div>
         </div>
+        <hr v-if="index + 1 != overTimes.length" class="row my-0">
     </div>
 
 
@@ -189,6 +190,7 @@
                 </div>  
             </div>
         </div>
+        <hr v-if="index + 1 != holidayTimes.length" class="row my-0">
        </div> 
     </div>
 
