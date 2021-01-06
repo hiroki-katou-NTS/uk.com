cd3a9ae4f97bff8adf0cf1efc16c3a80eae951c8
NTS-PhamVanDan <dan_pv@nittsusystem-vn.com>
2021-01-06 17:02:30 +0900

    add @ZeroPaddedCode

--- nts.uk/uk.at/at.ctx/aggregation/nts.uk.ctx.at.aggregation.dom/src/main/java/nts/uk/ctx/at/aggregation/dom/scheduledailytable/ScheduleDailyTableCode.java
+++ nts.uk/uk.at/at.ctx/aggregation/nts.uk.ctx.at.aggregation.dom/src/main/java/nts/uk/ctx/at/aggregation/dom/scheduledailytable/ScheduleDailyTableCode.java
@@ -4,6 +4,7 @@ import nts.arc.primitive.constraint.CharType;
 import nts.arc.primitive.constraint.StringCharType;
 import nts.arc.primitive.constraint.StringMaxLength;
 import nts.uk.shr.com.primitive.CodePrimitiveValue;
+import nts.uk.shr.com.primitive.ZeroPaddedCode;
 
 /**
  * 勤務計画実施表のコード
@@ -13,6 +14,7 @@ import nts.uk.shr.com.primitive.CodePrimitiveValue;
  */
 @StringCharType(CharType.NUMERIC)
 @StringMaxLength(2)
+@ZeroPaddedCode
 public class ScheduleDailyTableCode extends CodePrimitiveValue<ScheduleDailyTableCode> {
 
 	/**
