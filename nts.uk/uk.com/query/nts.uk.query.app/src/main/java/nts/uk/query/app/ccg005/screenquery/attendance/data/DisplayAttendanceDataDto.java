52278dafb1e4e0f5a6d76500b130b70bc757b807
nws-dongnt <dongnt@newwave.vn>
2021-01-11 14:40:14 +0700

    create SQ SearchEmployeeScreenQuery

--- nts.uk/uk.com/query/nts.uk.query.app/src/main/java/nts/uk/query/app/ccg005/screenquery/attendance/data/DisplayAttendanceDataDto.java
+++ nts.uk/uk.com/query/nts.uk.query.app/src/main/java/nts/uk/query/app/ccg005/screenquery/attendance/data/DisplayAttendanceDataDto.java
@@ -1,5 +1,18 @@
 package nts.uk.query.app.ccg005.screenquery.attendance.data;
 
+/*
+ * UKDesign.UniversalK.共通.CCG_メニュートップページ.CCG005_ミニ在席照会.A:ミニ在席照会.メニュー別OCD.表示初期の在席データ.表示初期の在席データDTO
+ */
 public class DisplayAttendanceDataDto {
-
+	//お気に入りの指定
+	
+	//在席情報DTO
+	
+	//感情状態を利用する
+	
+	//担当者か
+	
+	//申請名
+	
+	//自分のビジネスネーム
 }
