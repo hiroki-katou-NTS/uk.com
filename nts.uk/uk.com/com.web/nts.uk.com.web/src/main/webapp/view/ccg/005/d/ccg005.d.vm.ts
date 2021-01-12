4844853e57b0d02ef92e6dd86e0c4608ad2e03dc
hieutt <hieutt@newwave.vn>
2021-01-12 15:28:15 +0700

    Coding UI screen D

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/005/d/ccg005.d.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/005/d/ccg005.d.vm.ts
@@ -2,7 +2,6 @@
 module nts.uk.at.view.ccg005.d.screenModel {
   @bean()
   export class ViewModel extends ko.ViewModel {
-    listDataMulti: KnockoutObservableArray<any> = ko.observableArray([]);
     selectSingle: KnockoutObservable<string> = ko.observable();
     isEnable: KnockoutObservable<boolean> = ko.observable(true);
     isEditable: KnockoutObservable<boolean> = ko.observable(false);
@@ -12,16 +11,36 @@ module nts.uk.at.view.ccg005.d.screenModel {
     columns2: KnockoutObservableArray<any>;
     items: KnockoutObservableArray<any> = ko.observableArray([]);
     cId: KnockoutObservable<string> = ko.observable("");
+    name: KnockoutObservable<string> = ko.observable("");
+    option: any =  ko.mapping.fromJS(new nts.uk.ui.option.TextEditorOption({
+      textmode: "text",
+      width: "200px",
+      textalign: "left"
+    }))
+    option1: any =  ko.mapping.fromJS(new nts.uk.ui.option.TextEditorOption({
+      textmode: "text",
+      width: "155px",
+      textalign: "left"
+    }))
+    roundingRules: KnockoutObservableArray<any>;
+    selectedRuleCode: any;
+
     created() {
       const vm = this;
       vm.columns = ko.observableArray([
-        { headerText: vm.$i18n("CCG005_5"), key: "code", width: 100 },
+        { headerText: vm.$i18n("CCG005_5"), key: "code", width: 150 },
       ]);
       vm.columns2 = ko.observableArray([
         { headerText: vm.$i18n("CCG005_7"), key: "code", width: 50 },
         { headerText: vm.$i18n("CCG005_6"), key: "name", width: 80 },
       ]);
 
+      vm.roundingRules = ko.observableArray([
+        { code: '1', name: vm.$i18n("CCG005_13") },
+        { code: '2', name: vm.$i18n("CCG005_14") },
+        { code: '3', name: vm.$i18n("CCG005_16") }
+      ]);
+    vm.selectedRuleCode = ko.observable(1);
     }
 
     onClickCancel() {
