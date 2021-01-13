20cd5f4c186c0db5d9891d7e97896451fb0eba66
NSVN-TruongVanLai <NSVN-TruongVanLai@users.noreply.github.com>
2021-01-13 16:45:41 +0700

    redmine #113691

--- nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/query/kcp013/KCP013Result.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/query/kcp013/KCP013Result.java
@@ -5,20 +5,14 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import nts.uk.ctx.at.shared.dom.worktime.predset.PredetemineTimeSetting;
-import nts.uk.ctx.at.shared.dom.worktime.worktimeset.WorkTimeSetting;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class AcquireWorkingHoursDto {
-
-	private List<WorkTimeSetting> listWorkTime;
-
-	private List<PredetemineTimeSetting> predetemineTimeSettings;
+public class KCP013Result {
 	
+	private List<AcquireWorkHours> listWorkTime;
 	private boolean hasWorkTimeInModeWorkPlace;
-	
 	private boolean modeCompany; // company or workplace
 
 }
