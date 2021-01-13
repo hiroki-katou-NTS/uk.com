c25d1deb4c41223dd498d0ff36444c759e360aa0
nws-dongnt <dongnt@newwave.vn>
2021-01-13 13:55:38 +0700

    finish PersonalInfomation DomainService

--- nts.uk/uk.com/office.ctx/nts.uk.ctx.office.dom/src/main/java/nts/uk/ctx/office/dom/favorite/service/PersonalInfomationDomainService.java
+++ nts.uk/uk.com/office.ctx/nts.uk.ctx.office.dom/src/main/java/nts/uk/ctx/office/dom/favorite/service/PersonalInfomationDomainService.java
@@ -1,12 +1,10 @@
 package nts.uk.ctx.office.dom.favorite.service;
 
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
-
-import javax.inject.Inject;
 import nts.arc.time.GeneralDate;
 import nts.uk.ctx.office.dom.favorite.adapter.EmployeeBasicImport;
 import nts.uk.ctx.office.dom.favorite.adapter.EmployeeJobHistImport;
@@ -21,37 +19,90 @@ import nts.uk.ctx.office.dom.favorite.adapter.WorkplaceInforImport;
  * 個人情報を取得
  */
 public class PersonalInfomationDomainService {
-	//TODO
+
+	/**
+	 * [1] 個人情報を取得
+	 * 
+	 * 選択したお気に入りの個人情報を取得する
+	 * 
+	 * @param require
+	 * @param sids     List<社員ID>
+	 * @param baseDate 基準日
+	 * @return List<PersonalInfomationObj> List<個人基本情報>
+	 */
+	public List<PersonalInfomationObj> getPersonalInfomation(Require require, List<String> sids, GeneralDate baseDate) {
+		// 社員の職場IDを取得する
+		Map<String, String> employeesWorkplaceId = require.getEmployeesWorkplaceId(sids, baseDate);
+		// 社員の職位を取得する
+		Map<String, EmployeeJobHistImport> positionBySidsAndBaseDate = require.getPositionBySidsAndBaseDate(sids,
+				baseDate);
+		// 職位の序列を取得する
+		List<SequenceMasterImport> rankOfPosition = require.getRankOfPosition();
+
+		List<String> workSplaceId = (List<String>) employeesWorkplaceId.values();
+		workSplaceId.stream().distinct().collect(Collectors.toList());
+
+		// 職場情報を取得する
+		Map<String, WorkplaceInforImport> workplaceInfor = require.getWorkplaceInfor(workSplaceId, baseDate);
+
+		// create $職場情報SIDMap ：Map<社員ID、職場情報>
+		Map<String, WorkplaceInforImport> workplaceInforSidMap = sids.stream()
+				.collect(Collectors.toMap((sid -> sid), (sid -> workplaceInfor.get(sid))));
+
+		// 個人情報を取得する
+		Map<String, EmployeeBasicImport> personalInformation = require.getPersonalInformation(sids);
+
+		// Sort
+		List<PersonalInfomationObj> sortedList = this.sortingProcess(sids, workplaceInforSidMap,
+				positionBySidsAndBaseDate, rankOfPosition, personalInformation);
+		return sortedList;
+	}
+
 	/**
 	 * [pvt-1]ソートする処理
 	 * 
 	 * ソート：職場 → 職位 → 社員コード
 	 * 
-	 * @param sIds List<社員ID>
+	 * @param sIds          List<社員ID>
 	 * @param workplaceInfo Map<社員ID、職場情報>
-	 * @param positionId Map<社員ID、職位情報>
+	 * @param positionId    Map<社員ID、職位情報>
 	 * @param positionOrder List<序列>
-	 * @param personalInfo Map<社員ID、個人基本情報>
+	 * @param personalInfo  Map<社員ID、個人基本情報>
 	 * @return List<個人基本情報>
 	 */
 	private List<PersonalInfomationObj> sortingProcess(
-			List<String> sIds, 
-			Map<String, WorkplaceInforImport> workplaceInfo,
-			Map<String, SequenceMasterImport> positionId, 
-			List<SequenceMasterImport> positionOrder,
+			List<String> sIds,
+			Map<String, WorkplaceInforImport> workplaceInfo, 
+			Map<String, EmployeeJobHistImport> positionId,
+			List<SequenceMasterImport> positionOrder, 
 			Map<String, EmployeeBasicImport> personalInfo) {
 		// create ソート情報：｛社員ID、階層コード、Optional<並び順>、職位コード、社員コード｝
-//		List<PersonalInfomationObj> sortInfomation = sIds.stream().map(v -> {
-//			return PersonalInfomationObj.builder()
-//					.sid(v)
-//					.hierarchyCode(workplaceInfo.get(v).getHierarchyCode())
-//					.order(Optional.ofNullable(positionId.get(v).getSequenceCode().equalsIgnoreCase(positionOrder) ? positionOrder : null))
-//					.positionCode(positionId.get(v).get)
-//					.employeeCode(personalInfo.get(v).getEmployeeCode())
-//					.build();
-//			}).collect(Collectors.toList());
-		
-		return Collections.emptyList();
+		List<PersonalInfomationObj> sortInfomation = sIds.stream().map(v -> {
+			return PersonalInfomationObj.builder().sid(v).hierarchyCode(workplaceInfo.get(v).getHierarchyCode())
+					.order(getOrder(positionId.get(v).getSequenceCode(), positionOrder))
+					.positionCode(positionId.get(v).getSequenceCode())
+					.employeeCode(personalInfo.get(v).getEmployeeCode()).build();
+		}).collect(Collectors.toList());
+
+		// ソート実行
+		Comparator<PersonalInfomationObj> compare = Comparator.comparing(PersonalInfomationObj::getOrderOptional)
+				.thenComparing(PersonalInfomationObj::getPositionCode)
+				.thenComparing(PersonalInfomationObj::getEmployeeCode);
+		if (!workplaceInfo.isEmpty()) {
+			compare = Comparator.comparing(PersonalInfomationObj::getHierarchyCode)
+					.thenComparing(PersonalInfomationObj::getOrderOptional)
+					.thenComparing(PersonalInfomationObj::getPositionCode)
+					.thenComparing(PersonalInfomationObj::getEmployeeCode);
+		}
+		sortInfomation.sort(compare);
+		return sortInfomation;
+	}
+
+	private Optional<Integer> getOrder(String sequenceCode, List<SequenceMasterImport> positionOrder) {
+		List<SequenceMasterImport> filterList = positionOrder.stream()
+				.filter(position -> position.getSequenceCode().equalsIgnoreCase(sequenceCode))
+				.collect(Collectors.toList());
+		return filterList.isEmpty() ? Optional.empty() : Optional.ofNullable(filterList.get(0).getOrder());
 	}
 
 	public static interface Require {
@@ -95,6 +146,5 @@ public class PersonalInfomationDomainService {
 		 * 
 		 */
 		public Map<String, EmployeeBasicImport> getPersonalInformation(List<String> lstSid);
-
 	}
 }
