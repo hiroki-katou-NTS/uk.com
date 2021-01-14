9de095d5e78757176fb18008d7e76e643c3326bd
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2021-01-14 14:42:06 +0700

    KMK004 - update theo tài liệu thiết kế ver 10  (bổ sung msg cho trường hợp chọn năm đã tồn tại trên màn Q)

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/004/q/kmk004.q.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmk/004/q/kmk004.q.vm.ts
@@ -2,24 +2,27 @@
 
 module nts.uk.at.kmk004.q {
 
-
-	const API = {
-
-	};
-
 	@bean()
 	export class ViewModel extends ko.ViewModel {
 
 		year: KnockoutObservable<string> = ko.observable();
-		startYear: string;
+		//startYear: string;
 		endYear: string;
+		paramYears: Array<number>;
 
 		register() {
 			let vm = this;
 			if (nts.uk.ui.errors.hasError()) { return; }
-			vm.$window.close({
-				year: vm.year()
-			});
+
+			if (vm.paramYears.indexOf(Number(vm.year())) != -1) {
+				vm.$dialog.error({ messageId: 'Msg_2104' });
+				return;
+
+			} else {
+				vm.$window.close({
+					year: vm.year()
+				});
+			}
 		}
 
 		close() {
@@ -29,11 +32,11 @@ module nts.uk.at.kmk004.q {
 
 		created(params?: IParams) {
 			let vm = this;
-
+			vm.paramYears = params.years;
 			vm.year((params && params.years.length) ? String(_.max(params.years) + 1) : moment().format("YYYY"));
-			vm.startYear = vm.year();
+			//vm.startYear = vm.year();
 			vm.endYear = vm.defEnyear(params.startDate);;
-			
+
 		}
 
 		defEnyear(startDate: number) {
