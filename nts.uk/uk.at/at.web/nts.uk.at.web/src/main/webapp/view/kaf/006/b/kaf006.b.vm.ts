2f34356820f8b79d7dc5008d7efd23c847eee223
AnhNM <twas.horus@gmail.com>
2021-01-15 09:32:09 +0700

    refactor 5: KAF006: Update bug #113656

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/006/b/kaf006.b.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/006/b/kaf006.b.vm.ts
@@ -198,7 +198,8 @@ module nts.uk.at.view.kaf006_ref.b.viewmodel {
 				vm.endTime1(null);
 				vm.endTime2(null);
 
-				vm.$errors("clear");
+				// vm.$errors("clear");
+				nts.uk.ui.errors.clearAll()
 				
 				let appDates = [];
 				if (_.isNil(vm.application().opAppStartDate())) {
