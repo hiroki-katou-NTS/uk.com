8064cd33ff97e7631a545791a7debdd5be32d5f7
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2021-01-15 10:02:49 +0700

    Check null  start or end

--- nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/query/ksu/ksu002/a/RegisterWorkSceduleCommandHandler.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/query/ksu/ksu002/a/RegisterWorkSceduleCommandHandler.java
@@ -53,8 +53,13 @@ public class RegisterWorkSceduleCommandHandler extends CommandHandlerWithResult<
 
 		List<WorkScheduleSaveCommand> commands = param.registerDates.stream().map(m -> {
 			Map<Integer, TimeWithDayAttr> map = new HashMap<Integer, TimeWithDayAttr>();
+			if(m.getStart() != null){
 			map.put(31, new TimeWithDayAttr(m.getStart()));
-			map.put(34, new TimeWithDayAttr(m.getEnd()));
+			}
+			
+			if(m.getEnd() != null) {
+				map.put(34, new TimeWithDayAttr(m.getEnd()));	
+			}
 			
 			WorkInformationDto wif = new WorkInformationDto(m.getWorkTypeCd(), m.getWorkTimeCd());
 
