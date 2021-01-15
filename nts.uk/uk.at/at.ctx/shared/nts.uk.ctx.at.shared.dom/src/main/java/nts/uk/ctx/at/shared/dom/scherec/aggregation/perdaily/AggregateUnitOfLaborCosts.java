aeac2b2def998513be1649776d35f260a54b85d5
nsc-kmkotk <kumiko_otake@nittsusystem.co.jp>
2021-01-15 10:33:38 +0900

    Enum『人件費・時間の集計単位』 コメント修正

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/scherec/aggregation/perdaily/AggregateUnitOfLaborCosts.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/scherec/aggregation/perdaily/AggregateUnitOfLaborCosts.java
@@ -1,38 +1,33 @@
 package nts.uk.ctx.at.shared.dom.scherec.aggregation.perdaily;
 
+import lombok.RequiredArgsConstructor;
 import nts.arc.enums.EnumAdaptor;
 
 /**
  * 人件費・時間の集計単位
- * UKDesign.ドメインモデル.NittsuSystem.UniversalK.就業.contexts.予実集計.スケジュール集計.人件費・時間.人件費・時間の種類
+ * UKDesign.ドメインモデル.NittsuSystem.UniversalK.就業.shared(勤務予定、勤務実績).集計処理.日単位集計.人件費・時間の集計単位
  * @author dan_pv
  *
  */
+@RequiredArgsConstructor
 public enum AggregateUnitOfLaborCosts {
 
-    /**
-     * 合計
-     */
-    TOTAL(0),
+	/** 合計 **/
+	TOTAL( 0 ),
 
-    /**
-     * 就業時間
-     */
-    WORKING_HOURS(1),
+	/** 就業時間 **/
+	WORKING_HOURS( 1 ),
 
-    /**
-     * 時間外時間
-     */
-    OVERTIME(2);
+	/** 時間外時間 **/
+	OVERTIME( 2 ),
+	;
 
-    public int value;
 
-    private AggregateUnitOfLaborCosts(int value) {
-        this.value = value;
-    }
+	public final int value;
 
-    public static AggregateUnitOfLaborCosts of(int value) {
-    	return EnumAdaptor.valueOf(value, AggregateUnitOfLaborCosts.class);
-    }
+
+	public static AggregateUnitOfLaborCosts of(int value) {
+		return EnumAdaptor.valueOf(value, AggregateUnitOfLaborCosts.class);
+	}
 
 }
