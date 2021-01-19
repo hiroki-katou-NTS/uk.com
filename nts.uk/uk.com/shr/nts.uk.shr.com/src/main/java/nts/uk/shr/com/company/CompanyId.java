0008ca85640dd72f803a6876db15a142c5b15de7
m_kitahira <m_kitahira@nittsusystem.co.jp>
2021-01-19 15:54:06 +0900

    HL連携基盤のコードレビュー結果：会社コードと契約コードの取得方法を変更

--- nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/com/company/CompanyId.java
+++ nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/com/company/CompanyId.java
@@ -1,6 +1,11 @@
 package nts.uk.shr.com.company;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class CompanyId {
+	
+	private final String value;
 	
 	public static String create(String tenantCode, String companyCode) {
 		return tenantCode + "-" + companyCode;
@@ -8,5 +13,13 @@ public class CompanyId {
 
 	public static String zeroCompanyInTenant(String tenantCode) {
 		return create(tenantCode, "0000");
+	}
+	
+	public String tenantCode() {
+		return value.split("-")[0];
+	}
+	
+	public String companyCode() {
+		return value.split("-")[1];
 	}
 }
