aeb421adc125c21cd92ebe27141a5a67172aebb2
chinh.hm <chinh.hm@3si.vn>
2021-01-22 20:19:07 +0700

    update kwr005

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/workledgeroutputitem/WorkLedgerOutputItem.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/workledgeroutputitem/WorkLedgerOutputItem.java
@@ -56,43 +56,6 @@ public class WorkLedgerOutputItem extends AggregateRoot {
         return require.freeCheck(code, employeeId);
     }
 
-	/**
-	 * create
-	 *
-	 * @param id 				GUID
-	 * @param code 				コード
-	 * @param name				名称
-	 * @param settingCategory	定型自由区分
-	 * @return 勤務台帳の出力項目
-	 */
-	public static WorkLedgerOutputItem create(
-			String id,
-			OutputItemSettingCode code,
-			OutputItemSettingName name,
-			SettingClassificationCommon settingCategory) {
-
-			return new WorkLedgerOutputItem(id, code, null, name, settingCategory, null);
-	}
-
-	/**
-	 * create
-	 *
-	 * @param id 				GUID
-	 * @param code 				コード
-	 * @param name				名称
-	 * @param settingCategory	定型自由区分
-	 * @param employeeId		社員ID
-	 * @return 勤務台帳の出力項目
-	 */
-	public static WorkLedgerOutputItem create(
-			String id,
-			String employeeId,
-			OutputItemSettingCode code,
-			OutputItemSettingName name,
-			SettingClassificationCommon settingCategory) {
-
-		return new WorkLedgerOutputItem(id, code, null, name, settingCategory, employeeId);
-	}
 
     public interface Require {
 		/**
