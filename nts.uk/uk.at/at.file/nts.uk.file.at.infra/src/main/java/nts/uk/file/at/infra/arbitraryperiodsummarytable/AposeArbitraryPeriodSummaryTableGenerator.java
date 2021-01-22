bc33cf65c2d92e15c037744ce67d1d61860b0125
chinh.hm <chinh.hm@3si.vn>
2021-01-22 11:40:59 +0700

    update kwr007

--- nts.uk/uk.at/at.file/nts.uk.file.at.infra/src/main/java/nts/uk/file/at/infra/arbitraryperiodsummarytable/AposeArbitraryPeriodSummaryTableGenerator.java
+++ nts.uk/uk.at/at.file/nts.uk.file.at.infra/src/main/java/nts/uk/file/at/infra/arbitraryperiodsummarytable/AposeArbitraryPeriodSummaryTableGenerator.java
@@ -1,4 +1,19 @@
 package nts.uk.file.at.infra.arbitraryperiodsummarytable;
 
-public class AposeArbitraryPeriodSummaryTableGenerator {
+import nts.arc.layer.infra.file.export.FileGeneratorContext;
+import nts.uk.file.at.app.export.arbitraryperiodsummarytable.ArbitraryPeriodSummaryTableGenerator;
+import nts.uk.shr.infra.file.report.aspose.cells.AsposeCellsReportGenerator;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+
+
+@Stateless
+@TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
+public class AposeArbitraryPeriodSummaryTableGenerator extends AsposeCellsReportGenerator implements ArbitraryPeriodSummaryTableGenerator {
+    @Override
+    public void generate(FileGeneratorContext generatorContext, Object dataSource) {
+
+    }
 }
