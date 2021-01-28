af5ab00fc8ad904802c219851d5f9cb2b7fd3207
CORP\yuri_tamakoshi <yuri_tamakoshi@nittsusystem.co.jp>
2021-01-28 14:35:40 +0900

    update self check domain pull 20210128

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/remainingnumber/annualleave/empinfo/grantremainingdata/daynumber/AnnualLeaveRemainingDayNumber.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/remainingnumber/annualleave/empinfo/grantremainingdata/daynumber/AnnualLeaveRemainingDayNumber.java
@@ -1,7 +1,9 @@
 package nts.uk.ctx.at.shared.dom.remainingnumber.annualleave.empinfo.grantremainingdata.daynumber;
 
-import nts.arc.primitive.HalfIntegerPrimitiveValue;
-import nts.arc.primitive.constraint.HalfIntegerRange;
+/**
+ * 年休残日数
+ * */
+
 import nts.uk.ctx.at.shared.dom.remainingnumber.common.empinfo.grantremainingdata.daynumber.LeaveRemainingDayNumber;;
 
 //@HalfIntegerRange(min = -999.5 , max = 999.5)
@@ -12,12 +14,4 @@ public class AnnualLeaveRemainingDayNumber extends LeaveRemainingDayNumber{
 	public AnnualLeaveRemainingDayNumber(Double rawValue) {
 		super(rawValue);
 	}
-
-//	@Override
-//	protected Double reviseRawValue(Double rawValue) {
-//		if (rawValue == null) return super.reviseRawValue(rawValue);
-//		if (rawValue > 999.5) rawValue = 999.5;
-//		if (rawValue < -999.5) rawValue = -999.5;
-//		return super.reviseRawValue(rawValue);
-//	}
 }
