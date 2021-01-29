3758dbc9f1fda0eba7fa62c3a3502a3fd69f5809
chinh.hm <chinh.hm@3si.vn>
2021-01-29 14:06:08 +0700

    update kwr 007

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.app/src/main/java/nts/uk/ctx/at/function/app/query/arbitraryperiodsummarytable/AttendanceItemDisplayContents.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.app/src/main/java/nts/uk/ctx/at/function/app/query/arbitraryperiodsummarytable/AttendanceItemDisplayContents.java
@@ -2,7 +2,25 @@ package nts.uk.ctx.at.function.app.query.arbitraryperiodsummarytable;
 
 
 import lombok.Data;
+import nts.uk.ctx.at.function.dom.outputitemsofworkstatustable.enums.CommonAttributesOfForms;
+import nts.uk.ctx.at.shared.dom.scherec.dailyattendanceitem.enums.PrimitiveValueOfAttendanceItem;
+import org.eclipse.persistence.internal.xr.ValueObject;
 
+/**
+ * 勤怠項目表示内容
+ */
 @Data
-public class AttendanceItemDisplayContents {
+public class AttendanceItemDisplayContents extends ValueObject {
+
+    private PrimitiveValueOfAttendanceItem primitiveValue;
+    // 勤怠項目ID
+    private int attendanceItemId;
+    // 勤怠項目名称
+    private String attendanceName;
+
+    private CommonAttributesOfForms commonAttributesOfForms;
+    // 順位
+    private int rank;
+
+
 }
