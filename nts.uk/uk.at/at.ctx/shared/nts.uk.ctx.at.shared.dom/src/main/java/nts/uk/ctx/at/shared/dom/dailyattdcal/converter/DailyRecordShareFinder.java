9f74fad2d3a50108c6788aefa0a6bcbc13c93caf
NTS-NguyenXuanThanh <thanh_nx@nittsusystem.co.jp>
2021-02-01 11:29:34 +0900

    fix bug 114230v2

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/dailyattdcal/converter/DailyRecordShareFinder.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/dailyattdcal/converter/DailyRecordShareFinder.java
@@ -1,10 +1,12 @@
 package nts.uk.ctx.at.shared.dom.dailyattdcal.converter;
 
+import java.util.Optional;
+
 import nts.arc.time.GeneralDate;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.dailyattendancework.IntegrationOfDaily;
 
 public interface DailyRecordShareFinder {
 	
-	public IntegrationOfDaily find(String employeeId, GeneralDate date);
+	public Optional<IntegrationOfDaily> find(String employeeId, GeneralDate date);
 
 }
