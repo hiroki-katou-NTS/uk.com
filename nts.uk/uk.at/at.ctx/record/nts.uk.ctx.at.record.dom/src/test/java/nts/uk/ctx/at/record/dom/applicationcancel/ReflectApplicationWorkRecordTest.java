12fc0bc0e16f8fc2647f8bec0f0601aff84a049f
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2021-02-02 11:11:49 +0700

    Revert "beploy teamb"
    This reverts commit 79850e5b0c5f0b67fa303b4b4949728dffed1748.

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/test/java/nts/uk/ctx/at/record/dom/applicationcancel/ReflectApplicationWorkRecordTest.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/test/java/nts/uk/ctx/at/record/dom/applicationcancel/ReflectApplicationWorkRecordTest.java
@@ -43,119 +43,119 @@ import nts.uk.ctx.at.shared.dom.workrecord.workperfor.dailymonthlyprocessing.enu
 import nts.uk.ctx.at.shared.dom.workrule.goingout.GoingOutReason;
 import nts.uk.shr.com.time.AttendanceClock;
 
-//@RunWith(JMockit.class)
+@RunWith(JMockit.class)
 public class ReflectApplicationWorkRecordTest {
 
-//	@Injectable
-//	private ReflectApplicationWorkRecord.Require require;
-//
-//	/*
-//	 * テストしたい内容
-//	 * 
-//	 * → 打刻申請（NRモード）を反映する
-//	 * 
-//	 * → レコーダイメージ申請の対象日を取得する
-//	 * 
-//	 * → 勤務実績の更新
-//	 * 
-//	 * 準備するデータ
-//	 * 
-//	 * → 打刻申請モード = レコーダイメージ申請
-//	 * 
-//	 */
-//	@SuppressWarnings("unchecked")
-//	@Test
-//	public void test(@Mocked TimeStampApplicationNRMode nrMode, @Mocked GetTargetDateRecordApplication app) {
-//
-//		ApplicationShare application = ReflectApplicationHelper.createAppShare(ApplicationTypeShare.STAMP_APPLICATION,
-//				PrePostAtrShare.POSTERIOR, GeneralDate.ymd(2020, 01, 01));
-//		AppRecordImageShare appImg = new AppRecordImageShare(EngraveShareAtr.ATTENDANCE, new AttendanceClock(1),
-//				Optional.empty(), application);
-//
-//		appImg.setOpStampRequestMode(Optional.of(StampRequestModeShare.STAMP_ONLINE_RECORD));
-//
-//		ReflectStatusResultShare reflectStatus = new ReflectStatusResultShare();
-//		reflectStatus.setReflectStatus(ReflectedStateShare.WAITREFLECTION);
-//
-//		new Expectations() {
-//			{
-//
-//				// 「 打刻申請（NRモード）を反映する」のテスト呼び出す
-//				TimeStampApplicationNRMode.process(require, (GeneralDate) any, (AppRecordImageShare) any,
-//						(DailyRecordOfApplication) any, (Optional<Stamp>) any, (ChangeDailyAttendance) any);
-//				times = 1;
-//
-//				// 「レコーダイメージ申請の対象日を取得する」のテスト呼び出す
-//				GetTargetDateRecordApplication.getTargetDate(require, (AppRecordImageShare) any);
-//				result = Pair.of(Optional.of(GeneralDate.ymd(2020, 01, 01)), Optional.of(createStamp()));
-//				times = 1;
-//
-//			}
-//
-//		};
-//
-//		val actualResult = ReflectApplicationWorkRecord.process(require, ExecutionType.NORMAL_EXECUTION, appImg,
-//				GeneralDate.ymd(2020, 01, 01), reflectStatus);
-//
-//		assertThat(actualResult.getLeft().getReflectStatus()).isEqualTo(ReflectedStateShare.REFLECTED);
-//
-//		NtsAssert.atomTask(() -> actualResult.getRight().get(), any -> require.addAllDomain(any.get()));
-//
-//	}
-//
-//	/*
-//	 * テストしたい内容
-//	 * 
-//	 * → 申請の反映（勤務実績）
-//	 * 
-//	 * → 勤務実績の更新
-//	 * 
-//	 * 準備するデータ
-//	 * 
-//	 * → 打刻申請モード != レコーダイメージ申請
-//	 * 
-//	 */
-//	@Test
-//	public void testReflectAll(@Mocked RCCreateDailyAfterApplicationeReflect reflect) {
-//
-//		AppStampShare appImg = ReflectApplicationHelper.createAppStamp(PrePostAtrShare.POSTERIOR);
-//
-//		appImg.setOpStampRequestMode(Optional.empty());
-//
-//		ReflectStatusResultShare reflectStatus = new ReflectStatusResultShare();
-//		reflectStatus.setReflectStatus(ReflectedStateShare.WAITREFLECTION);
-//
-//		new Expectations() {
-//			{
-//
-//				// 「[RQ667]申請反映後の日別勤怠(work）を作成する（勤務実績）」のテスト呼び出す
-//				RCCreateDailyAfterApplicationeReflect.process(require, (ApplicationShare) any,
-//						(DailyRecordOfApplication) any, (GeneralDate) any);
-//				times = 1;
-//
-//			}
-//
-//		};
-//
-//		val actualResult = ReflectApplicationWorkRecord.process(require, ExecutionType.NORMAL_EXECUTION, appImg,
-//				GeneralDate.ymd(2020, 01, 01), reflectStatus);
-//
-//		assertThat(actualResult.getLeft().getReflectStatus()).isEqualTo(ReflectedStateShare.REFLECTED);
-//
-//		NtsAssert.atomTask(() -> actualResult.getRight().get(), any -> require.addAllDomain(any.get()));
-//
-//	}
-//
-//	public Stamp createStamp() {
-//		return new Stamp(new ContractCode("1"), // 契約コード
-//				new StampNumber("1"), // 打刻カード
-//				GeneralDateTime.ymdhms(2020, 01, 01, 10, 0, 0), // 打刻日時
-//				new Relieve(AuthcMethod.ID_AUTHC, // 打刻する方法. 認証方法
-//						StampMeans.NAME_SELECTION), /// 打刻する方法. 打刻手段
-//				new StampType(false, GoingOutReason.valueOf(1), // 外出理由
-//						SetPreClockArt.NONE, // 所定時刻セット区分
-//						ChangeClockArt.GOING_TO_WORK, // 時刻変更区分
-//						ChangeCalArt.NONE), // 計算区分変更対象
-//				new RefectActualResult(null, null, null, null), Optional.empty());
-//	}
+	@Injectable
+	private ReflectApplicationWorkRecord.Require require;
+
+	/*
+	 * テストしたい内容
+	 * 
+	 * → 打刻申請（NRモード）を反映する
+	 * 
+	 * → レコーダイメージ申請の対象日を取得する
+	 * 
+	 * → 勤務実績の更新
+	 * 
+	 * 準備するデータ
+	 * 
+	 * → 打刻申請モード = レコーダイメージ申請
+	 * 
+	 */
+	@SuppressWarnings("unchecked")
+	@Test
+	public void test(@Mocked TimeStampApplicationNRMode nrMode, @Mocked GetTargetDateRecordApplication app) {
+
+		ApplicationShare application = ReflectApplicationHelper.createAppShare(ApplicationTypeShare.STAMP_APPLICATION,
+				PrePostAtrShare.POSTERIOR, GeneralDate.ymd(2020, 01, 01));
+		AppRecordImageShare appImg = new AppRecordImageShare(EngraveShareAtr.ATTENDANCE, new AttendanceClock(1),
+				Optional.empty(), application);
+
+		appImg.setOpStampRequestMode(Optional.of(StampRequestModeShare.STAMP_ONLINE_RECORD));
+
+		ReflectStatusResultShare reflectStatus = new ReflectStatusResultShare();
+		reflectStatus.setReflectStatus(ReflectedStateShare.WAITREFLECTION);
+
+		new Expectations() {
+			{
+
+				// 「 打刻申請（NRモード）を反映する」のテスト呼び出す
+				TimeStampApplicationNRMode.process(require, (GeneralDate) any, (AppRecordImageShare) any,
+						(DailyRecordOfApplication) any, (Optional<Stamp>) any, (ChangeDailyAttendance) any);
+				times = 1;
+
+				// 「レコーダイメージ申請の対象日を取得する」のテスト呼び出す
+				GetTargetDateRecordApplication.getTargetDate(require, (AppRecordImageShare) any);
+				result = Pair.of(Optional.of(GeneralDate.ymd(2020, 01, 01)), Optional.of(createStamp()));
+				times = 1;
+
+			}
+
+		};
+
+		val actualResult = ReflectApplicationWorkRecord.process(require, ExecutionType.NORMAL_EXECUTION, appImg,
+				GeneralDate.ymd(2020, 01, 01), reflectStatus);
+
+		assertThat(actualResult.getLeft().getReflectStatus()).isEqualTo(ReflectedStateShare.REFLECTED);
+
+		NtsAssert.atomTask(() -> actualResult.getRight().get(), any -> require.addAllDomain(any.get()));
+
+	}
+
+	/*
+	 * テストしたい内容
+	 * 
+	 * → 申請の反映（勤務実績）
+	 * 
+	 * → 勤務実績の更新
+	 * 
+	 * 準備するデータ
+	 * 
+	 * → 打刻申請モード != レコーダイメージ申請
+	 * 
+	 */
+	@Test
+	public void testReflectAll(@Mocked RCCreateDailyAfterApplicationeReflect reflect) {
+
+		AppStampShare appImg = ReflectApplicationHelper.createAppStamp(PrePostAtrShare.POSTERIOR);
+
+		appImg.setOpStampRequestMode(Optional.empty());
+
+		ReflectStatusResultShare reflectStatus = new ReflectStatusResultShare();
+		reflectStatus.setReflectStatus(ReflectedStateShare.WAITREFLECTION);
+
+		new Expectations() {
+			{
+
+				// 「[RQ667]申請反映後の日別勤怠(work）を作成する（勤務実績）」のテスト呼び出す
+				RCCreateDailyAfterApplicationeReflect.process(require, (ApplicationShare) any,
+						(DailyRecordOfApplication) any, (GeneralDate) any);
+				times = 1;
+
+			}
+
+		};
+
+		val actualResult = ReflectApplicationWorkRecord.process(require, ExecutionType.NORMAL_EXECUTION, appImg,
+				GeneralDate.ymd(2020, 01, 01), reflectStatus);
+
+		assertThat(actualResult.getLeft().getReflectStatus()).isEqualTo(ReflectedStateShare.REFLECTED);
+
+		NtsAssert.atomTask(() -> actualResult.getRight().get(), any -> require.addAllDomain(any.get()));
+
+	}
+
+	public Stamp createStamp() {
+		return new Stamp(new ContractCode("1"), // 契約コード
+				new StampNumber("1"), // 打刻カード
+				GeneralDateTime.ymdhms(2020, 01, 01, 10, 0, 0), // 打刻日時
+				new Relieve(AuthcMethod.ID_AUTHC, // 打刻する方法. 認証方法
+						StampMeans.NAME_SELECTION), /// 打刻する方法. 打刻手段
+				new StampType(false, GoingOutReason.valueOf(1), // 外出理由
+						SetPreClockArt.NONE, // 所定時刻セット区分
+						ChangeClockArt.GOING_TO_WORK, // 時刻変更区分
+						ChangeCalArt.NONE), // 計算区分変更対象
+				new RefectActualResult(null, null, null, null), Optional.empty());
+	}
 }
