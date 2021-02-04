aec718a22d6dd282b7867df5ab9e0e15da4a48f3
Hop <hop.nt@3si.vn>
2021-02-04 08:46:05 +0700

    uncomment

---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.infra/src/main/java/nts/uk/ctx/at/record/infra/repository/workrecord/erroralarm/schedule/daily/JpaFixedExtracSDailyItemsRepository.java
@@ -0,0 +1,9 @@
+package nts.uk.ctx.at.record.infra.repository.workrecord.erroralarm.schedule.daily;
+
+import nts.uk.ctx.at.record.dom.workrecord.erroralarm.schedule.daily.FixedExtracSDailyItemsRepository;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class JpaFixedExtracSDailyItemsRepository implements FixedExtracSDailyItemsRepository {
+}
