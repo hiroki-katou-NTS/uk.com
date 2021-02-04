0d4645048fd04a31d7ff3f455425756280da1384
NSVN-PhamVanThanh <thanh_pv@nittsusystem-vn.com>
2021-02-04 14:39:27 +0700

    KAF011 Fix bug #113898

--- nts.uk/uk.at/at.file/nts.uk.file.at.app/src/main/java/nts/uk/file/at/app/export/statement/stamp/StampList.java
+++ nts.uk/uk.at/at.file/nts.uk.file.at.app/src/main/java/nts/uk/file/at/app/export/statement/stamp/StampList.java
@@ -24,8 +24,8 @@ public class StampList {
 	
 	private static final int maxCharLocation = 36; //số ký tự tối đa trên cell 打刻位置
 	private static final int maxCharClassification = 24; //số ký tự tối đa trên cell 出退勤区分
-	private static final int heightDefault = 49; //chiều cao mặc định của một dòng dữ liệu 
-	private static final int heightAddRow = 33; //chiều cao khi thêm một dòng dữ liệu 
+	private static final int heightDefault = 48; //chiều cao mặc định của một dòng dữ liệu 
+	private static final int heightAddRow = 34; //chiều cao khi thêm một dòng dữ liệu 
 
 	public void setDate(String datetime) {
 		this.date = datetime.split(" ")[0];
