db035ad99531a103928fcf311bc0114ffcb058c2
NSVC\dungbn <dung_bn@nittsusystem-vn.com>
2021-02-05 11:12:55 +0700

    KNR002: fix entity C

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.infra/src/main/java/nts/uk/ctx/at/record/infra/repository/employmentinfoterminal/infoterminal/JpaReqComStatusMonitoringRepository.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.infra/src/main/java/nts/uk/ctx/at/record/infra/repository/employmentinfoterminal/infoterminal/JpaReqComStatusMonitoringRepository.java
@@ -43,7 +43,7 @@ public class JpaReqComStatusMonitoringRepository extends JpaRepository implement
 //		List<Integer> listCode = listTerminalCode.stream().map(e -> Integer.parseInt(e.v())).collect(Collectors.toList());
 		return this.queryProxy().query(FIND_CONTRACTCD_CODE_CONNECTING, KrcdtTrRqStMonitor.class)
 					.setParameter("contractCode", contractCode.v())
-					.setParameter("listCode", listTerminalCode.stream().map(e -> e.v()))
+					.setParameter("listCode", listTerminalCode.stream().map(e -> e.v()).collect(Collectors.toList()))
 					.setParameter("connecting", connecting ? 1 : 0).getList().stream().map(e -> e.toDomain()).collect(Collectors.toList());
 	}
 
