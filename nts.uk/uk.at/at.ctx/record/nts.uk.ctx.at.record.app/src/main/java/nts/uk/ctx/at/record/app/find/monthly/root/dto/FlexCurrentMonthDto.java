dc1e053f004d3fc372fb04ff918c4dd6ecf0143f
NSVN-DuongHongTin <tin_dh@nittsusystem.co.jp>
2021-02-17 10:45:03 +0900

    コンバータ：月別実績のフレックス時間 （更新）

---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/find/monthly/root/dto/FlexCurrentMonthDto.java
@@ -0,0 +1,88 @@
+package nts.uk.ctx.at.record.app.find.monthly.root.dto;
+
+import java.util.Optional;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import nts.uk.ctx.at.shared.dom.attendance.util.item.AttendanceItemDataGate;
+import nts.uk.ctx.at.shared.dom.common.time.AttendanceTimeMonth;
+import nts.uk.ctx.at.shared.dom.common.time.AttendanceTimeMonthWithMinus;
+import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.converter.util.ItemConst;
+import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.converter.util.item.ItemValue;
+import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.converter.util.item.ValueType;
+import nts.uk.ctx.at.shared.dom.scherec.monthlyattdcal.monthly.calc.flex.FlexTimeCurrentMonth;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+/** 当月フレックス時間 */
+public class FlexCurrentMonthDto implements ItemConst, AttendanceItemDataGate {
+
+	/** フレックス時間 */
+	private int flexTime;
+
+	/** 基準時間 */
+	private int standardTime;
+
+	/** 週平均超過時間 */
+	private int excessWeekAveTime;
+
+	public FlexTimeCurrentMonth toDomain() {
+		return FlexTimeCurrentMonth.of(new AttendanceTimeMonthWithMinus(flexTime),
+									new AttendanceTimeMonth(standardTime),
+									new AttendanceTimeMonth(excessWeekAveTime));
+	}
+	
+	public static FlexCurrentMonthDto from(FlexTimeCurrentMonth domain) {
+		FlexCurrentMonthDto dto = new FlexCurrentMonthDto();
+		if(domain != null) {
+			dto.setFlexTime(domain.getFlexTime().valueAsMinutes());
+			dto.setStandardTime(domain.getStandardTime().valueAsMinutes());
+			dto.setExcessWeekAveTime(domain.getExcessWeekAveTime().valueAsMinutes());
+		}
+		return dto;
+	}
+
+	@Override
+	public Optional<ItemValue> valueOf(String path) {
+		switch (path) {
+		case FLEX:
+			return Optional.of(ItemValue.builder().value(flexTime).valueType(ValueType.TIME));
+		case STANDARD:
+			return Optional.of(ItemValue.builder().value(standardTime).valueType(ValueType.TIME));
+		case EXCESS + AVERAGE:
+			return Optional.of(ItemValue.builder().value(excessWeekAveTime).valueType(ValueType.TIME));
+		default:
+			return Optional.empty();
+		}
+	}
+
+	@Override
+	public PropType typeOf(String path) {
+		switch (path) {
+		case FLEX:
+		case STANDARD:
+		case EXCESS + AVERAGE:
+			return PropType.VALUE;
+		default:
+			return PropType.OBJECT;
+		}
+	}
+
+	@Override
+	public void set(String path, ItemValue value) {
+		switch (path) {
+		case FLEX:
+			flexTime = value.valueOrDefault(0);
+			break;
+		case STANDARD:
+			standardTime = value.valueOrDefault(0);
+			break;
+		case EXCESS + AVERAGE:
+			excessWeekAveTime = value.valueOrDefault(0);
+			break;
+		default:
+		}
+	}
+}
--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/find/monthly/root/dto/FlexTimeOfMonthlyDto.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/find/monthly/root/dto/FlexTimeOfMonthlyDto.java
@@ -9,8 +9,6 @@ import nts.uk.ctx.at.shared.dom.attendance.util.item.AttendanceItemDataGate;
 import nts.uk.ctx.at.shared.dom.common.time.AttendanceTimeMonth;
 import nts.uk.ctx.at.shared.dom.common.time.AttendanceTimeMonthWithMinus;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.converter.util.ItemConst;
-import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.converter.util.anno.AttendanceItemLayout;
-import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.converter.util.anno.AttendanceItemValue;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.converter.util.item.ItemValue;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.converter.util.item.ValueType;
 import nts.uk.ctx.at.shared.dom.scherec.monthlyattdcal.monthly.calc.flex.FlexCarryforwardTime;
@@ -26,30 +24,25 @@ import nts.uk.ctx.at.shared.dom.scherec.monthlyattdcal.monthly.calc.flex.FlexTim
 public class FlexTimeOfMonthlyDto implements ItemConst, AttendanceItemDataGate {
 
 	/** フレックス繰越時間: フレックス繰越時間 */
-	@AttendanceItemLayout(jpPropertyName = CARRY_FORWARD, layout = LAYOUT_A)
 	private FlexCarryforwardTimeDto carryforwardTime;
 
 	/** フレックス時間: フレックス時間 */
-	@AttendanceItemLayout(jpPropertyName = TIME, layout = LAYOUT_B)
 	private FlexTimeMDto flexTime;
 
 	/** フレックス超過時間: 勤怠月間時間 */
-	@AttendanceItemValue(type = ValueType.TIME)
-	@AttendanceItemLayout(jpPropertyName = EXCESS + TIME, layout = LAYOUT_C)
 	private int excessTime;
 
 	/** フレックス不足控除時間: フレックス不足控除時間 */
-	@AttendanceItemLayout(jpPropertyName = SHORTAGE + DEDUCTION, layout = LAYOUT_D)
 	private FlexShortDeductTimeDto shortDeductTime;
 
 	/** フレックス不足時間: 勤怠月間時間 */
-	@AttendanceItemValue(type = ValueType.TIME)
-	@AttendanceItemLayout(jpPropertyName = SHORTAGE, layout = LAYOUT_E)
 	private int shortageTime;
 
 	/** 時間外超過のフレックス時間: 時間外超過のフレックス時間 */
-	@AttendanceItemLayout(jpPropertyName = EXCESS, layout = LAYOUT_F)
 	private FlexTimeOfExcessOutsideTimeDto excessOutsideTime;
+	
+	/** 当月精算フレックス時間 */
+	private int flexSettleTime;
 
 	public FlexTimeOfMonthly toDomain() {
 		return FlexTimeOfMonthly.of(flexTime == null ? new FlexTime() : flexTime.toDomain(),
@@ -58,7 +51,7 @@ public class FlexTimeOfMonthlyDto implements ItemConst, AttendanceItemDataGate {
 				carryforwardTime == null ? new FlexCarryforwardTime() : carryforwardTime.toDomain(),
 				excessOutsideTime == null ? new FlexTimeOfExcessOutsideTime() : excessOutsideTime.toDmain(),
 				shortDeductTime == null ? new FlexShortDeductTime() : shortDeductTime.toDomain(),
-				new AttendanceTimeMonthWithMinus(0));
+				new AttendanceTimeMonthWithMinus(flexSettleTime));
 	}
 
 	public static FlexTimeOfMonthlyDto from(FlexTimeOfMonthly domain) {
@@ -66,10 +59,11 @@ public class FlexTimeOfMonthlyDto implements ItemConst, AttendanceItemDataGate {
 		if(domain != null) {
 			dto.setCarryforwardTime(FlexCarryforwardTimeDto.from(domain.getFlexCarryforwardTime()));
 			dto.setFlexTime(FlexTimeMDto.from(domain.getFlexTime()));
-			dto.setExcessTime(domain.getFlexExcessTime() == null ? null : domain.getFlexExcessTime().valueAsMinutes());
 			dto.setShortDeductTime(FlexShortDeductTimeDto.from(domain.getFlexShortDeductTime()));
-			dto.setShortageTime(domain.getFlexShortageTime() == null ? null : domain.getFlexShortageTime().valueAsMinutes());
 			dto.setExcessOutsideTime(FlexTimeOfExcessOutsideTimeDto.from(domain.getFlexTimeOfExcessOutsideTime()));
+			dto.setExcessTime(domain.getFlexExcessTime() == null ? 0 : domain.getFlexExcessTime().valueAsMinutes());
+			dto.setShortageTime(domain.getFlexShortageTime() == null ? 0 : domain.getFlexShortageTime().valueAsMinutes());
+			dto.setFlexSettleTime(domain.getFlexSettleTime() == null ? 0 : domain.getFlexSettleTime().valueAsMinutes());
 		}
 		return dto;
 	}
@@ -82,6 +76,8 @@ public class FlexTimeOfMonthlyDto implements ItemConst, AttendanceItemDataGate {
 			return Optional.of(ItemValue.builder().value(excessTime).valueType(ValueType.TIME));
 		case SHORTAGE:
 			return Optional.of(ItemValue.builder().value(shortageTime).valueType(ValueType.TIME));
+		case CUR_MONTH + FLEX:
+			return Optional.of(ItemValue.builder().value(flexSettleTime).valueType(ValueType.TIME));
 		default:
 			return Optional.empty();
 		}
@@ -124,6 +120,7 @@ public class FlexTimeOfMonthlyDto implements ItemConst, AttendanceItemDataGate {
 		switch (path) {
 		case (EXCESS + TIME):
 		case SHORTAGE:
+		case CUR_MONTH + FLEX:
 			return PropType.VALUE;
 		default:
 			return PropType.OBJECT;
@@ -138,6 +135,9 @@ public class FlexTimeOfMonthlyDto implements ItemConst, AttendanceItemDataGate {
 			break;
 		case SHORTAGE:
 			shortageTime = value.valueOrDefault(0);
+			break;
+		case CUR_MONTH + FLEX:
+			flexSettleTime = value.valueOrDefault(0);
 			break;
 		default:
 		}
