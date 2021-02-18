dd2f0db0ef67dba0a2bc895b68ed1bc137566425
Thoang Le <thoang.lc@3si.vn>
2021-02-18 10:57:53 +0700

    update code KMT001 - A

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmt/001/a/kmt001.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmt/001/a/kmt001.a.vm.ts
@@ -1,40 +1,54 @@
 /// <reference path="../../../../lib/nittsu/viewcontext.d.ts" />
 
 module nts.uk.at.view.kmt001.a {
+
+  const PATH = {
+    saveRegistrationWork: '',
+    deleteRegistrationWork: '',
+    getRegistrationWork: '',
+  };
+
   @bean()
   class ViewModel extends ko.ViewModel {
 
-    selectedCode: KnockoutObservable<string> = ko.observable(null);
-    currentCodeList: KnockoutObservable<string> = ko.observable(null);
-    workList: KnockoutObservableArray<any> = ko.observableArray(null);
+    selectedWorkCode: KnockoutObservable<string> = ko.observable(null);
+    currentCode: KnockoutObservable<string> = ko.observable(null);
+    registrationWorkList: KnockoutObservableArray<any> = ko.observableArray(null);
     externalCodeList: KnockoutObservableArray<any> = ko.observableArray([]);
+    externalCode: Array<any> = [];
+    workList: KnockoutObservable<any> = ko.observable([]);
 
     model: KnockoutObservable<ModelItem> = ko.observable(null);
+    isNewMode: KnockoutObservable<boolean> = ko.observable(true);
 
     constructor(params: any) {
       super();
       const vm = this;
 
-      vm.workList([
-        { code: 'A0000000001', name: 'Text 1' },
-        { code: 'A0000000002', name: 'Text 2' },
-        { code: 'A0000000003', name: 'Text 3' },
-      ]);
-
       vm.externalCodeList([
-        { code: 'KMT001_36', value: ko.observable('Text 1') },
-        { code: 'KMT001_37', value: ko.observable('Text 2') },
-        { code: 'KMT001_38', value: ko.observable('Text 3') },
-        { code: 'KMT001_39', value: ko.observable('Text 4') },
-        { code: 'KMT001_40', value: ko.observable('Text 5') },
+        { code: 'KMT001_36', value: ko.observable(null) },
+        { code: 'KMT001_37', value: ko.observable(null) },
+        { code: 'KMT001_38', value: ko.observable(null) },
+        { code: 'KMT001_39', value: ko.observable(null) },
+        { code: 'KMT001_40', value: ko.observable(null) },
       ]);
 
-      let externalCode: any = [
-        ko.observable('ド1'), ko.observable('ド2'), ko.observable('ド3'), ko.observable('ド4'), ko.observable('ド5')
-      ];
-      
-      vm.model(new ModelItem('code', 'name', 'Abname', '#ff0000', '2021/02/17', '9999/12/31', 'Remarks', externalCode));
-      console.log(vm.model());
+      //init mode
+      vm.getRegistrationWorkList();
+      vm.getWorkList();
+
+      vm.model(new ModelItem());
+      vm.addNewRegistrationWork();
+
+      vm.selectedWorkCode.subscribe((newValue) => {
+        nts.uk.ui.errors.clearAll();
+        vm.loadRegistrationWork();
+      });
+
+      vm.currentCode.subscribe((newValue) => {
+        nts.uk.ui.errors.clearAll();
+        vm.loadRegistrationWork();
+      });
     }
 
     created(params: any) {
@@ -45,17 +59,120 @@ module nts.uk.at.view.kmt001.a {
       const vm = this;
     }
 
-    AddNewWork() {
+    addNewRegistrationWork() {
+      const vm = this;
+      let currentDate = moment(new Date()).format('YYYY/DD/MM');
+      vm.externalCode = [
+        ko.observable(null), ko.observable(null), ko.observable(null), ko.observable(null), ko.observable(null)
+      ];
+      vm.model().update(null, null, null, null, currentDate, '9999/12/31', null, vm.externalCode);
 
+      vm.isNewMode(true);
+      $('#KMT001_21').focus();
     }
 
-    RegistrationWork() {
-
+    saveRegistrationWork() {
+      const vm = this;
+       /*  vm.$blockui('show');
+       vm.$ajax(PATH.saveRegistrationWork, { parrams: null }).done(() => {
+         vm.$dialog.info({messageId: 'Mgs_15'}).then(() => {
+           vm.getWorkList();
+           vm.$blockui('hide');
+         });
+       }).fail((error) => {
+         vm.$dialog.error({messageId: error.messageId}).then(() => {
+           vm.$blockui('hide');
+         });
+       }); */
     }
 
-    DeleteWork() {
-
+    deleteRegistrationWork() {
+      const vm = this;   
+      vm.getNextPreviousItem(vm.currentCode());
+      /*  vm.$blockui('show');
+       vm.$ajax(PATH.getRegistrationWork, { workId: vm.currentCode() }).done((data) => {
+         vm.$dialog.info({messageId: 'Msg_16'}).then(() => {
+           vm.getNextPreviousItem(vm.currentCode());
+           vm.$blockui('hide');
+         });
+       }).fail((error) => {
+         vm.$dialog.error({messageId: error.messageId}).then(() => {
+           vm.$blockui('hide');
+         });
+       }); */
     }
+
+    loadRegistrationWork() {
+      const vm = this;
+      /*vm.$blockui('show');
+      vm.$ajax(PATH.getRegistrationWork, { workId: value }).done((data) => {
+        if(data) {          
+          vm.model().update(data);
+          vm.isNewMode(false);
+        }
+        vm.$blockui('hide');
+      }).fail((error) => {
+        vm.$dialog.error({messageId: error.messageId}).then(() => {
+          vm.$blockui('hide');
+        });
+      }); */
+
+      vm.model().update(vm.currentCode(), 'B', 'C', '#bf0', '2002/12/31', '9999/12/31', 'tets', vm.externalCode);
+      vm.isNewMode(false);
+    }
+
+    getRegistrationWorkList() {
+      const vm = this;
+      vm.registrationWorkList([
+        { code: 'A0000000000000000001', name: 'Text 1' },
+        { code: 'A0000000000000000002', name: 'Text 2' },
+        { code: 'A0000000000000000003', name: 'Text 3 + 作業 1 + 作業 1 + 作業 1 + 作業 1 + 作業 1 + 作業 1 + 作業 1 + 作業 1' },
+      ]);
+
+      if (_.isNil(vm.currentCode()) && vm.registrationWorkList().length > 0) {
+        let firstItem = _.head(vm.registrationWorkList());
+        vm.currentCode(firstItem.code);
+      }
+    }
+
+    getWorkList() {
+      const vm = this;
+
+      vm.workList = ko.observableArray([
+        { code: '001', name: '作業 1' },
+        { code: '002', name: '作業 2' },
+        { code: '003', name: '作業 3' },
+      ])
+    }
+
+    getNextPreviousItem(codeBeforeRemove: string) {
+      const vm = this;
+      let currentCode: string = null;
+      //delete from DB
+
+      let findIndex = _.findIndex(vm.registrationWorkList(), (x) => { return x.code === codeBeforeRemove });
+      if(vm.registrationWorkList().length > 1) {
+        if( findIndex === vm.registrationWorkList().length - 1) 
+          findIndex = findIndex - 1;
+        else 
+          findIndex = findIndex + 1;
+
+        let nextItem = vm.registrationWorkList()[findIndex];
+        if(!_.isNil(nextItem)) {
+          currentCode = nextItem.code;
+        }
+
+        let registrationList = _.filter(vm.registrationWorkList(), (x) => { return x.code !== codeBeforeRemove });
+        vm.registrationWorkList.removeAll();
+        vm.registrationWorkList(registrationList);
+        vm.currentCode(currentCode);
+      } else {
+        vm.registrationWorkList.removeAll();
+        vm.currentCode(null);
+        vm.addNewRegistrationWork();
+      }
+    }
+
   }
 
   export class ModelItem {
@@ -66,7 +183,7 @@ module nts.uk.at.view.kmt001.a {
     expStartDate: KnockoutObservable<string> = ko.observable(null);
     expEndDate: KnockoutObservable<string> = ko.observable(null);
     remarks: KnockoutObservable<string> = ko.observable(null);
-    externalCode: Array<string> = [];
+    externalCode: KnockoutObservable<any> = ko.observable(null);
 
     constructor(code?: string, name?: string, abbreviatedName?: string, color?: string,
       expStartDate?: string, expEndDate?: string, remarks?: string, externalCode?: Array<string>) {
@@ -77,7 +194,19 @@ module nts.uk.at.view.kmt001.a {
       this.expStartDate(expStartDate);
       this.expEndDate(expEndDate);
       this.remarks(remarks);
-      this.externalCode = externalCode;
+      this.externalCode(externalCode);
+    }
+
+    update(code?: string, name?: string, abbreviatedName?: string, color?: string,
+      expStartDate?: string, expEndDate?: string, remarks?: string, externalCode?: Array<string>) {
+      this.code(code);
+      this.name(name);
+      this.abbreviatedName(abbreviatedName);
+      this.color(color);
+      this.expStartDate(expStartDate);
+      this.expEndDate(expEndDate);
+      this.remarks(remarks);
+      this.externalCode(externalCode);
     }
   }
 }
