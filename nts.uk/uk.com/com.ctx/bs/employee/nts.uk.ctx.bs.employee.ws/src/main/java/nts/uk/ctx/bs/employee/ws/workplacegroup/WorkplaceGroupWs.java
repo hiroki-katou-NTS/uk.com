e4cb98517f9b2746e07a58d30ee98ce29f954ac3
chinh.hm <chinh.hm@3si.vn>
2021-02-18 16:58:58 +0700

    update ksm007

--- nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.ws/src/main/java/nts/uk/ctx/bs/employee/ws/workplacegroup/WorkplaceGroupWs.java
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.ws/src/main/java/nts/uk/ctx/bs/employee/ws/workplacegroup/WorkplaceGroupWs.java
@@ -1,6 +1,11 @@
 package nts.uk.ctx.bs.employee.ws.workplacegroup;
 
-import java.util.List;
+import nts.arc.layer.ws.WebService;
+import nts.uk.ctx.bs.employee.app.command.workplace.group.*;
+import nts.uk.ctx.bs.employee.app.find.employeeinfo.workplacegroup.*;
+import nts.uk.ctx.bs.employee.app.query.hospitalofficeinfo.GetOptionInformationQuery;
+import nts.uk.ctx.bs.employee.app.query.hospitalofficeinfo.OptionInforDto;
+import nts.uk.ctx.bs.employee.dom.workplace.master.service.WorkplaceInforParam;
 
 import javax.inject.Inject;
 import javax.ws.rs.POST;
@@ -8,97 +13,90 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
-import nts.arc.layer.ws.WebService;
-import nts.arc.time.GeneralDate;
-import nts.uk.ctx.bs.employee.app.command.workplace.group.DeleteWorkplaceGroupCommand;
-import nts.uk.ctx.bs.employee.app.command.workplace.group.DeleteWorkplaceGroupCommandHandler;
-import nts.uk.ctx.bs.employee.app.command.workplace.group.RegisterWorkplaceGroupCommand;
-import nts.uk.ctx.bs.employee.app.command.workplace.group.RegisterWorkplaceGroupCommandHandler;
-import nts.uk.ctx.bs.employee.app.command.workplace.group.ResWorkplaceGroupResult;
-import nts.uk.ctx.bs.employee.app.command.workplace.group.UpdateWorkplaceGroupCommandHandler;
-import nts.uk.ctx.bs.employee.app.find.employeeinfo.workplacegroup.AffWorkplaceGroupDto;
-import nts.uk.ctx.bs.employee.app.find.employeeinfo.workplacegroup.AffWorkplaceGroupEmployeeQuery;
-import nts.uk.ctx.bs.employee.app.find.employeeinfo.workplacegroup.DateRequest;
-import nts.uk.ctx.bs.employee.app.find.employeeinfo.workplacegroup.WorkplaceGroupDto;
-import nts.uk.ctx.bs.employee.app.find.employeeinfo.workplacegroup.WorkplaceGroupFinder;
-import nts.uk.ctx.bs.employee.app.find.employeeinfo.workplacegroup.WorkplaceInfoRequest;
-import nts.uk.ctx.bs.employee.dom.workplace.master.service.WorkplaceInforParam;
+import java.util.List;
 
 /**
  * @author anhdt
- *
  */
 @Path("bs/schedule/employeeinfo/workplacegroup")
 @Produces(MediaType.APPLICATION_JSON)
 public class WorkplaceGroupWs extends WebService {
 
-	@Inject
-	private WorkplaceGroupFinder wkpGroupFinder;
+    @Inject
+    private WorkplaceGroupFinder wkpGroupFinder;
 
-	@Inject
-	private RegisterWorkplaceGroupCommandHandler registerWkpGroupCmd;
-	
-	@Inject
-	private UpdateWorkplaceGroupCommandHandler updateWkpGroupCmd;
+    @Inject
+    private RegisterWorkplaceGroupCommandHandler registerWkpGroupCmd;
 
-	@Inject
-	private DeleteWorkplaceGroupCommandHandler deletewkpGroupCmd;
-	
-	@Inject
-	private AffWorkplaceGroupEmployeeQuery workplaceGroupEmployeeQuery ;
+    @Inject
+    private UpdateWorkplaceGroupCommandHandler updateWkpGroupCmd;
 
-	/**
-	 * Get all team setting
-	 * 
-	 * @return list
-	 */
-	@POST
-	@Path("getAll")
-	public WorkplaceGroupDto getWorkplaceGroup() {
-		return wkpGroupFinder.getWorkplaceGroup();
-	}
+    @Inject
+    private DeleteWorkplaceGroupCommandHandler deletewkpGroupCmd;
 
-	@POST
-	@Path("getListWorkplaceId/{id}")
-	public List<String> getListWplId(@PathParam("id") String WKPGRPID) {
-		return wkpGroupFinder.getLstWorkplaceId(WKPGRPID);
-	}
+    @Inject
+    private AffWorkplaceGroupEmployeeQuery workplaceGroupEmployeeQuery;
 
-	@POST
-	@Path("getWorkplaceGroup/{id}")
-	public WorkplaceGroupDto getWorkplace(@PathParam("id") String WKPGRPID) {
-		return wkpGroupFinder.getWkplaceGroup(WKPGRPID);
-	}
+    @Inject
+    private GetOptionInformationQuery getOptionInformationQuery;
 
-	@POST
-	@Path("getWorkplaceInfo")
-	public List<WorkplaceInforParam> getWorkplaceInfo(WorkplaceInfoRequest request) {
-		return wkpGroupFinder.getWorkplaceInfo(request.getWorkplaceIds(), request.toDate());
-	}
+    /**
+     * Get all team setting
+     *
+     * @return list
+     */
+    @POST
+    @Path("getAll")
+    public WorkplaceGroupDto getWorkplaceGroup() {
+        return wkpGroupFinder.getWorkplaceGroup();
+    }
 
-	@POST
-	@Path("registerWorkplaceGroup")
-	public ResWorkplaceGroupResult registerWorkplaceGroup(RegisterWorkplaceGroupCommand command) {
-		return registerWkpGroupCmd.handle(command);
-	}
-	
-	@POST
-	@Path("updateWorkplaceGroup")
-	public ResWorkplaceGroupResult updateWorkplaceGroup(RegisterWorkplaceGroupCommand command) {
-		return updateWkpGroupCmd.handle(command);
-	}
+    @POST
+    @Path("getListWorkplaceId/{id}")
+    public List<String> getListWplId(@PathParam("id") String WKPGRPID) {
+        return wkpGroupFinder.getLstWorkplaceId(WKPGRPID);
+    }
 
-	@POST
-	@Path("deleteWorkplaceGroup")
-	public void deleteWorkplaceGroup(DeleteWorkplaceGroupCommand command) {
-		deletewkpGroupCmd.handle(command);
-	}
+    @POST
+    @Path("getWorkplaceGroup/{id}")
+    public WorkplaceGroupDto getWorkplace(@PathParam("id") String WKPGRPID) {
+        return wkpGroupFinder.getWkplaceGroup(WKPGRPID);
+    }
 
-	
-	@POST
-	@Path("workplacegroupemployee")
-	public AffWorkplaceGroupDto getWorkplaceGroupEmployee(DateRequest date) {
-		return workplaceGroupEmployeeQuery.getWorkplaceGroupOfEmployee(date.toDate());
-	}
+    @POST
+    @Path("getWorkplaceInfo")
+    public List<WorkplaceInforParam> getWorkplaceInfo(WorkplaceInfoRequest request) {
+        return wkpGroupFinder.getWorkplaceInfo(request.getWorkplaceIds(), request.toDate());
+    }
+
+    @POST
+    @Path("registerWorkplaceGroup")
+    public ResWorkplaceGroupResult registerWorkplaceGroup(RegisterWorkplaceGroupCommand command) {
+        return registerWkpGroupCmd.handle(command);
+    }
+
+    @POST
+    @Path("updateWorkplaceGroup")
+    public ResWorkplaceGroupResult updateWorkplaceGroup(RegisterWorkplaceGroupCommand command) {
+        return updateWkpGroupCmd.handle(command);
+    }
+
+    @POST
+    @Path("deleteWorkplaceGroup")
+    public void deleteWorkplaceGroup(DeleteWorkplaceGroupCommand command) {
+        deletewkpGroupCmd.handle(command);
+    }
+
+
+    @POST
+    @Path("workplacegroupemployee")
+    public AffWorkplaceGroupDto getWorkplaceGroupEmployee(DateRequest date) {
+        return workplaceGroupEmployeeQuery.getWorkplaceGroupOfEmployee(date.toDate());
+    }
+
+    @POST
+    @Path("optioninformation")
+    public OptionInforDto getOptionInfor() {
+        return getOptionInformationQuery.getInfor();
+    }
 }
