35430d6bd491184a457b460f659ad41a8e51b391
3si\hai.tt <hai.tt@3si.vn>
2021-02-18 15:51:58 +0700

    [KMK013D] Update source

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.app/src/main/java/nts/uk/ctx/at/shared/app/find/workrule/func/SelectFunctionFinder.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.app/src/main/java/nts/uk/ctx/at/shared/app/find/workrule/func/SelectFunctionFinder.java
@@ -66,7 +66,7 @@ public class SelectFunctionFinder {
 			dto.setUseAggDeformedSetting(optAggSetting.get().getUseDeformedLabor().value);
 		}
 
-//		dto.setOptionLicenseCustomize(AppContexts.optionLicense().customize().ootsuka());
+		dto.setOptionLicenseCustomize(AppContexts.optionLicense().customize().ootsuka());
 
 		// ドメインモデル「臨時勤務利用管理」を取得する
 		Optional<TemporaryWorkUseManage> optTempWorkUse = tempWorkRepo.findByCid(companyId);
