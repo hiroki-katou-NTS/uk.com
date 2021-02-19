170f6179931134cbd298ce641e0242e76694c67d
m_kitahira <m_kitahira@nittsusystem.co.jp>
2021-02-19 11:55:56 +0900

    PrimitiveValueの制約漏れを修正

--- nts.uk/uk.com/com.ctx/sys/gateway/nts.uk.ctx.sys.gateway.dom/src/main/java/nts/uk/ctx/sys/gateway/dom/securitypolicy/password/complexity/PasswordMinimumLength.java
+++ nts.uk/uk.com/com.ctx/sys/gateway/nts.uk.ctx.sys.gateway.dom/src/main/java/nts/uk/ctx/sys/gateway/dom/securitypolicy/password/complexity/PasswordMinimumLength.java
@@ -1,7 +1,12 @@
 package nts.uk.ctx.sys.gateway.dom.securitypolicy.password.complexity;
 
 import nts.arc.primitive.IntegerPrimitiveValue;
+import nts.arc.primitive.constraint.IntegerRange;
 
+/**
+ * パスワード最低桁数
+ */
+@IntegerRange(min = 1, max = 8)
 public class PasswordMinimumLength extends IntegerPrimitiveValue<PasswordMinimumLength> {
 
 	/** serialVersionUID */
