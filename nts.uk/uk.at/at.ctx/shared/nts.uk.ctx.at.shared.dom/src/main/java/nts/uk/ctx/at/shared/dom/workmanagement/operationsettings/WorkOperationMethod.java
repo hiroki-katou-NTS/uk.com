01f31dc51e4c07d64f25495058789d1684198855
chinh.hm <chinh.hm@3si.vn>
2021-02-22 10:45:48 +0700

    update wrk

---
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/workmanagement/operationsettings/WorkOperationMethod.java
@@ -0,0 +1,24 @@
+package nts.uk.ctx.at.shared.dom.workmanagement.operationsettings;
+
+
+/**
+ * Enumeration: 作業運用方法
+ * UKDesign.ドメインモデル.NittsuSystem.UniversalK.就業.shared(勤務予定、勤務実績).作業管理.運用設定.作業運用方法
+ *
+ * @author chinh.hm
+ */
+public enum WorkOperationMethod {
+    // 予定で利用
+    USE_ON_SCHEDULE(0),
+    // 利用しない
+    DO_NOT_USE(1),
+    // 実績で利用
+    USED_IN_ACHIEVENTS(2);
+
+    public final int value;
+
+    WorkOperationMethod(int value) {
+        this.value = value;
+    }
+
+}
---
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/workmanagement/operationsettings/WorkOperationSetting.java
@@ -0,0 +1,13 @@
+package nts.uk.ctx.at.shared.dom.workmanagement.operationsettings;
+
+import nts.arc.layer.dom.AggregateRoot;
+
+/**
+ * AggregateRoot: 作業運用設定
+ * Ea: UKDesign.ドメインモデル.NittsuSystem.UniversalK.就業.shared(勤務予定、勤務実績).作業管理.運用設定.作業運用設定.
+ * @author : chinh.hm
+ */
+public class WorkOperationSetting extends AggregateRoot {
+    // 作業運用方法
+    private WorkOperationMethod workOperationMethod;
+}
