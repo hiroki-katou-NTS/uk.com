8910b10269bb04afee98ceea33dc52b2fc4325d5
NTS-PhamVanDan <dan_pv@nittsusystem-vn.com>
2021-02-24 14:14:49 +0900

    use BusinessException::getMessage in method getErrorInfoWithWorkTimeZone

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/schedule/workschedule/CreateWorkSchedule.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/schedule/workschedule/CreateWorkSchedule.java
@@ -8,7 +8,6 @@ import java.util.stream.Stream;
 
 import lombok.RequiredArgsConstructor;
 import nts.arc.error.BusinessException;
-import nts.arc.i18n.I18NText;
 import nts.arc.task.tran.AtomTask;
 import nts.arc.time.GeneralDate;
 import nts.gul.util.OptionalUtil;
@@ -137,10 +136,11 @@ public class CreateWorkSchedule {
 			return Optional.empty();
 		}
 		
-		String errorMessage = I18NText.main("Msg_1781").addIds(
-				stateOfTime.getTimeSpan().get().getStart().getInDayTimeWithFormat(), 
-				stateOfTime.getTimeSpan().get().getEnd().getInDayTimeWithFormat())
-			.build().buildMessage();
+		String errorMessage = new BusinessException(
+					"Msg_1781", 
+					stateOfTime.getTimeSpan().get().getStart().getInDayTimeWithFormat(), 
+					stateOfTime.getTimeSpan().get().getEnd().getInDayTimeWithFormat()
+				).getMessage();
 		
 		return Optional.of(
 				ErrorInfoOfWorkSchedule.attendanceItemError(employeeId, date, workTimeZone.attendanceItemId, errorMessage));
