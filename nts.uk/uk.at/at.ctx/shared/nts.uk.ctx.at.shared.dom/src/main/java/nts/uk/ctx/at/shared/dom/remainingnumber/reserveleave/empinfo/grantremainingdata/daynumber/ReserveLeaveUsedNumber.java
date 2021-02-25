726cf9c9510a70a68f2fb4c458912b56403a4ce7
masaakijinno <fairvancs@gmail.com>
2021-02-25 10:25:34 +0900

    付与残数処理：dummyフラグ関連修正　積休
    (cherry picked from commit 5aefa729232767a47e7e534ecd5338ebde3eaa3f)
    (cherry picked from commit f78aa6cd4a220652878f28b57e0d649a337ee08e)
    (cherry picked from commit fd920927b324c99218978c61c471e86944b4e220)

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/remainingnumber/reserveleave/empinfo/grantremainingdata/daynumber/ReserveLeaveUsedNumber.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/remainingnumber/reserveleave/empinfo/grantremainingdata/daynumber/ReserveLeaveUsedNumber.java
@@ -8,20 +8,20 @@ import nts.uk.ctx.at.shared.dom.remainingnumber.common.empinfo.grantremainingdat
 @Getter
 public class ReserveLeaveUsedNumber extends LeaveUsedNumber {
 
-	/**
-	 * 日数
-	 */
-	private ReserveLeaveUsedDayNumber days;
-
-	/**
-	 * 上限超過消滅日数
-	 */
-	private Optional<ReserveLeaveOverLimitDayNumber> overLimitDays;
-
-	public ReserveLeaveUsedNumber(double days, Double overLimitDays) {
-		this.days = new ReserveLeaveUsedDayNumber(days);
-		this.overLimitDays = overLimitDays != null ? Optional.of(new ReserveLeaveOverLimitDayNumber(overLimitDays))
-				: Optional.empty();
-	}
+//	/**
+//	 * 日数
+//	 */
+//	private ReserveLeaveUsedDayNumber days;
+//
+//	/**
+//	 * 上限超過消滅日数
+//	 */
+//	private Optional<ReserveLeaveOverLimitDayNumber> overLimitDays;
+//
+//	public ReserveLeaveUsedNumber(double days, Double overLimitDays) {
+//		this.days = new ReserveLeaveUsedDayNumber(days);
+//		this.overLimitDays = overLimitDays != null ? Optional.of(new ReserveLeaveOverLimitDayNumber(overLimitDays))
+//				: Optional.empty();
+//	}
 
 }
