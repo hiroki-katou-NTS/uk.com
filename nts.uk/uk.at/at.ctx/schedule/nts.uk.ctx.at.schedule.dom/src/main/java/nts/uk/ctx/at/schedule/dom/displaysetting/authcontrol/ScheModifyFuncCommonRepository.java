0a49cf90352520358359e6cfbcfe502e21fa8633
NTS-PhamVanDan <dan_pv@nittsusystem-vn.com>
2021-02-25 10:56:22 +0900

    update: remove methods(insert, update, get, getList), add method(getAll)

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/displaysetting/authcontrol/ScheModifyFuncCommonRepository.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/displaysetting/authcontrol/ScheModifyFuncCommonRepository.java
@@ -1,30 +1,12 @@
 package nts.uk.ctx.at.schedule.dom.displaysetting.authcontrol;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ScheModifyFuncCommonRepository {
 	
 	/**
-	 * @param domain スケジュール修正共通の機能
+	 * @return all
 	 */
-	void insert(ScheModifyFuncCommon domain);
-	
-	/**
-	 * @param domain スケジュール修正共通の機能
-	 */
-	void update(ScheModifyFuncCommon domain);
-	
-	/**
-	 * @param functionNo 機能NO
-	 * @return
-	 */
-	Optional<ScheModifyFuncCommon> get( int functionNo );
-	
-	/**
-	 * @param functionNoList　機能NO一覧
-	 * @return
-	 */
-	List<ScheModifyFuncCommon> getList( List<Integer> functionNoList);
+	List<ScheModifyFuncCommon> getAll();
 
 }
