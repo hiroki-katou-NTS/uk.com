e14eabcd8f7846c0ef0a13e13415dca24eeed1ef
NTS-PhamVanDan <dan_pv@nittsusystem-vn.com>
2021-02-25 11:18:59 +0900

    update: remove methods(insert, update, get, getList), add method(getAll)

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/displaysetting/authcontrol/ScheModifyFuncByPersonRepository.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/displaysetting/authcontrol/ScheModifyFuncByPersonRepository.java
@@ -1,30 +1,12 @@
 package nts.uk.ctx.at.schedule.dom.displaysetting.authcontrol;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ScheModifyFuncByPersonRepository {
 	
 	/**
-	 * @param domain スケジュール修正個人別の機能
+	 * @return all
 	 */
-	void insert(ScheModifyFuncByPerson domain);
-	
-	/**
-	 * @param domain スケジュール修正個人別の機能
-	 */
-	void update(ScheModifyFuncByPerson domain);
-	
-	/**
-	 * @param functionNo 機能NO
-	 * @return
-	 */
-	Optional<ScheModifyFuncByPerson> get( int functionNo );
-	
-	/**
-	 * @param functionNoList　機能NO一覧
-	 * @return
-	 */
-	List<ScheModifyFuncByPerson> getList( List<Integer> functionNoList);
+	List<ScheModifyFuncByPerson> getAll();
 
 }
--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/displaysetting/authcontrol/ScheModifyFuncByWorkplaceRepository.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/displaysetting/authcontrol/ScheModifyFuncByWorkplaceRepository.java
@@ -1,30 +1,12 @@
 package nts.uk.ctx.at.schedule.dom.displaysetting.authcontrol;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ScheModifyFuncByWorkplaceRepository {
 	
 	/**
-	 * @param domain スケジュール修正職場別の機能
+	 * @return all
 	 */
-	void insert(ScheModifyFuncByWorkplace domain);
-	
-	/**
-	 * @param domain スケジュール修正職場別の機能
-	 */
-	void update(ScheModifyFuncByWorkplace domain);
-	
-	/**
-	 * @param functionNo 機能NO
-	 * @return
-	 */
-	Optional<ScheModifyFuncByWorkplace> get( int functionNo );
-	
-	/**
-	 * @param functionNoList　機能NO一覧
-	 * @return
-	 */
-	List<ScheModifyFuncByWorkplace> getList( List<Integer> functionNoList);
+	List<ScheModifyFuncByWorkplace> getAll();
 
 }
