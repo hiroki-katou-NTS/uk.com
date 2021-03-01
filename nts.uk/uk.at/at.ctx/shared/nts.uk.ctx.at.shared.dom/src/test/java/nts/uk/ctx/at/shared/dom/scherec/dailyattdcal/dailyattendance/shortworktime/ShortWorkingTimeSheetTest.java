bb0abff6b800717a88bb7e185301de5a8c50bc7b
NTS-PhamVanDan <dan_pv@nittsusystem-vn.com>
2021-03-01 10:49:21 +0900

    add @Before, add test minute

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/test/java/nts/uk/ctx/at/shared/dom/scherec/dailyattdcal/dailyattendance/shortworktime/ShortWorkingTimeSheetTest.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/test/java/nts/uk/ctx/at/shared/dom/scherec/dailyattdcal/dailyattendance/shortworktime/ShortWorkingTimeSheetTest.java
@@ -2,6 +2,7 @@ package nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.shortworkt
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import nts.arc.testing.assertion.NtsAssert;
@@ -10,14 +11,19 @@ import nts.uk.shr.com.time.TimeWithDayAttr;
 
 public class ShortWorkingTimeSheetTest {
 	
-	@Test
-	public void testGetters() {
-		
-		ShortWorkingTimeSheet target = new ShortWorkingTimeSheet(
+	ShortWorkingTimeSheet target;
+	
+	@Before
+	public void init() {
+		target = new ShortWorkingTimeSheet(
 				new ShortWorkTimFrameNo(1), 
 				ChildCareAttribute.CARE, 
-				TimeWithDayAttr.hourMinute(15, 0), 
+				TimeWithDayAttr.hourMinute(15, 30), 
 				TimeWithDayAttr.hourMinute(17, 0));
+	}
+	
+	@Test
+	public void testGetters() {
 		
 		NtsAssert.invokeGetters(target);
 	}
@@ -25,16 +31,12 @@ public class ShortWorkingTimeSheetTest {
 	@Test
 	public void testConvertToTimeSpanForCalc() {
 		
-		ShortWorkingTimeSheet target = new ShortWorkingTimeSheet(
-				new ShortWorkTimFrameNo(1), 
-				ChildCareAttribute.CARE, 
-				TimeWithDayAttr.hourMinute(15, 0), 
-				TimeWithDayAttr.hourMinute(17, 0));
-		
 		TimeSpanForCalc result = target.convertToTimeSpanForCalc();
 		
 		assertThat( result.getStart().hour() ).isEqualTo( 15 );
+		assertThat( result.getStart().minute() ).isEqualTo( 30 );
 		assertThat( result.getEnd().hour() ).isEqualTo( 17 );
+		assertThat( result.getEnd().minute() ).isEqualTo( 0 );
 	}
 	
 }
