96cf9efd244a168b13c70c98acee04119d654fe0
bigzero <chithoang@gmail.com>
2021-03-03 13:44:33 +0700

    update code ksm001

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksm/011/e/ksm011.e.start.ts
+++
@@ -1,9 +0,0 @@
-module nts.uk.at.view.ksm011.e {
-    let __viewContext: any = window["__viewContext"] || {};
-    __viewContext.ready(function() {
-        let screenModel = new viewmodel.ScreenModel();
-        screenModel.startPage().done(() => {
-            __viewContext.bind(screenModel);
-        });
-    });
-}
