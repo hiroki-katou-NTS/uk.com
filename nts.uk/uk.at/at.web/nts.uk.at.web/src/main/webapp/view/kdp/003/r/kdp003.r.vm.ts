bebd44f035a6e3c5a4a0b638214c3b1914a0a8bf
Thieu Thanh Tu <tu_tt@nittsusystem-vn.com>
2021-03-04 16:01:49 +0700

    Update code UI screen R

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdp/003/r/kdp003.r.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdp/003/r/kdp003.r.vm.ts
@@ -4,29 +4,32 @@ module nts.uk.at.kdp003.r {
 
     @bean()
 	export class ViewModel extends ko.ViewModel {
-
+        // R5 利用停止前内容
         beforeStopNoticeList: KnockoutObservableArray<any> = ko.observableArray([]);
-            
+        
+        // R1 本部見内容
         headOfficeNoticeList: KnockoutObservableArray<any> = ko.observableArray([]);
 
+        // R2 職場メッセージ
         workplaceNoticeList: KnockoutObservableArray<any> = ko.observableArray([]);
 
+        // R6 利用停止中内容
 		stoppingNotice: KnockoutObservable<string> = ko.observable('');
-        stopColor: KnockoutObservable<string> = ko.observable('');
-        
-        created() {
+
+        isNormalMode: KnockoutObservable<boolean> = ko.observable(true);
+
+        parentParam = new ParentParam();
+
+        created(parentParam: ParentParam) {
             const vm = this;
-            vm.beforeStopNoticeList = ko.observableArray([{notice: '本日１０：００よりをメンテナンスのためシステムを停止致します。'}]);
-            
+
+            vm.parentParam = parentParam;
+
 			vm.headOfficeNoticeList = ko.observableArray([{notice: '全職場に表示されるメッセージですテストのメッセージ2行目'}]);
             
 			vm.workplaceNoticeList = ko.observableArray([{notice: '職場管理者からのお願いです。今週土曜日は本社にてメンテナンスによる停電があります。'},
-            {notice: '１２３４５６７８９①１２３４５６７８９②１２３４５６７８９③１２３４５６７８９④１２３４５６７８９⑤１２３４５６７８９⑥１２３４５６７８９⑦１２３４５６７８９⑧１２３４５６７８９⑨１２３４５６７８９⑩。'},
-			{notice: '１２３４５６７８９①１２３４５６７８９②１２３４５６７８９③１２３４５６７８９④１２３４５６７８９⑤１２３４５６７８９⑥１２３４５６７８９⑦１２３４５６７８９⑧１２３４５６７８９⑨１２３４５６７８９⑩。'}]);
+            {notice: '１２３４５６７８９①１２３４５６７８９②１２３４５６７８９③１２３４５６７８９④１２３４５６７８９⑤１２３４５６７８９⑥１２３４５６７８９⑦１２３４５６７８９⑧１２３４５６７８９⑨１２３４５６７８９⑩。'}]);
        		
-			vm.stoppingNotice = ko.observable('現在システムはメンテナンスの為、停止されいています。メンテナンス終了予定は１５：００となります。');
-			vm.stopColor('stopColor');
-			
  }
 
         mounted() {
@@ -38,9 +41,28 @@ module nts.uk.at.kdp003.r {
 		}
     }
 
-    export interface MessageNotice {
-        notice: string;
-        period: string;
+    export class ParentParam {
+		noticeSet: NoticeSet;
+	}
+
+    export class NoticeSet {
+        comMsgColor: ColorSettingDto ; // 会社メッセージ色
+        companyTitle: string; //会社宛タイトル
+        wkpMsgColor: ColorSettingDto //職場メッセージ色
+        wkpTitle: string //職場宛タイトル
+        constructor(init?: Partial<NoticeSet>) {
+			$.extend(this, init);
+		}
     }
+
+    export class ColorSettingDto {
+		textColor: string; // 文字色
+        backGroundColor: string // 背景色
+		constructor(init?: Partial<ColorSettingDto>) {
+			$.extend(this, init);
+		}
+	}
+
+
 
 }
