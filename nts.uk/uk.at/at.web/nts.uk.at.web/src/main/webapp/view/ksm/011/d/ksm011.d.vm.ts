4cc5e1561edea866c0242eceb686f58dbe4a96e1
Thoang Le <thoang.lc@3si.vn>
2021-03-05 16:57:59 +0700

    update code

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksm/011/d/ksm011.d.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksm/011/d/ksm011.d.vm.ts
@@ -1,11 +1,51 @@
 /// <reference path="../../../../lib/nittsu/viewcontext.d.ts" />
 module nts.uk.at.view.ksm011.d {
+  
+  const PATH = {
+    getSetting: ''
+  }
+
   @bean()
   class ViewModel extends ko.ViewModel {
 
+    switchItems: KnockoutObservableArray<any>;
+    switchItems1: KnockoutObservableArray<any>;
+    regularWork: KnockoutObservable<number> = ko.observable(1);
+    fluidWork: KnockoutObservable<number> = ko.observable(0);
+    flexTime: KnockoutObservable<number> = ko.observable(0);
+    workTypeControl: KnockoutObservable<number> = ko.observable(1);
+    achievementDisplay: KnockoutObservable<number> = ko.observable(1);
+    workTypeList: KnockoutObservableArray<number> = ko.observableArray([]);
+    workTypeListText: KnockoutObservable<string> = ko.observable('勤務種類リスト + 勤務種類リスト + 勤務種類リスト');
+
+    selectedCode: KnockoutObservable<string>;
+    selectableCode: KnockoutObservable<string>;
+
+    daysList: KnockoutObservableArray<any>= ko.observableArray([]);
+    deadlineSelected: KnockoutObservable<number> = ko.observable(0);
+    workDisplay: KnockoutObservable<number> = ko.observable(0);
+    abbreviationDisplay: KnockoutObservable<number> = ko.observable(0);
+    shiftDisplay: KnockoutObservable<number> = ko.observable(0);
+    lastDayDisplay: KnockoutObservable<number> = ko.observable(0);
+    _28DayCycle: KnockoutObservable<number> = ko.observable(0);
+    displayByDate: KnockoutObservable<number> = ko.observable(0);
+    
     constructor(params: any) {
       super();
       const vm = this;
+
+      vm.switchItems = ko.observableArray([
+        { code: 1, name: vm.$i18n('KSM011_56') },
+        { code: 0, name: vm.$i18n('KSM011_57') }
+      ]);
+
+      vm.switchItems1 = ko.observableArray([
+        { code: 1, name: vm.$i18n('KSM011_21') },
+        { code: 0, name: vm.$i18n('KSM011_22') }
+      ]);
+
+      //vm.getSetting();
+      vm.getDayList();
     }
 
     created(params: any) {
@@ -14,6 +54,82 @@ module nts.uk.at.view.ksm011.d {
 
     mounted() {
       const vm = this;
+    }
+
+    list(str: string):Array<string>{
+      return _.split(str, ',');
+    }
+
+    openDialogKDl002() {
+      const vm = this;
+      let lstselectedCode = vm.list(vm.selectedCode());
+      let lstselectableCode = vm.list(vm.selectableCode());
+      nts.uk.ui.windows.setShared('KDL002_Multiple', true, true);
+      //all possible items
+      nts.uk.ui.windows.setShared('KDL002_AllItemObj', lstselectableCode, true);
+      nts.uk.ui.windows.setShared('kdl002isSelection', true);
+      //selected items
+      nts.uk.ui.windows.setShared('KDL002_SelectedItemId', lstselectedCode, true);
+      nts.uk.ui.windows.sub.modal('/view/kdl/002/a/index.xhtml', { title: '乖離時間の登録＞対象項目', }).onClosed(function (): any {
+        var lst = nts.uk.ui.windows.getShared('KDL002_SelectedNewItem');
+        vm.selectedCode();
+        let strLstCode: Array<string> = [],
+            strLstName: Array<string> = [];
+        
+            _.each(lst, (item, index: number) => {
+          strLstCode.push(item.code);          
+          strLstName.push(item.name);          
+        });
+
+        vm.workTypeListText(_.join(strLstName, ' + '));
+        vm.selectedCode(_.join(strLstCode, ','));
+      });
+    }
+
+    getSetting() {
+      const vm = this;
+
+      vm.$ajax(PATH.getSetting).done((data) => {
+        if( data ) {
+          vm.regularWork(data.regularWork);
+          vm.flexTime(data.flexTime);
+          vm.fluidWork(data.fluidWork);
+          vm.workTypeControl(data.workTypeControl);
+          vm.achievementDisplay(data.achievementDisplay);
+          //vm.selectedCode(data.selectableCode);
+          //vm.selectableCode(data.selectableCode);
+        }
+      });
+    }
+
+    saveData() {
+      const vm = this;
+      let params = {
+        regularWork: vm.regularWork(),
+        flexTime: vm.flexTime(),
+        fluidWork: vm.fluidWork(),
+        workTypeControl: vm.workTypeControl(),
+        achievementDisplay: vm.achievementDisplay()
+        //vm.selectedCode(data.selectableCode);
+        //vm.selectableCode(data.selectableCode);
+      };
+
+      vm.$ajax(PATH.getSetting).done((data) => {
+        if( data ) {
+         
+        }
+      });
+    }
+
+    getDayList(){
+      const vm = this;
+      vm.daysList = ko.observableArray([]);
+      let days = [];
+      for( let day = 0; day < 30; day++) {
+        days.push( { day: day, name: (day + 1) + vm.$i18n('KSM011_105')});
+      }
+      days.push( { day: 30, name: vm.$i18n('KSM011_106')});
+      vm.daysList(days);
     }
   }
 }
