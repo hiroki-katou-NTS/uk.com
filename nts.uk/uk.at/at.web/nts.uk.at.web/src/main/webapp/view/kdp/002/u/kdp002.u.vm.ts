fab106d3a09502c8f31da99dd81af5ee8fbfa4c8
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2021-03-08 10:49:52 +0700

    Update kdp002.u.vm.ts

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdp/002/u/kdp002.u.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdp/002/u/kdp002.u.vm.ts
@@ -11,14 +11,22 @@ module nts.uk.at.kdp002.u {
 
 		created() {
 			const vm = this;
-			const data1: IModel = {message: '休暇を取得する場合は、休暇申請をおこなってください。詳細が不明な場合は、内線：101　日通までお知らせください。',
-						URL: 'http://jhvnsnvjskv.co.jp',
-						author: '日通　A子',
-						time: '7/5～7/10'};
+			const data1: IModel = {
+				message: '休暇を取得する場合は、休暇申請をおこなってください。詳細が不明な場合は、内線：101　日通までお知らせください。',
+				URL: 'http://jhvnsnvjskv.co.jp',
+				author: '日通　A子',
+				time: '7/5～7/10'
+			};
 
 			vm.model.push(data1);
 			vm.model.push(data1);
 			vm.model.push(data1);
+		}
+
+		closeDialog() {
+			const vm = this;
+
+			vm.$window.close();
 		}
 	}
 
