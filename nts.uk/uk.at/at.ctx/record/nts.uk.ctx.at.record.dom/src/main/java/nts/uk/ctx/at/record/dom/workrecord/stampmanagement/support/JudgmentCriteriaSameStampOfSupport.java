36a17680805956fd26438a1a3496030760c4b8d5
NSVN-TruongVanLai <NSVN-TruongVanLai@users.noreply.github.com>
2021-03-10 16:33:16 +0700

    func [1] 同一と認識すべきの打刻か

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/workrecord/stampmanagement/support/JudgmentCriteriaSameStampOfSupport.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/workrecord/stampmanagement/support/JudgmentCriteriaSameStampOfSupport.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import nts.arc.enums.EnumAdaptor;
 import nts.arc.layer.dom.AggregateRoot;
 import nts.uk.ctx.at.shared.dom.common.CompanyId;
+import nts.uk.shr.com.time.TimeWithDayAttr;
 
 /**
  * AR: 応援の同一打刻の判断基準
@@ -49,5 +50,19 @@ public class JudgmentCriteriaSameStampOfSupport extends AggregateRoot {
 		return new JudgmentCriteriaSameStampOfSupport(cid, sameStampRanceInMinutes, supportMaxFrame);
 	}
 	
-	
+	/**
+	 * [1] 同一と認識すべきの打刻か
+	 * path : UKDesign.ドメインモデル.NittsuSystem.UniversalK.就業.contexts.勤務実績.打刻管理.応援.応援の同一打刻の判断基準.同一と認識すべきの打刻か
+	 * 「パラメータ」 
+	 * ・基準打刻：時刻（日区分付き） standardStamp
+	 * ・対象打刻：時刻（日区分付き） targetStamp
+	 * 「Output」 ・Boolean
+	 */
+	public boolean checkStampRecognizedAsSame(TimeWithDayAttr standardStamp, TimeWithDayAttr targetStamp) {
+		int standardStampMinutes = standardStamp.minute();
+		int targetStampMinutes   = targetStamp.minute();
+		if (Math.abs(standardStampMinutes - targetStampMinutes) <= this.sameStampRanceInMinutes.v())
+			return true;
+		return false;
+	}
 }
