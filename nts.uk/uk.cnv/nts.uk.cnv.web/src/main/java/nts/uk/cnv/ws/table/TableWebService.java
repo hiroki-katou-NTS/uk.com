172db291d3e35f5938e6bcec130c4711a703a9b2
m_kitahira <m_kitahira@nittsusystem.co.jp>
2021-03-16 19:14:07 +0900

    ちょいリファクタ

--- nts.uk/uk.cnv/nts.uk.cnv.web/src/main/java/nts/uk/cnv/ws/table/TableWebService.java
+++ nts.uk/uk.cnv/nts.uk.cnv.web/src/main/java/nts/uk/cnv/ws/table/TableWebService.java
@@ -1,8 +1,5 @@
 package nts.uk.cnv.ws.table;
 
-import java.util.Arrays;
-import java.util.List;
-
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -14,29 +11,23 @@ import javax.ws.rs.core.MediaType;
 import nts.uk.cnv.app.td.command.TableDefinitionRegistCommand;
 import nts.uk.cnv.app.td.command.TableDefinitionRegistCommandHandler;
 import nts.uk.cnv.app.td.schema.prospect.TableListProspectQuery;
+import nts.uk.cnv.app.td.schema.prospect.TableProspectDto;
 import nts.uk.cnv.app.td.schema.prospect.TableProspectQuery;
 import nts.uk.cnv.app.td.service.TableDesignerService;
-import nts.uk.cnv.dom.td.schema.prospect.definition.TableProspect;
 import nts.uk.cnv.dom.td.schema.prospect.list.TableListProspect;
-import nts.uk.cnv.dom.td.schema.tabledesign.column.DataType;
-import nts.uk.cnv.ws.table.column.ColumnDefinitionDto;
-import nts.uk.cnv.ws.table.column.ColumnTypeDefinitionDto;
 
 @Path("td/tables")
 @Produces(MediaType.APPLICATION_JSON)
 public class TableWebService {
-
-	@Inject
-	TableDesignerService tdService;
-
-	@Inject
-	TableDefinitionRegistCommandHandler handler;
 	
 	@Inject
 	TableListProspectQuery tableListProspect;
 	
 	@Inject
 	TableProspectQuery tableProspect;
+
+	@Inject
+	TableDefinitionRegistCommandHandler handler;
 
 	@GET
 	@Path("list")
