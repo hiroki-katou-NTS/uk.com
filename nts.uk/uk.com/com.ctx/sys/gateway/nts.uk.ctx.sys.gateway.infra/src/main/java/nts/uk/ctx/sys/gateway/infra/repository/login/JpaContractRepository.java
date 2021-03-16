2df5f6efe49a52a496eb3e44f8d4d1f00263d595
m_kitahira <m_kitahira@nittsusystem.co.jp>
2021-03-16 15:24:39 +0900

    bugfix: #114921

--- nts.uk/uk.com/com.ctx/sys/gateway/nts.uk.ctx.sys.gateway.infra/src/main/java/nts/uk/ctx/sys/gateway/infra/repository/login/JpaContractRepository.java
+++ nts.uk/uk.com/com.ctx/sys/gateway/nts.uk.ctx.sys.gateway.infra/src/main/java/nts/uk/ctx/sys/gateway/infra/repository/login/JpaContractRepository.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -16,8 +17,14 @@ import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import nts.arc.layer.infra.data.JpaRepository;
+import nts.arc.time.GeneralDate;
+import nts.arc.time.calendar.period.DatePeriod;
 import nts.uk.ctx.sys.gateway.dom.loginold.Contract;
+import nts.uk.ctx.sys.gateway.dom.loginold.ContractCode;
+import nts.uk.ctx.sys.gateway.dom.loginold.ContractGetMemento;
 import nts.uk.ctx.sys.gateway.dom.loginold.ContractRepository;
+import nts.uk.ctx.sys.gateway.dom.loginold.HashPassword;
+import nts.uk.ctx.sys.gateway.dom.tenantlogin.TenantAuthenticationRepository;
 import nts.uk.ctx.sys.gateway.infra.entity.login.SgwmtContract;
 import nts.uk.ctx.sys.gateway.infra.entity.login.SgwdtContract_;
 
@@ -27,30 +34,34 @@ import nts.uk.ctx.sys.gateway.infra.entity.login.SgwdtContract_;
 @Stateless
 public class JpaContractRepository extends JpaRepository implements ContractRepository {
 
+	@Inject
+	private TenantAuthenticationRepository tenantRepo;
+	
 	/* (non-Javadoc)
 	 * @see nts.uk.ctx.sys.gateway.dom.login.ContractRepository#getContract(java.lang.String)
 	 */
 	@Override
 	public Optional<Contract> getContract(String contractCode) {
+		
+		// Contractクラスは削除予定。それまでの仮実装
+		return tenantRepo.find(contractCode)
+				.map(t -> new Contract(new ContractGetMemento() {
+					
+					@Override
+					public HashPassword getPassword() {
+						return new HashPassword(t.getHashedPassword());
+					}
+					
+					@Override
+					public DatePeriod getContractPeriod() {
+						return new DatePeriod(GeneralDate.min(), GeneralDate.max());
+					}
+					
+					@Override
+					public ContractCode getContractCode() {
+						return new ContractCode(t.getTenantCode());
+					}
+				}));
 
-		EntityManager em = this.getEntityManager();
-
-		CriteriaBuilder builder = em.getCriteriaBuilder();
-		CriteriaQuery<SgwmtContract> query = builder.createQuery(SgwmtContract.class);
-		Root<SgwmtContract> root = query.from(SgwmtContract.class);
-
-		List<Predicate> predicateList = new ArrayList<>();
-
-		predicateList.add(builder.equal(root.get(SgwdtContract_.contractCd), contractCode));
-
-		query.where(predicateList.toArray(new Predicate[] {}));
-
-		List<SgwmtContract> result = em.createQuery(query).getResultList();
-		//get contract
-		if (result.isEmpty()) {
-			return Optional.empty();
-		} else {
-			return Optional.of(new Contract(new JpaContractGetMemento(result.get(0))));
-		}
 	}
 }
