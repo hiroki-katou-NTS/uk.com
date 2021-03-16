ae56a961c4aafc1a9f579348c305440fae8ffd89
ai_muto <ai_muto@nittsusystem.co.jp>
2021-03-17 08:15:12 +0900

    エラーの修正

--- nts.uk/uk.cnv/nts.uk.cnv.web/src/main/java/nts/uk/cnv/infra/td/entity/event/NemTdDeliveryEventAltarationPk.java
+++ nts.uk/uk.cnv/nts.uk.cnv.web/src/main/java/nts/uk/cnv/infra/td/entity/event/NemTdDeliveryEventAltarationPk.java
@@ -11,7 +11,6 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @Embeddable
-@NoArgsConstructor
 @AllArgsConstructor
 @NoArgsConstructor
 public class NemTdDeliveryEventAltarationPk implements Serializable {
--- nts.uk/uk.cnv/nts.uk.cnv.web/src/main/java/nts/uk/cnv/infra/td/entity/event/NemTdOrderEventAltarationPk.java
+++ nts.uk/uk.cnv/nts.uk.cnv.web/src/main/java/nts/uk/cnv/infra/td/entity/event/NemTdOrderEventAltarationPk.java
@@ -11,7 +11,6 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @Embeddable
-@NoArgsConstructor
 @AllArgsConstructor
 @NoArgsConstructor
 public class NemTdOrderEventAltarationPk implements Serializable {
