d8b4854c55dd9b1e129f4ffeed47aca6c294f752
Hiroki Katou <hiroki_katou@nittsusystem.co.jp>
2021-03-18 08:23:12 +0900

    Featureにまつわるドメイン実装

---
+++ nts.uk/uk.cnv/nts.uk.cnv.web/src/main/java/nts/uk/cnv/infra/td/repository/JpaFeatureRepository.java
@@ -0,0 +1,28 @@
+package nts.uk.cnv.infra.td.repository;
+
+import java.util.List;
+
+import nts.arc.layer.infra.data.JpaRepository;
+import nts.uk.cnv.dom.td.feature.Feature;
+import nts.uk.cnv.dom.td.feature.FeatureRepository;
+import nts.uk.cnv.infra.td.entity.feature.NemTdFeature;
+
+public class JpaFeatureRepository extends JpaRepository implements FeatureRepository
+{
+
+	@Override
+	public void insert(Feature domain) {
+		this.commandProxy().insert(NemTdFeature.toEntity(domain));
+		
+	}
+
+	@Override
+	public List<Feature> get() {
+		
+		String jpql = "SELECT * FROM NemTdAlteration f";
+
+		return this.queryProxy().query(jpql, NemTdFeature.class)
+				.getList(entity -> entity.toDomain());
+	}
+
+}
