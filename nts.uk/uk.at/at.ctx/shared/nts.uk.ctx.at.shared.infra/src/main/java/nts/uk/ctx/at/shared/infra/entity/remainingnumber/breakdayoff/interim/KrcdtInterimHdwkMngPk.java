9a4e5271579a159ab1ac716580fcf8b8b0c278ea
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2021-03-18 15:32:07 +0700

    Zantei de-ta - Update DayOff Entity

---
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/entity/remainingnumber/breakdayoff/interim/KrcdtInterimHdwkMngPk.java
@@ -0,0 +1,34 @@
+package nts.uk.ctx.at.shared.infra.entity.remainingnumber.breakdayoff.interim;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+import lombok.AllArgsConstructor;
+import nts.arc.time.GeneralDate;
+
+/**
+ * 
+ * @author sonnlb
+ *
+ */
+@Embeddable
+@AllArgsConstructor
+public class KrcdtInterimHdwkMngPk implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	/** 会社ID */
+	@Column(name = "CID")
+	public String companyID;
+
+	/** 社員ID */
+	@Column(name = "SID")
+	public String sid;
+
+	/** 対象日 */
+	@Column(name = "YMD")
+	public GeneralDate ymd;
+
+}
