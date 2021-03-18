dfc1d056721a8eaaa6fc24af4edb59dc4d928a38
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2021-03-18 15:31:45 +0700

    Zantei de-ta - Update HD60 Entity

---
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/entity/remainingnumber/holidayover60h/KrcmtInterimHd60hPK.java
@@ -0,0 +1,32 @@
+package nts.uk.ctx.at.shared.infra.entity.remainingnumber.holidayover60h;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+import lombok.AllArgsConstructor;
+import nts.arc.time.GeneralDate;
+
+@Embeddable
+@AllArgsConstructor
+public class KrcmtInterimHd60hPK {
+
+	/** 会社ID */
+	@Column(name = "CID")
+	public String companyID;
+
+	/** 社員ID */
+	@Column(name = "SID")
+	public String sid;
+
+	/** 対象日 */
+	@Column(name = "YMD")
+	public GeneralDate ymd;
+
+	/** 時間消化休暇かどうか */
+	@Column(name = "TIME_DIGESTIVE_ATR")
+	public Integer timeDigestiveAtr;
+
+	/** 時間休暇種類 */
+	@Column(name = "TIME_HD_TYPE")
+	public Integer timeHdType;
+}
