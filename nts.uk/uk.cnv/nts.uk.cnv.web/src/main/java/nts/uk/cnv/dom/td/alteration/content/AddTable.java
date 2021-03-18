bb2d47d44334a87f3c9cd0bcbd49b734824252cb
m_kitahira <m_kitahira@nittsusystem.co.jp>
2021-03-18 11:24:04 +0900

    永続化のためGetter追加

--- nts.uk/uk.cnv/nts.uk.cnv.web/src/main/java/nts/uk/cnv/dom/td/alteration/content/AddTable.java
+++ nts.uk/uk.cnv/nts.uk.cnv.web/src/main/java/nts/uk/cnv/dom/td/alteration/content/AddTable.java
@@ -5,12 +5,15 @@ import java.util.List;
 import java.util.Optional;
 
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import nts.uk.cnv.dom.td.alteration.AlterationType;
 import nts.uk.cnv.dom.td.schema.prospect.definition.TableProspectBuilder;
 import nts.uk.cnv.dom.td.schema.tabledesign.TableDesign;
 
 @EqualsAndHashCode(callSuper= false)
 public class AddTable extends AlterationContent {
+	
+	@Getter
 	private final TableDesign tableDesign;
 
 	public AddTable(TableDesign tableDesign) {
