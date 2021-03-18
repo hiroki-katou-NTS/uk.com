affe7587697e0a2bd1be34b9aebc375f9887bcc1
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2021-03-18 15:30:53 +0700

    Zantei de-ta - Update Care Entity

---
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/entity/remainingnumber/nursingcareleave/care/interimdata/KshdtInterimCareDataPK.java
@@ -0,0 +1,42 @@
+package nts.uk.ctx.at.shared.infra.entity.remainingnumber.nursingcareleave.care.interimdata;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+import lombok.AllArgsConstructor;
+import nts.arc.time.GeneralDate;
+
+/**
+ * 
+ * @author sonnlb
+ *
+ */
+@Embeddable
+@AllArgsConstructor
+public class KshdtInterimCareDataPK implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	/** 会社ID */
+	@Column(name = "CID")
+	public String companyID;
+
+	/** 社員ID */
+	@Column(name = "SID")
+	public String sid;
+
+	/** 対象日 */
+	@Column(name = "YMD")
+	public GeneralDate ymd;
+
+	/** 時間消化休暇かどうか */
+	@Column(name = "TIME_DIGESTIVE_ATR")
+	public Integer timeDigestiveAtr;
+
+	/** 時間休暇種類 */
+	@Column(name = "TIME_HD_TYPE")
+	public Integer timeHdType;
+
+}
