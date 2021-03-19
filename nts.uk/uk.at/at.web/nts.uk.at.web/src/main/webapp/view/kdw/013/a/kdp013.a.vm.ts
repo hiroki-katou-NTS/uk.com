94ae41a24834cc1bfe467d65c6afbc6819b47a88
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2021-03-19 09:00:04 +0700

    remove sample at com

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/013/a/kdp013.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/013/a/kdp013.a.vm.ts
@@ -233,4 +233,143 @@ module nts.uk.ui.at.kdp013.a {
             _.extend(window, { vm });
         }
     }
+
+    export module components {
+        @component({
+            name: 'fc-event-editor',
+            template:
+                `<div class="fc-editor-container">
+                    <div class="toolbar">
+                        <svg width="20" height="20" viewBox="0 0 24 24" focusable="false" title="Email">
+                            <path d="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm-.8 2L12 10.8 4.8 6h14.4zM4 18V7.87l8 5.33 8-5.33V18H4z"></path>
+                        </svg>
+                        <svg width="20" height="20" viewBox="0 0 24 24" focusable="false" title="Edit" data-bind="click: edit, timeClick: -1">
+                            <path d="M20.41 4.94l-1.35-1.35c-.78-.78-2.05-.78-2.83 0L3 16.82V21h4.18L20.41 7.77c.79-.78.79-2.05 0-2.83zm-14 14.12L5 19v-1.36l9.82-9.82 1.41 1.41-9.82 9.83z"></path>
+                        </svg>
+                        <svg width="20" height="20" viewBox="0 0 24 24" focusable="false" title="Remove" data-bind="click: remove, timeClick: -1">
+                            <path d="M15 4V3H9v1H4v2h1v13c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V6h1V4h-5zm2 15H7V6h10v13z"></path>
+                            <path d="M9 8h2v9H9zm4 0h2v9h-2z"></path>
+                        </svg>
+                        <svg width="20" height="20" viewBox="0 0 24 24" focusable="false" title="Close" data-bind="click: $component.close, timeClick: -1">
+                            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"></path>
+                        </svg>
+                    </div>
+                    <div>
+                    </div>
+                </div>
+                <style>
+                    .fc-editor-container {
+                        width: 300px;
+                    }
+                </style>`
+        })
+        export class FullCalendarEventDetail extends ko.ViewModel {
+            constructor(private params: any) {
+                super();
+            }
+
+            edit() {
+
+            }
+
+            remove() {
+                const vm = this;
+                const { params } = vm;
+
+                params.remove();
+            }
+
+            close() {
+                const vm = this;
+                const { params } = vm;
+
+                params.close();
+            }
+        }
+
+        type COPYDAY_PARAMS = {
+            close: () => void;
+            data: KnockoutObservable<{
+                from: Date;
+                to: Date
+            }>;
+        };
+
+        @component({
+            name: 'fc-event-copy-day',
+            template:
+                `<div>
+                    <table class="copy-day">
+                        <tr>
+                            <td>
+                                <div data-bind="ntsFormLabel: {}">From</div>
+                            </td>
+                            <td>
+                                <div data-bind="ntsDatePicker: { value: $component.model.from }"></div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div data-bind="ntsFormLabel: {}">To</div>
+                            </td>
+                            <td>
+                                <div data-bind="ntsDatePicker: { value: $component.model.to }"></div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colspan="2" style="text-align: center">
+                                <button class="small" data-bind="i18n: 'Copy', click: $component.copy"></button>
+                                <button class="small" data-bind="i18n: 'Close', click: $component.close"></button>
+                            </td>
+                        </tr>
+                    </table>
+                    <style>
+                        table.copy-day,
+                        table.copy-day td {
+                            padding: 0;
+                            padding-bottom: 5px;
+                            border: 0px solid transparent;
+                        }
+                        table.copy-day tr:last-child td {
+                            padding-bottom: 0;
+                        }
+                    </style>
+                </div>`
+        })
+        export class FullCalendarEventCopyDay extends ko.ViewModel {
+            public model: {
+                from: KnockoutObservable<Date | null>;
+                to: KnockoutObservable<Date | null>;
+            } = {
+                    from: ko.observable(null),
+                    to: ko.observable(null)
+                };
+
+            constructor(private params: COPYDAY_PARAMS) {
+                super();
+            }
+
+            copy() {
+                const vm = this;
+                const { model, params } = vm;
+                const data = ko.toJS(model);
+
+                // bind value to model
+                params.data({
+                    from: moment(data.from, 'YYYY/MM/DD').toDate(),
+                    to: moment(data.to, 'YYYY/MM/DD').toDate()
+                });
+
+                params.close();
+            }
+
+            // close dialog (call from parent component)
+            close() {
+                const vm = this;
+                const { params } = vm;
+
+                params.close();
+            }
+        }
+    }
 }
