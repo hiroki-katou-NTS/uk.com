ba227786a72734e4ae1b9cfb6268fa6422f4cf56
nws-dongnt <dongnt@newwave.vn>
2021-03-22 15:17:19 +0700

    create ToppageAlarmPub

---
+++ nts.uk/uk.com/com.ctx/sys/portal/nts.uk.ctx.sys.portal.pubimp/src/main/java/nts/uk/ctx/sys/portal/pubimp/toppagealarm/ToppageAlarmPubImpl.java
@@ -0,0 +1,16 @@
+package nts.uk.ctx.sys.portal.pubimp.toppagealarm;
+
+import nts.arc.task.tran.AtomTask;
+import nts.uk.ctx.sys.portal.pub.toppagealarm.DeleteInfoExport;
+import nts.uk.ctx.sys.portal.pub.toppagealarm.ToppageAlarmExport;
+import nts.uk.ctx.sys.portal.pub.toppagealarm.ToppageAlarmPub;
+
+public class ToppageAlarmPubImpl implements ToppageAlarmPub {
+
+	@Override
+	public AtomTask create(String cid, ToppageAlarmExport alarmInfo, DeleteInfoExport delInfo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
