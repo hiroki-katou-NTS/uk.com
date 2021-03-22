da94ffe43b20d8e94a756f1750d74629a6396912
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2021-03-22 13:36:55 +0700

    resizable sidebar control

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/013/a/cores/fullcalendar.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/013/a/cores/fullcalendar.ts
@@ -75,13 +75,29 @@ module nts.uk.ui.components.fullcalendar {
             position: relative;
             overflow: hidden;
         }
+        .fc-container.resizer {
+            cursor: col-resize;
+            -webkit-touch-callout: none; /* iOS Safari */
+            -webkit-user-select: none; /* Safari */
+            -khtml-user-select: none; /* Konqueror HTML */
+            -moz-user-select: none; /* Old versions of Firefox */
+                -ms-user-select: none; /* Internet Explorer/Edge */
+                    user-select: none; /* Non-prefixed version, currently
+                                        supported by Chrome, Edge, Opera and Firefox */
+        }
         .fc-container .fc-sidebar {
             float: left;
-            width: 210px;
+            min-width: 210px;
+            max-width: calc(100vw - 670px);
             min-height: 1px;
+            height: calc(100vh - 162px);
             overflow: hidden;
             margin-right: 10px;
             box-sizing: border-box;
+            border-right: 1px solid #ccc;
+        }
+        .fc-container.resizer .fc-sidebar {
+            border-right: 2px solid #aaa;
         }
         .fc-container .fc-sidebar .fc-datepicker .datepicker-container.datepicker-inline {
             position: static !important;
@@ -1454,8 +1470,14 @@ module nts.uk.ui.components.fullcalendar {
                                         const { innerHeight } = window;
 
                                         vm.calendar.setOption('height', `${innerHeight - top - 10}px`);
+
+                                        const sidebar = $('.fc-sidebar').get(0);
+
+                                        if (sidebar) {
+                                            ko.applyBindingsToNode(sidebar, { 'sb-resizer': vm.calendar }, vm);
+                                        }
                                     }
-                                });
+                                })
                         }
                     }
                 },
@@ -2458,6 +2480,70 @@ module nts.uk.ui.components.fullcalendar {
 
                 $(document).off('click', vm.event);
             }
+        }
+
+
+        @handler({
+            bindingName: 'sb-resizer',
+            validatable: true,
+            virtual: false
+        })
+        export class SidebarResizerHandler implements KnockoutBindingHandler {
+            init = (element: HTMLElement, valueAccessor: () => FullCalendar.Calendar, allBindingsAccessor: KnockoutAllBindingsAccessor, viewModel: any, bindingContext: KnockoutBindingContext): void | { controlsDescendantBindings: boolean; } => {
+                const calendar = valueAccessor();
+                const cache = { md: -1, cw: 0 };
+                const ctn = $('.fc-container.cf').get(0);
+
+                $(element)
+                    .on('mousemove', (e: JQueryEvent) => {
+                        const oe = e.originalEvent as MouseEvent;
+                        const bound = element.getBoundingClientRect();
+
+                        if (bound.right - 7 <= oe.clientX && bound.right >= oe.clientX) {
+                            ctn.classList.add('resizer');
+                        } else {
+                            ctn.classList.remove('resizer');
+                        }
+                    })
+                    .on('mouseout', () => {
+                        if (cache.md === -1) {
+                            ctn.classList.remove('resizer');
+                        }
+                    })
+                    .on('mousedown', (evt: JQueryEvent) => {
+                        const oe = evt.originalEvent as MouseEvent;
+
+                        if (ctn.classList.contains('resizer')) {
+                            cache.md = oe.clientX;
+                            cache.cw = element.offsetWidth;
+                        }
+                    })
+                    .on('mouseup', () => {
+                        cache.md = -1;
+                        cache.cw = 0;
+                    });
+
+                $(window)
+                    .on('mousemove', (evt: JQueryEvent) => {
+                        const { cw, md } = cache;
+                        const oe = evt.originalEvent as MouseEvent;
+
+                        if (md !== -1) {
+                            element.style.width = `${cw + oe.clientX - md}px`;
+
+                            calendar.updateSize();
+                        }
+                    })
+                    .on('mouseup', () => {
+                        cache.md = -1;
+                        cache.cw = 0;
+
+                        ctn.classList.remove('resizer');
+                    });
+
+                return { controlsDescendantBindings: true };
+            }
+
         }
 
         @component({
