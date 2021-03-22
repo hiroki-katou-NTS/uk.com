67e42f67cdb64b375b775d557bff5fc1ba193299
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2021-03-22 14:10:40 +0700

    new UI: KAF000A: update component 1 22/03/2021

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/000/shr/kaf000.shr.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/000/shr/kaf000.shr.vm.ts
@@ -262,7 +262,7 @@ module nts.uk.at.view.kaf000.shr.viewmodel {
 			if(appDeadlineUseCategory) {
 				deadlinePart = vm.$i18n('KAF000_40', [value.appDispInfoWithDateOutput.opAppDeadline]);	
 			}
-            vm.deadline(prePart + postPart + deadlinePart);    
+            vm.deadline(prePart + '<br/><br/>' + postPart + '<br/><br/>' + deadlinePart);    
         }
         
         public static checkUsage(
