e433929259fb4faeddf779c81f8337c006b65ba8
Thieu Thanh Tu <tu_tt@nittsusystem-vn.com>
2021-03-23 15:40:56 +0700

    update UI

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdp/003/r/kdp003.r.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdp/003/r/kdp003.r.vm.ts
@@ -14,22 +14,22 @@ module nts.uk.at.kdp003.r {
         workplaceNoticeList: KnockoutObservableArray<any> = ko.observableArray([]);
 
         // R6 利用停止中内容
-		stoppingNotice: KnockoutObservable<string> = ko.observable('');
+		stoppingNotice: KnockoutObservable<string> = ko.observable('現在システムはメンテナンスの為、停止されいています。メンテナンス終了予定は１５：００となります。');
 
-        isNormalMode: KnockoutObservable<boolean> = ko.observable(true);
-
+        isNormalMode: KnockoutObservable<boolean> = ko.observable(false);
+ 		
         parentParam = new ParentParam();
 
-        created(parentParam: ParentParam) {
+        created() {
             const vm = this;
 
-            vm.parentParam = parentParam;
+            //vm.parentParam = parentParam;
 
 			vm.headOfficeNoticeList = ko.observableArray([{notice: '全職場に表示されるメッセージですテストのメッセージ2行目'}]);
             
 			vm.workplaceNoticeList = ko.observableArray([{notice: '職場管理者からのお願いです。今週土曜日は本社にてメンテナンスによる停電があります。'},
+            {notice: '１２３４５６７８９①１２３４５６７８９②１２３４５６７８９③１２３４５６７８９④１２３４５６７８９⑤１２３４５６７８９⑥１２３４５６７８９⑦１２３４５６７８９⑧１２３４５６７８９⑨１２３４５６７８９⑩。'},
             {notice: '１２３４５６７８９①１２３４５６７８９②１２３４５６７８９③１２３４５６７８９④１２３４５６７８９⑤１２３４５６７８９⑥１２３４５６７８９⑦１２３４５６７８９⑧１２３４５６７８９⑨１２３４５６７８９⑩。'}]);
-       		
  }
 
         mounted() {
