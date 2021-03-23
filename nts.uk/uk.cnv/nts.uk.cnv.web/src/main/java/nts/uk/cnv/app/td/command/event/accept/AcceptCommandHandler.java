c9fb45a80c68065951d17c71379024095636df74
CORP\keisuke_hoshina <keisuke_hoshina@nittsusystem.co.jp>
2021-03-23 13:46:53 +0900

    fix コンフリクトのマージ

--- nts.uk/uk.cnv/nts.uk.cnv.web/src/main/java/nts/uk/cnv/app/td/command/event/accept/AcceptCommandHandler.java
+++ nts.uk/uk.cnv/nts.uk.cnv.web/src/main/java/nts/uk/cnv/app/td/command/event/accept/AcceptCommandHandler.java
@@ -10,6 +10,7 @@ import javax.inject.Inject;
 import lombok.RequiredArgsConstructor;
 import nts.arc.layer.app.command.CommandHandlerContext;
 import nts.arc.layer.app.command.CommandHandlerWithResult;
+import nts.uk.cnv.dom.td.alteration.Alteration;
 import nts.uk.cnv.dom.td.alteration.summary.AlterationSummary;
 import nts.uk.cnv.dom.td.alteration.summary.AlterationSummaryRepository;
 import nts.uk.cnv.dom.td.devstatus.DevelopmentProgress;
@@ -18,21 +19,23 @@ import nts.uk.cnv.dom.td.event.AcceptEventRepository;
 import nts.uk.cnv.dom.td.event.AcceptService;
 import nts.uk.cnv.dom.td.event.AcceptedResult;
 import nts.uk.cnv.dom.td.schema.snapshot.SchemaSnapshot;
+import nts.uk.cnv.dom.td.schema.snapshot.SnapshotRepository;
+import nts.uk.cnv.dom.td.schema.snapshot.TableSnapshot;
 
 @Stateless
 public class AcceptCommandHandler extends CommandHandlerWithResult<AcceptCommand, List<AlterationSummary>> {
 	@Inject
 	private AlterationSummaryRepository alterationSummaryRepo;
-
 	@Inject
 	private AcceptEventRepository acceptEventRepo;
-
 	@Inject
 	private AcceptService service;
-
+	@Inject
+	private SnapshotRepository snapshotRepository;
+	
 	@Override
 	protected List<AlterationSummary> handle(CommandHandlerContext<AcceptCommand> context) {
-		RequireImpl require = new RequireImpl(alterationSummaryRepo, acceptEventRepo);
+		RequireImpl require = new RequireImpl(alterationSummaryRepo, acceptEventRepo,snapshotRepository);
 		AcceptCommand command = context.getCommand();
 		AcceptedResult result = service.accept(
 				require,
@@ -57,7 +60,8 @@ public class AcceptCommandHandler extends CommandHandlerWithResult<AcceptCommand
 	private static class RequireImpl implements AcceptService.Require {
 		private final AlterationSummaryRepository alterationSummaryRepo;
 		private final AcceptEventRepository acceptEventRepo;
-
+		private final SnapshotRepository snapshotRepository;
+		
 		@Override
 		public Optional<String> getNewestAcceptId() {
 			return acceptEventRepo.getNewestAcceptId();
@@ -75,13 +79,23 @@ public class AcceptCommandHandler extends CommandHandlerWithResult<AcceptCommand
 			acceptEventRepo.regist(orderEvent);
 		}
 		@Override
-		public void regist(SchemaSnapshot snapShot) {
-			
-		}
-		@Override
 		public List<AlterationSummary> getEvent(String deliveryEventId, DevelopmentProgress devProgress) {
 			return alterationSummaryRepo.getByEvent(deliveryEventId, devProgress.getBaseline());
 		}
-
+		@Override
+		public void registSchemaSnapShot(SchemaSnapshot schema) {
+			snapshotRepository.regist(schema);
+		}
+		@Override
+		public void registTableSnapShot(List<TableSnapshot> table) {
+			
+		}
+		@Override
+		public List<Alteration> gets(List<String> alterationIds) {
+		}
+		@Override
+		public List<TableSnapshot> getTablesLatest() {
+			return snapshotRepository.getTablesLatest();
+		}
 	};
 }
--- nts.uk/uk.cnv/nts.uk.cnv.web/src/main/java/nts/uk/cnv/dom/td/event/AcceptService.java
+++ nts.uk/uk.cnv/nts.uk.cnv.web/src/main/java/nts/uk/cnv/dom/td/event/AcceptService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 
 import lombok.val;
 import nts.arc.error.BusinessException;
@@ -14,6 +15,8 @@ import nts.arc.task.tran.AtomTask;
 import nts.arc.time.GeneralDateTime;
 import nts.uk.cnv.dom.td.alteration.summary.AlterationSummary;
 import nts.uk.cnv.dom.td.devstatus.DevelopmentProgress;
+import nts.uk.cnv.dom.td.schema.snapshot.CreateShapShot;
+import nts.uk.cnv.dom.td.schema.snapshot.CreateShapShotImpl;
 import nts.uk.cnv.dom.td.schema.snapshot.SchemaSnapshot;
 
 /**
@@ -23,15 +26,20 @@ import nts.uk.cnv.dom.td.schema.snapshot.SchemaSnapshot;
  */
 @Stateless
 public class AcceptService {
-	public AcceptedResult accept(Require require, String deliveryEventId, String eventName, String userName) {
+	
+	@Inject
+	private CreateShapShot createSnashot;
+	
+	public AcceptedResult accept(Require require, String deliveryEventId, EventMetaData meta) {
+		
 		val deliverySummares= require.getEvent(deliveryEventId, DevelopmentProgress.deliveled());
 		if(deliverySummares.isEmpty()) throw new RuntimeException("検収できるものが1つも存在しません。");
 		
 		String featureId = deliverySummares.stream().findFirst().get().getFeatureId(); 
-		val alterations = deliverySummares.stream().map(alter -> alter.getAlterId()).collect(Collectors.toList());
+		val alterationIds = deliverySummares.stream().map(alter -> alter.getAlterId()).collect(Collectors.toList());
 		
 		List<AlterationSummary> alterSummares = require.getByFeature(featureId, DevelopmentProgress.accepted());
-		boolean allUnaccepted = alterations.stream()
+		boolean allUnaccepted = alterationIds.stream()
 				.allMatch(alt -> alterSummares.stream().anyMatch( altSum -> altSum.getAlterId().equals(alt)));
 		if(!allUnaccepted) {
 			throw new BusinessException( new RawErrorMessage(
@@ -41,30 +49,27 @@ public class AcceptService {
 		// この制御は発注に限らずある…？
 		List<AlterationSummary> errorList = new ArrayList<>();
 		alterSummares.forEach(alterSummary -> {
-			errorList.addAll(require.getByTable(alterSummary.getTableId(), DevelopmentProgress.notAccepted()));
+			errorList.addAll(require.getByTable(alterSummary.getTableId(), DevelopmentProgress.notDeliveled()));
 		});
 
 		if(errorList.size() > 0) {
 			return new AcceptedResult(errorList, Optional.empty());
 		}
-
+		val acceptEvent = AcceptEvent.create(require, meta, alterationIds);
 		return new AcceptedResult(errorList,
 			Optional.of(
 				AtomTask.of(() -> {
-					val acceptEvent = AcceptEvent.create(require, eventName, userName, alterations);
 					require.regist(acceptEvent);
-					require.regist(new SchemaSnapshot("", 
-							GeneralDateTime.now(), 
-							acceptEvent.getEventId().toString()));
+					createSnashot.create(require, acceptEvent.getEventId().getId(), alterationIds);
 				}
 			)));
 	}
 
-	public interface Require extends EventIdProvider.ProvideAcceptIdRequire{
+	public interface Require extends EventIdProvider.ProvideAcceptIdRequire,
+															CreateShapShotImpl.Require{
 		List<AlterationSummary> getEvent(String deliveryEventId, DevelopmentProgress devProgress);
 		List<AlterationSummary> getByFeature(String featureId, DevelopmentProgress devProgress);
 		List<AlterationSummary> getByTable(String tableId, DevelopmentProgress devProgress);
 		void regist(AcceptEvent create);
-		void regist(SchemaSnapshot snapShot);
 	}
 }
