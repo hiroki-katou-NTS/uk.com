69f366064ec7e871ae0452cbee59319c88135332
NSVN-TrieuKhacTu <tu_tk@nittsusystem-vn.com>
2021-03-24 15:04:03 +0700

    fix error kdl045 and ksus02
    (cherry picked from commit 12512e9daa466157f6903429a252a312051221c9)

--- nts.uk/uk.mobile/ClientApp/views/ksu/s02/component/a/index.ts
+++ nts.uk/uk.mobile/ClientApp/views/ksu/s02/component/a/index.ts
@@ -327,7 +327,8 @@ export class CalendarAComponent extends Vue {
                     $(dataArea).append(element);
                 }
                 let element = document.createElement('span');
-                element.innerHTML = '。。。';
+                element.innerHTML = this.$i18n('KSUS02_24');
+                element.classList.add('font-size-7px');
                 // element.classList.add('point-css');
                 $(dataArea).append(element);
             } else {
