e8e1b1505154e8aeaab131ee59175c711f756242
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2021-03-24 22:29:51 +0700

    update view d

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/013/d/kdw013.d.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/013/d/kdw013.d.vm.ts
@@ -1,10 +1,28 @@
 module nts.uk.ui.at.kdp013.d {
+
+    type DataContent = {
+        id: string;
+        targetDate: string;
+        description: string;
+        link: string;
+    };
+
     @bean()
     export class ViewModel extends ko.ViewModel {
-        constructor(private params: any[]) {
+        dataSource: KnockoutObservableArray<DataContent> = ko.observableArray([]);
+
+        constructor(params: DataContent[]) {
             super();
+
+            if (!params || !params.length) {
+                this.close();
+            }
+
+            // binding to grid
+            this.dataSource(params);
         }
 
+        // close dialog
         close() {
             const vm = this;
 
