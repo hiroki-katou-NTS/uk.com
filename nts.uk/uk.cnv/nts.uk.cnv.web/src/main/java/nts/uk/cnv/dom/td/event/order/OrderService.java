8a0fa8d04ba5cb1d907f2bb152099d05ba870ee7
Hiroki Katou <hiroki_katou@nittsusystem.co.jp>
2021-03-25 19:34:25 +0900

    Importの削除

--- nts.uk/uk.cnv/nts.uk.cnv.web/src/main/java/nts/uk/cnv/dom/td/event/order/OrderService.java
+++ nts.uk/uk.cnv/nts.uk.cnv.web/src/main/java/nts/uk/cnv/dom/td/event/order/OrderService.java
@@ -2,13 +2,9 @@ package nts.uk.cnv.dom.td.event.order;
 
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
-
-import javax.ejb.Stateless;
 
 import lombok.val;
 import nts.arc.task.tran.AtomTask;
