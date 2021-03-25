de0e136096652c664b8bece624faa2fed3c9a964
VietTX <viet.tx@3si.vn>
2021-03-25 09:30:34 +0700

    KAL003-B: change range primitive value  * 職場日次チェック条件値時間

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/workrecord/erroralarm/primitivevalue/WorkplaceDailyCheckConditionTime.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/workrecord/erroralarm/primitivevalue/WorkplaceDailyCheckConditionTime.java
@@ -7,7 +7,7 @@ import nts.arc.primitive.constraint.TimeRange;
  * 職場日次チェック条件値時間
  *
  */
-@TimeRange(min = "-9999:99", max = "9999:99")
+@TimeRange(min = "-9999:59", max = "9999:59")
 public class WorkplaceDailyCheckConditionTime extends TimeDurationPrimitiveValue<WorkplaceDailyCheckConditionTime> {
 	private static final long serialVersionUID = 1L;
 
