d19deeb93b1dc1250d7dcbbcec6c9219c7b26105
m_kitahira <m_kitahira@nittsusystem.co.jp>
2021-03-27 11:59:28 +0900

    AlterationMetaDataの制約

--- nts.uk/uk.cnv/nts.uk.cnv.web/src/main/java/nts/uk/cnv/dom/td/alteration/AlterationMetaData.java
+++ nts.uk/uk.cnv/nts.uk.cnv.web/src/main/java/nts/uk/cnv/dom/td/alteration/AlterationMetaData.java
@@ -1,17 +1,31 @@
 package nts.uk.cnv.dom.td.alteration;
 
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import nts.arc.error.BusinessException;
+import nts.arc.error.RawErrorMessage;
+import nts.gul.text.StringUtil;
 
 /**
  * メタ情報
  */
 @Getter
 @EqualsAndHashCode
-@AllArgsConstructor
 public class AlterationMetaData {
-	String userName;
-	String comment;
-
+	private final String userName;
+	private final String comment;
+	
+	public AlterationMetaData(String userName, String comment) {
+		
+		if (StringUtil.isNullOrEmpty(userName, true)) {
+			throw new BusinessException(new RawErrorMessage("nameを入力してください"));
+		}
+		
+		if (StringUtil.isNullOrEmpty(comment, true)) {
+			throw new BusinessException(new RawErrorMessage("コメントを入力してください"));
+		}
+		
+		this.userName = userName;
+		this.comment = comment;
+	}
 }
