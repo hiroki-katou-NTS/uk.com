439ad24670700aed604d9f94e21fd384ce33e936
NTS-NguyenXuanThanh <thanh_nx@nittsusystem.co.jp>
2021-03-29 14:34:08 +0900

    junit test  振休申請の反映
    (cherry picked from commit 2314169336bd39d64c5c3ea71c2fb9402d05f585)

---
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/test/java/nts/uk/ctx/at/shared/dom/scherec/appreflectprocess/appreflectcondition/vacationapplication/subleaveapp/RCSubstituteLeaveAppReflectTest.java
@@ -0,0 +1,93 @@
+package nts.uk.ctx.at.shared.dom.scherec.appreflectprocess.appreflectcondition.vacationapplication.subleaveapp;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import org.assertj.core.groups.Tuple;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import lombok.val;
+import mockit.Injectable;
+import mockit.integration.junit4.JMockit;
+import nts.uk.ctx.at.shared.dom.WorkInformation;
+import nts.uk.ctx.at.shared.dom.application.reflectprocess.common.ReflectApplicationHelper;
+import nts.uk.ctx.at.shared.dom.common.TimeZoneWithWorkNo;
+import nts.uk.ctx.at.shared.dom.scherec.application.common.ApplicationTypeShare;
+import nts.uk.ctx.at.shared.dom.scherec.application.common.PrePostAtrShare;
+import nts.uk.ctx.at.shared.dom.scherec.application.furiapp.AbsenceLeaveAppShare;
+import nts.uk.ctx.at.shared.dom.scherec.application.furiapp.TypeApplicationHolidaysShare;
+import nts.uk.ctx.at.shared.dom.scherec.appreflectprocess.appreflectcondition.reflectprocess.DailyRecordOfApplication;
+import nts.uk.ctx.at.shared.dom.scherec.appreflectprocess.appreflectcondition.reflectprocess.ScheduleRecordClassifi;
+import nts.uk.ctx.at.shared.dom.scherec.appreflectprocess.appreflectcondition.vacationapplication.VacationAppReflectOption;
+import nts.uk.ctx.at.shared.dom.scherec.appreflectprocess.appreflectcondition.vacationapplication.leaveapplication.ReflectWorkHourCondition;
+import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.common.timestamp.TimeChangeMeans;
+import nts.uk.shr.com.enumcommon.NotUseAtr;
+
+@RunWith(JMockit.class)
+public class RCSubstituteLeaveAppReflectTest {
+
+	@Injectable
+	private SubstituteLeaveAppReflect.RequireRC require;
+
+	/*
+	 * テストしたい内容
+	 * 
+	 * →休暇系申請の反映を呼ぶ
+	 * 
+	 * ①勤務情報の反映
+	 * 
+	 * ②始業終業の反映
+	 * 
+	 * ③ 出退勤の反映
+	 * 
+	 * 
+	 * 準備するデータ
+	 * 
+	 * →休暇系申請の反映の設定
+	 * 
+	 */
+	@Test
+	public void test() {
+
+		/// 勤務情報、出退勤を反映する
+		VacationAppReflectOption workInfoAttendanceReflect = new VacationAppReflectOption(NotUseAtr.USE, // 1日休暇の場合は出退勤を削除
+				NotUseAtr.USE, // 出退勤を反映する
+				ReflectWorkHourCondition.REFLECT);// 就業時間帯を反映する
+
+		DailyRecordOfApplication dailyApp = ReflectApplicationHelper.createRCWithTimeLeav(ScheduleRecordClassifi.RECORD,
+				1);// no = 1, 就業時間帯コード = 001
+
+		val absenceLeavApp = new AbsenceLeaveAppShare(Arrays.asList(new TimeZoneWithWorkNo(1, 777, 999)), // 勤務時間帯
+				new WorkInformation("003", "005"), // 勤務情報
+				NotUseAtr.USE, // するしない区分
+				Optional.empty(), // 変更元の振休日
+				TypeApplicationHolidaysShare.Abs, //
+				ReflectApplicationHelper.createAppShare(ApplicationTypeShare.COMPLEMENT_LEAVE_APPLICATION,
+						PrePostAtrShare.PREDICT));
+		new SubstituteLeaveAppReflect(workInfoAttendanceReflect).process(require, absenceLeavApp, dailyApp);
+
+		// ①勤務情報の反映
+		assertThat(dailyApp.getWorkInformation().getRecordInfo().getWorkTypeCode().v()).isEqualTo("003");
+
+		assertThat(dailyApp.getWorkInformation().getRecordInfo().getWorkTimeCode().v()).isEqualTo("005");
+
+		// ②始業終業の反映
+		assertThat(dailyApp.getWorkInformation().getScheduleTimeSheets())
+				.extracting(x -> x.getWorkNo().v(), x -> x.getAttendance().v(), x -> x.getLeaveWork().v())
+				.contains(Tuple.tuple(1, 777, 999));
+		
+		//③ 出退勤の反映
+		assertThat(dailyApp.getAttendanceLeave().get().getTimeLeavingWorks())
+		.extracting(x -> x.getWorkNo().v(), // No
+				x -> x.getStampOfAttendance().get().getTimeDay().getTimeWithDay().get().v(), // 出勤 .時刻
+				x -> x.getStampOfAttendance().get().getTimeDay().getReasonTimeChange().getTimeChangeMeans(), // 出勤.時刻変更手段
+				x -> x.getStampOfLeave().get().getTimeDay().getTimeWithDay().get().v(), // 退勤 .時刻
+				x -> x.getStampOfLeave().get().getTimeDay().getReasonTimeChange().getTimeChangeMeans())// 退勤.時刻変更手段
+		.contains(Tuple.tuple(1, 777, TimeChangeMeans.APPLICATION, 999, TimeChangeMeans.APPLICATION));
+
+	}
+
+}
