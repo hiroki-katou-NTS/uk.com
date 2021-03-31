3d03dc37b9940ae69f6792f5b3473db376922a66
chinh.hm <chinh.hm@3si.vn>
2021-03-31 16:26:38 +0700

    fix bug focus kal

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kal/002/b/kal002.b.start.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kal/002/b/kal002.b.start.ts
@@ -2,6 +2,7 @@ module nts.uk.at.view.kal002.b {
     __viewContext.ready(function() {
         var screenModel = new viewmodel.ScreenModel();
         screenModel.startPage().done(function() {
+            $('#idFocus').focus();
             __viewContext.bind(screenModel);
         });
     });
