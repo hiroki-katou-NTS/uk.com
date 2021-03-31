625283633509c5149dced0f81243811a1091f099
chinh.hm <chinh.hm@3si.vn>
2021-03-31 18:40:34 +0700

    fix bug 42854

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kal/002/b/kal002.b.start.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kal/002/b/kal002.b.start.ts
@@ -2,8 +2,8 @@ module nts.uk.at.view.kal002.b {
     __viewContext.ready(function() {
         var screenModel = new viewmodel.ScreenModel();
         screenModel.startPage().done(function() {
-            $('#idFocus').focus();
             __viewContext.bind(screenModel);
+            $('#idFocus').focus();
         });
     });
 }
