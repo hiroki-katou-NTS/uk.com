1f8ae624063825a31b8ed571d0b9a1df4012ad4b
NSVC\dungbn <dung_bn@nittsusystem-vn.com>
2021-04-01 09:53:30 +0700

    KMF004: http://192.168.50.4:3000/issues/115358

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/specialholiday/algorithm/service/MatchingElapsedYearsTblWithNumDaysGrantedTblService.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/specialholiday/algorithm/service/MatchingElapsedYearsTblWithNumDaysGrantedTblService.java
@@ -1,6 +1,7 @@
 package nts.uk.ctx.at.shared.dom.specialholiday.algorithm.service;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import nts.uk.ctx.at.shared.dom.specialholiday.grantinformation.ElapseYear;
 import nts.uk.ctx.at.shared.dom.specialholiday.grantinformation.GrantDateTbl;
@@ -15,7 +16,9 @@ public class MatchingElapsedYearsTblWithNumDaysGrantedTblService {
 	// 	[1] 付与日数テーブルを整合する
 	public static List<GrantDateTbl> consistentGrantDaysTbl(Require require, ElapseYear elapseYear, GrantDateTbl grantDateTbl) {
 		
+		AtomicInteger checkExist= new AtomicInteger(0);
 		List<GrantDateTbl> listGrantDateTbl = require.findBySphdCd(elapseYear.getCompanyId(), elapseYear.getSpecialHolidayCode().v());
+		
 		listGrantDateTbl.forEach(e -> {
 			if (e.getCompanyId().equals(grantDateTbl.getCompanyId()) 
 					&& e.getSpecialHolidayCode().v() == grantDateTbl.getSpecialHolidayCode().v()
@@ -25,8 +28,13 @@ public class MatchingElapsedYearsTblWithNumDaysGrantedTblService {
 				e.setElapseYear(grantDateTbl.getElapseYear());
 				e.setSpecified(grantDateTbl.isSpecified());
 				e.setGrantedDays(grantDateTbl.getGrantedDays());
-			}
+				checkExist.addAndGet(1);
+			} 
 		});
+		
+		if (checkExist.get() == 0) {
+			listGrantDateTbl.add(grantDateTbl);
+		}
 		
 		List<GrantDateTbl> afterMatchingList = elapseYear.matchNumberOfGrantsInGrantDaysTable(listGrantDateTbl);
 		
