1fc996ffee901fb3534e9dcb7d07d0020e1ecf76
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2021-04-02 13:25:07 +0700

    KDp002 Update UIA21

--- nts.uk/uk.at/at.screen/nts.uk.screen.at.ws/src/main/java/nts/uk/screen/at/ws/kdp/kdp002/b/Kdp002BWebService.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.ws/src/main/java/nts/uk/screen/at/ws/kdp/kdp002/b/Kdp002BWebService.java
@@ -5,6 +5,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import nts.uk.screen.at.app.query.kdp.kdp002.a.GetWorkManagementMultiple;
 import nts.uk.screen.at.app.query.kdp.kdp002.a.SettingsStampCommon;
 import nts.uk.screen.at.app.query.kdp.kdp002.a.SettingsStampCommonDto;
 import nts.uk.screen.at.app.query.kdp.kdp002.b.ContentOfNotificationByStamp;
@@ -26,6 +27,9 @@ public class Kdp002BWebService {
 	@Inject
 	private SettingsStampCommon settingsStampCommon;
 	
+	@Inject
+	private GetWorkManagementMultiple getWorkManagementMultiple;
+	
 	@POST 
 	@Path("notification_by_stamp")
 	public ContentOfNotificationByStampDto notificationByStamp(ContentOfNotificationByStampInput param){
@@ -33,9 +37,15 @@ public class Kdp002BWebService {
 	}
 	
 	@POST 
-	@Path("settingsStampCommon")
+	@Path("settings_stamp_common")
 	public SettingsStampCommonDto settingsStampCommon(){
 		return settingsStampCommon.getSettingCommonStamp();
+	}
+	
+	@POST 
+	@Path("work_management_multiple")
+	public boolean getWorkManagementMultiple(){
+		return getWorkManagementMultiple.getWorkManagementMultiple();
 	}
 	
 }
