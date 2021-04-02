f76c1e57c51baf71beb7ebf5c50deb719a2f65d1
nws-dongnt <dongnt@newwave.vn>
2021-04-02 09:00:00 +0700

    update pub
    (cherry picked from commit fdb1ff10ed249a31f4ff56d81122aef11e805137)

--- nts.uk/uk.com/com.ctx/sys/portal/nts.uk.ctx.sys.portal.pub/src/main/java/nts/uk/ctx/sys/portal/pub/toppagealarm/ToppageAlarmPub.java
+++ nts.uk/uk.com/com.ctx/sys/portal/nts.uk.ctx.sys.portal.pub/src/main/java/nts/uk/ctx/sys/portal/pub/toppagealarm/ToppageAlarmPub.java
@@ -1,6 +1,6 @@
 package nts.uk.ctx.sys.portal.pub.toppagealarm;
 
-import nts.arc.task.tran.AtomTask;
+import java.util.List;
 
 /**
  * UKDesign.ドメインモデル.NittsuSystem.UniversalK.システム.ポータル.トップページアラーム（ver4～）.Export.トップページアラームデータを作成する.トップページアラームデータを作成する
@@ -10,10 +10,9 @@ public interface ToppageAlarmPub {
 	/**
 	 * 作成する( 会社ID, トップアラームパラメータ, 削除の情報)
 	 * 
-	 * @param 会社ID         cid
-	 * @param トップアラームパラメータ alarmInfo
-	 * @param 削除の情報        delInfo
-	 * @return AtomTask
+	 * @param 会社ID               cid
+	 * @param List<トップアラームパラメータ> alarmInfos
+	 * @param 削除の情報              delInfo
 	 */
-	public AtomTask create(String cid, ToppageAlarmExport alarmInfo, DeleteInfoExport delInfo);
+	public void create(String cid, List<ToppageAlarmExport> alarmInfos, DeleteInfoExport delInfo);
 }
