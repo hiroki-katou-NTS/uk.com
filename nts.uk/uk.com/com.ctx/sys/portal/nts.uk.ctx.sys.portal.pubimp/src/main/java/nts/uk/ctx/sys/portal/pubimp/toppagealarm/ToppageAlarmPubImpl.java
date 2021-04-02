fdb1ff10ed249a31f4ff56d81122aef11e805137
nws-dongnt <dongnt@newwave.vn>
2021-04-02 09:00:00 +0700

    update pub

--- nts.uk/uk.com/com.ctx/sys/portal/nts.uk.ctx.sys.portal.pubimp/src/main/java/nts/uk/ctx/sys/portal/pubimp/toppagealarm/ToppageAlarmPubImpl.java
+++ nts.uk/uk.com/com.ctx/sys/portal/nts.uk.ctx.sys.portal.pubimp/src/main/java/nts/uk/ctx/sys/portal/pubimp/toppagealarm/ToppageAlarmPubImpl.java
@@ -1,6 +1,6 @@
 package nts.uk.ctx.sys.portal.pubimp.toppagealarm;
 
-import nts.arc.task.tran.AtomTask;
+import java.util.List;
 import nts.uk.ctx.sys.portal.pub.toppagealarm.DeleteInfoExport;
 import nts.uk.ctx.sys.portal.pub.toppagealarm.ToppageAlarmExport;
 import nts.uk.ctx.sys.portal.pub.toppagealarm.ToppageAlarmPub;
@@ -8,9 +8,8 @@ import nts.uk.ctx.sys.portal.pub.toppagealarm.ToppageAlarmPub;
 public class ToppageAlarmPubImpl implements ToppageAlarmPub {
 
 	@Override
-	public AtomTask create(String cid, ToppageAlarmExport alarmInfo, DeleteInfoExport delInfo) {
+	public void create(String cid, List<ToppageAlarmExport> alarmInfo, DeleteInfoExport delInfo) {
 		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
