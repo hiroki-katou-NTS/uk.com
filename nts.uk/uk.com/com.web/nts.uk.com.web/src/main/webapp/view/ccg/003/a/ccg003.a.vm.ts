157354b950f72f8f257134359e0f26222775d71f
nws-dungdv <dungdv@newwave.vn>
2021-04-05 14:08:17 +0700

    display ccg003

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/003/a/ccg003.a.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/003/a/ccg003.a.vm.ts
@@ -93,7 +93,7 @@ module nts.uk.com.view.ccg003.a {
             <div class="mr-data no-border-radius">
               <!-- A5_2 記念日内容 -->
               <div>
-                <span style="white-space: pre-wrap;" data-bind="text: anniversaryNotice.notificationMessage"></span>
+                <span class="ccg003-data" data-bind="text: anniversaryNotice.notificationMessage"></span>
               </div>
             </div>
           </div>
@@ -112,7 +112,7 @@ module nts.uk.com.view.ccg003.a {
             <!-- A6_2 メッセージ内容 -->
             <div class="mr-data no-border-radius">
               <div>
-                <span style="white-space: pre-wrap; word-break: break-all; display: inline-block; width: 430px;" data-bind="html: messageDisplay"></span>
+                <span class="ccg003-data" data-bind="html: messageDisplay"></span>
                 <span class="ccg003-block-5" data-bind="text: $component.$i18n('CCG003_8', [creator])"></span>
                 <span class="ccg003-block-5" data-bind="text: $component.$i18n('CCG003_9', [dateDisplay])"></span>
               </div>
@@ -123,6 +123,12 @@ module nts.uk.com.view.ccg003.a {
     </div>
   </div>
   <style>
+    .ccg003-data {
+      white-space: pre-wrap;
+      word-break: break-all;
+      display: inline-block;
+      width: 430px;
+    }
     #A0-CCG003 {
       min-height: 150px;
       z-index: 90 !important;
