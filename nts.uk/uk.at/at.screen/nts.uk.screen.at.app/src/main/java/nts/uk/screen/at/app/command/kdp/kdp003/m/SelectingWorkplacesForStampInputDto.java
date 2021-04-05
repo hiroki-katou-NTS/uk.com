0419425b216570e90f483622de53190365c802e7
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2021-04-05 09:37:05 +0700

    Update SelectingWorkplacesForStampInputDto.java

--- nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/command/kdp/kdp003/m/SelectingWorkplacesForStampInputDto.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/command/kdp/kdp003/m/SelectingWorkplacesForStampInputDto.java
@@ -17,26 +17,29 @@ import lombok.NoArgsConstructor;
 @Data
 public class SelectingWorkplacesForStampInputDto {
 
+	//社員職場ID＝社員所属職場履歴を取得.職場ID
 	public SWkpHistExport sWkpHistExport = new SWkpHistExport();
 
+	//職場情報一覧(List)
 	public List<WorkPlaceInfo> workPlaceInfo;
 
-	@AllArgsConstructor
-	@NoArgsConstructor
-	@Data
-	class SWkpHistExport {
+}
 
-		// 職場ID
-		public String workplaceId;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+class SWkpHistExport {
 
-		/** The workplace code. */
-		public String workplaceCode;
+	// 職場ID
+	public String workplaceId;
 
-		/** The workplace name. */
-		public String workplaceName;
+	/** The workplace code. */
+	public String workplaceCode;
 
-		/** The wkp display name. */
-		// 職場表示名
-		public String wkpDisplayName;
-	}
+	/** The workplace name. */
+	public String workplaceName;
+
+	/** The wkp display name. */
+	// 職場表示名
+	public String wkpDisplayName;
 }
