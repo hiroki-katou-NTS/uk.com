045261bf69eeb6d414bee8a47a4d315d0165932a
NTS-PhamVanDan <dan_pv@nittsusystem-vn.com>
2021-04-08 18:56:05 +0900

    update unit-test with review

--- nts.uk/uk.at/at.ctx/aggregation/nts.uk.ctx.at.aggregation.dom/src/test/java/nts/uk/ctx/at/aggregation/dom/schedulecounter/aggregationprocess/workplacecounter/CountNumberOfPeopleByEachWorkMethodServiceTest.java
+++ nts.uk/uk.at/at.ctx/aggregation/nts.uk.ctx.at.aggregation.dom/src/test/java/nts/uk/ctx/at/aggregation/dom/schedulecounter/aggregationprocess/workplacecounter/CountNumberOfPeopleByEachWorkMethodServiceTest.java
@@ -4,48 +4,60 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
-import org.junit.Before;
 import org.junit.Test;
 
-import mockit.Expectations;
+import lombok.val;
 import mockit.Injectable;
 import nts.arc.testing.assertion.NtsAssert;
 import nts.arc.time.GeneralDate;
 import nts.arc.time.calendar.period.DatePeriod;
 import nts.uk.ctx.at.aggregation.dom.common.IntegrationOfDailyHelperInAggregation;
+import nts.uk.ctx.at.shared.dom.WorkInformation;
+import nts.uk.ctx.at.shared.dom.dailyattdcal.dailyattendance.workinfomation.WorkInfoOfDailyAttendanceHelper;
 import nts.uk.ctx.at.shared.dom.scherec.aggregation.perdaily.AggregationUnitOfWorkMethod;
-import nts.uk.ctx.at.shared.dom.scherec.aggregation.perdaily.DailyAttendanceGroupingUtil;
-import nts.uk.ctx.at.shared.dom.scherec.aggregation.perdaily.NumberOfEmployeesByWorkMethodCountingService;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.dailyattendancework.IntegrationOfDaily;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.workinfomation.WorkInfoOfDailyAttendance;
 import nts.uk.ctx.at.shared.dom.workrule.organizationmanagement.workplace.TargetOrgIdenInfor;
 import nts.uk.ctx.at.shared.dom.worktime.common.WorkTimeCode;
+import nts.uk.ctx.at.shared.dom.worktype.WorkTypeCode;
 
 public class CountNumberOfPeopleByEachWorkMethodServiceTest {
+	
+	@Injectable
+	CountNumberOfPeopleByEachWorkMethodService.Require require;
 	
 	@Test
 	public void testMapping() {
 		
 		Map<String, BigDecimal> planNumberList = new HashMap<>();
-			planNumberList.put("001", new BigDecimal(1));
-			planNumberList.put("002", new BigDecimal(2));
-			planNumberList.put("003", new BigDecimal(3));
-			
 		Map<String, BigDecimal> scheduleNumberList = new HashMap<>();
-			scheduleNumberList.put("002", new BigDecimal(4));
-			scheduleNumberList.put("003", new BigDecimal(6));
-			scheduleNumberList.put("004", new BigDecimal(8));
-			
 		Map<String, BigDecimal> actualNumberList = new HashMap<>();
-			actualNumberList.put("003", new BigDecimal(9));
-			actualNumberList.put("004", new BigDecimal(12));
-			actualNumberList.put("005", new BigDecimal(15));
+		
+		planNumberList.put("001", BigDecimal.valueOf(1));
+		// schedule -
+		// actual   -
+		
+		planNumberList.put("002", BigDecimal.valueOf(2));
+		scheduleNumberList.put("002", BigDecimal.valueOf(4));
+		// actual   
+		
+		planNumberList.put("003", BigDecimal.valueOf(3));
+		scheduleNumberList.put("003", BigDecimal.valueOf(6));
+		actualNumberList.put("003", BigDecimal.valueOf(9));
+
+		// plan     -
+		scheduleNumberList.put("004", BigDecimal.valueOf(8));
+		actualNumberList.put("004", BigDecimal.valueOf(12));
+
+		// plan     -
+		// schedule -
+		actualNumberList.put("005", BigDecimal.valueOf(15));
 		
 		List<NumberOfPeopleByEachWorkMethod<String>> result = 
 				NtsAssert.Invoke.staticMethod(CountNumberOfPeopleByEachWorkMethodService.class, "mapping", 
@@ -69,235 +81,171 @@ public class CountNumberOfPeopleByEachWorkMethodServiceTest {
 		
 	} 
 	
-	public static class TestWithRealData {
+	@Test
+	public void testCount(
+			@Injectable WorkInfoOfDailyAttendance dummyWorkInfo
+			) {
 		
-		@Injectable
-		CountNumberOfPeopleByEachWorkMethodService.Require require;
+		GeneralDate ymd0401 = GeneralDate.ymd(2021, 4, 1);
+		// GeneralDate ymd0402
+		GeneralDate ymd0403 = GeneralDate.ymd(2021, 4, 3);
+		GeneralDate ymd0404 = GeneralDate.ymd(2021, 4, 4);
+		DatePeriod period = new DatePeriod(ymd0401, ymd0403);
 		
-		private GeneralDate ymd2021_4_1;
-		private GeneralDate ymd2021_4_2;
-		private GeneralDate ymd2021_4_3;
+		@SuppressWarnings("serial")
+		List<IntegrationOfDaily> workList = new ArrayList<IntegrationOfDaily>() {{
+			// 4/1
+			add( IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp1", ymd0401, Helper.createWorkInfo("dummy", "001")) );
+			// 4/2 empty
+			// 4/3
+			add( IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp1", ymd0403, Helper.createWorkInfo("dummy", "001")) );
+			add( IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp2", ymd0403, Helper.createWorkInfo("dummy", "001")) );
+			add( IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp3", ymd0403, Helper.createWorkInfo("dummy", "002")) );
+			// 4/4
+			add( IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp3", ymd0404, dummyWorkInfo) );
+		}} ;
 		
-		private DatePeriod period;
+		Map<GeneralDate, Map<WorkTimeCode, BigDecimal>> result = NtsAssert.Invoke.staticMethod(
+				CountNumberOfPeopleByEachWorkMethodService.class, 
+				"count", 
+				require,
+				period,
+				workList,
+				AggregationUnitOfWorkMethod.WORK_TIME,
+				( (Function<String, WorkTimeCode>) WorkTimeCode::new)
+				);
 		
-		@Before
-		public void init() {
-			ymd2021_4_1 = GeneralDate.ymd(2021, 4, 1);
-			ymd2021_4_2 = GeneralDate.ymd(2021, 4, 2);
-			ymd2021_4_3 = GeneralDate.ymd(2021, 4, 3);
-			
-			period = new DatePeriod(ymd2021_4_1, ymd2021_4_2);
-		}
+		assertThat( result ).containsOnlyKeys( ymd0401, ymd0403 );
 		
-		@SuppressWarnings("unchecked")
-		@Test
-		public void testCount(
-				@Injectable WorkInfoOfDailyAttendance workInfo1,
-				@Injectable WorkInfoOfDailyAttendance workInfo2,
-				@Injectable WorkInfoOfDailyAttendance workInfo3,
-				@Injectable WorkInfoOfDailyAttendance workInfo4,
-				@Injectable WorkInfoOfDailyAttendance workInfo5
-				) {
-			
-			IntegrationOfDaily work1 = IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp1", ymd2021_4_1, workInfo1);
-			IntegrationOfDaily work2 = IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp1", ymd2021_4_2, workInfo2);
-			IntegrationOfDaily work3 = IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp1", ymd2021_4_2, workInfo3);
-			IntegrationOfDaily work4 = IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp1", ymd2021_4_2, workInfo4);
-			IntegrationOfDaily work5 = IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp1", ymd2021_4_3, workInfo5);
-			
-			List<IntegrationOfDaily> workList = Arrays.asList( work1, work2, work3, work4, work5);
-			List<IntegrationOfDaily> filteredWorkList = Arrays.asList( work1, work2, work3, work4 );
-			
-			Map<GeneralDate, List<WorkInfoOfDailyAttendance>> workListForEachDate = new HashMap<>();
-			List<WorkInfoOfDailyAttendance> workInfoList41 = Arrays.asList(workInfo1);
-			List<WorkInfoOfDailyAttendance> workInfoList42 = Arrays.asList(workInfo2, workInfo3, workInfo4);
-			workListForEachDate.put(ymd2021_4_1, workInfoList41 );
-			workListForEachDate.put(ymd2021_4_2, workInfoList42 );
-			
-			Map<String, BigDecimal> countResult41 = new HashMap<>();
-			countResult41.put("001", new BigDecimal(1));
-			
-			Map<String, BigDecimal> countResult42 = new HashMap<>();
-			countResult42.put("001", new BigDecimal(2));
-			countResult42.put("002", new BigDecimal(1));
-			
-			AggregationUnitOfWorkMethod unit = AggregationUnitOfWorkMethod.WORK_TIME;
-			
-			new Expectations( DailyAttendanceGroupingUtil.class, NumberOfEmployeesByWorkMethodCountingService.class ) {{
-				
-				DailyAttendanceGroupingUtil.byDateWithAnyItem(filteredWorkList, (Function<IntegrationOfDaily, WorkTimeCode>) any );
-				result = workListForEachDate;
-				
-				NumberOfEmployeesByWorkMethodCountingService.count( require, unit, workInfoList41 );
-				result = countResult41;
-				
-				NumberOfEmployeesByWorkMethodCountingService.count( require, unit, workInfoList42 );
-				result = countResult42;
-			}};
-			
-			Function<String, WorkTimeCode> function = x -> new WorkTimeCode(x);
-			
-			Map<GeneralDate, Map<WorkTimeCode, BigDecimal>> result = NtsAssert.Invoke.staticMethod(
-					CountNumberOfPeopleByEachWorkMethodService.class, 
-					"count", 
-					require,
-					period,
-					workList,
-					unit,
-					function
-					);
-			
-			assertThat(result.keySet()).containsOnly( ymd2021_4_1, ymd2021_4_2 );
-			
-			assertThat( result.get(ymd2021_4_1).entrySet() )
-				.extracting( 
-					d -> d.getKey().v(),
-					d -> d.getValue().intValue() )
-				.containsOnly( 
-					tuple( "001", 1));
+		assertThat( result.get(ymd0401).entrySet() )
+			.extracting( Map.Entry::getKey, Map.Entry::getValue )
+			.containsExactlyInAnyOrder( 
+				tuple( new WorkTimeCode("001"), BigDecimal.valueOf(1)) );
 
-			assertThat( result.get(ymd2021_4_2).entrySet() )
-				.extracting( 
-					d -> d.getKey().v(),
-					d -> d.getValue().intValue() )
-				.containsOnly( 
-					tuple( "001", 2),
-					tuple( "002", 1)
-					);
+		assertThat( result.get(ymd0403).entrySet() )
+			.extracting( Map.Entry::getKey, Map.Entry::getValue )
+			.containsExactlyInAnyOrder( 
+					tuple( new WorkTimeCode("001"), BigDecimal.valueOf(2)),
+					tuple( new WorkTimeCode("002"), BigDecimal.valueOf(1)));
+	}
+	
+	@Test
+	public void testCountByWorkMethod(
+			@Injectable TargetOrgIdenInfor targetOrg,
+			@Injectable WorkInfoOfDailyAttendance dummyWorkInfo
+			) {
+		
+		GeneralDate ymd0401 = GeneralDate.ymd(2021, 4, 1);
+		GeneralDate ymd0402 = GeneralDate.ymd(2021, 4, 2);
+		GeneralDate ymd0403 = GeneralDate.ymd(2021, 4, 3);
+		GeneralDate ymd0404 = GeneralDate.ymd(2021, 4, 4);
+		GeneralDate ymd0405 = GeneralDate.ymd(2021, 4, 5);
+		DatePeriod period = new DatePeriod(ymd0401, ymd0404);
+		
+		val scheList = new ArrayList<>();
+		val actList = new ArrayList<>();
+		{
+			/** 4/1 */
+			// [001] schedule: 1 / actual: 0 
+			scheList.add( IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp1", ymd0401, Helper.createWorkInfo("dummy", "001")) );
+			// [002] schedule: 0 / actual: 2
+			actList.add( IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp2", ymd0401, Helper.createWorkInfo("dummy", "002")) );
+			actList.add( IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp3", ymd0401, Helper.createWorkInfo("dummy", "002")) );
+			
+			/** 4/2 */
+			// [003] schedule: 0 / actual: 1
+			actList.add( IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp1", ymd0402, Helper.createWorkInfo("dummy", "003")) );
+			
+			/** 4/3 */
+			// [---] schedule: 0 / actual: 0
+			
+			/** 4/4 */
+			// [001] schedule: 2 / actual: 0
+			scheList.add( IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp1", ymd0404, Helper.createWorkInfo("dummy", "001")) );
+			scheList.add( IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp2", ymd0404, Helper.createWorkInfo("dummy", "001")) );
+			// [002] schedule: 1 / actual: 1
+			scheList.add( IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp3", ymd0404, Helper.createWorkInfo("dummy", "002")) );
+			actList.add( IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp4", ymd0404, Helper.createWorkInfo("dummy", "002")) );
+			// [003] schedule: 0 / actual: 3
+			actList.add( IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp5", ymd0404, Helper.createWorkInfo("dummy", "003")) );
+			actList.add( IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp6", ymd0404, Helper.createWorkInfo("dummy", "003")) );
+			actList.add( IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp7", ymd0404, Helper.createWorkInfo("dummy", "003")) );
+			
+			/** 4/5 */
+			// [dummy] schedule: 1 / actual: 1
+			scheList.add( IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp1", ymd0405, dummyWorkInfo ) );
+			actList.add( IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp2", ymd0405, dummyWorkInfo ) );
+			
 		}
 		
-		@SuppressWarnings("unchecked")
-		@Test
-		public void testCountByWorkMethod(
-				@Injectable TargetOrgIdenInfor targetOrg,
-				
-				@Injectable WorkInfoOfDailyAttendance scheWorkInfo1,
-				@Injectable WorkInfoOfDailyAttendance scheWorkInfo2,
-				@Injectable WorkInfoOfDailyAttendance scheWorkInfo3,
-				@Injectable WorkInfoOfDailyAttendance scheWorkInfo4,
-				@Injectable WorkInfoOfDailyAttendance scheWorkInfo5,
-				
-				@Injectable WorkInfoOfDailyAttendance actWorkInfo1,
-				@Injectable WorkInfoOfDailyAttendance actWorkInfo2,
-				@Injectable WorkInfoOfDailyAttendance actWorkInfo3,
-				@Injectable WorkInfoOfDailyAttendance actWorkInfo4,
-				@Injectable WorkInfoOfDailyAttendance actWorkInfo5
-				) {
-			
-			// Schedule
-			IntegrationOfDaily sche1 = IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp1", ymd2021_4_1, scheWorkInfo1);
-			IntegrationOfDaily sche2 = IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp1", ymd2021_4_2, scheWorkInfo2);
-			IntegrationOfDaily sche3 = IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp1", ymd2021_4_2, scheWorkInfo3);
-			IntegrationOfDaily sche4 = IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp1", ymd2021_4_2, scheWorkInfo4);
-			IntegrationOfDaily sche5 = IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp1", ymd2021_4_3, scheWorkInfo5);
-			
-			// Actual
-			IntegrationOfDaily act1 = IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp1", ymd2021_4_1, actWorkInfo1);
-			IntegrationOfDaily act2 = IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp1", ymd2021_4_2, actWorkInfo2);
-			IntegrationOfDaily act3 = IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp1", ymd2021_4_2, actWorkInfo3);
-			IntegrationOfDaily act4 = IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp1", ymd2021_4_2, actWorkInfo4);
-			IntegrationOfDaily act5 = IntegrationOfDailyHelperInAggregation.createWithWorkInfo("emp1", ymd2021_4_3, actWorkInfo5);
-			
-			// Schedule
-			List<IntegrationOfDaily> scheList = Arrays.asList( sche1, sche2, sche3, sche4, sche5);
-			List<IntegrationOfDaily> filteredScheList = Arrays.asList( sche1, sche2, sche3, sche4 );
-			
-			List<IntegrationOfDaily> actList = Arrays.asList( act1, act2, act3, act4, act5);
-			List<IntegrationOfDaily> filteredActList = Arrays.asList( act1, act2, act3, act4 );
-			
-			// Schedule
-			Map<GeneralDate, List<WorkInfoOfDailyAttendance>> scheListForEachDate = new HashMap<>();
-			List<WorkInfoOfDailyAttendance> scheInfoList41 = Arrays.asList(scheWorkInfo1);
-			List<WorkInfoOfDailyAttendance> scheInfoList42 = Arrays.asList(scheWorkInfo2, scheWorkInfo3, scheWorkInfo4);
-			scheListForEachDate.put(ymd2021_4_1, scheInfoList41 );
-			scheListForEachDate.put(ymd2021_4_2, scheInfoList42 );
-			
-			// Actual
-			Map<GeneralDate, List<WorkInfoOfDailyAttendance>> actListForEachDate = new HashMap<>();
-			List<WorkInfoOfDailyAttendance> actInfoList41 = Arrays.asList(actWorkInfo1);
-			List<WorkInfoOfDailyAttendance> actInfoList42 = Arrays.asList(actWorkInfo2, actWorkInfo3, actWorkInfo4);
-			actListForEachDate.put(ymd2021_4_1, actInfoList41 );
-			actListForEachDate.put(ymd2021_4_2, actInfoList42 );
-			
-			// Schedule
-			Map<String, BigDecimal> scheCountResult41 = new HashMap<>();
-			scheCountResult41.put("001", new BigDecimal(1));
-			
-			Map<String, BigDecimal> scheCountResult42 = new HashMap<>();
-			scheCountResult42.put("001", new BigDecimal(2));
-			scheCountResult42.put("002", new BigDecimal(1));
-			
-			// Actual
-			Map<String, BigDecimal> actCountResult41 = new HashMap<>();
-			actCountResult41.put("001", new BigDecimal(1));
-			
-			Map<String, BigDecimal> actCountResult42 = new HashMap<>();
-			actCountResult42.put("001", new BigDecimal(2));
-			actCountResult42.put("002", new BigDecimal(1));
-			
-			AggregationUnitOfWorkMethod unit = AggregationUnitOfWorkMethod.WORK_TIME;
-			
-			new Expectations( DailyAttendanceGroupingUtil.class, NumberOfEmployeesByWorkMethodCountingService.class ) {{
-				// Schedule
-				DailyAttendanceGroupingUtil.byDateWithAnyItem(filteredScheList, (Function<IntegrationOfDaily, WorkTimeCode>) any );
-				result = scheListForEachDate;
-				
-				// Actual
-				DailyAttendanceGroupingUtil.byDateWithAnyItem(filteredActList, (Function<IntegrationOfDaily, WorkTimeCode>) any );
-				result = actListForEachDate;
-				
-				// Schedule
-				NumberOfEmployeesByWorkMethodCountingService.count( require, unit, scheInfoList41 );
-				result = scheCountResult41;
-				
-				NumberOfEmployeesByWorkMethodCountingService.count( require, unit, scheInfoList42 );
-				result = scheCountResult42;
-				
-				// Actual
-				NumberOfEmployeesByWorkMethodCountingService.count( require, unit, actInfoList41 );
-				result = actCountResult41;
-				
-				NumberOfEmployeesByWorkMethodCountingService.count( require, unit, actInfoList42 );
-				result = actCountResult42;
-			}};
-			
-			Function<String, WorkTimeCode> function = x -> new WorkTimeCode(x);
-			
-			Map<GeneralDate, List<NumberOfPeopleByEachWorkMethod<WorkTimeCode>> > result = NtsAssert.Invoke.staticMethod(
-					CountNumberOfPeopleByEachWorkMethodService.class, 
-					"countByWorkMethod", 
-					require,
-					targetOrg,
-					period,
-					scheList,
-					actList,
-					unit,
-					function
-					);
-			
-			assertThat(result.keySet()).containsOnly( ymd2021_4_1, ymd2021_4_2 );
-			
-			assertThat( result.get(ymd2021_4_1) )
-				.extracting( 
-						d -> d.getWorkMethod().v(),
-						d -> d.getPlanNumber().intValue(),
-						d -> d.getScheduleNumber().intValue(),
-						d -> d.getActualNumber().intValue())
-				.containsOnly(
-						tuple( "001", 0, 1, 1));
-			
-			assertThat( result.get(ymd2021_4_2) )
+		Map<GeneralDate, List<NumberOfPeopleByEachWorkMethod<WorkTimeCode>> > result = NtsAssert.Invoke.staticMethod(
+				CountNumberOfPeopleByEachWorkMethodService.class, 
+				"countByWorkMethod", 
+				require,
+				targetOrg,
+				period,
+				scheList,
+				actList,
+				AggregationUnitOfWorkMethod.WORK_TIME,
+				( (Function<String, WorkTimeCode>) WorkTimeCode::new)
+				);
+		
+		// test key
+		assertThat( result ).containsOnlyKeys( period.datesBetween() );
+		
+		// 2021/4/1
+		assertThat( result.get( ymd0401) )
 			.extracting( 
 					d -> d.getWorkMethod().v(),
 					d -> d.getPlanNumber().intValue(),
 					d -> d.getScheduleNumber().intValue(),
 					d -> d.getActualNumber().intValue())
 			.containsOnly(
-					tuple( "001", 0, 2, 2),
-					tuple( "002", 0, 1, 1)
+					tuple( "001", 0, 1, 0),
+					tuple( "002", 0, 0, 2)
 					);
+		// 2021/4/2
+		assertThat( result.get(ymd0402) )
+		.extracting( 
+				d -> d.getWorkMethod().v(),
+				d -> d.getPlanNumber().intValue(),
+				d -> d.getScheduleNumber().intValue(),
+				d -> d.getActualNumber().intValue())
+		.containsOnly(
+				tuple( "003", 0, 0, 1)
+				);
+		
+		// 2021/4/3
+		assertThat( result.get(ymd0403) ).isEmpty();
+		
+		// 2021/4/4
+		assertThat( result.get(ymd0404) )
+		.extracting( 
+				d -> d.getWorkMethod().v(),
+				d -> d.getPlanNumber().intValue(),
+				d -> d.getScheduleNumber().intValue(),
+				d -> d.getActualNumber().intValue())
+		.containsOnly(
+				tuple( "001", 0, 2, 0),
+				tuple( "002", 0, 1, 1),
+				tuple( "003", 0, 0, 3)
+				);
+	}
+		
+
+	public static class Helper {
+		
+		/**
+		 * 日別勤怠の勤務情報を作成する
+		 * @param workTypeCode 勤務種類コード（String)
+		 * @param workTimeCode 就業時間帯コード(String)
+		 * @return
+		 */
+		public static WorkInfoOfDailyAttendance createWorkInfo(String workTypeCode, String workTimeCode) {
+			return WorkInfoOfDailyAttendanceHelper.getData(
+					new WorkInformation(new WorkTypeCode(workTypeCode), new WorkTimeCode(workTimeCode)) );
 		}
 		
 	}
-
+	
 }
--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/test/java/nts/uk/ctx/at/shared/dom/scherec/aggregation/perdaily/NumberOfEmployeesByWorkMethodCountingServiceTest.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/test/java/nts/uk/ctx/at/shared/dom/scherec/aggregation/perdaily/NumberOfEmployeesByWorkMethodCountingServiceTest.java
@@ -8,15 +8,12 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 import org.junit.Test;
 
-import mockit.Expectations;
 import mockit.Injectable;
 import nts.uk.ctx.at.shared.dom.WorkInformation;
 import nts.uk.ctx.at.shared.dom.dailyattdcal.dailyattendance.workinfomation.WorkInfoOfDailyAttendanceHelper;
-import nts.uk.ctx.at.shared.dom.scherec.aggregation.AggregationByTypeService;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.workinfomation.WorkInfoOfDailyAttendance;
 import nts.uk.ctx.at.shared.dom.worktime.common.WorkTimeCode;
 import nts.uk.ctx.at.shared.dom.worktype.WorkTypeCode;
@@ -26,7 +23,6 @@ public class NumberOfEmployeesByWorkMethodCountingServiceTest {
 	@Injectable
 	public NumberOfEmployeesByWorkMethodCountingService.Require require;
 	
-	@SuppressWarnings("unchecked")
 	@Test
 	public void testCount() {
 		
@@ -34,7 +30,7 @@ public class NumberOfEmployeesByWorkMethodCountingServiceTest {
 		WorkInformation workInfo2 = new WorkInformation(new WorkTypeCode("wtp-code2"), new WorkTimeCode("002") );
 		WorkInformation workInfo3 = new WorkInformation(new WorkTypeCode("wtp-code3"), new WorkTimeCode("002") );
 		WorkInformation workInfo4 = new WorkInformation(new WorkTypeCode("wtp-code4"), new WorkTimeCode("002") );
-		WorkInformation workInfo5 = new WorkInformation(new WorkTypeCode("wtp-code4"), null );
+		WorkInformation workInfo5 = new WorkInformation(new WorkTypeCode("wtp-code5"), null );
 		
 		WorkInfoOfDailyAttendance workInfoOfDailyAttendance1 = WorkInfoOfDailyAttendanceHelper.getData(workInfo1);
 		WorkInfoOfDailyAttendance workInfoOfDailyAttendance2 = WorkInfoOfDailyAttendanceHelper.getData(workInfo2);
@@ -50,31 +46,8 @@ public class NumberOfEmployeesByWorkMethodCountingServiceTest {
 				);
 		
 		Map<String, BigDecimal> mockResult = new HashMap<>();
-		mockResult.put("001", new BigDecimal(1));
-		mockResult.put("002", new BigDecimal(3));
-		
-		
-		new Expectations(AggregationByTypeService.class, AggregationUnitOfWorkMethod.class) {{
-			
-			AggregationUnitOfWorkMethod.WORK_TIME.getWorkMethod(require, workInfoOfDailyAttendance1);;
-			result = Optional.of("001");
-			
-			AggregationUnitOfWorkMethod.WORK_TIME.getWorkMethod(require, workInfoOfDailyAttendance2);;
-			result = Optional.of("002");
-			
-			AggregationUnitOfWorkMethod.WORK_TIME.getWorkMethod(require, workInfoOfDailyAttendance3);;
-			result = Optional.of("002");
-			
-			AggregationUnitOfWorkMethod.WORK_TIME.getWorkMethod(require, workInfoOfDailyAttendance4);;
-			result = Optional.of("002");
-			
-			AggregationUnitOfWorkMethod.WORK_TIME.getWorkMethod(require, workInfoOfDailyAttendance5);;
-			result = Optional.empty();
-			
-			AggregationByTypeService.count( (List<String>) any );
-			result = mockResult;
-			
-		}};
+		mockResult.put("001", BigDecimal.valueOf(1));
+		mockResult.put("002", BigDecimal.valueOf(3));
 		
 		Map<String, BigDecimal> result = 
 				NumberOfEmployeesByWorkMethodCountingService.count(
@@ -87,8 +60,8 @@ public class NumberOfEmployeesByWorkMethodCountingServiceTest {
 					entry -> entry.getKey(),
 					entry -> entry.getValue() )
 			.containsOnly( 
-					tuple("001", new BigDecimal(1)),
-					tuple("002", new BigDecimal(3)) );
+					tuple("001", BigDecimal.valueOf(1)),
+					tuple("002", BigDecimal.valueOf(3)) );
 		
 	}
 
