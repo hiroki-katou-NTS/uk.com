024ebb1b42d8c6f41e1fbfcf068f8469981d65d3
PhamThanh <33304485+NSVN-PhamVanThanh@users.noreply.github.com>
2021-04-12 09:55:56 +0700

    KDP010 remove old screen

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdp/010_old/a/kdp010.a.start.ts
+++
@@ -1,8 +0,0 @@
-module nts.uk.at.view.kdp010.a {
-    __viewContext.ready(function() {
-        var screenModel =  __viewContext.vm = new viewmodel.ScreenModel();
-        screenModel.startPage().done(function() {
-            __viewContext.bind(screenModel);
-        });
-    });
-}
