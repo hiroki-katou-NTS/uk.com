14640d4a193606e10c93809a502ea75f73503dcd
NSVN-NguyenDinhHoang <61735854+NSVN-NguyenDinhHoang@users.noreply.github.com>
2021-04-14 13:05:16 +0700

    Fix dialog KAF011C

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/011/c/kaf011.c.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/011/c/kaf011.c.vm.ts
@@ -3,7 +3,6 @@ module nts.uk.at.view.kaf011.c.viewmodel {
    	import ajax = nts.uk.request.ajax;
 	import block = nts.uk.ui.block;
 	import dialog = nts.uk.ui.dialog;
-	import CommonProcess = nts.uk.at.view.kaf000.shr.viewmodel.CommonProcess;
 	
 	export class KAF011C {
 		displayInforWhenStarting: any;
@@ -37,6 +36,35 @@ module nts.uk.at.view.kaf011.c.viewmodel {
 			$('input').trigger("validate");
 			return !nts.uk.ui.errors.hasError();
 		}
+		public handleMailResult(result: any, vm: any): any {
+			let dfd = $.Deferred();
+			if(_.isEmpty(result.autoFailServer)) {
+				if(_.isEmpty(result.autoSuccessMail)) {
+					if(_.isEmpty(result.autoFailMail)) {
+						dfd.resolve(true);
+					} else {
+						vm.$dialog.error({ messageId: 'Msg_768', messageParams: [_.join(result.autoFailMail, ',')] }).then(() => {
+				        	dfd.resolve(true);
+				        });	
+					}
+				} else {
+					vm.$dialog.info({ messageId: 'Msg_392', messageParams: [_.join(result.autoSuccessMail, ',')] }).then(() => {
+						if(_.isEmpty(result.autoFailMail)) {
+							dfd.resolve(true);	
+						} else {
+							vm.$dialog.error({ messageId: 'Msg_768', messageParams: [_.join(result.autoFailMail, ',')] }).then(() => {
+					        	dfd.resolve(true);
+					        });	
+						}
+			        });	
+				}	
+			} else {
+				vm.$dialog.error({ messageId: 'Msg_1057' }).then(() => {
+		        	dfd.resolve(true);
+		        });
+			}
+			return dfd.promise();
+		}
 		
 		save(){
 			let self = this;
@@ -50,7 +78,7 @@ module nts.uk.at.view.kaf011.c.viewmodel {
 				block.invisible();
 				ajax('at/request/application/holidayshipment/saveChangeDateScreenC',{appDateNew: new Date(self.appDate()), displayInforWhenStarting: self.displayInforWhenStarting, appReason: self.appReason(), appStandardReasonCD: self.appStandardReasonCD()}).then((data: any) =>{
 					dialog.info({ messageId: "Msg_15"}).then(()=>{
-						return CommonProcess.handleMailResult(data, self).then(() => {
+						return self.handleMailResult(data, self).then(() => {
 							nts.uk.ui.windows.setShared('KAF011C_RESLUT', { appID: data.appIDLst[0] });
 							self.closeDialog();
                             return true;
