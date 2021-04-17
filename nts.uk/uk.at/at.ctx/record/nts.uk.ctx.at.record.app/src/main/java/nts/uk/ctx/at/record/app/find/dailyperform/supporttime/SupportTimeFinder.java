9df6ad2a458d09433c520d46ce6c40441b03fd78
NSVN-TruongVanLai <NSVN-TruongVanLai@users.noreply.github.com>
2021-04-17 13:31:26 +0700

    update finder, save supportTime

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/find/dailyperform/supporttime/SupportTimeFinder.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/find/dailyperform/supporttime/SupportTimeFinder.java
@@ -3,6 +3,7 @@
  */
 package nts.uk.ctx.at.record.app.find.dailyperform.supporttime;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -16,8 +17,10 @@ import lombok.val;
 import nts.arc.time.GeneralDate;
 import nts.arc.time.calendar.period.DatePeriod;
 import nts.uk.ctx.at.record.app.find.dailyattendance.timesheet.ouen.dto.OuenWorkTimeSheetOfDailyAttendanceDto;
+import nts.uk.ctx.at.record.app.find.dailyperform.remark.dto.RemarksOfDailyDto;
 import nts.uk.ctx.at.record.dom.daily.ouen.OuenWorkTimeSheetOfDaily;
 import nts.uk.ctx.at.record.dom.daily.ouen.OuenWorkTimeSheetOfDailyRepo;
+import nts.uk.ctx.at.record.dom.daily.remarks.RemarksOfDailyPerform;
 import nts.uk.ctx.at.shared.app.util.attendanceitem.FinderFacade;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.converter.util.item.ConvertibleAttendanceItem;
 
@@ -65,13 +68,31 @@ public class SupportTimeFinder extends FinderFacade{
 	@Override
 	@SuppressWarnings("unchecked")
 	public <T extends ConvertibleAttendanceItem> List<T> find(Map<String, List<GeneralDate>> param) {
-		return (List<T>) param.keySet().stream()
-						.map(c -> param.get(c).stream().map(d -> finds(c, d))
-													.flatMap(List::stream)
-													.filter(d -> d.isHaveData())
-													.collect(Collectors.toList()))
-						.flatMap(List::stream)
-						.collect(Collectors.toList());
+		
+		List<OuenWorkTimeSheetOfDaily> domains = this.repo.find(param);
+		List<OuenWorkTimeSheetOfDailyAttendanceDto> dtos = group(domains);
+		return (List<T>) dtos;
+	}
+	
+	private List<OuenWorkTimeSheetOfDailyAttendanceDto> group(List<OuenWorkTimeSheetOfDaily> domains) {
+		Map<String, Map<GeneralDate, List<OuenWorkTimeSheetOfDaily>>> supports = domains.stream()
+				.collect(Collectors.groupingBy(OuenWorkTimeSheetOfDaily::getEmpId,
+						Collectors.collectingAndThen(Collectors.toList(), list -> list.stream()
+								.collect(Collectors.groupingBy(OuenWorkTimeSheetOfDaily::getYmd, Collectors.toList())))));
+
+		List<OuenWorkTimeSheetOfDailyAttendanceDto> dto = new ArrayList<>();
+
+		supports.entrySet().forEach(es -> {
+			es.getValue().entrySet().forEach(ves -> {
+				ves.getValue().forEach( tves -> {
+					tves.getOuenTimeSheet().forEach( s -> {
+						dto.add(OuenWorkTimeSheetOfDailyAttendanceDto.from(es.getKey(), ves.getKey(), s));
+					});
+				});
+			});
+		});
+
+		return dto;
 	}
 
 	@Override
