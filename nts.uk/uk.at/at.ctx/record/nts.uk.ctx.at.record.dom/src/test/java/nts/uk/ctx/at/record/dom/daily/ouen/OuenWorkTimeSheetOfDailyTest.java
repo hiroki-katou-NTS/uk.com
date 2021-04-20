2780b68e5ecb3915747af8aaf5346e34c3cfb7dc
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2021-04-20 14:11:14 +0700

    Update  RegisterOuenWorkTimeSheetOfDailyServiceTest

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/test/java/nts/uk/ctx/at/record/dom/daily/ouen/OuenWorkTimeSheetOfDailyTest.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/test/java/nts/uk/ctx/at/record/dom/daily/ouen/OuenWorkTimeSheetOfDailyTest.java
@@ -1,15 +1,45 @@
 package nts.uk.ctx.at.record.dom.daily.ouen;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
+import mockit.Mock;
+import mockit.MockUp;
 import nts.arc.testing.assertion.NtsAssert;
+import nts.arc.time.GeneralDate;
+import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.converter.util.AttendanceItemIdContainer;
+import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.converter.util.AttendanceItemUtil.AttendanceItemType;
+import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.converter.util.item.ItemValue;
 
 public class OuenWorkTimeSheetOfDailyTest {
 
+	private String empId = "empId";
+	private GeneralDate ymd = GeneralDate.today();
+
 	@Test
 	public void testOuenWorkTimeSheetOfDaily_contructor() {
-		OuenWorkTimeSheetOfDaily rs = OuenWorkTimeSheetOfDailyHelper.getOuenWorkTimeSheetOfDailyDefault();
+		
+		new MockUp<AttendanceItemIdContainer>() {
+			@Mock
+			public  List<ItemValue> getIds(AttendanceItemType type) {
+				return new ArrayList<>();
+
+			}
+		};
+
+		OuenWorkTimeSheetOfDaily rs = new OuenWorkTimeSheetOfDaily(empId, ymd, new ArrayList<>());
 		NtsAssert.invokeGetters(rs);
 	}
+
+	// @Test
+	// public void testMethodChange() {
+	// OuenWorkTimeSheetOfDaily rs =
+	// OuenWorkTimeSheetOfDailyHelper.getOuenWorkTimeSheetOfDailyDefault();
+	//
+	// AttendanceItemToChange attendanceItemToChange =
+	// rs.change(OuenWorkTimeSheetOfDailyHelper.getListOuenWorkTime());
+	// }
 
 }
