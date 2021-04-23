6e3810cb1561d2ef8301d11a7e5d6204d5e95905
VietTX <viet.tx@3si.vn>
2021-04-23 10:08:56 +0700

    KAL001: change period sche year and sche daily by QA

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/alarm/extractionrange/DefaultExtractionRangeService.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/alarm/extractionrange/DefaultExtractionRangeService.java
@@ -28,8 +28,10 @@ import nts.uk.ctx.at.function.dom.alarm.extractionrange.daily.EndSpecify;
 import nts.uk.ctx.at.function.dom.alarm.extractionrange.daily.ExtractionPeriodDaily;
 import nts.uk.ctx.at.function.dom.alarm.extractionrange.daily.StartSpecify;
 import nts.uk.ctx.at.function.dom.alarm.extractionrange.month.ExtractionPeriodMonth;
+import nts.uk.ctx.at.function.dom.alarm.extractionrange.month.FixedMonthly;
 import nts.uk.ctx.at.function.dom.alarm.extractionrange.month.SpecifyEndMonth;
 import nts.uk.ctx.at.function.dom.alarm.extractionrange.month.SpecifyStartMonth;
+import nts.uk.ctx.at.function.dom.alarm.extractionrange.month.YearSpecifiedType;
 import nts.uk.ctx.at.function.dom.alarm.extractionrange.month.mutilmonth.AverageMonth;
 import nts.uk.ctx.at.function.dom.alarm.extractionrange.periodunit.ExtractionPeriodUnit;
 import nts.uk.ctx.at.function.dom.alarm.extractionrange.year.AYear;
@@ -273,15 +275,34 @@ public class DefaultExtractionRangeService implements ExtractionRangeService {
 	 * @param yearMonth
 	 * @return
 	 */
-	public CheckConditionTimeDto getScheYearTime(CheckCondition c, int closureId, YearMonth yearMonth) {	
+	public CheckConditionTimeDto getScheYearTime(CheckCondition c, int closureId, YearMonth yearMonth) {
+		GeneralDate sysDate = GeneralDate.today();
 		YearMonth startMonthly = yearMonth;
 		YearMonth endMonthly = yearMonth;
 		ExtractionPeriodMonth extractionPeriodMonth =  (ExtractionPeriodMonth) c.getExtractPeriodList().get(0);
 		
 		// 固定の月度を指定する
-		// 「パラメータ．処理月」-「固定月度．指定月」を開始月とする　(lấy 「パラメータ．処理月」-「固定月度．指定月」trở thành start month)
 		if (SpecifyStartMonth.SPECIFY_FIXED_MOON_DEGREE == extractionPeriodMonth.getStartMonth().getSpecifyStartMonth()) {
-			startMonthly = yearMonth.addMonths((-1) * extractionPeriodMonth.getStartMonth().getFixedMonthly().get().getDesignatedMonth());
+			if (extractionPeriodMonth.getStartMonth().getFixedMonthly().isPresent()) {
+				FixedMonthly fixedMonthly = extractionPeriodMonth.getStartMonth().getFixedMonthly().get();
+				int fixMonNo = fixedMonthly.getDesignatedMonth();
+				YearMonth targetYearMonth = YearMonth.of(sysDate.year(), fixMonNo);
+						
+				// 固定月度．年の種類　＝　本年の場合
+				if (YearSpecifiedType.FISCAL_YEAR == fixedMonthly.getYearSpecifiedType()) {
+					// 開始月の年　＝　システム日付．年
+					startMonthly = targetYearMonth;
+				} else {
+					// 固定月度．年の種類　＝　本年度の場合
+					// 固定月度．指定月　＜　４　の場合　開始月の年　＝　システム日付．年　－　1
+					if (fixMonNo < 4) {
+						startMonthly = targetYearMonth.addYears(-1);
+					} else {
+						// 固定月度．指定月　＞＝　４の場合　開始月の年　＝　システム日付．年
+						startMonthly = targetYearMonth;
+					}
+				}
+			}
 		} else {
 			// 締め開始月を指定する
 			int startMonthNo = extractionPeriodMonth.getStartMonth().getStrMonthNo().get().getMonthNo();
@@ -289,9 +310,9 @@ public class DefaultExtractionRangeService implements ExtractionRangeService {
 		}
 		
 		// 開始から期間を指定する
-		// 終了月　=　「パラメータ．処理月」　-　「月数指定．月数」とする (để end month = 「」 - 「」)
+		// 終了月　=　「パラメータ．処理月」　-　「月数指定．月数」とする 
 		if(extractionPeriodMonth.getEndMonth().getSpecifyEndMonth().value == SpecifyEndMonth.SPECIFY_PERIOD_FROM_START_MONTH.value ) {
-			endMonthly = yearMonth.addMonths((-1)*extractionPeriodMonth.getEndMonth().getExtractFromStartMonth().value);
+			endMonthly = startMonthly.addMonths(extractionPeriodMonth.getEndMonth().getExtractFromStartMonth().value - 1);
 		} else {
 			// 締め終了月を指定する
 			int endMonthNo = extractionPeriodMonth.getEndMonth().getEndMonthNo().get().getMonthNo();
@@ -328,11 +349,12 @@ public class DefaultExtractionRangeService implements ExtractionRangeService {
 
 		} else {
 			YearMonth extractStartMon = startMonthly.addMonths((-1)*extraction.getStartDate().getStrMonth().get().getMonth());
+			// QA#115836
 			if (c.isScheduleDaily()) {
 				extractStartMon = extractStartMon.addMonths(extraction.getStartDate().getStrMonth().get().getMonth());
 			}
 			
-			DatePeriod datePeriod = ClosureService.getClosurePeriod(require, closureId, extractStartMon);
+			DatePeriod datePeriod = ClosureService.getClosurePeriod(require, closureId, yearMonth);
 			if(datePeriod == null) {
 				return new CheckConditionPeriod(GeneralDate.ymd(startMonthly, 1).date(), GeneralDate.ymd(endMonthly.addMonths(1), 1).addDays(-1).date());
 			}
