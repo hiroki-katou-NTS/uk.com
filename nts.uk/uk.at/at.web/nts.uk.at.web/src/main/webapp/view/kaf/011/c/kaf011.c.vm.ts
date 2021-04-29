2a56c7c3a482248d7e8d1e9b3b53e8c49c46ef75
NSVN-NguyenDinhHoang <61735854+NSVN-NguyenDinhHoang@users.noreply.github.com>
2021-04-29 11:14:50 +0700

    #116188 KAF011

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/011/c/kaf011.c.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/011/c/kaf011.c.vm.ts
@@ -87,7 +87,7 @@ module nts.uk.at.view.kaf011.c.viewmodel {
 				ajax('at/request/application/holidayshipment/saveChangeDateScreenC',{appDateNew: new Date(self.appDate()), displayInforWhenStarting: self.displayInforWhenStarting, appReason: self.appReason(), appStandardReasonCD: self.appStandardReasonCD()}).then((data: any) =>{
 					dialog.info({ messageId: "Msg_15"}).then(()=>{
 						nts.uk.request.ajax("at", "at/request/application/reflect-app", data.reflectAppIdLst);
-						return CommonProcess.handleMailResult(data, self).then(() => {
+						return self.handleMailResult(data, self).then(() => {
 							nts.uk.ui.windows.setShared('KAF011C_RESLUT', { appID: data.appIDLst[0] });
 							self.closeDialog();
                             return true;
