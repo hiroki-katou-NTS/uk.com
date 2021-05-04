81e99369fe6dc0df8eca009f93e7fcf4f58bfdb6
PhamThanh <33304485+NSVN-PhamVanThanh@users.noreply.github.com>
2021-05-04 14:59:48 +0700

    KDW013 Add Domain service

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/jobmanagement/manhourrecordreferencesetting/ManHourRecordReferenceSettingRepository.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/jobmanagement/manhourrecordreferencesetting/ManHourRecordReferenceSettingRepository.java
@@ -4,30 +4,30 @@ import java.util.Optional;
 
 /**
  * @author thanhpv
- * @name 作業変更可能期間設定Repository
+ * @name 	工数実績参照設定Repository	
  */
 public interface ManHourRecordReferenceSettingRepository {
 
 	/**
-	 * @name [1] Insert(作業変更可能期間設定)
+	 * @name [1] Insert(工数実績参照設定)
 	 */
 	void insert(ManHourRecordReferenceSetting domain);
 	
 	/**
-	 * @name [2] Delete(作業変更可能期間設定)
+	 * @name [2] Delete(工数実績参照設定)
 	 */
 	void delete(ManHourRecordReferenceSetting domain);
 	
 	/**
-	 * @name [3] Update(作業変更可能期間設定)
+	 * @name [3] Update(工数実績参照設定)
 	 */
 	void update(ManHourRecordReferenceSetting domain);
 	
 	/**
 	 * @name [4] Get
-	 * @Description 作業変更可能期間設定を取得する
+	 * @Description 工数実績参照設定を取得する			
 	 * @input companyId 会社ID
-	 * @output Optional<作業変更可能期間設定>
+	 * @output Optional<工数実績参照設定>
 	 */
 	Optional<ManHourRecordReferenceSetting> get(String companyId);
 }
