e597cdf10c328ecffef2745090de280d19bc33b9
huylq <huy_lq@nittsusystem-vn.com>
2021-05-06 19:01:37 +0700

    KDW006 d

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.infra/src/main/java/nts/uk/ctx/at/record/infra/repository/workrecord/operationsetting/JpaDailyPerformanceAuthorityRepository.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.infra/src/main/java/nts/uk/ctx/at/record/infra/repository/workrecord/operationsetting/JpaDailyPerformanceAuthorityRepository.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.ejb.Stateless;
 
@@ -26,6 +27,10 @@ public class JpaDailyPerformanceAuthorityRepository extends JpaRepository
 		implements DailyPerformAuthorRepo {
 
 	private static final String GET_DAI_PER_AUTH_WITH_ROLE = "SELECT da FROM KrcmtAttendanceAut da WHERE da.pk.roleId =:roleId";
+	
+	private static final String GET_DAI_PER_AUTH_BY_CID = "SELECT da FROM KrcmtAttendanceAut da WHERE da.pk.companyId =:companyId";
+	
+	private static final String GET_DAI_PER_AUTH_BY_CID_AND_ROLE = GET_DAI_PER_AUTH_BY_CID + " AND da.pk.roleId =:roleId";
 
 	private static final String GET_DAI_PER_AUTH_WITH_ROLE_AND_FUNCTION_NO = "SELECT da FROM KrcmtAttendanceAut da"
 			+ "	WHERE da.pk.roleId = :roleId"
@@ -45,6 +50,42 @@ public class JpaDailyPerformanceAuthorityRepository extends JpaRepository
 				availability = true;
 			}
 			results.add(new DailyPerformanceAuthority(ent.pk.companyId, roleId, ent.pk.functionNo, availability));
+		});
+		return results;
+	}
+	
+	@Override
+	public List<DailyPerformanceAuthority> findByCid(String cid) {
+		List<KrcmtAttendanceAut> entities = this.queryProxy()
+				.query(GET_DAI_PER_AUTH_BY_CID, KrcmtAttendanceAut.class)
+				.setParameter("companyId", cid).getList();
+		List<DailyPerformanceAuthority> results = new ArrayList<>();
+		entities.forEach(ent -> {
+			BigDecimal avaiBigDecimal = ent.availability;
+			boolean availability = false;
+			if (avaiBigDecimal.intValue() == 1) {
+				availability = true;
+			}
+			results.add(new DailyPerformanceAuthority(ent.pk.companyId, ent.pk.roleId, ent.pk.functionNo, availability));
+		});
+		return results;
+	}
+	
+	@Override
+	public List<DailyPerformanceAuthority> findByCidAndRole(String cid, String roleId) {
+		List<KrcmtAttendanceAut> entities = this.queryProxy()
+				.query(GET_DAI_PER_AUTH_BY_CID_AND_ROLE, KrcmtAttendanceAut.class)
+				.setParameter("companyId", cid)
+				.setParameter("roleId", roleId)
+				.getList();
+		List<DailyPerformanceAuthority> results = new ArrayList<>();
+		entities.forEach(ent -> {
+			BigDecimal avaiBigDecimal = ent.availability;
+			boolean availability = false;
+			if (avaiBigDecimal.intValue() == 1) {
+				availability = true;
+			}
+			results.add(new DailyPerformanceAuthority(ent.pk.companyId, ent.pk.roleId, ent.pk.functionNo, availability));
 		});
 		return results;
 	}
@@ -96,6 +137,31 @@ public class JpaDailyPerformanceAuthorityRepository extends JpaRepository
 		//	ドメインモデル「勤務実績の権限」が取得できなければ、
 		//	自由設定区分は「false：使用しない」として取り扱うこと。
 		return oKrcmtDaiPerformanceAut.isPresent();
+	}
+
+	@Override
+	public void copy(String companyId, List<DailyPerformanceAuthority> sourceData, List<String> targetRoleList) {
+		//INPUT．「複写先リスト」をループする
+		targetRoleList.forEach(roleId -> {
+			//複写先のドメインモデル「勤務実績の権限」を削除する
+			List<KrcmtAttendanceAut> deleteEntities = this.queryProxy()
+					.query(GET_DAI_PER_AUTH_BY_CID_AND_ROLE, KrcmtAttendanceAut.class)
+					.setParameter("companyId", companyId)
+					.setParameter("roleId", roleId)
+					.getList();
+			this.commandProxy().removeAll(deleteEntities);
+			this.getEntityManager().flush();
+			
+			//ドメインモデル「勤務実績の権限」を新規登録する
+			List<DailyPerformanceAuthority> newDatas = sourceData.stream()
+					.map(data -> {
+						return new DailyPerformanceAuthority(companyId, roleId, data.getFunctionNo().v(), data.isAvailability());
+					})
+					.collect(Collectors.toList());
+			newDatas.forEach(data -> {
+				this.save(data);
+			});
+		});
 	}
 
 }
