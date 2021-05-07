1714bf4fc5be0f79a462da59e282479cc61ed521
NSVN-TrieuKhacTu <tu_tk@nittsusystem-vn.com>
2021-05-07 13:07:10 +0700

    fixbug 115846

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.ac/src/main/java/nts/uk/ctx/at/function/ac/alarm/BasicSchedulePubAdapterImpl.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.ac/src/main/java/nts/uk/ctx/at/function/ac/alarm/BasicSchedulePubAdapterImpl.java
@@ -1,15 +1,16 @@
 package nts.uk.ctx.at.function.ac.alarm;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+import nts.arc.time.GeneralDate;
 import nts.arc.time.calendar.period.DatePeriod;
 import nts.uk.ctx.at.function.dom.adapter.alarm.BasicScheduleAdapter;
 import nts.uk.ctx.at.function.dom.adapter.alarm.BasicScheduleImport;
-import nts.uk.ctx.at.schedule.pub.schedule.basicschedule.ScBasicScheduleExport;
 import nts.uk.ctx.at.schedule.pub.schedule.basicschedule.ScBasicSchedulePub;
 
 @Stateless
@@ -27,6 +28,13 @@ public class BasicSchedulePubAdapterImpl implements BasicScheduleAdapter {
 						, e.getWorkTypeCode()
 						, e.getWorkTimeCode()))
 				.collect(Collectors.toList());
+	}
+
+
+	@Override
+	public Optional<GeneralDate> acquireMaxDateBasicSchedule(List<String> empIds) {
+		Optional<GeneralDate> data = pub.acquireMaxDateBasicSchedule(empIds);
+		return data;
 	}
 	
 }
