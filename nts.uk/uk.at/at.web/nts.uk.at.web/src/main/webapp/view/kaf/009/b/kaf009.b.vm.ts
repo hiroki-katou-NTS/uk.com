cc3001860d1ee890a551cafb79b76d1627109a85
NSVN-NguyenDinhHoang <61735854+NSVN-NguyenDinhHoang@users.noreply.github.com>
2021-05-07 17:12:13 +0700

    #115907 KAF009 07/05

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/009/b/kaf009.b.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/009/b/kaf009.b.vm.ts
@@ -150,7 +150,8 @@ module nts.uk.at.view.kaf009_ref.b.viewmodel {
                         appDispInfoStartup: ko.observable(res.appDispInfoStartup),
                         goBackReflect: ko.observable(res.goBackReflect),
                         lstWorkType: ko.observable(res.lstWorkType),
-                        goBackApplication: ko.observable(res.goBackApplication)
+                        goBackApplication: ko.observable(res.goBackApplication),
+						workInfo: res.workInfo
                     });
                     vm.printContentOfEachAppDto().opInforGoBackCommonDirectOutput = ko.toJS(vm.dataFetch);
                 }
@@ -352,6 +353,8 @@ module nts.uk.at.view.kaf009_ref.b.viewmodel {
         lstWorkType: KnockoutObservable<any>;
 
         goBackApplication: KnockoutObservable<any>;
+
+		workInfo: any;
     }
     export class GoBackApplication {
         straightDistinction: number;
