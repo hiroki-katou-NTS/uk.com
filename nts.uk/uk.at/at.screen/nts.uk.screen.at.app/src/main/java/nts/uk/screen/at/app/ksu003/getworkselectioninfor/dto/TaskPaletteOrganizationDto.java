1225f5e5c5413d9c41ee916b3a5ef9bba1d13f3d
NSVN-HieuLT <king.of.throw.game@gmail.com>
2021-05-11 07:27:02 +0700

    code thuật toán 作業選択準備情報を取得する

---
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/ksu003/getworkselectioninfor/dto/TaskPaletteOrganizationDto.java
@@ -0,0 +1,46 @@
+package nts.uk.screen.at.app.ksu003.getworkselectioninfor.dto;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import lombok.Getter;
+import lombok.Value;
+import nts.uk.ctx.at.schedule.dom.schedule.task.taskpalette.TaskPaletteOrganization;
+import nts.uk.ctx.at.shared.app.find.workrule.shiftmaster.TargetOrgIdenInforDto;
+import nts.uk.ctx.at.shared.dom.scherec.taskmanagement.taskmaster.TaskCode;
+
+@Value
+@Getter
+public class TaskPaletteOrganizationDto {
+	
+	/* 対象組織*/
+	public TargetOrgIdenInforDto  targetOrgIdenInforDto;
+	/* ページ */
+	public int page;
+	/*表示情報*/
+	public TaskPaletteDisplayInfoDto taskPaletteDisplayInfoDto;
+	
+	public Map<Integer , String> tasks;
+
+	public TaskPaletteOrganizationDto(TargetOrgIdenInforDto targetOrgIdenInforDto, int page,
+			TaskPaletteDisplayInfoDto taskPaletteDisplayInfoDto, Map<Integer, String> tasks) {
+		super();
+		this.targetOrgIdenInforDto = targetOrgIdenInforDto;
+		this.page = page;
+		this.taskPaletteDisplayInfoDto = taskPaletteDisplayInfoDto;
+		this.tasks = tasks;
+	}
+	
+	public static TaskPaletteOrganizationDto toDto(TaskPaletteOrganization domain){
+		TargetOrgIdenInforDto idenInforDto = TargetOrgIdenInforDto.toDto(domain.getTargetOrg());
+		int pages = domain.getPage();
+		
+		Map<Integer , String> tasksMap = new HashMap<>();
+		for (Map.Entry<Integer, TaskCode> map : domain.getTasks().entrySet()){
+			tasksMap.put(map.getKey(), map.getValue().v());
+		}
+		TaskPaletteDisplayInfoDto displayInfoDto = TaskPaletteDisplayInfoDto.toDto(domain.getDisplayInfo());
+		TaskPaletteOrganizationDto dto = new TaskPaletteOrganizationDto(idenInforDto, pages, displayInfoDto, tasksMap);
+		return dto;
+	}
+}
