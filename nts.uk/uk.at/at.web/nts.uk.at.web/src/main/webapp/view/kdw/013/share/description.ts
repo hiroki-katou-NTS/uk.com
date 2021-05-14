24989acaffb999bc54dce1845e9b057e9a897c3f
Nguyễn Văn Vương <nsvn-nguyenvanvuong@users.noreply.github.com>
2021-05-14 10:40:15 +0700

    update message msg2164

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/013/share/description.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/013/share/description.ts
@@ -51,7 +51,7 @@ module nts.uk.ui.at.kdw013.share {
                 .$validate
                 .constraint(constraint)
                 .then((value: vm.Constraint) => {
-                    console.log(value);
+                    // console.log(value);
                 });
 
             value
