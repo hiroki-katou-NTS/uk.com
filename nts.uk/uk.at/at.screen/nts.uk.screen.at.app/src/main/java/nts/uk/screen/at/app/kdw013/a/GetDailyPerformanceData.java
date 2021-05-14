49a4e2de4e7c1a00b4a23d43c33fc9805fba43a8
NSVC\sonnlb <son_nlb@nittsusystem-vn.com>
2021-05-14 08:09:31 +0700

    Update GetDailyPerformanceData.java

--- nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/kdw013/a/GetDailyPerformanceData.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/kdw013/a/GetDailyPerformanceData.java
@@ -1,6 +1,7 @@
 package nts.uk.screen.at.app.kdw013.a;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -42,7 +43,7 @@ public class GetDailyPerformanceData {
 		List<IntegrationOfDaily> lstIntegrationOfDaily = getter.getIntegrationOfDaily(sId, period);
 
 		for (IntegrationOfDaily i : lstIntegrationOfDaily) {
-			List<TimeLeavingWork> timeLeavingWorks = i.getAttendanceLeave().get().getTimeLeavingWorks();
+			List<TimeLeavingWork> timeLeavingWorks = i.getAttendanceLeave().map(x-> x.getTimeLeavingWorks()).orElse(Collections.emptyList());
 
 			for (TimeLeavingWork time : timeLeavingWorks) {
 
@@ -75,19 +76,21 @@ public class GetDailyPerformanceData {
 				// 休憩リスト
 				actualContent.setBreakTimeSheets(i.getBreakTime().getBreakTimeSheets());
 				// 休憩時間
-				actualContent.setBreakHours(Optional.of(i.getAttendanceTimeOfDailyPerformance().get()
-						.getActualWorkingTimeOfDaily().getTotalWorkingTime().getBreakTimeOfDaily().getWorkTime()));
+				actualContent.setBreakHours(Optional.ofNullable(i.getAttendanceTimeOfDailyPerformance().map(
+						x -> x.getActualWorkingTimeOfDaily().getTotalWorkingTime().getBreakTimeOfDaily().getWorkTime())
+						.orElse(null)));
 				// 総労働時間
-				actualContent.setTotalWorkingHours(Optional.of(i.getAttendanceTimeOfDailyPerformance().get()
-						.getActualWorkingTimeOfDaily().getTotalWorkingTime().getTotalTime()));
+				actualContent.setTotalWorkingHours(Optional.ofNullable(i.getAttendanceTimeOfDailyPerformance()
+						.map(x -> x.getActualWorkingTimeOfDaily().getTotalWorkingTime().getTotalTime()).orElse(null)));
 
 				if (time.getWorkNo().v() == 1) {
 					// 開始時刻
-					actualContent
-							.setStart(Optional.of(time.getAttendanceStamp().get().getActualStamp().get().getTimeDay()));
+					actualContent.setStart(Optional.ofNullable(time.getAttendanceStamp()
+							.map(x -> x.getActualStamp().map(y -> y.getTimeDay()).orElse(null)).orElse(null)));
 
 					// 終了時刻
-					actualContent.setEnd(Optional.of(time.getLeaveStamp().get().getActualStamp().get().getTimeDay()));
+					actualContent.setEnd(Optional.ofNullable(time.getLeaveStamp()
+							.map(x -> x.getActualStamp().map(y -> y.getTimeDay()).orElse(null)).orElse(null)));
 				}
 
 				// 実績内容
