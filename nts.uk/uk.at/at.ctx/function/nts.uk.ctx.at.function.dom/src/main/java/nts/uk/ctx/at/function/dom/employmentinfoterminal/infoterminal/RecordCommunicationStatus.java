4d4efb85c671686dbf077d0175f88f360cd26596
NTS-NguyenXuanThanh <thanh_nx@nittsusystem.co.jp>
2021-05-15 15:31:58 +0900

    code update status NR(通信エラーの検知（最終通信時刻を書き込む）)

---
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/employmentinfoterminal/infoterminal/RecordCommunicationStatus.java
@@ -0,0 +1,82 @@
+package nts.uk.ctx.at.function.dom.employmentinfoterminal.infoterminal;
+
+import java.util.Optional;
+
+import lombok.val;
+import nts.arc.task.tran.AtomTask;
+import nts.arc.time.GeneralDate;
+import nts.arc.time.GeneralDateTime;
+import nts.uk.ctx.at.function.dom.adapter.employmentinfoterminal.infoterminal.FuncEmpInfoTerminalImport;
+
+/**
+ * @author thanh_nx
+ *
+ *         通信状況を記録する
+ */
+public class RecordCommunicationStatus {
+
+	// [1] 通信状況を記録する
+	public static AtomTask recordStatus(Require require, ContractCode contractCode, EmpInfoTerminalCode terCode) {
+
+		AtomTask task = AtomTask.none();
+		GeneralDateTime systemTime = GeneralDateTime.now();
+
+		// 就業情報端末を取得する
+		Optional<FuncEmpInfoTerminalImport> empTerInfo = require.getEmpInfoTerminal(terCode.v(), contractCode.v());
+		if (!empTerInfo.isPresent())
+			return task;
+
+		// 就業情報端末通信状況を取得する
+		Optional<EmpInfoTerminalComStatus> empTerComstatus = require.getEmpTerComStatus(contractCode, terCode);
+
+		if (!empTerComstatus.isPresent()) {
+			return task;
+		}
+
+		// 通信異常があったか判断する
+		if (empTerComstatus.map(
+				x -> x.isCommunicationError(new MonitorIntervalTime(empTerInfo.get().getIntervalTime()), systemTime))
+				.orElse(false)) {
+			val empTerComStsNew = empTerComstatus.get().createAbnormalPeriod(systemTime);
+
+			task.then(() -> {
+				require.insertEmpComAbPeriod(empTerComStsNew);
+			});
+
+		}
+
+		// $就業情報端末通信状況Update ＝ $就業情報端末通信状況.最終通信日時を更新する($システム日時)
+		task.then(() -> {
+			require.updateEmpTerStatus(empTerComstatus.get().updateLastTime(systemTime));
+
+			// 過去の「就業情報端末通信異常期間」は削除する。
+			require.deleteEmpTerComAbPast(contractCode, terCode, EmpInfoTerComAbPeriod.getDayRemoveInfo());
+		});
+
+		return task;
+	}
+
+	public static interface Require {
+
+		// [R-1]就業情報端末を取得する
+		// RQEmpInfoTerminalAdapter.getEmpInfoTerminalCode
+		public Optional<FuncEmpInfoTerminalImport> getEmpInfoTerminal(String empInfoTerCode, String contractCode);
+
+		// [R-2]就業情報端末通信状況を取得する
+		// EmpInfoTerminalComStatusRepository.get
+		public Optional<EmpInfoTerminalComStatus> getEmpTerComStatus(ContractCode contractCode,
+				EmpInfoTerminalCode empInfoTerCode);
+
+		// [R-3]就業情報端末通信異常期間をInsertする
+		// EmpInfoTerComAbPeriodRepository.insert
+		void insertEmpComAbPeriod(EmpInfoTerComAbPeriod empInfoTerComAbPeriod);
+
+		// [R-4]就業情報端末通信状況をUpdateする
+		// EmpInfoTerminalComStatusRepository.update
+		public void updateEmpTerStatus(EmpInfoTerminalComStatus empInfoTerComStatus);
+
+		// [R-5]過去の就業情報端末通信異常期間をDeleteする
+		// EmpInfoTerComAbPeriodRepository.deletePast
+		void deleteEmpTerComAbPast(ContractCode contractCode, EmpInfoTerminalCode code, GeneralDate dateDelete);
+	}
+}
