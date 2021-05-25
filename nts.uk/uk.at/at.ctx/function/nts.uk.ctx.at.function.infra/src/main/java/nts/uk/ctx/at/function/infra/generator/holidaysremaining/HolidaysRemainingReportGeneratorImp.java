4ee5732ae84d1da0cba51a7c91a6eb732181a401
chinh.hm <chinh.hm@3si.vn>
2021-05-25 20:10:27 +0700

    update kdr001

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.infra/src/main/java/nts/uk/ctx/at/function/infra/generator/holidaysremaining/HolidaysRemainingReportGeneratorImp.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.infra/src/main/java/nts/uk/ctx/at/function/infra/generator/holidaysremaining/HolidaysRemainingReportGeneratorImp.java
@@ -462,7 +462,7 @@ public class HolidaysRemainingReportGeneratorImp extends AsposeCellsReportGenera
 
         }
         double pages = tottal / (MAX_ROW_IN_PAGE - 6);
-        for (int i = 0; i <= pages; i++) {
+        for (int i = 0; i < pages; i++) {
             int indexRow = first + i * (MAX_ROW_IN_PAGE - 6);
             if (i >= 1) {
                 cells.insertRows(indexRow, 6);
