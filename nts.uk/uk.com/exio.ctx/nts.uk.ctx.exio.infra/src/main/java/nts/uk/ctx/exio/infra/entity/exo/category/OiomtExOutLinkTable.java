f37dba8edf3cb465143b2d4eccc0bc54061eb521
NSVN-NguyenThiHoangYen <yen_nt@nittsusystem-vn.com>
2021-05-25 11:40:23 +0900

    CMF002: OiomtExOutLinkTableの契約を削除する。

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.infra/src/main/java/nts/uk/ctx/exio/infra/entity/exo/category/OiomtExOutLinkTable.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.infra/src/main/java/nts/uk/ctx/exio/infra/entity/exo/category/OiomtExOutLinkTable.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import nts.uk.ctx.exio.dom.exo.category.ExOutLinkTable;
-import nts.uk.shr.infra.data.entity.ContractUkJpaEntity;
+import nts.uk.shr.infra.data.entity.UkJpaEntity;
 
 /**
  * 外部出力リンクテーブル
@@ -20,7 +20,7 @@ import nts.uk.shr.infra.data.entity.ContractUkJpaEntity;
 @NoArgsConstructor
 @Entity
 @Table(name = "OIOMT_EX_OUT_LINK_TABLE")
-public class OiomtExOutLinkTable extends ContractUkJpaEntity implements Serializable {
+public class OiomtExOutLinkTable extends UkJpaEntity implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	/**
