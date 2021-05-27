4f91a6868170a7b2ca435954c97241b217f76f6b
CORP\keisuke_hoshina <keisuke_hoshina@nittsusystem.co.jp>
2021-05-27 15:56:38 +0900

    fix enum検証の実装が動かなかったため修正

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/validation/classtype/ValidationEnum.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/validation/classtype/ValidationEnum.java
@@ -6,7 +6,7 @@ public class ValidationEnum {
 	public static void run(String fqn, Object value) {
 		try {
 			Class<?> pvClass = Class.forName(fqn);
-			EnumAdaptor.valueOf((int)value, pvClass.getClass());
+			EnumAdaptor.valueOf((int)value, pvClass);
 		} catch (Exception ex) {
 			//エラー処理
 			throw new RuntimeException("Enumの検証　仮置きエクスセプションです。");
