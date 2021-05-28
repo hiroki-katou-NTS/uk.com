5adc36e89039a5a57b573673db78321ceb7a9311
NSVC\quytb <quy_tb@nittsusystem-vn.com>
2021-05-28 08:18:55 +0700

    KSU003C: fix bug add search for combo box

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksu/003/c/ksu003.c.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ksu/003/c/ksu003.c.vm.ts
@@ -33,6 +33,7 @@ module nts.uk.at.view.ksu003.c {
         selectedTaskCode: KnockoutObservable<string> = ko.observable("");
         isEnable: KnockoutObservable<boolean>;
         isEditable: KnockoutObservable<boolean>;
+        isReload: KnockoutObservable<boolean> = ko.observable(false);
         singleSelectedCode: any;
 
         endStatus: KnockoutObservable<string> = ko.observable("Cancel"); 
@@ -120,16 +121,16 @@ module nts.uk.at.view.ksu003.c {
                     _.each(data, item => {
                         taskList.push(new TaskMaster(item.code, item.taskName));                        
                     });            
-                    self.taskMasterList(_.sortBy(taskList, task => task.code ));         
-                    self.selectedTaskCode(data[0].code);                    
-                }   
-
+                    self.taskMasterList(_.sortBy(taskList, task => task.code ));   
+                    if(!self.isReload()) {
+                        self.selectedTaskCode(data[0].code);
+                    }                                        
+                }
             }).always(() => {
                 self.$blockui("hide");
             });
             $('#task-list').focus();
         }
-
         
         registerOrUpdate(): void {
             const self = this;            
@@ -164,6 +165,7 @@ module nts.uk.at.view.ksu003.c {
             self.$blockui("invisible");
             self.$ajax(Paths.REGISTET_TASK_SCHEDULE, command).done(() => {
                 self.$dialog.info({ messageId: 'Msg_15'}).then(() => {
+                    self.isReload(true);
                     self.loadData();
                 });
                 self.endStatus('Update');
