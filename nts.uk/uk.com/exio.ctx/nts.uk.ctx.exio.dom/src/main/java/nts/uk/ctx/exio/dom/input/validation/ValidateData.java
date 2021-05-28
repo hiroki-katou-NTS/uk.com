69cd85ac40a5b64050955d4b89c68eed3f6eea44
CORP\keisuke_hoshina <keisuke_hoshina@nittsusystem.co.jp>
2021-05-29 08:46:03 +0900

    feat 値の検証DSをシステムから指定した範囲での検証ＤＳとユーザが指定した範囲での検証ＤＳに分割

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/validation/ValidateData.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/validation/ValidateData.java
@@ -1,29 +1,20 @@
 package nts.uk.ctx.exio.dom.input.validation;
 
-import java.util.List;
-
-import lombok.val;
 import nts.uk.ctx.exio.dom.input.revise.reviseddata.RevisedDataRecord;
+import nts.uk.ctx.exio.dom.input.validation.systemrange.ValidateSystemRange;
+import nts.uk.ctx.exio.dom.input.validation.userrange.ValidateUserRange;
 
 public class ValidateData{
-
+	
 	/**
 	 * 妥当な数値であるか検証する
 	 */
-	public static void validate(Require require, RevisedDataRecord record) {
-		val importableItems = require.getDefinition(record.getCategoryId());
-		
-		record.getItems().forEach(dataitem ->{
-			importableItems.forEach(importableItem ->{
-				//編集済みデータと受入可能項目を突合させ、受入できるかチェック
-				if(importableItem.getItemNo() == dataitem.getItemNo()) {
-					importableItem.validate(dataitem.getValue());
-				}
-			});
-		});
+	public static void validate(ValidateRequire require, RevisedDataRecord record, String externalOutputCode) {
+		ValidateSystemRange.validate(require, record);
+		ValidateUserRange.validate(require, record, externalOutputCode);
 	}
 	
-	public static interface Require{
-		List<ImportableItem> getDefinition(int categoryId);
+	public static interface ValidateRequire extends ValidateUserRange.UserRequire,
+																				ValidateSystemRange.SystemRequire{
 	}
 }
