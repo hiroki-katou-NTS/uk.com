c1bc02a8d5d3e596a8598fa0b85c7d82358106e7
TranXuanViet <viet.tx@3si.vn>
2021-06-01 17:52:04 +0700

    Impl VO: SummaryItemDetail

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/workrecord/workmanagement/manhoursummarytable/SummaryItemDetail.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/workrecord/workmanagement/manhoursummarytable/SummaryItemDetail.java
@@ -1,13 +1,16 @@
 package nts.uk.ctx.at.record.dom.workrecord.workmanagement.manhoursummarytable;
 
 import lombok.Getter;
+import lombok.val;
 import nts.arc.time.GeneralDate;
 import nts.arc.time.YearMonth;
 import org.eclipse.persistence.internal.xr.ValueObject;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * 	集計項目詳細
@@ -39,15 +42,56 @@ public class SummaryItemDetail extends ValueObject {
         this.totalPeriod = Optional.empty();
     }
 
-    public void calculate(TotalUnit totalUnit, List<GeneralDate> dateList, List<YearMonth> yearList){
-//        List<VerticalValueDaily> lstVertical = dateList.
+    /**
+     * 	[1] 合計を計算する
+     * @param totalUnit 合計単位
+     * @param dateList List<年月日>
+     * @param yearMonthList List<年月>
+     */
+    public void calculateTotal(TotalUnit totalUnit, List<GeneralDate> dateList, List<YearMonth> yearMonthList) {
+        if (totalUnit == TotalUnit.DATE)
+            calculateVerticalTotalByDate(dateList);
+        else
+            calculateVerticalTotalByYearMonth(yearMonthList);
+
+        calculateHorizontalTotal();
+    }
+
+    /**
+     * [2] 横計を計算する
+     */
+    public void calculateHorizontalTotal() {
+        val workingHours = verticalTotalList.stream().mapToInt(VerticalValueDaily::getWorkingHours).sum();
+        totalPeriod = Optional.of(workingHours);
     }
 
     /**
      * [prv-1] 年月日別に縦計を計算する
      * @param dateList 	List<年月日>
      */
-    private void calculateVerticalTotalByDate(List<GeneralDate> dateList){
-//        List<VerticalValueDaily> lstVertical = dateList.
+    private void calculateVerticalTotalByDate(List<GeneralDate> dateList) {
+        List<VerticalValueDaily> lstVertical = new ArrayList<>();
+        for (GeneralDate date : dateList) {
+            val childVerticalList = childHierarchyList.stream().flatMap(x -> verticalTotalList.stream()).collect(Collectors.toList());
+            val workingTime = childVerticalList.stream().filter(x -> x.getDate().equals(date)).mapToInt(VerticalValueDaily::getWorkingHours).sum();
+            lstVertical.add(new VerticalValueDaily(workingTime, null, date)); //TODO
+        }
+        if (!lstVertical.isEmpty())
+            verticalTotalList = lstVertical;
+    }
+
+    /**
+     * [prv-2] 年月別に縦計を計算する
+     * @param yearMonthList List<年月>
+     */
+    private void calculateVerticalTotalByYearMonth(List<YearMonth> yearMonthList) {
+        List<VerticalValueDaily> lstVertical = new ArrayList<>();
+        for (val ym : yearMonthList) {
+            val childVerticalList = childHierarchyList.stream().flatMap(x -> verticalTotalList.stream()).collect(Collectors.toList());
+            val workingTime = childVerticalList.stream().filter(x -> x.getYearMonth().year() == ym.year()).mapToInt(VerticalValueDaily::getWorkingHours).sum();
+            lstVertical.add(new VerticalValueDaily(workingTime, ym, null));  //TODO
+        }
+        if (!lstVertical.isEmpty())
+            verticalTotalList = lstVertical;
     }
 }
