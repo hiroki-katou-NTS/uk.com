241ed869009825f98d791d3f01c9f2469791f79f
AnhNM <twas.horus@gmail.com>
2021-06-03 11:22:13 +0700

    Update tempalte edit test

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/test/template/test.template.start.ts
+++
@@ -1,9 +0,0 @@
-module nts.uk.at.view.test.template {
-    __viewContext.ready(function() {
-
-        var screenModel = new nts.uk.at.view.test.template.viewmodel.ViewModel();
-        screenModel.start().done(function() {
-            __viewContext.bind(screenModel);
-        });
-    });
-}
