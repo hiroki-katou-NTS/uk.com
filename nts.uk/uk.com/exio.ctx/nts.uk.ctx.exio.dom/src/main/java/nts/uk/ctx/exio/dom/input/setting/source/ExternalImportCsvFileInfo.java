a07e6e6bb490fc166f68b5ce5683b86cff986733
m_kitahira <m_kitahira@nittsusystem.co.jp>
2021-06-07 16:59:14 +0900

    CSVファイル情報から不要な属性を削除

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/setting/source/ExternalImportCsvFileInfo.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/setting/source/ExternalImportCsvFileInfo.java
@@ -2,22 +2,17 @@ package nts.uk.ctx.exio.dom.input.setting.source;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
+/**
+ * CSVファイル情報
+ */
 @Getter
 @AllArgsConstructor
 public class ExternalImportCsvFileInfo {
-	
-	/** CSVファイルのファイルID */
-	private String fileId;
-	
-	/** 文字コード */
-	private ExternalImportCharset charset;
 	
 	/** CSVの項目名取得行 */
 	private ExternalImportRawNumber itemNameRawNumber;
 	
 	/** CSVの取込開始行 */
 	private ExternalImportRawNumber importStartRawNumber;
-	
 }
