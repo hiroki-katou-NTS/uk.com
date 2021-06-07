e2aeab305321f9b34f3bf0e502bd9587d6bea8fb
m_kitahira <m_kitahira@nittsusystem.co.jp>
2021-06-07 16:58:25 +0900

    CsvRecordリファクタ

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/csvimport/CsvRecord.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/csvimport/CsvRecord.java
@@ -2,25 +2,14 @@ package nts.uk.ctx.exio.dom.input.csvimport;
 
 import java.util.List;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.Value;
 
 /**
  * CSV1行分のデータ
  */
-@Getter
-@NoArgsConstructor
+@Value
 public class CsvRecord {
 	
 	/** CSV1行分のデータ */
 	private List<String> rawItems;
-	
-	/**
-	 * 項目の追加
-	 * @param lineNumber
-	 * @param value
-	 */
-	public void addItem(String value) {
-		this.rawItems.add(value);
-	}
 }
