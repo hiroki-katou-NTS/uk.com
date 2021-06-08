bc617cbf22950a074d0fe5c08c2ff9f1661c26cb
m_kitahira <m_kitahira@nittsusystem.co.jp>
2021-06-08 13:41:36 +0900

    受入実行するCommand（Require未実装）

---
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.app/src/main/java/nts/uk/ctx/exio/app/input/execute/ExternalImportExecuteCommandHandler.java
@@ -0,0 +1,41 @@
+package nts.uk.ctx.exio.app.input.execute;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.inject.Inject;
+
+import lombok.val;
+import nts.arc.layer.app.command.AsyncCommandHandler;
+import nts.arc.layer.app.command.CommandHandlerContext;
+import nts.arc.task.tran.AtomTask;
+import nts.arc.task.tran.TransactionService;
+import nts.uk.ctx.exio.dom.input.ExecuteImporting;
+import nts.uk.shr.com.context.AppContexts;
+
+@Stateless
+@TransactionAttribute(TransactionAttributeType.SUPPORTS)
+public class ExternalImportExecuteCommandHandler extends AsyncCommandHandler<ExternalImportExecuteCommand>{
+
+	@Inject
+	private ExternalImportExecuteRequire require;
+	
+	@Inject
+	private TransactionService transaction;
+	
+	@Override
+	protected void handle(CommandHandlerContext<ExternalImportExecuteCommand> context) {
+		
+		val command = context.getCommand();
+		String companyId = AppContexts.user().companyId();
+		val require = require.create(companyId);
+		
+		Iterable<AtomTask> atomTasks = ExecuteImporting.execute(
+				require,
+				companyId,
+				command.getExternalImportCode());
+		
+		transaction.separateForEachTask(atomTasks);
+	}
+
+}
---
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.app/src/main/java/nts/uk/ctx/exio/app/input/execute/ExternalImportExecuteRequire.java
@@ -0,0 +1,148 @@
+package nts.uk.ctx.exio.app.input.execute;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.inject.Inject;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import nts.arc.diagnose.stopwatch.embed.EmbedStopwatch;
+import nts.arc.time.GeneralDate;
+import nts.uk.cnv.core.dom.conversionsql.ConversionSQL;
+import nts.uk.cnv.core.dom.conversiontable.ConversionCodeType;
+import nts.uk.cnv.core.dom.conversiontable.ConversionTable;
+import nts.uk.ctx.exio.dom.input.ExecuteImporting;
+import nts.uk.ctx.exio.dom.input.ExecutionContext;
+import nts.uk.ctx.exio.dom.input.canonicalize.CanonicalizedDataMetaRepository;
+import nts.uk.ctx.exio.dom.input.canonicalize.existing.AnyRecordToChange;
+import nts.uk.ctx.exio.dom.input.canonicalize.existing.AnyRecordToDelete;
+import nts.uk.ctx.exio.dom.input.canonicalize.groups.GroupCanonicalization;
+import nts.uk.ctx.exio.dom.input.importableitem.group.ImportingGroup;
+import nts.uk.ctx.exio.dom.input.setting.ExternalImportCode;
+import nts.uk.ctx.exio.dom.input.setting.ExternalImportSetting;
+import nts.uk.ctx.exio.dom.input.transfer.ConversionTableRepository;
+import nts.uk.ctx.exio.dom.input.transfer.TransferCanonicalDataRepository;
+import nts.uk.shr.com.history.DateHistoryItem;
+
+@Stateless
+@TransactionAttribute(TransactionAttributeType.SUPPORTS)
+public class ExternalImportExecuteRequire {
+
+	public Require create(String companyId) {
+		return EmbedStopwatch.embed(new RequireImpl(companyId));
+	}
+	
+	public static interface Require extends ExecuteImporting.Require {
+		
+	}
+	
+	@Inject
+	CanonicalizedDataMetaRepository canonicalizedDataMetaRepo;
+	
+	@Inject
+	ConversionTableRepository conversionTableRepo;
+	
+	@Inject
+	TransferCanonicalDataRepository transferCanonicalDataRepo;
+	
+	@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+	public class RequireImpl implements Require {
+		
+		private final String companyId;
+
+		@Override
+		public Optional<ExternalImportSetting> getExternalImportSetting(String companyId,
+				ExternalImportCode settingCode) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public ImportingGroup getGroupTransactionStrategy(int groupId) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public List<String> getAllEmployeeIdsOfCanonicalizedData(ExecutionContext context) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public List<AnyRecordToChange> getAllAnyRecordToChange(ExecutionContext context) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public List<AnyRecordToDelete> getAllAnyRecordToDelete(ExecutionContext context) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public GroupCanonicalization getGroupCanonicalization(int groupId) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public void deleteTask(String companyId, int taskFrameNo, String taskCode) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void changeEmploymentHistory(String companyId, String employeeId, DateHistoryItem historyItem) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void deleteEmploymentHistory(String companyId, String employeeId, DateHistoryItem historyItem) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void deleteDailyPerformance(String employeeId, GeneralDate date) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public List<AnyRecordToChange> getEmployeeAnyRecordToChange(ExecutionContext context, String employeeId) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public List<AnyRecordToDelete> getEmployeeAnyRecordToDelete(ExecutionContext context, String employeeId) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public List<String> getImportiongItem() {
+			return canonicalizedDataMetaRepo.get(companyId);
+		}
+
+		@Override
+		public List<ConversionTable> getConversionTable(int groupId, ConversionCodeType cct) {
+			val source = conversionTableRepo.getSource(groupId);
+			return conversionTableRepo.get(groupId, source, cct);
+		}
+
+		@Override
+		public int execute(List<ConversionSQL> conversionSqls) {
+			return transferCanonicalDataRepo.execute(conversionSqls);
+		}
+		
+	}
+}
