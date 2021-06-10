d4bebbd0a608fe6e4d9a39839b4a80769b29ef30
ai_muto <ai_muto@nittsusystem.co.jp>
2021-06-11 08:57:01 +0900

    ビルドエラー修正

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/test/java/nts/uk/ctx/at/shared/dom/scherec/dailyattdcal/ortherpackage/classfunction/TimevacationUseTimeOfDailyTest.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/test/java/nts/uk/ctx/at/shared/dom/scherec/dailyattdcal/ortherpackage/classfunction/TimevacationUseTimeOfDailyTest.java
@@ -1,37 +1,37 @@
 package nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.ortherpackage.classfunction;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 import java.util.Optional;
 
 import org.junit.Test;
 
 import nts.uk.ctx.at.shared.dom.common.time.AttendanceTime;
+import nts.uk.ctx.at.shared.dom.scherec.addsettingofworktime.AdditionAtr;
 import nts.uk.ctx.at.shared.dom.scherec.addsettingofworktime.HolidayAddtionSet;
 import nts.uk.ctx.at.shared.dom.scherec.addsettingofworktime.LeaveSetAdded;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.TimevacationUseTimeOfDaily;
-import nts.uk.ctx.at.shared.dom.scherec.addsettingofworktime.AdditionAtr;
 import nts.uk.shr.com.enumcommon.NotUseAtr;
 /**
  * 日別実績の時間休暇使用時間のテスト
  */
 public class TimevacationUseTimeOfDailyTest {
-	
+
 	/**
 	 * 加算使用時間の計算
 	 * 加算区分全ての場合のテスト
-	 * 
+	 *
 	 */
 	@Test
 	public void testCalcTotalVacation() {
 
 		TimevacationUseTimeOfDaily timeDaily = new TimevacationUseTimeOfDaily(
 													new AttendanceTime(60),
-													new AttendanceTime(180), 
-													new AttendanceTime(240), 
-													new AttendanceTime(120), 
+													new AttendanceTime(180),
+													new AttendanceTime(240),
+													new AttendanceTime(120),
 													Optional.empty(),
-													new AttendanceTime(300), 
+													new AttendanceTime(300),
 													new AttendanceTime(360));
 
 		int actualResult = timeDaily.calcTotalVacationAddTime(Optional.empty(), AdditionAtr.All);
@@ -48,11 +48,11 @@ public class TimevacationUseTimeOfDailyTest {
 	public void testCalcTotalVacation2() {
 		TimevacationUseTimeOfDaily timeDaily = new TimevacationUseTimeOfDaily(
 													new AttendanceTime(60),
-													new AttendanceTime(180), 
-													new AttendanceTime(240), 
-													new AttendanceTime(120), 
+													new AttendanceTime(180),
+													new AttendanceTime(240),
+													new AttendanceTime(120),
 													Optional.empty(),
-													new AttendanceTime(300), 
+													new AttendanceTime(300),
 													new AttendanceTime(360));
 		int actualResult = timeDaily
 				.calcTotalVacationAddTime(
@@ -61,7 +61,7 @@ public class TimevacationUseTimeOfDailyTest {
 		assertThat(actualResult).as("A2").isEqualTo(1080);
 
 	}
-	
+
 	/**
 	 * 加算使用時間の計算
 	 * 加算区分　＝　就業加算時間のみの場合　かつ　年休加算する、特別休暇加算しない
@@ -70,11 +70,11 @@ public class TimevacationUseTimeOfDailyTest {
 	public void testCalcTotalVacation3() {
 		TimevacationUseTimeOfDaily timeDaily = new TimevacationUseTimeOfDaily(
 													new AttendanceTime(60),
-													new AttendanceTime(180), 
-													new AttendanceTime(240), 
-													new AttendanceTime(120), 
+													new AttendanceTime(180),
+													new AttendanceTime(240),
+													new AttendanceTime(120),
 													Optional.empty(),
-													new AttendanceTime(300), 
+													new AttendanceTime(300),
 													new AttendanceTime(360));
 
 		int actualResult = timeDaily
@@ -84,7 +84,7 @@ public class TimevacationUseTimeOfDailyTest {
 		assertThat(actualResult).as("A3").isEqualTo(1140);
 
 	}
-	
+
 	/**
 	 * 加算使用時間の計算
 	 * 加算区分　＝　就業加算時間のみの場合　かつ　年休加算しない、特別休暇加算する
@@ -93,11 +93,11 @@ public class TimevacationUseTimeOfDailyTest {
 	public void testCalcTotalVacation4() {
 		TimevacationUseTimeOfDaily timeDaily = new TimevacationUseTimeOfDaily(
 													new AttendanceTime(60),
-													new AttendanceTime(180), 
-													new AttendanceTime(240), 
-													new AttendanceTime(120), 
+													new AttendanceTime(180),
+													new AttendanceTime(240),
+													new AttendanceTime(120),
 													Optional.empty(),
-													new AttendanceTime(300), 
+													new AttendanceTime(300),
 													new AttendanceTime(360));
 
 		int actualResult = timeDaily
@@ -107,7 +107,7 @@ public class TimevacationUseTimeOfDailyTest {
 		assertThat(actualResult).as("A4").isEqualTo(1200);
 
 	}
-	
+
 	/**
 	 * 加算使用時間の計算
 	 * 加算区分　＝　就業加算時間のみの場合　かつ　年休加算する、特別休暇加算する
@@ -116,11 +116,11 @@ public class TimevacationUseTimeOfDailyTest {
 	public void testCalcTotalVacation5() {
 		TimevacationUseTimeOfDaily timeDaily = new TimevacationUseTimeOfDaily(
 													new AttendanceTime(60),
-													new AttendanceTime(180), 
-													new AttendanceTime(240), 
-													new AttendanceTime(120), 
+													new AttendanceTime(180),
+													new AttendanceTime(240),
+													new AttendanceTime(120),
 													Optional.empty(),
-													new AttendanceTime(300), 
+													new AttendanceTime(300),
 													new AttendanceTime(360));
 
 		int actualResult = timeDaily
@@ -130,23 +130,23 @@ public class TimevacationUseTimeOfDailyTest {
 		assertThat(actualResult).as("A5").isEqualTo(1260);
 
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * 合計使用時間の計算
 	 * 合計時間を求めるテスト
-	 * 
+	 *
 	 */
 	@Test
 	public void testTotalVacation() {
 
 		TimevacationUseTimeOfDaily timeDaily = new TimevacationUseTimeOfDaily(
 													new AttendanceTime(60),
-													new AttendanceTime(120), 
-													new AttendanceTime(180), 
-													new AttendanceTime(240), 
+													new AttendanceTime(120),
+													new AttendanceTime(180),
+													new AttendanceTime(240),
 													Optional.empty(),
-													new AttendanceTime(300), 
+													new AttendanceTime(300),
 													new AttendanceTime(360));
 
 		int actualResult = timeDaily.totalVacationAddTime();
@@ -154,13 +154,13 @@ public class TimevacationUseTimeOfDailyTest {
 		assertThat(actualResult).as("B").isEqualTo(1260);
 
 	}
-	
-	
+
+
 	private Optional<HolidayAddtionSet> createHASet(NotUseAtr annualHoliday,NotUseAtr specialHoliday){
 		return Optional.of(new HolidayAddtionSet(
-				null, null, NotUseAtr.NOT_USE, null, 
+				null, null,
 				new LeaveSetAdded(annualHoliday, null, specialHoliday),
 				null));
 	}
-	
+
 }
