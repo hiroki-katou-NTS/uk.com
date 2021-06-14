a43a3f1918637f284ef2e6ca8b9eef1b3046accf
m_kitahira <m_kitahira@nittsusystem.co.jp>
2021-06-14 15:27:47 +0900

    FQLドメイン制約のチェック処理をEnum側に移動

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/importableitem/CheckMethod.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/importableitem/CheckMethod.java
@@ -1,16 +1,44 @@
 package nts.uk.ctx.exio.dom.input.importableitem;
 
+import lombok.RequiredArgsConstructor;
+import nts.arc.enums.EnumAdaptor;
+import nts.arc.primitive.PrimitiveValue;
+
 /**
  * 受入値の検証方法
  */
+@RequiredArgsConstructor
 public enum CheckMethod {
-	PRIMITIVE_VALUE(1),
-	ENUM(2),
+	
+	PRIMITIVE_VALUE(1) {
+		@Override
+		public boolean validate(Class<?> pvClass, Object value) {
+			try {
+				PrimitiveValue<?> pv = (PrimitiveValue<?>) pvClass.getConstructors()[0].newInstance(value);
+				pv.validate();
+				return true;
+			} catch (Exception ex) {
+				//エラー時処理
+				throw new RuntimeException("PrimitiveValueの検証　仮置きエクスセプションです。", ex);
+			}
+		}
+	},
+	
+	ENUM(2) {
+		@Override
+		public boolean validate(Class<?> pvClass, Object value) {
+			try {
+				EnumAdaptor.valueOf((int)value, pvClass);
+				return true;
+			} catch (Exception ex) {
+				//エラー処理
+				throw new RuntimeException("Enumの検証　仮置きエクスセプションです。", ex);
+			}
+		}
+	},
 	;
 	
 	public final int value;
-
-	private CheckMethod(int value) {
-		this.value = value;
-	}
+	
+	public abstract boolean validate(Class<?> pvClass, Object value);
 }
--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/importableitem/DomainConstraint.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/importableitem/DomainConstraint.java
@@ -2,8 +2,7 @@ package nts.uk.ctx.exio.dom.input.importableitem;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import nts.arc.enums.EnumAdaptor;
-import nts.arc.primitive.PrimitiveValue;
+import lombok.SneakyThrows;
 
 /**
  *  値のドメイン制約
@@ -16,36 +15,11 @@ public class DomainConstraint {
 	private String fqn;
 
 	public boolean validate(Object value) {
-		switch(this.checkMethod) {
-			case PRIMITIVE_VALUE:
-				return validatePrimitiveValue(value);
-			case ENUM:
-				return validateEnum(value);
-			default:
-				throw new RuntimeException("チェック方法が定義されていません。:" + this.checkMethod);
-		}
+		return this.checkMethod.validate(getConstraintClass(), value);
 	}
 	
-	private boolean validatePrimitiveValue(Object value) {
-		try {
-			Class<?> pvClass = Class.forName(fqn);
-			PrimitiveValue<?> pv = (PrimitiveValue<?>) pvClass.getConstructors()[0].newInstance(value);
-			pv.validate();
-			return true;
-		} catch (Exception ex) {
-			//エラー時処理
-			throw new RuntimeException("PrimitiveValueの検証　仮置きエクスセプションです。", ex);
-		}
-	}
-	
-	private boolean validateEnum(Object value) {
-		try {
-			Class<?> pvClass = Class.forName(fqn);
-			EnumAdaptor.valueOf((int)value, pvClass);
-			return true;
-		} catch (Exception ex) {
-			//エラー処理
-			throw new RuntimeException("Enumの検証　仮置きエクスセプションです。", ex);
-		}
+	@SneakyThrows
+	public Class<?> getConstraintClass() {
+		return Class.forName(fqn);
 	}
 }
