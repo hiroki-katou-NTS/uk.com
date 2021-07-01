205c8a79e2e60626d1e6248967b9d4d3d4869aa8
Hiroki Katou <hiroki_katou@nittsusystem.co.jp>
2021-07-01 14:09:38 +0900

    インフラデプロイエラーの改善

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.infra/src/main/java/nts/uk/ctx/exio/infra/entity/input/revise/type/codeconvert/XimmtCodeConvert.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.infra/src/main/java/nts/uk/ctx/exio/infra/entity/input/revise/type/codeconvert/XimmtCodeConvert.java
@@ -1,20 +1,16 @@
 package nts.uk.ctx.exio.infra.entity.input.revise.type.codeconvert;
 
 import java.io.Serializable;
-import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinTable;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import nts.arc.layer.infra.data.jdbc.map.JpaEntityMapper;
 import nts.uk.shr.infra.data.entity.ContractUkJpaEntity;
 
 /**
@@ -40,10 +36,7 @@ public class XimmtCodeConvert extends ContractUkJpaEntity implements Serializabl
 	@Column(name = "IMPORT_WITHOUT_SETTING")
 	private int importWithoutSetting;
 	
-	@OneToMany(targetEntity = XimmtCodeConvertDetail.class, cascade = CascadeType.ALL, mappedBy = "XimmtCodeConvert"
-			+ "", orphanRemoval = true, fetch = FetchType.LAZY)
-	@JoinTable(name = "XIMMT_CODE_CONVERT_DETAIL")
-	public List<XimmtCodeConvertDetail> ximmtCodeConvertDetail;
+	public static final JpaEntityMapper<XimmtCodeConvert> MAPPER = new JpaEntityMapper<>(XimmtCodeConvert.class);
 	
 	@Override
 	protected Object getKey() {
--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.infra/src/main/java/nts/uk/ctx/exio/infra/repository/input/revise/type/codeconvert/JpaExternalImportCodeConvertRepository.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.infra/src/main/java/nts/uk/ctx/exio/infra/repository/input/revise/type/codeconvert/JpaExternalImportCodeConvertRepository.java
@@ -1,18 +1,20 @@
 package nts.uk.ctx.exio.infra.repository.input.revise.type.codeconvert;
 
+import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.ejb.Stateless;
 
 import org.apache.commons.lang3.BooleanUtils;
 
+import lombok.val;
 import nts.arc.layer.infra.data.JpaRepository;
 import nts.uk.ctx.exio.dom.input.revise.type.codeconvert.CodeConvertCode;
 import nts.uk.ctx.exio.dom.input.revise.type.codeconvert.CodeConvertDetail;
 import nts.uk.ctx.exio.dom.input.revise.type.codeconvert.ExternalImportCodeConvert;
 import nts.uk.ctx.exio.dom.input.revise.type.codeconvert.ExternalImportCodeConvertRepository;
 import nts.uk.ctx.exio.infra.entity.input.revise.type.codeconvert.XimmtCodeConvert;
+import nts.uk.ctx.exio.infra.entity.input.revise.type.codeconvert.XimmtCodeConvertDetail;
 
 @Stateless
 public class JpaExternalImportCodeConvertRepository extends JpaRepository implements ExternalImportCodeConvertRepository{
@@ -22,26 +24,39 @@ public class JpaExternalImportCodeConvertRepository extends JpaRepository implem
 		
 		String sql = " select f "
 				+ " from XimmtCodeConvert f"
-				+ " where f.pk.companyId =:cid AND  f.pk.code =:convertCd ";
+				+ " where f.pk.companyId =:cid "
+				+ " and f.pk.code =:convertCd ";
 		
-		return this.queryProxy().query(sql, XimmtCodeConvert.class)
+		val entities = this.queryProxy().query(sql, XimmtCodeConvert.class)
 				.setParameter("cid", cid)
 				.setParameter("convertCd", convertCd.toString())
-				.getSingle(c -> toDomain(c));
+				.getSingle();
+		if(entities.isPresent()) {
+			return Optional.empty();
+		}
+		val details = getDetails(cid, convertCd);
+		return Optional.of(toDomain(entities.get(), details));
 	}
 	
-	private ExternalImportCodeConvert toDomain(XimmtCodeConvert entity) {
+	private List<CodeConvertDetail> getDetails(String cid, CodeConvertCode convertCd){
+		
+		String sql = " select f "
+				+ " from XimmtCodeConvertDetail f"
+				+ " where f.pk.companyId =:cid "
+				+ " and f.pk.convertCode =:convertCd ";
+		
+		return this.queryProxy().query(sql, XimmtCodeConvertDetail.class)
+				.setParameter("cid", cid)
+				.setParameter("convertCd", convertCd.toString())
+				.getList(rec -> rec.toDomain());
+	}
+	
+	private ExternalImportCodeConvert toDomain(XimmtCodeConvert entity, List<CodeConvertDetail> details) {
 		return new ExternalImportCodeConvert(
 				entity.getPk().getCompanyId(),
 				entity.getPk().getCode(), 
 				entity.getName(), 
 				BooleanUtils.toBoolean(entity.getImportWithoutSetting()),
-				entity.ximmtCodeConvertDetail.stream().map(itemDetail -> {
-					return new CodeConvertDetail(
-							itemDetail.getPk().getCompanyId(),
-							itemDetail.getPk().getConvertCode(), 
-							itemDetail.getPk().getTargetCode(), 
-							itemDetail.getSystemCode());
-				}).collect(Collectors.toList()));
+				details);
 	}
 }
