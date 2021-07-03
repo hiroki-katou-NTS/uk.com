54dede412670e16da242b75d594f0f248cec0ec7
m_kitahira <m_kitahira@nittsusystem.co.jp>
2021-07-03 15:29:30 +0900

    bugfix

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.infra/src/main/java/nts/uk/ctx/exio/infra/repository/input/validation/JpaImportingUserConditionRepository.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.infra/src/main/java/nts/uk/ctx/exio/infra/repository/input/validation/JpaImportingUserConditionRepository.java
@@ -1,13 +1,11 @@
 package nts.uk.ctx.exio.infra.repository.input.validation;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.Stateless;
 
 import nts.arc.layer.infra.data.JpaRepository;
 import nts.arc.layer.infra.data.jdbc.NtsStatement;
-import nts.gul.collection.CollectionUtil;
 import nts.uk.ctx.exio.dom.input.validation.ImportingUserConditionRepository;
 import nts.uk.ctx.exio.dom.input.validation.condition.ImportingUserCondition;
 import nts.uk.ctx.exio.infra.entity.input.validation.XimmtValidValue;
@@ -17,18 +15,17 @@ public class JpaImportingUserConditionRepository extends JpaRepository implement
 	
 	@Override
 	public List<ImportingUserCondition> get(String companyId, String settingCode, List<Integer> itemNoList){
-		String sql = "select * "
-						+ "from XIMMT_VALID_VALUE "
-						+ "where CID = @cid "
-						+ "SETTING_CODE = @settingCode"
-						+ "ITEM_NO in @itemNos";
-		List<ImportingUserCondition> result = new ArrayList<ImportingUserCondition>();
-		CollectionUtil.split(itemNoList, 1000, subNoList -> {
-			result.addAll(new NtsStatement(sql, this.jdbcProxy())
+		String sql = "select * from XIMMT_VALID_VALUE"
+				+ " where CID = @cid"
+				+ " and SETTING_CODE = @settingCode"
+				+ " and ITEM_NO in @itemNos";
+		
+		return NtsStatement.In.split(itemNoList, subNoList -> {
+			return jdbcProxy().query(sql)
 				.paramString("cid", companyId)
 				.paramString("settingCode", settingCode)
-				.getList(rec -> XimmtValidValue.MAPPER.toEntity(rec).toDomain()));
+				.paramInt("itemNos", subNoList)
+				.getList(rec -> XimmtValidValue.MAPPER.toEntity(rec).toDomain());
 		});
-		return result;
 	}
 }
