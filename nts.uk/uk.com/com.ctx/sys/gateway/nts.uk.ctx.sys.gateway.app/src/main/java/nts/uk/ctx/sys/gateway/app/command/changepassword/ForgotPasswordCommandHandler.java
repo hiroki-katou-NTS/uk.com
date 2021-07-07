d36d6ac74ced02a9c35755a44065440cf9b7aafc
Hiroki Katou <hiroki_katou@nittsusystem.co.jp>
2021-07-07 16:48:50 +0900

    ・コマンドハンドラの修正 ・リポジトリの修正

--- nts.uk/uk.com/com.ctx/sys/gateway/nts.uk.ctx.sys.gateway.app/src/main/java/nts/uk/ctx/sys/gateway/app/command/changepassword/ForgotPasswordCommandHandler.java
+++ nts.uk/uk.com/com.ctx/sys/gateway/nts.uk.ctx.sys.gateway.app/src/main/java/nts/uk/ctx/sys/gateway/app/command/changepassword/ForgotPasswordCommandHandler.java
@@ -4,63 +4,44 @@ import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 
-import nts.arc.error.BundledBusinessException;
+import lombok.val;
 import nts.arc.layer.app.command.CommandHandler;
 import nts.arc.layer.app.command.CommandHandlerContext;
-import nts.gul.text.StringUtil;
-import nts.uk.ctx.sys.gateway.dom.adapter.user.CheckBeforeChangePass;
-import nts.uk.ctx.sys.gateway.dom.adapter.user.UserAdapter;
+import nts.arc.task.tran.TransactionService;
+import nts.uk.ctx.sys.gateway.dom.login.password.userpassword.ChangeLoginPasswordOfUser;
 import nts.uk.shr.com.url.RegisterEmbededURL;
 
 @Stateless
 @Transactional
 public class ForgotPasswordCommandHandler extends CommandHandler<ForgotPasswordCommand> {
 	
-	/** The user adapter. */
 	@Inject
-	private UserAdapter userAdapter;
+	private ForgotPasswordCommandRequire requireProvider;
+	
+	@Inject
+	private TransactionService transaction;
 	
 	@Inject
 	private RegisterEmbededURL registerEmbededURL;
-
+	
 	@Override
 	protected void handle(CommandHandlerContext<ForgotPasswordCommand> context) {
-
-		
 		ForgotPasswordCommand command = context.getCommand();
+		val require = requireProvider.createRequire();
 		
-		String userId = command.getUserId();
-		String newPassword = command.getNewPassword();
-		String confirmNewPassword = command.getConfirmNewPassword();
-		
-		//check PassLimitExpire
+		// 埋め込みURLが正しいか検証
 		this.registerEmbededURL.checkPassLimitExpire(command.getEmbeddedId());
 		
-		if (!StringUtil.isNullOrEmpty(newPassword, true)
-				&& !StringUtil.isNullOrEmpty(confirmNewPassword, true)) {
-			// Check password - Request List 445
-			CheckBeforeChangePass checkResult = this.userAdapter.checkBeforeResetPassword(userId, newPassword, confirmNewPassword);
-			if (checkResult.isError()) {
-				// Throw error list
-				BundledBusinessException bundledBusinessExceptions = BundledBusinessException.newInstance();
-				checkResult.getMessage().forEach(item -> {
-					// get messageId
-					String msgId = item.getMessage();
-					String param = item.getParam();
-					if (param != null) {
-						bundledBusinessExceptions.addMessage(msgId, param);
-					} else {
-						bundledBusinessExceptions.addMessage(msgId);
-					}
-
-				});
-				if (!bundledBusinessExceptions.cloneExceptions().isEmpty()) {
-					throw bundledBusinessExceptions;
-				}
-			} else {
-				// Update password - Request List 384				
-				this.userAdapter.updatePassword(userId,newPassword);
-			}	
-		}
+		val atomTask = ChangeLoginPasswordOfUser.change(
+				require, 
+				command.getUserId(), 
+				command.getNewPassword(), 
+				command.getConfirmNewPassword());
+		
+		transaction.execute(atomTask);
+	}
+	
+	public static interface Require extends ChangeLoginPasswordOfUser.Require {
+		
 	}
 }
