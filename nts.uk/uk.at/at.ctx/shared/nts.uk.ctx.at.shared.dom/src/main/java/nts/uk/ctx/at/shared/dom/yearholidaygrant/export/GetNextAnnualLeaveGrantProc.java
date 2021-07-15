881a613283a88e4cb68d838dd922ebcb7ba7c09d
masaakijinno <fairvancs@gmail.com>
2021-07-15 16:11:36 +0900

    Redmine #118140

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/yearholidaygrant/export/GetNextAnnualLeaveGrantProc.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/yearholidaygrant/export/GetNextAnnualLeaveGrantProc.java
@@ -8,6 +8,8 @@ import lombok.val;
 import nts.arc.layer.app.cache.CacheCarrier;
 import nts.arc.time.GeneralDate;
 import nts.uk.ctx.at.shared.dom.vacation.setting.annualpaidleave.AnnualPaidLeaveSetting;
+import nts.uk.ctx.at.shared.dom.workingcondition.LaborContractTime;
+import nts.uk.ctx.at.shared.dom.workingcondition.WorkingConditionItem;
 import nts.arc.time.calendar.period.DatePeriod;
 import nts.uk.ctx.at.shared.dom.yearholidaygrant.GrantHdTbl;
 import nts.uk.ctx.at.shared.dom.yearholidaygrant.GrantHdTblSet;
@@ -27,6 +29,7 @@ public class GetNextAnnualLeaveGrantProc {
 	/**
 	 * 次回年休付与を取得する
 	 * @param companyId 会社ID
+	 * @param employeeId 社員ID
 	 * @param grantTableCode 年休付与テーブル設定コード
 	 * @param entryDate 入社年月日
 	 * @param criteriaDate 年休付与基準日
@@ -37,13 +40,14 @@ public class GetNextAnnualLeaveGrantProc {
 	public List<NextAnnualLeaveGrant> algorithm(
 			RequireM1 require, CacheCarrier cacheCarrier,
 			String companyId,
+			String employeeId,
 			String grantTableCode,
 			GeneralDate entryDate,
 			GeneralDate criteriaDate,
 			DatePeriod period,
 			boolean isSingleDay){
 
-		return algorithm(require, cacheCarrier, companyId, grantTableCode,
+		return algorithm(require, cacheCarrier, companyId, employeeId, grantTableCode,
 				entryDate, criteriaDate, period, isSingleDay,
 				Optional.empty(), Optional.empty(), Optional.empty());
 	}
@@ -51,6 +55,7 @@ public class GetNextAnnualLeaveGrantProc {
 	/**
 	 * 次回年休付与を取得する （※付与年月日、期限日をセットするだけに変更）
 	 * @param companyId 会社ID
+	 * @param employeeId 社員ID
 	 * @param grantTableCode 年休付与テーブル設定コード
 	 * @param entryDate 入社年月日
 	 * @param criteriaDate 年休付与基準日
@@ -59,11 +64,10 @@ public class GetNextAnnualLeaveGrantProc {
 	 * @param grantHdTblSetParam 年休付与テーブル設定
 	 * @param lengthServiceTblsParam 勤続年数テーブルリスト
 	 * @param closureStartDate 締め開始日
-	 * @param contractTime 契約時間
 	 * @return 次回年休付与リスト
 	 */
 	public static List<NextAnnualLeaveGrant> algorithm(RequireM1 require, CacheCarrier cacheCarrier,
-			String companyId, String grantTableCode, GeneralDate entryDate, GeneralDate criteriaDate,
+			String companyId, String employeeId, String grantTableCode, GeneralDate entryDate, GeneralDate criteriaDate,
 			DatePeriod period, boolean isSingleDay, Optional<GrantHdTblSet> grantHdTblSetParam,
 			Optional<List<LengthServiceTbl>> lengthServiceTblsParam, Optional<GeneralDate> closureStartDate
 			){
@@ -152,6 +156,17 @@ public class GetNextAnnualLeaveGrantProc {
 					period, isSingleDay, nextAnnualLeaveGrantList);
 //		}
 
+		// アルゴリズム「社員の労働条件を取得する」を実行し、契約時間を取得する
+		Optional<WorkingConditionItem> workCond
+			= require.workingConditionItem(employeeId, criteriaDate);
+
+		// 契約時間を取得する
+		int contractTimeTmp = 0;
+		if (workCond.isPresent()) {
+			contractTimeTmp = workCond.get().getContractTime().v();
+		}
+
+		final int contractTime = contractTimeTmp;
 		for (val nextAnnualLeaveGrant : nextAnnualLeaveGrantList){
 
 			// 付与回数をもとに年休付与テーブルを取得
@@ -167,7 +182,7 @@ public class GetNextAnnualLeaveGrantProc {
 
 			// 要修正　契約時間を取得する
 			nextAnnualLeaveGrant.setTimeAnnualLeaveMaxTime(
-					grantHdTbl.getLimitTimeHd().map(c->new LimitedTimeHdTime(c.v() * 480)));
+					grantHdTbl.getLimitTimeHd().map(c->new LimitedTimeHdTime(c.v() * contractTime)));
 		}
 
 		// 期間がNULLであった場合は取得した付与年月日の最初の1件にする
@@ -214,5 +229,9 @@ public class GetNextAnnualLeaveGrantProc {
 		Optional<GrantHdTbl> grantHdTbl(String companyId, int conditionNo, String yearHolidayCode, int grantNum);
 
 		AnnualPaidLeaveSetting annualPaidLeaveSetting(String companyId);
+
+		// 社員の労働条件を取得する
+		Optional<WorkingConditionItem> workingConditionItem(String employeeId, GeneralDate criteriaDate);
+
 	}
 }
