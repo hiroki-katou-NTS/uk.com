3b3b6f5877827591e85160252431d070e0498785
TranXuanViet <viet.tx@3si.vn>
2021-07-15 10:14:35 +0700

    Edit param

--- nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/kha003/exportcsv/ManHourDataSummaryQuery.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/kha003/exportcsv/ManHourDataSummaryQuery.java
@@ -4,16 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import nts.uk.ctx.at.record.dom.workrecord.workmanagement.manhoursummarytable.ManHourSummaryTableFormat;
 import nts.uk.ctx.at.record.dom.workrecord.workmanagement.manhoursummarytable.ManHourSummaryTableOutputContent;
+import nts.uk.screen.at.app.kha003.ManHourSummaryTableFormatDto;
 import nts.uk.screen.at.app.kha003.b.ManHourPeriod;
 
 @AllArgsConstructor
 @Getter
 public class ManHourDataSummaryQuery {
     /** 工数集計表フォーマット */
-    private ManHourSummaryTableFormat summaryTableFormat;
+    private ManHourSummaryTableFormatDto summaryTableFormat;
 
     /** 工数集計表出力内容 */
-    private ManHourSummaryTableOutputContent outputContent;
+    private ManHourSummaryTableOutputContentDto outputContent;
 
     private ManHourPeriod period;
 }
