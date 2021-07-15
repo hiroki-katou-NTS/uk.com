37b6e2f1991949997533c75553bbe41d961528bc
m_kitahira <m_kitahira@nittsusystem.co.jp>
2021-07-15 17:22:15 +0900

    警告除去

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/csvimport/ExternalImportCsvFileInfo.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/csvimport/ExternalImportCsvFileInfo.java
@@ -4,7 +4,6 @@ import static java.util.stream.Collectors.*;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.function.Consumer;
