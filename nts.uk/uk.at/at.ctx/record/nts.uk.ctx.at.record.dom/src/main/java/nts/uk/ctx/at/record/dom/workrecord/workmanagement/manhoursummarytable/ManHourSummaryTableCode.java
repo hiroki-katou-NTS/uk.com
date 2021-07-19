2df05a9cd37a75b0f619cf1de4a8975ae21603fe
3SI\hung.tt <hung.tt@3si.vn>
2021-07-19 11:43:12 +0700

    kha003 fix bug A4_1

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/workrecord/workmanagement/manhoursummarytable/ManHourSummaryTableCode.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/workrecord/workmanagement/manhoursummarytable/ManHourSummaryTableCode.java
@@ -1,12 +1,15 @@
 package nts.uk.ctx.at.record.dom.workrecord.workmanagement.manhoursummarytable;
 
 import nts.arc.primitive.StringPrimitiveValue;
+import nts.arc.primitive.constraint.CharType;
+import nts.arc.primitive.constraint.StringCharType;
 import nts.arc.primitive.constraint.StringMaxLength;
 
 /**
  * 工数集計表コード
  */
 @StringMaxLength(3)
+@StringCharType(CharType.ALPHA_NUMERIC)
 public class ManHourSummaryTableCode extends StringPrimitiveValue<ManHourSummaryTableCode> {
     private static final long serialVersionUID = 1L;
 
