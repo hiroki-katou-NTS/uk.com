c8fd9c695cbfe78f1de41fbc3a9cd1765fa5495c
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2021-07-20 10:49:24 +0700

    Update command mobile

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/kdp/kdp001/a/RefectActualResultCommand.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/kdp/kdp001/a/RefectActualResultCommand.java
@@ -5,10 +5,13 @@ import java.util.Optional;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import nts.uk.ctx.at.record.app.command.stamp.card.stampcard.management.personalengraving.WorkGroupCommand;
 import nts.uk.ctx.at.record.dom.workrecord.stampmanagement.stamp.RefectActualResult;
 import nts.uk.ctx.at.record.dom.workrecord.stampmanagement.stamp.WorkInformationStamp;
 import nts.uk.ctx.at.record.dom.workrecord.stampmanagement.support.SupportCardNumber;
 import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.common.timestamp.WorkLocationCD;
+import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.timesheet.ouen.work.WorkCode;
+import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.timesheet.ouen.work.WorkGroup;
 import nts.uk.ctx.at.shared.dom.worktime.common.WorkTimeCode;
 
 /**
@@ -24,7 +27,7 @@ public class RefectActualResultCommand {
 
 	@Getter
 	private String workPlaceId;
-	
+
 	/**
 	 * 打刻場所コード 勤務場所コード old
 	 */
@@ -42,17 +45,35 @@ public class RefectActualResultCommand {
 	 */
 	@Getter
 	private OvertimeDeclarationComamnd overtimeDeclaration;
-	
+
+	/**
+	 * 	作業グループ
+	 */
+	@Getter
+	private WorkGroupCommand workGroup;
 
 	public RefectActualResult toDomainValue() {
+
+		WorkGroup workGroupResult = null;
+
+		if (workGroup != null) {
+			if (workGroup.getWorkCode1() != null) {
+				workGroupResult = new WorkGroup(new WorkCode(workGroup.getWorkCode1()),
+						Optional.ofNullable(new WorkCode(workGroup.getWorkCode2())),
+						Optional.ofNullable(new WorkCode(workGroup.getWorkCode3())),
+						Optional.ofNullable(new WorkCode(workGroup.getWorkCode4())),
+						Optional.ofNullable(new WorkCode(workGroup.getWorkCode5())));
+			}
+		}
+
 		WorkInformationStamp workInformationStamp = new WorkInformationStamp(
-				this.workPlaceId == null ? Optional.empty() : Optional.of(this.workPlaceId), 
-				Optional.empty(),
-				this.workLocationCD    == null ? Optional.empty() : Optional.of(new WorkLocationCD(this.workLocationCD)),
-				this.cardNumberSupport == null ? Optional.empty() : Optional.of(new SupportCardNumber(Integer.valueOf(this.cardNumberSupport))));	
+				this.workPlaceId == null ? Optional.empty() : Optional.of(this.workPlaceId), Optional.empty(),
+				this.workLocationCD == null ? Optional.empty() : Optional.of(new WorkLocationCD(this.workLocationCD)),
+				this.cardNumberSupport == null ? Optional.empty()
+						: Optional.of(new SupportCardNumber(Integer.valueOf(this.cardNumberSupport))));
 		return new RefectActualResult(workInformationStamp,
 				workTimeCode != null ? new WorkTimeCode(workTimeCode) : null,
 				overtimeDeclaration != null ? overtimeDeclaration.toDomainValue() : null,
-				null);
+				workGroupResult);
 	}
 }
