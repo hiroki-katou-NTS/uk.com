5ef13348524350b1a46e372a44b0953343a4ea91
NSVN-TruongVanLai <NSVN-TruongVanLai@users.noreply.github.com>
2021-07-20 14:13:37 +0700

    Kojin_Johou1_Domain : UnitTest EmployeeUnitPriceHistory,EmployeeUnitPriceHistoryItem

---
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/test/java/nts/uk/ctx/at/shared/dom/dailyattdcal/empunitpricehistory/EmployeeUnitPriceHistoryItemTest.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package nts.uk.ctx.at.shared.dom.dailyattdcal.empunitpricehistory;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+
+import org.junit.Test;
+
+import nts.arc.testing.assertion.NtsAssert;
+
+/**
+ * @author laitv
+ *
+ */
+public class EmployeeUnitPriceHistoryItemTest {
+	
+	@Test
+	public void getters() {
+		EmployeeUnitPriceHistoryItem empUnitPriceHisItem = EmployeeUnitPriceHistoryHelper.getEmployeeUnitPriceHistoryItemDefault();
+		NtsAssert.invokeGetters(empUnitPriceHisItem);
+	}
+	
+	/**
+	 * Test func 社員時間単価を取得する() | getEmployeeHourlyUnitPrice()
+	 * case1 
+	 */
+	@Test
+	public void testgetEmployeeHourlyUnitPrice1() {
+		EmployeeUnitPriceHistoryItem empUnitPriceHisItem = EmployeeUnitPriceHistoryHelper.getEmployeeUnitPriceHistoryItemDefault();
+		
+		Optional<WorkingHoursUnitPrice> result = empUnitPriceHisItem.getEmployeeHourlyUnitPrice(UnitPrice.Price_2.value);
+		
+		assertThat(result.get().v()).isEqualTo(200);
+	}
+	
+	/**
+	 * Test func 社員時間単価を取得する() | getEmployeeHourlyUnitPrice()
+	 * case1 
+	 */
+	@Test
+	public void testgetEmployeeHourlyUnitPrice2() {
+		EmployeeUnitPriceHistoryItem empUnitPriceHisItem = EmployeeUnitPriceHistoryHelper.getEmployeeUnitPriceHistoryItemDefault();
+		
+		Optional<WorkingHoursUnitPrice> result = empUnitPriceHisItem.getEmployeeHourlyUnitPrice(20);
+		
+		assertThat(result).isEqualTo(Optional.empty());
+	}
+}
