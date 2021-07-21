1c432098e217a997632990c6c5174dcb7065c7a9
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2021-07-21 10:00:57 +0700

    Update UT VerifyEmployeeCodeAndPasswordTest

--- nts.uk/uk.com/com.ctx/sys/gateway/nts.uk.ctx.sys.gateway.dom/src/test/java/nts/uk/ctx/sys/gateway/dom/login/password/InspectionResultTest.java
+++ nts.uk/uk.com/com.ctx/sys/gateway/nts.uk.ctx.sys.gateway.dom/src/test/java/nts/uk/ctx/sys/gateway/dom/login/password/InspectionResultTest.java
@@ -4,7 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
 
-import nts.uk.ctx.sys.shared.dom.employee.GetAnEmployeeImported;
+import nts.uk.ctx.sys.gateway.dom.login.password.authenticate.getaneemployee.GetAnEmployeeImported;
 
 /**
  * 
--- nts.uk/uk.com/com.ctx/sys/gateway/nts.uk.ctx.sys.gateway.dom/src/test/java/nts/uk/ctx/sys/gateway/dom/login/password/VerifyEmployeeCodeAndPasswordTest.java
+++ nts.uk/uk.com/com.ctx/sys/gateway/nts.uk.ctx.sys.gateway.dom/src/test/java/nts/uk/ctx/sys/gateway/dom/login/password/VerifyEmployeeCodeAndPasswordTest.java
@@ -11,7 +11,8 @@ import mockit.Expectations;
 import mockit.Injectable;
 import mockit.integration.junit4.JMockit;
 import nts.arc.time.GeneralDate;
-import nts.uk.ctx.sys.shared.dom.employee.GetAnEmployeeImported;
+import nts.uk.ctx.sys.gateway.dom.login.password.authenticate.getaneemployee.GetAnEmployeeImported;
+import nts.uk.ctx.sys.gateway.dom.login.password.userpassword.LoginPasswordOfUser;
 import nts.uk.ctx.sys.shared.dom.user.User;
 
 /**
@@ -28,10 +29,11 @@ public class VerifyEmployeeCodeAndPasswordTest {
 
 	private String cid = "Dummy";
 	private String employeeCode = "Dummy";
-	private Optional<String> password = Optional.of("Dummy");
-	private Optional<String> passwordDone = Optional.of("qwerty");
-	private User user = User.createFromJavatype("Dummy", true, "qwerty", "Dummy", "Dummy", GeneralDate.today(), 1, 1,
-			"Dummy", "Dummy", "Dummy", 1);
+	private String password = "Dummy";
+	private String passwordDone = "password";
+	private User user = User.createFromJavatype("Dummy", true, "password", "qwerty", GeneralDate.today(), 1, 1, "Dummy",
+			"Dummy", "Dummy");
+	private LoginPasswordOfUser loginPasswordOfUser = LoginPasswordOfUser.initialPassword("Dummy", "password");
 
 	// if $社員.isEmpty
 	// return 検証結果#ユーザ検証失敗()
@@ -92,8 +94,8 @@ public class VerifyEmployeeCodeAndPasswordTest {
 		assertThat(result.getEmployeeInformation().isPresent()).isFalse();
 		assertThat(result.getVerificationFailureMessage().get()).isEqualTo("Msg_302");
 	}
-
-	// return 検証結果#検証成功($社員)
+	
+	// if not $ログインパスワード .現在のパスワードを照合する(パスワード)
 	@Test
 	public void test3() {
 		GetAnEmployeeImported employee = new GetAnEmployeeImported("Dummy", "Dummy", "Dummy", "Dummy"); // dummy
@@ -105,6 +107,33 @@ public class VerifyEmployeeCodeAndPasswordTest {
 
 				require.getByAssociatedPersonId("Dummy");
 				result = Optional.of(user);
+				
+				require.find("Dummy");
+				result = Optional.of(loginPasswordOfUser);
+			}
+		};
+
+		InspectionResult result = VerifyEmployeeCodeAndPassword.verify(require, cid, employeeCode, password);
+		assertThat(result.isVerificationSuccess()).isEqualTo(false);
+		assertThat(result.getEmployeeInformation().isPresent()).isFalse();
+		assertThat(result.getVerificationFailureMessage().get()).isEqualTo("Msg_302");
+	}
+
+	// return 検証結果#検証成功($社員)
+	@Test
+	public void test4() {
+		GetAnEmployeeImported employee = new GetAnEmployeeImported("Dummy", "Dummy", "Dummy", "Dummy"); // dummy
+
+		new Expectations() {
+			{
+				require.getEmployee(cid, employeeCode);
+				result = Optional.of(employee);
+
+				require.getByAssociatedPersonId("Dummy");
+				result = Optional.of(user);
+				
+				require.find("Dummy");
+				result = Optional.of(loginPasswordOfUser);
 			}
 		};
 
