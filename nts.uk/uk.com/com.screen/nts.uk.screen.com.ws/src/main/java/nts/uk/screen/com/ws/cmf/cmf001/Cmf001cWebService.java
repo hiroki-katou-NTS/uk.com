b5a8e9a6e6e9a41564a232ffd2ab217846ee9a6f
m_kitahira <m_kitahira@nittsusystem.co.jp>
2021-07-26 11:57:53 +0900

    Cmf001cWebServiceのガワ

---
+++ nts.uk/uk.com/com.screen/nts.uk.screen.com.ws/src/main/java/nts/uk/screen/com/ws/cmf/cmf001/Cmf001cWebService.java
@@ -0,0 +1,12 @@
+package nts.uk.screen.com.ws.cmf.cmf001;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("exio/input/setting")
+@Produces(MediaType.APPLICATION_JSON)
+public class Cmf001cWebService {
+
+	
+}
