5910001d56ad90452ade92f055222d6543127e5f
Hiroki Katou <hiroki_katou@nittsusystem.co.jp>
2021-07-26 10:08:45 +0900

    B画面のラフ実装

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmf/001/b/cmf001.b.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmf/001/b/cmf001.b.vm.ts
@@ -3,34 +3,120 @@ module nts.uk.com.view.cmf001.b {
 	
 	@bean()
 	class ViewModel extends ko.ViewModel {
-    dateValue: KnockoutObservable<ImportInfo> = ko.observable(
-			new ImportInfo({
-      code: '001',
-      name: 'NAME',
-      group: 1,
-      mode: 1,
-      itemNameRow: 1,
-      importStartRow: 2
-    }));
+
+		settingInfo: KnockoutObservable<SettingInfo> = ko.observable();
+		
+		importGroup: KnockoutObservableArray<any> = ko.observableArray([]);
+		importMode: KnockoutObservableArray<any> = ko.observableArray([]);
+		
+		selectedSetting: KnockoutObservable<string> = ko.observable("001");
+		selectedGroup: KnockoutObservable<number> = ko.observable(1);
+		selectedItem: KnockoutObservable<number> = ko.observable();
+		
+		settingList: KnockoutObservableArray<Setting> = ko.observableArray([]);  
+		layoutList: KnockoutObservableArray<Layout> = ko.observableArray([]);
+		
+		settingListColumns: KnockoutObservableArray<any> = ko.observableArray([
+			{ headerText: "コード", 				key: "code", 					width: 50 	},
+			{ headerText: "名称", 					key: "name", 					width: 200 	},
+			{ headerText: "", 							key: "configured", 		width: 25 	},
+		]);
+		layoutListColumns: KnockoutObservableArray<any> = ko.observableArray([
+			{ headerText: "NO", 						key: "itemNo", 				width: 100 , hidden: true },
+			{ headerText: "名称", 					key: "name", 					width: 200 		},
+			{ headerText: "型", 						key: "type", 					width: 75 		},
+			{ headerText: "受入元", 				key: "source", 				width: 50 		},
+			{ headerText: "詳細設定の有無", key: "alreadyDetail", width: 120 		},
+		]);
+
 		constructor(data: any) {
 			super();
-      const vm = this;
+			var self = this;
+			self.settingList.push(new Setting("001", "会社の受入", true));
+			self.settingList.push(new Setting("002", "社員の受入", true));
+			self.settingList.push(new Setting("003", "所属の受入", false));
+			self.settingList.push(new Setting("004", "勤務の受入", false));
+
+			self.settingInfo = ko.observable(new SettingInfo(ko.observable("001"), ko.observable("会社の受入"), ko.observable("1"), ko.observable("1"), ko.observable(1), ko.observable(2)));
+
+			self.importGroup.push(new comboBoxItem("1", "会社情報"));
+			self.importGroup.push(new comboBoxItem("2", "社員情報"));
+			self.importGroup.push(new comboBoxItem("3", "所属情報"));
+			self.importGroup.push(new comboBoxItem("4", "勤務情報"));
+
+			self.importMode.push(new comboBoxItem("1", "おまかせモード"));
+			self.importMode.push(new comboBoxItem("2", "既存データが存在しないデータのみ受け入れる"));
+			self.importMode.push(new comboBoxItem("3", "既存データが存在するデータのみ受け入れる"));
+			self.importMode.push(new comboBoxItem("4", "受入対象レコードのみ削除して受け入れる"));
+			self.importMode.push(new comboBoxItem("5", "受入対象グループのデータをすべて削除して受け入れる"));
+			
+			self.layoutList.push(new Layout(1, "会社コード", "数値", "CSV", true));
+			self.layoutList.push(new Layout(2, "会社名称", "文字型", "CSV", true));
+			self.layoutList.push(new Layout(3, "会社所在地", "文字型", "CSV", true));
+			self.layoutList.push(new Layout(4, "会社創業日", "日付型", "CSV", true));
+			self.layoutList.push(new Layout(5, "会社責任者", "文字型", "CSV", true));
+			self.layoutList.push(new Layout(6, "会社の存在意義", "文字型", "CSV", true));
+			self.layoutList.push(new Layout(7, "ブラック会社区分", "文字型", "固定値", false));
 		}
 
 		mounted() {
 			const vm = this;
 		}
-	}
-	class ImportInfo {
-    code: string;
-    name: string;
-    group: number;
-    mode: number;
-    itemNameRow: number;
-    importStartRow: number;
+	}	
+	
+	class Setting {
+		code: string;
+		name: string;
+		configured: string;
 
-		constructor(init?: Partial<ImportInfo>) {
-      $.extend(this, init);
-    }
-  }
+		constructor(code: string, name: string, configured: boolean) {
+				this.code = code;
+				this.name = name;
+				this.configured = configured ? "✓" : "";
+		}
+	}
+
+	class SettingInfo {
+		code: KnockoutObservable<string>;
+		name: KnockoutObservable<string>;
+		group: KnockoutObservable<string>;
+		mode: KnockoutObservable<string>;
+		itemNameRow: KnockoutObservable<number>;
+		importStartRow: KnockoutObservable<number>;
+
+		constructor(code: KnockoutObservable<string>,　name: KnockoutObservable<string>, group: KnockoutObservable<string>, mode: KnockoutObservable<string>, itemNameRow: KnockoutObservable<number>, importStartRow: KnockoutObservable<number>) {
+			this.code = code;
+			this.name = name;
+			this.group = group;
+			this.mode = mode;
+			this.itemNameRow = itemNameRow;
+			this.importStartRow = importStartRow;
+		}
+	}
+
+	class comboBoxItem {
+		code: string;
+		name: string;
+
+		constructor(code: string, name: string) {
+				this.code = code;
+				this.name = name;
+		}
+	}
+	
+	class Layout {
+		itemNo: number;
+		name: string;
+		type: string;
+		source: string;
+		alreadyDetail: string;
+
+		constructor(itemNo: number,　name: string, type: string, source: string, alreadyDetail: boolean) {
+			this.itemNo = itemNo;
+			this.name = name;
+			this.type = type;
+			this.source = source;
+			this.alreadyDetail = alreadyDetail ? "あり" : "なし";
+		}
+	}
 }
