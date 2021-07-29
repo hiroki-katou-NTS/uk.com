0a2efa1b7256d18c4b9449eda05bb77a9a98df36
NSVN-TruongVanLai <NSVN-TruongVanLai@users.noreply.github.com>
2021-07-29 11:18:20 +0700

    update UnitTest

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/test/java/nts/uk/ctx/at/record/dom/workmanagement/workinitselectset/TaskInitialSelHistHelper.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/test/java/nts/uk/ctx/at/record/dom/workmanagement/workinitselectset/TaskInitialSelHistHelper.java
@@ -29,15 +29,14 @@ public class TaskInitialSelHistHelper {
 		List<TaskInitialSel> lst = new ArrayList<>();
 		lst.add(item1);
 		lst.add(item2);
-		TaskInitialSelHist data = new TaskInitialSelHist(
+		TaskInitialSelHist taskInitialSelHist = new TaskInitialSelHist(
 				"000000000001",
 				lst);
-		return data;
+		return taskInitialSelHist;
 	}
 	
-	
-	public static TaskInitialSel getTaskInitialSelDefault(){
-		TaskInitialSel item = new TaskInitialSel("000000000001", new DatePeriod( GeneralDate.ymd(2021, 8, 01),  GeneralDate.ymd(9999, 12,31)), 
+	public static TaskInitialSel getParamTestaddHistory(){
+		return  new TaskInitialSel("000000000001", new DatePeriod( GeneralDate.ymd(2021, 7,15),  GeneralDate.ymd(9999, 12,31)), 
 				new TaskItem(
 						Optional.of(new TaskCode("TaskCode1") ),
 						Optional.of(new TaskCode("TaskCode2") ),
@@ -45,22 +44,26 @@ public class TaskInitialSelHistHelper {
 						Optional.of(new TaskCode("TaskCode4") ),
 						Optional.of(new TaskCode("TaskCode5") )
 						));
-		return item;
 	}
 	
-	public static TaskItem getTaskItemDefault(){
-		TaskItem taskItem = new TaskItem(
-				Optional.of(new TaskCode("TaskCode1") ),
-				Optional.of(new TaskCode("TaskCode2") ),
-				Optional.of(new TaskCode("TaskCode3") ),
-				Optional.of(new TaskCode("TaskCode4") ),
-				Optional.of(new TaskCode("TaskCode5") )
-				);
-		return taskItem;
-	} 
+	public static TaskInitialSel getParamTestdeleteHistory(){
+		return  new TaskInitialSel("000000000001", new 
+				DatePeriod( GeneralDate.ymd(2021, 07, 01),  GeneralDate.ymd(9999, 12, 31)), 
+				new TaskItem(
+						Optional.of(new TaskCode("TaskCode1") ),
+						Optional.of(new TaskCode("TaskCode2") ),
+						Optional.of(new TaskCode("TaskCode3") ),
+						Optional.of(new TaskCode("TaskCode4") ),
+						Optional.of(new TaskCode("TaskCode5") )
+						));
+	}
 	
-	public static DatePeriod getDatePeriodDefault(){
-		DatePeriod period = new DatePeriod(GeneralDate.ymd(2021, 04, 01), GeneralDate.ymd(2021, 05, 31));
-		return period;
- 	}
+	public static TaskItem getParamTaskItemChangeHistory(){
+		return  new TaskItem(
+				Optional.of(new TaskCode("TaskCode6")),
+				Optional.of(new TaskCode("TaskCode7")),
+				Optional.of(new TaskCode("TaskCode8")),
+				Optional.of(new TaskCode("TaskCode9")),
+				Optional.of(new TaskCode("TaskCode10")));
+	}
 }
--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/test/java/nts/uk/ctx/at/record/dom/workmanagement/workinitselectset/TaskInitialSelHistTest.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/test/java/nts/uk/ctx/at/record/dom/workmanagement/workinitselectset/TaskInitialSelHistTest.java
@@ -49,43 +49,17 @@ public class TaskInitialSelHistTest {
 	@Test
 	public void testaddHistory2() {
 		
-		TaskInitialSel item1 = new TaskInitialSel("000000000001", new DatePeriod( GeneralDate.ymd(2021, 06, 01),  GeneralDate.ymd(2021, 06, 30)), 
-				new TaskItem(
-						Optional.of(new TaskCode("TaskCode1") ),
-						Optional.of(new TaskCode("TaskCode2") ),
-						Optional.of(new TaskCode("TaskCode3") ),
-						Optional.of(new TaskCode("TaskCode4") ),
-						Optional.of(new TaskCode("TaskCode5") )
-						));
-		TaskInitialSel item2 = new TaskInitialSel("000000000001", new DatePeriod( GeneralDate.ymd(2021, 07, 01),  GeneralDate.ymd(9999, 12, 31)), 
-				new TaskItem(
-						Optional.of(new TaskCode("TaskCode1") ),
-						Optional.of(new TaskCode("TaskCode2") ),
-						Optional.of(new TaskCode("TaskCode3") ),
-						Optional.of(new TaskCode("TaskCode4") ),
-						Optional.of(new TaskCode("TaskCode5") )
-						));
-		List<TaskInitialSel> lst = new ArrayList<>();
-		lst.add(item1);
-		lst.add(item2);
-		TaskInitialSelHist taskInitialSelHist = new TaskInitialSelHist(
-				"000000000001",
-				lst);
-		TaskInitialSel param = new TaskInitialSel("000000000001", new DatePeriod( GeneralDate.ymd(2021, 7,15),  GeneralDate.ymd(9999, 12,31)), 
-				new TaskItem(
-						Optional.of(new TaskCode("TaskCode1") ),
-						Optional.of(new TaskCode("TaskCode2") ),
-						Optional.of(new TaskCode("TaskCode3") ),
-						Optional.of(new TaskCode("TaskCode4") ),
-						Optional.of(new TaskCode("TaskCode5") )
-						));
+		TaskInitialSelHist taskInitialSelHist = TaskInitialSelHistHelper.getTaskInitialSelHistDefault();
+		
+		TaskInitialSel param = TaskInitialSelHistHelper.getParamTestaddHistory();
+		
 		taskInitialSelHist.addHistory(param);
 		assertThat( taskInitialSelHist.getLstHistory() )
 		.extracting( 
 				d -> d.getDatePeriod()
 				)
 		.containsExactly(
-				 item1.getDatePeriod(),
+				taskInitialSelHist.getLstHistory().get(0).getDatePeriod(),
 				 param.getDatePeriod(),
 				 new DatePeriod(GeneralDate.ymd(2021, 07, 01), GeneralDate.ymd(2021, 7,14)));
 	}
@@ -95,142 +69,64 @@ public class TaskInitialSelHistTest {
 	 * case if $直前の履歴.isPresent = true
 	 */
 	@Test
-	public void deleteHistory1() {
-		TaskInitialSel item1 = new TaskInitialSel("000000000001", new DatePeriod( GeneralDate.ymd(2021, 06, 01),  GeneralDate.ymd(2021, 06, 30)), 
-				new TaskItem(
-						Optional.of(new TaskCode("TaskCode1") ),
-						Optional.of(new TaskCode("TaskCode2") ),
-						Optional.of(new TaskCode("TaskCode3") ),
-						Optional.of(new TaskCode("TaskCode4") ),
-						Optional.of(new TaskCode("TaskCode5") )
-						));
-		TaskInitialSel item2 = new TaskInitialSel("000000000001", new DatePeriod( GeneralDate.ymd(2021, 07, 01),  GeneralDate.ymd(9999, 12, 31)), 
-				new TaskItem(
-						Optional.of(new TaskCode("TaskCode1") ),
-						Optional.of(new TaskCode("TaskCode2") ),
-						Optional.of(new TaskCode("TaskCode3") ),
-						Optional.of(new TaskCode("TaskCode4") ),
-						Optional.of(new TaskCode("TaskCode5") )
-						));
-		List<TaskInitialSel> lst = new ArrayList<>();
-		lst.add(item1);
-		lst.add(item2);
-		TaskInitialSelHist taskInitialSelHist = new TaskInitialSelHist(
-				"000000000001",
-				lst);
-		TaskInitialSel param = new TaskInitialSel("000000000001", new 
-				DatePeriod( GeneralDate.ymd(2021, 07, 01),  GeneralDate.ymd(9999, 12, 31)), 
-				new TaskItem(
-						Optional.of(new TaskCode("TaskCode1") ),
-						Optional.of(new TaskCode("TaskCode2") ),
-						Optional.of(new TaskCode("TaskCode3") ),
-						Optional.of(new TaskCode("TaskCode4") ),
-						Optional.of(new TaskCode("TaskCode5") )
-						));
+	public void deleteHistory() {
 		
-		taskInitialSelHist.deleteHistory(param);	
+		TaskInitialSelHist taskInitialSelHist = TaskInitialSelHistHelper.getTaskInitialSelHistDefault();
+		
+		TaskInitialSel param = TaskInitialSelHistHelper.getParamTestdeleteHistory();
+		
+		taskInitialSelHist.deleteHistory(param);
+		
 		List<TaskInitialSel> lstHistoryResult = taskInitialSelHist.getLstHistory();
+		
 		assertThat(lstHistoryResult.get(0).getDatePeriod().start()).isEqualTo(GeneralDate.ymd(2021, 06, 01));
 		assertThat(lstHistoryResult.get(0).getDatePeriod().end()).isEqualTo(GeneralDate.ymd(9999, 12, 31));
-		System.out.println(taskInitialSelHist);
+
 	}
 	
 	/*
 	 * test Func changeHistory
-	 * case if 変更する履歴項目.期間 == 変更後の期間
+	 * case if 変更する履歴開始日 == 変更後の期間.開始日
 	 */
 	@Test
 	public void changeHistory1() {
-		TaskInitialSel item1 = new TaskInitialSel("000000000001", new DatePeriod( GeneralDate.ymd(2021, 06, 01),  GeneralDate.ymd(2021, 06, 30)), 
-				new TaskItem(
-						Optional.of(new TaskCode("TaskCode1")),
-						Optional.of(new TaskCode("TaskCode2")),
-						Optional.of(new TaskCode("TaskCode3")),
-						Optional.of(new TaskCode("TaskCode4")),
-						Optional.of(new TaskCode("TaskCode5"))
-						));
-		TaskInitialSel item2 = new TaskInitialSel("000000000001", new DatePeriod( GeneralDate.ymd(2021, 07, 01),  GeneralDate.ymd(9999, 12, 31)), 
-				new TaskItem(
-						Optional.of(new TaskCode("TaskCode1")),
-						Optional.of(new TaskCode("TaskCode2")),
-						Optional.of(new TaskCode("TaskCode3")),
-						Optional.of(new TaskCode("TaskCode4")),
-						Optional.of(new TaskCode("TaskCode5"))
-						));
-		List<TaskInitialSel> lst = new ArrayList<>();
-		lst.add(item1);
-		lst.add(item2);
-		TaskInitialSelHist taskInitialSelHist = new TaskInitialSelHist(
-				"000000000001",
-				lst);			
+
+		TaskInitialSelHist taskInitialSelHist = TaskInitialSelHistHelper.getTaskInitialSelHistDefault();			
 		
-		TaskInitialSel taskInitialSel = new TaskInitialSel("000000000001", new DatePeriod( GeneralDate.ymd(2021, 07, 01),  GeneralDate.ymd(9999, 12, 31)), 
-				new TaskItem(
-						Optional.of(new TaskCode("TaskCode1")),
-						Optional.of(new TaskCode("TaskCode2")),
-						Optional.of(new TaskCode("TaskCode3")),
-						Optional.of(new TaskCode("TaskCode4")),
-						Optional.of(new TaskCode("TaskCode5"))
-						));
-		DatePeriod period =  new DatePeriod( GeneralDate.ymd(2021, 07, 01),  GeneralDate.ymd(9999, 12, 31));
-		TaskItem taskItem = new TaskItem(
-				Optional.of(new TaskCode("TaskCode6")),
-				Optional.of(new TaskCode("TaskCode7")),
-				Optional.of(new TaskCode("TaskCode8")),
-				Optional.of(new TaskCode("TaskCode9")),
-				Optional.of(new TaskCode("TaskCode10"))
-				);
-		taskInitialSelHist.changeHistory(taskInitialSel, period, taskItem);
-		assertThat(taskInitialSelHist.getLstHistory().get(1).getTaskItem()).isEqualTo(taskItem);
+		TaskItem taskItemParam = TaskInitialSelHistHelper.getParamTaskItemChangeHistory();
+		
+		DatePeriod periodParam =  new DatePeriod( GeneralDate.ymd(2021, 07, 01),  GeneralDate.ymd(9999, 12, 31));
+		
+		GeneralDate dateParam = GeneralDate.ymd(2021, 07, 01);
+		
+		taskInitialSelHist.changeHistory(taskItemParam, periodParam, dateParam);
+		
+		assertThat(taskInitialSelHist.getLstHistory().get(1).getTaskItem()).isEqualToComparingFieldByField(taskItemParam);
 	}
 	
 	/*
 	 * test Func changeHistory
-	 * case if 変更する履歴項目.期間 != 変更後の期間
+	 * case if 変更する履歴開始日 <> 変更後の期間.開始日
 	 */
 	@Test
 	public void changeHistory2() {
-		TaskInitialSel item1 = new TaskInitialSel("000000000001", new DatePeriod( GeneralDate.ymd(2021, 06, 01),  GeneralDate.ymd(2021, 06, 30)), 
-				new TaskItem(
-						Optional.of(new TaskCode("TaskCode1")),
-						Optional.of(new TaskCode("TaskCode2")),
-						Optional.of(new TaskCode("TaskCode3")),
-						Optional.of(new TaskCode("TaskCode4")),
-						Optional.of(new TaskCode("TaskCode5"))
-						));
-		TaskInitialSel item2 = new TaskInitialSel("000000000001", new DatePeriod( GeneralDate.ymd(2021, 07, 01),  GeneralDate.ymd(9999, 12, 31)), 
-				new TaskItem(
-						Optional.of(new TaskCode("TaskCode1")),
-						Optional.of(new TaskCode("TaskCode2")),
-						Optional.of(new TaskCode("TaskCode3")),
-						Optional.of(new TaskCode("TaskCode4")),
-						Optional.of(new TaskCode("TaskCode5"))
-						));
-		List<TaskInitialSel> lst = new ArrayList<>();
-		lst.add(item1);
-		lst.add(item2);
-		TaskInitialSelHist taskInitialSelHist = new TaskInitialSelHist(
-				"000000000001",
-				lst);			
+		// gổm 2 khoảng lịch sủ là (2021-06-01, 2021-06-30), (2021-07-01, 9999-12-31)
+		TaskInitialSelHist taskInitialSelHist = TaskInitialSelHistHelper.getTaskInitialSelHistDefault();			
+		// TaskItem muốn thay đổi.
+		TaskItem taskItemParam = TaskInitialSelHistHelper.getParamTaskItemChangeHistory();
+		// DatePeriod muốn thay đổi.
+		DatePeriod periodParam =  new DatePeriod( GeneralDate.ymd(2021, 8, 01),  GeneralDate.ymd(9999, 12, 31));
+		// start date của khoảng lịch sủ muốn thay đổi (Trong trường hợp này là thằng lịch sử thử 2 trong list lịch sử của domian)
+		GeneralDate dateParam = GeneralDate.ymd(2021, 07, 1);
+
+		taskInitialSelHist.changeHistory(taskItemParam, periodParam, dateParam);
+		// update lại enđate của lịch sủ đầu tiên trong list lích sủ domain
+		assertThat(taskInitialSelHist.getLstHistory().get(0).getDatePeriod().start()).isEqualTo(GeneralDate.ymd(2021, 06, 01));
+		assertThat(taskInitialSelHist.getLstHistory().get(0).getDatePeriod().end()).isEqualTo(GeneralDate.ymd(2021, 07, 31));
 		
-		TaskInitialSel taskInitialSel = new TaskInitialSel("000000000001", new DatePeriod( GeneralDate.ymd(2021, 8, 01),  GeneralDate.ymd(9999, 12, 31)), 
-				new TaskItem(
-						Optional.of(new TaskCode("TaskCode1")),
-						Optional.of(new TaskCode("TaskCode2")),
-						Optional.of(new TaskCode("TaskCode3")),
-						Optional.of(new TaskCode("TaskCode4")),
-						Optional.of(new TaskCode("TaskCode5"))
-						));
-		DatePeriod period =  new DatePeriod( GeneralDate.ymd(2021, 9, 01),  GeneralDate.ymd(9999, 12, 31));
-		TaskItem taskItem = new TaskItem(
-				Optional.of(new TaskCode("TaskCode6")),
-				Optional.of(new TaskCode("TaskCode7")),
-				Optional.of(new TaskCode("TaskCode8")),
-				Optional.of(new TaskCode("TaskCode9")),
-				Optional.of(new TaskCode("TaskCode10"))
-				);
-		taskInitialSelHist.changeHistory(taskInitialSel, period, taskItem);
-		assertThat(taskInitialSelHist.getLstHistory().get(0).getDatePeriod()).isEqualTo(item1.getDatePeriod());
-		assertThat(taskInitialSelHist.getLstHistory().get(1).getDatePeriod()).isEqualTo(item2.getDatePeriod());
+		// update lại lịch sủ thứ 2 trong list lich sủ domain
+		assertThat(taskInitialSelHist.getLstHistory().get(1).getDatePeriod().start()).isEqualTo(GeneralDate.ymd(2021, 8, 01));
+		assertThat(taskInitialSelHist.getLstHistory().get(1).getDatePeriod().end()).isEqualTo(GeneralDate.ymd(9999, 12, 31));
+		assertThat(taskInitialSelHist.getLstHistory().get(1).getTaskItem()).isEqualToComparingFieldByField(taskItemParam);
 	}
 }
