47bd493aa0644981d64f73cb07deb281d2ce8556
m_kitahira <m_kitahira@nittsusystem.co.jp>
2021-08-02 10:28:14 +0900

    デプロイエラー解消

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.app/src/main/java/nts/uk/ctx/exio/app/input/command/setting/ExternalImportSettingAddCommandHandler.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.app/src/main/java/nts/uk/ctx/exio/app/input/command/setting/ExternalImportSettingAddCommandHandler.java
@@ -1,8 +1,14 @@
 package nts.uk.ctx.exio.app.input.command.setting;
 
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+
 import nts.arc.layer.app.command.CommandHandler;
 import nts.arc.layer.app.command.CommandHandlerContext;
 
+@Stateless
+@TransactionAttribute(TransactionAttributeType.SUPPORTS)
 public class ExternalImportSettingAddCommandHandler extends CommandHandler<ExternalImportSettingCommand>{
 
 	@Override
