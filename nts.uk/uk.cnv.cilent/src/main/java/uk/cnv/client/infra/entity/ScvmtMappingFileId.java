3ca5193ea18e1ff9527cbe7a4d1fa969f542bc45
ai_muto <ai_muto@nittsusystem.co.jp>
2021-08-03 11:41:02 +0900

    ファイルアップロード時のマッピングテーブルのレイアウト変更

--- nts.uk/uk.cnv.cilent/src/main/java/uk/cnv/client/infra/entity/ScvmtMappingFileId.java
+++ nts.uk/uk.cnv.cilent/src/main/java/uk/cnv/client/infra/entity/ScvmtMappingFileId.java
@@ -22,24 +22,16 @@ public class ScvmtMappingFileId implements InsertRequire, TruncateTableRequire {
 	private String fileType;
 
 	/** kojin_id **/
-	private int pid;
-
-	/** 会社CD **/
-	private int companyCode;
-
-	/** 社員CD **/
-	private String employeeCode;
+	private int kojin_id;
 
 	@Override
 	public PreparedStatement insert(Connection conn) throws SQLException {
-		String sql = "INSERT INTO SCVMT_MAPPING_FILE_ID (FILE_ID,FILE_TYPE,kojin_id,会社CD,社員CD)"
-				+ " VALUES(?,?,?,?,?)";
+		String sql = "INSERT INTO SCVMT_MAPPING_FILE_ID (FILE_ID,FILE_TYPE,kojin_id)"
+				+ " VALUES(?,?,?)";
 		val ps = conn.prepareStatement(sql);
 		ps.setString(1, this.fileId);
 		ps.setString(2, this.fileType);
-		ps.setInt(3, this.pid);
-		ps.setInt(4, this.companyCode);
-		ps.setString(5, this.employeeCode);
+		ps.setInt(3, this.kojin_id);
 
 		return ps;
 	}
