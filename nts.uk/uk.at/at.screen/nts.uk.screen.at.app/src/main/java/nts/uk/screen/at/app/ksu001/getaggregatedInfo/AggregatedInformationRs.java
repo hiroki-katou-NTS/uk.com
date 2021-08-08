971ee11697d16f357849c46a846ca8f002e37ac3
NSVN-TruongVanLai <NSVN-TruongVanLai@users.noreply.github.com>
2021-08-08 22:21:34 +0700

    get data A10

--- nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/ksu001/getaggregatedInfo/AggregatedInformationRs.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/ksu001/getaggregatedInfo/AggregatedInformationRs.java
@@ -4,11 +4,18 @@
 package nts.uk.screen.at.app.ksu001.getaggregatedInfo;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import nts.uk.ctx.at.shared.dom.schedule.basicschedule.WorkStyle;
+import nts.uk.ctx.at.shared.dom.workrule.shiftmaster.ShiftMaster;
+import nts.uk.screen.at.app.ksu001.getworkscheduleshift.ScheduleOfShiftDto;
+import nts.uk.screen.at.app.ksu001.processcommon.WorkScheduleWorkInforDto;
 import nts.uk.screen.at.app.ksu001.start.AggregatePersonalMapDto;
 import nts.uk.screen.at.app.ksu001.start.AggregateWorkplaceMapDto;
 import nts.uk.screen.at.app.ksu001.start.ExternalBudgetMapDtoList;
@@ -25,5 +32,9 @@ public class AggregatedInformationRs {
 	public List<ExternalBudgetMapDtoList> externalBudget = Collections.emptyList(); // Map<年月日, Map<外部予算実績項目, 外部予算実績値>>
 	public AggregatePersonalMapDto aggreratePersonal; // 個人計集計結果 ←集計内容によって情報が異なる
 	public AggregateWorkplaceMapDto aggrerateWorkplace; // ・職場計集計結果
+	
+	public List<WorkScheduleWorkInforDto> listWorkScheduleWorkInfor= Collections.emptyList();
+	public List<ScheduleOfShiftDto> listWorkScheduleShift= Collections.emptyList();
+	public Map<ShiftMaster,Optional<WorkStyle>> mapShiftMasterWithWorkStyle = new HashMap<ShiftMaster, Optional<WorkStyle>>();
 
 }
