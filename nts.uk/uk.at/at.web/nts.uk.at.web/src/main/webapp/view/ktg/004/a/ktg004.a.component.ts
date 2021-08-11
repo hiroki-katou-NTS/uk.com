62a28ce844f0b07ef34d7aab453f1760b3f9f814
NSVN-NguyenDinhHoang <61735854+NSVN-NguyenDinhHoang@users.noreply.github.com>
2021-08-11 08:22:53 +0700

    Fix layout KTG004

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ktg/004/a/ktg004.a.component.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/ktg/004/a/ktg004.a.component.ts
@@ -89,9 +89,11 @@ module nts.uk.ui.ktg004.a {
                         <tbody data-bind="foreach: { data: $component.specialHolidaysRemainings, as: 'row'}"> 
                             <tr class="row-show-ktg004">
                                 <td>
-                                    <div data-bind="ntsFormLabel: { required: false, text: row.name }"></div>
+									<div style="display: flex">
+	                                    <div style="position: relative; width: 50%" data-bind=" ntsFormLabel: { required: false, text: row.name }"></div>
+		                                <div style="width: 50%; margin-top: 5px " class="text-right" data-bind="i18n: row.specialResidualNumber"></div>									
+									</div>
                                 </td>
-                                <td class="text-right" data-bind="i18n: row.specialResidualNumber"></td>
                             </tr>
                         </tbody>
                     </table>	
