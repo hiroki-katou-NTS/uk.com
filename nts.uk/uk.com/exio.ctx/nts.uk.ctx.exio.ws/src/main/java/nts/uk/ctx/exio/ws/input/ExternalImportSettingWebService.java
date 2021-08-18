23c0794daa8850958488852d71ec33233b10f150
m_kitahira <m_kitahira@nittsusystem.co.jp>
2021-08-18 15:24:26 +0900

    消えちゃったソースコードを復旧

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.ws/src/main/java/nts/uk/ctx/exio/ws/input/ExternalImportSettingWebService.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.ws/src/main/java/nts/uk/ctx/exio/ws/input/ExternalImportSettingWebService.java
@@ -1,20 +1,43 @@
 package nts.uk.ctx.exio.ws.input;
 
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import nts.arc.layer.ws.WebService;
 import nts.uk.ctx.exio.app.input.command.setting.RemoveExternalImportSettingCommand;
 import nts.uk.ctx.exio.app.input.command.setting.RemoveExternalImportSettingCommandHandler;
+import nts.uk.ctx.exio.app.input.find.setting.ExternalImportSettingDto;
+import nts.uk.ctx.exio.app.input.find.setting.ExternalImportSettingListItemDto;
+import nts.uk.ctx.exio.app.input.find.setting.FindExternalImportSetting;
 
 @Path("exio/input/setting")
 @Produces("application/json")
 public class ExternalImportSettingWebService extends WebService {
 	
 	@Inject
+	private FindExternalImportSetting finder;
+	
+	@Inject
 	private RemoveExternalImportSettingCommandHandler removeCmd;
+	
+	@POST
+	@Path("find-all")
+	public List<ExternalImportSettingListItemDto> findAll() {
+		List<ExternalImportSettingListItemDto> result = finder.findAll();
+		return result;
+	}
+	
+	@POST
+	@Path("find/{settingCode}")
+	public ExternalImportSettingDto find(@PathParam("settingCode") String settingCode) {
+		ExternalImportSettingDto result = finder.find(settingCode);
+		return result;
+	}
 	
 	@POST
 	@Path("remove")
