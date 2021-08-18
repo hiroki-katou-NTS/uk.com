37bc701c0ecbe805d9dbb732a2ff9bd0bdfa056d
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2021-08-18 13:35:40 +0700

    Update DeleteWorkLocationCmdHandler.java

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/worklocation/DeleteWorkLocationCmdHandler.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/worklocation/DeleteWorkLocationCmdHandler.java
@@ -36,7 +36,7 @@ public class DeleteWorkLocationCmdHandler  extends CommandHandler<String>{
 			throw new BusinessException("Msg_1969");
 		}
 		
-		if(AppContexts.user().roles().isInChargeAttendance()) {
+		if(!AppContexts.user().roles().isInChargeAttendance()) {
 			throw new BusinessException("Msg_2214");
 		}
 		//3:
