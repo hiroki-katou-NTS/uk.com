a75f444cdd286d5f08b6fe5ccc81b20af1048b5f
NSC-LanLT <lan-lt@nittsusystem.co.jp>
2021-08-21 11:52:40 +0900

    DS_勤務計画実施表の個人計を集計するのコードとUTコード

---
+++ nts.uk/uk.at/at.ctx/aggregation/nts.uk.ctx.at.aggregation.dom/src/main/java/nts/uk/ctx/at/aggregation/dom/scheduledailytable/ScheduleDailyTableOfPersonCountAggregateService.java
@@ -0,0 +1,96 @@
+package nts.uk.ctx.at.aggregation.dom.scheduledailytable;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import nts.uk.ctx.at.aggregation.dom.common.ScheRecAtr;
+import nts.uk.ctx.at.aggregation.dom.common.ScheRecGettingAtr;
+import nts.uk.ctx.at.aggregation.dom.schedulecounter.aggregationprocess.TotalTimesCounterService;
+import nts.uk.ctx.at.shared.dom.common.EmployeeId;
+import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.dailyattendancework.IntegrationOfDaily;
+/**
+ * 勤務計画実施表の個人計を集計する
+ * UKDesign.ドメインモデル.NittsuSystem.UniversalK.就業.contexts.予実集計.勤務計画実施表.勤務計画実施表の個人計を集計する
+ * @author lan_lt
+ *
+ */
+public class ScheduleDailyTableOfPersonCountAggregateService {
+
+	/**
+	 * 集計する
+	 * @param require
+	 * @param inkanTarget 印刷対象
+	 * @param personCounter 個人計
+	 * @param dailyMap 日別勤怠Map
+	 * @return List<個人別の回数集計結果>
+	 */
+	public static List<ResultIndividualizedNoOfAggregation> aggregate(
+				Require require
+			,	ScheRecGettingAtr inkanTarget
+			,	List<Integer> personCounter
+			,	Map<ScheRecGettingAtr, List<IntegrationOfDaily>> dailyMap
+			){
+		
+		//予定集計
+		List<ResultIndividualizedNoOfAggregation> scheNoTimeTotalResult = new ArrayList<>();
+		if(inkanTarget.isNeedSchedule()) {
+			scheNoTimeTotalResult.addAll(
+					aggregateByScheRecAtr(	require, ScheRecAtr.SCHEDULE, personCounter
+										,	dailyMap.get(ScheRecGettingAtr.ONLY_SCHEDULE)));
+		}
+		
+		//予定集計
+		List<ResultIndividualizedNoOfAggregation> recNoTimeTotalResult = new ArrayList<>();
+		if(inkanTarget.isNeedRecord()) {
+			recNoTimeTotalResult.addAll(
+					aggregateByScheRecAtr(	require, ScheRecAtr.RECORD, personCounter
+										,	dailyMap.get(ScheRecGettingAtr.ONLY_RECORD)));
+		}
+		
+		List<ResultIndividualizedNoOfAggregation> results = new ArrayList<>();
+		results.addAll(scheNoTimeTotalResult);
+		results.addAll(recNoTimeTotalResult);
+		
+		return results;
+	}
+	/**
+	 * 予実区分によって集計する
+	 * @param require
+	 * @param scheRecAtr 予実区分	
+	 * @param personCounter 個人計
+	 * @param targetTotalList 集計対象リスト
+	 * @return List<個人計の回数集計結果>
+	 */
+	private static List<ResultIndividualizedNoOfAggregation> aggregateByScheRecAtr(Require require
+			,	ScheRecAtr scheRecAtr
+			,	List<Integer> personCounter
+			,	List<IntegrationOfDaily> targetTotalList){
+		
+		Map<EmployeeId, Map<Integer, BigDecimal>> totalResult = TotalTimesCounterService
+				.countingNumberOfTotalTimeByEmployee(require, personCounter, targetTotalList);
+		
+		return totalResult.entrySet().stream()
+				.map(entry -> {
+					return entry.getValue()
+								.entrySet()
+								.stream()
+								.map(item -> {
+									return new ResultIndividualizedNoOfAggregation(
+												entry.getKey()
+											,	item.getKey()
+											,	scheRecAtr
+											,	item.getValue());
+									})
+								.collect(Collectors.toList());
+				})
+				.flatMap(List::stream)
+				.collect(Collectors.toList());
+	}
+	
+	public static interface Require extends TotalTimesCounterService.Require{
+		
+	}
+}
---
+++ nts.uk/uk.at/at.ctx/aggregation/nts.uk.ctx.at.aggregation.dom/src/test/java/nts/uk/ctx/at/aggregation/dom/scheduledailytable/ScheduleDailyTableOfPersonCountAggregateServiceTest.java
@@ -0,0 +1,323 @@
+package nts.uk.ctx.at.aggregation.dom.scheduledailytable;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import mockit.Expectations;
+import mockit.Injectable;
+import mockit.integration.junit4.JMockit;
+import nts.arc.testing.assertion.NtsAssert;
+import nts.arc.time.GeneralDate;
+import nts.uk.ctx.at.aggregation.dom.common.ScheRecAtr;
+import nts.uk.ctx.at.aggregation.dom.common.ScheRecGettingAtr;
+import nts.uk.ctx.at.aggregation.dom.schedulecounter.aggregationprocess.TotalTimesCounterService;
+import nts.uk.ctx.at.aggregation.dom.schedulecounter.aggregationprocess.TotalTimesCounterService.Require;
+import nts.uk.ctx.at.shared.dom.common.EmployeeId;
+import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.breakouting.breaking.BreakTimeOfDailyAttd;
+import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.calcategory.CalAttrOfDailyAttd;
+import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.dailyattendancework.IntegrationOfDaily;
+import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.workinfomation.WorkInfoOfDailyAttendance;
+
+/**
+ * 勤務計画実施表の個人計を集計する
+ * @author lan_lt
+ *
+ */
+@SuppressWarnings("unchecked")
+@RunWith(JMockit.class)
+public class ScheduleDailyTableOfPersonCountAggregateServiceTest {
+
+	@Injectable
+	private ScheduleDailyTableOfPersonCountAggregateService.Require require;
+	
+
+	/**
+	 * test method 「予実区分によって集計する」
+	 */
+	@Test
+	public void testAggregateByScheRecAtr() {
+		
+		// 回数集計No = 1, 回数集計No = 2
+		List<Integer> personalCounter = Arrays.asList(1, 2);
+		
+		List<IntegrationOfDaily> dailyWorks = Arrays.asList(
+				Helper.createDailyWorks("sid_1")//社員1
+			,	Helper.createDailyWorks("sid_2")//社員2
+			,	Helper.createDailyWorks("sid_3")//社員3
+		);
+		
+		Map<EmployeeId, Map<Integer, BigDecimal>> totalNoTimeResult = new HashMap<EmployeeId, Map<Integer, BigDecimal>>() {
+			private static final long serialVersionUID = 1L;
+			{
+				//社員1の回数集計
+				Map<Integer, BigDecimal> totalNoTimeSid_1 = new HashMap<Integer, BigDecimal>(){
+					private static final long serialVersionUID = 1L;
+					{
+						put(1, new BigDecimal(100));//回数集計 = 1, 集計 = 100
+						put(2, new BigDecimal(200));//回数集計 = 2, 集計 = 200
+					}
+					
+				};
+				
+				put(new EmployeeId("sid_1"), totalNoTimeSid_1);
+				
+				//社員2の回数集計
+				Map<Integer, BigDecimal> totalNoTimeSid_2 = new HashMap<Integer, BigDecimal>(){
+					private static final long serialVersionUID = 1L;
+					{
+						put(1, new BigDecimal(300));//回数集計 = 1, 集計 = 200
+						put(2, new BigDecimal(400));//回数集計 = 2, 集計 = 300
+					}
+				};
+				
+				put(new EmployeeId("sid_2"), totalNoTimeSid_2);
+				
+				//社員3の回数集計
+				Map<Integer, BigDecimal> totalNoTimeSid_3 = new HashMap<Integer, BigDecimal>(){
+					private static final long serialVersionUID = 1L;
+					{
+						put(1, new BigDecimal(500));//回数集計 = 1, 集計 = 500
+						put(2, new BigDecimal(600));//回数集計 = 2, 集計 = 600
+					}
+					
+				};
+				put(new EmployeeId("sid_3"), totalNoTimeSid_3);
+			}
+		};
+		
+		new Expectations(TotalTimesCounterService.class) {
+			{
+				TotalTimesCounterService.countingNumberOfTotalTimeByEmployee((Require) any, (List<Integer>) any, (List<IntegrationOfDaily>) any);
+				result = totalNoTimeResult;
+			}
+			
+		};
+		
+		//Act
+		List<ResultIndividualizedNoOfAggregation> result = NtsAssert.Invoke.staticMethod(
+					ScheduleDailyTableOfPersonCountAggregateService.class
+				,	"aggregateByScheRecAtr", require
+				,	ScheRecAtr.RECORD, personalCounter, dailyWorks);
+		//Assert
+		assertThat(result)
+				.extracting(	d -> d.getSid().v()
+							,	d -> d.getScheRecAtr()
+							,	d -> d.getTotalCountNo()
+							,	d -> d.getValue())
+				.containsExactlyInAnyOrder(
+						tuple("sid_1", ScheRecAtr.RECORD, Integer.valueOf(1), BigDecimal.valueOf(100))
+					,	tuple("sid_1", ScheRecAtr.RECORD, Integer.valueOf(2), BigDecimal.valueOf(200))
+					,	tuple("sid_2", ScheRecAtr.RECORD, Integer.valueOf(1), BigDecimal.valueOf(300))
+					,	tuple("sid_2", ScheRecAtr.RECORD, Integer.valueOf(2), BigDecimal.valueOf(400))
+					,	tuple("sid_3", ScheRecAtr.RECORD, Integer.valueOf(1), BigDecimal.valueOf(500))
+					,	tuple("sid_3", ScheRecAtr.RECORD, Integer.valueOf(2), BigDecimal.valueOf(600))
+				);
+	}
+	
+	/**
+	 * test method 集計する
+	 */
+	@Test
+	public void testAggregate() {
+		// 回数集計No = 1, 回数集計No = 2
+		List<Integer> personalCounter = Arrays.asList(1, 2);
+		//予定
+		Map<EmployeeId, Map<Integer, BigDecimal>> totalNoTimeResult = new HashMap<EmployeeId, Map<Integer, BigDecimal>>() {
+			private static final long serialVersionUID = 1L;
+			{
+				//社員1の回数集計
+				Map<Integer, BigDecimal> totalNoTimeSid_1 = new HashMap<Integer, BigDecimal>(){
+					private static final long serialVersionUID = 1L;
+					{
+						put(1, new BigDecimal(100));//回数集計 = 1, 集計 = 100
+						put(2, new BigDecimal(200));//回数集計 = 2, 集計 = 200
+					}
+					
+				};
+				
+				put(new EmployeeId("sid_1"), totalNoTimeSid_1);
+				
+				//社員2の回数集計
+				Map<Integer, BigDecimal> totalNoTimeSid_2 = new HashMap<Integer, BigDecimal>(){
+					private static final long serialVersionUID = 1L;
+					{
+						put(1, new BigDecimal(300));//回数集計 = 1, 集計 = 200
+						put(2, new BigDecimal(400));//回数集計 = 2, 集計 = 300
+					}
+				};
+				
+				put(new EmployeeId("sid_2"), totalNoTimeSid_2);
+				
+				//社員3の回数集計
+				Map<Integer, BigDecimal> totalNoTimeSid_3 = new HashMap<Integer, BigDecimal>(){
+					private static final long serialVersionUID = 1L;
+					{
+						put(1, new BigDecimal(500));//回数集計 = 1, 集計 = 500
+						put(2, new BigDecimal(600));//回数集計 = 2, 集計 = 600
+					}
+					
+				};
+				put(new EmployeeId("sid_3"), totalNoTimeSid_3);
+			}
+		};
+		
+		new Expectations(TotalTimesCounterService.class) {
+			{
+				TotalTimesCounterService.countingNumberOfTotalTimeByEmployee((Require) any, (List<Integer>) any, (List<IntegrationOfDaily>) any);
+				result = totalNoTimeResult;
+			}
+		};
+		
+		Map<ScheRecGettingAtr, List<IntegrationOfDaily>> dailyMaps =  new HashMap<ScheRecGettingAtr, List<IntegrationOfDaily>>() {
+			private static final long serialVersionUID = 1L;
+			
+			{
+				put(	ScheRecGettingAtr.SCHEDULE_WITH_RECORD
+					,	Arrays.asList(
+							Helper.createDailyWorks("sid_1")
+						,	Helper.createDailyWorks("sid_2")
+						,	Helper.createDailyWorks("sid_3"))
+					);
+				
+				put(	ScheRecGettingAtr.ONLY_SCHEDULE
+						,	Arrays.asList(
+								Helper.createDailyWorks("sid_1")
+							,	Helper.createDailyWorks("sid_2")
+							,	Helper.createDailyWorks("sid_3"))
+						);
+				
+				put(	ScheRecGettingAtr.ONLY_RECORD
+						,	Arrays.asList(
+								Helper.createDailyWorks("sid_1")
+							,	Helper.createDailyWorks("sid_2")
+							,	Helper.createDailyWorks("sid_3"))
+						);
+				
+			}
+		};
+		
+		//予定のみ
+		{			
+			//Act
+			List<ResultIndividualizedNoOfAggregation> result = NtsAssert.Invoke.staticMethod(
+						ScheduleDailyTableOfPersonCountAggregateService.class
+					,	"aggregate", require
+					,	ScheRecGettingAtr.ONLY_SCHEDULE, personalCounter, dailyMaps);
+			
+			//Assert
+			assertThat(result)
+					.extracting(	d -> d.getSid().v()
+								,	d -> d.getScheRecAtr()
+								,	d -> d.getTotalCountNo()
+								,	d -> d.getValue())
+					.containsExactlyInAnyOrder(
+							tuple("sid_1", ScheRecAtr.SCHEDULE, Integer.valueOf(1), BigDecimal.valueOf(100))
+						,	tuple("sid_1", ScheRecAtr.SCHEDULE, Integer.valueOf(2), BigDecimal.valueOf(200))
+						,	tuple("sid_2", ScheRecAtr.SCHEDULE, Integer.valueOf(1), BigDecimal.valueOf(300))
+						,	tuple("sid_2", ScheRecAtr.SCHEDULE, Integer.valueOf(2), BigDecimal.valueOf(400))
+						,	tuple("sid_3", ScheRecAtr.SCHEDULE, Integer.valueOf(1), BigDecimal.valueOf(500))
+						,	tuple("sid_3", ScheRecAtr.SCHEDULE, Integer.valueOf(2), BigDecimal.valueOf(600))
+					);		
+		}
+		
+		//実績のみ
+		{
+			//Act
+			List<ResultIndividualizedNoOfAggregation> result = NtsAssert.Invoke.staticMethod(
+						ScheduleDailyTableOfPersonCountAggregateService.class
+					,	"aggregate", require
+					,	ScheRecGettingAtr.ONLY_RECORD, personalCounter, dailyMaps);
+			
+			//Assert
+			assertThat(result)
+					.extracting(	d -> d.getSid().v()
+								,	d -> d.getScheRecAtr()
+								,	d -> d.getTotalCountNo()
+								,	d -> d.getValue())
+					.containsExactlyInAnyOrder(
+							tuple("sid_1", ScheRecAtr.RECORD, Integer.valueOf(1), BigDecimal.valueOf(100))
+						,	tuple("sid_1", ScheRecAtr.RECORD, Integer.valueOf(2), BigDecimal.valueOf(200))
+						,	tuple("sid_2", ScheRecAtr.RECORD, Integer.valueOf(1), BigDecimal.valueOf(300))
+						,	tuple("sid_2", ScheRecAtr.RECORD, Integer.valueOf(2), BigDecimal.valueOf(400))
+						,	tuple("sid_3", ScheRecAtr.RECORD, Integer.valueOf(1), BigDecimal.valueOf(500))
+						,	tuple("sid_3", ScheRecAtr.RECORD, Integer.valueOf(2), BigDecimal.valueOf(600))
+					);		
+		}
+		
+		//予定＋実績
+		{
+			//Act
+			List<ResultIndividualizedNoOfAggregation> result = NtsAssert.Invoke.staticMethod(
+						ScheduleDailyTableOfPersonCountAggregateService.class
+					,	"aggregate", require
+					,	ScheRecGettingAtr.SCHEDULE_WITH_RECORD, personalCounter, dailyMaps);
+			
+			//Assert
+			assertThat(result)
+					.extracting(	d -> d.getSid().v()
+								,	d -> d.getScheRecAtr()
+								,	d -> d.getTotalCountNo()
+								,	d -> d.getValue())
+					.containsExactlyInAnyOrder(
+							tuple("sid_1", ScheRecAtr.RECORD, Integer.valueOf(1), BigDecimal.valueOf(100))
+						,	tuple("sid_1", ScheRecAtr.RECORD, Integer.valueOf(2), BigDecimal.valueOf(200))
+						,	tuple("sid_2", ScheRecAtr.RECORD, Integer.valueOf(1), BigDecimal.valueOf(300))
+						,	tuple("sid_2", ScheRecAtr.RECORD, Integer.valueOf(2), BigDecimal.valueOf(400))
+						,	tuple("sid_3", ScheRecAtr.RECORD, Integer.valueOf(1), BigDecimal.valueOf(500))
+						,	tuple("sid_3", ScheRecAtr.RECORD, Integer.valueOf(2), BigDecimal.valueOf(600))
+						,	tuple("sid_1", ScheRecAtr.SCHEDULE, Integer.valueOf(1), BigDecimal.valueOf(100))
+						,	tuple("sid_1", ScheRecAtr.SCHEDULE, Integer.valueOf(2), BigDecimal.valueOf(200))
+						,	tuple("sid_2", ScheRecAtr.SCHEDULE, Integer.valueOf(1), BigDecimal.valueOf(300))
+						,	tuple("sid_2", ScheRecAtr.SCHEDULE, Integer.valueOf(2), BigDecimal.valueOf(400))
+						,	tuple("sid_3", ScheRecAtr.SCHEDULE, Integer.valueOf(1), BigDecimal.valueOf(500))
+						,	tuple("sid_3", ScheRecAtr.SCHEDULE, Integer.valueOf(2), BigDecimal.valueOf(600))
+					);		
+		}
+
+	}
+	
+	public static class Helper {
+		@Injectable
+		private static WorkInfoOfDailyAttendance workInformation;
+		
+		@Injectable
+		private static GeneralDate date;
+		/**
+		 * 日別実績(Work)を作る
+		 * @param sid 社員ID
+		 * @param ymd 年月日
+		 * @return
+		 */
+		public static IntegrationOfDaily createDailyWorks(String sid) {
+			return new IntegrationOfDaily(
+					  sid, date, workInformation
+					, CalAttrOfDailyAttd.defaultData()
+					, null
+					, Optional.empty()
+					, Collections.emptyList()
+					, Optional.empty()
+					, new BreakTimeOfDailyAttd(Collections.emptyList())
+					, Optional.empty()
+					, Optional.empty()
+					, Optional.empty()
+					, Optional.empty()
+					, Optional.empty()
+					, Optional.empty()
+					, Collections.emptyList()
+					, Optional.empty()
+					, Collections.emptyList()
+					, Optional.empty());
+		}
+	}
+}
