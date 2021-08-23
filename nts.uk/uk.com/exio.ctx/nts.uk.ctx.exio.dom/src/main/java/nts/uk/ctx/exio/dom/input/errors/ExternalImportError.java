92404002a74d4ffb1926e58c110f45c9ba610cd2
m_kitahira <m_kitahira@nittsusystem.co.jp>
2021-08-23 17:05:20 +0900

    エラーの文字列化

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/errors/ExternalImportError.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/errors/ExternalImportError.java
@@ -1,6 +1,9 @@
 package nts.uk.ctx.exio.dom.input.errors;
 
 import lombok.Value;
+import nts.uk.ctx.exio.dom.input.ExecutionContext;
+import nts.uk.ctx.exio.dom.input.domain.ImportingDomainId;
+import nts.uk.ctx.exio.dom.input.importableitem.ImportableItem;
 
 @Value
 public class ExternalImportError {
@@ -21,4 +24,34 @@ public class ExternalImportError {
 		return new ExternalImportError(error.getCsvRowNo(), null, error.getMessage());
 	}
 	
+	public boolean isRecord() {
+		return itemNo == null;
+	}
+	
+	public boolean isItem() {
+		return !isRecord();
+	}
+	
+	public void toText(RequireToText require, ExecutionContext context, StringBuilder sb) {
+		
+		sb.append(String.format("%08d 行", csvRowNo));
+		
+		sb.append("\t");
+		
+		if (isItem()) {
+			String itemName = require.getImportableItem(context.getDomainId(), itemNo).getItemName();
+			sb.append(itemName);
+		}
+		
+		sb.append("\t");
+		
+		sb.append(message);
+		
+		sb.append("\r\n");
+	}
+	
+	public static interface RequireToText {
+		
+		ImportableItem getImportableItem(ImportingDomainId domainId, int itemNo);
+	}
 }
--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/errors/ExternalImportErrors.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/errors/ExternalImportErrors.java
@@ -1,8 +1,33 @@
 package nts.uk.ctx.exio.dom.input.errors;
 
-import lombok.Value;
+import static java.util.stream.Collectors.*;
 
-@Value
+import java.util.Comparator;
+import java.util.List;
+
+import lombok.val;
+import nts.uk.ctx.exio.dom.input.ExecutionContext;
+
 public class ExternalImportErrors {
 
+	private final List<ExternalImportError> errors;
+	
+	public ExternalImportErrors(List<ExternalImportError> errors) {
+		
+		this.errors = errors.stream()
+				.sorted(Comparator.comparing(e -> e.getCsvRowNo()))
+				.collect(toList());
+	}
+	
+	public String toText(RequireToText require, ExecutionContext context) {
+		
+		val sb = new StringBuilder();
+		
+		errors.forEach(e -> e.toText(require, context, sb));
+		
+		return sb.toString();
+	}
+	
+	public static interface RequireToText extends ExternalImportError.RequireToText {
+	}
 }
