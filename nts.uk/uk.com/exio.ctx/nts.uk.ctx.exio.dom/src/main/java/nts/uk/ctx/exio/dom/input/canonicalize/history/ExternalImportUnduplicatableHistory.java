f8640da280ddc38b338a183564c4b5a481046136
CORP\keisuke_hoshina <keisuke_hoshina@nittsusystem.co.jp>
2021-08-25 17:45:42 +0900

    重複禁止の履歴の基盤処理実装

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/canonicalize/history/ExternalImportUnduplicatableHistory.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/canonicalize/history/ExternalImportUnduplicatableHistory.java
@@ -14,10 +14,15 @@ public class ExternalImportUnduplicatableHistory
 	extends ExternalImportHistory
 	implements UnduplicatableHistory<DateHistoryItem, DatePeriod, GeneralDate>{
 
-//	@Override
-//	public void add() {
-//		
-//	}
+	@Override
+	public void add(DateHistoryItem itemToBeAdded) {
+		this.latestStartItem().ifPresent(latest->{
+			if(latest.compare(itemToBeAdded.span()).isDuplicated()) {
+				latest.shortenEndToAccept(itemToBeAdded);
+			}
+		});
+		super.add(itemToBeAdded);
+	}
 	
 	public ExternalImportUnduplicatableHistory(List<DateHistoryItem> period) {
 		super(period);
