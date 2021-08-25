b4b7679912a373cdd7a86d5851e37bfdae683a00
nws-dungdv <dungdv@newwave.vn>
2021-08-25 11:00:03 +0700

    feat: add eqCls webservice

---
+++ nts.uk/uk.com/office.ctx/nts.uk.ctx.office.ws/src/main/java/nts/uk/ctx/office/ws/equipment/classificationmaster/EquipmentClassificationWebService.java
@@ -0,0 +1,53 @@
+package nts.uk.ctx.office.ws.equipment.classificationmaster;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import nts.uk.ctx.office.app.command.equipment.classificationmaster.CreateEquipmentClsCommandHandler;
+import nts.uk.ctx.office.app.command.equipment.classificationmaster.EquipmentClsCommand;
+import nts.uk.ctx.office.app.command.equipment.classificationmaster.UpdateEquipmentClsCommandHandler;
+import nts.uk.ctx.office.app.query.equipment.classificationmaster.EquipmentClassificationDto;
+import nts.uk.ctx.office.app.query.equipment.classificationmaster.EquipmentClassificationListQuery;
+import nts.uk.shr.com.context.AppContexts;
+
+@Path("ctx/office/equipment/classificationmaster/")
+@Produces("application/json")
+public class EquipmentClassificationWebService {
+
+	@Inject
+	private EquipmentClassificationListQuery query;
+	
+	@Inject
+	private CreateEquipmentClsCommandHandler createCommandHandler;
+	
+	@Inject
+	private UpdateEquipmentClsCommandHandler updateCommandHandler;
+	
+	@POST
+	@Path("insert")
+	public void insert(EquipmentClsCommand command) {
+		this.createCommandHandler.handle(command);
+	}
+	
+	@POST
+	@Path("update")
+	public void update(EquipmentClsCommand command) {
+		this.updateCommandHandler.handle(command);
+	}
+	
+	@POST
+	@Path("delete")
+	public void delete(String code) {
+		
+	}
+	
+	@POST
+	@Path("getAll")
+	public List<EquipmentClassificationDto> getAll() {
+		return this.query.get(AppContexts.user().contractCode());
+	}
+}
