c8ee1e12f9505254ec3c9e941ff4895fd1f60fc0
nws-dungdv <dungdv@newwave.vn>
2021-08-25 11:49:36 +0700

    refactor

--- nts.uk/uk.com/office.ctx/nts.uk.ctx.office.ws/src/main/java/nts/uk/ctx/office/ws/equipment/classificationmaster/EquipmentClassificationWebService.java
+++ nts.uk/uk.com/office.ctx/nts.uk.ctx.office.ws/src/main/java/nts/uk/ctx/office/ws/equipment/classificationmaster/EquipmentClassificationWebService.java
@@ -1,7 +1,5 @@
 package nts.uk.ctx.office.ws.equipment.classificationmaster;
 
-import java.util.List;
-
 import javax.inject.Inject;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -11,16 +9,10 @@ import nts.arc.layer.ws.WebService;
 import nts.uk.ctx.office.app.command.equipment.classificationmaster.CreateEquipmentClsCommandHandler;
 import nts.uk.ctx.office.app.command.equipment.classificationmaster.EquipmentClsCommand;
 import nts.uk.ctx.office.app.command.equipment.classificationmaster.UpdateEquipmentClsCommandHandler;
-import nts.uk.ctx.office.app.query.equipment.classificationmaster.EquipmentClassificationDto;
-import nts.uk.ctx.office.app.query.equipment.classificationmaster.EquipmentClassificationListQuery;
-import nts.uk.shr.com.context.AppContexts;
 
 @Path("ctx/office/equipment/classificationmaster/")
 @Produces("application/json")
 public class EquipmentClassificationWebService extends WebService {
-
-	@Inject
-	private EquipmentClassificationListQuery query;
 	
 	@Inject
 	private CreateEquipmentClsCommandHandler createCommandHandler;
@@ -38,12 +30,6 @@ public class EquipmentClassificationWebService extends WebService {
 	@Path("update")
 	public void update(EquipmentClsCommand command) {
 		this.updateCommandHandler.handle(command);
-	}
-	
-	@POST
-	@Path("getAll")
-	public List<EquipmentClassificationDto> getAll() {
-		return this.query.get(AppContexts.user().contractCode());
 	}
 	
 }
