ecbdb1825b0a7f23f316d0159435f9a7629c5e39
CORP\keisuke_hoshina <keisuke_hoshina@nittsusystem.co.jp>
2021-08-26 11:11:29 +0900

    レビュー後修正の修正漏れ

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/canonicalize/domaindata/DomainDataRepository.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/canonicalize/domaindata/DomainDataRepository.java
@@ -1,10 +1,8 @@
 package nts.uk.ctx.exio.dom.input.canonicalize.domaindata;
 
-import nts.arc.time.GeneralDate;
 import nts.arc.time.calendar.period.DatePeriod;
+import nts.uk.ctx.exio.dom.input.canonicalize.history.ExternalImportHistory;
 import nts.uk.ctx.exio.dom.input.canonicalize.history.HistoryType;
-import nts.uk.shr.com.history.DateHistoryItem;
-import nts.uk.shr.com.history.History;
 
 /**
  * ドメインのテーブルに対して直接アクセスする汎用Repository
@@ -15,7 +13,7 @@ public interface DomainDataRepository {
 	
 	void delete(DomainDataId id);
 	
-	History<DateHistoryItem, DatePeriod, GeneralDate> getHistory(DomainDataId id, HistoryType historyType);
+	ExternalImportHistory getHistory(DomainDataId id, HistoryType historyType);
 	
 	void update(DomainDataId targetKey, DatePeriod period);
 }
