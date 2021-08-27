8d4fa6d38cd6813859271f7a0999b5cb6017d36e
Shuichi Ishida <shu@silver.ocn.ne.jp>
2021-08-27 23:52:39 +0900

    update DailyCalculation: Flex deduct GoOutSheet in CoreTime.

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/scherec/dailyattdcal/dailycalprocess/calculation/WithinOutingTotalTime.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/scherec/dailyattdcal/dailycalprocess/calculation/WithinOutingTotalTime.java
@@ -57,15 +57,16 @@ public class WithinOutingTotalTime {
 			TimeSheetRoundingAtr roundAtr,
 			GoingOutReason reason) {
 		
+		ConditionAtr conditionAtr = ConditionAtr.convertFromGoOutReason(reason);	// 控除種別区分
+		
 		// 所定内合計時間の計算
 		TimeWithCalculation withinDedTime = oneDay.getDeductionTime(
-				ConditionAtr.convertFromGoOutReason(reason),
-				dedAtr, StatutoryAtr.Statutory, roundAtr, Optional.empty());
+				conditionAtr, dedAtr, StatutoryAtr.Statutory, roundAtr, Optional.empty());
 		// コア内の外出時間の計算
 		FlexWithinWorkTimeSheet changedFlexTimeSheet = (FlexWithinWorkTimeSheet)oneDay.getWithinWorkingTimeSheet().get();
-		AttendanceTime withinFlex = changedFlexTimeSheet.calcOutingTimeInFlex(true, dedAtr, roundAtr);
+		AttendanceTime withinFlex = changedFlexTimeSheet.calcOutingTimeInFlex(true, conditionAtr, dedAtr, roundAtr);
 		// コア外外出時間の計算
-		AttendanceTime excessFlex = changedFlexTimeSheet.calcOutingTimeInFlex(false, dedAtr, roundAtr);
+		AttendanceTime excessFlex = changedFlexTimeSheet.calcOutingTimeInFlex(false, conditionAtr, dedAtr, roundAtr);
 		// 外出合計時間を返す
 		return WithinOutingTotalTime.of(
 				withinDedTime,
