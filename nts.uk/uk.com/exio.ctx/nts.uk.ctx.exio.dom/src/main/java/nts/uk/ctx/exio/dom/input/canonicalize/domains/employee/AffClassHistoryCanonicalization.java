1aa7052c9d3a064cf3adcfff0f7bf4e70895c1d8
CORP\keisuke_hoshina <keisuke_hoshina@nittsusystem.co.jp>
2021-09-01 12:00:38 +0900

    分類正準化　固定値埋める実装が抜けていたため修正

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/canonicalize/domains/employee/AffClassHistoryCanonicalization.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/canonicalize/domains/employee/AffClassHistoryCanonicalization.java
@@ -1,36 +1,65 @@
-package nts.uk.ctx.exio.dom.input.canonicalize.domains;
+package nts.uk.ctx.exio.dom.input.canonicalize.domains.employee;
 
 import static nts.uk.ctx.exio.dom.input.workspace.datatype.DataType.STRING;
 
 import java.util.Arrays;
 import java.util.List;
 
+import lombok.val;
+import nts.uk.ctx.exio.dom.input.ExecutionContext;
+import nts.uk.ctx.exio.dom.input.canonicalize.CanonicalItemList;
+import nts.uk.ctx.exio.dom.input.canonicalize.CanonicalizeUtil;
 import nts.uk.ctx.exio.dom.input.canonicalize.domaindata.DomainDataColumn;
+import nts.uk.ctx.exio.dom.input.canonicalize.domains.DomainCanonicalization;
 import nts.uk.ctx.exio.dom.input.canonicalize.domains.generic.EmployeeHistoryCanonicalization;
 import nts.uk.ctx.exio.dom.input.canonicalize.history.HistoryType;
 import nts.uk.ctx.exio.dom.input.workspace.domain.DomainWorkspace;
+import nts.uk.shr.com.context.AppContexts;
 
 /**
  *分類履歴グループの正準化用定義 
  */
-public class AffClassHistoryCanonicalization {
-	protected static EmployeeHistoryCanonicalization create(DomainWorkspace w) {
-		return new EmployeeHistoryCanonicalization(w, HistoryType.PERSISTENERESIDENT) {
+public class AffClassHistoryCanonicalization extends EmployeeHistoryCanonicalization{
+	
+	@Override
+	protected String getParentTableName() {
+		return "BSYMT_AFF_CLASS_HIST";
+	}
+
+	@Override
+	protected List<String> getChildTableNames() {
+		return Arrays.asList("BSYMT_AFF_CLASS_HIST_ITEM");
+	}
+
+	@Override
+	protected List<DomainDataColumn> getDomainDataKeys() {
+		return Arrays.asList(
+				new DomainDataColumn("HIST_ID", STRING));
+	}
+
+	public static AffClassHistoryCanonicalization create(DomainWorkspace workspace) {
+		return new AffClassHistoryCanonicalization(workspace, HistoryType.PERSISTENERESIDENT);
+	}
+	
+	private AffClassHistoryCanonicalization(DomainWorkspace workspace, HistoryType historyType) {
+		super(workspace, historyType);
+	}
+	
+	@Override
+	public void canonicalize(DomainCanonicalization.RequireCanonicalize require, ExecutionContext context) {
+		CanonicalizeUtil.forEachEmployee(require, context, this.getEmployeeCodeCanonicalization(), interm -> {
 			
-			@Override
-			protected String getParentTableName() {
-				return "BSYMT_AFF_CLASS_HIST";
-			}
-			@Override
-			protected List<String> getChildTableNames() {
-				return Arrays.asList("BSYMT_AFF_CLASS_HIST_ITEM");
-			}
+			val results = super.canonicalizeHistory(require, context, interm);
 			
-			@Override
-			protected List<DomainDataColumn> getDomainDataKeys() {
-				return Arrays.asList(
-						new DomainDataColumn("HIST_ID", STRING));
-			}
-		};
+			results.forEach(result -> {
+				result.addCanonicalized(getFixedItems());
+				require.save(context, result.complete());
+			});
+		});
+	}
+	
+	public static CanonicalItemList getFixedItems() {
+		return new CanonicalItemList()
+			.add(100, AppContexts.user().companyId()); // CID
 	}
 }
