e46190634d0a041c54472c09656a2c7618307e4a
akane_tanaka <akane_tanaka@nittsusystem.co.jp>
2021-09-04 16:29:07 +0900

    親画面を登録するとマッピング情報が消えちゃうバグの修正

--- nts.uk/uk.com/com.screen/nts.uk.screen.com.app/src/main/java/nts/uk/screen/com/app/cmf/cmf001/b/get/ExternalImportSettingDto.java
+++ nts.uk/uk.com/com.screen/nts.uk.screen.com.app/src/main/java/nts/uk/screen/com/app/cmf/cmf001/b/get/ExternalImportSettingDto.java
@@ -22,79 +22,77 @@ import nts.uk.shr.com.context.AppContexts;
 
 @Value
 public class ExternalImportSettingDto {
-	
+
 	/** 会社ID */
 	private String companyId;
-	
+
 	/** 受入設定コード */
 	private String code;
-	
+
 	/** 受入設定名称 */
 	private String name;
-	
-	/** 受入グループID */
+
+	/** 受入ドメインID */
 	private int domain;
-	
+
 	/** 受入モード */
 	private int mode;
-	
+
 	/** CSVの項目名取得行 */
 	private int itemNameRow;
-	
+
 	/** CSVの取込開始行 */
 	private int importStartRow;
-	
+
 	/** レイアウト項目リスト */
 	private List<Integer> itemNoList;
-	
-	public static ExternalImportSettingDto fromDomain(Require require, ExternalImportSetting domain) {
-		
+
+	public void merge(ExternalImportSetting domain) {
+
+		domain.setName(new ExternalImportName(name));
+		domain.setImportingMode(ImportingMode.valueOf(mode));
+		domain.getAssembly().setCsvFileInfo(toCsvFileInfo());
+
+		if(this.domain == domain.getExternalImportDomainId().value) {
+
+		}else {
+			domain.changeDomain(ImportingDomainId.valueOf(this.domain), itemNoList);
+		}
+
+
+	}
+
+	public static ExternalImportSettingDto fromDomain(ExternalImportSetting domain) {
+
 		return new ExternalImportSettingDto(
-				domain.getCompanyId(), 
-				domain.getCode().toString(), 
-				domain.getName().toString(), 
-				domain.getExternalImportDomainId().value, 
-				domain.getImportingMode().value, 
-				domain.getAssembly().getCsvFileInfo().getItemNameRowNumber().hashCode(), 
-				domain.getAssembly().getCsvFileInfo().getImportStartRowNumber().hashCode(), 
+				domain.getCompanyId(),
+				domain.getCode().toString(),
+				domain.getName().toString(),
+				domain.getExternalImportDomainId().value,
+				domain.getImportingMode().value,
+				domain.getAssembly().getCsvFileInfo().getItemNameRowNumber().hashCode(),
+				domain.getAssembly().getCsvFileInfo().getImportStartRowNumber().hashCode(),
 				domain.getAssembly().getMapping().getMappings().stream()
 				.map(m -> m.getItemNo())
 				.collect(Collectors.toList()));
 	}
-	
-	public ExternalImportSetting toDomain(Require require) {
+
+	public ExternalImportSetting toDomain() {
 		return new ExternalImportSetting(
-				companyId, 
-				new ExternalImportCode(code), 
-				new ExternalImportName(name), 
-				ImportingDomainId.valueOf(domain), 
-				ImportingMode.valueOf(mode), 
-				new ExternalImportAssemblyMethod(
-						new ExternalImportCsvFileInfo(
-								new ExternalImportRowNumber(itemNameRow), 
-								new ExternalImportRowNumber(importStartRow)), 
-						new ImportingMapping(createMappings(require))));
+				AppContexts.user().companyId(),
+				new ExternalImportCode(code),
+				new ExternalImportName(name),
+				ImportingDomainId.valueOf(domain),
+				ImportingMode.valueOf(mode),
+				ExternalImportAssemblyMethod.create(
+						toCsvFileInfo(),
+						itemNoList));
 	}
-	
-	private List<ImportingItemMapping> createMappings(Require require){
-		List<ImportingItemMapping> result = new ArrayList<>();
-		
-		for(int itemNo: itemNoList) {
-			val optRegisteredSetting = require.getSetting(AppContexts.user().companyId(), new ExternalImportCode(code));
-			if(optRegisteredSetting.isPresent()) {
-				List<ImportingItemMapping> mappings = optRegisteredSetting.get().getAssembly().getMapping().getMappings();
-				val configuredItem = mappings.stream().filter(m -> m.getItemNo() == itemNo).collect(Collectors.toList());
-				if(configuredItem.size() > 0) {
-					result.addAll(configuredItem);
-				}
-			}
-			result.add(new ImportingItemMapping(itemNo, Optional.empty(), Optional.empty()));
-		}
-		return result;
+
+	private ExternalImportCsvFileInfo toCsvFileInfo() {
+		return new ExternalImportCsvFileInfo(
+				new ExternalImportRowNumber(itemNameRow),
+				new ExternalImportRowNumber(importStartRow));
 	}
-	
-	public static interface Require {
-		List<ImportableItem> getImportableItems(ImportingDomainId domainId);
-		Optional<ExternalImportSetting> getSetting(String companyId, ExternalImportCode settingCode);
-	}
+
 }
