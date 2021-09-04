04765f7d8fedd389316ae878279e98b76d8f2584
CORP\keisuke_hoshina <keisuke_hoshina@nittsusystem.co.jp>
2021-09-04 11:37:05 +0900

    レビュー後修正　雇用正準化

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/canonicalize/domains/employee/EmploymentHistoryCanonicalization.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/canonicalize/domains/employee/EmploymentHistoryCanonicalization.java
@@ -3,17 +3,11 @@ package nts.uk.ctx.exio.dom.input.canonicalize.domains.employee;
 import java.util.Arrays;
 import java.util.List;
 
-import lombok.val;
-import nts.uk.ctx.exio.dom.input.ExecutionContext;
-import nts.uk.ctx.exio.dom.input.canonicalize.CanonicalItemList;
-import nts.uk.ctx.exio.dom.input.canonicalize.CanonicalizeUtil;
 import nts.uk.ctx.exio.dom.input.canonicalize.domaindata.DomainDataColumn;
-import nts.uk.ctx.exio.dom.input.canonicalize.domains.DomainCanonicalization;
 import nts.uk.ctx.exio.dom.input.canonicalize.domains.generic.EmployeeHistoryCanonicalization;
 import nts.uk.ctx.exio.dom.input.canonicalize.history.HistoryType;
 import nts.uk.ctx.exio.dom.input.workspace.datatype.DataType;
 import nts.uk.ctx.exio.dom.input.workspace.domain.DomainWorkspace;
-import nts.uk.shr.com.context.AppContexts;
 
 /**
  * 雇用履歴の正準化 
@@ -35,30 +29,7 @@ public class EmploymentHistoryCanonicalization extends EmployeeHistoryCanonicali
 		return Arrays.asList(new DomainDataColumn("HIST_ID", DataType.STRING));
 	}
 	
-	public static EmploymentHistoryCanonicalization create(DomainWorkspace workspace) {
-		return new EmploymentHistoryCanonicalization(workspace, HistoryType.PERSISTENERESIDENT);
-	}
-	
-	private EmploymentHistoryCanonicalization(DomainWorkspace workspace, HistoryType historyType) {
-		super(workspace, historyType);
-	}
-	
-	@Override
-	public void canonicalize(DomainCanonicalization.RequireCanonicalize require, ExecutionContext context) {
-		CanonicalizeUtil.forEachEmployee(require, context, this.getEmployeeCodeCanonicalization(), interm -> {
-			
-			val results = super.canonicalizeHistory(require, context, interm);
-			
-			results.forEach(result -> {
-				result.addCanonicalized(getFixedItems());
-				require.save(context, result.complete());
-			});
-		});
-	}
-	
-	public static CanonicalItemList getFixedItems() {
-		return new CanonicalItemList()
-			.add(100, AppContexts.user().companyId()) // CID
-			.addNull(103); //給与区分
+	public EmploymentHistoryCanonicalization(DomainWorkspace workspace, HistoryType historyType) {
+		super(workspace, HistoryType.PERSISTENERESIDENT);
 	}
 }
