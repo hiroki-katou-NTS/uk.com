df50996c2f4c934ab3f6748dc0e55722f80f07fd
CORP\keisuke_hoshina <keisuke_hoshina@nittsusystem.co.jp>
2021-09-04 12:42:30 +0900

    IndependentCano　正準化処理拡張に伴う修正

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/canonicalize/domains/employee/holiday/year/EmployeeYearHolidaySettingCanonicalization.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/canonicalize/domains/employee/holiday/year/EmployeeYearHolidaySettingCanonicalization.java
@@ -1,26 +1,14 @@
 package nts.uk.ctx.exio.dom.input.canonicalize.domains.employee.holiday.year;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
 
-import lombok.val;
-import nts.uk.ctx.at.shared.dom.remainingnumber.annualleave.empinfo.basicinfo.AnnualLeaveEmpBasicInfo;
-import nts.uk.ctx.exio.dom.input.ExecutionContext;
 import nts.uk.ctx.exio.dom.input.canonicalize.CanonicalItem;
-import nts.uk.ctx.exio.dom.input.canonicalize.CanonicalItemList;
-import nts.uk.ctx.exio.dom.input.canonicalize.CanonicalizeUtil;
 import nts.uk.ctx.exio.dom.input.canonicalize.domaindata.DomainDataColumn;
-import nts.uk.ctx.exio.dom.input.canonicalize.domaindata.KeyValues;
-import nts.uk.ctx.exio.dom.input.canonicalize.domains.DomainCanonicalization;
 import nts.uk.ctx.exio.dom.input.canonicalize.domains.generic.IndependentCanonicalization;
 import nts.uk.ctx.exio.dom.input.canonicalize.methods.EmployeeCodeCanonicalization;
 import nts.uk.ctx.exio.dom.input.canonicalize.methods.IntermediateResult;
-import nts.uk.ctx.exio.dom.input.errors.ExternalImportError;
 import nts.uk.ctx.exio.dom.input.meta.ImportingDataMeta;
 import nts.uk.ctx.exio.dom.input.workspace.domain.DomainWorkspace;
 
@@ -53,48 +41,19 @@ public class EmployeeYearHolidaySettingCanonicalization extends IndependentCanon
 	}
 	
 	/**
-	 * 正準化
+	 * 追加の正準化処理が必要ならoverrideすること
+	 * @param targetContainers
 	 */
-	@Override
-	public void canonicalize(DomainCanonicalization.RequireCanonicalize require, ExecutionContext context) {
-
-		// 受入データ内の重複チェック
-		Set<List<Object>> importingKeys = new HashSet<>();
-		
-		CanonicalizeUtil.forEachEmployee(require, context, employeeCodeCanonicalization, interms -> {
-
-			for (val interm : interms) {
-				val addedFixedItem = interm.addCanonicalized(addFixedItem(interm)) ;
-				val key = getPrimaryKeys(addedFixedItem, workspace);
-				if (importingKeys.contains(key)) {
-					require.add(context, ExternalImportError.record(interm.getRowNo(), "キーが重複しています。：" + key));
-					continue;
-				}
-				importingKeys.add(key);
-				super.canonicalize(require, context, addedFixedItem, new KeyValues(key));
-			}
-		});
-	}
-	
-	public static interface RequireCanonicalize{
-		Optional<AnnualLeaveEmpBasicInfo> getExistingEmployeeGrantHoliday(String employeeId);
+	protected IntermediateResult canonicalizeExtends(IntermediateResult targertResult) {
+		return addFixedItems(targertResult);
 	}
 	
 	/**
 	 *  受入時に固定の値を入れる物たち
 	 */
-	private CanonicalItemList addFixedItem(IntermediateResult interm) {
-	    List<CanonicalItem> items = new ArrayList<>();
-	    items.add(new CanonicalItem(100,null));
-	    
-	    if(!interm.getItemByNo(2).isPresent())
-	        items.add(CanonicalItem.of(2, 0));
-	    
-	    return new CanonicalItemList(items);
-	}
-	
-	private static List<Object> getPrimaryKeys(IntermediateResult record, DomainWorkspace workspace) {
-		return Arrays.asList(record.getItemByNo(workspace.getItemByName("SID").getItemNo()).get().getString());
+	private IntermediateResult addFixedItems(IntermediateResult interm) {
+	    return interm.addCanonicalized(CanonicalItem.nullValue(100), 100)
+	    		    		   .optionalItem(CanonicalItem.of(2, 0));
 	}
 	
 	@Override
--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/canonicalize/domains/employee/holiday/year/MaxYearHolidayCanonicalization.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/canonicalize/domains/employee/holiday/year/MaxYearHolidayCanonicalization.java
@@ -1,11 +1,9 @@
 package nts.uk.ctx.exio.dom.input.canonicalize.domains.employee.holiday.year;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import lombok.val;
@@ -17,7 +15,6 @@ import nts.uk.ctx.exio.dom.input.canonicalize.domains.DomainCanonicalization;
 import nts.uk.ctx.exio.dom.input.canonicalize.domains.generic.IndependentCanonicalization;
 import nts.uk.ctx.exio.dom.input.canonicalize.methods.EmployeeCodeCanonicalization;
 import nts.uk.ctx.exio.dom.input.canonicalize.methods.IntermediateResult;
-import nts.uk.ctx.exio.dom.input.errors.ErrorMessage;
 import nts.uk.ctx.exio.dom.input.errors.ExternalImportError;
 import nts.uk.ctx.exio.dom.input.meta.ImportingDataMeta;
 import nts.uk.ctx.exio.dom.input.workspace.domain.DomainWorkspace;
@@ -54,9 +51,9 @@ public class MaxYearHolidayCanonicalization extends IndependentCanonicalization{
 	public void canonicalize(DomainCanonicalization.RequireCanonicalize require, ExecutionContext context) {
 		CanonicalizeUtil.forEachEmployee(require, context, employeeCodeCanonicalization, interms -> {
 			for(val interm : interms) {
-					val result = FixedItem.getLackItemError(interm);
-					if(result.isPresent()) {
-						require.add(context, ExternalImportError.record(interm.getRowNo(), result.get().getText()));
+					val results = FixedItem.getLackItemError(interm);
+					if(!results.isEmpty()) {
+						results.stream().peek(result ->require.add(context, result));
 						continue;
 					}
 					//既存データのチェックと保存は継承先に任せる
@@ -67,18 +64,18 @@ public class MaxYearHolidayCanonicalization extends IndependentCanonicalization{
 
 	private static class FixedItem{
 		//半日上限回数、半休使用回数、残回数
-		private static final Map<Integer, String> timesItems = new HashMap<>();
+		private static final List<Integer> timesNumbers = new ArrayList<>();
 		static {
-			timesItems.put(2, "半日上限回数");
-			timesItems.put(3, "半休使用回数");
-			timesItems.put(4, "残回数");
+			timesNumbers.add(2);
+			timesNumbers.add(3);
+			timesNumbers.add(4);
 		}
 		//時間年休上限時間、時間年休使用時間、残時間
-		private static final Map<Integer, String> timeNumbers = new HashMap<>();
+		private static final List<Integer> timeNumbers = new ArrayList<>();
 		static {
-			timeNumbers.put(5, "時間年休上限時間");
-			timeNumbers.put(6, "時間年休使用時間");
-			timesItems.put(7, "残時間");
+			timeNumbers.add(5);
+			timeNumbers.add(6);
+			timeNumbers.add(7);
 		}
 		
 		/**
@@ -86,31 +83,26 @@ public class MaxYearHolidayCanonicalization extends IndependentCanonicalization{
 		 * 項目を歯抜けで受入れようとしている
 		 * @param interm 
 		 */
-		public static Optional<ErrorMessage> getLackItemError(IntermediateResult interm) {
-			val time = hasTimeAllItemNoOrAllNothing(interm, timeNumbers);
-			val times = hasTimeAllItemNoOrAllNothing(interm, timesItems);
-			if(!(time.isPresent() && times.isPresent())) {
-				return Optional.empty();
+		public static List<ExternalImportError> getLackItemError(IntermediateResult interm) {
+			val timeErrors = hasTimeAllItemNoOrAllNothing(interm, timesNumbers)
+					.stream()
+					.map(errorItemNo -> new ExternalImportError(interm.getRowNo(), errorItemNo, "値がありません。"))
+					.collect(Collectors.toList());
+			val timesErrors = hasTimeAllItemNoOrAllNothing(interm, timeNumbers)
+					.stream()
+					.map(errorItemNo -> new ExternalImportError(interm.getRowNo(), errorItemNo, "値がありません。"))
+					.collect(Collectors.toList());
+			if(timeErrors.isEmpty() && timesErrors.isEmpty()) {
+				return Collections.emptyList();
 			}
-			val errorMessage = time.isPresent() ? time.get() : times.get();
-			return Optional.of(errorMessage);
+			return timeErrors.isEmpty() ? timesErrors : timeErrors;
 		}
 		
 		/**
 		 * All or Nothing でTrue,歯抜けの時はは抜けてる項目名 
 		 */
-		private static Optional<ErrorMessage> hasTimeAllItemNoOrAllNothing(IntermediateResult interm, Map<Integer, String> items) {
-			if(items.keySet().stream().allMatch(itemNo -> interm.getItemByNo(itemNo).isPresent())
-		   || items.keySet().stream().allMatch(itemNo -> !interm.getItemByNo(itemNo).isPresent())) {
-				return Optional.empty();
-			}
-			return Optional.of(new ErrorMessage(
-					items.entrySet().stream()
-					.filter(no -> !interm.getItemByNo(no.getKey()).isPresent())
-					.map(item -> item.getValue())
-					.collect(Collectors.joining("、"))
-					+ "の値がありません。"
-				));
+		private static List<Integer> hasTimeAllItemNoOrAllNothing(IntermediateResult interm, List<Integer> items) {
+			return items.stream().filter(t -> !interm.getItemByNo(t).isPresent()).collect(Collectors.toList());
 		}
 	}
 	@Override
