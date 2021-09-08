7ad5a81b64d151408a56de8fb49894a4a6ad54c4
akane_tanaka <akane_tanaka@nittsusystem.co.jp>
2021-09-08 12:37:54 +0900

    不要なRequireを削除、削除ボタンの不具合修正、必須項目の判定の処理を変更

--- nts.uk/uk.com/com.screen/nts.uk.screen.com.app/src/main/java/nts/uk/screen/com/app/cmf/cmf001/b/get/GetExternalImportSettingRequire.java
+++ nts.uk/uk.com/com.screen/nts.uk.screen.com.app/src/main/java/nts/uk/screen/com/app/cmf/cmf001/b/get/GetExternalImportSettingRequire.java
@@ -23,42 +23,38 @@ import nts.uk.screen.com.app.cmf.cmf001.b.get.GetExternalImportSetting.Require;
 @Stateless
 @TransactionAttribute(TransactionAttributeType.SUPPORTS)
 public class GetExternalImportSettingRequire {
-	
+
 	@Inject
 	private ImportableItemsRepository importableItemsRepo;
-	
+
 	@Inject
 	private ExternalImportSettingRepository externalImportSettingRepo;
-	
+
 	@Inject
 	private ReviseItemRepository reviseItemRepo;
-	
+
 	public Require create() {
-		
+
 		return EmbedStopwatch.embed(new RequireImpl());
 	}
-	
+
 	@RequiredArgsConstructor
 	public class RequireImpl implements Require {
-		
+
 		@Override
 		public List<ImportableItem> getImportableItems(ImportingDomainId domainId) {
 			return importableItemsRepo.get(domainId);
 		}
-		
+
 		@Override
 		public List<ExternalImportSetting> getSettings(String companyId) {
 			return externalImportSettingRepo.getAll(companyId);
 		}
-		
+
 		@Override
 		public Optional<ExternalImportSetting> getSetting(String companyId, ExternalImportCode settingCode) {
 			return externalImportSettingRepo.get(companyId, settingCode);
 		}
-		
-		@Override
-		public Optional<ReviseItem> getRevise(String companyId, ExternalImportCode settingCode, int itemNo) {
-			return reviseItemRepo.get(companyId, settingCode, itemNo);
-		}
+
 	}
 }
