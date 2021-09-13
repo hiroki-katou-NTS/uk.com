551d46f9aeec7cd94aea08b38d8b4bcc0dd003e8
NSVN-PhongTQ <phong_tq@nittsusystem-vn.com>
2021-09-13 16:39:49 +0700

    # change name kdl005 old

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdl/0005/a/kdl005.a.start.ts
+++
@@ -1,8 +0,0 @@
-module nts.uk.at.view.kdl0005.a {
-    __viewContext.ready(function() {
-        var screenModel = new nts.uk.at.view.kdl0005.a.viewmodel.ScreenModel();
-        screenModel.startPage().done(function() {
-            __viewContext.bind(screenModel);
-        });
-    });
-}
