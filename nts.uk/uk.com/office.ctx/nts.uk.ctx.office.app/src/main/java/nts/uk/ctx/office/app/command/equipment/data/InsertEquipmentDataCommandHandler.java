60c5141ecff77dee68d4a22e7e40d62b48cae200
nws-dungdv <dungdv@newwave.vn>
2021-09-14 11:58:28 +0700

    refactor

--- nts.uk/uk.com/office.ctx/nts.uk.ctx.office.app/src/main/java/nts/uk/ctx/office/app/command/equipment/data/InsertEquipmentDataCommandHandler.java
+++ nts.uk/uk.com/office.ctx/nts.uk.ctx.office.app/src/main/java/nts/uk/ctx/office/app/command/equipment/data/InsertEquipmentDataCommandHandler.java
@@ -3,6 +3,9 @@ package nts.uk.ctx.office.app.command.equipment.data;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 import javax.inject.Inject;
 
 import nts.arc.error.BundledBusinessException;
@@ -20,6 +23,8 @@ import nts.uk.ctx.office.dom.equipment.data.RequireImpl;
 import nts.uk.ctx.office.dom.equipment.information.EquipmentCode;
 import nts.uk.shr.com.context.AppContexts;
 
+@Stateless
+@TransactionAttribute(TransactionAttributeType.SUPPORTS)
 public class InsertEquipmentDataCommandHandler extends CommandHandler<EquipmentDataCommand> {
 	
 	@Inject
