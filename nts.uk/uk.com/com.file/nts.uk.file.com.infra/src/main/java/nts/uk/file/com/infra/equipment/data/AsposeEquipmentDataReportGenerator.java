a62e43601c58708579b53404a4eba7247a6d015c
nws-dungdv <dungdv@newwave.vn>
2021-09-14 13:59:36 +0700

    refactor

--- nts.uk/uk.com/com.file/nts.uk.file.com.infra/src/main/java/nts/uk/file/com/infra/equipment/data/AsposeEquipmentDataReportGenerator.java
+++ nts.uk/uk.com/com.file/nts.uk.file.com.infra/src/main/java/nts/uk/file/com/infra/equipment/data/AsposeEquipmentDataReportGenerator.java
@@ -1,10 +1,13 @@
 package nts.uk.file.com.infra.equipment.data;
 
+import javax.ejb.Stateless;
+
 import nts.arc.layer.infra.file.export.FileGeneratorContext;
 import nts.uk.file.com.app.equipment.data.EquipmentDataExportDataSource;
 import nts.uk.file.com.app.equipment.data.EquipmentDataExportGenerator;
 import nts.uk.shr.infra.file.report.aspose.cells.AsposeCellsReportGenerator;
 
+@Stateless
 public class AsposeEquipmentDataReportGenerator extends AsposeCellsReportGenerator
 		implements EquipmentDataExportGenerator {
 
