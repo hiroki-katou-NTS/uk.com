10393fbb47e7640309a1a9509985eec8ee94879f
nws-ductm <ductm@newwave.vn>
2021-09-14 09:06:00 +0700

    Update screenQuery

--- nts.uk/uk.com/com.screen/nts.uk.screen.com.app/src/main/java/nts/uk/screen/com/app/find/equipment/data/EquipmentInitSettingDto.java
+++ nts.uk/uk.com/com.screen/nts.uk.screen.com.app/src/main/java/nts/uk/screen/com/app/find/equipment/data/EquipmentInitSettingDto.java
@@ -1,7 +1,11 @@
 package nts.uk.screen.com.app.find.equipment.data;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import nts.uk.ctx.office.dom.equipment.achievement.EquipmentPerformInputFormatSetting;
+import nts.uk.ctx.office.dom.equipment.achievement.EquipmentUsageRecordItemSetting;
 
 @Data
 @AllArgsConstructor
@@ -10,10 +14,10 @@ public class EquipmentInitSettingDto {
 	/**
 	 * 設備利用実績の項目設定<List>
 	 */
-	//TODO
+	private List<EquipmentUsageRecordItemSetting> itemSettings;
 	
 	/**
 	 * 設備の実績入力フォーマット設定<List>
 	 */
-	//TODO
+	private EquipmentPerformInputFormatSetting formatSetting; 
 }
