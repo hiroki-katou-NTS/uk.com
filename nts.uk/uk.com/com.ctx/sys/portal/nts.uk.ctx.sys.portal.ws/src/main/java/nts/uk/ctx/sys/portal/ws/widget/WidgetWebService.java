9fba935e643c77970a478edaac572fba72c96d54
nws-dungdv <dungdv@newwave.vn>
2021-09-15 11:59:53 +0700

    #119881 remove ccg009

--- nts.uk/uk.com/com.ctx/sys/portal/nts.uk.ctx.sys.portal.ws/src/main/java/nts/uk/ctx/sys/portal/ws/widget/WidgetWebService.java
+++ nts.uk/uk.com/com.ctx/sys/portal/nts.uk.ctx.sys.portal.ws/src/main/java/nts/uk/ctx/sys/portal/ws/widget/WidgetWebService.java
@@ -8,8 +8,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 import nts.arc.layer.ws.WebService;
-import nts.uk.ctx.sys.portal.app.command.widget.AddWidgetCommand;
-import nts.uk.ctx.sys.portal.app.command.widget.DeleteWidgetCommand;
 import nts.uk.ctx.sys.portal.app.command.widget.UpdateWidgetCommand;
 import nts.uk.ctx.sys.portal.app.command.widget.UpdateWidgetCommandHandler;
 import nts.uk.ctx.sys.portal.app.find.widget.OptionalWidgetDto;
@@ -19,14 +17,8 @@ import nts.uk.ctx.sys.portal.app.find.widget.OptionalWidgetFinder;
 @Produces("application/json")
 public class WidgetWebService extends WebService {
 
-//	@Inject
-//	private AddWidgetCommandHandler addWidgetCommandHandler;
-
 	@Inject
 	private UpdateWidgetCommandHandler updateWidgetCommandHandler;
-
-//	@Inject
-//	private DeleteWidgetCommandHandler deleteWidgetCommandHandler;
 
 	@Inject
 	private OptionalWidgetFinder optionalFinder;
@@ -38,20 +30,8 @@ public class WidgetWebService extends WebService {
 	}
 
 	@POST
-	@Path("add")
-	public void add(AddWidgetCommand command) {
-//		this.addWidgetCommandHandler.handle(command);
-	}
-
-	@POST
 	@Path("update")
 	public void update(UpdateWidgetCommand command) {
 		this.updateWidgetCommandHandler.handle(command);
-	}
-
-	@POST
-	@Path("delete")
-	public void delete(DeleteWidgetCommand command) {
-//		this.deleteWidgetCommandHandler.handle(command);
 	}
 }
