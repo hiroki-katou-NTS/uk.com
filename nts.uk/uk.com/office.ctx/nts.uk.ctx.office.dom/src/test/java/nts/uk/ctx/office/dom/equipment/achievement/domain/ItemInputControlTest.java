3e533b039ea0aeeeb43ae629574ee3604098962c
nws-dungdv <dungdv@newwave.vn>
2021-09-16 12:22:05 +0700

    add ItemInputControlTest

---
+++ nts.uk/uk.com/office.ctx/nts.uk.ctx.office.dom/src/test/java/nts/uk/ctx/office/dom/equipment/achievement/domain/ItemInputControlTest.java
@@ -0,0 +1,276 @@
+package nts.uk.ctx.office.dom.equipment.achievement.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import mockit.integration.junit4.JMockit;
+import nts.arc.error.ErrorMessage;
+import nts.arc.error.I18NErrorMessage;
+import nts.arc.i18n.I18NText;
+import nts.arc.testing.assertion.NtsAssert;
+import nts.uk.ctx.office.dom.equipment.achievement.EquipmentItemNo;
+import nts.uk.ctx.office.dom.equipment.achievement.ItemClassification;
+import nts.uk.ctx.office.dom.equipment.achievement.ItemInputControl;
+import nts.uk.ctx.office.dom.equipment.achievement.UsageItemName;
+import nts.uk.ctx.office.dom.equipment.data.ActualItemUsageValue;
+
+@RunWith(JMockit.class)
+public class ItemInputControlTest {
+	
+	/**
+	 * inv-1 項目分類　＝　文字　&&　桁数.isPresent()
+	 */
+	@Test
+	public void constructorWithInv1() {
+		NtsAssert.businessException("Msg_2248", () -> ItemInputControlTestHelper.caseInv1());
+	}
+	
+	/**
+	 * inv-2 （項目分類　＝　数字　||　時間）　&&　最大値.isPresent()
+	 */
+	@Test
+	public void constructorWithInv2() {
+		NtsAssert.businessException("Msg_2249", () -> ItemInputControlTestHelper.caseInv2(true));
+		NtsAssert.businessException("Msg_2249", () -> ItemInputControlTestHelper.caseInv2(false));
+	}
+	
+	/**
+	 * inv-3 （項目分類　＝　数字　||　時間）　&&　最小値.isPresent()
+	 */
+	@Test
+	public void constructorWithInv3() {
+		NtsAssert.businessException("Msg_2250", () -> ItemInputControlTestHelper.caseInv3(true));
+		NtsAssert.businessException("Msg_2250", () -> ItemInputControlTestHelper.caseInv3(false));
+	}
+	
+	/**
+	 * [C-1] 新規追加
+	 * 項目分類　＝　文字
+	 */
+	@Test
+	public void constructorWithText() {
+		ItemInputControl itemText = ItemInputControlTestHelper.newAddition(ItemClassification.TEXT);
+		NtsAssert.invokeGetters(itemText);
+	}
+	
+	/**
+	 * [C-1] 新規追加
+	 * 項目分類　＝　数字
+	 */
+	@Test
+	public void constructorWithNumber() {
+		ItemInputControl itemNumber = ItemInputControlTestHelper.newAddition(ItemClassification.NUMBER);
+		NtsAssert.invokeGetters(itemNumber);
+	}
+	
+	/**
+	 * [C-1] 新規追加
+	 * 項目分類　＝　時間
+	 */
+	@Test
+	public void constructorWithTime() {
+		ItemInputControl itemTime = ItemInputControlTestHelper.newAddition(ItemClassification.TIME);
+		NtsAssert.invokeGetters(itemTime);
+	}
+	
+	/**
+	 * [1] 項目制御のエラーをチェックする
+	 * if　@必須　＝＝　true　&&　入力値.isEmpty()　＝＝　true
+	 */
+	@Test
+	public void checkErrorsTest1() {
+		ItemInputControl item = ItemInputControlTestHelper.caseRequire(true);
+		EquipmentItemNo itemNo = new EquipmentItemNo("1");
+		UsageItemName itemName = new UsageItemName("ItemName");
+		Optional<ActualItemUsageValue> inputVal = Optional.empty();
+		Optional<ErrorMessage> actual = item.checkErrors(itemNo, itemName, inputVal);
+		ErrorMessage expect = new I18NErrorMessage(I18NText.main("Msg_2228").addRaw(itemName.v()).build());
+		
+		assertThat(actual).isPresent();
+		assertThat(actual.get()).isEqualByComparingTo(expect);
+	}
+	
+	/**
+	 * [1] 項目制御のエラーをチェックする
+	 * if　入力値.isEmpty()　＝＝　true
+	 */
+	@Test
+	public void checkErrorsTest2() {
+		ItemInputControl item = ItemInputControlTestHelper.caseRequire(false);
+		EquipmentItemNo itemNo = new EquipmentItemNo("1");
+		UsageItemName itemName = new UsageItemName("ItemName");
+		Optional<ActualItemUsageValue> inputVal = Optional.empty();
+		Optional<ErrorMessage> actual = item.checkErrors(itemNo, itemName, inputVal);
+		
+		assertThat(actual).isNotPresent();
+	}
+	
+	/**
+	 * [1] 項目制御のエラーをチェックする
+	 * switch: @項目分類
+	 * case 文字
+	 * if　＠桁数　＜　入力値.length()
+	 */
+	@Test
+	public void checkErrorsTest3() {
+		ItemInputControl item = ItemInputControlTestHelper.caseText(19);
+		EquipmentItemNo itemNo = new EquipmentItemNo("1");
+		UsageItemName itemName = new UsageItemName("ItemName");
+		Optional<ActualItemUsageValue> inputVal = Optional.of(new ActualItemUsageValue("ActualItemUsageValue"));
+		Optional<ErrorMessage> actual = item.checkErrors(itemNo, itemName, inputVal);
+		ErrorMessage expect = new I18NErrorMessage(I18NText.main("Msg_2229")
+				.addRaw(itemName.v())
+				.addRaw(item.getDigitsNo().get())
+				.build());
+		assertThat(actual).isPresent();
+		assertThat(actual.get()).isEqualByComparingTo(expect);
+	}
+	
+	/**
+	 * [1] 項目制御のエラーをチェックする
+	 * switch: @項目分類
+	 * case 文字
+	 * if　＠桁数　>　入力値.length()
+	 */
+	@Test
+	public void checkErrorsTest4() {
+		ItemInputControl item = ItemInputControlTestHelper.caseText(21);
+		EquipmentItemNo itemNo = new EquipmentItemNo("1");
+		UsageItemName itemName = new UsageItemName("ItemName");
+		Optional<ActualItemUsageValue> inputVal = Optional.of(new ActualItemUsageValue("ActualItemUsageValue"));
+		Optional<ErrorMessage> actual = item.checkErrors(itemNo, itemName, inputVal);
+		
+		assertThat(actual).isNotPresent();
+	}
+	
+	/**
+	 * [1] 項目制御のエラーをチェックする
+	 * switch: @項目分類
+	 * case 数字
+	 * if　＠最大値　＜　入力値
+	 */
+	@Test
+	public void checkErrorsTest5() {
+		ItemInputControl item = ItemInputControlTestHelper.caseNumber(10, 1);
+		EquipmentItemNo itemNo = new EquipmentItemNo("1");
+		UsageItemName itemName = new UsageItemName("ItemName");
+		Optional<ActualItemUsageValue> inputVal = Optional.of(new ActualItemUsageValue("11"));
+		Optional<ErrorMessage> actual = item.checkErrors(itemNo, itemName, inputVal);
+		
+		ErrorMessage expect = new I18NErrorMessage(I18NText.main("Msg_2246")
+				.addRaw(itemName.v())
+				.addRaw(item.getMinimum().get().v())
+				.addRaw(item.getMaximum().get().v())
+				.build());
+		assertThat(actual).isPresent();
+		assertThat(actual.get()).isEqualByComparingTo(expect);
+	}
+	
+	/**
+	 * [1] 項目制御のエラーをチェックする
+	 * switch: @項目分類
+	 * case 数字
+	 * if　＠最小値　>　入力値
+	 */
+	@Test
+	public void checkErrorsTest6() {
+		ItemInputControl item = ItemInputControlTestHelper.caseNumber(10, 1);
+		EquipmentItemNo itemNo = new EquipmentItemNo("1");
+		UsageItemName itemName = new UsageItemName("ItemName");
+		Optional<ActualItemUsageValue> inputVal = Optional.of(new ActualItemUsageValue("0"));
+		Optional<ErrorMessage> actual = item.checkErrors(itemNo, itemName, inputVal);
+		
+		ErrorMessage expect = new I18NErrorMessage(I18NText.main("Msg_2246")
+				.addRaw(itemName.v())
+				.addRaw(item.getMinimum().get().v())
+				.addRaw(item.getMaximum().get().v())
+				.build());
+		assertThat(actual).isPresent();
+		assertThat(actual.get()).isEqualByComparingTo(expect);
+	}
+	
+	/**
+	 * [1] 項目制御のエラーをチェックする
+	 * switch: @項目分類
+	 * case 数字
+	 * if ＠最小値　<　入力値 < ＠最大値
+	 */
+	@Test
+	public void checkErrorsTest7() {
+		ItemInputControl item = ItemInputControlTestHelper.caseNumber(10, 1);
+		EquipmentItemNo itemNo = new EquipmentItemNo("1");
+		UsageItemName itemName = new UsageItemName("ItemName");
+		Optional<ActualItemUsageValue> inputVal = Optional.of(new ActualItemUsageValue("9"));
+		Optional<ErrorMessage> actual = item.checkErrors(itemNo, itemName, inputVal);
+		
+		assertThat(actual).isNotPresent();
+	}
+	
+	/**
+	 * [1] 項目制御のエラーをチェックする
+	 * switch: @項目分類
+	 * case 時間
+	 * if　＠最大値　＜　入力値
+	 */
+	@Test
+	public void checkErrorsTest8() {
+		ItemInputControl item = ItemInputControlTestHelper.caseTime(3600, 1000);
+		EquipmentItemNo itemNo = new EquipmentItemNo("1");
+		UsageItemName itemName = new UsageItemName("ItemName");
+		Optional<ActualItemUsageValue> inputVal = Optional.of(new ActualItemUsageValue("4100"));
+		Optional<ErrorMessage> actual = item.checkErrors(itemNo, itemName, inputVal);
+		
+		ErrorMessage expect = new I18NErrorMessage(I18NText.main("Msg_2247")
+				.addRaw(itemName.v())
+				.addRaw(ItemInputControlTestHelper.formatTime(item.getMinimum().get().v()))
+				.addRaw(ItemInputControlTestHelper.formatTime(item.getMaximum().get().v()))
+				.build());
+		assertThat(actual).isPresent();
+		assertThat(actual.get()).isEqualByComparingTo(expect);
+	}
+	
+	/**
+	 * [1] 項目制御のエラーをチェックする
+	 * switch: @項目分類
+	 * case 時間
+	 * if　＠最小値　>　入力値
+	 */
+	@Test
+	public void checkErrorsTest9() {
+		ItemInputControl item = ItemInputControlTestHelper.caseTime(3601, 1000);
+		EquipmentItemNo itemNo = new EquipmentItemNo("1");
+		UsageItemName itemName = new UsageItemName("ItemName");
+		Optional<ActualItemUsageValue> inputVal = Optional.of(new ActualItemUsageValue("900"));
+		Optional<ErrorMessage> actual = item.checkErrors(itemNo, itemName, inputVal);
+		
+		ErrorMessage expect = new I18NErrorMessage(I18NText.main("Msg_2247")
+				.addRaw(itemName.v())
+				.addRaw(ItemInputControlTestHelper.formatTime(item.getMinimum().get().v()))
+				.addRaw(ItemInputControlTestHelper.formatTime(item.getMaximum().get().v()))
+				.build());
+		assertThat(actual).isPresent();
+		assertThat(actual.get()).isEqualByComparingTo(expect);
+	}
+	
+	/**
+	 * [1] 項目制御のエラーをチェックする
+	 * switch: @項目分類
+	 * case 時間
+	 * if ＠最小値　<　入力値 < ＠最大値
+	 */
+	@Test
+	public void checkErrorsTest10() {
+		ItemInputControl item = ItemInputControlTestHelper.caseTime(3600, 1000);
+		EquipmentItemNo itemNo = new EquipmentItemNo("1");
+		UsageItemName itemName = new UsageItemName("ItemName");
+		Optional<ActualItemUsageValue> inputVal = Optional.of(new ActualItemUsageValue("3000"));
+		Optional<ErrorMessage> actual = item.checkErrors(itemNo, itemName, inputVal);
+		
+		assertThat(actual).isNotPresent();
+	}
+	
+}
---
+++ nts.uk/uk.com/office.ctx/nts.uk.ctx.office.dom/src/test/java/nts/uk/ctx/office/dom/equipment/achievement/domain/ItemInputControlTestHelper.java
@@ -0,0 +1,127 @@
+package nts.uk.ctx.office.dom.equipment.achievement.domain;
+
+import java.util.Optional;
+
+import nts.uk.ctx.office.dom.equipment.achievement.ItemClassification;
+import nts.uk.ctx.office.dom.equipment.achievement.ItemInputControl;
+import nts.uk.ctx.office.dom.equipment.achievement.MaximumUsageRecord;
+import nts.uk.ctx.office.dom.equipment.achievement.MinimumUsageRecord;
+
+public class ItemInputControlTestHelper {
+	
+	/**
+	 * inv-1
+	 */
+	public static ItemInputControl caseInv1() {
+		// 項目分類　＝　文字
+		ItemClassification itemCls = ItemClassification.TEXT;
+		boolean require = true;
+		// 桁数.isPresent()
+		Optional<Integer> digitsNo = Optional.empty();
+		Optional<MaximumUsageRecord> maximum = Optional.empty();
+		Optional<MinimumUsageRecord> minimum = Optional.empty();
+		return new ItemInputControl(itemCls, require, digitsNo, maximum, minimum);
+	}
+	
+	/**
+	 * inv-2
+	 * @param isNumber 項目分類　＝　数字
+	 * @param isPresentMax 最大値.isPresent()
+	 */
+	public static ItemInputControl caseInv2(boolean isNumber) {
+		// 項目分類　＝　数字　||　時間
+		ItemClassification itemCls = isNumber
+				? ItemClassification.NUMBER
+				: ItemClassification.TIME;
+		boolean require = true;
+		Optional<Integer> digitsNo = Optional.empty();
+		// 最大値.isPresent()
+		Optional<MaximumUsageRecord> maximum = Optional.empty();
+		Optional<MinimumUsageRecord> minimum = Optional.empty();
+		return new ItemInputControl(itemCls, require, digitsNo, maximum, minimum);
+	}
+	
+	/**
+	 * inv-3
+	 * @param isNumber 項目分類　＝　数字
+	 */
+	public static ItemInputControl caseInv3(boolean isNumber) {
+		// 項目分類　＝　数字　||　時間
+		ItemClassification itemCls = isNumber
+				? ItemClassification.NUMBER
+				: ItemClassification.TIME;
+		boolean require = true;
+		Optional<Integer> digitsNo = Optional.empty();
+		Optional<MaximumUsageRecord> maximum = Optional.of(new MaximumUsageRecord(10));
+		// 最小値.isPresent()
+		Optional<MinimumUsageRecord> minimum = Optional.empty();
+		return new ItemInputControl(itemCls, require, digitsNo, maximum, minimum);
+	}
+	
+	/**
+	 * 新規追加
+	 */
+	public static ItemInputControl newAddition(ItemClassification classification) {
+		ItemClassification itemCls = classification;
+		boolean require = true;
+		Optional<Integer> digitsNo = Optional.of(10);
+		Optional<MaximumUsageRecord> maximum = Optional.of(new MaximumUsageRecord(10));
+		Optional<MinimumUsageRecord> minimum = Optional.of(new MinimumUsageRecord(1));
+		return new ItemInputControl(itemCls, require, digitsNo, maximum, minimum);
+	}
+	
+	/**
+	 * @必須: true | false
+	 */
+	public static ItemInputControl caseRequire(boolean require) {
+		ItemClassification itemCls = ItemClassification.TEXT;
+		Optional<Integer> digitsNo = Optional.of(10);
+		Optional<MaximumUsageRecord> maximum = Optional.of(new MaximumUsageRecord(10));
+		Optional<MinimumUsageRecord> minimum = Optional.of(new MinimumUsageRecord(1));
+		return new ItemInputControl(itemCls, require, digitsNo, maximum, minimum);
+	}
+	
+	/**
+	 * case 文字
+	 */
+	public static ItemInputControl caseText(Integer digitsNumber) {
+		ItemClassification itemCls = ItemClassification.TEXT;
+		Optional<Integer> digitsNo = Optional.of(digitsNumber);
+		Optional<MaximumUsageRecord> maximum = Optional.of(new MaximumUsageRecord(10));
+		Optional<MinimumUsageRecord> minimum = Optional.of(new MinimumUsageRecord(1));
+		return new ItemInputControl(itemCls, true, digitsNo, maximum, minimum);
+	}
+	
+	/**
+	 * case 数字
+	 */
+	public static ItemInputControl caseNumber(Integer max, Integer min) {
+		ItemClassification itemCls = ItemClassification.NUMBER;
+		Optional<Integer> digitsNo = Optional.empty();
+		Optional<MaximumUsageRecord> maximum = Optional.of(new MaximumUsageRecord(max));
+		Optional<MinimumUsageRecord> minimum = Optional.of(new MinimumUsageRecord(min));
+		return new ItemInputControl(itemCls, true, digitsNo, maximum, minimum);
+	}
+	
+	/**
+	 * case 時間
+	 */
+	public static ItemInputControl caseTime(Integer max, Integer min) {
+		ItemClassification itemCls = ItemClassification.TIME;
+		Optional<Integer> digitsNo = Optional.empty();
+		Optional<MaximumUsageRecord> maximum = Optional.of(new MaximumUsageRecord(max));
+		Optional<MinimumUsageRecord> minimum = Optional.of(new MinimumUsageRecord(min));
+		return new ItemInputControl(itemCls, true, digitsNo, maximum, minimum);
+	}
+	
+	/**
+	 * Format data to H:MM
+	 * @param minute
+	 * @return H:MM
+	 */
+	public static String formatTime(Integer minute) {
+		int h = Math.abs(minute) / 60;
+		int m = Math.abs(minute) % 60;
+		return h + ":" + (m < 10 ? "0" + m : m);
+	}
+}
