aca1298036ed304b12c83a6de9a3cac65edcd631
masaakijinno <fairvancs@gmail.com>
2021-09-21 22:41:57 +0900

    テストコードエラー修正

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/remainingnumber/annualleave/export/AddTempAnnualLeaveMngs.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/remainingnumber/annualleave/export/AddTempAnnualLeaveMngs.java
@@ -71,7 +71,7 @@ public class AddTempAnnualLeaveMngs {
 						RemainAtr.SINGLE,			//		残数分類　←単一
 						new WorkTypeCode("1"), 	//		勤務種類 = 1
 						new TempAnnualLeaveUsedNumber(
-								Optional.of(new TmpDailyLeaveUsedDayNumber(calcUsedNumber)), null), 	//		年休使用数．日数　←求めた使用数
+								Optional.of(new TmpDailyLeaveUsedDayNumber(calcUsedNumber)), Optional.empty()), 	//		年休使用数．日数　←求めた使用数
 						Optional.empty()));	//		時間休暇種類．時間消化区分 = 1
 				});
 		}
