1373a377bfb181af83a6231d37d21df7abdb4c03
NSC-LanLT <lan-lt@nittsusystem.co.jp>
2021-09-24 11:29:38 +0900

    OutCellのStringRegExを修正しました。

--- nts.uk/uk.at/at.ctx/aggregation/nts.uk.ctx.at.aggregation.dom/src/main/java/nts/uk/ctx/at/aggregation/dom/form9/OutputCell.java
+++ nts.uk/uk.at/at.ctx/aggregation/nts.uk.ctx.at.aggregation.dom/src/main/java/nts/uk/ctx/at/aggregation/dom/form9/OutputCell.java
@@ -8,7 +8,7 @@ import nts.arc.primitive.constraint.StringRegEx;
  * @author lan_lt
  *
  */
-@StringRegEx("[a-zA-Z]{1}[1-9]{1}[0-9]{0,6}")
+@StringRegEx("[a-zA-Z]{1,3}[1-9]{1}[0-9]{0,6}")
 public class OutputCell extends UpperCaseAlphaNumericPrimitiveValue<OutputCell> {
 	/** serialVersionUID */
 	private static final long serialVersionUID = 1L;
