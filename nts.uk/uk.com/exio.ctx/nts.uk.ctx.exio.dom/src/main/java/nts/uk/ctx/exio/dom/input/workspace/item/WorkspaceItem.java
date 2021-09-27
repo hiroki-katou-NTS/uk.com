50658073090a0c90bb032a599d2fbb61a5a7775e
CORP\keisuke_hoshina <keisuke_hoshina@nittsusystem.co.jp>
2021-09-27 16:19:11 +0900

    ImportableItemのItemTypeとWorkspaceItemのDataItemの整合性　機能強化　Enum以外は動作確認まで完了。 残：Enumの実装、リファクタ、レビュー

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/workspace/item/WorkspaceItem.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/workspace/item/WorkspaceItem.java
@@ -1,10 +1,15 @@
 package nts.uk.ctx.exio.dom.input.workspace.item;
 
+import java.util.Optional;
+
 import lombok.Value;
+import lombok.val;
 import nts.uk.ctx.exio.dom.input.domain.ImportingDomainId;
+import nts.uk.ctx.exio.dom.input.importableitem.DomainConstraint;
 import nts.uk.ctx.exio.dom.input.importableitem.ImportableItem;
 import nts.uk.ctx.exio.dom.input.workspace.datatype.DataType;
 import nts.uk.ctx.exio.dom.input.workspace.datatype.DataTypeConfiguration;
+import nts.uk.shr.infra.web.component.validation.Helper;
 
 /**
  * ワークスペースの項目
@@ -30,6 +35,73 @@ public class WorkspaceItem {
 	 * @return
 	 */
 	public boolean diagnose(ImportableItem importableItem) {
-		return dataTypeConfig.getType() == DataType.of(importableItem.getItemType());
+		val annotations = new AnnotationValues(importableItem.getDomainConstraint());
+		
+		return dataTypeConfig.getType().equals(DataType.of(importableItem.getItemType()))
+			&&	annotations.isSamesLengths(dataTypeConfig.getLength(), dataTypeConfig.getScale());
+	}
+	
+	private class AnnotationValues{
+		private Optional<Integer> maxLength = Optional.empty();
+		private Optional<Integer> mantissaMaxLength = Optional.empty();
+		
+		public AnnotationValues(Optional<DomainConstraint> domainContraint) {
+			domainContraint.ifPresent(constraint ->{
+				Class<?> pvClass = constraint.getConstraintClass();
+				Helper.processConstraints(pvClass, (name, value) ->{
+					sw(name, value);
+				});			
+			});
+		}
+
+		private void sw(String name, String value) {
+			switch(name) {
+				case "maxLength":
+					maxLength = Optional.of(Integer.parseInt(value));
+					break;
+				case "mantissaMaxLength":
+					mantissaMaxLength = Optional.of(Integer.parseInt(value));
+					break;
+				case "min":
+				case "max":
+					//HalfInteger系はここで対処
+					if(value.contains(".")) {
+						val vMantissaMaxLength = value.split("\\.")[1].length();	
+						val vTotalLength = Integer.toString(Math.abs(Integer.valueOf(value.split("\\.")[0]))).length() + vMantissaMaxLength;
+						if(!maxLength.isPresent() || (vTotalLength) > maxLength.get()) {
+							maxLength = Optional.of(vTotalLength);
+						}
+						if(!mantissaMaxLength.isPresent() || vMantissaMaxLength > mantissaMaxLength.get()) {
+							mantissaMaxLength = Optional.of(vMantissaMaxLength);
+						}						
+					}
+					//時分・時刻はここで
+					else if(value.contains(":")){
+						val parts = value.split(":");
+						val vLength = Integer.toString(Math.abs((Integer.parseInt(parts[0]) * 60 + Integer.parseInt(parts[1])))).length();
+						if(!maxLength.isPresent()||vLength > maxLength.get()) {
+							maxLength = Optional.of(vLength); 
+						}
+					}
+					else {
+						val absValueLength = Integer.toString(Math.abs(Integer.parseInt(value))).length();
+						if(!maxLength.isPresent()||absValueLength > maxLength.get()){
+							maxLength = Optional.of(absValueLength);
+						}
+					}
+					break;
+			}
+		}
+		
+		public boolean isSamesLengths(int maxLength, int mantissaMaxLength) {
+			return isSame(this.maxLength, maxLength)
+				&&   isSame(this.mantissaMaxLength, mantissaMaxLength);
+		}
+		
+		private boolean isSame(Optional<Integer> v ,int length) {
+			return v.map(value -> v.get().intValue() == length)
+					.orElse(length == 0);
+		}
+		
 	}
 }
