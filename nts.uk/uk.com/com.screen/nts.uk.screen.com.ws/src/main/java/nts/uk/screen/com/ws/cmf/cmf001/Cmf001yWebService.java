466fb2d4a5a294db0daa3675f52de3157f7c219c
ai_muto <ai_muto@nittsusystem.co.jp>
2021-09-29 13:20:48 +0900

    CSVベース受入設定の登録画面　仮置き

---
+++ nts.uk/uk.com/com.screen/nts.uk.screen.com.ws/src/main/java/nts/uk/screen/com/ws/cmf/cmf001/Cmf001yWebService.java
@@ -0,0 +1,51 @@
+package nts.uk.screen.com.ws.cmf.cmf001;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import nts.uk.screen.com.app.cmf.cmf001.b.get.ExternalImportSettingDto;
+import nts.uk.screen.com.app.cmf.cmf001.b.get.ExternalImportSettingListItemDto;
+import nts.uk.screen.com.app.cmf.cmf001.b.get.GetExternalImportSetting;
+import nts.uk.screen.com.app.cmf.cmf001.b.get.GetLayout;
+import nts.uk.screen.com.app.cmf.cmf001.b.save.Cmf001bSaveCommand;
+import nts.uk.screen.com.app.cmf.cmf001.b.save.Cmf001bSaveCommandHandler;
+
+@Path("screen/com/cmf/cmf001/y")
+@Produces(MediaType.APPLICATION_JSON)
+public class Cmf001yWebService {
+	
+	@Inject
+	private GetExternalImportSetting setting;
+	
+	@Inject
+	private GetLayout layout;
+	
+	@Inject
+	private Cmf001bSaveCommandHandler saveCmd;
+	
+	@POST
+	@Path("get/setting/csvbase")
+	public List<ExternalImportSettingListItemDto> getAll() {
+		List<ExternalImportSettingListItemDto> result = setting.getCsvBase();
+		return result;
+	}
+	
+	@POST
+	@Path("get/setting/{settingCode}")
+	public ExternalImportSettingDto get(@PathParam("settingCode") String settingCode) {
+		ExternalImportSettingDto result = setting.get(settingCode);
+		return result;
+	}
+
+	@POST
+	@Path("save")
+	public void save(Cmf001bSaveCommand command) {
+		saveCmd.handle(command);
+	}
+}
