bfa8321c4c1c0a5b500e635cf44bc2da735d75bc
NTS-hayata_maekawa <hayata_maekawa@nittsusystem.co.jp>
2021-10-01 18:45:21 +0900

    期間内の介護使用明細を取得すると期間内の子の看護使用明細を取得するのDS実装

---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/remainingnumber/childcarenurse/childcare/GetUsageDetailChildCareService.java
@@ -0,0 +1,43 @@
+package nts.uk.ctx.at.record.dom.remainingnumber.childcarenurse.childcare;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import nts.arc.time.GeneralDate;
+import nts.arc.time.calendar.period.DatePeriod;
+import nts.uk.ctx.at.shared.dom.remainingnumber.algorithm.DailyInterimRemainMngData;
+import nts.uk.ctx.at.shared.dom.remainingnumber.annualleave.ReferenceAtr;
+import nts.uk.ctx.at.shared.dom.remainingnumber.nursingcareleavemanagement.childcare.interimdata.TempChildCareManagement;
+
+public class GetUsageDetailChildCareService {
+	/**
+	 * 期間内の子の看護使用明細を取得する
+	 * @param companyId　会社ID
+	 * @param employeeId　社員ID
+	 * @param period　期間
+	 * @param criteriaDate 基準日
+	 * @param referenceAtr　参照先区分
+	 * @param require
+	 * @return
+	 */
+	public static List<TempChildCareManagement> getUsageDetailCareService(
+			String companyId, String employeeId,DatePeriod period, GeneralDate criteriaDate,
+			ReferenceAtr referenceAtr, Require require){
+		
+		
+		List<DailyInterimRemainMngData> interimRemainMng = require.getHolidayDetailByPeriod(
+				companyId, employeeId, period, criteriaDate, referenceAtr, require);
+		
+		return interimRemainMng.stream()
+				.filter(c -> !c.getRecAbsData().isEmpty() && !c.getChildCareData().isEmpty())
+				.map(c -> {
+					return c.getChildCareData();
+				}).flatMap(List::stream).collect(Collectors.toList());
+	}
+
+	public static interface Require{
+		List<DailyInterimRemainMngData> getHolidayDetailByPeriod(String companyId, String employeeId, 
+				DatePeriod period, GeneralDate criteriaDate, ReferenceAtr referenceAtr,Require require);
+	}
+	
+}
