6f43499cf7a13cd2f440e2f1cfb0d927d43d365b
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2021-10-01 10:57:15 +0700

    Kdw006-Add Command: 選択肢を更新登録する and WS: 選択肢を新規登録する

---
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/kdw/kdw006/k/UpdateAndRegisterOptionsCommandHandler.java
@@ -0,0 +1,41 @@
+package nts.uk.ctx.at.record.app.command.kdw.kdw006.k;
+
+import java.util.Optional;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.inject.Inject;
+
+import nts.arc.layer.app.command.CommandHandler;
+import nts.arc.layer.app.command.CommandHandlerContext;
+import nts.uk.ctx.at.record.dom.jobmanagement.tasksupplementaryinforitemsetting.TaskSupInfoChoicesDetail;
+import nts.uk.ctx.at.record.dom.jobmanagement.tasksupplementaryinforitemsetting.TaskSupInfoChoicesHistoryRepository;
+import nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.dailyattendance.timesheet.ouen.ChoiceCode;
+
+/**
+ * Command: 選択肢を更新登録する
+ * UKDesign.ドメインモデル.NittsuSystem.UniversalK.就業.contexts.勤務実績.作業管理.作業補足情報項目設定.App.選択肢を更新登録する
+ * @author chungnt
+ *
+ */
+
+@Stateless
+@TransactionAttribute(TransactionAttributeType.SUPPORTS)
+public class UpdateAndRegisterOptionsCommandHandler extends CommandHandler<RegisterNewOptionsCommand> {
+
+	@Inject
+	private TaskSupInfoChoicesHistoryRepository taskRepo;
+	
+	@Override
+	protected void handle(CommandHandlerContext<RegisterNewOptionsCommand> context) {
+		RegisterNewOptionsCommand command = context.getCommand();
+		
+		Optional<TaskSupInfoChoicesDetail> domain = this.taskRepo.get(command.getHistoryId(), command.getItemId(), new ChoiceCode(command.getChoiceCode()));
+		
+		if (domain.isPresent()) {
+			this.taskRepo.update(domain.get());
+		}
+	}
+
+}
