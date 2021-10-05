df900a33281b2be0da884e5e1a21794d9b0ef52c
Hiroki Katou <hiroki_katou@nittsusystem.co.jp>
2021-10-05 09:47:38 +0900

    BugFix#120249

--- nts.uk/uk.com/query/nts.uk.query.app/src/main/java/nts/uk/query/app/employee/LoginEmployeeQuery.java
+++ nts.uk/uk.com/query/nts.uk.query.app/src/main/java/nts/uk/query/app/employee/LoginEmployeeQuery.java
@@ -1,6 +1,7 @@
 package nts.uk.query.app.employee;
 
 import lombok.Value;
+import nts.arc.time.GeneralDate;
 import nts.arc.time.GeneralDateTime;
 
 @Value
@@ -9,5 +10,9 @@ public class LoginEmployeeQuery {
     GeneralDateTime baseDate;
 
     int systemType;
+    
+    public GeneralDate getDate() {
+    	return this.baseDate.toDate();
+    }
 
 }
