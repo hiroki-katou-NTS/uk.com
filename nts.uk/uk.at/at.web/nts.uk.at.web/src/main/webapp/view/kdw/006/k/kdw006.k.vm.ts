a0452443cfd455d7262b9dbd2cba0d4505a8f518
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2021-10-08 09:55:59 +0700

    KDW006-Update UI View K, J

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/006/k/kdw006.k.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/006/k/kdw006.k.vm.ts
@@ -3,24 +3,34 @@
 module nts.uk.at.view.kmk006.k {
 
     const API = {
-        LIST_WORK_INFO: 'at/record/kdw006/view-k/get-list-work'
+        LIST_WORK_INFO: 'at/record/kdw006/view-k/get-list-work',
+        LIST_HISTORY: 'at/record/kdw006/view-k/get-list-history',
+        WORK_INFO_DATAIL: 'at/record/kdw006/view-k/get-work-info-detail',
+        WORK_INFO_DATAIL_BY_HISTORYS: 'at/record/kdw006/view-k/get-list-work-info-detail',
+        REGISTER: 'at/record/kdw006/view-k/register-new-obtion',
+        UPDATE: 'at/record/kdw006/view-k/update-obtion',
+        REMOTE: 'at/record/kdw006/view-k/delete'
     }
+
+    const dateNow = new Date()
+    const DATE_FORMAT = 'YYYY/MM/DD';
 
     @bean()
     export class ViewModel extends ko.ViewModel {
 
-        public itemList: KnockoutObservableArray<ItemModel>;
-        public selectedCode: KnockoutObservable<string>;
+        public itemWorks: KnockoutObservableArray<IWorkInfo> = ko.observableArray([]);
+        public selectedItemWork: KnockoutObservable<String> = ko.observable('');
         public isEnable: KnockoutObservable<boolean>;
         public isEditable: KnockoutObservable<boolean>;
-
-        public datePeriod: KnockoutObservable<string> = ko.observable('1990/01/28 ~ 9999/12/31');
-        public item: KnockoutObservableArray<Model> = ko.observableArray([]);
-        public currentCode: KnockoutObservable<string> = ko.observable('');
-        public currentName: KnockoutObservable<string> = ko.observable('');
-        public currentMemo: KnockoutObservable<string> = ko.observable('');
-
+        public historys: KnockoutObservableArray<IWorkDetail> = ko.observableArray([]);
+        public currentCode: KnockoutObservable<String> = ko.observable('');
+        public startDate: KnockoutObservable<String> = ko.observable(moment(dateNow).format(DATE_FORMAT));
+        public endDate: KnockoutObservable<String> = ko.observable(moment(dateNow).format(DATE_FORMAT));
         public screenMode: KnockoutObservable<number> = ko.observable(SCREEN_MODE.UPDATE);
+        public historyLocals: IHistory[] = [];
+        public modelHistory: KnockoutObservable<IHistory> = ko.observable();
+        public model: WorkDetail = new WorkDetail();
+
 
         created() {
             const vm = this;
@@ -30,60 +40,351 @@ module nts.uk.at.view.kmk006.k {
                 items: [25, 26, 27, 28, 29]
             }
 
+            vm.selectedItemWork.subscribe(() => {
+                vm.reloadList();
+            });
+
+            vm.currentCode.subscribe(() => {
+                vm.reloadData();
+                vm.screenMode(SCREEN_MODE.UPDATE);
+                vm.screenMode.valueHasMutated();
+            })
+
             vm.$blockui('invisible')
                 .then(() => {
                     vm.$ajax('at', API.LIST_WORK_INFO, inputListWork)
-                        .then((data: any) => {
+                        .then((data: IWorkInfo[]) => {
                             if (data.length === 0) {
-                                // vm.screenMode(SCREEN_MODE.NOT_HISTORY);
+                                vm.screenMode(SCREEN_MODE.NOT_HISTORY);
+                            } else {
+                                vm.screenMode(SCREEN_MODE.UPDATE);
+                                vm.itemWorks(data);
+                                vm.selectedItemWork(data[0].itemId);
                             }
+                        })
+                })
+                .then(() => {
+                    vm.$ajax('at', API.LIST_HISTORY)
+                        .then((history: IHistory[]) => {
+                            vm.historyLocals = history;
+                            vm.getDate();
                         })
                 })
                 .always(() => {
                     vm.$blockui('clear');
-                })
+                });
 
-            vm.itemList = ko.observableArray([
-                { code: '1', name: '基本給' },
-                { code: '2', name: '役職手当' },
-                { code: '3', name: '基本給ながい文字列ながい文字列ながい文字列' }
-            ]);
-
-            vm.selectedCode = ko.observable('1');
             vm.isEnable = ko.observable(true);
             vm.isEditable = ko.observable(true);
-
-            for (var i = 1; i <= 20; i++) {
-                vm.item.push({ id: '000000000' + i, code: '00' + i, name: 'ちゅん　ちゅん' + i });
-            }
 
         }
 
         mounted() {
             const vm = this;
+            const index = _.map(ko.unwrap(vm.historys), m => m.code).indexOf(ko.unwrap(vm.model.code));
 
+            vm.startDate.subscribe(() => vm.reloadList(index));
+            vm.endDate.subscribe(() => vm.startDate.valueHasMutated());
+        }
+
+        getDate() {
+            const vm = this;
+
+            var historys: IHistory[] = [];
+            _.forEach(vm.historyLocals, ((value: IHistory) => {
+                if (value.itemId === ko.unwrap(vm.selectedItemWork)) {
+                    historys.push(value);
+                }
+            }));
+            console.log(ko.unwrap(vm.selectedItemWork));
+            if (historys.length > 0) {
+                _.orderBy(historys, ['itemId'], ['asc']);
+
+                vm.startDate(moment(historys[0].dateHistoryItems[0].startDate).format(DATE_FORMAT));
+                vm.endDate(moment(historys[0].dateHistoryItems[0].endDate).format(DATE_FORMAT));
+                vm.selectedItemWork.valueHasMutated();
+            }
+        }
+
+        reloadList(index?: number) {
+            const vm = this,
+                startDate = new Date(ko.unwrap(vm.startDate) as string),
+                endDate = new Date(ko.unwrap(vm.endDate) as string);
+            var listHistorys: IHistoryInfo[] = [];
+            var historyDateils: IWorkDetail[] = [];
+            let dfdGetAllData = $.Deferred();
+
+            vm.$blockui('invisible')
+                .then(() => {
+                    _.forEach(vm.historyLocals, ((value) => {
+                        if (value.itemId === ko.unwrap(vm.selectedItemWork)) {
+                            _.forEach(value.dateHistoryItems, ((item: IHistoryInfo) => {
+                                if (new Date(moment(item.startDate).format(DATE_FORMAT)) <= startDate && new Date(moment(item.endDate).format(DATE_FORMAT)) >= endDate) {
+                                    listHistorys.push(item);
+                                }
+                            }))
+                        }
+                    }));
+                })
+                .then(() => {
+                    _.forEach(listHistorys, ((item: IHistoryInfo) => {
+                        vm.$ajax('at', API.WORK_INFO_DATAIL, { historyId: item.historyId })
+                            .done((data: IWorkDetail[]) => {
+                                _.forEach(data, ((value: IWorkDetail) => {
+                                    historyDateils.push(value);
+                                }));
+                            })
+                            .then(() => {
+                                if (item.historyId === listHistorys[listHistorys.length - 1].historyId) {
+                                    dfdGetAllData.resolve();
+                                }
+                            });
+                    }));
+                });
+            $.when(dfdGetAllData)
+                .done(() => {
+                    vm.historys(_.orderBy(historyDateils, ['code'], ['asc']));
+
+                    if (index) {
+                        vm.currentCode(ko.unwrap(vm.historys)[index].code);
+                    } else {
+                        vm.currentCode(historyDateils[0].code);
+                    }
+                })
+                .then(() => {
+                    vm.$blockui('clear');
+                })
+        }
+
+        reloadData() {
+            const vm = this;
+            vm.model.update(_.find(ko.unwrap(vm.historys), ((value: IWorkDetail) => { return value.code === ko.unwrap(vm.currentCode) })));
         }
 
         openViewL() {
             const vm = this;
-            vm.$window.modal('at', '/view/kdw/006/l/index.xhtml');
+
+            var data: IHistory;
+            _.forEach(vm.historyLocals, (item: IHistory) => {
+                if (item.itemId === ko.unwrap(vm.selectedItemWork)) {
+                    if (_.find(item.dateHistoryItems, ((value: IHistoryInfo) => { return value.historyId === ko.unwrap(vm.model.historyId) }))) {
+                        data = item;
+                    }
+                }
+            });
+
+            const param = {
+                itemId: ko.unwrap(vm.selectedItemWork),
+                history: data
+            }
+
+            vm.$window.modal('at', '/view/kdw/006/l/index.xhtml', param);
+        }
+
+        new() {
+            const vm = this;
+            vm.model.update({
+                historyId: ko.unwrap(vm.model.historyId),
+                itemId: ko.unwrap(vm.model.itemId),
+                code: '',
+                name: '',
+                externalCode: ''
+            });
+            vm.screenMode(SCREEN_MODE.NEW);
+        }
+
+        addOrUpdate() {
+            const vm = this;
+            const param = {
+                historyId: ko.unwrap(vm.model.historyId),
+                choiceCode: ko.unwrap(vm.model.code),
+                optionName: ko.unwrap(vm.model.name),
+                eternalCodeOfChoice: ko.unwrap(vm.model.externalCode),
+                itemId: ko.unwrap(vm.model.itemId)
+            }
+            const index = _.map(ko.unwrap(vm.historys), m => m.code).indexOf(ko.unwrap(vm.model.code));
+            let reloadDateSuccess = $.Deferred();
+
+            if (ko.unwrap(vm.screenMode) === SCREEN_MODE.NEW) {
+                vm.$blockui('invisible')
+                    .then(() => {
+                        vm.$ajax('at', API.REGISTER, param)
+                            .done(() => {
+                                vm.$dialog.info('Msg_15');
+                                vm.screenMode(SCREEN_MODE.UPDATE);
+                                vm.reloadList();
+                                reloadDateSuccess.resolve();
+                            })
+                            .fail((data: any) => {
+                                vm.$dialog.error(data.messageId);
+                            });
+                    })
+                    .always(() => {
+                        vm.$blockui('clear');
+                    });
+
+                $.when(reloadDateSuccess)
+                    .done(() => {
+                        vm.currentCode(param.choiceCode);
+                    })
+            }
+            if (ko.unwrap(vm.screenMode) === SCREEN_MODE.UPDATE) {
+                vm.$blockui('invisible')
+                    .then(() => {
+                        vm.$ajax('at', API.UPDATE, param)
+                            .done(() => {
+                                vm.$dialog.info('Msg_15');
+                                vm.reloadList(index);
+                                reloadDateSuccess.resolve();
+                            })
+                            .fail((data: any) => {
+                                vm.$dialog.error(data.messageId);
+                            });
+                    })
+                    .always(() => {
+                        vm.$blockui('clear');
+                    });
+
+                $.when(reloadDateSuccess)
+                    .done(() => {
+                        vm.currentCode(param.choiceCode);
+                    })
+            }
+        }
+
+        delete() {
+            const vm = this;
+            const param = {
+                historyId: ko.unwrap(vm.model.historyId),
+                choiceCode: ko.unwrap(vm.model.code)
+            }, index = _.map(ko.unwrap(vm.historys), m => m.code).indexOf(ko.unwrap(vm.model.code));
+
+            nts.uk.ui.dialog
+                .confirm({ messageId: "Msg_18" })
+                .ifYes(() => {
+                    vm.$blockui("invisible")
+                        .then(() => {
+                            vm.$ajax('at', API.REMOTE, param)
+                                .then(() => vm.$dialog.info({ messageId: "Msg_16" }))
+                                .then(() => vm.reloadList(index))
+                                .always(() => vm.$blockui('clear'))
+                        })
+                })
+        }
+
+        validate(action: 'clear' | undefined = undefined) {
+            if (action === 'clear') {
+                return $.Deferred().resolve()
+                    .then(() => $('.nts-input').ntsError('clear'));
+            } else {
+                return $.Deferred().resolve()
+                    /** Gọi xử lý validate của kiban */
+                    .then(() => $('.nts-input').trigger("validate"))
+                    /** Nếu có lỗi thì trả về false, không thì true */
+                    .then(() => !$('.nts-input').ntsError('hasError'));
+            }
         }
     }
 
     interface ItemModel {
-        code: string;
-        name: string;
-    }
-
-    export interface Model {
-        id: string;
-        code: string;
-        name: string;
+        code: String;
+        name: String;
     }
 
     enum SCREEN_MODE {
         NEW = 1,
         UPDATE = 2,
         NOT_HISTORY = 3
+    }
+
+    interface IWorkInfo {
+        /** 項目ID*/
+        itemId: String;
+        /** 名称*/
+        name: String;
+        /** フォーマット設定に表示する*/
+        useAtr: boolean;
+    }
+
+    interface IHistory {
+        /** 項目ID */
+        itemId: String;
+        /** 履歴項目 */
+        dateHistoryItems: IHistoryInfo[];
+    }
+
+    class History {
+        itemId: KnockoutObservable<String> = ko.observable('');
+        dateHistoryItems: KnockoutObservableArray<HistoryInfo> = ko.observableArray([]);
+
+        constructor(param: IHistory) {
+            this.itemId(param.itemId);
+            var historyInfos: HistoryInfo[] = [];
+            _.forEach(param.dateHistoryItems, ((value: IHistoryInfo) => {
+                historyInfos.push(new HistoryInfo(value));
+            }));
+            this.dateHistoryItems(historyInfos);
+        }
+    }
+
+    interface IHistoryInfo {
+        historyId: String;
+        startDate: Date;
+        endDate: Date;
+    }
+
+    class HistoryInfo {
+        historyId: KnockoutObservable<String> = ko.observable('');
+        startDate: KnockoutObservable<Date> = ko.observable(new Date);;
+        endDate: KnockoutObservable<Date> = ko.observable(new Date);
+        period: KnockoutObservable<String> = ko.observable('');
+
+        constructor(param: IHistoryInfo) {
+            this.historyId(param.historyId);
+            this.startDate(param.startDate);
+            this.endDate(param.endDate);
+            this.period(param.startDate.toString() + ' ~ ' + param.endDate.toString());
+        }
+    }
+
+    interface IWorkDetail {
+        /** 履歴ID */
+        historyId: String;
+        /** 項目ID */
+        itemId: String;
+        /** コード */
+        code: String;
+        /** 名称 */
+        name: String;
+        /** 外部コード */
+        externalCode: String;
+    }
+
+    class WorkDetail {
+        historyId: KnockoutObservable<String> = ko.observable('');
+        itemId: KnockoutObservable<String> = ko.observable('');
+        code: KnockoutObservable<String> = ko.observable('');
+        name: KnockoutObservable<String> = ko.observable('');
+        externalCode: KnockoutObservable<String> = ko.observable('');
+
+        constructor(param?: IWorkDetail) {
+            if (param) {
+                this.historyId(param.historyId);
+                this.itemId(param.itemId);
+                this.code(param.code);
+                this.name(param.name);
+                this.externalCode(param.externalCode);
+            }
+        }
+
+        public update(param?: IWorkDetail) {
+            if (param) {
+                this.historyId(param.historyId);
+                this.itemId(param.itemId);
+                this.code(param.code);
+                this.name(param.name);
+                this.externalCode(param.externalCode);
+            }
+        }
     }
 }
--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/006/l/kdw006.l.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/006/l/kdw006.l.vm.ts
@@ -2,62 +2,67 @@
 
 module nts.uk.at.view.kmk006.l {
 
+    const DATE_FORMAT = 'YYYY/MM/DD';
 
+    const API = {
+        RESISTER: 'at/record/kdw006/view-l/register-history'
+    }
 
     @bean()
     export class ViewModel extends ko.ViewModel {
-        screenMode: KnockoutObservable<number> = ko.observable(SCREEN_MODE.NEW);
+        screenMode: KnockoutObservable<number> = ko.observable(SCREEN_MODE.SELECT);
         initMode: KnockoutObservable<number> = ko.observable(INIT_MODE.WORKPLACE);
         lstWpkHistory: KnockoutObservableArray<HistoryItem> = ko.observableArray([]);
         selectedHistoryId: KnockoutObservable<string> = ko.observable('');
         bkHistoryId: string;
         bkStartDate: string;
         bkEndDate: string;
-        selectedStartDateInput: KnockoutObservable<string> = ko.observable('');
-        selectedStartDateText: KnockoutObservable<string> = ko.observable('');
+        selectedStartDateInput: KnockoutObservable<string | null> = ko.observable('');
+        selectedStartDateText: KnockoutObservable<string | null> = ko.observable('');
         selectedEndDate: KnockoutObservable<string> = ko.observable('');
         copyPreviousConfig: KnockoutObservable<boolean> = ko.observable(true);
         isLatestHistory: KnockoutObservable<boolean> = ko.observable(true);
+        public model: HistoryItem = new HistoryItem();
+        public itemId: String = '';
 
-        created() {
+        created(param: any) {
             const vm = this;
-            for (var i = 1 ; i < 13; i ++) {
-                vm.lstWpkHistory.push(new HistoryItem({ historyId: i + '', startDate: '1990/' + i + '/28', endDate: '9999/12/31' }));
-            }
+
+            vm.itemId = param.itemId;
+            _.forEach(param.history.dateHistoryItems, ((value: IHistoryInfo) => {
+                vm.lstWpkHistory.push(new HistoryItem({
+                    historyId: value.historyId,
+                    startDate: moment(value.startDate).format(DATE_FORMAT),
+                    endDate: moment(value.endDate).format(DATE_FORMAT)
+                }));
+            }));
 
             vm.selectedHistoryId.subscribe((value: string) => {
-                const model = _.find(ko.unwrap(vm.lstWpkHistory), ((f: HistoryItem) => { return f.historyId === value; }));
-                if (model) {
-                    vm.selectedEndDate(model.endDate);
-                    vm.selectedStartDateText(model.startDate);
-                    vm.selectedStartDateInput(model.startDate);
-                }else {
-                    vm.selectedEndDate('');
-                }
+                vm.model.update(_.find(ko.unwrap(vm.lstWpkHistory), ((f: HistoryItem) => { return f.historyId === value; })));
+                vm.selectedEndDate(vm.model.endDate);
+                vm.selectedStartDateText(vm.model.startDate);
+                // vm.selectedStartDateInput(vm.model.startDate);
             });
 
             if (ko.unwrap(vm.lstWpkHistory).length > 0) {
                 vm.selectedHistoryId(ko.unwrap(vm.lstWpkHistory)[0].historyId);
             }
-
-            vm.selectedEndDate('');
-
         }
 
         mounted() {
             const vm = this;
-
         }
 
         addHistory() {
             const vm = this;
             vm.screenMode(SCREEN_MODE.ADD);
+            vm.selectedStartDateText(null);
         }
 
         updateHistory() {
             const vm = this;
             vm.screenMode(SCREEN_MODE.UPDATE);
-
+            vm.selectedStartDateText(vm.model.startDate);
         }
 
         deleteHistory() {
@@ -67,6 +72,24 @@ module nts.uk.at.view.kmk006.l {
         cancel() {
             const vm = this;
             vm.$window.close();
+        }
+
+        AddOrUpdate() {
+            const vm = this;
+            const param = {
+                itemId: vm.itemId,
+                historyId: ko.unwrap(vm.model.historyId),
+                startDate: new Date(ko.unwrap(vm.selectedStartDateText)),
+                endDate: new Date(ko.unwrap(vm.selectedEndDate))
+            }
+
+            if (ko.unwrap(vm.screenMode) === SCREEN_MODE.ADD) {
+                vm.$blockui('invisible')
+                    .then(() => {
+                        vm.$ajax('at', API.RESISTER, param)
+                            .then(() => vm.$dialog.info('Msg_15'))
+                    })
+            }
         }
 
         setEndDate(param: string) {
@@ -98,12 +121,22 @@ module nts.uk.at.view.kmk006.l {
     }
 
     class HistoryItem {
-        historyId: string;
-        startDate: string;
-        endDate: string;
-        displayText: string;
+        historyId: string = '';
+        startDate: string = '';
+        endDate: string = '';
+        displayText: string = '';
 
-        constructor(params: IHistoryItem) {
+        constructor(params?: IHistoryItem) {
+            const vm = new ko.ViewModel();
+            if (params) {
+                this.historyId = params.historyId;
+                this.startDate = params.startDate;
+                this.endDate = params.endDate;
+                this.displayText = params.startDate + " " + vm.$i18n("CMM011_125") + " " + params.endDate;
+            }
+        }
+
+        public update(params?: IHistoryItem) {
             const vm = new ko.ViewModel();
             if (params) {
                 this.historyId = params.historyId;
@@ -118,5 +151,46 @@ module nts.uk.at.view.kmk006.l {
         historyId: string;
         startDate: string;
         endDate: string;
+    }
+
+    interface IHistory {
+        /** 項目ID */
+        itemId: String;
+        /** 履歴項目 */
+        dateHistoryItems: IHistoryInfo[];
+    }
+
+    class History {
+        itemId: KnockoutObservable<String> = ko.observable('');
+        dateHistoryItems: KnockoutObservableArray<HistoryInfo> = ko.observableArray([]);
+
+        constructor(param: IHistory) {
+            this.itemId(param.itemId);
+            var historyInfos: HistoryInfo[] = [];
+            _.forEach(param.dateHistoryItems, ((value: IHistoryInfo) => {
+                historyInfos.push(new HistoryInfo(value));
+            }));
+            this.dateHistoryItems(historyInfos);
+        }
+    }
+
+    interface IHistoryInfo {
+        historyId: string;
+        startDate: Date;
+        endDate: Date;
+    }
+
+    class HistoryInfo {
+        historyId: KnockoutObservable<String> = ko.observable('');
+        startDate: KnockoutObservable<Date> = ko.observable(new Date);;
+        endDate: KnockoutObservable<Date> = ko.observable(new Date);
+        period: KnockoutObservable<String> = ko.observable('');
+
+        constructor(param: IHistoryInfo) {
+            this.historyId(param.historyId);
+            this.startDate(param.startDate);
+            this.endDate(param.endDate);
+            this.period(param.startDate.toString() + ' ~ ' + param.endDate.toString());
+        }
     }
 }
