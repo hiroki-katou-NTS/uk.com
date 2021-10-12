f3420d3382a81d5389c74023df2dcbf5b89260ca
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2021-10-12 15:21:16 +0700

    Kdw006L-Fix bug

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/006/l/kdw006.l.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdw/006/l/kdw006.l.vm.ts
@@ -132,7 +132,7 @@ module nts.uk.at.view.kmk006.l {
             }
             var hisLocal: HistoryItem[] = ko.unwrap(vm.lstWpkHistory);
 
-            if (ko.unwrap(vm.screenMode) === SCREEN_MODE.ADD) {
+            if (ko.unwrap(vm.screenMode) === SCREEN_MODE.ADD || ko.unwrap(vm.screenMode) === SCREEN_MODE.NEW) {
                 vm.$blockui('invisible')
                     .then(() => {
                         vm.$ajax('at', API.RESISTER, param)
