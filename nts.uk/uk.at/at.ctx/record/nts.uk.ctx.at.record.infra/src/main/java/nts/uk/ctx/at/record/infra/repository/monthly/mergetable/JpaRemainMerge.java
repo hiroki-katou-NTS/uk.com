beafb6b0912010263052d9a0dc11533c5b3cee41
NTS-hayata_maekawa <hayata_maekawa@nittsusystem.co.jp>
2021-10-12 16:51:28 +0900

    月別実績に公休項目追加

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.infra/src/main/java/nts/uk/ctx/at/record/infra/repository/monthly/mergetable/JpaRemainMerge.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.infra/src/main/java/nts/uk/ctx/at/record/infra/repository/monthly/mergetable/JpaRemainMerge.java
@@ -32,6 +32,7 @@ import nts.uk.ctx.at.shared.dom.scherec.monthlyattdcal.monthly.vacation.annualle
 import nts.uk.ctx.at.shared.dom.scherec.monthlyattdcal.monthly.vacation.care.CareRemNumEachMonth;
 import nts.uk.ctx.at.shared.dom.scherec.monthlyattdcal.monthly.vacation.childcare.ChildcareRemNumEachMonth;
 import nts.uk.ctx.at.shared.dom.scherec.monthlyattdcal.monthly.vacation.dayoff.MonthlyDayoffRemainData;
+import nts.uk.ctx.at.shared.dom.scherec.monthlyattdcal.monthly.vacation.publicholiday.PublicHolidayRemNumEachMonth;
 import nts.uk.ctx.at.shared.dom.scherec.monthlyattdcal.monthly.vacation.reserveleave.RsvLeaRemNumEachMonth;
 import nts.uk.ctx.at.shared.dom.scherec.monthlyattdcal.monthly.vacation.specialholiday.SpecialHolidayRemainData;
 import nts.uk.ctx.at.shared.dom.workrule.closure.ClosureId;
@@ -443,6 +444,17 @@ public class JpaRemainMerge extends JpaRepository implements RemainMergeReposito
 														(domain.getClosureDate().getLastDayOfMonth() ? 1 : 0)),
 								entity -> entity.toEntityRsvLeaRemNumEachMonth(domain));
 	}
+	
+	/** 登録および更新 */
+	@Override
+	public void persistAndUpdate(PublicHolidayRemNumEachMonth domain){
+		internalPersistAndUpdate(new KrcdtMonMergePk(	domain.getEmployeeId(),
+														domain.getYearMonth().v(),
+														domain.getClosureId().value,
+														domain.getClosureDate().getClosureDay().v(),
+														(domain.getClosureDate().getLastDayOfMonth() ? 1 : 0)),
+								entity -> entity.toEntityPublicHoliday(domain));
+	}
 
 
 	/** 削除 */
@@ -521,6 +533,11 @@ public class JpaRemainMerge extends JpaRepository implements RemainMergeReposito
 	@Override
 	public void removeSpecHoliday(String employeeId, YearMonth yearMonth, ClosureId closureId, ClosureDate closureDate, int no) {
 		internalRemove(employeeId, yearMonth, closureId, closureDate, entity -> entity.deleteSpeRemain(no));
+	}
+	
+	@Override
+	public void removeMonpublicHoliday(String employeeId, YearMonth yearMonth){
+		internalRemove(employeeId, yearMonth, entity -> entity.deletePublicHoliday());
 	}
 
 	private void internalRemove(String employeeId, YearMonth yearMonth, ClosureId closureId, ClosureDate closureDate, Consumer<KrcdtMonRemain> remove) {
