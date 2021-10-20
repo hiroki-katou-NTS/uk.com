b66da9889a6b647fbba5815e052611e41bd96f76
m_kitahira <m_kitahira@nittsusystem.co.jp>
2021-10-21 08:58:28 +0900

    テナントを作るDSリファクタ

--- nts.uk/uk.com/com.ctx/sys/tenant/nts.uk.ctx.sys.tenant.dom/src/main/java/nts/uk/ctx/sys/tenant/dom/CreateTenant.java
+++ nts.uk/uk.com/com.ctx/sys/tenant/nts.uk.ctx.sys.tenant.dom/src/main/java/nts/uk/ctx/sys/tenant/dom/CreateTenant.java
@@ -6,38 +6,34 @@ import nts.arc.error.RawErrorMessage;
 import nts.arc.task.tran.AtomTask;
 import nts.arc.time.GeneralDate;
 import nts.uk.ctx.sys.gateway.dom.tenantlogin.TenantAuthentication;
+import nts.uk.shr.com.tenant.event.CreatedTenantEvent;
 
 /**
  * テナントを作る
  */
 public class CreateTenant {
 
-	public static AtomTask create(
-			Require require,
-			String tenantCode,
-			String tenantPassword,
-			GeneralDate tenantStartDate,
-			String administratorLoginId,
-			String administratorPassword) {
+    public static AtomTask create(
+            Require require,
+            String tenantCode,
+            GeneralDate startDate,
+            String tenantPassword) {
 
-		if (require.existsTenant(tenantCode)) {
-			throw new BusinessException(
-					new RawErrorMessage("テナントコード " + tenantCode + " は既に存在します。"));
-		}
+        if (require.existsTenant(tenantCode)) {
+            throw new BusinessException(new RawErrorMessage("テナントコード " + tenantCode + " は既に存在します。"));
+        }
 
-		val tenantAuthentication = TenantAuthentication.create(tenantCode, tenantPassword, tenantStartDate);
-		
-		return AtomTask.of(() -> {
+        val tenantAuthentication = TenantAuthentication.create(tenantCode, tenantPassword, startDate);
+        val event = new CreatedTenantEvent(tenantCode);
 
-			require.add(tenantAuthentication);
+        return AtomTask.of(() -> {
+            require.save(tenantAuthentication);
+            event.publish();
+        });
+    }
 
-		});
-	}
-
-	public static interface Require {
-
-		boolean existsTenant(String tenantCode);
-
-		void add(TenantAuthentication tenantAuthentication);
-	}
+    public static interface Require {
+        boolean existsTenant(String tenantCode);
+        void save(TenantAuthentication tenantAuthentication);
+    }
 }
