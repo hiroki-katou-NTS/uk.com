4eba66716906ecab2e0fd1876fc93e7d9f681ccf
m_kitahira <m_kitahira@nittsusystem.co.jp>
2021-10-28 14:08:42 +0900

    tool PersistenceXml

---
+++ tool/Build4Cloud/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Build4Cloud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("コマンドライン引数でプロジェクト（comやatなど）を指定してください");
+                return;
+            }
+
+            var context = new Context
+            {
+                RootPath = FindRootPathFromCurrent(),
+                Project = args[0],  // "com" | "at" | ...
+            };
+
+            var xml = new PersistenceXml(context.GetPathToPersistenceXml());
+
+            xml.CreateCloudEdition(2);
+
+            Build(context);
+
+            xml.RestoreOriginalFile();
+        }
+
+        private static string FindRootPathFromCurrent()
+        {
+            for (string dir = Environment.CurrentDirectory; Directory.Exists(dir); dir = Path.Combine(dir, ".."))
+            {
+                string root = Path.Combine(dir, "nts.uk");
+                if (Directory.Exists(root))
+                {
+                    return root;
+                }
+            }
+
+            throw new Exception("root not found: " + Environment.CurrentDirectory);
+        }
+
+        private static void Build(Context context)
+        {
+            var processStartInfo = new ProcessStartInfo("gradle", "build");
+            processStartInfo.WorkingDirectory = context.GetPathToWeb();
+
+            var process = Process.Start(processStartInfo);
+            process.WaitForExit();
+            process.Close();
+        }
+    }
+
+}
+
