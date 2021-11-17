fc5586f1426c592f2d118ecc19c6070f9f59ad38
CORP\daiki_ichioka <daiki_ichioka@kinjiro-e.com>
2021-11-17 11:06:53 +0900

    単価を統一する対応

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/scherec/dailyattdcal/personcostcalc/employeeunitpricehistory/UnitPrice.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/scherec/dailyattdcal/personcostcalc/employeeunitpricehistory/UnitPrice.java
@@ -8,20 +8,35 @@ package nts.uk.ctx.at.shared.dom.scherec.dailyattdcal.personcostcalc.employeeuni
 
 public enum UnitPrice {
 	
-	// 単価１
+	// 単価１ 
 	Price_1(0),
-	
+		
 	// 単価2
 	Price_2(1), 
 	
 	// 単価3
 	Price_3(2),
 	
-	// 単価4
-	Standard(3),
+	// 単価4 
+	Price_4(3),
 	
 	// 単価5
-	Contract(4);
+	Price_5(4), 
+	
+	// 単価6
+	Price_6(5),
+	
+	// 単価7 
+	Price_7(6),
+	
+	// 単価8
+	Price_8(7), 
+	
+	// 単価9
+	Price_9(8),
+	
+	// 単価10
+	Price_10(9);
 	
 	public final int value;
 	
