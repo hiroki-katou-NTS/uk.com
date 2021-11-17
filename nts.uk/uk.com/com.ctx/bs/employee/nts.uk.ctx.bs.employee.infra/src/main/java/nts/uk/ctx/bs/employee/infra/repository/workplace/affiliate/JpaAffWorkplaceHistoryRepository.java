1db57361d30f9ab1109b9d7d7df4cb901f175276
nws-ductm <ductm@newwave.vn>
2021-11-17 08:35:57 +0700

    Fix merge source

--- nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.infra/src/main/java/nts/uk/ctx/bs/employee/infra/repository/workplace/affiliate/JpaAffWorkplaceHistoryRepository.java
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.infra/src/main/java/nts/uk/ctx/bs/employee/infra/repository/workplace/affiliate/JpaAffWorkplaceHistoryRepository.java
@@ -9,6 +9,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
