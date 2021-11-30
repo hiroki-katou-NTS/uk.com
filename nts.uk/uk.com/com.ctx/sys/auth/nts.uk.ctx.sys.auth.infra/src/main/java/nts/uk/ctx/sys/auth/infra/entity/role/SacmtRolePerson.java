ae3630f106cb6b16eee9d766355d1fceba896ccd
thienhoang <thien.hv@3si.vn>
2021-11-30 15:11:38 +0700

    #addCID fix

--- nts.uk/uk.com/com.ctx/sys/auth/nts.uk.ctx.sys.auth.infra/src/main/java/nts/uk/ctx/sys/auth/infra/entity/role/SacmtRolePerson.java
+++ nts.uk/uk.com/com.ctx/sys/auth/nts.uk.ctx.sys.auth.infra/src/main/java/nts/uk/ctx/sys/auth/infra/entity/role/SacmtRolePerson.java
@@ -13,7 +13,7 @@ import nts.uk.shr.infra.data.entity.ContractUkJpaEntity;
 @Entity
 @Getter
 @Setter
-@Table(name = "SACMT_ROLE_PERSON")
+@Table(name = "SACMT_ROLE_PERSONAL")
 public class SacmtRolePerson extends ContractUkJpaEntity implements Serializable {
 
 	private static final long serialVersionUID = 1L;
