4f78c52657626942d17aa0a3acd79f41c6daa077
Dang Hong Thai <danghongthai89@gmail.com>
2021-12-02 11:55:19 +0700

    Fix focus on A1_3

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmm/052/a/cmm052.a.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cmm/052/a/cmm052.a.vm.ts
@@ -5,6 +5,10 @@ module nts.uk.at.view.cmm052.a {
   @bean()
   export class ScreenModel extends ko.ViewModel {
 
+    mounted(){
+      $('#A1_3').focus();
+    }
+
     public openScreenCas003() {
       const vm = this;
       vm.$jump("/view/cas/003/a/index.xhtml");
