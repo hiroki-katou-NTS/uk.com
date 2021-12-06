39f1a39c0fe961ec9649dda68f99940471d3d3ff
NTS-PhamVanDan <dan_pv@nittsusystem-vn.com>
2021-12-06 15:03:04 +0900

    update FakeSupportTicket

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/schedule/support/supportschedule/FakeSupportTicket.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/main/java/nts/uk/ctx/at/schedule/dom/schedule/support/supportschedule/FakeSupportTicket.java
@@ -1,5 +1,32 @@
 package nts.uk.ctx.at.schedule.dom.schedule.support.supportschedule;
 
+import java.util.Optional;
+
+import lombok.Value;
+import nts.arc.time.GeneralDate;
+import nts.uk.ctx.at.shared.dom.common.time.TimeSpanForCalc;
+import nts.uk.ctx.at.shared.dom.workrule.organizationmanagement.workplace.TargetOrgIdenInfor;
+
+@Value
 public class FakeSupportTicket {
+	
+	private String employeeId;
+	
+	private TargetOrgIdenInfor supportDestination;
+	
+	private FakeSupportType supportType;
+	
+	private GeneralDate date;
+	
+	private Optional<TimeSpanForCalc> timeSpan;
+	
+	public boolean isSame(FakeSupportTicket otherTicket) {
+		
+		return this.employeeId.equals(otherTicket.getEmployeeId())
+				&& this.supportDestination.getTargetId().equals(otherTicket.getSupportDestination().getTargetId())
+				&& this.supportType == otherTicket.getSupportType()
+				&& this.date.equals(otherTicket)
+				&& this.timeSpan.equals(otherTicket.getTimeSpan());
+	}
 
 }
