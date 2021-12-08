25f80db8cbc8d23ae162ed950ba739e0c6b56ed8
NTS-PhamVanDan <dan_pv@nittsusystem-vn.com>
2021-12-08 13:18:24 +0900

    add unit test of TaskSchedule and TaskScheduleDetails

--- nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/test/java/nts/uk/ctx/at/schedule/dom/schedule/task/taskschedule/TaskScheduleDetailTest.java
+++ nts.uk/uk.at/at.ctx/schedule/nts.uk.ctx.at.schedule.dom/src/test/java/nts/uk/ctx/at/schedule/dom/schedule/task/taskschedule/TaskScheduleDetailTest.java
@@ -3,6 +3,7 @@ package nts.uk.ctx.at.schedule.dom.schedule.task.taskschedule;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Before;
@@ -140,5 +141,52 @@ public class TaskScheduleDetailTest {
 		
 		assertThat( result ).isTrue();
 	}
+	
+	@Test
+	public void testIsDuplicateWith() {
+		
+		List<TimeSpanForCalc> inputList = Arrays.asList( 
+				new TimeSpanForCalc(TimeWithDayAttr.hourMinute(6, 0), TimeWithDayAttr.hourMinute(7, 0)), // NOT_DUPLICATE
+				new TimeSpanForCalc(TimeWithDayAttr.hourMinute(7, 0), TimeWithDayAttr.hourMinute(9, 0)), // CONNOTATE_BEGINTIME
+				new TimeSpanForCalc(TimeWithDayAttr.hourMinute(9, 0), TimeWithDayAttr.hourMinute(10, 0)), // CONTAINS
+				new TimeSpanForCalc(TimeWithDayAttr.hourMinute(10, 0), TimeWithDayAttr.hourMinute(13, 0)), // CONNOTATE_ENDTIME
+				new TimeSpanForCalc(TimeWithDayAttr.hourMinute(13, 0), TimeWithDayAttr.hourMinute(15, 0)), // NOT_DUPLICATE
+				new TimeSpanForCalc(TimeWithDayAttr.hourMinute(7, 0), TimeWithDayAttr.hourMinute(13, 0)), // CONTAINED
+				new TimeSpanForCalc(TimeWithDayAttr.hourMinute(8, 0), TimeWithDayAttr.hourMinute(12, 0)) // SAME_SPAN
+				);
+		
+		
+		assertThat( this.target.isDuplicateWith(inputList.get(0)) ).isFalse(); // NOT_DUPLICATE
+		assertThat( this.target.isDuplicateWith(inputList.get(1)) ).isTrue(); // CONNOTATE_BEGINTIME
+		assertThat( this.target.isDuplicateWith(inputList.get(2)) ).isTrue(); // CONTAINS
+		assertThat( this.target.isDuplicateWith(inputList.get(3)) ).isTrue(); // CONNOTATE_ENDTIME
+		assertThat( this.target.isDuplicateWith(inputList.get(4)) ).isFalse(); // NOT_DUPLICATE
+		assertThat( this.target.isDuplicateWith(inputList.get(5)) ).isTrue(); // CONTAINED
+		assertThat( this.target.isDuplicateWith(inputList.get(6)) ).isTrue(); // SAME_SPAN
+	}
+	
+	@Test
+	public void testIsContainedIn() {
+		
+		List<TimeSpanForCalc> inputList = Arrays.asList( 
+				new TimeSpanForCalc(TimeWithDayAttr.hourMinute(6, 0), TimeWithDayAttr.hourMinute(7, 0)), // NOT_DUPLICATE
+				new TimeSpanForCalc(TimeWithDayAttr.hourMinute(7, 0), TimeWithDayAttr.hourMinute(9, 0)), // CONNOTATE_BEGINTIME
+				new TimeSpanForCalc(TimeWithDayAttr.hourMinute(9, 0), TimeWithDayAttr.hourMinute(10, 0)), // CONTAINS
+				new TimeSpanForCalc(TimeWithDayAttr.hourMinute(10, 0), TimeWithDayAttr.hourMinute(13, 0)), // CONNOTATE_ENDTIME
+				new TimeSpanForCalc(TimeWithDayAttr.hourMinute(13, 0), TimeWithDayAttr.hourMinute(15, 0)), // NOT_DUPLICATE
+				new TimeSpanForCalc(TimeWithDayAttr.hourMinute(7, 0), TimeWithDayAttr.hourMinute(13, 0)), // CONTAINED
+				new TimeSpanForCalc(TimeWithDayAttr.hourMinute(8, 0), TimeWithDayAttr.hourMinute(12, 0)) // SAME_SPAN
+				);
+		
+		
+		assertThat( this.target.isContainedIn(inputList.get(0)) ).isFalse(); // NOT_DUPLICATE
+		assertThat( this.target.isContainedIn(inputList.get(1)) ).isFalse(); // CONNOTATE_BEGINTIME
+		assertThat( this.target.isContainedIn(inputList.get(2)) ).isFalse(); // CONTAINS
+		assertThat( this.target.isContainedIn(inputList.get(3)) ).isFalse(); // CONNOTATE_ENDTIME
+		assertThat( this.target.isContainedIn(inputList.get(4)) ).isFalse(); // NOT_DUPLICATE
+		assertThat( this.target.isContainedIn(inputList.get(5)) ).isTrue(); // CONTAINED
+		assertThat( this.target.isContainedIn(inputList.get(6)) ).isTrue(); // SAME_SPAN
+	}
+	
 	
 }
