4d8aac1219e90e91205c45c814e58fe78ab7f00a
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2021-12-09 10:33:34 +0700

    Kdp002: Update DS: 社員の打刻データを取得する ver2

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/workrecord/stampmanagement/stamp/domainservice/StampDataOfEmployees.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/workrecord/stampmanagement/stamp/domainservice/StampDataOfEmployees.java
@@ -5,7 +5,6 @@ import java.util.List;
 import lombok.Getter;
 import nts.arc.time.GeneralDate;
 import nts.uk.ctx.at.record.dom.workrecord.stampmanagement.stamp.Stamp;
-import nts.uk.ctx.at.record.dom.workrecord.stampmanagement.stamp.StampRecord;
 
 /**
  * 社員の打刻データ
@@ -25,11 +24,6 @@ public class StampDataOfEmployees {
 	@Getter
 	private final GeneralDate date;
 	
-	/**
-	 * 打刻記録リスト
-	 */
-	@Getter
-	private final List<StampRecord> listStampRecord;
 	
 	/**
 	 * 打刻リスト
@@ -37,14 +31,10 @@ public class StampDataOfEmployees {
 	@Getter
 	private final List<Stamp> listStamp;
 
-	public StampDataOfEmployees(String employeeId, GeneralDate date, List<StampRecord> listStampRecord,
-			List<Stamp> listStamp) {
+	public StampDataOfEmployees(String employeeId, GeneralDate date, List<Stamp> listStamp) {
 		super();
 		this.employeeId = employeeId;
 		this.date = date;
-		this.listStampRecord = listStampRecord;
 		this.listStamp = listStamp;
 	}
-
-	
 }
