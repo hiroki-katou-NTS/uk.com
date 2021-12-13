093eb0848a298d2118f9fe299edc143c76aff2ba
NSVC\tutk <tu_tk@nittsusystem-vn.com>
2021-12-13 15:39:22 +0700

    fix conflict by Tutk ver 5

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/test/java/nts/uk/ctx/at/record/dom/daily/ouen/RegisterWorkHoursServiceTest.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/test/java/nts/uk/ctx/at/record/dom/daily/ouen/RegisterWorkHoursServiceTest.java
@@ -1,6 +1,7 @@
 package nts.uk.ctx.at.record.dom.daily.ouen;
 
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import mockit.integration.junit4.JMockit;
@@ -28,8 +29,8 @@ public class RegisterWorkHoursServiceTest {
 	private EditStateSetting editStateSetting = EnumAdaptor.valueOf(0, EditStateSetting.class);
 	
 	//if 作業詳細一覧.isNotPresent
-//	@Test
-//	public void test() {
+	@Test
+	public void test() {
 //		
 //		List<WorkDetailsParam> workDetailsParams = new ArrayList<>();
 //		
@@ -44,7 +45,7 @@ public class RegisterWorkHoursServiceTest {
 //		
 //		assertThat(result.getIntegrationOfDaily().isPresent()).isFalse();
 //		
-//	}
+	}
 //	
 //	//if 作業詳細一覧.isNotPresent
 //	@Test
