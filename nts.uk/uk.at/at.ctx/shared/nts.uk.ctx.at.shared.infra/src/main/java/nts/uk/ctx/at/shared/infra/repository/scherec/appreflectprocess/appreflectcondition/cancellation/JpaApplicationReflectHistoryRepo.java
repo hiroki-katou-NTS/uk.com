30934ba3b08393a7148b52a379638ba7545959f0
m_kitahira <m_kitahira@nittsusystem.co.jp>
2021-12-16 13:11:04 +0900

    bugfix: 承認直後のシステムエラー（boolean/intのDB型不一致）修正漏れ

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/repository/scherec/appreflectprocess/appreflectcondition/cancellation/JpaApplicationReflectHistoryRepo.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.infra/src/main/java/nts/uk/ctx/at/shared/infra/repository/scherec/appreflectprocess/appreflectcondition/cancellation/JpaApplicationReflectHistoryRepo.java
@@ -179,7 +179,7 @@ public class JpaApplicationReflectHistoryRepo extends JpaRepository implements A
 		lstRestore.add(restore);
 		return new ApplicationReflectHistory(rec.getString("SID"), rec.getGeneralDate("YMD"), rec.getString("APP_ID"),
 				EnumAdaptor.valueOf(rec.getInt("ATR"), ScheduleRecordClassifi.class), rec.getInt("DELETE_ATR") == 1,
-				lstRestore, new AppReflectExecInfo(rec.getInt("RE_REFLECT_ATR") == 1, rec.getString("EXECUTION_ID"), rec.getGeneralDateTime("REFLECT_TIME")));
+				lstRestore, new AppReflectExecInfo(rec.getBoolean("RE_REFLECT_ATR"), rec.getString("EXECUTION_ID"), rec.getGeneralDateTime("REFLECT_TIME")));
 
 	}
 
