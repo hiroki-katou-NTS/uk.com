1ee4f05311433d36405bd2542063029e8eedd94b
nws-dungdv <dungdv@newwave.vn>
2021-12-21 15:16:50 +0700

    wip: update 時間特別休暇の管理設定

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/workrule/vacation/specialvacation/timespecialvacation/TimeSpecialLeaveManagementSetting.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/workrule/vacation/specialvacation/timespecialvacation/TimeSpecialLeaveManagementSetting.java
@@ -5,8 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import nts.arc.layer.dom.AggregateRoot;
-import nts.uk.ctx.at.shared.dom.vacation.setting.ManageDistinct;
-import nts.uk.ctx.at.shared.dom.vacation.setting.TimeDigestiveUnit;
+import nts.uk.ctx.at.shared.dom.common.time.AttendanceTime;
+import nts.uk.ctx.at.shared.dom.vacation.setting.TimeVacationDigestUnit;
 
 /**
  * UKDesign.ドメインモデル."NittsuSystem.UniversalK".就業.shared.就業規則.休暇.特別休暇.時間特別休暇
@@ -21,8 +21,22 @@ public class TimeSpecialLeaveManagementSetting extends AggregateRoot {
     private String companyId;
 
     // 時間休暇消化単位
-    private TimeDigestiveUnit timeDigestiveUnit;
-
-    // 管理区分
-    private ManageDistinct manageType;
+    private TimeVacationDigestUnit timeVacationDigestUnit;
+    
+    /**
+     * [3]時間休暇が管理するか
+     * @param require
+     */
+    public boolean isManageTimeVacation(TimeVacationDigestUnit.Require require) {
+    	return this.timeVacationDigestUnit.isVacationTimeManage(require);
+    }
+    
+    /**
+     * [4]利用する休暇時間の消化単位をチェックする
+     * @param require
+     * @param time 休暇使用時間
+     */
+    public boolean checkVacationTimeUnitUsed(TimeVacationDigestUnit.Require require, AttendanceTime time) {
+		return this.timeVacationDigestUnit.checkDigestUnit(require, time);
+	}
 }
