71e7c03ff33138ec2a6ede25ec4e25f1fbc8a2c0
tuanla4 <tuanla4@newwave.vn>
2021-12-22 17:00:35 +0700

    commit redmine 121997

---
+++ nts.uk/uk.mobile/ClientApp/views/kdl/s10/test/index.vue
@@ -0,0 +1,8 @@
+<template>
+<div class="KdtS10Test">
+    <h3>{{title | i18n}}</h3>
+    <nts-text-editor v-model="data.selectedCode" name='selected code'/>
+    <nts-text-editor v-model="data.selectedName" name='selected name'/>
+    <button v-click:500="openDkls10" class="btn btn-secondary btn-block">open kdls10</button>
+</div>
+</template>
