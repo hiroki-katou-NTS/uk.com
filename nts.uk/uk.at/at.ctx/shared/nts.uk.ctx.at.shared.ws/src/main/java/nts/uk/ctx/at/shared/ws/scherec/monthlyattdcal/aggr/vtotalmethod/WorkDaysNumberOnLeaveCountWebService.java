6fd86cbce431fce929a44309020db8ef706edb50
tuanla4 <tuanla4@newwave.vn>
2021-12-27 18:21:04 +0700

    commit view value kmf001

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.ws/src/main/java/nts/uk/ctx/at/shared/ws/scherec/monthlyattdcal/aggr/vtotalmethod/WorkDaysNumberOnLeaveCountWebService.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.ws/src/main/java/nts/uk/ctx/at/shared/ws/scherec/monthlyattdcal/aggr/vtotalmethod/WorkDaysNumberOnLeaveCountWebService.java
@@ -1,15 +1,21 @@
 package nts.uk.ctx.at.shared.ws.scherec.monthlyattdcal.aggr.vtotalmethod;
 
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import nts.arc.enums.EnumAdaptor;
+import nts.arc.enums.EnumConstant;
 import nts.arc.layer.ws.WebService;
 import nts.uk.ctx.at.shared.app.command.scherec.monthlyattdcal.aggr.vtotalmethod.RegisterWorkDaysNumberOnLeaveCountCommand;
 import nts.uk.ctx.at.shared.app.command.scherec.monthlyattdcal.aggr.vtotalmethod.RegisterWorkDaysNumberOnLeaveCountCommandHandler;
 import nts.uk.ctx.at.shared.app.find.scherec.monthlyattdcal.aggr.vtotalmethod.WorkDaysNumberOnLeaveCountDto;
 import nts.uk.ctx.at.shared.app.find.scherec.monthlyattdcal.aggr.vtotalmethod.WorkDaysNumberOnLeaveCountFinder;
+import nts.uk.ctx.at.shared.dom.vacation.setting.ManageDistinct;
+import nts.uk.ctx.at.shared.dom.vacation.setting.TimeDigestiveUnit;
 
 @Path("at/shared/scherec/leaveCount")
 @Produces("application/json")
@@ -32,4 +38,16 @@ public class WorkDaysNumberOnLeaveCountWebService extends WebService {
 	public void register(RegisterWorkDaysNumberOnLeaveCountCommand command) {
 		this.registerCommandHandler.handle(command);
 	}
+	
+    @POST
+    @Path("/timemanagementdistinct")
+    public List<EnumConstant> findTimeManagementDistinct() {
+        return EnumAdaptor.convertToValueNameList(ManageDistinct.class);
+    }
+    
+    @POST
+    @Path("/timeunit")
+    public List<EnumConstant> findTimeVacationDigestiveUnit() {
+        return EnumAdaptor.convertToValueNameList(TimeDigestiveUnit.class);
+    }
 }
--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmf/001/n/kmf001.n.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmf/001/n/kmf001.n.vm.ts
@@ -3,7 +3,10 @@
 module nts.uk.at.view.kmf001.n {
   const API = {
     findOne: "at/shared/scherec/leaveCount/get",
-    register: "at/shared/scherec/leaveCount/register"
+    register: "at/shared/scherec/leaveCount/register",
+    findManageDistinct: "at/shared/scherec/leaveCount/timemanagementdistinct",
+    findTimeUnit: "at/shared/scherec/leaveCount/timeunit",
+    save: "at/shared/scherec/leaveCount/save"
   };
   const LEAVE_TYPE = 3;
 
@@ -11,16 +14,39 @@ module nts.uk.at.view.kmf001.n {
   export class ScreenModel extends ko.ViewModel {
     manageDistinctList: KnockoutObservableArray<any> = ko.observableArray(__viewContext.enums.ManageDistinct);
     selectedManageDistinct: KnockoutObservable<number> = ko.observable(0);
+    vacationTimeUnitList: KnockoutObservableArray<EnumerationModel> = ko.observableArray([]);
+    selectedVacationTimeUnit: KnockoutObservable<number> = ko.observable(0);
+    timeManageMentDistinctList: KnockoutObservableArray<EnumerationModel> = ko.observableArray([]);
+    selectedTimeManagement: KnockoutObservable<number> = ko.observable(1);
+    enableTimeSetting: KnockoutObservable<boolean>= ko.computed(function() {
+      return this.selectedTimeManagement() == 1;
+    }, this);
 
     mounted() {
       const vm = this;
+      let dfd = $.Deferred();
       vm.$blockui("grayout");
       vm.$ajax(API.findOne).then((result: WorkDaysNumberOnLeaveCountDto) => {
         const isCounting = !!_.includes(result.countedLeaveList, LEAVE_TYPE);
         vm.selectedManageDistinct(isCounting ? 1 : 0);
-        // Fix tabindex
+      // Fix tabindex
         vm.$nextTick(() => $("#N1_4").attr("tabindex", 1));
       }).always(() => vm.$blockui("clear"));
+      // call api time management
+      vm.$ajax(API.findManageDistinct).done(function(res: Array<EnumerationModel>) {
+        vm.timeManageMentDistinctList(res);
+        dfd.resolve();
+      }).fail(function(res) {
+          nts.uk.ui.dialog.alertError(res.message);
+      });
+      // call api time unit
+      vm.$ajax(API.findTimeUnit).done(function(res: Array<EnumerationModel>) {
+          vm.vacationTimeUnitList(res);
+          dfd.resolve();
+      }).fail(function(res) {
+          nts.uk.ui.dialog.alertError(res.message);
+      });
+      return dfd.promise();
     }
 
     public processSave() {
@@ -46,5 +72,19 @@ module nts.uk.at.view.kmf001.n {
 
     // カウントする休暇一覧
     countedLeaveList: Number[];
+  }
+
+  export class EnumerationModel {
+
+    value: number;
+    fieldName: string;
+    localizedName: string;
+
+    constructor(value: number, fieldName: string, localizedName: string) {
+        let self = this;
+        self.value = value;
+        self.fieldName = fieldName;
+        self.localizedName = localizedName;
+    }
   }
 }
