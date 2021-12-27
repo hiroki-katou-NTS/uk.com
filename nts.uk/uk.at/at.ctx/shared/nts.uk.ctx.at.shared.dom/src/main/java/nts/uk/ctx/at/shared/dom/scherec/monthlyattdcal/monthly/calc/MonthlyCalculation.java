eaabf4e73756706d6dce8f3421faf0028675146a
misaka mikoto <tin_dh@nittsusystem-vn.com>
2021-12-28 08:58:28 +0900

    revert

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/scherec/monthlyattdcal/monthly/calc/MonthlyCalculation.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/scherec/monthlyattdcal/monthly/calc/MonthlyCalculation.java
@@ -993,10 +993,8 @@ public class MonthlyCalculation implements SerializableWithOptional {
 	public void calcTotalWorkingTime() {
 
 		this.totalWorkingTime = new AttendanceTimeMonth(this.aggregateTime.getTotalWorkingTargetTime().v()
-				+ this.actualWorkingTime.getTotalWorkingTargetTime().v());
-		
-		if (this.workingSystem == WorkingSystem.FLEX_TIME_WORK) 
-			this.totalWorkingTime = this.totalWorkingTime.addMinutes(this.flexTime.getTotalWorkingTargetTime().v());
+				+ this.actualWorkingTime.getTotalWorkingTargetTime().v()
+				+ this.flexTime.getTotalWorkingTargetTime().v());
 	}
 	
 	/**
