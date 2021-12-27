8fb383795d468ccfb7675cc60d3d870309ecf1f1
nws-dungdv <dungdv@newwave.vn>
2021-12-27 18:44:37 +0700

    update UT 代休管理設定

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/test/java/nts/uk/ctx/at/shared/dom/vacation/setting/compensatoryleave/CompensatoryLeaveComSettingTest.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/test/java/nts/uk/ctx/at/shared/dom/vacation/setting/compensatoryleave/CompensatoryLeaveComSettingTest.java
@@ -21,7 +21,6 @@ import nts.uk.ctx.at.shared.dom.vacation.setting.ManageDistinct;
 import nts.uk.ctx.at.shared.dom.vacation.setting.TimeDigestiveUnit;
 import nts.uk.ctx.at.shared.dom.vacation.setting.TimeVacationDigestUnit;
 import nts.uk.ctx.at.shared.dom.vacation.setting.compensatoryleave.CompensatoryLeaveComSetting.RequireM7;
-import nts.uk.shr.com.license.option.OptionLicense;
 
 @RunWith(JMockit.class)
 public class CompensatoryLeaveComSettingTest {
@@ -39,19 +38,37 @@ public class CompensatoryLeaveComSettingTest {
 	}
 	
 	/**
-	 * Test 時間代休を管理するか判断する
+	 * Test [2] 時間代休を管理するかどうか判断する
+	 * Case 1: $Option.就業.時間休暇 = true
 	 */
 	@Test
-	public void testIsManagedTime() {
+	public void testIsManagedTime1() {
 		val domain = create();
 		new Expectations() {
 			{
 				require.getOptionLicense();
-				result = new OptionLicense() {};
+				result = CompensatoryLeaveComSettingTestHelper.getOptionLicense(true);
 			}
 		};
 		boolean isManagedTime = domain.isManagedTime(require);
-		assertThat(isManagedTime);
+		assertThat(isManagedTime).isTrue();
+	}
+	
+	/**
+	 * Test [2] 時間代休を管理するかどうか判断する
+	 * Case 2: $Option.就業.時間休暇 = false
+	 */
+	@Test
+	public void testIsManagedTime2() {
+		val domain = create();
+		new Expectations() {
+			{
+				require.getOptionLicense();
+				result = CompensatoryLeaveComSettingTestHelper.getOptionLicense(false);
+			}
+		};
+		boolean isManagedTime = domain.isManagedTime(require);
+		assertThat(isManagedTime).isFalse();
 	}
 	
 	/**
@@ -67,11 +84,11 @@ public class CompensatoryLeaveComSettingTest {
 			}
 			{
 				require.getOptionLicense();
-				result = new OptionLicense() {};
+				result = CompensatoryLeaveComSettingTestHelper.getOptionLicense(true);
 			}
 		};
 		boolean checkVacationTimeUnitUsed = domain.checkVacationTimeUnitUsed(require, "DUMMY-CID", AttendanceTime.ZERO, "DUMMY-SID", GeneralDate.today());
-		assertThat(checkVacationTimeUnitUsed);
+		assertThat(checkVacationTimeUnitUsed).isTrue();
 	}
 	
 	/**
@@ -97,11 +114,42 @@ public class CompensatoryLeaveComSettingTest {
 			}
 			{
 				require.getOptionLicense();
-				result = new OptionLicense() {};
+				result = CompensatoryLeaveComSettingTestHelper.getOptionLicense(true);
 			}
 		};
 		boolean checkVacationTimeUnitUsed = domain.checkVacationTimeUnitUsed(require, "DUMMY-CID", AttendanceTime.ZERO, "DUMMY-SID", dummyDate);
-		assertThat(checkVacationTimeUnitUsed);
+		assertThat(checkVacationTimeUnitUsed).isTrue();
+	}
+	
+	/**
+	 * Test [7] 利用する休暇時間の消化単位をチェックする:
+	 * Case 3: 「休暇使用時間」 % 「@消化単位」 != 0
+	 */
+	@Test
+	public void testCheckVacationTimeUnitUsed3() {
+		val domain = create();
+		val dummyDate = GeneralDate.today();
+		new Expectations() {
+			{
+				require.getCmpLeaveComSet("DUMMY-CID");
+				result = Optional.of(new CompensatoryLeaveComSetting("DUMMT-CID",
+						ManageDistinct.YES,
+						null,
+						null,
+						new TimeVacationDigestUnit(ManageDistinct.YES, TimeDigestiveUnit.OneHour),
+						ManageDistinct.YES));
+			}
+			{
+				require.getEmploymentHis("DUMMY-SID", dummyDate);
+				result = Optional.of(new SEmpHistoryImport("DUMMY-SID", "DUMMY-SCD", "DUMMY-SNAME", new DatePeriod(dummyDate, dummyDate)));
+			}
+			{
+				require.getOptionLicense();
+				result = CompensatoryLeaveComSettingTestHelper.getOptionLicense(true);
+			}
+		};
+		boolean checkVacationTimeUnitUsed = domain.checkVacationTimeUnitUsed(require, "DUMMY-CID", new AttendanceTime(11), "DUMMY-SID", dummyDate);
+		assertThat(checkVacationTimeUnitUsed).isFalse();
 	}
 	
 	/**
@@ -118,11 +166,11 @@ public class CompensatoryLeaveComSettingTest {
 			}
 			{
 				require.getOptionLicense();
-				result = new OptionLicense() {};
+				result = CompensatoryLeaveComSettingTestHelper.getOptionLicense(true);
 			}
 		};
 		boolean manageTimeOffAccordingEmpSettings = domain.manageTimeOffAccordingEmpSettings(require, "DUMMY-CID", "DUMMY-SID", dummyDate);
-		assertThat(manageTimeOffAccordingEmpSettings);
+		assertThat(manageTimeOffAccordingEmpSettings).isFalse();
 	}
 	
 	/**
@@ -148,10 +196,37 @@ public class CompensatoryLeaveComSettingTest {
 			}
 			{
 				require.getOptionLicense();
-				result = new OptionLicense() {};
+				result = CompensatoryLeaveComSettingTestHelper.getOptionLicense(true);
 			}
 		};
 		boolean manageTimeOffAccordingEmpSettings = domain.manageTimeOffAccordingEmpSettings(require, "DUMMY-CID", "DUMMY-SID", dummyDate);
-		assertThat(manageTimeOffAccordingEmpSettings);
+		assertThat(manageTimeOffAccordingEmpSettings).isTrue();
+	}
+	
+	/**
+	 * Test [8]雇用設定に従う時間代休を管理するかどうか判断する
+	 * Case 3: 管理区分 = 管理しない
+	 */
+	@Test
+	public void testManageTimeOffAccordingEmpSettings3() {
+		val domain = create();
+		val dummyDate = GeneralDate.today();
+		new Expectations() {
+			{
+				require.getCmpLeaveComSet("DUMMY-CID");
+				result = Optional.of(new CompensatoryLeaveComSetting("DUMMT-CID",
+						ManageDistinct.NO,
+						null,
+						null,
+						new TimeVacationDigestUnit(ManageDistinct.NO, TimeDigestiveUnit.OneHour),
+						ManageDistinct.NO));
+			}
+			{
+				require.getOptionLicense();
+				result = CompensatoryLeaveComSettingTestHelper.getOptionLicense(true);
+			}
+		};
+		boolean manageTimeOffAccordingEmpSettings = domain.manageTimeOffAccordingEmpSettings(require, "DUMMY-CID", "DUMMY-SID", dummyDate);
+		assertThat(manageTimeOffAccordingEmpSettings).isFalse();
 	}
 }
