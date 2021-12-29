9c42f94499fd4de0df8633eefe2e5c3c5ce6a674
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2021-12-29 09:30:57 +0700

    Ksu002 Create GetEmployeeInformations

---
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/query/ksu/ksu002/a/dto/GetEmployeeInformationsDto.java
@@ -0,0 +1,24 @@
+package nts.uk.screen.at.app.query.ksu.ksu002.a.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 
+ * @author chungnt
+ *
+ */
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class GetEmployeeInformationsDto {
+
+	// ・社員情報リスト：List＜社員ID、社員コード, 社員名>
+	public List<EmployeeInfoDto> employeeInfos;
+
+}
+
