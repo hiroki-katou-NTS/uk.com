75ea589ec4d486e034158c3c708de90fb3767929
3SI\hung.tt <hung.tt@3si.vn>
2022-01-12 18:21:20 +0700

    fix bug kdr003, kdr004 (cont)

--- nts.uk/uk.at/at.file/nts.uk.file.at.infra/src/main/java/nts/uk/file/at/infra/holidayconfirmationtable/AsposeHolidayConfirmationTableGenerator.java
+++ nts.uk/uk.at/at.file/nts.uk.file.at.infra/src/main/java/nts/uk/file/at/infra/holidayconfirmationtable/AsposeHolidayConfirmationTableGenerator.java
@@ -282,6 +282,8 @@ public class AsposeHolidayConfirmationTableGenerator extends AsposeCellsReportGe
         if (!dataSource.isLinking()) {
             cells.deleteColumn(COLUMN_LINK);
             cells.hideColumn(18);
+        } else {
+            cells.hideColumn(19);
         }
 
         // remove template rows
