8ade20e16aac3b907e42cbb8f61b35665ea563ff
nws-dongnt <Poppadong12312>
2022-01-12 15:28:52 +0700

    SMM001: - Edit enum and screen a,b

--- nts.uk/uk.com/com.ctx/link/smile/nts.uk.smile.dom/src/main/java/nts/uk/smile/dom/smilelinked/cooperationoutput/LinkedPaymentConversion.java
+++ nts.uk/uk.com/com.ctx/link/smile/nts.uk.smile.dom/src/main/java/nts/uk/smile/dom/smilelinked/cooperationoutput/LinkedPaymentConversion.java
@@ -17,7 +17,7 @@ public class LinkedPaymentConversion extends AggregateRoot {
 	 * Name: 支払コード					
 	 * Type: 支払区分	
 	 */
-	private final PaymentCategory paymeRequestByWorkplaceWebServicentCode;
+	private final PaymentCategory paymentCode;
 	/**
 	 * Name: 選択雇用コード					
 	 * Type: List 雇用と連動月設定 								
