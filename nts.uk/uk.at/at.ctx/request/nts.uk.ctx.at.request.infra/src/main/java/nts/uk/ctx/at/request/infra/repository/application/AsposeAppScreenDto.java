e6a6f18748fa41ad4bbc91d370c2f8672e8cc9ed
AnhNM <twas.horus@gmail.com>
2022-01-14 20:17:05 +0700

    CMM045: Fix bug #122520
    (cherry picked from commit eeb63bb1b1723320b3822fbe194a9099bb58dffd)

--- nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.infra/src/main/java/nts/uk/ctx/at/request/infra/repository/application/AsposeAppScreenDto.java
+++ nts.uk/uk.at/at.ctx/request/nts.uk.ctx.at.request.infra/src/main/java/nts/uk/ctx/at/request/infra/repository/application/AsposeAppScreenDto.java
@@ -10,6 +10,7 @@ import nts.arc.i18n.I18NText;
 import nts.arc.time.GeneralDate;
 import nts.uk.ctx.at.request.dom.application.ApplicationType;
 import nts.uk.ctx.at.request.dom.application.PrePostAtr;
+import nts.uk.ctx.at.request.dom.application.applist.service.content.ComplementLeaveAppLink;
 import nts.uk.ctx.at.request.dom.application.applist.service.param.ListOfApplication;
 
 @Getter
@@ -65,10 +66,11 @@ public class AsposeAppScreenDto {
 				domain.getInputDate().toString("M/d(E) H:mm"), 
 				I18NText.getText(domain.getReflectionStatus()), 
 				domain.getOpApprovalStatusInquiry().isPresent() ? domain.getOpApprovalStatusInquiry().get().toString() : "", 
-				convertStartEndDate(domain.getOpAppStartDate(), domain.getOpAppEndDate(), domain.getAppType(), domain.getAppDate()));
+				convertStartEndDate(domain.getOpAppStartDate(), domain.getOpAppEndDate(), domain.getAppType(), domain.getAppDate(), domain.getOpComplementLeaveApp()));
 	}
 	
-	public static String convertStartEndDate(Optional<GeneralDate> startDate, Optional<GeneralDate> endDate, ApplicationType appType, GeneralDate appDate) {
+	public static String convertStartEndDate(Optional<GeneralDate> startDate, Optional<GeneralDate> endDate, ApplicationType appType, 
+	        GeneralDate appDate, Optional<ComplementLeaveAppLink> opComplementLeaveApp) {
 		String result = "";
 		
 		if(startDate.isPresent() && endDate.isPresent()) {
@@ -83,9 +85,14 @@ public class AsposeAppScreenDto {
 			}
 		}
 		if(appType.value == 10) {
-			result = new StringBuilder(result)
-					.append("\n'")
-					.append(appDate.toString("M/d(E)")).toString();
+		    if (opComplementLeaveApp.isPresent() && opComplementLeaveApp.get().getApplication() != null) {
+		        result = new StringBuilder(opComplementLeaveApp.get().getApplication().getAppDate().getApplicationDate().toString("M/d(E)"))
+		                .append("\n")
+		                .append(result)
+		                .toString();
+		    } else {
+    			result = new StringBuilder(result).toString();
+		    }
 		}
 		
 		return result;
