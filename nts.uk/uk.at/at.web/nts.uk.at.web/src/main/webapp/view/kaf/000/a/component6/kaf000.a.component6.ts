d554712be025a04dfd31cf86cd80fdf747c21764
m_kitahira <m_kitahira@nittsusystem.co.jp>
2022-01-20 11:36:29 +0900

    申請：承認者欄の微調整とステータスアイコン

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/000/a/component6/kaf000.a.component6.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/000/a/component6/kaf000.a.component6.ts
@@ -7,14 +7,14 @@ module nts.uk.at.view.kaf000.a.component6.viewmodel {
 	            <div id="kaf000-a-component6" data-bind="click: openApproverDetail">
 					<div style="display: inline-block;" data-bind="if: approvalRootStateShort().length != 0 && approvalRootDisp">
 						<div data-bind="foreach: approvalRootStateShort" style="padding-top: 5px;">
-							<div class="approver-block" style="width: 112px;">
-								<div style="height: 24px;">
+							<div class="approver-block">
+								<div>
 									<div class="limited-label" style="vertical-align: middle;" data-bind="ntsFormLabel:{}, text: $component.getApproverLabel($index())"></div>
 								</div>
-								<div style="min-width: 112px;">
+								<div>
 									<div class="limited-label" style="vertical-align: middle;" data-bind="text: $data.approverName"></div>
 								</div>
-								<div data-bind="if: $data.representerName()" style="min-width: 112px;">
+								<div data-bind="if: $data.representerName()">
 									<div class="limited-label" style="vertical-align: middle;" data-bind="text: '(' + $data.representerName() + ')'"></div>
 								</div>
 							</div>
