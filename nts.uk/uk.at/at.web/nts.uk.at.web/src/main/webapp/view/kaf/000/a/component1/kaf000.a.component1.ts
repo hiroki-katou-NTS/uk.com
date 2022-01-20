cb617dc8f751a0804d790c9e65b67566b7be4d9e
m_kitahira <m_kitahira@nittsusystem.co.jp>
2022-01-20 10:40:13 +0900

    申請：right-panel内の微調整

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/000/a/component1/kaf000.a.component1.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/000/a/component1/kaf000.a.component1.ts
@@ -5,14 +5,18 @@ module nts.uk.at.view.kaf000.a.component1.viewmodel {
         name: 'kaf000-a-component1',
         template: `
             <div id="kaf000-a-component1">
-				<div class="right-panel-block" data-bind="if: displayMsg">
-					<div style="padding-top: 20px" class="message-div">
-						<div style="word-break: break-all; word-wrap: break-word; line-height: 1.6;" data-bind="html: message"></div>
-					</div>
+				<div data-bind="if: displayMsg">
+                    <div class="right-panel-block"
+                        <div style="padding-top: 20px" class="message-div">
+                            <div style="word-break: break-all; word-wrap: break-word; line-height: 1.6;" data-bind="html: message"></div>
+                        </div>
+                    </div>
 				</div>
-				<div class="right-panel-block" data-bind="if: displayDeadline">
-                    <div class="header" data-bind="text: $i18n('KAF000_2')"></div>
-                    <div class="content" data-bind="html: deadline" style="line-height: 1.8;"></div>
+				<div data-bind="if: displayDeadline">
+                    <div class="right-panel-block">
+                        <div class="header" data-bind="text: $i18n('KAF000_2')"></div>
+                        <div class="content" data-bind="html: deadline" style="line-height: 1.8;"></div>
+                    </div>
 				</div>
 			</div>
         `
