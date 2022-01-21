0bff8964aa41349779840c5a77417352adddc8f6
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2022-01-21 14:16:18 +0700

    uk/release_pj/lunch_order_1162: fix bug #122480

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/reservation/bento/RegisterReservationCorrectCommandHandler.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.app/src/main/java/nts/uk/ctx/at/record/app/command/reservation/bento/RegisterReservationCorrectCommandHandler.java
@@ -3,6 +3,7 @@ package nts.uk.ctx.at.record.app.command.reservation.bento;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
@@ -10,6 +11,8 @@ import javax.inject.Inject;
 import nts.arc.task.tran.AtomTask;
 import nts.arc.time.GeneralDate;
 import nts.arc.time.clock.ClockHourMinute;
+import nts.gul.collection.CollectionUtil;
+import nts.uk.ctx.at.record.dom.reservation.bento.BentoReservation;
 import nts.uk.ctx.at.record.dom.reservation.bento.BentoReservationRepository;
 import nts.uk.ctx.at.record.dom.reservation.reservationsetting.ReservationRecTime;
 import nts.uk.ctx.at.record.dom.reservation.reservationsetting.ReservationSetting;
@@ -33,18 +36,27 @@ public class RegisterReservationCorrectCommandHandler {
         // 1: get(会社ID=ログイン会社ID)
         ReservationSetting setting = reservationSettingRepository.findByCId(AppContexts.user().companyId()).get();
         
+        List<BentoReservation> bentoReservationLst = new ArrayList<>(); 
+        if(!CollectionUtil.isEmpty(bentoReservations)) {
+        	bentoReservationLst = bentoReservationRepository.getReservationInformation(
+        			bentoReservations.stream().map(x -> x.getBentorReservation().getRegisterInfor()).collect(Collectors.toList()), 
+        			bentoReservations.get(0).getBentorReservation().getReservationDate());
+        }
+        
         // 2: 社員は予約内容を修正できるか(char, 年月日, 年月日, 時刻時分, int, boolean, 予約受付時間帯)
         //      (ロールID=ログイン者の就業ロールID, 注文日=システム日付, 予約日=ループ中の弁当予約．予約日, 予約時刻=システム日時, 枠NO=ループ中弁当予約．予約対象日．締め時刻枠, 発注区分=ループ中の弁当予約．発注済み,予約受付時間帯=取得した予約設定．予約受付時間帯(ループ中弁当予約．予約対象日．締め時刻枠）)
-        bentoReservations.forEach(s -> {
+        for(BentoReservationWithEmp s : bentoReservations) {
+        	BentoReservation bentoReservation = bentoReservationLst.stream().filter(x -> 
+        		x.getRegisterInfor().getReservationCardNo().equals(s.getBentorReservation().getRegisterInfor().getReservationCardNo())).findAny().get();
             boolean flag = setting.getCorrectionContent().canEmployeeChangeReservation(
                     AppContexts.user().roles().forAttendance(), 
-                    s.getBentorReservation().getReservationDate().getDate(), 
+                    bentoReservation.getReservationDate().getDate(), 
                     ClockHourMinute.now(), 
-                    s.getBentorReservation().getReservationDate().getClosingTimeFrame().value, 
+                    bentoReservation.getReservationDate().getClosingTimeFrame().value, 
                     GeneralDate.today(), 
-                    s.getBentorReservation().isOrdered(), 
+                    bentoReservation.isOrdered(), 
                     setting.getReservationRecTimeZoneLst().stream()
-                        .filter(x -> x.getFrameNo().value == s.getBentorReservation().getReservationDate().getClosingTimeFrame().value)
+                        .filter(x -> x.getFrameNo().value == bentoReservation.getReservationDate().getClosingTimeFrame().value)
                         .findFirst().get());
             
             /**
@@ -72,7 +84,7 @@ public class RegisterReservationCorrectCommandHandler {
                 
                 exceptions.add(new RegisterErrorMessage("Msg_2257", Arrays.asList(param0, param1, param2)));
             }
-        });
+        }
         
         result.forEach(atomTask -> {
             atomTask.run();
