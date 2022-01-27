a37f0084b8888d3a314297bf173952f161a7e6c1
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2022-01-27 16:12:03 +0700

    Fix bug #122648: Change path

--- nts.uk/uk.mobile/ClientApp/views/ccg/007/a/index.ts
+++ nts.uk/uk.mobile/ClientApp/views/ccg/007/a/index.ts
@@ -42,11 +42,11 @@ export class Ccg007AComponent extends Vue {
                 vm.model.contractCode = contract.code;
                 vm.model.password = contract.password;
                 vm.$goto('ccg007b');
-            }else {
-                vm.$http.post(servicePath.getIsCloud)
+            } else {
+                vm.$http.post('at', servicePath.getIsCloud)
                     .done((data: boolean) => {
                         if (!data) {
-                            vm.$http.post(servicePath.getIsCloud)
+                            vm.$http.post('at', servicePath.getIsCloud)
                             .done((contact: any) => {
                                 storage.local.setItem('contract', { code: contact.code, password: '' });
                                 vm.$goto('ccg007b');
