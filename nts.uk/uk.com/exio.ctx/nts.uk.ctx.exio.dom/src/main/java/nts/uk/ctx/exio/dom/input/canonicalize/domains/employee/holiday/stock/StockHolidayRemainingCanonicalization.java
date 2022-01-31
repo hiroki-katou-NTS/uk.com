2a36c3041ca9670657114bbb32a5993f16077cf4
CORP\keisuke_hoshina <keisuke_hoshina@nittsusystem.co.jp>
2022-01-31 17:27:06 +0900

    エラーを出すためにContextも欲しくなったので、引数に追加

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/canonicalize/domains/employee/holiday/stock/StockHolidayRemainingCanonicalization.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/main/java/nts/uk/ctx/exio/dom/input/canonicalize/domains/employee/holiday/stock/StockHolidayRemainingCanonicalization.java
@@ -101,7 +101,9 @@ public class StockHolidayRemainingCanonicalization  extends IndependentCanonical
 	}
 
 	@Override
-	protected IntermediateResult canonicalizeExtends(DomainCanonicalization.RequireCanonicalize require, IntermediateResult targertResult) {
+	protected IntermediateResult canonicalizeExtends(DomainCanonicalization.RequireCanonicalize require, 
+																						ExecutionContext context, 
+																						IntermediateResult targertResult) {
 		return addFixedItems(targertResult);
 	}
 	
