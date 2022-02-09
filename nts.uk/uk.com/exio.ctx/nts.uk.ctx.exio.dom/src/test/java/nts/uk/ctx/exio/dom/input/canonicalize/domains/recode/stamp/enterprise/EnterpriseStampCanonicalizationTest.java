0402844c7f5537605434c7162b1332ba25cac033
m_kitahira <m_kitahira@nittsusystem.co.jp>
2022-02-09 10:12:51 +0900

    外部受入／打刻データE版：UT実装とバグ修正

---
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/test/java/nts/uk/ctx/exio/dom/input/canonicalize/domains/recode/stamp/enterprise/EnterpriseStampCanonicalizationTest.java
@@ -0,0 +1,12 @@
+package nts.uk.ctx.exio.dom.input.canonicalize.domains.recode.stamp.enterprise;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class EnterpriseStampCanonicalizationTest {
+
+    @Test
+    public void test() {
+        assertThat(true).isFalse();
+    }
+}
