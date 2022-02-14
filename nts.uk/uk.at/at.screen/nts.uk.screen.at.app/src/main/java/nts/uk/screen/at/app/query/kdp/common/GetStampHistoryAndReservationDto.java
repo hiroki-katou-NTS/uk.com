d2e91dd78e2dc2ae73e5f6be0c39d529bacaa54a
nsvn-nguyenthanhchung <61732548+nsvn-nguyenthanhchung@users.noreply.github.com>
2022-02-14 15:24:15 +0700

    Dakoku8: Create SQ: 打刻入力の打刻履歴と予約を取得する

---
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/query/kdp/common/GetStampHistoryAndReservationDto.java
@@ -0,0 +1,13 @@
+package nts.uk.screen.at.app.query.kdp.common;
+
+import javax.ejb.Stateless;
+
+/**
+ * @author chungnt
+ *
+ */
+
+@Stateless
+public class GetStampHistoryAndReservationDto {
+
+}
