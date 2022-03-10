6b28c5b5cab0150e2eeec51eea6a2e6c71af2280
NSVN-NguyenThiHoangYen <yen_nt@nittsusystem-vn.com>
2022-03-10 18:15:02 +0900

    外部受入初期データボタンを対応する

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/smm/001/a/smm001.a.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/smm/001/a/smm001.a.vm.ts
@@ -5,7 +5,8 @@ module nts.uk.com.view.smm001.a {
   const API = {
     // <<ScreenQuery>> 初期起動の情報取得する
     getInitialStartupInformation: 'com/screen/smm001/get-initial-startup-information',
-    registerSmileCooperationAcceptanceSetting: 'com/screen/smm001/register-smile-cooperation-acceptance-setting'
+    registerSmileCooperationAcceptanceSetting: 'com/screen/smm001/register-smile-cooperation-acceptance-setting',
+    initDataRegister: 'ctx/link/smile/accept/setting/generatedata'
   };
 
   class ItemModel {
@@ -286,5 +287,106 @@ module nts.uk.com.view.smm001.a {
         // End: Process send request
       }
     }
-  }
+    
+    /**
+     * event when click init data create button 
+     */
+    createInitialData(acceptanceItem : number, settingCode: String){
+        const vm = this;
+        let initData = {
+            acceptanceItem : acceptanceItem,
+            settingCode : settingCode
+        }
+        vm.$ajax('com', API.initDataRegister, initData).then(() => {
+            vm.resA("Msg_15");
+        }).fail((err) => {
+            vm.$dialog.error(err);
+        }).always(() =>{
+            vm.$blockui('clear');
+        })
+    }
+    
+    createOrganization(){
+        const vm = this;
+        nts.uk.ui.errors.clearAll()
+        if(!vm.initDataText1()){
+            vm.$dialog.error("Msg_3325");
+            return;
+        }
+        $('.init-text1').ntsError('check');
+        if(!nts.uk.ui.errors.hasError())
+            vm.createInitialData(1, vm.initDataText1());
+    }
+    
+    createHuman(){
+        const vm = this;
+        nts.uk.ui.errors.clearAll()
+        if(!vm.initDataText2()){
+            vm.$dialog.error("Msg_3325");
+            return;    
+        }
+        $('.init-text2').ntsError('check');
+        if(!nts.uk.ui.errors.hasError())
+            vm.createInitialData(2, vm.initDataText2());        
+    }
+      
+    jobSystem(){   
+        const vm = this;
+        nts.uk.ui.errors.clearAll()
+        if(!vm.initDataText3()){
+            vm.$dialog.error("Msg_3325");
+            return;
+        }
+        $('.init-text3').ntsError('check');
+        if(!nts.uk.ui.errors.hasError())
+            vm.createInitialData(3, vm.initDataText3());         
+    }
+      
+    address(){
+        const vm = this;
+        nts.uk.ui.errors.clearAll()
+        if(!vm.initDataText4()){
+            vm.$dialog.error("Msg_3325");
+            return;
+        }            
+        $('.init-text4').ntsError('check');
+        if(!nts.uk.ui.errors.hasError())
+            vm.createInitialData(4, vm.initDataText4());            
+    } 
+        
+    leaveWork(){
+        const vm = this;
+        nts.uk.ui.errors.clearAll()
+        if(!vm.initDataText5()){
+            vm.$dialog.error("Msg_3325");
+            return;
+        }            
+        $('.init-text5').ntsError('check');
+        if(!nts.uk.ui.errors.hasError())
+            vm.createInitialData(5, vm.initDataText5());         
+    }
+
+    belongs(){
+        const vm = this;
+        nts.uk.ui.errors.clearAll()
+        if(!vm.initDataText6()){
+            vm.$dialog.error("Msg_3325");
+            return;
+        }            
+        $('.init-text6').ntsError('check');
+        if(!nts.uk.ui.errors.hasError())
+            vm.createInitialData(6, vm.initDataText6());         
+    }
+      
+    employee(){
+        const vm = this;
+        nts.uk.ui.errors.clearAll()
+        if(!vm.initDataText7()){
+            vm.$dialog.error("Msg_3325");
+            return;
+        }            
+        $('.init-text7').ntsError('check');
+        if(!nts.uk.ui.errors.hasError())
+            vm.createInitialData(7, vm.initDataText7());          
+    }      
 }
