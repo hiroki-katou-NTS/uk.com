1e9640d4880ba49248575e0b1416ed4b1a317e41
NSVN-PhamVanDan <dan_pv@nittsusystem-vn.com>
2022-03-11 13:22:59 +0700

    fix #123355

--- nts.uk/uk.at/at.ctx/aggregation/nts.uk.ctx.at.aggregation.dom/src/main/java/nts/uk/ctx/at/aggregation/dom/form9/Form9Code.java
+++ nts.uk/uk.at/at.ctx/aggregation/nts.uk.ctx.at.aggregation.dom/src/main/java/nts/uk/ctx/at/aggregation/dom/form9/Form9Code.java
@@ -4,6 +4,7 @@ import nts.arc.primitive.StringPrimitiveValue;
 import nts.arc.primitive.constraint.CharType;
 import nts.arc.primitive.constraint.StringCharType;
 import nts.arc.primitive.constraint.StringMaxLength;
+import nts.uk.shr.com.primitive.ZeroPaddedCode;
 
 /**
  * 様式９のコード
@@ -13,6 +14,7 @@ import nts.arc.primitive.constraint.StringMaxLength;
  */
 @StringMaxLength(10)
 @StringCharType(CharType.ALPHA_NUMERIC)
+@ZeroPaddedCode
 public class Form9Code extends StringPrimitiveValue<Form9Code> {
 
 	/** serialVersionUID **/
