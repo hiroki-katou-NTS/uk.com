4ee8723967e45d752fa7f3c48ff2236080f61234
m_kitahira <m_kitahira@nittsusystem.co.jp>
2022-03-11 11:55:41 +0900

    外部受入／F画面　要らなくなったWebAPIとクエリ

--- nts.uk/uk.com/com.screen/nts.uk.screen.com.ws/src/main/java/nts/uk/screen/com/ws/cmf/cmf001/Cmf001fWebService.java
+++ nts.uk/uk.com/com.screen/nts.uk.screen.com.ws/src/main/java/nts/uk/screen/com/ws/cmf/cmf001/Cmf001fWebService.java
@@ -1,7 +1,5 @@
 package nts.uk.screen.com.ws.cmf.cmf001;
 
-import java.util.List;
-
 import javax.inject.Inject;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -10,13 +8,10 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import nts.uk.ctx.exio.dom.input.setting.ExternalImportCode;
-import nts.uk.screen.com.app.cmf.cmf001.b.get.GetLayoutParam;
 import nts.uk.screen.com.app.cmf.cmf001.f.delete.Cmf001fDeleteCommand;
 import nts.uk.screen.com.app.cmf.cmf001.f.delete.Cmf001fDeleteCommandHandler;
-import nts.uk.screen.com.app.cmf.cmf001.f.get.CsvBaseLayoutDto;
 import nts.uk.screen.com.app.cmf.cmf001.f.get.CsvBasedImportSettingDto;
 import nts.uk.screen.com.app.cmf.cmf001.f.get.GetCsvBasedImportSetting;
-import nts.uk.screen.com.app.cmf.cmf001.f.get.GetLayoutForCsvBase;
 import nts.uk.screen.com.app.cmf.cmf001.f.save.Cmf001fSaveCommand;
 import nts.uk.screen.com.app.cmf.cmf001.f.save.Cmf001fSaveCommandHandler;
 
@@ -34,22 +29,11 @@ public class Cmf001fWebService {
 	}
 	
 	@Inject
-	private GetLayoutForCsvBase layout;
-	
-	@Inject
 	private Cmf001fSaveCommandHandler handler;
 	
 	@Inject
 	private Cmf001fDeleteCommandHandler delHandler;
 	
-	
-	@POST
-	@Path("get/layout/detail")
-	public List<CsvBaseLayoutDto> getLayoutDetail(GetLayoutParam query) {
-		List<CsvBaseLayoutDto> result = layout.getCsvBaseDetail(query);
-		return result;
-	}
-
 	@POST
 	@Path("save")
 	public void save(Cmf001fSaveCommand command) {
