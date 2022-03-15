02556ff2960feb93863cdbb4d1483054b78f89a1
wempwoo <wemp.woo@gmail.com>
2022-03-15 18:19:33 +0900

    bugfix: TenantLocator利用環境で共有打刻を開き、契約認証をすると、システムエラー。 なぜか、画面を再リロードしてからでないと、セッションのDataSource情報が取得できないため、契約認証をしたらリロードするようにした。 ただ、単純にリロードするだけではcharacteristicsの書き込みが遅延しているため、契約ログイン画面側は書き込みが終了してからダイアログを閉じるように修正。

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/007/a/ccg007.a.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/ccg/007/a/ccg007.a.vm.ts
@@ -29,12 +29,15 @@ module nts.uk.pr.view.ccg007.a {
                 blockUI.invisible();
                 if (!nts.uk.ui.errors.hasError()) {
                     service.submitForm({ contractCode: _.escape(self.contractCode()), password: _.escape(self.password()) }).done(function () {
-                        nts.uk.characteristics.remove("contractInfo");
-                        nts.uk.characteristics.save("contractInfo", { contractCode: _.escape(self.contractCode()), contractPassword: _.escape(self.password()) });
-                        nts.uk.ui.windows.setShared('contractCode', _.escape(self.contractCode()));
-                        nts.uk.ui.windows.setShared('contractPassword', _.escape(self.password()));
-                        nts.uk.ui.windows.setShared('isSubmit', true);
-                        nts.uk.ui.windows.close();
+                        $.Deferred().resolve()
+                            .then(() => nts.uk.characteristics.remove("contractInfo"))
+                            .then(() => nts.uk.characteristics.save("contractInfo", { contractCode: _.escape(self.contractCode()), contractPassword: _.escape(self.password()) }))
+                            .then(() => {
+                                nts.uk.ui.windows.setShared('contractCode', _.escape(self.contractCode()));
+                                nts.uk.ui.windows.setShared('contractPassword', _.escape(self.password()));
+                                nts.uk.ui.windows.setShared('isSubmit', true);
+                                nts.uk.ui.windows.close();
+                            });
                     }).fail(function (res: any) {
                         nts.uk.ui.dialog.alertError({ messageId: res.messageId, messageParams: res.parameterIds });
                     }).always(() => {
