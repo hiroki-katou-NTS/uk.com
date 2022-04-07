be87d0a72c847072aa882a55b9e6b62a9c2ca982
CORP\kaito_kunii <kaito_kunii@kinjiro-e.com>
2022-04-07 11:43:11 +0900

    #124054 UnitTest

--- nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/test/java/nts/uk/ctx/exio/dom/input/setting/assembly/revise/type/time/TimeBase60DelimiterTest.java
+++ nts.uk/uk.com/exio.ctx/nts.uk.ctx.exio.dom/src/test/java/nts/uk/ctx/exio/dom/input/setting/assembly/revise/type/time/TimeBase60DelimiterTest.java
@@ -5,6 +5,9 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.Test;
 
 import lombok.val;
+import nts.arc.testing.assertion.NtsAssert;
+import nts.gul.util.Either;
+import nts.uk.ctx.exio.dom.input.errors.ErrorMessage;
 
 public class TimeBase60DelimiterTest {
 
@@ -42,6 +45,36 @@ public class TimeBase60DelimiterTest {
 	public void COLON_minus_0_01() {
 		val either = TimeBase60Delimiter.COLON.toMinutes("-0:01");
 		assertThat(either.getRight()).isEqualTo(-1);
+	}
+
+	@Test
+	public void NOTHING_COLON_error() {
+		Either<ErrorMessage, Integer> result = NtsAssert.Invoke.privateMethod(
+				TimeBase60Delimiter.COLON,
+				"convertDelimiter",
+				"08");
+
+		assertThat(result.getLeft()).isEqualTo(new ErrorMessage("時間の区切り文字が正しく含まれていません。"));
+	}
+
+	@Test
+	public void TWO_COLON_OK() {
+		Either<ErrorMessage, Integer> result = NtsAssert.Invoke.privateMethod(
+				TimeBase60Delimiter.COLON,
+				"convertDelimiter",
+				"08:30:35");
+
+		assertThat(result.getRight()).isEqualTo(510);
+	}
+
+	@Test
+	public void OVER_TWO_COLON_OK() {
+		Either<ErrorMessage, Integer> result = NtsAssert.Invoke.privateMethod(
+				TimeBase60Delimiter.COLON,
+				"convertDelimiter",
+				"08:30:35:44");
+
+		assertThat(result.getRight()).isEqualTo(510);
 	}
 
 }
