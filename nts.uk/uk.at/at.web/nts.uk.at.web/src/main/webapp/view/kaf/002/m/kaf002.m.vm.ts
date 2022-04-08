7bc134349f4b75f194d2bd098c0948db88cae063
nws-ductm <ductm@newwave.vn>
2022-04-08 13:48:59 +0700

    Update KAF002 #123792

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/002/m/kaf002.m.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/002/m/kaf002.m.vm.ts
@@ -716,7 +716,7 @@ module nts.uk.at.view.kaf002_ref.m.viewmodel {
                     { headerText: self.$i18n('KAF002_23'), key: 'endTime', dataType: 'string', width: 140 },
                     {
                         headerText: self.$i18n('KAF002_81'), key: 'workplaceId', dataType: 'string', width: 230,
-                        ntsControl: 'Button_WorkPlace', hidden: self.selectedTemp() === STAMPTYPE.ATTENDENCE, //self.kaf002Data && self.kaf002Data.appStampSetting.wkpDisAtr == 0,
+                        ntsControl: 'Button_WorkPlace', hidden: self.selectedTemp() === STAMPTYPE.ATTENDENCE || (self.kaf002Data && self.kaf002Data.appStampSetting.wkpDisAtr == 0),
                     },
                     {
                         headerText: self.$i18n('KAF002_82'), key: 'workLocationCD', dataType: 'string', width: 230,
