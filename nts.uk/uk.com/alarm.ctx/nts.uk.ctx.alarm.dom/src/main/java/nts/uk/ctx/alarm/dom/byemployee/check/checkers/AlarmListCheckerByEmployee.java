5be223b4c81d25340c0973327fefc084d6cdc6c3
m_kitahira <m_kitahira@kinjiro-e.com>
2022-04-11 13:29:06 +0900

    見込み月次のクラス名を正す

--- nts.uk/uk.com/alarm.ctx/nts.uk.ctx.alarm.dom/src/main/java/nts/uk/ctx/alarm/dom/byemployee/check/checkers/AlarmListCheckerByEmployee.java
+++ nts.uk/uk.com/alarm.ctx/nts.uk.ctx.alarm.dom/src/main/java/nts/uk/ctx/alarm/dom/byemployee/check/checkers/AlarmListCheckerByEmployee.java
@@ -8,7 +8,7 @@ import nts.uk.ctx.alarm.dom.byemployee.check.checkers.appapproval.AppApprovalChe
 import nts.uk.ctx.alarm.dom.byemployee.check.checkers.record.daily.DailyCheckerByEmployee;
 import nts.uk.ctx.alarm.dom.byemployee.check.checkers.master.MasterCheckerByEmployee;
 import nts.uk.ctx.alarm.dom.byemployee.check.checkers.record.monthly.MonthlyCheckerByEmployee;
-import nts.uk.ctx.alarm.dom.byemployee.check.checkers.prospect.monthly.ScheduleMonthlyCheckerByEmployee;
+import nts.uk.ctx.alarm.dom.byemployee.check.checkers.prospect.monthly.ProspectMonthlyCheckerByEmployee;
 import nts.uk.ctx.alarm.dom.byemployee.check.context.CheckingContextByEmployee;
 
 /**
@@ -21,7 +21,7 @@ public interface AlarmListCheckerByEmployee {
     interface Require extends
             // 全CheckerのRequireCheckを追加する
             ScheduleDailyCheckerByEmployee.RequireCheck,
-            ScheduleMonthlyCheckerByEmployee.RequireCheck,
+            ProspectMonthlyCheckerByEmployee.RequireCheck,
             DailyCheckerByEmployee.RequireCheck,
             CheckErrorAlarmWeekly.RequireCheck,
             MonthlyCheckerByEmployee.RequireCheck,
