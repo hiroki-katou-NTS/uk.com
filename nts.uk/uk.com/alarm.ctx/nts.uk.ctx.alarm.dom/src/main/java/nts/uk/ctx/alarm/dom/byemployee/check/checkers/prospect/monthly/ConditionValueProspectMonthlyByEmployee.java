85cfee2b397985cd56179948e0a1bed388482722
ai_muto <ai_muto@nittsusystem.co.jp>
2022-04-13 20:05:45 +0900

    見込み月次の固定ロジック追加

--- nts.uk/uk.com/alarm.ctx/nts.uk.ctx.alarm.dom/src/main/java/nts/uk/ctx/alarm/dom/byemployee/check/checkers/prospect/monthly/ConditionValueProspectMonthlyByEmployee.java
+++ nts.uk/uk.com/alarm.ctx/nts.uk.ctx.alarm.dom/src/main/java/nts/uk/ctx/alarm/dom/byemployee/check/checkers/prospect/monthly/ConditionValueProspectMonthlyByEmployee.java
@@ -3,13 +3,31 @@ package nts.uk.ctx.alarm.dom.byemployee.check.checkers.prospect.monthly;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
+import lombok.val;
+import nts.arc.layer.app.cache.CacheCarrier;
+import nts.arc.time.GeneralDate;
+import nts.arc.time.YearMonth;
+import nts.arc.time.calendar.period.DatePeriod;
 import nts.uk.ctx.alarm.dom.byemployee.check.aggregate.AggregateIntegrationOfDaily;
+import nts.uk.ctx.alarm.dom.byemployee.check.aggregate.AggregateIntegrationOfMonthly;
 import nts.uk.ctx.alarm.dom.byemployee.check.checkers.AlarmListCategoryByEmployee;
 import nts.uk.ctx.alarm.dom.byemployee.result.DateInfo;
 import nts.uk.ctx.alarm.dom.conditionvalue.ConditionValueContext;
 import nts.uk.ctx.alarm.dom.conditionvalue.ConditionValueLogic;
+import nts.uk.ctx.at.shared.dom.scherec.statutory.worktime.UsageUnitSetting;
+import nts.uk.ctx.at.shared.dom.scherec.statutory.worktime.algorithm.monthly.MonAndWeekStatutoryTime;
+import nts.uk.ctx.at.shared.dom.scherec.statutory.worktime.algorithm.monthly.MonthlyStatutoryWorkingHours;
+import nts.uk.ctx.at.shared.dom.scherec.statutory.worktime.monunit.*;
+import nts.uk.ctx.at.shared.dom.workingcondition.WorkingCondition;
+import nts.uk.ctx.at.shared.dom.workingcondition.WorkingConditionItem;
+import nts.uk.ctx.at.shared.dom.workingcondition.WorkingConditionItemWithPeriod;
+import nts.uk.ctx.at.shared.dom.workingcondition.WorkingSystem;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * 条件値チェック(社員別・見込み月次)
@@ -19,15 +37,64 @@ public enum ConditionValueProspectMonthlyByEmployee implements ConditionValueLog
 
     支給額(1, "（予定時間＋総労働時間）×基準単価", c -> c.aggregate.aggregate(c.require, data -> {
 
-        int tanka = 0; // TODO: どうにかして取ってくる
-
-        double totalTime = data.getAttendanceTimeOfDailyPerformance()
+        // 就業時間金額＋割増時間金額を求める
+        // 就業時間金額
+        double withinWorkTimeAmount = data.getAttendanceTimeOfDailyPerformance()
                 .get()
                 .getActualWorkingTimeOfDaily()
-                .getTotalWorkingTime().getTotalTime().valueAsMinutes();
+                .getTotalWorkingTime()
+                .getWithinStatutoryTimeOfDaily()
+                .getWithinWorkTimeAmount()
+                .v();
+        // 割増時間金額
+        double totalAmount = data.getAttendanceTimeOfDailyPerformance()
+                .get()
+                .getActualWorkingTimeOfDaily()
+                .getPremiumTimeOfDailyPerformance()
+                .getTotalAmount()
+                .v();
 
-        return totalTime * tanka;
+        return withinWorkTimeAmount + totalAmount;
     })),
+    所定公休日数比(2, "対比：所定公休日数比", c -> c.aggregate.calcRatioValue(c.require,
+    data -> {
+        // TODO: 保留
+        // 実際の公休日数
+        //return data.getPublicHolidayLeaveRemain().get().getPublicHolidayday().v();
+        return null;
+    },
+    date -> {
+        // 所定公休日数
+        // TODO: 保留
+        return null;
+    })),
+//    基準時間比_通常勤務(3, "対比：基準時間比（通常勤務）", c -> c.aggregate.calcRatioValueDenominatorFromOther(c.require,
+//    data -> {
+//        // 総労働時間
+//        return data.getAttendanceTimeOfDailyPerformance()
+//                .get()
+//                .getActualWorkingTimeOfDaily()
+//                .getTotalWorkingTime()
+//                .getTotalTime()
+//                .v().doubleValue();
+//    },
+//    (employeeId, period) -> {
+//        // MonthlyStatutoryWorkingHours で労働制ごとに取得
+//        val workingSystems = c.require.workingCondition(employeeId, period).stream()
+//                .map(wc -> wc.getWorkingConditionItem().getLaborSystem())
+//                .collect(Collectors.toList());
+//        val workingSystem = workingSystems.get(0);
+//        if (workingSystems.size() > 1){
+//            // 月中で労働制変わってた場合基準時間ってどうなんの？？？
+//        }
+//
+//        if (workingSystem.isFlexTimeWork()){
+//            return c.require.flexMonAndWeekStatutoryTime(workingSystem);
+//        }
+//        else {
+//            return c.require.monAndWeekStatutoryTime(workingSystem);
+//        }
+//    })),
 
     ;
 
@@ -44,8 +111,10 @@ public enum ConditionValueProspectMonthlyByEmployee implements ConditionValueLog
         return getValue.apply(context);
     }
 
-    public interface Require extends AggregateIntegrationOfDaily.AggregationRequire {
+    public interface Require extends
+            AggregateIntegrationOfDaily.AggregationRequire{
 
+        //List<WorkingConditionItemWithPeriod> workingCondition(String employeeId, DatePeriod datePeriod);
     }
 
     @Value
@@ -65,7 +134,7 @@ public enum ConditionValueProspectMonthlyByEmployee implements ConditionValueLog
 
         @Override
         public DateInfo getDateInfo() {
-            return new DateInfo(aggregate.getClosureMonth());
+            return new DateInfo(aggregate.getAggregationPeriod());
         }
     }
 }
