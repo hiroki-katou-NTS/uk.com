3b3fbb4ba28120506104b21dc83deabb07eaecb3
Hiroki Katou <hiroki_katou@kinjiro-e.com>
2022-04-15 19:53:49 +0900

    年休使用義務関係リファクタ

--- nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/vacation/obligannleause/ObligedAnnLeaUseService.java
+++ nts.uk/uk.at/at.ctx/shared/nts.uk.ctx.at.shared.dom/src/main/java/nts/uk/ctx/at/shared/dom/vacation/obligannleause/ObligedAnnLeaUseService.java
@@ -3,7 +3,6 @@ package nts.uk.ctx.at.shared.dom.vacation.obligannleause;
 import java.util.Optional;
 
 import nts.arc.time.GeneralDate;
-import nts.uk.ctx.at.shared.dom.remainingnumber.annualleave.ReferenceAtr;
 import nts.uk.ctx.at.shared.dom.remainingnumber.annualleave.empinfo.grantremainingdata.AnnualLeaveGrantRemainingData;
 import nts.uk.ctx.at.shared.dom.remainingnumber.annualleave.empinfo.grantremainingdata.daynumber.AnnualLeaveUsedDayNumber;
 import nts.arc.time.calendar.period.DatePeriod;
@@ -15,39 +14,36 @@ import nts.arc.time.calendar.period.DatePeriod;
 public interface ObligedAnnLeaUseService {
 
 	/**
+	 * 年休使用日数が使用義務日数を満たしているかチェックする
+	 * @param obligedUseDays
+	 * @param annualLeaveUsedDays
+	 * @return
+	 */
+	boolean checkObligedUseDays(ObligedUseDays obligedUseDays, AnnLeaUsedDaysOutput annualLeaveUsedDays);
+
+	/**
 	 * 使用義務日数の取得
-	 * @param companyId 会社ID
-	 * @param distributeAtr 期間按分使用区分
 	 * @param criteria 基準日
 	 * @param obligedAnnualLeaveUse 年休使用義務日数
 	 * @return 年休使用義務日数
 	 */
-	Optional<AnnualLeaveUsedDayNumber> getObligedUseDays(String companyId, boolean distributeAtr,
-			GeneralDate criteria, ObligedAnnualLeaveUse obligedAnnualLeaveUse);
+	Optional<ObligedUseDays> getObligedUseDays(GeneralDate criteria, ObligedAnnualLeaveUse obligedAnnualLeaveUse);
 	
 	/**
 	 * 義務日数計算期間内の年休使用数を取得
-	 * @param companyId 会社ID
-	 * @param employeeId 社員ID
 	 * @param criteria 基準日
-	 * @param referenceAtr 参照先区分
-	 * @param distributeAtr 期間按分使用区分
 	 * @param obligedAnnualLeaveUse 年休使用義務日数
 	 * @return 年休使用数Output
 	 */
-	AnnLeaUsedDaysOutput getAnnualLeaveUsedDays(String companyId, String employeeId,
-			GeneralDate criteria, ReferenceAtr referenceAtr, boolean distributeAtr,
-			ObligedAnnualLeaveUse obligedAnnualLeaveUse);
+	AnnLeaUsedDaysOutput getAnnualLeaveUsedDays(GeneralDate criteria, ObligedAnnualLeaveUse obligedAnnualLeaveUse);
 	
 	/**
 	 * 按分が必要かどうか判断
-	 * @param distributeAtr 期間按分使用区分
 	 * @param criteria 基準日
 	 * @param obligedAnnualLeaveUse 年休使用義務日数
 	 * @return　true:必要、false:不要
 	 */
-	boolean checkNeedForProportion(boolean distributeAtr, GeneralDate criteria,
-			ObligedAnnualLeaveUse obligedAnnualLeaveUse);
+	boolean checkNeedForProportion(GeneralDate criteria, ObligedAnnualLeaveUse obligedAnnualLeaveUse);
 
 	/**
 	 * 年休使用義務日数の按分しない場合の期間を取得
@@ -55,8 +51,7 @@ public interface ObligedAnnLeaUseService {
 	 * @param obligedAnnualLeaveUse 年休使用義務日数
 	 * @return 期間
 	 */
-	Optional<DatePeriod> getPeriodForNotProportion(GeneralDate criteria,
-			ObligedAnnualLeaveUse obligedAnnualLeaveUse);
+	Optional<DatePeriod> getPeriodForNotProportion(GeneralDate criteria, ObligedAnnualLeaveUse obligedAnnualLeaveUse);
 	
 	/**
 	 * 年休使用義務日数の按分しない期間の付与日数を取得
@@ -64,8 +59,7 @@ public interface ObligedAnnLeaUseService {
 	 * @param obligedAnnualLeaveUse 年休使用義務日数
 	 * @return 年休付与残数データ
 	 */
-	Optional<AnnualLeaveGrantRemainingData> getGrantInfoForNotProportion(GeneralDate criteria,
-			ObligedAnnualLeaveUse obligedAnnualLeaveUse);
+	Optional<AnnualLeaveGrantRemainingData> getGrantInfoForNotProportion(GeneralDate criteria, ObligedAnnualLeaveUse obligedAnnualLeaveUse);
 	
 	/**
 	 * 付与期間と重複する付与期間を持つ残数履歴データを取得
@@ -73,15 +67,12 @@ public interface ObligedAnnLeaUseService {
 	 * @param obligedAnnualLeaveUse 年休使用義務日数
 	 * @return 年休付与情報Output
 	 */
-	AnnLeaGrantInfoOutput getRemainDatasAtDupGrantPeriod(GeneralDate criteria,
-			ObligedAnnualLeaveUse obligedAnnualLeaveUse);
+	AnnLeaGrantInfoOutput getRemainDatasAtDupGrantPeriod(GeneralDate criteria, ObligedAnnualLeaveUse obligedAnnualLeaveUse);
 	
 	/**
 	 * 年休使用義務日数の期間按分
-	 * @param distributeAtr 期間按分使用区分
 	 * @param obligedAnnualLeaveUse 年休使用義務日数
 	 * @return 年休使用義務日数
 	 */
-	Optional<AnnualLeaveUsedDayNumber> distributePeriod(boolean distributeAtr,
-			ObligedAnnualLeaveUse obligedAnnualLeaveUse);
+	Optional<AnnualLeaveUsedDayNumber> distributePeriod(ObligedAnnualLeaveUse obligedAnnualLeaveUse);
 }
