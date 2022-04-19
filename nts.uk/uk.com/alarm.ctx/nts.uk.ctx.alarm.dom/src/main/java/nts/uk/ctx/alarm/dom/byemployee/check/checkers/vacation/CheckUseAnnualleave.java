7295ae1d69b3cd2c07747cc5edc1b9a913e4bf43
Hiroki Katou <hiroki_katou@kinjiro-e.com>
2022-04-19 17:17:13 +0900

    使用区分の考慮漏れ

--- nts.uk/uk.com/alarm.ctx/nts.uk.ctx.alarm.dom/src/main/java/nts/uk/ctx/alarm/dom/byemployee/check/checkers/vacation/CheckUseAnnualleave.java
+++ nts.uk/uk.com/alarm.ctx/nts.uk.ctx.alarm.dom/src/main/java/nts/uk/ctx/alarm/dom/byemployee/check/checkers/vacation/CheckUseAnnualleave.java
@@ -1,30 +1,21 @@
 package nts.uk.ctx.alarm.dom.byemployee.check.checkers.vacation;
 
-import lombok.val;
 import nts.arc.time.GeneralDate;
 import nts.uk.ctx.alarm.dom.AlarmListAlarmMessage;
 import nts.uk.ctx.alarm.dom.byemployee.check.checkers.AlarmListCategoryByEmployee;
 import nts.uk.ctx.alarm.dom.byemployee.result.AlarmRecordByEmployee;
 import nts.uk.ctx.alarm.dom.byemployee.result.DateInfo;
-import nts.uk.ctx.at.record.dom.workrecord.remainingnumbermanagement.RCAnnualHolidayManagement;
-import nts.uk.ctx.at.shared.dom.remainingnumber.annualleave.ReferenceAtr;
-import nts.uk.ctx.at.shared.dom.remainingnumber.annualleave.empinfo.grantremainingdata.AnnualLeaveGrantRemainingData;
-import nts.uk.ctx.at.shared.dom.remainingnumber.annualleave.empinfo.grantremainingdata.daynumber.AnnualLeaveUsedDayNumber;
-import nts.uk.ctx.at.shared.dom.remainingnumber.common.empinfo.grantremainingdata.daynumber.LeaveGrantDayNumber;
-import nts.uk.ctx.at.shared.dom.remainingnumber.common.empinfo.grantremainingdata.daynumber.LeaveGrantNumber;
-import nts.uk.ctx.at.shared.dom.vacation.obligannleause.ObligedAnnLeaUseService;
-import nts.uk.ctx.at.shared.dom.vacation.obligannleause.ObligedAnnualLeaveUse;
 import nts.uk.ctx.at.shared.dom.yearholidaygrant.service.Period;
 
-import javax.inject.Inject;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 /**
  * 年休取得のチェック条件
  */
 public class CheckUseAnnualleave {
+
+	/** 使用するか */
+	boolean enabled;
 
 	/** 次回の年休付与日が指定した月以内にある人のみチェックする */
 	private boolean onlyWithinNextGrantDate;
@@ -44,14 +35,15 @@ public class CheckUseAnnualleave {
 	 * @return
 	 */
 	public Optional<AlarmRecordByEmployee> checkIfEnabled(RequireCheck require, String employeeId) {
-		// 次回の年休付与日が指定した月以内にある人のみチェックする
-		if(onlyWithinNextGrantDate &&
+		if (enabled) {
+			// 次回の年休付与日が指定した月以内にある人のみチェックする
+			if(onlyWithinNextGrantDate &&
 				require.checkExistHolidayGrantAdapter(employeeId, GeneralDate.today(), new Period(GeneralDate.today(),GeneralDate.today().addMonths(months.get())))){
-			return Optional.empty();
-		}
-
-		if(!obligedUseAnnualLeave.check(require, employeeId)) {
-			return Optional.of(alarm(employeeId));
+				return Optional.empty();
+			}
+			if(!obligedUseAnnualLeave.check(require, employeeId)) {
+				return Optional.of(alarm(employeeId));
+			}
 		}
 		return Optional.empty();
 	}
