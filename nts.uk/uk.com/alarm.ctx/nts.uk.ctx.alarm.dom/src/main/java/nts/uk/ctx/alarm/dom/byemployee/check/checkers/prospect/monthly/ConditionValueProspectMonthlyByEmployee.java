e9d16ad9716b0602d18ff76783160410c72722cb
RaikiAsada <raiki_asada@kinjiro-e.com>
2022-04-19 20:00:01 +0900

    ビルドエラー削除

--- nts.uk/uk.com/alarm.ctx/nts.uk.ctx.alarm.dom/src/main/java/nts/uk/ctx/alarm/dom/byemployee/check/checkers/prospect/monthly/ConditionValueProspectMonthlyByEmployee.java
+++ nts.uk/uk.com/alarm.ctx/nts.uk.ctx.alarm.dom/src/main/java/nts/uk/ctx/alarm/dom/byemployee/check/checkers/prospect/monthly/ConditionValueProspectMonthlyByEmployee.java
@@ -1,5 +1,7 @@
 package nts.uk.ctx.alarm.dom.byemployee.check.checkers.prospect.monthly;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
@@ -16,12 +18,12 @@ import nts.uk.ctx.at.record.dom.require.RecordDomRequireService;
 import nts.uk.ctx.at.shared.dom.adapter.employment.BsEmploymentHistoryImport;
 import nts.uk.ctx.at.shared.dom.scherec.statutory.worktime.algorithm.monthly.MonthlyStatutoryWorkingHours;
 import nts.uk.ctx.at.shared.dom.workingcondition.WorkingConditionItemWithPeriod;
-import nts.uk.shr.com.context.AppContexts;
 import nts.uk.shr.com.time.closure.ClosureMonth;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import nts.arc.time.calendar.period.DatePeriod;
 import nts.uk.ctx.alarm.dom.byemployee.check.checkers.prospect.countdays.AbsenceDaysProspector;
 import nts.uk.ctx.alarm.dom.byemployee.check.checkers.prospect.countdays.AttendanceDaysProspector;
 import nts.uk.ctx.alarm.dom.byemployee.check.checkers.prospect.countdays.HolidayWorkDaysProspector;
@@ -31,7 +33,12 @@ import nts.uk.ctx.alarm.dom.byemployee.check.checkers.prospect.countdays.WorkDay
 import nts.uk.ctx.alarm.dom.byemployee.check.checkers.prospect.countdays.WorkTypeCountProspectorBase;
 import nts.uk.ctx.at.record.dom.remainingnumber.annualleave.export.GetAnnAndRsvRemNumWithinPeriod;
 import nts.uk.ctx.at.record.dom.remainingnumber.annualleave.export.GetAnnLeaRemNumWithinPeriodProc;
+import nts.uk.ctx.at.shared.dom.holidaymanagement.publicholiday.configuration.AggregationPeriod;
+import nts.uk.ctx.at.shared.dom.holidaymanagement.publicholiday.configuration.PublicHolidayManagementUsageUnit;
+import nts.uk.ctx.at.shared.dom.holidaymanagement.publicholiday.export.query.publicholiday.GetRemainingNumberPublicHolidayService;
+import nts.uk.ctx.at.shared.dom.holidaymanagement.publicholiday.export.query.publicholiday.param.AggrResultOfPublicHoliday;
 import nts.uk.ctx.at.shared.dom.remainingnumber.annualleave.export.InterimRemainMngMode;
+import nts.uk.ctx.at.shared.dom.workrule.closure.ClosureId;
 
 
 /**
@@ -62,20 +69,16 @@ public enum ConditionValueProspectMonthlyByEmployee implements ConditionValueLog
         return withinWorkTimeAmount + totalAmount;
     })),
     所定公休日数比(2, "対比：所定公休日数比", c -> {
-        Double numerator = c.aggregate.aggregate(c.require,
-                data -> {
-                    // TODO: 保留
-                    // 実際の公休日数
-                    //return data.getPublicHolidayLeaveRemain().get().getPublicHolidayday().v();
-                    return null;
-                });
-        Double denominator = c.aggregate.aggregate(c.require,
-                date -> {
-                    // 所定公休日数
-                    // TODO: 保留
-                    return null;
-                });
-        return numerator / denominator * 100;
+        Double numerator = getPublichHolidays(c);
+        PublicHolidayManagementUsageUnit unit = c.require.getPublicHolidayManagementUsageUnit(c.getCompanyId());
+        AggregationPeriod period = new AggregationPeriod(c.closureMonth.getYearMonth(), c.closureMonth.defaultPeriod());
+        Double denominator = unit.GetNumberofPublicHoliday(c.require, c.getCompanyId(), c.getCompanyId(), Arrays.asList(period), period.getPeriod().end())
+                .stream().filter(numberOfPublic -> numberOfPublic.createYearMonth().equals(c.closureMonth.getYearMonth()))
+                .findFirst().map(numberOfPublic -> numberOfPublic.getInLegalHoliday().v()).orElse(0.0);
+        // 所定公休日数の設定が無い場合、0が返ってくるので、その場合はアラームチェックをしない
+        return denominator > 0.0 
+                ? numerator / denominator * 100
+                : null;
     }),
     基準時間比_通常勤務(3, "対比：基準時間比（通常勤務）", c -> {
         // 労働制を取得（月中で労働制変わってた場合は考慮していない（終了日基準））
@@ -186,53 +189,52 @@ public enum ConditionValueProspectMonthlyByEmployee implements ConditionValueLog
     })),
 
     出勤日数(3, "日数：出勤日数", c ->  {
-        AttendanceDaysProspector prospector = new AttendanceDaysProspector(c.require, c.getCompanyId(), c.aggregate);
+        val prospector = new AttendanceDaysProspector(c.require, c.getCompanyId(), c.aggregate);
         return prospector.prospect(c.require, c.getCompanyId(), c.getEmployeeId());
     }),
     
     休日日数(3, "日数：休日日数", c-> {
-        HolidaysProspector prospector = new HolidaysProspector(c.require, c.getCompanyId(), c.aggregate);
+        val prospector = new HolidaysProspector(c.require, c.getCompanyId(), c.aggregate);
         return prospector.prospect(c.require, c.getCompanyId(), c.getEmployeeId());
     }),
     
     休出日数(3, "日数：休出日数", c-> {
-        HolidayWorkDaysProspector prospector = new HolidayWorkDaysProspector(c.require, c.getCompanyId(), c.aggregate);
+        val prospector = new HolidayWorkDaysProspector(c.require, c.getCompanyId(), c.aggregate);
         return prospector.prospect(c.require, c.getCompanyId(), c.getEmployeeId());
     }),
     
-    公休日数(3, "日数：公休日数", c-> {
-        
-        return 0.0;
-    }),
+    公休日数(3, "日数：公休日数", c-> getPublichHolidays(c)),
     
     特休日数合計(3, "日数：特休日数合計", c-> {
-        SpecialVacationDaysProspector prospector = new SpecialVacationDaysProspector(c.require, c.getCompanyId(), c.aggregate);
+        val prospector = new SpecialVacationDaysProspector(c.require, c.getCompanyId(), c.aggregate);
         return prospector.prospect(c.require, c.getCompanyId(), c.getEmployeeId());
     }),
     
     欠勤日数合計(3, "日数：欠勤日数合計", c-> {
-        AbsenceDaysProspector prospector = new AbsenceDaysProspector(c.require, c.getCompanyId(), c.aggregate);
+        val prospector = new AbsenceDaysProspector(c.require, c.getCompanyId(), c.aggregate);
         return prospector.prospect(c.require, c.getCompanyId(), c.getEmployeeId());
     }),
     
     年休使用数(3, "日数：年休使用数", c-> {
+        DatePeriod period = c.closureMonth.defaultPeriod();
         val aggrResult = GetAnnLeaRemNumWithinPeriodProc.algorithm(
-                c.require, new CacheCarrier(), c.getCompanyId(), c.getEmployeeId(), null/*期間*/, InterimRemainMngMode.OTHER,
-                null/*基準日*/, false/*未使用*/, Optional.empty()/*上書きフラグ*/, Optional.empty()/*上書き用の暫定年休管理データ*/, Optional.empty()/*前回の年休の集計結果*/,
+                c.require, new CacheCarrier(), c.getCompanyId(), c.getEmployeeId(), period/*期間*/, InterimRemainMngMode.OTHER,
+                period.end()/*基準日*/, false/*未使用*/, Optional.empty()/*上書きフラグ*/, Optional.empty()/*上書き用の暫定年休管理データ*/, Optional.empty()/*前回の年休の集計結果*/,
                 Optional.empty()/*過去月集計モード*/, Optional.empty()/*年月※過去月集計モード  = true の場合は必須*/, Optional.empty()/*上書き対象期間*/);
         return aggrResult.flatMap(r -> r.getAsOfPeriodEnd().getRemainingNumber().getAnnualLeaveWithMinus().getUsedNumberInfo().getUsedNumber().getUsedDays().map(day -> day.v())).orElse(null);
     }),
     
     積立年休使用数(3, "日数：積立年休使用数", c-> {
+        DatePeriod period = c.closureMonth.defaultPeriod();
         val aggrResult = GetAnnAndRsvRemNumWithinPeriod.algorithm(
-                c.require, new CacheCarrier(), c.getCompanyId(), c.getEmployeeId(), null/*期間*/, InterimRemainMngMode.OTHER, 
-                null/*基準日*/, true/*未使用*/, true/*未使用*/, null/*上書きフラグ*/, null/*上書き用の暫定年休管理データ*/, null/*上書き用の暫定積休管理データ*/, 
-                null/*不足分付与残数データ出力区分*/, null/*集計開始日を締め開始日とする　（締め開始日を確認しない）*/, null/*前回の年休の集計結果*/, null/*前回の積立年休の集計結果*/, null/*上書き対象期間*/);
-        return 0.0;
+                c.require, new CacheCarrier(), c.getCompanyId(), c.getEmployeeId(), period/*期間*/, InterimRemainMngMode.OTHER, 
+                period.end()/*基準日*/, true/*未使用*/, true/*未使用*/, Optional.empty()/*上書きフラグ*/, Optional.empty()/*上書き用の暫定年休管理データ*/, Optional.empty()/*上書き用の暫定積休管理データ*/, 
+                Optional.empty()/*不足分付与残数データ出力区分*/, Optional.of(true)/*集計開始日を締め開始日とする　（締め開始日を確認しない）*/, Optional.empty()/*前回の年休の集計結果*/, Optional.empty()/*前回の積立年休の集計結果*/, Optional.empty()/*上書き対象期間*/);
+        return aggrResult.getReserveLeave().map(r -> r.getAsOfPeriodEnd().getRemainingNumber().getReserveLeaveWithMinus().getUsedNumber().getUsedDays().v()).orElse(null);
     }),
     
     勤務日数(3, "日数：予定勤務日数＋勤務日数", c-> {
-        WorkDaysProspector prospector = new WorkDaysProspector(c.require, c.getCompanyId(), c.aggregate);
+        val prospector = new WorkDaysProspector(c.require, c.getCompanyId(), c.aggregate);
         return prospector.prospect(c.require, c.getCompanyId(), c.getEmployeeId());
     }),
     ;
@@ -250,6 +252,18 @@ public enum ConditionValueProspectMonthlyByEmployee implements ConditionValueLog
         return getValue.apply(context);
     }
 
+    /**
+     * 対比：所定公休日数比
+     * 日数：公休日数
+     * で使う　公休日数を取得する処
+     */
+    private static double getPublichHolidays(Context context) {
+        AggrResultOfPublicHoliday aggrResult = GetRemainingNumberPublicHolidayService.getPublicHolidayRemNumWithinPeriod(
+                context.getCompanyId(), context.getEmployeeId(), Arrays.asList(context.closureMonth.getYearMonth()), context.closureMonth.defaultPeriod().end(), InterimRemainMngMode.OTHER, 
+                Optional.empty(), new ArrayList<>(), Optional.empty(), Optional.empty(), new CacheCarrier(), context.require);
+        return aggrResult.createPublicHolidayRemainData(context.getEmployeeId(), context.closureMonth.getYearMonth(), ClosureId.valueOf(context.closureMonth.closureId()), context.closureMonth.closureDate())
+                .getPublicHolidayday().v();
+    }
 
     public interface Require extends
             AggregateIntegrationOfDaily.AggregationRequire,
@@ -260,13 +274,16 @@ public enum ConditionValueProspectMonthlyByEmployee implements ConditionValueLog
             SpecialVacationDaysProspector.Require, 
             AbsenceDaysProspector.Require,
             WorkDaysProspector.Require,
+            GetRemainingNumberPublicHolidayService.RequireM1,
             GetAnnLeaRemNumWithinPeriodProc.RequireM3,
-            GetAnnAndRsvRemNumWithinPeriod.RequireM2 {
+            GetAnnAndRsvRemNumWithinPeriod.RequireM2,
+            PublicHolidayManagementUsageUnit.RequireM1{
 
         List<WorkingConditionItemWithPeriod> getWorkingConditions(String employeeId, GeneralDate baseDate);
         Optional<BsEmploymentHistoryImport> employmentHistory(CacheCarrier cacheCarrier, String companyID, String employeeId, GeneralDate baseDate);
         RecordDomRequireService requireService();
         String getCompanyId();
+        PublicHolidayManagementUsageUnit getPublicHolidayManagementUsageUnit(String companyId);
     }
 
     @Value
